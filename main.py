c87aa9c38bbfa47a4944779c832740998a69dbed
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:15:32 +0000

    worried load play less bread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
