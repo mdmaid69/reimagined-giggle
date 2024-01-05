36095c05446f78d8c55311fa58c453eaded9548f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:20:28 +0000

    mend convince that drawing blue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_priority_queue():
+        return collections.deque()
