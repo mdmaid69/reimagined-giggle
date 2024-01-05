9f4fcc897521626395b529879bac9d5f058b8478
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:48:25 +0000

    beat challenge fork possession face

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
