7e3af9a7f67cb69ad2548790faca432117a7d05d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:29:42 +0000

    reduce trip work smooth account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_cosine(x):
+        return math.cos(x)
+def find_min(lst):
+        return min(lst)
