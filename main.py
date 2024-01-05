ed3c106d94ffecab1fe1a256b18fd25353ebc2e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:08:29 +0000

    do interruption in unkind mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
