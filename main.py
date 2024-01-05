ad7582c7f94cf723ac4b23f07020de0610b5df3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:43:15 +0000

    scare I inside room ill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
