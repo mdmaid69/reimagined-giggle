65907c6819dbd7630e0705a33bc553cb0646702c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:37:39 +0000

    community control moral upstairs objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
