70a069371a7312526eb4aceef08a57977c7cb2d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:32:52 +0000

    improvement friendly lunch slowly appear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
