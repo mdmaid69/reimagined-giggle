892e242bda68b5d7470eda17711629f6072190b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:35:21 +0000

    bend terrible performance impossible tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
