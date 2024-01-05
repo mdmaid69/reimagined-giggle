1565bdf6209fe967bf8681d84ff9ea75837fa28f
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:23:19 +0000

    schedule busy bite relation activity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
