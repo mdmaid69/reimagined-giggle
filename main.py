7cdade1d14dc18cbcae7da62553db18be195ac71
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:35:50 +0000

    offer loan please crash correct

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
