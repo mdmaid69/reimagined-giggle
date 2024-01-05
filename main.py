e4d9ddd53a6434027a5e26333f2719649c9e7a02
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:29:05 +0000

    defend company doctor certain fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
