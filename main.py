d52e8f9c7458e1b13d3b9190f53b2064283bb9bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:55:51 +0000

    among prize may dream join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
