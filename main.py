5d5565500ebe52a306323db7cbb844d8510c9e06
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:43:27 +0000

    relief holdout care bit example

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def is_even(n):
-        return n % 2 == 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def convert_to_octal(n):
+        return oct(n)
