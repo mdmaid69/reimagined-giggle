bb2ddd28c8aea0b8a96f4d2b6092f6863efb85cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:11:58 +0000

    occasional certainly burst shape authority

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def print_python_version():
-        return sys.version
-import sys
-def exit_program():
-        sys.exit()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
