ef3e2984d38a401c0f112ffbca4218169a13d619
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:43:04 +0000

    least cold mean quick wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def append_to_array(array, item):
+        array.append(item)
