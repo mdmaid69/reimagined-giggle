9a807a14d248b34d97432ca655efd8a0a8e0c421
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:34 +0000

    increase sensible otherwise chase travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
