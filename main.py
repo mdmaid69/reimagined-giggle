b8b7a88e946bbf9f8171ff08ba4f653636084556
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:20:00 +0000

    over open urgent travel very

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_characters(sentence):
+        return len(sentence)
