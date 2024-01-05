a0ab35c993377b4be4b6d3234b243f1bb52dc7c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:52:11 +0000

    carry lift hospital mine side

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def count_elements(lst):
+        return len(lst)
