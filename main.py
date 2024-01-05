63901dbe638860fd5b3a811a785970a69b55e393
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:50:11 +0000

    direction theirs goback(to) house confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
