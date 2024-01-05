18ac7ab9a424e05fd9cfb477638f3626d4bd1ed9
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:09:05 +0000

    justice excuse put innocent what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import sys
-  def get_python_version():
-        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
