89f94008ad9cce23b546824cbbceeeae1ca078c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:35:35 +0000

    wrong turnback bag private mistake

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
