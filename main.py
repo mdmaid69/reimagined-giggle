4cff18f324838f6847783ab2c343f7613d1c2efc
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:03:17 +0000

    concerning rush mistake that side

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_volume(length, width, height):
-        return length * width * height
+import os
+print(os.getcwd())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
