f62e88a9f8f16a2550b420ddcfff4ec715c76821
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:41:04 +0000

    argument sport Hh goup progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
