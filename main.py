7a9afa559a2052fee1342b7ff62860f579c3685a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:18:57 +0000

    watch waste burst breakin weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
