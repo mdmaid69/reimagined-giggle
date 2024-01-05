78383ec5c080fcdcd095dc7d4d7ec56adc161408
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:47 +0000

    chance whoever special disappointed tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
