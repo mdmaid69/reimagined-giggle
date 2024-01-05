18f15bf16a5e881717a3e5b2d2ae8a3f6a3edcb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:25:33 +0000

    answer roof hate unfair unit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def subtract_numbers(x, y):
+        return x - y
