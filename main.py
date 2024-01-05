33652f4263b5ab406a0772901d6f81a15c071cf1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:10:24 +0000

    bored below circle heavy pinch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
