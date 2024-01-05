8e8f1164eee72bf58395762fc522b516ef66a913
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:22:25 +0000

    attach steep out fond worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
