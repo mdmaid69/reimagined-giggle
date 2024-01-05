14843cc5b40e9870d718dd2302338aa9ad3bf8b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:12:19 +0000

    speak back criticize statement better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
