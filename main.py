10f5b109471f984f0b265b85865e0595556ac319
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:37:38 +0000

    nose listenfor science level custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
