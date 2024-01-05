85e5ae23ed34e2a0310137422a17cbcf7c9bf380
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:46:49 +0000

    convince wild crowded suit dangerous

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
