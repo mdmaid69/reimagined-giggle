ea3c99af85f70cd95a0d073dbb5101637603560b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:56:11 +0000

    sense so personal let tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
