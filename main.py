eb47434c3f01fc3c7d0e6a05df32d35e1063a5ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:01:33 +0000

    common price comeup against wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
