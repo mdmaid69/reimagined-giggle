faaea79cd41a968572178b6663c4c9fa3334277e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:26:35 +0000

    slightly accidental geton ago doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
