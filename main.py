aac51a144edbc973a5f8ed23e7df217466e401d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:38:32 +0000

    escape comment close concerned welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
