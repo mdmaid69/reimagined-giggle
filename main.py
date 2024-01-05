b90c8f4f7b587f1d38e32deeb9ada8885eefac9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:44:53 +0000

    born call ahead low pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
