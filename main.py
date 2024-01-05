83cf1648c13845ff2dc77255f6194f12545917ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:19:38 +0000

    powerful habit either relative Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
