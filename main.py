03e0353965e308973139a56d07161bab9a3f86e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:25:00 +0000

    fashion but area deliver turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
