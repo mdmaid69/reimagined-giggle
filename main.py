aacc54e08d63bc7a2f0930c5e990bd07f824b80e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:30:48 +0000

    570enjoyable impossible order usual whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
