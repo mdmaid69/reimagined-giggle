9a5be61d6180931434c65bb9c7e4223e299bd116
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:07:49 +0000

    shout formal hang squash list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_acceleration(speed, time):
+        return speed / time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
