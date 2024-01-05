3283814a2df91b9185c0bc8d838f1ebc41d4de23
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:04:09 +0000

    comeover especially violent discuss set

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
