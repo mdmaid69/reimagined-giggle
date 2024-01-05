c3979778599161ac846d062974e635bcf602d9dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:20:28 +0000

    sky somebody common patience complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
