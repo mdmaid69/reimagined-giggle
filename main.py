f648ff7bf8055c7a60ac243944434a572fe0a469
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:35:54 +0000

    round table stamp cry target

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_work(force, distance):
+        return force * distance
