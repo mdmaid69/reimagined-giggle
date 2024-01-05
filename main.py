f3e71d06cfc65ca772c0b2d9ca24cb09a6e2c790
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:11:36 +0000

    again become comein though happen

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
