662783cc0bc5632557f9d7e8a98e6e792146543e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:46:34 +0000

    judge exciting small comein Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
