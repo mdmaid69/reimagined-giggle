7646b5da620c409b34f904fc04265a08f3a5ae44
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:40:00 +0000

    near scale off life sweet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
