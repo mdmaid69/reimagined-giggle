8e8ca53654aaa005b1b1f545eed08687e89d5100
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:39:45 +0000

    muscle impression common mad innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
