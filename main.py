ada452337a4079c13ba5de5f2b73468505175b7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:19:35 +0000

    else object twist crisis properly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def find_max(numbers):
+        return max(numbers)
