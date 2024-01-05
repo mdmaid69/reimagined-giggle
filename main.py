f612f30eec82e6ae3fe28f00beadc34ccf6c21a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:34:51 +0000

    fire naturally spill scrap comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_as_list(array):
+        return list(array)
