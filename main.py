14cf1ffcec2924d669bb60a50301ba8e434a5759
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:49:27 +0000

    push clean deep desperate handle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
