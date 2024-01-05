0e9478d6a2eddac46d71bc606149bed1bffc3e45
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:41:18 +0000

    cost slightly workon impatient heavy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
