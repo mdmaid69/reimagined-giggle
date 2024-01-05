e962f90708fc8c2e11a0f67f12de8ae1e30225c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:15:44 +0000

    takeback chief serve 572enter skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
