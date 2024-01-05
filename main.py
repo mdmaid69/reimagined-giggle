a3c76f69043c56a622e5c46931944fe0592f2687
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:51:10 +0000

    rather herself steal foot kick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
