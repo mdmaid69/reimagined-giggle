d4daa9943d9ffb7d59c51fc5b50eb2ca728ce742
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:13:24 +0000

    idea cook sorry fair steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
