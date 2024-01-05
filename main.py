58ba28624f87a8715595f03acc5d5cb8a19adaa3
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:26:45 +0000

    embarrassed circle square when off

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
