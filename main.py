7b0b767c6f9584ecb421c1fba1eae0c490da858b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:12:49 +0000

    rub show manner union mysterious

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
