34098933571d22650fb6a4c54220eba0803181f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:57:40 +0000

    preparation advantage trap certain have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
