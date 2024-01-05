9d4fa81443ce80e28b572f9745d3d1c8e833ed8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:03:08 +0000

    translate pupil 572enter instrument earth

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+def get_today_date():
+        return datetime.date.today()
