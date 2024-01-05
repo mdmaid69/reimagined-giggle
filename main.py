d171afb11574b647871dd8b376202309be52c4db
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:33:00 +0000

    refuse wipe guess certainly stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
