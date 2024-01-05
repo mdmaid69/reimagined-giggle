bcfae15a9bf97cc678517a35b0dbb2328a19ced5
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:40:36 +0000

    legal announce learn consist house

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def count_characters(sentence):
-        return len(sentence)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
