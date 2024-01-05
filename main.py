87d064bab60af84879f870a9a343af83b790b6d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:27:26 +0000

    small custom soak force burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
