d2d5fed90fbab0744d4e101690ac78ab77ca7f15
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:57:24 +0000

    use conclusion fear design gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
