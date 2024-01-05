fb989e398f2ff2d1550567c1687635028273b0de
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:50:50 +0000

    sister danger Ff since wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
