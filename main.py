73ec3964852ec9ca636fd1e69480c912c4a261e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:39:52 +0000

    inside squeeze guess tool his

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def find_max(lst):
+        return max(lst)
