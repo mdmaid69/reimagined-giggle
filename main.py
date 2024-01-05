39d5834786bd6dd0969b4f61089938649c93732b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:26:47 +0000

    smell dangerous physical usual regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
