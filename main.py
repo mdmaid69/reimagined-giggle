f46bcc6cdd9338e690b84da0e15367b91f0186a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:09:43 +0000

    treatment battery see demand special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
