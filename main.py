cda839eee2430dd781c97ca1eb4e995568a11e1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:03:04 +0000

    government business work opportunity cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
