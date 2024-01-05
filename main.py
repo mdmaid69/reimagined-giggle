31e3dc59f3352e4081ef97caa62bd18b88d09b65
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:24:25 +0000

    share 566end bother autumn middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
