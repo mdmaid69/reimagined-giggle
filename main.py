a75b0cb36ea024f38ee33e6c9d44d14a508e0dcf
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:04:55 +0000

    interrupt pressure interview weekend brush

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def square_number(x):
+        return x**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
