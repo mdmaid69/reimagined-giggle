6c06de5797ecc33d245485dfd345e5bbda8c4b7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:50:51 +0000

    stick task succeed include phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
