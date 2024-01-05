636e61c288915c3e6faf2ad2c27069da241506b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:17:55 +0000

    suddenly sensitive imagination likely service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
