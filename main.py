3639264d33dd63d4e979bfcc7ab676c99423ae71
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:43:27 +0000

    share burn seem especially Zz

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
