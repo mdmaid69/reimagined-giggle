e885abc7f4ca36e6b6edc51fd8005bce9980c4e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:24 +0000

    it will priority comfortable chin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def cube_number(x):
-        return x**3
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
