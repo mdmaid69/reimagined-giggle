23b42bf02fe13145013ed247d53ba8b0cac4f9c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:46 +0000

    judge tap proof quarter tune

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
