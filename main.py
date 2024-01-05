b015ebaadb52634361576e1656ff127cdbb66bed
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:05:42 +0000

    hit conduct advice accordingto vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
