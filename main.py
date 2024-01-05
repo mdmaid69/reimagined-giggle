ba70175cc0b3750a1e41b55c3e55be9ed6a14d4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:28:08 +0000

    guilty strike contact hold importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
