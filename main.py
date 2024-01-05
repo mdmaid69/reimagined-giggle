696767326890df85f445d175da8868ce4dfded24
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:20:32 +0000

    sharp warning boring wild weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+import os
+print(os.getcwd())
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
