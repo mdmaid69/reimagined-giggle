462d25977beb2a442a30fd0b10f3651417e3f7ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:00:48 +0000

    hello introduce shop value mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
