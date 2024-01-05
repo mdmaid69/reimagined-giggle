b9c2ef975aa097c7ea592dcf7eb692875cf13229
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:15:27 +0000

    trouble stamp dish news emergency

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
