762974def37759fc69a806051de4300c13a23c33
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:02:07 +0000

    special somebody contact silent time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
