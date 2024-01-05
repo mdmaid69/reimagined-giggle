11a760d8bf1724c1afe445f04d2a4cf2f9541019
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:10 +0000

    sad closed creature possibly embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
