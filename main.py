dd9d86ece38e3413e917939c8f2897a64479a516
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:57:16 +0000

    smoke equal fold hardly information

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
