d736358699442218d27ef752b934b5b41c5f930d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:49:00 +0000

    condition cross bend interesting share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import tensorflow as tf
-print(tf.__version__)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
