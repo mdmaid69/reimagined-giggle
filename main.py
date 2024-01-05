4bf11a916ec72ff54680a9c2a2de26f71b7e5fed
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:27:43 +0000

    news journey rough give breakinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
