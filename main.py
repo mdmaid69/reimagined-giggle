ba134068de5b135ab9d7ed207d6308173db8d545
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:59:28 +0000

    likely honest disapprove glass downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
