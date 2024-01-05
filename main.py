8b7aa8bb3c0cfa68ffbb254f42cb4c8597dc5a26
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:26:40 +0000

    raw note owe surprised mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
