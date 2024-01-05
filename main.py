4fda399fa6ef3f740c2ce98bb92f6c1d5e5528bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:31:05 +0000

    immediately lift experience plan challenge

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
