f9181bf0883bdf431a23d7cd6099be605f193e4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:20:16 +0000

    ought silent why ought we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-print(os.getcwd())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
