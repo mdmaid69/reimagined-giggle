53e7cb7bf02987c1b45d3dfcd2f02401a2d67f3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:07:55 +0000

    except considering file comment type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
