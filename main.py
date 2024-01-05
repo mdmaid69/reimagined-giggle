af3b859a9eea07593ccdff371889ab5b1ad11493
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:00:58 +0000

    clear talkoutof frame mind choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
