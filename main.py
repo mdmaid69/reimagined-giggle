30fac5500308cce39cc1c7d8275e131cdf3674d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:41:46 +0000

    stop something charge bath tell

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
