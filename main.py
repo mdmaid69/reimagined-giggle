c9f0f210181957b73a3705d3191b9e1c4db0245e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:44:47 +0000

    cost state couple accommodation arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_str(array):
-        return str(array)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
