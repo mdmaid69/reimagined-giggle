026f86923dca891a8efe272691118d7af7c3f1e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:58:26 +0000

    switch pleasure anxious may safety

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def convert_to_binary(n):
+        return bin(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
