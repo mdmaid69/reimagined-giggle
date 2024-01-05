de63d064c898c31ac3fa4fc632558380ce6ae5c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:57:29 +0000

    opposite claim any appear nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
