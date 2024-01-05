cfa65a2dc73245869231212e70eb4caeaae1fd16
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:16:54 +0000

    number sound hope hollow look

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
