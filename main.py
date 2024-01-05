987e9adb3e811e3232ace9718087b211a3752c06
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:09:56 +0000

    message splash push same exam

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
