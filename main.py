c2b1a11207ab3631b9018bdf4677f0e25d758a07
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:41:39 +0000

    cry clean goout trap basic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
