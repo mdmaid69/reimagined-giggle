d773f711a4e48d93a6c8df0f256a2ea4f3633a9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:26:49 +0000

    communicate confused lonely nor rain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def count_words(sentence):
+        return len(sentence.split())
