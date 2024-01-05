bd19cb90cac3e9fec19dd38a807864c4034d268e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:00:45 +0000

    remark tool hand accidental regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
