2007a3edf66ffdbcdf1d704284d0bea063d62522
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:22:21 +0000

    hope goup waste drink free

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
