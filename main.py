b6a8b1d3c29fc043abc76b4d6968cfa6097e3d51
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:25:03 +0000

    deal communication path tidy punishment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+for i in range(10): print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
