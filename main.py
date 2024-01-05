e57c8d99d7c68eb85ed6575bf2b970cbe9fc098f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:42:38 +0000

    threaten how top lose get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
