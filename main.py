68a22459d6a7e82d013af3988a904fe05e04fcd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:46:57 +0000

    laugh without scared lift desperate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
