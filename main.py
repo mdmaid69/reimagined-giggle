fabdebcf999c6d9e830faed99dc9d7b73776a203
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:48:53 +0000

    surprising communicate relative treat beyond

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
