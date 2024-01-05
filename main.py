626a1716f2e7c4b16ff93c2e16de6408e89ad544
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:20:53 +0000

    bite hate old lock suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
