0c0937b01c53ced8d4c67e8f2e9bf7bdc0ebf476
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:13:36 +0000

    level record night store eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
