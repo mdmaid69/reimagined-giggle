3f1d31b26c2aaeb07428aaa6a22042a9ba932a70
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:39:41 +0000

    callback I seem middle least

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
