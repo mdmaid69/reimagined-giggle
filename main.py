1d2b8824bdcb89e50d3ab312b2ff310e678208b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:58:37 +0000

    female flood lookup disappear seem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
