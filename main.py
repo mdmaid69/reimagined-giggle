cd8d80685b6a2ba2e76c1e90e25f1758330c6bfe
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:57:46 +0000

    workon reduce achievement stage meeting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
