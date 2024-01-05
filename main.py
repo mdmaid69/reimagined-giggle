b795207b1a2cba09e1334c5a5d3a22fd6b6aaf20
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:19:26 +0000

    exam let safety I let

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_area_rectangle(l, w):
-        return l * w
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
