7917d71ccb0049481093ab1d13abf8934aa816c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:56 +0000

    obtain put argue improve forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
