e9dc7f86a0498c7d547ce5321aeccc375b0a97b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:03:14 +0000

    borrow goback(to) scream brother emotion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
