6bfee3fec50e43964e4920be1707f1ef79f6c27f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:19:27 +0000

    force impossible relative relax once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
