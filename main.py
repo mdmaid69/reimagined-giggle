02ed9459b29621c9e6f82866b66c0204b4b86ba1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:37:09 +0000

    post hide direct job stiff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
