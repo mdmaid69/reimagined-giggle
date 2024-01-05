c2f8fba92b095cb24fbadcafc48df717dce5d014
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:24:27 +0000

    lookup used thinkover fold convincing

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
