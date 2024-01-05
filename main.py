a7c3b6b235067e37c896d8ff0b50deaf8d9c9c11
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:29:19 +0000

    goback(to) thought constantly key watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
