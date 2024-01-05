abd9150980af6197fda5d2eaa8f4b6be3a85e121
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:29:41 +0000

    buy among shame laugh guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def find_min(lst):
+        return min(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
