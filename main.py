e16d6aca9e48f22f3d4d16dea88ec546901bfbd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:08:19 +0000

    realize scene unless strict abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
