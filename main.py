e452d2d3df71d8b34fe624c64ed7b763c29d6f8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:16:41 +0000

    twist game shallow suit border

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
