8791b91d5c8db3e0414a42777478c23e47b000e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:14:37 +0000

    lookup risk disagree bringback vanish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
