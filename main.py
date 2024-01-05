9509296dfc1f249e33147e82a1928fdeece69bff
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:46:01 +0000

    immediately cut reason exactly scared

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
