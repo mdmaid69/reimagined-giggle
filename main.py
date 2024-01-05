e96b730d118a1479909cd8666afacf84a33cf945
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:42:03 +0000

    protection keepfrom make surface unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
