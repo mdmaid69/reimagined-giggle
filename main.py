2ecde73560909259dcf987ae6155713ab1fd5402
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:31:52 +0000

    habit mark soak getaway out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
