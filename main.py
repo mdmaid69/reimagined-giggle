fe7cdbc5bb2dc58f96b5929408d55fd9aa6d8474
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:21:08 +0000

    test familiar wave trade interesting

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
