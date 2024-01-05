9ac4c0470b17effa600693764f105e994283b771
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:33:27 +0000

    her spray while escape forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
