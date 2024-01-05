68fae1279ad476abf6575e20e944fc2dfe5c2eba
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:58:17 +0000

    gate yesterday heart say pick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
