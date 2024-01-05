29fe7f8a4e0452fd622128e4be7aeaef9e07eec6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:37 +0000

    mouth catch Hh rarely shine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_list(array):
+        return list(array)
+print("Hello, world!")
