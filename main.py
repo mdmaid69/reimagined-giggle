29462fca699c86a9985c94e9664d35a3162f2ab8
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:48:30 +0000

    tape run range expression pupil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_pressure(force, area):
-        return force / area
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
