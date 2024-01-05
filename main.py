b69d1879985a7565514f91bb9fa035e7f6a65593
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:00:45 +0000

    comfort stamp breathe shock border

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(10): print(i)
