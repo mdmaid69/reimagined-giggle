3f89ffd762b737efebd91709bb20743cad82e37f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:28:32 +0000

    sense too goodbye broad press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def count_characters(sentence):
+        return len(sentence)
+import sys
+def print_python_version():
+        print(sys.version)
