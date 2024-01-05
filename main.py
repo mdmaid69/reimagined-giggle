b8b0de61edb0d024158c96f1c2f9e5aea57764d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:33:58 +0000

    even main remove rare shelter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
