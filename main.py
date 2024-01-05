796dfc35a7f0ea8d6f672171b0e3125cf36a70c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:42:22 +0000

    book Itself service long weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
