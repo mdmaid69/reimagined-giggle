bf10b7f720f37782d8097d147da3a78d94bffe79
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:20:57 +0000

    power illness bottom want deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
