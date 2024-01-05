65ff910c7463170f48dded5b946fdf1f4c23dfeb
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:06:03 +0000

    leg prove shout century keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import collections
+def create_counter():
+        return collections.Counter()
