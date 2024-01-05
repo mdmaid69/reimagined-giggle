4365886dd0f9a4edecf30978a58651a1f122958f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:54:55 +0000

    lend heart reason recover moment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def reverse_string(s):
+        return s[::-1]
+import datetime
+def get_current_date():
+        return datetime.date.today()
