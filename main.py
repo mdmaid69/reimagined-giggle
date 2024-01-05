c2d944d70be78a1f6bd09541141e26833831691b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:53 +0000

    influence Ll further complete design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
