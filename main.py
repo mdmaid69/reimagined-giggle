77011a3272114c41301a421a320eda811ea433e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:21:16 +0000

    university power burn 571enough belief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
