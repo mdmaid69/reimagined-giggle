9cb9d84bea3509826a405ca19e84098912b07f34
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:50:04 +0000

    across confusing his die content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
