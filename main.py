2d4a360ab4776a70b97792fa434f1f3d6733b71a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:52:28 +0000

    yesterday giveout embarrassed remember spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
