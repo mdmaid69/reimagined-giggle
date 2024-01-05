19b6a06b42003524762cb80a9a4903cbc81249f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:11:52 +0000

    proud suddenly die shop tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
