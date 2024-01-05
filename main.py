868053887b10ced4aaf0450b9c50ba8b2568b71c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:44:44 +0000

    sign straight bag tour great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
