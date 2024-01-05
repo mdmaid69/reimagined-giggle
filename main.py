823374218472149ef4c4b1b01292e360eb3fbfd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:31:03 +0000

    war relative dry pleasant negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
