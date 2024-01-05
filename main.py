b35a287dd453b759c1c0f8249effef81c0f5c374
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:24:33 +0000

    combination get right wide calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_work(force, distance):
-        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
