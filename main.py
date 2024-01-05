7c4ab0532ff7bac7caf4297ddfd332763eb1cf62
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:23:40 +0000

    direct cause calmly level takeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
