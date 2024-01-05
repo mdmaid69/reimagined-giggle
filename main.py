0b8cd995ca4046dbbd0991a2776104b0d39efa54
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:34:52 +0000

    though cruelty Kk sense seriously

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
