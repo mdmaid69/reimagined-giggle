af9c044c0445eb318c2ad2a98b0448cbd88cb4e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:25:37 +0000

    suddenly surprising earn tense may

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+print("Hello, world!")
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
