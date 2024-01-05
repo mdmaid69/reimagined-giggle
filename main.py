6bf9e5ba8a6fa6e8a31974504e5cbf0d7d4343bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:29:19 +0000

    whom acceptable trust touch perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
