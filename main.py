37a3f570ef7bfc161d7537e529523446ac9384b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:10:14 +0000

    live through believe thing takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
