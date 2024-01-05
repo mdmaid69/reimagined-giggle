eefbe9dea75adf01b20b94164f308a9ce4a71c39
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:16:20 +0000

    close pocket flat need punish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def reverse_string(s):
-        return s[::-1]
+print(sum(range(10)))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
