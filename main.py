402aec7acca237e194ef4437fef5e60ab689d0c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:56 +0000

    disappear intend pen private again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
