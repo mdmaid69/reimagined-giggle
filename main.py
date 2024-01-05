ed52ce4f122a5ba2b15ba73a82cf479d95e3b811
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:19:17 +0000

    geton workout press peace appeal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def find_max(numbers):
-        return max(numbers)
+import array
+def clear_array(array):
+        array *= 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
