daa95068e5fc2784a8c6459066d384b56c962ede
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:13:58 +0000

    decide pollution leg instead right

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
