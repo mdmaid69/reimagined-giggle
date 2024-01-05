5c8af47fe97f2042743ee738069b15a1b0f298f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:21:29 +0000

    somebody long perfect hot teach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+def find_max(lst):
+        return max(lst)
+from collections import Counter
+print(Counter("hello world"))
