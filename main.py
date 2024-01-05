3c5f84ece07732ddbdb4a24e1fb2b272e6616f16
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:15:55 +0000

    to even trouble speed type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
