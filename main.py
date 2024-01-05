9580f6850ea835294745ed3dfb414744c758abca
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:55:20 +0000

    stir between raw dig complicated

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
