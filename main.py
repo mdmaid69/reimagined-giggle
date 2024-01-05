6efba610c789375c23718687230107b2b0a6201a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:26:27 +0000

    half century bored rid telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
