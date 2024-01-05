5cbdc12a744c0b17f7468b858d740497233927eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:22 +0000

    sure correction search matter question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
