76e7df26811bfb1cd9e2d8e4c5fb13c4079e82a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:15:01 +0000

    yesterday become where complain coat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_volume(length, width, height):
+        return length * width * height
