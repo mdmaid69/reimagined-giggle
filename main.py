c62e567273ec447efa835ced14637243e9eedd65
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:33:48 +0000

    otherwise leg harm him fashion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
