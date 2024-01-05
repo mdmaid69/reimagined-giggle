ff1e89129b4b298b09bd76149a803d51f0c207b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:15:58 +0000

    middle dig centre design increase

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-def count_characters(sentence):
-        return len(sentence)
-print([x**2 for x in range(10)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
