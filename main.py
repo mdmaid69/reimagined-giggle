53f58d9e6b2358e1f33f74d9318f03fbe136ee4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:37:32 +0000

    fortunate otherwise worried violence clever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
