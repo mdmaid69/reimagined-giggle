cde3faf8dbb1054636acceb407eb329175b030b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:46:25 +0000

    tread discuss well language present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import collections
+def create_counter():
+        return collections.Counter()
