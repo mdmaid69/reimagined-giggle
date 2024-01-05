c14f5feb344887dd1b44e5a357a255060fd5e8d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:30:47 +0000

    grand show trouble reduce loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
