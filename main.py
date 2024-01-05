4b5786253127eea7dfd50bea8b9829116b3571f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:25:22 +0000

    peace traffic stranger comeover phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
