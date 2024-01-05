7ba924692a145fa46ea3fed92256c74ffdc3225d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:55:26 +0000

    somebody everyone collection snow scared

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
