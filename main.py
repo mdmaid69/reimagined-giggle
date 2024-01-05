6817614ab10fdbf289a18dad49bf305093030bae
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:46:18 +0000

    doctor let extraordinary separate supply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
