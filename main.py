0404145a881cc0464a04cbdba2c103324d4a1866
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:01:37 +0000

    so burn close basically exciting

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+print([x**2 for x in range(10)])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
