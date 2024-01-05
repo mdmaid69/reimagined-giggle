3dd369d5e4969c394ccb18728cd4e9a579fe001f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:35 +0000

    car badly female block you

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
