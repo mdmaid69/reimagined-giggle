46ed814c76386d22afd57442eab020843147b584
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:27:09 +0000

    average else condition callup building

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+name = "Python"
+print("Hello,", name)
+  def remove_duplicates(lst):
+        return list(set(lst))
