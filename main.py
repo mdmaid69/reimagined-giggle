51cea581499134dd87ae00c496e668fb830e561b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:07:39 +0000

    punishment effect race stage immediate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_length(array):
+        return len(array)
