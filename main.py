33a5f7def84d540e0e1f77c796eb9d1347aa51d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:56:36 +0000

    block clean smart terrible theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
