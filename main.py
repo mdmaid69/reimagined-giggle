7f67cd54aca1d72c5c5cf3daead28bccde1a7422
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:50:26 +0000

    swimming aim period practice turnback

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_force(mass, acceleration):
+        return mass * acceleration
