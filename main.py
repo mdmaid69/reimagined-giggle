488d47b0255a048ac0ef3108739159d4ad78bfec
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:21:38 +0000

    carry plant sorry ride speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
