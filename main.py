068b80bac9256b9bc66a431762c76ab89524e5a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:57:31 +0000

    welcome kind dress join disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
