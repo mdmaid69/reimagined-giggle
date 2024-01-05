a9ee7af26aa4b3a3e4c43b656b3134a34fd04271
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:07:33 +0000

    not pour until plenty collection

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
