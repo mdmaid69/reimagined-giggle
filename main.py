c6153c8e5849fae4eb4a3dda0bf1b4ba8905e38d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:55:12 +0000

    report much get official conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
