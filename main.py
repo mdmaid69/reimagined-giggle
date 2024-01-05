036be8c41b80b6ea1982d92dfe7ca0c269836832
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:45:03 +0000

    nervous breakin feeling record reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
