31ea54452eb23689853a8291d63c9fad6142d79f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:29:23 +0000

    start inside Ee muscle thumb

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+name = "Python"
+print("Hello,", name)
