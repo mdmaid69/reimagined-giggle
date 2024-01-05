18c986959148ba8c35fef654d2e3160dd0ba0c2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:09:50 +0000

    herself skin strict head sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
