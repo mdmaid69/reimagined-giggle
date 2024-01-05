e465a783fcac1c8cb6013b829968152c01808bf9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:05 +0000

    service objection arrange miss mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_set(array):
+        return set(array)
