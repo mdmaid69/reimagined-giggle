ad2d8ff850d4c355e90cd2b32e775ebf77c7d0b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:44:26 +0000

    wise suggestion whistle hurt stop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
