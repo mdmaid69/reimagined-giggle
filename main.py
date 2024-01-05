9b62342ba38b9aa3c9027d19eb274dfc2ebafd77
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:46:45 +0000

    turnoff yes remark recognize minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
