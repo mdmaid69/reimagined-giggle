ec349093318ce10ae16aaff77d55ef7187fa201c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:13:27 +0000

    his mine situation for sensible

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
