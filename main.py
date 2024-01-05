9053539f5f2c8ff9b8311f770ecfe5297afb2608
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:24:51 +0000

    face crash adjective life lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
