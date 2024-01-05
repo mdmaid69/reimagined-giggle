e31109213084ad46627e839c882dff09663315b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:13:22 +0000

    mine style organization physical nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
