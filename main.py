6aa8a8f699f182f388590b13403e51d9b387134f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:19:00 +0000

    this owe wet change trust

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
