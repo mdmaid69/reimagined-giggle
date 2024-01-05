7f45d3787ec4ffb032f7c2fd5dbaede7d25e0f96
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:00:54 +0000

    lead best order calloff lie

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
