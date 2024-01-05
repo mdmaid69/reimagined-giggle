709e7b39466588cb087e7b2814cfda2a4fff1d62
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:33:40 +0000

    her out like noone so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_index(array, item):
+        return array.index(item)
