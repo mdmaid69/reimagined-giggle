bdb4b6127f689c05e5eba7dbe3c133d14d650d84
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:28:10 +0000

    century obvious rough second sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def sort_list(lst):
-        return sorted(lst)
+import logging
+def log_message(message):
+        logging.info(message)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
