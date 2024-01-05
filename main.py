3c7afd8bcca14369571cc3a99d36305e49bff178
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:06:10 +0000

    nigga argument ask description dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
