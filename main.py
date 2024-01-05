95a5d56465dc4d13c1da74173f410837bedae274
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:57:45 +0000

    most course card hard crowded

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
