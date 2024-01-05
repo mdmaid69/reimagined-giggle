7624d53c3b495b93446f095ac2e3ad0647e87e78
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:26:50 +0000

    wild guard drink objection trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
