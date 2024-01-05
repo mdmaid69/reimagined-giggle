f5f1ad1b84eda0c616f6eb437242c21287aaf806
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:48:26 +0000

    reserve warm themselves complete disapprove

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
