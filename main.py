81de32a406918e5234669e6e1502e80c9b7cbc3d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:24:44 +0000

    tap before fresh tell how

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_power(work, time):
-        return work / time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
