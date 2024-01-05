261f1072c319061a6296a8797113621e155b90fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:22:05 +0000

    tend begin effect angry bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_item(array, i):
+        return array[i]
