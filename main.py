fd59f76bb984760ed91ee7cf16c6dea9b46b312e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:45:15 +0000

    jump avoid comment scream space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
