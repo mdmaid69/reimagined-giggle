6ee6b505755dfab0421aeb61b2c477bc301e711a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:20:07 +0000

    flat this admire offer common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
