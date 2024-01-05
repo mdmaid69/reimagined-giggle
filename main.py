7e29153eabd99e1a9d6605c20ad8a5dae16a2fc3
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:05 +0000

    find drive error unite performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
