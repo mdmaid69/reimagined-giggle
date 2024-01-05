417bd251dd415021195a0c50e212e0361c2d025e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:00:47 +0000

    fasten build contain drive whom

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def exit_program():
+        sys.exit()
