67dad3a0b1502e489e62f5e2cc7f38f8964e3792
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:32:38 +0000

    smart complete form soil giveback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def sort_list(lst):
+        return sorted(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
