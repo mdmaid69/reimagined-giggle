b66846df79383157af108f6344b6ca1330588319
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:30:17 +0000

    gap pay wing taste noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_counter():
-        return collections.Counter()
+import sys
+def exit_program():
+        sys.exit()
+import os
+print(os.getcwd())
