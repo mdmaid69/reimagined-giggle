2e7ed56268e707ffe2a826598e789e0b88540bf7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:49:58 +0000

    turnon formal tap crowded wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
