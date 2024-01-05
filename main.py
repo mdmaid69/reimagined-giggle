31f611853e605383fac5e216ac9d080b48b5a950
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:44:26 +0000

    arm hers regular century whistle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Hello, world!")
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
