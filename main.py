c4de950a45d50432e9bc8b96fd82ab49ae8f8a88
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:48:54 +0000

    shame moment talkback picture turnaway

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def remove_directory(path):
+        os.rmdir(path)
