570f2e183a95174552827febe72c53ca5897909c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:20:39 +0000

    sensitive takeover butter invite blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
