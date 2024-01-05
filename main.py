b00cfc4213eed77464cafb144a69a3f8e7af4d94
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:53:41 +0000

    crash refuse as even bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_str(array):
+        return str(array)
