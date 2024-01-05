32b85335e79c8ddca8996e645071ea181f9f327b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:52:07 +0000

    anywayanyhow night child evening when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
