8c70ee39f295884bb1ad98c2c92e84a877b9232a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:19:43 +0000

    simple protest bottom praise takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
