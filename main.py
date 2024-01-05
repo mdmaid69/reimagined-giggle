4bfcaf95e7f0ef338bac495eed7b039e1d0dfda9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:01:31 +0000

    alone Kk nerve unpleasant recovery

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
