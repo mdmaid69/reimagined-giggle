13c0802c482780f34375abff165143ca84e85272
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:30:29 +0000

    criticism than last sensible culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def convert_to_binary(n):
-        return bin(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
