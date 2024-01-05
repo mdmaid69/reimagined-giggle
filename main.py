8d15af360f659bd42f873662210a5f4b0c7e2e70
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:29:19 +0000

    shop question bin burst crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
