b4af2ff6a84e9fbb4a64bc5e7c3132a95fd5e491
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:00:40 +0000

    payment almost show hurt handle

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
