145d094497ffa31923bf55156559d8abf386a181
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:24:19 +0000

    flavour extraordinary spread that train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def convert_to_hex(n):
-        return hex(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
