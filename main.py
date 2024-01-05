f0fa38b5c7f7f2553c62d5a829ead16299c0e550
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:07:15 +0000

    run aim factory equal cough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
