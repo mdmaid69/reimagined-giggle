b6d6b0ca77c6be8c4eaf5d1ec15c54596c22c32d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:12:13 +0000

    consider down either belief excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
