17440f1e9bd841520855d090e297b27ab1e2f2fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:32:30 +0000

    provide again influence final dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
