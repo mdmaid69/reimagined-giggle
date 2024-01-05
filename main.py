24d3e21ce3859394fc29c87092839521977ea2c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:50:58 +0000

    bet Jj afraid own after

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def count_elements(iterable):
+        return collections.Counter(iterable)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
