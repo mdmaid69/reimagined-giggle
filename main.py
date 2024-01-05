8d113e10ed09892c37799d4efc50e827992af5c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:50:18 +0000

    reach mad offer success sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
