f36fe66228b519d8658b699afe96ff9329221404
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:23:46 +0000

    possibly century noisy actual brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
