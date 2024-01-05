4c4d59c0182fe142c8db30e98fb9aa86ac1529d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:51:29 +0000

    shelter middle teach walk stupid

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
