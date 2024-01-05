ba29eb5a4cff3a34d9f279e2f9929568e36ab4ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:11:16 +0000

    willing eager cause operation interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
