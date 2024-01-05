11e34bf4bb8d39e8d4e0f62e41b6dc7250dfc42e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:57 +0000

    let burn again remember war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
