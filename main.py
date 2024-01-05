b120b737134883afa8f397e1f4032f3b0d3971da
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:29:51 +0000

    press reply official communicate sleep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
