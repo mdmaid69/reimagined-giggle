a154cd85743c2bea627cfd2101cabed6aace3500
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:14:22 +0000

    thought quiet score means smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def find_min(numbers):
+        return min(numbers)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
