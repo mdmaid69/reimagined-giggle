3150f4813c066243f40136bc340686446351b6e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:36:27 +0000

    hungry whistle bored breathe nerve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
