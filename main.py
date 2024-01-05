eedb5a68b6013eac1a1d2e253984b0bfc23c1f2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:35:00 +0000

    repair thanks fair immediate expect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
