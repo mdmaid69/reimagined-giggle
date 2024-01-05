18056196c3a632e0e3c133ce2da40273c3c0f1a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:55:38 +0000

    alive warning programme discussion tip

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+def get_current_working_directory():
+        return os.getcwd()
