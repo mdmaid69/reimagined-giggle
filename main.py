c6a54befd6839f4b823f82504f7b39b1ae6c30c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:30:29 +0000

    holiday note safe speed hot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
+def calculate_density(mass, volume):
+        return mass / volume
