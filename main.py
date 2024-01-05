61a20cc63e88768cc208e985cf362a7d07e0a881
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:24:25 +0000

    dance snow flat throughout frighten

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_palindrome(s):
+        return s == s[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
