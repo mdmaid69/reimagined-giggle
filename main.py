3d5740ccd9ba450a69467ed9857093aa2bbb5735
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:21 +0000

    promise basis change keepoff would

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def reverse_list(lst):
+        return lst[::-1]
+def remove_duplicates(lst):
+        return list(set(lst))
