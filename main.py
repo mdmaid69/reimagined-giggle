615cf6bc5abfe479ecad6f074d49c308fae4d21b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:24:05 +0000

    cost month generous print ordinary

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
