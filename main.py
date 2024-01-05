d64d15bf10a90d47510f4f42f086373e659381a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:41:44 +0000

    recently bath lost against length

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
