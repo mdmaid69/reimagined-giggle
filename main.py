4d0c49c92bfd2e0b0a040f5c4436ab4ee784903d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:38:40 +0000

    confuse busy nature unlikely proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-print(os.getcwd())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
