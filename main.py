b59c8d80699e3ca6353e2db3ec03d12efb41b5c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:59:22 +0000

    almost check zero unite range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
