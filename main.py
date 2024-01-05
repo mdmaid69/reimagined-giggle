4c6b54f350756f637e1d6ccd8cd4dcde3f4404b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:08:31 +0000

    reply responsibility send new own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
