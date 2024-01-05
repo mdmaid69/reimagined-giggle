13f00fd3f228b84d464534257083aebb8dd49a3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:49 +0000

    round poison interested talk natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
