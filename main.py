50f03fb3449d13a099b65437715dba0b6527c8ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:21:26 +0000

    clean pour hard love Mm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_power(work, time):
-        return work / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
