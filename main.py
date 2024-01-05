84ead026312bae25f0bb9b73a2362842cca560b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:06:25 +0000

    care unfair concentration perfect confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
