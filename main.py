97b3b2379d58c9bfc0e085cd88eeb6f0f549c1ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:29:35 +0000

    attitude leave OKorokay anyone order

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-from collections import Counter
-print(Counter("hello world"))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
