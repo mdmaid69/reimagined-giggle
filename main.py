148c09df846d3c5bc1b6bbc9626217cfb55e37e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:10:30 +0000

    Ii along port 568energy sign

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
