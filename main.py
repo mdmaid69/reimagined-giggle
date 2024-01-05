06799a204c80e59f93c35ab16c7747a4cf0e1a34
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:56:27 +0000

    think really stir interested fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import collections
-def create_queue():
-        return collections.deque()
-import time
-def get_time_since_epoch():
-        return time.time()
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
