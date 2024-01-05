065adbf64bf52c3bd008a8d5572532e7311b341c
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:54:46 +0000

    fast development legal neither write

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
