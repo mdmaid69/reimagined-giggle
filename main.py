0197df746e74c17d865a4fba06849e7281d0e31b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:30:38 +0000

    how difference if equal copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
