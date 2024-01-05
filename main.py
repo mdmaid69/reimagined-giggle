1c00e1fb87b0a33d0fe8cd265641fe0261fd8148
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:33:40 +0000

    trick charge Qq stick summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
