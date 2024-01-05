cd6f75b7613a2977a1969df226563d16c63030fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:40:29 +0000

    soon stick able past strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
