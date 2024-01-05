695fd1099d5cf537aca0c0eb107de1d03dd98a1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:41:10 +0000

    carry count long winter successful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_work(force, distance):
-        return force * distance
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
