02330376dbbd250d78e41d96ad319a82dbdf1b95
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:36:20 +0000

    innocent boring crazy broad represent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
