15ed8c8194f09b6e32bc573476bd5f05ff9ba01e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:49:21 +0000

    estimate land language manner wrap

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def count_words(sentence):
+        return len(sentence.split())
