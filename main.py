3dffaf76756b95bc6f44650a8977e72ec608c2a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:17 +0000

    operation bad coat sink ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import sys
+def exit_program():
+        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
