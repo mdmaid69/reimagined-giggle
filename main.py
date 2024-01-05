849685cd375bfed17da79a0c7ac1c2c42735b458
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:07:53 +0000

    attractive ill noisy illness difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import datetime
+print(datetime.datetime.now())
