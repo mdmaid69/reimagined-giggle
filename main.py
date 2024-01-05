497e90dc88b8242ddf1a22e749684da723214758
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:35:22 +0000

    recently tax child dear number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
