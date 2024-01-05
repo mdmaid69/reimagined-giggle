f6ac91f4d539dff35dc7398540e12e4279b7045d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:19:53 +0000

    excuse greeting how water level

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
