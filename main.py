86d3a894e0f1616e3ea54d0afd51a8ce6cc78f4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:12:51 +0000

    hope favourite goodbye any pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def square_number(x):
+        return x**2
