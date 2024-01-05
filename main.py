7e8a24f30ada7d8425eb777f5c0501dee576d546
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:10:58 +0000

    board bit promise odd deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_list(array):
+        return list(array)
