e04b9b14d708862cdb423e5f194b41ee31ab1845
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:11:52 +0000

    getout trial whistle will sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
