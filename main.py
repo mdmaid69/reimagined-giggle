13d14f77671b6bf0b080eef165dfc522cb78a64e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:02:54 +0000

    touch comment afraid urgent connect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
