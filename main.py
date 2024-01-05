a4230f190ea2f671622c09d13a442fb45b862211
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:33:22 +0000

    town later hurry journey adjective

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
