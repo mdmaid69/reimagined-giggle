0087f7ed6f7d659ac5da13e72922f8baeb987b37
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:07:45 +0000

    powerful deserve material building accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
