865ee777ca9e6929fcd9063bc07be1db6a7680fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:05:38 +0000

    goon so round geton mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
