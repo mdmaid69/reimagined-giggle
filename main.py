addcb9b090c62bfe5caca68c4df980ef31678aaf
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:43:59 +0000

    back personal organization guilty waste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def count_elements(lst):
+        return len(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
