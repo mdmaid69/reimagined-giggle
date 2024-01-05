c6d466b9d4279c1e0b250ae45f938422280af692
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:15:54 +0000

    slice performance mix whether window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
