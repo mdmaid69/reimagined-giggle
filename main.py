cbee34535f0deda9f4150bc305e170252ee73364
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:00:10 +0000

    comeout several dress direct build

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+print("Hello, world!")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
