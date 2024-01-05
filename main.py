fc99d3b5af3d1c719a86ba139d0456e53c2c8433
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:05:48 +0000

    language measure health little run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
