6a441f855387560f4ffedc04ab913f265baae9d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:26:43 +0000

    shock busy control statement lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def square_number(x):
+        return x**2
