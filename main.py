5845cf94371e9f1008f4297ddacb98ec4e5ab67c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:58:16 +0000

    solve together sign expect drawing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import getpass
-def get_username():
-        return getpass.getuser()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
