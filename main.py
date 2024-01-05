c62b83fa970aee60eecd240c1f878ac475871fbb
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:55:31 +0000

    battery print used father stop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
