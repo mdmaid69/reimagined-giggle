f4febe13a0be5cb4173493ff8886d4ad23ba9cc8
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:15:28 +0000

    hour string rush recommend tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
