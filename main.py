af0d86a9d3737da811bb1cad1620ca57bea1dda0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:50:12 +0000

    interruption 569enjoy mainly cold thorough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
