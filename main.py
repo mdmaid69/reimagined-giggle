4b71158531ec7d146364abe93dd32101c2f035b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:41:42 +0000

    find tragedy job early black

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
