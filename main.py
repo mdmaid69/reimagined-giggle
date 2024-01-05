fa92d0735bfcba18c76cc93d076856ab56f96d0a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:32:09 +0000

    choose lookafter form future week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-print(math.pi)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
