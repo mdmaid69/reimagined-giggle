d284be3b417bbef0f88876021b5d84e07b5abaa9
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:30:33 +0000

    behind so question range worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
