ba8f5797c0a402ab6d9296a882911439333ea004
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:00:18 +0000

    school spoil strange crime dark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
