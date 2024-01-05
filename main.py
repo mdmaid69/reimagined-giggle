79463a42c9d6ddd23bb741de156b71012c324212
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:21:43 +0000

    hit separate turn badly guilty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
