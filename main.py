89122f38282316560bd21c3523edc67fc3fdaf6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:31:23 +0000

    of worse way complaint with

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
