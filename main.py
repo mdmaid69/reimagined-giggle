66e5f563c9148612903afc72d2ba1152d8391b59
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:02:17 +0000

    tape degree paper start rent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def cube_number(x):
-        return x**3
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
