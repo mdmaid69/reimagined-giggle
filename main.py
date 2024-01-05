9751d30c74d2a5771262434eaf0efeaa012a1db0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:00:31 +0000

    some quality watch eye regular

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
