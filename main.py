47fbeef3e1d16938bc0ad99dee9677d67b7c5647
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:35:08 +0000

    muscle secret rest home main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import time
-def get_current_time():
-        return time.time()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
