c55add059fb86ff32b5629b463f1d6957d07edce
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:40:08 +0000

    free last there mean catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def convert_to_binary(n):
+        return bin(n)
