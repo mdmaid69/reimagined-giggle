c09d1c7c563a7e123382990835a451ba7173c1e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:03:29 +0000

    flat trade mad false raw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 def calculate_sign(x):
         return math.copysign(1, x)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
