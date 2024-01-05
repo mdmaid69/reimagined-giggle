6df97bae025a406dfbd62f486d1cf56ae3b07481
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:27:01 +0000

    straight compare point prove able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
