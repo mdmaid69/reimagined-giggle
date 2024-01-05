34c89864059e5f9aa1bf0618d253adb293751c53
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:31:32 +0000

    hate couple suggestion scared smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
