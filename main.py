1ea5378810b636257b49d510fbc7ccda5a211c0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:46:58 +0000

    favour pronunciation convinced bringback cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
