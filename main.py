7eeceb94b3cba9e96e53f1a246ac58471447f1f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:56:10 +0000

    country shape state fire ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
