2564ea7cba541c592f60ad459663896012ba4df7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:26:54 +0000

    object formal error office distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import os
+def change_working_directory(path):
+        os.chdir(path)
