b49d6a489d55357e06b3b3d8b0303ef4ea6d258b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:45 +0000

    scale date warm uncertain confident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
