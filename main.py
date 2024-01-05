908f6c4d634e2458acc3e3d486d9be976b521b02
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:13:37 +0000

    dream lock method pin emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
