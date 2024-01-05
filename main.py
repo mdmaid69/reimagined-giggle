e9e0570789100d5c8c363ac3f482fc82ed44de8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:24:18 +0000

    broken however recognize mess callat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
