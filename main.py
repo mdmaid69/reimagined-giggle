58ceeabce540122f6c37ee9334f8b1d38e50e48c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:09 +0000

    beside burn sink guard slice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
