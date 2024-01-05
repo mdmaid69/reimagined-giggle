89461061572e526c93a8b337260b7b727eeeb7fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:18:28 +0000

    dark make ride off develop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
