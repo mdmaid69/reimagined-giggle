abcbb5ba3259c6f6e1549124dc03373423e45d0a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:23:19 +0000

    valuable improvement ordinary interview holiday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
