3243dd28f7dfa523711e6c73cfe38ea8a0bed0e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:25:25 +0000

    height by play nose spoil

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
