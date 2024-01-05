4e40cc235d3e8dee23b679cb48dcb5281c2ba0b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:01:12 +0000

    threaten hole breakinto shock build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
