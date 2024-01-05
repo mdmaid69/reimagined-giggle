536b4488f41486aa6190c9eda35cd6d166922d62
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:04:47 +0000

    absolutely impression hand exactly once

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
