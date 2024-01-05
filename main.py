adc6dbd098ca2c4d58fb52ec87a5225e2a0b8429
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:49:23 +0000

    wait nigga lookback(on) cry form

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
