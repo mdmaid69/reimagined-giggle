da59bc83cc71e4eeac1e248ccc0e8b7a4b8d9f06
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:13:51 +0000

    account raise study class place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
