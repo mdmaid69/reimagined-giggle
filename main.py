b5e6d07fb8ed68e4f7cf2ad9e0ff8621afe661a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:21:03 +0000

    provide bus stare stand concerned

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
