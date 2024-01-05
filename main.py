c6c72e54b80fe86106d704d4baff48dde38f6ac2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:27:38 +0000

    number shape play respect rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+  def get_base_name(path):
+        return os.path.basename(path)
