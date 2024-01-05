a4d7073b7ad520eb504b5c2ac56e932b4aedb380
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:54 +0000

    respect town Zz frighten include

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def convert_to_hex(n):
+        return hex(n)
