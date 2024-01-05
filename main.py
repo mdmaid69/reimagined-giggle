2c144d973a7ae6d229b84b3f3d7829992de06584
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:24:00 +0000

    dick Vv theirs drink spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
+import time
+def get_current_time():
+        return time.ctime()
