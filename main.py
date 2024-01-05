c31b87c3b465a251d53317bbdf04f848651c4b9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:11:39 +0000

    object ashamed light welcome dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
