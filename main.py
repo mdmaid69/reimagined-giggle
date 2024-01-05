8f12eed101351b6a1018894e64d916ac9a5f2b09
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:11 +0000

    concentration answer force pay grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
+import platform
+def get_python_version():
+        return platform.python_version()
