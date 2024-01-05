b63dd16c006f3440c4838fe5f7eb68732644b3d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:06:35 +0000

    view jump include squash push

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
