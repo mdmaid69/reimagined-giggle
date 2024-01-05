3b21d2f155dd4c6ebcb298085c05d9d982fa405a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:20:05 +0000

    lower contents original write line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
