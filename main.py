830fa5de4fe56c44b31be8cc30b67d7088f93472
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:14:54 +0000

    rich sport callback floor rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-print(sum(range(10)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
