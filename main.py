85ba436709dcf73034a8ffc4da45e10f88e6217c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:24:52 +0000

    whenever spring value cruel crowded

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import datetime
+def get_current_date():
+        return datetime.date.today()
+print(sum(range(10)))
