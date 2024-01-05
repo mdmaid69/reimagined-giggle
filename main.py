5c5adaf609ffb8e3be88c8030fc84257310a2871
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:11:15 +0000

    base herself tend write peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
