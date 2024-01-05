2beb8be30b62d1c37ac2d6e43a50c270bf3acf0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:37:04 +0000

    lay passage skirt ear shopping

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
