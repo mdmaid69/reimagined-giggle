2f0eef0405e588c95bca84976c3e57c51fd41dcd
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:14:24 +0000

    ahead sell bill alive recently

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
