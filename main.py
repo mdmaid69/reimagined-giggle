4b2c55d655339d634f029353d233cf98b8f27f79
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:18:13 +0000

    thinkthrough suffer patience over exchange

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
