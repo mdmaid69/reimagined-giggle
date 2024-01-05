9e8b9506dd2fb5421f217121e231ee72f0c92c37
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:23:12 +0000

    punish freedom couple swing share

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
