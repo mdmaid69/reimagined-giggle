cc6a371679d488d2c1b5d8a9f52b63a9accc647d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:42:10 +0000

    ear bill meanwhile sport board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
