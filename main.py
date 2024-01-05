24f9e570011ef76d24ea469f641a5b3a15a48a1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:32:10 +0000

    upwards cry money improvement die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
