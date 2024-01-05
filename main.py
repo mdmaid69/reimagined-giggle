6ebe3aae74f48162a7fca8413630b532fb6b69b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:49:40 +0000

    urgent fear port impossible put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
