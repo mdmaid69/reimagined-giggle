1fc0ad64af6fe4a86f6e9defa9330ea2b2e14679
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:21:48 +0000

    clear best national write project

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
