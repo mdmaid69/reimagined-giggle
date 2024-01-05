5783821ceddf98055e43c23c18122c2691175296
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:25:46 +0000

    ticket runover system every arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
