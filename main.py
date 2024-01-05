3ae2bf15570f907d360be3947ce7218e5273ae9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:59:09 +0000

    rob ask runover vote content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_current_directory():
+        return os.getcwd()
