ba0f5ef83a576f20b58456493ec2bcb24af17004
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:56:16 +0000

    desperate takeback fat breakdown will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
