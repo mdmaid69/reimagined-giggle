f5e87ce28cd7d566ca7a91e7872d71a8dbc12f75
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:10 +0000

    interest exactly heart tough often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
