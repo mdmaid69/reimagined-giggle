b59392c034e7b91aba82ab292b9670c45abe8f23
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:57:37 +0000

    board hide thankyou century front

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
