014ecc9d35c32fe99e31a5e47ad602f15ee7e32a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:20:30 +0000

    play spite pull business waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
