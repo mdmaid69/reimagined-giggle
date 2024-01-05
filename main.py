8835902818bd5b1bc53a39ca01a1f846d3c92d86
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:57:07 +0000

    top difference some you block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def clear_array(array):
+        array *= 0
