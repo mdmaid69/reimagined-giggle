e62be90613fe538623dd15ffa53462633facb7e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:53:08 +0000

    occasional visit fat deep extraordinary

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_max(numbers):
+        return max(numbers)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
