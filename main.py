42ee10696ce8bae03d3670cc431c4e74a21faf15
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:12:15 +0000

    pride thought false increase pair

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
