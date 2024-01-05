cd75400b710891bcf2dea4cdd4d544f0bbcac60f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:01:20 +0000

    dark roll pay regional brave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
