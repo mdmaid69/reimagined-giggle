608b4b24ac9550a4dde38faa8beb68b9175ba898
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:02:01 +0000

    design number spoil news difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
