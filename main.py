f70eb6209a4c667f8979a6a139a8c87a06917ea5
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:21:13 +0000

    question warn precise beyond mine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
