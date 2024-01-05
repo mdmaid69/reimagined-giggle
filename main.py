b54867b04fae6c99734e6fd716aafd077e402ff1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:13:50 +0000

    dark keepaway quickly secret event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
