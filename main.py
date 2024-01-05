de93618d81755ac1d4b601b65e007bbb66140280
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:31:28 +0000

    treat wave agreement shop room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def divide_numbers(x, y):
+        return x / y
