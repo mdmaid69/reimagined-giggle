cdd558bce68ead6c283231a7ad31185fd67027d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:47:48 +0000

    in cum concerned correction before

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
