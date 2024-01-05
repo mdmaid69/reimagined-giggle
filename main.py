88fc752cfa8364e692efaab3adc5cf72c9e296b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:46:24 +0000

    plant bring ride any not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
