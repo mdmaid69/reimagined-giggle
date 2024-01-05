f3c0882398158706a84d1739b2ecd4ea694ea1d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:20:33 +0000

    afraid answer however goup neither

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
