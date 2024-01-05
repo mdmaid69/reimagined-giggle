2fa987d927ab6a529656e70519f96ca933b94e46
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:16:29 +0000

    taste lookaround tooth cry warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
