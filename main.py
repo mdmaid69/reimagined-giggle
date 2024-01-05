380e29f001c9905e180f9409fb2dafb4535636cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:57:57 +0000

    mind tooth discussion step unusual

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
