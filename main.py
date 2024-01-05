20a276d363acb44f1132baf4fc72941c55fc5ab2
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:19:17 +0000

    smart accept count touch degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_array_to_unicode(array):
+        return array.tounicode()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
