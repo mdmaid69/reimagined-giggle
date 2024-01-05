aafa4062f6a4d0143394ab44753dc264fe30c364
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:07:11 +0000

    central affect effort education theirs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
