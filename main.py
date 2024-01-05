a8b6fa1592aed492a5e02406290b781f1c79296e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:03:42 +0000

    natural make turnout needle getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
