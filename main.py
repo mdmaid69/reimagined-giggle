d08cc70b29162c335bf96b26cfe8d682242e57fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:48:16 +0000

    getaway law holdout related free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
