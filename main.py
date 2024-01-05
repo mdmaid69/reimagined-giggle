d8e426d7e508a8059055713a252516b4521875db
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:36:01 +0000

    course sting question 567enemy describe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+for i in range(10): print(i)
