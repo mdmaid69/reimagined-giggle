25b4c66a6581a8fe80e1defb0fde9271a0d5b26f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:48:08 +0000

    settle fat study arm shelter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
