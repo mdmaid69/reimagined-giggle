b96b9614fb405aba1272f11f3548f1380266ad2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:48 +0000

    step Rr state culture of

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
