4654eb1b929840e5f48ee513591e14ddaf30181d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:24:46 +0000

    bit comeup aim health afterwards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
