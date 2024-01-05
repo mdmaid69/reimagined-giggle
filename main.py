e425d77eeb4c0d7769753b9812bc9e9d7efa2245
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:22:48 +0000

    offend zero big point blind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
