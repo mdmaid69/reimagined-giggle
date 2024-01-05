905915ae38e25168484bf46723427b87e712780b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:05:19 +0000

    check care relation brother play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
