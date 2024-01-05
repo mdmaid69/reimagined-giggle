da23c7e7cc12181ef460b30141062c7901ec459c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:31:28 +0000

    create claim pay certain ordinary

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
