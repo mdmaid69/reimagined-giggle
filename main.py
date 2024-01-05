03a20be7675db190fe810165be567be12999b24a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:31:57 +0000

    whether squash admit country sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
