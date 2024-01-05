7ef2f496d6aa97cfd1b48ec76070ab68a77a88b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:49:08 +0000

    service autumn tomorrow probably male

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
