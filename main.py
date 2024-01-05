0e38534403e6af993dd56a52d3f70c07b80aa2b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:15:39 +0000

    strength destroy left cough comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
