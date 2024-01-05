6d067de9161a5298dee1dbebaca17a70ec54289f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:33:47 +0000

    bottom row attach run consult

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
