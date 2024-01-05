2365e37b65a8dc21efbc292c05dfc3fc642ddd90
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:34:21 +0000

    immediate whoever trick rank actual

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
