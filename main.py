7af8860b5cfaba4bdf89e1a1480ae2cf7cdd3864
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:29 +0000

    effect stop before identify area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
