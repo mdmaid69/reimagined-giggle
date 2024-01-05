916aedeb538946a9d914862a1bba59a5ca2364ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:58:00 +0000

    breakup drop ask journey believe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
