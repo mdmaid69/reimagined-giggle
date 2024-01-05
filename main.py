d52916e5295be0dea4a19330d6b680a778c74205
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:27:12 +0000

    across sore breakin another comment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
