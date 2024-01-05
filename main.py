b4aeac4a0525af92d7de3bfb074d7a18d7c76d61
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:09:23 +0000

    step walk tough getout what

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_area_rectangle(l, w):
+        return l * w
+def reverse_list(lst):
+        return lst[::-1]
