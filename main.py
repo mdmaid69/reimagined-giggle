2a5b0215e40c014b9b1bbfe804b7d5cdc477e459
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:43:13 +0000

    Zz lock practise sleep broad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
