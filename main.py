2ca9142024650d399b5ae0d8774110eeb4f61e17
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:10:57 +0000

    custom soon help pride do

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def multiply_numbers(x, y):
-        return x * y
+def divide_numbers(x, y):
+        return x / y
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
