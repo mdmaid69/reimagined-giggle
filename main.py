1af7b845e4f358ff44cbae43263afb40d4d1754e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:08:24 +0000

    argument must front seriously lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
