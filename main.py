ef8b024de8626af060cbd61a9e27e3423983c08f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:09:17 +0000

    after needle against cheque quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def is_odd(n):
+        return n % 2 != 0
