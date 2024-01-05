e39517fe7776a00db5e8a4f4faa11a1c5d7afe5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:15:13 +0000

    frighten sudden proper trick feeling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(10): print(i)
+for i in range(5):
+        print(i)
+def find_max(numbers):
+        return max(numbers)
