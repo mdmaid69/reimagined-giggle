f696ef743ca2ac7a5282e33f592809e14987a300
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:10 +0000

    do tragedy getaway collect exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
