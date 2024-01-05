c6de3524b9c90eba83d4f6f701188b55fbb79386
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:59:24 +0000

    feeling smoke straight punish mad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
