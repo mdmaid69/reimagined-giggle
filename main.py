4e586bb415d748166170c3c2fdd480f07b60803e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:58:50 +0000

    something note concern earn use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
