a2479e4568401b0db4314e7193d44fdbfe95d63d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:57:48 +0000

    travel the offend unlucky dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
