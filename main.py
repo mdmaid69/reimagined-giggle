d69d7789c2de06148080f3e5e36114080a4a00a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:17:35 +0000

    public reliable natural mind gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
