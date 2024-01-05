be4a4c1eba9313262c8c9b812d9f817c8212f59f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:07:44 +0000

    benefit flat risk prize workout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import time
+print(time.time())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
