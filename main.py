01d5334ff7ee8c36f0dd27a8e0d56face90fa121
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:04:14 +0000

    welcome main correct worth shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
