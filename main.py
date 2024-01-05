3d0e36607a3591226c5f7230068094339419ef34
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:36:08 +0000

    unite tune impossible match then

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import sys
-  def get_python_version():
-        return sys.version
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
