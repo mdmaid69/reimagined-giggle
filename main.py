36cf13b97c23a2668465219a709f292e8df3895e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:41:52 +0000

    scheme half sound judge base

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
