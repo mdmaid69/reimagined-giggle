09098ef539713af263730da590ab8bf08598263b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:10:51 +0000

    several there narrow damage clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_hex(n):
-        return hex(n)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
