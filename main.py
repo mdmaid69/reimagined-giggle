254ad86bb63ae48fc342770724e9939c55ee8e90
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:57:27 +0000

    sea address all sense niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_item(array, i):
+        return array[i]
