b8d88d3b6903a0a77694057dd51c6eff60b89ba1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:32 +0000

    wide some upwards branch lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
