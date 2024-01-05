ba427f715a3074e33cc4db7f836a20d2156b2333
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:24:06 +0000

    straight will somehow block crisis

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+from collections import Counter
+print(Counter("hello world"))
