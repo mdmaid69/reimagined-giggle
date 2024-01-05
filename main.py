a94cd16a166f1c9acd3c51fdf77dcd80af3dd67a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:53:54 +0000

    might deaf practise Mm sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
