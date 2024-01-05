67ab84a6eeaca48bc343305699bd90d12a44d260
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:04:41 +0000

    class speak connect factory concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_stack():
+        return collections.deque()
