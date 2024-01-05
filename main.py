aba9ed98fa7f0771442ee2139aabde109bc71de9
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:46:51 +0000

    on count than cry temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
