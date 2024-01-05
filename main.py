4ecf91cdbb5fe1aadd8bfa15408f9d046af99cb6
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:56:34 +0000

    edge may invitation satisfied precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
