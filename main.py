573156092b1b1c793de611beea534990ccf997df
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:10:48 +0000

    total comein for flavour consult

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
