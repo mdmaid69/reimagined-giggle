e7675fc557a1956c28a5828790edc63545d92b1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:41:44 +0000

    keepoff fill pupil state class

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
