8754d8449537a989e0ab9808a4d6784979dc88df
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:13:37 +0000

    action never bringforward possession personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        print(sys.version)
