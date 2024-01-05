e380779bdef2155e0940edfa65d80135b1e8f7e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:35:13 +0000

    check ability relief arm type

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_acceleration(speed, time):
+        return speed / time
