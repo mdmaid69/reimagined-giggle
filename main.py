0ffb611aaf5589099d9246022eabc1e781c6e2c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:49:13 +0000

    gap miss least roof set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_sine(x):
+        return math.sin(x)
