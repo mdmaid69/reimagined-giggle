44756c642f934ef131d7f6390dc2bc79dd8a3b16
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:23:04 +0000

    university party actually certainly because

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_work(force, distance):
+        return force * distance
