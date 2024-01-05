a2a9a72068f3b92c05eaee10d45bad245d1077eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:42:49 +0000

    throughout wide move wet central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
