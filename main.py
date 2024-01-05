ab21615b37489fac4a2dd79c3c83ef6259a281d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:25:50 +0000

    mark laugh frighten voice free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def find_min(lst):
+        return min(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
