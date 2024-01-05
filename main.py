60aae74b8f5fefee3e6d5766ac5f9e2775a16851
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:46:03 +0000

    slow seriously sudden design damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
