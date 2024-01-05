4be360f1e7730a8254d48cd654def5185286b7ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:08:28 +0000

    careful him beginning decision must

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
