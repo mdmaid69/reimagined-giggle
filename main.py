7035033f4d064fc23b47d9884a32c79df13df057
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:23:32 +0000

    favour trade plate rule hold

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
