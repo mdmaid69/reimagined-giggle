31196bb50fd8bf8086ed4d45706d9c87eb2d5903
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:12:44 +0000

    wash match swim water report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
