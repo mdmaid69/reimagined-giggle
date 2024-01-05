a9f199f9ba5863cfc66641600907188bbee0d0a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:50:05 +0000

    easy doctor refuse sign computer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def cube_number(x):
+        return x**3
+def calculate_density(mass, volume):
+        return mass / volume
