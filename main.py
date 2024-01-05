2c418a397e2089e9ec074a31353aed8d1961ed5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:07:16 +0000

    finger getout basis plain substance

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+for i in range(10): print(i)
