fec5880c73a9a9aade9dd1d31b0122fdddf6fa65
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:16:59 +0000

    impossible blind year night switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
