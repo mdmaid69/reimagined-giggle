73334af8a363eab5a71445c828acc5818aa86505
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:55:10 +0000

    frightened comeround speak fire entrance

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
