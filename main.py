cace54c447a4fda15b632d939ff13273e958d7f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:34:37 +0000

    ocean tight morning trousers start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
