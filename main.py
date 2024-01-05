b4cf3ebdfe082be44a9949e1c768ee44624cdec3
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:00:54 +0000

    makeinto sea rush yes turnoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
