7cae9607a0deb02295bd99af7d59509f00112963
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:59:13 +0000

    university custom breakinto play custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
