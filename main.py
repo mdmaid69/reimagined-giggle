3bc93ab3296a237f6d67d11a91e771065ca62d7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:03:17 +0000

    together underneath zero window getoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
