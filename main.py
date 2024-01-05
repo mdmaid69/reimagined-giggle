cc7d1bdf203fdfd162de87ffcb2d43b174d8714e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:00:12 +0000

    contain passenger married fierce nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
