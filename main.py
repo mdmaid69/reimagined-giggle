db9e5944eca95109d7a5deb21eba5784e2945177
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:30:08 +0000

    mark steam convinced borrow professional

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_rectangle(l, w):
+        return l * w
