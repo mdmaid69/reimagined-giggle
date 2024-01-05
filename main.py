6679d4f3ec8ee35a05f6796f2aa31c78f2effd86
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:01:56 +0000

    tune female age stupid edge

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
