6e2da8cdffb07e679481dc94196d2b9081627406
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:14 +0000

    only person damage law deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
