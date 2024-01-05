5939d36ffcd41afadb5ba2ccb4b64f780dd8ff84
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:22:45 +0000

    break we bringback Gg precise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
