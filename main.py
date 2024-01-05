c7a9bc576e06374a40107a615df01844af0edcd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:04:54 +0000

    which accommodation awake rent breakup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
