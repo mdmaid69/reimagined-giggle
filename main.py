1945fa2e57c5f8c7ce8c3ffc64e2d201ec063d7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:02:39 +0000

    sky succeed ocean name gentle

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
