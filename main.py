8edb2505df0a267caa41f149b959f2502ed17c8e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:56:27 +0000

    storm true talkback everywhere skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def reverse_list(lst):
+        return lst[::-1]
