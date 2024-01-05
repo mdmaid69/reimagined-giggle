fdd0b77446378abc4c5bd7fc2c2ed302b4b3a9fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:27:02 +0000

    taste face normal table boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
