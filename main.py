d080b8679474b4dae6fd444bc1cdfca30e62d6eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:13 +0000

    splash catch Itself runover walk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
