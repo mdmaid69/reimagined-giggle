bcf417be076e02120ec5d44b8f8b2d1822c6cd89
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:18 +0000

    date risk anybody flavour attach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
