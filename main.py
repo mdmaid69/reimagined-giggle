f70c743cbad9720fb73691dcdbe09ac65bcfac8e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:31:52 +0000

    ready scrap strange train ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
