1f090bf961b660d0a96719f3f45396a83c862381
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:32:01 +0000

    except relation offer around around

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
