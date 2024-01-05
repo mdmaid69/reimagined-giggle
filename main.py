933ccfb058f253297872c53467cf55ee445d93c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:28:32 +0000

    accidental house ready emergency read

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
