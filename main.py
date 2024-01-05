3b624871837e1e3d30e266754b606c44bf6bbfc1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:56:44 +0000

    Ww condition do dark message

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
