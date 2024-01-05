5da4f87bb1d23f7aea7e6a0d8b84d83a7c97a8a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:03:06 +0000

    furniture shrink shape Ss trust

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
