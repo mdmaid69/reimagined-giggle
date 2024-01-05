4a04a7c7be7dee3b84d1595131aff0ebc8e58619
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:51:01 +0000

    pair major nigga face necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def is_palindrome(s):
+        return s == s[::-1]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
