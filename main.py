a512366412ce115b0db80a0d8ff72a25ae441cba
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:50 +0000

    movement always otherwise cost each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
