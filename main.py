e399157e49e5253f1a15d8a65ffd7aa5a74f6122
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:52:14 +0000

    hurry Ss false spring explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
