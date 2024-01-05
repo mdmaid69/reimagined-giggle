472ed9654c91a71ed7f418db17255faf5bfa5ed1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:48:01 +0000

    pupil hurry poison embarrassed stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def add_numbers(x, y):
+        return x + y
