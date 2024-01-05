5c3db0367a469e00caa081354ac3a9bc92bd3242
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:22:05 +0000

    I loose persuade rate example

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def add_numbers(a, b):
-        return a + b
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
