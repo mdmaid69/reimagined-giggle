f2555f68977b407e4bb213bde016bd0be5e63099
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:17:03 +0000

    estimate refer recovery each opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
