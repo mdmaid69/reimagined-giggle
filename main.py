98530ed6206f51ace04a971c0ccdec9c20e6fe3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:17:41 +0000

    attempt whom principle worse swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
