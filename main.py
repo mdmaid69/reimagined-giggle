bbe8674bc807dc0c85bf329f097dcdcac38f30f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:40:42 +0000

    keep hope intelligent trust chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
