e2a61501f5fd32be2ed49e62fe138ecd154240f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:13:04 +0000

    solve harm instead party duty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
