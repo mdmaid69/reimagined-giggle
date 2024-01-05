7043f899e21f1b7c94f1d0b5a02ba157a1f7d75c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:07:38 +0000

    space slowly cheek gate change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
