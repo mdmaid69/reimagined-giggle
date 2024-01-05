b03d977aa3be446dfb94384d022458d7ff28c622
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:40 +0000

    miss band window Oo fear

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import sys
+def exit_program():
+        sys.exit()
