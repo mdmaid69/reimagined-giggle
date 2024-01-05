382b7a2bb1044028fcfcfeeb7b04eb1813454450
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:20:07 +0000

    count hit somewhere mine reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_str(array):
+        return str(array)
