697633fc7e7c02dbfd5691f90b3c51e758dfedb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:27:00 +0000

    attach suit doubt involve fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
