f451b3c8c40db1e6cd61315c89c4ffac43f93cef
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:11:11 +0000

    damp success proud fat public

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print("Hello, world!")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
