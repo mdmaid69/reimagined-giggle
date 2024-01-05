76697b6fdf18057b4ce014feba40cc4e9b4c6dbf
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:47:18 +0000

    suggest left better project dig

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-  def convert_to_binary(n):
-        return bin(n)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
