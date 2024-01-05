446ddd09f01d1e152327344848faf81ed1433814
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:39:34 +0000

    best excuse Pp bus none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_density(mass, volume):
+        return mass / volume
