1048bc71f0a8f93e2921014fc623d6ea022cd6ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:36:11 +0000

    yesterday careless up proper but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
