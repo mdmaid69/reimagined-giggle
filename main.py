b7490fdf90a5b6df2af7825aa06eef5897b758b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:28:21 +0000

    post peace maybe protest service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
