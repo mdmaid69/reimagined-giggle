91ec0c5006d6ce2e083d0fdbaae4bf990f7ac320
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:23:34 +0000

    ground Bb sound call plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def find_unique_words(sentence):
+        return set(sentence.split())
