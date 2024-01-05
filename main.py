5b7a25e28feef43e82b04237174429adbb0a1b46
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:24:59 +0000

    exercise add brown learn rest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
