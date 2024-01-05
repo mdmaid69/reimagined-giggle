a7fc1566c146b682d834c2c10855f8f27c92d806
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:12:00 +0000

    stick make week bar impression

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
