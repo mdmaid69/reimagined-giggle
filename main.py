9e2535324eff8003c35805e2f38e65664f2d08b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:22:54 +0000

    furniture cause close sad example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
