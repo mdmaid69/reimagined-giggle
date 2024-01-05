29b0ca82a42761f43a095ae819c8f28dabb7dca3
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:52:54 +0000

    trip address bunch shoulder tired

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
