0153e18e12ddf519a457d41ba49387cfae13e5d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:41:10 +0000

    Pp cost vision length coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def add_numbers(a, b):
-        return a + b
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
