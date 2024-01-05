520b5ae08fe06eb38638219e80df858cfd21f577
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:36:41 +0000

    hello watch platform lot give

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
