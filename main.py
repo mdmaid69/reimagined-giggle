376da77950efc5ab76bee1364f072f303d621ef7
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:35:07 +0000

    time transparent movement inside pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
