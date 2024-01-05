c88d392605a26a666509a397717fd0b9fe18c071
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:35:03 +0000

    mainly never nearly heavy what

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def reverse_list(lst):
+        return lst[::-1]
