9fea6b620148ef87351aab113ff0cdbe546498e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:39 +0000

    trust before myself undo dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
