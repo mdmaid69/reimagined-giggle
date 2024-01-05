e2f43928b9d9b51a4458a4be7f1df0d34ec3ed7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:12:19 +0000

    murder demonstrate sign Ll disagree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
