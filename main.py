326f6af3921cd61e406ce85dc5cdbf94587a0a86
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:16:28 +0000

    our work delight bring opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
