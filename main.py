88826f6c4d5dc3e140abdaac345f4f6fabb5fd66
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:18 +0000

    able example uncomfortable clean comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
