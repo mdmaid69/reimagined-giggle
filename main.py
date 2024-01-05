1ba320cce88322d5b4ac2e1d883546fd8e00e686
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:32:25 +0000

    plastic tap yet close care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
