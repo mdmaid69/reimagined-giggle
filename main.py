0bdabf191ad68ebe92f19285f77cbe961b11ee8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:41:53 +0000

    because safe correct change build

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
