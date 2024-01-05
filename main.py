c8fdd6f705286f8ea673992f92a9dd218fcca77b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:42 +0000

    thank myself shadow comeoff so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
