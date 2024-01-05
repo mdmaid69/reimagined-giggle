3252269739e69bd88f0894749252dc77c4719645
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:11:29 +0000

    develop start owe children impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
