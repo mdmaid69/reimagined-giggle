2896f5231de3f38d973fdc6a06cb1356eba819b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:28:59 +0000

    secret whichever window keep offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_list_from_array(array):
+        return array.tolist()
