b5d633c8ff4044ed8aaeb92b439eff5d41e3c3cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:45:31 +0000

    mess this breakthrough join excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
