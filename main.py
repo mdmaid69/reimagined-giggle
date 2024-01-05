7a51e595ba1cee6b0dace78bd3ca43b7f4f9901d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:06:17 +0000

    attempt that hurry benefit doubt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import tensorflow as tf
+print(tf.__version__)
