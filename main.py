fc698f1c76346cedbe17a24fce679000557d6b83
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:30 +0000

    so night crisis ability snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
