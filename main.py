d5be4e3569a047dc3ede4c3d5fe3dcdd28c65140
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:14:48 +0000

    joke middle method crowded keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sys
+def exit_program():
+        sys.exit()
