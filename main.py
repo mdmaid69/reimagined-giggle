5536ff44137ef597de5a687ae742abe0e695cbd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:14:55 +0000

    relief open lucky as mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
