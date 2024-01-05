732546aefcafd21336307110cfc9e87fc89c967f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:34:15 +0000

    important scene thin backwards frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
