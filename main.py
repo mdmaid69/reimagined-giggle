d2f2641f509fedd16a72bafcb2ec25305078b729
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:13:25 +0000

    also serious how accident broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
