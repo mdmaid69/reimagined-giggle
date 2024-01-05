4a3a47113ef9db36a336ece3cb3a55e774dc2a7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:35:50 +0000

    greeting harm consist refer control

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
