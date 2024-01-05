f1176803e729d84f522f718049179c198caf0f09
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:46:15 +0000

    subject lend machine win during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def subtract_numbers(x, y):
+        return x - y
