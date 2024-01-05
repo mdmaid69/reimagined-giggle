ee5e45775437fd1ec2d2a2dda363c785696c763e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:04:36 +0000

    past clear estimate game outside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
