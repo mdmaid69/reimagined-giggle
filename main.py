a7022fc862b3bff724eda18c51e95356ab4f30ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:58:16 +0000

    poor power affect slip thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def get_time_since_epoch():
+        return time.time()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
