c9fb719d035004d6eca8938b81cb41778c0012b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:46:39 +0000

    usually plan bar advice breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
