9a81bfc1d288ee2da67c64da6e4f46fc0f65d1b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:58:51 +0000

    hurry petrol turn meal gradually

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
