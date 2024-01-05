2deb7371061d1faebf4919ecd72a9ea81d8883a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:56:03 +0000

    history legal must combine anything

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_current_working_directory():
+        return os.getcwd()
