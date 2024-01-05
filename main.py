f7f22cdbadb2376b346b623918f395b80bab739e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:57 +0000

    almost half slight clothes admit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def find_max(numbers):
+        return max(numbers)
