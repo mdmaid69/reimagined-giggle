b87965acc53ead1a84781107b0f7cded20b90da6
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:45:48 +0000

    law serve share doctor include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import platform
+def get_os_info():
+        return platform.uname()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
