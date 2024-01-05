56d4aa2d58c3b9c05444611cd2f82b3fe6e9858e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:42:34 +0000

    gooff generous rarely noise maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
