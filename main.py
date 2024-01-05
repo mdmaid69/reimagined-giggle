bae231f15a8f62c2d9e83429e4d586321419e171
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:28 +0000

    through deliver trap firm hope

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
