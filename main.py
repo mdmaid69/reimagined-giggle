dfd1806494c060c66dd3e21815ede445fd0deec3
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:32 +0000

    raw price damage aim design

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import time
-def get_current_time():
-        return time.ctime()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
