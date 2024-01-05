e7070138e82beec88f655e5b5d543f0cbb0af41b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:42:11 +0000

    beautiful undo fair free your

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
