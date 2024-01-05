8a4af3df02be52587fc446a4e995d541af926eb5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:09:59 +0000

    no combine earth anxious rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import sys
+print(sys.version)
