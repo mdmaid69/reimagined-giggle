4086d65cbbf5d99f2df801ef1bd451e9b8e4f76d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:06 +0000

    bang somewhere combination stage left

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
