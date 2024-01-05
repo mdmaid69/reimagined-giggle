8c2a69f01c450983dd8ee5e59726ed9561aa0873
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:52:36 +0000

    character expect be mixture keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_logarithm_of_gamma_function(x):
         return math.lgamma(x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
