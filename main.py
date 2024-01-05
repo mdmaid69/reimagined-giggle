3cd6b2a9c5668f556d30d96ac015e9a60b6c290d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:32:02 +0000

    generous until average hollow child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
