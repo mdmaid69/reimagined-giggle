022ad9ab4d3e2f6de9e6c43874659d41a2665611
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:49 +0000

    across impossible mark same insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
