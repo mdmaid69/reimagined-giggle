f2e5580cac7d3a067cc402d12f3aa5db7abd7089
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:28:20 +0000

    plant create fear yes shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
