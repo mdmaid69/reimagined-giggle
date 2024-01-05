f9546c13b387f18d1971a70acba75fb2583fb0d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:40:01 +0000

    issue limit opposite surprise prize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
