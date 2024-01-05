5fa373c4d509fccd78cf65dd4b0ec58835e8f1a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:41:38 +0000

    fork tap count bread field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
