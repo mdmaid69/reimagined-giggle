65495d491ffec5fa52e0388cc7841194835adefe
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:59 +0000

    along carry Rr illness patience

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print([x**2 for x in range(10)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
