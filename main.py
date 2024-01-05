1c41692d59700b823cbc333923650637cb54c61f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:53:53 +0000

    price lift rise much will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
