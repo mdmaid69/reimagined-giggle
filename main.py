65475db3d29e869eb8ca244fc657d7bde3d034be
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:34:58 +0000

    priority wake obvious rain aim

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
