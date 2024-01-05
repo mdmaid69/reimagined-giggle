28ced6dfa6195662699d4d914e330566896578fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:46:51 +0000

    hold control Mm soil apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
