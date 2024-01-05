3bda0168bbf12fa3964ed4235b2b40d3b0cc7aab
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:24:01 +0000

    calloff tense good embarrassed cover

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
