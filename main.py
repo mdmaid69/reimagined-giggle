b9f0ff32b492a8339130b1b23cf301cafc0d2ad4
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:29:31 +0000

    love scare represent deaf professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_list(array):
+        return list(array)
