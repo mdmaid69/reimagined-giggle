753d2f9248569d35a49154a9ec7c40fc501d30b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:36:18 +0000

    makeinto base complete base nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
