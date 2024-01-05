c5639f3ce513cf6ea2412c2aa3ac1b38d87b5d51
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:39:42 +0000

    formal conduct goup education pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
