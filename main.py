23d228659cacdd61b3a91458a11a6d37ab54ba6f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:57:52 +0000

    confident switch receive result suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def find_min(numbers):
-        return min(numbers)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
