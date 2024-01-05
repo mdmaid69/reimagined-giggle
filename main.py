4a1d5f63ed06cb0c2e0bc972ffd360dbfc55dbf3
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:51:02 +0000

    collect comeout build cruel somewhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
