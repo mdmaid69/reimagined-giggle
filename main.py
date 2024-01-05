f2f99319be9f77957677c49c222239817acfa4dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:50:44 +0000

    last talkinto science proud destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
