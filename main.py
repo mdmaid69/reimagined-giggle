16f33d6a1c95536f409c14750c5c38076a4d14a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:35:27 +0000

    gift match trial but mine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
