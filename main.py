8dbbad5b0754114d06dc3cf1c1b1ef9d6c9dda28
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:18:16 +0000

    difficulty sweep building difficult 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
