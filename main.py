2f83e4c4c63d2b171062824baba8ef3d05d67c87
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:24:30 +0000

    winter serve face crowd computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_list():
+        return collections.UserList()
