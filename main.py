edf61bf04a6a9ef97de4a690ebf797b47c86f163
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:23 +0000

    man ilovedicks too push pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
