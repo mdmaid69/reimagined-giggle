537929211c7570224c224166062a8c6b7fc085d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:01:52 +0000

    unfortunate confused laugh cinema deaf

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
