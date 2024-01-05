bfd0a3d56237f484dcbd32ac41379dcc601d61d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:09:23 +0000

    teach wide suck rate damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
