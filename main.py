e9e693be16fb9da97581efac7023af85f92cca30
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:58:12 +0000

    who survive remember giveaway shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
