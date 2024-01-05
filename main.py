728aca4b3eacc7684921a8c63caa460f46d1a9da
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:38:32 +0000

    dangerous display soon actually read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+name = "Python"
+print("Hello,", name)
+import platform
+def get_os_info():
+        return platform.uname()
