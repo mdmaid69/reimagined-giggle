590e3133d199ffbcf1a424d95108a6cce6a04462
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:50:48 +0000

    bottom wherever rent ago Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
