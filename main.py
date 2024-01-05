ae55199b9548041539576b02bc4db583ccbc46f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:11:43 +0000

    switch joke demonstrate pass train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
