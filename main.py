d43094355ad3a8607e1a906968e9599df1eef5f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:29:35 +0000

    develop tip callup satisfied open

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
