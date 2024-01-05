8eaa8fef72b6556bdfddd96b60d59447a095c3c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:01:52 +0000

    man conclusion fire spill realize

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import tensorflow as tf
-print(tf.__version__)
-def add_numbers(a, b):
-        return a + b
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
