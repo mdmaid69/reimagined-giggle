c4cadaf6758a537b3ba356b5de4c0036e0f6469b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:56:39 +0000

    read not fall boil get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def delete_file(file_name):
+        os.remove(file_name)
+import sys
+def print_python_version():
+        print(sys.version)
