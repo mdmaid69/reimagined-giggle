82daa2dd425ee4a4fca68a2cfc04a964c6133a9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:33:37 +0000

    regularly Bb contrast term pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
