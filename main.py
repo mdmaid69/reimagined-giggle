7411d39724da863d5e308a9fce04adfa6ca8a9a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:04:11 +0000

    them light person funny little

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
