d7c719c003c981e26ae53d425b3b45b23f752292
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:16:34 +0000

    cap science getoff attention change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
