68ffedc46cde46d07b6d1bd89d448ff12d222c17
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:25:32 +0000

    influence exam finally happen why

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
