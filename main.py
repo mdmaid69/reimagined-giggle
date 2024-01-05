e7c65f727de0266dc03586a4b5c932049f202ae6
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:22:49 +0000

    proud wind advise success jump

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def set_array_item(array, i, item):
+        array[i] = item
