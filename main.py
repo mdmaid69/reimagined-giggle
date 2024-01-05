182f3fec263b269e2d73d02daaa8b8a78a73dff0
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:21:30 +0000

    dish seat hurt ignore more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
