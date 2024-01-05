6304fa68e995fd12a04c35db8398895c50ee138d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:04:45 +0000

    anybody see collect emergency forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
