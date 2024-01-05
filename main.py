292c83c7785df387d38aaf4a0668ccde8b98853e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:45:34 +0000

    emergency instruction regional future getoff

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
