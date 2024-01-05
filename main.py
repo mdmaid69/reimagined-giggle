63358edca204a6f16466a78f6fbba76a2002f06a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:48:01 +0000

    label they taste settle thinkof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import numpy as np
+print(np.array([1, 2, 3]))
