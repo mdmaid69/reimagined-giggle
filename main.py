5264c83878ed123ebacff97f44757b4ae477a6b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:41 +0000

    attitude doubt correct century come

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
