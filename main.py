73bc4454c9a6154bb0f6c077ec46ce30743d5c4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:32:00 +0000

    uniform rate unit trade late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
