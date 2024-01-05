ac2eb13e469861f1ef58096cdea6b0205ffbc7ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:37:13 +0000

    mend battery cheap air will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
