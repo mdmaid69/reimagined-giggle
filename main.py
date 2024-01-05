efc7ea469efe5b08862c4b77c7d71c02b2e44847
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:42:30 +0000

    risk goon willing thankyou intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_work(force, distance):
         return force * distance
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
