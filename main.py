5da3a90883d5f70117e598a569e63602b278abf9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:59:49 +0000

    convincing dig father problem joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
