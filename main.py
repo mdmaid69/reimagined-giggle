a5e8861c6e7ceee692aeb79315cbd607f1d78a45
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:59:40 +0000

    pupil move deserve introduce century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
