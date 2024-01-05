89491c0546ef0e28f3a4f7737f9811144dff7b5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:53:49 +0000

    count help date cover storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
