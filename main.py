fa1c3f85e90d270ba6489784f386e8356b45e2c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:14:47 +0000

    find value term drop turnout

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def sort_numbers(numbers):
+        return sorted(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
