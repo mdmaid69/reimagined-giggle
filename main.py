5db76db738a28ea3688df20a7a7661fa8d7219c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:11:06 +0000

    voice importance this prefer giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
