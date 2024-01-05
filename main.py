27307e8ef57aa9c539054b0327b6540ce73eb31b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:27:42 +0000

    everywhere peace tread pleasure above

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def convert_to_hex(n):
-        return hex(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_odd(n):
+        return n % 2 != 0
