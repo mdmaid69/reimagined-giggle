93a7fa01668ec54d122fda6e01dbbfe26c0cb91e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:35:18 +0000

    tonight full rent communicate brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_python_version():
+        return platform.python_version()
