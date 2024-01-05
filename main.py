e67585491c5a08c51eb27b0ab29f03f4602a2d8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:56:18 +0000

    man importance turninto family Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
