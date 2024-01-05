79497d17668cd1c3137afc3a2001416159a916bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:41:21 +0000

    level tradition quick tonight accommodation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
