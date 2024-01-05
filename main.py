653b4ca7eabb87b6ec503283ac83ee51639720ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:30:20 +0000

    far path smell injury person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def add_numbers(x, y):
+        return x + y
