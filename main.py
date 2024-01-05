3f646345ffa3f037691dee5802a808405330cf27
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:25:45 +0000

    reach hot sticky pity call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
