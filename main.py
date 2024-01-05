5a6e9d9ba063182319f0c1a5e7ca6df5cb1ee49a
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:58:58 +0000

    steady degree wander spin experience

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_power(work, time):
-        return work / time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
