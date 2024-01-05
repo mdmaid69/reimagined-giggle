eea68e572c2bc80d4fce27b73aa982dc367e79d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:59:03 +0000

    mistake everyone whether comeoff dry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
