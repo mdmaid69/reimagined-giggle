af35df71cc54cf92e75858cdf41dc1951fe3083b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:21:54 +0000

    goodbye underneath famous paper complaint

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
