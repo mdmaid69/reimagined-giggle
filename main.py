4c45df831cb1f65931c51c9c566087566a0b8642
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:00:30 +0000

    567enemy thinkthrough condition upstairs rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
