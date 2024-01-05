72b4feb30f4f981b267b8843230e7f9d4854864e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:46:27 +0000

    cry display active trousers shoot

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
