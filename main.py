2928b0e9c1094c322f787e4c555c209b85ead66c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:49:42 +0000

    dare full today fear breakin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
