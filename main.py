a26c0fd16bafa4e3121c2715489a02749665f4ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:43:18 +0000

    beat sky team cap watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
