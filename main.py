20def546b8235023a55284755e3ff8918cef0d71
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:58:04 +0000

    direct no all instead backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
