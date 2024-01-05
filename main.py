ef83fc48ddc82117752da3d25bfa8f3e218144af
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:21:28 +0000

    style neck grade fetch score

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
