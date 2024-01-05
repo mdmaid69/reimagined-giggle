86a074442184d7f1e8ecf4458e56edf278872865
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:19:01 +0000

    square patience term female square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_slice(array, i, j):
+        return array[i:j]
