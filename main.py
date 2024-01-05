00ae4589dedfba27b339ba7c3b6188f0a2ce3514
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:19:04 +0000

    middle clearly view kind wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
