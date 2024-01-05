32129e3347d92b2de4cf689fdf79a180c24684b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:55:16 +0000

    trouble book broken second form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
