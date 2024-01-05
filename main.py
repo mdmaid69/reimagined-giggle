34e2cfbc592ec73c852f6d0497bbbbc4edbd05f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:01:46 +0000

    wild back health mend idea

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
