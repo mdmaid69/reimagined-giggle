8493b046c2205929c4889b9356e5a18db311b1f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:11:21 +0000

    busy play certainly sound flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
