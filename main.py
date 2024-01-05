be5eed03de2d0998566d8efb56bae1c88db18c95
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:46:01 +0000

    bit forever close float wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
