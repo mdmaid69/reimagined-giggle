1ca8316a372f3c8305e75c107e1feac88dfd02c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:33 +0000

    ago injury quick compete correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
