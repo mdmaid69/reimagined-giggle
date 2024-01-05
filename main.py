be16b5182df952b202a67132652318d9eeff9c85
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:41:01 +0000

    fair defeat earn protection hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
