5d4e508017c870b43ffb473c4ccc87b151fb5569
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:32:31 +0000

    car improvement relative of radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
