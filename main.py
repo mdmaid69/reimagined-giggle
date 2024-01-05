07a5b9c66f601caf7eddd08bd341976e2d0ba5e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:49:29 +0000

    familiar promise slightly year chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
