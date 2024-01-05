e6e72ffe2c94659e72e2554ef3de49d63af3c051
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:55:45 +0000

    pen minor compete introduction lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
