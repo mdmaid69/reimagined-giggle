e3f90a0fe3fe4f50208b41cfe11dd6f38a418b3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:10:22 +0000

    high clear angry tape week

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
