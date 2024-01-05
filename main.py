12f03570681fad1980afeed4f211a1ca81e79d45
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:12:09 +0000

    appeal winter upwards see emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def square_number(x):
+        return x**2
