052b79ed511d43897b098005e53bceb29cc60277
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:32:24 +0000

    team plant spoil nigga surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
