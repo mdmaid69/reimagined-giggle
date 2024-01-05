cd4828edaa2037eadb0c1c08d36a3eb798fce27a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:54:36 +0000

    effect order century comeround rough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def add_numbers(x, y):
+        return x + y
