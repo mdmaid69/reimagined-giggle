a32448d3ebcdb5b4d27c2c2ed2410d6173f9dc4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:28:12 +0000

    flavour scare case however person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
