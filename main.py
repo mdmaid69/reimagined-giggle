9f42f06eb9b0352f4ac29cb2c5551a7ed38a7ce7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:09:23 +0000

    study trick shut gift neat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
