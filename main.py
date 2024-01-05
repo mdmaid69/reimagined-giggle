9b905e07ec17c5ddd89ec5135240a2e5f9762ed6
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:19:14 +0000

    this land join while fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
