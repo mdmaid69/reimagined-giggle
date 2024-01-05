cc0631cd13e6cebc6381e1ba9338681e554ac432
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:12:46 +0000

    body human manage brain message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_typecode(array):
+        return array.typecode
