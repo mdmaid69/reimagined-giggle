7b95c8f7c2be6cea059f0b381baba6428b5d4dd7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:58:49 +0000

    friend hang smell rich along

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
