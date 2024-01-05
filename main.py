6d620626ebce4968958b02391d67d40e681ce3b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:11:42 +0000

    return pity plant world any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
