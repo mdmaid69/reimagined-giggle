765fa056e41b1ca66f3d6e0953651cff48a03bc5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:56:55 +0000

    far average arrange centimetre if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
