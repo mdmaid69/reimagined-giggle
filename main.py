3a5ae1983f1b8cb907361b1e2bedc772b88d7b53
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:42:46 +0000

    punish agreement goaway dark getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
