fdf21f106145b17945ab0a2c9ae2646f83a4e29d
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:11:20 +0000

    entrance close afraid disgusting wing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import sys
+def exit_program():
+        sys.exit()
