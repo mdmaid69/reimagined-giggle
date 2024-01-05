804228cbaa680db44d6d6dc84b7ae9bcf098024b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:34:49 +0000

    collection pick buy difficulty turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
