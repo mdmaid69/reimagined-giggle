1949c9eb90144febb9bca60aca5a7eafa4fa8b1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:55:29 +0000

    book spill knock priority crash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
