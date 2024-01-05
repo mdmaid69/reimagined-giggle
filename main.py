7665de5932d62de08060a55f72496ebff6fc47ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:40:16 +0000

    wish work embarrassed rain danger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
