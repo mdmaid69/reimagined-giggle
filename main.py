83355ff0a96360e635a71b762386799469eae2a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:14:49 +0000

    who soil cover afford not

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+print([x**2 for x in range(10)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
