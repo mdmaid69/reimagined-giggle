685c72ba8eee172607c57d0c3ba6f67bd4339645
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:38:03 +0000

    responsible badly healthy invitation glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import datetime
+def get_current_date():
+        return datetime.date.today()
