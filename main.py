cf9c28120df900147ea6057c85e833c1552f6523
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:22:19 +0000

    favour healthy marry care cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
