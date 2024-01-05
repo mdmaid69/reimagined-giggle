3cd92ed557188872215b690b11568b17a923cf05
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:43:04 +0000

    dry effect slightly lend culture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
