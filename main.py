260369dbe725e87ccad086897a003e0284673c27
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:49:48 +0000

    holdup term freedom shout quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
