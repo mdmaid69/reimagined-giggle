76eca6f4fca794621c3798afeb51e7195e29abf8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:40:01 +0000

    place dig bear badly check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
