43f981f3fba961d86c60bfdf3a1baad85a567afb
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:25:05 +0000

    lot body damp scheme from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_bytes(array):
+        return bytes(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
