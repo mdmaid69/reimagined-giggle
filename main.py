f94ced6d899fa3dd952d497e4e9cbbb5b2e7146c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:10:12 +0000

    task quantity describe later character

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
