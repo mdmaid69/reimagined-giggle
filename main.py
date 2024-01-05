c19e944a5888caabe2934977999e53eaf868d1a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:20:24 +0000

    unfortunate earth especially produce equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
