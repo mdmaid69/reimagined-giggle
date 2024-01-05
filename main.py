977112a59117f63eb0d305eadcaeb2e8f72f51b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:42:53 +0000

    drink wall fierce visit bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
