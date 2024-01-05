8ecd877ac18935fe2bed15684f36cdb10e116da0
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:58:43 +0000

    welcome class turnover person fasten

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def is_palindrome(s):
+        return s == s[::-1]
+import re
+print(re.match("h.*o", "hello world"))
