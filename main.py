76f2fa6a96a8ebdeed9df0119cef9fd74125babe
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:48:31 +0000

    month top near blame star

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area(radius):
+        return 3.14 * radius * radius
