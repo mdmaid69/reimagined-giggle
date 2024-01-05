ab7f0a86b9b1aa76b364b6e413381013fd574450
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:01:04 +0000

    coat arrange yesterday answer beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import time
-print(time.time())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
