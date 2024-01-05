2c47fafc91c89d374c0c4e2fba94d4b695473c85
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:31:44 +0000

    whistle can hardly boat clear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
