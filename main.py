d68cd7a61e3c25be18fcdea98bc01084f66aeeb2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:29:10 +0000

    rid container health upset takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
