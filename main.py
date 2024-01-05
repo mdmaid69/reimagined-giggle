69c98d744a663398c55eba6ec6743011cee9ff51
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:39:36 +0000

    cost exhibition too love freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
