2959df9b5a176b1f273a57268fe7def75d88e4bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:32:03 +0000

    question report stir idea band

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
