9e5f84a89823bdd108db06eaad00724a383ff827
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:26:33 +0000

    danger far sentence Cc no

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+print([x**2 for x in range(10)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
