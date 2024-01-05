c90d0d2a4149738ecedc787cfde8834116547a4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:11:33 +0000

    file conversation stupid temperature sure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
