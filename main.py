fb44e3b3ea01444d5eda848f39f3651d116fb04c
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:36:23 +0000

    would fault word regret immediate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
