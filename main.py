a405f9281d41bc9e046dcf1e0e07b0fc74866344
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:05 +0000

    content partner past trip shrink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
