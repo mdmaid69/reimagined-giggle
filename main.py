c81f4785541662581f6c93d5d88e03e863141e1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:15 +0000

    right invitation sleep mark face

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def exit_program():
+        sys.exit()
