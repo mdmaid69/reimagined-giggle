07257935bdb6eee09aad974e8acc7f55dd6c7adf
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:46:15 +0000

    protect pretend eye dress slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
