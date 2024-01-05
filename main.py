415ac75ac2e1cef9dad72c0c2476c89bdd2ca592
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:16:06 +0000

    relation tragedy with lock exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
