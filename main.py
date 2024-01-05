9037c480f2e3908e74ebd904eb0c830468cdd239
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:34:48 +0000

    original piece secret warm mind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
