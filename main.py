7ce12c97a904d13e398239dfe12dde67c98a03eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:05:24 +0000

    clean talkback interruption speech generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
