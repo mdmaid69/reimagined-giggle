46f41cf3a0623aaf598613ca5231c6dccd386c7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:45:42 +0000

    sell choice around better bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
