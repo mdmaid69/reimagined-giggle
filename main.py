85994b4b4bdb27e61c56ab4d7bcf459cd648bd23
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:52:24 +0000

    workon juice warn eager possibility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def convert_to_octal(n):
+        return oct(n)
