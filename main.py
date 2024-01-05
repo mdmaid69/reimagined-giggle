958da59fa179e84f0d0f5ad2b001aa5b1d7b1d7b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:28:11 +0000

    best top statement cheat no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
