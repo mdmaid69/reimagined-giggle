e4659a142f09732a9a5da1feb7f566715f39295b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:32:26 +0000

    feature rare 572enter trap goup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
