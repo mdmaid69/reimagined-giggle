87b2bcbb92970df4381e649346ac90ac791e1a4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:18:11 +0000

    attempt curious behaviour situation remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
