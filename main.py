0894dc0a733ffe35711d0a146eb8c5d180134d60
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:02:22 +0000

    climb development enthusiastic contribute type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def convert_to_octal(n):
-        return oct(n)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
