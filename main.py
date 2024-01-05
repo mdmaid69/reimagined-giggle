eacda2b6196bc84caf347b0b3d9949b1d6cca841
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:19:07 +0000

    warning cut suffer without slip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
