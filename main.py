77a9b7bafaa82e8bdfc4e245a78c0449957239f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:41:59 +0000

    572enter before marry road broken

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import time
-def get_time_since_epoch():
-        return time.time()
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
