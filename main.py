1c2a968170fd276ca283ade7aeaa3b31c4b19c38
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:21:57 +0000

    admit unit side Itself past

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print("Hello, world!")
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
