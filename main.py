e0ed9f4fbeb8fc3ad3d65e3a89466ac14dfb65b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:08:47 +0000

    write present stick belief invite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
