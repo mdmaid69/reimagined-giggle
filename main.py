8ef58bceb302c4d33bfb075b4580f6b31ab3837a
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:16:59 +0000

    for show recent event rank

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
