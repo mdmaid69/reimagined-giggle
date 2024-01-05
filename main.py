0efbbd083eeb73e93dd1b586cb525fea69fd266e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:00:08 +0000

    cold knock game regularly us

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
