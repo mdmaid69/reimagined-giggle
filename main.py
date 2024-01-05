cd72c359ca043b37ed8830bcbc6ed3f404c8e1e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:13:54 +0000

    stir punish of rest morning

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
