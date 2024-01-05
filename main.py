bc7ceba09174b7dda6ad1355a8c1a757a7aaa3ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:38:01 +0000

    skin force draw listenfor calm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
