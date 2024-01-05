b9f869e13688fd7bfcf734dab644c27ffd5ae0db
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:10:00 +0000

    matter degree marry recognize although

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
