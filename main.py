dec39e79b264a8f1788a6c53fbd348bd49f066cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:05:45 +0000

    repeat turnoff ability pity mention

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def remove_duplicates(lst):
-        return list(set(lst))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
