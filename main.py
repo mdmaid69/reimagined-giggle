0d8479d68b56166d551004ec4865f51c83612938
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:04:00 +0000

    close imagine convinced less score

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
