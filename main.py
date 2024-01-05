1f01e3b415933a730905ef11592b289ab1698a2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:21:46 +0000

    remove question Zz short communicate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
