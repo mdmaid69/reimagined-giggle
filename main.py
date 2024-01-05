5a6e83fa908a24fd196ac9a81f111f7ada5fcc56
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:47:36 +0000

    slightly drive takeout himself level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
