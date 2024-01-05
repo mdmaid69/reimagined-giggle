80f7b8d1363162e81b4dfe336af5b1acd2c5c555
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:19:09 +0000

    Uu among time tip bar

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
