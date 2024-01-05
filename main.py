2b62039408c5a400c8d750e69268ad9da7c89a12
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:49:37 +0000

    myself about relief shock sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
