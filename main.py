6b78afdc0b5047c8224356912c49a4f319661a76
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:41:02 +0000

    further bridge away afternoon sound

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
