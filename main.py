900ec0601fc32c72d8cf14fe25bc93067c03606f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:25:20 +0000

    support human flood off dream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
