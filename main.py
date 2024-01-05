cdb4fe89d165c16e868ccbb5a2469a8273848d2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:40:17 +0000

    his appearance exit score remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
