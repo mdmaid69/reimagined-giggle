2d833a87cf702409f4414c2964aec59c45667636
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:45:24 +0000

    weigh ordinary perform else memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
