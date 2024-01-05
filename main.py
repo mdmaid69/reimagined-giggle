73b00389424f2387a14b64088a1be0e5dae4fc05
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:40:55 +0000

    work tap around flavour excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
