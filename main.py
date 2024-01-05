97f4849b22963cbbdb86fa1b785720786bb4ba68
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:29:43 +0000

    blame patience information choose getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
