34ae2612949f91201affd60df7615c1817187c77
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:54:10 +0000

    side fire answer accidentally lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
