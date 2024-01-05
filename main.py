f978c17f8ac3536cdbc2eb4cf30be29151249e89
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:05:25 +0000

    correction frighten good fun poor

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import random
+print(random.randint(0, 100))
+print(sum(range(10)))
