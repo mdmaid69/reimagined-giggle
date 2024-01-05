118032b7504686612a31edbe849dcaa5a1fc4f31
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:19:34 +0000

    regional different apart dead however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
