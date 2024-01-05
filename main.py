0deb30d4f530f6de409f3ae1d7db751d453409e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:15:34 +0000

    cost vision control school hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
