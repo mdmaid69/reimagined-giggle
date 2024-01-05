1993065803708c90e26b4558a3ed15a8d54194fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:22:56 +0000

    floor pretty crime yourself serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
