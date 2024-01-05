ad8ebefe377c7c5d9755b3e9d1e56c6098099a03
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:53:44 +0000

    copy violence goback(to) why lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
