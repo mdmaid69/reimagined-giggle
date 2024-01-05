9585e27ccb23225e929e153d3264a4c4a5e7ef14
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:05:25 +0000

    story escape lesson message pin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
