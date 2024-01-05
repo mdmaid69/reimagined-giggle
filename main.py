e7ea3ee6c38159a69410419158532225f138a383
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:54:50 +0000

    uncomfortable shut second tip explanation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print("Hello, world!")
