0f43a6e8e1dff83d085393257d24b0284f57e81d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:15:49 +0000

    basically comeback lock familiar count

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_current_date():
+        return datetime.date.today()
