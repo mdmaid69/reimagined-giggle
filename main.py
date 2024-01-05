fa597f2a5ba69823ba9c4ec06a3420c588799803
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:32:09 +0000

    many experiment interrupt measure such

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
