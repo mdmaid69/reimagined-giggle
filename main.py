c816f5d6891af8f1c2e80a6fa65b23f620d8430d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:06:20 +0000

    terrible model apart over hello

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
