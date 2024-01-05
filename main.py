86c73143d7016ea4386dd53e97a91d1f79e58ade
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:17:00 +0000

    could several reasonable happen anything

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_item(array, i):
+        return array[i]
