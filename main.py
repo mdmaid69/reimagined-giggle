473661537251091a7e4933022f8e6d37ec1b46e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:18:52 +0000

    though breakin punish tune ought

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
