beabf12409d5b83fc4c439623f4fb6eccccf7e3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:33:03 +0000

    appearance familiar great insist broad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
