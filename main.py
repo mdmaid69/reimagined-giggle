4294b8702d9de6191fda1d90c26c7bebd7f78e07
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:44:07 +0000

    near company fix provide invite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
