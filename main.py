c5e4b8293f368f16f0d411f3a54be606be251015
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:56:23 +0000

    react right circumstance custom claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
