895ce038cd6901271071c69668f86b33e7ee8893
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:21:32 +0000

    tooth Ee mark 569enjoy school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
