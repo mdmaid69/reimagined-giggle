8edd06703636a89a3a33279502d40136392f65b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:43:01 +0000

    wonder experience switch myself limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
