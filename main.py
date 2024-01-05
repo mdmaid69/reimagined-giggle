07a445ff343dc97e324595b59c6cc134ea4bc904
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:21:07 +0000

    mystery autumn anything lunch there

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
