cc4f345a8f87c24642e039a6c91fbd691fa2baa9
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:11:39 +0000

    duty correct evening future yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
