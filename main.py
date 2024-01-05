951e664ab77c4454756194f894bbde4146a02552
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:09:30 +0000

    fast trouble must onto future

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
