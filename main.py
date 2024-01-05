f7132c079b92b0e145e349f9c73156b0d1e75d42
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:58:35 +0000

    lean table exercise never moon

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
