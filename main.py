4e7d7957bffb7cdbc25629520b1284694ba45611
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:38:05 +0000

    height smell fit obtain murder

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
