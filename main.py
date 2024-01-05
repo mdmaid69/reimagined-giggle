ebc914936744d72e3bc5d36b9e10f1d3418cd9ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:25:43 +0000

    storm everyone sore accommodation breath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
