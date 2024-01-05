714712e67e359f81bdbdb60fe9ff056eb6360fde
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:58:30 +0000

    clear only pronounce part room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
