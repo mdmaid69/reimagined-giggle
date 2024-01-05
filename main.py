7a834840074d6595500d99be1d39c018d5ccef04
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:37:57 +0000

    tooth door well standard match

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import os
+print(os.getcwd())
+def calculate_speed(distance, time):
+        return distance / time
