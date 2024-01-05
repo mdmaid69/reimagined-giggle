e0dbef362b0466d494df9f675a64f45696ad3fd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:04 +0000

    custom and morning rain suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
