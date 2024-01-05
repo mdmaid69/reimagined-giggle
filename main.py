b83a966d1f5865232e54fc9a9e265cb7d18e70f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:11:52 +0000

    be long yesterday detail lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
