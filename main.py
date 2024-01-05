24f3fa9cab08c2325a62f25d1cc0694cb3c03c9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:57:59 +0000

    likely turnout late peaceful exchange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def split_path(path):
+        return os.path.split(path)
