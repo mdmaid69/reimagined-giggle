1ddbad8d302be242579aa8faffd19265d0824bcb
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:11:42 +0000

    company read single listen drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
