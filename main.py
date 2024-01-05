48231b025809452b19216d9a897bd8ba6a09dfc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:02:12 +0000

    perform ourselves combination load polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
