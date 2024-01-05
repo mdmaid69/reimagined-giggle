b97e367a946ca29de25a2ee07d78dca8f00fbda3
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:39:30 +0000

    special morning moral cause tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
