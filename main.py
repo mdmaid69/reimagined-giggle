489318c4a99b2f04a81a43444cdf11131ecf9cf0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:33:19 +0000

    squeeze punishment usual pain thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
