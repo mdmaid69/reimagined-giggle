0d9ee5f0885b1ed0dd0666dbadc62ff83a96adf4
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:36:30 +0000

    exercise desperate may get bringdown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
