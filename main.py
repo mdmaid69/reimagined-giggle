81c177105205cd338de7c1385948a9601186e148
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:39:49 +0000

    wet press warn interested few

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
