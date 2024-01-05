a0295403d0ec7e037628515b688c61d8ff84e77d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:50:20 +0000

    aim ability consult mean accidental

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def square_number(x):
+        return x**2
