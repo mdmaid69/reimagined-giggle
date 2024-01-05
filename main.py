435b12b3eaf44c6599b4711a2aba81fb51b38bfb
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:31:31 +0000

    bag light aim software time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
