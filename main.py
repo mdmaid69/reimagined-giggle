7784bf97acdae1aeca8ab48d7acb2e09becac27e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:29:29 +0000

    variety pin him perform people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
