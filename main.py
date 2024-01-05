7dc18b85462b979f1ca38dfefc5e0f10e9eb424a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:37:57 +0000

    window hold question develop spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
