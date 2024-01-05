8f3c6d0d0013c5a963537ebcbb83dab413a8aca3
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:24:22 +0000

    illness neither copy cost mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
