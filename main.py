1a93fc20c096d2602dbb439afd3455fed99a110f
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:54:13 +0000

    reject section case behind fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
