d574c17399657f7f0610a49e35f3eb9f61a95584
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:47:13 +0000

    funny describe intelligent report believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
