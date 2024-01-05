fde36ce60360f03fd37a876f26eb6a94dbe7829b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:57 +0000

    destroy show fail suggest disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
