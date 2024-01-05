9e660f74c7bd36cf11b4e14a77b1706cf96bae26
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:03:56 +0000

    belong defeat mass nor scale

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import platform
-def get_os_info():
-        return platform.uname()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
