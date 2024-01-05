035f1871eb8eb39760975bbf6fb6fda12c596f12
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:42:50 +0000

    secret present feel difficulty day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_list_from_array(array):
+        return array.tolist()
