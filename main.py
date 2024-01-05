e4b73c33e5c920a7626f398e672f426a91b60595
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:42:29 +0000

    tape upstairs before if else

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-print("Hello, world!")
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
