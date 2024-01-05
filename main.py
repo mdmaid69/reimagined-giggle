f126d5ea1db788e6847c1fdf7e3be5da60a4200e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:50:30 +0000

    only slip throw expression someone

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+print(sum(range(10)))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
