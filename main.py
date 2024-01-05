6777eef4d1943078d37f6f6a73c8618bf4d069f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:02:43 +0000

    could Mm world open on

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
