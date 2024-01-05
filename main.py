7f4c8018a199a4932633743ec8324cd4c83259f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:41:22 +0000

    now keepout official dance worse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def cube_number(x):
-        return x**3
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
