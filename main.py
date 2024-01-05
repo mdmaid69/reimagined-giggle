7be48804109c8f1548b2781c50df7dfc22a88451
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:18:04 +0000

    effort study today place reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_ceiling(x):
+        return math.ceil(x)
