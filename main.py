50ab146ea91d23159f55cc8d62f97834860296e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:44:52 +0000

    skirt relative lower fact who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_floor(x):
+        return math.floor(x)
+import os
+print(os.getcwd())
