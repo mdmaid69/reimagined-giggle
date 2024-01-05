2b8dd3febc2fd929a7bb76df0247956e89fd2c86
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:07:39 +0000

    occasional public gradually alive tap

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
