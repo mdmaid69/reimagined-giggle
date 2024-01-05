e9e00f87a77ea7f5594e024a829bfbd3c608d344
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:56:13 +0000

    boat left left organization influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import sys
+def exit_program():
+        sys.exit()
