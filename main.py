327e4e13da0187b0c1e439c65bd20ac1f1eba36b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:28:52 +0000

    lonely neither afraid combine advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
