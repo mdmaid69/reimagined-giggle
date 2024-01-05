70221d4f6bdb44bc40bab3ef4fa7d7b983db2a59
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:04:35 +0000

    search dull breakthrough off giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
