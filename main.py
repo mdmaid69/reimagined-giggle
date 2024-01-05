12b664787cb58c3dadaa1c0bb480d2db2664e6fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:25:07 +0000

    beautiful deep break away over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
