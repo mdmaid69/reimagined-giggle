689a518552e99edf60dd8d9e219a5f6ff88fc174
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:18:40 +0000

    accidental unit risk round plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
