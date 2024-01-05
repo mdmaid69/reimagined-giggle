ea02ab5e5d2344359624e3156569aee80731215c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:02 +0000

    Qq afternoon everybody present block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def is_odd(n):
+        return n % 2 != 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
