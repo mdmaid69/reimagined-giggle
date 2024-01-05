567f6fd10634bce0ad86dc8f1030998b8901eb1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:56:42 +0000

    set kill concerned face Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
