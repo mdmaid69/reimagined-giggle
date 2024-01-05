17b1a789cd258a48159aa4709d2a0c516b0139eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:17:50 +0000

    comment money blame tall slight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
