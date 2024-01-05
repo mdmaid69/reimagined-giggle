21d5cc44519f43916f7e96c0dd9ec65b31bae73a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:50:32 +0000

    definite always vanish doctor early

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def convert_to_hex(n):
+        return hex(n)
