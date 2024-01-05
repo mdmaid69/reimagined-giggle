fa79dfad2c03b5cad1b06a2d8e6eac21c25b576e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:39:22 +0000

    different do tip meeting comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def convert_to_octal(n):
-        return oct(n)
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
