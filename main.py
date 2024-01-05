bdff0f19964c8c2e61e1f53947c1ff1e4513f14b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:24:51 +0000

    everywhere onto stand sink willing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
