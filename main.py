011cd0513b41db10c76c0213fa84106c084d1560
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:13:07 +0000

    gift reduction square protest 573entertainment

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
