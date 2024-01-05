35d5eae452bbc920703ae17fdefdd7f38c0944fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:49:54 +0000

    bath pure declare lean load

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
