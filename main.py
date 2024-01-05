765e90a835149460bf0276e5b74cc6f9362c9ce5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:59:32 +0000

    gradually exchange plan return not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
