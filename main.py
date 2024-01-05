08a0408c84843c00d05d35ee420796a2cced3c78
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:14:31 +0000

    callat well common slightly maybe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
