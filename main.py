362c73f1cd6e80e44c652e4bd47b9a97e041b114
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:13:14 +0000

    sink hurry label tip forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
