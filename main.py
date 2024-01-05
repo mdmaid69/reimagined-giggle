22cf6c62b7f71b3e562f8888bf1ab8d179c22323
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:01:22 +0000

    achievement question turnaway separate street

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import time
+print(time.time())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
