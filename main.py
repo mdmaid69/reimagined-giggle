db1260771a24462e00035e3dca1220dc387c78ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:43:53 +0000

    post mark invite generally steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
