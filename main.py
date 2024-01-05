ac60d6f05a38774687bd758ba38cd5e5a9326d8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:05:23 +0000

    find right shout market talkinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def add_numbers(x, y):
+        return x + y
