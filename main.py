8f433b44e53a21207f53710c099f52695cabcc72
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:37:55 +0000

    town guard contain performance argue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
