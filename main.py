6c5c3514a13071f60b022a8881622c749221abb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:08:59 +0000

    ignore belong uncertain hear complaint

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
