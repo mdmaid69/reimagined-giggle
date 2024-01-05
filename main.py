3528a73876bd4972d2bb56ccb038c602681873dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:12:38 +0000

    spill past size sea heat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
