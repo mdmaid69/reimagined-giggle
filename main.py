3266156f862b85396b7e42d150a3b4b632e72f32
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:30:33 +0000

    difficulty child typical promise week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
