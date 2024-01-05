ba0786399e4131efda926ecd6235edfd8edff856
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:19:45 +0000

    report communication target accommodation custom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
