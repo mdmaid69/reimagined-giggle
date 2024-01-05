8aabdbe294f9fb23e24d52c5e97a76d744e66fd5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:15 +0000

    increase strict afternoon contrast interest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
