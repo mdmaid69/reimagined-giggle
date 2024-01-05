a9c246d75620dcb2320b0908991acd86ba7a4db9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:07 +0000

    check cruelty comeover promise below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
