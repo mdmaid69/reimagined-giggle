1e9328c0ee7532561a6cd44589d6bf05a8c8a461
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:53:34 +0000

    human straight organize right offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
