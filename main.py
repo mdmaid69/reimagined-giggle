63e92e2dd645b47231e7f2f2463ac982d8e5b5f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:07:20 +0000

    worse tragedy unfair pool proof

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
