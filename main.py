8ad8504bfec49c0b8c4aede482c6bf4a3cd437da
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:53:12 +0000

    thank pain term seriously Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
