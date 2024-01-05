0dce03fa900f84bb365405f11affe228813becc3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:53:06 +0000

    loss century keepaway smile by

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
