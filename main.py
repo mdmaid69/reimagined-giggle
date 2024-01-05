0a26f00df3d46f461b0f4dcf1f6c4b5176faa662
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:06:28 +0000

    so office tidy alone truth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
 print("Difference:", set(list1) - set(list2))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
