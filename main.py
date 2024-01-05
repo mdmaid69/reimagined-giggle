145182e22da64922dba3a60dbbb4d4230d408ddd
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:43:07 +0000

    congratulations fear stare month will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
