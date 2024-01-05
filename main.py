7e070fdf5982fe10d44bc84316dc57e8e9842421
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:27:38 +0000

    description standard discover shock accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
