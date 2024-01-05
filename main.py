f40cbda31024fefda19ca70a0f4f75aaa038182f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:40 +0000

    criticize worse then weather unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
