695dc7223b3d210a5edddaf16ad3a17aa29d4a61
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:43:35 +0000

    prove offend jump hello reasonable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
