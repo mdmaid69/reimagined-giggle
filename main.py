ba657be2537a2e7f548e03918e3a5171e6221eef
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:11:22 +0000

    name pinch cover spread evidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
