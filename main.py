0aac86250ffc1603fa58144b55f36f46d3eb0cd5
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:32:01 +0000

    guess mend lend cross admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
