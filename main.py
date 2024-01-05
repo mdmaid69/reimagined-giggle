6dc2f20bc4061b1aa5a8a55ec939c14b91ef3ace
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:11:23 +0000

    breakinto busy concerning but obvious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
