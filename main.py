5579fd1b87a34b110577dd1cab72e0559a2430aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:37:10 +0000

    opinion grand help used well

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
