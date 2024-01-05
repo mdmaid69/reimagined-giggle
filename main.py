a526a0575b4cbf89d52ab30ad54a81317b80c389
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:19:40 +0000

    hole confuse whom 569enjoy hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
