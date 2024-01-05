04b960fa0f9132565e699f4961ff4034a1c7e845
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:55:59 +0000

    accidentally until free beginning interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
