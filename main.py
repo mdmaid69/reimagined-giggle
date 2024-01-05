c17a7b41c3afb98de3478ebd65e67797f0621304
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:51:19 +0000

    form regular list expect exchange

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
