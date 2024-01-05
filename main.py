f83e2aa4eedc376d62cb5c43496571e56c97c8d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:47:37 +0000

    another breath disapprove hard chief

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
