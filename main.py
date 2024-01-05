9bc6e3a9a249bed3feecdd8850d5e6758006feb7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:35:37 +0000

    shape police picture success injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
