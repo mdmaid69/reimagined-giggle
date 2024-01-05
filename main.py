9a216f47eb3373d7b8a082af62e4870fee51d5b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:48:55 +0000

    too relief unfair official course

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_work(force, distance):
+        return force * distance
