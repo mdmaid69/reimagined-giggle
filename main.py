1596175115fa50c9dfba1f18a7aaf5ab1894e311
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:21:10 +0000

    generally reduce divide sleep behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
