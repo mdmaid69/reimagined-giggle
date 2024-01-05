f224d3ece88c1a2abcf538fe6dc370480f9840a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:00:29 +0000

    holdon probably live tend after

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+def get_current_working_directory():
+        return os.getcwd()
