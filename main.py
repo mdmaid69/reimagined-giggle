1fb89e0f5b31457a13b8e650642b596544943dd0
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:57:27 +0000

    Zz unpleasant possible objection weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def square_number(x):
+        return x**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
