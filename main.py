8673167a52668b44dba34b8e3bc731f1e77101f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:20:45 +0000

    sort sure interruption unless weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
