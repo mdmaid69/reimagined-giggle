c0efd74fd5b32524de261bbf1ddf7c336d98e88f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:44 +0000

    world radio politics possibility offer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
