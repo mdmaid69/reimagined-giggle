d4f03f6637dcd703649240691829f9fbd0a7f3ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:58:41 +0000

    risk skill majority Mm closely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+print(time.time())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
