5e451f3c4ff755a1d4b5ded3bef48fb526a11b21
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:36:42 +0000

    thanks wipe keepoff 573entertainment wish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
