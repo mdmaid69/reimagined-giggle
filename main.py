5be8a2cb37a3118a8ad4a8e0c5d71d2fc0ec15c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:52:33 +0000

    achieve forward fortunate roll bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
