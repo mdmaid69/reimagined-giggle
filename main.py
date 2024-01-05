398693436c99f88808abc455795bb061388022b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:20:36 +0000

    shake performance persuade immediate wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import sys
+def exit_program():
+        sys.exit()
