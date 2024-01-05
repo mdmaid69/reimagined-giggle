5dfe4ea5d0c263f8d2465c5842ac115f2ad024a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:13:36 +0000

    team process travel size check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
