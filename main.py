001f85bc34f6396ed7ae3e81b331cc3fb78b9344
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:32:42 +0000

    moment nor continue walk welcome

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
