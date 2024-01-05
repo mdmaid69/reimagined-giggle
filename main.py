a58cf653691233649e9c352b71331ce720f85c03
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:47:48 +0000

    very seriously cross constantly any

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
