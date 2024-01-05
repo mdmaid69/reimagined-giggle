8e314f6f233a36eae1c6dd5414591d0a39e24653
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:43:47 +0000

    Uu both ball comeover forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
