6f1a07cdf37e519f095c9ac5b5d338f0aae23ca7
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:26:46 +0000

    write recognize surprise shallow sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
