1dbf8d903394ff35fce98cbb7b9e003c6d1d44bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:36:49 +0000

    freeze way both shout sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
