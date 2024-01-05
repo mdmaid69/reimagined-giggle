d26522fefd1ed921fd9c016f8ab1e842428f2f7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:48 +0000

    conclusion emphasis who opponent accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_length(array):
+        return len(array)
