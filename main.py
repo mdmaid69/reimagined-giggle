4caf15b58b567dc20633adac2e261d416fceed58
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:50:09 +0000

    second I precise strong shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
