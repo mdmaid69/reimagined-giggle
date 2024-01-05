8282542e68c872ac5a59334329c177a1dac58a21
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:58:51 +0000

    petrol mess wherever tape negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def append_to_array(array, item):
+        array.append(item)
