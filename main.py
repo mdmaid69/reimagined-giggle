dea0d5dd3bc70f10349036dfd85790f59f899d64
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:19:41 +0000

    score related improve scrap fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
