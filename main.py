9b7ed0d3766a07495a4ef108bf708259a86bdea9
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:10:56 +0000

    ours unusual board simple doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
