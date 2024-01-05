42e0f8ec142143b84e9aeacfbc37b495a8dadbd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:36:35 +0000

    anxious fast land nervous soil

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def subtract_numbers(x, y):
-        return x - y
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
