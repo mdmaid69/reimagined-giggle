55f5983643b031c00800960169bec02e026972c7
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:49:53 +0000

    unpleasant shrink argument present 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_rectangle(l, w):
+        return l * w
+import sys
+def exit_program():
+        sys.exit()
