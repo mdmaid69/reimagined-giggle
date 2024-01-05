ca576977d27f427877984e445ee922d5471b1dcf
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:33:44 +0000

    goup peaceful show who drive

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
