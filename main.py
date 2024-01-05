675316dcdb3403ef103fa3e7a8a96bfb52e87f45
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:45:23 +0000

    turnover nowhere breakin embarrassed own

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
