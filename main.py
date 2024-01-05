0e1ed187e46f3fcfcbc463b82945ffbdcd579f52
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:29:24 +0000

    poor rise dance trial nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
