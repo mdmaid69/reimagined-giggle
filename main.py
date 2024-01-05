8a29a1ba4fe10857861a7cb7ad6f792245c42b14
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:04:24 +0000

    sometimes dress sort makeinto estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
