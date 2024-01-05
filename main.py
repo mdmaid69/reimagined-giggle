5d03db07f108a652a902635194626a643b60d8c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:36:54 +0000

    achievement join speak welcome put

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
