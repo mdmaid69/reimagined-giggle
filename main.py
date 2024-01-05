c5a973a5098eb082b7dac6395ff315a97d1c9eb7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:15:45 +0000

    complete look deserve perfect straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
