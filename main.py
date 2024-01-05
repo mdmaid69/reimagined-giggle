85b4bdf5d614df4cf51b2ce7399d4b45efa6f93d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:07:59 +0000

    her promise jump be traffic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
