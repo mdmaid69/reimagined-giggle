e151c888f21ce93873d2a0d40f6b6bfe6486c7a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:46:52 +0000

    take sky study open year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
