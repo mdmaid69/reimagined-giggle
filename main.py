3d16cfea8bb73caca9641460583b03455f31ada0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:29:03 +0000

    shut cough holiday scale uniform

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_item(array, i):
+        return array[i]
