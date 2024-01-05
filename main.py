b0c687f867991eea5a64612318a05fe2085615d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:04:17 +0000

    you fold criticize shout tip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
