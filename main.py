78412f1e60c2513f68b723f0ea7927524f3a2b4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:29:27 +0000

    complicated seem boat freedom star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
