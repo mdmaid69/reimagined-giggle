ad25f6a3616c47c48f67bf32d3a8c8c14fd284e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:18:38 +0000

    half happen convenient score connection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
