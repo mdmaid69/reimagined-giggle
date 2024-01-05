ccf485e0f738de8b4bad51fa29866ea715ed93c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:10:29 +0000

    proud week follow minority godown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+print([x**2 for x in range(10)])
