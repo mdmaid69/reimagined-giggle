f485e8871d0a2665b81245d08f8da7b50d048a52
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:17:07 +0000

    organize perfect Ii urgent takeaway

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import tensorflow as tf
+print(tf.__version__)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
