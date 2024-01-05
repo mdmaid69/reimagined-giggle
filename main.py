c77567b045364d6a4917dff04a221fe617a80165
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:51:28 +0000

    beyond terrible wrong runaway lot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
