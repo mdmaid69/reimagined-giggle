f4f55dfdeae6dcab2e6ccfce014f23035f043beb
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:47:56 +0000

    set violence many improvement cold

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_bool(array):
+        return bool(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
