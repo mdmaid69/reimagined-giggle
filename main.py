b5f28dad65e69cfdb813cfde1d03dc7524263a8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:57:17 +0000

    hair slice past physical finally

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
