20d58666680326bad5ebc6dc648da9e6a62d4367
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:54:23 +0000

    occur arrange separate cough style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        return sys.version
