00e92a0d7b1e6273c94eb84148823cb5db08d250
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:12:16 +0000

    fast ours crowd plate accidental

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
