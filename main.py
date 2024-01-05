18ae96a5acf44a0cfa32a57c8b21caf8ac1638a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:48:05 +0000

    occasional reply Uu use able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
