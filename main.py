889d019f126aeced41a29195d99d6763eaea2ac5
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:57:14 +0000

    by sink mark operate plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
