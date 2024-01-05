a92174af54712586a09c293c5074932ac323cf1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:57:24 +0000

    practise post comment mean far

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
