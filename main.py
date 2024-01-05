5888ce6954510f56924700eae1bfb65932899753
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:50:35 +0000

    soak work except swim refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def subtract_numbers(x, y):
+        return x - y
