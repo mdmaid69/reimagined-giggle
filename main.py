ca3b3561a8e7f9953e1850defc2150a41489c79b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:10:33 +0000

    building control squash finally friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
