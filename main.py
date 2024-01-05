7f755a00aa6baeb034de3c232a2cbb6f6ffd6a28
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:41:59 +0000

    decide do warn poor again

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_repr(array):
+        return repr(array)
