96ba54db397dd9fa7f2b37744af065e09062d925
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:17:15 +0000

    declaration bridge bus declare lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
