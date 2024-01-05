7efa84163fff45e5af5ca15062875007b93a672d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:15:25 +0000

    raw position pleasure report consider

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
