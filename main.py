3b253eb55081395922769875d3966362f825a08f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:39:58 +0000

    everyone between target maybe hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
