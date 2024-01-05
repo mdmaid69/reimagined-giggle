edd0dce7549135db1a66afd91a66e291015a048b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:08 +0000

    murder it plan Yy prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
