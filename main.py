d4fe496b4a592f43279992b5770edd79e30eb977
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:40:29 +0000

    Hh concern attempt act start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
