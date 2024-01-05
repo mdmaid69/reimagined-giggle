99513c19f68ed5e6a69a0c5730bb443080d18611
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:04:34 +0000

    accommodation nurse mood improve black

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def add_numbers(a, b):
-        return a + b
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
