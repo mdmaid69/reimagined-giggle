c705f2cb4d430630581ee0b65ccab9b8394bde2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:23:15 +0000

    hate space practise foreign view

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
