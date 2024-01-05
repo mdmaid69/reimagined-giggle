d8cf5d1b62a6a9c9d88b225714b48d60e6eb4925
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:42:11 +0000

    actual closely broad straight public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
