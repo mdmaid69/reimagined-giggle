e13d879d690fcb30963581dca3c7c84f9bc4dad1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:12 +0000

    below bin plate happy past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
