9dc954eb4997bd1d7706f77322b4f007afd36ce9
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:25:18 +0000

    only lower mother field new

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Characters:", len(text))
