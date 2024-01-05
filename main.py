04b101a2d4b540d075cee2e1dabc452ae3b2ddfa
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:35:15 +0000

    name middle march tragedy us

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def count_words(sentence):
+        return len(sentence.split())
