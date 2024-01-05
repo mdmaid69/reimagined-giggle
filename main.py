f37821c0079e0ead032211364469022b0546296d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:25:29 +0000

    path swimming angry wash proof

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
