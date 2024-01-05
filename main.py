5b8273129554a96901574f875c26affccc5fc781
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:14:50 +0000

    last unite every about hour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_perpetuity(payment, rate):
+        return payment / rate
