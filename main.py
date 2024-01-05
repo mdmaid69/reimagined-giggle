bbabb9289cd2cf51f5efec2a5d7815ba91e11dd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:03:55 +0000

    equal balance joke move squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
