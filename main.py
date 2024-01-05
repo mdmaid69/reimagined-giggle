9b02842d46f412a363f00709a16fb5f972fb0fe9
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:48:22 +0000

    female couple its sing uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
