45666df6224e33d56307aa2ea7845101d105eca5
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:55:38 +0000

    year busy square clean period

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_speed(distance, time):
+        return distance / time
