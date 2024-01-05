f795d2bd4f9372882bee3644d7c71eba79ebc901
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:51:10 +0000

    too fill true upset description

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
