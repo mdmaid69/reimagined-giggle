beaecbe0ed180b4de732dcc8d0616ebb02dd194e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:18:40 +0000

    street punishment follow far reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
