2ad247518936e81053a05f77e28827cde3809ced
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:11:40 +0000

    pencil size autumn in dream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
