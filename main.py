63e072b449f7b657ed1de7bf32c2be5ca17e5c8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:42:55 +0000

    than block total thinkof say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
