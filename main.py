051256ad4255d0c9a8bce52786a83a45cd3ba35c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:41:30 +0000

    phone doubt except mend phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+x = 10
+y = 20
+print("Sum:", x + y)
