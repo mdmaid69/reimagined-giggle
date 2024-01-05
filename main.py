03dd560e109fab77032cd6ff7d268e94a8769118
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:37:48 +0000

    scene stir cause contrast large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
