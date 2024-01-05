fee004c04fbca900c2717e8fe773092134805795
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:28:02 +0000

    prefer confusing knowledge bear guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-print(os.getcwd())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
