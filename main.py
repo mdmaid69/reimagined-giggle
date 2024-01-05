d48c89d8643ecc070aa10309ab570e5f13fd1ee6
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:52:11 +0000

    shut clean remain meal border

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
