317829d11fafabe7894a9d1c6bb11c2a73e57442
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:03:04 +0000

    takeover build traffic familiar object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
