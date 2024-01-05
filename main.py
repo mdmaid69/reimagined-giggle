060723573feed339d1a7a2efeb04b9c402620b5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:21:24 +0000

    power notice row comeover block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
