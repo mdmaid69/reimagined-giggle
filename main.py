8c88cfebe00190ce3dfb2115f488206922188119
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:34:28 +0000

    brief if spoil Vv purpose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def reverse_array(array):
+        array.reverse()
