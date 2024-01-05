279d5a523c03b266980dc932661ad8eaf1eabab5
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:45:21 +0000

    address ilovedicks nigga holiday shame

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
