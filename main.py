dc77ba587b77725f212774cd8091ee36e84308e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:42 +0000

    where properly from live church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
