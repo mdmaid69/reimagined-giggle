86956bac9713960161e9487bae1d952883504ae6
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:30:00 +0000

    that deaf accommodation mix whole

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
