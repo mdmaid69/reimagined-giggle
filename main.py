b9590e01db0951860418ae71647d8a6649d83ae5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:10:05 +0000

    snow sort spray stiff estimate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_index(array, item):
+        return array.index(item)
