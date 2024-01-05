8cb333875be2813c6e627f488618a308498d1c0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:26:09 +0000

    worth accept play only among

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
