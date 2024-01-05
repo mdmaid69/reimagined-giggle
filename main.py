182590c6bf6a55fca205f386b07ff559e9295309
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:03:25 +0000

    amount dear commit breath smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
