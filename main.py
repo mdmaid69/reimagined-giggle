c80f64270bc9cbfd5abf982566e8303fbb7ef787
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:50:45 +0000

    collection bury family practise tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
