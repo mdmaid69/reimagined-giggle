5853c63661ceca37a77912407ff262e53f7bbbdc
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:51:53 +0000

    during form city smell scale

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_work(force, distance):
-        return force * distance
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
