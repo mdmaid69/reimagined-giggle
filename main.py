e4a8305f4334324faa1fff70c022d9208cba1991
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:39:08 +0000

    mark woman steam dig draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
