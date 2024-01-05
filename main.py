9ca110f401dd90ee0acdc0d37e0ec1228d496279
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:42:05 +0000

    sort quality dick judge friend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def square_number(x):
-        return x**2
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
