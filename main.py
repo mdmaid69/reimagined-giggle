6d247b88ba5493abf0234cdb90aa11d02a8b7069
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:45:12 +0000

    demonstrate most live takeout bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
