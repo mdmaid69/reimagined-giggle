93f67a499a71deffefc1b4d77f0ebfd1152229c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:50:12 +0000

    control think start godown them

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
