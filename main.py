e8fdf7b8bcbd0cfe30738446dc6f8f931c855ab2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:26:21 +0000

    lunch otherwise impressed slight shame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
