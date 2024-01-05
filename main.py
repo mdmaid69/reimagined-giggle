058b11989fd36e52ed4b68710e195bf369d70677
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:06:14 +0000

    several sleep through although cap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
