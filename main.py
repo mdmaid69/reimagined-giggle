7b202cc69b01602bc6270d320ea98eda51be18d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:39 +0000

    try bit serve interesting air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
