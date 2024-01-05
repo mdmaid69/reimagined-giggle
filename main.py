2e92ff4f68aeedd506e22a668533d5f2171680f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:12:01 +0000

    mother television lower deliver crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
