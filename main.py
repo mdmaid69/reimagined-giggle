519688119f038111b62d9be248d123caa40e5b17
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:32:28 +0000

    character bored rush excellent like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
