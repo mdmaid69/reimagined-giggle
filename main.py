5e3e4f99eb09fdd0636873cf9542175dcbbb0f0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:12:14 +0000

    complete someone cheek mouth over

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
