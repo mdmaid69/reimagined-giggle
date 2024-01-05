9a80b5b025d443d9983003fbd415a119390a085c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:15 +0000

    legal destroy prefer consider thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
