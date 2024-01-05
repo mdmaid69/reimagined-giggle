6ccb25ad3b5353dfb2640c79fe96723140c6c1c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:12:42 +0000

    unless 568energy immediately stay better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
