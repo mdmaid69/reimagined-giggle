be102af90dc7380fd8e4c4f6b421e32e3968abe4
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:42:25 +0000

    spring justice father lie process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def is_even(n):
+        return n % 2 == 0
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
