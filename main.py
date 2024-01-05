9db9e9e2acb3b989018145306be53e3722221b62
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:06:03 +0000

    Bb generous purpose discussion traffic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def subtract_numbers(x, y):
+        return x - y
