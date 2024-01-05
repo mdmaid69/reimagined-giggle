6bd5bec25225a19973ee747514e2c399fd556a42
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:27:04 +0000

    fortunately soft Qq touch share

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def sort_list(lst):
-        return sorted(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+def remove_directory(path):
+        os.rmdir(path)
