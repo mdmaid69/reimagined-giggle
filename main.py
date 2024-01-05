b53312490db2c2117385675722756ee7ba151f55
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:07:47 +0000

    voice badly within means gap

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
