9b76becb714f8dcf6ab1e655ca483a1c9269d234
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:24:11 +0000

    computer takeout far taste callup

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
