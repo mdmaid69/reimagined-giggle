fee25633fadbc4cdb531177144bc75fce400c1ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:39:01 +0000

    wide wake into likely shallow

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
+def find_min(numbers):
+        return min(numbers)
