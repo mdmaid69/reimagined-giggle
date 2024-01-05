1b0dc09f64d20eaaab2db3f07d8786cfdad0d189
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:58:48 +0000

    quarter cap watch head urgent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import json
+print(json.dumps({"name": "John", "age": 30}))
