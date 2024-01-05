9c40f271fca54cce0b3dc45ab3c245a4c2f5ecb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:18:19 +0000

    crisis wipe object murder command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def find_max(numbers):
-        return max(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
