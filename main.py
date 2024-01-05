81de01da553cdc16bbf311cd75c8baf8e6fcd4de
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:41:01 +0000

    Vv label much about mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
