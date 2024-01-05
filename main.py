8a8719090de731d16962acd03b7788a1f0f576d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:21 +0000

    escape search probably break recover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
