5580cf84f5853cbe79cb55d826a31ef728e8a912
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:26:42 +0000

    invite mystery prefer walk block

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import sys
+print(sys.version)
+def reverse_list(lst):
+        return lst[::-1]
