38332b4a82a27bd09f75383693463dd869c8d8b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:47 +0000

    plan grateful call 572enter callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
