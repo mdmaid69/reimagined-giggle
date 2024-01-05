ed68e00ea8426cd8ff4c20a029fc06835828d298
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:53:53 +0000

    shut loss person detail study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
