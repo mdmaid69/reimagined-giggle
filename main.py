419ffc8dad5cf95362cde99fc1a6a7d42f743cd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:53 +0000

    backwards grade rich interruption question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def append_to_array(array, item):
-        array.append(item)
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
