e41c239803a19cc9b77ae0a4249a5bf1efdb1087
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:03:35 +0000

    though concerning measure over along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
