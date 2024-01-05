e36d68ca9cde5669d904b6d14f4c7fae154a124d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:55 +0000

    act interfere defend natural rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
