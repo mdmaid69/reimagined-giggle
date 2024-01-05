02116e37cc5c8f01ced7fc923612fa72f48bb6c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:49:18 +0000

    recommend about problem badly fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
