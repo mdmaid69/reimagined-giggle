ec0e5c01c55655479479c0b4b77a8fc20806e18e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:29:13 +0000

    power through why alive run

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
