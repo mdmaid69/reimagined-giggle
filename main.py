fb51e93ceb634eb79ce7779c340580dae461893b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:17 +0000

    perhaps distant fight might interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
