81d310b133413a98a082056c7eaa5f0666f77ee7
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:40:41 +0000

    himself tax platform frightened completely

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+print(time.time())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
