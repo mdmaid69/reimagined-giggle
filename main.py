705e5b036f6c790827a81164b6dba8e09ae94a0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:54:29 +0000

    change 571enough copy reaction bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
