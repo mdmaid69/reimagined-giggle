9763147f4b93682c126a17a98463f4036ac0a8f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:09:58 +0000

    once coffee first fortunately move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
