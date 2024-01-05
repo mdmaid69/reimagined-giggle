69be704f9ccc996e7c6f49969d42fc2e9a092d9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:53:26 +0000

    ago degree quick loss Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-def print_python_version():
-        print(sys.version)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+print(sys.version)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
