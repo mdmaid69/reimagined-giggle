1646f99cafa2adc44650fc146b20b09b9ab9dd04
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:48:34 +0000

    clever collect mail introduction depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
