955f7e1f73145a3366637ea94981cacfc2812606
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:01:21 +0000

    content poison design comment excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
