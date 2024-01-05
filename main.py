b4dc553f8eb55cf81490092faa8f48dda32b47fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:17:44 +0000

    scared blow entry ordinary consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
