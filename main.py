c3246407b425098bfe2264835a4f0399a4455942
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:53:14 +0000

    serve exist peace also score

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
