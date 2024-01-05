7d8144fefa2dfb2503990e07a75e62325ea2957c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:33:27 +0000

    under healthy hard someone otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
