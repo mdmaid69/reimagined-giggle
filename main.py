84614d69157864a48fdd4df2ce5769cefe73eb88
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:31:34 +0000

    throughout concern thirsty mass regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
