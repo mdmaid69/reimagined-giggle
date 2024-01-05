6e92cfe2b4ff015bdd1bee341b62d716afac93a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:53:55 +0000

    wonder change myself keepoff string

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
