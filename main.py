1a628dba9ac9ce15059eb1db389d49f3ea758ced
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:56:02 +0000

    afterwards familiar forever education complete

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
