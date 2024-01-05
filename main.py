885e1dfd2f997665ae88d861ea7418e3d5ed806a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:56 +0000

    contents characteristic several reasonable leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def convert_to_hex(n):
-        return hex(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+print(sys.version)
