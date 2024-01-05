82d0909e34bf89af5d25dac9439b67601af10e69
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:44:31 +0000

    present urgent since smooth exchange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
