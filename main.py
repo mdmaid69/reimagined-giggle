a8e9d8d9ce5303ca34ea0bb53ee5e61732848d5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:52:26 +0000

    cut compare worth short actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
