df275fb9e8a4876e9bfb58f9ce8cfc1e04a45fbf
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:05 +0000

    you event outside develop read

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
