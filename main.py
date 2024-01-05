04e0124b9040676e1dc46581b7a4e38d269a243f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:29:08 +0000

    rest prefer mad minority now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
