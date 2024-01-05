4787b77362d2a741ecd3e4582201b2cc7afc9b20
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:33:14 +0000

    smart black live question future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
