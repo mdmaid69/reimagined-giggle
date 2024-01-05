647e40b2e3503218b9b2fe0f8723bcfc1656ecc3
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:52:35 +0000

    careless concerned thinkover identify trade

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_work(force, distance):
+        return force * distance
