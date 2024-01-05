6ffea376b937097c68efe589ad113c4da856b334
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:24:36 +0000

    damage include interest range temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
