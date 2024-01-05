118837e5bb5b35d0af2f8d06258f505f46c79b51
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:14:06 +0000

    bend weekend believe lunch deal

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
