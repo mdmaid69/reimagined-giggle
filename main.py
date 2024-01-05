038939788ceec10d205c32390662c822f9f48a9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:02:33 +0000

    taste underneath that convenient help

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Uppercase:", text.upper())
