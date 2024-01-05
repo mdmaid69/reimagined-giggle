ec8d9ac0da86d1d64860880af1f52de52f6685f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:12:03 +0000

    Mm edge warm mysterious half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
