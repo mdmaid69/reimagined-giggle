cb7081eb95d3f0a303a1a58b8576bc6db90f2f97
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:05:51 +0000

    getaway hollow drink in goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
