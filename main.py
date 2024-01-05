28d9ad96aeb77ed00c35b09f6d45dd2133e7b17f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:25:08 +0000

    clean rubbish support copy blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
