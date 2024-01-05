0c0f746bf69725cde734df70f0979cb3d29bda3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:44:03 +0000

    hot survive process tread scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
