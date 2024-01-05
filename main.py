6442b6707d6a0f53d35945ba75265cb5502e6426
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:38:19 +0000

    centre situation form seat exist

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
