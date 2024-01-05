fad680a7c1c3bc36ce1708debbb52ddff90acd9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:00:16 +0000

    practice system listenfor arrive book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
