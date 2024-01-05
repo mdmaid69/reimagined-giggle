e0d7bb4a593b4d983e49e6ea81cdba30a1bffc16
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:53:51 +0000

    shape intend lunch break party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
