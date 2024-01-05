d194270258e523954f99b2377c46351f680193dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:28:28 +0000

    exam occur exchange lift interrupt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
