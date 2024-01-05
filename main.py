ed43c09ff465fe7a247d454bebddc8d7f012a774
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:22:01 +0000

    birthday boat generally takeoff lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_stack():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
