b8eabf8f7c4e26d8ea094902be875d4c1ec8e708
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:55:10 +0000

    pause Tt dear uponon satisfactory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_repr(array):
+        return repr(array)
