91b1593889e3582bf078dba940b2f4374a6d1f71
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:55:25 +0000

    stand gradually plenty sense but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def log_message(message):
+        logging.info(message)
