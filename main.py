4d3bbc03807550143718a73a5b6eeb290d6b652a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:20:42 +0000

    free trip size eat whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
