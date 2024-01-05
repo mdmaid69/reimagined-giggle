b7a0f2762268c9f6079e1531ecd3e9a735391ae9
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:21:55 +0000

    understand noise his train idea

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
