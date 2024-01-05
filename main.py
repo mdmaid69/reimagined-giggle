e398e352b2ebb27a0a612cb81211f1022223200c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:10:41 +0000

    which wave Cc sort heavy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_directory_name(path):
+        return os.path.dirname(path)
