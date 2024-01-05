709bc038b6c516a4ecf05a1fb09ae0e27e2008cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:37:02 +0000

    offend object runout order confused

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
