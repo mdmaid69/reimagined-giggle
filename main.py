44abdf670d844179f5da77656d98db15e924e706
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:55 +0000

    stare far not group film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
