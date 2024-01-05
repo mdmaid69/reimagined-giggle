6623f9b92cfcbb1cb60036addcb356145804861f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:41:41 +0000

    name future absolutely leg shade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
