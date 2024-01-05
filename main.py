44c12729831fb05b0af2404f2a54fec8b6d801c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:33:42 +0000

    with spend suffer really rain

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_array_to_string(array):
+        return array.tostring()
