2576f4f78e35809c6d670716943a4394237cfd19
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:57:26 +0000

    argument fetch uncomfortable hurry address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
