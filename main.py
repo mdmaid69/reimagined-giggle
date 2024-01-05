eeac1c2ec0f91bbad517c1af3dddbcfb2c334417
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:51:10 +0000

    comfort Ww appeal outside strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
