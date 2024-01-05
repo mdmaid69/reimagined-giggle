8105cd90fcefc0e0b41eba0c14191a4765d3ed38
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:51:58 +0000

    past doctor everybody boring light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
