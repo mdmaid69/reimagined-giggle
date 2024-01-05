e7f79b846377519f5df22c54b710d404bd1317e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:07:16 +0000

    lookup broad drop hospital total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
