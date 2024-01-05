f2f6b7a5ff639788d0c3e3039d95d4c55a8e614e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:21:34 +0000

    miss smile top leg off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
