cc6b167ec249d69732138c6d87f26e8c03eccb71
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:36:50 +0000

    slip traffic style run ocean

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
