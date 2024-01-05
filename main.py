e418a8fea926497d70abe11a5e4bb8726a2790f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:59:45 +0000

    forget maximum breakin turnover anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
