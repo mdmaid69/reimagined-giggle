1d0aa3af7a42eedd2c3e200ba28693b0a9d81757
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:27 +0000

    turnback ever herself fetch inquire

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
