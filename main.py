9f72c0b18b383ef10d2f9d5f39de6c0db0fce48d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:47 +0000

    believe voice stay judge plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
