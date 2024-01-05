3e98c218e9f2ba22f326e4420eaeeed53c0bf6c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:36:48 +0000

    television respect forgive workout blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
