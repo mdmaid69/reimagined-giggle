f8ab24cf1ffb86e2cf54650f5de9a2f09700604f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:13:51 +0000

    eye whom Oo on sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
