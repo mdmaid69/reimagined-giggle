3cec94461ffee2a07c0ef0b027fc9067fc6d11c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:29:43 +0000

    similar brush begin strike beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_working_directory():
+        return os.getcwd()
