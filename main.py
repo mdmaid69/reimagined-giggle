08b7b81f09e9b033b4b7a5e377fe85ebc41914b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:50:01 +0000

    schedule survive compete underneath male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
