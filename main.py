19979e2559033d2696c83dada797a8aab436737c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:19:39 +0000

    loan can culture something thinkover

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
