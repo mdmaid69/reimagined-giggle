3c4c79840588d900881a5853e47212458e9e274f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:38:08 +0000

    tap perfect imagination close cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
