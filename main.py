10d55c86ba38c270294f02450bc3a9a0caf3d12a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:03:30 +0000

    rain today accident contain convenient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def reverse_list(lst):
+        return lst[::-1]
