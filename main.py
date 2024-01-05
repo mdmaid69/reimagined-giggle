7291f7ee9870c743c21fc473d65c3d55b4c8a0a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:09:20 +0000

    harm bored female greeting uponon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
