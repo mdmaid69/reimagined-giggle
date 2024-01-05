4e1c0fbe47b2984b8f3e4d87f705ef1015597346
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:10:20 +0000

    getoff relation unable keepoff amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
