97c6dbdc430d0d459c4ca1e58dc342591f7b7a18
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:08:00 +0000

    listen gooff unlucky before major

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
