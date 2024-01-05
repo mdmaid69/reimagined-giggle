8b11d358e503128d7d3622490d0b73fdfd665ff0
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:05:09 +0000

    remember room influence table calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
