85657966808a65a8824f7d042b54634abd363bad
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:18:57 +0000

    party leg problem figure keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
