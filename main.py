ce288149b50b82ce53b6b47ed4d37c247f9141a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:42:07 +0000

    correction press stretch little rob

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
