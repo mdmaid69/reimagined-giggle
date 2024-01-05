db8e266391b7c3943d0ace85f70d064fdeb9520f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:23:56 +0000

    minimum bite steal shoot bright

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
