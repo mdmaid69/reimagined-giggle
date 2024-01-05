90d4ccf30998c0a8cae3f8ce79e809f6a5b1940b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:58:27 +0000

    glass shadow stand actual far

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
