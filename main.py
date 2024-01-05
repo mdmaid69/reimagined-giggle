717de93ba9f490506da46bfb487c450c5c03a6b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:29:21 +0000

    attention turnoff knock that influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
