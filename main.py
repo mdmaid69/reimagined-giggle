e60f094e5c837cae793c9a2ee65ef93d3f441fdb
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:13:26 +0000

    goback(to) add steady matter rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
