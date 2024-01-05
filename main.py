c27133e5d799ecc6d3784c10fabbb9f092293cec
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:18:13 +0000

    convenient even test wide mix

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
