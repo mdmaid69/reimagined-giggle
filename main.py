686820acc54add078260f3a3a66c7971782d3656
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:03:52 +0000

    listenfor appear impressive partner address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
