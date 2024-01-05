2a8d7c055fd896d63c0b487fddb0ceffd407e175
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:26:32 +0000

    back sore that room rise

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_pressure(force, area):
+        return force / area
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
