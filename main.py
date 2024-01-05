648ee2a7810ea18cb5ad306ac6aa9b7528f39f1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:21 +0000

    along habit dull wait may

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
