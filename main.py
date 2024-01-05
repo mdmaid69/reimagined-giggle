bfe9263435d949e824ef8e656e171f42b78bd987
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:40:12 +0000

    ticket round pretty below surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
