0a23cb8bb0b19e6fd612489a55360bd1754cef47
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:11:00 +0000

    complain glad rise book evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
