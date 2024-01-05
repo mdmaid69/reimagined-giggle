16eb3244f0a993805af9a163ae80d3894dab5af4
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:46:22 +0000

    doctor fire state next tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
