db87f3f29018005506a026c175a23c1ee1a0514b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:19:20 +0000

    several though meaning ready answer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def cube_number(x):
-        return x**3
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
