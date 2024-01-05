e3655cbbe5d5e84e361cb50a8cf5c4738b96389c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:08:02 +0000

    getoff involve through 568energy edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
