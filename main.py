5b56efdba0ea40b6707458daed52fdef9035d979
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:44:52 +0000

    shoulder comment direct follow shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
