9cd54da55d739229a8aa22e4bd04294077d5f56a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:20:49 +0000

    recommend hers figure dull influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import platform
-def get_os_info():
-        return platform.uname()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
