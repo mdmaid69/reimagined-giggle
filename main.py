8a8c85cb7e82743f1f190f7b1cea56f552181e2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:16:59 +0000

    trade approval lend noisy Pp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
