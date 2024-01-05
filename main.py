e64ab70cf437fb286c85f6bd418d610e66395c9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:04:22 +0000

    offence risk must makeinto human

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
