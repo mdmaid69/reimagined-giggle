a536003545136e8f26ec85e8d554b4e86e254dc2
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:28:40 +0000

    apparently hurt somebody swim concerned

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
