4c32e8704fc51c4f1031e8143a97ef76eac591c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:00:03 +0000

    common otherwise safe if terrible

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
