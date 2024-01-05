3822958cce40195ff399ecd95907972c9df4b185
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:24:22 +0000

    eye either first warn turnon

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
