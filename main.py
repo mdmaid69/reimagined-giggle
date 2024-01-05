647b6daa877d124b91608044e591dca88d72a674
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:59:46 +0000

    discovery cruelty aim each mass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print("Hello, world!")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
