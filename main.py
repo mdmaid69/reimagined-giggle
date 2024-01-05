1a757efe3a07e13b4b9b7abbe9e8d179f6bc9096
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:26:24 +0000

    believe quality sick man offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
