a5b8a2ec3c6aa473ee8917bf87485c5dd1eb2811
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:00:17 +0000

    tour art what trick everybody

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_average(lst):
+        return sum(lst) / len(lst)
