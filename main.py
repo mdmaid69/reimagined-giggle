8c80385dda57c183ec957862670c669a93bf3d71
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:37:41 +0000

    handle workupto comeout goout important

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
