5f1883dfffe76c651054b3a8987b781b9f0ce8b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:17:04 +0000

    crisis tool under fast disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
