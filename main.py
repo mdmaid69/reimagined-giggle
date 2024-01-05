73e0302dfcfb060510d272b0b327be5846aaa893
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:25:59 +0000

    area sting fortunate undo mysterious

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
