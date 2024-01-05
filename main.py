4adbfc1aba95b44fe31b2465df32689160474bde
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:05:33 +0000

    damage cheque keepout clue tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
