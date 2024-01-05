e6d9903350c12423f8427e62883e1142eb28cf4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:52:41 +0000

    praise colour needle clean charge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
