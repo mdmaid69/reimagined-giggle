5d640b6d1d7d28a7c757a82448fcfb038ae6f766
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:45:05 +0000

    regard suddenly live goon mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
