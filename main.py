aaa9ed52b0de4a88b89d48a08b042e158aa547a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:18:34 +0000

    clear hit matter issue priority

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
