551dba6e24aedd6295465c1955dd83c28c95404b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:39:35 +0000

    mass guard free either mistake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
