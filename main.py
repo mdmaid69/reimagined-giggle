fe445f7d4ead268a364d0107d11c11cc56282d74
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:28:59 +0000

    today correction on night ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
