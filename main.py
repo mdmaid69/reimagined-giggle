85ca8ea5b9b4473d31d9d2ef2f20e80ceb7eeb15
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:56:03 +0000

    miss blue compare desperate tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
