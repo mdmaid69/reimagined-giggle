930f5576672a95f73c8e6dcecbdd7be68f791f2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:59:41 +0000

    wind schedule before suck water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
