4684701537e8319799bf55050cfaa0c9c0760546
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:59:39 +0000

    town push use go opposite

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
