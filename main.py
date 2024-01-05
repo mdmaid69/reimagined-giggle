2e83aee3d9e65122ebe27143eac132f96a1f2342
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:10:54 +0000

    own switch round shame maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
