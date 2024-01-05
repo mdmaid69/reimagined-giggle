6003d5c815a117c8ab4778126d3b13617868db2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:32:37 +0000

    your themselves string game 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
