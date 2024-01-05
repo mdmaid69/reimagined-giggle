4b222958e4385e9b3996e0aea92ce044b396af25
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:26:45 +0000

    such even Cc lonely thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
