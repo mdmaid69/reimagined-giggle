6c81084fc28100ea5a94431e6502b3ff346d0517
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:54:10 +0000

    before smell reserve pump block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
