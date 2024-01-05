7742c25b13221f4b8ad9455866975d2c57219661
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:01 +0000

    relax branch target if stamp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
