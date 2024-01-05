9585483d62335c39339cf5924cf4f6259c4165ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:11:26 +0000

    famous expression certain write agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
