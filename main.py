f4133734d39b92f1138cde2c9cbf1383ea9160f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:31 +0000

    shout natural father score everyone

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_base_name(path):
+        return os.path.basename(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
