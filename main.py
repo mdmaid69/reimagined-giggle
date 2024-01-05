0e9e215df9510d37b4955bc442e665273c67ac4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:41:09 +0000

    second list by concerning personal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
