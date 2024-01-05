db7e98036eedab0ea26627b662305b4d2614fa54
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:04:52 +0000

    business dull like mistake centimetre

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
