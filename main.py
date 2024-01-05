ecb1752a31311f828971a7db9937edba0028b43e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:20:39 +0000

    date description yet successful performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
