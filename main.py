81420d410122caf678179770043ac06cbcb1e5cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:53:06 +0000

    rough interested tax destroy eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_repr(array):
+        return repr(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
