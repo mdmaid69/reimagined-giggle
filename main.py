61b3e523630638e9b1622c4af76b6dae0518c114
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:59:36 +0000

    slightly since challenge middle watch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_stack():
+        return collections.deque()
