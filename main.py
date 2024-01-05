27205e497d409ae96fdb7e510a56427993512b31
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:24:09 +0000

    suit choose score then practice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
