6c7d18c767ad0781cb47a7ff13db13b59599fd9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:12:58 +0000

    attempt necessary makeup speech conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
