81c4088e836ba4fb7f77bf364f781f1e69e5ce4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:45:19 +0000

    simple fairly separate private question

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
