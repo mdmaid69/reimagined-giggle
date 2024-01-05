16bd534b43b6dc58fe7557ba5893caf8048bdfa8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:17:42 +0000

    apologizeapologise praise admire drop recognize

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import tensorflow as tf
+print(tf.__version__)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
