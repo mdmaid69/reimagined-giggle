28c245c62b4ff3bd5b82160b4f679a7b7f3e9afa
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:09:57 +0000

    sense sorry along game line

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
