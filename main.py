2ac34618230e8bb569f9c4674330b0611fb200a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:41:25 +0000

    grow weak miss bound giveup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
