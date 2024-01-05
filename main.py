05ffcb719d1ec75d18845e7ae8e1d0e06184cec4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:43:10 +0000

    want evidence once month plastic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+print(sys.version)
