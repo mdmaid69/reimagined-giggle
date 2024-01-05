a26b4640f72cca996c41bd27b464cdaf5c4bf04f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:36:35 +0000

    crash lock tip often eat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
