5a5f29469ae4f5ef6b1a7caf1dfa7499fdbb9d30
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:10 +0000

    design possibly wild flavour pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def reverse_list(lst):
-        return lst[::-1]
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
