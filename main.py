389c3d46e48c04460b46e832af79f15012167fe4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:58:18 +0000

    job mad ball ask responsibility

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
