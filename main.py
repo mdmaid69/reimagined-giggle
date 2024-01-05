48d8fcc6bd2b9f0e9062926cc604f464c946d8fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:27:14 +0000

    hurry other black promise front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
