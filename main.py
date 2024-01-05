99a9d637dd32a687c07bf4ef838d486bbac665ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:26:06 +0000

    frame care neither stay marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
