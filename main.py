f416b8428375bf56ec5ba86e880462489d4c7a28
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:59:48 +0000

    extraordinary remark flat shoot finish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
