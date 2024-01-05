8f1fba7d62e8abcea5d430046d75497d4adf2d75
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:07:13 +0000

    field nature several receive kill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
