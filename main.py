b99409565b95d1f4786bf30a1be6da81ca0e8728
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:59:27 +0000

    vote take plan happy trial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_length(array):
-        return len(array)
+def add_numbers(a, b):
+        return a + b
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
