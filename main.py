080c7bc615c23a923ce9df7d129151ef8a9e641a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:55:17 +0000

    grade neither goup actual forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_list(array):
+        return list(array)
+import collections
+def create_user_string():
+        return collections.UserString()
