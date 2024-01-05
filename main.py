bebb68af7ea8f32013e00820d4b4f18ebbd1ccad
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:57:10 +0000

    dish brother happen believe broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(lst):
-        return max(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
