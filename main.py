9a54a3fca43fb68fa445a9c69fe1928953839d5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:18:10 +0000

    cut runinto stamp whistle quickly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
