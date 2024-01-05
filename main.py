c42fd9df7fc1e1a3ba29690a0eff3c4d0540a3cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:30:15 +0000

    some failure interfere section declare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
