26705ee9a33de7bf92394995295b56fd154d322b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:37:00 +0000

    absolutely damp train forgive interrupt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def convert_to_octal(n):
-        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def count_characters(sentence):
+        return len(sentence)
