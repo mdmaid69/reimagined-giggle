909fb7578f5ae71f364aacf98b1ec21f14eb5767
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:30:04 +0000

    slide his valuable grade own

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
