ae5738549325efed029c49aa814292ff82348572
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:00:52 +0000

    themselves government keepoff exact side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
