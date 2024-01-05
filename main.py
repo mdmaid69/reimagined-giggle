b67f7fcfdc42323c89a0bc335ae388f72512ea7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:22:34 +0000

    naturally dry event refer autumn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
