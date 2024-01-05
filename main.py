15ba65ce4c3fe533d8c69ca0b9866bb4f1fda271
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:19:41 +0000

    post dark lead country relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
