9aa45494aa9e6d8aba6f94c5fd281aaee55aa072
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:40:16 +0000

    increase tough formal innocent music

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
