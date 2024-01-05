ad9eb94c6d5c45df0d3c48cc0f8cb69123cae80a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:21:44 +0000

    reply high rule neat sister

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
