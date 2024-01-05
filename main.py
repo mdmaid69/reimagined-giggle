993a18dac4b40e4a80f926b8df467ff6156c74c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:01:32 +0000

    safety repeat goback(to) reserve operate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def get_array_length(array):
-        return len(array)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def reverse_array(array):
+        array.reverse()
