fd7b9b5cf8b7dd61715d8519959ec6c8f1d86f69
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:47:21 +0000

    dish proof order protest join

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
