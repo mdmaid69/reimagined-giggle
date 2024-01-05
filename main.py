2d25a8f5964891807a975967ef1826953e9f210b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:21:27 +0000

    full fit temperature accidental nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
