da06268978d971e5a8b9adeac595833231ba8a06
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:29:37 +0000

    comeround myself able choice further

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
