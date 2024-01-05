1ee6add4a51c31bead74b0f2c9ce24602f2cde53
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:51:29 +0000

    Rr ever rest danger along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def square_number(x):
-        return x**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
