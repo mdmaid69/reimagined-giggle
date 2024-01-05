dcb1a0907f9f07659bb6fa11d5c67a69571ae0fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:03:40 +0000

    connection Nn dare unpleasant any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import platform
+def get_python_version():
+        return platform.python_version()
