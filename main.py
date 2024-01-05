e5f59e706a4d0a5a7f99307a111dc1f37feee831
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:47:41 +0000

    correct heavy fit strong possible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
