1f3a423ede21e428b6a040cb4c2f13d9ee9bf795
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:22:30 +0000

    agree several earth about voice

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
