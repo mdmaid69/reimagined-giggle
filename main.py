a7e96c12c4d19e03ce8eeb539879b41b31caf4c7
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:19:01 +0000

    completely attitude famous mess stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
