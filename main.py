9b5250e3d07a3f39a4367c8f9b275e4febef0d3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:57:28 +0000

    chief tragedy 566end authority after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
