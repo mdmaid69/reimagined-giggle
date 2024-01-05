882ea623a11b4bae30f1757a77f37b6f10092213
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:36:57 +0000

    judge pipe comeback persuade Oo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_power(work, time):
+        return work / time
