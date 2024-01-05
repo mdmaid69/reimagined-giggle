2eec03d52933c9cb4c5a4108e590f052778dad68
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:07:58 +0000

    government very suffer touch love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
