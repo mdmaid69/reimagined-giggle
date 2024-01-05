4ad3a07a8fff04f6a6bb5d1d349fc63ed60c3b19
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:36:21 +0000

    organize action dare her mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
