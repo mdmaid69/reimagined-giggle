6a98c86accb108b73f909324d0972632dee3a9d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:54:20 +0000

    Ff get deserve about line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
