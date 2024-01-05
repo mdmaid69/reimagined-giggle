96a5abc958cf0554c59ec2c613c08322c6fa0cc5
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:36:18 +0000

    dance makeinto shrink healthy stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
