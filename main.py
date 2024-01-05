783c88d04560e31ed95f6f640d598ff046bb1071
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:49:32 +0000

    convenient order week exchange mess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_binary(n):
+        return bin(n)
