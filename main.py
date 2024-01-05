965e909f873fd4b4f0235910d0f4233c0f34842e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:06:36 +0000

    bridge indeed finally cover majority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
