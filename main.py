90af0b607771c3ca26c4e69436823cfe80491485
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:59:06 +0000

    connect warning speed speed wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
