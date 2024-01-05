d550ec5a0698a2ea5e42016a4c94a9ba0c758e59
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:27:38 +0000

    unlikely wander book takeaway sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
