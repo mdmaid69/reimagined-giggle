2a3505fa8a894c3cdc4b7cfe5438f226aeb92dec
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:51:55 +0000

    unpleasant dry giveback flow pick

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
