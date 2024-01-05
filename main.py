bc36fe876ca867212af76c6bcccc98ffbcda843f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:47:39 +0000

    concerned adjective dance use crowded

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
