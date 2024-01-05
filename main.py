25599a7540a336be8c2021f379618ae8f69c4839
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:10:42 +0000

    speak next tough pocket serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
