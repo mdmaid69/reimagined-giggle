13c04e64c4a28e5792ac3e86c1cc1926ea3a2899
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:38:29 +0000

    free allow he door hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import sys
-print(sys.version)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
