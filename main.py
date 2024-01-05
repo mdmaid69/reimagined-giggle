be52c9a7c6b50028e3926f85f66049fd45b26317
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:52:50 +0000

    my increase load around become

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def get_array_from_bytes(bytes, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
