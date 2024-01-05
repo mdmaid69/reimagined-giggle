2e5b4b5b785dac931b01948f5698ac3b71be611e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:25:58 +0000

    natural whether contact finish face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
