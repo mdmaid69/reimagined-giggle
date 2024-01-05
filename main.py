5b376f45a3b0afe3f0a591b60d7035780a451c0b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:06:54 +0000

    good disappointed treat tap Mm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
