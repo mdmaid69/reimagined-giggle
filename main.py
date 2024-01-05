6f74986d15ea420efecc02df5707e29347c120ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:24:46 +0000

    bringup important dry second attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def cube_number(x):
-        return x**3
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
