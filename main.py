ce0b8e1c2ee43672fd9a0627a047936d7bae98c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:20:26 +0000

    nose touch taste money project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
