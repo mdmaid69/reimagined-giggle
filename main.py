2d809cc302a7b26593bae93e4d46238ca6c79679
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:53:38 +0000

    attitude sting instruction reduction understand

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import collections
+def create_stack():
+        return collections.deque()
+print("Hello, world!")
