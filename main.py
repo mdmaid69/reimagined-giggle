4230dea45af1aca36f98552f0d63510fe2a0d84a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:39:49 +0000

    regional Tt once collection far

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
-print(sys.version)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
