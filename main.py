3a2ed9704c8c6d1c6b89ec6bc780233bc4947a87
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:27 +0000

    steady attract adjective black activity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-from collections import Counter
-print(Counter("hello world"))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_current_directory():
+        return os.getcwd()
