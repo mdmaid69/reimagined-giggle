46f9dc99d37479ad79d99069dd773463632c6701
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:39:28 +0000

    gentle ordinary hold slowly suckme

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
