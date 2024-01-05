580fa8e825bf7118dd08d7720cf24e52b6a87825
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:33:07 +0000

    bin practise marry meaning company

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_max(lst):
+        return max(lst)
+  def convert_to_hex(n):
+        return hex(n)
