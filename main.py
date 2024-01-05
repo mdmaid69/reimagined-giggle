4cfe9024215f60e962dac58e31475403bfaaf518
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:58:45 +0000

    escape neither stand turnon sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_list(lst):
-        return lst[::-1]
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
