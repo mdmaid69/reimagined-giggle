3fd081f3e78f79fae7be4704ac6a5b68ef68755d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:13:23 +0000

    stop mean waste short bear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_item(array, i):
+        return array[i]
