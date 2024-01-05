781ff9598e50905b0491f5c0e4e2f28cea786053
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:13:53 +0000

    interview appeal total variety convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_pressure(force, area):
+        return force / area
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
