e6b675f73eeaba4be3d6f4a3acaf2135c10a2f8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:32:01 +0000

    win flow off number unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
