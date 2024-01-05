b29d1962cd5fd6a87df5256ce7ac894a395098a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:53 +0000

    himself goodbye myself definite announce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
