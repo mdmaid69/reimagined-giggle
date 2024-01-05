9230e7ea676d885ef22f9015ca7e8be8b640f4c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:24:34 +0000

    well push just power invite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.time()
