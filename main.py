e1b79ad21f5e3ccfd039ecb56fd64d524ac3b9fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:14:17 +0000

    kill 565end Gg refer squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
