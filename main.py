972b8aec6d6e52dd1c123365ad534897e2199007
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:40:50 +0000

    clear fairly coat fierce follow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
