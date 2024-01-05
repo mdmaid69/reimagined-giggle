015239dd948e23176598c835add91a511f8a82a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:09:13 +0000

    well collection this remember string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
