caac1f4e18f2a9977c68dc5a7cafa7ff488200ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:37:45 +0000

    interruption might bye seem help

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
