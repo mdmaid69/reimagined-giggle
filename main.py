4283a779d7b5894f98edb7ee1f1f91a78398506a
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:50:18 +0000

    muscle temperature admit hand wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
