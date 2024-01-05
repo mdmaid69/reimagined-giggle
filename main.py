5176e64f0a1d8f43d8c6ec648164d0ecf6b94c51
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:08 +0000

    height flat possible your fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def cube_number(x):
-        return x**3
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
