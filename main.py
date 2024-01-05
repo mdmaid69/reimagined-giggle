d288211f671d48728540146d41c324f2ff3cabe5
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:03:18 +0000

    long impatient Cc own nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def reverse_list(lst):
+        return lst[::-1]
