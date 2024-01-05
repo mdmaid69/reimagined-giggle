05efbf966e53360db26542b193391767d36a9bfd
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:29:06 +0000

    read fashion manner sore seriously

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_length(array):
-        return len(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
