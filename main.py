133b97ba9f6d4d0aa51c49b82790ad4df3899b91
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:21:19 +0000

    Ee priority introduce improve question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def convert_to_octal(n):
+        return oct(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
