944c5cf2836ca74d522a4cc013e98a86eb75989b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:35:33 +0000

    lonely nature advice first offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
