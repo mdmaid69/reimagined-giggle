7d623ad4d2cc0f2351c61d91e01dc80cee774d91
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:28:17 +0000

    whole risk congratulate hers already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
