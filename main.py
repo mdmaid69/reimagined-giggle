caef6ca7db9610f74ad186a0b61ad796cfc644f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:50:41 +0000

    motion contact meet university justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
