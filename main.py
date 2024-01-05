836ff194d9da9f574e5febaca5cbe05a6457f4db
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:33:55 +0000

    make sore acceptable left sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_item(array, i):
+        return array[i]
+def convert_to_binary(n):
+        return bin(n)
