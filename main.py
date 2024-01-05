0dbb85445eedc52261a93ff460ff6a2984271fa6
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:02:49 +0000

    underneath justice top cry desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def remove_duplicates(lst):
+        return list(set(lst))
