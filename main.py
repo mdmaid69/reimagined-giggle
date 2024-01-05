e8642272f03b5a17240a1143c29f41955737a3d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:37:00 +0000

    godown stage rush mistake frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_string_from_array(array):
+        return array.tobytes()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
