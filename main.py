1ffecdc7be4becabe1e4795e868ac44944c40b93
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:31:42 +0000

    build field kind half gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import collections
-def create_stack():
-        return collections.deque()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
