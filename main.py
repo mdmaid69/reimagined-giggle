52ac2b16c0914af7e4b52308d8e1596907050483
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:34:17 +0000

    wipe door live front flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
