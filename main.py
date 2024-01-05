d7fd541419223057dd7b13dddd7c6e71860a635b
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:02:04 +0000

    empty share whether bet before

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import tensorflow as tf
+print(tf.__version__)
