82c6fcd1f5669e389778218b010c1bdb555a9d10
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:13:21 +0000

    shut cruelty wish further recover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
