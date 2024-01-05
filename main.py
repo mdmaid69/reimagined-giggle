a1b561659fa73f2095fb8c60d68aeba0f7961d87
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:32:19 +0000

    discovery young artificial send noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
