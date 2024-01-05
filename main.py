bfbd4653e563ab3ed2bfda064247c6ab6fc58630
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:24:52 +0000

    class machine common mind responsibility

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_ceiling(x):
+        return math.ceil(x)
