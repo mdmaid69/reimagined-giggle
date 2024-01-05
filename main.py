2373a48d801d8a7d6c6e7f923ffe7bc59d59a262
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:13:47 +0000

    miss total peaceful too sensitive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_power(work, time):
-        return work / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
