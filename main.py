d639ab3dc2cf8b6a7396e15b2827023ac25ff670
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:43:50 +0000

    wherever organization breakdown upwards call

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
