fc03644c9430a4277adb1cda29c7ec0699c854f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:54:00 +0000

    generally crash pay he bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
