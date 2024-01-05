9eed930bf9a4586c863cea39f0409106f3853bfa
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:36:03 +0000

    loud impatient building matter window

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-for i in range(10): print(i)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
