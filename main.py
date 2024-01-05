0c5ac931d3e69354f1ea4b47fc0a2019a5efe066
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:36 +0000

    wing tax building description themselves

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def reverse_list(lst):
+        return lst[::-1]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
