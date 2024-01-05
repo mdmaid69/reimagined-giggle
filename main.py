9373940d1695089c2c4ec0c1f4e1e7d444e05e84
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:28:06 +0000

    thanks lookafter expensive priority only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_python_version():
-        return platform.python_version()
+def convert_to_binary(n):
+        return bin(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
