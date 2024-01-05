e6ed39ac3f97b4e00382ec2c0431e46fcc48e726
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:53 +0000

    smile wish combine journey straight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def subtract_numbers(x, y):
+        return x - y
