d7a0fb5eb8298969bee486671d3c91d0a4365cf1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:59:47 +0000

    save press closely care judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
