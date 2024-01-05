67dd65d537ab724338f81e811e0d37b241864bb5
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:43:36 +0000

    character rent tragedy middle radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
