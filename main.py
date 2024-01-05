559d3b52c006e7e9ad07d612d105c32bde9aa8ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:10:05 +0000

    belong mass tape basically bye

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
