c41dcff5e165a045ee1487079c40de1ed6be3b31
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:53:12 +0000

    correct please charge regularly break

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
