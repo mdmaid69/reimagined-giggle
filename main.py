63fbd90cc6f00f9529c63a402032ff02d34b568b
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:13:32 +0000

    treatment but politics cut message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
