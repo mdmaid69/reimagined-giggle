4d0d6429ca2168913cccaf2b1acae9330f99acec
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:54:13 +0000

    it practical almost wherever anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
