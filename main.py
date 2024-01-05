d88d6a50fbd4fb95ca1c2c9a267cb903aa0b880c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:55 +0000

    window tip waste block clue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
