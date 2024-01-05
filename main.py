e3eef1f940d19ca05950059670eebb950ec85f06
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:16:58 +0000

    lonely tread plant turn cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
