660a32db75738ba2299134b84a5739edefe3329e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:23:31 +0000

    violence list listenfor name moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
