532e168d47968e4fe5b36aa9ae56abf2fd6ab230
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:35:15 +0000

    grateful crazy will shout bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def count_elements(lst):
+        return len(lst)
