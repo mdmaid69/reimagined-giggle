cf783e895e3ad424dd699e9b928dbd6ef3ad73bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:06:21 +0000

    cruelty eat door type pinch

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import platform
+def get_python_version():
+        return platform.python_version()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
