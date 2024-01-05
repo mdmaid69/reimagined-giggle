94f9f5310b5cbf9695c0217a93c03d04914d58e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:10:43 +0000

    top speak short ocean continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
