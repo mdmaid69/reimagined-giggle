02f84d4f361858458105d9085a129593b4779499
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:19:14 +0000

    education interest noisy patient circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
