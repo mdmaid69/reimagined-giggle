b474bcddde685a2db30179ab938d27e22fe10837
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:36:10 +0000

    decide twist crazy you talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import random
-def roll_die():
-        return random.randint(1, 6)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
