967f180f58debbd55319b01cb61c28680858a5c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:49:06 +0000

    eat how read occasional no

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+for i in range(5):
+        print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
