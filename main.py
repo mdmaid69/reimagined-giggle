015054d751cb9702daf4b3dfac536edacb789c8e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:56:57 +0000

    serious among past ground strong

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
