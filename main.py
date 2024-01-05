14b59e7555b309350ec50d2eb5a519a933d16ee6
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:47:08 +0000

    bend yet afraid which furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
