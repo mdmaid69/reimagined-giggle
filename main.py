31ca19b1b2c0e66529b311d39b057c0259aa71e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:57:41 +0000

    chance fresh conclusion concern swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
