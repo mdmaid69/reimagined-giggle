48b29378fec498985bd33dcde541d7bb7727f63d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:02:02 +0000

    owe gas belief callup popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
