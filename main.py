92b6a7ca6b75f6f7dd22ef6879d8978a913ae86f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:37:08 +0000

    violent expensive money tune stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_queue():
-        return collections.deque()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
