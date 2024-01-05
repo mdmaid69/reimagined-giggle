6e1d1bc0b97f4bbbde7698d1ff608d53c28b528d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:56:50 +0000

    transparent rubbish reply mad bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
