cea1b0f2bde5d8956b7285c6efb03deafc38afad
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:04:54 +0000

    uncertain cinema almost cheat that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
