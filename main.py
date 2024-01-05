065fd8ed56fd5000c3193716204e8d73a4706940
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:14:19 +0000

    fasten shadow rubbish string type

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_tangent(x):
+        return math.tan(x)
