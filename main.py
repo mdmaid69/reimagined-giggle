368c8a7ddf8becbde8acede748f467dea2c90ce8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:29:14 +0000

    gas complaint although lower surprise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
