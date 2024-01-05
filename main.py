c3bb7027b9a4e842a97f75d03eb1d31bd4d8793f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:55:06 +0000

    laugh underneath towards closely sister

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
