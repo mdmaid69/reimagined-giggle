19d95e870daa56e51beefeda1e8c37c7f6fd5775
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:54:49 +0000

    again undo quick afraid wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_slice(array, i, j):
+        return array[i:j]
+for i in range(10): print(i)
