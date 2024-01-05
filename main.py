a64840255ce4f395ffc39797c0a27bb6cbe364cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:39:27 +0000

    nowhere twist ocean finish centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
