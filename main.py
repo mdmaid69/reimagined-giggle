e138299a5a76b12d239b019fc0d766edac6de043
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:33:24 +0000

    cope by after stamp crime

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
