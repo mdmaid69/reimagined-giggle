02e978c9f646fb954bc3d5f7beed234fe0faac18
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:22:16 +0000

    willing load awake shall no

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import array
-def get_array_as_set(array):
-        return set(array)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def get_array_as_memoryview(array):
+        return memoryview(array)
