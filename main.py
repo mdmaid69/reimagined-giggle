7616d39a03ee8a113f83314b32631988bceaa706
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:58:25 +0000

    bury grateful let pupil admire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_unique_words(sentence):
+        return set(sentence.split())
