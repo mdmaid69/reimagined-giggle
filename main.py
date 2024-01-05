bbb38549f542f6698828632ddd911acd374468e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:27:11 +0000

    rule scheme rule Ii turnover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
