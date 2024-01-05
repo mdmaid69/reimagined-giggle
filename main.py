cfa642f189b5273e5b5efbf2ba904306ec6474ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:49:39 +0000

    influence building peaceful wind stand

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
