0312cf657f7596f125aab219977854e08f20181a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:17:23 +0000

    disappear rest contrast place comedown

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
