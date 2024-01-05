93b9276c23814ee6209a3d6980da61f30c1b8880
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:08:45 +0000

    certainly advantage wrong disagree hair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def count_characters(sentence):
-        return len(sentence)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+print(os.getcwd())
