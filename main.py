cb6f280f1f176b71dbe96986c9bee69fb0ae3aae
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:36:40 +0000

    memory eye dish weak surprise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
