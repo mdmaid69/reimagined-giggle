3efae9bc247d7cb898848b552e2393262b5f1992
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:39:43 +0000

    left occasion completely on consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
