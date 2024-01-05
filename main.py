04b8db137fee2e4e4b720be94f0872ff3b8e7623
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:11:31 +0000

    unable operate disappear arrive chin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
