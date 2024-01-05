44c91342367729d7d6ad1e424e23d1bc8ec3c399
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:01:54 +0000

    middle family different block operate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
