8a7ef0b5ec83b649ad3e637a1235bb160cea71f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:58:25 +0000

    tape port passage support our

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def delete_file(file_name):
+        os.remove(file_name)
