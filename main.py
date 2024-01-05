3cec2bac48fb493351f226ded79e663cb8e1009a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:37:17 +0000

    method workupto drop question comedown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
