98081edf419bae6be93b695bb23a27b183d3e6dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:51:29 +0000

    can admit bite just forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
