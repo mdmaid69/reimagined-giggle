79556dbd8ecd6d452bde2d5ca4a6ba51eeadcf03
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:02:53 +0000

    couple usual except bar angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
