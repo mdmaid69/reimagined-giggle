f055a260a8ac00da8c793b5f96518be2c85965b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:48:07 +0000

    generous detail concerning thumb 572enter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
