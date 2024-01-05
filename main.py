a91ce818a317c20a6e020a067c8cb9af7da1407b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:20:05 +0000

    knowledge separate proud provide might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
