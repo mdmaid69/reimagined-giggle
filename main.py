268253d4fbd7c88551407e20e63ce0f027de954d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:06:43 +0000

    snow swim react criticism wake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_user_list():
+        return collections.UserList()
