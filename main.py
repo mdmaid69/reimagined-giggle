daae820089bd3722a3a6d900ae3bcbee15cc4f36
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:32:13 +0000

    glad few concentrate rank runaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
