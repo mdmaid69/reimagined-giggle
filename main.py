c8ee8a91e4607636673a96a2497737572b540da6
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:34:40 +0000

    delicate exploration report push cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
