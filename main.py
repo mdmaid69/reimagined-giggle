722ec3c2bb1baabfd1bf3131364f7aada96e2425
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:46:11 +0000

    judge claim centimetre advise ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
