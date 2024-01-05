f87f67e19a16c96b7c0feb84e2fb63b34be5ad23
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:14:42 +0000

    rain cool somehow report study

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
