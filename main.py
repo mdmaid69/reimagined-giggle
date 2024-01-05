62111ca22a90137a0aa5c8b65e8a7badf09d860c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:34:06 +0000

    cum game pleased public work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
