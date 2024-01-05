85e0648608a579e40969ea29463e7999537013a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:13:16 +0000

    watch perhaps ill book for

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def is_odd(n):
+        return n % 2 != 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
