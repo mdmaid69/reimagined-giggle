f7f8f63e07b2f714d5719f4b7d3750432e3560e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:05:11 +0000

    house pity attitude ground physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
