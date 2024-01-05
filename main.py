91735352fd62354fe68cffc4e50c4aa5342db968
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:45:13 +0000

    myself wait comeout herself keep

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_array_to_string(array):
+        return array.tostring()
