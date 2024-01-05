108ff839f5b2a57d4e692f94fbe0871da853a02f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:26:16 +0000

    exam cruelty damage another considering

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
