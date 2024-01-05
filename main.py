fc3d2bf118e49afd52ac8872798b42240bcf0f62
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:14:25 +0000

    careful bill acceptable believe score

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
