59df68b8cde1471d408ccdf9402593042e624276
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:00:05 +0000

    strength apologizeapologise friendly worth union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
