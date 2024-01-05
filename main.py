a30eaf6d1b22d86f090bc0074dddab9a4da8e4fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:09:53 +0000

    under brush depend hello culture

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
