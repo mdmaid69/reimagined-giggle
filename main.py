893203a7ff2aa238bc9ca609e34718523ee83e2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:59:30 +0000

    flight control firm trap in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
