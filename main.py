b6c10fa2efc40b08107217a71446980a25c10aa8
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:04:02 +0000

    comment Itself lookback(on) traffic favourite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
