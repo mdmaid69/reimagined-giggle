9d63bb72e629dabde4ebee3e4fba6df4a3ba91fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:03:00 +0000

    muscle loud tone tone tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
