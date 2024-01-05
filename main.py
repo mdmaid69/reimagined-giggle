c9c6afc27561677cd61eac77a5e1baaae7ea7e5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:39:46 +0000

    experience phone statement perhaps borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
