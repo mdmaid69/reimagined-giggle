42b6b95015a9d6a188dd15c27c3380ea82843cd5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:39:24 +0000

    should card squeeze imagination clear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
