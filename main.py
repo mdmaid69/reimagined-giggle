74d47c9287fa84f728a607dc1190af789d6be03e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:10:34 +0000

    phone rid before club taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
