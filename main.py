85fc371285f4785410bf29a666cfa5479e361551
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:42:14 +0000

    when turnover recommend friend whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
