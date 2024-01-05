0d217e7484c3008ec5cbc7c7a2b4f9d393d3c86c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:19:19 +0000

    perform well defend firm turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
