d56b1dfd1b82b8f1577654ffe7952ae01cd7f67c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:05:15 +0000

    appeal considering tongue issue crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+print(time.time())
