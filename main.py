75f5ea6020b8fea096e179cd878175b730f2bcdd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:37:37 +0000

    certainly interruption notice neat stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
