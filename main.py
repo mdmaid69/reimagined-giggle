af329e4296cd35a704a3288b1b13a1fa2489f728
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:11:41 +0000

    notice ahead behaviour unlikely switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
