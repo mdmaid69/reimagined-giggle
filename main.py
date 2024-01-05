6b9716ca6a3214a535aa970ee58a01ff1b7dbda0
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:59:04 +0000

    welcome outside happen past bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def reverse_array(array):
+        array.reverse()
