52f505f1805645c8874060789d3d198976f31247
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:15:37 +0000

    warn related important slide dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_bytearray(array):
+        return bytearray(array)
