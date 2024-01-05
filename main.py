ae060731d23b43721608b65ab4952181af85a516
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:37:23 +0000

    workon many rush miss track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def remove_from_array(array, item):
+        array.remove(item)
