ffcb53d2c0074c2037bbaf1bf8ed6de39ceb743b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:17:10 +0000

    best home judge behave equal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_acceleration(speed, time):
+        return speed / time
