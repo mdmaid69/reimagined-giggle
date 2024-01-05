46252f9c0baf4d411542ce87bea751d0f637e981
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:27:40 +0000

    direct change guess major night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_sine(x):
+        return math.sin(x)
