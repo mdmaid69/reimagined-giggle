3d4056d8c81896760f85d134e891b750df731570
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:00:14 +0000

    their Rr connected table Ff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
