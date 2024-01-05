3d07c0b2c1ee5b781431f0ace6e62c4efc830dec
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:23:03 +0000

    getup practical size contact skin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
