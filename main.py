945833d7739d9a1c5e1c4bb80154174440382bc5
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:17:31 +0000

    last treat herself exchange especially

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
