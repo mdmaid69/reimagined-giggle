f7533aa5de73ca6ec3dbae61a581a88d2537542c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:06 +0000

    rent typical call hello cause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
