5aea6aea3eab55a30e0b9ba27000f2525e12ab43
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:13:21 +0000

    flavour interview rather harm gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
