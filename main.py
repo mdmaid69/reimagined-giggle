4cc5307386a608305ff3885b4f6ed3cce2af9a52
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:18:08 +0000

    mean illness comeup trip pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def find_min(numbers):
-        return min(numbers)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
