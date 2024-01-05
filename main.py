0453e6996140a69551a83a79317a997df754cc6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:00:25 +0000

    frame talk word line seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
