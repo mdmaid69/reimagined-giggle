e4922436571c86f4de6ceee0ccc5afdb99b104a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:17:55 +0000

    unfortunate top performance sad grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
