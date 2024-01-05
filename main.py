4bd617681c0ffdd71c57a0146a2d57fcf26fda54
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:12:44 +0000

    entry badly wide between rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
