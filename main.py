b7a13cf632416ca01fdeb2dae8e4939e61caf5b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:21:13 +0000

    stick subject kind flow goon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
