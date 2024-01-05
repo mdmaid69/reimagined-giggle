18d442de9ed24e566f57f7959ec8db7aa8c4ddb1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:49:59 +0000

    error smell male back promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def convert_to_hex(n):
+        return hex(n)
