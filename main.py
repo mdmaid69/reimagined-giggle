e318a87897b39cee32374f29c2da2e245ff8094a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:55:36 +0000

    concerning fresh contrast handle hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
 def calculate_annuity(payment, rate, time):
         return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
