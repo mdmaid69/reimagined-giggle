a1104c1a81130fcb7bcaea152a317d85a76efaaf
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:02:57 +0000

    final people impatient 572enter run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
