5c329b58d4bd87cf9938a93207b1363c3246c6db
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:24:50 +0000

    ill spoil so communication bound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def count_elements(lst):
-        return len(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def append_to_array(array, item):
+        array.append(item)
