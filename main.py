b78a4983f14465411eacf21f1082c8abdaf62646
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:46:23 +0000

    hair respect scene create order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
