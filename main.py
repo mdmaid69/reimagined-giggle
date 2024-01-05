0caafab6b590b65e80a1accc773fbcba2a295f35
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:18:26 +0000

    make at attractive hope pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_pressure(force, area):
+        return force / area
