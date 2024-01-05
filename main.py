fb96692296263e72688430bc63a1f6e3f205ccfc
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:27 +0000

    keepup official tell ear last

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
