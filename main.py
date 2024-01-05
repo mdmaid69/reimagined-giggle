45decbcaced335ec57bc29f840d5642006519afe
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:47:06 +0000

    speed further exploration wander man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
