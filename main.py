26e98a54b30fab2ae1c8846404dfc98b32c5dddf
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:35:33 +0000

    consult activity holiday trick last

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
