4939a64fe6c99fadbf73ace5e798cbbe95f3d35d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:05:43 +0000

    Zz dish experience state back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_acceleration(speed, time):
+        return speed / time
