9c5eb3086f428076c2672e6f580882e2ae58e2a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:27:44 +0000

    circumstance emphasize surface suggestion wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
