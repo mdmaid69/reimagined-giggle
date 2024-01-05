487eb54cbba3bbfdda30ee05b22910fd72b0f393
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:17:32 +0000

    behind cool project could expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
