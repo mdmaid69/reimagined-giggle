44e118926126a801aedeeb0247fc73dafbfb41c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:01:19 +0000

    loud correct them or switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def sort_list(lst):
-        return sorted(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
