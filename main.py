930f1e367aef90f7c503b9eaebf3bcca256407bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:28:13 +0000

    distance anxious well demonstrate business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import numpy as np
+print(np.array([1, 2, 3]))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
