aaa3b9f35c11c17c899081dcf9fdfa7f1091dbad
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:01 +0000

    whoever public earn active start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
