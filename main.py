14fb0576c1462a2dcedfdbb1123b17428d3fd22c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:04 +0000

    frequent connect offence ought illness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
