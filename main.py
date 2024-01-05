ffffc8ec10db83e41d7ca0fe810ddbbee41c62a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:40:27 +0000

    shade trip dry none agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
