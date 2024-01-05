3cead2079e2402c88aa2943db3b21a943799dc15
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:12:14 +0000

    somebody pretend effort strong OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
