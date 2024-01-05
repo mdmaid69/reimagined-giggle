5cd992eca45b2d4555b5dd2d4c6361528498a7d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:20:26 +0000

    skin last challenge game otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
