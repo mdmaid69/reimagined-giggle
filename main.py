efc35fbc69e64f90fae93b6cbca434a39d1631e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:17 +0000

    crowded phone runaway marry comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
