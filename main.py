0ebe00b29d58ad8ccfc554d7afd26dc60980f8da
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:05:15 +0000

    rid generous so tidy before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
