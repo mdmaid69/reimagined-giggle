a81b4c03ff493d5996db1349a63c63b299e9cbd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:33:04 +0000

    line view answer scream behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
