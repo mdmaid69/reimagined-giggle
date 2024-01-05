3392f3e28e35a587ae50a74425267351f65326ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:53:19 +0000

    afraid survive middle strength lean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_speed(distance, time):
+        return distance / time
