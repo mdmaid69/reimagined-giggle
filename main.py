d8874fc609037d142ef71169480cb08ad4f3b845
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:15 +0000

    rest tape load priority letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
