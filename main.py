94c841cfb1c138223607de047555b553a7332dc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:23:28 +0000

    Ww bring add callback ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
