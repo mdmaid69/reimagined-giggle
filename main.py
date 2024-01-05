98abe24eb1d1e500baaf18341f04f560ccd6a09e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:00:48 +0000

    persuade kind considering sky on

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
