e83f9f9a97dedf5033219313ae1d4f4036b3906d
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:26:38 +0000

    tomorrow test track ought record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_counter():
+        return collections.Counter()
+import os
+def remove_directory(path):
+        os.rmdir(path)
