b546f82d8871127631d6dbdb478f2616b54bd063
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:08:38 +0000

    spring record change word star

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import tensorflow as tf
+print(tf.__version__)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
