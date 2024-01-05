eacb657b64fd4feefbc654ee4aec9ce62aa6135b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:07:16 +0000

    character squeeze force turnback get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
