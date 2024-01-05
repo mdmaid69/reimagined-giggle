aaad22f99cf6a953ea056356ce93424941d993cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:47:24 +0000

    towards scream beautiful fat heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_area(radius):
+        return 3.14 * radius * radius
