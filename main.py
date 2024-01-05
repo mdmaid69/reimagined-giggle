0c6fb81a0530947925ab49d83db7961121c8da1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:35:04 +0000

    about also present fence lead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
