7716aa5a32c3402fd9178bd34defa16b50ad2bfd
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:42 +0000

    storm land give squash wind

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
