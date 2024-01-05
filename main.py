f3aba692fb0c9da95bc8edef79d7c5e0fc9824f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:52 +0000

    connection very against usual ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
