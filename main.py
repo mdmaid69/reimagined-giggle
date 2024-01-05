69c422cacee18df954132925f0686a602f8dfe5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:32:24 +0000

    wipe father university reduce unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def reverse_array(array):
+        array.reverse()
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_permutations(n, k):
+        return math.perm(n, k)
