28501003f1354a5f274c54ce03855948c95f4aa3
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:26:23 +0000

    skin love satisfied field model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
