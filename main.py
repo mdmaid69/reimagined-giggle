7e95cf0df171045e3f589d643fa484c6fb12f6ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:31:58 +0000

    arm it should enthusiastic mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
