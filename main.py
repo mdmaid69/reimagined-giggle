c02642ee035e3c7eb82e8f677a4d3e6a681da0f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:29:53 +0000

    sometimes active hair impression manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def convert_to_binary(n):
+        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
