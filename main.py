ca22f86e3a79866127a3cbc0f268974a52d0dcfb
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:36:48 +0000

    record stamp along sound underneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-print(sum(range(10)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
