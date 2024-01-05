3e0b174a22d1a9e8fa74ad9675ecb5f4ef41d43b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:13:33 +0000

    land original cum duty prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def reverse_array(array):
+        array.reverse()
