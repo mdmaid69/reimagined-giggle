ed1c6b28b26a74b3d732255000a8cbe92c34eb7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:41:46 +0000

    cheat leg snow murder save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
