27658d701986076caae16bde726a0705c8a89201
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:17:35 +0000

    bridge hard personal callup door

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-import time
-def get_current_time():
-        return time.time()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
