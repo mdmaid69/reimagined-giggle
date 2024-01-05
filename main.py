46ae887f2eac1e6a441f4ff3b1af5c276f72bece
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:12:40 +0000

    escape willing full copy takeout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def delete_file(file_name):
+        os.remove(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
