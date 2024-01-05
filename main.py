aecfc8a8f426641ef2acca73e7a221adf5c22632
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:08:22 +0000

    dear often he communication surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
