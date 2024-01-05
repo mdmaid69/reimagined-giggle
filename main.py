5584f4b550af8cd6075f7ad0c9438768f0ab3358
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:32:03 +0000

    day stop draw begin battery

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
