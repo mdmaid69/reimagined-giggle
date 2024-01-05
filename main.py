bd439cf37aa5c616efca07208574db2cf3e34dc8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:56:35 +0000

    realize me my house exciting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
