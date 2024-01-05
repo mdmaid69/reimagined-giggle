651b3bea4f53d3cdcd0b627f215fc02f7ff62357
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:25:31 +0000

    with basically fit scheme shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
