a2599ef600470ac3e8ac2ece4d626403b28725e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:26:00 +0000

    major disappear manner anybody pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
