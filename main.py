cfe1c5a740cb3627c2ac7ab9145cb623bce543e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:32:13 +0000

    hello thought close thank understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_str(array):
+        return str(array)
