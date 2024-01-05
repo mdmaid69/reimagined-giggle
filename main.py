e19b633f81f6317b0864bdffb5fc5610b4ecc1e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:34:41 +0000

    start steep someone picture comedown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
