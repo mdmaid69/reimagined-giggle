41166a21ae7846331a1bc85467c9c591d84ea408
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:06:12 +0000

    operation feature flow bother attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(x, y):
-        return x + y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
