c4796c98a456134b826aab6ce34e7bd26f20ffc5
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:36:37 +0000

    bringup label load gooff nerve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
