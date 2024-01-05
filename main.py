a24e8111b0ef8f321d2ef1fd2d838f5268f70d01
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:32:50 +0000

    interested himself interested miss glass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print(sum(range(10)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
