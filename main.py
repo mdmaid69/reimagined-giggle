f5a4df4ca99d404719017c616d15b8c6fd27e2c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:56:33 +0000

    pretty steep appearance either no

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
