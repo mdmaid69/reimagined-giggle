3dbf6e0311e7c4504edad70f065a351df782bcd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:39:27 +0000

    loss holdup clearly top wrap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
