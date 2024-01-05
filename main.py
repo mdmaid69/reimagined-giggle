d9834faf3c381cce0b994009dd5c6f289a5b1a34
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:59:49 +0000

    rent admit passenger wave music

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print("Hello, world!")
