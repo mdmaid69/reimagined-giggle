fcb2d5596db9a649f4c7d6928efd5816379a2207
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:42:02 +0000

    what immediate balance figure dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def cube_number(x):
-        return x**3
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
