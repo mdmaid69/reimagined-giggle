f01ae2dd5d18e6e2b0122782167e55bd51ef03f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:09:37 +0000

    myself temperature occasion Bb through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
