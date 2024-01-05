fd5a6de1448b8b507502d62ad3a37585f9bfbfdf
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:37:21 +0000

    hear patient right platform frame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
