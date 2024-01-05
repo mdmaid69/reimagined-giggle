ec652c6c5e8d00d015373b5b149dde927bbd9c65
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:26:35 +0000

    innocent traffic state accept emotion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_length(array):
+        return len(array)
