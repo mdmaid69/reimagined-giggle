c89f832d9c5f1034335a3c1d9f629bdeae067074
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:54:19 +0000

    have hurt throw divide rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
