aaeff67de5018f0fd9225123d08c1f4bb494fcad
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:59:11 +0000

    experience frequent standard comment Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+def exit_program():
+        sys.exit()
