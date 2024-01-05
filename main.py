f5fc5daec4c43d9f852d4f05ce6f560400017f6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:03:53 +0000

    tone apply interesting card height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
