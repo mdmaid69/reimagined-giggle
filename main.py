ba1c434c4bb3cdf6528c50fef3f6e9c5805a11aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:15:07 +0000

    past so together start dream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def is_odd(n):
+        return n % 2 != 0
