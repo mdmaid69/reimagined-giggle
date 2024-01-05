f771bf2a99255e4459767a5407f932d667eb6fb2
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:06:11 +0000

    interfere rely usually Qq can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import time
-def get_current_time():
-        return time.time()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
