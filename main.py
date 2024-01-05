7cfd58ac28ebcc81f2c80fe10ab18f223d683499
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:48:33 +0000

    most turnoff sister off pain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def convert_to_binary(n):
+        return bin(n)
