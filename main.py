2550eb8b9d2ebfd16db936c728dc3e3017ea37bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:28:06 +0000

    in deliberately dark keep affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
