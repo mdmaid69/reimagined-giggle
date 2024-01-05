cd72e4ee431ebed3914eef262ff096f71e7f6296
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:20:39 +0000

    switch minor boat plate cost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
