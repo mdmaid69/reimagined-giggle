d2f02d3f4626096f7b6406d8808a461ee488e934
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:05:37 +0000

    crowded within slice 569enjoy strike

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_float(array):
+        return float(array[0])
