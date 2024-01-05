2f635efc451b5b25883cdc0e09bd61d858b7224b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:44:50 +0000

    connected lookafter forever bet entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
