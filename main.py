41ff9077bf0752bc8d3db93b19d0a6fb66b31fcd
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:26:37 +0000

    talkback sore you breakin mark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(a, b):
+        return a + b
