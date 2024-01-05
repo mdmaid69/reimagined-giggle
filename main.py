a8f5a4ce653151e34090f9d968baa9af22e792f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:09:58 +0000

    gather bet house touch though

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
   def delete_directory(dir_name):
         os.rmdir(dir_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
