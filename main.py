b6fdce0d52350067ce060d4ab84164c52da454fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:46:10 +0000

    brave what nerve thin kind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def cube_number(x):
-        return x**3
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
