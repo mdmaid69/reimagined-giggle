2308c2899dd0923abd4eaf75443ef463b79accab
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:50:29 +0000

    inform guilty flat reply feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
