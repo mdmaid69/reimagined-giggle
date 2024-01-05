d7e311bfa122ddfd464704c831503356dd40447a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:00:52 +0000

    little right concentration certainly instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
