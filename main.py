24886f69b9c9e4142809312c009a1ac8a0c34657
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:58:38 +0000

    order scheme gather trap press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
