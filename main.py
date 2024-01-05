a164afdbc5d8ae1b78fdb8239d24d55e9726e614
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:24:00 +0000

    second treat rarely understand me

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
