6ecf161697c1c1530fd18cd60b04cb59477525b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:39:57 +0000

    waste operation question invite foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
