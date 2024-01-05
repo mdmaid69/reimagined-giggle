a6d207a9da4cab53a71d23c5fed08936661b6a68
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:00:32 +0000

    manner waste interruption instant below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
