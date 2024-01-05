4af5d31952bfd516140e4c24f2cead532f7127bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:46:13 +0000

    comeout television disturb fight marry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
