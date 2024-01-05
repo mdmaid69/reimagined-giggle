27dbcf2a48afaf67dbf01c7f1749a3f0a5815a26
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:12:07 +0000

    glad positive well dance seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
