36c979a1c9768646e8275844405b74e77bd795dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:08:50 +0000

    no nor morning worry convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
