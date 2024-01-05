44f5f019fab2aa0c98758606e7874aaf01c420e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:29:30 +0000

    remain company tidy pause far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
