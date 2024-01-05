b913fbeff125ed8f1fdd30faa6a7979ed72b8e02
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:33:18 +0000

    disgusting recover arm related gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
