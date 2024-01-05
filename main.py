0a6bf2f456ccc2ac4d280d80ea7fd36bc0ac64b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:21:45 +0000

    apparently worth card start suggest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
