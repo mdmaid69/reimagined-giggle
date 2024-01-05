1cce479d8c3182f465f00c2774195e2eb7137813
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:40:49 +0000

    business succeed move lost well

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_array_item(array, i):
+        return array[i]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
