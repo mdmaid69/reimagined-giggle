df6deca9cccac6eaeaab0f10d8e23e611e06bf34
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:39:29 +0000

    worth better hope wrong call

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
+def cube_number(x):
+        return x**3
