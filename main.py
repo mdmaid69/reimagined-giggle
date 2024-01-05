4ef111785d94a418350350ccfed1d61afebc6da0
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:59:36 +0000

    fine roll shelter help healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
