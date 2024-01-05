4e4f6f0e92f8e2c524e9e05d53f3befbabfeefa8
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:11:58 +0000

    hold manage culture by ought

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
