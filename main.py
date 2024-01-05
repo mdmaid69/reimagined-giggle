2e43d9ff60d5d78abae4cdfdb456d1b430359088
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:32:41 +0000

    funny ever certainly cry earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
