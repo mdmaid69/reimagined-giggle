8d0a5efbf267fe791b2c83dc05de2e12670f25a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:04:13 +0000

    central his goback(to) majority city

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def cube_number(x):
-        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
