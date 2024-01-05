2dffdbb867a83e34746d36cd29d37cdc9f42411a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:39:46 +0000

    acceptable nature skirt ourselves wind

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
