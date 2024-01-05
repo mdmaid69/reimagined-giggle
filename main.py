438d98a5cfd8939488a884a2ccfa25cd6ce181b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:04:09 +0000

    mainly responsibility back clue recent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
