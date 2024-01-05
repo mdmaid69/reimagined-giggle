69426d3a0aae887e866217dccd94617431fd567a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:47:52 +0000

    awake repeat allow speed vanish

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
