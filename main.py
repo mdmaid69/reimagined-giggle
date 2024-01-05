b574a5db72711a9d648c3e9763307507d7b344b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:55:45 +0000

    middle combination experiment Uu everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
