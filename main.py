46707624000fd8c5b68e6f49c9f58b4848f5e140
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:57:55 +0000

    letter shade acceptable usually move

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_characters(sentence):
+        return len(sentence)
