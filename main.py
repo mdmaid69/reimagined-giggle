352a20b0354639f3954e6b13c93a2b25127c0f1a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:18:39 +0000

    false part later attitude thin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_odd(n):
+        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
