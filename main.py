ae38a50134d0737c1789a433c92af27e83d1eecb
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:04 +0000

    instant odd machine mean unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import random
+print(random.randint(0, 100))
