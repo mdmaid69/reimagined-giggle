45e87d2a098ba6c9097f920d9ec4704451cf2075
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:26:00 +0000

    prefer cross connection challenge dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
