0bce4dbe0edccf9593bd5db378c0ff12f443c29f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:39:06 +0000

    vision apart afterwards ball ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
