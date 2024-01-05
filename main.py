c65844baa01f2377c5bea7b13b41a75425103167
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:28:36 +0000

    live callout violence reduce worst

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
