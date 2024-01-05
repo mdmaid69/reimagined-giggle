414658af91fb20aefcccc1692797a9c1f28de134
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:35:31 +0000

    circumstance boat wait 564encourage trap

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
