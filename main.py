8b59e50dab0895bee6b2cbb6ddf49d04a6de6fa6
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:16:12 +0000

    exam along pressure funny bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_speed(distance, time):
+        return distance / time
