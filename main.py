50ef2f340897c0b58b5dc5fbcc98e4d7f5161b6c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:32:44 +0000

    pot runaway minimum unfortunate wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
