e27207202580115549e6c363bc98b6d4944bcebf
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:22:35 +0000

    related strike hit disturbing someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-for i in range(5):
-        print(i)
