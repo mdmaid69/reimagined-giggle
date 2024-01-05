431772e575d77d42aba1a5ff7bead36109cba442
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:30:01 +0000

    future doctor among public proof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   def is_even(n):
         return n % 2 == 0
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def convert_array_to_list(array):
+        return array.tolist()
