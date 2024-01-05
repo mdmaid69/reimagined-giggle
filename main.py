4e32252a56c517c797c9d3a86e775089a9b8e44b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:30:12 +0000

    keepoff ear bag stay fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def is_odd(n):
+        return n % 2 != 0
