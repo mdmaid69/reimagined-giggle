6ddd186df09d8d42658d517553fc95000c44baac
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:52:52 +0000

    file twist illness prize company

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
