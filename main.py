bf5c693de0df47a92f79c2708e1e0c4009d3126f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:55 +0000

    woman world leg crash talk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
