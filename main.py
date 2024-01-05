4de39f191f7a0f3dd7c16a45af61adf60f909918
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:39:57 +0000

    inform warning laugh especially keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
