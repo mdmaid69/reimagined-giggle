5d91f53f949f93a12b16b547b3d1fb991a911924
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:54 +0000

    Ll shoot name loud bother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
