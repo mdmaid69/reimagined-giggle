bc91834f392ff46981c5d3387d147c17c0eca0e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:30:53 +0000

    heavy talk possibly listen duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
