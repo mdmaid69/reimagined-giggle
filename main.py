80d3b9fad2966e31f2e188063b1d9c90ce6fa673
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:27 +0000

    even dig couple mine hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
