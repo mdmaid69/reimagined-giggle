5bd2b74f768452a59d5a27f5e921fabc7cc86aff
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:15:14 +0000

    scrap search early what measure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
