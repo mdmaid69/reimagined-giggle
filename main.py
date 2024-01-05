feabf6b3441fa6a8e62071c77eef6746b7f1c9eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:23:39 +0000

    muscle fall getup stare order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
