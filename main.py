c40dad0b149bce8c25e935f5d975299b28d893e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:48:00 +0000

    track firm tragedy calmly worth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_max(numbers):
-        return max(numbers)
+def divide_numbers(x, y):
+        return x / y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
