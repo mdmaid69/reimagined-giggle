c2fb7ed44761dae33f25b6eb624f58955ac48ab2
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:22:54 +0000

    far set Vv flood protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
