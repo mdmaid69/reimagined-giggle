69f0dfa0cde31dba72d2f48acbc206a608cf41a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:27:56 +0000

    action immediately poison tap official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
