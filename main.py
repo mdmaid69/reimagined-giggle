17ac27e852e38757ea71700ece73aa324e01fa73
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:38:34 +0000

    comfort arrangement bit section splash

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_length(array):
+        return len(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
