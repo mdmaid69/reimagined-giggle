bc6424d33d291bef46c9749e013cf15d9dab089b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:56:39 +0000

    trip catch laugh female callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  def remove_duplicates(lst):
+        return list(set(lst))
