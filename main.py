d3a5898ca016bf08e1776a34a1569c89347b30d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:42:14 +0000

    sound intelligent beside runinto head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+text = "Hello, world!"
+print("Words:", len(text.split()))
