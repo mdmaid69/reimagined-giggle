1f4b250cbc84b14015aba366fba4626aa3a20b8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:37:06 +0000

    pressure advice hole order corner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
