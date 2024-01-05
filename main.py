f7955891abb34b08de7bb3686bb6245b948569e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:34:46 +0000

    trouble discussion trade image doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
