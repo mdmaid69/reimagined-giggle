67f4bcaf9eaab5b6c06161e468d29195c0080e71
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:04:44 +0000

    over fix disturbing mass pump

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print(sum(range(10)))
