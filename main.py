c199549111bef9c8859c4f9ce29282b01b447ea6
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:26:28 +0000

    rarely subject rent gradually build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import sys
-def print_python_version():
-        return sys.version
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def add_to_python_path(path):
+        sys.path.append(path)
