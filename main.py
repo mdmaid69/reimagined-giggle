dd4f1ad286698aae9b300d98c245b38bb44ec8ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:07 +0000

    praise sport in favour press

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import tensorflow as tf
-print(tf.__version__)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
