e038bbc778a11d83c2fb343495c92d542a84c685
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:55:48 +0000

    amount know night turninto communication

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
