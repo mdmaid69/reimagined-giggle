3eedb9077764597d4d2f3b13271290dd3cf538ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:23:50 +0000

    demonstrate conversation favour congratulate blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
