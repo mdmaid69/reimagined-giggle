7937f2fc8d28552ad3d6ab6c89c14c79d763d22d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:08:57 +0000

    increase reliable tune form call

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
