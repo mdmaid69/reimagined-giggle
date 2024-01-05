37edabfcb5826c7680e18267f88730cb8e31eadb
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:40:44 +0000

    port violence street who weekend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def is_even(n):
-        return n % 2 == 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
