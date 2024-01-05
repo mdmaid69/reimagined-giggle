6e89a625362b3182e065d3671325b2bdc34b2ca3
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:09:21 +0000

    pride table sore still disturb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
