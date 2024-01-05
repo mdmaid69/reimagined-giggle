da1d2448f0fc6bf78f073ee5f41d4791eee5a6db
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:22:04 +0000

    bye face runover world anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
