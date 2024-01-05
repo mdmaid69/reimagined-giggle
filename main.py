a4aaee45248065482f043a041dacedd045af95fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:04:39 +0000

    whisper first march safe Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
