f48b1feb218eb373dbc934508fcf5aec82c22630
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:33 +0000

    air dry avoid develop dinner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
