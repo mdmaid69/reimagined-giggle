fea25c5ee4bee6fcc16e0f910a89a51e06d8d82c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:14:04 +0000

    muscle forward say watch swing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
