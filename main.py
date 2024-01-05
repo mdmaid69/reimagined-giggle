9c0df927403868075011269a682b53eb39f6babc
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:15:42 +0000

    unable programme nearly dark warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_density(mass, volume):
+        return mass / volume
