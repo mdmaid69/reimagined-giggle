13bfa384d9128f72e0ff030b9ae955736da6cfeb
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:42:04 +0000

    punishment peaceful takeout fork peaceful

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
