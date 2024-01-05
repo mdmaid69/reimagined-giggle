9fe45875f7842b0c984b0f57f8dba45c44f85d35
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:21:20 +0000

    time cruel joke hold shame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_area_rectangle(l, w):
+        return l * w
