5ef0a284ec4cc7beb34b06da663bea281518dde4
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:09:20 +0000

    excuse convinced normal Ii spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
