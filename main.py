ea1d177f20c63d9761fd12fe57cbc7178eca51de
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:50:20 +0000

    middle believe extra them to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
