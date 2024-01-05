563973a75a75188ede0108b5d6287b415cacb28a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:25:31 +0000

    pretty kill major approve hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
