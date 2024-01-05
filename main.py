ef8bf86d659d1900d5ea13bdd8a2a712942161f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:41:49 +0000

    pair land broken comein accept

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sys
+print(sys.version)
