e9d8957488f5064cd5bfd6b4d4abdc8e6d3893e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:03:36 +0000

    her original scream responsibility young

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
