cbed197e832205042725173dfd0747f421b42f17
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:56:48 +0000

    terrible just angry unfortunate deep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
