41f6006159b281835547ef0e0523adbc539ee952
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:19:15 +0000

    curious admire naturally odd back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
