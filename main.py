fb4ce51334e1c7965da82b01a93cc41570a666d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:08:04 +0000

    listenfor ring failure deliberately read

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
