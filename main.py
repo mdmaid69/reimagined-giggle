7f1c967af18ab93691e583fd606a842b03670dd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:11 +0000

    official used bottom play pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
