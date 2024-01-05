0d8854574d981be03a57059528cc52bcc036b9dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:01:40 +0000

    thank winter thinkover capital stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
