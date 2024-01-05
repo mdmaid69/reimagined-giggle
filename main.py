3af546d11649adda1d7453c4be92a066a380dde5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:29:12 +0000

    chat accidentally pleasure bath good

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def change_working_directory(path):
+        os.chdir(path)
