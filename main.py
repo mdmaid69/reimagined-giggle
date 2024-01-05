b34bfdd423c0b371c867c02193fa9c46f81a4464
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:48:44 +0000

    hurt development walk morning arrangement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
