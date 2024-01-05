e460efe41e9f9319b9d31095101697186f9b64bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:35:49 +0000

    bank embarrassing sometimes direction mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
