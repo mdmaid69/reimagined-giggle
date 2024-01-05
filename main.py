26dc678fd6f453bb5896597920baa87c50f4a00f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:54:57 +0000

    judge smell morning swallow protest

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
   import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
