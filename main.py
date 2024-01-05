d7c882b719edfff3c8afe53eddf2f39071e72c65
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:53:56 +0000

    half left holdup foreign honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
