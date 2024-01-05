7b508db422d18a84ba6a74e547b26f0a94e26d6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:51:45 +0000

    interview now common of spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
