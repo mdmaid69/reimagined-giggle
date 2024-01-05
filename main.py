20064924900c12662f2dd18c5a1f6da872f23b79
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:29:03 +0000

    limit come material future form

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
