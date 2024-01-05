7301f286949f77ebc1336b1290216e7f1ad5d2a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:19 +0000

    hair through spill tidy none

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_pressure(force, area):
+        return force / area
+def calculate_perpetuity(payment, rate):
+        return payment / rate
