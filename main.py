efec7d96244912f9923dde9f57ce05f5e21db445
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:11:19 +0000

    valuable whatever train mix excited

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
