b3d422f2d495ea8250a8a094b9e812c55fafa8b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:23 +0000

    label mistake nurse even discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
