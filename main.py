69c07437177ebd1a134447c92d83616eb8952af1
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:35 +0000

    slip excited excuse before worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
