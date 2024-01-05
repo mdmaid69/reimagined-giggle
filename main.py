cdb6c119dced2b839aa75c64a09449b78d903c45
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:47:22 +0000

    offend bring loud single culture

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-x = 10
-y = 20
-print("Sum:", x + y)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def convert_to_octal(n):
+        return oct(n)
