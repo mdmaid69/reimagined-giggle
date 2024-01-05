6602d9f7d367b761c3c10be80795d06dbe106b8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:24:09 +0000

    holdup start holdon plastic thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
