477cc6e20cb8677058ede3716414f71635739cae
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:29:57 +0000

    otherwise behind bus switch broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
