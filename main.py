d86d91d662a91204e7a323a126be287ac35a03da
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:03:15 +0000

    season take private frame surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
