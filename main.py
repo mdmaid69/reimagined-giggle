bc972c9ab435a93c427fc834303cdcb8004ce1ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:04:03 +0000

    news amount miss disappointed eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
