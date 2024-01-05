e2ba0de310f97a4c571789ea9eed15672f565754
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:52:36 +0000

    bringdown secret gather ability convenient

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
