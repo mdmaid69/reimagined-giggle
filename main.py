5a424ae6a212527cbd028b71ae0f762e1e423cf8
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:08:00 +0000

    satisfied shelter close pay car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
