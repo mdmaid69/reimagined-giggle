3434ccb96852b2192b93443e52f217620c2600cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:11:26 +0000

    negative service wet block exercise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
