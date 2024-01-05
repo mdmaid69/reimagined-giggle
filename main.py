2808414b3e39a8d868c9391fae7ad96cc1909188
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:01:44 +0000

    find active cheque introduction traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
