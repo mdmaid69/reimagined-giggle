d8024a3523b1ba2a54ba3194b0fef93266f07e47
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:15:10 +0000

    pause us at impatient smile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
