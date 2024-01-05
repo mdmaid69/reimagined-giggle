567517e1e5a201877c1dbdcbdfd5520198a771c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:12:25 +0000

    bored Kk damage soak more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
