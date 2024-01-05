f492352b625e9d9b4b7f14e7beb8bf7b9f5ab38e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:29:56 +0000

    tie Ii begin cost pleased

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
