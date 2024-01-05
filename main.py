9a1116d56df57e9ef388e8939e7d962cec2a38c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:44:36 +0000

    advanced this artificial comein exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
