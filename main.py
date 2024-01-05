0870516b0cc4a51778d27578ec7c1e124f6de21f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:41:31 +0000

    believe carry brush board move

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+for i in range(5):
+        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
