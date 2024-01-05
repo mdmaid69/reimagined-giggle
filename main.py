35b3ba1983a471d5c3694c043589b06e8d333bfc
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:22:11 +0000

    or burn blue unpleasant occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
