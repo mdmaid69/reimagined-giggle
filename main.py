41bafdaa35d6b679787d89da034f5f904bbfbcaf
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:04:03 +0000

    point below yourself battery underneath

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
