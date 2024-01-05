db5439596908fd5e9c077c0a8fc88d6c0d4a3b41
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:22:16 +0000

    suddenly position operate low shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
