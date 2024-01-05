c17902d28f6982e6693e844db4f42a41f572b653
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:11:25 +0000

    spite cap permission geton day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
