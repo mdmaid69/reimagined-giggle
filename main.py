c01783c24a5e31bde722c021560f46a1a2b89e24
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:28:01 +0000

    unlikely emotion demand tie summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_current_time():
-        return time.ctime()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
