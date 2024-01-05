787fe5f17babb9ac8d8e89eced49be5ef413982b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:32:49 +0000

    building crime name rent within

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
