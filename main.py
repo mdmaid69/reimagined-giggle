140bbb89e59cecb1189518561e41a76ba3f2b589
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:10:27 +0000

    loose borrow minor no rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
