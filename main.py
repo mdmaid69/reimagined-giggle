e545ce69d6a98a7f04ba6ab4fd3699b314813719
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:37:27 +0000

    activity damage interview clever sweet

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
