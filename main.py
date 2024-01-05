9f3fa2c9e083f427c0401c2502e9af45179e3b2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:07:15 +0000

    block appear occasional software before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+print(math.pi)
