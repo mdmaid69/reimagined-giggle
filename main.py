8aabfa50de82b23efb8cd28a14064addf5148c2a
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:32:36 +0000

    wide precise grow uniform interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
