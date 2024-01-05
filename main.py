30a722b67394125d334373fb3e564c78edeff06b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:55:41 +0000

    off warn joke trust apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
