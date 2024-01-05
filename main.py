5b2f4e84a946883661d0c62295a9d269c7f9bb2a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:42:14 +0000

    other unite choice perform temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
