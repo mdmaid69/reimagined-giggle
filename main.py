78e6e8dc6c1df8086622e0f72e70439b5062b0f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:00:59 +0000

    bin right principle change hot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
