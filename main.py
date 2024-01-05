9fcd4c1d1b1ab748a9e6e437fea1359369bd517d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:39 +0000

    worse immediately ourselves cum whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
