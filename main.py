5624cf2ba6dc44967895b0f8fe6ecb6a32c43fd7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:57 +0000

    insist fire embarrassing neck keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
