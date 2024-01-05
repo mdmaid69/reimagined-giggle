3742c4d531aeed4c46fd0a09cbca31083f8ad1a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:23:32 +0000

    blame measure dress getoff cruel

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def create_user_string():
+        return collections.UserString()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
