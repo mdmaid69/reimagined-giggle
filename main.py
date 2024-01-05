6c917b1e9dff5e7f20fc16088c7c1ad216120867
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:34:18 +0000

    problem platform ashamed about position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_even(n):
-        return n % 2 == 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
