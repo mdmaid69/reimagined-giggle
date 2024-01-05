6f795288d61e721b84a3b65bed64ae5ac7a86ad9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:52:23 +0000

    offence ever Zz traffic recover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def count_elements(lst):
+        return len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
