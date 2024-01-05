253293b4034b865f25c2c9857ba67397c505d925
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:08:57 +0000

    study marry bear callout steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def reverse_array(array):
+        array.reverse()
+  def square_number(x):
+        return x**2
