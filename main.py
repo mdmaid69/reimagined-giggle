c5fb8a325eb9a465b3536f546b4e60f0027209a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:53:32 +0000

    swing risk rare line offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+print(math.pi)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
