8d4d0d9900e10fc96837342e6e9e75844008f950
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:25:01 +0000

    fetch behaviour contrast her 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
