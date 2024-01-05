001df1e8b429461092ae8c1ab167aa22ced51a80
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:03 +0000

    bunch burn schedule kick nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
+        return list(set(lst))
