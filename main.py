6f6596d69381fa7d0c1fdb24bc1ca0d8fdea1890
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:17:06 +0000

    plain rough influence service black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
