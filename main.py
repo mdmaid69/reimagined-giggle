f27f204bc2eeafb29f499fbe159ad2b813d38cea
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:55 +0000

    land extraordinary world switch year

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_item(array, i):
+        return array[i]
