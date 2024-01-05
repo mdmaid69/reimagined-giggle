c14d229f2c76886aec3e495e36645782be1038f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:47:35 +0000

    evening relax wild accept year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
