ff5c8efad47c4ff02b4c7ad83d815ad8a0bb0fc8
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:44:33 +0000

    bet sting lean except door

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
