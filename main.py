172400b9f6bc3c0ba501a4bf92ae7a6eed6090ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:49:12 +0000

    kick attract original side ordinary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
