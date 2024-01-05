7a58faf61e3f4a6e7b690959fc007beafb350165
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:22:52 +0000

    hair must miss urgent turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
