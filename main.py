5047c0701fbeaf8c6d14c2f78dc435d8987d553e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:29:52 +0000

    unite get slide address unless

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
