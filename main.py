87a40449f2b2054b3112f6e6ab65d54f4512b7da
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:17:26 +0000

    uponon either shock appearance contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
