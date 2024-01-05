f17f106d8aae5f300ed4481f79d443421b523c65
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:48:45 +0000

    crisis measure welcome deep sudden

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Characters:", len(text))
