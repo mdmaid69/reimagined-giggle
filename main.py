d25f7916c1ac9b6d31af286816e7d5a9d19f4488
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:54:49 +0000

    wash comfortable gentle expect listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def reverse_list(lst):
+        return lst[::-1]
