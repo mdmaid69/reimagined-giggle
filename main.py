39e4742296cd8242f4771ff6d3393c3018c86e58
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:21:02 +0000

    perform heat tidy for face

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
