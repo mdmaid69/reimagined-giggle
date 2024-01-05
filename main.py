3f10cf80697b027910daf8bd5d8e957d5da453c7
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:06:05 +0000

    chin convinced culture lot 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
