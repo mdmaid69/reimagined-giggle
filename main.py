50301bd72273b2f5f2d9ed750bd8aedd7549c2ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:47:39 +0000

    callback pen mess yesterday try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
