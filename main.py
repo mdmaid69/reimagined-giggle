cbcbbd8c00493df4a98a253895076b7070ce9fc7
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:49:58 +0000

    equal similar taste place task

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def find_max(numbers):
-        return max(numbers)
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
