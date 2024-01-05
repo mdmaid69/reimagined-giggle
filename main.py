e020ac531cda0d88ab49990590ad4e47768ff5c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:14:58 +0000

    uncertain low contain account 571enough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
