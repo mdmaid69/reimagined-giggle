2262b645f40553fcaf53251f31c43116d2add7de
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:00:14 +0000

    back usually appeal get me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
