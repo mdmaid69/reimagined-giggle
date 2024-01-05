571b047716b2fbb6dcb44a9abd1e9940bbb8205b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:32:54 +0000

    wall offend see everyone Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
