54655b45a5f6959988c8ff05225958d035ad3280
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:45:02 +0000

    afraid ago wide collect down

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import logging
+def log_message(message):
+        logging.info(message)
