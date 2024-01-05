049554af6ba561cd71b25fde772bf28ed0f33a08
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:33:45 +0000

    crash job sweep smell getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_error_function(x):
+        return math.erf(x)
