e485c01a2258118fced2afe7c7d19be90a94f4c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:00:54 +0000

    trap speak whole along before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
