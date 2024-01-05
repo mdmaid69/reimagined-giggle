9e726c5155de9f6fea91c02cc8b7cebdac5a5949
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:36:59 +0000

    back while clearly drive ago

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
