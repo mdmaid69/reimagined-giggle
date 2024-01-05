ddd0389415038fa32e7b801dc04c20822eb5b156
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:07:22 +0000

    claim begin question balance world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
