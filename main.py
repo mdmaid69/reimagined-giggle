aeb2f9febc54091ff76caf06f37e510120f4d983
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:41:50 +0000

    ill minimum frame sink unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
