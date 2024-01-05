6fe64e6bb672fc792f9e2767ef21ac0ba7c08b3d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:44:17 +0000

    success popular new squeeze nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
