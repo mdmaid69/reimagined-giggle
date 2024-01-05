61b8e3f062d5bc06f631b2fbdca4356bba9cd7da
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:05 +0000

    able false centimetre hard whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
