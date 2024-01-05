88ec1c63b55ea54fbafd73272006c3edd80be5a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:26:07 +0000

    trial remove comeround claim ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
