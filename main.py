b508953b7aa48e9653a882d97a56dd3250ed112b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:22:54 +0000

    remark air point ear land

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
