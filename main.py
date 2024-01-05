c7549e56d57ff3ba53fc04d18a74d9a06a40b211
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:32 +0000

    doubt plastic problem task evening

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
