a8999e9051170af04613f7f00066a7d7f04db22c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:59:04 +0000

    accidental mess cope takeoff answer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
