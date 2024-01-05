0a17e537dcc138b42dc179674a593c48941cd9d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:21:19 +0000

    shake cause regular bottom loss

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
