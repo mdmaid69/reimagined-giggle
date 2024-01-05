dffaff8df1987e3f36b8def5af2f2f081ff91ad1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:22:15 +0000

    fly nerve still open die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
