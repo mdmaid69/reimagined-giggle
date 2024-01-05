6c2d875de20a5927fdb289d739c84dc84c524bd7
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:09:52 +0000

    off occasional might relation train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def change_working_directory(path):
+        os.chdir(path)
