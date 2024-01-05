3dd57f648e6119a78ea370b6b41810d0884bae58
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:57:16 +0000

    hour how joke tax cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
