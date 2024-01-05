0c9814141d28c7f81290ecf5346a34efaa7bd458
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:54:48 +0000

    fall enthusiastic either crash tie

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
