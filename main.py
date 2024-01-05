7cbfd10576af227b0e59b70e830e69164249b100
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:35:36 +0000

    rule talk rely market floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
