c44b2a2b46715e3d71228d6b7633b8f1521366b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:06:33 +0000

    brother tense project run side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
