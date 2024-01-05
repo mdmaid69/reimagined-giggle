aeaf215069c7c879d75ba31639833d1b119906e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:42:52 +0000

    store danger replace send unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
