968d5b0aa2b57158ed360afda98faf69eb14a8fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:22:02 +0000

    again rob holdout chief argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def get_time_since_epoch():
+        return time.time()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
