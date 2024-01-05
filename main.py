28953e7a26dd10991ea362487f46dae36c615ae6
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:37:02 +0000

    shame Dd please support point

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
