cc2aab7a4178519aea77db05d90ca84652086699
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:56:47 +0000

    machine face aim pain scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
