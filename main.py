dfc060c94dd9c3141feab7d45422acd8f7a4dbd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:27:13 +0000

    form attention ocean variety family

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+print(time.time())
+def divide_numbers(x, y):
+        return x / y
