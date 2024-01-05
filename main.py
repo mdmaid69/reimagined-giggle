ec479bfa7966c1e6ad68231bac3840713c1d78f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:56:09 +0000

    curious Qq stamp organize none

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
