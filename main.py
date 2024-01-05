1cb62d1dae4400f03dbc276832e6f5a6a8f56b50
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:49:10 +0000

    along smart authority polite remark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_octal(n):
+        return oct(n)
