18074335bbb66b7ac9e2c6016f26f1c51172e27f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:15 +0000

    flood try giveup comeback teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
