5487c3dca2a74518e7e335d944dabf2273ab4730
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:04:40 +0000

    ever neither capital enthusiastic together

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
