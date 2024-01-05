002e5162826b9e34ceea51dc18cb2753ad9b0b53
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:27:11 +0000

    accept from decide under over

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
