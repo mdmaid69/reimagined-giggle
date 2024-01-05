16b448bd3d2f44a19cac7992013a04916d6a00b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:47:21 +0000

    result plate trouble callout sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
