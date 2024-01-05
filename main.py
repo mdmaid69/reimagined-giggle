2c18f67304f0ffd73ba5e87a2aaef2973afd99e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:06:28 +0000

    workout interest standard equal right

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import datetime
+print(datetime.datetime.now())
