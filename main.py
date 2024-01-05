c977561981501aef5aaeffb376ab9c77d042471e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:34:13 +0000

    the real tired mess breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_square_root(x):
+        return math.sqrt(x)
