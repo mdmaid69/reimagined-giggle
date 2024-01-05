ddefdc15150c5e054dc475694ad77cdbacbab555
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:25:31 +0000

    day safety there before funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
