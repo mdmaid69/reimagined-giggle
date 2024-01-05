eecdb1e8445e35d547732d85f5c4c15973bbd8b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:07:18 +0000

    somebody frame turnout bear tune

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
