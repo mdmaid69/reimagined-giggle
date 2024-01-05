db974e7cbfdd9541557d1c95214ec2ecdaccf1f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:03:43 +0000

    justice evidence wherever star spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
