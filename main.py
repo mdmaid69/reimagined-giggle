157a69b99d682fd668ec660ff76874a5b9d3c4d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:18:32 +0000

    party confident stop goon hardly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def add_numbers(x, y):
+        return x + y
