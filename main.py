7698aaa653a34693574aacbc92a33021fffe029e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:53:09 +0000

    also platform similar goup pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
