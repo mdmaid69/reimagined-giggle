81cdd885d3288e49c1824d3ae99af66f6b5bc127
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:41:42 +0000

    anxious total Ii broad beside

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_counter():
+        return collections.Counter()
