7dea3707351b5bcadeb335e3f1feb5ee5db22910
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:58:06 +0000

    frightened take surprised friendly movement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
