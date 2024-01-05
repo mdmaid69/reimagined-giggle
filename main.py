c8ef67b279328e5f7766990c5163cf477d7ad14a
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:58:21 +0000

    extraordinary clean illness meet pass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_palindrome(s):
+        return s == s[::-1]
+  def convert_to_octal(n):
+        return oct(n)
