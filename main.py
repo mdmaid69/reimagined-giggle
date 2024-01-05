81774e1325d2d30771158d4c7dbc192233090b3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:13:06 +0000

    whistle unhappy hit guilty support

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
