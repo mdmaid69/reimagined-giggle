2bda0431dde6a1b493be2b97094ed9250fad0668
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:38:16 +0000

    comment bag thirsty high niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
