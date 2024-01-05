2053f6656c1ecf52b0d1683e9d3ddb05593cbfe6
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:22:05 +0000

    rare shopping else takeaway between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
