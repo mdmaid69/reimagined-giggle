c6c5f4df58b1c35c36a4e2965373e7a8f27e3aad
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:07:58 +0000

    getout watch judge smile provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
