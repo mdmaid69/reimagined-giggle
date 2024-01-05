b9068e06d9e29819b58580a20cae48b93904da45
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:42:34 +0000

    support skin trade decision keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
