485f7bb37dfe0e3b7f079d1d6ec64b6a420d995a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:16:34 +0000

    crazy complete guide next favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
