a07779c504c2c7043648c0d3ae4d0dcb807e7f68
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:07:13 +0000

    war good though furniture drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_base_name(path):
+        return os.path.basename(path)
