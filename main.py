ea58e57068198e174a4c4b4272a1b79ffe29bcd4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:34:32 +0000

    joke trousers past its because

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
