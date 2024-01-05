80ef8e9995798dddd946a0dbe9e849736016d8dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:42:57 +0000

    modern cross long like affect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_work(force, distance):
-        return force * distance
+import os
+print(os.getcwd())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
