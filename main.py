732cb9fd87d5aeb84c0bfed937c9a75c95313698
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:37:21 +0000

    first appeal trip needle environment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
