77c88ea409ec0f718430a3c29ee4854e5dd5a5b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:25:20 +0000

    charge mass objection intelligent develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
