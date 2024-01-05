a1d453d8f692852e6bc67a10bf550156dffc636f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:52:31 +0000

    capital jump side of direct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
