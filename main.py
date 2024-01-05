46ab11ebacca17e06509746443fd8ea929628453
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:15:47 +0000

    preparation introduction entry defeat Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_string():
-        return collections.UserString()
+  def convert_to_octal(n):
+        return oct(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
