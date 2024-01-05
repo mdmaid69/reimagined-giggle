ff43c2a3477f6a5374894d4f60b5e059880278be
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:22:10 +0000

    weekend heat underneath drive comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
