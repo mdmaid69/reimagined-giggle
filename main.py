0112d1dca1776f4d79a45734b9ab7e8c20b7a38a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:10:01 +0000

    away closely squeeze impatient like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
