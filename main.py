6d20105acb766abc77d023df7ee43c3ca001e862
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:30:23 +0000

    negative date film chase tough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
