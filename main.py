df67294eab1e4aad56a055791915a7eb4cf6978e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:17:54 +0000

    book muscle instant understand 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
