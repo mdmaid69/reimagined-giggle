76c97ed61a448c4fe0b0e5d694f9eb23e33632f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:43:15 +0000

    ignore takeoff wish spring concern

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
