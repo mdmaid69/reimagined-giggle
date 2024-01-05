30a53b73868f2d559c13d02c103f200c01727538
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:34:27 +0000

    brother child broken breathe ago

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
