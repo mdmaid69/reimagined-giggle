7afe1df40ac29e51655a369d90f411b4e64bd19f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:35:26 +0000

    repeat breakout pass keepup quick

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
