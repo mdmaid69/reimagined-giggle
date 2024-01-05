3a7c6e88e67b98e30ea14c68782b0c652b9585aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:56:44 +0000

    song touch father goaway compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
