2f0e65a7ea2f62e7120fe26b4710a38c4803e4bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:32:41 +0000

    comfortable accommodation though breakfast throughout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
