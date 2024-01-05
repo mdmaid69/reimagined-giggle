3c5d41bdf21d273023a80fed408b61e7a226bf29
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:59:11 +0000

    perhaps approval container round by

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
