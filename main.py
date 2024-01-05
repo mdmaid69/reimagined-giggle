e0f638dbf0a9fe97d907de6c3d652895921480f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:00:42 +0000

    but return keepaway welcome reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
