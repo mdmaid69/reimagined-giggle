ffee168e987131d6c387d7729e4a746ad2c91496
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:33:15 +0000

    Yy bag myself pay success

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
