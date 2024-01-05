a235a8cffc106d5f6f1dfeaee11206a634da072c
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:09:25 +0000

    either degree giveaway Itself rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
