5dbe513212ca73b6986e602a8964a1fe7741e04f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:25:32 +0000

    coffee temperature combine excellent odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
