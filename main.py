7035fe435afcae1c978fe8f515649fde2bb194fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:03:58 +0000

    spare importance true whoever base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
