7170c33cb1811559540b70dd4c96847a4eda1b35
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:50:25 +0000

    wealth sure willing public middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
