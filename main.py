f43503285bca557f1d7bab4ba2aa6b6287636ff1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:20:03 +0000

    rest through possible reaction later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
