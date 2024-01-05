5be05ccf521f7f062ed08e8fd4f8584c64707357
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:32:04 +0000

    sight mine card much load

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
