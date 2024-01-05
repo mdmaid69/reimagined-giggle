a3e7597e4986d8a704828724ea454995c008b483
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:17:37 +0000

    culture left area window test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
