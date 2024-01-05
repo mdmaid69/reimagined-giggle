3069c0c5134971425c36fda765dfecaf7dd14247
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:34:26 +0000

    clever exactly count quiet now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
