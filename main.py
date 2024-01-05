798218748f9cbc465fd724f839cc271e1fdddea3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:16 +0000

    row waste march regular even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
