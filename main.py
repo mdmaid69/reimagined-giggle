fee019cb7e28a03c0ec1804a07c4e3661ec03176
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:53:37 +0000

    sort godown instant surprising comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
