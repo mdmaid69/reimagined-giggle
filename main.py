a99601d393eb2879541bf86a23d6e78a77635bec
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:31:03 +0000

    discover responsible advise first careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
