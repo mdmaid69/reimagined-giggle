2689334d95e37f022d527978eadebda1ce7d3d37
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:56:04 +0000

    suppose determined match communicate callup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
+def count_characters(sentence):
+        return len(sentence)
