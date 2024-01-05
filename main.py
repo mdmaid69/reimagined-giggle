80ef216571e8216fafba13758ec4fd9dff932588
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:32:15 +0000

    sore ticket attract unable turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_floor(x):
+        return math.floor(x)
