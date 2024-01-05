d7642ab8383e9393a002641bb8b69340ef9a5d2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:40:39 +0000

    kind up leave that race

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
