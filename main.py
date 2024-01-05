fee9fa99e2a1515acb0e73849cd731f5ca2d0d4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:55:39 +0000

    effort circumstance dangerous improve wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
