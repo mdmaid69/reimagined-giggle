0f5f8c23e7563639e65ecb3c5439461626b54412
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:00:49 +0000

    catch hit male place priority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
