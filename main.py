38f039cab3ad0b6de1a0e6262292e9e127c8c97c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:32:23 +0000

    run unhappy order burst will

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
