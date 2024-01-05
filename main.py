788b584c6613021f0f4bccf4c15e230ccbb59660
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:55:05 +0000

    wire no fond divide eager

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
