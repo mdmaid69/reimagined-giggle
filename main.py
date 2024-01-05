33cad77b2963a6011c2278efd041ce8eadecfa20
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:39:10 +0000

    disturb feature body quantity wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
