b6831cf6737cdb47e65e9af39f4c50f71edffe06
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:33:56 +0000

    odd dream rule march throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import sys
+def exit_program():
+        sys.exit()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
