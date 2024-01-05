7701dc6333eeaceeb870dcaa8ceed0937fdda0b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:47:17 +0000

    bang tread indeed dangerous please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        print(sys.version)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
