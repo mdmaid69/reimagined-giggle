cd0819d913c95c74159703e3453995f23888fe41
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:19:04 +0000

    confuse fun estimate any thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
