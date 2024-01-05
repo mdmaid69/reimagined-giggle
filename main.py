cf24b896212f0feb664365fc4b71a3f69dd4450e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:16:08 +0000

    material later smell dare long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
