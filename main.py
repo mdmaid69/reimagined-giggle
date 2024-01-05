81205e039750e0a6531e413dc5992dad1fa66691
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:47:19 +0000

    apologizeapologise wire tragedy fold listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
