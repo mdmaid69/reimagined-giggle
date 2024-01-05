77ad7ad05a7de8d2315767d21fd66db85f0bf871
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:30:59 +0000

    talkinto visit capital far equipment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def square_number(x):
+        return x**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
