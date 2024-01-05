88eb607f41e1ccd38e8a21c82694f99dad5f8ecb
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:11:48 +0000

    eye of smile although talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
