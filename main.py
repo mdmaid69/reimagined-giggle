b6fc39a1fdb0e6428c5c44fb6457610a4d875d12
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:57:50 +0000

    embarrassed standard concentrate realize question

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def is_odd(n):
+        return n % 2 != 0
