e4efc04bb5fa3a9ddc0c32a31d73404317c18b9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:35:22 +0000

    wide doubt turnon damp Qq

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
