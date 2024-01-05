ffe8c98dda77cf6294f85b538c966bce0617d323
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:44:29 +0000

    past radio wheel conclusion talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
