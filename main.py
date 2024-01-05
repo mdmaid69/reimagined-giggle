9b206d5a8530e1831c76b2262bfdad1fbd53678b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:33:27 +0000

    bang all creature battery stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
