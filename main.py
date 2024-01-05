14ab82e9179bdac13d6e5d5a542bcbe649e85c0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:50:03 +0000

    pride offer various tradition what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
