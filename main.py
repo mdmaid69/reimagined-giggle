4f14a3402a5806cb34c73ec5e29d3d521d87ce37
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:00:19 +0000

    support war unkind mean pleasure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
