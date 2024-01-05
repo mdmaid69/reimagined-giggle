de881b9f64faebf6434ffa5262c1418eaff1f2fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:30:44 +0000

    slice print burn left phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
