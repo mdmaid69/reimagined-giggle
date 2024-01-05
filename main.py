a7e0920b3f0fffa8da25d5a8aef7cb5367f1f9e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:04:35 +0000

    care print badly manner scene

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
