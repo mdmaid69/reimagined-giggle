b9640f81eace76277c1454c95f3c2b0d8a97a5fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:03:13 +0000

    smooth wish broken funny flat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
