72d4a2ec43d62ce48c5300399fc59dace01df939
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:44:24 +0000

    Ii appearance fortunately breakup prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
