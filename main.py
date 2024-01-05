a9f62a36c4e5940fa6b877191f96e1b5a17c7391
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:55:27 +0000

    sometimes young art advise well

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_list_from_array(array):
+        return array.tolist()
