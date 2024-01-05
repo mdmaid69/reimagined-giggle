1d7c4c6d2942355eeee5be76e83dd9f1ca923bd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:47:13 +0000

    most it yesterday across point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def add_numbers(x, y):
+        return x + y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
