eae7da059c249a88d5df4ef88c94c06eedc3e259
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:37:30 +0000

    level comeback beautiful skill less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
