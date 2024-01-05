fd822f1ec0f67e73335517342c2be367c10aad34
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:03:34 +0000

    sort hide preparation equal jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
