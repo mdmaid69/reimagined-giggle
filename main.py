96e7083bc0944f695ce25d050cd2606fed3807c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:26:52 +0000

    almost table view prove succeed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+text = "Hello, world!"
+print("Characters:", len(text))
 n = 10
 print("Cube numbers:", [x**3 for x in range(n)])
