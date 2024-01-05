eb262d653a0d77aae9929f01db4ad403f21c1e41
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:09:01 +0000

    charge damage operation task because

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_typecode(array):
+        return array.typecode
