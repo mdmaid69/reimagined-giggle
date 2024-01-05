9469a356d72f068962857effca2a289e5dcd789f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:27:47 +0000

    family shoulder that wait company

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
