df97e6a97ab908aeaa50d5d84895f2c6da3b4840
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:57:06 +0000

    past familiar actually card choice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
