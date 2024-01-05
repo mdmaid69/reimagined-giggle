36c4153d1c0d4a2bceab2bf0d72fc9086488a3fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:37:32 +0000

    they schedule inside tight callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
