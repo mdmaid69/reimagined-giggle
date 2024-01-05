894f57f63aa598728741078ca5f5a99debc7ab6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:56:04 +0000

    Bb wind belong 564encourage aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
