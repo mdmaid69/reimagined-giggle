d51dbf0f6edc379929028ab530a32f8be5bfe903
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:42:06 +0000

    easy operation embarrassing mouth lookup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import time
-def get_current_time():
-        return time.ctime()
+for i in range(5):
+        print(i)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
