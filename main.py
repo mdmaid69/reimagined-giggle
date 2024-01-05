251f5463dfc169db0b1465db09e42d81855124af
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:20:02 +0000

    vanish mouth march note same

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
