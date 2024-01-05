942461f029f40ae2c9282f2cade56a91818d266e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:06:20 +0000

    necessary emergency trap introduce unlucky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
