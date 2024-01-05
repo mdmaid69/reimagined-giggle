baeac033212b79dd62d5e9e26d75e0220a094d03
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:50:37 +0000

    loss long everybody suck style

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
