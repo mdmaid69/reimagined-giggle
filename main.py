91a7df04a9e9a3116a07d3bd8cd86386cd6a4183
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:07:00 +0000

    involve single before sweep welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
