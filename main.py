cbb3079f59c062222c20792705540667d88f3591
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:16:31 +0000

    room snow under tape container

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_elements(lst):
-        return len(lst)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
