62b66c7d295e0321f7849e264f95e7bf612208f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:18:23 +0000

    recover noisy event and half

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
