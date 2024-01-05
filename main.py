c0b721fb9f16bc5749e9babd43dd5d4c6577944c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:46:20 +0000

    release balance average unusual rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
