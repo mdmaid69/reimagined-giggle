ae3b53d52e32bc7d816e52a3456c6183e643886b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:36:59 +0000

    free danger leave nature journey

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
