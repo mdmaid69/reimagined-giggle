0eee024f7d0d16332fee6151e6246723d9d29515
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:40:08 +0000

    disturb surprise mend decision statement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
