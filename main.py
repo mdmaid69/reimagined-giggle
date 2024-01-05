fb3674f4ee51d5b5325e5dec3e85eac4b2311d85
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:57:46 +0000

    direct welcome many tough total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
