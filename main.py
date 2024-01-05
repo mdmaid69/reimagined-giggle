1f330c9e589686c7398dabe575366dd8144bd955
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:21:44 +0000

    film supply hello word closed

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import time
+def get_current_time():
+        return time.time()
