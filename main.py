a433b68175dc8cfd241a2e0705f9345212d57683
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:46:09 +0000

    beneath whether involve floor material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
