8399095f4563de0ec98aebca7300ea55cd1a517f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:23:57 +0000

    correction wheel experience us worth

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
