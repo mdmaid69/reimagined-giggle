3fbdfd522b95a9e93969ce843dada1faecee8625
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:28:06 +0000

    freedom recently constantly distant holdup

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
