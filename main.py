eabd39fe95d7c4675784dd98df8c23d0fa96e1c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:58:05 +0000

    draw sorry use fear immediate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
