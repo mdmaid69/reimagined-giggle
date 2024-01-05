f8a7396568328adceb6ae8e9ab5c6e43082be2f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:45:04 +0000

    kind takeoff zero full straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
