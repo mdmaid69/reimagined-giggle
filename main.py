6242810a11685f6693c8c096c1760b5855c1457d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:02:25 +0000

    quick gather raise hear shout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def cube_number(x):
+        return x**3
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
