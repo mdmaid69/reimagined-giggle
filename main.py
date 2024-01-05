107a6eaed9a35f5dcfebed9f3b2bf70912be6370
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:02:46 +0000

    authority gate worry ticket trust

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def convert_to_hex(n):
+        return hex(n)
