7a3b4703587ff5895a3412f46b7ab522e5ecb34c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:29:51 +0000

    rule disappear raw start make

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+for i in range(10): print(i)
