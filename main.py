5248770ca5f5d3588ee08e7af96be2fa5e088151
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:33:06 +0000

    major niggaacademy takeout excuse nothing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
