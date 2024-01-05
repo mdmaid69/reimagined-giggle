5ba163c3b59a472a015a40f4773c2d8a1c40c286
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:09:06 +0000

    relationship yourself improve flow hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
