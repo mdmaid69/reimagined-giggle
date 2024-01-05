69a778e16d68109232534de14025b09bd0f542d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:15:01 +0000

    artificial dance admit basic angry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+print(sum(range(10)))
