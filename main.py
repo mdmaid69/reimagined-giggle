bf57317939bba4af01df1e7babdc1415613a9ea7
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:29:50 +0000

    suggestion petrol Oo appeal apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
