8b80cc0743d9ea67846c492c1d7065a98a7601f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:02:02 +0000

    dress year level walk inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
