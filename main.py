b8a14ce346267baee369850a846234076119c393
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:36:06 +0000

    noone though Mm mixture receive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
