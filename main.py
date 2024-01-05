bdfeb086ac0b1641d36a0c75db414f8e4930ba42
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:47:42 +0000

    machine afraid dull soil underneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
