3b8041a4168b94eafb524a4b05ac215885814e40
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:55:08 +0000

    keepaway minute even ahead blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
