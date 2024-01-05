2f0726fd2bb2200084f5cc52da799b603a0953c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:25:26 +0000

    corner offend die house who

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
