2865af7cfe1b5b3a5b9849a316f6c6fadf8e5f79
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:34:17 +0000

    bright will on waste key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
