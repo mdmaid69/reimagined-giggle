ce24328996ee12f618566604816cf803b309c922
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:31:38 +0000

    clear unfair worth frighten offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
