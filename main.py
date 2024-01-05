aa3e5a43d173f27d8dcad0af125dc92f396f6484
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:04:06 +0000

    report include half rough show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
