8c358b3ece6a7e3e043295d3102ac34d8f7de74b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:05:21 +0000

    best interested impatient prevent desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import os
-print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
