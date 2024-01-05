9c2e7f12e06e89fde38e9095cef4adf61ed5e213
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:53:43 +0000

    create more bath in flow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
