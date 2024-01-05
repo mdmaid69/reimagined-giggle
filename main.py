8362195f6425fdc3dc0c2015ab9be3f54fc69961
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:34:05 +0000

    bend hang hurt confuse plan

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def is_even(n):
-        return n % 2 == 0
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
