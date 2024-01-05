43fb848d2429cc05736d5bb41baa28f28871c9a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:05:31 +0000

    coffee reply kind may opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
