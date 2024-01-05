de60561e566dafe247e2e759f5c0633382f96e08
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:17:33 +0000

    journey equipment risk welcome classroom

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
