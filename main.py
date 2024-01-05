09b7dc37f0e76a37820ac25448ff53017f5ec2f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:35:34 +0000

    city effect loud familiar takeover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
