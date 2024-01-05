13aaab83f172f887841f2f7ab56210bba9ba128b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:10:32 +0000

    same sea mad area please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
