cb85ac15ae69287f4533fa461c549074c35c7bf4
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:15 +0000

    round error apparently transport tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_priority_queue():
-        return collections.deque()
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
