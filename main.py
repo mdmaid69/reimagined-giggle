fc8f8decc3cdf31e94119e5ee910932e0edc55d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:44:43 +0000

    follow agreement means stop 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
