fb93c7f479975502a688c65223c01333e76c84dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:53:51 +0000

    middle between couple roll cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
