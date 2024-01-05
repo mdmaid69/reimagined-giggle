37a646c342c0f7c363ee4d27727ef1707488259d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:29:40 +0000

    personal wise exaggerate basic process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
 def calculate_bessel_function_of_second_kind(n, x):
         return math.yn(n, x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
