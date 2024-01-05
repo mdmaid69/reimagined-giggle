947c4f8f5bf910b43eda79ef1e126e86f61ae83c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:24:42 +0000

    Ff split crash reaction apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
