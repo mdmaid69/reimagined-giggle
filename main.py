2085729853a1694f1f368d4c3bcd9322b8545a93
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:29:56 +0000

    stamp remain arrival ourselves impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
