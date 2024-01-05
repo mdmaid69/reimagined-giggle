dc9b9f47ff2fe9c935fab5db0dc0c645a4ce88aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:07:13 +0000

    treat become past separate claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
