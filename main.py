5baae8f054a669e523429d24047aa8b065c6e1e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:55:54 +0000

    light bringdown roll never plant

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
