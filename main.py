b6759f4a0e00ed5ccaf349f3aee1bc58a66b0ed9
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:30 +0000

    study walk top line flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_acceleration(speed, time):
+        return speed / time
