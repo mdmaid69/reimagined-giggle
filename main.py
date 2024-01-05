0c4ca7cfee4dadc54ae4c3cf21bc17b61161935f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:37:57 +0000

    make either silent because chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def add_numbers(x, y):
+        return x + y
+import os
+def change_working_directory(path):
+        os.chdir(path)
