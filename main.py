89709d32e18789e25056d17bf26aa01d7569e7f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:32:34 +0000

    smell passenger stop spill stone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
