b859b45fbcf9443d463ed03c9455535c1f1a7e04
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:01:24 +0000

    damage wherever result custom block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def sort_list(lst):
+        return sorted(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
