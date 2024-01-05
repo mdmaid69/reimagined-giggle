d507a8114a457c98ede00f022c92642d74d061f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:47:16 +0000

    heart show instrument wherever pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
+  def convert_to_binary(n):
+        return bin(n)
