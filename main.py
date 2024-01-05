fbcb073fe53e45e1bae77a08679270cbf1480646
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:56:53 +0000

    about expensive takeoff honest main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
