46211dc504a9f7d8008a3378b9001c9c76fd1413
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:38:43 +0000

    attach government flow stamp main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
   def subtract_numbers(x, y):
         return x - y
