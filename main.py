9fc6fb882f1d605dd5a0bfd03d0c5a5d08976bba
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:36:43 +0000

    order fresh opportunity born joke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
