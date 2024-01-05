aa66f6e2beb699b7b339b9bdded89923c737267e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:05:26 +0000

    hospital degree interested tend borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
