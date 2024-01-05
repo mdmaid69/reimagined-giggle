8082daa4ab85b1deb06f73c1def651f69e431231
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:13:06 +0000

    ordinary holdup our cope meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
