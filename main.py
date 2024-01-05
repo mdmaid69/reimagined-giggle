98dd65b31d854ae0e74a281f1a44325d4735725e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:21:18 +0000

    laugh hot area for weather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(numbers):
-        return max(numbers)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
