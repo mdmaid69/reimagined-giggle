98722038632f0e45fe8ea9a6d73933d1ca620d2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:45:04 +0000

    record feature waste quit want

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
