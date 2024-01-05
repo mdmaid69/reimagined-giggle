701202a42325252ed24c19ffa863ad3726c36823
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:22:45 +0000

    effort joke prize sad write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
