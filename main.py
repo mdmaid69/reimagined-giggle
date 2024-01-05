631d89cfc3e82fca85a8806c533b0a9ae425a3ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:49:54 +0000

    listenfor love coat smell breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
