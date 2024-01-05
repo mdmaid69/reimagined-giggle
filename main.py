733bb56bc446a7082c647ed26866a2de6f70e14e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:34:51 +0000

    while argument sentence trip goout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import matplotlib.pyplot as plt
   def plot_histogram(data, bins):
         plt.hist(data, bins=bins)
         plt.show()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
