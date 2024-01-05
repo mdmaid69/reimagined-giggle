eb1e53f357c1149a2082168b01a04ddcb2324bae
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:04:58 +0000

    protest neither lookup every return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
