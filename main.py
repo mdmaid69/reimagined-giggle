fc77e2b1e9e1ee2e08e50ce477a3ff9589fbfb17
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:48:55 +0000

    violent mine public appeal touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def get_array_as_frozenset(array):
+        return frozenset(array)
