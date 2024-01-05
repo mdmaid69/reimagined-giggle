d0aa77d7e427bbca750d1f50fb11b01fa24fa15c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:20:30 +0000

    standard proof word action back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
