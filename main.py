7ba0478833d843b064c064d0597408b2006c104e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:26:26 +0000

    price result organization wave matter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
