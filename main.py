76eacf4b31994b6b5d5d3bfc695bf45fb1dc7a43
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:12 +0000

    hope squash scared sticky respect

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
