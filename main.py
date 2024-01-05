cc1e3d0d32e1f6f594e83ab18915be11326ff048
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:16 +0000

    sometimes national or land unkind

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
