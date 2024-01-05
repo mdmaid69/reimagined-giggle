7693a29edcaf1dad6e9f57f39e6a3fbca99bbffc
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:20:47 +0000

    wide dress mean design can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import platform
+def get_os_info():
+        return platform.uname()
