04271ec3ab0204b5a75dd579d1aeadabc300da4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:39:55 +0000

    unable steady trouble size score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
