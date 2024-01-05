a0ae70a4d48425b1d857112e60f95ab2e13639b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:05:55 +0000

    private feature Itself borrow lock

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
