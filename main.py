4e76bbc5fa26e3ced3a298997410a77dac730f79
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:21:05 +0000

    lookaround regularly dear meeting trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_typecode(array):
+        return array.typecode
