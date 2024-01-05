79bb7d64ba45f0648831c658b1530a17e1b5bd91
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:01:24 +0000

    Gg underneath deep above temper

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
-print(time.time())
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
