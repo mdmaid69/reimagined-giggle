5521a9d78312d2620dae095ecd5be39f9f882472
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:01:10 +0000

    punish friendly total safe hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_factorial(n):
+        return math.factorial(n)
