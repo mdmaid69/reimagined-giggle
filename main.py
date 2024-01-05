b18ab1374831411fa084354da72d1cbddf77742a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:26:08 +0000

    run clean delicate lend pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
