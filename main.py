42a8dff360aa03f21029b3c1f4bbb0f491fb040c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:24 +0000

    fence forever talkback naturally wrap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def square_number(x):
+        return x**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
