e32bc2f6d51a0bae897988421144cfb10975bf70
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:15:56 +0000

    degree near game visit slowly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_max(lst):
-        return max(lst)
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
