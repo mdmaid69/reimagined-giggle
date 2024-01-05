c12f151ff1b26cd5c043c7b147e28337f5158752
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:40:24 +0000

    wear it language ride ticket

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import random
+print(random.randint(0, 100))
