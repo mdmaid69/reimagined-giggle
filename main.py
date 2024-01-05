d2908d62b1041e1bf51c6bcc74d437268a76f53c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:58:20 +0000

    warning from community interview correct

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def count_words(sentence):
+        return len(sentence.split())
+print(sum(range(10)))
