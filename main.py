57f4376c3f4ad4dd44501fbf3f087546273bbf68
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:32:53 +0000

    excuse imagination smoke giveout stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
