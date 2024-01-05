1ac324ff6976050940b0b664a5486c1a05219cd3
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:25:10 +0000

    declaration Gg tone fork place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
