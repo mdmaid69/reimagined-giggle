29c7352a1a6beeea345ce7a0bb181b4619236fbc
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:50:37 +0000

    closely public tell walk strange

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
