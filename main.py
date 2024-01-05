309f548a6535d3c2199e75befdab002896e7b7c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:26:11 +0000

    present relax past clean find

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
