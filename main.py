9e4f1257016d8ecad9535a8f1389f6afa579c120
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:22:55 +0000

    themselves pocket especially violence workout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
