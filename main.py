dd03f784f22402e5b999d23732ee59cd0f6cca30
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:02:00 +0000

    calm takeback account into lose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
