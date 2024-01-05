4b906ce234ce7a7a21ceafb096b826f48ee5ad39
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:18:40 +0000

    matter brush waste such adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def divide_numbers(x, y):
+        return x / y
