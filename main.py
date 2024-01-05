b5f8b2209514c7043d7829a5c94eca5d76fd143f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:22:38 +0000

    behind muscle ticket respect voice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
