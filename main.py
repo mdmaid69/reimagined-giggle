5bc47af764efe54311b488c4954324244f4f7dbf
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:39:33 +0000

    store question appear statement certain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_area_circle(r):
+        return 3.14 * r**2
