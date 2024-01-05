7c02102c21e14cc67ee71211c103020a5cee6021
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:33 +0000

    closely match strange excuse but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
