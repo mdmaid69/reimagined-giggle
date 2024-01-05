c687dbc3d45d8322e5fc734e9d96483c26d4f3fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:04 +0000

    plenty goup zero dead pattern

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def reverse_list(lst):
-        return lst[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
