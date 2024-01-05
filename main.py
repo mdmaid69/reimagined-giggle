e42a55b0c792f0c6713a10d3bf465f1c601c3e27
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:19:54 +0000

    might giveback school block cover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+text = "Hello, world!"
+print("Characters:", len(text))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
