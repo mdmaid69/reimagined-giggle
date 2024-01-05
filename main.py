cad5265c4979289a72625096cc3201305f511a98
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:02:50 +0000

    rid lend impossible weak damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
