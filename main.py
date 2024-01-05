2f3d7f287b94761af40e2f75b7defa1367e46b7b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:34:06 +0000

    polite often unit oil light

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
