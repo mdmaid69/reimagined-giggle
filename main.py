eb44ffdc4c51f4e55df2c47455996d174f619afa
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:35:21 +0000

    breakthrough in call urgent crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-x = 10
-y = 20
-print("Sum:", x + y)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
