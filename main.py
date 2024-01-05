ce6f77fffc213ea4feb5b4579f4c4c2868ec5d38
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:55:15 +0000

    storm fine wind low hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_str(array):
-        return str(array)
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
