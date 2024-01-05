b1481febbb2f88d41ed4e75bc886aa8420b66124
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:58:21 +0000

    certain tie lay plenty culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
