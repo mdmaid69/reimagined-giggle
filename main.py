06688682578ee65aebc1da62b319779ff0bb280d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:05:27 +0000

    connection hurry male satisfactory letter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
