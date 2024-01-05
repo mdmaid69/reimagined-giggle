aa43da671b14a00e3d0b3cbfdd0b050be60d2e09
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:59:54 +0000

    forget tense hand ability crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
