e2e50b3f12eceb1d8221ad0b7d2968530557a023
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:15:41 +0000

    area responsibility ignore comeback gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import time
-print(time.time())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
