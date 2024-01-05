b0b829a24ef4137e122ca3abf31ffb4195f2076e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:30:51 +0000

    our port exit ear life

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
