66dbfa38457eff58de60339e23163a4f144fb977
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:09:07 +0000

    they week nice arm convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def remove_duplicates(lst):
+        return list(set(lst))
