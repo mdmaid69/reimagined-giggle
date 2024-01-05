e0989b97a9ed29f5c0276d3a95db498e72663247
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:45:52 +0000

    lead touch wind else high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
