c0473d0005d9948141ebea13d491c2363154e04b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:22 +0000

    tell serious Bb bang children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
