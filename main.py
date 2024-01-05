5a36a8eab223e5cf8ad5f2e6017d1e59e6e00d73
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:31:16 +0000

    consult never wave comein kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
