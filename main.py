9b7cc1f4f972debbd38b29df38a4e6b689147b72
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:49:34 +0000

    reliable oil speech rank every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
