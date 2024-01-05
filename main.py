7c94ee9dee12f6f526c17532afc020fcc6fa8649
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:41:12 +0000

    furniture main catch equal contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def reverse_list(lst):
+        return lst[::-1]
