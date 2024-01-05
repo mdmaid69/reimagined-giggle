c3f63fe84b8a59780b3dd25a9e4f16fa277cc320
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:10:10 +0000

    blow inquire profit listen keepfrom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
