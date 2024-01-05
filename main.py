1c78e42d846f905c92b4ec45a40fa2bea547b9a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:43:48 +0000

    nigga prize off bury blame

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import os
-print(os.getcwd())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
