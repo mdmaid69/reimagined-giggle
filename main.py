2fa05b90bd605b636b8e3cac45be148ce3f84ba2
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:17:37 +0000

    crazy reply catch conduct say

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
+import math
+def calculate_sine(x):
+        return math.sin(x)
