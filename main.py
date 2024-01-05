af275266b13ad0c63e6c87ce6ef2a3e3193d7173
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:25:56 +0000

    pronounce surface coffee rest blow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_rectangle(l, w):
-        return l * w
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
