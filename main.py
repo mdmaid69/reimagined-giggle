ad6f6e9a25709966173f64170543243a1d6b99f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:14:37 +0000

    no card fast shape shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+print(math.pi)
