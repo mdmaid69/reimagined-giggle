020cd6fb0259acee03078cebf52cc1475ef0ce73
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:43:08 +0000

    by search classroom strength address

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
