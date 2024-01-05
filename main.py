3d019aa0892479b6a075fd7e117ab5843ebe829b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:30:54 +0000

    suffer unusual Qq health Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
