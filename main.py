c6139bf1e667f407b1456f97bd548c5726e7ab48
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:55:06 +0000

    operation only none do corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
