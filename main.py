7420ec3d2f8bc250b437ee969c65bf54c38cc625
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:03:02 +0000

    godown negative stiff quiet file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
