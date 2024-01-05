a75035db28fb4f6151f8ac73d2b8d50d05a13944
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:47:39 +0000

    safe rest apparently slide switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
