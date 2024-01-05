cbab2ad6a7139c671d27d36da050ac4648ca2cef
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:27:08 +0000

    mess turnoff necessary explain crazy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def create_counter():
-        return collections.Counter()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
