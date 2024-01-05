f7a38d31adc2169d71734072f4880abf02f486d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:12:35 +0000

    Mm like giveup skin noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_unique_words(sentence):
+        return set(sentence.split())
