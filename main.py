90ba44d708df327d3b6bd0d7e5bce4c085092a0b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:29 +0000

    corner slice by waste uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_array_as_list(array):
+        return list(array)
