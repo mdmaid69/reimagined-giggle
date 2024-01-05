02ba1bf69808bb570b1ba21a1639ccbab701436b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:46:19 +0000

    grade bunch interview equipment cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
