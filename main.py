5cf152bc79bd0bf3545d2b4a5dadacdcd41383fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:18:42 +0000

    autumn spite copy straight funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
