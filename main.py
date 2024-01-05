e8a33bc5aa522843f322d49ae637c7d1ea312e1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:31:21 +0000

    battery away deaf swim eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        print(sys.version)
