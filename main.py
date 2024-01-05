c77f09c4bf72486287177922ebff3d6b4614f92c
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:56:35 +0000

    any behave motion hurt moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import tensorflow as tf
+print(tf.__version__)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
