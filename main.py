e8020cb14cc1e82984e066b1c97d84915cd9f28f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:53 +0000

    sharp tend bright passenger limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
