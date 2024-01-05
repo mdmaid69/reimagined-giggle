99e8564b156afa13471fed565e9b0e38b9417f8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:09:46 +0000

    unkind pleasure small other generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
