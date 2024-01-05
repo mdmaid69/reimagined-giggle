975f7aa5611857f3d496e97a05d26d4673147578
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:25:18 +0000

    thanks he meaning Kk also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_base_name(path):
+        return os.path.basename(path)
