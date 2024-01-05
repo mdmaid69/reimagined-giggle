9b9a2257c71a91d5fab98f6210c60b25dbed0bee
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:41:07 +0000

    drop ring furniture development this

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
