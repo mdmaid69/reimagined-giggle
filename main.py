a204aeb657a5ed36747655299c9be5e9908bf3c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:47:15 +0000

    concentration protest born roof serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
