18987db2db2aefd529bdc6117165c89d38f8305b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:19:48 +0000

    open bottom party mouth contents

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
