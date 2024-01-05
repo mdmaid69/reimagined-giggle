efe1a93511ef2fb32fa0b1d0e794424e38e9cff4
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:07:30 +0000

    right zero advantage but crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
