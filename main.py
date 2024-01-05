99301a1391530634d6067921646a2edee2d245ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:28:33 +0000

    consist talkinto now noone wipe

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_memoryview(array):
+        return memoryview(array)
