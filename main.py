f03e2707a683d77ebd8f636356e978ab5720c87b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:11:40 +0000

    other mine always modern holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
