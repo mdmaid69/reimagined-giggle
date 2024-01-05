7d4b3858e8055651bb2dcb1c10734fd699ac8d2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:25:39 +0000

    male building tall group education

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
