1fdcb47386ff76e2561565cf9b9bdc324b056cdd
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:47:52 +0000

    manage deep generous loan persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
