d8cf14c8ad08992c70276530cfe700d7f6b64fd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:20:07 +0000

    gas exercise scrape divide tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
