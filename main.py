25015709fa8f3aad8d19dcc449112898bee83105
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:40:24 +0000

    realize take nothing him pay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
