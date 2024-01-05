6a7cf01ca13c375121dd92f7b64e0f8bdd13dbb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:00:49 +0000

    lower unlikely whichever artificial meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
