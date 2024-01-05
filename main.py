59c3f4399fad88e589ed3d0b17c8174af10e9a06
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:28:44 +0000

    protection touch careful where forget

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_as_tuple(array):
+        return tuple(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
