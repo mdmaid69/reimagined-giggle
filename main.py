f301b740a904d9292a68825a14df41a016669716
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:48:11 +0000

    direct bite terrible position clearly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import random
 print(random.randint(0, 100))
