2946ed96e445583f370c59accddd1aaead99f8f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:12:04 +0000

    getout where risk bag train

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
