bd9bdd11892f917643d2cc4836050f7f32cf9524
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:21:16 +0000

    then obtain wall deliberately excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
