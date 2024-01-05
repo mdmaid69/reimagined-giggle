cfaee621a14bdee5d2c630ec2f1292a9731e0007
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:29:46 +0000

    skin unfair party frequent trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
