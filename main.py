af9550895966f8870083964d0484b450b3ef9f93
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:20:51 +0000

    poor precise heart comeout claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
