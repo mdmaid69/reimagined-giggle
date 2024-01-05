378af887eec2b1e1dae590f72494af3c70edc1de
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:53:45 +0000

    promise fix defeat limit complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
