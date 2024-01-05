54c8e2de10e6b405bb3fa34476eb7e27c73d9f4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:27:00 +0000

    swallow funny size meal news

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
