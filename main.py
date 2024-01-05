1ce4cfab39127dd916a9dd0508478b18acad22f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:16:03 +0000

    positive free distance standard read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
