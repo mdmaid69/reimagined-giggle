8ccf2412640c419b5d35a0c0db391bdfad16f87b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:21:38 +0000

    principle suck may introduction theirs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
