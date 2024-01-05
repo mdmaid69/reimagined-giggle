f09509921b0859e679a56d4a8840a8069ca212dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:56:51 +0000

    strange flood advise sale cruelty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
