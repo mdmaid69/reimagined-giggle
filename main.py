39fb09b879377786cc02c29e61473b47f66d700b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:49:19 +0000

    design smell environment connect manage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
