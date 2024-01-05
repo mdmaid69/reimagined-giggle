ca2bfd8b25ec2068c681a98c75d4bcab69fb4073
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:22:47 +0000

    mess word bend collect drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
