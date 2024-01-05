52fc3ab60a1c843cc40079ac6c06b968356f9115
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:11:15 +0000

    must flood hurt seem climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def remove_duplicates(lst):
+        return list(set(lst))
