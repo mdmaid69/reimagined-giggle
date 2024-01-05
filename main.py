4cda5dd944af1245d428cb0e104083a9eb8844af
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:43 +0000

    thanks music complain nervous vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
