510a9fda437bcd8edfd6a1dfa221bd84b479ec85
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:59:40 +0000

    in hot shopping force hurt

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
