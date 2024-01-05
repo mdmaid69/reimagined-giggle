8e70548281388f9bbadefb3848f56571bec32773
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:40:51 +0000

    basically star manner programme subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_max(lst):
+        return max(lst)
