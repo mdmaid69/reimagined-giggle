f6666732c160e486e638477e3343c83816a22f5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:13:11 +0000

    bet answer general his plenty

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
