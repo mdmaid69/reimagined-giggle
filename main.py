9297028ab38e1faf12f2a5802c95b0e8a27d2db7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:59:13 +0000

    please charge risk before check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
