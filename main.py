66660dd8416f808953b8c1f18cc6552ec0ce13d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:33:16 +0000

    film post address home much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
