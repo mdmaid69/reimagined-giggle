f92ce6dff8cda4ce801340c66d727d30ed6e975e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:30:03 +0000

    interrupt phone after there commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
