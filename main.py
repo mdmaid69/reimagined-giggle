784816dc50015c89a1afa0a173cca1f6fad9f6dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:42:47 +0000

    welcome ours nonsense keep miss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
