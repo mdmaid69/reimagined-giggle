7afe7dd20cabc32ffda43dcee5c8b11f56f323b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:55:04 +0000

    wind together today comfortable so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def convert_to_hex(n):
+        return hex(n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
