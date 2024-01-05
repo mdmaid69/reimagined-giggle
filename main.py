c74d2ca598a7efcfe70cf88687420ce22e3adb38
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:11:18 +0000

    gentle stamp beyond butter spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_working_directory():
+        return os.getcwd()
