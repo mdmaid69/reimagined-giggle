e801514c11b5776cf7882565918159cd030e3465
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:13:51 +0000

    buy early correct dinner concerning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
