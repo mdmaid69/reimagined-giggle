1528a0715f6db5fd79cce06cb67c1cbd3e6580c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:30:05 +0000

    little smell approve closely upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
