525c6bc74ec140f410560416a109b5a0d801b34f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:19:09 +0000

    police print side pressure front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
