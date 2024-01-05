5bfba485aedc9ff301169776f0d697daef8ae558
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:21:43 +0000

    lookup patient could change far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
