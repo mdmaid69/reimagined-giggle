1d871f71ff97a5814ec0d0d53c12fba3526e3fe0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:47:19 +0000

    she about delicate innocent trust

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print("Hello, world!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
