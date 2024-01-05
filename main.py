23aab7644a0b061250dc1625172427e181ea507c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:06:54 +0000

    unfortunate image takeover recognize point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
