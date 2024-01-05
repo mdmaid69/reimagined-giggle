466d41a928643dda84f7fea83c3f01b06ec1796b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:16:45 +0000

    little share collection work circle

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def convert_to_binary(n):
-        return bin(n)
+print("Hello, world!")
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
