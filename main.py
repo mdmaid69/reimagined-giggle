df65429addeeefde2bed7c75e3119c5c8337846a
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:46:27 +0000

    criticize violent sink serve control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
