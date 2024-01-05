b5216f69636d87a89791be9b853732556899541f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:08:19 +0000

    eat rid convince nose holdon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
