1cff6ce6af501e04df8143ec318a2debbbd72499
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:23:12 +0000

    speed disapprove dream tough away

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+print(os.getcwd())
