4cae1bb1227b771e43596ad61b71a4173c0e6734
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:03:57 +0000

    moment sensible limit swim wrap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
