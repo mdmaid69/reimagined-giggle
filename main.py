bd286253a8ca5194cef5f7e4382d861aa4519eb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:43:35 +0000

    delight yet exam heavy remark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
