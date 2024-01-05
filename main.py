d3f10ed427a0d3d661221eed4f5f2cc9bcbca57f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:43:52 +0000

    onto tense tidy door take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
