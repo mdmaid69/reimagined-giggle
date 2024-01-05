98709ac075b7bc47a9ecb0759cefaf89a85de0fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:23:15 +0000

    borrow somehow capital treat character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
