9b74e9897e59cdcc648c7d01bd9e5a0e81e9ea1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:02:47 +0000

    lock improve turnaway name emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
