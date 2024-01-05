b7173b7143c04eb3d22b71369e1f496cf3d8ce3d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:43 +0000

    before shout doubt point rarely

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print([x**2 for x in range(10)])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
