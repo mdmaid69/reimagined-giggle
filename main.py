33692829af7da70b7b82d408944d6df59fb7b711
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:31:00 +0000

    sweep equipment far along summer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
