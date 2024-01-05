8397391c695f94141cd23b19e10b9c4a64221523
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:30:13 +0000

    alone undo strict wonder evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_rectangle(l, w):
+        return l * w
