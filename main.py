6cbb18d980c692a99a5d137db043a07235c5e531
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:28:19 +0000

    good permission spite each division

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def add_numbers(x, y):
+        return x + y
