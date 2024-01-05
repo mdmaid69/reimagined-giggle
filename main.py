7101dcb872964686dd8a1734d52bad01d4043c0d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:52:19 +0000

    temperature thumb top hand hers

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
