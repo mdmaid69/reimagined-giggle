394d9c346edaa00e7d2506080732921a6ee8b7dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:36:40 +0000

    spring class reply equal complaint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def is_odd(n):
+        return n % 2 != 0
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
