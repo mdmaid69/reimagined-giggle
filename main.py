c5222d94f240402daf10932416a1b13fe38c50ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:59:28 +0000

    minute thin Zz problem honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
