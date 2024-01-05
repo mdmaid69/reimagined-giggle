c2ac64542f3fde8aca440814551b5b7bb45f7ef7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:25:51 +0000

    arrange form I last period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import sys
+  def get_python_version():
+        return sys.version
