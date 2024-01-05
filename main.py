53a3938f1a98b84319410a643e4ad9faf4b76029
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:11:34 +0000

    mystery suit promise university task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
