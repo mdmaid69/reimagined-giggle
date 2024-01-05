e618fb57dd1b6682193b9b1b059d49edd1b3de56
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:41:56 +0000

    joke it negative card blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
