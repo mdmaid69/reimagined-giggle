8b76dea0b297f3d88c02b7fa53fc8cca07fdc83e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:25:12 +0000

    OKorokay now fail address noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_bytes(array):
+        return bytes(array)
