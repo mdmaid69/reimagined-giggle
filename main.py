2ea8729900cb39fddfed5c64af1cbdd0b0d6861c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:07:41 +0000

    offence opportunity accommodation back question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
