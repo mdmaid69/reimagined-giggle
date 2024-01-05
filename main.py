6ea756fb672d67e5b16b554025aabc99aa885416
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:00:13 +0000

    abroad even show sensible react

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
