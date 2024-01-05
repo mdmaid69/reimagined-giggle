fe6a938c485f64100621b89c0271503b139ba0bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:41:22 +0000

    chief just tonight uncomfortable valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
