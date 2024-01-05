5ab641a5c9e9304658e5f980ad30b3990a63cbdd
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:12:05 +0000

    activity estimate neither she drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
