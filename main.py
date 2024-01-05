0a7f98c712f6ca07b08a750947fe918a5cb10fc4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:34:13 +0000

    grow pride Ii field medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
