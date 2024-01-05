4411784b87a6895f8490ef5897f6b39291d8999c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:13:45 +0000

    form describe wait accommodation price

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
