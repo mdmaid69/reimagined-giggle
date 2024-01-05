a282e9eede0c7595e8da229c2af3a36cc81fbef9
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:42:04 +0000

    fairly dangerous refuse wind fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
