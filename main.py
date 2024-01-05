fe337cd90440bd5abdc8f3b023c2b6a64f162f57
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:22:11 +0000

    far fashion print getaway perfect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
