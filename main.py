8e4851730e2f88a2132e8e522dad7e223676f584
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:16:27 +0000

    wait year finally age like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
