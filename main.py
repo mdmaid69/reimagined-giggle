3abfe902d9d9b09c3c0b5c8856feedeb977e9fd7
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:05:32 +0000

    comeround style system politics shoot

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def sort_list(lst):
-        return sorted(lst)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
