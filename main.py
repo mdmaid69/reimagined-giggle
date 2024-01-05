d87c86a447ea04f2dfa7ec3a012fab3826f1afbe
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:19:15 +0000

    impossible onto anything agree interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
