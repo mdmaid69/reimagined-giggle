f2c38fdcce23d72bc3f53c5b8be204c020ccfde4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:17:04 +0000

    exercise dig standard turndown notice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
