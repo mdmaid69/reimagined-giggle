362d5b59c42f66374b20b8af3a60a533b8767126
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:45:47 +0000

    cough difference desperate visit mark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
