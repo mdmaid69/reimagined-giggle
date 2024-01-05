fdb5a8deda54fbc1c7b5d418a3f3d35bdc778d56
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:23:49 +0000

    basis love see path injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_str(array):
+        return str(array)
+def count_characters(sentence):
+        return len(sentence)
