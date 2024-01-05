691b3ef26dee14637451d592f43f9fccf1dc5b1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:21:10 +0000

    pipe smell breakout close anyone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def clear_array(array):
+        array *= 0
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
