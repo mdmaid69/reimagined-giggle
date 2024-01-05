d19348c6bddb81a15e7984e5e174a0d5cb6103c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:58:32 +0000

    bored cheek perhaps hurry appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
