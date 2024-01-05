e92768934a8a1512b0fc7c445591bc0845483202
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:15:41 +0000

    in criticism usually add either

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_odd(n):
-        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
