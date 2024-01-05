cfb6ead527a5df843ec92730a6bc6a760f4cd799
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:14:39 +0000

    recognize pencil present represent by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
