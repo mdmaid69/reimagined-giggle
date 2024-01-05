3e9789c9ebafbac8cb7fb2efaf60970d8708c914
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:55:06 +0000

    show tough priority kill swim

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def cube_number(x):
+        return x**3
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
