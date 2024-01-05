3038464d1d3efe0d7510f97d73f5cf35e52d0471
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:24:11 +0000

    forget over yet rain smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
