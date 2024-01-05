aa64f19c34e8a01b9a4d058f315d97824a48a3f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:18:21 +0000

    visit repeat to comeout worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_area_rectangle(l, w):
-        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
