ee3076c4de258cac8b9008184f1e75769b8ab19f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:49:50 +0000

    where mine goodbye willing anywhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_debt_ratio(total_debt, total_assets):
         return total_debt / total_assets
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
