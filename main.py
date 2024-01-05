438257ab29f37701834957c7772f19f37110255a
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:25:30 +0000

    average conversation guilty neither regret

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
