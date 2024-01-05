a2ba04082e7f664954d018c6d6ef4b51dc160927
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:36 +0000

    key warn wind mess beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_itemsize(array):
+        return array.itemsize
