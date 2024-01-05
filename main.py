33719966d860a5e6fe1bce1daedea72bd8a349a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:52:27 +0000

    cost free sail check drink

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
