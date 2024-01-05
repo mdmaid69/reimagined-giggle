61337e72d42c16f124f4bad0f0be38470ef76426
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:53:44 +0000

    so intend involve long ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
