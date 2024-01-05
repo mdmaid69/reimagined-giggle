13b823bdf07cf9d37743cc33728f8d605ae03e6a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:39:19 +0000

    contents still money dick giveback

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(10): print(i)
