e08a0c4b158b443cb132f73926f69008a630ce01
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:14:44 +0000

    approve pleasant win hour notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
