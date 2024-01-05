23e1eef223143331548c710e3406c84dd2d1ae77
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:41:32 +0000

    lend noisy share own before

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+print(math.pi)
