ff5fdb4017d04391b3a26755fff2d43c2e60999b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:26:03 +0000

    cost start plenty shadow punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def remove_duplicates(lst):
-        return list(set(lst))
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
