16136abfc2e121f47085877bb7f0f3cca59dc579
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:26:44 +0000

    develop cause juice apart outside

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import collections
+def create_counter():
+        return collections.Counter()
