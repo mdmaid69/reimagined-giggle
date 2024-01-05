34a53f698241de35ab913f2902b28e632c070d19
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:40:39 +0000

    occur tooth young mend holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
