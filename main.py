c5c158978bba44d2bfad5fc139cd1d500d177721
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:08:06 +0000

    children behind bite genuine basis

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
