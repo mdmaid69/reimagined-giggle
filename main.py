ac8a84f3dc81b362ab845e29f0a68e12b7681097
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:36:02 +0000

    comment beyond find both finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
