50abd5715e0336f8c1819edafcc3e39158c5c635
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:29:38 +0000

    provide quarter instruction defend project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
