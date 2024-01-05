3a384915c9aed140cb2cffcd557fab82ed4ab2bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:53:47 +0000

    argue worth partner properly church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
