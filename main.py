02ed05494659e8a2799ef680971d8e571f79bdfc
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:32:19 +0000

    child tap workon to bit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
