25a92654027110008faa38f98bb06f95c889ba93
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:43:04 +0000

    bringforward equal succeed generous die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
