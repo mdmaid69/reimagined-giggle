f97906c449fa895fa0b16d92eef33b8b3604a454
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:54:38 +0000

    beginning pleasure too popular pinch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_min(numbers):
+        return min(numbers)
