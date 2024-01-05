9cfd2db4f2a2c2319be68f50152d5f19a659642e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:31:32 +0000

    dig whistle turnout get doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_area_circle(r):
+        return 3.14 * r**2
