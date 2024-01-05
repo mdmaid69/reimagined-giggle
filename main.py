3b29afd03f52578e45611948cf6533ab58c73a55
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:26:14 +0000

    confuse dark hard opinion past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
