f3b4083a0746cd8ed803346ba50de862bdc0c020
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:18:09 +0000

    development Yy steep street sky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-  import re
-  def find_all_occurrences(pattern, string):
+import re
+def find_pattern(pattern, string):
         return re.findall(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
