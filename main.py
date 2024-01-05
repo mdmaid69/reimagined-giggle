5cef8c295d68fb4f12870df516ec7593f3e73b36
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:57 +0000

    bringup mouth crash destroy takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
