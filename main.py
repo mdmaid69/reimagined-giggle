e12f312efbc6937932b3a99f4b5fb3f53724575a
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:15:36 +0000

    patience true dare congratulations hurry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_work(force, distance):
-        return force * distance
+for i in range(10): print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
