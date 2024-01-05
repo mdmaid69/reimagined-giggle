0fea94339f5a928320ae9de23ea0ffad9d44379f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:46:09 +0000

    side Vv leg worth course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
