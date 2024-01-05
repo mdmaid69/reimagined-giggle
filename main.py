5418a9b440d80febec59d4cb57cbf086095ae2b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:21:48 +0000

    bang equal scene might believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
