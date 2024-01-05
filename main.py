2863c4ead210f8772d781ed5a25929b21c486a5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:54:05 +0000

    production wave account away mixture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
