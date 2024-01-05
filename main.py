4c6aef3749866fcc55f6d325782b4d3a313a623a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:04:02 +0000

    subject second practical furniture front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_length(array):
+        return len(array)
