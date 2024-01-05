f51c448c2787c50326058058f1c3155703f5b1b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:19:27 +0000

    door nerve speed permission vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import collections
-def create_priority_queue():
-        return collections.deque()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
