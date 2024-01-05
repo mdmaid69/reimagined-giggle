93f0f34dc393efff4e4e1e104a25cede1b608dae
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:59:05 +0000

    foreign difference law impressed scrap

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
