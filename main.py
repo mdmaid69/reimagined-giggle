1d80ebd2776a873b6feeb369a4daa6ba811e8f0a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:55:55 +0000

    pattern recover market sentence me

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
