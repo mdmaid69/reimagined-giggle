fce2473ad7a96d04469f528bd1a1e3fc53b166b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:20:32 +0000

    beside minor name lie prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_power(work, time):
+        return work / time
