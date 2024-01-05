db76e48b1dc0af31fcdb319906dc57d6598bc874
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:45:08 +0000

    separate offence share unhappy month

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_area_rectangle(l, w):
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_rectangle(l, w):
         return l * w
