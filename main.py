4b6b95504ac62be517f742e39906824b46757cd3
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:50 +0000

    term suppose shock runout confidence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
