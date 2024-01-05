644af831a3a21d066505adfed78f00d8e0353f5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:38 +0000

    wrong claim real exit hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
