383efaf021c3ef740f1d8cc19eb20c6ff197f36a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:42:10 +0000

    OKorokay problem trade cover wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
