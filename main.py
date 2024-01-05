97070e02822dcbc3c6ef1815f48b8b044c112e13
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:37:37 +0000

    characteristic everywhere silent smell fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
