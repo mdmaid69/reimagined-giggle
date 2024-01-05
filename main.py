555fcb55d1174b07dea23d5db305fb2dffa016b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:58:52 +0000

    value during gooff criticize pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
