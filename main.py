ce53aac1e77b2adb07356a3d397b7e5091d99cde
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:55:10 +0000

    mail support cost flow cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_error_function(x):
+        return math.erf(x)
