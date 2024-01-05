1fc7f282d91daf0c0b97af79ec70032aef6ee691
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:12:41 +0000

    protest shock blind pretty reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
