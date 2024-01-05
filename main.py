a6a1370d5c9d0df905f6dcdb32183678334c2fde
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:27:06 +0000

    comfortable trap between cry trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
