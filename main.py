1ab114157249654114c830488096f81637635ed8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:02:08 +0000

    time disturbing century lock sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
