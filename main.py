99b0bee3c5b3b2f34d191087a4be8229bfc2f043
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:42:29 +0000

    success date discover gather wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
