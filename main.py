5fa4a80fe93765408df93c87d6dc8aeaaef9ffcc
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:38:58 +0000

    name detail complicated brush board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def clear_array(array):
+        array *= 0
