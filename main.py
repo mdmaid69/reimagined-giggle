c9ccd52759e461ba527b475bc2788d80ddee1391
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:47:29 +0000

    balance aim record Qq different

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_current_directory():
-        return os.getcwd()
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
