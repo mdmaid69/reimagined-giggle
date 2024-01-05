f8bbcc650ab6641a20e39bad2e80434eda79c1f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:22:04 +0000

    skill sale female board scream

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
