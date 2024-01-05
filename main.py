a119168b89974e3dbb2dc45216d59c3c99ae4723
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:20:59 +0000

    good control project Ee fair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def sort_list(lst):
+        return sorted(lst)
