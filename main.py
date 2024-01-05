98b5ee0c14f1f596afc87ecbca22bd958ee805fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:00 +0000

    majority go result frame anything

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
