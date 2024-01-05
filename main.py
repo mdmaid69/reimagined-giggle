396ee134ecbe08716b2a25cb3fa6c6d72c80afb7
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:02:40 +0000

    sudden 569enjoy bury freedom everyone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
