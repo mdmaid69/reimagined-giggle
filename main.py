5bd51a7a889a3b2644bac2a7c752fc696952573e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:12:13 +0000

    swimming alive persuade circumstance dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def clear_array(array):
-        array *= 0
+  def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_user_list():
+        return collections.UserList()
