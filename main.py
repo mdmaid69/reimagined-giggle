4e76a31843241a1d933fa381e5933d0149547f4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:16:00 +0000

    see copy normal married move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_counter():
+        return collections.Counter()
