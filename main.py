06e9c49e38ff1fb17671edec601d9e7577774c7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:08:41 +0000

    smart breakdown cross change suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
