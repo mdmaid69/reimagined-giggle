f8a62d547b48b932c45fa53db8e2fd1a8190c04f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:31:08 +0000

    settle sensitive expression weather bunch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
