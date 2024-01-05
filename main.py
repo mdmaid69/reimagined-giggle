417bcc578c7bf4a2db04e04deb398f66a53bee3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:23:46 +0000

    concern whichever remark weigh past

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
