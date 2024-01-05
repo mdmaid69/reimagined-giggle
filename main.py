9ad42408e082e4f100a0a04d846255574bc9526d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:24:04 +0000

    flow smoke evidence image keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def is_odd(n):
+        return n % 2 != 0
