955035cbe5a9cda739840be449d0f0ea64d9777e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:37:57 +0000

    lonely base buy ahead speak

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
