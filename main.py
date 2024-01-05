7afd1ee10631a3cf3adfe4f65b661820b0709fde
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:23:10 +0000

    yourself finger cause sensitive me

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
