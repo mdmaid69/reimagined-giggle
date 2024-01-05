1df7119491998dea73345f973d3f160317f86b0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:43:03 +0000

    emotion scream about doctor depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
