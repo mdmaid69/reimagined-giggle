3e2388829684bf741d36cfc66ff77daec6c1d886
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:11:20 +0000

    opposite mean gate thankyou gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_rectangle(l, w):
-        return l * w
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_list_from_array(array):
+        return array.tolist()
