33226fd3dda63235d4be03cb0d5c54258fe46874
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:25:41 +0000

    morning instead Ww behind myself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
