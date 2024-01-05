711cea70eaa434099f31a86e2861f2d323e00f5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:56:32 +0000

    off will late scared Bb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def find_max(lst):
-        return max(lst)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def is_even(n):
+        return n % 2 == 0
