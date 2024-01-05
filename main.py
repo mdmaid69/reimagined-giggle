f6ac11edafe3e28f556ef17f71d9e81615981318
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:03:41 +0000

    change otherwise difficult know sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import sys
-def exit_program():
-        sys.exit()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
