3e216ec41821a0a78416a86d7b732957ecc0e413
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:34:51 +0000

    unlucky address soak past turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
