223f4338b430ea7b1b52a6357aef218e4776c28d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:08:10 +0000

    week supply break married feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
