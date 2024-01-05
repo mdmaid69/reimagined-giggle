09fa5d470fe0301a097e26a8ba9840a3dbd37349
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:49:00 +0000

    dark scale fresh bye activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def find_max(numbers):
+        return max(numbers)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
