b5b7b74ff83dd58af2ed77456090796b7800d085
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:17:15 +0000

    sweet join murder stir learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_force(mass, acceleration):
+        return mass * acceleration
