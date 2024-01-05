1a4f770751dc5468b51c6a7268438a6dc2767a14
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:36:47 +0000

    accommodation man relax effect advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
