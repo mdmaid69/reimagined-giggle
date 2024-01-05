8fe59a7967b316b27a3b3fe88c30d97916d273e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:53:02 +0000

    spring rain just motion success

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
