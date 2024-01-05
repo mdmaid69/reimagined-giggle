80182fd15dda35b2582080288d573acfc7f5f0ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:59:02 +0000

    damp target rid ground include

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+from collections import Counter
+print(Counter("hello world"))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
