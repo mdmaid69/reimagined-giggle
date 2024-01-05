21c656d31dab0acd05c3975be7a2b75580d4bafd
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:08:21 +0000

    tomorrow do wealth stamp lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def sort_numbers(numbers):
+        return sorted(numbers)
