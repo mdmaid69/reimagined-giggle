3edb0d7887712349d78d2484881943646b247528
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:21:04 +0000

    afraid scale insist tape safe

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
