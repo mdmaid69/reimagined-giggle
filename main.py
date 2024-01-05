706fcd480d769a6a1ef4cf74212f75da2b88fca5
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:32:18 +0000

    contents comment hole occur maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import numpy as np
+print(np.array([1, 2, 3]))
