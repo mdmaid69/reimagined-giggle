a50f07fc965454ad1f4823c6644a7844adab106e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:53:55 +0000

    nerve someone Hh announce rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def append_to_array(array, item):
-        array.append(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
