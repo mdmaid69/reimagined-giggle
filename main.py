8f6486a7cecae2fc6a937ddd6347c89825c119a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:30:00 +0000

    interruption now solve concentrate full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
