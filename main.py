05879b4d16f653fd943804fa310100f49af8349b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:42:18 +0000

    no post allow no score

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
