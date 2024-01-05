be82f92bd3e1c830cfc324daafa9364278b98ede
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:15:55 +0000

    high reject broad along before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
