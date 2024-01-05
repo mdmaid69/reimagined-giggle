b09690215a692e4e90ec903355c16f3926c61510
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:51:54 +0000

    sick book intend petrol by

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def count_elements(lst):
+        return len(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
