3c53e84f6ee5fe305eade945129b623a8101764a
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:19:59 +0000

    report makeinto tomorrow spare excuse

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
