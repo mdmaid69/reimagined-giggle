7ad3edec0a2196accecd919de8553328c64d88a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:02:10 +0000

    guilty aware Yy last scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def reverse_string(s):
+        return s[::-1]
