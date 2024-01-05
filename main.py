64c4467b2ef11f48c3804c97ba17acbe5093c41f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:23:43 +0000

    party hurt favourite accidental job

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_list_from_array(array):
+        return array.tolist()
