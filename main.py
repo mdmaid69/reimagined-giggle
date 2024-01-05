df6e18c47f649f2c4982305c8222135785276c3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:07:59 +0000

    tall thing 568energy mood principle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
