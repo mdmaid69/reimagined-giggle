d39a211a1f63ba4497635dd0dd9e3cb4bd9fd93e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:17:57 +0000

    only pool frighten hold row

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print(sum(range(10)))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
