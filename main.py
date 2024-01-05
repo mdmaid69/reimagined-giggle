5a39c18a1b29d5998e05861153f761e435dcdf27
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:19:16 +0000

    healthy giveback discuss important after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
