3fbc3b62dc8adffee658d88d37294b0de79c66dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:01:35 +0000

    keepup tip suffer right bound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def greet(name):
+        print(f"Hello, {name}!")
