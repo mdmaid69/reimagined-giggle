22d290c35ebac106a01ade03266ce007fcf7a673
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:41:39 +0000

    guilty profit joke letter cook

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_length(array):
+        return len(array)
