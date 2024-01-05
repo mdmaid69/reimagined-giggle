c6c0ed6d2070a2b7914959e8a78263bd164193e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:32:29 +0000

    position more machine satisfied target

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
