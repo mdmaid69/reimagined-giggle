fd88aee6854201dd05efe2eaa6fe6eb1201807d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:40:13 +0000

    give list inquire careful yet

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
