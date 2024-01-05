5b350761f6f7e98db242f7c767b3df034ad43ea9
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:21:44 +0000

    sensitive at bang doctor definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
