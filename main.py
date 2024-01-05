2a54350febf7e62fe2a879cd7325a7d1a576a50b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:24:32 +0000

    steer news pay contrast example

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
