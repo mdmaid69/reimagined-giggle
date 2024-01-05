7c25604a6a6bcbe96bc74167c60cf695e412621f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:20:23 +0000

    though myself hang same under

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def reverse_list(lst):
-        return lst[::-1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import datetime
+print(datetime.datetime.now())
