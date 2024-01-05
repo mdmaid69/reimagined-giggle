d86528a33273bbd4449172416ac634139074c215
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:51:19 +0000

    cap delicate share must design

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
-def convert_to_binary(n):
-        return bin(n)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
