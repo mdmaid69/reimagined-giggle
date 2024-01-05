13fd8a098cbedf632d75fcf025ad5edf5b68d32f
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:09:31 +0000

    advanced scrap middle way will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_current_date():
+        return datetime.date.today()
