f2e6573ec88b9e5df476bc86756362044c47ac69
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:35:50 +0000

    point pure rain roll confident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
