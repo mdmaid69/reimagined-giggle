371e9ffb10a3380ede80b4ae8e2007c946d6a0a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:02:31 +0000

    activity choice rather refuse mess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def subtract_numbers(x, y):
+        return x - y
+def find_max(numbers):
+        return max(numbers)
