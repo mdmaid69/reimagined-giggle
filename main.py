7ced39e5940fcf9560cd617634572f970b7b0d11
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:37 +0000

    air naturally fairly scream flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
