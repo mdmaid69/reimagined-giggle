4f377a961821f59826d31309c0d0e2cfffb33fe3
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:05:25 +0000

    instruction interview mother single already

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
