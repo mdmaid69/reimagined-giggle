de5bee4a6a3ea208ad263bcdc5c6eab6ac549713
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:22:12 +0000

    shut summer load geton pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
