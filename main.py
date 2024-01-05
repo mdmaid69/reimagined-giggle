271e06126a9b1f9b3650bdc80d24d215564a939f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:25:10 +0000

    work mark now attitude niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def divide_numbers(x, y):
+        return x / y
+def calculate_pressure(force, area):
+        return force / area
