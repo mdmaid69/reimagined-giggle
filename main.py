8db8bbdeaac1f01226d8fd95715165ec6ff9a801
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:40:29 +0000

    computer display show present rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def reverse_list(lst):
+        return lst[::-1]
