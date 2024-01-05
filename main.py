0b6c44d64ba21aa218eab3920dc9cc2f24c28eb1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:40:00 +0000

    exactly pity brave demonstrate yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def reverse_list(lst):
+        return lst[::-1]
