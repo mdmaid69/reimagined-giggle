65eae44841e0e0a08df7746307fd24b3256d8b4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:32:49 +0000

    trade injury repeat ashamed excuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
