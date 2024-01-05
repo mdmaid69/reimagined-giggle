0117160f74c5122d6e5be4ddbe55dfed8cfa7eab
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:50:05 +0000

    advise bet complete responsibility plastic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_set(array):
+        return set(array)
