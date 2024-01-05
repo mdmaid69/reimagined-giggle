420722719e4c30bd360517118a2956c932c29366
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:59:59 +0000

    bringup carry week care offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
