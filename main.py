32a69563cc1620b86c3a2af2a74c0918799ac28e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:21:13 +0000

    slip alone name show arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
