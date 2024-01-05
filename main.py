e3a92f357eb97dd0d94da4a24366a9bb94031530
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:23:05 +0000

    butter suppose failure reasonable stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def greet(name):
+        print(f"Hello, {name}!")
