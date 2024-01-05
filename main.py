0d6622f0fbe80b7d989640f7db9644c8ca7c10f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:08:50 +0000

    beautiful pencil further immediately dead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
