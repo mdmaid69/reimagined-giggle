a41b11d07cf13bf0677d58b122adfaf54c2765ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:29:16 +0000

    seat climb lonely division fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
