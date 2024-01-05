e579099b6cb6d0aacbbbd9acb9fb604bba697130
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:50:11 +0000

    fear ill heat mad explanation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
+def calculate_density(mass, volume):
+        return mass / volume
