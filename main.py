e5a8125126e4138aff3f6fc38978ec62bb40ac7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:01:58 +0000

    ourselves treat part lost ear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+print(sum(range(10)))
+import math
+def calculate_floor(x):
+        return math.floor(x)
