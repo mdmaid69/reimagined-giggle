68eefffc693ad3b09c6e0b01b253d040157d2aef
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:47:56 +0000

    name throw pattern shop insist

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
