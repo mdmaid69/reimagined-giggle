56727b84f790bc872e1f11f340a9ed2eeb953a25
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:30:13 +0000

    trade snow active arm rescue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
