0f5773edfe7cdfe0d7b1f668c817c9fb47d770bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:27:45 +0000

    supply clean should close you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def add_numbers(a, b):
+        return a + b
