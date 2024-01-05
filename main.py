36b6d40ae11f926d4b1a5057ac2708e5439326a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:05:44 +0000

    benefit clean broad immediate contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
