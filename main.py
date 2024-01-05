5f42bfdc9fe03f4acb2a883de941479583f33f39
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:43:54 +0000

    pair secret cap basis contrast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
