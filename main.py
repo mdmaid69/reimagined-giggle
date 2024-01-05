43cd9c6d37ecb303cdc7e0c213da96aca804d08b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:05:04 +0000

    advantage arrive couple mood serious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
