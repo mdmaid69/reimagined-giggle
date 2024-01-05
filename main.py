e8551432ee4edfabdda7631974feea0446d942f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:08:27 +0000

    get land stare off fail

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
