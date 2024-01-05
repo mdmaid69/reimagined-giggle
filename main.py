a0cedf3ccc2a60bc1bd2a029d6be6b63346aa2bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:07:49 +0000

    earn unhappy tear march yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-print(math.pi)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def count_elements(lst):
+        return len(lst)
