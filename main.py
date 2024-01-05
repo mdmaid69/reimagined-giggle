cf771f2527d7c07c43ba7058b1879528b73816a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:20:43 +0000

    receive smell sick correct closely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
