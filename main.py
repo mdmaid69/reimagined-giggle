2c45efcf82bb8f92d9891d0ddb7012adafe218d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:15:18 +0000

    nose interruption independent off spread

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
