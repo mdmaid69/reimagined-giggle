cdab0fcb752309a909ac64c3a277af6b62b3145e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:06:00 +0000

    glad suppose somebody part bringdown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_gamma_function(x):
+        return math.gamma(x)
