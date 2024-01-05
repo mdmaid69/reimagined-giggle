21f5bf856ba181b481b485a8417a7de3b93a5c2a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:37:19 +0000

    well pause skin guard spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
