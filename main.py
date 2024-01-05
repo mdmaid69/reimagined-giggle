d1fd2649b652f490f31426868e3b826aa6922478
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:36:55 +0000

    judge some pain part claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
