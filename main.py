c7c2fd76ece5cb1cadd465f2b952922d668ab781
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:10:17 +0000

    generous listen Ii pupil move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def add_numbers(x, y):
-        return x + y
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
