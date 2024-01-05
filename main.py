5640e5eb15246b902b9eadee89f3fa12085a3b14
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:18:53 +0000

    pronunciation evidence heart wild cope

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
