1dd7e974e08e2503fe25a6eb689070c37ee5e8f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:24:20 +0000

    shoulder except comein without any

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
