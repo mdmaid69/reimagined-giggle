dd5a58036c533f66cecc702eb353c77c38ab5f8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:57 +0000

    unit base excuse rich share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
