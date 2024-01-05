226a75b67a4d20c53b3ef18e73908069c125055b
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:51:37 +0000

    drive search lucky getup regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_binary(n):
-        return bin(n)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
