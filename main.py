e106f28a38eae75cd6ecb535b6065bf70d518f2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:46:34 +0000

    hard mistake belong regularly lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-name = "Python"
-print("Hello,", name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
