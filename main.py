f4656d1edc009c7ae8f31098ea57b36273d507f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:33:32 +0000

    should offend pull actually wise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def reverse_list(lst):
+        return lst[::-1]
+  def convert_to_octal(n):
+        return oct(n)
