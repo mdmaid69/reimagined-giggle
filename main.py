5b619c19210c283f7c8495de46608210dab05312
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:14:35 +0000

    whistle cry generally wealth reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
