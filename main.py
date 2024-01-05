4917d14b737cfa1630c871e47a4b801654f35f2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:42:53 +0000

    now field underneath below pronounce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
