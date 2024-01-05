3ef5ee36a6223a23811e040ae5c9e55c5e10d709
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:06:32 +0000

    strong method majority still keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+  def add_numbers(x, y):
+        return x + y
