73288fcad5ed78a87db9aea37f3aa7eb28cb7950
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:42:09 +0000

    unit whoever list television illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
