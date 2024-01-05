f73f16f8111fe4d0a212399fb9fd1c3a81a0ab9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:55:58 +0000

    greeting class possession cause pump

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
