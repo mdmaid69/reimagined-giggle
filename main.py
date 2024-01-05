1880c2d5d08b4cd42a6d162cd82e4620fa4bdba9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:45:03 +0000

    lean against than major affect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
