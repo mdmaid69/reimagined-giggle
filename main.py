d73aa1efef817092fd840e5f0b82e3b26e6a8c44
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:03:23 +0000

    complaint turn special likely development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
