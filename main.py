ca3ac776c0b6d25f992540891519dbf62d532b9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:28:42 +0000

    result Vv offence down wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
