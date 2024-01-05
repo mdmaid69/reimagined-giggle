39cc117ef61212f50609907a5c57fd50fc58e758
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:34:44 +0000

    tomorrow shop section upstairs family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
