4873c34023ca357165c61251e287ba9b32a1d699
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:02:59 +0000

    smooth service share land picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_string(array):
+        return array.tostring()
