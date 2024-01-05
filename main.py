dd37ca76c077ab1dad51a73d91e2008efa44dd6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:41:15 +0000

    character swing underneath record alive

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
