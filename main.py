2c54fa126b3bb878fc1c72499892150cfd905ab5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:52:33 +0000

    spring powerful any theirs mother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sine(x):
+        return math.sin(x)
