dcc8d01f62de4c8d98d074edd2dc798be1d48c3a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:51:40 +0000

    scream drop children challenge arrival

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
