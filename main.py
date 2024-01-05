acd26ac3dff35d6095eba4be17bc1f6f20380085
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:01:30 +0000

    relation collection bury loose dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
