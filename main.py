63763ecf7d5e0c9a21c9db16f3c562759a0e2a1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:34:37 +0000

    boil issue race fairly there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
