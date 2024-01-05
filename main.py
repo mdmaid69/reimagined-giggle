b25ff02d13b665de12f08febbf906cdb93696d71
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:45:33 +0000

    murder deliver key confused transport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
