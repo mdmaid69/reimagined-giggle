e39f851cccd4279aa2e533a10b8e88b519571fd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:40:49 +0000

    someone day outside near glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import sys
-def exit_program():
-        sys.exit()
+import time
+print(time.time())
+import array
+def get_array_length(array):
+        return len(array)
