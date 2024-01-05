bb91cb1a17ec12d8762ae346130232f9eb07dc44
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:41:25 +0000

    tip fast make information problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
