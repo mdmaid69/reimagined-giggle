206f0efb08f73dab74634663b2fcfe458694ba9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:44 +0000

    repeat urgent miss arrival transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
