17607a6b5e15e963e5a63fea2727d4ec62a45a6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:24:57 +0000

    minute slightly mass into realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
