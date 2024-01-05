93940e26b3ae01d55c313cd0e9076b30c66edc38
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:14:26 +0000

    arrange us figure star dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
 def get_string_from_array(array):
         return array.tobytes()
