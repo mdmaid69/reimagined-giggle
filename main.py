1d7c36ea1ad79bf5588282e8c104265c4d49bce5
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:47:54 +0000

    this hold event lookup mistake

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import re
+print(re.match("h.*o", "hello world"))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
