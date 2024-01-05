6da1753dc3a97c490ddd8751cc77b7a3e81d6544
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:07:02 +0000

    quarter normal telephone shock straight

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
