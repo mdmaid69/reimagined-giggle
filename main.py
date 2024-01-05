26ec77e7fa307b062f094586c06caa536b2c19fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:21:27 +0000

    special rub side bear treat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
