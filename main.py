2501952e8a4e447da77e9ecacef3dd315684e092
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:59:25 +0000

    fall successful important error fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_length(array):
+        return len(array)
