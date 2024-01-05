afca24ad39cd0dbdd89d0bf315a31c62f7abe457
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:30:12 +0000

    ability frightened matter receive fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
