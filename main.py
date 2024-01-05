ad18217eee63145d8c0f504781f1638750d4035d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:46:36 +0000

    throughout dry ever home Gg

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
