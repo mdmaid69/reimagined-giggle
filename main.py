6d722cf6ebeb5d2b38489df89c082d893f397483
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:52:40 +0000

    share bringdown advantage reserve important

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
