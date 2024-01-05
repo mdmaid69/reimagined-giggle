b207f1db1911697c1b71d080557e13e0d05c0812
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:07:13 +0000

    pocket legal go within raw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_sign(x):
+        return math.copysign(1, x)
