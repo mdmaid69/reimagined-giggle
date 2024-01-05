f30adebff1b7cb1e62330126ca0af5291c258b1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:11 +0000

    cough weigh situation like rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
