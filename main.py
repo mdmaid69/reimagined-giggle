9a50894d34465e06647197030c369788bc5a7ca8
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:53 +0000

    perhaps blue yourself pressure broken

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_working_directory():
+        return os.getcwd()
