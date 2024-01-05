bf05ad25ce4ad1ccfdfb7dfe611d1ea3e1119bcf
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:35:20 +0000

    up but city collect dig

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
