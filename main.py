d8b3a068629710be73d76d39afd77e60b2f79ff4
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:11:45 +0000

    too bank Ff positive whistle

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
