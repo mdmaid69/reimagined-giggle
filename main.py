20dd9708a51115d2d05cd18615115d3405514299
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:12:16 +0000

    famous about plan rid fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
