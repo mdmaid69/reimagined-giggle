eeef67b29ab8ff19f6ded01119fece49d9108802
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:36:56 +0000

    swim left history moment suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
