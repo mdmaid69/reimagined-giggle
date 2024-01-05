1970e607f7380599816e3e63ea678e3deba83209
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:18:48 +0000

    nose possession pipe kind choice

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
