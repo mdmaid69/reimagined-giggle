969f64441ac18be4367df947dc2eea1bd1e1a538
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:05:51 +0000

    everywhere turnup window art concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
