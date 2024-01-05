634a1222a3fc4346a55151ec2b5e1a414db9fe28
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:17:38 +0000

    recognize over effect spread slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
