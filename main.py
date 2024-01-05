2e0256db0b6a509b019f83388af915395a98840d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:21:03 +0000

    instrument achieve war unit warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+def get_current_time():
+        return time.time()
