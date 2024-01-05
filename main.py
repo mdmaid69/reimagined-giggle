6cf6d438f15591b9b80296d62c9991202f741005
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:35:18 +0000

    familiar achievement present dull phone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
