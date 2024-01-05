9a8fedae5a3a4bf5cbde9eb560c14eb52e13aa87
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:23:15 +0000

    much about news shadow frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_repr(array):
+        return repr(array)
