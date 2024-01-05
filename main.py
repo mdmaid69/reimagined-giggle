dc491b2cc1288554d4469f1738672eb171165a9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:10:16 +0000

    answer plastic contact no after

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
