fa37c410c59d7bbb5166caf44469d87302aac276
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:02:07 +0000

    become stop really judge bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
