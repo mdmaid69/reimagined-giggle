142d25a33987a46a0ba3ad2ffbbf166baa0bc1a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:55:54 +0000

    getaway regularly bother themselves bother

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import platform
+def get_os_info():
+        return platform.uname()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
