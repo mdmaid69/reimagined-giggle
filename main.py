4f2253530100329eab80b90acb164da8303ea4b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:06:28 +0000

    keep copy thinkthrough luck programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
