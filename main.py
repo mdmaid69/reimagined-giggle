5943ac91d8cb58baf1b7eff056f796e14adf8e59
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:12:28 +0000

    yourself settle detail success call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
