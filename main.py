4d7c073c042d2150348fb3a61361ec318f29f938
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:06:37 +0000

    favourite thick settle uponon really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
