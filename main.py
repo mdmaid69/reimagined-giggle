6d35a1cf14b651d729665f8933bf057c4a96663f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:00:20 +0000

    negative so complain church 568energy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
