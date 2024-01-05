5ba8cad03b9e181f016e7d39ad86bf352f4d7aac
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:08:25 +0000

    discovery base limit he insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def subtract_numbers(x, y):
+        return x - y
