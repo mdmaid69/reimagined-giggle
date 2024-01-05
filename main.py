d85df0c79f7412c5e4180c309828ac4acc4182a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:21 +0000

    tomorrow add hide false regular

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
