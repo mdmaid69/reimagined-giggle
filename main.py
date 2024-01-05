e01903bb573ae55783b14f5f5b2e80f170e65cb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:46:30 +0000

    trouble small achieve similar welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
