8719afe9d7fe08a04b6a6b4c4b04c98f62d4bcca
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:09:18 +0000

    odd nervous square share brown

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
