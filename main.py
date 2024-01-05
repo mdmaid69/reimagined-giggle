a01befac472465e2292b73e7ab31486c367725a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:27:17 +0000

    duty become future fairly complete

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
