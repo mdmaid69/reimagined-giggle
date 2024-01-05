ccfa976d355b4ccb6dba79ec0daf66b99530eacb
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:15:42 +0000

    suit anywhere split race system

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
