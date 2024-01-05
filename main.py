069042d1388c48d775ce75c608671e9cefab8207
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:17:42 +0000

    limit answer summer clean discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
