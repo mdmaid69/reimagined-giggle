740cb8870b396e1e23f087a170a8481f2a4855e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:10:36 +0000

    doctor basis influence even flight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def clear_array(array):
+        array *= 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
