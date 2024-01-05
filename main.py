a2629e55a36b016fe86caf719db35e5cd7e3ea21
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:04:23 +0000

    talkinto likely question concern priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
