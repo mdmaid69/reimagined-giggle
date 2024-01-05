587f7ac39440a1b84e4f081426033e9cec39b8a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:45:29 +0000

    knowledge back now probably habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
