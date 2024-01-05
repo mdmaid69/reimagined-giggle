5a3255baed267991e6f327d7a27bc64a66d0f3eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:59:40 +0000

    grade thought shade all see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
