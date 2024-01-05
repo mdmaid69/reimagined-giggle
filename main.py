a35a5cb3a632a6031f47d2f1e4aadabf44baa75d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:36:17 +0000

    upstairs surface prove black Rr

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
