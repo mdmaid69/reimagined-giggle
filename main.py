4da052e72c5dfb4a71722770eafbc528dc756157
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:52:34 +0000

    fit possibly mean smile crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
