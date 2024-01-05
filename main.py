93ee620b1e894942a83b4ebc12a2e75deda8375e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:58 +0000

    straight friendly failure average manage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_binary(n):
-        return bin(n)
+  def multiply_numbers(x, y):
+        return x * y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
