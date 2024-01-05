7d2a09d9e1a5d17951f1665809995c9a4002191c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:27 +0000

    drawing pressure example bear post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
