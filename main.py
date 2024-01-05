76ef6dddf0ed69a719d3fa82b0c29ee4bbfb27e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:52:49 +0000

    young mainly woman expensive note

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
