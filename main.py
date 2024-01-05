8b13708830e21533ee75a1b09e5176d9f642655f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:07:02 +0000

    rarely speak brief sting schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
