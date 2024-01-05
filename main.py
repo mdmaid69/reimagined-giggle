6b8ec27a85dc2df68dde6592bae6f204d04f4460
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:23 +0000

    answer phone sell use plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
