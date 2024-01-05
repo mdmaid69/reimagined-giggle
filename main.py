3286fa120db8aa7845b06b333162d6625243b135
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:18:55 +0000

    storm separate learn major question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
