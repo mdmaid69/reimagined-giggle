ddf8356fc03b06427ef79850981c1525b7462f2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:45:49 +0000

    male comfortable middle forget fear

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_item(array, i):
+        return array[i]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
