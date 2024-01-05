a2efe8b98175e148e294b56f4e92c83114dbd39d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:39:10 +0000

    blind insist rob yourself negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
