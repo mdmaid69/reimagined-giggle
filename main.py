7dc82c3e35f5289fc8636cf7e6018ccd49852605
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:26:12 +0000

    rule heavy callat wild total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
