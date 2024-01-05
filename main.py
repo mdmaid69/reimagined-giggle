a29fce9beb5a61958bc7c0346ea16baa96c2b2f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:24:38 +0000

    punish exaggerate miss that clear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
