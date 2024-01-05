a76ad49334a40d7d73f9715ca7d265c5b4059d11
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:36:09 +0000

    where glass will morning wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
