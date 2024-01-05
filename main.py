ba63a92ca233ffb0d4933c5338c47ba6865a97d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:25:56 +0000

    present common reserve date straight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
