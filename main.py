2fdd4d29b2aee30d6c241b624a3e3fa4d942a2be
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:38:36 +0000

    risk play model exact tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_working_directory():
+        return os.getcwd()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
