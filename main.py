d5aaf59084937cce8b257c71e35bf5b7f8f4da70
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:13:50 +0000

    instant shape bad suckme store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
