d444f6ec7f24037c36fdca2d3eb2367dad487163
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:48:56 +0000

    vote month bus respect town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
