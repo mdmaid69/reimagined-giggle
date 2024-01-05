cc5df1c9701fd402918480005782e42afc50cc4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:51:21 +0000

    involve language advise underneath risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
