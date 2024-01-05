a2aeac41a5b3dfa87b76f3f15957e9eb2b92da0d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:28:19 +0000

    gooff your wheel mess advise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import random
+print(random.randint(0, 100))
