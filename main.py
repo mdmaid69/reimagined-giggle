fdb5d784240a382c3c399867ebcb777fde4ed72e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:26:33 +0000

    area appear back wander undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
