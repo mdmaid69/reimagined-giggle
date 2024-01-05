db301403f5851fb43c3c2fa0e5238016b57f3725
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:49:22 +0000

    feed strict sense certainly total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
