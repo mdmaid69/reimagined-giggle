6fdc6b6ef2ec63ed3c8e671bcdea97f02bcbd6e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:10:07 +0000

    lookback(on) heavy impressive form rub

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
