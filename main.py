83e09607ef898ff086c14b3abf0b7869477ee461
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:11:46 +0000

    such shopping stiff at understand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
