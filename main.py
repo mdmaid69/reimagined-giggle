d5321cdbfc5c0b3d2c967b42adebed54fb18b8d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:39:51 +0000

    notice push regional preparation father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
