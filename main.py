12f01367fc219bdf24d1b2005cf4a05f1c7f4eed
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:18:30 +0000

    occasional land fond board strange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
