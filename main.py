161648424b09e1090555da63075e92ebf0e73285
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:44:07 +0000

    swimming ground group board estimate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
