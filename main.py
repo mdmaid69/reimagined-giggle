2ff64bb548dd2ccf285014e9eb77b229418b5f8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:49:00 +0000

    within fear more switch immediate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+def count_characters(sentence):
+        return len(sentence)
