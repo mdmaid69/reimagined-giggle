24e949f4355397170281466004c94bdf01b7c6ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:10:23 +0000

    make compete steady 570enjoyable over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
