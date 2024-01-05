5b9e5467107b8e7066a67cffefd22ae528813c1a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:56:08 +0000

    be enthusiastic splash whoever fit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def clear_array(array):
-        array *= 0
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
