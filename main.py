1aa234e92d33f73bb2258e89cc8eb6b8d0adcd45
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:31:59 +0000

    small back Gg soak gentle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
