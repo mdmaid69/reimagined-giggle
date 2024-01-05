192a123665db5621055172f72672a15d8c9ee9f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:16:03 +0000

    standard comment talkback to fear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
