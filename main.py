5ff18adb0d17ab83a92937523f33277a56e7ca96
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:39:37 +0000

    advanced remember meanwhile than callat

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def add_numbers(a, b):
-        return a + b
-import sys
-def print_python_version():
-        return sys.version
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
