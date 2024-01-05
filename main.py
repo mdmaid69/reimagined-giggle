929b761974e3a5286f48ab240130f6519baeae2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:19:01 +0000

    use may knock too its

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
