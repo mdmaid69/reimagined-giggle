04233558948e6ad9a6bfffce137504330f730575
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:06:48 +0000

    far estimate call main damage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
