3d471e50d7494a2b411cb02a383392eb90eb52b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:20:13 +0000

    reply allow search rest level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+print(datetime.datetime.now())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
