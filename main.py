5f5ca5a21df24734cfb7e33dcedefc32620481d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:37:12 +0000

    care fine talkoutof mention number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
