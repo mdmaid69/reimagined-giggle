6d6f5468185576ee5b982f5db087a74451ca8ca9
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:33:24 +0000

    fond way seem careless add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
