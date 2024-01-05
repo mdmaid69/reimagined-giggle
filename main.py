f88000951b8960c495bb99b89020e2a03fc7a261
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:35:28 +0000

    arrival female worth trade mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_length(array):
+        return len(array)
