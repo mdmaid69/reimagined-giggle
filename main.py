bbba07ff14ced006549b3deff8aa94dd3cbf284d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:52:46 +0000

    underneath equal tool public clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
