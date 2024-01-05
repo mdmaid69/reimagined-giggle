1cfa0eea0a0b581b5c9fb2f523c84692446ec65f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:23:14 +0000

    getup rather cut proof mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
