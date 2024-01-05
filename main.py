814f5c736cea92345bbe8df05a5d9eda86770aeb
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:32:48 +0000

    agree hurry spill city performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
