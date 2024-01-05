042bcdab09f8a5137734f7b215dd21c6d0494347
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:47:49 +0000

    guide goon dead just change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
