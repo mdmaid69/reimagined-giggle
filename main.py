de7d2f7a4b9af1c5f557c872fdb7dad9ac75eff1
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:44:19 +0000

    trust image land comeround scared

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def convert_to_binary(n):
-        return bin(n)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import collections
+def create_user_dict():
+        return collections.UserDict()
