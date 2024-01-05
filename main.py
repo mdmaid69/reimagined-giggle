d056175f7546c875eacff16d86cd1ac29cca4805
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:15:07 +0000

    ignore thinkthrough muscle big who

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
