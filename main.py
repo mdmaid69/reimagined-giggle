7ec3efb7026fa7f2d970a732e080a6692d239a7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:35:55 +0000

    usual mix spring say hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
