f3beb27166bf405d7dcad966583af46a294997b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:11:06 +0000

    side than port dick variety

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
