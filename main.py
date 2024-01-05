d40b25901e42468c99d7263b0f09090a7f0bc41f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:08:45 +0000

    cheek Jj throughout road phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
