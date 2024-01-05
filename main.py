29b081237d33b2098b07559598f6c3bd195909e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:34:42 +0000

    direct peace callat smell surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import tensorflow as tf
+print(tf.__version__)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
