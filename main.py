72e27f5f706851de787ac56a064d9b65efdcf87a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:47:20 +0000

    last charge road education street

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_acceleration(speed, time):
+        return speed / time
