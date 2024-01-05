0ba4f0ef77a9be5e420057edaba5b0bc7e5935c7
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:08:45 +0000

    secret light giveup vision offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
