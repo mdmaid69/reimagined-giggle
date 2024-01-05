48879ac7812e0e2f6fe189b00ac579795978cf6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:39:53 +0000

    majority enthusiastic balance try on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
