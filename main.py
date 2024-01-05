4bcd97345b3de7be34dfb62bfe41ace0cb3217b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:51:29 +0000

    left book past season post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
