4776da2e8f72b2e600ac9f8caac01e5cd0d96df1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:50:35 +0000

    sharp wipe separate tell cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
