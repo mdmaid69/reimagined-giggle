3e2205180ab0ff6a69514933b912926dba8bc4fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:19:09 +0000

    push excuse belong rub call

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def convert_to_binary(n):
+        return bin(n)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
