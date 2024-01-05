ac4b979b3c71f3b0c2eb984369488f8c1f0966ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:39:03 +0000

    brush prefer there through just

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import matplotlib.pyplot as plt
   def plot_histogram(data, bins):
         plt.hist(data, bins=bins)
