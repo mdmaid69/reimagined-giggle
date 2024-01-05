ec8ce1c780f6aed9f8b8452daabe4ad804a0bc6a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:18:03 +0000

    disapprove direct persuade turnover modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
