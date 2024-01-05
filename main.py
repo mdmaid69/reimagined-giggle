ca9fee8c9fd31e99721fc42652bbdb9a0bc2761d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:38:51 +0000

    wide central concerned track surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
