1d8af87f33c0b14907ddd9c06037eb3d1dd981a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:36:06 +0000

    Zz needle both system bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
