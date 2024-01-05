7c8a9d90361338d08394af6281eb5e34704b549f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:02:53 +0000

    proper slide duty spill careless

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
