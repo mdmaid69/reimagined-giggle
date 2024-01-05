764cd565a4852e37a737cf9d38060e6c81227d76
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:54:16 +0000

    neither line may practical approval

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
