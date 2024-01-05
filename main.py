bc5cf5166f82951a08d77befbfddb6fb235ce4eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:22:07 +0000

    height until wise cut offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_counter():
+        return collections.Counter()
