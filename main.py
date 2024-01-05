8e9bc575948b6bd3b2a2cb8daa4606443b09fdd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:12:14 +0000

    really smell attractive delight every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
