00abb0ac77ae43d0804d042b3e99bed338684b5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:49:22 +0000

    middle meet will treat search

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
