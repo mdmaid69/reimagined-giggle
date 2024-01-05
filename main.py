ac82b331554486a4132b36a6c10a23086424fb4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:07:26 +0000

    pencil store stranger hour something

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
