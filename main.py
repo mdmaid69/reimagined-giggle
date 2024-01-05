5d3aeb92ab61467f103c863ff682f0ff5cb11e7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:18:07 +0000

    deal special region achievement crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def split_path(path):
+        return os.path.split(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
