ce822e7821bb1a565dfe9eadd95796338d85b5c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:33:59 +0000

    eye ability pleased depend split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
