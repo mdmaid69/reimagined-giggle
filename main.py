90785ddfab81725fb963b256bceafd07a25da357
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:59:57 +0000

    explain protect change row important

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
