6d2bf1d0c1dab36c72318e1068552ac95cdc0872
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:56:23 +0000

    rush relation achieve lookaround furniture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def add_numbers(a, b):
-        return a + b
+for i in range(5):
+        print(i)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
