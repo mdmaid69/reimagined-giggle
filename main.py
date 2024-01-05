712cfa77b0d7cbfc0ebd3dac03a164b3288f380e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:06:20 +0000

    block half last century unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
