3eaa582dbd5cbfcaa5b12d8eac50cf60b3125fda
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:16:42 +0000

    finally your school during weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def count_characters(sentence):
+        return len(sentence)
