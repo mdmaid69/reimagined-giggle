22d0cc58c6c9b56ed8641d135a58bba94e2dbd11
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:56:40 +0000

    none takeback someone beat catch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
