5ed6dad932d8a01027ab9d5fd8a4f5becc7bd3d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:54:59 +0000

    nose beautiful destroy tonight cost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
