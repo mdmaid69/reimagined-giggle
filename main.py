3f1410d443fe166f4f25d8ecd6740a3069b58363
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:32:49 +0000

    scared apparently stranger callback study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
