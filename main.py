f5d77973b52bc8ff2b7d1f46d88624c0e1f26596
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:03:19 +0000

    runover reject herself building teach

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
