25be841c05a5e30b638c34d74b9228ec3bb83014
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:00 +0000

    whisper scale never admire history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
