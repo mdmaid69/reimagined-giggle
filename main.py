eb04ae7e28bee478785d7396a94f05a543ec7137
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:07:37 +0000

    hang suddenly home getback disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
