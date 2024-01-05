ba8f537f14c77d764369a3ebb4d67548666629a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:14:40 +0000

    opposite age snow black shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import sys
-def exit_program():
-        sys.exit()
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
