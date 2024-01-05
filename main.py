faee5c6ffba4d2fdf51812f4ab79e8c75666c936
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:27:55 +0000

    couple scene talk unhappy move

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
