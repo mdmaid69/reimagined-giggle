17bf742d27dbdec13fd614d0a3bce8866c334fce
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:53:03 +0000

    exaggerate Dd must top sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_work(force, distance):
-        return force * distance
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
