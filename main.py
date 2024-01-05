7d0dc36bfaf5557a4c6a9a8ab99f5c5f32701fc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:52:43 +0000

    quantity collection polite crash squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
