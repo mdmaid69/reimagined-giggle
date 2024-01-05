9634b0a0e76db1cc873d72e9cd4b27ddae989b83
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:03:23 +0000

    there university table content safety

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
