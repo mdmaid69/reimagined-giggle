e21af97c32ded6097a3c3c3b57ff7c962b4d59da
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:03:48 +0000

    habit various frequent bet point

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
