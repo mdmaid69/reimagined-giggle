b4a0b0be34d50c5189d468c2c52c0e4ae7a70070
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:52:55 +0000

    beyond common fall unusual damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
