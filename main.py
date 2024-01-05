df8f10c79c4c3cd9205cba2baebafaaf317b3f3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:58:33 +0000

    church black natural convenient occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
