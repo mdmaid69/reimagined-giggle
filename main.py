5d60f3d2297b8d342d50c87ef03a34dd7e34e4ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:46:04 +0000

    dick fast to just somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
