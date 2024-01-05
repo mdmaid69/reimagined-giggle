316d8362dc17efb7e1c7a959ec68e80c27993e16
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:32:45 +0000

    single combination few sound frame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sys
+def print_python_version():
+        return sys.version
