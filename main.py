ae024bb612bc13e0101c435f47e5fa475416c2c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:28:44 +0000

    loan shoulder next transport disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
