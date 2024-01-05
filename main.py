8a8f402065decdacf055f2d348d0f07f3eeed258
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:37:46 +0000

    loud live expensive head strange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
