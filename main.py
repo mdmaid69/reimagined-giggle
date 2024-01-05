f2c2cfa0d65a60aa17b07d635682e52f298ad226
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:13:18 +0000

    cruelty line kind grow vision

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
