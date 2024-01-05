7eae6d4dafb6aae7e28b0ab1f4e73766700ca6a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:37 +0000

    interfere dangerous theirs generous improve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
