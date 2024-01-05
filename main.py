49d81248267d7adb1a302aa618594a720fcf4726
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:38:33 +0000

    whether towards think shallow unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def convert_to_hex(n):
-        return hex(n)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
