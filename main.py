d19dec807929289ae0c0d7580898142ea8852bd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:39:06 +0000

    tip boil hold sensible turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
