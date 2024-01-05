a0f6e1f487eb1164582b78898b1fc7950a623591
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:32:31 +0000

    score crowd teach petrol morning

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
