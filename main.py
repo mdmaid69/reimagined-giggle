1c8964d66646d7b290fd13035eb04be18e56a89f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:15:14 +0000

    medium accept bus possible stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
