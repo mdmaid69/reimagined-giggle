eadae195f237261f0308519f9e4f2053c229e47f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:57:01 +0000

    her sticky rather whole conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
