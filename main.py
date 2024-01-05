e9505997681fb70683e559e084ccd9669bfdf019
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:49:24 +0000

    factory health closely length value

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
