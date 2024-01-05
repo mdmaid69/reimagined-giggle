87fa3196ea5ee5adfcb6f2675cc2b46547e86919
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:21:29 +0000

    like debt news dig view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
