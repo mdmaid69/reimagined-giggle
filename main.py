2b7b8ccff983eab24ed5f2c1ab3da3cda525ad73
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:08:20 +0000

    movement breath know welcome holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
