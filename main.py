493dce47bed344b41e1c84699e445e29fc95fa84
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:19:09 +0000

    absolutely hungry comeoff hard somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
