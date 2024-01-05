c1a273a64c0b27d47f1881b0ffd4b26ba2c4e26c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:37:37 +0000

    separate quality human shock lead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sys
-print(sys.version)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
