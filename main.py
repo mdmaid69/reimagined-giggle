e9e020c8c21c82833485ff5414505d1e0d636612
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:18:06 +0000

    sing fast listenfor century generous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
