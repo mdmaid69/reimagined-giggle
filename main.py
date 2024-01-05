93924edf899332831148bb7d9acd8119595d6ad2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:36:00 +0000

    takeoff transparent soil bang heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
