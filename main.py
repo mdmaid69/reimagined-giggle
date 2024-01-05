f922042847b89783b3dd69fadc96ec9186a55b4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:40:43 +0000

    rush breath address lay runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
