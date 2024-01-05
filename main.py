8aa93ddea10d63bdd66c8830e6b86cd00af570d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:30:55 +0000

    expect positive priority none opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
