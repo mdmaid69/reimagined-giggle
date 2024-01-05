3efd2b8c61f1b60fe80452c7f479f579b094a4db
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:35:43 +0000

    sometimes tense wherever black cheek

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_stack():
-        return collections.deque()
+def convert_to_hex(n):
+        return hex(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
