622818f02f544abb1e48f8d7e4c4c7491b8608aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:12:24 +0000

    always operate impressive hard black

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
