5ac8e4b98fb4efc0c6573ad1fbbeaba1f4ebebc1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:03:19 +0000

    ashamed kind scare giveout difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
