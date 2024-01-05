93c6733b16bf80bcf926cf9d36294634907fa04c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:19:16 +0000

    consist spot she wish small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
