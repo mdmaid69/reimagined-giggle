17d920194e23792b07002a4635286d272a81811f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:44:53 +0000

    off taste tragedy total need

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
