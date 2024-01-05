eaf3c1a97cd6d1f6591f52f536d7d0cdd9811dd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:23:21 +0000

    guard spin unable move direct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
