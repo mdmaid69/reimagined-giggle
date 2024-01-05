e4d482ce8b1096c0c1fe84ad5ed019ec27c34d46
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:52:45 +0000

    store blow final order entrance

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_area_rectangle(l, w):
-        return l * w
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def multiply_numbers(x, y):
+        return x * y
