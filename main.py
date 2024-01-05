7cc0b3e3229c60351673db2b24c2aa2d5fd4d55f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:12:30 +0000

    young instead damage quantity more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
