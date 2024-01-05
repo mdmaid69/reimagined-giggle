9b6f79a40a8ea99adce519b51c45ea9eaf91bb7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:18:07 +0000

    organize accidental closed lost that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
