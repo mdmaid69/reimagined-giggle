fb80643175109a7b816f54855bcb95b49161d01b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:41:20 +0000

    sport recover section skirt almost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_set(array):
+        return set(array)
+for i in range(10): print(i)
