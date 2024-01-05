c162d3ccdc373446105bfc361a465bd63b9dbb6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:32:13 +0000

    taste this the mysterious modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
