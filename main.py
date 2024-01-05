18c285854541c51f63e4dfdee5c27dddb9b7deda
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:02:59 +0000

    angry whisper peace loose nor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
