432e9ae2174edbf5aa2c758853998e9e375fbaa1
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:26:09 +0000

    and plain land takeaway pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
