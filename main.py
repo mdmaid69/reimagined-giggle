3631a864662fb017e3a0a00bc5adc0f22e1a65a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:28:48 +0000

    blow choose little behave one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def split_path(path):
+        return os.path.split(path)
