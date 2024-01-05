5b0c0fc46a4577ac4047dc44e44c3c70e783e959
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:33:09 +0000

    excuse herself flow single onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
