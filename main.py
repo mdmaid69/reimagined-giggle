5a585d79d766c49589ecf778d6db2fe8da7852ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:37:51 +0000

    OKorokay medium combine well lay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
