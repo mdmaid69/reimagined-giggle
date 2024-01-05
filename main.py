9e46e84be333f318ae68f11473a342b2cdb0ea0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:13:57 +0000

    bridge new except become minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
