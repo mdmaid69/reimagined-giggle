1e5d03ddd4d7c5e23d53a5e4ecd7171f89c80062
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:21:57 +0000

    behind do freeze will keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
