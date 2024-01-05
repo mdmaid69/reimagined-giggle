ee93bb46581ce2403b97a0fb60954e25f4148fc4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:05:59 +0000

    mine trust argument argument open

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
