38a9d1d5ba7dd4abf6b57c32df8dde6232fc23fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:39:20 +0000

    something arrival anything string gas

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
