b8a5f37c317e7955708e8d5844b92e73ce443eb3
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:13:17 +0000

    develop interfere weekend sound interview

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
