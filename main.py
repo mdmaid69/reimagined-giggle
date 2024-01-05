ab82fedb3bdc973a85007ba317ac25615d02e7be
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:06:49 +0000

    bright creature feed flow us

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import sys
+def print_python_version():
+        return sys.version
