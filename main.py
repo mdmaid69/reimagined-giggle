2f9e2247471e73d3252496f09891b83fe8e48907
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:17:16 +0000

    happen judge immediately flat equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
