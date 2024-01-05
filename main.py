d4e5406fa0f36051a823b4209dc9846f66c21357
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:24:19 +0000

    suggestion breathe between thumb maybe

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_str(array):
+        return str(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
