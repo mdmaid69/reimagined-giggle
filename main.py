1f7ffec7b2fcceb1f5d3e41388634f69921f5ebd
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:26:37 +0000

    police relative spring ours begin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
