c76b9559c3423ef7ed16bf42e6b4ecdff6daa275
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:29:06 +0000

    month every speak straight value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
