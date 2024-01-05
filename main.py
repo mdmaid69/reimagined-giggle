c4add0a57b4c171b235279e86f6ff037e4302e4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:38:38 +0000

    571enough centre indeed left prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
