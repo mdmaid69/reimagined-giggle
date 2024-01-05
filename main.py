079acd9a803df3892dcba173c75c26b721500cca
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:33:40 +0000

    mysterious sticky secret paper company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
