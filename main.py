e61c635c0ec8c285b94fe85e6a5646e42fda60d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:23:01 +0000

    point concerned obvious successful forever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
