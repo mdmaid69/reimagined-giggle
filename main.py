bf1e5b4158db645500907874e1b486b82b31fe3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:06:42 +0000

    experience we down hand prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
