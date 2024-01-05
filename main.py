20005995b33370ea62d859434b0a55e56b97336f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:17:23 +0000

    anything suppose some concentrate either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
