7b9ac6b20c9911ec5873109e71fc2fa7ec0278d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:50:33 +0000

    treat closely easy experience rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
