2d17eda0025fe2872bcad3c598227c93ae30d5e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:37:31 +0000

    visit slowly whichever Itself nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
