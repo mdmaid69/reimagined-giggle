63df9412f11002ca2aa140dae501b74ab1467e2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:51:47 +0000

    very lesson little force from

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
