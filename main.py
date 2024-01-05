f0177b23d6d65f4556305ab1fb858006a8eb1538
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:17:51 +0000

    born only sad big suffer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_power(work, time):
-        return work / time
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
