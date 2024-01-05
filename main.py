496fbb7093ca45693e89e8fa75cea0533f3135e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:25:06 +0000

    woman hollow normal attempt enthusiastic

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def remove_duplicates(lst):
+        return list(set(lst))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
