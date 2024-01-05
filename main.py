d112c2a14a7f59215a4dc287636d8eb1c30be82f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:21:10 +0000

    its talkoutof cry standard chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
