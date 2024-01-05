6e8f2b8a81066f3417b69d0cc859daa54f60b31a
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:52:15 +0000

    climb manage public answer mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
