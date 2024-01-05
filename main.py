250287166431d2528a91bed33ad367e9469e366e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:35:22 +0000

    apply ticket furniture circle safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
