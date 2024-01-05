6e560bad5d736e91d74eb79db533b70fcf947fba
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:42:47 +0000

    thing tense fall invitation instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
