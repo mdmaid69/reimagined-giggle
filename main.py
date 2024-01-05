3b3051d00f59524cf5f540851c5e1b7443043a4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:59:38 +0000

    soon either bringforward copy similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
