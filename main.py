c69b612bf3b65c20e3c5857aec173f4608645499
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:16:54 +0000

    bend attempt sure purpose cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
