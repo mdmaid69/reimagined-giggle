94dd60d577c8a9b894b130a075d6b6a4339e3ca0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:45:23 +0000

    label point intelligent even tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
