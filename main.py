fae9b3a5e1acaeed0c98bb0b84b5565298dede0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:14:20 +0000

    accident breakinto cover display involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
