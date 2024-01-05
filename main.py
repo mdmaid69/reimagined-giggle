b19a9c70665536528a9dfcff55f8e378665ceaf2
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:37:56 +0000

    wide doctor machine way size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
