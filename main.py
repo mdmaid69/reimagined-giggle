c8b2670a5913159c9dca05c494f078b338821d21
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:21:40 +0000

    clue well who same later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
