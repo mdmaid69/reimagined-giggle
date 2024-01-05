5312fdf0228db125c582802d54a76e2de77551e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:23:07 +0000

    pour guess whenever pass film

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
