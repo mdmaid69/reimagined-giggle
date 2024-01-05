7afd687897c8ceb9fef2f364b14da82e72827a80
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:15:58 +0000

    whole typical butter exhibition store

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def is_even(n):
+        return n % 2 == 0
