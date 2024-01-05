52d5a66b1517d549054be2e070443df2810014bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:18:24 +0000

    lunch phone language no rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
