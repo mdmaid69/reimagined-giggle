65d8ece7d25439c8b725a967e5a01c9d3eb99469
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:14:00 +0000

    build mistake near yesterday should

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
