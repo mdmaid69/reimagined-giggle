58508643185c44c1bbedb6297eb125853c747896
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:35:20 +0000

    pull fortunate crowd passage square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
