d468fb98a3a98f32ebc69720e837e237643b762c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:23:33 +0000

    main choose comfort noone cruel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
