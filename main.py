536b7e0e948a2d3aef0cef71ea1cb65828f3f6a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:39:01 +0000

    pay talk change weekend strict

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
