8ff5b101b22c5b7cc46f8caf6e272228bf7929c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:59:04 +0000

    combine goup collect standard later

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
