cfd5c409bd032d28d4fec078bcd6be4badcffc66
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:40:56 +0000

    frame same live giveup problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
