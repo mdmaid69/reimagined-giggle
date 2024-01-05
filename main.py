624b4cc9cd6fce4385f15700b8326a6df0027b58
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:49:02 +0000

    no scheme laugh usual Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
