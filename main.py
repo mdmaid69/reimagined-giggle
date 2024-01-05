357123092883f394a0b05da24a04f89489ec65d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:46:50 +0000

    rent rise frightened spite partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
