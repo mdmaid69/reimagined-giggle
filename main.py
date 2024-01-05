586fc53080e9c84ad185459b95b6af6b2cca0851
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:18:18 +0000

    record age spray ever will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
