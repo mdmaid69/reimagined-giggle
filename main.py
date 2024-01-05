2dbc0158f8375ce49061af82cbaf0a4a542d6c28
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:10:22 +0000

    clearly thinkof centimetre maybe song

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
