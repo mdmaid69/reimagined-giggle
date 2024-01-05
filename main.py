0df51efcd460b49ac7577d88a80898fbac9ec54f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:05:06 +0000

    pain start justice rush else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
