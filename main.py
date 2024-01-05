3f4bcdaa80f608bf9ddfc2241c89cb22887fd229
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:44:18 +0000

    find run there chance wonder

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
