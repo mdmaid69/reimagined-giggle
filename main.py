b35707e2fcacc5cc191a467416d343dad5960191
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:10:03 +0000

    throw shelter interview freeze water

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
