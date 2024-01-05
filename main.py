b55f0bd88cf5a77565da690ecea34f27fda2cb4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:36:24 +0000

    goodbye school chin female rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_hex(n):
-        return hex(n)
+def find_min(lst):
+        return min(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
