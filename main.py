25dd402ea268339437133f152b699d48d1132787
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:21:25 +0000

    around sing trip towards reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_current_date():
+        return datetime.date.today()
