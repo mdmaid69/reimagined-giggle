9ac92c01a6608662e82af9bdf51aa715b0d38802
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:03:23 +0000

    friend average difficult convince whistle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
