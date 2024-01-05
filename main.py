09d4da7c9bf475045c219769c986a8b95ea100cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:47:43 +0000

    real runover unkind relative hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+print(os.getcwd())
+import array
+def get_array_as_repr(array):
+        return repr(array)
