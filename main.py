2773d76f2bf439f379600ee2ef649b273c70f4bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:29:45 +0000

    feed shall workout grateful less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
