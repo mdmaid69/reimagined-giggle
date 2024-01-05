a20b1b3470d8d05798836bcb03721156a4f1e094
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:17:00 +0000

    or lunch repeat complete arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+print(json.dumps({"name": "John", "age": 30}))
