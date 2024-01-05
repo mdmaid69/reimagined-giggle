9c7793dce05eba0b75b5086484776d6d105565e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:07:07 +0000

    relax programme back gap sort

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
