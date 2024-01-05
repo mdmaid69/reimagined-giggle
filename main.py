5cfc52c57c0f3079a193b06f3849a157fc95e185
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:47:53 +0000

    cope bus hit exhibition here

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
