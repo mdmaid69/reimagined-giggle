0692f7ddd4153be09700fa4b93d48a29653cbf9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:44:15 +0000

    complain sport runover treat cheat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
