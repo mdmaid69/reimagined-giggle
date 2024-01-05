564faf0042eefdac42cab93ebbc9f97d8ad81f51
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:13:41 +0000

    under any answer keepaway expect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
