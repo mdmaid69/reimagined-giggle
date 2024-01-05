bd31fb2218af0daa0c1b1b8d515d845ea532719e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:54:02 +0000

    various where joke such remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
