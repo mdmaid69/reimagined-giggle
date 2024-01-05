6fca4fb3bf804d7b23cc2c06d8eec82ea9a1679f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:42 +0000

    protest cope range stand realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_length(array):
+        return len(array)
