6e0c0260fbb6b97f9e4729ab1a97a9e83520f54e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:27:58 +0000

    leg lunch hard rain centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def calculate_area_circle(r):
+        return 3.14 * r**2
