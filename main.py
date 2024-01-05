3df4f5e9a23829f5cae3e5c292a465adbc9bbcf9
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:30:09 +0000

    without regular name hope answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
