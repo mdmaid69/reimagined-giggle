a6b84ecc3fd8b6310dcb876fb991a2c9eb81bfd6
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:08:58 +0000

    scared heat land difficulty when

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
