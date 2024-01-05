76b7ed7468b138cb435ccbd53f9d38477ff64827
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:28:07 +0000

    class basis scared method unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
