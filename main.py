c7fc0f11015fcc81f755c684158a8391ec9859b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:19:08 +0000

    this want form stretch valuable

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
