c09c01b29f9ad12a2945441d2448e273eacc997c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:05 +0000

    conclusion Cc difficulty traffic fat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
