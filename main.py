111981c206ecd1e5aa36eafa9e55996311d3d06a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:43:44 +0000

    press pronounce takeback surface tour

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
