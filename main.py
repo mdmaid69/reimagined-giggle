bc0169718ac344724305e3f2e8f96524a64aa4b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:06:37 +0000

    hold survive mother identify print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
