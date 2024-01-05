ed52e5cf93b1909675a91eb5af17ce066d91d6a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:19:13 +0000

    high stage balance part everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
