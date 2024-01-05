97de9ff1dadac1af9344b412f6ef73cd35e7c073
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:20:43 +0000

    tread shock yesterday innocent careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
