1988a6b0343cbb56aedad7bf573ade514798b8f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:03:48 +0000

    pity he past fact near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
