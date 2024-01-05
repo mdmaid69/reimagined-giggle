011ae09b277921275fbb907dbc529a832c4471ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:55:06 +0000

    speed pinch smart suck rent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def square_number(x):
+        return x**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
