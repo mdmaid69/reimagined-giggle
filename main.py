ead673b41bd9873adeca55395f418233d57292c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:20:01 +0000

    meanwhile away order legal what

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
