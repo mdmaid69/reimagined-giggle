bc785ab6cf5a7ec4c1c2ccf2ad142d109afb93b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:13:34 +0000

    across half Gg machine prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_sine(x):
+        return math.sin(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
