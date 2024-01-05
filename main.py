7c3e4bb91d772c858cbcdfdc55794ff9b5f77c3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:33:20 +0000

    return uponon comeup thanks beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
