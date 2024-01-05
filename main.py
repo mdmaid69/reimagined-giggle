2953de3bf3a08251effd4beba771b0533a2dec2a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:44 +0000

    plan talkback around copy contents

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
