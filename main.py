a102549552b436d85b21754035a8c6aa2673a504
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:23:03 +0000

    there time sharp violent within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
