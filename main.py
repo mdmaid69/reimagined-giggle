d6a901c9f2a31c39f944e9aedef0187d8ca2c49d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:29:26 +0000

    whom imagination progress explain offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
