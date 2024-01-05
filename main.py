1c9b5604fe4340211bdda239bd817afcdc9c95b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:08:56 +0000

    freeze mean pleasant book suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
