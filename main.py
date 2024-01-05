77ecff01f90a8de58a86bfc5ed5b8f9556b5615f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:35:23 +0000

    rush corner side below rain

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
