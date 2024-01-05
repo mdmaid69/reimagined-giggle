c3f7c21b8e33ed3e2754eed0f94ba9b49a2f906b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:12:24 +0000

    wash scrape practise bye prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def add_numbers(a, b):
+        return a + b
