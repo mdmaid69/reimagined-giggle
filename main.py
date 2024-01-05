9f0643a91763b1b975a50f3b867f8ea94bc6c0ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:58:23 +0000

    blue since ride Ss luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
