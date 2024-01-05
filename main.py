e29ed09f19dcc674d1ebde202721df1f9d329050
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:02:35 +0000

    beyond so 565end maintain whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def subtract_numbers(x, y):
-        return x - y
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
