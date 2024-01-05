adc03fb498ee9a45ed83f0d0cd8d59cf21a5f592
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:25:54 +0000

    greeting already control like consult

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
