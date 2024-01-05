39d7bae72a50bc726df75821efc3786458c95bed
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:58:02 +0000

    well statement by jump patient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
