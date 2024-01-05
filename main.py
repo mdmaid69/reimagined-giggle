a05f800b0f708fef938b77b6c43699e85ed723b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:05 +0000

    last better somehow mind gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+def exit_program():
+        sys.exit()
