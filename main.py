57f39457e591f47d8f42497877606ebdfbd8e03c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:07:13 +0000

    however actual powerful giveaway perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
