3e1398788306a423723e953c881977d748fee7d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:31:35 +0000

    lend through nature across shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_exponential(x):
+        return math.exp(x)
