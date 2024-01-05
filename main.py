08a8780c5c10362a45b25185cf70ad363deb2028
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:48:34 +0000

    mistake book warm accept wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
