c914cd948592346ca9d180e060fef5f9ca0795a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:41:48 +0000

    tooth partner well pleasure noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
