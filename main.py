b931ebee2b393999dfdaf2bd2e472f6dba3af600
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:26:19 +0000

    ignore push sometimes within major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
