afe25d21bbed266cf4d18c7427928ec88deb0138
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:49:44 +0000

    in water rare pleased workupto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
