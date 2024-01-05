3fba7bcc61d74304f37db64b57e72654252cbac0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:01:28 +0000

    face emphasis further forever account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
