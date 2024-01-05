6c309f129863f2ce702fd728dcab681570364252
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:58:43 +0000

    godown week compete quantity trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_repr(array):
+        return repr(array)
