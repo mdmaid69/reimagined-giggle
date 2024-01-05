6c0eaee7a1c55cc61a874d93e31f3d3a1d32d97c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:25:57 +0000

    suddenly effect nearly responsibility recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_list_from_array(array):
+        return array.tolist()
+import random
+print(random.randint(0, 100))
