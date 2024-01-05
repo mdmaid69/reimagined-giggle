ce196611a892d1d9c6030f61893b1458dda5d2d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:51:27 +0000

    accommodation underneath famous suggestion corner

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
