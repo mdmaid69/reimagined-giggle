9d1bbca8e7b3fb51dbb48afaadd8819e918f3608
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:07:11 +0000

    pool guard opinion protection urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
