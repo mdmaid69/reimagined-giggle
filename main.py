99866bca5e7b75322c0eee591f97e0b377408473
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:22:27 +0000

    uniform place small unlikely tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
