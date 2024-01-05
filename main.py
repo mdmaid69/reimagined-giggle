c158322f14fbd5eabf4b429916f38253e558c603
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:38:01 +0000

    possibility difference left glass save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
