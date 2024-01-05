6181b6dfa5c53017817babbbe018bc224222fa05
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:55:19 +0000

    must front contents spoil tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def find_max(lst):
+        return max(lst)
