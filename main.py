6d56f5f75f0cd01f63ce339efe6ac6fd084045f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:51:31 +0000

    average list hardly comein smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
