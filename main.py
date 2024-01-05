f755fcbf82417be53be76dabd9c102d0816f71b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:40:46 +0000

    wheel order future term away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
