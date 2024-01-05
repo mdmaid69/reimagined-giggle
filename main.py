3b3ca4329b02a63c862b3524e62e61a93de3d441
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:52:25 +0000

    fresh please possession weekend recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
