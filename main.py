b3ad284b34ae0d35078cdbfc2b4cbab7bfbd97d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:30:24 +0000

    coat height godown why shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
