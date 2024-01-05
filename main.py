6b23c1a051f7179f342f3f03e4aa23536e7259d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:18:40 +0000

    round wonder loss spare transport

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
