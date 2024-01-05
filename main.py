b6ea8fb9ca3ce6b405f7ee26d686d62141b0b6fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:53:46 +0000

    acceptable thorough eye even drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
