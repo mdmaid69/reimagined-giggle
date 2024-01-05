013f1b448bc6773724a2f0b7bfcf1f7662317e38
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:00:31 +0000

    trust cheap lay separate frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
