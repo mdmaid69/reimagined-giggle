73e07cc34328cc99377fcfd7f469c0c360bc505d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:44:56 +0000

    regret shoot spill doubt together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
