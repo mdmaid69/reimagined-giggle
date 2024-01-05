ee1adf2c4b44f1ac17e4ab9fb80bee725e544122
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:14:16 +0000

    maintain object guilty room connection

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
