9a58a7ec032e669f7c9b26ede9b73214af10547e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:07:25 +0000

    frame separate relationship whistle breakfast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
