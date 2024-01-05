43fe78af7bf63a57e236992c271ce4b1f877beb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:45:02 +0000

    anywhere measure drink contact influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
