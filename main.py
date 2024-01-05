d0c29bf1d14c0c9109398d8007dd8261ce66eb26
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:58:18 +0000

    thought anywayanyhow shop boil reasonable

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
