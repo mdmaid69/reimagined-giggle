190aee0faf7b32979d938ed9fa1a940d51cd87bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:22:56 +0000

    card keepfrom calloff obvious plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_typecode(array):
+        return array.typecode
