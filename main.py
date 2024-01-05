0f94f1b8e79f67fac93639a985478a86524d9a42
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:42:34 +0000

    suckme day rich good once

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
