d75c7ec9a5c8c17f0a73723550c7e0f29b13884f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:47:57 +0000

    scared damp murder keep scrape

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def clear_array(array):
+        array *= 0
