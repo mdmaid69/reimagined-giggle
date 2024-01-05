1e93f1179d38d43a339c8ed86da7c35d839883bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:55:55 +0000

    keepout almost occur her wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
