9df1f963a16eac58f53292ae8a24993694ebe8b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:14:20 +0000

    appeal 572enter ring apart scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
