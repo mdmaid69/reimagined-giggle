1916e362f1c5999de41587efcce5756646f4217e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:33:40 +0000

    upstairs take equal spring exam

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-print(sum(range(10)))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
