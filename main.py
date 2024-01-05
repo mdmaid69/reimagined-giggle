5649b44754a14ca3e40fc1a563fd3083be626e7b
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:31:25 +0000

    nothing rich offer lie spread

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def split_path(path):
+        return os.path.split(path)
