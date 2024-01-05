8da30ba828421d735681a7fae08a8b3613b819d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:10:54 +0000

    in corner charge wrap importance

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
