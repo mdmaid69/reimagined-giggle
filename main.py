fcb27db7e2ec70b24c4ecbabc2ebb97a8873fcd3
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:29:50 +0000

    community copy special introduce have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
