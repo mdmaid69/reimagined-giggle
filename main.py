875a1a4c7798ef3647df940f4cb043a9636a2ee6
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:44:54 +0000

    smell fit occur see only

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
