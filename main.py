96b94b30f36b0dd827f9cbee365cb27fba564232
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:51:35 +0000

    corner treat difference after computer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def add_numbers(x, y):
-        return x + y
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
