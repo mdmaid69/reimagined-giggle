e02b680717dc2faed3620006404c3d06dadc827c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:51:58 +0000

    fortunately pride surprising shrink hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def add_numbers(x, y):
-        return x + y
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
