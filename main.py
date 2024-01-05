b18bb0e2c4b7f4d4bca97ec4d47df6f0d1700fe6
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:29:12 +0000

    equal concentration never stage bit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import sys
+print(sys.version)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
