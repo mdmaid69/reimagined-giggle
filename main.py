84eed7eb2d8efa5e7d3455f37bbcd3d39d81b8ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:32:17 +0000

    help exactly calmly stick comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-print(os.getcwd())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
