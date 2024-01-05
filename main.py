359f40d4d030a93f23093fa2ac03672ab7e8479b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:15:09 +0000

    free attempt takeaway need ourselves

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def is_odd(n):
-        return n % 2 != 0
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
