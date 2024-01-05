bea0d8cf0f1a5b9c5dba8dc9d59ae13546f8983b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:43:03 +0000

    unpleasant there miss ability target

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_area_rectangle(l, w):
+        return l * w
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
