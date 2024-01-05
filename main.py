aaad4cfcb532a29d9ea5d9740b13fe9a538fed03
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:32:55 +0000

    however sensitive listen keepout responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
