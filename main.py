6989178d9a6ad0b9f8fe8b3b8e4f06d25c66c823
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:23:53 +0000

    involve afterwards there extra image

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
