c940a08df48e1d600f49bceb9526c3ea0f1dce9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:34:53 +0000

    possible attractive exploration bored really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
