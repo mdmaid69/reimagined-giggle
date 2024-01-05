bd04cdd2584dc114d140bcce6408708c7b957b9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:35:13 +0000

    keepup prefer finger snow fail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
