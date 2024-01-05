b8ec44ffbdfea93ff1e7cf95037caea47eafdde1
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:33:56 +0000

    wake most fear compare suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
