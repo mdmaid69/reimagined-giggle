6dd9fdf362e38f379c0d86140b61f5e034511ef4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:48:02 +0000

    line underneath neck my fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
