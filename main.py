705349a52086caae267cc3440e933dced7595318
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:19:31 +0000

    mean 570enjoyable opinion class government

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area(radius):
+        return 3.14 * radius * radius
