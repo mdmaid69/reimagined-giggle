402e04b86122a2f275fcf72a65f0456fb21818de
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:10:55 +0000

    slice would regional except section

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
