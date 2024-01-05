467909ece908e3d0e3654c94831e01562e047455
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:53:18 +0000

    insist terrible giveback firm breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def square_number(x):
+        return x**2
