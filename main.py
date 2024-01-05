f68bf596dc6a7f76ab8c01122248c32ac694e7cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:56:50 +0000

    worth group perfect talk fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
