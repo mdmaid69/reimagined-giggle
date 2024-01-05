e791ca3cb7070423696398517d05e47984a706d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:10:45 +0000

    modern sort computer impressed holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def cube_number(x):
-        return x**3
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_time_since_epoch():
+        return time.time()
