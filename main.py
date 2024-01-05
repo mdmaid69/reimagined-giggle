ba505d7efc61862d9b573920469626517ec7aa51
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:00:33 +0000

    thankyou may fasten funny open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
