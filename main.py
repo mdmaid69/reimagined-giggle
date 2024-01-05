dfe17e697826cde5036159378d7d3316bbc32429
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:28:52 +0000

    real recover repeat lookaround which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def convert_to_binary(n):
-        return bin(n)
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
