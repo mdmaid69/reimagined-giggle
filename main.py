bc4fee198949b74ca06c225ad78b67f9ebfbdcbb
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:02:41 +0000

    luck fair while far since

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-for i in range(10): print(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+print(os.getcwd())
