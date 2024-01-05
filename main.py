81c31021409b4fefd9dd34d6179cd5f092a55d2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:26:00 +0000

    might criticism Ii build muscle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_list(lst):
-        return lst[::-1]
+import sys
+print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
