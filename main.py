e22a185f124b0c9e0244c749653b616b0f884283
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:51:25 +0000

    raw create do recognize pen

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_unique_words(sentence):
+        return set(sentence.split())
