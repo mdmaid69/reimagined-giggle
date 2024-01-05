43b606f6ba1f1f75fc3d2ef08dc0d41335692ad3
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:08:08 +0000

    cool cinema breathe lower shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import getpass
+def get_username():
+        return getpass.getuser()
