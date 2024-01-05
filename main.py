7521496430bb4c0453439673c1c5e1ce53b69786
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:39:17 +0000

    mouth ring practice reduce greeting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
