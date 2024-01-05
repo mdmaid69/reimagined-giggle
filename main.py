b2984064a4b347f99d383c6a66fb2b3425cb2f59
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:50:10 +0000

    indeed ask arrange goon regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
