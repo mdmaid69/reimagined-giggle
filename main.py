afaf040a70b4cd1254053a074f3667a103b7cbca
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:18:27 +0000

    something since few cover definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
