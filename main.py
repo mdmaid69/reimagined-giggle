51c7114366fb8ba77fe3c55ce8f85b2f6c5abba0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:35:08 +0000

    poison connect tense answer entry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
