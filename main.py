f5789397584f0142ba72f4d08a8b7b5854eca8b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:02:10 +0000

    test giveaway fat complaint environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_power(work, time):
-        return work / time
+def calculate_pressure(force, area):
+        return force / area
+import getpass
+def get_username():
+        return getpass.getuser()
