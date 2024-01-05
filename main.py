a7a545bc8fe7bfa8c4c7f3576967c7d1a8a68b4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:30:47 +0000

    that their spray store part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
