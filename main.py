98a69b9661a83425e3687f1039d9094726c15981
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:56:10 +0000

    busy cope replace definite feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
         return bin(n)
