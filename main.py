3a10d09da7d93d69d42ea8d5ade10a37b352bb8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:19:16 +0000

    constantly generally manner with allow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
