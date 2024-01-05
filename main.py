b2d3dcb64e04c700a0845b617c326c1a8f10f1b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:00:21 +0000

    love mention workout press watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
