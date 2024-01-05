3c9bf731ba715119ee08b528586922e65d8ec6c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:46:01 +0000

    obvious minimum laugh minimum your

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
