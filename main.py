f98cfe9feef0090e3796f6e2dc87617b250479f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:44:03 +0000

    easy soil club high guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
