daa01b85526aeab4b8a83d0c227e9c62eadbd8b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:49:43 +0000

    generally ability judge play keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
