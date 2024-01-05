99cc4ed32d8a90f5596c6631e3e2f69ab86e09e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:59:38 +0000

    even genuine area fire plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
