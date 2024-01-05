1973d1e5c15c6bae93fd26f540828fd316f5ff47
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:30:13 +0000

    sound wake letter disapprove own

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
