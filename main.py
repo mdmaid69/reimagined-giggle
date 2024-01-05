3d5ed3a0b69665bb52f95971fb47d2ab071bee99
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:47:37 +0000

    piece path absolutely regularly rush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_item(array, i):
+        return array[i]
