ad5a14e8fbe5d7ee3b0a81d54e00dc251efb69ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:28:38 +0000

    minute tread contrast attempt bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
