d80ca0495fd58d0c2d3ff7a9cddd2adfb28de3d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:09:19 +0000

    century away freeze area difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
