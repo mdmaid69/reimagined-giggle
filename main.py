2c73df2b7a537f3861f2e3d0791de5182dc1a54d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:23:52 +0000

    getout expression strict make shape

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
