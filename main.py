5712deaa7a53e606e9a0a4cc6dcadd19e78d42ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:28:18 +0000

    roll little mine regular basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
