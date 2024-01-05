efe158801964b187747d7bf82c80b5974441f0b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:04:51 +0000

    serve less mistake art prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import array
+def get_array_item(array, i):
+        return array[i]
