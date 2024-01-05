ce1051fea97c514968b7393bcd3b13ad1c8715db
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:21:03 +0000

    lucky have beneath phone straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import os
+def change_working_directory(path):
+        os.chdir(path)
