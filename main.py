a28b220097dc2b5ee887e14f620b8ab24347ea89
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:41:36 +0000

    combination care fall easy lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
