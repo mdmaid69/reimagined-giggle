2d0e4283b60aa0dadb14bcbd4970e8801a674f74
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:54:11 +0000

    unite forever mess pass brother

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
