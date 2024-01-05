557a657056cdc9ed083034355854e63c91e6a2db
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:40:39 +0000

    no and downstairs arm world

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print(sum(range(10)))
