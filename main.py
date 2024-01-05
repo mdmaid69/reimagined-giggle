4ec2714a10ecceaec92843639e025d0b8457f903
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:01:48 +0000

    great know rough thinkof history

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
