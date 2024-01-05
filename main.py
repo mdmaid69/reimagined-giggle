dbf1929d1c92f60f5aaed8e3fedbf1bc75120b11
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:28:52 +0000

    convinced freeze marry often aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
