e7c16615ae40c6e3dedf5933d23fa2047df5dc89
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:44 +0000

    same brother surprise pile command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
