41d975ff9b8e1e47fc8aeabafc91b849f32ae6a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:49:47 +0000

    accidentally up ours but serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
