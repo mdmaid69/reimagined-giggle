fc062d7551f19bf8ba753138409db3ac4d84507d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:15:28 +0000

    might someone clean comfortable workout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
