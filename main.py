3ba8dcf6522327af62b64a2fda84e59a0197996e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:11 +0000

    plan might century task anyone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
