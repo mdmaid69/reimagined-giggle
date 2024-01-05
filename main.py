48823a7eec0a888bb91bb7c1596da6734325d0b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:16:08 +0000

    bill trap tonight away already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
