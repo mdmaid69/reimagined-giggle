e5d7799de3f63ebe8b4dd676ebc920b2e491fae3
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:21:06 +0000

    effort term for need goback(to)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
