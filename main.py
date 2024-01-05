b7df8b510fc050bef6637d599344d5165d3be17a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:24:57 +0000

    warn cum accordingto general fine

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-print(math.pi)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def reverse_list(lst):
+        return lst[::-1]
