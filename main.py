b15ecc8fd48a75ad3da4ce1a86a7cc8c07f12dd6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:39:52 +0000

    what willing consult wash as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 n = 10
 print("Powers of 2:", [2**x for x in range(n)])
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
