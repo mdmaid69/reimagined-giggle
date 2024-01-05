711caeda08498286c47e2a72bcdb3e88ae43b26d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:50:01 +0000

    bend within accidental somehow joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
