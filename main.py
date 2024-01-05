01b0020ae3c195bbbdf27bff5db86c05fdb66daf
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:01:50 +0000

    hello deliberate already getaway in

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
