af8b209a7a1c1176622f535021e9a6025879c828
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:18:23 +0000

    identify exactly that party disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
