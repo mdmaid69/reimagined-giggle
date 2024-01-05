b7bafe4f655437ab7c159bc02cb1976145b8408d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:11:03 +0000

    circle whether lunch complete ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def square_number(x):
+        return x**2
