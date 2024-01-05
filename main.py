491a3cad1d90ffe8583abe8ab13f98778435bd87
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:28:03 +0000

    justice harm tidy intelligent principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
