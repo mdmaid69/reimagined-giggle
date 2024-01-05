0a9b3a38124b48109778763687617b4f2acd44cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:03:50 +0000

    sing near awake correct pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def split_path(path):
+        return os.path.split(path)
