75cf10226b52292a7697495c9103098fa5c529c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:06:04 +0000

    everyone effect receive speak type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
