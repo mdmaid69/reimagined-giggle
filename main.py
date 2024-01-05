470b8e76ad7be9928e006f77d073440ef59d8e1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:56:43 +0000

    let rid intend past space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
