54d375b373dda3af288a5a6f226de6027abd16d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:16:14 +0000

    pressure well bath nervous flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
