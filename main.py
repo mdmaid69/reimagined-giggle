ca4dad4fade93419b3a29e8b6e9e2bf8382311c7
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:19 +0000

    wide away schedule smooth emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
