52e56cf939dad81691113ca766270c2ce0f51327
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:27:18 +0000

    complain without niggaacademy certain reach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
