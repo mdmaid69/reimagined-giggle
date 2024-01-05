fce4f15a137eb368ef5d9474f702d37843e4693f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:31:46 +0000

    character detail how fire remember

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
