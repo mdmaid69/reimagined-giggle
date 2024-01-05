e7b1f0ee1d4530f263158a11b597b3f2eb268a4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:15:57 +0000

    once only go water party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def greet(name):
-        print(f"Hello, {name}!")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
