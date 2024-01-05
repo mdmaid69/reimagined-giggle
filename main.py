46c659649a8cd2d4de6477f2eca97676121fed16
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:42:16 +0000

    skill open connection build brother

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def reverse_string(s):
-        return s[::-1]
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
