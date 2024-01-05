34b0ea4bb99ba3076223d529495755a474379488
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:16:19 +0000

    use guard constantly hit wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def change_working_directory(path):
+        os.chdir(path)
