2b4e2d7e3afe98aeab5d5b547190865db674eb43
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:54:27 +0000

    pretend drink deserve nearly majority

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
