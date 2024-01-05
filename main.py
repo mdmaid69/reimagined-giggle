bf46c813ede47096089dbff72692f9143c5aa90f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:42:16 +0000

    great trip pleasant birth suddenly

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
