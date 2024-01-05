1bcaba52e02fd483660e4bf86436a3bc5487c376
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:59:15 +0000

    scrap succeed type pronounce live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
