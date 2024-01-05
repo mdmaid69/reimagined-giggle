397feec94cc7a20feb045df30c68b8ca6e7f1828
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:54:22 +0000

    fact sick pretty so traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  def convert_to_hex(n):
+        return hex(n)
