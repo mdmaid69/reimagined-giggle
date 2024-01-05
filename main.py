cd339dda0f51924cd1a610f48487596ca7cfa921
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:58:21 +0000

    team deliberately decision winter minute

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
