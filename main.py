07a2449dcd4e0dcefcfc0f029990b69e96be9472
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:21:19 +0000

    gradually add cover circle past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
