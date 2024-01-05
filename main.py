4e285164cb9e59d7870c84b57681d979bd781ad9
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:55:13 +0000

    my without such suit breathe

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
