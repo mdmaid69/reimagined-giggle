adf5d639e3d908ad314d7623ebd31091c25b9e90
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:47:33 +0000

    runover room school block medium

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
