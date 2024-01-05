cf8fdc6f49f9aa2a028505019c352405180b8eda
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:57:27 +0000

    glad play machine uncertain professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
