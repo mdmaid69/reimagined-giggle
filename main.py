2981ec03032104196416a7662ab7ca12b928b5ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:12:58 +0000

    runout apply bunch turnup never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
