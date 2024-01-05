648418bfda8315d72648703073881edf09d504b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:31:34 +0000

    safety debt image score card

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_area_rectangle(l, w):
+        return l * w
