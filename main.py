cdb3343c5a02aea97e90ce083e4325da76a04f4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:20:11 +0000

    rely statement sound success bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
