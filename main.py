8eb91e4999c9d0ad2f4baf0c312fcef4cfe15d6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:35:18 +0000

    climb strange sweet giveout punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
