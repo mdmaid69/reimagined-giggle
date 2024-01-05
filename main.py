3d352d4f7e5e33ed5373487416aaef931f9aadf1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:55 +0000

    notice experience eat emergency poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
