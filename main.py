850c619daad5514b0463ca459cc68004f9a7032b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:08 +0000

    relation far fall few takeoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
