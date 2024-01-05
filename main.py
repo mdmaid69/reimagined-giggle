00ca93309b4c6331620f7f64b9ea94c2b8b9f61d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:08:19 +0000

    idea work bite object force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
