0669df82e1ca1a1c5db413975e5131e6b2df2e4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:35:09 +0000

    deliberate crash successful cover dance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def cube_number(x):
-        return x**3
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
