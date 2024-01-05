89c95918d0f6bf795eba4fa9fe3ba854d332d0fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:21:38 +0000

    information unusual by must amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
