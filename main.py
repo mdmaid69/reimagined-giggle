87ca3f181dd57616165d331d75f30b0d73dbf0ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:21:59 +0000

    thinkover help nonsense mind national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
+def get_array_length(array):
+        return len(array)
+import array
 def reverse_array(array):
         array.reverse()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
