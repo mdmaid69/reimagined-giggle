06afc1bde0b2560bd08b7d7f92c79af49e07a1f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:16:04 +0000

    ticket warm able choice pity

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
