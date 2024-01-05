ee231c0f4287762eae1a19d5c5b3473040744f52
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:15:24 +0000

    bread neither bother shut balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
