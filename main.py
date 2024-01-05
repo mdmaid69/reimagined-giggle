6273b6680a2f297c1aa2ae26184ff7fb4299ec0d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:46:05 +0000

    trouble approve curious wave promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
