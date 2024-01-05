08bc96b33319862f49ebe0439d363d9d203900cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:53:28 +0000

    disappointed area life effect force

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def divide_numbers(x, y):
-        return x / y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
