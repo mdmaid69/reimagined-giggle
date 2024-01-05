36375bf1712a87bfdf038e4b8ec3f3e9cf4bdca9
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:28 +0000

    keepup century length physical blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def get_array_as_str(array):
+        return str(array)
