1e9b75860599e2e2aad8b2b1f3a7e6859d4d213a
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:02:00 +0000

    finally image event visit money

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def convert_to_hex(n):
+        return hex(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
