fd489a7e878740949114fa8dae80171257d3d720
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:02:34 +0000

    swallow effect eager trick lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
