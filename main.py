4e339185878a02e20072763f7dc941430f39f829
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:34:38 +0000

    influence comment last day cry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
