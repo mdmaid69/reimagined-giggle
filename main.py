a14ce310bcf9fc4568a0e8be200dc6b9f52d9f76
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:47:15 +0000

    win 569enjoy achievement neither praise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
