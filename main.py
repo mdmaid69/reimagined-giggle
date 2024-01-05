fc029206e15f1dc9740a96658d2804bc379fb174
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:43:17 +0000

    offer challenge centimetre comeoff catch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
