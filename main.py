2e6906fd1c9cb59520cada50b6e737b72e403f66
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:33:51 +0000

    mixture 571enough getback string towards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def find_max(numbers):
-        return max(numbers)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import datetime
+def get_current_date():
+        return datetime.date.today()
