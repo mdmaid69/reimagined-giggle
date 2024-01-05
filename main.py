ed28f42deb463365dc63bf89f3d4156305d12b3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:47:39 +0000

    door boil memory vote clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
