70172a04abb71b75e71646d9a2a2193ac08d1564
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:25 +0000

    once great wing smile experiment

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
