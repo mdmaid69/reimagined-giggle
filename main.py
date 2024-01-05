b41ac867971c70d6af6e73cc98b7a05d5a6ac42a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:01:09 +0000

    surprised than character hurt force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
