8acf3d90b37184eecf96e54bd41aae31a5124e60
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:01:50 +0000

    pretend alive class entrance first

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
+def find_unique_words(sentence):
+        return set(sentence.split())
