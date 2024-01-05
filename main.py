77afc0d42825296f60358d90f02cc79dd52da9b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:28:23 +0000

    entry goup age talkinto needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
