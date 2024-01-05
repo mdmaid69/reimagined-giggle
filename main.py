ef36de5af7034269e7d3803ee3956319e75bfd1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:04 +0000

    repair cover myself herself without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
