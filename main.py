ff27f6bc816361956e3db586d0c4f8736ce91635
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:04:41 +0000

    pity activity chase pause closely

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def find_min(numbers):
+        return min(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
