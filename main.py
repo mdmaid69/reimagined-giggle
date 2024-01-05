0901c2b353ca6d7fe59685a5e0a5712512f93eac
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:34:29 +0000

    sight proud strength minute stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
