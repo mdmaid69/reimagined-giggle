e0a31f960f666ae3cbe87405f0bf3a67d96f3e41
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:12:08 +0000

    regular reliable sensible responsibility duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
