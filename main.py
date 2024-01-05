750fc45f15c368b55b6df05814036bd76cf99d88
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:18:03 +0000

    until allow create innocent long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def reverse_array(array):
+        array.reverse()
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
