d74e917179b028a29c2a3332f6c060c0c947348e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:57 +0000

    plate warning urgent substance ocean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
