234ab993aeb79c63f09fe9543ad70c60c4f8af9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:03:49 +0000

    so rather notice then among

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
