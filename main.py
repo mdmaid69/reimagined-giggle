44ab923ddfa7f5f18b726af21944864f40ead068
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:20:39 +0000

    high human indeed dream change

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def square_number(x):
+        return x**2
+print([x**2 for x in range(10)])
