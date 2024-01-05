6b0bcfd424a74c09fcf3784d534c5f8838eacc24
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:36:36 +0000

    country appeal wander disappointed battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
