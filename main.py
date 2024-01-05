633e5bdff805614f260165f889ca2473a7528d6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:14:06 +0000

    illness still tear direct herself

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
