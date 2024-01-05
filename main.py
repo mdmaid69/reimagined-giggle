ba4283e9b6d8326b2887a51f1c19e1188b32cbf0
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:55 +0000

    damp listen your leave voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
