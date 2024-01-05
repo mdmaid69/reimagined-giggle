4458aea3c12c5de272fe3c69fd9f349f923064fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:48:51 +0000

    correct thing least influence error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def split_path(path):
+        return os.path.split(path)
