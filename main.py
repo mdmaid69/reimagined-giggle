65aa7b4c386564a1429497f6586edf893b69fc70
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:27:52 +0000

    company control system shade group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
