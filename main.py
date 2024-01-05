98a74db54bbf6dab9a7c7ea09e6fb9ca8d9074c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:10:20 +0000

    low before word invitation doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
