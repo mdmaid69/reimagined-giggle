bf10fe5c0d2e05f425a9e6d18bde8f27337c806b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:12 +0000

    responsibility school stop while flavour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-for i in range(5):
-        print(i)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
