b27bd8aa9f63db147e5a93ff301fe5b94141a59d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:17 +0000

    though bank temper cheque than

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
