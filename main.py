8668ce5c64eece03c715b10ce18689a329ba70e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:40:25 +0000

    deliberately used suppose interrupt frame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-print(os.getcwd())
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
