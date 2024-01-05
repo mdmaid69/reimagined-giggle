516b6f48cd76d4adda309251287df0d68d5590cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:14:43 +0000

    statement hot fond comeover contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
