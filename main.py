cf4c1974f5c362b14041d80b74b3475d6bcef0ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:21:03 +0000

    walk slowly hold especially level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def count_elements(lst):
-        return len(lst)
+def extend_array(array, iterable):
+        array.extend(iterable)
