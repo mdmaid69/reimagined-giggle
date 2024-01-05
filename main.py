76d0557dd0dfff362bd550c9f716043b2f66a240
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:14:14 +0000

    slip Ee callback remark obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
