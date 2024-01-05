39e47221388de62e1422a953da329ccd716a3874
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:36:14 +0000

    forward reply blame badly every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
