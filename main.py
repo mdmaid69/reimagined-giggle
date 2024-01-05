24ac7ad1d7210455890b60f98b02a5412e33fc35
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:03:13 +0000

    even rate hungry spoil godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
