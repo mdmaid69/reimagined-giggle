e08d558f37fac577faa4ebd6fd646c6d102e9d68
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:50:03 +0000

    next statement weigh so soon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+import time
+print(time.time())
