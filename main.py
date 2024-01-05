a0ba612eaca104a85dc7c3310e9e5b266eec02d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:21:51 +0000

    Tt hurt that peaceful activity

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
