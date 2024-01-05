4909ea0ee8875a836d61979e8d78a09c2e2c5533
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:40:52 +0000

    takeback better short write frightened

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
