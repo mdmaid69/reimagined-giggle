e262684a00f5b2a18968c64acba39a4b8b9211ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:52:34 +0000

    wind agree cook 573entertainment broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
