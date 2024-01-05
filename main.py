ffc20d6c1fc2123a62661b04eb447a4655dabd4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:45:50 +0000

    brush suit progress upset continue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
