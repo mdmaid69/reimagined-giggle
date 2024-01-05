539511ff66c7430f98404a739b5804aa784f3045
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:32:20 +0000

    or circumstance beneath put store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
