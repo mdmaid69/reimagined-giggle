ad374474bd381e3bc4c93a044c0ef2480f1b401e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:31:33 +0000

    favourite though set mind maintain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
