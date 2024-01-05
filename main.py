878bd4f5432d8d265102b1006b2083679f1ef71a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:06:46 +0000

    plenty mark goout throughout check

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print("Hello, world!")
+def find_max(numbers):
+        return max(numbers)
