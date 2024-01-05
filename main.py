a74daed002af84655e6eb12f5d5a3fae488dcd4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:38:54 +0000

    sorry thinkover sell clear draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
