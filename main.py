e269acaa6e4db3b27c9847139936dc2b036644ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:03:52 +0000

    fence few complicated whenever result

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
