ce94694c4e26d19e75e1c74e4fba6636bdfaf0bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:23:32 +0000

    anxious organization plant deserve argument

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
