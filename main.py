e78b94cd2b456449a27cea6b2adfef40f5148feb
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:14:20 +0000

    experience skill smile what spray

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_hex(n):
-        return hex(n)
+import platform
+def get_os_info():
+        return platform.uname()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
