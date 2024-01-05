3b7999200aca2377605805947cb0859358712ce0
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:59:57 +0000

    risk when shout they watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
