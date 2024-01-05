7f09de42d0507cf2d34ae4108ce5762992d7a09d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:13:20 +0000

    piece turnout complicated clean equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
