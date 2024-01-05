5a89c60550405f36a9461e1aaea121bbd2810fad
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:59 +0000

    dance water middle permission post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import time
+print(time.time())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
