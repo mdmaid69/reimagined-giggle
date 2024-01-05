72844c6ebaf509c9136671460f0e2520119b31ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:58:35 +0000

    love so achieve see body

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def reverse_list(lst):
+        return lst[::-1]
