a81a1e2a5c59b8b5cacd026490c5103e3db45585
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:52:47 +0000

    everyone achieve use confuse everywhere

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
