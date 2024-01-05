04eee578e6cf9e7896cbc38a2d0630cc884249e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:59 +0000

    majority deserve business beyond desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
