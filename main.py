9a2f0b9d50fae68e0f6495f26450de4c484c3b39
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:26:00 +0000

    shallow takeaway away size same

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
