d50e8815597255fc3ef3217c316fe8465356fed9
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:37:19 +0000

    permission wonder ocean characteristic content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def reverse_list(lst):
+        return lst[::-1]
