d8a06f9bb960a17763265844b15ece02542e7c2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:32:45 +0000

    target nothing whatever scare window

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
