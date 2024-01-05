d5c3ae5d7ca0ce4eafdd523e73235ebd82e22f0a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:25:13 +0000

    he community quickly lower arm

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
