3f3e1818373f77bdaec636e3cb44f61b82e7fd3d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:11:53 +0000

    by underneath matter so tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_volume(length, width, height):
-        return length * width * height
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
