3915aa0ae7dc0f13d40153f795189d7186cd3c87
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:24:30 +0000

    market protest communicate quarter suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        print(sys.version)
