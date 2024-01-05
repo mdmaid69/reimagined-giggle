7ca426d139ff297ebce8a0d6d798e3fb03c5d2c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:53:39 +0000

    breakin club date goup neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
