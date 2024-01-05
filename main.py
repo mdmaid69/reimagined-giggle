478b1e170f33b144017a05a9bb55dfac62beddcc
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:24:48 +0000

    kind cover moment point frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+print(math.pi)
