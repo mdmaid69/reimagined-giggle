8492f235caf73d09a257a4b9091fa44ff07dc2fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:49:37 +0000

    especially forever herself drop comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
