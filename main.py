1d20663237e750b06f7449d63433ff5c45ccc794
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:56:55 +0000

    comein trip arrangement Mm theirs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
