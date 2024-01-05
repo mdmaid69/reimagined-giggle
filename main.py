5bcc23e1cb4bebe4c14baffec4224788200c1d40
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:24:35 +0000

    comment father prefer on breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
