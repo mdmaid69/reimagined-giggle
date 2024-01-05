a4b079236c2de6fbbc2426bd0ae02dba18b5bb38
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:16:58 +0000

    afterwards visit generally pronunciation somebody

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
