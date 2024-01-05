bb25ce2a4018c9d196206827b3dd822566e6d3d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:42:08 +0000

    ought set turnaway deep disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
