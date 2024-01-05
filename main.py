4085f9dc42393dab16e4b3a68caa3976e4cc50b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:09:55 +0000

    introduction rather warn hit snow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
