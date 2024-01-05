4fb78ce9349d1ca5412460bdfc5e639ce664de28
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:49:39 +0000

    field method shoot set closed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
