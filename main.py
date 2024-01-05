cda5dedef429e90e937fe022094d7d4647ebb924
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:18:36 +0000

    crowd lay enthusiastic war sleep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
