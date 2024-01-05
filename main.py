7d4b8803637313edf66e9fe037739d7a6339554b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:20:57 +0000

    influence impressed discovery scrap Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
