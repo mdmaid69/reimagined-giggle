dfbe1cd3e685979b13e71ba1602114e9b8c471e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:10:36 +0000

    near keen miss sound community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
