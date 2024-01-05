703588efd77883239aabf8c96a0035be7de3ae3d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:42:02 +0000

    person school movement variety accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
