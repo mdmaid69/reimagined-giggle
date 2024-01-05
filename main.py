bf50f42162575b4b1f17c3bcd1a9863670079987
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:29:35 +0000

    education wait wild succeed approval

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
