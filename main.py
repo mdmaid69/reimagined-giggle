1ebeeb7c7996f26a5fb6137d07fdf42bf66f3c93
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:46:58 +0000

    below tough wealth holiday confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
