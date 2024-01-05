d9ccf67ec370ece12c86cb89c5331b93184b6616
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:14:33 +0000

    bet Zz shop birth shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_list(array):
+        return list(array)
