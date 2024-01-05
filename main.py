3d01bf9b842ce6aa3bb417731a95bb023d6252a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:52:16 +0000

    skirt admit zero sensitive case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
