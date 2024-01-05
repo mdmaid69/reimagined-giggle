1eefd49b47a2796067f76db99bf24afbd2d372e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:02:04 +0000

    face sure score around actually

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
