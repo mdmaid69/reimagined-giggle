bc21779e7e51aeb2e1071f3a6d3a11be37755adc
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:49:06 +0000

    mood 569enjoy communicate star anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
