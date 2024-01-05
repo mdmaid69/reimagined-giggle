ac1fefa83a0abe7a5d3fd910c425cb527d696682
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:18:26 +0000

    freedom middle produce collect standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
