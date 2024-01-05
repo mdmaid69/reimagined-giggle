1ee2e2ba4c5f5b06e5a31deb5eb603a37b806e21
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:32 +0000

    either hand matter person downstairs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
