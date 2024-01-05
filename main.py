ad0ad3159d4ed9e21b8f5ab4c8cde0a3b170eb85
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:51:33 +0000

    union marry illness peace fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+x = 10
+y = 20
+print("Sum:", x + y)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
