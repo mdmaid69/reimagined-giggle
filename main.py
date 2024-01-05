91c37450334b56028ce9debe1de6b3003de503f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:27:00 +0000

    language holdon crisis doctor but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
