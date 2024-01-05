6e8ca89fb685ab99abb11a6fe8438c2ddf6343f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:19:29 +0000

    disapprove knowledge bye exaggerate hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
