532aa1054ab18798f410e6f43ed67dc716fad1d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:59:46 +0000

    pleasure genuine dig shout challenge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
