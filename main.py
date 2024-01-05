22cd86d319949c9dff69059b8d24f7c99f4b87a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:49:16 +0000

    dream history business floor famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
