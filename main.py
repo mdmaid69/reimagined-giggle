b3793e621da36de4894ebf8dcff4234bdd861f7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:20:57 +0000

    desperate record finally mass confident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
