aa5bbd2cb9feb556561d21a4ac242e88d37d7aee
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:59:48 +0000

    careless especially convince quickly extra

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
