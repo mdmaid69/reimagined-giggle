85203b09df6c10d99c4a85ebce4194ef726a588e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:36:04 +0000

    there nothing invitation block possible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_acceleration(speed, time):
+        return speed / time
