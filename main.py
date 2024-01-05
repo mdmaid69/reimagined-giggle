35a6acd900bb4c237fe84eae3db8b5cdba88dc10
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:08:36 +0000

    far machine high possibility once

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
