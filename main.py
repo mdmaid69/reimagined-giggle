2b4dede68d05360fc144c0bca01dc56cae059a62
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:46 +0000

    turnout sink praise achieve arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
