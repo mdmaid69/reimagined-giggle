3e2f66189f45498ceab6cdd2a89b493c2dc6a74d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:23:48 +0000

    patient steady play strength later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
