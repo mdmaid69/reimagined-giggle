3b0d56d16bede6d8016c6b1b9d0165ab96fef5e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:42:35 +0000

    opposite order goon more shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
