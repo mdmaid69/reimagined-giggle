6ba38f0409cb661b73c1d4e3fc4566d33498811b
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:42:38 +0000

    brother type hard genuine pipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def convert_to_octal(n):
+        return oct(n)
+for i in range(5):
+        print(i)
