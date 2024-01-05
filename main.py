a973f4e22720a681ca0e1356364affd841a09635
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:18:47 +0000

    place skin invitation report order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
