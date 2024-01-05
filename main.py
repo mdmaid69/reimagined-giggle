a68317976ccc5ee120e812ac21c154493b2a3a10
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:58:05 +0000

    base accident out front wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
