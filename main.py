d795c65d1d98b47cea8c9ca9f9157b611ba510bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:56:34 +0000

    second deep Gg sail difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
