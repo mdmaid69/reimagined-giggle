4596245de8ca8204705b1fae4c52db592da2f956
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:27 +0000

    our belief steady act worried

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
