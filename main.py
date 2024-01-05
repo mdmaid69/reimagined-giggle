47006ddf20c50f50f4bf65ed3d5a7285708c0065
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:04:43 +0000

    sink stop marry glad immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def is_odd(n):
+        return n % 2 != 0
+import platform
+def get_python_version():
+        return platform.python_version()
