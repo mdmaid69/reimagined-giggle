4472c34e21685c38caedc56703d0631151c3fd6a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:29:10 +0000

    exit life ours copy glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
