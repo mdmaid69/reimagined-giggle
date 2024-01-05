0bc8f02e71253b6ce93dffa79775f47cdc7155e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:28:17 +0000

    ride runout joke now nerve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
