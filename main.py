960ac9ccd7f29fc2de6cade366e9991bf908f824
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:28:16 +0000

    careless able sing breakdown develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
