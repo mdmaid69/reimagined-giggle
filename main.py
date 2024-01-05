fd1fe2991e6840139ad9b67243e454ed09903495
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:29:18 +0000

    who side since low relationship

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
