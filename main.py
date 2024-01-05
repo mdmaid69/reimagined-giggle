dc575392a333ebb9684f8899bfe9d7ac99736e67
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:48:20 +0000

    loud whether cheque brother steal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
