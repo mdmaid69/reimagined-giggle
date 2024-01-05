ad1285557d1234fc6a8492e07a627839ec5fa1ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:27:23 +0000

    square square as deep giveout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
