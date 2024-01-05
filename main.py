d7e39253a4dc721d705d79946f345450470f9998
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:10 +0000

    extremely chance drop agree 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def add_numbers(x, y):
-        return x + y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
