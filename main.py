beb8ec852d66c5e8631098280d03d1779861c786
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:23:01 +0000

    check doubt spread should food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
