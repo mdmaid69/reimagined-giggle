d2107f0ed19812ac09e0da193b72a57354040f75
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:29:34 +0000

    deliberate Uu both board way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
