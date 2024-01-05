5d101087ccb8722574068fd50cbcb8e88babf8f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:22:03 +0000

    welcome acceptable love create serve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+for i in range(5):
+        print(i)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
