386e2376fa591e7dfb0c80c9d94a8cdbee6e174f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:07:11 +0000

    knowledge unfair keepout comein payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
