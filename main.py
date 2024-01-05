b5d3af6d4013bc730537aa3d59bc6594622602ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:00:56 +0000

    expect one mistake strength plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
