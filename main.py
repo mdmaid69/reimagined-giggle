ecf81f937024600111d928b89b950e6e4b42916f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:32:32 +0000

    may fire debt truth themselves

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
