c202411f08d1e5923b47026401d6f1e4fb06e404
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:37:48 +0000

    rely copy her damage appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
