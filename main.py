52274d353775a4b4ba6785ff72c261af1851cc5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:04:05 +0000

    crisis miss smell along deep

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print([x**2 for x in range(10)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
