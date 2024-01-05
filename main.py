749ef1622cf51674141c926540c0db73dc14c320
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:27:21 +0000

    warning basically sign exam available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
