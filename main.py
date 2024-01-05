c18bef797b780d7068f959e1d2fa48746ffa79cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:01:07 +0000

    maybe disgusting test warm with

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
