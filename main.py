77b02d1ff157ec9e3d7a4529a180c643379395f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:38:32 +0000

    contrast strike reply obtain connected

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_area_rectangle(l, w):
+        return l * w
