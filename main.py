4ce1d7d4016c8a0975343ebc999388dd59dd9b19
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:57:09 +0000

    cap tip high hurry chat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
