afbbf79034a7e3d211d395251a840e29aa556f8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:20:19 +0000

    frame mine wave attract for

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
