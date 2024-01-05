70052ca9f169a41b4fd82814ed58abc47cc6bc4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:17:19 +0000

    talkback come reason reduce possibly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
