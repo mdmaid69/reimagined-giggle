627371aa9557f6fc7472187d36cfd5bbc6d6e29c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:05:51 +0000

    off satisfied rich no rent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tensorflow as tf
-print(tf.__version__)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
