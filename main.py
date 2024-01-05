5e94d2e4715e4ab327174e35ee37dbbf5b62ce27
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:15:17 +0000

    better drawing over callat hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
