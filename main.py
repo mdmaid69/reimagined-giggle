c9a680bd7294f1f9e0d50a6885617bd4f7d8a726
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:43:21 +0000

    lose sense issue strike include

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
