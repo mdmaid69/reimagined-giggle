f52abdb5af76b2372433d6a4c589ab3a1bf178bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:30:28 +0000

    suit sure unlucky sound answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_float(array):
+        return float(array[0])
