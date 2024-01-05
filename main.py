2eb9af4cd277ccdc2a1eab53a99a4868a66d0458
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:05:37 +0000

    thought none clue cough feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def count_elements(lst):
-        return len(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
