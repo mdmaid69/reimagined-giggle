f7372a86ed38ae33858b8e5429ed41d3551c7f4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:52:58 +0000

    well onto moment schedule dry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+print(time.time())
