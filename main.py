583a16faf71d761afbefaab63c3d1ad727c0c1d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:16:42 +0000

    address night minority improve blame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
