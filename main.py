5f20336308412d1a8a8fe52060e5350bd7e9c00b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:47:50 +0000

    ours seem tragedy satisfactory free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_as_repr(array):
+        return repr(array)
