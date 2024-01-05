0a2dabd0b19c71a24607a4e75b3d0cd46f52452b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:22:39 +0000

    young odd dry gift offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def clear_array(array):
+        array *= 0
