09bdd2f480eaad91f116511be931bdc499591c8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:32:15 +0000

    branch confidence what maybe afterwards

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
