ac5555db96c5da28b984e2775de361b650905ef9
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:42:37 +0000

    fit swing pronounce brain laugh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
