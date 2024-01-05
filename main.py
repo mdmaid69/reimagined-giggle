fa50a163d0cf9b048f3d1bc17dee8c18c2079d56
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:56:23 +0000

    term sign different accidental by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_sine(x):
+        return math.sin(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
