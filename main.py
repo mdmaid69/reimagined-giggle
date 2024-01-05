92450326599f0a5051d8de22cd644f3346bb2351
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:07:02 +0000

    mark extraordinary cross cost expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(numbers):
-        return min(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
