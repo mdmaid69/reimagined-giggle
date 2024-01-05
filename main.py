82fad1ea0394e1d050a9583a498c802a0f46f6cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:52:55 +0000

    successful downstairs rise satisfied lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
