7d13b6c4c058a55f5bea251b7ec2f67297d01707
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:53:34 +0000

    cross morning getoff bring average

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
