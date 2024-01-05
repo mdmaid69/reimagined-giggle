7e45c8f8d00ce5df6fa296ca39c19a7890df64d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:25:27 +0000

    again mess that settle angry

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
