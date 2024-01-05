44dde985b8e76a61f75493900faa53df1a5d9e65
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:03:41 +0000

    statement mystery breakinto taste carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
