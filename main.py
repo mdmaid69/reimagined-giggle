3c6dd5bed63d69ef9b33943a7233e3dec03edcf0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:41:48 +0000

    sort total mess that building

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.time()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
