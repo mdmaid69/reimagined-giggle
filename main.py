31d6b7e148c831f308fa06ec4dec412a3661c35f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:49:00 +0000

    much sail workon Ll left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
