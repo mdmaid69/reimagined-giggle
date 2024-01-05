dcff8893d6ef564fa601516ea5703c9798e4bbe0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:02:09 +0000

    birth public shopping that getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
