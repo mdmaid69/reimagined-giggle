328f278d5b984346f2bbd0ca3b5c51a7c4016d27
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:54:40 +0000

    Oo steer accordingto giveaway glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
