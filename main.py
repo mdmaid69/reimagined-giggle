0d0f4dc61ee291b7810559ecec42e38315a40577
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:56:33 +0000

    correct tight black approval rate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
