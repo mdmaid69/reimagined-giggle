0b08a4a2a0fdb25db5684969d1e0606e17df61cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:26:49 +0000

    now perform comment need spot

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
