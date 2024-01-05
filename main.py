b266f5d86a48e1cc449074a532b1afac8fdd2202
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:44:43 +0000

    thick become stone thumb whoever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+for i in range(10): print(i)
