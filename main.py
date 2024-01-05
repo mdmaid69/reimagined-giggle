e34e933f615a04123c7ab49d064a44283fd3e1db
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:32:04 +0000

    flat study message steal welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
