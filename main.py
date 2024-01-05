af5ff9faf8daa7d007815be4e546cec71db53722
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:42:59 +0000

    develop unlikely insist fly several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
