54952a0fa6f355a65524ea7663db09d34682974e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:39:26 +0000

    glass hold display mystery size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_string(array):
+        return array.tostring()
