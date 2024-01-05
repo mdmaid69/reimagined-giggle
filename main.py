8deb7c1e05e81de8420389d9733589629a831d9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:20:52 +0000

    pronounce transport hungry such noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def multiply_numbers(x, y):
+        return x * y
