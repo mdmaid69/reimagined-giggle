2bdaf41cd236d6dd77c91184a73967688ae88900
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:31:31 +0000

    finally common more undo separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
