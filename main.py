27f29870e847c0ebbf0afae4cd01fb5a913ce1d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:46:28 +0000

    limit catch smell bet vanish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
