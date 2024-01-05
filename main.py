a3fb9d68c45aabccc72f4eda97a49c4aa69e4c22
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:37:42 +0000

    invitation lay mixture today view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
