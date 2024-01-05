bb93c85da75ef5eaff03bad6cb9c4d6786d6db64
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:53:49 +0000

    like confidence stage with frequent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
