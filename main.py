abe144e7a6f780714a9c5711462f997ca23d55ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:21:51 +0000

    swimming shoot fortunately hungry transparent

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def reverse_list(lst):
+        return lst[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
