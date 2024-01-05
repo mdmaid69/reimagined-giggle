d4a56c30fa59dae05d35f5338a674df82c741996
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:39:14 +0000

    suckme imagination month among yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
