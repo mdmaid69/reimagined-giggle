6ce98a24489a2790b543a243f0e77a6b2cc10f9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:33:37 +0000

    quarter bury impossible before 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_pressure(force, area):
-        return force / area
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
