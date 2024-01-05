864b2498cbfeef4ad432fe92398cf5378ddaeb1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:15:09 +0000

    fun sleep mess report keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print([x**2 for x in range(10)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+def get_today_date():
+        return datetime.date.today()
