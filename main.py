e66e0fbb4aa8c8f3c8422a7b8605f425a3475ddd
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:35:35 +0000

    rush service between silent wave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
