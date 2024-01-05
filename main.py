6f4d13118ea53d4b4536ef8a124cf393692b728d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:36:44 +0000

    behind dance have late team

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import collections
+def create_priority_queue():
+        return collections.deque()
