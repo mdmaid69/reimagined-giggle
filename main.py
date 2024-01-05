e8852e054e19cc36411d7cd2bd49520ee60b547b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:49:11 +0000

    Yy test direction accommodation say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
