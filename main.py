3cc5b903dc0e5919d2bf73e3347f2e486327690e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:56:13 +0000

    minority turninto apologizeapologise bad run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
