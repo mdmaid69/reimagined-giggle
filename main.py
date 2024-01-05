d8c9b1fe261781141d81d880cd785daa5b664c5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:37:06 +0000

    question total lookaround my callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_queue():
+        return collections.deque()
