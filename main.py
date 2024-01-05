e939b27a28b029413b2204a84e23a0041733e13c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:46:43 +0000

    spring rush around surprising children

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
