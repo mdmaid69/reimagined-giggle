4f2b64d6c6b8542b43242d2f4602a0be8a0e25f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:39:49 +0000

    miss tip active bet fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
