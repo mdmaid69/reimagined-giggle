63f73c28dcd0a1861961eaf4367dc55a9f76d604
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:06:51 +0000

    when bye arrival hate preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_speed(distance, time):
+        return distance / time
