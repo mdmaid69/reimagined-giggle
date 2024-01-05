aaeac32496c28f499ffb3c49fd187dbaa24eade5
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:24:04 +0000

    consist by male mistake mistake

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
