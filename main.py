8d0b3f07d2137e40db093fa03187e121908fa48f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:42:04 +0000

    takeback regard activity female wipe

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
