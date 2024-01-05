d37007023922eba1948c23b55fd83c63145d2062
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:18:47 +0000

    cross bringback open wide same

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
