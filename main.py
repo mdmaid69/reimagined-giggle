0e57fe64e2fe94056f3675370ede1eac9ae9d5c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:49:35 +0000

    tap cut discover journey keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
