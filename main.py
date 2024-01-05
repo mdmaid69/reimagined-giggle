dcc6d7b0d572ece9ae18ae20a8a14545f9e8e881
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:41:28 +0000

    hurry Ii recognize follow choice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import random
-print(random.randint(0, 100))
+import numpy as np
+print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
