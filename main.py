4e1b5272db8960f0ba735ca6d288d5045b093c99
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:29:24 +0000

    pride stare shopping minimum shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
