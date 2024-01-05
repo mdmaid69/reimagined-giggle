856441d470276c1898ff3d2c7cb43185edf3caf9
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:13:04 +0000

    hole steal freedom Gg fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
