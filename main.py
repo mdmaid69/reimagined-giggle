e0bbc486eaba4cd005a6fc9532c0b07210075b83
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:08:26 +0000

    like quarter common hospital first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
