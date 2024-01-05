add9a573cec24f8f4fd1b64446ac49ae2787bfbc
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:01:10 +0000

    crisis plain thing can dance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+name = "Python"
+print("Hello,", name)
