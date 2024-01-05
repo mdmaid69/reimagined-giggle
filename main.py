b2c8acea2a662ce4958ed37868fee0ea3d1fd90b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:46:06 +0000

    collect could travel test dry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_pressure(force, area):
+        return force / area
