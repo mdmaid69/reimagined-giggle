1a4b0410b8c69f48d3cf6073c6a1724938904a07
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:13:02 +0000

    complaint experiment wealth 572enter lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
