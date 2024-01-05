740cc4c721b95f08bfdcaddd2af3f3ff6d4a3ef2
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:15:20 +0000

    commit guide move please suckme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
