7e5db7363f153b56b39d11d99c38a82bab62b2bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:26:25 +0000

    recently on actually comedown sick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-print(math.pi)
+def calculate_volume(length, width, height):
+        return length * width * height
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
