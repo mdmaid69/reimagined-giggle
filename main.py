2ef326520d9f85ed9d7342cdb41319fa383b9153
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:29:55 +0000

    him nose become speech serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
