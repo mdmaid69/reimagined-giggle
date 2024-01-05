dd8cf2f1bccd107424580e27d343f0ca206ec0d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:09:57 +0000

    favourite comeout connect on interview

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_array_to_string(array):
+        return array.tostring()
