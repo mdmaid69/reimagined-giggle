fb3d008cde28f6a5e88e799747bcbddf6fd74345
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:45:16 +0000

    break answer to will smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
