8896defb9f5bff68d509c1a36727ebdf04c8c49d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:13:36 +0000

    sign hope subject interrupt basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
