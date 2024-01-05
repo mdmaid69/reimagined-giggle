2067d78d1e763d75944dea8f74bcf75dff0bb686
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:49:25 +0000

    that most spend will Mm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
