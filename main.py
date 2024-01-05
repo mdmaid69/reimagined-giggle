9513b4e855f619ae1b6def98d82b28b5412be5fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:13:56 +0000

    find eye artificial rather round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
