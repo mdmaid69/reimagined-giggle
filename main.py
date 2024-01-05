6badb2d09d1237e15638916e871a1326f2a2a418
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:32:00 +0000

    far legal risk comeround language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def reverse_list(lst):
+        return lst[::-1]
