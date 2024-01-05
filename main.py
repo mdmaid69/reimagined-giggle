d25b113c5f486240a0ba825ee392a1f3b9e870bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:33:23 +0000

    excuse goout side regional later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
