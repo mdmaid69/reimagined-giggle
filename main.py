18740354e5bdabb274af34abeac0a4985a3ab742
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:12:52 +0000

    wait quantity call mess it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
