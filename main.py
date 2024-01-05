03115f95f57f4a57f45f095aa6bda65b78930932
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:13:22 +0000

    mean sing pass refer game

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def square_number(x):
+        return x**2
