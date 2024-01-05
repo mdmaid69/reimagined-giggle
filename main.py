8fb9e69b61f17c6e6a6470e3c8e3647319a6462b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:02:28 +0000

    every turn imagine genuine means

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
