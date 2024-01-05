a501ec86eed8aafb7a84737307bef0f215eea22c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:00:03 +0000

    cost state shop realize everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
