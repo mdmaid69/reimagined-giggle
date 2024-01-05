ddfaa0e3b06722dcb6c8d733e7c1c2f46987e478
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:29:30 +0000

    dinner measure excuse surprising teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
