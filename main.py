ddeb5a235f5547751654a057e951d6e6472d61bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:23:28 +0000

    final lesson forward run book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
