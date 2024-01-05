67ff3ed60b41e56476f6f81de5b06498fa13a8ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:56 +0000

    funny rate hold doubt change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
