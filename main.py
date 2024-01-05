5a74468758057f76812707198b5d772f906d8f3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:21:04 +0000

    cheap ask part figure easy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
