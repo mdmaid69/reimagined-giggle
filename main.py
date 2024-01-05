76df933aa868c749d8c40d64a30efac93eeb2d26
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:56:34 +0000

    right pattern family fine care

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
