f83bc93dbd43831ecad2ddecbcc8c775f105d39b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:11:59 +0000

    leg breakin spite close decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
