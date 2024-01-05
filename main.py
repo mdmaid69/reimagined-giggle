5e374029b0334f7084a8530c2c382192a2ca62a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:26:13 +0000

    turnback dinner honest funny transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_stack():
+        return collections.deque()
