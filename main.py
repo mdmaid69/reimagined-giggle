cdabdbb48457da7050b1fb2ced4e428d520b797c
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:03:15 +0000

    when opinion skirt used interfere

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
