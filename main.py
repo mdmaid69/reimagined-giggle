efc22df647a06c4033562a7844f9b6f53b2bca80
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:54:09 +0000

    proper violence physical partner one

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
