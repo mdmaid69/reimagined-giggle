faf093e7cbc78873bac9050e7b53f3cc5c19edc9
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:24:36 +0000

    holiday married branch talk or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import time
-def get_current_time():
-        return time.time()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
