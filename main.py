259484c281ea8abe4c72cb950bb8afdeac562f56
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:37:56 +0000

    keepup contrast handle smile reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
