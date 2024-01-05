0311e26b3b2ad39e5c61ba1c178990350b2346c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:27:51 +0000

    could solve comefrom recently sorry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+for i in range(5):
+        print(i)
