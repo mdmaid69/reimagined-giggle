450b2317fc7c1144be02daa4898c81a35f086378
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:54:17 +0000

    emotion table behave turnup vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
