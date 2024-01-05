c0e6ee558f93adb9a2e3f54480a76c1f62a4ac5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:31:15 +0000

    keepfrom gather downstairs yours oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
