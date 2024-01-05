82c83bf59063f0c5385ea88d281409878a9cf67c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:33:50 +0000

    cinema cause opposite confusing judge

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import getpass
+def get_username():
+        return getpass.getuser()
+print([x**2 for x in range(10)])
