0d21959dde930b58cd902975a0583e11ee140a58
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:41:52 +0000

    bright soil anybody shade capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
