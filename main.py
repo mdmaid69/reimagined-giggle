f1fbc5279783a0bfa8a8c55e23145a2a3d3d9cba
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:34:13 +0000

    early kick let separate dream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
