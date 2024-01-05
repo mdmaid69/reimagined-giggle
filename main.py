62514434b97bb31ad78f35be43cb68f25b1b6d4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:04:30 +0000

    space giveaway exam explanation call

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_arc_tangent(x):
+        return math.atan(x)
