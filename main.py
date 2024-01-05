e8b20ba28e6d7b2d2f93ff203063934444f574b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:34:49 +0000

    fight attitude or regularly country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
