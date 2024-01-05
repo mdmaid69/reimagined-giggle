52e051f6c04c3bd517170ccf4ba85480a0c30cb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:35 +0000

    work collection wake steal belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
