990df71a654dd5a0a396672be8e9226cf8d647eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:12:55 +0000

    injury proof pleasant relative juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
 import array
-def get_array_typecode(array):
-        return array.typecode
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
