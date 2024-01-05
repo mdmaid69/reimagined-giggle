2b817e6e025456d41f957bca8ce0ca781d31ae9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:55:29 +0000

    top come contact stand miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
