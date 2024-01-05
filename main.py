6abbd00cf25564aa6be8a1331268aad70ce9f2a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:42:37 +0000

    agree reasonable obtain loan appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_speed(distance, time):
+        return distance / time
