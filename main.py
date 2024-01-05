12632d231a90850aaee7a4319ac434ef77b664e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:29:00 +0000

    mine wish onto birth come

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+print(re.match("h.*o", "hello world"))
