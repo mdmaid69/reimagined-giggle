c830d4fbcc6fd5695eca8c2575e37fb891be0b8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:56:06 +0000

    exchange luck regret surprising exploration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
