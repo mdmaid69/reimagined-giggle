126830a3804220769c9d7de9a3ad0c50914528de
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:03:59 +0000

    attempt sore experience convenient taste

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
