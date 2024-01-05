dcdddf190fb36254a85336b231b6e5025d453ec0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:30:12 +0000

    drawing handle original search Ss

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
