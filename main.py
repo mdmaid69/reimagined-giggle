5a859eb6e586568803aefcb4517abd37d655bd6c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:44:49 +0000

    serious behave suppose limit past

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
