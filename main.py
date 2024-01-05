105b8d23730ed61aed73b703a76d8098bec7b076
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:37:09 +0000

    neither activity warning serious beat

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
