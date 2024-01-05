481e16bef38aeb6fe99f57c9c4fc1a2b250a44a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:52:11 +0000

    object feel slice exaggerate hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def add_numbers(x, y):
+        return x + y
