8fe14ea6306bb6c171366de4314743ffc0d50fcd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:10 +0000

    room to emotion disgusting awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+import array
+def clear_array(array):
+        array *= 0
