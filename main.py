132f43fab1aabf477103409f5c4ec0506eb5a71a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:45:10 +0000

    steep over Uu complete furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
