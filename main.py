1851e11d9d461011b7bb94b660a6059f965060b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:15 +0000

    gentle sail contrast steady seem

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
