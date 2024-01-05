e232e647e7e75b417bbf5152c7da68486a1a09aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:33:18 +0000

    unable room ticket 564encourage slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
