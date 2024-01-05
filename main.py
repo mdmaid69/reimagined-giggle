fbe953a8f0e797cbb1a70839bed10f75ef9d6c0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:21:46 +0000

    apply butter winter swing passenger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_bool(array):
+        return bool(array)
