cf425d1969d04492974e4639290133d0a94b5190
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:45:12 +0000

    feeling way damage advice another

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_set(array):
+        return set(array)
