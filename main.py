75f2ed8b659b1baaa04ed5fd1b58f69f36e4873e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:42:31 +0000

    attention comefrom excited getaway holiday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 def find_max(numbers):
         return max(numbers)
