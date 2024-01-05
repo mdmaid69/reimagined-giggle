374e17fe9daacc652592992b0f7b46b4c784d190
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:21:03 +0000

    piece summer see disappear deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
