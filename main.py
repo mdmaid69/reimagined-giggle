141204d72ec1b59fb90052e4c48f1fc1219f4429
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:23:55 +0000

    escape build politics put battery

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
