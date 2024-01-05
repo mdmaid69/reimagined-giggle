e848c73c3cb2b53c523dd9a1fe4ae0a45b8a4f86
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:14:45 +0000

    fairly crash clean test scheme

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
