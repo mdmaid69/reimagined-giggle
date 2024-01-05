7263af01fbd33b76fda04c9810b8e61b8b311bc2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:50:56 +0000

    hungry sink morning attractive never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
