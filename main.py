3622f98dbfae349478fb0967d1f0cceaa610977a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:43:06 +0000

    thought worse raw destroy interfere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def find_min(lst):
+        return min(lst)
