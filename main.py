e1f5131d2a6a92dd990cfb050882ee7e3b38147b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:43:34 +0000

    would unite post comefrom mention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
