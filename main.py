4b135a0fbcc8db0900977e5c8c96aacf89879629
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:45:14 +0000

    supply me light whom creature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def convert_to_binary(n):
-        return bin(n)
+import time
+print(time.time())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
