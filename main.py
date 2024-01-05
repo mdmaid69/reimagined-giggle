0899f5410545d399b5a42aaffb6d3876129c8d21
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:32:36 +0000

    commit invite swim store off

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def reverse_string(s):
+        return s[::-1]
