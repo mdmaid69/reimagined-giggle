e8d9aea5a9a5f3a4f3963bcb92547d2890812abd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:33:24 +0000

    tie butter meal middle middle

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
