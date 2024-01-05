67905cfe3027592b8be4d7c1f354e01fa3b8b8d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:06:53 +0000

    awake thin movement ignore excellent

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-print([x**2 for x in range(10)])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
