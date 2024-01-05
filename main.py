5e64cce2374d95d970fb63eeff7813a2d61925fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:12:10 +0000

    lie bet work cry clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
