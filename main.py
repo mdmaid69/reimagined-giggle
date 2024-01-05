b6ef587ad259bbb0e768d9c0052d8d9d1b4d6db1
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:21:37 +0000

    brush height wrap lend unless

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
