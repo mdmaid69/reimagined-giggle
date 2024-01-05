5f0b9719fc4af7954a4b80547ff50222763a825b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:33:59 +0000

    Uu joke sound disapprove wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(5):
-        print(i)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
