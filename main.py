bd0bbcf73dfc9f152f5982f8b6d0378344de4f07
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:55:18 +0000

    step soon mix by live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
