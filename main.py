b9000b4e70ac0769325519e0390688f7c79ad863
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:41:23 +0000

    minute importance wash prefer separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_as_int(array):
+        return int(array[0])
