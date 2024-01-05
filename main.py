b3e31a9ac86b8ce29fdbe2db7c89b8994ba26b06
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:34:44 +0000

    affect spring achievement recognize sweet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def sort_list(lst):
+        return sorted(lst)
