cbb288afe78cfb546915ea43e319a660e0633479
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:38:08 +0000

    influence accordingto small realize comfort

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
