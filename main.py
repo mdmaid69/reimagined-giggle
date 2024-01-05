59e641a8ad3cea838b64e6611248b3cdf9f43f5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:48:20 +0000

    rubbish Tt confusing rare pay

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
