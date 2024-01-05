4b473dee11b6820ef4d7933f283e49a8d9bea347
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:43:48 +0000

    secret close programme confident cold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
