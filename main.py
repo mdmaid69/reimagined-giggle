58e57f2bb5411014d37e27306759af43be1b4e9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:22:25 +0000

    achieve behaviour blue centimetre move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def convert_to_octal(n):
+        return oct(n)
