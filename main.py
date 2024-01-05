22830b62e29700cd2c20e36db2b7db00f26b6fe2
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:29:58 +0000

    divide save invitation before dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-print(os.getcwd())
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
