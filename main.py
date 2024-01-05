ba5a3a71e80d4f1d2697eac6d7e5f1141f2a2fa7
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:52:10 +0000

    start tidy mean comeover raw

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import os
+print(os.getcwd())
