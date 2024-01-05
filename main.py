a12502cf06ad30308dfdf2005afc56e8fd7013d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:56:40 +0000

    yourself talkinto negative frame find

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print(sum(range(10)))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
