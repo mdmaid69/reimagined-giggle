2095606a9627a34ef5f45a528967cf518f4fcbc8
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:36:21 +0000

    tidy use fast 573entertainment lie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
