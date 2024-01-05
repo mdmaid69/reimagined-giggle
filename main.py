7568c096699c1890f3060c0f24004f9e7cff02ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:14:20 +0000

    giveup correct little unpleasant unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
