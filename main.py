601859abbaab1e92629a7e79e789211bfcc58daa
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:36:33 +0000

    round respect right before even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def count_elements(lst):
+        return len(lst)
