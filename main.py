5380c06c5bdde66ff4c1c0efcde2b766fef12f99
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:31:31 +0000

    word carry later breakfast discuss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
