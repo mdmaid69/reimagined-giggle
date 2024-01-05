3ed025edbcf11e81d74a40443d8c3488723c63e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:42 +0000

    sort finger getup everywhere eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+print(math.pi)
