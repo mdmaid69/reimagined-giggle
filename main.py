bf31480fdffd2427bbd84fc4a142a6bfbfdada22
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:46:37 +0000

    consist total scream temper bring

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
