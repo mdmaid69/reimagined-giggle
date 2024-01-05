b6bd2820582a934ced1144e0560f108a2fad3f51
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:05:08 +0000

    takeover 564encourage family stir drop

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
