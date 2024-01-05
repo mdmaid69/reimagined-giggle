ffed020cac51847732b894044978d19fd91e1810
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:15:09 +0000

    delicate personal object perform violent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
