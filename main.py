28639a7b7c8d7485dfe952bf17f1129d0542111f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:51:39 +0000

    throughout start relative battery plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import sys
+def exit_program():
+        sys.exit()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
