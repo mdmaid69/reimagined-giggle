250d32b0d13f9b1028e2bcbce804e296b9f0ac0a
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:24 +0000

    arrive blame before moment late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
