1a7dbffd4cb488bbffcfd5cd78fbfc5161741c25
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:09:58 +0000

    avoid actually remove go bunch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
