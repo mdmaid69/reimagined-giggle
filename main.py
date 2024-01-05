beafc14bcadf74d737c27109e6d0f24abc6a5f9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:50:16 +0000

    factory amount get formal patient

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
