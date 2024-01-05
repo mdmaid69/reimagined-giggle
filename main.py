22685e7846b92be0a5556718c8c681bee4a68498
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:04:46 +0000

    see distant ever breakdown drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
