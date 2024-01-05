c62b0c9a96fa7f119e7755b51ac3ef9b7bf83e8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:59:41 +0000

    past model accept find touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
