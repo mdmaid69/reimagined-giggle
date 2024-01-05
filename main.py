5e355b341906c6779d673eb3040a60f223252b52
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:33:47 +0000

    beyond affect stranger show course

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def add_numbers(x, y):
+        return x + y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
