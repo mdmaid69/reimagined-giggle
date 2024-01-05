fbc3551e56a4ae9e98ec3e04fb83591c6054e491
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:05 +0000

    wait turnaway contribute free tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def find_min(lst):
+        return min(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
