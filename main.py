640b075f2394b8b0ffbff444ce41687afc641109
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:52:57 +0000

    Nn angry way proof rub

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def cube_number(x):
-        return x**3
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
