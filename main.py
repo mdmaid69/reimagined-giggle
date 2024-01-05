d1ecadf4a92fc70e18fb80e5e06be45ac2afb3a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:45:48 +0000

    turnaway aware table passage advise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
