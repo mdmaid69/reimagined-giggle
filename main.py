d530c0a446cb6848e939fa4ef92f0e1ccef3b3f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:37:51 +0000

    meeting fill recovery hole clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_power(work, time):
+        return work / time
