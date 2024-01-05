d5a7136ea1dd31b520712852c661477cc43cc1d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:32:04 +0000

    while eye comedown pleasure unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
