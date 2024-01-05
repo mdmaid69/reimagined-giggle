386331da79d7bee6a99a6e439ddb6607430fafa0
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:24:44 +0000

    weak believe contribute 571enough play

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
