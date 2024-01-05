5f4da550e44b6d4449078c17d2cd3923f0138b97
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:49:59 +0000

    passage today moon account smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
