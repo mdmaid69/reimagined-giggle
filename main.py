661ac2f1e292ef7b8df1ca697aea77882e5341de
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:53:50 +0000

    comment squeeze relief shout switch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_set(array):
-        return set(array)
+def cube_number(x):
+        return x**3
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
