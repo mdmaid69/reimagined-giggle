c0b8131a0e84ee1bad8f618086d6349ebe800f71
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:55:37 +0000

    best tragedy switch deal impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def print_python_version():
-        return sys.version
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
