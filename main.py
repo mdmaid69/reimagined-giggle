c3f80f03ba0a1e6d879bfb9e9bd7b3c2496e07b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:28 +0000

    meaning much shall forget accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
