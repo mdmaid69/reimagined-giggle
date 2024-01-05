84ddaa9cbde5aac4e5a690c450cef6c6f0a6f945
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:29:46 +0000

    health successful related many store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
