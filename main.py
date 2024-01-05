53aa1cfb4405550111b706b7361f659bafcb9e75
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:49:31 +0000

    mouth stupid Uu circumstance luck

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
