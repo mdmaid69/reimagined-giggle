562642c2b6523c21645297dd1492281acf7bed8e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:35:54 +0000

    determined delicate day people giveout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
