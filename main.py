96d82c8c4cf2bff2a36e145e302191be5922cb0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:27:15 +0000

    repair tidy awake underneath public

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def divide_numbers(x, y):
-        return x / y
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def convert_to_binary(n):
+        return bin(n)
