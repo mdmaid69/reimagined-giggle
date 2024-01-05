d72fe8e0617e7fd1bcfb98434b115ffb37171531
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:11:00 +0000

    roll afternoon trousers apologizeapologise shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def cube_number(x):
+        return x**3
