f4557affcae6728507c981fc3b55fa33eab1d52a
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:51:11 +0000

    underneath flow turndown switch coffee

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
