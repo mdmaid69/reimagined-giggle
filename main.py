22f106833b1f5495f06a74598d758d5289ea4e05
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:12:06 +0000

    pick chin solve embarrassing birth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
