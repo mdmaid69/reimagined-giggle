38467dc8baa27659b76818e96c9e9246df1055a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:06:38 +0000

    foot long lie lock clearly

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_palindrome(s):
+        return s == s[::-1]
