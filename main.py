2a2ddec184c4f272fa10ef8cf812fa12173e4c48
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:39:07 +0000

    clean become must past near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
