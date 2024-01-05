4c6f34989fea28954e53a5cd47c5472ff40a34e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:09:21 +0000

    emphasis debt march chance getup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
