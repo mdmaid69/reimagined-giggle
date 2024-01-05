747d98ccb89a0898a1274f46486fea3e578881a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:40:26 +0000

    business they complete beautiful calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_tangent(x):
+        return math.tan(x)
