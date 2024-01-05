606d231dbc1a65ca335f0de1494a4f89e0e3cc24
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:34 +0000

    extra respect back trick description

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
