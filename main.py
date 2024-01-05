21a0648d163698c116a8f103d9355204c490aa03
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:50:33 +0000

    medium but spot gift female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
