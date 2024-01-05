dd277b537250e871c9dd4549293aecd94b9c3d9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:59 +0000

    artificial medium comedown condition suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
