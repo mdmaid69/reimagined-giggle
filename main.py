9b3e793f62bb70490487dbe16dfe8b0600267f07
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:50:07 +0000

    twist lie fasten keepfrom seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def remove_duplicates(lst):
+        return list(set(lst))
