7b62a881f119917a4a895793e12abfad0368b34b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:38:13 +0000

    because leg fence sort before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
