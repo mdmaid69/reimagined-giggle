cb27b727447414938aa9a3991991ca6b20e4d121
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:43:56 +0000

    force organize plate thick accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
