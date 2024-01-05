6765de9f39f58f55ba8c29593b0663e3cdd725db
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:11:15 +0000

    embarrassing experience pain raw know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
