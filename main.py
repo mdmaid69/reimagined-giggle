1337f327dce8a77ebc619456826856346789a5cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:28 +0000

    song thirsty schedule factory worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
