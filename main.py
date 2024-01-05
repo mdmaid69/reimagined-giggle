d83eaf054d304aa4485fd51f98024c8c969d430b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:11:37 +0000

    figure port principle transparent fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
