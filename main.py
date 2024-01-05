6e157056b9e91a2d2fb5a16e1b945e40d10e8430
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:06:41 +0000

    importance someone talk preparation dress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import sys
+print(sys.version)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
