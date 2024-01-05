fc0a3adff422a78fad1275429bab54194a3ea067
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:08:03 +0000

    every slice target early hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
