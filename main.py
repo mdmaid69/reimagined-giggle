38963a47d13850c30b967ec34ac9c938013881aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:18:44 +0000

    rush lose study wise sea

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
