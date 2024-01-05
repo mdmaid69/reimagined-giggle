b53e6ae09e0ad46a548596f83c69121675a1cd2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:49:55 +0000

    reliable soil excited in force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-for i in range(5):
-        print(i)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
