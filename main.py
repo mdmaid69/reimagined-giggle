22b8e408deaf3b9eb1f89a39122b3291c89d346a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:41:06 +0000

    hope game far first exchange

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import sys
+def exit_program():
+        sys.exit()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
