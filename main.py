d3bd33564bc1a4da93d7bec6383e6a838c60f8ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:25:45 +0000

    handle welcome wrong steady react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
