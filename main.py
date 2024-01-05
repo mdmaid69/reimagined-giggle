8cd8af9849fadb60480def4592cfd836521dab45
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:33:56 +0000

    except unlucky extraordinary lookback(on) train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
