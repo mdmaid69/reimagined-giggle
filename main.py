5029b529fb92c24cde28e4f9e92bb12a7304f5b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:27 +0000

    under tap argue survive build

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
