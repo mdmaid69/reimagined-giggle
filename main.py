54795d910a0be95fb57e484c10e29779417ad4c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:14:29 +0000

    frighten strong thing steep violent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
