d124ea19018bbfb3eb97e1a515a141da46bbedfe
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:16:53 +0000

    medium why worth authority shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_repr(array):
+        return repr(array)
