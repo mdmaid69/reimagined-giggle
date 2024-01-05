55c99befa5b871059873b445fb96e8bb94d2084e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:37:15 +0000

    promise getoff favour after survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
