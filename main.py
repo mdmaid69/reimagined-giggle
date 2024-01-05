cade616c6058c69a21062ecb0593714abaf7f599
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:05:53 +0000

    anywayanyhow combine rubbish drink apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
