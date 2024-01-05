42f419ea0b1e73977add9d964d5fbcdd6a5207ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:11:49 +0000

    juice tour visit room swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_power(work, time):
+        return work / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
