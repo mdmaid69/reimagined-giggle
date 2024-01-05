101a21df10243e3e1c39183c2a2ba5d529ca95b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:49:44 +0000

    medium fire professional keepaway play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-print(math.pi)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
