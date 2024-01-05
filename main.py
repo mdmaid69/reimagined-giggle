8c8a480f1c30ffa64671e58b37dc44062592848a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:55:10 +0000

    long autumn safety think fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
