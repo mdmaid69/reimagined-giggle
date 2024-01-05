3b2fc327f345d04ffe2cef7a7eed980bdd431730
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:04:07 +0000

    uponon available settle weather other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
