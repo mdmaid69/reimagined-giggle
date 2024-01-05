8a438c50a306d2d27a0f7ba92d363f0a76ed76fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:23 +0000

    name workupto demand fire match

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
