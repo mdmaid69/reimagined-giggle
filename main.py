a77a82e49a9213d20b9cb9287c20c6d13156cfe9
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:41:34 +0000

    this wrap dangerous lesson open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
