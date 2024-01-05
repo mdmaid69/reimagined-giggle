d4d5bbb53b797dda9db0d76d7631fe399406ca90
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:52:28 +0000

    disturbing reason side confident rule

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_sine(x):
+        return math.sin(x)
