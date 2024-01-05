244a00edd8e31921ce5053fc19f79ca1b602c146
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:06 +0000

    direct Mm maybe on fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
