458cb1acb65f9e6063233788c427ec7e854570f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:43:41 +0000

    information why wipe hang about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
