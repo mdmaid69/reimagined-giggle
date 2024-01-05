b57ee05a17f5251f6a8c09ea11cb3d700ebd7b15
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:38:39 +0000

    sweep ashamed reply 565end fast

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
