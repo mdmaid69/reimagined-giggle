9dfa09533baa95ec3770bd84fe19c2d9c0618a2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:40:02 +0000

    call detail dead style used

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
