cfba6f6bc31336c447482fd49a6b7a518da66e02
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:52:02 +0000

    cut scared aim still hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import math
+print(math.pi)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
