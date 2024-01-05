fefb502182094b45808f3c0d36a1ab60ade558a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:02:37 +0000

    poor coffee total Ll get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
