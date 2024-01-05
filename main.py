5efb86093b5e08fe31c8a49544fb4b94d5c5e615
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:18:44 +0000

    improve already like surface high

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
