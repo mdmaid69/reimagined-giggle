38c09b9bfb42d38381c95beb8e12a02356fd7934
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:48:14 +0000

    amount bother various shoulder secret

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
