fa524d2d5b5324f6bdbbb2fe6c0aadc531cc6b36
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:24:18 +0000

    Ss tired collect deaf right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-name = "Python"
-print("Hello,", name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
