270fde0a276d674a241c1b11f3920adae027f3c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:49:12 +0000

    university shine concerning clean boil

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def subtract_numbers(x, y):
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def subtract_numbers(x, y):
         return x - y
