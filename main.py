d58962d83d867b421da52d4d4a1556303d26e651
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:37:53 +0000

    chase join fall false genuine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_repr(array):
+        return repr(array)
