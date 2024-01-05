41fe93571c17010a1d181b4360d742cc3c59d119
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:34:41 +0000

    gradually train traffic shelter confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def count_words(sentence):
+        return len(sentence.split())
