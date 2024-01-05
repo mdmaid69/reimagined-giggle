71cd63a7ef70c50933c8564215c38d3d01b8f4ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:47:41 +0000

    whatever hurt mad this public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_sign(x):
+        return math.copysign(1, x)
