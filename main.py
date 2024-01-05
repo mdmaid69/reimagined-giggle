f09e2ea7c01a462670fbea0909a70577d09f2222
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:14:53 +0000

    long free lesson different smooth

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
