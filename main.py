8e8859cdf647ff6a393d243f6d36a3d558825000
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:39:11 +0000

    man closely fortunately address year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
