25eac45109a4e0c448572cbefd431996ac5fd7ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:16 +0000

    flow stupid transport wish unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
