d4ee8aecffcf1f736cf99305de6a7d0052785d99
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:19:41 +0000

    drink mad company ticket I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
