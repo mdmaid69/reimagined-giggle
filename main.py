18ea8ba707e6944c5c0e4e821e30d1c3c0fd0548
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:05:34 +0000

    smoke effort suckme shame Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
