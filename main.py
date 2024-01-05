f7efe02bf05941b8f70cbf33906fa7d82079f35d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:10:25 +0000

    show fine accept stretch open

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
