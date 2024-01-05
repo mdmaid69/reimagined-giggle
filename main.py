a0a5c9a35805ccd3517c12aacf80cd47a7388952
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:49:36 +0000

    unable lookback(on) report consult example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
