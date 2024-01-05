6169bb33fa126f7d93974fec5eae63bf9a7f8e9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:24:12 +0000

    dig practical difference fetch issue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
