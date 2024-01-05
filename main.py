4d57c1d609439ed6b854d537c0bc8cfbde404048
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:11:25 +0000

    indeed equipment turn react convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
