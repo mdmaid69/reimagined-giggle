adaa202c100d2aba97c1eab6e99416ae7ab45d47
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:19:08 +0000

    fence dig fold no passenger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
