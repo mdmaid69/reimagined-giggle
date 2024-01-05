43ab7de63a465e27c7a8a9328d1bbd1db4606f0d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:06:20 +0000

    know second low convince 572enter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import collections
+def create_user_dict():
+        return collections.UserDict()
