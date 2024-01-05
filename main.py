2e453f8ca4d55436b3ef71806d6671d0de757b5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:53:02 +0000

    recommend proof public boring than

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
