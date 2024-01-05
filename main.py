af2a8b1f6b2cc78e6a5164f5a6e7466c8f42c5fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:06:12 +0000

    sea into right pass forgive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_density(mass, volume):
+        return mass / volume
