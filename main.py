2368faf09bc1312cfb8be682f90738cf4a3f7726
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:49:28 +0000

    increase chin turnout vanish brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
