34590452064c37215f619d1d417d01715e871c34
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:33:19 +0000

    noisy find danger earn disagree

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def clear_array(array):
+        array *= 0
