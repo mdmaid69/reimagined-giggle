8904fea55829688ea860800826f8a61d26ff294a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:08:32 +0000

    error sick lay practice giveback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Characters:", len(text))
+def find_max(lst):
+        return max(lst)
+import re
+print(re.match("h.*o", "hello world"))
