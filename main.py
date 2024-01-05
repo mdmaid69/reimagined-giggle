1ea33a38d4dd4bf715b5ea22410999946ee3a791
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:28:27 +0000

    street likely break letter confident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def reverse_array(array):
-        array.reverse()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
