2779ddc0aa78e132b3779623db2771dee71e1ec8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:47:17 +0000

    anyone Gg cum instruction noone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_max(numbers):
+        return max(numbers)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
