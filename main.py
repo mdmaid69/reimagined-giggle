fadc44b5ad1822889015c17d3ec93dd6dedf572e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:14:57 +0000

    Gg leave stranger else service

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def calculate_area_rectangle(l, w):
+        return l * w
