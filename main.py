c9e6ab590e028d99346e005e2ef5d3e43d122302
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:39:31 +0000

    though order write example motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
