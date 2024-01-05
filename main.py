e83896b3a85f978ba8a5cf8437ea35fb2f0adf1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:57:25 +0000

    bend see closely nothing force

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
