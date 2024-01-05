dfba384fa1cc069e12a7a5088dcee678ecd37ee5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:00:26 +0000

    rule callat reason division slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
