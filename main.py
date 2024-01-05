adfa53b7ff22f3e95379f3dce6fd7289850f184d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:55:39 +0000

    unhappy cut although cover tape

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
