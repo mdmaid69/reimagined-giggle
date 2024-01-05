bfe18a14bd532420c4884a684f624bcbef18b1a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:02:18 +0000

    repeat left instead but kill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+name = "Python"
+print("Hello,", name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
