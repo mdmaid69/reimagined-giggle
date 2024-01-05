f34f30cd393501a046f00f646fae7bab2b21c98d
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:25:23 +0000

    drink event long any rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
