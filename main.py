5fcf3ca957e2dbc296a5569b4591ab7c83e32c99
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:29:23 +0000

    score pleased open complete accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def square_number(x):
+        return x**2
