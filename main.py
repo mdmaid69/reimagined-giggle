afb8a56f4aa9279820311e47925c7579f42b1ddf
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:58:44 +0000

    effort neither speak smile keep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
