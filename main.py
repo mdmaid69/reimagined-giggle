89aea2c5ffb546b5ea53a77c7a314de896e071f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:46:47 +0000

    plate deserve match fetch holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
