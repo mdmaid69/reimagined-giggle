1a8d1e79843eeb7a5df04012421b513eed1c0769
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:57:11 +0000

    sound load base mess choice

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
