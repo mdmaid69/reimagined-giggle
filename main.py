e7051c8c23e2c08910ec965de6a1c40acb53a070
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:18:07 +0000

    light perhaps uncertain convincing support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
