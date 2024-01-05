f7bc0bd387748f19095d43a62800fc94349cd235
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:26:35 +0000

    band squash bear water wide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import datetime
+print(datetime.datetime.now())
