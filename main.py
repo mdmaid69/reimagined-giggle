95ce24682cadb1c55559b0f1878f42d9dbfd45d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:52:11 +0000

    fairly challenge medium mysterious common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_acceleration(speed, time):
+        return speed / time
