b31fe85e289f0376dd29bc6d544da6ca8b935f57
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:21:34 +0000

    wire practise doubt body formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
