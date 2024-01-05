b9f7f3d140f81d3c4ebd4d595c89c4fa4d467f8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:40:59 +0000

    disappear future achievement workout total

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
