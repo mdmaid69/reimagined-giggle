8d17ed8b0c11ce3886ba073d9c186937c87f8698
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:36:41 +0000

    brief wing stone hot song

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def square_number(x):
+        return x**2
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
