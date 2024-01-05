2473aa6325831157aa035cf21c10310ecb75a92b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:48:06 +0000

    cry keepfrom muscle decide air

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
