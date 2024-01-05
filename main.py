5eb653ee824936df32addae3d6f9ebc38122d02c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:44:56 +0000

    rule mess still completely under

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
