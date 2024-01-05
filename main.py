14226056b8d19def144af5ab6417c9ed301e66a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:27:53 +0000

    achievement another possible false fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_list(array):
+        return list(array)
