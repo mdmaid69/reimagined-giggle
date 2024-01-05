2480007f70f596b8cbc997a457f6900d0e4c1a1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:01:56 +0000

    sticky occur everywhere see human

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def cube_number(x):
+        return x**3
