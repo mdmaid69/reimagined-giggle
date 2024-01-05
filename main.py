17312f8b6549acfed5a6d2c39b6427365d73aea3
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:53:11 +0000

    spare pronunciation declare rest image

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
