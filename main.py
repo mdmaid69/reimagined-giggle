797801151a1927a3e6501cf32d170ad7fc0e1b25
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:57:07 +0000

    autumn method truth lie cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
