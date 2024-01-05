3280b17414c46272bec0c3ae37fe2a6fa8220fe1
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:35:19 +0000

    throw divide although tap upset

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
