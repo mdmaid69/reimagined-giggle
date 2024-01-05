1869c638dad9718e724e0c0a1c36306ca8218dbb
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:12:28 +0000

    whether upset use Dd children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
