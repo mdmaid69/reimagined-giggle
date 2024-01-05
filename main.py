25743ebadaf8bac1bba637ac79d39e9f9fcb2dbd
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:58:00 +0000

    vision later majority of respect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
