fdbba1711df37281a9cf74f8993e0c9f5d2aa917
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:24:10 +0000

    arrive age stretch beyond keepaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import time
+print(time.time())
