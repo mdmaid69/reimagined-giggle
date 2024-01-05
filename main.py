4b0e706564c782c3a6bd8a1c4fd932b7a1a33291
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:32:31 +0000

    vision quantity politics plan matter

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
