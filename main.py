c2d952b838cc16c70aaf8492126254103c9d841a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:28:06 +0000

    float so chin usual runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_repr(array):
+        return repr(array)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
