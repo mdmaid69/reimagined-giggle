2868c88849cd8e9b0078e6856d6ffb2c45f37f68
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:58:04 +0000

    fortunately transport sting temperature call

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
