4f85b9a26486bdcb644340c1f8b45b0f29ece20f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:24:12 +0000

    load rubbish contact substance recent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
