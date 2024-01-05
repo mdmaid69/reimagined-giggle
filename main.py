84f71517ba213bd0ca667521225f5621b51f225d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:49:28 +0000

    comeround food squeeze at centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
