e07f9a55b0b61709fe2fec41a0b1eea7d0dfdeaf
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:43:04 +0000

    future test bring condition event

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
