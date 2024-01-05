56bfabd3267bf9e771408dcf8b23ce7e20739abd
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:19:37 +0000

    study room day rare welcome

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import math
-print(math.pi)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
