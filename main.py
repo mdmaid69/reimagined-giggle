ff64723e1a7d3842d78374975228dae131ab2d11
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:26:30 +0000

    shut fun lead deaf objection

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
