661a13398e8c9927cdd0be47b37e86664968b8cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:27 +0000

    talk dream too way little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
