b89cb93bf08d9c7867b9df2804819401e19088cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:17:49 +0000

    holdout adjective joke explain ear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
