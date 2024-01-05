a7abee3cd52c48d34d792bccf30710ca32f6f238
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:30:58 +0000

    perhaps instead Hh hand owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
