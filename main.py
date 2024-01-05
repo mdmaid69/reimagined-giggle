b95c760827bc4e3c852bf0c85772b22b32034026
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:41:37 +0000

    yours keepout software news poison

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
