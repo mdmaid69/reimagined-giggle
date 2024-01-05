a2de87f320b050651d45f3faa9c196193ede0c89
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:18:32 +0000

    reasonable invitation tidy suckme no

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
