b5ade86cd784fb3b3e13661b0ac4429135923ce8
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:22:53 +0000

    scrape bunch repeat genuine different

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+print("Hello, world!")
