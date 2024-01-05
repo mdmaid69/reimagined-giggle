4013e86936183315fe385b2a5ffff8c98f9d8c46
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:42:12 +0000

    flood thinkthrough manner react maybe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import random
-print(random.randint(0, 100))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
