4ddaaccf4ab68548713fe9368d4f822277f5373f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:51:04 +0000

    drive success come would sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
