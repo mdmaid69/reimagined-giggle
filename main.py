e852b68c94729c49b9e2adef66cdf5be4329b08a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:16:40 +0000

    slightly worth near somebody passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
