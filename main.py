6e7504856492b3e414e876ffbc0ee6598070057b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:12:58 +0000

    conversation Nn light process cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
