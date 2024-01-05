8c1b7a374dfcb2b09313baebf1dcd8c3fad22352
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:19:11 +0000

    back welcome either tone reach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
