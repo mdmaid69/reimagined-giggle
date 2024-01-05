900822e7a2081b45bedd121c47921878f5c50efb
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:38:21 +0000

    rarely large pay arm flow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
+  def square_number(x):
+        return x**2
