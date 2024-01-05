9b570712f7b47162d632d01283d300aa9449e5e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:07:21 +0000

    change act speech gate fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
