3257d96641b36969599240de9c3a46c59c0e3398
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:07:23 +0000

    herself big write basically split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
