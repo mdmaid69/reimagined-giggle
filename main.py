fcce2f4d52a81badf30888fbf7bec04d5700345b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:55 +0000

    side rate before you content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+print(math.pi)
+import collections
+def create_user_list():
+        return collections.UserList()
