cdad516d5bfed4aac2547138ec1a1810a5eba209
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:37:42 +0000

    contents exercise very earn Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_directory():
+        return os.getcwd()
