f1ae80f6af42e3b4825bd38ef84f5f0cdea3712c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:30:25 +0000

    march invitation acceptable kill sea

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
