74e3cf0060fdceb9872cbad1dfae821cb967acd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:58:45 +0000

    runover common skirt equal search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+print(time.time())
