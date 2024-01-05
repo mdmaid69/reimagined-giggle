8347e5eabfc244b50218db63e93e601cba7d0359
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:36:39 +0000

    keepaway load store uponon moral

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(5):
-        print(i)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
