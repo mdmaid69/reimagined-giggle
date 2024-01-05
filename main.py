bb77af09105dafd9d1f266d8d966a7ec740ac8bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:59:56 +0000

    collect steep Kk bang remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
