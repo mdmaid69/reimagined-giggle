cb0d151048c71929350f6aa13f4e54a47e3fc9c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:19:33 +0000

    fear claim traffic force forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
