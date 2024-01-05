2c228bb670028446e8f3c17b224eb4e830d1dca1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:53:10 +0000

    bag building address list touch

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
