687a49cc4fcb76e6514ddbeaf636b31bfeb8148c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:07:41 +0000

    will cross collect pour pronounce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+for i in range(10): print(i)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
