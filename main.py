391455e22a91dd7c40ce518309a0acf8fbe3b607
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:07 +0000

    fill stare hear move future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
