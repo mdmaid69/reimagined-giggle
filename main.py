c8adb0c4fdf3d79bc674934ca8ea3b749de60548
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:32:26 +0000

    better size uncertain smoke forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
