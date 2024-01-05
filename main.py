c110125694a6f1050fd7136ae31434002d32a4e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:49:55 +0000

    history impatient rich discussion teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
