6eb2886fc1ac31b9caf39e1d3a480640cd47b863
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:40:22 +0000

    certain proud recognize possibly perfect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
