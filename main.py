523df0e5f6e475a0cb9866da74bdeca034055613
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:40:07 +0000

    let kill light risk path

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
