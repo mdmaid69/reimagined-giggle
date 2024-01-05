4a9da7e4ffe67c189238f9505ac2c92c7cbeb05d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:33:34 +0000

    exhibition visit environment straight congratulations

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
