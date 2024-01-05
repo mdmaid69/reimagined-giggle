6efed3fd2ec09ab372a88292cc5a8f15d7094e83
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:07:05 +0000

    promise group slice more scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
