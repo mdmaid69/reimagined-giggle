8cef5b29a3a8f10812ce0db77d511b7b780a38b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:53:48 +0000

    report manage much he dream

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import sys
-def print_python_version():
-        return sys.version
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
