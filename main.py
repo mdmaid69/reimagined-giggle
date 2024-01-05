d06ac95ac7dad5af67fe42210d777c96a356389f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:00:39 +0000

    difficult poison claim relief such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
