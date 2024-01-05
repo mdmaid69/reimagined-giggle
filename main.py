bf6f644ad4a51240cc4c10a1967a04de6bbd98ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:21:27 +0000

    region bit classroom uniform cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
