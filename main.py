265730faab6a3d7a2ad98a536885706ef439dba5
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:17:41 +0000

    sleep lost finger too true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def cube_number(x):
-        return x**3
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
