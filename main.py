3cea7891819041d97d3ec7569ce9fd55b635250b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:03:33 +0000

    government tend breakfast commit often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
