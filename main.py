cc9831472b8e0f170645c934ac509d9403dcca75
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:57:20 +0000

    tread scale lose bang party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_odd(n):
+        return n % 2 != 0
