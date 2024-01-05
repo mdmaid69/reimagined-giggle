11ecb6659daf1a27981fe85f183f5afc0f7d07da
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:29:23 +0000

    plan alone fond whisper land

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
