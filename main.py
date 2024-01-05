687195facfafee53a82c7221ff4c189dfacc8ea2
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:27:13 +0000

    bus shape breakinto match good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
