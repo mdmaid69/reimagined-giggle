24d89754e825e5d2dd1f347f4cc30ee28e9be884
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:59 +0000

    size makeup organization direction oil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
