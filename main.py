41fa8b2344971589240b215e8fca0b426fbccd95
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:02:05 +0000

    breath war complete connect skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_length(array):
+        return len(array)
