d7a66b8ff2ae0f9ad8aef2a3f5474b90a2f4bf51
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:38 +0000

    willing turnover involve born wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def is_even(n):
-        return n % 2 == 0
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
