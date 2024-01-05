9d15c36d0ccba9cff9bd8d65062b08871f00d58d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:48:45 +0000

    maximum demonstrate yesterday time wealth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
