88069f77913db862cf225230cc6a6b8cf24de8cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:13:33 +0000

    entrance please sound period what

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
