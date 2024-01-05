079dbb58eefbc6043fe6023a38bba6ac5985c4df
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:04:22 +0000

    mass mind couple priority hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
