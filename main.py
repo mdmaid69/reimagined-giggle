4a37fcb17897fabd437c1d15be5fdca758438912
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:03:11 +0000

    punish plan test that rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
