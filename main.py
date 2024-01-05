4475e1bf3fb4b567f277ad569dae6c75c61761f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:03:36 +0000

    dead say and threaten since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
