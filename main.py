78aa4d18e26a8086b6e26ef34c452879f1062aef
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:47:45 +0000

    regular Ff rain how strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
