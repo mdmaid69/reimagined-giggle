e6b04175a082a580e4526e0680db0166423076e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:28:17 +0000

    error quiet advise protect crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
