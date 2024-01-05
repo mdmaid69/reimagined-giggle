df21cae44779152e3a6ed912316b8bc580deed3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:40:27 +0000

    we sit back rub warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
