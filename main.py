077fd7348a216835e231e31888e14cf042019207
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:14:30 +0000

    must lean news public plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
