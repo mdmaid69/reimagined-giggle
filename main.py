f2d02a2c98937323ebb0b44eb3df230f331dea85
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:46:09 +0000

    future drink change position pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
