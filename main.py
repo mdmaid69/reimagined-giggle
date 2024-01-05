6df2b9e701c4e8fd49b4efa82b7a60efabd2c617
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:03:28 +0000

    nice disapprove married runout deal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
