28bdc05be534b9f69fa724e6f4d7b305fca01bb6
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:42:09 +0000

    university friend tall collection successful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_power(work, time):
+        return work / time
