1bf6c61c7b2732da46b53edc11eea5d33279135c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:25:45 +0000

    talk name obtain bank sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
