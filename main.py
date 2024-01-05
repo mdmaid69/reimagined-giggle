914a84de4255b875f908ee409056c50570941fb2
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:58:09 +0000

    mention goout race on young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+print(math.pi)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
