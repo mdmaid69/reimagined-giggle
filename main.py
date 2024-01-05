9f6ad2fb32b878dec6b697476e40005df8df9099
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:14:35 +0000

    ask shallow complete sale set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
