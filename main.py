f65b9ea11b29f609892125ac683cb098e03199a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:46:36 +0000

    can workout skirt violent expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
