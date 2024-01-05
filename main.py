17e413e68713c05a7007e4a9faf94b980ac3948a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:50:46 +0000

    yesterday gate nature have tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
