270912aff94edab01fa26d7d8cb22b433dac7082
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:13:38 +0000

    repeat miss everything concerned number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
