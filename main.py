296c40ae61cfe372fd421a827e58c36d3b35875c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:53:24 +0000

    event second behave stay head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def add_numbers(a, b):
-        return a + b
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
