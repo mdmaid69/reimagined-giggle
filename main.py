9bb478e54a9952c954e54ab5913b34b91164d78a
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:28:15 +0000

    sleep steam attitude recently pain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
