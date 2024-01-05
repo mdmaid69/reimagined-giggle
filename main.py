a7b92ca17eeba97240b8b7afd9431142f6b67fbe
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:10:58 +0000

    home bang advanced young task

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+  def add_numbers(x, y):
+        return x + y
+def divide_numbers(x, y):
+        return x / y
