452686bfcfd03d72a65ba57787c6b18efa16ef63
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:48:17 +0000

    choice ask suffer sink which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
