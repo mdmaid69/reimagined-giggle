61647eac51810ce0e47f0f4b463fbdbf0d34409b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:32:47 +0000

    dinner draw skirt opposite Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
