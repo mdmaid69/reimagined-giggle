57e100fbfe2ae64df7f4759304c27561185f2969
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:02:48 +0000

    memory turnup education chief you

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
