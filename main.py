67f4ab60f907aa633485038b370d71b43e540d99
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:46:35 +0000

    this spoil time arrange human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
