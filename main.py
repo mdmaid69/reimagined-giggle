b6778a8a6512beaf6cac9249d3b79ffbd04c729f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:40:33 +0000

    before trousers fortunately punishment because

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
