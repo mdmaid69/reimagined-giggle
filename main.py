de4fe974585e113635a5c4cd1fdb885bfe7c9152
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:33:29 +0000

    turn neck tax cry trip

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
