202e374210d97c06ee0d45313ee841ca70f29ef2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:58:33 +0000

    dick right task experience hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
