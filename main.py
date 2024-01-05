16b90a65e2e31d3a00e54db7239f169fef252efa
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:55:53 +0000

    outside getup bank best face

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def remove_duplicates(lst):
+        return list(set(lst))
