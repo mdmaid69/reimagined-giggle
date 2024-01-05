4eccce32ed5ba10406ee8ebfd3d7c2e02dd8aa42
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:51:41 +0000

    whether safe away instrument Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
