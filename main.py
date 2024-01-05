67975086ff949344283b76f6ae2f7cef809cdc8e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:29:12 +0000

    mention especially refuse another next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
