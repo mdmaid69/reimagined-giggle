4558e1029d79ac7261cf4a8f04a16446c5461719
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:14:18 +0000

    bend touch adjective comfort real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
