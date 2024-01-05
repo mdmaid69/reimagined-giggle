a46133e0ac5e6557435eb56132ce99449fa633df
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:08:33 +0000

    narrow circumstance flat himself main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import numpy as np
+print(np.array([1, 2, 3]))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
