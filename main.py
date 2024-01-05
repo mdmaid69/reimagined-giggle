ec34fa539f291044131439be76fbe541c4134c97
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:28:26 +0000

    blame guess shame various variety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
