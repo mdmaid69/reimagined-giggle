9a26ea80b437a0df1fa6c492f55ef9b961ced7e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:29:17 +0000

    birthday reach Hh organization opinion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
