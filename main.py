1f7564c1f81b4bb890aa8ca82c1ebf6e171e46bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:04:47 +0000

    wake wake style recent immediately

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def remove_duplicates(lst):
+        return list(set(lst))
