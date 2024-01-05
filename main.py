42a65415f4b5ddf5fbf9d8481ba73138fb70a708
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:34:09 +0000

    opinion at pure exciting path

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_stack():
-        return collections.deque()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
