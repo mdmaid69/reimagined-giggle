682b0abf6131206073ec8b105be7dd8e9ea9adcb
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:45:50 +0000

    depend speak outside tall hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def find_min(lst):
+        return min(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
