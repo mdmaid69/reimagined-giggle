45a57ae89dd0a0728ea0916e66f0f9d1d9246e05
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:09:29 +0000

    takeout continue form false familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
