83c6cdcf4a386ca171050afc09537f2d3fe73fdb
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:54:19 +0000

    effect holiday face roll repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
