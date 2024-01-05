a059494939c9769f6eedf026be384d6a7875a617
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:47:29 +0000

    believe air shut early bag

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
