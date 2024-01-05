e1277b05e1340ce98c44dbf747b4d0b4ce4c2a69
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:22:58 +0000

    pause because copy attach dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
