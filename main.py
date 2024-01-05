17ca5f33044fc147e5b51ad9cd382bb3b04dbb1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:57:18 +0000

    rush lesson ticket join once

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
