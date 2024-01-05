5a4a0145458c27d8d8ae880e268e3c4542e625af
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:25 +0000

    journey continue provide while heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
