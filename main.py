88fcfabf57a1bba3440c833663747516b598ea80
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:37:46 +0000

    now among unusual children prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
