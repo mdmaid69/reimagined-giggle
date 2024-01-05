f435d05c3e3436fb151dcfe5fdd941d3962b7ec6
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:27:34 +0000

    often Ii not nervous type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def is_odd(n):
+        return n % 2 != 0
