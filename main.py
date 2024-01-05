b4ffed0056dda3536e84742593929c84f3632f3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:51:37 +0000

    pleasure worst middle niggaacademy contrast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
