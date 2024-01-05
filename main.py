0ccceed10c083545e6612ff7e4d98f0f50e5e4e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:00:18 +0000

    Nn makeinto on behaviour breakinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   def find_min(lst):
         return min(lst) if len(lst) != 0 else "List is empty"
