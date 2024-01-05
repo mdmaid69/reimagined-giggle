e73b6d7237f63ccb83b62670d4fb59a649542a94
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:40:37 +0000

    go field lot thorough prove

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+print(math.pi)
