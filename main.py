2ac23afdaf3b60e375acd0bd31ef0f21febd158c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:00 +0000

    wrap regret foreign board Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def convert_to_binary(n):
+        return bin(n)
