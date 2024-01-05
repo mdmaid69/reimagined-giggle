a266c74684c50a8c6b7b40268fe479b497892365
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:52:01 +0000

    empty practice fail whisper although

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import time
+print(time.time())
