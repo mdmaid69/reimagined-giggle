a3d8a7d0efb96af496c58dc4c3408fa086713ec9
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:41:21 +0000

    keepfrom respect equal wander hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-print(os.getcwd())
+import array
+def get_array_typecode(array):
+        return array.typecode
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
