e73a6a3f786b9bd7dce6b4e14dd06236a33f88e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:49:00 +0000

    curious check serious similar bet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_pressure(force, area):
+        return force / area
