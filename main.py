1ee09efeb5fa232bbb3f52b77cacece3e31b56fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:36:06 +0000

    remain healthy nor kind around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def square_number(x):
-        return x**2
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
