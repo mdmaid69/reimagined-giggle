09cfa57d2d8b806f0ac86b1af67cabcb17404744
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:59:56 +0000

    give recent awake violent abroad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
