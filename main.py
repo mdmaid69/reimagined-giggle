7458160defc544330dfbe8ee275b3be410db27f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:43:00 +0000

    importance match flat public guide

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
