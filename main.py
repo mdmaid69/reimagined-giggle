24f4d833ef2ebf8cc6b8f55f0c6a868bff143681
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:50:26 +0000

    neat hurry always clear case

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
