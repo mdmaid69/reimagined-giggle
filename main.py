4a6543511ec7166eacc9664b9d9d52553a7ec636
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:00:56 +0000

    statement closed their contrast sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
