4e41783d2d5cfbb16e917ad8ba3d03838e65db38
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:09:39 +0000

    equal than nervous earth to

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
