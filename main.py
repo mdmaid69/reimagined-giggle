fc2921edae334fe5d891dc79117aff32c58e5408
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:29:26 +0000

    board use succeed prepare tour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
