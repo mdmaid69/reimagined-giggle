966e7df8e86992e127a32a9342fbd4ee40e30f16
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:20:02 +0000

    much sick broad thought trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
