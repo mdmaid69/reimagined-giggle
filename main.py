82b2e6b22e4f84ae21815837290791414a95da72
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:44:53 +0000

    past through cruel air card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
