28ecfaadc37538920a68c24c45c69e8ff5df6111
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:32 +0000

    point street belief road plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
