67bb9d693d0f6c37a170f79200a987bc3b4c4c61
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:24:27 +0000

    vote accidentally curious brush talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
