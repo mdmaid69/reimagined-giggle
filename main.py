cc735fea0be6771406c51e13d091442f9adb2678
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:27:47 +0000

    cause check action go free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
