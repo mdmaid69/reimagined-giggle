0624ea393f217e53b47d655e76ad22c2f35aaf50
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:36:05 +0000

    spin down unless prefer scale

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
