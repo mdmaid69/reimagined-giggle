9f8cc967198e0ac369232afe6065ee2c5a52574c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:21:35 +0000

    anybody tidy as winter slow

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
