380f85aed9db06e7c80834e1970b3e56f2286fb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:22:43 +0000

    study flow properly answer balance

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def count_characters(sentence):
+        return len(sentence)
+def find_unique_words(sentence):
+        return set(sentence.split())
