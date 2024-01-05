f7e005c18c721b69f7024587183e05fb8d0f0a93
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:35:02 +0000

    divide catch by confident range

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
