4fb9b05097ae7209003b8f5e13fa5b764d4fdb4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:05:59 +0000

    violence trousers stage everything laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
