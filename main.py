d5bf2307522b98757a112a1232604bf0ad3973a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:38:46 +0000

    respect unless little season indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_string_from_array(array):
+        return array.tobytes()
