30e861a63b14f1af756f1503185da5ef3c8b6219
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:07:51 +0000

    block silent expect well cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
