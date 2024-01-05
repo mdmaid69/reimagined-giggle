0fc86b193c0c4d8409dfae7688ea800e3e82ccfb
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:10:09 +0000

    settle music weigh available downstairs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
