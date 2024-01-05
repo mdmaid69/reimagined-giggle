91ce016f37470b4b527830e4fdda1c8f2ec92b84
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:49:35 +0000

    this give fence sad firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
