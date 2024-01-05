5111c7a6aff2fe1f03770107addbdea8df0525bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:55:58 +0000

    behaviour clean achieve turnon similar

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
