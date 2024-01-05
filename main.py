4b916010fd47d70e86035e67dbb9bc0888bfca11
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:05:30 +0000

    fierce by for steam must

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
