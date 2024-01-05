8099fc0cc23535ed4cd4b0b94ccceca8a2702add
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:45:32 +0000

    future hard weak question pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_octal(n):
+        return oct(n)
