8a25389743dde1f504e01413201fca7010c68f23
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:42 +0000

    mine walk normal wrap problem

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import sys
-def exit_program():
-        sys.exit()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
