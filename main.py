e76b59327b12cacfe37827ee6f0d9d0e7bb51960
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:58:59 +0000

    live Oo strike please when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
