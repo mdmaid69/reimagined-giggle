bb20692bbf571c528840f82d6e6595ba721aac10
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:13:30 +0000

    brave I whether independent lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
