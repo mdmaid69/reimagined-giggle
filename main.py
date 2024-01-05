4ad70a94b8742089ed1c1b9f6ab2d2a5b578dd4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:07:26 +0000

    image temperature please classroom escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
