042bfb5f3d65c4c5bf10aadb196c3101bb398f1a
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:07:20 +0000

    strict listenfor better cause up

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
