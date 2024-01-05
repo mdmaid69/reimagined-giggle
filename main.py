024215dc7d2a295869a9849eef9e2a4489e41836
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:04:24 +0000

    pleasant persuade concern workon your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
