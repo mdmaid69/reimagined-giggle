ac861797d29d45c4cf78b2da390a625ac711e849
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:50:04 +0000

    family sweep tradition shape strong

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
