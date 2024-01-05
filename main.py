e0c9272303fa52697143b167bd27f76e79cc0ad9
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:30:59 +0000

    issue bright rent report discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_palindrome(s):
+        return s == s[::-1]
