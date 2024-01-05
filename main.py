61db2ca6924b6892ee773f3aecc7da93aade3220
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:37:08 +0000

    human call each breakdown bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
