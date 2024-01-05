3ffaf4f5aaeeac1b99a1b70599537314cc4e2d10
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:10:00 +0000

    waste murder brain urgent person

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def convert_to_binary(n):
+        return bin(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
