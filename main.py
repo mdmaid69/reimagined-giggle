1b0f732dd112434c37a4359c98d891e87e37c98e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:06:35 +0000

    comment just foot block gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
