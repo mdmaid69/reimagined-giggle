6196da3d30123b565d4ae90bae90316ecb5b9964
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:14:23 +0000

    fond smell bath takeover human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
