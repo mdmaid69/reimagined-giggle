8369966306745dae72d9d2701cd3eeb960340414
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:10:58 +0000

    loose Dd getup they threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
