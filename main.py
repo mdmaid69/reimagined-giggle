6774e3d01a458beba32a1b022658c83a588c5f72
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:33:50 +0000

    hand upwards cross might convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def convert_to_octal(n):
+        return oct(n)
