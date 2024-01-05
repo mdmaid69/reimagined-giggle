f65e6331cdbb64d1fb82a062f4b23bf4cc4c352e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:32:46 +0000

    shut explain style power cross

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_speed(distance, time):
-        return distance / time
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
