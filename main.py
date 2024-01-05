e07282f16d9a311666df850f56ecec65442f22df
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:09:02 +0000

    lookafter illness kind breakout frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_area_rectangle(l, w):
+        return l * w
