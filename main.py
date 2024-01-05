da9ed5accf4a16a1830ff5d71600222a1e7b0371
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:12:09 +0000

    refuse command tool use rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import platform
+def get_os_info():
+        return platform.uname()
