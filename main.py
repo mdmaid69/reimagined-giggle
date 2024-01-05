c26bef329e8244d312ca78ebf820339f78a63179
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:46:03 +0000

    wash region control thing correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_directory():
+        return os.getcwd()
+  def square_number(x):
+        return x**2
