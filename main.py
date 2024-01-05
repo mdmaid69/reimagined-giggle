32ad3de27cf9c65649a8dcc8e62d9ecdd8f760b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:31:30 +0000

    finish forever front shout union

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-text = "Hello, world!"
-print("Characters:", len(text))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
