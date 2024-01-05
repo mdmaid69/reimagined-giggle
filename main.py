fbf1e6dd9cf4b420417d754229646038bdb37cf6
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:29 +0000

    hardly short swing guard miss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def find_union(list1, list2):
+        return set(list1) | set(list2)
