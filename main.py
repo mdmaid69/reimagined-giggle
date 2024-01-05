bc39e570c3569ebfa7be26d5e7b6340be95cb752
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:30 +0000

    offend anything towards runout sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
