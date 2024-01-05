52823fc35431c7bd5059d0fb795ca18e6b12b740
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:47 +0000

    keepoff goon callback habit book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_max(lst):
+        return max(lst)
