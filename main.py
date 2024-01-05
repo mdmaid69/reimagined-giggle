9b137a99641759cf68d4c0b23f92dc204310d55a
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:24 +0000

    pity Mm sure round share

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
