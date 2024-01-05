abb05d3ab52ef60f972ef52e30dcf8fa99c7c947
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:09:02 +0000

    do whenever interview boil say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
