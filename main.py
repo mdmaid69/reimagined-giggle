55fc3c26bf70dd3846afeb28f59ca239186873ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:54:46 +0000

    down rid lift suck disapprove

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
