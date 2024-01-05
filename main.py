82efe4c5a11c2c4a764e32844d79a5d8c857b24e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:17:29 +0000

    work cost nearly hide straight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def convert_to_hex(n):
-        return hex(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
