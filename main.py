eaca5c9ed0810ef711d18cb0ef0c538a10d876c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:52:30 +0000

    ourselves small direction opponent equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
