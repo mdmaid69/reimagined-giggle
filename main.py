a247c5e7cc664fe32e69ea7421e1d3b158dc5e63
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:25:21 +0000

    human damage weigh check divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
