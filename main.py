0c3874b2977a42e4020cf2a186518df9938e6bd5
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:25:23 +0000

    special dull transport worse worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
