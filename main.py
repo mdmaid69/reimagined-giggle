ebae1814b47f6160be875aec4691daf129a48367
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:17:50 +0000

    any mouth movement spread notice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
