71e5fe9d43e22957a24ff38eceab94f4a32663c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:23:17 +0000

    thank practical term coat experiment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
