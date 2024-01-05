68400741bb07390e5dbfb1e4898167f9af6b1318
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:24:26 +0000

    lot recognize bringup welcome breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
