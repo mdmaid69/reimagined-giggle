8e08c9fa744e41993d66607b2e705e86dd16a3a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:32:30 +0000

    quick treatment satisfied any sky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
