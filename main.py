a0a75a0df04d299c801d47ae844bd474d9219950
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:35:27 +0000

    organization behaviour fairly heart thorough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
