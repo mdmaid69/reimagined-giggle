684af5c872ac4b90e455ddeafbd13d0ec161bd7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:30 +0000

    cause hit warn land decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
