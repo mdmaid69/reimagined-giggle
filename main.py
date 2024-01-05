a81bbf474c5f780f5e7fc9c079aad9d7da923ef4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:19 +0000

    usually fail appearance love grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_list(array):
+        return list(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
