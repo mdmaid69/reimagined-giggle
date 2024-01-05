cb5e583bd35a7e79b501a6f6d26e3940a1cb4083
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:53 +0000

    lock absolutely left that bye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
