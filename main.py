ff2639bca4bf28beebd843e47f8b4ce69ecf7694
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:33 +0000

    shut only step mess mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
