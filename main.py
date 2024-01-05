4e2aec9b852a273630dde948e723eb63308aa89c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:55:41 +0000

    argue artificial bit sensitive respect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_cosine(x):
+        return math.cos(x)
