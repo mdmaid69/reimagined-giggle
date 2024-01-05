38a0ddd4637fc976a9d65f7d840a8c6a0dc54984
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:17:41 +0000

    gooff also mine ill build

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Powers of 2:", [2**x for x in range(n)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
