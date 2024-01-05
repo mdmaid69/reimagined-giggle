38bc231c8960ba88f386bed9c22f7e8def944836
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:32:24 +0000

    problem organize not just politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_min(lst):
-        return min(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_priority_queue():
+        return collections.deque()
