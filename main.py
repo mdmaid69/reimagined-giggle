1ed1d9f6c7095cf82b61c2cccffa4dfe51cf2bd5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:28:34 +0000

    view transport something somebody store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
