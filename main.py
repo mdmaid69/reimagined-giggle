40158e1f4244ba9047fb3f1db7174a3ce6cef57d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:34:39 +0000

    deep travel heat thankyou confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
