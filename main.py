d622e9b36620accc0d6ec1c02a7c23da2ba10ad1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:03:03 +0000

    perform patience steal past thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
