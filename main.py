6e9b00b60354b40fb412b6b6d864745244ef1000
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:52:28 +0000

    accept calloff welcome nonsense figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
