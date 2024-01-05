5d3fa370eb898ee5ed2e4de0de94ae16250c547d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:58:28 +0000

    comment actual ignore responsible giveup

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_characters(sentence):
+        return len(sentence)
+print([x**2 for x in range(10)])
