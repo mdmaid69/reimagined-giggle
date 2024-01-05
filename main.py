4d45cd93a37d73b204d50db1ea5fe34a18abd3b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:53:47 +0000

    event measure trust blow motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
