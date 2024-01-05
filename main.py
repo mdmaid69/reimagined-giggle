945a41387954fb344301212339d9a99d5061f518
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:35 +0000

    even handle sound when discover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  def is_odd(n):
-        return n % 2 != 0
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
