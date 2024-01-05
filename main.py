2b02821ab86be4c2684a022aea6f72808a63da9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:00:06 +0000

    congratulations disagree form tie exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
