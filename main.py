5a864e495dddd5a5b33084d038e6540c592c3c90
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:29:37 +0000

    track characteristic bringdown kind temper

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_set(array):
+        return set(array)
