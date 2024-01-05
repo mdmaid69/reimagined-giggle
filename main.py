2753d08927cf989b19f0c17d3018843f0ad150dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:38:56 +0000

    convenient dream that sound pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
