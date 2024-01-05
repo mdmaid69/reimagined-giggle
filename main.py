d76837e037b63ab1d5c1378f980998da9f3dc2c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:51:15 +0000

    or plate beyond difficulty market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
