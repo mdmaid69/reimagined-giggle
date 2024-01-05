5e4fc5ff238c35100834e30bda6152db27d79b45
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:18:50 +0000

    show movement speech complete scream

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import platform
-def get_os_info():
-        return platform.uname()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def sort_list(lst):
+        return sorted(lst)
