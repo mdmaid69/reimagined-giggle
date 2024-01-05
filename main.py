3fa9705924328736ab296f6ac45e00c5e5f46595
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:40:01 +0000

    tragedy confused medium slight dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
