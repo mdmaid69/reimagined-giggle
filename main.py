1e3313780083cb0cac4a8b5f10b04a0d7a6ebfce
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:15:44 +0000

    sure pleasure 566end godown but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_density(mass, volume):
-        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+print(sum(range(10)))
