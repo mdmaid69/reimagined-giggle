56135f50e7486a5bad3e6edefea7077ded3b617e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:31:45 +0000

    trick guard responsible before unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
