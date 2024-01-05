e23f88149748092c828242ea9e57612d1ea27ee0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:20:11 +0000

    success Uu whoever account tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_area(radius):
+        return 3.14 * radius * radius
