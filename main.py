8015a9cadf79870b99852b62fadea4905078c62e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:50:24 +0000

    calm law course remember custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
