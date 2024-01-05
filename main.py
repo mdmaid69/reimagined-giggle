6b6569e8ba767cca0863961f1d9b5ed67d7914a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:45:39 +0000

    soft country sting good communicate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def square_number(x):
+        return x**2
