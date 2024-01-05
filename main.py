2bb834ec79adf903f6dc075433f4b3790b87a9ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:06:41 +0000

    prize statement rank kind aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
