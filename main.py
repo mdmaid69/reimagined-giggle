48a121018bb61ddea8956bc2c9bbc12446919623
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:52:46 +0000

    ability behaviour watch acceptable enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def create_user_list():
+        return collections.UserList()
