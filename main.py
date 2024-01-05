6fa3abd6889f5f946ffc2dddc65b7f70d5bfe6c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:42:20 +0000

    plastic fear ill door arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_floor(x):
+        return math.floor(x)
