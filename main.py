23f6eaccda29ffb285fe9c17e2c7810ce3977fef
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:28:55 +0000

    sure easy whistle beginning could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
