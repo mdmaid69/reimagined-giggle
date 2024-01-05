8c0b836edd98f0eedc0fb33d43d7a3ed4fda05b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:26:17 +0000

    traffic artificial wear help pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
