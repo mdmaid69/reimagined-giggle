b73497a825a9c0adbd749eac28559986e8d824b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:36:36 +0000

    live shake further we dead

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_max(lst):
+        return max(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
