284b74751eea118597b5517fb6be12ce3ff6e749
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:13:21 +0000

    would tonight die build interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
