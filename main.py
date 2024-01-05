5f2766e147db536fbd3de3c1a23560a9f5b59f52
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:38:21 +0000

    keen home maximum sign coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
