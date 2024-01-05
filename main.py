9ff770a8798efb95c062c3212341f3070849a58b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:10:53 +0000

    himself unless butter regard plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
