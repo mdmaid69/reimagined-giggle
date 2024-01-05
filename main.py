9f6781073b372202e04e76ac3db8172993dd36ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:06:29 +0000

    rush meaning pleasant straight spill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def reverse_array(array):
+        array.reverse()
