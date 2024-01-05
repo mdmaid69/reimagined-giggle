11c4b1c52f9bbc947c24ca05ae13bfe6566b750c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:48:33 +0000

    tall fortunate who happen summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def split_path(path):
+        return os.path.split(path)
