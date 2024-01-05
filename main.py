a5fa01c0883dd17dd8038ee2a418916af84c2960
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:43:13 +0000

    shop leg afraid comment workupto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
