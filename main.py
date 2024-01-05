6dbceb57e3383b374f49294b5052ab56bbbbcec7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:33:43 +0000

    takeout position speech myself protest

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
