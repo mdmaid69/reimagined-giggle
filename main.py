7cefd3309bb46761f46b18d97de0bb36266bbe07
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:36:07 +0000

    actually wrap dry true ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
