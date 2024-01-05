ac2a6ae4d67e6bf4b74736b81a77d8bbc2dc7069
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:57:40 +0000

    tradition rid design safe star

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
