72d5253b9e207326ca113cfc9aec206fd5d5bb4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:13:57 +0000

    show off pupil this rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
