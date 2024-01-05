2af0e02f34c907c1873126bd898ceafc2d05b03c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:59:52 +0000

    birthday instrument war drink kind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
