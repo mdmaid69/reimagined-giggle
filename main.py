25d1a44431451893312408149dfd333b1df29205
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:27:29 +0000

    mistake hope that expression turnaway

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def subtract_numbers(x, y):
+        return x - y
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
