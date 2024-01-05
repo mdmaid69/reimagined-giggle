2b74240b448fb792a623fc786a212b8666e4bb99
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:41:13 +0000

    slide splash lunch might hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import time
+def get_current_time():
+        return time.ctime()
