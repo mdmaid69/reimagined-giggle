e872f5a2d7ced755e03021f76869091bf0c22054
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:31:55 +0000

    age confuse instant public comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
