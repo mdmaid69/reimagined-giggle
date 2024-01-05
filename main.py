56e8753d3b27d0dc7db794777f4e584c672d1d2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:46:17 +0000

    connection flow properly miss to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
