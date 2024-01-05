4dec47ddeb23f7321fef544ca699fcafe9ecf7b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:23:01 +0000

    brief Oo possibility recommend because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
