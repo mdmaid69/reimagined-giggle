230b20a1c78ccdaa9476f5def074872fc3dd6873
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:49 +0000

    safety nervous seem little judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import time
-def get_current_time():
-        return time.time()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
