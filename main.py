826bb1ebccc3bad994faa468350ed0619979fc86
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:35:57 +0000

    good wild total expect deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_as_bool(array):
+        return bool(array)
