d5535b9bed4f61c598a4fd84801f19b609de8a89
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:19:27 +0000

    each excited store one 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sys
+def print_python_version():
+        return sys.version
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
