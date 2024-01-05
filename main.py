578e269dbd1eabf1232a21faacd8ac83b070d5a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:41:40 +0000

    shine risk damage later card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-print(sum(range(10)))
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
