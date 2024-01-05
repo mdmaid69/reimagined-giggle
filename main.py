02f5bc14d3431f7770856cd7fa93677abce41848
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:45:09 +0000

    clean plant damage weak search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import tensorflow as tf
+print(tf.__version__)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
