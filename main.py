73abd9f0443f583f096ec8691c8041b0c763c469
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:47:09 +0000

    splash disappointed attention special trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
