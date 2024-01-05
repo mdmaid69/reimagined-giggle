e11d40eb5970252a1d26bdcd835aaf4efc0b23f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:09:16 +0000

    change meeting middle mad false

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
