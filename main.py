8b2a038ad0786a7cf75b2f5227aff1cd5bd49e2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:35:24 +0000

    as keepoff comeback tap stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def get_list_from_array(array):
+        return array.tolist()
