636ddc4100d4eb6aba77d58751774a8324dce44e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:24:25 +0000

    peace obtain obtain pool disturb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
