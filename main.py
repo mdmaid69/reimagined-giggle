5fe5a9aa12d2389fa05153aa21151e1d240f1a0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:52:27 +0000

    their somewhere turnoff accept sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
