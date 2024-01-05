29193de952da0d1a4b39136fa3d891b17e886556
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:03:45 +0000

    band cum slight they bury

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
