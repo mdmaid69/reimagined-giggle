846184c66269769d238a195154fe17f459e3bb57
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:47:48 +0000

    instrument into regret high rest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
