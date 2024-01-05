d4b5ba07a5ca5782f46f72e6a3e83cd05bddac8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:33:48 +0000

    entry with fat unless treat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
+  def convert_to_hex(n):
+        return hex(n)
