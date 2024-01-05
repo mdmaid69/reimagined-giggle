cbc80cb7887195289065075e49f1ad21fa803429
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:45:52 +0000

    pretty price strong tall secret

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
