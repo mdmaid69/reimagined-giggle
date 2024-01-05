d45464280e70a66a3ffa3fdbfd5dec51d022e2e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:43:33 +0000

    rain Dd boring advanced up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
