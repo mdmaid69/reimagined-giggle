653de7158ae087a780e0165a0c0dc0413ac148f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:51:16 +0000

    speak eye first tense bring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
