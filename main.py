be9de9ac98d70a3a2d19201097a06c015563df6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:43:20 +0000

    breakfast bright slow tell takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
