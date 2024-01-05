d5abce23267717302d7134c9e97585ebca0f002f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:28:13 +0000

    recommend model race persuade describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
