72090aab829b82102e65dc132a1a0b791093ec8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:22:30 +0000

    boring turndown comein top big

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def reverse_list(lst):
+        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
