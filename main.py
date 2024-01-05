97c68cac6c9491ac9f1053ceb6e4d4305bcd8865
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:08:18 +0000

    first bag aim congratulate to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
