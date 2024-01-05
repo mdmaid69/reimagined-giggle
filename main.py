8b43ccdc531969f0f173cb7d2c881fa2006f8321
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:13:55 +0000

    excellent wish lonely balance wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def square_number(x):
+        return x**2
