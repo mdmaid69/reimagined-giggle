fe854da702462505b251b3692c0b87b51ce47d2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:53:20 +0000

    damp strong compare guilty juice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
