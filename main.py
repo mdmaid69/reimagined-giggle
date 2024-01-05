10abc70f1933c5fa3f4d23f4cea1609fed8a4921
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:59:23 +0000

    soft realize bear complete change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
