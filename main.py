468efbd6c6c5829cb6ed525463d6f5a7d87d3f1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:02:40 +0000

    fit set disturb repair afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def count_elements(lst):
+        return len(lst)
