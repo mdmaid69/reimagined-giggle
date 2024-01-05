db625310a69bce9e7be5ad8a117e2d2e3afe0109
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:06:15 +0000

    otherwise where backwards standard hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
