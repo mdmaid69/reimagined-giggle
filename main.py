8ca1ecbbe1657776bc97e310fc4abba1232fda10
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:24:06 +0000

    period rain wrap weigh fashion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def greet(name):
+        print(f"Hello, {name}!")
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
