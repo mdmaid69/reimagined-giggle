29ddb55773d4b1d24e4d16048f3789a120e7b016
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:02:27 +0000

    innocent separate shine contain meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_circle(r):
+        return 3.14 * r**2
