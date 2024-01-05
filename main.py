30b06e641e5dfac3eda6cf97f8345c3c1a651acf
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:05:27 +0000

    slowly exhibition wire OKorokay typical

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
