d06b7c96e87388997969c4acba66a14bf826b143
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:55:43 +0000

    refer size dish organization stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
