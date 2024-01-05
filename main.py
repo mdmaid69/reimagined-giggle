0ff75ad50f8a294cd8aacdf77b0b855da19cf117
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:34:43 +0000

    mistake call suppose disagree his

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
