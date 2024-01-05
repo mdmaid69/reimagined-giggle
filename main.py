4bed8102ca7e316f476ed790e874560e759ed5dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:01:25 +0000

    sink study report feature bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
