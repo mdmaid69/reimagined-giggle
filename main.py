ec40f2a4fd5f32b0d9934557420b9b6ed1144957
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:19:40 +0000

    pump much possibly spot special

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
