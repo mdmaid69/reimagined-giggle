edceb140ad01b3941617b2091315ba06c7396572
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:29:46 +0000

    relationship equal visit size past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
