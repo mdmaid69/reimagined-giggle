48c2622439dcc799c4b9e1be387bf6f22c1e5dad
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:51:14 +0000

    birth politics harm tap clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def convert_to_octal(n):
+        return oct(n)
