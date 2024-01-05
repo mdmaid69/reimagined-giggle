d8cded22c2b626496817713d75525df75007a04c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:04:18 +0000

    slow refuse total choose sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
