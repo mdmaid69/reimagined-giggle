ab66efb458029a2deac1e72839a375ecc659a295
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:27 +0000

    along part often upwards bite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
