b1dc65f264641588322a2b9cf860dae141c57f59
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:57:10 +0000

    separate Qq flow clue open

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
