d176f991da74091eadb29cffd0fde4ea3f5a3e51
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:46:30 +0000

    ignore loose give purpose clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
