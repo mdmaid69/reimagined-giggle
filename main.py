1a1a0982f5a72a4cc8b1119d0f7e267d8d9f7280
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:02:29 +0000

    good centre mean burn let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
