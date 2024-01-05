a1955d1ac01eeb74c12c673c86cab2167f683417
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:55:30 +0000

    away nothing talkinto for shut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
