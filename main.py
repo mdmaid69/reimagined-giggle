9a373d60900ac1ba220b411556522d88a852f44d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:34:01 +0000

    crowded hear flat fairly major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
