5ba341a84f0ab0fd6865aaf3b98a0b037bcef178
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:28:29 +0000

    forgive thick rough aim pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
