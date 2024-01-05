2cc76cbb8e4c45ac19fd94045643686f932f1190
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:57:04 +0000

    they turnon hold instruction defend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+print([x**2 for x in range(10)])
