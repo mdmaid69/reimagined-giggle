7cd1f04025b7300b82ae48e93593217a152d7307
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:04:03 +0000

    home already commit negative connected

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
