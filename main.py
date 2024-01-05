438e08fdb1a1b7ef7c568787005d403ee213acb9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:37:29 +0000

    reject message wing right either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
