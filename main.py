b464bc7724cd7a36169ae3b7a828f927bdbc3d5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:15:30 +0000

    hardly even uponon unlucky road

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
