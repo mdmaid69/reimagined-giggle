eceadb5df97ba4fbc17e441c3dd0be36afb9acce
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:07:17 +0000

    fit national bang bad sore

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_item(array, i):
+        return array[i]
