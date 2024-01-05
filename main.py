3e400bc4f859a6471143956cead540224751e50b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:45:23 +0000

    phone before unfair study stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
