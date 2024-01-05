790facc50d8e29208d5fe93cc2e653e1f9f7575e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:53:20 +0000

    food it trousers anyone married

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def convert_to_octal(n):
+        return oct(n)
