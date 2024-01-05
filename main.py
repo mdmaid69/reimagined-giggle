7229db05d7ca40a661baece561f6dd685226195f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:06:14 +0000

    quick software write later mother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_work(force, distance):
+        return force * distance
