9ae0e70ce46e8c67bef67baecc2cce70ccb463b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:49:26 +0000

    future speech visit arrival Ss

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def convert_to_octal(n):
+        return oct(n)
+name = "Python"
+print("Hello,", name)
