a73e726724b482a0d99eda45a8c08cf7351a18df
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:22:58 +0000

    butter squeeze smell sign contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  def convert_to_binary(n):
-        return bin(n)
