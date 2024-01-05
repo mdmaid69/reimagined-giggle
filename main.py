91927df00938b145061041d6af5b460c1d26dbe3
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:27:30 +0000

    somehow claim action vision classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
