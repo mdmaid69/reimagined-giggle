274074ae6a7e61ea8722b33bb6743075ce065acb
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:31:37 +0000

    create answer swallow rob mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
