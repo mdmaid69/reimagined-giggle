d645d454a4c021de80a9d9b6edab8fb23a02ffb2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:14:58 +0000

    half Hh fault wild turnback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
