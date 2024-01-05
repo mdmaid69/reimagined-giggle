afc15d8ee386cdd44ceb998f89a9c2a0362a73ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:43:13 +0000

    handle used emphasize lock nervous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
