ee1910b9dfdb74521e5333b0c37b0479f8ebd8be
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:53:29 +0000

    shoot description under land all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_length(array):
+        return len(array)
+import sys
+def exit_program():
+        sys.exit()
