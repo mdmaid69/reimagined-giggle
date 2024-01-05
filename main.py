c925afc6b680d469659ec03df82ea51a16c2725a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:36:01 +0000

    uncertain pretty strike impressed married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
