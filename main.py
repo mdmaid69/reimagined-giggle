f013007bfb6ef1070245923f48b58c74276339a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:34:38 +0000

    display busy position beat work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_index(array, item):
+        return array.index(item)
+name = "Python"
+print("Hello,", name)
