42b4dec9e3c25ca28e71804daf8547e34d54fe31
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:25:22 +0000

    relationship refer night reduction talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
