5523415f615b2b3d22f25ec86221101fef2eb75f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:06:26 +0000

    sink like set fasten present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def multiply_numbers(x, y):
+        return x * y
