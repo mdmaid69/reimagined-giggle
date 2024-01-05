68539d2ab51dd974bc2a1ec7350dad31d231d17c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:28:25 +0000

    plate fortunate search deep order

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
