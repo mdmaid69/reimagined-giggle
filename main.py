b72752839e7c4b9bc48598f9dd2474974ebe23ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:07:29 +0000

    similar slight walk variety sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
