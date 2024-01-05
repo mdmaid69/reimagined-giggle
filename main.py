277d1a92636ec0d84f60af4c1819094cd01982ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:29:22 +0000

    strange pump love cause subject

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def print_python_version():
+        print(sys.version)
