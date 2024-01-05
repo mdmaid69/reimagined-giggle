db1dc6f18356dfe7617e0c2cb43b3e2f6574b351
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:53:18 +0000

    honest town after book number

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
