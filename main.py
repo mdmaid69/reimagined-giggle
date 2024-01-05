fec777ccc01aa3a23074bb919704635bb18ff422
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:37:55 +0000

    welcome part question lie special

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import time
+def get_time_since_epoch():
+        return time.time()
