78b34e210458a3af2e105f01efbc4815215408b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:46:22 +0000

    tool address wake hurt autumn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
