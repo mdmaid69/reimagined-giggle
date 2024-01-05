fb596dc8ee5ca25712a21b067a994bd97cfe40de
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:43:15 +0000

    talkback after warn among into

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
