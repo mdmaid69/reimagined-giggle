9118ac7cd9d819631d6ad331de465717e8d5d849
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:45:53 +0000

    worse close consist welcome stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
+def convert_to_binary(n):
+        return bin(n)
