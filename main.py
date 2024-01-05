8a1a01b42ef962e8898151e2e5047fdb2dbc0afa
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:47:00 +0000

    recently successful physical break opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
