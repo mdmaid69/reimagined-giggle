1a2dd52774030981f6494ec36087f3ebd2045400
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:34:17 +0000

    necessary refuse issue different minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
