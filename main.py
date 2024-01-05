42792cacc16892243e63d47191f7ce199fe3e32f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:46:19 +0000

    really day design today mix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
