42884ef139c46c53a0672cdce2f979d047ecd622
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:18:17 +0000

    container spin sit comedown dear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def find_max(numbers):
+        return max(numbers)
