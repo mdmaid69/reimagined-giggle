30b2c5d56dff8d11af9df298b6eb4aa3ff83ae62
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:26:45 +0000

    sink explanation tired takeover voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
