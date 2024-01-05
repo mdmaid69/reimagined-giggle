24b73f53bb10d50d3ae3d66a8816e7a7a4fb4619
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:35:02 +0000

    goout neck process inquire remark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
