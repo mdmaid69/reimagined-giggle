4e73c9883e2c24fc28426976e21ac6b5d57d520d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:08:32 +0000

    morning Hh wear mother time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
