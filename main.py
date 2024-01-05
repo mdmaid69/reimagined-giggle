52be1cbe0ea267965bb50a3ed3830b3a72f8bae4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:55:52 +0000

    impressed immediate bring brush mother

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import sys
+def print_python_version():
+        return sys.version
