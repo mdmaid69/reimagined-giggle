bf45c3af8c3920ce28535709b31c42ff7fe8040b
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:38:21 +0000

    afraid outside head about rub

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import random
 def generate_random_choice(choices):
         return random.choice(choices)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
