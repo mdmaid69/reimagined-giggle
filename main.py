ca8af13c3d2eaf4f4ac672a30a24ad8efb5b048a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:24:00 +0000

    roll comedown show mind 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
