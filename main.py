fea7c86684aaf016012f8917309664b5eeedb2ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:22:51 +0000

    estimate avoid twist public occur

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
