0bd7d4c028916f775cc38651b925d3f810430cbf
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:00:28 +0000

    deliver might proof classroom murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
