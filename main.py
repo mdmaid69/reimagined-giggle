35303e76d65816b8a6e31516d21c3a3164f7567c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:38:02 +0000

    medium joke describe justice Tt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
