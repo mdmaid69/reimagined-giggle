b8a615259a923ac3dd53924540fa7b3446bcabcc
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:48:43 +0000

    change reason can shop idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_index(array, item):
+        return array.index(item)
