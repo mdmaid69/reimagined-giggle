b4dc2c7c56b85f8a793a319a8d6f440fa7017f9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:51:16 +0000

    go defeat big material careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
