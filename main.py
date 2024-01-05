4df1480cd1e06a7ae57b1b31058735956477943e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:39:40 +0000

    copy talkinto flow suckme cost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
