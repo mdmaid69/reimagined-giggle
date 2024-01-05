5d419b0593e4b9c0a5f42642d6d5e1d6fb67c6d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:36:53 +0000

    copy chase live rich computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
