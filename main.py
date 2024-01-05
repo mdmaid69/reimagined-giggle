68538c9c27e5de67520f21e8a6cde877195d357d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:09:55 +0000

    him shut concerned machine doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
