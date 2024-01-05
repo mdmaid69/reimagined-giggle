e384efe6b8126b51483cbae7a3c487d719feef9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:45:52 +0000

    mean pump left leg stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def square_number(x):
-        return x**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
