f95b8e7fa89685fa2b58946c5beaeba4d41a007f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:54:43 +0000

    hold smile fire pair thank

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
