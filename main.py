4e84f099a182afef1eb6b4117736e43b7850f733
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:12:39 +0000

    school statement damage black public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
