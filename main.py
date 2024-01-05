027ed2259b0204f16aac41719ca1e8bff11214f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:30:29 +0000

    line thick cold brush read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_bool(array):
+        return bool(array)
