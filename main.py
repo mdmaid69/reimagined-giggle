6221f8bf8395b9a6eda2046768d9f6ad5627cc7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:55:30 +0000

    consist fill grand expensive concerned

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
