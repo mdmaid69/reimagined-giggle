931747e07d1e94aef7a5d865b9c004b82ed20184
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:57:46 +0000

    spring song Hh nigga but

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
