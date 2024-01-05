ec8ca416c0bc951e26dcce9bf1688e1741b824bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:57:31 +0000

    dick embarrassed want alive 565end

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
