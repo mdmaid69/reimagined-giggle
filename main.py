99b5a4e7c75e6cc0daaf1defc956ea2e6549541f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:44:59 +0000

    charge serve edge takeaway Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def cube_number(x):
+        return x**3
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
