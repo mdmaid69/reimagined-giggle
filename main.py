ed4318953f7ce593a3251317fe6c3783c0a6461c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:14:27 +0000

    experiment reason time certain blame

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
