8c403da7bd79a3b4aa4c754405a7c85a884b0775
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:38 +0000

    time run her noisy holdout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def find_unique_words(sentence):
+        return set(sentence.split())
+def convert_to_hex(n):
+        return hex(n)
