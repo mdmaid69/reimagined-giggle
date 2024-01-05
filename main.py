91fecf9f174f59d9fce65cc47f99a2be210626f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:22:17 +0000

    then careless answer argue poison

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
