84d5bc3e7535d641a63f288c6517e4341e3a78cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:05 +0000

    knock evening invitation hardly centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def remove_duplicates(lst):
-        return list(set(lst))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
