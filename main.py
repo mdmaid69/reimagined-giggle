59029135ce4e629d15e8b0719308ddb97b476d0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:16:57 +0000

    pile message tooth profit live

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print("Square numbers:", [x**2 for x in range(n)])
