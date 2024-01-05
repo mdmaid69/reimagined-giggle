cbae5daa70187a0ef81eb0931e79fd55658eb74c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:49:59 +0000

    godown eager Dd discovery measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
