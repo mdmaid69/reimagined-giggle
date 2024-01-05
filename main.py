8198e3f86d20a9dd9ad9372174fef0265d186060
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:52:24 +0000

    face reasonable day by unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
