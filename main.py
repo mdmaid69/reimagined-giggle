2946a8b932b2baa478018ab0486dfaba3916250d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:45:52 +0000

    physical yours high square evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
