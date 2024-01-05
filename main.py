7f4a89e058a0220c4a524eff294f894a6be73e32
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:55 +0000

    waste snow declare rank concentration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def multiply_numbers(x, y):
+        return x * y
