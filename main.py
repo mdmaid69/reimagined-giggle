582fb36e5b6e687e263acad44d6ef40c6ec34375
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:45:38 +0000

    spray base content nose everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
