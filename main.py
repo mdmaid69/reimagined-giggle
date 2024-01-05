e44728bf8ef32c3d2d07ee8f596c1148c42f8293
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:09:26 +0000

    in back interfere foot car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
