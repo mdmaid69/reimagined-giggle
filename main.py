c93371fc168e10dfb13683973c687ccc43d51c04
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:59:21 +0000

    rid naturally silent deliberate gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def create_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
