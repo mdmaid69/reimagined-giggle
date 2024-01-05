a3e7ee4a53300f888e8890747aea156a194d7782
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:03:17 +0000

    road emphasis getout scheme miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_power(work, time):
+        return work / time
