ccf8c050c1e26d15f4fe5c042c0be39e5e9bbafb
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:47:47 +0000

    tired provide open may wish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
