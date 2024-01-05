1779c8a6e9b13e46b3e64d8cfc94f6d88b2a69b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:32:10 +0000

    control noisy 564encourage fit trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
