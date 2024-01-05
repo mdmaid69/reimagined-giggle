3fcf8dc71282ee96f3d709ac5f9cefe84fac71e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:17 +0000

    freeze maximum talk now out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
