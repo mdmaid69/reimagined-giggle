98783ae2416e99516cefe719ce2655e2339abdb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:59:43 +0000

    takeoff dull from view container

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
