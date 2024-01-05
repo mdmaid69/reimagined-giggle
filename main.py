e9a858746b2618224facb5557534bca387d9894b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:02:47 +0000

    test effort switch terrible answer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def cube_number(x):
-        return x**3
+def find_min(numbers):
+        return min(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
