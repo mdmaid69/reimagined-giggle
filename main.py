994638dd8c54a9a1fcaa91e9cd0be1df539e3431
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:23:57 +0000

    food count taste open fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def remove_directory(path):
+        os.rmdir(path)
