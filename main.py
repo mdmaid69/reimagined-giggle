4d3bb4f4af7220413f1c969085768aa53ac2939f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:25:44 +0000

    cold nurse stranger female eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print([x**2 for x in range(10)])
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
