b81fbc9885ffaf08cf3c7734b881ea02afd282ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:29:24 +0000

    cook pride clean contents hole

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_power(work, time):
-        return work / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
