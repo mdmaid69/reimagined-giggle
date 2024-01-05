d034d45155c854aeae667bd6bab5da75c4677880
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:34:19 +0000

    around ilovedicks tell belief group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
