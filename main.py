8ef2a678a23c44593ba04b13517bf0968e1dfbe5
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:03:03 +0000

    runinto together detail lean against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
