22d3884c09b7f1942c6db2a944511a10233e0878
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:20:18 +0000

    almost dinner understand Dd have

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_area_rectangle(l, w):
+        return l * w
