f7e3f638929fd5a153e208470b4bfefaf1a4729b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:26:48 +0000

    intelligent mail brave dance sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def reverse_array(array):
+        array.reverse()
