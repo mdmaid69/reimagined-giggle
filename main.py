83a4c521a19e95057164328adcd4232caaea7d14
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:04:51 +0000

    for music sleep board press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
