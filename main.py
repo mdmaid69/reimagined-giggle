d67f46a8e64c88721a75e7bc12aa9b72998f8073
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:02:38 +0000

    even sure set risk in

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
