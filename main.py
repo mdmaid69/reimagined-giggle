fedc8787953b6f51e9627ad857a3d7d2875d2176
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:18:08 +0000

    unite design little seem factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def find_max(numbers):
-        return max(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
