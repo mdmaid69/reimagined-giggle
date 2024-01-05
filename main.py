e0a1cdac32228aacebc8e7dd00d42cb27a2f0cbc
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:14:51 +0000

    during sit finally friendly danger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+name = "Python"
+print("Hello,", name)
