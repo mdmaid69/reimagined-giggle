6a4436556213653542f04b5516be0de135d4a670
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:38:31 +0000

    near form indeed else nearly

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
