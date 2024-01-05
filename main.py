eabf544ae3eb16ffc01135988e3cee4a478029f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:30 +0000

    grade pair detail accept welcome

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
