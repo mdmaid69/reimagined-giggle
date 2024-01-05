e49148d68f45912af01f60e5989460ecd8f55fac
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:12:12 +0000

    sentence confident thorough count go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
