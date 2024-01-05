d95ad46c749e788780947f511f129b5c8fbf50bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:20:27 +0000

    lost excellent dig that public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
