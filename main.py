435123b3918f41bf1b5bcb22599d72a3c3f97a95
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:47:47 +0000

    change contact mail rush seem

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
