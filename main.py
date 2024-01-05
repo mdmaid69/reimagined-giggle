e2239c88b0da95b62fb54e93187fbdbb336f4352
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:04:36 +0000

    abroad attempt intelligent beginning whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
