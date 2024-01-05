8ffe9b98cf7f9a3d1bfcb038c15094a4b13800b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:02:34 +0000

    pride tap on exploration fear

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
