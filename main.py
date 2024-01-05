5cbc81d16d810fe6c4b8df27360dd3305ffd5da1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:45:14 +0000

    allow pretend into take already

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
