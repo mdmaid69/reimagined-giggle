fc2e9d59b5ed37d58b6aeaa09dbce07859bc0e18
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:22:33 +0000

    easy moral talk strange against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
