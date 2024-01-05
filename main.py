2dfa0ef3adf16851aee27dea91053adaac9658ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:06:44 +0000

    look once principle peace stupid

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
