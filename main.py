172cbe07b606605d7672b389df482edb62a30c22
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:28 +0000

    close door shadow holiday speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
