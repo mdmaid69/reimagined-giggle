6087e07cfa5538e32c27356d13edd9a24d889e0b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:38:45 +0000

    watch on spring usual repeat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bool(array):
+        return bool(array)
