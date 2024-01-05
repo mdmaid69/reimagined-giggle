d8084350fcb967df0d3f1dd075c08b7e5fa6ed26
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:56 +0000

    heavy gradually off national short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
