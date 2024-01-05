1b5683124bca09e09b43e2d30d2e5d29801726bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:35:10 +0000

    find right reduction win watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
