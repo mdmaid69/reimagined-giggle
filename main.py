04fa6eb2f1635c3568e38c4c6894de7b0ba3bc6a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:47:01 +0000

    main slow back invitation measure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
