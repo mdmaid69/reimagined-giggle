a2fe72fc74d615ebc7d81e85f52c4d168294b7eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:35:05 +0000

    produce explain chase smooth dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
