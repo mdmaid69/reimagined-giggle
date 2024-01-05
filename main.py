434669eed59bf57a12918e767385495692724829
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:30:06 +0000

    correction corner book emergency ours

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-print(os.getcwd())
-import array
-def reverse_array(array):
-        array.reverse()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
