bc9d365d2e72891c72ab42cca750955ff8e0c863
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:41:17 +0000

    table attempt upwards settle thought

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
