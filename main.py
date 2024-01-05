a074cd71b59c081a89d91ebfe3411ea504066f34
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:09:50 +0000

    leg guard also may lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def append_to_array(array, item):
+        array.append(item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
