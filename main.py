554c474e0177b9182888b8a4092a7d7a10f6fc43
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:50:42 +0000

    beside fashion impossible runinto until

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
