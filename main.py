981f6e2ac2806c491d8da863fa66883f35d5cd53
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:50:29 +0000

    common which Rr ill yours

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
