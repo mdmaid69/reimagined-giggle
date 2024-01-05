39c21e4086ae4ff7a5838042d64e0db9f4a00c0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:10:04 +0000

    long become reserve intelligent criticism

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
