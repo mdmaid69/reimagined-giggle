fa9a3b65aacd14b988ebfc7541d1254bdb470206
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:30:18 +0000

    duty pipe embarrassing so please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def find_min(numbers):
+        return min(numbers)
