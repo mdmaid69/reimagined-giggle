413e7d289f3fcff14a767275bebf79de538f2a27
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:01:55 +0000

    building expect apply shop thought

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def square_number(x):
-        return x**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_list(array):
+        return list(array)
