c34970fa715d59f0c263bae149e4f780476ec5cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:37:40 +0000

    emphasis wing brief among trick

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
