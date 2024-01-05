eb8e521f4d667130e1c3e25e74bab213956ba3f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:38:32 +0000

    flavour Bb noone modern onto

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_hex(n):
+        return hex(n)
