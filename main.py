1beb2e30ca059c45557afef3cb31be4723cc1c31
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:18:32 +0000

    boat natural dry peaceful guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
