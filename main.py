ba290d5b6ecb4d9e9f3bd8d4d895c32e24dece9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:22:03 +0000

    warning practise hers fly put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_list(array):
+        return list(array)
