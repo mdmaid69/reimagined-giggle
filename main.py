0e293e2f6d669ab37e2bd238d5cfc498ee891684
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:13:32 +0000

    quiet female temper love Mm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
