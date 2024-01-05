c1e7f360e47c9e069e5b9497970845318c3e58c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:48:05 +0000

    exact cheat rest hurt beyond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def square_number(x):
+        return x**2
