1250dba7ab4cf98750dbd264deb06afd86736aa4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:40:10 +0000

    understand label game march winter

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+print(math.pi)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
