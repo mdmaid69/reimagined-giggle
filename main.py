d5d82d9f1a1f4f4df7afed048b23c0b85fd7ba21
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:30:13 +0000

    address chance reduction spoil friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
