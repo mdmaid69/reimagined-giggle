807ee513b2ca188c72e6ad3e67afed4685811fdc
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:30:53 +0000

    operate after which benefit difficult

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
