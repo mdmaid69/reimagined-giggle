3c761e464acef121bd7adbcc457a5caa72ff5605
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:18:10 +0000

    everywhere produce earth sing score

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
