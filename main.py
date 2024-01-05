668e73c6b18603865146a4a9cf2822c11d46e38f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:48:16 +0000

    ignore save clue lot memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
