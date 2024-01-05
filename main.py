04f0dd7db6efb1f389999ac818df62a46902b65f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:56:20 +0000

    soak false Hh sad service

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
