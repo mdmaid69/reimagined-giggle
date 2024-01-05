0e74e5ad4506d918a4a87897864b3b5582cebff5
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:21:31 +0000

    advise law butter separate say

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
