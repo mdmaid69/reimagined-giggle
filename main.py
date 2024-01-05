5eef3eae89bf38028f979a77cf2c0bd4c0314900
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:14:50 +0000

    previous estimate lean keepaway wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_stack():
-        return collections.deque()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
