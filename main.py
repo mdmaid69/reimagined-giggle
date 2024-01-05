ce31b5dd9a6281deb39aa6e14ec15f31158b0637
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:51:58 +0000

    early across book perfect serious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
