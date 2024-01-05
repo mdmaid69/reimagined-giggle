dbec2b9551105ce2cfe9058872ca7aa9c1852858
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:31:26 +0000

    approval obtain ordinary onto situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
