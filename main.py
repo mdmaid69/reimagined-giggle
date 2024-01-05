327837e2e898f915a4d7885a10a3284f72c59ac2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:45:38 +0000

    square unusual accordingto comeout once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
