85e99c5fe21a8a913f360ceca62772cd774eb157
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:51:39 +0000

    takeback contact collect appeal later

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+print(math.pi)
