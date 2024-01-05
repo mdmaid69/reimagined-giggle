5818c7179fe3c91b587a4079522817f2e3a7274a
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:25:58 +0000

    everybody taste distant gentle stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
