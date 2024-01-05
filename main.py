1404fe85209971072713d7a07f1453d4596a2baa
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:27:24 +0000

    upset fail remain public about

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
