14421d914e8ca454ca5a417bfb62f5b49fbf9149
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:10:28 +0000

    worry forgive balance breakinto base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
