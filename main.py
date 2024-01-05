6ead9c37294e6a64bc520d410c0cb4f32c48a82e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:30:46 +0000

    buy guess much standard alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def reverse_list(lst):
+        return lst[::-1]
