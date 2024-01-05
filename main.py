71dec08a1fd99a6dc981d3b541417e3d414dd3fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:56:27 +0000

    back pause mad bang spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
