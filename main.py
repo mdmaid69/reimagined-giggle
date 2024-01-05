fede6c9e9d4a1435e0309f15095cf8449f173f5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:18:05 +0000

    pause miss spin pronounce fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
