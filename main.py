6bc25999665bdeceb3e2976944fe0bb4f450bab4
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:29:41 +0000

    naturally roof knock hot limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
