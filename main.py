632f1e4f7021da36adf65b251c7a8f1d9a022061
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:21:38 +0000

    clean benefit surprised quiet middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
