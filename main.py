a7bb9203850c420699b898582e4643e6621ce645
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:06:13 +0000

    shout ear police sensitive lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
