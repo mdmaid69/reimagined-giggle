2a3fc931a4b013bed2a42ec148c7f70669de85df
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:11:31 +0000

    confused wait couple image reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_current_directory():
+        return os.getcwd()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
