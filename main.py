ddfd0d37fdff86ff25f784b79411f005088e0eb6
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:00:58 +0000

    talk damp favourite confident somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
