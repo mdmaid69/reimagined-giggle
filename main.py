aae0141cbc1a3f8aacbefc8ae91a757897196ab8
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:55:25 +0000

    Rr ahead big effort into

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import sys
+def exit_program():
+        sys.exit()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
