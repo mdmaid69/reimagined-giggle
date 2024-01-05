e201242a881c7123ea1fb268bbacad423c31469d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:05:23 +0000

    class threaten release rise try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
