efde1f0579a6911a2d11cb5fbc9281d6637ab6a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:54:11 +0000

    willing rest interruption except right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
