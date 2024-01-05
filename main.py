56beb596ba0baafd3ba8bcea8717ce27d8c58261
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:19:46 +0000

    book act confidence Cc criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
