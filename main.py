38bd4e28d252ec9e50e54420938adefa14b226aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:56:37 +0000

    makeup past embarrassed somehow many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
