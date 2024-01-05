3436aaeb7e30ebc704288ffc9fb29f93624a6ac7
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:57:38 +0000

    visit forward task fasten question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
