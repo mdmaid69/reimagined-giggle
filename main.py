894c422fe7f487f3814216fe3f8af7ee93940de3
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:55:23 +0000

    possibly average exercise section movement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
