2558a3564ec180ab8e558c049325179a542345eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:13:59 +0000

    say that join rich while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
