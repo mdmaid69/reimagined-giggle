d8176e34ac148e8943f89e446cb62c5e527e446c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:36:28 +0000

    reserve difficult certain sure attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
