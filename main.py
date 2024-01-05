916b4c2f30313498a61830a4991bffb90f6f5961
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:04:43 +0000

    Tt space remember godown finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
