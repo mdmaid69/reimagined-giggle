6eb2dfdfd1f6f0b9643199febbd07ff812fd1f8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:31:48 +0000

    probably type stir possibly hot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
