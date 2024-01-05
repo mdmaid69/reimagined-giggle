e4fde3172843987e5d2b438dbaccba6f39811447
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:16:19 +0000

    mind basically meaning mother crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
