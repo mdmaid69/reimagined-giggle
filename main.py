c933d94553bbb1229719699adff29528fe606e72
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:20 +0000

    write difference conduct rough ability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import time
-print(time.time())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
