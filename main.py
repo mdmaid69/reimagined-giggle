edba43a3202c10db8a2eaad142512981585a2003
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:17:01 +0000

    pleased yet telephone hope supply

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
