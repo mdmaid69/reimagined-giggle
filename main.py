2df315ec287b47e60c3043007259500158598db9
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:03 +0000

    practical criticize takeback fence smoke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import sys
+print(sys.version)
