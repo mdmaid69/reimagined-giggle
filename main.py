d071043d43226b91beaa265f3877ae89f373a12a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:49:06 +0000

    eat report depend write ours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
