06b659085715c4d2b38b851636a28c5b151ab688
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:04:48 +0000

    thanks scheme getout sensible grade

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import sys
+  def get_python_version():
+        return sys.version
