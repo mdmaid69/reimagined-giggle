a8e8b70664de434e5f480fa728785ebfc15cb198
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:24:55 +0000

    nearly strength next cheap wise

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def cube_number(x):
-        return x**3
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+print(math.pi)
