c125487727ca13a80769f2e14f0e9a9f99fe2f4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:24:06 +0000

    correct stiff possibly concerned peaceful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
