793a63948a1a433a5990860c09ff662d6001c287
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:24:22 +0000

    fold supply lot obtain print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
