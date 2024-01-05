b261bb3fd034fc8bab44b5f44f60199d30626353
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:57:03 +0000

    apply party congratulate her support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import array
-def reverse_array(array):
-        array.reverse()
+from collections import Counter
+print(Counter("hello world"))
