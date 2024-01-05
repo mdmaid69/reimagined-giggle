01515004775eeee7b1bcc1545ca3c8607504ca5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:01:12 +0000

    570enjoyable married laugh roll tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_mean(arr):
+        return np.mean(arr)
+import getpass
+def get_username():
+        return getpass.getuser()
