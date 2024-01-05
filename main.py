ed8244225e0f0276aa53a5d3b9d66719cb39e278
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:29:24 +0000

    news plate breakout indeed environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
