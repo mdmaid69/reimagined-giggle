8ca1438b45f3217749c0b8f5750e341c1de2c698
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:44:02 +0000

    false pass field guess negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
