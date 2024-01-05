d8dc46c579d3f76c25f0ca751ef6d3297bb426c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:44:53 +0000

    light behind whistle no point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
