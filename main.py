1cee5f4caea32921977bdbf4894ed56790dd93f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:21:44 +0000

    characteristic job switch water raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
