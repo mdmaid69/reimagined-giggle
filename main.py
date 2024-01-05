fec860b9bebb248f6ad894fc5d610835e92ffea4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:05 +0000

    mass support soft broad secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
