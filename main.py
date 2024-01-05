e8c6f876f9eecac1568910b67b42efb35640f3b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:11:50 +0000

    unpleasant just cut study tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+print(re.match("h.*o", "hello world"))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
