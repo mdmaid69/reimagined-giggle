07baa0b3b588e5331f0092b1d77840a2cd121bb9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:58:23 +0000

    makeinto deserve risk towards terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
