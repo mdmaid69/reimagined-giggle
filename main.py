018a406e7750d1b6cfadad55409b4ef70103d408
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:18:28 +0000

    nearly compare entry scrape bank

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
