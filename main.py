afd8118ac83689292cf6b987f1691cfde80bd9b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:45:52 +0000

    song to sea reply worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+print(math.pi)
