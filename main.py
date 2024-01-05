d3c3e1d0b27b11d289804c9c1a7469072f587011
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:17:34 +0000

    illness increase before maybe breakinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
