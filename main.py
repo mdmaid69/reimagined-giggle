62997c08bac327258d54f671305add3dd791d0d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:25:01 +0000

    drop method joke female satisfactory

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+print("Hello, world!")
