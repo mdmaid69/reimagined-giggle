998afdce34e3bb353c995af28a5096e87dfaa855
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:31:39 +0000

    centre unite impressive bath store

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_speed(distance, time):
+        return distance / time
