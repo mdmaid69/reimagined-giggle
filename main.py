b315893e4e67a4ca9eac2f1952d7a1ea34edf0c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:10:02 +0000

    find accommodation turnover pass respect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
