c1f9271c11e5f4623ac7c09af14a55af195860bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:38:41 +0000

    especially brown quiet while holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
