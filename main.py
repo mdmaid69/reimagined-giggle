8251e851512fd3fc22c8d1a34e1bb158db7620bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:19 +0000

    fear whistle target future plastic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
