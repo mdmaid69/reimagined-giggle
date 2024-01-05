878375e55e2b4371f44896d0a1e6649ef25b6fba
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:52:29 +0000

    crazy the pen moment squeeze

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
