d1c553826161c215fd0449f0b15e8574798176e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:15:28 +0000

    manage warning gradually load sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
