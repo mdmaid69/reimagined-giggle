81f4f2b75937aff81e6621c06b9c76c9ae5d3394
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:10:42 +0000

    figure comeround conduct bridge belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
