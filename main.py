219f77e62fdfc05fdd691f0379b8696350ad0a31
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:50:59 +0000

    passenger strict rest how exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
