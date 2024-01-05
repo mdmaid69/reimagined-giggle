959aed632b98caa5aa22db913403dd4173b36560
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:07:17 +0000

    attractive fight meanwhile objection close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def add_numbers(a, b):
-        return a + b
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
