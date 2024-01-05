a986eed2e53f803af0f52f85c837508731e483c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:52:03 +0000

    gradually tour inside target gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import time
-print(time.time())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
