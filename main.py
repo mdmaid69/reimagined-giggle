c25c495f066996149f5143392f8b02fc2865f8b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:55:08 +0000

    load wet welcome film learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
