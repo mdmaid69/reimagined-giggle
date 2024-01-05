1a725bbc60cad59f1b6451a0e82848d55ffc0d2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:40:53 +0000

    born anybody convincing life male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
