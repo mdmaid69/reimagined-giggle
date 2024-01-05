95a2e2f0813e2f466a3912efdab4694103e82321
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:28:41 +0000

    violent considering comfort onto season

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
