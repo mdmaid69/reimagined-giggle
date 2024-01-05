8067fcc748ff54a2468ce16f56d3a102d3f0822f
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:23:29 +0000

    bread makeinto contents business yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_set(array):
+        return set(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
