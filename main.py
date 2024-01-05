a7ba4c190b1fe12acc4cd48a26108f5ec3c90b8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:09:34 +0000

    arm discuss silent chance medium

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_density(mass, volume):
-        return mass / volume
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
