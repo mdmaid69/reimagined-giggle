ef1275cca122eeeb121a0a9b6f5b5611c3499e68
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:38:24 +0000

    immediately yours wander sure lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
