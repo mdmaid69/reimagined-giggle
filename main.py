e46b39599ba7f65888f4355c86cdb19f30abda82
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:07:23 +0000

    certain risk desperate adjective flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
