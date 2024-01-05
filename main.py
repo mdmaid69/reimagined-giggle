ec4e4bfc0feb00956bfb37de3d4723f41ee57880
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:04:32 +0000

    kind leave morning steam serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import time
-def get_time_since_epoch():
-        return time.time()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
