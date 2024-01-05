7ffad70225e1744f8500b0b6c0c5cb651f51e63e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:06:54 +0000

    hurry callout talkback firm right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
