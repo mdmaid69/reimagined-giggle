02f04ecc2b08426ff37cc7fbb6a37b1cf7e67774
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:02:25 +0000

    rain exaggerate taste mistake defend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
