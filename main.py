1f01ee7f71c02dbd665c547e7f0438d0b6ff85c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:58:32 +0000

    straight deaf beginning case bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
