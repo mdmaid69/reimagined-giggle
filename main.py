0d95b4723f35a362c2aa5a2f2f081ea4f98cad76
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:04:53 +0000

    married visit contents more fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
