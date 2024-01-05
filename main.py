1ffa944973b7dbbd86d12340b38cb2db2c15d472
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:15:30 +0000

    coat little meet look left

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
