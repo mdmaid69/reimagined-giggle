eb150141e0023ccfe23f2ed287e692734079e7ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:10:03 +0000

    idea think right pump recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
