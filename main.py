0d69b6fcb8e949c2f33fbaabed27d73fb4528ee0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:50:39 +0000

    test throughout while finally rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def add_numbers(a, b):
-        return a + b
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_list():
+        return collections.UserList()
