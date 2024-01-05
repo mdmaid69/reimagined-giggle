8ecab7b8397839069cc95cc11c4fbdc526c23f81
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:29:47 +0000

    slightly ordinary advice makeup communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
