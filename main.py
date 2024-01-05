1dfd96c164151a02b91c030beefc48f7fd3b1459
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:56:05 +0000

    power breakthrough side Yy throughout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
