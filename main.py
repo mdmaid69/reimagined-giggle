a49424045b6fe4f24f53c530f4ab688d35de6d25
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:21:02 +0000

    bar summer official takeback name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
