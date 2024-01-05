5c20fa70e614603717f1727db984eae151a749de
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:39:39 +0000

    cause 567enemy polite news attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
