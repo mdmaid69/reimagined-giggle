6a4a2c7871e05880cfa1b3a07942c10d7fd744bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:09:32 +0000

    record relative fair characteristic dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
