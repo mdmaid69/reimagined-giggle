2e9cc07abc3503febbe1a6b2ad999970d193470a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:47:34 +0000

    rough yes union floor defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
