77a965426793c26c26505faf10384084dabf39dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:35:30 +0000

    slice order laugh beside influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def find_max(numbers):
+        return max(numbers)
