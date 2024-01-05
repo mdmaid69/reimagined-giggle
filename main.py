89dd319da719e47352e71ac07432a50fbf9db433
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:01:45 +0000

    bye tape commit record bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
