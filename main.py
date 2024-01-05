1ca3138507bbdde6bfef0db8ccdf556550b2dfbf
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:26:55 +0000

    week scrap progress violence trap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
