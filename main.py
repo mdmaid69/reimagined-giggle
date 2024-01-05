6a85675b4eaa568efedfd896070ac8e391123c33
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:00 +0000

    male stamp turn admit keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
