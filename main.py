07a88d8a2032b691041627b4975b98a3d9c5a41f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:46:15 +0000

    communicate safe cheap natural each

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
