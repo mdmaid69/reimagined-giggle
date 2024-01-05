e852a3420de7c826c59787713a630803563d1cb5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:27:05 +0000

    Gg cough contact makeinto frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
