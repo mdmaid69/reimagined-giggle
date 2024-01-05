3b150b28d26d781291e41e889bdf7ceefa5d2fb1
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:16:33 +0000

    rent ear cost upstairs noone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def clear_array(array):
+        array *= 0
 def calculate_acceleration(speed, time):
         return speed / time
