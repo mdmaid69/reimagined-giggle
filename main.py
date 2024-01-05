88228f5ea3dea5d41cfdfadc7817ff30e9e6c54e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:34:53 +0000

    settle oil deep hold sleep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+print([x**2 for x in range(10)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
