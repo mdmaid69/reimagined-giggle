0532954db66c5eab39ac1a6384caf48ac2d677a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:37:02 +0000

    near whatever minute sad seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
