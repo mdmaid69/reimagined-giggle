01c73a88aca5d950c30db4a8003965b8a7264a36
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:33:50 +0000

    connect straight brother immediate trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
