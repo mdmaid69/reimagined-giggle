59c45317a42667d7a243037d8dc3b7c9911e18bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:54:16 +0000

    scare emergency excited Gg delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
