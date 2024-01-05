58455b502670adba169b2903f75b06df0bd5ce2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:14:09 +0000

    study practical catch discussion high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_user_string():
+        return collections.UserString()
