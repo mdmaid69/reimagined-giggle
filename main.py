12b0f197a881ff07fca2566be955001276e6ee87
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:41:59 +0000

    answer consider separate centre lonely

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-print(math.pi)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
