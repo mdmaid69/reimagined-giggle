7c56920e8bef68f75caa0f015eb86e29c8d9c7cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:15:22 +0000

    false share beneath glass dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
