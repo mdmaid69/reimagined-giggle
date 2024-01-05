9ae3798cb6db0bf1aad41a7195e3384ac4ff4b7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:33:29 +0000

    motion bright bored word between

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
