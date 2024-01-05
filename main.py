450b9fecbf7a8bdfc6f95e89212bedd56d009d58
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:11:55 +0000

    holdon yourself skirt wave quick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
