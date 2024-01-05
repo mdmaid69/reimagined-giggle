a99bdaed1bc7f1b7706df86db89d42bb9dab44ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:25:37 +0000

    over slide art cry answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_item(array, i):
+        return array[i]
