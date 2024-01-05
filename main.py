03cb4fec9b2496a5fc5971cbd9937fde38b23d72
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:51:59 +0000

    hair comeover main left unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def is_odd(n):
-        return n % 2 != 0
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
