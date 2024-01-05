be3134bd34ea64701e5ef6b31b50e72e29ef96e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:37:17 +0000

    star thin zero cut battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+import math
+print(math.pi)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
