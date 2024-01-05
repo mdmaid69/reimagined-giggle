273ca2949ba5a8d5b83c7d1954302d5d8edcb7c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:30:11 +0000

    see thumb substance otherwise reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
