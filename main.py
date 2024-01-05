892923be54358b8384b87e5b8143cdd0223966af
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:33:28 +0000

    opposite promise turnup century exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
