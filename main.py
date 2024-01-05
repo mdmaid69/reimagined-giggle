05e3b22052c5b4fba8cbb66fde4ee8fc69a6565e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:42:55 +0000

    why remain else whenever feeling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
