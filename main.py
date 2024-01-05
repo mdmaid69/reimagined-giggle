36d71e84663e8d7de9096d0ad49bf8beb00fefe0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:54:33 +0000

    keepfrom exciting last interview neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_user_list():
+        return collections.UserList()
