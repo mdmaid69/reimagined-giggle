f1ec7862f167278013bef1e93011ff6f01ca1e8e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:36:00 +0000

    right upstairs foot time wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
