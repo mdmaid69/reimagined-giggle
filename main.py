dac6463b1700e3ee5e616de9ac4827f4efb94f42
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:40:06 +0000

    turnaway tune single suggest still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
