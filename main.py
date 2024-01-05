c7f53c79bfeb733ca124ad4fe9422f65fdbf5a1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:39:30 +0000

    runout embarrassed above beautiful bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
