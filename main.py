4433ef377eb14387b3f0b93886e5757a87f6de63
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:48:10 +0000

    fight always decide accordingto mixture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
