af8375830dcde86a2b32ac66ca6dcb6ca341b66a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:40:32 +0000

    case loose fork spare congratulate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import tensorflow as tf
+print(tf.__version__)
