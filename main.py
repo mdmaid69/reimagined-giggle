3f89b17135285968b7cecfcc25ec4f413cd39a50
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:39:37 +0000

    interest also string similar attractive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
