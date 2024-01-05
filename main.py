ff2dc41c8af9361e599b85fc56bc2727f39a7ea7
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:18:00 +0000

    spite repeat kick steer sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
