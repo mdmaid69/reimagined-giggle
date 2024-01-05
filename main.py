914dd3b2c033de2579be701ff6e81708668c2492
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:28:56 +0000

    politics expect face stupid rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
