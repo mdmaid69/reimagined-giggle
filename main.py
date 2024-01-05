d0415fe65567b896ba3ce2279ba5f2ea05f3d78f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:06:30 +0000

    stay approval that hole communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
