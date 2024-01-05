1403fa2dd8f3888da09c46876db3c083ed3fbce7
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:29:30 +0000

    fire runout complain firm content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
