145482d80cbac8e3ff765628b5521029e440f7e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:56:04 +0000

    out gate birth lot mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
