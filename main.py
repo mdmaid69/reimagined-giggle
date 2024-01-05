39be8089df616d17e3ec37b5360272f02027f28a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:50:12 +0000

    kill condition Rr decide theirs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
+def multiply_numbers(x, y):
+        return x * y
