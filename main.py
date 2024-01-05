77f3b45a30e259e40906bd8e0e7fa6b1348b01b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:47:55 +0000

    separate needle low wait turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
