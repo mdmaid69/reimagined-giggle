9392c357ad3251b711785fbd7ed62f672af077a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:37:09 +0000

    turnout hardly bring congratulate reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import json
-print(json.dumps({"name": "John", "age": 30}))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
