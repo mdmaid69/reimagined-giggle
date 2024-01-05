95d1e7f61fe47a829d7723e31279df239d20046e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:07:09 +0000

    answer suit round talkoutof interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import collections
+def create_user_string():
+        return collections.UserString()
