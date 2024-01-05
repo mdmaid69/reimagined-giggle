a1a1526844da731ce0dfa720a32ae66a3f93f8e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:10:01 +0000

    smell tense release circle male

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
