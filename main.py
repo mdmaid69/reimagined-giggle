c6a7952bba4280c4417e380353ff2613845a349d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:11:36 +0000

    ourselves hit rob cold sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
