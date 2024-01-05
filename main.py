bbac2a70d99488f9537e53b15524f9df39692ecb
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:20:15 +0000

    anyone attention range air tip

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
