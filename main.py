67e138043ca675a619da63747dc0936974390254
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:10:20 +0000

    bank quantity save advice female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
