76761dd2f51af2df87c0f058e7971d9108de229a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:37:11 +0000

    somehow advanced without round its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import time
+def get_current_time():
+        return time.ctime()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
