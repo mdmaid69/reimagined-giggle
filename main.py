e97943eef73eefbb693e90157ef38aa4a049776b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:25:14 +0000

    workupto scream passage back fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
