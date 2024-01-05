a84ba55f3a8926af0ecab69fb08a4fec9be2f176
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:00:53 +0000

    reasonable score poor colour sore

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
