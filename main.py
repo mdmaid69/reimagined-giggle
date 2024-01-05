fa742c8029f00121b782d39da8485228f83ccb9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:46:54 +0000

    stir Oo fire skin avoid

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import collections
+def create_user_string():
+        return collections.UserString()
