d0dae05794ea7770a2e92f646860a81578ced670
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:29:42 +0000

    precise reject star machine class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
