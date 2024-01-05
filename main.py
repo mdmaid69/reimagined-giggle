9642c40de630a2691eae68575cdf385b5c038b4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:55:04 +0000

    contents react fly lead finally

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
