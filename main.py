5d557a52e1bd73323f001409f30aaf3739bb7f06
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:29:05 +0000

    any popular debt waste doubt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
