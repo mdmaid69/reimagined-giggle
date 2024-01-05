8c1e3a4c85c7baaf611f11c7c0dfe75f4fca05d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:47:12 +0000

    past across lean only born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+print(math.pi)
