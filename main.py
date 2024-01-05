d2f09193ecab346bd8195f0289763f84a2df3787
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:53:17 +0000

    fair information exam lower pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
