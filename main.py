8a8962bf21a0d2320bdb94fffe7dc7d364fba27e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:40:28 +0000

    giveaway hungry some not detail

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
