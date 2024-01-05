c60f4efd7cf3b616dab6e79b36cb20779489f3bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:16:49 +0000

    takeout book make excuse error

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def count_elements(lst):
-        return len(lst)
+def find_min(lst):
+        return min(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
