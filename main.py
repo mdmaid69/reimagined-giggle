2660e0443d88bef4aff63320abada72b684bc895
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:37:04 +0000

    burn honest hair receive gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
