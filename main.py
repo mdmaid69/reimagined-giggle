8e21247e66852aa775c54a2e3b369937a8941b76
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:38 +0000

    problem chase top ground thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
