f436505ab358a3c30a5d5f83ab2360a9d65f49ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:22:24 +0000

    study food reduction congratulations cut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
