2944f3e5203696146019aad4a3fd0af35fb086b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:36:11 +0000

    target entry programme different usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
