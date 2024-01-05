63a2cbc68eed97f4f85ee7e5fa1ddb0c0b4f0a1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:22:07 +0000

    its friend trust pour patient

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
