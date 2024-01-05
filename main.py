bd176bb92dab775787f8d07b157d8f008a4b97d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:41:26 +0000

    Jj off circumstance damage sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
