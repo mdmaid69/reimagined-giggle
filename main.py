1536d5a35a35579e6e180d95056be6fef0b6bd95
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:56:14 +0000

    release defend round soft broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
