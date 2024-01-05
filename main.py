8fd78ce57102af53fb3c21dedc44151ba1360026
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:22:48 +0000

    Oo satisfactory formal fear woman

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_as_set(array):
-        return set(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
