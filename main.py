2c18ba2c318dd3275620687bd91a574dc75dfe00
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:50:07 +0000

    belief frame arrive part negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
