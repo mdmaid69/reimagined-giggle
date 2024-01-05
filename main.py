8f4ecdbbf57dbe2aed1f97c95e048c8aa9ec2b9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:08:55 +0000

    fit trouble stamp nonsense fasten

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
