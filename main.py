966c3fd465502a3b7dfc88b3ec684ee4be098dd4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:07:53 +0000

    clue safe lead square middle

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
