1a7d03424eba6dc29a3f5eba693bd2546c9b9c17
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:58:15 +0000

    raw replace path really unfair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
