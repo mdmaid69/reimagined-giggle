03a20462a104bdd914b9552cb070c74daee9787b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:34:19 +0000

    attract board job pupil else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def multiply_numbers(x, y):
+        return x * y
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
