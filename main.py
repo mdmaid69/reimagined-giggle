ba876f479484a2d99056d618129a3be4093c8d10
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:27:05 +0000

    teach drop use needle sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
