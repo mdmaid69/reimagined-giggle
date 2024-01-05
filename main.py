1f694ab88cc11fa360487fffb4ade85489a80359
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:03:03 +0000

    rest thinkof deliver pay wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
