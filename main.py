ff13a2755ef7cd42af26bd17f1c724a782f919d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:21:08 +0000

    mysterious callup pen church task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
