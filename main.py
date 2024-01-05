231c445b655ba0feeef10bfd753900da7d6269c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:52:28 +0000

    lookback(on) stop cover fall comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
