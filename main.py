cbcb0dd3c523670bebdcab3ef90fed8e2e12d82e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:02:47 +0000

    future successful accept tense rare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_float(array):
+        return float(array[0])
