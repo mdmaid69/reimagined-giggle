48a9d91929f319d7ad5ac706bc4f44471f570448
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:09:54 +0000

    creature ticket breakin angry towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
