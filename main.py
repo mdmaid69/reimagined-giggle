92c7ea074b9fe077bfe7540e3fbb479da8d12f21
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:49:57 +0000

    rule scare whenever dare minute

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_speed(distance, time):
-        return distance / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
