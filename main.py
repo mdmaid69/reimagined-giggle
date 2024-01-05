3f594e389b8c62a1005ae09174555347dabd6cc2
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:51:42 +0000

    ball trick with married comeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
