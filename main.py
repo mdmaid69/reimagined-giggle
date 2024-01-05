ea3fe902a308a7a7e2256d134d8768e4a70dc696
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:53:57 +0000

    train fashion long think matter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def is_even(n):
-        return n % 2 == 0
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
