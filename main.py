5b068bff7adf80ccc1bd025e67e6e04e8cefe9e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:41:04 +0000

    whistle turnaway dear hour oil

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_min(numbers):
-        return min(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
