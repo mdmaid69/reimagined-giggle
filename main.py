101309437a611f25ed08b3fd7cd8c724ccba53f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:02:02 +0000

    cool guide law strike ahead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
