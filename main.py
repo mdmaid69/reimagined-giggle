4f74ac8835b2a1a52e2abad92ca3b99d9a5f4123
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:13:26 +0000

    country comein above provide sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_floor(x):
+        return math.floor(x)
