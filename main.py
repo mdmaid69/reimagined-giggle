51a74b9397b52b6e77fc5c1c80fea68006454b5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:13:37 +0000

    mass pen question approval kick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-print(os.getcwd())
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
