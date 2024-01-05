634723a51a9a47651f964e35634bd855e5a0c9ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:44:48 +0000

    intend example low favour reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
