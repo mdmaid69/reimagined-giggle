264de12940278a5455eb2d3b80aad52f31363280
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:14:19 +0000

    active story variety power minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
