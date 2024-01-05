cca1ec7201154f213ab3e5d0c270b594570e96e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:09:49 +0000

    test subject tread music run

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
