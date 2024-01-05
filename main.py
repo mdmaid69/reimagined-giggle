ff2746ec0d31a9bfb1c7d0196fdb2db23cba4cfb
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:43:36 +0000

    to ought formal slide dress

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
