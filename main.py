9988ca7a737e706e9a12c1b99c7893aaa805204d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:25:52 +0000

    complete close transparent comment probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def square_number(x):
+        return x**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
