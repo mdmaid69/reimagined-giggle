e2e31e0a8ac7355bb7ec75769803e0afb56990f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:21:05 +0000

    turnup hair opposite excellent throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import json
+print(json.dumps({"name": "John", "age": 30}))
