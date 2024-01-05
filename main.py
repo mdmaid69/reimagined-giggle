9fc3706586edaac366761db399067207eadba408
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:36:42 +0000

    friendly whisper present talkback level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
