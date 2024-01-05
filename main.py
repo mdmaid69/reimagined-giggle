bb08b0ee42030c7d0ecca04d8d164396f7a75178
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:28:22 +0000

    insist cool cheap error forever

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
