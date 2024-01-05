8d270d2414c7c792d96940a083a31e7639d3d47e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:59:38 +0000

    idea store cheat fail case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
