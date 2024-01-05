8b0cd07c07ae7c34cc34bddf47fa211eb2b0bb89
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:22:28 +0000

    179bite silent press cross alive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_str(array):
+        return str(array)
