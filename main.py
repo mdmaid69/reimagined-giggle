5f10b5aa76992d4558653b2f8e45063fddd9ade8
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:32:25 +0000

    understand report doubt forever problem

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def square_number(x):
-        return x**2
-print(sum(range(10)))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
