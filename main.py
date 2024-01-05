41fc80345b5e003772e658db6ddd80e3e0ca3777
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:40:21 +0000

    pinch Nn kind ever argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
