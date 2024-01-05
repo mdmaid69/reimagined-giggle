02fe286446be65169938422573e46f8ec8b74fab
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:45:25 +0000

    emphasize permission tape task match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
