8296e29330269d0d6f84c79f3321b551dde56b10
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:14:17 +0000

    fortunately confused responsibility mainly offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
