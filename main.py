fd1401f2506019039f4ce93698dc35c86100bd6f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:16:05 +0000

    which Cc answer basis healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
