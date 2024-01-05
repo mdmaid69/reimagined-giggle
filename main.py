940dc4ca98ab019a37defad3be52fb52dbb8504b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:33:01 +0000

    Ii street usually anxious partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
