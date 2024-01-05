3987793197c837fb9ba01b7576045b2639ff4b9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:16:54 +0000

    careful damage accidentally wire bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
