8f25f6274ff57069b7b9f78f384d65946a6b0819
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:58:03 +0000

    day once ever reach before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_directory():
+        return os.getcwd()
