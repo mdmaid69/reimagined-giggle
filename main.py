5f448f788a487384c2bfdd73c58a1a19872bc883
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:06:26 +0000

    some instead admire key hurry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_pressure(force, area):
+        return force / area
