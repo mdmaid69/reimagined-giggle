ae24442ed7126cc2c7fc167c7ed0f26426f3c172
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:39:07 +0000

    thinkover main emergency angry possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
