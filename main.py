b2c536087e9e3f8313adc7f49586c50ac016f2b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:15:52 +0000

    shoot start blind means day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
