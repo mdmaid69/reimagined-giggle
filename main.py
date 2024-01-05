decf2ceecca37d698e22a5f7636771e18950e38b
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:16:53 +0000

    production Nn transparent goout produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
