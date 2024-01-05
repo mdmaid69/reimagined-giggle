629d7c7654b10dfb7900d61bde620bbee59394e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:25:57 +0000

    while event trip circumstance emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def set_array_item(array, i, item):
+        array[i] = item
+import os
+def remove_directory(path):
+        os.rmdir(path)
