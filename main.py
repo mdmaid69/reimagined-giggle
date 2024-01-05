a9f40ab5f0135719ce1822229d0f794ec6417fdd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:18:42 +0000

    responsibility university complicated well contain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sys
+def exit_program():
+        sys.exit()
