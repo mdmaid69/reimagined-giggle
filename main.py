62b1f4d568392aa77780cf231e97bafe8e95f53a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:56:44 +0000

    cheap create breakthrough luck plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
