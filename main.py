03167cfb93f15cb509ac4ce9dc06abc219523aab
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:14:38 +0000

    promise lead narrow couple take

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def cube_number(x):
-        return x**3
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
