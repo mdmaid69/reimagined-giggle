755df22d183f2cac0c6e21ff517e6bbdf77f9bdc
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:51:54 +0000

    single intend shape divide live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-print(os.getcwd())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import sys
+  def get_python_version():
+        return sys.version
