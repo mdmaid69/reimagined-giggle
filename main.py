6429939fe12080d15afe4e1bcd195ccde4cc1660
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:04:16 +0000

    control loose vision race state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
