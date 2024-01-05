70fdea096c702ca8e2cad62a78fc9de261f2a759
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:37:15 +0000

    description active emphasis disgusting control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(x, y):
-        return x + y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
