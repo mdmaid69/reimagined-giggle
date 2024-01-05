d66ae8d00c1fb8ebad532e71ada2bb38f64c8eb6
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:28:03 +0000

    both delight almost operate tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
