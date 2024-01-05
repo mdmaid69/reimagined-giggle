ef6f14874eb6fbfada23e452fe5792831e96319d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:29:06 +0000

    necessary regard concerning opponent back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
