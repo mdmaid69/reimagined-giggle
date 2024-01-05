a9046e5f4661ebc18d433f0494b6260c4d316426
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:54:15 +0000

    back hurt defend breakthrough eager

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_palindrome(s):
+        return s == s[::-1]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
