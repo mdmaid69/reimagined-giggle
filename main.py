d0869b0273eb6a68acb62f152b78cd3eb2a3bc7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:52:16 +0000

    swing yesterday Ss carry goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
