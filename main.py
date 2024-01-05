c382f0c84b39b2e41bf7920810ac68a9e28d712b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:11:08 +0000

    regular after calm check regularly

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+print([x**2 for x in range(10)])
