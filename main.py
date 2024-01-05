e34485bcebbbd24d987e57eadf104f52b79b8d0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:12:36 +0000

    test equal belong pocket car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
