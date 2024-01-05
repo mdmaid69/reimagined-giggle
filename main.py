785c07b39614532f59a41c473941ab9539b0a4fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:45:24 +0000

    pride machine while invitation disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        print(sys.version)
