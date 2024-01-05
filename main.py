c9ae585b43ef9576007832e38b33bbf3d7e1f119
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:43:49 +0000

    manage shade continue herself holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
