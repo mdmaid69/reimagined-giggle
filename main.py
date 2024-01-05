173a5cca258c8549d0447d94ed200a69c8558119
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:59:11 +0000

    Ww himself before trick shoot

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
