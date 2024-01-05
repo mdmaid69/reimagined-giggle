d913a9c66ebaf7dcaada02b2dbcf998bb43be6b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:31:16 +0000

    constantly recover process arrival clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
