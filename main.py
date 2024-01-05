2e0c432aa896e30ee47e59197259f29fa2c80d82
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:19:52 +0000

    room calmly earn steady cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
