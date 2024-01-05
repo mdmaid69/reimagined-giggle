7a61dec06b0b4a3489115dda7c71219d19110c00
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:31:26 +0000

    language comment view soon everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
