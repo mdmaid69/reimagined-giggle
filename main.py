b8e26095d363c4c27439f6294d068e13f3e0a37f
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:47:56 +0000

    pool finger central yes brain

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+print(sum(range(10)))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
