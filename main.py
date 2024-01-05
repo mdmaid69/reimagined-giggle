c9593e50766b14702256acd454d18b8ef1df310e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:25:47 +0000

    join spring want silent ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def square_number(x):
+        return x**2
