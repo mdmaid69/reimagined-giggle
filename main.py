c94212ef5e84f2d70ca3f12ab6f38df39468c93e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:52:30 +0000

    style right fall sit give

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
