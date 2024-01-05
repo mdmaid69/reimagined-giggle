4265d6d32adbdc59763b2b4ea9b8457e6d752385
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:38 +0000

    arm 564encourage shop adjective unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import sys
+def exit_program():
+        sys.exit()
