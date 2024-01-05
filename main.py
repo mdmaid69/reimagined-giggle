19576d82107698a7d93cf46e4ca5830b6f85200f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:27:02 +0000

    polite emphasis be impressed confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
