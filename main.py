abc85b97731c041f535bac12f950ab901eacd05d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:22:27 +0000

    transport that expression wheel nearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
