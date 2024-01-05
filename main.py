27633eac0936d699cc5ab3f7bef36cbb0d7687c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:27:32 +0000

    somewhere dream anybody fierce weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  def add_numbers(x, y):
-        return x + y
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
