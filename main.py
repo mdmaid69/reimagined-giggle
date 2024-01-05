809a51168dc9a74a2a4997c2bace461d906a1e98
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:24:31 +0000

    breakin church peace most come

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
