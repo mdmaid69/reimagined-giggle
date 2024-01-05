bbdb1d9e10bb6591ddf0e31e18225a31d76c41b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:40:44 +0000

    face reduce shop mean behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
