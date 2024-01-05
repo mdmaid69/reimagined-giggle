ca99caecd37058afd166b099a334b4eab0681511
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:21:37 +0000

    beat thin bunch whisper discussion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
