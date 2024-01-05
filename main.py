772d08445d5c49404537d8fa5250ca46105a5f24
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:53 +0000

    interruption grow moment write whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
