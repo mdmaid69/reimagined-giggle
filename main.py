c4d9e269075ed9d7f6d2526996624fe1760f564d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:26:13 +0000

    clearly trip week want workon

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def cube_number(x):
-        return x**3
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_bool(array):
+        return bool(array)
