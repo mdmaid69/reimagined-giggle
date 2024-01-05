3a32673604244c1d1f8846eed9f86607f5c2c778
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:52:22 +0000

    interest squeeze politics exactly correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
