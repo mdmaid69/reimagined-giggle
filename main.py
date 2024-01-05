0c8047b0e408ea10e4b408dc2ae0d5e32144a070
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:51:11 +0000

    film cope beneath interruption achieve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def sort_list(lst):
+        return sorted(lst)
