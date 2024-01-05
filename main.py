8908931a82d9641648ff4537828c0b30caa6ff13
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:38:59 +0000

    practice pronunciation excited argue offend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
