dbe5a23d50671944a4d744de4c4b9e3c28946bc4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:32:03 +0000

    hospital strike plastic your bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
