4ad0ef932a9c7f6fb93df3e2efd7ca59b39aac8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:04:13 +0000

    callout Hh guess sink deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
