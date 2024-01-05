e600cc03f26a8255ab28363d2830ca8a5a176e34
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:56:48 +0000

    song skill position tired add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
