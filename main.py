32d1408216deeda2c946031a3d30658692e0232d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:23:21 +0000

    damage trade serve go sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
