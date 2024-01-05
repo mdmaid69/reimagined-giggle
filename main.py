f3d91c986d04d5c2a41f49277e0f19a986ddb7ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:45:17 +0000

    doubt feature lot sink turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
