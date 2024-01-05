c88ad0037c97200f8ab75e48e9b78e30b5aef803
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:38:07 +0000

    square every pollution friendly sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
