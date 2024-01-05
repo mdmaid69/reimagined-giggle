fc630bbc3efc4e61e0ba797286a2e989d6508396
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:47:11 +0000

    vanish bit influence emphasize pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def roll_die():
+        return random.randint(1, 6)
