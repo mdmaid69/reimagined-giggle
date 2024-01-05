de57dab11c5f46d4c0ca1fe53e48b240ff01b726
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:43:02 +0000

    minority lesson communicate bag say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
