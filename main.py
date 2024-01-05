1ef67dc664e54c33c0162bbba452f259042d3416
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:18:00 +0000

    object exactly pen here who

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
