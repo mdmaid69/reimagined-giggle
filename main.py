d3282bae727c07141db50abb8a46fabb26d49e47
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:47:50 +0000

    Rr Vv truth blame main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
