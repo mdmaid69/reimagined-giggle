071a7cfadd1c08ae0f8d0e2eda03fd6c450ea41d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:53:06 +0000

    say comfortable culture interfere communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
