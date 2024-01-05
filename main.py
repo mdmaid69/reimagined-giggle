5ca7fc4c8bda5f9a0623c4bbcde17f35bea0bde3
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:39:59 +0000

    characteristic thought change substance free

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_str(array):
+        return str(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
