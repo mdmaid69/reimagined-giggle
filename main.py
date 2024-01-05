57ac36d0499c0fead037044d17c6b9e1a1873c42
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:24:34 +0000

    whatever head nature beginning loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
