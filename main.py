32913e44ec61878d851c3979389fe2f4334a6c70
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:33:12 +0000

    leg worried destroy its sort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
