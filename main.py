638aea8008ebfc22e014cebd51b793bdf6fe27f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:50:33 +0000

    embarrassed stir cross little wipe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_exponential(x):
+        return math.exp(x)
