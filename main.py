a278fa5072ad2de3a38c297971b46bfe1c6c5558
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:00:15 +0000

    like attitude base original rather

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
