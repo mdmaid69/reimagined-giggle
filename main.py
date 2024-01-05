63bc02dc1925baad2affa06e7edf03644f942f50
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:28:11 +0000

    turnout rest that demand before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
