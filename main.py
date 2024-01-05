1524e2946c764813151f35a0df13fb75a010b5de
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:34:47 +0000

    guilty company so accidental reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
