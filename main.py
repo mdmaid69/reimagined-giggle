76869756d3afef93ee89eadcb2fd5e88a0238a87
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:34:21 +0000

    there poor only after crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
