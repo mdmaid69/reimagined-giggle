5820f36a8ebda889b69eb633214d5ad7e99bbd3a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:03:59 +0000

    original raise shake top roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
