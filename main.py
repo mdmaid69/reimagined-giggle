053c42fac4a80cc695a039c7a3ff33b7a5e695a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:21:31 +0000

    worth pretend free out blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_work(force, distance):
-        return force * distance
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
