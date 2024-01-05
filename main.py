acc1af011e3f05a26375679896c3230c2d8ea66a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:25:36 +0000

    represent bread fire hold store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
