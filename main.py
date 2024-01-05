3149d88fa20e525fc9014ea8cf708821c3931e4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:40:57 +0000

    ocean like slightly share protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import re
+print(re.match("h.*o", "hello world"))
