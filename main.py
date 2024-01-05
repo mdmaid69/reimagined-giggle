a731439946e2d5d49aa8dffc4b5cc4b6825ea0cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:09:01 +0000

    cheque plenty especially organization relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_average(lst):
+        return sum(lst) / len(lst)
