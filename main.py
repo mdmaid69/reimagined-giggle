d2af7d000aa11c54709ed6c9b6707e0c697281c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:55 +0000

    week interview help either middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
