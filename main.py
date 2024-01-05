b7ace1826132f889b827d51a577fc65f88358ba1
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:05:15 +0000

    hurry model by damage tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
