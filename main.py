158b4f8692aa5aeb9d628bf8b61e69769a628ef9
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:09:41 +0000

    however admire relief with except

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
