5dc22f90ecb15a34e5e881024e62d0e2263ded48
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:05:32 +0000

    shadow succeed he score whoever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
