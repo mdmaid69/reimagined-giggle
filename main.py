db53dbe95d876b7b04533a15dcd1d6d46ddd3ae6
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:51:52 +0000

    traffic bend chin everything than

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import datetime
-print(datetime.datetime.now())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
