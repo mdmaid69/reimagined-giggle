eda3c420ca6b4b230f91c90cd1ce50371274cb6a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:38:46 +0000

    silent have frightened urgent strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def add_numbers(a, b):
+        return a + b
