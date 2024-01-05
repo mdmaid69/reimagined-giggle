d1c25139d69e899ed68ab8d4da275e087372fce7
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:04:49 +0000

    jump like best formal fierce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
+x = 10
+y = 20
+print("Sum:", x + y)
