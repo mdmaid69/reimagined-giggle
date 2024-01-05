fd7c59aa58ec6823e9072d7a0348887059c01116
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:52:28 +0000

    run visit might comein meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
