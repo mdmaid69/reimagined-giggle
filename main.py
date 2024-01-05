f57d0967d359849faaefb45afa4ce0dc644df897
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:18:47 +0000

    challenge tend and petrol concerned

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
