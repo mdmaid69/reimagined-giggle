54b8a5d740353095482bbf9750aeaa24e3e77fed
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:48:02 +0000

    search key price excuse fill

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
