a0bab722e4afdba32375e8eb557a4a602f115b1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:43:25 +0000

    spend go convinced road excellent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
