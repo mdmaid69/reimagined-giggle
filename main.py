e4c2004866edd469cb1a7bddb625297f55184ac7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:17 +0000

    match cruel late worth learn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
