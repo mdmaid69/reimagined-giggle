2e2635dbf112ba6b3bfa6a436c71448cfe86c14f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:05:11 +0000

    satisfactory now main nurse secret

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+import os
+print(os.getcwd())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
