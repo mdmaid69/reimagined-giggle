7c3d8bc544f97d6cd79232b0ecd49abc2a6af9f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:14:13 +0000

    machine shrink earth sight autumn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def reverse_string(s):
-        return s[::-1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
