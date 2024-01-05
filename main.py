c497ea0e28420ad49404f44f34845bc73cde4d12
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:20:09 +0000

    himself lookback(on) fond improve run

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_power(work, time):
-        return work / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
