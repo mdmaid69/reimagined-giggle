00b86e2f739057ccbd854858986d16a51be764e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:05 +0000

    table turnon persuade enthusiastic slight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
