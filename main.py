6fde19d7e72743191d4ab8ba13c622b8560459b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:50:02 +0000

    interrupt instruction grateful hour place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
