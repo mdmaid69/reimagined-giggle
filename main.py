45ca60ad8741e330f4b4b726f91a7c1ba4c2f759
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:53:37 +0000

    performance will sound needle turnback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
