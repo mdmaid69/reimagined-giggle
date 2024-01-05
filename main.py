9a75015d0b7c0589b17dbb0ef8da5bc4abc8429a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:46:26 +0000

    sure address glass worth upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
