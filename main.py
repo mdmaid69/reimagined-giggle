7e7975559a4e808b9335b49f2f04e7673ebf5278
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:41:13 +0000

    belief soft drop minute curious

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
