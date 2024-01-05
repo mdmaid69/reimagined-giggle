1dd1f5e7835648d8630523d0a133502cb2bc28d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:08:22 +0000

    scream thorough niggaacademy second spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
