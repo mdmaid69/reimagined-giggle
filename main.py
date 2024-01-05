28fa0a1d88d9c36476be2fb86400a344700b77f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:08:23 +0000

    ilovedicks contribute region excellent knowledge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
