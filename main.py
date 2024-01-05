38e076a5ebbf3fc85f0c7e48daeba04d774adf4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:48:41 +0000

    force serve cover match include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import datetime
+def get_today_date():
+        return datetime.date.today()
