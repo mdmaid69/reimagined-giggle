18383bb9fac765f4fefc97f2f6db3fdc8193725e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:31:43 +0000

    use afterwards outside fond any

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
