8f75536d30cc373d74a60a80fc25195c053cffae
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:25:43 +0000

    run amount amount mess above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
