9d17ed3f5f14e41a272d322390838ddd1ff76a72
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:05:30 +0000

    exact history equal visit shake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def add_numbers(a, b):
+        return a + b
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
