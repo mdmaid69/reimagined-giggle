9c13b9d7eafec7c8d5dc8bed479f5da720678f4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:22:20 +0000

    post square border fold past

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
