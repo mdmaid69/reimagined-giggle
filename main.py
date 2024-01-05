d5dcb3a68a9c1439441ad119e12f375f26dfbebb
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:46:38 +0000

    unhappy try characteristic own replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
