120a30b582a55d2362b0df11e7d795c81cec3893
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:58:16 +0000

    ride joke otherwise respect body

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-name = "Python"
-print("Hello,", name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
