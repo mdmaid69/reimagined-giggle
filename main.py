f91c4ec919f82d9c6ad627584cb3feda12b439f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:04:21 +0000

    tap grand fit firm exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
