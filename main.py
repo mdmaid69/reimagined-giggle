faeea39f369f94613223dc5419852dcdee46f72a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:36:43 +0000

    lean accidentally suit odd wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def reverse_array(array):
+        array.reverse()
