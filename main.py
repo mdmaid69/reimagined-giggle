35a73937e4395e573f446229dd20425b34fa9c1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:06:39 +0000

    hello acceptable fierce force draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
