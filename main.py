7f51cb4d08009d2ceb523b7b1cc3d86b98b21875
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:30:11 +0000

    winter careful apply concerned serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
