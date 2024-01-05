b32b7b524d311379ffc12ae73605d00a258ce811
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:08:45 +0000

    kind coat but welcome sort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_base_name(path):
+        return os.path.basename(path)
