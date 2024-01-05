e07e57744471e554c36c4b6eb0055f9c72c5b8f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:09:54 +0000

    hurt support contain since wait

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
