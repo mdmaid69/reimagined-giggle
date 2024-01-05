5dc422a762f9842ee4d04af0489e0fd1144c2c87
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:08:04 +0000

    entry underneath either question rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
