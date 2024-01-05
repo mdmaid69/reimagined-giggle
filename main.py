647c5ea6db72604e72b4470718fb519ea6367e0a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:28:52 +0000

    back ear tune form clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
