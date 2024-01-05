8b40d0f6789c4645ab6a2110a0b343a2228c7497
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:05:23 +0000

    leave large shock black relative

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 def calculate_area_triangle(b, h):
         return 0.5 * b * h
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
