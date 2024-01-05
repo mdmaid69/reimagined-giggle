21a3df6bddc0a6f9929552b776cdd9d13c8c5013
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:51:46 +0000

    taste suggest conduct anxious tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
