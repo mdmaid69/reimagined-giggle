a8a9726c8e267472834417ac5a1356a794a2e585
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:18:56 +0000

    opposite action necessary anybody vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
