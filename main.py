1c0b3164ee4b50f9853735d052b8b062f550619e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:07:55 +0000

    error whatever ilovedicks or birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_string():
+        return collections.UserString()
