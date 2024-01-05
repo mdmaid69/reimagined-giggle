738dd808f2e3e99a7f8aadeffce16d66e494b1d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:42:29 +0000

    include coffee deal hide turnon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_list(array):
-        return list(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
