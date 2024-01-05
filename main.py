579ff00af7e59d72b13d56d20a5c4669ebba9a57
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:48:31 +0000

    ask score offer tend sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
