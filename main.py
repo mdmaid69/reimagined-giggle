bdebb40105d9f32feb24940f382e1a34ddfdf6d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:39:27 +0000

    continue over question look small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def count_words(sentence):
-        return len(sentence.split())
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
