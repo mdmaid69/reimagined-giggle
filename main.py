809946bcbbea89cf548d704c63295df76fabf4a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:50:47 +0000

    question fond quickly invitation period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
