d3f82c95085e2288208dc4edf849214a68e6eccf
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:49:44 +0000

    circumstance fire noisy break mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_max(numbers):
+        return max(numbers)
