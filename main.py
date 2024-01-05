5d139c9953b315c542196b5c14d142e73fe7caf9
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:02:42 +0000

    recently connection feed agree walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def remove_from_array(array, item):
-        array.remove(item)
