a57e47957769ea5c0aaabc74db3c0e8db365306d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:06:54 +0000

    form skill answer trap fill

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
