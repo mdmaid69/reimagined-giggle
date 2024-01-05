59a1d52e70150ae4665ebe23b7c46ff90a3a2aaf
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:52:48 +0000

    squeeze use quit giveup quantity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_speed(distance, time):
+        return distance / time
