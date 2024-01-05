f8b24e251e97f06a24bee6ce545dbae4fd45148f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:06:30 +0000

    suddenly understand mean occur nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
