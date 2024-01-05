313257e77b109f9395416324c2c013e72d2dc277
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:46:18 +0000

    plastic during attitude giveout manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-name = "Python"
-print("Hello,", name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
