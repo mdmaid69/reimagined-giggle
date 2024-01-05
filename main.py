14be7036e0ea85b9e47af6fb415bcac121603e60
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:06:21 +0000

    Kk rule each insist light

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def square_number(x):
-        return x**2
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def convert_to_binary(n):
+        return bin(n)
