c9428fe5ea11db435d2a8ab6b394b0966e5b28f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:03:38 +0000

    impressed here successful visit valuable

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
