ee0c92e504c8921cc965d6fcde706ffbb636695b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:54:51 +0000

    base moon sweep normal do

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
