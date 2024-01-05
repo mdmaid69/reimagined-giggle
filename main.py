e4e5dd1664ab7b37c4731736e7c599375f6812c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:52:53 +0000

    post count generous hold while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
