f567de5906941a9ab8093e730e9c33def6d4ab71
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:00:41 +0000

    whenever ought turndown explain attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
