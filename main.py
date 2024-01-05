ba764bd99b9ab7cc01c1536df4eba62917cebc44
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:25:52 +0000

    official worse fast achieve once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
