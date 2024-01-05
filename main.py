99ef06117afe8b4195e8b4a36df736b510260e9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:17:42 +0000

    roll sorry compare blame statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def subtract_numbers(x, y):
+        return x - y
