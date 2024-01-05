4dfda9b5b0760b4c4dcc12b966ae7eebf5739df7
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:05:45 +0000

    geton top some Tt cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
