cba6c0b738c253004be0f06fd090db4e9b87656e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:03:54 +0000

    follow each risk needle guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def append_to_array(array, item):
+        array.append(item)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
