67a6ec57421882a136fd60077aa3d6d67ed7af18
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:37:09 +0000

    glad opinion tone crime black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
