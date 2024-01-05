e353dcd129edb955cb4eea7ac950c8b95bc74593
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:32:38 +0000

    drive turnup strike constantly in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
