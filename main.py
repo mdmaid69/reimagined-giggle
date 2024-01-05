6c85d50a784753dc355fb045f883c53e3bf33258
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:07:41 +0000

    complaint familiar several giveout bringback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
