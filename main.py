9535294c562ff4f421bfe0b7f88860bb1ea598a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:45:31 +0000

    what reserve worried especially tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import random
+def roll_die():
+        return random.randint(1, 6)
