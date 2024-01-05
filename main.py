be4b6f81dc2f00bb3d1189a5a6a54850e11b61e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:32:52 +0000

    callback history stick Cc take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def convert_to_hex(n):
+        return hex(n)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
