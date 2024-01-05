442fc4d3e37673e4db627a636e6b365cda8d4049
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:25 +0000

    shadow damage pupil thought remark

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
