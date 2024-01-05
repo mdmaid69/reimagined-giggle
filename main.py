1c2601fccf6b81cda28f924dc4686a60449947f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:30:45 +0000

    conduct finish look rubbish far

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
