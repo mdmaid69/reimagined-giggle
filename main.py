bfbcdc0e2f3d3332d6a67c2a2287de554f870e04
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:11:50 +0000

    push period sweet thinkover leave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
