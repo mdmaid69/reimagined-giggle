a437fd91f67e214f581f02eafc9e7dd0cf637bba
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:51:53 +0000

    aim connect square wave represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def add_numbers(x, y):
-        return x + y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
