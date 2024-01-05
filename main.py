9a6bd4afb9e00df0cd80d77b353a3f7336716853
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:03:09 +0000

    takeaway bored high straight terrible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
