84483f922aa936956ee2dd4717f3bbf465d69715
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:22:32 +0000

    crash sudden release exhibition his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
