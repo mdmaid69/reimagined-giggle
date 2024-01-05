4c034ed560d7dd3e6dae9d6146e8f611d50d0f3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:18:04 +0000

    left during breath thinkover convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
