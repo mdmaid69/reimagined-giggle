823d3f7b5d956b4f575a70ef2bb3bb528d4b0fba
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:34:33 +0000

    reach or count write slightly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_acceleration(speed, time):
+        return speed / time
+def find_max(numbers):
+        return max(numbers)
