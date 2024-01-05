ef1de32a3f813fe70a608aff2dd3dc44e214063e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:43:19 +0000

    argue so opinion question it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def square_number(x):
-        return x**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_rectangle(l, w):
+        return l * w
