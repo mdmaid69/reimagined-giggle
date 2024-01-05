4ab2165835a9e42f254d51c0d3d35f99684a2f31
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:25:24 +0000

    pause serious discovery should turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
