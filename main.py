3a76b9da597e94392d33f6d5a5975fef51d63adc
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:15:37 +0000

    runover show declare risk practical

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
