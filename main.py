5a761568a0ec0ab5da86d2a696d32684606bea51
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:38:23 +0000

    correct clearly afford closed tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
