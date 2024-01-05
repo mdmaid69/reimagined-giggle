d43b767095678952ef2353ae555687d15d1416c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:14:23 +0000

    complaint keepfrom care bringback town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
