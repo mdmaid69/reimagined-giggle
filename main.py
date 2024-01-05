009b0bdbf3807d010ab02b3ba55aea637eb5afe2
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:13 +0000

    dinner side some build into

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-print(math.pi)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_string():
+        return collections.UserString()
