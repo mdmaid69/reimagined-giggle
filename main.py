ef33578a34f49073be5ce95f5c0e49cda4ece7cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:06:19 +0000

    evidence sweet as thanks work

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import tensorflow as tf
-print(tf.__version__)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
