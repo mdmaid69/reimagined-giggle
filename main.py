9870e15d73ead27850746fd54036926a1f051b15
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:26:37 +0000

    angry famous twist male fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
