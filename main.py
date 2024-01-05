f6fcebcb67a109670336314c3abac808da154525
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:58:43 +0000

    there similar justice someone ordinary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
