d135054edfd9fb383faeabbf650862878deba3ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:30:35 +0000

    child nurse mistake thick worry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_directory():
+        return os.getcwd()
