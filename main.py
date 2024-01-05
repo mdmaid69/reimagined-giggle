6f0a6a0dbaf77aefac691e9a5de2153c42affb61
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:03:13 +0000

    strict them yours badly introduce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_str(array):
+        return str(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
