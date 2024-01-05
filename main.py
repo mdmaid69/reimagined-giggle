e76f93818d2a80c7c275f924116a9d905191b30c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:25:48 +0000

    laugh aware by risk comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
