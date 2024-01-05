196117b475a3953f75c0ad9a3f7db63e51beb61d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:58:14 +0000

    weather father comeup regularly along

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
