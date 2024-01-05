d8d1d3db92f97c0fbfef4bc025b762757830ee8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:47:02 +0000

    near clue black well cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_float(array):
+        return float(array[0])
