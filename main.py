3dea9818a602475de0600f879841198b3fb52411
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:08:58 +0000

    concentration record confidence himself hide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import sys
+def exit_program():
+        sys.exit()
