55e5fff13d90ceb4cb153a196a335bbb8513fa7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:47:26 +0000

    murder deal old clear church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
