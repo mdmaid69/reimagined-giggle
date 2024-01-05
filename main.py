43423ba65996e4d0aed9f6a479a61301f1138324
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:53:47 +0000

    union fire fault middle precise

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_user_list():
+        return collections.UserList()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_item_count(array, item):
+        return array.count(item)
