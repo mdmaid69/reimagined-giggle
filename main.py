a3c923095dd386a3cc50ae0dd8a40ef10a186f85
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:24:38 +0000

    bringback intelligent duty do capital

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
