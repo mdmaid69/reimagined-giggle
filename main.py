2e1c159c1856111e9176572c54f9aa33af153547
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:04:34 +0000

    unusual ride dig schedule secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
