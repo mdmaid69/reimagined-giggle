ad4f99c9eeabd1aa58921ae38ad209556507a30c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:36:49 +0000

    frame force carry truth gather

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def get_array_item(array, i):
-        return array[i]
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
