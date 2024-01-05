aecbfc55b8d0e37e18e7c787750bbf94180616df
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:54:43 +0000

    clear shoot spread winter exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
