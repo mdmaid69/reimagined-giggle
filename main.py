e69698e97f4a85e88100f84a033d31c6b72b45d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:37:29 +0000

    necessary traffic keen convincing communicate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(10): print(i)
