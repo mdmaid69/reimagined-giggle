62e55a777cb52e37201bee3d378ea1d9839e7784
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:42:57 +0000

    comfortable begin section amount back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
