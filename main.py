0b9c0161b636d6ecfd8c74054d94efe576bf6052
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:12:24 +0000

    bad middle shape protection broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
