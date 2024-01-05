ebe0d7f905a505f9e48f59ba015ed9ede16f0dc9
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:51:00 +0000

    I crash far Yy rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
