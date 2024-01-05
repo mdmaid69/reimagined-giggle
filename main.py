b781dd84c60aa82a85b43f096cbc3bac69557633
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:33:53 +0000

    strike guess interruption spring connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
