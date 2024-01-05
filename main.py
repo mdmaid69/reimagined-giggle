bf1139c3c6be09adf94a0f681c34594869ed6ae3
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:52:32 +0000

    grow whatever up fetch laugh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import numpy as np
+print(np.array([1, 2, 3]))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
