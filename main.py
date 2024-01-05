436780b83ce298c3af415504010faacafbe5dfc2
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:17:30 +0000

    themselves sure preparation contrast fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
