ddd9a658fb671c2f972e9c6095eb2f7b3e8f5282
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:48:09 +0000

    ashamed worth dead listen common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_work(force, distance):
+        return force * distance
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
