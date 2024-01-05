1e9be91f9669e8e34262cd3013e7a28ca9659d54
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:35:36 +0000

    complete ever crime by afraid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def find_max(lst):
-        return max(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
