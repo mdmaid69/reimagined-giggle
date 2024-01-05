8a3eab98fd796f4ccc008c2e0173dc43a481b8f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:16:19 +0000

    match sleep convincing wipe we

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
