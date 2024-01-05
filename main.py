a4168dbdae2f6c73f23072449db0843e1ffa13b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:52:22 +0000

    keepout further push score state

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_pressure(force, area):
+        return force / area
