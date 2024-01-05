01cd16210dda067ba366d128247a59f24bbef5e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:47:41 +0000

    trousers formal look very excited

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
