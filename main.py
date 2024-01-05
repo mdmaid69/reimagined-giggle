560f5d5e708eac6fef8e29a83081a1af0cab11f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:43:33 +0000

    nor clothes every square keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
