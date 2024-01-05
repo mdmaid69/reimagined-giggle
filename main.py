21e972b764ffe56a15810929c0ea9c4a7a8ff31b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:51:28 +0000

    range exact soon next control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
