f427f9ec251a55a4c098422e5979625ba7c95bf1
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:07:21 +0000

    each operation offence breakthrough estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def sort_list(lst):
+        return sorted(lst)
