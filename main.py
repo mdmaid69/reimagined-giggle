7a43a5c41cc33df757a1751cf56e0dbdbfb9cd32
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:52:43 +0000

    possible float computer destroy pot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_area_rectangle(l, w):
+        return l * w
