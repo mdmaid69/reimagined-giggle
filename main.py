2681855fc7a0c3e5b6cf0c5f7923836f66abc015
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:00:02 +0000

    themselves tragedy violence question male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
