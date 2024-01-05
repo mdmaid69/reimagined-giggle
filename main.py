7c963e5c80fb0d29fca5c300ffe280627e540138
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:06:48 +0000

    fence somewhere house 566end absolutely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def cube_number(x):
+        return x**3
+def calculate_volume(length, width, height):
+        return length * width * height
