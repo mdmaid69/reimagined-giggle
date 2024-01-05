724a685fdf12552fa7631bb559fd7e2ce68bb78a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:32:50 +0000

    Uu cause soil who interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
