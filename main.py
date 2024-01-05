24ef90c09785444a6978c5e95f02410131ffd7de
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:17:06 +0000

    rely learn bang far spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
