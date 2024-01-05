c3bc51ed04ad4763f120f87182acecb667c0a712
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:39:01 +0000

    design sore back pass new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
