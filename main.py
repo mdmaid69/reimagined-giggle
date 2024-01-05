5a5ba5097db1e4436f4b1643b16ea1f576588968
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:53:09 +0000

    still politics hard certain peace

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-print(math.pi)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
