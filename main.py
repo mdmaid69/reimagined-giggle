554dbdd070e21e2b2146f9c038e9de12c8534f3a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:53:56 +0000

    near ride instant impatient against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
