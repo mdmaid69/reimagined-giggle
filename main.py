a61469b5b63ed1848c58ecdd92864d77d24ba20e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:51:06 +0000

    home regard cry shop workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
