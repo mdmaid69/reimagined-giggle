b3ccc677f7cdcc8309cf1caa6b0d984f0b1e10e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:19:50 +0000

    everybody form smile coffee service

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def find_min(numbers):
-        return min(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
