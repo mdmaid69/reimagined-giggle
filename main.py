08614da33cef80609940dd77c3dc0f4e2b1ae00e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:43:59 +0000

    emphasize comeround mistake world commit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_pressure(force, area):
+        return force / area
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
