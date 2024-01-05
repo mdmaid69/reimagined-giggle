2bf40eecdf6db5ef86985c50a1b3d763fef99234
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:41:42 +0000

    regional thankyou obtain speed test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def cube_number(x):
+        return x**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
