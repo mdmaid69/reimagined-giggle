82c1f25fc1924c4bdd8fd58dc4f55fc4bc46132b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:29:18 +0000

    degree build scheme anything suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
