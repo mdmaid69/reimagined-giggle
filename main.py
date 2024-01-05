11b597fe730adad5913ad4c6f919536ae148525f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:05:46 +0000

    yet ours getout along effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
