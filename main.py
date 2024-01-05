410c1fbd8309c4c31620e204353f4e951e045d00
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:01:31 +0000

    extremely basis past thinkof much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
