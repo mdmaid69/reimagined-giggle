d83595a5a8dab172526584f105ba0e190033a2c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:52:39 +0000

    wild original runinto region final

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
