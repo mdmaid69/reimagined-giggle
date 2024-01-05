a6b813ca9256d7b2d01f93dce080aae823930196
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:28:10 +0000

    major reasonable bad breath congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
