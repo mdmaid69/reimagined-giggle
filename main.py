8de0b00cdd37807ac7c924aac06e40aea2e0a93f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:13:58 +0000

    thumb reject bear collect right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
