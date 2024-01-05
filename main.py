8d6b2ad5a1ca5ae3b62a2fdb0303b5ea5afc47be
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:35:34 +0000

    gas vanish school win pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-name = "Python"
-print("Hello,", name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
