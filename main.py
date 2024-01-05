6c04497ead0ef5c1c1a293a039a1d3cd437421e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:03:12 +0000

    press either passage seat soil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
