a19dba5dcd5ee9260c2731dc2eb0508ca49b7561
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:15:53 +0000

    quit spite laugh calm house

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
