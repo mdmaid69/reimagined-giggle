9c6ddc26147e8ff0b6cdd50a6489466f602c9cf9
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:16:52 +0000

    record sensitive exist belief private

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
