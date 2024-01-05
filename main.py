10a106fdf2959373c584c1123531278c74a4e3d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:29 +0000

    miss pity suit comeup thinkof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
