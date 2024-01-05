2e33eb6d3c67ff08c251899633916d29b43497b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:20:34 +0000

    introduction operation weak this earth

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
