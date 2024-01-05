586c98f88f23b40343e18cd075c21dabb4f216c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:52:08 +0000

    steer valuable war necessary exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
