20a5ed5afb9395cfcbc9574ca48cbc187839a0f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:23:25 +0000

    soil art dream shop slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
