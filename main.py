e09fc5a96119c07f1124a5a4c8e618971c199b58
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:53:56 +0000

    close exercise last favourite Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
