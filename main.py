58c7b0bd6276d786c29173f902b4b7660ff0e762
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:17:08 +0000

    instead principle shut work swing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+for i in range(5):
+        print(i)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
