6ab9864ec0e23ea990c137214ca902f471517480
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:09:01 +0000

    wet ashamed fall rarely convinced

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+name = "Python"
+print("Hello,", name)
