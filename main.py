b424b2ed4147ead0ed8ba917e1be8a3cc79313c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:23:11 +0000

    regional upstairs bound right delight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
