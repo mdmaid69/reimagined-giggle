ab0e571b44020dbefd2484624a377bc6b773ec00
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:19:37 +0000

    small government cheat suit measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
