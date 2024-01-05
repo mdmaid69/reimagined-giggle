795f9e887c53e2116fe7e2d363ecf85c583c31e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:41:16 +0000

    takeover juice turnout climb ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
 import array
 def get_array_as_format(array, format_spec):
         return format(array, format_spec)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
