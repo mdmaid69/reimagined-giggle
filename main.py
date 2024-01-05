667fcdb3029158bae3755bb4b5eee7cc5dedf1e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:36:29 +0000

    joke serious feature sister man

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
