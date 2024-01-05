1b0e18be2fd074dd96dfd91663f627c1804b33ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:19:10 +0000

    phone explain rain head check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
