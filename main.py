cd786f7a2e96ae80e5ee2ee5c11bf18df665d4fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:39:39 +0000

    clearly Nn profit argue trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
