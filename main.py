b4b15eef3354b44247bba01c353a290a0318036c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:00:17 +0000

    trip about slide gooff dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
