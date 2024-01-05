c57e52b9a72ea359d373730c36d3f9a17e8b7e43
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:28:50 +0000

    broad glass soak wide appear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-for i in range(10): print(i)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_power(work, time):
+        return work / time
