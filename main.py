1091395957bcd93b23b56f5bdb242cb209f930f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:42:37 +0000

    key nearly insist minor broad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
