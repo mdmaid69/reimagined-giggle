958d3668d4748b18c0ed6885dda8c7bcfaa20ed0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:55:13 +0000

    refer touch aim spite give

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
