5054412f1533c0ba1566315adec67215545a22b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:18:42 +0000

    once generous 568energy design calmly

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
