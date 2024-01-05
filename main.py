9becd16f7b404837f40c605b5b40fb42d93a73fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:28:04 +0000

    please warn smart excuse grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
