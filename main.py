f358e330e7ff4e3d1878f329cc8a7428d29a1851
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:15:39 +0000

    fork I press actual plant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
