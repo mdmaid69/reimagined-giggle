b78aa1ddc0ce413e4df4a525ffc1ea194c1210c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:01:33 +0000

    male wind indeed what thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
