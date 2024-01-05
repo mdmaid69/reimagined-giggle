96c025bf62e39ff6af4502b66f37864a6da5f73b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:17:51 +0000

    peaceful Hh law hurry accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def divide_numbers(x, y):
+        return x / y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
