034af5691d5495e14d620076bd01ea3e5581ff62
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:30:38 +0000

    bringup whistle admit most explain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
