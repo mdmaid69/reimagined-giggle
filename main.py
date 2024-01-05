be9866b279b9ecf411fb788ceaac50f93bcc06d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:14:24 +0000

    attempt obtain university second drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import sys
+  def get_python_version():
+        return sys.version
