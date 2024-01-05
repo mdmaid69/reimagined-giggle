7427fee2817d04c193fb4a6feba5bf8a508759d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:52:38 +0000

    see operate sorry unkind event

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
