d59dee68230133b3344b71fc8e38a18d3ca98a1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:11:18 +0000

    strict try Itself dull expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
