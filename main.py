6ccf80db1f9ecd90832215636772a40e5014a306
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:12:25 +0000

    creature refer file spite scrape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
