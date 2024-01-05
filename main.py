51deb5253303d204e8010f774486d2fcac394fc5
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:47:10 +0000

    conclusion dish cover deal become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
