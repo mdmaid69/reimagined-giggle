d69e4bac2323e93db44acfdd5083378f57d2ca80
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:57:39 +0000

    plain lonely greeting pause however

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_int(array):
+        return int(array[0])
