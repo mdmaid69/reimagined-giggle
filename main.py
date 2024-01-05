00e3aa5594eeb853d00c831dc2f77e0a42807d5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:25:34 +0000

    message square light control sleep

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+print(os.getcwd())
