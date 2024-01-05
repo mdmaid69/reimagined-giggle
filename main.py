2ceeb0148148537a5e70a58cfec5441c510ef6a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:23:59 +0000

    contents consult apart somewhere brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
