5652d8ff9d6b0f0fc5e5d8339f074221ab2f395a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:44:17 +0000

    hair habit relation oil check

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(a, b):
-        return a + b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
