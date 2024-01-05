9811b80cd34625338c7910d67ea085a56ef73342
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:50:42 +0000

    choose almost prefer bright tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def convert_to_binary(n):
+        return bin(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
