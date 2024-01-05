4d0bde375e9492c86c5271697428e4bf9c0ac5e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:02:40 +0000

    peace Gg turninto fire order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
