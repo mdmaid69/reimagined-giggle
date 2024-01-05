d6c02be0c814600dd0915a619ee98bbd0b0020fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:04:00 +0000

    limit practise coffee evidence side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
