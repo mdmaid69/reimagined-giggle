102a0773aa49805c98ce4dbd5532693bd762a3e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:14:10 +0000

    regret familiar circumstance similar vision

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
