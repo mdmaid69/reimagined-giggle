be742aefae13ce30763c8ffe7eaf8bd052d4f4b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:59:03 +0000

    cap hospital polite term stick

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
