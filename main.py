8fc9c106a26a7aa70ddaaf2c80f2051be5d60e73
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:04:51 +0000

    pour search join because steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
