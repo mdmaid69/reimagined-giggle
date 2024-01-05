50dc4a36c75379a4d07a3f2bcbf4010c6d9863ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:36:57 +0000

    cause deserve estimate ordinary organization

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
