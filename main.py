7854859789def66bd8a389e1fd850c7c27b305f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:11:46 +0000

    disturbing space night pure hang

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
