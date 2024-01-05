63865bec1c80837f0ea9ce78a1e2da167e1aee97
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:10 +0000

    shut able wander approval congratulations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
