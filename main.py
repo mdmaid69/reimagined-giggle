c13be3d8170633c85d5dad2f7d46dd641e66176e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:55:24 +0000

    onto few week either breakup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def calculate_area_rectangle(l, w):
+        return l * w
