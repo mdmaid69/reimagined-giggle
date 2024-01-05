38e39fa95b2345b783ae8ce595ecbbfb1042e820
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:10:48 +0000

    fast still dull goon frame

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import platform
+def get_os_info():
+        return platform.uname()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
