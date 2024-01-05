7a0cdcb5d2c81ba41d9eed58dcc4221a88edb9a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:13:05 +0000

    herself notice ashamed popular bother

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
