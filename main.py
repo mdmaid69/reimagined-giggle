f26af461f07bc2a3a9d712b5dcea6976d8d309ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:33:16 +0000

    quarter passage since beginning ago

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
