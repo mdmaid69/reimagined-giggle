05e52de51491f4c7d1b3ae7215989000fce34a03
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:39:33 +0000

    unit sensible high must world

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
