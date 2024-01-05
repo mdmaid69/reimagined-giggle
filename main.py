2e85805010ae3fc2d93f677e4eda135111b55af8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:30:00 +0000

    complete ear ago brother frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
