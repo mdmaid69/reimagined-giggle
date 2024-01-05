edd6f9c327ac234be0eb60e4d9ae4d86b7461164
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:31:52 +0000

    poor cope in word neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_logarithm_of_gamma_function(x):
         return math.lgamma(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
