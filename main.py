f7217ae559ece99ee8bebc419526e1cf509c2174
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:03:06 +0000

    holdup lot Dd rent dream

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def convert_to_hex(n):
-        return hex(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
