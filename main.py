e7e1d963ebc76fbce4cff52ed7b34087c959aa93
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:11:37 +0000

    majority exactly either nothing empty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
+  def reverse_list(lst):
+        return lst[::-1]
