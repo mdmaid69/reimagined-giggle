e78cb834392061914b4bac54e44af1d8b48dacf1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:25:07 +0000

    tool feed fire frighten travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
