66d419d3a43c33694c2423f7357573ee97ed77f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:45:48 +0000

    violence contain now bit impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def reverse_list(lst):
-        return lst[::-1]
+def reverse_array(array):
+        array.reverse()
