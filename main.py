cd93b22dd0eefa4d788dbd0d14e93462029eee7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:07:17 +0000

    rush hello deep obtain when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_area_rectangle(l, w):
+        return l * w
+import getpass
+def get_username():
+        return getpass.getuser()
