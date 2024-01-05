03fdcdf2ab56764d6ad7f05be58be8ea2c6e3982
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:01:41 +0000

    against rid computer shout space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
