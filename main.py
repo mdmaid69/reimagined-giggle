777452000171db2fc650895bc4af185ad9af7657
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:32:10 +0000

    name spoil tune common scene

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def subtract_numbers(x, y):
+        return x - y
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
