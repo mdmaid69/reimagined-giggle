46d33995dd7a1ae52bbb3d962dff962524552baf
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:26:15 +0000

    exaggerate shut behind empty Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perpetuity(payment, rate):
+        return payment / rate
