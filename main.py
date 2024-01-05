887cd4c6d2d1fab6ef0989496ae1392a430cf5c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:35:47 +0000

    definite floor flat temperature collection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import collections
+def create_counter():
+        return collections.Counter()
