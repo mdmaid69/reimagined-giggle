df1179d67b2b85c7cc8e3b58f24a2cd9bb29357b
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:08:16 +0000

    sign spot water cheat town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
