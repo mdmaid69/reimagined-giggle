1c7fd2c94a8d7784f1cabfb3a3fc37bca5700dd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:39:31 +0000

    border available serious apply chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
