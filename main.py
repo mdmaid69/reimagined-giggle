d32bbed90866c07578db5562e90218ee04e255e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:36:50 +0000

    thumb so importance sing soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
