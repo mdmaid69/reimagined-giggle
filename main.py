5224aecf7537952dd76a1b86e75be8c7cf2c9193
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:18:17 +0000

    push period coat law sore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def convert_to_hex(n):
+        return hex(n)
