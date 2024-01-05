d332d8f9bb62cb08a7d37522c72b0d294040a9f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:33:00 +0000

    rely bang education schedule drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
