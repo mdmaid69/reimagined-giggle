1d7b30e90bfb3f6997a1492db867aef97a64287a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:58:59 +0000

    cold nose seem start wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def is_even(n):
+        return n % 2 == 0
