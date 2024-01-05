e23f15c594a5324be46941017def83bd527f5921
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:27:03 +0000

    hers hope cum fear man

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_exponential(x):
+        return math.exp(x)
+  def subtract_numbers(x, y):
+        return x - y
