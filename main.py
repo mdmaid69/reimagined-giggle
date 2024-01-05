d81f727902016b51a518569a6baf039c9bcc17f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:56:56 +0000

    repeat during unlucky direct score

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-print([x**2 for x in range(10)])
+def add_numbers(x, y):
+        return x + y
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
