127bd94e72775fa27c5faf78b84c2c3d14e070c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:05:10 +0000

    satisfied slide after offence sea

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_work(force, distance):
+        return force * distance
