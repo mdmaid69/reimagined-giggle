cffcb21a737fa97aba7eae3be7f5faf3b6fd0429
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:24:14 +0000

    positive challenge welcome skill false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
