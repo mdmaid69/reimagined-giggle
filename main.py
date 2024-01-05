c7859a7feaea56007e1ffe218f7d03b4cfd4bb22
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:13:53 +0000

    himself trip keep middle pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
