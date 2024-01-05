84490083d2286e5111d491afab0f29e5d0eb0585
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:50 +0000

    murder pronunciation consult if absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
