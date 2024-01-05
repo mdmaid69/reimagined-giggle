7a1f72f6255770002c7ed284607d9122681bb61a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:32:24 +0000

    birthday sure notice shut deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
