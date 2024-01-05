622bcdcb7fdf91389ecd8a023027a8a56329d4c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:24:01 +0000

    movement belief free luck opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
