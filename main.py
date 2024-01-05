638a99b97af1beb2f220033b643b41237f3e68bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:31:21 +0000

    squeeze characteristic difficulty maximum lay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import tensorflow as tf
+print(tf.__version__)
+def is_odd(n):
+        return n % 2 != 0
