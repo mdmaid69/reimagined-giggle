ba73611724510ce02f7cca89207775497bda9b66
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:19:39 +0000

    formal hurt speed try pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
