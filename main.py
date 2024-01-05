0fe848e566abddb06b87910b81558c99506f4a9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:13:39 +0000

    himself bang variety circumstance obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
