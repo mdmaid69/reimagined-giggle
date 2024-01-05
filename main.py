85115ddf8fb6341abfbefa0642a38a78203d5b26
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:38:42 +0000

    drink ordinary couple form rid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
