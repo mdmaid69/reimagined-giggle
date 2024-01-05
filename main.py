606428f61d51d9e9c356c9de4d4037ad456e8074
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:14:35 +0000

    skill gap else leave sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import functools
 def memoize(func):
         cache = {}
