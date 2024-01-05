fa06f84c86db75758f30eb8f6ede58c5c8835538
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:42:28 +0000

    strict accident reasonable limit send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
