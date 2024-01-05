b4f2e3e067c6389bb6e404ba9e0122118f960135
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:30:19 +0000

    inquire defend shock dark trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
