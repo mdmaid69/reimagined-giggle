17ecec5c90727a95ab716ebf83ba302bb165810d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:22:49 +0000

    guilty of you history completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
