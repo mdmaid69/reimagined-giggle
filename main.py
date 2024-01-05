eddd40938fce11f04550c4f4c0ab592dcdca05b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:31:48 +0000

    drive mind of speak Qq

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_itemsize(array):
+        return array.itemsize
