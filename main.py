50c3c754a77f6478ce2a25a86f8c149514c9d8c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:28:42 +0000

    town street direct afraid soft

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
