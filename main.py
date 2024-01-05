cc2a5c7ce2526fb3bad978e97ed0068695830648
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:26:07 +0000

    bang pen better wire juice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_power(work, time):
-        return work / time
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
