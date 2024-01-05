c574c2e28480cc0251e6e18231d33e68e3c55831
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:42:54 +0000

    deliberately bet quiet wrap single

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(5):
+        print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
