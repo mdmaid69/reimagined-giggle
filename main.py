b936e845666ee39a93cb3b6e91642898da7594f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:56:27 +0000

    takeover giveback pinch stage power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
