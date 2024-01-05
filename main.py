444d7a2e81fe1374f3e9b0f7435687231f29c1ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:23:28 +0000

    shame otherwise tragedy makeinto boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
