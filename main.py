479f7d2b8ffed699f3020fcae5a3497560ed86cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:49:09 +0000

    inquire dish door arrive correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
