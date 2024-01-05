e3c26e07375ea6ae389434720fd6d67c20135fd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:40 +0000

    personal regard doubt mixture choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
