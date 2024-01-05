3ba909fc9b4c774de8d408d0537b8a8205062dc7
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:37:25 +0000

    only definite less branch place

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_speed(distance, time):
-        return distance / time
+import collections
+def create_user_list():
+        return collections.UserList()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
