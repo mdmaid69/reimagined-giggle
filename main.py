d4e2ee533289510fd0fe22884572718605031db9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:21:04 +0000

    rush reply work sort degree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
