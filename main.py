239448efc9bc5cb0d5cd4b1cee8f6ae1613ad727
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:32 +0000

    wide married interesting understand concern

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
