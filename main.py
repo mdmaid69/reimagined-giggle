0cb5c9bd6702b26017c8620b6e4c58e9c5b5caf8
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:00:19 +0000

    cough doctor breakin reaction base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
