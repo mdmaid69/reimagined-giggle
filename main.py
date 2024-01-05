506a1cf262266ff9b6b7d00c9ddb3845f6ccf61a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:31:49 +0000

    service window woman peaceful bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
