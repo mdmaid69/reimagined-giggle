311d98fba2df794c7093180c38f203746484ee3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:38:46 +0000

    hello you lead kick geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
