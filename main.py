8c7a75c83c3783aecb4ea8725832d5c6376e1b78
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:56:00 +0000

    cross circle frightened bill impatient

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
