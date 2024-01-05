3b8152f1e34858d4feffc1e301e0296a9a848850
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:50:03 +0000

    even calloff drink Cc scene

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
