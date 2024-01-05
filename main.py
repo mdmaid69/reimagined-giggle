cf50dae4db8aa9fb8377743fe2a6117f48b1231a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:34:18 +0000

    war snow comfort port knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_area_circle(r):
+        return 3.14 * r**2
