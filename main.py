9614c8678a5495fda9db8fa602e25f5bb477a642
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:27:45 +0000

    increase male intelligent dangerous estimate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
