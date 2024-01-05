5069e5afe2fd9a300831b7cb357b7a676a7c4b62
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:26:26 +0000

    public believe embarrassed run provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
