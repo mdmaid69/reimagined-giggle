7339c9cfeff2ac1fc6d2ce7cdb2e7f5b0353bcbf
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:18:51 +0000

    doctor regular country instead eye

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
