9112a408decd13e4b58978897d746c1c54c0ca46
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:35:48 +0000

    getoff whom teach news basically

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-def is_palindrome(s):
-        return s == s[::-1]
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
