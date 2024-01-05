46ea00657e4f9452086642a3947ea249061e5cae
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:01:11 +0000

    problem foot distant dinner judge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def remove_duplicates(lst):
+        return list(set(lst))
