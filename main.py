72a87ac063ea5044d13018bce08577cdaa60d0c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:00:21 +0000

    room mistake although key cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
