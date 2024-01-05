c07a802e99a060b7d554eddac03d6eedba5030a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:35 +0000

    thick workupto care last raise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
