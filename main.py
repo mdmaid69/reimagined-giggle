54509afb21207081cd0e360afcd031bd8a21135f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:25:38 +0000

    should hit belong bag earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
