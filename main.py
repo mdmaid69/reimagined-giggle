665672af35af749dd9d7c2f53a6e056d657ade5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:02:36 +0000

    list start sentence proud me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
