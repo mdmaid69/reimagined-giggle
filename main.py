5c16ecc3fa2a93249508e9c65cb6b8576fa5fa5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:28:38 +0000

    unusual behind welcome top search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import sys
-  def get_python_version():
-        return sys.version
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def split_path(path):
+        return os.path.split(path)
