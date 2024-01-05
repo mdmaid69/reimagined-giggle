2e67180c0c1db3e621651f07cef90866fd817b59
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:17:24 +0000

    dance survive combination bang gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
