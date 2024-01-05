c8c4d9bb97c60c7814df37c8648e89d019b248d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:26:06 +0000

    drawing rush house bite protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
