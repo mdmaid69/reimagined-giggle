86a80ba463bef005df8d11736c4c94301c83e578
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:58 +0000

    equal you fairly whisper nurse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
