cc74366b4b86629fc3910073ac0843e6d7ff52e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:23:09 +0000

    pleasure during recent dare improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
