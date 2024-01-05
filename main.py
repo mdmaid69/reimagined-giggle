1be5e65cdc3b2a437e0823f82df1084340f19b31
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:09:55 +0000

    disturb put boil fork about

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
