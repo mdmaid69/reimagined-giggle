4aa6d1e5ce0946a660855ce27dc660422d0d78ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:40 +0000

    study party almost height go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
