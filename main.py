4c1e22b4b8fca4088ec285c72cf52bea8a127f19
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:20:19 +0000

    comedown whom long main fear

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
