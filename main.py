e6ecfb8da042c4ee4d004fed317b7b1124ace177
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:57:29 +0000

    live swimming how rain closed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def calculate_power(work, time):
-        return work / time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
