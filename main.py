bd80541b8aef80d0b190050ac70ce10bf1942f3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:21:23 +0000

    female rain matter tradition cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
