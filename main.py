add8f364e04964f4420c5390bc8bd9bf947a1f38
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:31 +0000

    myself shut regret split post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_acceleration(speed, time):
+        return speed / time
