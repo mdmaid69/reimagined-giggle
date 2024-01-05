d7fbbac2b17b05a1bd212745e1d02a60db3d2ba3
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:39:35 +0000

    factory image provide declare bend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
