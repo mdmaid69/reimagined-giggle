c7d8a76960f5d171b0f7db9a4d9e39cea78029a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:50:35 +0000

    damage before look intend tip

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
