b1ba7dff9269ea526c29974ee28a2282c4cea8a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:05:05 +0000

    compare around black share happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
