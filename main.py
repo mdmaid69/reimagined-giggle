937f703b8ecae250c3fde7ca0d14cda947cde70f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:56:41 +0000

    break come bye behind flat

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_index(array, item):
+        return array.index(item)
