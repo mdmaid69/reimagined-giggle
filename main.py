dbebdf44846fad325895754487f1623a4b048f5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:19:26 +0000

    believe wet accidentally connection free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
