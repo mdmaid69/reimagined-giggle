64bf25b96c672ad64112aae12e7e4c1ca025db2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:52:24 +0000

    leave female and war protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
