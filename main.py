5756d2b8e60b6cd211f87fc363e2c1d369e61863
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:20:51 +0000

    plastic repeat earn makeinto change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
