ddccecb447292a626f0f70e483670d877b178701
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:11:15 +0000

    boring operate experiment best offer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import time
-def get_current_time():
-        return time.ctime()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
