9b1450d4697245b4f5d4380ceaf7509e55c3746f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:13:06 +0000

    wall nature serve than range

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
