c5320c0201944eb6fdee5010bd0d5e4443b339ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:30:49 +0000

    dinner holdon write science plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def reverse_list(lst):
-        return lst[::-1]
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
