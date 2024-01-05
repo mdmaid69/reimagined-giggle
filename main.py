622970324f9b2f4143de009de1eba06ce6ce0528
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:06:09 +0000

    though tax little goout grade

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def square_number(x):
-        return x**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
