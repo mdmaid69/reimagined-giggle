30a773ef9bbbd11e0b52fb6500543dc27e316db3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:01:46 +0000

    depend look plain tear flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
