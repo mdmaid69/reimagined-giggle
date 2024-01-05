b4faa9cd1680513fed01bcee7eda1434af9e7b88
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:50:41 +0000

    poison connect inside unusual at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
