b7383efe9f87b41be1d01cfbd30949a4cf74ecd3
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:56:08 +0000

    pull star breakinto demonstrate undo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
+def divide_numbers(x, y):
+        return x / y
