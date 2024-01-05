fade0305f6a12638c8f1564709620af328a4d8a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:04:27 +0000

    sail discover wing murder instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
