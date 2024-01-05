ceb08b07f43fe4a5522fe087bb60a4c4dc051277
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:00 +0000

    holdout appeal quality stick task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_sine(x):
+        return math.sin(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
