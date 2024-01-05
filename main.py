dd01c14d3227e8e649ed979f5a15eb99684add1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:35:26 +0000

    typical makeup recognize form either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
