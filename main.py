cd5e4f85dcf1b84404489de506891bb0bd08235d
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:38:29 +0000

    slight squeeze offence port scare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
