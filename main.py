92db8ee4f5de35ae7bd9cfdb8844faa6f5926700
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:20:01 +0000

    nerve drink branch science night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
