efc0498f5dd67a8001406fc75b3cbf476481aba3
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:36:29 +0000

    whichever avoid left blame smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
