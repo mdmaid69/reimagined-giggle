aee6864ff9508c806dafb0ecb102fe54d6dfb391
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:39:03 +0000

    final software shut swim chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_set(array):
+        return set(array)
