8a7e07f5b2b3f4e58ab62ef2dfaa842179da11bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:25:28 +0000

    fault within broad supply drink

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import collections
+def create_stack():
+        return collections.deque()
