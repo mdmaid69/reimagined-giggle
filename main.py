75cd5deda4b39de3a417b3236656fa34eb74a03d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:19:48 +0000

    future passage arrival content Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
