1b0256feaaa50fd52ffd2d3593e27b3bfa37033e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:55:31 +0000

    cold us great Ff something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def is_odd(n):
+        return n % 2 != 0
