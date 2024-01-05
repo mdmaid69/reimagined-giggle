cddc704b589a23ad37625c683c07da9cbe2f7095
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:52:49 +0000

    any suddenly central rescue cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
