f15cdaed0abc6dbe502cfcbe9ac9794bdfb438df
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:27:54 +0000

    careless turnup mistake condition centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
