bd5a36d21a36db2c3a6f6839f59833e6996cc343
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:41:22 +0000

    spin attention string takeout goodbye

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_acceleration(speed, time):
+        return speed / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
