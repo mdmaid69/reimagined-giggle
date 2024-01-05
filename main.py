753d8f1007e77ec9b1ea94bc8c09c0c21f9dde07
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:06:27 +0000

    fight convinced harm doubt black

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
