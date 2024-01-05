0309b29aa31a96bd1ab067db318a9a19c35a07cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:42:33 +0000

    urgent pleasant purpose will bread

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
