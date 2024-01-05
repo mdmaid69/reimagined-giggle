a26aefa0b62bb19033329c2ac5c7cffea5627eea
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:54:02 +0000

    skin meet instead dress callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
