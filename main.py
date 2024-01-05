c83fa501587be4caa7d9ae1942434364df1941ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:33:01 +0000

    eager listenfor new calloff idea

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
