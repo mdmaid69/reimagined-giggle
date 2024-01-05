84de41579636be039f8e7dffaa8e69c8bb8f5470
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:50:33 +0000

    performance gentle but prepare after

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
