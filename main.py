5df2023fd70e411323f861a63df60f484f76424c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:55:46 +0000

    choice row across back sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
