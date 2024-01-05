024056a5863451cea459fee606576161885e8cef
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:08:49 +0000

    practical considering stranger pause learn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
