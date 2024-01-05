c6229cb66ac134aa829f5aaedd9263e0ff877939
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:48:43 +0000

    catch bring little disturb money

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
