f889dc080888db987dc2ef582d5600cfa142a53f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:33:29 +0000

    kick warning immediate shut burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
