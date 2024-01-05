66d112d54c2a7f08ebbbb00e7306e19e25016966
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:52:05 +0000

    bury table speech bill neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import getpass
-def get_username():
-        return getpass.getuser()
+print("Hello, world!")
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
