9521a595b1bd51ade13b5733e7b26d48d11ab4f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:33:09 +0000

    thorough of stiff confuse unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
   def delete_directory(dir_name):
         os.rmdir(dir_name)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
