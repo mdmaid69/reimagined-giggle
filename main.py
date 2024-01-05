cccd4ac7835b781098218424d599fc3b8412fa6a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:17:36 +0000

    some clean suit schedule about

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def divide_numbers(x, y):
+        return x / y
