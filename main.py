cf7104718e8ecd62379401bc2e2899e5aaada9c7
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:19:37 +0000

    home sad woman pin last

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
