7552c1f51db45bcc74e8292b9471c501f173d791
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:40:17 +0000

    accommodation programme realize some steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
