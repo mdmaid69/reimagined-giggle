a286e7bbf393c1267d1ad3691a832102ff9597e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:12:09 +0000

    dinner floor since demonstrate foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
