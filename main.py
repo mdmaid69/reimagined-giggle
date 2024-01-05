3015a2969eb6755a674bec8039e30f0a6d0fd481
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:28:15 +0000

    organize after interested reserve stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
