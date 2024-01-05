f1628d109a965b3c74fc06d183f7aeb8d6549736
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:41:17 +0000

    talk both touch same week

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
