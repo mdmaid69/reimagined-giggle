8b63aae0e424b1227701c814e4694d147574a1f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:11:13 +0000

    takeover row cool tip save

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_min(lst):
+        return min(lst)
