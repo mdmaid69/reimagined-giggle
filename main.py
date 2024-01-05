b3eb519eb0043765313d8102e629ec1cafa41035
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:38:26 +0000

    able away bored else broad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
