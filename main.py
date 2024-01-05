4732a5cb1c9e6fa04fcec95863fb9411c16fbe98
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:34:04 +0000

    sea foot twist term far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
