6e552562ffa707eef2ce4b88d51b1a7df8fa6466
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:29:16 +0000

    judge rarely exactly occasion during

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def count_characters(sentence):
+        return len(sentence)
