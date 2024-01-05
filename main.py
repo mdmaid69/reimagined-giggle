6483d7c0ee6796198d875fa8e9846cf820e057e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:55:53 +0000

    copy doubt upwards trip follow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
