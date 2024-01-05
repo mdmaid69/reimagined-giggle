a6d687555de9ef01686b63f2b1e7f7149661257c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:43:42 +0000

    only possible trap unlikely rush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
