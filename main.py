e5f809de8381f30409ef830f080422037697bbae
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:07:56 +0000

    solve damp start estimate near

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
