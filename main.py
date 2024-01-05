21b33aa8abfa07411a32bd34bbaa445aedd42a7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:46:28 +0000

    Ll calm direction belief greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def count_elements(lst):
+        return len(lst)
