4ff6125d06519c9f7849ee48cdc03c77dbdaad76
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:49:54 +0000

    physical thank he plenty appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
