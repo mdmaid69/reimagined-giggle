716b49d7198221e61ea26526783b63f8bed435fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:50:29 +0000

    if tongue Yy Vv team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
