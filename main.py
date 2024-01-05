f5529bac01ea99565c584e4d87071864058d64d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:07:11 +0000

    judge bad mistake freedom level

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
