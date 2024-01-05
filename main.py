e39107abe49d6b29df4317bfd7a5fa0430c208c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:29:41 +0000

    suck print no absolutely show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+text = "Hello, world!"
+print("Reversed:", text[::-1])
