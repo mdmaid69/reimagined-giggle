8dc21b7acab7b166c9632e4bd07394f721257291
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:34:32 +0000

    reaction number lead look fail

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
