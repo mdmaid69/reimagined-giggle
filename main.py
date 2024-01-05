054249e33fde7a8e1eea0a74a83ba9792ba02817
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:22:59 +0000

    callup arrival send approval punish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
