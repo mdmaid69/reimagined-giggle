982bb0294633568c99903bc40d3f54a00eed4c14
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:22:08 +0000

    on comeback risk exam reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
