f5b23fab22894d86cc08dd42cf5f4a1f367fa257
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:28:38 +0000

    really anxious development easy definite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 n = 10
 print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
