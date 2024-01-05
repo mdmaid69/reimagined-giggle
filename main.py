3d4a5b321821c21123e5cb863649ba56d34c8d5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:05:31 +0000

    attitude upset form road play

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
