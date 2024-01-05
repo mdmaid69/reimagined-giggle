18176a26c279b0fe96e19dc30f15dde0e34b4532
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:10:43 +0000

    air count uncomfortable break change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
