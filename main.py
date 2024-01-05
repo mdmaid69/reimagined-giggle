2501a3dcb95d0ddd97d433f91601dbc2366d987f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:00:46 +0000

    man too show person bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
