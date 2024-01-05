22bf7dbbe36478c99bbd2d7f6a94718a22aa242c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:59:24 +0000

    ear report because month awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
