596ea236b6746463c013eced947e904887b61a7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:18:36 +0000

    level failure sure worth side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
