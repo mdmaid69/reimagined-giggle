3f8cede21cba58e4431f3a377f893d2381fb7e60
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:57:06 +0000

    home position post head dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
