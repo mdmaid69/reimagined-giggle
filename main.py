aec95ab9d2c14faf9df676ba99e78efc23a8dff8
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:26:11 +0000

    national contact burst below react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
