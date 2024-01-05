daff1fefb2a3a5cfea3ffc9946be23715765671c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:56:10 +0000

    level oil suppose original dark

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def is_odd(n):
-        return n % 2 != 0
-def divide_numbers(x, y):
-        return x / y
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def convert_array_to_list(array):
+        return array.tolist()
