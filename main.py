c4d45ee6dbc97148d8b95810be565bffad02ad1a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:01:17 +0000

    mess squeeze bet read sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def sort_list(lst):
+        return sorted(lst)
