6abe847a3167319e8704cbac4e6f4b6835eea424
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:08:14 +0000

    tune line manage police goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_power(work, time):
-        return work / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+from collections import Counter
+print(Counter("hello world"))
