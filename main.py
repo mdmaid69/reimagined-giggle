e924cf6676770781c32b9aaca8df241386def809
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:04:57 +0000

    legal ought name similar Dd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
