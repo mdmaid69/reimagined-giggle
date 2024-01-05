8eef1e0224f630c601b9f78d92bad5ab094a3e2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:08:43 +0000

    week dry roll underneath tense

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
