b91d3ef04b7cff09dbbe7e06fd7094efc6d54e69
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:00:44 +0000

    visit morning marry pleasure whether

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def append_to_array(array, item):
+        array.append(item)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
