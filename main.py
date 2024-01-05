3746dccd58becd223475a7145dde4a614c984769
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:54:15 +0000

    gooff demand shock sad watch

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-name = "Python"
-print("Hello,", name)
-  def subtract_numbers(x, y):
-        return x - y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
