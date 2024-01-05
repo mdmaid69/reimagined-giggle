d4585ca430527121ed6624e013882ce070d8b39e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:38:49 +0000

    right clear summer computer reply

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def add_numbers(a, b):
-        return a + b
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
