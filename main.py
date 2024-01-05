b9b8053fc932210301f6dea13978d98146e440b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:50 +0000

    rain survive brown slightly government

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_rectangle(l, w):
+        return l * w
