bd9cac32daa91e8a695e0b2af0468967bad2610e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:09:36 +0000

    side opportunity sit create detail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
