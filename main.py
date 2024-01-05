7f3aeb41903ffdb2ee63a90d1e936511855c59a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:15:39 +0000

    tomorrow little earn sing replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
