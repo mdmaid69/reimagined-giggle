54cd3e3f2f6847a62aee9983b13b95a4004fc934
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:58:22 +0000

    free answer rule straight runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
