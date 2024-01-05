0b229f770af834d9aae324ac9a372b93df32e63c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:30:25 +0000

    simple trouble escape soil past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_palindrome(s):
+        return s == s[::-1]
