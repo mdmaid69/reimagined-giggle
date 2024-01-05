167063f72664c138fdcb9d17e8860ca0db30898c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:13:58 +0000

    short complain slightly noone crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
