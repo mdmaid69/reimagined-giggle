2abf3a2a86cb6280a9e2d4fe0c8b288c6f05c4c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:59 +0000

    must mention control field protest

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
