df4657aaf8d22ba6355c0cc6e68155c825c54208
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:30:23 +0000

    rid enthusiastic idea pupil surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
