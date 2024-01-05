b1f3443b6867872835a52212696af747fb933bf6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:03:24 +0000

    judge shop tongue bottom minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
