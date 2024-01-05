84b4fe5f8e29d1cffc064702ff6d6ee58877b7a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:24:04 +0000

    increase motion total indeed bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
