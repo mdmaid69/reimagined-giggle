c931a67d310e75c23d52102ea3b221d33a195127
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:47:11 +0000

    several importance hole favour objection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
