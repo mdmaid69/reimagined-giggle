03afd328e897620aa0f31aa9a2006b8d3d7eef8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:16:55 +0000

    shape sail consult pull speech

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def square_number(x):
-        return x**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
