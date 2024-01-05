10407eeb0fe64b516e9b7b519661d976a6c065bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:28:38 +0000

    range slow dance ourselves rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
