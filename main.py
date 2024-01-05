748819ee0d69c78301d56f3adc2617fed6852cca
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:02:53 +0000

    grateful add chase official along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
