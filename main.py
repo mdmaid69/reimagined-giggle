5b8f68da5758e5222bf3b69abb9e1ecc5f8b95c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:52:36 +0000

    afternoon drive defend innocent equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def is_odd(n):
+        return n % 2 != 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
