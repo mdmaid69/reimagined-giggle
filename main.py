f670ef029e2b42ebc589fa42c3b891e578c7c7f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:21:12 +0000

    defeat illness colour town entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
