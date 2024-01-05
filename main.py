be0ccaf961b32ee649cd99d97e7c3a67eba3f799
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:52:03 +0000

    guess pronunciation snow price 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
