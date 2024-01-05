ae1b5c4298d696d996cc755f9a75a61ddbcb76a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:43:29 +0000

    still mark but he callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
