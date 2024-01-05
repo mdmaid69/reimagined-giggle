c6492eeace0b7b9ebec46c49e8a319ef8c9db7dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:48:20 +0000

    getout short position stupid offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
