b66b2357b0e55ec2553ee107fa80859fc72323b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:15:24 +0000

    ticket connected goon possession protest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
