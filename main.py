218e285012f0119715325ac4210a8c68e9812622
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:17:21 +0000

    congratulations whisper crowd lay insist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
