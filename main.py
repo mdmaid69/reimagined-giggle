fbfc0ee211b299bbd47bbda83ee204e4e0f8c47a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:33:15 +0000

    familiar arm lonely skin dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def clear_array(array):
+        array *= 0
