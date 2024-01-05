a9651d88bceadd06f29cfaec38a48295fea69db4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:44:03 +0000

    wave callup certainly reach check

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
