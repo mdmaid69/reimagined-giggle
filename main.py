e65b76dfc6a8996b473e6a48aec5ca9e602c5e59
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:24:17 +0000

    basic congratulate same them programme

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
