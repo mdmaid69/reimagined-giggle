7075fba297cca2bc846451de8fc087dfa3926ee4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:53:11 +0000

    plant shop professional like official

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def count_elements(lst):
+        return len(lst)
+def find_min(lst):
+        return min(lst)
