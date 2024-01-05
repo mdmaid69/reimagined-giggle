a423e68ad035f5ea4a73e7bb9c8d39f72323e440
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:13:51 +0000

    stamp still trip transport maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def square_number(x):
+        return x**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
