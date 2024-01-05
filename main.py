31808f12831a5d9b23990a8786b9b195cf44159b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:20:21 +0000

    show fasten doctor mine over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
