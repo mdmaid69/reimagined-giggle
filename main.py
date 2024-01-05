d863ca562f4e8891c754c14f2b84a3fda8349152
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:25:09 +0000

    sign environment small hard 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
