e6197701d4c2eb4e6d31832c9db5198fa4eb58fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:33:46 +0000

    runout but see rise runinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
