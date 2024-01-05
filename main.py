76acfff4c3493ee81b616d9ec0a1babbb3b31f62
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:52:11 +0000

    last unhappy account apart performance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
