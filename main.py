42a6570e802d7e6c3d7748bf69a998150498761f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:15:59 +0000

    related Pp better avoid dick

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
