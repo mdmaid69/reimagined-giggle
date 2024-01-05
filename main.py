037daec71450dee3fb36600331ac4013995b2983
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:56 +0000

    error instruction judge open where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
