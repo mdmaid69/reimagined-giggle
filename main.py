a0e846f1b65337b60fd70784551860a05a22092a
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:38:28 +0000

    hold relative mysterious callout near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
