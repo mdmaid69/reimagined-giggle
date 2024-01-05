35773b952440ae874754c0c47c9054f704f35235
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:45:56 +0000

    free yet announce recent soon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
