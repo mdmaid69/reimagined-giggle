ac2f4a07914c3b76f786385efa56f89999f39bcc
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:08:26 +0000

    smell arrive like action easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
