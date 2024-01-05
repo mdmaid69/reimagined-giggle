96b6f9696c940704b1a47eacd175af0a1f9259a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:59:58 +0000

    method getoff mark turninto please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
