7c5cc87948a6ad3ab19714c5344fde5ffb7452c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:36 +0000

    bank across centimetre succeed call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
