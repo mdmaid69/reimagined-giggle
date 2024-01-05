04d01995a5eaf565547d9091c5dfcf7d8994a719
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:38:25 +0000

    dance rub replace but between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
