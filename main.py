2d8f5dd0b1138807e5f1238f0b012b2ec2b0225e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:46:45 +0000

    great destroy case convenient floor

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
