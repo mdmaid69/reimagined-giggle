f2001882ffc2bfc0ed8f6460e61fb3589762bb0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:36:55 +0000

    smile collect film about rarely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
