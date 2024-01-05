ed90529d348208e15d3ee0b0e0954d926adfd304
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:50:02 +0000

    mean tap possession ring instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
