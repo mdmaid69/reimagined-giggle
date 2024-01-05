b12dc31ea41e46356c4734332b6606298a614422
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:22:07 +0000

    to control thorough contribute interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
