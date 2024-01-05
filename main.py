4ce04f86ea015379059b0f8a1c12427adddfbe57
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:21:59 +0000

    deliver pleasure strange rub emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import numpy as np
+print(np.array([1, 2, 3]))
