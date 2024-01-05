f2f6f911a5493a42ce1e76b9ff6368c23742708b
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:56:47 +0000

    book test help damage Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import sys
-def exit_program():
-        sys.exit()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
