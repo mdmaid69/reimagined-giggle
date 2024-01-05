d335fab7a9695032e5819de5437d8bbbb76c99e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:39:53 +0000

    land tomorrow minute finish certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
