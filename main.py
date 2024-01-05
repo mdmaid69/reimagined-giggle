4cd845a45bc548a6f13885cdcb2446d3cd5350ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:26:51 +0000

    bread interrupt agreement ring degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
