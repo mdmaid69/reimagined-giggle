8ac7c77387035d9d0cdfc01e1c0b8c7311ed445a
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:34:59 +0000

    punish phone top month laugh

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
