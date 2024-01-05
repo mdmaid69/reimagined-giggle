a8c1fe311af2c7905347858720620d870d15c230
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:40:41 +0000

    fresh absolutely surprising Ll tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
