e12df59cf79a19117f86db05bc54c28a93a20e1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:41:34 +0000

    odd congratulate recent concerning birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
