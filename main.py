fc37cf55221f5a5cb3e57c1eef83e8489e8b7ab0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:17:45 +0000

    pair stretch reserve history shoulder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
