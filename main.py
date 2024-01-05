7618f439e28d0e387da908dc7f54e3661200464d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:28:28 +0000

    himself instrument house drawing need

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sys
+  def get_python_version():
+        return sys.version
