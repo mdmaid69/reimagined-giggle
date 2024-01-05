6c68851397e1ce9b1e17e0e91c4cb78b759f267a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:26:03 +0000

    excuse listenfor clean television niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
