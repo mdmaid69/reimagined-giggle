bf1690f3dfbcc56a1f74f35a52b78c5c84056c36
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:19:03 +0000

    black after far goodbye generally

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import os
+print(os.getcwd())
