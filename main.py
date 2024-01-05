5868361f9d6d5b4620b2ef0ac2d3c1588d58f29d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:21:08 +0000

    evidence breakthrough quiet short not

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
