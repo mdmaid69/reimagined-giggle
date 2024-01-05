62af40aa3d1c7b22eba8586693950b2e6b9d8b06
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:26:50 +0000

    me comeback fly guess rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
