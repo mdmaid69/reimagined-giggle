4816c96df80d13d955a0b5f0fcab4d1aae3cb97d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:45:41 +0000

    spring process waste fire strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
