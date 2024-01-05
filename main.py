34268881e440d0580ea7ff1aae4f12127f27ac5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:00:54 +0000

    direct stiff protest sick popular

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def convert_to_hex(n):
+        return hex(n)
