5789fbca3d9ccdcf00231bcf5a00158fde2e0d6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:30 +0000

    night rest previous lead suddenly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
