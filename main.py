21f8650b0ddb095f66299752b753f836819fbaa1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:26:20 +0000

    bar space need save field

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-x = 10
-y = 20
-print("Sum:", x + y)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
