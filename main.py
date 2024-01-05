b0ff02e2322b520cc877a71f5e077a59f9c06a28
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:04 +0000

    during model cross risk hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
