775a61ed5cca3e0226b08e7f87a30a4ce70013c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:50:11 +0000

    fit supply getaway smell base

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
