293f27706025bdf04b10ceb9e72ce63030b13011
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:03:34 +0000

    coffee nothing maybe unit fall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
