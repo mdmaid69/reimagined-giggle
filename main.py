a8196575361b5bab6a04172a228efb4447c9f2bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:26:08 +0000

    modern this feature choose pronounce

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
