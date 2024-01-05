e6aa56bfae25ed3cb460674cc34e986284a365e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:03:32 +0000

    port danger interview principle breakup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
