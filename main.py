11a071f0e67be2e7add18c27efa7834d5e9a1aaa
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:26:56 +0000

    transparent impressed mine coffee block

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def subtract_numbers(x, y):
+        return x - y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
