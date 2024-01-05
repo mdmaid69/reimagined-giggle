6c3b56e6bfac3abae49db734c717ca812c0414c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:44:00 +0000

    square silent welcome already song

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import platform
+def get_python_version():
+        return platform.python_version()
