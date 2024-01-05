2417d719eff0e9c454b8522c5bea4fc9857a7233
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:19:12 +0000

    for spring nowhere issue responsible

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_power(work, time):
-        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_density(mass, volume):
+        return mass / volume
