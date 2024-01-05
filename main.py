20b83f19a857181e7d4c4cc9b3fb6f885c6f35a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:20 +0000

    length imagination accordingto unlucky help

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
