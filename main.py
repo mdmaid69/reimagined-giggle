89f21f29a55084257df0da885c2a01a495cf2cc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:39:31 +0000

    taste attention delicate excuse repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
