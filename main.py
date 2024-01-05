6cdb1dba8cc4ec756a3abba344c1f8fbc8b4f01b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:53:51 +0000

    last suckme stretch giveout also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
