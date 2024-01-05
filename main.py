4dc30741f10a9444e117a8afc91e39316d9f76da
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:15:01 +0000

    kill move stand load thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
