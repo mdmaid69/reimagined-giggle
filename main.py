5286485b577e6565b8ce9b4e8661634983f1ddb6
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:32:42 +0000

    children fast post copy importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
