8eef054f2204de0430a1d5d37980c9eaf2188eb1
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:10:13 +0000

    open within explain event amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-x = 10
-y = 20
-print("Sum:", x + y)
+def sort_list(lst):
+        return sorted(lst)
+import collections
+def create_user_list():
+        return collections.UserList()
