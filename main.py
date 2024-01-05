4403f733888c059f358606460c66ae85c368f13c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:45:56 +0000

    count rank crash colour stone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def sort_list(lst):
+        return sorted(lst)
