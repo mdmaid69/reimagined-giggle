50695f8911ab4225d63f509722ed0f1dfd900a33
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:28:42 +0000

    defend pollution hers Hh false

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
