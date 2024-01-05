6ee9cbb9fde0e617210eb7076d6db34d05fae97c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:09:43 +0000

    pass public break kill person

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def convert_array_to_unicode(array):
+        return array.tounicode()
