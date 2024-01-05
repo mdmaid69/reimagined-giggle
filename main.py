9fb7c71bc791cec9f1da6ffbee391a16ca727727
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:50:11 +0000

    your degree confusing Pp dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
