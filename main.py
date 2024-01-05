e5575f21696b20e7623ef157cca94d5aab02f571
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:12:32 +0000

    wear thick wind calloff easy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def convert_to_hex(n):
+        return hex(n)
