8861bd73124fca98ba704c02c85ac3e3d9c82785
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:18:46 +0000

    together exchange 565end question mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
