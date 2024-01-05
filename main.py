37b67052f94d057318640555ce1b3f5beb1b3d12
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:32:17 +0000

    fight payment card bunch detail

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
