217045e919b67dc1f4693a3196ead465822c72c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:00:12 +0000

    plastic its next sail small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
