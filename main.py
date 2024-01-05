ec1b7bddcb8e8ef08a086771cfc4388d5cb1ba40
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:50:03 +0000

    awake equal last nearly bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_area_rectangle(l, w):
+        return l * w
