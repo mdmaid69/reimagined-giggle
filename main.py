74692f8caca55ad13b6c4e808fdd027133963958
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:32:16 +0000

    letter actually Oo turndown objection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
