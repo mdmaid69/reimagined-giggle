72b2d67e8f532ae2dc0c3e54df725f00f76db676
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:53:31 +0000

    when squeeze think branch duty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
