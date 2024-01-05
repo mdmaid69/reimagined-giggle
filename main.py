5b8b0a147a1f51ad1655ae4e35526d16c24af4f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:03:08 +0000

    their back range sport godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
