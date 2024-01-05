120deb5a352b5c44bf308579e37f9321453b1d44
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:27:01 +0000

    dig before more embarrassed murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
