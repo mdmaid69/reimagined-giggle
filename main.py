8c83e548f3c2164b42edbfd4a3fda93d0682fef5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:29:16 +0000

    children surface in prepare stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
