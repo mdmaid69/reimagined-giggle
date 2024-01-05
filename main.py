268ca2cfffe65284c2548e3a33741f6f55ff2306
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:46:23 +0000

    activity warm muscle agreement burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
