46231eac34c6fd34fd746e11b1ecafe2ca341186
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:26:03 +0000

    steep line call project day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
