1dd96b5529afb3c639525095eff6f3919b1e20e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:14:34 +0000

    whenever strange mind another mistake

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
