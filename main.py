ace5853b158b2481e41c2954e7146129e63d2e85
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:58:51 +0000

    strange content sick criticism high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
