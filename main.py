c599f78d437e5302fc5eb5c3db40368ce8b91000
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:04:26 +0000

    square conversation stand recover mail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
