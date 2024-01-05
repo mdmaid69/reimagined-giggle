27438d45277ae52f995ed1c74628ea46205d39cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:25:50 +0000

    comment earn regularly speak various

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
