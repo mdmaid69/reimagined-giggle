e081c57995bb45efa6fed294ba43f33b1fef39d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:16 +0000

    port hate concerned holdout holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
