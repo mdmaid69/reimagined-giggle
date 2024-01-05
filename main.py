c9cac3212509987145424fc35e2eab3e385c8e96
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:52:19 +0000

    properly tap more conclusion study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(a, b):
+        return a + b
