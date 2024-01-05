970a72d08d9c8a0c97cdda6cf3e1ee1419686078
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:54:38 +0000

    strict face juice row concern

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
