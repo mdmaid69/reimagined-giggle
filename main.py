1f80561d548519b7df159877ead8eceaf2505d13
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:10:56 +0000

    push colour smooth sleep action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_rectangle(l, w):
+        return l * w
