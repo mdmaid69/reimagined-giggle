d6ee2effec62d0a18042e49ddead1ea345e81140
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:24:21 +0000

    give serve crazy sound load

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def square_number(x):
+        return x**2
+for i in range(5):
+        print(i)
