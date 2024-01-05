e2432a98c2545e9a09ba8464edfeed24a8a9ddb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:24:46 +0000

    ignore line square Cc characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
