4e735635da38ae8da79c116ad922b0febea21451
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:10:47 +0000

    lie drawing again software Ss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def convert_to_hex(n):
+        return hex(n)
