f5a143dd61549280a7a765f8b35d6bf075b6c30b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:29:15 +0000

    stage because unit police damp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_density(mass, volume):
+        return mass / volume
