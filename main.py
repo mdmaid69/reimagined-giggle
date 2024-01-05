fce585e66f890649859fa140c269ab7b457ad501
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:37:19 +0000

    sky intelligent decision breakin workupto

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
