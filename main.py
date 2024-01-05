28032da4fdd903ea5ada7e64aee513cf2f21462b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:56:42 +0000

    recently slow clear community sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
