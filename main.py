49d19bedd9aeb79c4acd8d2d22084957c2f2287f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:26:41 +0000

    brown note that watch slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
