d653ed2375851545d9407efc532cd955e5da005c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:14:00 +0000

    excuse shut natural objection recently

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
