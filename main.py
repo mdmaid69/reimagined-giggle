195975b9a8fb8048211e535427deb5b917182010
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:11:25 +0000

    statement mass takeaway from leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
