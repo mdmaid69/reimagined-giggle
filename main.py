910414f882fb183234b822f4d9c34c59b6f567a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:26:21 +0000

    model clear justice have attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_directory():
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def get_current_working_directory():
         return os.getcwd()
