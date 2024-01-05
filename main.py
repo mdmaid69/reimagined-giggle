7d05b10f89380e15576d06ea5a17f1d96d0ee118
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:22:40 +0000

    many case rent calloff mistake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_pressure(force, area):
+        return force / area
