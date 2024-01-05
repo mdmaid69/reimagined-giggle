1bd655e437a2511fe01a6b36d4c6a8e24c95d02a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:09:52 +0000

    eager test that none steady

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
