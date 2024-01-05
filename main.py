6460ff089104d9051e282fe9c00c912c1964ad5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:55:44 +0000

    start area I invitation comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
