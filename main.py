4ccf5dba825089c55c5b5a2f1e0ddd12e208b998
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:22:33 +0000

    protest practice space drive extremely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
