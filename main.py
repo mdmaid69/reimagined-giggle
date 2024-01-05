3414fc1d1adef1db3a15bf28880bbc578253b599
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:58:03 +0000

    close party first address pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_pressure(force, area):
+        return force / area
