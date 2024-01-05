6b52948bdd65e1e95eb899fb4578b27a4a40aecf
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:40:59 +0000

    regularly only bringforward another wing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
