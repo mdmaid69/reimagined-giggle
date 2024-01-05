78c01f9da4f986c9cef8a99af95a29992a498223
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:48:49 +0000

    each coat insist somewhere chance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
