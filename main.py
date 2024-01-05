d44004e5f0857ae88c454c65547a6910d3c48cd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:27:23 +0000

    difficult connected operate Ff poison

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
