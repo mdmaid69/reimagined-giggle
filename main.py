3a3596a6071339c3ee5681f27f852672bce62b7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:25:08 +0000

    importance side comment foreign cold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
