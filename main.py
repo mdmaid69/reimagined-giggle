36a4e9a66328dfd3b25603338d573e7ff9e7a1c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:47:47 +0000

    arm blame situation cheque badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(numbers):
-        return min(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
