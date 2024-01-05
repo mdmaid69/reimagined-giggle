056cefea8fb0b005a553dca3b02be0c4cf32a5b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:39:36 +0000

    port only such separate coat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_tangent(x):
+        return math.tan(x)
