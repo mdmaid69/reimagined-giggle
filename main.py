80d75d10013bb04665e39b9dff98baec17e4f96b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:54:25 +0000

    respect bad high scared expensive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
