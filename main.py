4437304dcb4f7d1c212c687dcd230d0b85c6d8a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:53:49 +0000

    worst learn violent bottom high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def add_numbers(a, b):
+        return a + b
