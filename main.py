c7e819bc8ed5d18b252112e89ddeb0e46d810238
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:18:36 +0000

    drive cover light before before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
