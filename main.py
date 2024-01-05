f8774acc70bc75242a023ad05b956c615a57a8cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:39:03 +0000

    expensive listenfor may live no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def cube_number(x):
+        return x**3
