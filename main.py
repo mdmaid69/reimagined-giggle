5565dd12f21c1d7a563b0193acc8cd92ea632e2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:23:37 +0000

    scheme drink agree rank she

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+print(sys.version)
+def divide_numbers(x, y):
+        return x / y
