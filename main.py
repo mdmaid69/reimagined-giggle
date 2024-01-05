b98a93c106c79c8fa91431de6ab7c3f5825301c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:49:40 +0000

    see corner half stiff instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_pressure(force, area):
-        return force / area
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
