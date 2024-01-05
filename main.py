dfebfd59899d2416d5e27fb2d4e1d9f5838c6be5
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:02:36 +0000

    murder keepout wonder deliberate expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
