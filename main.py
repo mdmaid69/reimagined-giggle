a89e056b7bb76c2be555cf16642a0d8e1631f9d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:35:17 +0000

    spring threaten mine fasten though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
