f7442e4078c69b92b1c12ba9a3e9dae5d6c926c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:57:55 +0000

    if public excited attractive black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
