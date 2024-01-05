61ba1ed015d10a457797ece6f9980c9fd32d92f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:34:30 +0000

    crazy stir lesson right term

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_area_rectangle(l, w):
+        return l * w
