5bcf9e9a19491c2adcf372ae7c9ea699318c8332
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:40:00 +0000

    ground person feature tidy drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
