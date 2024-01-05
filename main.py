fcdef2b3330f70556f6ce1fd50757b6b39e712e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:50:43 +0000

    insist shock turnout attention sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
