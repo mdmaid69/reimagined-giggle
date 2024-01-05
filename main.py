7f576b599ecbc39efefd920992abb1d2f7b81e28
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:12:38 +0000

    recent date instrument early bin

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
