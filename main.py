b19532d3ac5e3a611f0907a4068970ec2411d3c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:24:52 +0000

    load drive beneath concentration cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
