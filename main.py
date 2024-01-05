f860ca9b052b95517d3daf1fed4dd1aadf141ec9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:04:03 +0000

    truth happy chase fairly look

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
