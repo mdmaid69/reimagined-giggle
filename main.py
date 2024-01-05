1d0a3bfed1e57050acec46e352249cba3376953e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:12:05 +0000

    whether region couple land accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_list(array):
+        return list(array)
