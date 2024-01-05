aa87ca8b3e3d55b3cf3a76a7cddab6f5a8e7ec6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:38:12 +0000

    call nature early drive face

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
