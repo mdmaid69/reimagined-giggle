36ad8d84539482a7c7ffeab177e709bca4c9b808
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:13:18 +0000

    lift Tt contain zero date

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def is_odd(n):
+        return n % 2 != 0
+def calculate_area_rectangle(l, w):
+        return l * w
