0b8a100cc44b3cb7bd556cb16e8fd6cb69abd446
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:57:56 +0000

    suck runover quantity declaration mouth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
