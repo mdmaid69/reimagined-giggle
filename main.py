6004e9c3c66a84afbcffc4bc5777ed5ff55d8665
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:42 +0000

    space dress practise description last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
