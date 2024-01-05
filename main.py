7458e908b51a34a547eed81701c97d297e665f18
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:13:08 +0000

    year distance classroom rent lean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
