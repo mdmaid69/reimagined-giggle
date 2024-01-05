065f8ecce8e52f6be3152c51793d7d4ba0583604
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:31:18 +0000

    mark punish by listenfor odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
