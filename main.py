2bc60d149fb2c7249f799d44a3d9f6560af3ccdb
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:00:23 +0000

    grade season grow fire scrape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import random
+print(random.randint(0, 100))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
