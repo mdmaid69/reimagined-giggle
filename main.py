5dc9d9066c548e999beb0e1f8c844ff16b9b7281
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:47:30 +0000

    luck hard realize test hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_elements(lst):
-        return len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
