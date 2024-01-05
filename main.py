af6f1f77991cbbd081bb471d20db7a4c380111c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:47:57 +0000

    whichever apply system him stare

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
