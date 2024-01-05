99cdaaa7dbaf075c099d00e8de286932a616c328
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:58:14 +0000

    air keep hard term various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def set_array_item(array, i, item):
+        array[i] = item
