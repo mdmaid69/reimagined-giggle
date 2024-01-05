52d4cad572f298760b241b48fc7989677cac492c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:16:55 +0000

    active turn accordingto meaning father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
