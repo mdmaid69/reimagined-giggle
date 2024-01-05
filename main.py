93b588bc7d078b3345410771ccd8c38756f2db31
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:54:27 +0000

    neck every upwards act light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
