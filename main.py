fd8dd1b7788f8d8f6b447284dad9d10c805ff415
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:04:09 +0000

    convenient needle upwards lookaround blow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
