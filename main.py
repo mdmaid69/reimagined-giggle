a81da45965ea3e770b0beaa4073100f7f12269bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:35:29 +0000

    worry flood complain guard attention

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def remove_duplicates(lst):
+        return list(set(lst))
