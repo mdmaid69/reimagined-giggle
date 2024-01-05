73460a2cd65f80f240b4609fbec70c772e671060
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:52:59 +0000

    under Ii fat runover frightened

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def subtract_numbers(x, y):
+        return x - y
