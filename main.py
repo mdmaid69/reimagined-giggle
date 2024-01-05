5df854872086d094eb3bc989f52d2343cb8e8636
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:33:47 +0000

    professional embarrassing OKorokay arrange lock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_work(force, distance):
+        return force * distance
