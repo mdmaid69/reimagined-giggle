bba17fb7342f385135c10af7c4f5f3c248df2f82
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:31:41 +0000

    shelter change protest independent his

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
