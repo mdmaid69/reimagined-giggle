99b06a70e71662b4df445e7b7d8c6d0372233d02
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:53:18 +0000

    certain pretend of plan pride

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+  def count_elements(lst):
+        return len(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
