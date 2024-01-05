1fdc1247dff2eefa35aed93013f8dd69f53a4c41
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:58:22 +0000

    story kind allow later punishment

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_typecode(array):
-        return array.typecode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def find_min(numbers):
+        return min(numbers)
