d504247e8703fa71440c8c19e409c836c74ff958
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:04:34 +0000

    forgive or and collect prepare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def count_words(sentence):
+        return len(sentence.split())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
