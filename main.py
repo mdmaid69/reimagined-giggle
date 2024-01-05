f022fc2c9f84e357761a9af2684b358b6cc9ae38
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:30:40 +0000

    replace service burn teach remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
