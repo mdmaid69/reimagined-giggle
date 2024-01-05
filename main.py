975bdc29cc73d399191305b967ff6ba1ff2c0499
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:37:50 +0000

    rich reliable information ability pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
