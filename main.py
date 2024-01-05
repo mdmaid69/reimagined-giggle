5c901aeff01981b98415aad306b1f403699103c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:56:35 +0000

    mend spray name father excited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
