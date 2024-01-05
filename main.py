fd20a33d0f5cc94004d8e1a67366a027cbaaab16
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:42:52 +0000

    city wheel discovery charge use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
