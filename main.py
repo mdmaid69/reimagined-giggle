5d24af2c1c94c6a4ed66bd2bf4d2c08131c5af7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:25:12 +0000

    sky tool later herself arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
