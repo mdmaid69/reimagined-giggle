4fae8f3baa4a59ebe9d3b16e90348b38c309d8a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:06:36 +0000

    touch ill mean guess lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
