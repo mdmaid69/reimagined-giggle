e85e0b702f187c4de6cd03dc527d97fbbda7e1dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:12:51 +0000

    bit split name muscle dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
