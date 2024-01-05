9c88e9a5f61030b3f2dcd957b4921b335cca891c
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:34:40 +0000

    bend onto attitude unfair event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
