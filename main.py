f13ed453358b3b63ecd1091cc1d5106231f537d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:31:18 +0000

    mark ahead nor recover quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
