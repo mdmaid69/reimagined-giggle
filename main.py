5bd367f732a9d794e4d4d0093599b04a4a01950f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:53:16 +0000

    roll realize die broad man

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
