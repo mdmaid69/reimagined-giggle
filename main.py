5a1eeb394877d6d993752c8ea11f3034e10ca51c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:07:15 +0000

    past stranger hurt row priority

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
