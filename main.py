3d8e7720e501ada694965051009a2afd168ecf2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:20:18 +0000

    nose early refuse view excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
