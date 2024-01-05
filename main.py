e019c492596ec9ee74a99f33c2c4d181b83af57d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:27:19 +0000

    side total wherever alone main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def convert_to_binary(n):
+        return bin(n)
