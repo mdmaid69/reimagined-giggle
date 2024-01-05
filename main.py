f48574c05028ff13af87565f3293c6f3424467e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:41:02 +0000

    571enough touch sign open shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
