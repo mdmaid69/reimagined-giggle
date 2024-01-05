b3dacdc02c4b414992a9db5731f1d3b0d7810fb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:33:20 +0000

    herself respect judge exist slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def get_array_item(array, i):
+        return array[i]
