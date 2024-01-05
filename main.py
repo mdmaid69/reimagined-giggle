6394554c43419ca3cabec9d4b0ea1288532d3887
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:40:35 +0000

    niggaacademy shame smell improve because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
