d02e790287bb3031486d6fc8b6bef322f6888c95
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:27:47 +0000

    agreement downstairs soon skirt shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
