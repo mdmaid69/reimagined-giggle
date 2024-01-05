6eeced3e82795a9896bc8b26c29159e2d3160d60
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:00:00 +0000

    talk length together turnoff lot

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
