df14852784a885b3f22befc7928f23d4718f613d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:29:52 +0000

    represent win future spot harm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
