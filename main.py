8a290e5a3b4ee133ada3e516d9a8adca273f91a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:09:10 +0000

    service let phone image desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def remove_duplicates(lst):
-        return list(set(lst))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
