7fd179677cf7e402f46c68d79cc21802b75a325b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:08:51 +0000

    team formal yours wherever order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
