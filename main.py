50f2e72df9819c83d49c59ba16a01661d2b598f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:27:35 +0000

    again Oo underneath wall correct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
