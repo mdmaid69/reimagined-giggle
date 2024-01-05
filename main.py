a57d787293eef5f823597f4334f9719cf70e9e58
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:55:04 +0000

    violence that happen pain slowly

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_odd(n):
-        return n % 2 != 0
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
