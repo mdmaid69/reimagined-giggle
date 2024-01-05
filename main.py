4515a1f13d5bdc5f7c931b0fca4f7f01ec28887a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:45:49 +0000

    rid produce most low otherwise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
