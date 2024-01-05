c422cb05c132f2d7478ce6587ddbc9c275454469
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:55:20 +0000

    front right order string mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
