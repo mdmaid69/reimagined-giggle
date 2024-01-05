c25f994078e6792fa8d13cbb0317d3e6ef1b355b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:14:59 +0000

    easy part command father name

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def calculate_area_rectangle(l, w):
-        return l * w
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
