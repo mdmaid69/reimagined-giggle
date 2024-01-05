ecc28eadb066d5de322a8eff47e5fda67c263c5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:56:26 +0000

    mistake frightened excited mine than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
