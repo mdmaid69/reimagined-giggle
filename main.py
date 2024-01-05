b2a4914870456f37d0138c596ef728510a1a96b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:32:27 +0000

    simple business shame model 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def get_time_since_epoch():
+        return time.time()
