4b5b1eff6023105ccf3a9ebc0d0b98b714213eee
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:15:00 +0000

    square risk demonstrate mass get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
