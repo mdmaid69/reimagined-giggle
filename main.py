d1449611b87415b999091d1fc9098bc61300c8e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:55:48 +0000

    exploration wet entrance runout point

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
