1197e8db88c8c79641441ac148a2066f9016b8e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:56:42 +0000

    weigh fail person business become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
