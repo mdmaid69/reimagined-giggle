9f3732d0ebb3088ca637a8e2cdd65ea15bb6538d
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:09:32 +0000

    square might pollution then comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def convert_to_octal(n):
+        return oct(n)
