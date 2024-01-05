68fd6437366cf787cf2db3605c03fed79d423afe
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:52 +0000

    heavy frighten no mark listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
