8af598eb138914ec0bd7dde0eb22af22dc715c8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:34:24 +0000

    adjective heart suit too also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
