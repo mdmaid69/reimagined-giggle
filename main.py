fe598a8794b8c51d8ec201debf44fe301c563b69
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:10:31 +0000

    backwards talk passenger here belong

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-print([x**2 for x in range(10)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
