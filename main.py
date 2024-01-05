fffb665e9123f7c7eb62ee32ddef360760736cc7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:04:26 +0000

    before duty continue turn I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_int(array):
+        return int(array[0])
