2cea7bea9d8d805f2cffa03f4bec66fd105e7b51
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:02 +0000

    speak card scream card scheme

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
