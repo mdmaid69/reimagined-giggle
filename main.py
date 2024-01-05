f354548d6cc5f309dce7261e93b4da0e856e3d1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:12 +0000

    though surprise less Jj failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
