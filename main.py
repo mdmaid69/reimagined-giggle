a99309869277326c9b15fb5f7dfd92a9cacc5077
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:46:40 +0000

    trouble reaction calm blow soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
