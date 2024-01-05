3dc86bc4c80bb85b89f9be0c5cc496c73810c4c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:42:04 +0000

    swing yesterday basic answer moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
