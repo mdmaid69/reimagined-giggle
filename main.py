d55da5dc95af3c1de813fd41a06c5ee24fc30fb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:18 +0000

    programme talk slide save main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_power(work, time):
+        return work / time
