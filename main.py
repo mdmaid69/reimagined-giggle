efef30ae1953813a9f38097c19fbcfe32bb02a44
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:32:45 +0000

    embarrassing towards centre pain bright

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
