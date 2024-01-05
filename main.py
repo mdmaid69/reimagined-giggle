c58a23466b704268494ce6f9cb2788d3d6cecfed
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:49 +0000

    find answer circle careful mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_list(array):
-        return list(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
