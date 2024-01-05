39dab6875793ab9a01e7948388a2441b8d266a18
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:55:10 +0000

    police bear stamp work pleasure

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
