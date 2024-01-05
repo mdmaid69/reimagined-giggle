7394bae9ceb4cd729eaabb88c41f22fe52676e9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:52:57 +0000

    where too early weekend port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
