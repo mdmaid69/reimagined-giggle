046bd6e9cbd0726c2936ab4b1cb67fb001567a1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:42:24 +0000

    claim their responsible relationship secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
