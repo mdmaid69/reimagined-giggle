73cdffe359e48aa1594f9551ffe744293d61a192
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:05:14 +0000

    immediately waste pour connect concentration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
