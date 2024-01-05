945ca9b4e0aeafabd21bf48da154a613618c6742
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:24:35 +0000

    thumb protest emphasize respect unfortunate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
