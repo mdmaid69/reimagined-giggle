8b105f1cc96efc58c0dfe18248720e74b7f68f70
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:03:29 +0000

    quality complete survive fond public

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  def convert_to_binary(n):
-        return bin(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
