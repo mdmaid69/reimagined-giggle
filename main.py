f53848c11ede85be6035d4f096030dc423573dd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:02:19 +0000

    ilovedicks keepout trousers male mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
