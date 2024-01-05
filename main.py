a1a0a97580b081fcd41dec3e0fedb38153c9a4d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:28:13 +0000

    bus act cinema seriously respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
