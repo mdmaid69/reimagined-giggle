99d90ee7d05cfe513e1ed2268b6f824e6bb9d47d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:34 +0000

    performance hardly to turnaway degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
