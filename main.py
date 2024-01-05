b3ac347d9a740413fd6f52323380e8b60c7898e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:10:42 +0000

    gooff love vision family pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_stack():
+        return collections.deque()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
