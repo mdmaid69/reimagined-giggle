6615d93f76112c02ec0de05f11666a97a7525dec
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:06:45 +0000

    live under typical wear reserve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
