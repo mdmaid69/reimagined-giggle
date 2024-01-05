82e832fd55c20fb7ce94770cdc8f684c1c01bab3
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:10:20 +0000

    comeoff no 564encourage capital good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def is_even(n):
+        return n % 2 == 0
