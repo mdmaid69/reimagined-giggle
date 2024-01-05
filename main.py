97eac14a9b8eac09650d1333c2fce6a18aa4a9d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:42:27 +0000

    side emotion scrap variety scream

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
