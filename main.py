feb98a95d9863167ffdca5924f95858d5aed069f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:09:39 +0000

    supply the guess sweep breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
