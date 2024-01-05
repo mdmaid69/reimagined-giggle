231fe456fb6b5f30e142c1edf8093c60cc5a89b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:58:01 +0000

    turnback traffic needle home wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
