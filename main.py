f9b272885de610931305afb31393d13d2bafd67e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:26:12 +0000

    stage bear lock unless wing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
