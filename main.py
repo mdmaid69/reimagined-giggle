703ee5ae2361782bbd3c47dd02e80c98c91a73d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:26:07 +0000

    organize pour game cheat complain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
