bbd1b8c0aa25c53a545380934db2fda87006ad09
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:31:47 +0000

    hot sharp then bound appear

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
