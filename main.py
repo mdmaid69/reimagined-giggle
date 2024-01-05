59c9c6f153a1708fc442b02f0ab9895977eb1443
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:00:32 +0000

    power way only store strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import platform
-def get_os_info():
-        return platform.uname()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
