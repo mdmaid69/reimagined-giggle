9f7093fe34576b1a1408b0ec852f68db5770f1a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:00:24 +0000

    along consider usually innocent round

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
