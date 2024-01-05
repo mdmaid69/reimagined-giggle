77e3e4f6323597930dcf294f7336e4f761c2f296
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:34:19 +0000

    cost remark border job male

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import tensorflow as tf
+print(tf.__version__)
