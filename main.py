7e32f843348c11e6b465659c9132eb7ff13239f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:11:28 +0000

    everything slightly trade thank bite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
