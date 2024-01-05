42212c4c9c02d01c34be3e3b3a9eb257a569db09
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:16:32 +0000

    bored cheap effort education close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
