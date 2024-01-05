772655f239cecbf9846dd05a60ccbd937f999a96
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:30:53 +0000

    pattern sure related case copy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
