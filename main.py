ef68824a6da8d1fb477c39b67fff7a2780cd3498
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:04:51 +0000

    body distant communication fold demonstrate

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
