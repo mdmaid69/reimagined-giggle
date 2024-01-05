ff8c296a0aa8c7000cf082b3c9ff36fab14fa498
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:05:39 +0000

    mess frequent sore quick school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+print(sum(range(10)))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
