6289f38712ac607c65d7ece9f5dd122b65bb4397
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:11:18 +0000

    class run town wave collect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def reverse_list(lst):
+        return lst[::-1]
