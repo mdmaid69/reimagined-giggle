9bbdd943fc57cf211df2a98ee7f3e497358f6759
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:30:13 +0000

    trap turnout that field flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_list(array):
+        return list(array)
