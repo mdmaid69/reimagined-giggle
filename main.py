afe20afcfa194b46200f4523f837759a38582315
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:44:11 +0000

    description Yy suck letter band

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
