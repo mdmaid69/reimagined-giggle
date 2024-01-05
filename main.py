66e39be18a16c80577acad9895a2068858fe1abd
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:16:52 +0000

    shame loss exchange train keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   def find_max(lst):
         return max(lst) if len(lst) != 0 else "List is empty"
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
