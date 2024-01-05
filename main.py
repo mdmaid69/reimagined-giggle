5bce2efeb06ac565c23439d8fe0617944b4d364d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:59:59 +0000

    display last hard number organize

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def count_elements(lst):
-        return len(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import collections
+def create_user_dict():
+        return collections.UserDict()
