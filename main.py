8337da839f51511de3a6f86989af62c7b4c52240
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:09:51 +0000

    criticize ever have whom thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
