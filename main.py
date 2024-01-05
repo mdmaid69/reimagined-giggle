bf7dc56dff926f925daccfa95daf6b4714431138
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:31:43 +0000

    head welcome personal drop journey

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
