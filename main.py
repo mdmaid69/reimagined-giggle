3005deb6918f3db03ff2ccc80ca51cab45e2d6de
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:24:11 +0000

    occasional form opposite direct careful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
+import sys
+def print_python_version():
+        print(sys.version)
