1628f0e720b97bbf17966f6f52fe80bbccccd251
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:27:20 +0000

    now attractive fast reply back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_octal(n):
+        return oct(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
