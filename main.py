b52f91e7e2cbc3127f808ee5920b5b23b7c5ae25
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:47:55 +0000

    quantity takeover corner demand unless

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_bytes_from_array(array):
+        return array.tobytes()
