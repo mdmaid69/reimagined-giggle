bc69a6a6b04b30e859ff345ee5d0fccea16a67a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:52:13 +0000

    foot wind cold from team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
