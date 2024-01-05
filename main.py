2f561a5018a92c211f3d12afd84c7836ab169f7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:49:26 +0000

    pretend develop field design smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
