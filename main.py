7a059f1d5e9770e47d37e960317f88eb1322d419
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:45:29 +0000

    compare by callout exact Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
