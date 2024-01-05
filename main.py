2b17dd728cba630848e56e2563f0ba2b4f345679
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:13:18 +0000

    necessary ahead noise steal thumb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
