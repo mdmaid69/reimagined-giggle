f8ba5ec9e4eb94e9a65d38f05299628e8838b828
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:56:26 +0000

    serve trick chase smooth total

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
