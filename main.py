66c3f986e782697b49e62ac48cb7a4c2ca4c678e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:40:04 +0000

    beyond throw curious power public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
