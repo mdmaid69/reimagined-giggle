196ddb3a56f804bae7d9caae1ff46766097e2620
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:19:29 +0000

    indeed can abroad fear keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
