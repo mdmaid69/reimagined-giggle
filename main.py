cec83d207b22b8db5ccf52911d43e5f8c305a751
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:35:36 +0000

    doubt cheat relax gate prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def count_characters(sentence):
+        return len(sentence)
