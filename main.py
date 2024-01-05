521c3dc5c232caac7c217fe0264b256a8a074dba
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:05:49 +0000

    upwards start base impressed opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
