5a677bb34b96e2fa48e8824e0a0709a5181db458
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:27:34 +0000

    goback(to) custom range unlucky call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
