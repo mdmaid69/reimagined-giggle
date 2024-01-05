a6962b152400f3d1d74f161b7da82132e5094ae4
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:08:02 +0000

    some left official word forward

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
 def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-print(os.getcwd())
+        return math.factorial(n)
