e5a7799da44fd8948e6d994ff6bd2c46992f9419
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:40:10 +0000

    complain Kk nor apologizeapologise odd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_str(array):
-        return str(array)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def count_elements(lst):
+        return len(lst)
