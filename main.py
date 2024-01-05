2ac4a1930d25b001d00804ab25329a7c6d2d11fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:21:46 +0000

    owe tradition now trap breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_work(force, distance):
+        return force * distance
