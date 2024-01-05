474b33ce83f3b87ce351fa50eb33f9b4b28cd781
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:37:41 +0000

    somewhere exploration piece jump turnback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
