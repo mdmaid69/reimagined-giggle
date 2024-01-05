f4de3be3f28735d6b9d5984e9b828608db44b071
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:43:21 +0000

    equal beyond operation Yy discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
