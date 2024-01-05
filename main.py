8171bb7481eafc4caa0da0e346ed722dc0340ad0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:25:47 +0000

    close lookup callup respect turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
