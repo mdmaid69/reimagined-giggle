2509cf3fe116fb4236224e4a3fbc8c9298fa79b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:34:57 +0000

    cover maintain concerning change wide

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def count_characters(sentence):
-        return len(sentence)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
