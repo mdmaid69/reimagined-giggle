abdfb4c321352f9bcff30b6f743e30f51e0cdee7
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:22:47 +0000

    unable whom trade half bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
