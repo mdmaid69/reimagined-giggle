e6d45f67a2a19478d33f432aa30326877f4415c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:07:44 +0000

    drink power mistake nearly cheek

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print(sum(range(10)))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
