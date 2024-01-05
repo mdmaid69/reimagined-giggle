e195546672e907d23cd02546a43434ac7ed0c4d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:51:37 +0000

    afterwards improvement computer this float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
