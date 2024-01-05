c11bb8eddbe1d97d5e906ee6e23051da13494519
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:08:21 +0000

    reduction pot degree wipe straight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
