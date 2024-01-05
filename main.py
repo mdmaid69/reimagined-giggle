727d4c1cd0464de551164e03c5d38189164f8db0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:57:57 +0000

    decision bringdown tired would fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
