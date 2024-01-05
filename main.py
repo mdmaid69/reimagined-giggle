d731cf9685f59b412218ee6c83c0a5fd4821770c
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:32:04 +0000

    care collection such scale cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
