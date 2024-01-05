7b571455f674f7a4a83fea66e8de81fe46f642c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:49:10 +0000

    cap obvious attempt outside nurse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
