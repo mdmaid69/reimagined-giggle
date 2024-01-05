83778330b95b1adcbe9bf4a5cd32983ad1e3e0bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:12 +0000

    move matter market wish themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
