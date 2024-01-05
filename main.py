a72aa8748c3a6c94781aa6262293449e2d71e131
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:58:16 +0000

    ball wrong kind official concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
