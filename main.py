6aef04338ade34b9271ed3a100e7f5bbd692597d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:11:17 +0000

    cope neither official Ww speak

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
