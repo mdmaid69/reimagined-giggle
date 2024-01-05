ae3f144a448573bc7cdd399596ebf0504dbd30e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:31:42 +0000

    hope wake friendly born wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def multiply_numbers(x, y):
+        return x * y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
