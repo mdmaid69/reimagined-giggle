792157b6013af7e08b0e65e6f7389020cc13f437
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:15:43 +0000

    determined train hour best noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
