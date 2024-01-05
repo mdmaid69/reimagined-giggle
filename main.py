e452d1b4b8af7ac4a4f814391756709372e8e2b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:16:28 +0000

    rule new capital example minute

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
