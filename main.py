d182acd2e4f983e7cf449323a3c8b5a3981fa923
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:43:39 +0000

    567enemy Cc cool work turnback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
