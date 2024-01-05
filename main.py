e2bf9b8fc1199ece52014caf2e632be815d94c1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:59:57 +0000

    scheme swing Qq mind cruel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
