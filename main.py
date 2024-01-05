e0b475063562cd939282f88363a8c8c3269c0814
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:03:05 +0000

    steer settle greeting handle uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+for i in range(5):
+        print(i)
