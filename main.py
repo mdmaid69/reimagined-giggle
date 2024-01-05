db9ddf6ace0b4f0d09de54f045fe0dc51dcef7fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:26:00 +0000

    science correction sister test chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
