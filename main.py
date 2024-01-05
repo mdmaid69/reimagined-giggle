b9bf19ae1584d3a2b06945078647ed37896d0bac
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:02:33 +0000

    heavy break test breathe furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def subtract_numbers(x, y):
+        return x - y
