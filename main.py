4d13f6ae04f663c078f05197e0a12e6938d14996
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:50:46 +0000

    thank rely child regional now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
