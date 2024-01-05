bf8c4d9f57be130abd3e4148871919b6d7047708
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:05:00 +0000

    test empty responsible to open

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
