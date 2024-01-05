de2dd01b3ad55473b9b02a99534a8d3f6e8eb10d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:52:59 +0000

    tight passenger stupid at our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def convert_to_binary(n):
+        return bin(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
