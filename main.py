ce1a5aa465efc07bcc09e7a99e94d775e97f0748
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:06:00 +0000

    feel impatient knock please period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_density(mass, volume):
+        return mass / volume
