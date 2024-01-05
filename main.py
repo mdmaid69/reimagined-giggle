d955c37f3ac31a7793ec472a1a7188ea1a7b0805
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:04:50 +0000

    speech takeaway rate offer punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
