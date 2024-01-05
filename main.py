e4a5b99e2d32227ef4571154c5cddf9d3dc3c0bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:10:52 +0000

    fast record minimum bright calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
