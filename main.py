283024e85f2cf52f6614263f16e7375477ccb762
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:15:00 +0000

    raw holdon vote remain cold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
