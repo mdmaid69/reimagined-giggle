67889caf7200bedce09c39e0e435271bf1b09d86
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:54 +0000

    card paper leave question free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def reverse_list(lst):
-        return lst[::-1]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import collections
+def create_queue():
+        return collections.deque()
