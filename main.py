9a430b9d3e55889b3f7b42422efe147afe208c53
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:48:36 +0000

    visit discuss wheel crazy cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
