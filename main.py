b23eaf682115b7c39b2b9497f87f10683ca17e4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:08:40 +0000

    along divide bin convinced after

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_itemsize(array):
+        return array.itemsize
