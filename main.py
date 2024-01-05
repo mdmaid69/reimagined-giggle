1d01c414a5cee471cd8fe8276094cc79fcd15bca
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:18:55 +0000

    guess lead become class direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
