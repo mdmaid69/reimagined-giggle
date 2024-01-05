b210826890e1a7e624787bdc7d2bf34b670636ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:36:25 +0000

    sport discover takeback by him

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+print(re.match("h.*o", "hello world"))
