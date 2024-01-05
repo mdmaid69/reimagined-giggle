02f7002ed7d4b47e3eb5d807c82311719e07d0d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:34:30 +0000

    opposite development office sudden reach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
