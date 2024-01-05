1956637011fbe7e091c3136ed37e1fdcb871f880
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:40:19 +0000

    stamp quality dry gas be

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
