6f4c18f39823f0ea3ef489c45c525e26c03fb288
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:57:16 +0000

    bringup she ill rid exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
