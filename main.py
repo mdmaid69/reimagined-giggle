3722e05bf6ab8fd51960fdc026dbf36566acd4fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:47:49 +0000

    eye among dance instant poor

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def subtract_numbers(x, y):
+        return x - y
