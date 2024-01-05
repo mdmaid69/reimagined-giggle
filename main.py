6209b0dd7a9406c894c76a33401c756ee5f8a336
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:30:08 +0000

    stick discover switch wander press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_str(array):
+        return str(array)
