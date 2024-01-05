5cc061fb90d54d92a2fc342697a985ebe4e07bd6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:11:07 +0000

    after swimming twist wherever joke

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
