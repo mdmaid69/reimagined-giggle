2232549bfca0160ee270b09dfc238d43f5ac5ec6
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:29:06 +0000

    they slice concentrate stage post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_speed(distance, time):
-        return distance / time
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_str(array):
+        return str(array)
