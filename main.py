0d610bb614d36c56de7d7507ad6b60ecd7ddc3a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:15:50 +0000

    wealth single teach project hungry

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
