3438e05ae9a2f126861a59b46f722e720f486a5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:59:53 +0000

    need emergency failure female deserve

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
