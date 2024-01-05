1fbee79bd30dd56d7816242b80e738262615e95f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:30:07 +0000

    classroom takeoff still artificial normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
