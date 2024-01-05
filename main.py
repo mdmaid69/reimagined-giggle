901f3d814f8a1c93c19f3168928c1854a68933c7
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:23:17 +0000

    relation half fault paper really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
