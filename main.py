034f741ad0e13db8e452da1d42a79d1dff2ea322
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:18:56 +0000

    game promise runover usual band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_set(array):
+        return set(array)
