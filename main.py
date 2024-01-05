3c47fec2528d86ba56b4486a2e9576dd5079234a
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:19:12 +0000

    patient have lay cruelty them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
