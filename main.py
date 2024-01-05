2d2dd287c5d2590fb16197f2bdd6b0202d0048b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:21:23 +0000

    rob knowledge mystery failure bottom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
