93abae3266a668037555156751a89620c5d52e6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:56:28 +0000

    connected kill bath paper they

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
