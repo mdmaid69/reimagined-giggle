13c6f6f3eb082f996bcb053392ee7624d42f0976
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:13 +0000

    so model whisper contact sudden

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_list(lst):
+        return sorted(lst)
