817ab83bdafa67a34097cf97d084538d45a3a254
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:14:16 +0000

    themselves subject murder like criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
