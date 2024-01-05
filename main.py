22710a8ab55ef7f35768882344d344c44fb2e263
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:39:59 +0000

    sure programme method about Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
