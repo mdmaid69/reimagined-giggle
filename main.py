a86e5ed58d4ce7c8c1f352bdd1846f291cbc5eea
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:44:04 +0000

    table reliable equal like how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
