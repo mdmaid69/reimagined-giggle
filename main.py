be641d1b0d849b72f275bcebbbf68274e5192b17
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:26:23 +0000

    tour honest recommend radio like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
