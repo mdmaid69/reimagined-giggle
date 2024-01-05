2d71499a2250c8b7c81310349b7f41ca6a3039b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:56:49 +0000

    touch head beginning report destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_odd(n):
+        return n % 2 != 0
