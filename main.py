fe3a9935e92e6d0c456479ddbb39235c42488a2a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:16:30 +0000

    when success basic description again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import datetime
+print(datetime.datetime.now())
