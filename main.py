bafc7ae1cddecef13f3fda02736e7cc223e35731
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:32:35 +0000

    bang anybody sorry ocean achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_set(array):
+        return set(array)
+def cube_number(x):
+        return x**3
