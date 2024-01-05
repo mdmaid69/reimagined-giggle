6c26fe8e91bbcb3f0af835a2bf9d2c46d6e64ebb
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:33:19 +0000

    shame valuable anything convince between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
