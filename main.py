4ab986ddff849438f9f50d76054815e0b678b015
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:26:26 +0000

    average owe conclusion familiar part

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
