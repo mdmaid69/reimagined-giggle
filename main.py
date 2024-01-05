11b6de2eb2c92657bd891283711bd08f3c3be070
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:14:42 +0000

    earn demonstrate further equipment Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def is_odd(n):
+        return n % 2 != 0
