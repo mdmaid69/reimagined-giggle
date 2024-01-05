7c70491c9d68a1ee4de2daca2d34012b59999605
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:34:58 +0000

    arrangement now waste spray score

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
