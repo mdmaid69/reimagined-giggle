22023dc504059832cf9d98b6bc90bf1437627f0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:13:05 +0000

    everywhere fight experience roll comeround

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
