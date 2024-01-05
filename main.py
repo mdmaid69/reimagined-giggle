309ec159e841ae86b767239dc9595d4aae99f44c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:08:34 +0000

    support murder dream belief clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
