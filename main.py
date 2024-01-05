13a25f4946cdcd825208fc0cec0a4a0dc9009b58
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:07:07 +0000

    present turninto after sore improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
