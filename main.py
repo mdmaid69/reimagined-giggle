4a28749dccf39f33641c4c5376e75c547b18b6b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:42:58 +0000

    drop effect 179bite wheel inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
