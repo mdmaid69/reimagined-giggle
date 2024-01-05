793f713b8f809816cf71d1d7248f75791003ad6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:08:22 +0000

    frighten attract progress modern process

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
