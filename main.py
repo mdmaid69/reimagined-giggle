73069572d31cb87f66b8b935f01b8acfc89bb789
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:24:15 +0000

    fair instruction fight early family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def reverse_list(lst):
+        return lst[::-1]
