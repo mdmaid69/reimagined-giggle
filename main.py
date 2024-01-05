1ffcd3dcbfd850902fd83795be4555c36c1a3f3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:52:57 +0000

    little month frightened supply thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
