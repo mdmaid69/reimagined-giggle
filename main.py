042cbf3407d2e0229c97276f5c36d68abae283bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:56:26 +0000

    lead there safety determined holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_volume(length, width, height):
+        return length * width * height
