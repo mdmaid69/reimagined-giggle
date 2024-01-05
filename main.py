0d3f375794accc51598fa7f0b34ea9d44c14b605
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:28:55 +0000

    note nurse approval ignore rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def convert_array_to_list(array):
+        return array.tolist()
