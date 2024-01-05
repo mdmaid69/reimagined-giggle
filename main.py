6661263e525ae4e446e4f8cd72fe93c61dbfcd66
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:59:05 +0000

    common union represent style basically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
