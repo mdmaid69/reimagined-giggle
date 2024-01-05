eec6830dddccd64302e0fcc49823c2b633f46e66
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:50:22 +0000

    crazy especially real public succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
