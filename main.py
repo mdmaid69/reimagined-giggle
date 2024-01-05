42cf6324a4616b8457b4d03a8c1724c7bbf2c674
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:35:49 +0000

    crazy wake ask thinkthrough welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
