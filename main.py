d89d6a17ddd2c67b7634b3ae1b61462a13ce95c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:24:53 +0000

    rare hurry move pen day

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_list_from_array(array):
+        return array.tolist()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
