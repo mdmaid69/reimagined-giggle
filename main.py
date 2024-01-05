a87e8f4efc3360dd5950d1615090492dda438698
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:25:29 +0000

    man glass partner mine dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
