d6f5cb61eaf7f9c25bc598c0d9596d7a4cc9c3c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:23:36 +0000

    street mouth share everybody replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
