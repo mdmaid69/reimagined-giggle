4e62b902233c42b4748d9a1d955db611f633724d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:30:43 +0000

    day exactly like interfere certainly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_min(lst):
+        return min(lst)
