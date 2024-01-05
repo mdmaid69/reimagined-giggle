8d43ad9338bf11d86bdce79774aec2b847c3b6ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:06:19 +0000

    group forgive frequent ourselves once

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
