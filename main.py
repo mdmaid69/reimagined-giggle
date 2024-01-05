27e2f67afe39dd09efc0699ae1c0d93f0f5df8be
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:31:48 +0000

    rank pupil fall when production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
