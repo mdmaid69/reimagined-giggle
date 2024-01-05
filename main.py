f725613501b911e0a4a1d9e33194604ed890b89c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:03:35 +0000

    busy even quick correct use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
