0c5ac1c127baeae776b5758540df280c253abff4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:28:45 +0000

    calm big of guide coffee

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
