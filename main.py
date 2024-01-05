18aa7b6a919e86136fd97c02345842053e4f9999
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:53:25 +0000

    change medium makeinto cry before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+for i in range(5):
+        print(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
