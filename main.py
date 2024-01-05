d66599281f184eadc381103b366444274be8d0ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:59:07 +0000

    Ff prefer means alive put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_list(array):
+        return list(array)
