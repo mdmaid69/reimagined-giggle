5f6ada3d36b95aba2bff783343c5c12fe6c55191
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:31:19 +0000

    describe himself intend bringback declare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_error_function(x):
+        return math.erf(x)
