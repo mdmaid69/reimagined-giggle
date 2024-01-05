0b531baf8a668055d5432482e1b4fb8f68028caf
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:38:03 +0000

    carry block trial model high

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def subtract_numbers(x, y):
-        return x - y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_list_from_array(array):
+        return array.tolist()
