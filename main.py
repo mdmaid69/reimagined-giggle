8c4bd7f44e4dd457665552177bb712c04ce638a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:00:06 +0000

    far shade Oo progress turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
