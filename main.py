9db45490973cbea7e10b84403d53408805b417d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:02:29 +0000

    bus score accident win themselves

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
