23d1eba5f9f2943e0f897f3e14f4477829cc9fb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:58:37 +0000

    properly bill callup sore along

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
