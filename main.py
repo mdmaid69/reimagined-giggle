4aded19385a213a1aef9bcf2c569e99894fe206b
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:04:44 +0000

    it owe disgusting summer female

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
