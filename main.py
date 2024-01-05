838d74f1a76fe05463f0cf36bac0de671bc8aed0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:27:57 +0000

    cool failure large cool pride

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
