8063b9af31e54b6ce35e3aec9288a57f4cabb23f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:45:16 +0000

    scale allow admire Yy fire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
