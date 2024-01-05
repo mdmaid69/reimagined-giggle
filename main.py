ecfe6394db5b3a20760542f4b9e14002c105e316
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:33:20 +0000

    far relative of fight teach

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
