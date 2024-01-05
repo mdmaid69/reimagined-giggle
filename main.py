44442e12ffd5093fbd4104d053d7336fdc83405c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:04:54 +0000

    circumstance apart mention can sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
