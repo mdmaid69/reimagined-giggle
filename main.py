2fe6c223711ad37861bc132db119826e11bf5ead
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:41:52 +0000

    myself downstairs sell evidence send

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
