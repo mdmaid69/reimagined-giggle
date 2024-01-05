d2a4a1aca06c7946480c55e1f3d71011a31ca308
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:35:42 +0000

    low pretend begin this section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
