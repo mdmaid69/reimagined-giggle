dc7c9344f4a684e7f03e7e329a9af782bbf52952
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:22:37 +0000

    community beside runinto proud instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
