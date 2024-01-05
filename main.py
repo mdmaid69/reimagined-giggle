4a381f0af771b7b3612532d746757d45137737bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:02:19 +0000

    shop getaway reserve cheat holdout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print("Hello, world!")
