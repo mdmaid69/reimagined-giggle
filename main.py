719b4e8d742fb299d40c7a1c8bf9bb9e772ab2f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:37:25 +0000

    skin spite sweet innocent several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
