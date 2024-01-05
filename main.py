392e34448635d3e2176e78653ee56afbebc0d59b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:44:46 +0000

    commit front ashamed goout smooth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
