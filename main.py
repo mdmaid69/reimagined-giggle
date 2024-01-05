a58b4db609e393cc5f8a68404e3b9e089ef09cc7
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:09:25 +0000

    black file back squash nigga

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
