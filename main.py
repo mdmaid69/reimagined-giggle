3fcd440c35effd5c44b977f4fc63c3c942202c21
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:52:44 +0000

    rush enthusiastic ability develop crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
