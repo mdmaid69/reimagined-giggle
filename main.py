d9f780dc2cb07414ee943b08d15869a50d16d80d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:23 +0000

    shelter cut train hope guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
