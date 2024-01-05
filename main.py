f9137b6ff94bd29040e78d0302c9e92e5a99f823
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:11:04 +0000

    computer safety bye direction 568energy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
+def add_numbers(x, y):
+        return x + y
