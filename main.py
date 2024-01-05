4ed72358131b5c3b358ff384b6717872e8bd861f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:57:39 +0000

    workout anyone lot dress crash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
