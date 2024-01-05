298671c1a05ed332b0806965c8e2095e7a83dfd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:05:27 +0000

    plan alone politics thinkof married

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import random
-print(random.randint(0, 100))
-print(sum(range(10)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
