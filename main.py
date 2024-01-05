44a57044d98b4abc89c291ae11846c977a6c9e47
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:46:23 +0000

    on treat mistake passage also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
