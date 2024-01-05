2850509e73370540c28a518345822a8ab7a7d928
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:25:59 +0000

    little bringup defend mistake purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
