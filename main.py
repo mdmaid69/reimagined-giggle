1f4c9c05baf4d4b991318584e82dd22d80c4a852
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:26:30 +0000

    total them cheat settle organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
