6782f276fab64af3f59050a66b357e59ae1bea91
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:41:14 +0000

    bush over murder laugh sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
