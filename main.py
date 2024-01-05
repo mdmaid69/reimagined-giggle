b31cc24a96b680660a7e8ecea3d52dca5bfe74e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:26:22 +0000

    Bb estimate modern out become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_speed(distance, time):
+        return distance / time
