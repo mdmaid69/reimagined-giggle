f737d8d0251650c08e660baa38cc7e0c6fd5c44c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:49:55 +0000

    scare disturb favour sharp force

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_speed(distance, time):
+        return distance / time
