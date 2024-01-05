324d0a306add0ffffa4722e76447af153e8c2df5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:46 +0000

    habit travel drink quit getout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def convert_to_hex(n):
-        return hex(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
