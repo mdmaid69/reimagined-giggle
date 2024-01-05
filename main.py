e476c316bb5931722497548e75d17fbac0403714
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:12:43 +0000

    sweep autumn force warning expect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
