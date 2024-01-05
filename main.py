f61f13643d08e9b27870aac85a7468dfa4d1d863
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:51:56 +0000

    attitude lean motion moon nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def count_words(sentence):
+        return len(sentence.split())
