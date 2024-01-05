4d8573da48bfee87be2ddcb25a6d89f914837fed
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:16:52 +0000

    argument spot obvious skill actual

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_float(array):
+        return float(array[0])
