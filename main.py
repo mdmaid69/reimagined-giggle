b11288f266d8859cdf4e551208b465e0abf3679d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:06:41 +0000

    like whichever speech reason destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
