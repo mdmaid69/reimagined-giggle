71d7bc40890491c2b0b1305c23e40793bbdb21ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:33:33 +0000

    eat tone lend married definite

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def reverse_list(lst):
+        return lst[::-1]
