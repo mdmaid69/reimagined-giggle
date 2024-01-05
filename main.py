e3ffb79ea20d9e3b059d95b9b71d6a341a1a766c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:09:26 +0000

    habit scale still unless rate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
