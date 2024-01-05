2e070cd456833eb18cfc94c411126c9df8d676db
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:00:37 +0000

    equipment number busy score break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
