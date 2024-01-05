47d54f023ff374fff04f8e750a2aa415d73d2bc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:55:52 +0000

    him strike persuade regular definite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
