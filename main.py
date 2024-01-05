ad506201d5ea0cb37e2bf135a043a1ec2efc17f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:29:59 +0000

    dream wide success total far

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
