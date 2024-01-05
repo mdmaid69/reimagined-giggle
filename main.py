8c08026709039f6209a4dbb63cd56bc4f4081ee1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:27:39 +0000

    explain worry characteristic crowded thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
