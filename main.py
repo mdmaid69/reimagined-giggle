8f824ab06f2f4cecdf2edb9682328002a30f8647
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:48 +0000

    little kind bank fortunate long

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
