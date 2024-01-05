7ecc67a5fd9cf9910b8d7949196c19be39166704
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:56:38 +0000

    comeround to disturbing then lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
