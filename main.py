07506b2a2bf638705523c5dcdc098f7e15d32108
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:48:12 +0000

    movement sudden attempt comeround make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
