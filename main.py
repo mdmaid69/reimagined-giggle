4afe63368c0805f302151e9494f477dc0eaf8d8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:44:27 +0000

    makeinto coffee hold share touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_set(array):
+        return set(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
