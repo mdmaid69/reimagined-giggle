184f07a0d1ccea150080acdbbf2f8ec13e7f387f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:05:48 +0000

    lock likely use friend transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
