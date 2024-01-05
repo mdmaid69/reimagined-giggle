4fd6ff50c573465080f2a9e86508b5e9083c10c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:06:22 +0000

    purpose face force smell general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
