9ce37c6edbea39d783623ffb375f43b73c42ad8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:06:36 +0000

    distance shame happy best negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
