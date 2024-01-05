ee4ffba7ebfab61918ac24aa9b7e2c569ceb64a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:41:13 +0000

    bored road steady back yours

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_octal(n):
+        return oct(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
