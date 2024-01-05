8fdf9ace89ae1191ced712945d7b6fb35ab77a58
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:36:48 +0000

    limit afterwards mother question account

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_work(force, distance):
+        return force * distance
