6b69827fb8c2e9467d8e8280a259a12db9a23682
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:32:35 +0000

    takeaway together harm load worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
