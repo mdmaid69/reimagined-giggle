7f5df9151b8b11668f945c0932409caf7358e088
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:29:00 +0000

    close lean cry top possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
