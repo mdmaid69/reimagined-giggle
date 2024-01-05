744713111eab8a894bb23005f00687618b92b539
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:42:40 +0000

    past pretty admit slowly computer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
