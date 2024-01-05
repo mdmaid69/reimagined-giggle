af57babfc25859dff57194be8f571715f431ea41
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:42:21 +0000

    hard arrange wet dish accident

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
