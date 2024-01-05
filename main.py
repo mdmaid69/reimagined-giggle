f78ab6fb462cda464036dd62a86334983856187f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:05:21 +0000

    react trip letter middle large

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
