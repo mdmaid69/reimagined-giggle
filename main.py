0db2f897edb4377db718bb5983085ae7f6696c17
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:36:45 +0000

    announce light hope shake disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import time
+def get_current_time():
+        return time.ctime()
