9e7b6d9bd2d78aed6ed25e9b06c112f8c68ffd90
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:28:24 +0000

    anxious mine hang breathe strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
