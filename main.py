9ab518ae1c7303b5aaa333ece1ef2857180a597b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:15:05 +0000

    sort prevent afford wander discover

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def get_array_itemsize(array):
+        return array.itemsize
