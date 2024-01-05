2276602a8dcee9c135635fc3ae385ad7a30ac631
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:58:06 +0000

    lend lay legal traffic lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
