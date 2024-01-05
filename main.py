255669b19dc0cf22bd0be75a6ca41264d8440dd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:54:55 +0000

    several clever fact memory way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
