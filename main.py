54ed2981efe2c443a855d6df0c5bd5d5f4b282e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:23:57 +0000

    moment concern certain quality harm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
