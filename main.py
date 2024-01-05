9e763f6913b1dcf66494f88ec557ec325052c27c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:07:50 +0000

    represent connect talkinto address attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
