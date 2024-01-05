d7a9be4b3a7be9bbf0df913151c9d54ab3f78e7b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:13:34 +0000

    form increase escape mention pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
