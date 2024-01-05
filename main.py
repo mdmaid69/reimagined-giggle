b08698774f16c0bc9ae0723f4b47f44721475d73
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:29:25 +0000

    hot meal blow count environment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def square_number(x):
-        return x**2
+import numpy as np
+print(np.array([1, 2, 3]))
+def divide_numbers(x, y):
+        return x / y
