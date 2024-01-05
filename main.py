6dd82d4b2abd8f5d1a26fadb243d0cab5874a9c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:15:56 +0000

    centre stir union excuse table

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
