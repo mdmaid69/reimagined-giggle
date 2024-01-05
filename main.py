2738514c964bcef893ce8720aedd3e131ec4e623
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:16:19 +0000

    first ordinary hand guard surprise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
