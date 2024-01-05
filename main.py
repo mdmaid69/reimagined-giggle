9eac9712588b02470b43c47bc38658c6e9b9401f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:27:15 +0000

    mark test both plain bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
