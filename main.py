6379ae3dc2af0463aa3a6bff631c78a2886d5d6a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:59:29 +0000

    length advantage deliver considering worth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
