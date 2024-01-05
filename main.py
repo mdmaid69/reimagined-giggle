eb4a12b447f945b5af540d3d04eefc08af4592a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:10:36 +0000

    interested ignore light attention score

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
