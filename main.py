0e36c25361d3b51bdead123f374105e10d1eafd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:21:41 +0000

    ear check flavour field see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
