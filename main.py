e9f490caf5c668bdda08cfb947b22d35453e8bcd
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:25:40 +0000

    Rr satisfactory interview forward show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
