33ae4a88d79370aaa9d01b9c7ef4cd5a49f41ad8
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:23:24 +0000

    prefer slowly bringdown neck born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def cube_number(x):
+        return x**3
