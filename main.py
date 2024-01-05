46ffb896fcac2cf675d2d24c22442aa5ab23c401
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:20:38 +0000

    record offer with why desperate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-def find_min(numbers):
-        return min(numbers)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
