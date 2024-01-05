8528ac62cfc2ded5e70983706c1b055c93b19b75
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:06:43 +0000

    nurse born as brave basically

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
