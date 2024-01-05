f6aa5665675f83a82b7ead545c63b5a59b7be038
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:08:30 +0000

    trip over warm represent lie

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import datetime
+def get_today_date():
+        return datetime.date.today()
