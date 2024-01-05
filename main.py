b19be0310d157d097b1540a9088fe7121e15bdbf
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:55:34 +0000

    frame principle boil head roll

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_int(array):
+        return int(array[0])
