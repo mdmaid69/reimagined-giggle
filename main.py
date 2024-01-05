0fa768cd27f709b4dcb3ea12eb63215476742e73
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:53:45 +0000

    niggaacademy understand search common also

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
