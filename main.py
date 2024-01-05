9cd5157e4b743a99e69b127bfbed12fe351a068f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:48:12 +0000

    offence whistle instant allow progress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_pressure(force, area):
+        return force / area
