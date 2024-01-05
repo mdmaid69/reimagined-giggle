3c499f87d959d352c7aa28af265f9d1c5c76622c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:23:07 +0000

    increase light on clean keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import sys
-def exit_program():
-        sys.exit()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
