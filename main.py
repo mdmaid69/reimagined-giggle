808ee111f5b8ec85c370ae58d8a3edc2ca34be29
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:22:30 +0000

    practise manage tip unit suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
