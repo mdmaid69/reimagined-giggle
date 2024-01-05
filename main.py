151247729ebc6ef67821978e30d1e93b3f0b2cb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:43:21 +0000

    better concentration house frame disappointed

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_base_name(path):
+        return os.path.basename(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
