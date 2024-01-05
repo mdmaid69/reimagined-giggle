cecc155a67d36f228b997d133823db8c76fa2005
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:13:15 +0000

    straight maybe consist schedule with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_exponential(x):
+        return math.exp(x)
+def is_odd(n):
+        return n % 2 != 0
