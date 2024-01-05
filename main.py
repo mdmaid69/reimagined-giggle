2dc127009a3995dfdb57538b272b2e193f185145
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:47:33 +0000

    continue display seem quality travel

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
