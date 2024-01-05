6060816828ba27f46c61b1859ffb0612c5b6fc47
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:15:29 +0000

    goback(to) left consult suckme fix

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
