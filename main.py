fbc93bfceaf765aa5dcd183c69c515d93dddb881
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:02:31 +0000

    approve insist number Ll abroad

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
