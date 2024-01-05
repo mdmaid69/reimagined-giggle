2b07b14eea144a9bbe88dc7f4ac95abd05ac7a84
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:08:04 +0000

    drawing earn previous nurse breathe

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import numpy as np
+print(np.array([1, 2, 3]))
