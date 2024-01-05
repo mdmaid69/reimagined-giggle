3cb59cacd598b6c4ba424e2ac76eacdfc84402b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:30:59 +0000

    hear test anybody gradually design

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-print(os.getcwd())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
