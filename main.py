da1d8b481111e6b714d2859edb71b330f95e513a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:25:04 +0000

    set common grade angry target

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
