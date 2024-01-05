04ccc3ea6b957997cbef00c4a8f33c8e796e6de1
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:31:11 +0000

    push cross road eat responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
