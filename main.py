04db45063b1af1dbbb14e4a0d06b0e399dc6f4ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:33:02 +0000

    perhaps bang record finally backwards

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
