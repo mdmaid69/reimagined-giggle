1079c687cc4482fe973bbd31e5555f514b9e3ad6
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:50:41 +0000

    suit its family takeoff anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
