25e741306cd4acab6a94795365432398a3650b5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:39:04 +0000

    injury round contain likely journey

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
