a8632bea4528ec6080f21f261c886a2d9d61e299
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:47:17 +0000

    away trip deliberately wherever gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
