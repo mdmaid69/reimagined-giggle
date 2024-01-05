784847c17a19fe1d31e453563c26d6f6d73b2170
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:45:25 +0000

    sentence silent violence great position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
