bd78935f3dc511d672764fdc1f9179cd40802ae6
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:29:45 +0000

    impression constantly university thirsty brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_python_version():
-        return platform.python_version()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def reverse_list(lst):
+        return lst[::-1]
