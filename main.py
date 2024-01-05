8e7396e6235fd298dfb6089ef87cb6b109295a87
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:39:03 +0000

    hard pin either you delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+def log_message(message):
+        logging.info(message)
