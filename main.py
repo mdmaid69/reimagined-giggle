d69be53bdb7bba817b6a465fdb1d31888a023b8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:28:05 +0000

    convinced fit knowledge ability apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
