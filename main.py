bf39f3ddb0da6c7f98983f49b34e92da90073ff4
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:24:07 +0000

    collection rank drop comeup short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_floor(x):
+        return math.floor(x)
