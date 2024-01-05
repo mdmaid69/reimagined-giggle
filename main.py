da73a8b3865002ed16280314967f4e58c37b89a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:00:51 +0000

    treatment sit work emotion repeat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def is_odd(n):
+        return n % 2 != 0
