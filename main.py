1fc4dc8d4a85cbc90406b743e0924a8b76be8090
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:27:24 +0000

    convincing beneath or snow lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
