7d80b5cc47aad813d1e2ace0576159df0e1cb834
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:10:08 +0000

    violence previous cruelty task circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
