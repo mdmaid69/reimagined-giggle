673d03c2f0ef542cebe890cc6ba2049f2df9a763
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:46:15 +0000

    wish insist cruelty anxious phone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
