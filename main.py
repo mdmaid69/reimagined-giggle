83f0cbcc5571487759fe1de7e74127735db92fb7
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:01:00 +0000

    hungry shall put plain pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def reverse_array(array):
-        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
