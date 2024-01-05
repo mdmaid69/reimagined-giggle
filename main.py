5f6c046b62cf736802b180306a1b45ec901aa9be
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:05:57 +0000

    bang limit test exaggerate pocket

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
