a30b29601fef0298e649016802fa91a7f135aea7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:28:45 +0000

    bush put callup height keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
