b27a312896934b4f8b011c32fbaacfd520916715
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:14:35 +0000

    face like partner everybody rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
