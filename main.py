4a6b4b614ec5f8120a236ddf811ecbb955611e67
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:30:55 +0000

    partner design wise whether grateful

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
