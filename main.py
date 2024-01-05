b076aa5db22a2cd847d7c94984976b5662d63d04
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:05:51 +0000

    bridge opportunity feature meet shake

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
