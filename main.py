77532bff4808fee4fe53ceafd055284e709ee814
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:37:13 +0000

    material crisis stay popular sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_string_from_array(array):
+        return array.tobytes()
