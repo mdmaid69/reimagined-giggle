b33d9ddfc675fc293926054fec4f2f298227deac
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:07:17 +0000

    relation rely image drive protest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
