8acb0e991a4e50a95ada94935411871c38d9263e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:39:33 +0000

    base no raise tough lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
