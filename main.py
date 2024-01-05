bc877b91dfbae02620178db20c3bc9c7ceb4e1cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:16:31 +0000

    build ill store cost quit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def roll_die():
-        return random.randint(1, 6)
+import collections
+def create_user_list():
+        return collections.UserList()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
