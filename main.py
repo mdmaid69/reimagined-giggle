114d5b041ba1efcd72a19d846141d36362fdf41f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:47:43 +0000

    later declare present address wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def is_even(n):
+        return n % 2 == 0
