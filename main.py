2a0469a4892ef5120b31a89246aae7c0c7b1be7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:10:05 +0000

    mainly everyone promise ignore rid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
