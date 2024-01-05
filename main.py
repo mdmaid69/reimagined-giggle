93f039fd11c69f344de2a8b429accf46a9097fbe
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:42:49 +0000

    soil ignore throughout whether beyond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
