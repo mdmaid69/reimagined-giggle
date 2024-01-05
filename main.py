8d8b15dfac2889f308d1edf4d1b27aac85cc5ed9
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:49:04 +0000

    field unfair rush score tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
