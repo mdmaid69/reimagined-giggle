a4c4e378d23d909e5108f3bb9b3bd3b8d9111de8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:54:43 +0000

    turndown life band OKorokay mood

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+def convert_to_binary(n):
+        return bin(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
