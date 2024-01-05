1a8d1571c1745a0f2f24b5b8c8bfbad965b6afac
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:06:29 +0000

    slow point from university many

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
