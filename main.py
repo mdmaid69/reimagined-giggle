8aedf278b3f505303fb959dde52e14117419524d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:52:56 +0000

    matter present march crash slip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def divide_numbers(x, y):
+        return x / y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
