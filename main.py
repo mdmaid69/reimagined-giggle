3429be7d163f70b36842e0ec861da4d18e7a07c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:12:18 +0000

    feed lookback(on) suck thought downstairs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def is_odd(n):
+        return n % 2 != 0
