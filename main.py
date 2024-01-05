86e4bba0daa97a302c077c4df1f5f206d38d98a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:50:24 +0000

    chat rush birthday regret spoil

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
