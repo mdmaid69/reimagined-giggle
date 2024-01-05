dd9235a56ee6576bdf806c2b27009b73f654ec5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:51 +0000

    takeover catch smoke word comeover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
