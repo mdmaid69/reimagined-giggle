45d20a865341609b990d167e0748c11d4beaf97f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:46:07 +0000

    injury corner stretch hide healthy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
