517dd890b8717dc59018ab60fbf8f47d5442c2b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:59:54 +0000

    walk treat male add top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_as_str(array):
-        return str(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
