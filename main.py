1990d8fae55613f154a52edc96065c95e1aded78
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:56:55 +0000

    health impressed interfere concentration service

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
