e2f3dc1e402cebd6dd4254189ef4f21274c18d5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:38:41 +0000

    attempt busy foot sink unlikely

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import numpy as np
-print(np.array([1, 2, 3]))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
