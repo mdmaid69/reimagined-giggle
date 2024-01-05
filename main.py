9fd93109fa080e1cea8e2b97492dc8e290f4d12b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:02:26 +0000

    brown runover fail present alive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
