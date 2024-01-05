be3c21bb130b93c00f0589d2d30179972c8180bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:44:27 +0000

    bang have giveback film include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
