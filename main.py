cbcaf22ae6992278ede5946e64b2c0521cf43eb6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:25:25 +0000

    about mix fire ball scrape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
