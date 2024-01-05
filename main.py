a49d8112e61220f19b4faa35726c7451fe64022d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:33:31 +0000

    disturbing worried breakfast result since

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
