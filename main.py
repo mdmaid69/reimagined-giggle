c2915d70284a1fe8cc05965c2d41177b903fd2a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:00:43 +0000

    address sound forever exactly cry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def add_numbers(x, y):
-        return x + y
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
