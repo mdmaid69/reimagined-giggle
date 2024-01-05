b9f2ad4e59c4184469d08534a20034faceadc647
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:15:12 +0000

    off confidence instant sure lookup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
