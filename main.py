2eccbfeb65aabb27a69f3c34be51be26b4698792
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:31:22 +0000

    radio guard person regard fashion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
