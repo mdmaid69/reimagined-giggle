a63a3d65e74a6a07c9c104ba97389bc1d0de1577
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:19:42 +0000

    related history compare report goout

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
