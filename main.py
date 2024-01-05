73154197b481517b5a91d7decc2f8f04924590dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:37:37 +0000

    promise common rent give sound

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
