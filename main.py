e107273a816352ee76360298b0e400a1de372992
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:11:40 +0000

    contrast somebody correct rather quantity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def convert_to_octal(n):
-        return oct(n)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
