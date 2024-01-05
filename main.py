1010c31cb75204a50f6fc827fab00b91ef2c898a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:15:12 +0000

    mess exercise connect use search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
