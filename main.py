c6547564d7891c388c2e4fdf69bcfa1c39ac358f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:43:37 +0000

    obvious know Ff used Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
