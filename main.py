83005d8fc468bbee70e9509dc2d47954fedf9df4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:42 +0000

    closely step rare short Bb

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
