a005becbbbb6e71dc611738425e3298c78a60f16
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:59:46 +0000

    custom twist swimming hardly education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
