9c8acebadbc19aa40ed949f1f6876e9a624761ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:16 +0000

    accidental emotion contain Cc doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
