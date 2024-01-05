3f57ff9e7cc420665354b84ea0702246f43eaa05
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:45:58 +0000

    mess book form together shame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
