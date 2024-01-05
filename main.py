ca242d12e240bb92849a86bb539c14807d121da0
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:26:13 +0000

    sure square greeting open nose

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
