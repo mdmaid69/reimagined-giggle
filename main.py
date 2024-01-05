1b19448cad0819c556ba70b322c35529e22fb0f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:07:49 +0000

    dangerous beneath measure lonely design

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
