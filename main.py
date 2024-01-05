746bd9f30632b1ed0e6d53bc5c0dc71ae99de681
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:38:24 +0000

    ride language paper law side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
