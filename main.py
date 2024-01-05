d9ee57423fcf98c83b7017aae722bda61b5bc445
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:22:17 +0000

    present our everywhere waste word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
