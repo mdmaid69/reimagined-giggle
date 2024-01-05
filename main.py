1d489d392fd0849d0cabd1479a01c433ce6567a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:23:16 +0000

    quarter weigh Tt apologizeapologise open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
