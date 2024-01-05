b291f075bfad07e835a6279cab13c94b392cdb78
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:23:25 +0000

    weather taste badly book blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
