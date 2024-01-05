6f2865bd8f8c10aaa978b346a8483b11ddd9ffdf
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:35:55 +0000

    trousers anywayanyhow die nowhere match

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
