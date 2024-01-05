f49b2114f8ddd76d3e71b81655414726dc25cf51
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:00:45 +0000

    thought find benefit stand save

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
