946f2698d1aa13f8eb6628c5ff466e13544de63f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:55:38 +0000

    between and shrink load thumb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
