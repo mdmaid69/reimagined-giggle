84b7338dc71db41fedd849e5728feac0b1465bc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:34:09 +0000

    always future breakinto serve keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
