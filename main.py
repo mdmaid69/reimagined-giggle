feb9d23c8016b6a459a5d1b84b2be1737f9f3bec
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:24:43 +0000

    company copy century Yy careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
