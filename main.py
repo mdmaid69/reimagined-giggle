77ecaa69563477fe2c1727d3030255078f576f5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:32:00 +0000

    none prevent substance simple whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
