7b6c9be6738a6bac2867a90bbbe00652f3bd9997
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:26:40 +0000

    lost mystery borrow fear recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
