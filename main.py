042cda65161c934810dfa354bd4424e54a030190
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:06:42 +0000

    drive burn favourite ground church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
