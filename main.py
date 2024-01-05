c44f2f10f4bec1c308b1b02f583e35772aa1a1e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:54:31 +0000

    better combination style rely who

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
