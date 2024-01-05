6970d38c9f262b1bfe0fb6953c74e963d0c38a9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:12:11 +0000

    land break warning tour right

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
