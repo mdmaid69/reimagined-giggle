53720550a1106b3554913d1a794c75dae693102f
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:19:11 +0000

    distance fight demonstrate extremely fair

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
