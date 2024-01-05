775c296668436771b8da3a0ecdab957aa36e8735
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:33 +0000

    dry yet score square relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
