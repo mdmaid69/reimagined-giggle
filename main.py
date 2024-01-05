d300f6105ba8297a09b8a8dd86271b5844bc3cbb
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:40:58 +0000

    operate curious place now towards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
