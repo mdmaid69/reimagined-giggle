9c0a644af4a8ed5cc9aaa32eefe3e5aba2319128
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:15:07 +0000

    another any weather nice smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
