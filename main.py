154c11571f9ed30ba89f51b25fc0942bf062429f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:03:42 +0000

    safe really fence previous free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
