d9edff8de8badc6afcab37b558ac10e7205666a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:58:58 +0000

    study murder mixture the trial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
