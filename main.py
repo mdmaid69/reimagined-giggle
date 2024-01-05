14e5934c112a47ef749a0c43ac905125aaca4e86
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:28:36 +0000

    determined every keepoff fire plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
