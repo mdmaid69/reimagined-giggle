7de2eb152415dc36e3e431dd699de9a1171152f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:20:30 +0000

    until bet national nigga disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
