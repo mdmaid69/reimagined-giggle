07f3e3bfd1e269c9e24f2ea92f77636a9af17880
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:42:17 +0000

    Yy operation excuse tomorrow thing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+def get_current_time():
+        return time.time()
