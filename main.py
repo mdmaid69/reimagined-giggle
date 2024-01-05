91688e7bf57850892b516cbf635a37074aad3980
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:55:51 +0000

    opposite group with scared begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
