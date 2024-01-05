62f95a61f23354a77cf386fabb0cc23d03bfd4a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:09:36 +0000

    complete experience front suggest which

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
