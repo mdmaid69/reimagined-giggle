d100d90eaedd637c0e6cf9addccd635946280a44
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:49:23 +0000

    broad meet general below mainly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
