ba366b5b1008c7555bbc61ee7560f638837b5d9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:31:38 +0000

    ignore instant discuss basic word

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import sys
+def exit_program():
+        sys.exit()
