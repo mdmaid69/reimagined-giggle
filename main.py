dfeb6ce88c60e439563f850ca83c8ce46449e53f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:59:07 +0000

    question inquire plan press cheek

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
