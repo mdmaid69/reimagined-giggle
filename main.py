f7edd072158b55945c6b6bea10ee58352f4008f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:02:40 +0000

    emphasis break share clean deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
