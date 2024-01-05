5d4f7b1d9c6c66dabe498279aa2069cab1e8ea38
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:13:45 +0000

    slow guess method trick foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
