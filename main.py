81497e0318680afe038190e0ee1771c17e93552b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:11 +0000

    steep mass the grade more

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 def calculate_area(radius):
         return 3.14 * radius * radius
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
