1dd4d4620c0b62c359028fe3ad2b1eb7cf10333b
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:49:36 +0000

    band that wish shock search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
