6a7497bd5df74a4d46636b0318f9332aeb2fbf7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:02:55 +0000

    reply easy post worried quick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
