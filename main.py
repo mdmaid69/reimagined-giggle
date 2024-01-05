c788ace51fb7463e3926806967ae4b1ad84ea39d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:47 +0000

    relation during wet more workout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def is_odd(n):
+        return n % 2 != 0
