ca80cdc51e1c3cdb54521981ce489f3b54d7d9c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:19:27 +0000

    pronounce grand high form rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
