8ca416d072937e77ad403f3d9ac82a6593383694
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:37:06 +0000

    bit scene know check manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
