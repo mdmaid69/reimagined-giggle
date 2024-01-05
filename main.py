a49f2d342e97433caf9245c00fa8cd7d7a694764
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:30:07 +0000

    attract square create blame mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
