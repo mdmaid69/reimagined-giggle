3ffd091c249268c78df648f1bd232e8fcee7af4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:56:09 +0000

    nerve programme never damage share

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
