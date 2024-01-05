ad9ee64040e1677f30c2cade38435c2a70fef143
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:24:13 +0000

    path awake complain notice Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
