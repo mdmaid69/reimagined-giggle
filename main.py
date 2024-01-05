02a3a03485581cdb1b994550a5c5cab5c9dc3018
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:43:10 +0000

    agreement right whisper place bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_elements(lst):
-        return len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
