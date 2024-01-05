914a9f7a5527b09ed582cbf9df655232947c5f9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:02:25 +0000

    pity telephone wind stretch empty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def is_odd(n):
+        return n % 2 != 0
