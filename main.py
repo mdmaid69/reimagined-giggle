27536bd642b3117b1bba0b7ec19006d29f859bd7
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:48:37 +0000

    rate goodbye 570enjoyable rush serve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def square_number(x):
+        return x**2
+def calculate_area_rectangle(l, w):
+        return l * w
