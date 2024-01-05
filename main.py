9c814add8344b7bab2747bda974eb9a888868d8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:51:57 +0000

    exaggerate easy heavy correction thumb

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+print([x**2 for x in range(10)])
