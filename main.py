7097ae690686700a73955b08f1dc67d3d6d70105
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:06:12 +0000

    scale goaway exchange onto close

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def count_words(sentence):
-        return len(sentence.split())
+import sys
+print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
