032294ca913c530eedaebbc39b478bde50ac6f39
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:52:06 +0000

    door favourite tidy trousers poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
