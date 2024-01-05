19377f4cae970b220a18dd79a669e7d7398147e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:25:36 +0000

    top while body smoke round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
