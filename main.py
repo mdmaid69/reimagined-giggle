d28c4cd2811dd5039f33b482b4917d27f903da66
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:06:49 +0000

    confuse tune live injury wild

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
