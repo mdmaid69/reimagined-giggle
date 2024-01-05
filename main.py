295c93e8b6651889b92ef74d00213e3a2ce03948
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:08:10 +0000

    imagination breakfast eat classroom dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
