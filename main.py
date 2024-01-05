7985839c43fe8cf7edf8a7c42bb9ab8230b50512
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:43:04 +0000

    question cruelty 571enough slow turnaway

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def count_elements(lst):
-        return len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+for i in range(10): print(i)
