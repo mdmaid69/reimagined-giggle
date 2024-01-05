9bcbd3f1dd89db7756307f7e7f4afd069e23d5b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:32:28 +0000

    571enough work creature check well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
