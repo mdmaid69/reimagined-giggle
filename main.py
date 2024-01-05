6de76fed2f37d34c62b98d4ce90837bddfbb2eca
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:18:48 +0000

    he culture past float crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_os_info():
+        return platform.uname()
