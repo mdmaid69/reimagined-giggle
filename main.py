077a7f1d319909478010e04eadf804408a877926
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:41:24 +0000

    exercise charge impression holdup average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
