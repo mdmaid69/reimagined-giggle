2ed17a3707ff301a8c7eb6c86dab305f32fb9a13
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:41:00 +0000

    stay wash cost pay simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
