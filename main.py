4080bcea45fee548049b2ffd3ab0f84eba859ed2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:59:03 +0000

    tidy start congratulate organization left

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
