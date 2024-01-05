0da8ede6402aceb8606a42adef628be7a106ab67
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:34:18 +0000

    word slice joke partner lookup

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_binary(n):
-        return bin(n)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
