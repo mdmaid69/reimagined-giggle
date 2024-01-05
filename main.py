4b1aef2ae03ae67c962e8587f506f2cf2ebfc59c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:45:09 +0000

    calloff butter left on drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
