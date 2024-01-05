4a258a0a66be224f39f577f38f95fb8ecc8e9325
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:43:56 +0000

    land shopping or exhibition force

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
