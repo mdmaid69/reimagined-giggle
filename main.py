1bb576b9b0b5a381553a458f368b9da247403ce9
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:56:57 +0000

    compete follow delight press contain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import sys
+  def get_python_version():
+        return sys.version
