2ffddf4f341a2443982c1185208d251abafb7b72
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:53:08 +0000

    cry from giveup agreement beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def square_number(x):
-        return x**2
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+text = "Hello, world!"
+print("Words:", len(text.split()))
