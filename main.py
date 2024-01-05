243cfccbbeb60ef4615c42173253e2c8506112e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:34:49 +0000

    arm stupid relation hungry it

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def square_number(x):
         return x**2
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
