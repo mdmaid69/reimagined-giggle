8d195f4051da30b64fd10f2435781f40ac26c50a
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:47:59 +0000

    this rare hurry strike hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
