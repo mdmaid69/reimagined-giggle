640d2fe78c46e0b4be408ea2c31b4187f1e5db32
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:58:04 +0000

    getout real voice fine that

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
