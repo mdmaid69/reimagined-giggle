734f75eefef22c1e20628da0a7224ffa09d6abb5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:36:41 +0000

    free far first circumstance attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
