6f70555144888e39e37d613c048ff72ffb190abd
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:01:46 +0000

    emotion translate even anywhere appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
