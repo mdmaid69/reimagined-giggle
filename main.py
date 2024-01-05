18b16ea6ef7bf6e14fcc4071f92bb0465405de7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:40:30 +0000

    bar sleep anywayanyhow house tap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
