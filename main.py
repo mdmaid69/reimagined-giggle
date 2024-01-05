65a2b7a34cc48ee3f86e66da4d754848966aee50
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:36:19 +0000

    spring reply in neck silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
