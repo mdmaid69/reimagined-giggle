20a4c4e55a7faca044df8fd6757210604718f79e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:27:45 +0000

    interrupt mend plan cook turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
