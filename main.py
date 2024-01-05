c8c14ae29bb1add1855e980a4b0f9362d4070c8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:48:36 +0000

    lookback(on) various slow total summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_complex(array):
+        return complex(array[0])
