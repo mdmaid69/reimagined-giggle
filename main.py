56ba190734d51606c102a29b4fd1c99da1428a13
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:29:32 +0000

    pretty although shade popular voice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def find_min(lst):
-        return min(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
