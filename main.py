71b006d76ae08417420dbd8dea44a8517037628e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:58:56 +0000

    weekend before slightly chin team

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
