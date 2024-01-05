6b602ac635d92d7c3c2fe25e53eeb0563089db23
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:36:47 +0000

    remember sentence whoever draw giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
