0ff9ab22ab35b5b7532a11f1cd6d4bb77f15a5d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:59:09 +0000

    care winter sometimes create catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
