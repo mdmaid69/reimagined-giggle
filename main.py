882452362736e2dd2910628c7b239bd3606677f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:24:50 +0000

    mention waste regional meet polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
