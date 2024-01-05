14fc27b424b050cb697963c77a8774548872d383
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:21:42 +0000

    bit comment sell prefer just

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
