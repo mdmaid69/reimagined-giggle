30c961fcb90fb42b5ef698ff47c1d0c4c76b5712
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:25:47 +0000

    vision union bit build way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_typecode(array):
+        return array.typecode
