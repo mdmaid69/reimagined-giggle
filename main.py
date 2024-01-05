12506cbe3a63a1f3d8c84f9b00d27934545c0b81
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:25:06 +0000

    shoot today young use maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
