15a9e70a508e39b4f6df2bd73bbbbed3faa00cc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:03:09 +0000

    strict its ocean thin cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
