0f5d730febac93d8bd16545c5439689497baef80
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:58:35 +0000

    rush consist single nothing big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
