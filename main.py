1e03e1d8c4c06d6663b4ae86138b93653ace8148
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:46:33 +0000

    never upset swing Ss ready

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
