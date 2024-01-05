354b70413af5deec72bcffb929188733db755024
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:27:54 +0000

    meet tax any rather list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
