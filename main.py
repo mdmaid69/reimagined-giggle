18836bf5ab734533c113242364f619d78f0c82d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:04:14 +0000

    move workout taste later month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def sort_list(lst):
+        return sorted(lst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
