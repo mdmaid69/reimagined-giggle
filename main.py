6e5af81f68e54a85eb1dabff260fd5a992c0fa5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:20:59 +0000

    backwards expect talkoutof sure success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
