a88557c92fdff99620473cf2f9adb9ede73e2feb
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:13:59 +0000

    tough cool thinkover either 572enter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_odd(n):
+        return n % 2 != 0
