07ee5992c21325212c8236efdeb36c60fe54f533
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:05:21 +0000

    tour wealth raw himself necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
