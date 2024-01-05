fdfd185e20490a765f681236fb676e8912fde8dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:14:25 +0000

    suit exactly deep could doctor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+text = "Hello, world!"
+print("Characters:", len(text))
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
