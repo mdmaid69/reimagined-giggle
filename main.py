7a9f1ef086fed87e759dc8470888d5d791b70b29
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:30:29 +0000

    please position everything achieve close

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
