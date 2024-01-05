2823e62f571309c5403fd2961f7f3d310ede56a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:15:10 +0000

    dry interview immediately first Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import collections
+def create_priority_queue():
+        return collections.deque()
