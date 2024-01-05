4ef70fff185899e94497b09dc1889b55b0f3ed3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:32:03 +0000

    murder excited record Tt creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def reverse_list(lst):
-        return lst[::-1]
+  import sys
+  def get_python_version():
+        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
