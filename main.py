48981618166fe647f16d6dcd9f6ae23eb015a360
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:34:24 +0000

    hair lay believe organize taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
