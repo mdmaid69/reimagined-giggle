8bc1d980797024ad3461f1f47350a8835a3f7926
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:31:27 +0000

    female absolutely best false passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import platform
-def get_os_info():
-        return platform.uname()
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_str(array):
+        return str(array)
