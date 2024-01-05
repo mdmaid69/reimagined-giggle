3cd3b1592c336cd995ad038973edd3f0329d405c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:58:39 +0000

    hungry cover nigga sign would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
