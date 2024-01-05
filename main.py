573036bb55c4c498199506a6e614347a35e75bc1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:11:38 +0000

    involve mark react autumn command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
