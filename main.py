cccac52eaf96f7018a675bd180aca0d6a8f0c739
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:57 +0000

    needle behaviour thick ever Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def multiply_numbers(x, y):
-        return x * y
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
