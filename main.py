fbbe8d7820bb26608644ce7c11b4beb0db7d6274
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:41:25 +0000

    beside myself circumstance instant impression

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
