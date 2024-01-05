6890661fabb6e23d88966622098a282bc1d8bd34
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:29:06 +0000

    open fence float precise cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
