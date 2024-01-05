51a08df257d3d4b9de7ab04d910f7f129c825869
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:55:30 +0000

    long off bye aim feel

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
