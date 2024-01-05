3d1a1edf608b637825f29d9fb75c306ae2ef553f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:26:02 +0000

    ready respect bringup slip Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
