c7b39db0c6dd9f405e01ebd3b98636cab13abd5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:56:08 +0000

    ear see mistake makeup theirs

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
