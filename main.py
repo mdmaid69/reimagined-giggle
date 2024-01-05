5e38cacf07f08f0021298f4edba76badf8186fc8
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:52:51 +0000

    giveup 566end where old world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
