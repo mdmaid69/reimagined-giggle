fa006a13defe03ac61aaca98aaa47a62223ecdda
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:48:57 +0000

    sign cheap dry aim summer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def count_words(sentence):
+        return len(sentence.split())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
