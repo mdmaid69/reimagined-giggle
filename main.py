4969e8278a8e4a37ccf1fd3f1d6b28f07b6d6d35
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:57:52 +0000

    furniture upwards switch figure breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
