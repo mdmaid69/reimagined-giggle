af97997097c0143616f38baf6a3aaea5017e6bc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:02:42 +0000

    evidence slice home before search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
