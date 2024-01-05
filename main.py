f98d8e903ee43145cbb1aab54354a9daf977131c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:21:49 +0000

    personal authority opposite order phone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_pressure(force, area):
+        return force / area
+def calculate_area_circle(r):
+        return 3.14 * r**2
