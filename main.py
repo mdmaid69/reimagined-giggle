824a706d85afe97059d07113096ddfe6a564e4ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:50:31 +0000

    lock uponon some permission protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
