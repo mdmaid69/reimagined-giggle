d7035fa63d1adc6749efd1338ef1f45f976dbd0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:12:11 +0000

    talkoutof shallow hardly shade turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
