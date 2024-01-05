546e7b5b298ab4b13f7245b75f0437f6b62dd518
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:03:50 +0000

    thorough crowd need raise head

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_string(array):
+        return array.tostring()
