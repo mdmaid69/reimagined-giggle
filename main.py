8acbcc050e7e9a98abf0e290f48fc852eab5e149
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:02:24 +0000

    respect wear sting moon relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def convert_array_to_list(array):
+        return array.tolist()
