53454c7a2aaff6a4b440401419fcd3856cfd7c30
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:38 +0000

    want public trouble glad defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
