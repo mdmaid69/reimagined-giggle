41ac1fc854f305b172420f2cc626ffbf39adcbb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:47:12 +0000

    smart climb scream march talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
