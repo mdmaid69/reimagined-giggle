319312fce749a558dd01ff5017e29c423d8c288c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:26:29 +0000

    sort key but busy professional

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
