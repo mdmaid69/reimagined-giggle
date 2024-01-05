db1e14eeb7df2d317e9f346e8644413ae7c80337
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:57:49 +0000

    thinkof pipe classroom gentle blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
