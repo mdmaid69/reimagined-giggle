dc62aa2711dd6775f6dcb2bf4d6dbbc871af2003
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:43:27 +0000

    until long 572enter mail wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
