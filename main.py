ed800b5247b12453721ee0f417e3ce6944943c70
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:34:09 +0000

    reason show appearance event Qq

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
