13b2a42c502aff01f1e1cdd9403d7ed6f8ec485d
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:55:14 +0000

    reliable rarely scrape every keepaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def convert_to_binary(n):
-        return bin(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
