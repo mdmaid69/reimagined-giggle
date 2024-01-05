58ad6f58eaa798fb13cd9a6ffad31867c1ad1003
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:42:56 +0000

    hope kind shelter turn speed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def convert_to_hex(n):
+        return hex(n)
