e9b1aec787d13af2d90f8472080baf567b1c3c0a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:14:22 +0000

    getback separate minor embarrassing escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
