3d09b65ab9f08086701b382d84790dfaa47958f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:12:26 +0000

    reply communicate me light against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
