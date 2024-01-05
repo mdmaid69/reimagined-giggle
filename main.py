68ef6c770a93b1c87eb2640152c3eb0c8ca017c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:08:49 +0000

    list community sound lonely wander

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
