6eca855a7ab20db9fbf9d9c9147f16f46da6cd18
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:43:47 +0000

    activity keepoff sweet single scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_even(n):
+        return n % 2 == 0
