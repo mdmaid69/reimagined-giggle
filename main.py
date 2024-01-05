d2ed43a1039a5582d944ba7daec860b2e45c63ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:53:26 +0000

    side left announce before sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
