ac75094c1a58997b7be8478c4c996107e01700e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:35:09 +0000

    none punish long welcome good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
