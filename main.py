5bef4c4989da08b643e590e9aa0c1b0e32941aa5
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:27:25 +0000

    cost survive against worth sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+x = 10
+y = 20
+print("Sum:", x + y)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
