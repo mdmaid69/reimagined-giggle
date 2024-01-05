c638d80faeac0743a115c5634ccfccec4f1b8432
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:51:25 +0000

    about public lead fasten true

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import sys
+print(sys.version)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
