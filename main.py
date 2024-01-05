f95f2eac8b8d6dc5a269201a009b049470ec6446
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:28:47 +0000

    government community Pp minute spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_complex(array):
+        return complex(array[0])
