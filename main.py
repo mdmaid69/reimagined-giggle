6d5c6be9e8eefe3e2cbd4a4ba411f99f744d2d13
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:02:15 +0000

    present die blame accordingto protection

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
