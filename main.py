55dddd4a1797316874361d41bcb27c9a1b36c961
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:51:36 +0000

    catch hide underneath all bury

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def cube_number(x):
+        return x**3
