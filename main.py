96d10cecf920343e3f0bf6dc2fd166d0b6eb5797
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:01:22 +0000

    getout right and available other

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
