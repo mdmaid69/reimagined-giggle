4f2df0b8a01ef1e652133ffb98260af30f811bd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:34:45 +0000

    so make close immediately public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
