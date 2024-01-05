a47a75ed0356280ec91d6d6afd53bfda6310a0c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:47 +0000

    science solve sale much allow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
