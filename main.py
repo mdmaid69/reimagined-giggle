48495513489f8409d775d969fd8f95d28cef6e14
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:35:19 +0000

    decision regular high trust proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
