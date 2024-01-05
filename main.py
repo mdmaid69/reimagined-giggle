2fdabf0dce2c17575fe2094da6df634d6979a190
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:47:23 +0000

    clear tight basic exploration ashamed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
