4ead896a985e6e705b544385655f1b30007313a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:46:28 +0000

    alone him turnover rest respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
