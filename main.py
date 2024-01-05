f8eb7427fd2b19258bb69ac18f3be5c13de466a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:53 +0000

    cause recent slowly birth butter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
