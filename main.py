acb8e239164905dd115cd8dda06318cef79e057e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:26:55 +0000

    interest rather bend after direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
