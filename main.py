92d1bcde8bd71228a4b5bd5ee0d5336a525bf452
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:05:02 +0000

    past organize blame hide concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
