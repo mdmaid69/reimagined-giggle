5c0a20cea6bba57d897b857b390faf2f79787523
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:38:11 +0000

    comfort upset work thin act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
