a52565c2dfbcad0e91d94a0ae803d0067dfba2a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:20:36 +0000

    impressed discovery there point press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
