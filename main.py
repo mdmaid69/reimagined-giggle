bfb2b26b276b62310abdf3ba3e6b826923551dd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:48:11 +0000

    comfortable except steady throughout exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
