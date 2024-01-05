c779fc169eef9b9caf31fc7645c95ec6bdd860f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:45:03 +0000

    thin very yourself shout argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
