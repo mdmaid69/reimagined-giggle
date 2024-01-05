6b4e0e83fe1f7e5d9d96cda2b16f31401fd066ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:56:44 +0000

    material swim indeed dress test

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
