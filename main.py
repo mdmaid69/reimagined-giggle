013b5aea460bd85e75e315a160316d09b65e5468
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:38:44 +0000

    sort through shock shrink improve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
+from collections import Counter
+print(Counter("hello world"))
