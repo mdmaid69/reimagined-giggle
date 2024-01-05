2a24a235698b7c052057ac690f2552a9a7d9c369
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:06 +0000

    hope defeat dance that concerning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def cube_number(x):
+        return x**3
