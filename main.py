f7892ae619d3171f26dccde8bf43bca5d88914e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:08:43 +0000

    popular fight shallow common different

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
