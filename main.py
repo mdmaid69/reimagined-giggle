a5120a59f1ac74e3cce09208424c56770cd1f0d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:57:14 +0000

    complicated appearance zero perfect fill

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def is_odd(n):
-        return n % 2 != 0
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
