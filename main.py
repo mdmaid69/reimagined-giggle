902cf2155d6e9d248350b014151fff111808ac02
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:18:31 +0000

    talkoutof offend spray comeround Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
