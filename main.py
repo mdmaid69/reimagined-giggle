01f5d305d4c484a0c81ba236dcce6c244d744b09
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:09:40 +0000

    any group impossible win example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
