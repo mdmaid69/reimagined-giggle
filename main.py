dbf7f4d9ce0f968ff4c4261d307e02d743d4335c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:47:28 +0000

    wind return influence instant sweet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
