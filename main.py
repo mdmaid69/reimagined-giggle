c4043174ce52437e087f668dc024f0ea2c8443ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:22:52 +0000

    takeoff against keen tool one

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
