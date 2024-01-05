e897b4c4eadebadf1391d9a8c7a956c1698c8be5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:32:39 +0000

    murder getaway promise brown else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
