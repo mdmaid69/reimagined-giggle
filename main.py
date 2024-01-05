3102b1bd475c27f228bccf9f392edeb372cb45e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:33:09 +0000

    write difficulty everywhere pay shame

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
