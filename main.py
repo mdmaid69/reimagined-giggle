49b218e90cc0272408368def469267c74693885b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:16:33 +0000

    pleased and glass here coffee

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def reverse_string(s):
+        return s[::-1]
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
