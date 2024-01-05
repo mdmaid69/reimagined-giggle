d16028885cb9a3d783a1e65b6c18b3b878b018d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:24 +0000

    cause flat condition exercise punish

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
