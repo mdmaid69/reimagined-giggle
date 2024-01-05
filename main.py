232e6f14442cdb3d3b8cafa9186987267a841936
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:32:40 +0000

    face twist group well would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
