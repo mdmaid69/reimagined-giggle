7ea589b16346eedf528f4e8624518d8ccb821029
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:54:07 +0000

    seem should confusing high start

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
