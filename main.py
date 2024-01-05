d1e7325b3aac50531200f1d7b1710f2b9bf1bc6f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:45:04 +0000

    suit goout sport dark expensive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
