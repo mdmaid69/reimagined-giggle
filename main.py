1ded50cb5a4e3aacf325b9f59db6b0b6233959c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:57:36 +0000

    impression scream circumstance object connection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
   def split_path(path):
         return os.path.split(path)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
