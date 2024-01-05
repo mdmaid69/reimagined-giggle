e3441792ca0a112de9e59c42467565d9a8772a74
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:52:55 +0000

    square part bath prepare crime

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
