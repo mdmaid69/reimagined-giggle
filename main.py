d259ef52bdd4324b1adef576d68db506546ed2e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:31:09 +0000

    wrong bag lunch that mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+print(math.pi)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
