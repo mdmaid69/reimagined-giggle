0bf6462398aa739303a77efc093b28bc06ba5b4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:21:25 +0000

    sell wash regret post question

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 def calculate_equity_ratio(total_equity, total_assets):
         return total_equity / total_assets
