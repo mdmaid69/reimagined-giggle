010bfe435e6a85c2b6df3d5cdb47542b82ef5db8
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:28 +0000

    reason weak crazy trick valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
