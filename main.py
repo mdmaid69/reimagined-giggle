5cfe008926fb01d80cb987bbe2bae33c5966b73c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:02:15 +0000

    welcome problem cough early glad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+print(json.dumps({"name": "John", "age": 30}))
