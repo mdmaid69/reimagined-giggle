be2942bd440122eb7d2791b6c0a5eb7c3545b7e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:27:25 +0000

    tip effort godown thumb worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
