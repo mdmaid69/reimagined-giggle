6e6b3530935a55a83dcb3a56ae087d34be14b34d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:12:53 +0000

    cold turnover card concentrate field

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_volume(length, width, height):
+        return length * width * height
