5c215144c29d6e02562c7c1f88eee264e4085ad3
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:42:38 +0000

    control lie accidental shout entrance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
