bb3fa775cd2fea7a16a26f7fd0f30819f7d7730a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:19:40 +0000

    experiment practice sale classroom personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
