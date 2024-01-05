a809d4c5171f77aede8bfd51bfea5e9c14ccb48e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:38:39 +0000

    sing shopping university first love

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-print(sum(range(10)))
+def calculate_density(mass, volume):
+        return mass / volume
+import numpy as np
+print(np.array([1, 2, 3]))
