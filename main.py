5060ba85b3c0c5269d1cb4930bb09f06131f3163
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:53:43 +0000

    well question frequent nose holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
