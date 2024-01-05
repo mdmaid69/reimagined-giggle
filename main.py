fe7c2fe9d98f792ea51d0aa0b331d519c2509acc
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:13:57 +0000

    home failure heat spray first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
