290b5c835b24bbf74ba84a34319afab7f7f7555d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:37 +0000

    hear arm 564encourage earth exchange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
