54357ff1a586cf04fedecfcedd186ab46a9eab68
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:51:04 +0000

    floor wrong practise sense guard

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
