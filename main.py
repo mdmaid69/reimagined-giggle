74e50f4c694a0e31a42d90b4d08feb9bb395ae39
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:24:05 +0000

    bag full even day listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
