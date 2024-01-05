39ccb956095400e87b698e5345a1d2866d004b6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:15:14 +0000

    his study owe spot arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
