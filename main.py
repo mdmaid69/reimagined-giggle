62351147287af56bc5f9311136749589ec5780b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:28 +0000

    private voice worth obtain intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
