f4bb41894ed1178423886bf916fe948c3551bedc
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:07:33 +0000

    remain close pocket chin mood

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
