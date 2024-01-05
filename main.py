247f7a072be1738a61e9476f07957ab264685849
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:12:17 +0000

    upset cut neither confusing touch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
