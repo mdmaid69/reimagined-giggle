497a80ff5f07784702f7da8d918a8b80f6909378
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:57:42 +0000

    solve play wide relax sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def sort_list(lst):
+        return sorted(lst)
