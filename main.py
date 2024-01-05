57a34945845f22fa7ae63c09cf6f410dcf394e90
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:03:14 +0000

    indeed balance fashion around risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_as_bool(array):
+        return bool(array)
