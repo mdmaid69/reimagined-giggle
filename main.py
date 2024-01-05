c3d84c2de586ee6c0b56b639bc6bb73952fb0f83
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:47:15 +0000

    expensive fun Ww doubt cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
