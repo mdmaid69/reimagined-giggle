913cadccb5b31cbf744311aca2aaa39885967b90
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:33:32 +0000

    greeting switch trouble extraordinary prepare

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
