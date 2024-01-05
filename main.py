b1c18947b2d8ca251f6d0a1b666fe618c4e02888
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:50:20 +0000

    exhibition stand behave fault time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
