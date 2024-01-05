0397c6012f3b3532887d03949722742fff045e04
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:11:44 +0000

    do fence goout phone switch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
