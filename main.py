603d2bee61401426ddcba38e4428cd3f38471d18
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:02:44 +0000

    space lower this convenient getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def reverse_list(lst):
+        return lst[::-1]
