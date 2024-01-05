115446c1f6ecaac481adc4fb4a5488eb212c4c3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:15:24 +0000

    remain nervous choose several communicate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
