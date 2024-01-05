a1ce69c1e0462cc9c36341bad7fca509d03fd18a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:06:07 +0000

    aim slow fair turnon feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
