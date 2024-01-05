d2d9139776213c95647cd0d7e72ff16cf265472e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:04:20 +0000

    late neat like before character

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
