a4554f6d1295756bc425d7e1670caa36f2d7819a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:22:58 +0000

    late bunch pronounce slide sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
