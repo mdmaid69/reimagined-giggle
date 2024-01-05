9d5d28351ee3de96604105ce58ee8aff235a96d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:27:19 +0000

    giveout 567enemy steep bet sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
