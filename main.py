d3c36c81a92e374a1d69526576f86771990d3a61
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:52:06 +0000

    take actual escape heavy light

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
