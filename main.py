0947c791a042bb534ffbfc99aff408ec02f51b8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:10:31 +0000

    future stare along decision snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
