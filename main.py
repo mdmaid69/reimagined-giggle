1fb8f69ba7fa23f58a147ebc07fdaa346bdff76d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:52:44 +0000

    tear grateful reasonable catch study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
