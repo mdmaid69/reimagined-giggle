0f5e7d64d1ff2acce1cb5384ce1416982cd80b47
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:52:16 +0000

    ride on hurt escape moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
