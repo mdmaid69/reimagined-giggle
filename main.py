6fac1d602e6c4e6d0231566254c6df2fa54847fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:00:36 +0000

    eat division sight long lay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
