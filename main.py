e9c55bd32f73d3c97ada04c526402255b244a391
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:35:10 +0000

    show stare rest watch nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
