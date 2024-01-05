cf540dbd0e69338f85ed63283ea137d4dd4174ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:16:29 +0000

    talk exactly more accept come

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
