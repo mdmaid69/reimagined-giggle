da38314a0f33d91b4b8c832a65c26477cced7456
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:15:10 +0000

    meeting train off trap bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def find_min(numbers):
-        return min(numbers)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
