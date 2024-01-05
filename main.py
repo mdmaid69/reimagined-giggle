75327e8eb8fdba20449474d083bf2d34b23e5f17
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:30:28 +0000

    brush sure him nerve material

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
