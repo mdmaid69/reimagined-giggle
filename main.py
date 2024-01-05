8557c642ce09ac36483b8346320ac204594618f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:17:40 +0000

    pot change position getback noise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import datetime
-print(datetime.datetime.now())
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
