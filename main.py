720b4b0fcf4290f2b32ed07ae05d699e30156b94
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:01:43 +0000

    beat else still gap certain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
+text = "Hello, world!"
+print("Uppercase:", text.upper())
