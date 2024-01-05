8f7b135f6316fd82201a286f73708216915dfd50
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:23:14 +0000

    miss drink object dick ear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
