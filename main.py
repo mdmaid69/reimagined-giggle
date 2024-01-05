af298ca01dd8c7c136c0c27334ee354c5284d764
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:10:56 +0000

    drop achieve mess cold dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
