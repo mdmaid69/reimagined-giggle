8d8ae195e9ac3248d1bca5988168c46dd22ba18c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:23:54 +0000

    case sting total pause purpose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
