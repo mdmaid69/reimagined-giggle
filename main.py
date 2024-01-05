6773c158df926cc25bc056ad62903dbcb3890b3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:55:25 +0000

    start exercise rich exit escape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def square_number(x):
-        return x**2
+import datetime
+print(datetime.datetime.now())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
