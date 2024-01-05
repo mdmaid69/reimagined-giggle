4e8a6b7d32b51be5f0e33d0b61724a55e99baa6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:08:31 +0000

    succeed level away precise label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_array_to_bytes(array):
+        return array.tobytes()
