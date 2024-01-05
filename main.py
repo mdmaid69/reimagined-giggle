149023ca556bc1dec3792ff7b45eafe4301713e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:45:21 +0000

    interview child bottom but birth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
