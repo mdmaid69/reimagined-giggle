fd467dc084c9b7cf4fae2d3e45e83c5bced21853
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:23:53 +0000

    over instead check makeinto prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_list(array):
+        return list(array)
