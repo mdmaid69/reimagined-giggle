492335f5bdb233dd8d8228d5407945e78b63750d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:25:53 +0000

    best good rate except spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_user_dict():
+        return collections.UserDict()
