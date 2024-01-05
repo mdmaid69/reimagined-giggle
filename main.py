9a8e4aab747e971f13ed9663f3ce9f531de88695
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:04:52 +0000

    OKorokay delight she lookaround relief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
