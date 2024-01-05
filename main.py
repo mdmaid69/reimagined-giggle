23f0ea080068d05b484cbd5b8751b74908079ea7
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:09:20 +0000

    way watch preparation transport tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
