75181d7c997142e0e2f3bbea37fcfab2f62c0610
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:07:37 +0000

    arrangement hope less sign benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
