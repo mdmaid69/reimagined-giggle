1ff3931f25ba3cc67349a27e93d5460ca64ab9f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:11:34 +0000

    couple shut material be bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
