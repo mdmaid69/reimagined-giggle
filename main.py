35442ef1ba579c708c6b981342423f9a2cbee03f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:33:21 +0000

    thirsty more die flat morning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
