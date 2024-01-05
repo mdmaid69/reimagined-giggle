fb65b0fdf56953fdefe1e6fe1cacb48d6af2d9c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:35 +0000

    sport support union special permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
