3981d0532449528daf41a52c203b01abaafc7d21
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:26:29 +0000

    fit gradually edge steer ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def list_files_in_directory(path):
+        return os.listdir(path)
