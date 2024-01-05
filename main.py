9b73695d3b8b37bec865be485503367759e84ca4
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:21:08 +0000

    male excuse skill money inside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
