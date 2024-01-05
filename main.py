3ef060b7ce1e1814149754ddc578a8cbb6f364d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:27:29 +0000

    repair listenfor for whistle crisis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def convert_to_binary(n):
-        return bin(n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def append_to_array(array, item):
+        array.append(item)
