bfe18ee3255149e2dd14d1fcaaa41da90d1f7d99
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:14:44 +0000

    shoot production them interesting fortunately

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
