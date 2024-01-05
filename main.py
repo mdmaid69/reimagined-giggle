ebcfa83216292a3a9ff2c5b35f86b9881e1fc024
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:20:44 +0000

    getout tall shout keepoff yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
