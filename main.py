a54eaa8143178787600338923e840e64528952f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:10:42 +0000

    wake search Ww black unit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print("Hello, world!")
