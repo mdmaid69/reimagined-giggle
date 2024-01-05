1f7b329454554f5d8895f29b7500883ba504fa6f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:54:51 +0000

    mysterious trick print getoff old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
