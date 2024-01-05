d10616d8d6cacb19d57a700410fe9245bff5fade
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:27:40 +0000

    sport so ahead check dig

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
