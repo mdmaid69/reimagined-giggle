37aeb6283b1da60df19fde9ac10fbe500b373c7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:18:20 +0000

    tidy past band unable tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
