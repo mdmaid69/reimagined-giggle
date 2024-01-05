af27b4d3d42710537e49caf337818319b2496564
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:25:37 +0000

    sometimes slide comeup Nn imagine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def convert_to_binary(n):
+        return bin(n)
+  def count_elements(lst):
+        return len(lst)
