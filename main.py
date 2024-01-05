19b9a7cd0974867b744c0af61645f7d1c3eb0b25
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:36:51 +0000

    turnback forward pay address beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
