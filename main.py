e2beff05daaa5e04999fa620e1a5c78d3d6224f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:05:56 +0000

    die keen land minimum winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
