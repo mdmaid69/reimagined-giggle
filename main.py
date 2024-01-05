493151fe96dc9de47d8bf537e1d01fe92fe1fe2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:57:12 +0000

    degree pronounce confusing visit achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
