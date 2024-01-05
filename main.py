baeab75994b84b44145ef272d24061f7236a42ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:53:07 +0000

    his obvious hear old neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
