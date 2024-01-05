cdb6bd3b5c6bbb46f4c14b8f9f70d46fe3bc0a44
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:50:20 +0000

    promise shop good minimum finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
