b10db00903dfbf2c58c42d84c9090c00ac7b9764
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:59:08 +0000

    consider straight plastic consult dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
