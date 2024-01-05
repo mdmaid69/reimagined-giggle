60bc85f01d7378f4a42112f17c354dec572e1933
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:45:46 +0000

    news tap shame what Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
