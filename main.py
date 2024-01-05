89ef941ace37ed4d41d74ad2fbff88277e7bc28b
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:05:47 +0000

    whenever mess popular burst key

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
