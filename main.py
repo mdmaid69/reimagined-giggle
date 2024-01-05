ba11d869d98a22f6d5482a3f1aaa5601f83e1acf
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:59:15 +0000

    greeting soil strict several meeting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
