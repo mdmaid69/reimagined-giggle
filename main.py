3711c4c8b7241a0821b6f817c4acc4c519e86222
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:21:18 +0000

    demonstrate to maximum public over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
