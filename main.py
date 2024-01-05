6bf9e8376be7fb16950a27039b11a762af03e079
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:38:40 +0000

    fence dinner prevent gift whichever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_current_directory():
+        return os.getcwd()
