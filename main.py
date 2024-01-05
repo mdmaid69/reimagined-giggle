8be91d8131bdf79ad614096a286b5cb86885f1f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:12:17 +0000

    hate few pay face trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
