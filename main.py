416c87cb788d46648105feae5046f365bbf4ea31
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:39:24 +0000

    undo solve throughout model bother

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
