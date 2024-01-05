1d75b93ac91b522f8e5272811097b508014c1da7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:59:03 +0000

    swimming win cruel fair black

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
