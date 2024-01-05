b5871f440b18d50613a401a62a41713bf5f37089
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:36:56 +0000

    length chat seriously force goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
