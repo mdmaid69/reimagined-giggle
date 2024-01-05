1b79ddaf88cd29557a5cfdab614d94c97127b0f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:20:05 +0000

    fix light influence really walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
