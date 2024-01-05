f4ae8ab759f5bb0d6128a6e0db10c307d23b273a
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:12:23 +0000

    train view bush any what

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
