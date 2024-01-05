6857e0a916c0cb93200e515e2bb31f1a2ed39b60
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:55:48 +0000

    hole opposite ago strange Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
