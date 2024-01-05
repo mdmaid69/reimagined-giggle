7e77b031cffb04ddffe352c55441b554debd5b1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:44:34 +0000

    make beside mark example bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
