8f9775e09c56c4c77fc45cd96ebedbe6bd54aa66
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:37:43 +0000

    goup charge once Itself hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
