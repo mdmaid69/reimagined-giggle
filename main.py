821b15d754c064b2e66866b63cb55efaf84dd641
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:06:53 +0000

    bend Hh only alive command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
