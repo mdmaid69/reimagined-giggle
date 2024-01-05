86f30a1a1c572b42090c0ca24def9064dc13af3d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:34:48 +0000

    real sweep border ordinary owe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
