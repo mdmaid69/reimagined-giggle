d3bfd00576b21ed7e5a477107ea5e67acaf58eef
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:45 +0000

    kill side weigh large exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
