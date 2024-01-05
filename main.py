47d4ffdbce7af45eebb4bf18daee5ceec5980863
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:52:33 +0000

    joke drink fill careful travel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
