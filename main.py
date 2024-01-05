00625b7a0af098f210b6833c94b87a6133158e42
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:31:30 +0000

    like only platform blame trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def remove_duplicates(lst):
-        return list(set(lst))
+def get_array_as_complex(array):
+        return complex(array[0])
