ef1e3706918eb8785618ac41acc927410823a766
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:28:36 +0000

    tie choose judge control sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def square_number(x):
-        return x**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import sys
+  def get_python_version():
+        return sys.version
