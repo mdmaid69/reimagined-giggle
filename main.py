1816de8b54945dd97215b203a693a8539bc9ef03
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:09:41 +0000

    needle glad shop large female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def cube_number(x):
-        return x**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
