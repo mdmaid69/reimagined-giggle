6ee411b60454a6c03c9c12c9a48cb29060053f2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:32:46 +0000

    desperate clean moral unlikely preparation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
