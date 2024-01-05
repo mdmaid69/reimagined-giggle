1cbd95bed001195b4594512781d6e6ffb64b9ba1
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:49:44 +0000

    seem cross add welcome exam

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
