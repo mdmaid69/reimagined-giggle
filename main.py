861f1fb22361f0d9f425f40fa8d78144402ea959
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:10:46 +0000

    short move extra side theirs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_unique_words(sentence):
+        return set(sentence.split())
+def remove_duplicates(lst):
+        return list(set(lst))
