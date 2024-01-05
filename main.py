14d6d5972f8612f4588b43c2447adeed2ea925b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:38:13 +0000

    breakdown certainly pleased discovery meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
