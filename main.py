5583b211a6cd43177ef6904fd51776c0696ed866
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:15:36 +0000

    picture performance sign jump quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def cube_number(x):
+        return x**3
+import os
+def remove_directory(path):
+        os.rmdir(path)
