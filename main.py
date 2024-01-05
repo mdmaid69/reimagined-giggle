b4c78e51d47405775ff14689084a7a1502e13ac2
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:25:57 +0000

    practical fire absolutely weak television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_index(array, item):
+        return array.index(item)
