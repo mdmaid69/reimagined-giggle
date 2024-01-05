ce8bad774685370893ccce7b21f24889c164bcdb
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:46:00 +0000

    pot backwards instead attention opposite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
