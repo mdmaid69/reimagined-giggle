6373669d3afba48f411ca905198c1f0cccf61769
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:38:15 +0000

    clean day warn skill giveback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
