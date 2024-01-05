c0a9d163f3d6a7f4c473d4edd1b93bd69924794c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:40:49 +0000

    rob pull pick drink balance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
