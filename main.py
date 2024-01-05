092545b431f27bcd1b62e8c1a09aa3726be1514b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:24:15 +0000

    probably choose Pp minute phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
