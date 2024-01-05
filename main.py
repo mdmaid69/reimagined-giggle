2b04afef84404780fba4d1f2f104052dd8e11853
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:37:51 +0000

    patient tend pipe public regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
