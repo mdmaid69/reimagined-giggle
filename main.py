3f8711d35da149d00ca1d7c17e257d00a9dbeefb
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:37:51 +0000

    awake transparent lookaround be music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
