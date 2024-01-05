844e2dd98a6dca829b301290f6db73eb9eba3cbc
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:45:02 +0000

    detail handle complete fast tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
