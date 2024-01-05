ce379d9775586946ecad5cae4b79de1d0986dbd7
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:18:25 +0000

    rough neat before whether paper

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def cube_number(x):
+        return x**3
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
