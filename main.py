936e3bbd9c837e709c6fbb51dda9948f17eb6f57
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:32:27 +0000

    bottom announce listenfor drawing last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
