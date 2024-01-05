a3a19f59f18daf25763e31bc38fcfd109db1ebb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:12:44 +0000

    serious sensible hot thick ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
