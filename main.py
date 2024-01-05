98d0833543410c835291cee7df251f670992a4a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:07:33 +0000

    bad though properly save communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def square_number(x):
-        return x**2
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
