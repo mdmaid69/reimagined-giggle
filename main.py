025bd50ea480c369e5e1ffff1bef59da263557f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:55:36 +0000

    platform very pollution post land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
