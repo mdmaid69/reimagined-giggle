a5126451557451006e9fe63483a48dc0e2b08397
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:39:51 +0000

    even gas still well ours

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
