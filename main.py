e72c87be72c573f5ae4f54f441d61bd3ba95fb01
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:20:02 +0000

    opposite unhappy only himself condition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def iterate_over_array(array):
+        for item in array:
+        print(item)
