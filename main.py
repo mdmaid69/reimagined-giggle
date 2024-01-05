ba54cafaa9a751ba2ff0ddd024ebbc3448167e01
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:55:18 +0000

    shape plate fresh pronounce practise

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
