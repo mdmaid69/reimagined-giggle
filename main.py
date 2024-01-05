d218fa928cdb695fdf88e5da79eaafd971f7a20f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:09:54 +0000

    sale history range detail shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
