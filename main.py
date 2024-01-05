cb26c256e71f0db5aa9d2291c7292983fe0a5697
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:45:26 +0000

    best throw eager high depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def print_python_version():
-        print(sys.version)
+def find_max(numbers):
+        return max(numbers)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
