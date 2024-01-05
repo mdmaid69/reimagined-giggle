99966f012d6744b1b9bf6aac68eac39b5c7a47e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:49:38 +0000

    throughout firm callback central anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
