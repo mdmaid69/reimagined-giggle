d2adc2d58afe6b7bc409884b27150809da244982
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:46:55 +0000

    angry pretend organize surface estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def append_to_array(array, item):
-        array.append(item)
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
