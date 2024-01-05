cb58a3e058a59e91d9cfb817879f4bfdf3ea0b77
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:31:21 +0000

    callback second believe display behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
