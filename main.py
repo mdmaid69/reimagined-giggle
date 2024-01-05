2c5c1fa844c9d6621aae0697cdc0c843456151e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:00:22 +0000

    unpleasant whole vote list shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
