4fa7ed0bd658f4a0dcc0e1def594dd5ec93a2ba1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:48:25 +0000

    open confused single rare closely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import datetime
-print(datetime.datetime.now())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def sort_list(lst):
+        return sorted(lst)
