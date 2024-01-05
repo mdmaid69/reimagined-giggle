f41e6fa3c1642ae1bfe71fc2235739e0f76fba95
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:35:16 +0000

    partner measure factory talk exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_list_from_array(array):
+        return array.tolist()
