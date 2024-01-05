5bcdc5a6fb7a58a0bb29ba076fb1046b0152c365
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:33:12 +0000

    translate tap shop sale benefit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
