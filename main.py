b95c486647c6a6c69be196a69411fff2479e0751
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:52:31 +0000

    expect lesson wear approval get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
