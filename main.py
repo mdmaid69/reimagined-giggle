9720328e981c6d2aaaf3db3cd7bffac90ce6c376
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:14:56 +0000

    wear complete scale kick regard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
