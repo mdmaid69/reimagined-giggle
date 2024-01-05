f6dab5586483032368d879fc7b29b4157d478088
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:38:45 +0000

    exam before 565end great narrow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
