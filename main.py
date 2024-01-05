8f7bac6c237d3c8309a90d54218bd009493c4f95
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:46:48 +0000

    turnon card high rare behind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
