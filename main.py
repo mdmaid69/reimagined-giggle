4fdea6c0fb7a4fb9fe35817683db6e3e73155aab
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:06:06 +0000

    exhibition target mixture grateful tap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
