1267fb5e357b4e87891b51e42109b300c81c02b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:26:17 +0000

    party mean cap so freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def check_if_array_contains_item(array, item):
+        return item in array
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
