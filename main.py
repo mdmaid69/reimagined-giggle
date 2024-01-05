e67c0a7c550406e2ef20ffeb8a2aaeedcf9c1e4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:14:17 +0000

    scene down problem spring attitude

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_power(work, time):
+        return work / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
