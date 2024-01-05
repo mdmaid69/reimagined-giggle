a8f0c6a50d7fd00fad9d7357041f0c5d8120301c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:35:57 +0000

    enthusiastic exam principle load nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
