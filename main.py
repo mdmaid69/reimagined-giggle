6c5c96b4e5386bcc2d0d1087bddf0e0098881a5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:57:22 +0000

    across inside Bb tough comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_error_function(x):
+        return math.erf(x)
