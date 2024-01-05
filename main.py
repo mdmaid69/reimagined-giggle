faad5e143bb809493132313423656fd97f62fd9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:26:26 +0000

    any maybe wise set process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+import sys
+def print_python_version():
+        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
