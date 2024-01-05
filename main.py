4d409910372a192d9b8542e2f2039b0dbeb75a49
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:36:16 +0000

    action level freeze type better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
