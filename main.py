51ff6ecdf794c3ebfc4f28b1d390a94961a5c558
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:13:29 +0000

    regional appearance harm rarely obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
