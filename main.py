196a9982b9c3b85eade7c96ddbbdb93b5601b30a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:22:18 +0000

    compete tax manage bang step

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
