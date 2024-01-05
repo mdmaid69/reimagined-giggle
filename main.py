0d9a008684dfdd4767ad84bc90b6d15eef75d58c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:31:30 +0000

    year read deliver lonely autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
