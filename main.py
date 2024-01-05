89b8cb34ae7d22403b15c861f57a92f199af57c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:37:23 +0000

    activity strict Ee secret study

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
