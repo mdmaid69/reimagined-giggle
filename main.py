151d458e099f42a9f0960ff16eb6fda4ea2d5bcd
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:02:08 +0000

    town plant taste taste whichever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
