1a5ad6986014b409686d66366317e55598e97154
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:22:22 +0000

    world high even yesterday statement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
