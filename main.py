ee3712609e6969d5ce0450369733cd82f356afc2
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:34:42 +0000

    concentration experience hit land artificial

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def convert_to_hex(n):
+        return hex(n)
+print("Hello, world!")
