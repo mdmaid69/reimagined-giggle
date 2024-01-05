d669381be56a1a589f7997f1070c4c118834cbe6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:05:03 +0000

    issue money rush temperature fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import sys
+def print_python_version():
+        print(sys.version)
