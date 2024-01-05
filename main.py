841cf239652de5e8c2841308daa0e261c2c9916b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:46:00 +0000

    some beat urgent exam watch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
