82ca36f08206c8c73fbb68adba9114cc610766ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:13:37 +0000

    church play cruel disturb cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_current_working_directory():
+        return os.getcwd()
