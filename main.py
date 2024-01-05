2910fd715c7003fda9c593d896583ec82b9d2233
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:09:56 +0000

    manner contents firm remain occasion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
