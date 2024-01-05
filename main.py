6078059cf33beb23d17a0d64ad5ee0bec9824a35
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:55:54 +0000

    alive language expression accident though

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
