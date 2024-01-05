89c9f97a0c2a9772b626b2af672decb9446c8696
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:13:05 +0000

    passage love destroy among off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
