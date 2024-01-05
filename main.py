c0fdcb9fd82c18e6a324839c22589839c4db329a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:35:02 +0000

    illness disappointed dance alive now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
