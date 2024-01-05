d5aad0705a766f2f0d67fa9bea78d456177401ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:38:06 +0000

    expect wish date learn operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_octal(n):
+        return oct(n)
+import random
+def roll_die():
+        return random.randint(1, 6)
