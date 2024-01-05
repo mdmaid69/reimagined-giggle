6fa5a4b95486f20719bd8f7accfad425f058ece0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:42:45 +0000

    problem ourselves hold night giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
