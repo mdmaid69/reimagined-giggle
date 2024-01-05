160c5dfbd90074c656963b8eb48e6e335ffcbbfb
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:00:43 +0000

    pure preparation workon concern dick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import sys
+print(sys.version)
