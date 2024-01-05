7dd2e989388e25ae7ff41e66bac02cebf8e7b9d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:53:04 +0000

    bridge talk after miss intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
