3f04084f1e7773d7829f74d2ff2d9a25613991cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:32:12 +0000

    grateful watch accident over office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
