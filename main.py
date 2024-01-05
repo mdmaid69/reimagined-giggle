7a382d3871eb1458274b6ba61d73175511271986
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:49 +0000

    save accidentally even snow below

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
