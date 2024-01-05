8841dae0de4335794c99d158d1e8339f336c1e7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:06:10 +0000

    exit dangerous form talk everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
