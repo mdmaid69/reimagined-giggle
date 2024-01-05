7159fff40daaca8d629207fbe4d067a20ee400b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:35:12 +0000

    unless prove copy problem easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
