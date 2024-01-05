a80dce04e29a21316df67b207f8e54b99b0bebb1
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:01:36 +0000

    house minimum chin equal believe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
