31f4fc2038c70d95645cacbff5d837f4b36a7f5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:00:52 +0000

    spite cause guess whistle intelligent

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
   def list_files_in_directory(dir_name):
         return os.listdir(dir_name)
