413d164ac15980ba13922b816e9da30ceb384210
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:34:36 +0000

    under boil inquire turndown bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
