cfde6d9e771eea5f5d560c69b955a61232565c1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:21:03 +0000

    bite by test square cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_str(array):
+        return str(array)
