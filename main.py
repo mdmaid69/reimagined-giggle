a1effd2d08090f921eeba91189c053ccc9c56f83
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:36:02 +0000

    communicate give simple straight male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
