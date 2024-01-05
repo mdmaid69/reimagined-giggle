af50d4036bef410e47656c49526dcefb8f456ffa
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:20 +0000

    passage cut feed issue game

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
