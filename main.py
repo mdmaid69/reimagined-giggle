af5a246737841b91bfae152e1db832fe9c4709ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:49 +0000

    doubt belong somebody gate effort

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import collections
+def create_priority_queue():
+        return collections.deque()
