41393f1672bb4d832e46182e04ada5d9ac14552c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:51:26 +0000

    smell factory its traffic plastic

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+for i in range(10): print(i)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
