cec023450dec18b8987ff4b4c06e3eb7713929c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:02:23 +0000

    plain truth section star word

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def add_numbers(x, y):
-        return x + y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
