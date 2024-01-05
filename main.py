2bb5aadbe38c0ebe55a81a6cd34c1a876bb5f140
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:09:38 +0000

    solve dream trick confident off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
