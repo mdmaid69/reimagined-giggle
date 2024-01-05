100ee59fbff5d54d1e65372502fb6a36f0d55214
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:24:59 +0000

    under characteristic geton especially obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_max(numbers):
+        return max(numbers)
