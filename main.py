5df95868b4b801349c6e09410ae3ddb70b63ae95
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:54:58 +0000

    glad himself job couple score

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+print(time.time())
