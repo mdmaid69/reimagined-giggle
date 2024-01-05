59c6b586cd6d42225d5eaa73cd32342a3d300a19
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:52:41 +0000

    set off motion contribute pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print([x**2 for x in range(10)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
