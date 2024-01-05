823227db1bf06941edce6b4ad52cca53fa9b5aad
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:57:13 +0000

    Uu big chief noone mad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
