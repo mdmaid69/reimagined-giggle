635a906568e979566c993886690f88a738ca7cac
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:47:48 +0000

    agree goodbye serious label necessary

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
