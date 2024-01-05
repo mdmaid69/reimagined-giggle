974cf78a942892bf4ec2f03d8d82b03627cb8b5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:12:54 +0000

    name middle funny will drive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_speed(distance, time):
+        return distance / time
