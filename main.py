9c44c5fa7ca4fa4f1e8b967165f940a842591cc8
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:26:43 +0000

    bored usual slide breakthrough broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def subtract_numbers(x, y):
-        return x - y
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
