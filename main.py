5f8474e709d142d07fcb37fb1e9ba5e097218ca7
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:31:39 +0000

    during maybe grow paper honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
