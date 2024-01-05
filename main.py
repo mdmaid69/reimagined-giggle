e2d7e0ca02a041ab4a95f5d3945d493ab21e9ad9
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:50:46 +0000

    positive imagine glad happy interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
