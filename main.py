6f1afc6ad469ae38ddc958b1090cd8411008081d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:55:25 +0000

    hard uncomfortable rid occasion closed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
