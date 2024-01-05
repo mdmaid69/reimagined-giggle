77a7db8f52801023034c3381b9cdb08e96c1cb1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:52:51 +0000

    cut direct door cover feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
