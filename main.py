8522ab64d3e235d5ac880f79c3fe639061e84f09
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:13:55 +0000

    achievement report copy impressed exit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
