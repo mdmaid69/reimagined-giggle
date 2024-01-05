13e10e71710b9a7da0dcb69c8866d125dbee4ea5
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:52:38 +0000

    closed later stiff possession circle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+print(re.match("h.*o", "hello world"))
