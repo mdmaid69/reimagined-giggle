5c0a52cfec5a4af2011cc0308aafaee7ec937bdc
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:21:52 +0000

    size criticize extra top half

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_rectangle(l, w):
+        return l * w
