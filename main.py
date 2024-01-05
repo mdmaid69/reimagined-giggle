d74c74ef52366544e118c4d054083684dcfeb09f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:38:21 +0000

    protection succeed promise disturb least

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
