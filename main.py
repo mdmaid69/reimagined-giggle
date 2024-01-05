e5a6a50a86530ee938a91b9f516ec1cc8c9a1732
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:06:41 +0000

    doctor except well bush deliver

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
