d9d496803cb0276d8e9aaf2f2a911f306547966d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:05:25 +0000

    immediately display punish considering before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
