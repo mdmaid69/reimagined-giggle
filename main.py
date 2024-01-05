0a4bd4f39fdbbc9634753708bfc75ec9782f9ae2
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:49:38 +0000

    drop physical comeover taste relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
