877ea7e8ffe35b407733bc18d960be271a4fe399
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:29:25 +0000

    react cheek wind dark Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
