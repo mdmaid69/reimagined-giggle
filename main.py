cfcae3f03a4112d697dd230b0db9e816650919f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:36:10 +0000

    platform completely high set please

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_base_name(path):
+        return os.path.basename(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
