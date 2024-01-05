17c2aa702a070659b3bd610818421c4f3b3dc617
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:26:38 +0000

    birthday us worry rich arrive

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
