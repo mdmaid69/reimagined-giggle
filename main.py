19e7d8a8ac1632caa4317402d4c23b61fb63ffde
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:57:18 +0000

    arrange mother smile reduction loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
