bdf6ed686ee595162b87bef4a3e6ad6df9b8e120
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:20:54 +0000

    hers dress about look comeround

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
