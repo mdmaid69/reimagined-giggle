1979b358b9dd8190f29f0cbd8ae1f076ec7180fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:02:40 +0000

    father promise giveout wind strange

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
