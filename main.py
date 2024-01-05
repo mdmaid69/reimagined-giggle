77663ef8a45dd6eb03b7a5eddeae6b281eac42b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:36:21 +0000

    appear offence face out pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
