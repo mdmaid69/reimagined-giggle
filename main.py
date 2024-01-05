e97f48a337292f0f8f538386e69c09283838f5a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:45:36 +0000

    hungry challenge game meanwhile themselves

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
