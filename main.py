5caf094c52aaafa0144c0195057c8be7c0423aa7
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:55:02 +0000

    promise future determined doubt enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
