a9a2138aa3a53bafaf08ea206305dbad9b706732
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:57:24 +0000

    post cut used plan somebody

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
