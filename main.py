d72306998f7ca5d378c403bd622650f967313a8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:07 +0000

    discussion condition pinch conduct concerned

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
