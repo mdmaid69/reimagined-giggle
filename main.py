f4148b6e224f581eced2c1b2c49a1df81a5a4e7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:08:30 +0000

    measure national centimetre mad pocket

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
 n = 5
 result = 1
 for i in range(1, n + 1):
         result *= i
 print("Factorial:", result)
+import sys
+def exit_program():
+        sys.exit()
