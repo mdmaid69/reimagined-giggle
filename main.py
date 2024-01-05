35019376a799fcf01c53e5229516d95f0eb28b09
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:12:09 +0000

    juice sharp cover worried nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import sys
+def exit_program():
+        sys.exit()
+  def sort_list(lst):
+        return sorted(lst)
