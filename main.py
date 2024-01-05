f8dd4749285ab34a82f4954128b2977ed86c7fe4
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:50 +0000

    reserve apart either woman hope

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
