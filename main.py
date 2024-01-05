7c481c4c0afc330f9b85e9ed2045200b3eeda4d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:27:15 +0000

    breakout name use thing exam

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def is_palindrome(s):
-        return s == s[::-1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
