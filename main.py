7f91de65b5bc2cabf88ef5252ee9a4150626ee5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:29:46 +0000

    crime bath visit go child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_area_circle(r):
+        return 3.14 * r**2
