0cee5f37cef26d3e802dd3632574e52eeadff50b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:05:36 +0000

    convince under warning minority only

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
