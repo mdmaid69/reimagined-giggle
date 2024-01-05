186dd735cce23ad3584ea542bc38a19dc92cb3a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:30:51 +0000

    confidence give needle protest basically

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import sys
+def exit_program():
+        sys.exit()
