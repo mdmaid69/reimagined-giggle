5fcfcc0b8941a0bc4f4d0cc6f0625c77a6fbfcb6
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:46:20 +0000

    purpose water sell design tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
