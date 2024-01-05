33a5f3d1b76087f1f6a5f23a4877eca761521e69
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:32:38 +0000

    refer crash schedule discovery Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
