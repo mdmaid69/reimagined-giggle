c160120269a5e600208d0afe65e2f08c09bbaad9
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:44:18 +0000

    Pp future so apply play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
