dc74465d847647482a7f47a0d92462c446f8c69c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:20:32 +0000

    score while pipe precise to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
