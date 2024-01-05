63edbd7813a028d2553da4a100e584138efb9552
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:35:04 +0000

    crash breakfast separate hard dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
