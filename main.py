c0006ee199f12100fbe95f85b06863d86054e559
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:52:28 +0000

    hit attempt advise left protest

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import sys
+print(sys.version)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
