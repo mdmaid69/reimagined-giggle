5fb517272c5831d9151398716fe453d78e843727
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:28:55 +0000

    wear important even message talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
