e55b95a4e796fe7a00c82a34e3441418da98221a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:52:00 +0000

    country everyone admire tone exhibition

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import sys
+def print_python_version():
+        print(sys.version)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
