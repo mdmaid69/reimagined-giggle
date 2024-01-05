17a3ac50ee6e04467bd89a7584a4a6cfe5b41d1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:33:45 +0000

    again the sit cope advantage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
