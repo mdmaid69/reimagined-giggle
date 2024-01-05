4266671b7665491fdc3a45bc074d17205f295ae6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:40:40 +0000

    sick takeout sit surface cough

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def is_even(n):
-        return n % 2 == 0
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_power(work, time):
+        return work / time
