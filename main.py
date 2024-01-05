05ddc8ef4f8344d477bfebc4abc1a462a5339a34
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:17:45 +0000

    wheel plate room whistle suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
+def square_number(x):
+        return x**2
