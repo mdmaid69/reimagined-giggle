ffbd0431dbc888425cdf617346d01445be56d89e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:05:52 +0000

    money conversation period exploration wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
