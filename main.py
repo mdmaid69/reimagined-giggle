c939d62893f1e0ad120b5e3145719c8d712a6fee
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:43:28 +0000

    tap cost follow interfere close

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
