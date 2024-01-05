2359bb78945be2453209f3a2c7e29d59f4b93f22
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:09:44 +0000

    period best wear favourite design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
