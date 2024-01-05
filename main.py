b41d46a5eba28a288adf664c7f562dc17e0bc6cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:25:31 +0000

    contrast unhappy delight miss owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
