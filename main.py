5049d623d6438b49f5e0cddfff319206e9a3b003
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:55:01 +0000

    party plan compete travel cover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
