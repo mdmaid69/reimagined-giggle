10a870598e8172885df71782c114b103571db8f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:41:21 +0000

    insist star offend pocket section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
