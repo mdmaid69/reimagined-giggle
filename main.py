f2f5d3c9b020d5d7dfb3e0f6d6cd055069fcba4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:29:51 +0000

    talkback sit trouble interest pot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
