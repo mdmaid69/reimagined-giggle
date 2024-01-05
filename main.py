4a251f250e138925cf5d7d2df0b7506333359511
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:15:39 +0000

    minimum give typical alive crash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
