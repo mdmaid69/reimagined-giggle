f2b2caafe029acb80f195d6248358021d33eb030
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:07 +0000

    swim space rest light intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
