320018d5b745105816539a99a6010396c267a8be
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:44:42 +0000

    gooff difficulty final record swing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def clear_array(array):
+        array *= 0
