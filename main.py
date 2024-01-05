d79f01c2045ce0e40ded79dbc17c294404451a67
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:50:31 +0000

    explanation 564encourage hide tour consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
