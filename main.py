9cd3c55b477778d378a21444ff30554e9609c6c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:27:41 +0000

    creature rest ring meeting runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
