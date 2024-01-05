ba3b02dced07db77d69b59dd2a7e6f4763aa8fdf
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:01:06 +0000

    wash accidentally deaf precise different

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def count_characters(sentence):
+        return len(sentence)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
