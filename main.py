d1daa5ebcd6072fc63152813dcf240e5e32ce6f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:12:42 +0000

    comfort violence live advantage square

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def greet(name):
+        print(f"Hello, {name}!")
