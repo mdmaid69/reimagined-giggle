cf36a9236f7c725732d5b6d17af660a074b7589f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:04:17 +0000

    buy estimate blue any design

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
