b6c8a4dae733bfbb5d9331cea7dfb91aba26d6f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:12:17 +0000

    punish bringdown matter minimum language

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(5):
-        print(i)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
