cd96ebd2266d5ed3c7225727d4656549b6e6c810
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:41:54 +0000

    purpose brain insist switch first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_directory():
+        return os.getcwd()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
