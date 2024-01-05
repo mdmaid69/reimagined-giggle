a3283bc9d2f76091fd261b6b135c4538cd40447e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:32:40 +0000

    off evidence workout money example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def convert_to_binary(n):
+        return bin(n)
