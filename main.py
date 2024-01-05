9d76bbb418c6a7de27edaeae32ee7666aa0ea4d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:11:40 +0000

    ago watch club tap wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def is_even(n):
+        return n % 2 == 0
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
