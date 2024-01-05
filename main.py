67c2c457092b4856d279057823bb570f6a107cd6
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:31:22 +0000

    test worried attention family eye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
