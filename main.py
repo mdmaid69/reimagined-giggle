7e34905355499863cd70765ba721113eb1b35051
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:24:42 +0000

    escape size which firm female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
