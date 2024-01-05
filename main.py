50820a906e7eba4ee66e7eb639a756463e050522
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:50:33 +0000

    noise minute 572enter whom upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
