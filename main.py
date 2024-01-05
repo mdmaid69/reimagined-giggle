e2fb3defba6f2bf547c876d4e1a1b9363e0a062e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:30:52 +0000

    law black powerful know trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
