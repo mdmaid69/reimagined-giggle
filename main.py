fe2531e3e6f73a0524ab7bdcc4a095324eaf6e08
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:57:37 +0000

    takeout care friend admire lean

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
