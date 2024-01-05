67e06bbb3fdd2340717aef21102996cb5cf442c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:26:40 +0000

    pocket address balance reply 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
