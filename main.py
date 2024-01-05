eae3777ce7c82f710c3532ddddafda3e5106ccde
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:13:34 +0000

    shape warn port artificial abroad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
