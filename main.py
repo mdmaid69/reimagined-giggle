dc4fdf8735fc62ba38c6c978ecb662385574e65a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:13:14 +0000

    charge comment almost stamp section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
