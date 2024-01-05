2bc5924f8dbf3f099d8e15f7bfa63d6dc058485e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:16:08 +0000

    scream method sleep voice dinner

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+print(math.pi)
