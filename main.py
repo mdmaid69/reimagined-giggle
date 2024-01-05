92d1934fccaf1c3b08798409c0c21e18a3b68b89
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:28:50 +0000

    escape sharp be sure time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
