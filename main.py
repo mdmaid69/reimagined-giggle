d81975f538a9781ed705f972ee8df585cc7e17fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:09:06 +0000

    unlucky scrape prepare off enthusiastic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
