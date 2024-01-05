9fd30da92223216d76e3f5a1096f7c5c6e1c14d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:25:53 +0000

    me wet desperate shock back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
