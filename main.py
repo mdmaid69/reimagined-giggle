9c616956c71b21741e1ce6c18dc2680efd031270
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:46:30 +0000

    discussion view prize scare dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
