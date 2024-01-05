3c82914a996c081bfd2bd07ed470cbfbb5ff7668
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:17:46 +0000

    sign theirs health sink party

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-print([x**2 for x in range(10)])
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
