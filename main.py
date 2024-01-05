e2e772cc12e5b93bb37b22bc222ec30784410cf4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:34:23 +0000

    age announce invitation positive means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
