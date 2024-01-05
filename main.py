196871f95e8044e2e040a88945d9998a306a2c67
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:47:42 +0000

    breakinto burst possibly means exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-print(datetime.datetime.now())
+  def get_directory_name(path):
+        return os.path.dirname(path)
