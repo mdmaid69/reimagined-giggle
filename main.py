805586f251c14f3c4092044db0335599a5f6be46
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:44 +0000

    generous brush note emotion official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
