5f072eb17a370999d64ac569b5d935208f584187
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:44:25 +0000

    familiar unless beat to zero

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sys
+print(sys.version)
