6dca26d24e5937c23477d3f6bcc4475367491804
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:57:12 +0000

    come male rest 565end wind

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
 import time
 def get_current_time():
         return time.ctime()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
