702a879a61e3eb59cd67ce7d27c3df536cdec592
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:47:49 +0000

    pencil agree concentrate demonstrate Ll

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
