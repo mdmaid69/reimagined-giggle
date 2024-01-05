9e4532fed22ed55b887a9ada9ae25f4b7ab48652
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:23:32 +0000

    neither improvement watch natural trip

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 def calculate_average(numbers):
         return sum(numbers) / len(numbers)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
