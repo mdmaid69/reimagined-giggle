e35b54e6dc04f7337bf0c64b5211f367ca33a8c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:27:02 +0000

    shopping press cheat cry reject

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
