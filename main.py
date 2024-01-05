a1afba99ed9b81b7488c485d50a14e4215d2d954
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:04:09 +0000

    achieve record murder responsibility sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
