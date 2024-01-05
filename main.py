50387de34e7b6d867aa630dd54ce680c9474e190
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:40:32 +0000

    wash unlikely but tap spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
