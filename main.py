36eecd5c021b707dbde6f46a6610e47073096dfa
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:16:09 +0000

    present square anything arrange fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_pressure(force, area):
+        return force / area
