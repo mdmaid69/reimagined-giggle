b2d877062e8c118b8d1c25eea1e2e454fba95e6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:09:50 +0000

    major ought look beat who

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_density(mass, volume):
+        return mass / volume
