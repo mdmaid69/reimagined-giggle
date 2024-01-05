581f125abda4bdcea0eba6a1adefcdd36996ce34
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:26:04 +0000

    about doctor after point shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_cosine(x):
+        return math.cos(x)
