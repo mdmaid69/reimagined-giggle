c158bc455fc5c5cb8f0ad0fc2149cbcac7d1e753
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:33:24 +0000

    afraid declare try bottom lucky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import collections
 def create_queue():
         return collections.deque()
