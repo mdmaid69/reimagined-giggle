d254782029eadd49c828cb856b924c70e63a4d71
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:14:45 +0000

    rubbish loan here delight complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
