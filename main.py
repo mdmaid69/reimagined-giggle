b59a53d804f8ae65d2dd0f0ff809124f46f50923
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:22:05 +0000

    action week simple study boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
