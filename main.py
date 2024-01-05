d8f90cc5927eb1370d635548b1de1bbb3df11876
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:26:06 +0000

    question side may mean skirt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
