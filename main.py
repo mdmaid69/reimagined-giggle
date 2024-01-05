98c10a0ec46735c0c92f323dbd02baaae4a43add
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:43:22 +0000

    born goon failure scare love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
