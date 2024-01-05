09b329d94f82b273b95f5830a545700ad05e3495
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:41:20 +0000

    use shake cut convince total

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def cube_number(x):
+        return x**3
