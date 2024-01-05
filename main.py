f14fa045721f476da15afa694540719893f577b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:22:36 +0000

    trust mix left fork few

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_str(array):
+        return str(array)
