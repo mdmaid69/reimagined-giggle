6fa9bf1c8e1619005e3f9f30a6f4742af707f242
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:42:12 +0000

    recently feature weigh group mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
