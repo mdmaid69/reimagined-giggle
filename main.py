c4bfe8248012b4be2f11fc07eefaf85fa5cd58f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:31:01 +0000

    string copy shout really so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
