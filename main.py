e15f443b9a333a626e49c76b61f2983afa9a9419
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:45:21 +0000

    violence swim direction punish during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
