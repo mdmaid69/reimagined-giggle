a244000746c94caefd6a277308301016738d1adb
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:19 +0000

    goaway strength among along arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_typecode(array):
+        return array.typecode
