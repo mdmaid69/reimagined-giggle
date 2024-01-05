0468bff5d14123b461e6c72700776cb39e77b552
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:13:24 +0000

    hard raw turn niggaacademy part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_work(force, distance):
+        return force * distance
