f99f142c72d1c948ab1a7051fa39b5d0ba50495f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:06:44 +0000

    image silent say answer public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
