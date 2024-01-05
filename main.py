41bcd0534ed4bc3c5778a61bfe64f3f21995b07e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:48:31 +0000

    break follow precise before rich

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
