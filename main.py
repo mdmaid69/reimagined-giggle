3f49e374550f7fc891761cd8a0088d848a29cbd0
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:24:04 +0000

    declare question naturally body move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def set_array_item(array, i, item):
+        array[i] = item
