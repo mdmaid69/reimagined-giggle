4f2f5f2967122d7195d8f4cc459ba310dcd58074
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:52:31 +0000

    crime purpose many correct dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
