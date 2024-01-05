d1ceca0897d76bafd769575bcab7ebe8d938d8f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:29:14 +0000

    suck greeting underneath club fork

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def remove_duplicates(lst):
+        return list(set(lst))
