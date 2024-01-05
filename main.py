d7afd1364d850b8fa8fec9de0a6f3f0140e94546
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:41:04 +0000

    patient several dress face dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def find_max(numbers):
+        return max(numbers)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
