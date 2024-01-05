7fcca6aa14f15400175f1acb86f83ad6a21a0202
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:35:57 +0000

    heavy offer ability tidy maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def sort_list(lst):
+        return sorted(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
