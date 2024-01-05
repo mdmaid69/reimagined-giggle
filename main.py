04612ac00f49617f3ea3e2561cbef1eb0948d63f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:37:59 +0000

    black future until marry recently

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
