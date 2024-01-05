ac7496043d5c8082e5f63acb2874ba1c944a3992
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:15:51 +0000

    confusing single stiff entry properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sys
+  def get_python_version():
+        return sys.version
