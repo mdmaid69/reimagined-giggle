eaa71455972e243c4824054b28a87bd1e46f9ebe
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:09:16 +0000

    lookup tragedy upstairs way all

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def sort_list(lst):
+        return sorted(lst)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
