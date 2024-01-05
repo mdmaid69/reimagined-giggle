98bd3c5faf1fc534ea0bdf8108413723371117dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:16:06 +0000

    experience expensive impressive proud pass

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-print("Hello, world!")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def find_min(numbers):
+        return min(numbers)
