593b7fca3142232c39235b5013b5d8ff727aa0d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:21:42 +0000

    hit improvement design right nice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
