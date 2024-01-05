5c8ca88e8a7c128bdf9161eb447c9f4298601393
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:30:58 +0000

    all settle wherever ordinary curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
