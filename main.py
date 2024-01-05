4bec3cb3ea4656d05d3d32f2ca191bb305891ad0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:51:57 +0000

    skirt soil city edge pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
