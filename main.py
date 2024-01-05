14fe96f2ba3c1762ceae867fa44e6c8c4894958f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:01:26 +0000

    fire guess condition open storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
