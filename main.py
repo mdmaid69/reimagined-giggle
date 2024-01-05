1639377c7092a0d689455af91bb364a35ec52620
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:35:16 +0000

    bit pour look when interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Characters:", len(text))
