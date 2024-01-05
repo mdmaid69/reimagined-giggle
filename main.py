e9e9b2ef02d35fb38068234f416cc2a6b08108eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:37:05 +0000

    large refuse spin play steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
