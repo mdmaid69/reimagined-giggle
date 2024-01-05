6bb1626f23cccd78d41b0902d93601207eb00c90
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:26:56 +0000

    shake fly Vv them payment

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
