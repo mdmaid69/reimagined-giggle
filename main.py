61adfe956a30e178218eb942487e7e639949c5c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:03:53 +0000

    tidy future complete shape piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def square_number(x):
+        return x**2
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
