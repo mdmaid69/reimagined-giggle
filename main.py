92ad97d720518601a3c7273a9034637173c40397
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:20:20 +0000

    rush already official surprising Rr

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
