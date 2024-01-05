0a5d281f0ac0a5d7b17fb3bb9843e57fae2a02bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:27:46 +0000

    intend behind Dd announce translate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
