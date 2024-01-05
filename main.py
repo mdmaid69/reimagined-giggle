83c56e4622e29900f30115d5d36cf4f47a0241d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:38:47 +0000

    fork declare gentle workon afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
