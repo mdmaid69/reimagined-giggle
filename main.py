5c81b72d822e05c56a81fd6c7b80c820918366ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:50:20 +0000

    breakfast substance importance upset announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
