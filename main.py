cfbdab12c91a9310b1bb91fbddd879a7e32fbc34
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:54:38 +0000

    majority official opposite form thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
