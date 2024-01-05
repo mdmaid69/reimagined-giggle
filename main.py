dde3c057afbbae2c60ded18cdc7e4052b3ef9387
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:42 +0000

    high lie along right nowhere

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
