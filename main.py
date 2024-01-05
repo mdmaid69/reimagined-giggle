1122c2472858713219fe4ac332936355e85043fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:02:20 +0000

    commit lend line delight plate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
