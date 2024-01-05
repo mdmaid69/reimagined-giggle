5dc9f4226e9a4792c22965b12579d4a8cd66eff5
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:46:16 +0000

    spill now Pp wrap either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+name = "Python"
+print("Hello,", name)
