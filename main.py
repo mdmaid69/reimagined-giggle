ce29a0a0c2219de2f2f63b5849fa6438685c968c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:46:58 +0000

    union exactly no giveout increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
