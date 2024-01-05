9d9bcaca605c6d8d78b0fce23f2a5fb78fd8e3c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:31:49 +0000

    steer wild separate unfair suck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_array_to_bytes(array):
+        return array.tobytes()
