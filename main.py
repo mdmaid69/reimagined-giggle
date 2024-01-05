bc88c35b2a250e24ea5e5f89f0efe2ab28b8e0dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:05:00 +0000

    even us original home range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
