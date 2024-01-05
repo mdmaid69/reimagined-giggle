9ad7ef911cba8e067ada939faece103e207ab607
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:11:29 +0000

    threaten central tight guard mad

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
