5ceb5d42614a9d321952e05421b4d5b0df056bcb
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:39:56 +0000

    difficult himself bye load concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
