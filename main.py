4413afd8ebf496beba15823a282bd109bcaa9ba4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:28:46 +0000

    public possible week present disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_list(array):
+        return list(array)
