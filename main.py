adcdda35434354f2f61c1985662eed56da0eb51d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:36:19 +0000

    talkback female leave away bang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
