cb8d680d6ec8a1863d86bfee4cc6109fe35105fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:24 +0000

    gift cough advantage healthy lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
