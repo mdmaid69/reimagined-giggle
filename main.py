a53b030cf2625427c9a15736f8be48da021e5923
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:12 +0000

    find date change score shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
