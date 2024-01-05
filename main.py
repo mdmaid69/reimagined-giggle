9aaf90e96631fb33772e57e20366a174a048f214
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:51:47 +0000

    fold trip sense contact confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
