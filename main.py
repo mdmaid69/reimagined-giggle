dc5105ce029dbfbd7fafa9070aeaf81126403e2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:19:46 +0000

    deep getout dream string convince

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
