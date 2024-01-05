8000e0671abac0f271bce5111d3af929d5033d5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:38:45 +0000

    pressure come few report street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
