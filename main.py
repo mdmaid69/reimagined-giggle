15a6706762f9bd26c8b7d92d382768b31583098b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:42:33 +0000

    behaviour boring loan clothes ball

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
