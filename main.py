22f7138358e8e334320c166ae71cf52f8e1c8bfb
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:24:45 +0000

    or brush feature free government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
