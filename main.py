f19a62cc1287e8e92a03eeb31c66020404e1dbd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:05:32 +0000

    lesson along characteristic music leave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
