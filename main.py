106eb91cb47b6e65932a991fcf3df3189ec14a67
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:14:51 +0000

    loan normal more quality hot

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
