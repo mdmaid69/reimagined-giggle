4c16e5927143e75a1a81e4983fad86561b8cca52
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:39:12 +0000

    careless minimum good dream walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
