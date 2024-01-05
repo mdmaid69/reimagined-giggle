023c049c0037ca254a79d394bd58e03ef2f2ce9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:23:44 +0000

    person tidy precise maybe speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
