2243f680532eb37ebb4d8f2d37075569fab80e54
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:51:57 +0000

    holiday introduction spite through sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
+def calculate_acceleration(speed, time):
+        return speed / time
