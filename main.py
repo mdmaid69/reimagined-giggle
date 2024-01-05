ec7655e6b123758a5aad1964c04264131b6f57bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:46 +0000

    believe earth date grand cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
