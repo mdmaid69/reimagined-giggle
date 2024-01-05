f76a6f2e2c07107c4d5f1b4b5de0a42a035cc91e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:27:20 +0000

    proud try afraid aim not

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def convert_to_hex(n):
+        return hex(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
