219d2be5275338135d1c68b36164f0d186475b4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:14:29 +0000

    reduce suckme dig regional but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def reverse_string(s):
+        return s[::-1]
+def sort_numbers(numbers):
+        return sorted(numbers)
