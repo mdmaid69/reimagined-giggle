37c1fdc52f5df28def40195b72ed6c0880f739cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:19 +0000

    instruction year prize anywayanyhow failure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_rectangle(l, w):
+        return l * w
