07c3cedd51a7585f81bd5d4e9c70fe941b9af234
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:00:21 +0000

    cope otherwise that free lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_int(array):
+        return int(array[0])
