b4ddb30aaa973eed8082f8950617af3f82aa4ebc
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:53:35 +0000

    tone may father both thankyou

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-print([x**2 for x in range(10)])
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
