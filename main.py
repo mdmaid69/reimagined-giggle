3a0a94f2f27d08e8e20d9607548aee668574eea9
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:10:08 +0000

    worth method smile 569enjoy bet

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
