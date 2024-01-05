618130f7e800df21aab1d4b548456e20c4f4139e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:00:48 +0000

    convinced to prefer message protection

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
