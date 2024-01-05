13bc5da8642e5a6f54a389425e2e86f4124be248
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:49:58 +0000

    practise luck deaf difficult survive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
