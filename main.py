464ccbbde97c39677504a09229fc7960b847078d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:46:28 +0000

    store curious old towards copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import sys
+def print_python_version():
+        return sys.version
