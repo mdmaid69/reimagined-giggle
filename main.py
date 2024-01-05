d79e2b0c44c7c6eab4b10ea30e51478994114f02
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:06 +0000

    there lookup visit name turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def sort_numbers(numbers):
+        return sorted(numbers)
