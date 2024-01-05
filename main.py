11dde1e2c5ca139e45e6a24233c6968a89d8c2d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:50:09 +0000

    voice test wave piece idea

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_array_to_string(array):
+        return array.tostring()
