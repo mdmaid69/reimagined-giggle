64c3eb1159a6d56b14b102ccacf9ac93fb5b31f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:03:25 +0000

    gooff give private practical care

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
