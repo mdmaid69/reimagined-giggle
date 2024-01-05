dde3bb6d893c98e8da4a381d41d50b764444975c
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:55:08 +0000

    hope immediate steal dinner produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
