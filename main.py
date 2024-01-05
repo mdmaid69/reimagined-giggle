08e8465a61cb7b93fa9653f94e3d8cbba58a3336
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:50:25 +0000

    control idea claim cost chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
