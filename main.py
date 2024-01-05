bad3552852e08ed5a579a22abc259ce479fb8a6a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:09:37 +0000

    union slowly comefrom fall agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
