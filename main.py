9bba0d76276d9a64ac079ad33985f15d2509b877
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:19:08 +0000

    cool swing kind in paper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def clear_array(array):
+        array *= 0
