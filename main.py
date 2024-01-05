fff8da5cd0570e18afa4b83d5e490e15350ad8f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:51:37 +0000

    love side term centimetre calloff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
