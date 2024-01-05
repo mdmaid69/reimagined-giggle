a5ce4ea8266b2c188bcd01d5eebb2a86e92a53f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:58:41 +0000

    boil increase plan claim behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
