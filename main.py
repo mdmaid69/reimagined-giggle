e07b986b0c89fad5cdc1e39e88f5dd21b1b87f75
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:44:47 +0000

    concerning 573entertainment accidentally exaggerate also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
