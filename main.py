d0ad993995e982e6ddc68d664f3ceadfc127f673
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:57:27 +0000

    protection compare swim cum hit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_min(lst):
+        return min(lst)
+def calculate_power(work, time):
+        return work / time
