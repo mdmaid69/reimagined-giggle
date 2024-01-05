db8e3e423e455dd5f53ed6a92e66f902cd48a14b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:54:11 +0000

    inquire nose short seat play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
