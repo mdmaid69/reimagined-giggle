c30511a68426405b0ea51f08203d61bd46f3354e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:49:35 +0000

    thank pinch only takeout disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
