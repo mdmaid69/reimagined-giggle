bc6a76a14c3eb3fe4cd3295e16a32185e09bc151
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:17:48 +0000

    family female weather relax almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
