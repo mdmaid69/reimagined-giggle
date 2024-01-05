eae31cf7400ce27ad268fccb6b6c8ca1710ffe1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:51:47 +0000

    sharp plastic situation include unusual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
