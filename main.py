4bdb63667ae5ab5f2291e42021a63d763abcfdef
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:34:25 +0000

    model emergency design claim by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
