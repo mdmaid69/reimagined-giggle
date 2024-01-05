c3ee3cdf51e462b6915b2444d78245e692fc566d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:39:43 +0000

    at name fun lean space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
