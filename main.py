7d9998d5f02215756cfe8f7390038a09c37b93e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:51:09 +0000

    drink war part behave mark

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_min(lst):
+        return min(lst)
