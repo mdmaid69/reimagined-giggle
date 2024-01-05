10d252891a073567dcda5e6264d9b3760d2bfc95
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:18:52 +0000

    bread lookaround repair used goon

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def sort_list(lst):
-        return sorted(lst)
+import collections
+def create_user_list():
+        return collections.UserList()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
