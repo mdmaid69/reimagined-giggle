7b45d07492ec5ff4b931e929f30ec74488a6c805
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:45:25 +0000

    ought gate sometimes rain tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_float(array):
+        return float(array[0])
