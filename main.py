097ba75354287b586a9a0af85f9fbf904088e4d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:08:28 +0000

    rest meet Bb print turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
