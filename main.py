8566daa495afe259a7a69f8e26cc53d6fe8889bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:09:53 +0000

    trip rob thin attract quantity

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
