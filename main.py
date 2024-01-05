f083eebd10a6ea91fc472b38033157ac24b7ec3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:51:53 +0000

    shake university sentence general say

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def add_numbers(a, b):
+        return a + b
