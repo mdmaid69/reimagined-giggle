37bd0f847b39994fdd4a857fee6714dac04c86c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:44:56 +0000

    successful Gg suggestion cold include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def get_array_as_bool(array):
+        return bool(array)
