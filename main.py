512c64c9a0fe81332e07dc9283f6f769633573de
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:36:45 +0000

    before announce who interesting record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def find_max(numbers):
-        return max(numbers)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
