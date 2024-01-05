80010bedf13b599fcbd1365e79fd8a28d5c275ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:29:36 +0000

    ride woman repair because new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
