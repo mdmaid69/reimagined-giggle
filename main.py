f5baff502416044491e2c8522f3ad63f05838f27
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:35:00 +0000

    list runover name shadow soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_current_working_directory():
+        return os.getcwd()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
