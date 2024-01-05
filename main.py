7f2d512f390f01f4c9d1f61639c51c6b67951e4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:54:05 +0000

    night idea sad change simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
