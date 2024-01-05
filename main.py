cc1535e1a9768575e392d7f5b0e74e3c96257199
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:39:29 +0000

    off telephone shopping workout also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_counter():
+        return collections.Counter()
