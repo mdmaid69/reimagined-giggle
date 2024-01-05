d70bc8653f3444ca65ac1b605fc58c98d71ff0b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:13:11 +0000

    minute phone turnout dangerous fail

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-print(random.randint(0, 100))
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
