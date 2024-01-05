c587551c9ba50642d8e0e1af751d3ddb0c78dd25
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:46:34 +0000

    threaten promise roof Tt visit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
