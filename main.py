7c77084ac65b1abe4b2ba0a2bd533b2d4787eaeb
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:44:49 +0000

    or between bored dress hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
