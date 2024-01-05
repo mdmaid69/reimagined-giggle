6b4bdd8baa316bb33e83d8e80e61049c868784ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:06:58 +0000

    prepare bar warn snow authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
