73ce99d1cc1506b62c3f842cfcb8c11374275244
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:12 +0000

    normal common thin sure push

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def cube_number(x):
+        return x**3
+def find_max(numbers):
+        return max(numbers)
