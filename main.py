fc9b105e6e4a468cdec7b0a591820d914f7a5e75
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:39:26 +0000

    book blind throughout likely perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
