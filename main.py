b59bdacc140e9bf0e7feb1341bdefab4030733bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:26:23 +0000

    embarrassing meeting loud hold pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def set_array_item(array, i, item):
+        array[i] = item
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
