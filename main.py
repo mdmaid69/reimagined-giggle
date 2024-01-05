e62fa9d0f666e4074a7875202fc27c6ff015a6e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:35:10 +0000

    control great nurse separate dead

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def delete_directory(dir_name):
         os.rmdir(dir_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
