f2e349d898f186716ccbccb8f09dd7e5a2e2e95d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:31:29 +0000

    secret entrance contact ilovedicks bend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
