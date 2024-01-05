0a1eef548e4af7c8af53369bdcd40d5fe250b033
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:23:33 +0000

    holdup edge paper block game

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def multiply_numbers(x, y):
+        return x * y
