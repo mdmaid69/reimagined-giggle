0d33bc0a1b0be5203b38e91a87ed3e73516e9096
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:12:47 +0000

    power breakup empty friendly deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def reverse_list(lst):
-        return lst[::-1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
