14422f709c90738c04759fae9b2c41f94fc4bdfe
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:37:14 +0000

    delicate trust shock yours weather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
