4a633a14c201a64b48d37be20dc9b7690ad5911e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:15:03 +0000

    scrap film nonsense post eager

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
