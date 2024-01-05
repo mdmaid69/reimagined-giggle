3866f959e7110d5a08cf21af29678ba1d5a9cfad
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:09:31 +0000

    quit choose hide understand wrong

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print([x**2 for x in range(10)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
