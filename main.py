6ccd3b2385f6ba7a7af500a4e1003aa692523b33
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:15:56 +0000

    clear pretend crash among depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def multiply_numbers(x, y):
+        return x * y
