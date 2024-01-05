da328b60a2ddf54c89ef683be014a05c1ddafbed
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:10 +0000

    desperate report file arm issue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-name = "Python"
-print("Hello,", name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
