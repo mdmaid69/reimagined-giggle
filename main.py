c21db80f83de4bd50b77090623ae9e299bad8ebf
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:50:34 +0000

    pain if period defend soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
