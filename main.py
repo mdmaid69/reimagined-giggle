84d37003d98d42dd0704338dbad5e476a901851f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:45:13 +0000

    badly absolutely wander interrupt destroy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def count_elements(lst):
+        return len(lst)
+def is_odd(n):
+        return n % 2 != 0
