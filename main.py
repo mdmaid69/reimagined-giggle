a0a6803017e7d85c75ac771b44e9d8ace094148f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:21:48 +0000

    please regional below afternoon bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
