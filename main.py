807b6572e46fadff291bd78ee91fd5ba28df5980
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:30:07 +0000

    still foreign interesting contain only

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
