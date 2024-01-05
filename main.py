09fdee2408f4e739f9cb7fc5cc98e7f4e33409a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:11:47 +0000

    computer church beautiful conduct be

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def add_numbers(a, b):
-        return a + b
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
