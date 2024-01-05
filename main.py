804b387199ca0e274dcf82759eac3e9809745bdc
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:45:06 +0000

    regret rid report reserve heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
