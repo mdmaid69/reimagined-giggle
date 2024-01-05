2443ae6c131473801e57feeb8704cb9fdf31ef51
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:19:43 +0000

    tread worth prefer shut powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import random
-print(random.randint(0, 100))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def sort_list(lst):
+        return sorted(lst)
