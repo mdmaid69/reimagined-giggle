84a626b624ccf57d218a55db9ff7d72fe02e2e44
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:35:09 +0000

    interested sign eager terrible impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
