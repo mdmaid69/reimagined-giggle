81d5aa27d5dbe4f5735290a37995289d5e9c42c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:34:51 +0000

    woman spot strike thought everywhere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
