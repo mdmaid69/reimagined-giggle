6b6582979072e22c30403597d89d9114674f349c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:46:14 +0000

    over Tt touch tread rarely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
