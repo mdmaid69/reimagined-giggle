2bacbff5cf4f3e4708e7b4c5048ffbcd1f5bd3d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:22:08 +0000

    stand balance various excuse powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
