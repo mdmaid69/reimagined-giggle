4c21ffe06c20c7e8590afe9943662654a6c22f16
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:42:00 +0000

    blue rather almost rent phone

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import platform
+def get_python_version():
+        return platform.python_version()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
