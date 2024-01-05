b906ec3f51766c8728e4f20f3efaa26329caf0ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:29:57 +0000

    turnover advantage concentrate tense explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
