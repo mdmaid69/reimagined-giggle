5d157d95b98495ea2f47e0ab3071979f48a5880e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:26:14 +0000

    store steer tense not yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
