470b21155e34cb4240e62c3ce432197ba248bdce
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:41:34 +0000

    beside drop hold collection ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
