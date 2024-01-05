a3a688eac35cd33759a44968e9c0de771c6dadf3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:23:53 +0000

    neither could human win friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def square_number(x):
+        return x**2
