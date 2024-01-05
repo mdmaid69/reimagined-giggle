268e6edc11a89f466382f0532e126cb9777e80a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:05:29 +0000

    flow question only use star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
