18e43159af42aac18ff47e65ec2d0bf1e9ddf7a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:50:23 +0000

    area activity think degree skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
