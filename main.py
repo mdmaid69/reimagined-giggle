1ba9b887186b431dbb448f0c6aed71cdc7f7c3d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:53:05 +0000

    group somehow any look dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
