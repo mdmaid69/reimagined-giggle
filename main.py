7592bcd0335f3f188dfb23a4bc9d7007b44f15f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:55:51 +0000

    welcome performance some herself properly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
