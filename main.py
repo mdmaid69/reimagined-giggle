faa81bc10f460994cdf52a407bc545e7ea466d35
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:51:29 +0000

    wipe speed crime lesson reply

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
