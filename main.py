b7f368dce5cd4edeed6aae1c13d791f9dcc994eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:23:54 +0000

    Vv reliable Nn leg hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_speed(distance, time):
-        return distance / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_index(array, item):
+        return array.index(item)
