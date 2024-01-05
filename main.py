890d78cebd4d59b84b9345e75a539892081410b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:13:20 +0000

    bunch butter trap tall visit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
