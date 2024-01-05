bcf01b96e4645292f5a23023099a2960e0347fd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:46:17 +0000

    569enjoy tragedy together blame anyone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def convert_to_binary(n):
+        return bin(n)
+import sys
+def exit_program():
+        sys.exit()
