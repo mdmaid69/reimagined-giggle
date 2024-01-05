f04c41f7dbeaad32c743ab66acabb7af94ce1ed6
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:42 +0000

    room terrible us give 568energy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def reverse_string(s):
+        return s[::-1]
