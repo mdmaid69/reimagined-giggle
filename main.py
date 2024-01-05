efaecba12bea67561665b6852723a1d6f1447ae5
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:14:00 +0000

    sort prove will small medium

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def is_even(n):
+        return n % 2 == 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
