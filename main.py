ddc153c3b5da874633e656995e329d5f5a89126a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:20:36 +0000

    problem autumn power danger programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def split_path(path):
+        return os.path.split(path)
