ff6124518f6767e9228b18096f068e3ca54ec673
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:57:37 +0000

    instead lead thing soft water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import collections
-def create_user_string():
-        return collections.UserString()
+def create_priority_queue():
+        return collections.deque()
