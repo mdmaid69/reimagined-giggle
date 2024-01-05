baaf41051f672ab90b98f3c82dc13920c29e6de0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:57 +0000

    closed insist likely ordinary fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
