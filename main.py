df3c1533b521db677e05f05f7b164241027302d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:55:45 +0000

    its able journey expression effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
