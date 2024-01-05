89ca0fb3230033c03283d8a707f2247397f273ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:00:17 +0000

    rain ear design theirs crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_base_name(path):
+        return os.path.basename(path)
