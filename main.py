36fb038ae5dda0cdfbd4ca1ebd062b45e615609c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:18:28 +0000

    break keepaway artificial fun listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_unique_words(sentence):
+        return set(sentence.split())
