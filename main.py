223acb55733caa87449f9022aa4b8778a19c3853
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:15:22 +0000

    equal standard sport nowhere wave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def square_number(x):
-        return x**2
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
