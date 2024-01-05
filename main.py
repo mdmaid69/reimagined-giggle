24a039cf65393a514eae28ddee2eec1e0f7d4e2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:26:12 +0000

    blue with keepoff urgent comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
