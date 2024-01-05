2a5eef6b0b54970ec8f17abd528e00e08fd132ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:16:59 +0000

    field welcome wrap show university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+from collections import Counter
+print(Counter("hello world"))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
