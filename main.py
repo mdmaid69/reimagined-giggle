115ddb67471c191215fff4e3fa29c3de7e35910e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:23:08 +0000

    bright bring crowd copy ill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
