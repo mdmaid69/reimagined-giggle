b9b4ca940e358141817c3658a764de1c8650e759
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:37:10 +0000

    fear eat often heart rubbish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
