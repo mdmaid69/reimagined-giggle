596248afa7d55104cce44f3083b771406b40700c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:42:29 +0000

    space mistake natural upset pattern

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
