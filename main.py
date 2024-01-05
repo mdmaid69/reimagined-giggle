8f6348793db4dce534f1f824d4cc24f9f29e523e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:52:14 +0000

    takeback heat thin switch float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
