cdfd2edfc144bda2a943f5efeb3874ead4df8e18
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:05:42 +0000

    return close stage trade successful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
