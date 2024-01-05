b69c9fdbaa080fedd88b6609854f2e7f3565ac15
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:00:17 +0000

    grow into target term mother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
