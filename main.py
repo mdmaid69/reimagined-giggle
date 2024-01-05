2a7f9fc380ca8155c85d519172b9026babd24ff9
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:31:37 +0000

    plant copy force design popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
