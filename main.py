2b219eccdacd6b8a331eb1b3fe3515240004e593
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:33:58 +0000

    sky prevent rate couple stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
