5f8d4a7c374fd0f14789ceb41e2e897e846360f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:19:20 +0000

    cope actually crash could understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
