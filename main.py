d8a4ea2087699964198d5e953faeb7b72b1801fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:28:18 +0000

    lock support close mixture follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import os
+print(os.getcwd())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
