ffdb943306005ed66a77739d0026831caf532b68
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:05:30 +0000

    earn set impossible slowly destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
