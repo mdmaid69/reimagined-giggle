a4b313d30ff3a9b00f5a72e4a4252f25797fc311
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:04:22 +0000

    rain lost television obtain school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
