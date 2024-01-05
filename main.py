f654a4227332e15b096c940b05242654d5ea38e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:23:59 +0000

    regret forever settle seem at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_elements(lst):
+        return len(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
