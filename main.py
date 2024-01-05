ca1a06c81d7ed41f926d4c7b2c5ca17191b3ea96
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:52:00 +0000

    mood development home shame thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
