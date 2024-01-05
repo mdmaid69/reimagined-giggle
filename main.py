fa2db1737efe76b3145a4dffd2dafc860df0d686
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:44:38 +0000

    hear minute grateful Rr during

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def square_number(x):
-        return x**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
