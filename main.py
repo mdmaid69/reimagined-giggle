ae01b5d2712d94b1062a490fc2a728bc104edae6
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:35 +0000

    how certainly powerful opponent list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_distance(x1, y1, x2, y2):
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
