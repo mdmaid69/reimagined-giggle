bb3006fc6377f1b8fae37fdf50fde261fda102fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:09:20 +0000

    contribute fat human area noone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
