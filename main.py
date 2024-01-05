82c03918c51a9c93e34c4b3ef4eab80a1c407609
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:48:14 +0000

    aim intend well door under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
