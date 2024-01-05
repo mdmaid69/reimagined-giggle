3ef7269810149924b3ba92a0e2a023095d80be9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:28:05 +0000

    everybody understand cook thinkover lie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area(radius):
+        return 3.14 * radius * radius
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
