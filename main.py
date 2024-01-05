ce868e954a0438805e4dfefb05958f1b1663e95c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:42:35 +0000

    plant somehow workupto lonely themselves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
