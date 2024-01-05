b6463356c63220a45f3a2fe3e37d6fb3894d9036
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:37:23 +0000

    sing murder black impossible this

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def reverse_array(array):
+        array.reverse()
