7431181bd19ebc003e9cdc46638da54bee510f84
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:27:44 +0000

    glad easy usual judge thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
