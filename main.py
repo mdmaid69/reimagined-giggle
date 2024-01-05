83b80ff105c24a3c0b4beaa3f46fa6353903a62e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:45:19 +0000

    bored waste once consult Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def clear_array(array):
+        array *= 0
