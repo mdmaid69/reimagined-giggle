ce6f8ee24cbe9bd201e29e6ad8cc37983504169c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:46:56 +0000

    fit worst believe cold unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def clear_array(array):
+        array *= 0
