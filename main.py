b2f9dc2e008ea3f53de4517c3044e8b8406dfef2
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:25:43 +0000

    store 567enemy anxious far subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def count_elements(lst):
-        return len(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
