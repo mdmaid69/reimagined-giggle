bddde7f96ae242c66d6e251002bcdf1f51d1e40f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:06:30 +0000

    central home settle angry bored

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
