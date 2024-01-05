b4249526a8637530bb5041e7c685ada9fd0c0454
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:13:58 +0000

    broken remark attach record reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
