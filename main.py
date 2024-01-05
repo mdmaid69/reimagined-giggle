fb716a56dee5982c1100d9808b81f669ac91103e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:33:52 +0000

    runover goodbye post sensitive content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
