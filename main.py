c3d4c3b19ce7d32641fb0cdaea4b4db9f7f9f68a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:37:41 +0000

    contain relation equal prove narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
