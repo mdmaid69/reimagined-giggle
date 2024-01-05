c72ddc45cdb0f5d6616b7c8fa05ce8c2bf2a88f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:54:09 +0000

    suddenly straight guess convenient comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
