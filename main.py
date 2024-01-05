10ba729d5f05eaa528a4c0c8c814e0771359234d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:56 +0000

    limit fairly tour wealth artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
