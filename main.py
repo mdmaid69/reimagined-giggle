8a27f5e8a80c5c2ade0ebcdfb819d9e605425c8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:50:21 +0000

    emotion workout tap final scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
