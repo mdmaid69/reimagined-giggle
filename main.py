13ad2267e016a217a955287f6b9103f6d8da7215
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:53:10 +0000

    command exit giveout knowledge same

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+print(sum(range(10)))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
