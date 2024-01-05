1b042b338042664b1ba2fc229d45f14a6520c43d
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:11:58 +0000

    crime add takeaway deep to

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def multiply_numbers(x, y):
+        return x * y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
