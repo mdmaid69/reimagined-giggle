a9f4c8e1b6240e19a99a4166fc451a1c66451606
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:53:36 +0000

    of contain they direction question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
