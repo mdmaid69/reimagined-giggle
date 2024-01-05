7ffa48034fddba341bd1d8f280d3d2030ade8afe
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:26:55 +0000

    enthusiastic please relative fault moral

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
