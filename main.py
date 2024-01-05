aeae5543bad75f6fe5c7e809d4d9946431f88797
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:12:11 +0000

    interest legal breakout stop tap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
