13dcfff42af589ba34dacbd9b366ee2b56dd5542
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:34:46 +0000

    flight heavy help correct goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
