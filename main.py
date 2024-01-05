c53ade85123b84db049a23cf4efa0979f7f86d4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:19:12 +0000

    level influence smart control live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_octal(n):
+        return oct(n)
