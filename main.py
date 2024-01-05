322a6994128d9f9bc2faba81c05566b5979d014c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:42:38 +0000

    closed inform congratulate argue raw

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
