4986a32b37d5cabcad12bfed457870c7f9fcd5ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:33:07 +0000

    whoever congratulations report base recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
