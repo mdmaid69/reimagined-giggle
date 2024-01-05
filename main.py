ca108e101ea3bc33c977ba109c450342df1b5f7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:26:28 +0000

    advanced book float expensive stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
