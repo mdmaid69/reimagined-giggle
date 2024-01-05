061e29096c2e3bf2972f8e334d3f5d73e1a7dd60
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:34 +0000

    Dd onto on strike spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
