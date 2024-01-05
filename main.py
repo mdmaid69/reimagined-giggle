20470b0fb182a0d5044de4bf7308467d44414d40
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:02:39 +0000

    government recommend exploration niggaacademy slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
