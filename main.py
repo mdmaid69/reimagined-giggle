d712e88ac1bdf230a482a56d4c66d4d29d89c20d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:44:28 +0000

    lay invite tight speech mistake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
