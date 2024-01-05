42783a889ba94951bbd785469488f5ebf7aa38a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:03:12 +0000

    broad combination react guard really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_binary(n):
+        return bin(n)
 import collections
 def count_elements(iterable):
         return collections.Counter(iterable)
