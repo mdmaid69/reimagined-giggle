fa44eca8da095d7d01896caa6a0cb0cf18de5d68
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:22:21 +0000

    capital adjective left nowhere get

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def get_array_index(array, item):
+        return array.index(item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
