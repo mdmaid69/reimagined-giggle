8f30ca4af3c6ae5119a93bce974fdf49db55986e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:46:13 +0000

    use trap noisy centre Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
