102217acb430bde3913935a0dc88ccb85c349df7
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:07:49 +0000

    handle comeup expect sit music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
