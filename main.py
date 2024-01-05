64b18bc2307a2bb1f1f3a0aa6560b948f869c5c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:19:14 +0000

    smell desperate concern hungry discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
