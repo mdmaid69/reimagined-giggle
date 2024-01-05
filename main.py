35dbb636a0357990ed4d3860a73ad59ff995446c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:25:12 +0000

    convince battery corner terrible mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
