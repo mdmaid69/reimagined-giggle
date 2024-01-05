7ad72de5629ee5e8b71a78f0f12c3466d54d3939
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:32:11 +0000

    immediate quality judge continue grow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
