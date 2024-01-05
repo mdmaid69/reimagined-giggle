8a783ed5cc8d6922afe6d56090673589ca9d81bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:49:56 +0000

    past mean pressure slow impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
