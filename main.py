3b03e673ad007940894119a3eeb9a670298f18b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:44:42 +0000

    reliable stare trousers couple stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
