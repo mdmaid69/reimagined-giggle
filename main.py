da563314cc171b1be93b76ad1ba34172f0fca482
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:47:00 +0000

    wrong sail accidental process sweet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+text = "Hello, world!"
+print("Characters:", len(text))
