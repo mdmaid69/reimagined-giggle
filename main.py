079dab979e2a10fb3ee50136d2182e8f793e9fef
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:36 +0000

    crowd blind charge turnup connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import platform
-def get_os_info():
-        return platform.uname()
+import collections
+def create_user_string():
+        return collections.UserString()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
