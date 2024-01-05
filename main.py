434820b13ee4df226bedac1381322f2eaecb7955
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:31:19 +0000

    flat below blow field lay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
