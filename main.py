88af88f26eed08a3729a4e4e2ad824d90a2f8795
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:24:32 +0000

    finger stick unpleasant duty attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
