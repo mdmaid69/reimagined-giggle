b52de2f9d2f54505895fba66590f6382aa2da461
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:33:30 +0000

    warning judge boat should forward

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def count_elements(lst):
+        return len(lst)
+print("Hello, world!")
