4b2a0dbb14ef1e05504d62a617e92e611654e4e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:25:01 +0000

    minor badly offence mess still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
