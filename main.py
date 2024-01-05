9aea9c019d94964afbd009d2b732aca2c1cf08b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:23:45 +0000

    cheek just face backwards thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def find_max(numbers):
+        return max(numbers)
