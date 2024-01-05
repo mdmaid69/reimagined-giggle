a318fca7e3dca70a8a0baf69f2615d2194ea1ce8
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:54:51 +0000

    move sweep unfair clothes prove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
