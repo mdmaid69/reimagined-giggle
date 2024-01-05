ab31bc2c61444a9c214163ab21aae9050a36d3b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:55:46 +0000

    sensible thumb normal disappear throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
