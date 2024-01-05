237d43e7f56773948fbf05334b0fa0a137b81b0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:41:55 +0000

    far moon manner I prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
