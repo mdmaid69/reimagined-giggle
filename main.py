0af467de957b76fbc83a7f92d9d1b3047bb8d564
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:05:05 +0000

    organization calmly common meal false

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
