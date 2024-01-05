e1625dc3f1ab8efc9a4fb517a009bbea673d4dfd
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:03:51 +0000

    broad guard autumn fire clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
