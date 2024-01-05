c96fbfcc780c880f0a5a9c0234c32a60dd15f7e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:05 +0000

    holiday progress fine famous steep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
