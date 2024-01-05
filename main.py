378e714f4f157eb33075989ad32084d0d228e7d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:49:17 +0000

    again think dear wash progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
