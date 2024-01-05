66593f6da2f50719f2686212585ce4648bb7a6ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:56:05 +0000

    buy flood schedule watch tense

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
