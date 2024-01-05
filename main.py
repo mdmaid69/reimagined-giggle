c99a6af34fb8382da604967001fea404ccd9d258
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:16:17 +0000

    declaration law clear play goaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import os
-print(os.getcwd())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
