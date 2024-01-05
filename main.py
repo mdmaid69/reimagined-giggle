52ecb3a1b04face2e5e20b10a7d6d7d213db6733
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:11:46 +0000

    present key only describe will

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import sys
-def exit_program():
-        sys.exit()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def print_python_version():
+        return sys.version
