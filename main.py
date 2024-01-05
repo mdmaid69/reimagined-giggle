0a31797703ff746c0b3824dcab9af01c4c6edf89
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:50:45 +0000

    material somebody consider rob door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
