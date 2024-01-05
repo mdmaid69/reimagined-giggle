70f08d7dce278d2108466d5ba9aec5a56e04b13e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:11:19 +0000

    tune bread after offend this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
