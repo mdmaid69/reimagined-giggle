3a3641c3c448a0ceb8c65f38233056e39332d3f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:41:15 +0000

    chat chief compete trick shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
