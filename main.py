aa8e68d36e39779031de0e77f412edb4505c9c48
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:59:23 +0000

    live meaning during size meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
