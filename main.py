8ae8b9948d7ba5ecd9e69655e0f0b2afba038dc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:42:15 +0000

    radio frame scare guess broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
