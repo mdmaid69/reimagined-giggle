9f45852a330a272a52a38e6b42cc1cc76cdcf459
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:15:03 +0000

    copy look lot sail cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
