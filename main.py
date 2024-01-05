e7f908bb5b83acc65ca999c94d88374c9ffdaf9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:14:22 +0000

    drive case soon know familiar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+print("Hello, world!")
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
