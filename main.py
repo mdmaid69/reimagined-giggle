58cb1aa85d69380c143bc34ca1f285380db48b3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:48:51 +0000

    treat surprise dark keen length

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_list(array):
-        return list(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
