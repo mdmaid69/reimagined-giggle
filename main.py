09bcf5f5f2e5f16ee342f059559c4bc6def24a7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:36:36 +0000

    pot during pause clear direct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import random
+print(random.randint(0, 100))
