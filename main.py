02d6f23b7c9f8151b2ce8f6885beabe455ee3f53
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:12:06 +0000

    autumn wheel understand fight for

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
