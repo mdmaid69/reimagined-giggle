f73ee1284183134d11aba4256477c50e45e56f39
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:35:50 +0000

    gather birthday slowly neither him

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
