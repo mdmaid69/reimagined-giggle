c203c54f2f62d776ee9c07d929694d2dcff2e31b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:54 +0000

    cheek free run radio against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def check_if_array_contains_item(array, item):
+        return item in array
