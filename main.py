dcbba7409d6dcaa234b8da49c81e0e9b61474baf
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:16:57 +0000

    hot trousers common travel fortunate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+print(sum(range(10)))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
