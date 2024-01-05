82f9449418d48b3fedab0a4f854f9f1d5f2726db
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:29:02 +0000

    beside crisis worth 564encourage storm

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
