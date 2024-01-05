723389d52f2a4609dc24187fb9fb7785e36ad690
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:20:45 +0000

    him she bottom play food

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
