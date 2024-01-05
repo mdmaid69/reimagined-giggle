1b8c1458333cfe2070baa8e2e8bbbacb8d756684
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:35:29 +0000

    leave brief disapprove pretty hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
