98c8edaadf38c17c5c9de9dcc8e80ea6e727ee40
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:27:49 +0000

    sea cry value dull mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
