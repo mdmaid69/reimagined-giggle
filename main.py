da59e933f17a3c3b66daefaf314bab66ec412769
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:00:30 +0000

    until damage reserve willing government

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_as_set(array):
-        return set(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
