5ea38f8f7f4c61a9652b46f8d71b3d711cb58da5
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:52:16 +0000

    proper cost raise failure shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
