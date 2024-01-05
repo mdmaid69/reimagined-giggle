d8f1f6d5a886f5a3cf750d0907506d96c73b6d9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:18:24 +0000

    quality react mouth rescue not

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
