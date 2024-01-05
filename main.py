4952990576a1bca273b1b8b3beed544843459e58
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:34:02 +0000

    innocent freeze just influence excited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def convert_to_hex(n):
-        return hex(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
