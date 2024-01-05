00ff06e991361b39b5099ce9344cbdca32b82f62
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:26:00 +0000

    suck limit lookaround remain lookaround

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def convert_to_hex(n):
-        return hex(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
