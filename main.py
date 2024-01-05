c35346577b9fcee0147450c0708645d3f38a55f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:41:58 +0000

    dinner comment quickly Kk hate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
