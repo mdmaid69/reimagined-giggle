d5fe0c907ba654b24220edc63798608c17c5254d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:20:38 +0000

    ahead never relief slide weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_max(numbers):
+        return max(numbers)
