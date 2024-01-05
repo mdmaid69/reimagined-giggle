723a08727b98517f759892fd40f9b458dac21a2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:34:51 +0000

    related together name chase achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+for i in range(5):
+        print(i)
