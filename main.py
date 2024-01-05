5a3679aaac3ab314177dd6f899701f45810ac590
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:21:08 +0000

    hit mad run regret attractive

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
