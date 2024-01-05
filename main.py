540f6fb42fde77150744e84174382175ad0fd48b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:48:06 +0000

    hurt again centimetre sink precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_time_since_epoch():
+        return time.time()
