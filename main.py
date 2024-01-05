45daea4a5b7f74064acf773350561fa4710cb760
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:11:17 +0000

    probably lookback(on) second table patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
