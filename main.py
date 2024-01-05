96d7cbb2d8751a185412f3041b77ea8ce7ce1f4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:21:42 +0000

    object flavour escape convince even

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
