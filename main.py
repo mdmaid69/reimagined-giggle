1cadc2d3edadf170f22d52b5e8cd81b32503f452
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:09 +0000

    practical breakup defeat Tt fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
