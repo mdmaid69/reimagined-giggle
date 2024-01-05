2907273c7999267969386d2ec9fc1f418275ae72
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:09:32 +0000

    school maybe smart demand idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
