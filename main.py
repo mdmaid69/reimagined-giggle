dc5276abfca08d7db8086c013d9c68c96d623e5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:59:51 +0000

    sound congratulations list fortunate arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
