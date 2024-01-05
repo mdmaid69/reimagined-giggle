84e810bea097385b02ae112ad4768d897e448650
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:28:17 +0000

    definite interfere rent exercise only

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
