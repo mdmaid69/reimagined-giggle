f33ea5561d396f7bc2b6b9b4a71ddac04ccbd847
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:07:48 +0000

    spite judge clue everybody attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
