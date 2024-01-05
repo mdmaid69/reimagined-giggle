d0ffa13e19647c85733072aa3862aaae253d0022
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:15:07 +0000

    black lose day attract fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_density(mass, volume):
+        return mass / volume
