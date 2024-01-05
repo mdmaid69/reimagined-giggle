6c7e1b1241ec691b67a013396a4134e6b56d6084
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:46:11 +0000

    duty split half whisper best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
