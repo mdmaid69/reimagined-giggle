7359dda77a2fbe4eca7a11d18d65fc120fb991c7
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:39:18 +0000

    dress tear similar might debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
