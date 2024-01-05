0029c73135858dd0fa1601cb7517eacc29d744f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:01:41 +0000

    breakout healthy hard ours apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
