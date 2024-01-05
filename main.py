4675ad563a9ae17670a9ddb79e9c344a6e7487fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:44:14 +0000

    sense getback hold stop contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
