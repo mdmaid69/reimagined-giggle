d2118dcf2e275d280b9aa0c051526ffb8f6d31d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:51:44 +0000

    comefrom shape father mess accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
