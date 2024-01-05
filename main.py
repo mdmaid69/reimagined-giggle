e81cedc5806ff767c5fefa605a2d4e8b944b825d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:47:34 +0000

    style translate workon picture have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(lst):
+        return max(lst)
