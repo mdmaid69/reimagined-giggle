8d1cc666deff31c9a74feedd645b6448d001ee93
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:29 +0000

    uncomfortable bad heart snow finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
