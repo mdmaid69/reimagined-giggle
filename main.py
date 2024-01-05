cf1b2ee685ae6102c5019082a3bfc9c86fcfed42
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:50:17 +0000

    schedule pipe note suffer recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
