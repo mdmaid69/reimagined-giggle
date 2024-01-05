df031965f0310542dc22e19b61c84903915ac70b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:15:29 +0000

    cap anybody defeat idea fond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+def remove_duplicates(lst):
+        return list(set(lst))
