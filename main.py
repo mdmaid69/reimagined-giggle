f2c18c473a800542cac4e6400baa095cc7bd202b
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:12:29 +0000

    seriously spare appeal callup avoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
