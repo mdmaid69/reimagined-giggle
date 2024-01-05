79b8453cdf4b86733e62b33ee46474e5f9d7df59
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:46 +0000

    professional introduction suit act price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
