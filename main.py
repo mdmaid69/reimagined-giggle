e9fa8030a4ff379d8bbe00f7df0b469ac2590c28
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:53:44 +0000

    come crisis display fact lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def convert_array_to_list(array):
+        return array.tolist()
