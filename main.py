9a8bf987e3856f5b1ac3fb0c3d32e98269e4b6d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:33:33 +0000

    Qq downstairs after sink undo

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_volume(length, width, height):
+        return length * width * height
