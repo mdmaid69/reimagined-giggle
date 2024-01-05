c9550abb5ad6346f0637584d438b5d3ddd2c91b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:12:11 +0000

    land else female spend 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_current_directory():
-        return os.getcwd()
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
