e32792ba7a3ea987867e9a444ddd63c891c791d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:29:23 +0000

    finger nor wrong Qq slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
