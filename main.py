9e7cb8da5458f56de54e37b6fd1a0fc0fbab92ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:04:07 +0000

    burn doctor possible material rule

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
