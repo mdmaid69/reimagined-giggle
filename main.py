f0290d992b9a9a4b592ce3bb1ea9df9f90e2c127
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:55:47 +0000

    lose extremely sleep just lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def find_min(numbers):
-        return min(numbers)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
