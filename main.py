6bbe46e08d1fe88e796f7a3bfad744075f94d6c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:41:55 +0000

    land smoke steady avoid shame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
