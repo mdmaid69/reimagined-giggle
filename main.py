ec48466ea438c901ace73ec0968407bedcec80af
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:03:19 +0000

    almost ever turninto walk shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
