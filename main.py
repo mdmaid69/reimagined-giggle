95522ba994968215a584846008e186f3dede9c6f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:03:19 +0000

    considering big subject moral together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+import math
+print(math.pi)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
