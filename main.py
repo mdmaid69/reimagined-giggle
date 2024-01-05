26de3fc16e0140d70d078caf27d773dc36a10acd
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:35:18 +0000

    leave bother tax instead organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
