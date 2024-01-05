15c4d6db0283016c65162fb55b7bbe5a9146fafa
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:02:00 +0000

    bread type Vv scared various

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
