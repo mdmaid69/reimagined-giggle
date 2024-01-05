47c6e943af7cf265c1e9b3f4b9e090d19e9ea2b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:04:25 +0000

    now shrink alive 564encourage cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
