9685bb833ee6ae0cd951727f7b4216a4de987dfe
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:14:48 +0000

    strike chase save wide medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
