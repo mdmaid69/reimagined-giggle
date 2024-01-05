4b0d0c4710759d81538829efd95de18f2178b02c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:53:02 +0000

    content own now comein cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
