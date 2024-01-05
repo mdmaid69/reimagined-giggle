d1863dedb5371b9ed1c3c26deb4f4fd28917cb33
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:09 +0000

    mind seat now regard pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
