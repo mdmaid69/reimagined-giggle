6ba3da51f22fa7cc6b23029997873aa5b30ac762
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:14:24 +0000

    centimetre little as ring gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
