b96effc129398d3a380f835ffb3d10d70ced0985
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:41:07 +0000

    569enjoy face but impossible sentence

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
