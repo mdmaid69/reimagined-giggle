0c49a94d42009e9e17f53ce7097fd54e0ee544d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:28:26 +0000

    education frequent pick back gradually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
