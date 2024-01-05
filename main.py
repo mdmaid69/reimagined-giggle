9d0286ce3c3e101dfa42b43b1c0b4c02680f6fad
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:13:31 +0000

    science quiet plain better number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_exponential(x):
+        return math.exp(x)
