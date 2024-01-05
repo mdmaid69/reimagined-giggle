4b3ed78ace5d9d7e64b1265b67248ac4fc7cf84a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:19:34 +0000

    arrival laugh let offer wake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def clear_array(array):
+        array *= 0
