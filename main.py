ccba5ff5f79b201cb3bb421f77defa743f65d96e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:33:25 +0000

    tool stop matter field callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
