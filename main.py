45918334fe8387ca8f32d0c0f97c6b0a9f88caab
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:13:35 +0000

    rent sore goodbye today gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
