9126b2ce50d98665b3960fa563627a9e08729b1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:57:28 +0000

    how respect boring brush pass

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
