7119a748f1801132d001022b096a1cc0da48a82a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:41:42 +0000

    quiet plate home before try

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
