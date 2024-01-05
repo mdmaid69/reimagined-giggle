fd811e7b005ae94e54f2e53e9e63598d900dfc34
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:43:27 +0000

    influence down exam event makeinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import os
+print(os.getcwd())
