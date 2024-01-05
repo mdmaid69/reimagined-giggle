f18a9491c29b0a0f51de71d3ed7bedb2a37a9fda
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:40:03 +0000

    money act pinch above free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
