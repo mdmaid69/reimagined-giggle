a40f8f41d97e6d33705d89cf3e7324e64a68a03d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:01:11 +0000

    down age sleep side guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
