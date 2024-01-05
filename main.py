6be0f3606b0f09a676692235933947cd4d63f602
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:10:49 +0000

    more here more listen bad

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+print(sum(range(10)))
