4f8ba280240295704527798b12c69ce2b59c28f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:39:07 +0000

    face live admit pain drink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
