67be6d923c615368af2db452c000404cd79545b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:19:48 +0000

    face upwards furniture succeed fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
