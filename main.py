caab6a85f8ce21165622f5af97c6a31bfda114f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:50:14 +0000

    cross disgusting case change fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
