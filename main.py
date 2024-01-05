afb5745b265cd080e58af7b6df048a911fd5e11e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:10:57 +0000

    aim lookback(on) fear harm dream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def greet(name):
-        print(f"Hello, {name}!")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
