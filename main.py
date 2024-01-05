d6cdac7c426537140860cbd7ff64461ce1096182
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:09:17 +0000

    strength well feeling rise number

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
