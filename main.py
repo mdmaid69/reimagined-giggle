a5e2ed76d3095ec49afdb2e2903a1394e49dd416
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:45:47 +0000

    gas system plan niggaacademy perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
