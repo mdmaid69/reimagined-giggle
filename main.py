e20edca27e6497889b832df6266df03b2a1dd2e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:47:29 +0000

    now determined record almost Uu

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
