ff7a1310fcedc75524dada3d2002217601e887db
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:16:50 +0000

    look communication tragedy study relax

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
