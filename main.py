a1bacbc9f3e1bcfa46b1bd4aac1aa40ea95f0f7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:53:36 +0000

    wash advanced turnback nearly well

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
