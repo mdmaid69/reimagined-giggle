8464ab8e50ad6359d8ef0b760ac9e77b07509c50
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:46 +0000

    human eat spray up underneath

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
