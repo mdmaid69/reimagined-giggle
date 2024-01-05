4c92b03b112294646317931081909472d14cef98
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:57:16 +0000

    across whistle feature Gg live

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+print(sum(range(10)))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
