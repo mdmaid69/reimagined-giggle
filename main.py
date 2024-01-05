a0479380b55d1e16419f073947664be7ebfe76fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:12:21 +0000

    bet board that quantity capital

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import platform
+def get_os_info():
+        return platform.uname()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
