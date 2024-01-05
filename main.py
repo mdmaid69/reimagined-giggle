82d180b683f3c4ac53f0e099ca40f8597afd422e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:16:13 +0000

    turninto further smooth promise picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
