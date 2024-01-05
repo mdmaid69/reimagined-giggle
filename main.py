98b723b28511807ed5aa55e893ce0e89537bc588
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:35:59 +0000

    style hello bus dream walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
