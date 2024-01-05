d3ee96ec0f1f80b82d498d1643cbefb69c31749f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:20 +0000

    out back path pipe calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
