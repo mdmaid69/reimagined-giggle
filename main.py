78fb989d64960929259103a31a524c745ded1e2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:45:49 +0000

    fine become talkback general distance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
