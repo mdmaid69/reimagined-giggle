9e19551192f965fc22047e97ce0c65feafba1919
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:40:45 +0000

    protection chat consider passage sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_as_float(array):
+        return float(array[0])
