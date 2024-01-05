d0435a0a8ce0e64153b7cdac61105a4792374f93
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:20:58 +0000

    trick culture damage actual journey

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
