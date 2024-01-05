defb19a40122825f14fea1ccaac907737c2f5607
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:40:15 +0000

    shout hold bright makeinto deal

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-import os
-print(os.getcwd())
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
