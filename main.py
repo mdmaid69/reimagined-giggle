260c039a144a0cb7068548d58edb41c16ce019c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:18 +0000

    lost plain turnout word between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
