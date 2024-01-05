13542bca642aed1150eadeb8145de6c93d86dc29
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:23:18 +0000

    easy design whom keepup steer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
