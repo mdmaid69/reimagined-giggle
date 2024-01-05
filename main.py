7eb2c11170a8199fcdb2510e3643342ee5704612
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:37:08 +0000

    naturally fork makeup tour chief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import time
-def get_current_time():
-        return time.ctime()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
