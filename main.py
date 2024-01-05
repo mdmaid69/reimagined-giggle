0af8d4e701fb662be7a6984102912b61a5893cc5
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:13:18 +0000

    behave open hospital niggaacademy paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+print(os.getcwd())
