01146172b406a71c0a93dc2afb1f3f47c9cc6d0d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:04:59 +0000

    politics especially choose cap side

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
