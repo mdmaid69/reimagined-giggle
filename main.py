6a8cdd92868e9c726e50dc1737e59b344348cf30
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:43:33 +0000

    firm passenger dinner event persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
