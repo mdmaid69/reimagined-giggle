794762557f99ec5436a7bd9b256069739f2beb1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:37:53 +0000

    567enemy claim everyone getaway pile

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_current_working_directory():
+        return os.getcwd()
