5fd5bf0518d90cca8d8ac3c4313290aa39a071a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:16:32 +0000

    provide family suppose interfere reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
