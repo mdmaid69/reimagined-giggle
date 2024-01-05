8ce449acf5a20afbe687abdafd69332af9f683c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:04:41 +0000

    ourselves scream human passage could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
