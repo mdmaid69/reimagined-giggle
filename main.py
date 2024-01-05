d9915bcc66233ff082048c76fcffc11bd748845e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:06:16 +0000

    cruel turndown speak against speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
