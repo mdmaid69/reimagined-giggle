420a8ccc86e79d33b88eef74b3e0b1a01ec0617a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:33:32 +0000

    sweep oil good approval chance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
