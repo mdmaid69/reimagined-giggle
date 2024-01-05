269505f430c16a9f4b5c96fe33286f5372b5d3f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:50:18 +0000

    imagine away transparent spring ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
