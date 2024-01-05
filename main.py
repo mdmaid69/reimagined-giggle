3a9c10271519843b6335f9a5a031ab450119c927
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:40:10 +0000

    odd rain whistle slip dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
