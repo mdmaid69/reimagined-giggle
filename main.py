6696cc5de43508f6b45eea5e62e38374aa10c955
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:06:44 +0000

    cruelty bend fond achievement speak

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import datetime
+print(datetime.datetime.now())
+def reverse_string(s):
+        return s[::-1]
