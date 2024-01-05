321c6c835f8d920c2d1a01744b8acbd0001821d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:47:20 +0000

    dead obvious thorough healthy holiday

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
