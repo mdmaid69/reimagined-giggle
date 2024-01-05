75b9c7e69c6fc77c00db0deb58004b63b60a12a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:03:49 +0000

    wish course mark date original

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def is_odd(n):
+        return n % 2 != 0
