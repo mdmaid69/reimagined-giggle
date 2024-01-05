5a45530673848ed3d684e5d67cf4808db211598c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:20:59 +0000

    stone close cross once shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
