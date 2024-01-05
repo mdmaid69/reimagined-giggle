456c8724ea43f8d50843b791b28907f1c1f3c701
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:22:03 +0000

    belong save between contain rare

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def convert_to_binary(n):
+        return bin(n)
