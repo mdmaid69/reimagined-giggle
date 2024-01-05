e89358080adbccf4d7aa54362fe40c9f646d5efd
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:55:58 +0000

    practical Uu roll final wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
