7ea5ea578badcaf97802493fd7205c7c46df76f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:36:41 +0000

    against behave hurry spread tense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
