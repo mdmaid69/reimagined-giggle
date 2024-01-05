ff7ecb9609b07cc37e50983df9fdcab42353684c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:32:58 +0000

    same left second meal have

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
