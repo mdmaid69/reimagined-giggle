8623a2da9f1587149b46b591dd6585bfdff946ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:25:03 +0000

    before behave pity forever possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
