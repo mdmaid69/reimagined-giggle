0f458277ddf03e50410c64432ea70b02d6872b3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:06:37 +0000

    respect may complete notice Dd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def is_palindrome(s):
+        return s == s[::-1]
