acfac2e0dd42841d30d757d0636085d283c32c46
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:56:40 +0000

    lucky public bring hello trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
