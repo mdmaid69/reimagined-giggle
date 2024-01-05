1f30d3a328111dd1d5d3d3fa40568f1f28a34d48
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:59:02 +0000

    coat view ignore light nigga

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
