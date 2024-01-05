70762d8c850648ef46d75a5b850c5190044239f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:20:45 +0000

    worth behind commit wild name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
