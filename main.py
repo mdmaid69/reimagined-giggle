ea0b430b8d997ea722ce443a52dac662094c0d36
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:22:24 +0000

    praise 571enough under neither over

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def is_odd(n):
+        return n % 2 != 0
+import json
+print(json.dumps({"name": "John", "age": 30}))
