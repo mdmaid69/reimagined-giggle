e8290a6d3923dd76421e6236d999464ed0379f20
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:06:08 +0000

    accommodation impression guess pollution beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import sys
+  def get_python_version():
+        return sys.version
