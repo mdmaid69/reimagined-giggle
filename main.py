55ace4934bb16718ea3642a730c8e57ff43eac4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:29:06 +0000

    close impossible past invite turninto

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
