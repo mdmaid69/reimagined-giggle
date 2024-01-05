9fb30c086d8fa1541ae4c35ae31eb5f69e955208
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:39 +0000

    switch nerve accidental bringdown cause

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
