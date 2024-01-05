4e98c58ac6b49347b966f82356754c6ef62c50d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:47 +0000

    crowded discussion loose admit whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-print(os.getcwd())
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
