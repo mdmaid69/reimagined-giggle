013ee7b2f4fac6373c11fabfd8bc4e52f659b403
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:28:46 +0000

    measure wake spread long really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
