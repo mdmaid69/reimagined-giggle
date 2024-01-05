37c2d383f1cee3c0d8c7e306b0cfda3f15660ec4
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:10:59 +0000

    rubbish fierce feel arrival compare

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
