55c48ecdb36d5da78b97bd12e5d126e71b43d737
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:22:39 +0000

    concentration demonstrate that shop shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
