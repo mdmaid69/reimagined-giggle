0e886f35ef5ae35ef2b0a249853aed67e6565034
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:56:54 +0000

    bang temper price trick poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
