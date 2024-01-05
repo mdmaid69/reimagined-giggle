8ee5482006f0823942943839a603730d22b33d22
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:41:08 +0000

    cook gift original cold hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
