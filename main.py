9479e16a5e1b98fd7e97a7139db84181bf6a74fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:16:06 +0000

    towards ever concern chase police

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
