a504382d64c5e57ed96e34213afc90cb7521770e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:42:17 +0000

    against contents accommodation convincing right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
