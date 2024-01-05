2e9bc48eea6a907e67a1a96080e94a14525346be
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:14 +0000

    tune allow shout sort generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
