b34f433e720501ad988b89a8d745dd8e4f29ff94
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:57:44 +0000

    there thirsty try excuse no

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
