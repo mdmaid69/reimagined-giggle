28f7fb4b78d51e16b9c1f6f227f5cfeea59e660c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:54:56 +0000

    laugh contrast attitude less preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
