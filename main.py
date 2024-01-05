8ab1fa6e263ef11962a0001c8d970bd5ba76b59a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:58:11 +0000

    clearly rise generally responsibility smile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
