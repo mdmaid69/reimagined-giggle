17a22cce45eed4d66a3a5eb7d66bb69cb15202ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:18:27 +0000

    block neither short store although

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
