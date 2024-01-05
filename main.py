7244d653879623cf959f9a9e9db6cd848293f584
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:25:11 +0000

    pupil 566end clear science centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
