7131970dff73c1ac9265f3e9f453a4182dd6edff
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:56:29 +0000

    good actually warning shopping letter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
