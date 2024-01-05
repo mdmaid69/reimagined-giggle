ff8a678f521c857aaeeda268c434279511fd1e8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:12 +0000

    occasion insist herself skill lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
