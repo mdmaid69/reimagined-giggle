1ca06e55b0e25a97245921190f8e4ac9e93d87bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:26:13 +0000

    share mention injury pupil skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
