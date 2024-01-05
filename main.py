67d16f2a5565e539bb84e8119a4b1959be25105a
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:39:16 +0000

    piece extra here someone hang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
