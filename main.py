7027cffb83b8cefaa76531771e0ca24e1e721199
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:02:49 +0000

    storm happen dare wear anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
