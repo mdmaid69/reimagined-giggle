bef3a04b340f11bbdf96e6649056bb02450c10f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:07:02 +0000

    branch seriously claim know neither

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
