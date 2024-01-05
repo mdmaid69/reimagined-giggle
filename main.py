fc5248673c21ed454df039a4d876573bcf39323a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:26:55 +0000

    Ss sail settle figure broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
