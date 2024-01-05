ffca977d7d2e6efdea45c4d75e733a6097e89546
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:57:16 +0000

    upset look snow plate our

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Words:", len(text.split()))
