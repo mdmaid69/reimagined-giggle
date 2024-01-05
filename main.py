b46a5a15a15d6681db237505f735b1cb18965ef1
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:03:54 +0000

    face what machine noone unlucky

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+print(math.pi)
+def calculate_pressure(force, area):
+        return force / area
