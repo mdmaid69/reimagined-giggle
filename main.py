b1d551fa75373d69b43e371ca5d34f5659d16631
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:27:38 +0000

    everything any adjective used none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
