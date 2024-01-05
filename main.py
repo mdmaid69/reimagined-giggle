c6beb0b7df8cba693307e7bb7266d61f0cb85cc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:32:11 +0000

    learn urgent interview on breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
