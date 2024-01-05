56d0475646154e1289f5d531ef6eae307f7dddee
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:51:26 +0000

    cough basic estimate sharp object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
