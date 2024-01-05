107fd11799e8a6845f21a5f0eb64a1ce8b0db3bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:02:38 +0000

    intelligent hour Bb bet room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
