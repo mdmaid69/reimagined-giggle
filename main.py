372c2b60f48c4b610be66d9011d07cb662caac7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:18 +0000

    what aim illness advice light

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
