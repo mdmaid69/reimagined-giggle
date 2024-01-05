08878418dbfcb04d1358ba2a5990eb86ca16336e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:52:49 +0000

    bath off method consider tear

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
