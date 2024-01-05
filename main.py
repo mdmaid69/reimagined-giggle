249e6cbb580ef512c4b70e95cda7484aaa53463b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:45:55 +0000

    stiff blind love nonsense right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
+def convert_to_octal(n):
         return oct(n)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
