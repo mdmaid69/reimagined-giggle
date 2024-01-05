7766ca069f6f87ef0de3246af6778d0b252bc201
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:53:03 +0000

    miss injury choose dare Dd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_index(array, item):
+        return array.index(item)
