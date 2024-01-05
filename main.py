bf73e86099caf1d399535357b0e1ecff018aaeb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:34:34 +0000

    paper hold design command drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
