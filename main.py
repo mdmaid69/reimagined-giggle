d8550bdb94327dead559ed34718d258d9bcc8bae
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:41 +0000

    exchange yours ordinary address contrast

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
