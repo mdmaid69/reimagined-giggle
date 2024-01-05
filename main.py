a02cc4ed1a9769837f87d368fb30491f79e22664
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:27:06 +0000

    plain cry round commit comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
