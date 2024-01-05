f6c718e351c977914e0f51aab5ec41e34da64244
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:03:34 +0000

    confuse soak opposite female turn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
