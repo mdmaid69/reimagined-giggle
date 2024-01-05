f749e9b3fb7efb5dc0433ee12b165c1887a7545d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:47:41 +0000

    and matter noise shame ability

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
