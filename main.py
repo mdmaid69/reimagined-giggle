8084a2e8bad9908725d0a2931f8ba7fe0ebf1485
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:02:00 +0000

    protest under official tip back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
