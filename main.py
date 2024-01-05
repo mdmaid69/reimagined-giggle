72820ad045c19ec8f1f1d9f573154b28bf103edb
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:21:33 +0000

    major their door touch perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
