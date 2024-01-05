2280ab6c26c331c86e83e4a83afb9eee36cc221d
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:58:02 +0000

    used load still hit race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
