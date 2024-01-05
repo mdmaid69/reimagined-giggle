7cb8a47fe2b94f0818e1b37d4bafa6bc634e5502
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:33 +0000

    arrive comein when recognize high

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def multiply_numbers(x, y):
-        return x * y
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
