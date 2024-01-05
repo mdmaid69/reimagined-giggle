1d1e45a3807c172e255ba7dc9d6ced30ce5550e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:13:25 +0000

    turninto few list increase light

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def square_number(x):
-        return x**2
+import os
+print(os.getcwd())
+def cube_number(x):
+        return x**3
