bda1a54f8b73593ba26fd2aa527a86125359ae41
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:14:32 +0000

    hole trap hour cheat pile

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
