d5352ca430ece624fe13105a6629c0bebcd38135
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:13:03 +0000

    worth inquire store thinkof insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
