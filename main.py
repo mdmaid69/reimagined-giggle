91318c1c6063a5f8f70665052f21ef12db743c5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:18:12 +0000

    light before physical fun sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
