c621d9d7ac55d8abcabd3efc251a03fb08431e81
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:34:16 +0000

    exactly bringdown burn exciting under

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_even(n):
+        return n % 2 == 0
