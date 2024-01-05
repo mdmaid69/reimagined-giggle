b6db89b24a5a8c13cbdc930c024023c4c8190d31
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:10:10 +0000

    spin swallow cool yesterday ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import datetime
+print(datetime.datetime.now())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
