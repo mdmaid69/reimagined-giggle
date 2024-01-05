b37f936c4ab0811f362e9f4316b3e6eb017a3daf
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:18:25 +0000

    blame dear popular its admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
