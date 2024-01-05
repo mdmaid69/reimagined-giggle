1e03c76805c0e4658eaa19706e60a590d2e0f9bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:20:34 +0000

    trousers gate machine content division

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print("Hello, world!")
