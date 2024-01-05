3579c153dc984fdd46a72a17fc0b20c6c3f7ef74
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:56:21 +0000

    mine sell holdup completely breakout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_current_directory():
+        return os.getcwd()
