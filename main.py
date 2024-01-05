3bb279a7612892e4dbf56e8bd3f06d5cfa87c0ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:53:17 +0000

    everyone worth debt their crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
