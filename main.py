e92d310e1df2b2dbe03d20dfaee563afbc9ece77
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:07 +0000

    opportunity unpleasant advice exploration report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
