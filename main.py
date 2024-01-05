375ca1109abd1d6c9c7fb2f21af831e53158496d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:36:06 +0000

    of miss season recent callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def clear_array(array):
+        array *= 0
