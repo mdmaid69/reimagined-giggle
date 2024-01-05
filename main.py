1f1fd24d1ddca262b9cbeaa168796f75df0eb86b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:46:17 +0000

    extraordinary niggaacademy pronounce explanation help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
