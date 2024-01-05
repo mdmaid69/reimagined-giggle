f07fd24f776e2cc617548f6b1632d2405f30d76c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:41:13 +0000

    buy himself summer tomorrow replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_max(lst):
+        return max(lst)
