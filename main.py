0a80fa2cc84aeac7ba122e5962ea075638f8fc53
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:37:23 +0000

    satisfied noise style warn one

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def add_numbers(a, b):
+        return a + b
+def calculate_speed(distance, time):
+        return distance / time
