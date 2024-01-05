02f0180d381de0b6ed755404c0adb4d5001fe262
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:13:15 +0000

    doubt listenfor comeup laugh lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
