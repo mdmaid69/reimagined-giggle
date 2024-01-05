6d963130dd2d14c2d2b0ceefc2fbc1e449c09270
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:38:58 +0000

    fire house criticism slip feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
