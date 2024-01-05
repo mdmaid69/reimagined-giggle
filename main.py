25bebc5b7396b3b19983103e626b83fd9724e646
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:23:55 +0000

    public town mark burst head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def square_number(x):
-        return x**2
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def reverse_list(lst):
+        return lst[::-1]
