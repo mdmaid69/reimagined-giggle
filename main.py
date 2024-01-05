69821a932d751b01a8012ee74ce612c387a8de9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:22:01 +0000

    deliberate unpleasant pretty otherwise miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
