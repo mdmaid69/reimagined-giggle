20642b0d83872b5b2ee5371f49cdec35da8307c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:09:32 +0000

    doctor who distance reply uponon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def convert_to_binary(n):
+        return bin(n)
