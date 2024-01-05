44bfea4fa7d6e50cef5b1c9a6bdd58d5099c5458
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:56:40 +0000

    public mother hair upstairs hollow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print(sum(range(10)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
