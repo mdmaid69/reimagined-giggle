fb2960e8e508c3973120baf43ecd5128ae9a7284
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:42:57 +0000

    occasional active instruction cry between

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_current_working_directory():
+        return os.getcwd()
+  import sys
+  def get_python_version():
+        return sys.version
