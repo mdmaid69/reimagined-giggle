82092044abd4a52ca58b7df3abc0bcd96aa127b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:22:37 +0000

    slip prize cap birthday middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
