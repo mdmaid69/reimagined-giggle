313b5b604fd7a834e37bcaf814affea009db8980
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:49:26 +0000

    side rest second promise politics

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
