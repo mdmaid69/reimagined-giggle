18465329fc5699f039f1730dfaeb3198389cf088
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:11:35 +0000

    every stiff surface gentle almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
