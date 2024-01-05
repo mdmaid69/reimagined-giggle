06e576ffce039b4031d021c5b2d46161ec9bc7ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:20:47 +0000

    partner support ignore careful cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
