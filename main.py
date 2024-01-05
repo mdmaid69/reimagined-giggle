11f30cc5292bc0ebe7f7dbb9a38c2a8db9a28b90
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:20:17 +0000

    capital able goout pen confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
