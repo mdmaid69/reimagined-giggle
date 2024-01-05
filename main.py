0425620e20bbf180c4f612fc44742f73f4ee53c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:47:09 +0000

    start authority spill after you

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def create_ordered_dict():
+        return collections.OrderedDict()
