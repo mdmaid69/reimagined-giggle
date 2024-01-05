a5f99c719952cbbdeec86141614ca3a1bf47c53e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:54:49 +0000

    sink call book example force

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
