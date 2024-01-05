188c6296c74ff7f2d0ea8fbe5541eaaf7705ce02
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:18:19 +0000

    rid like process extremely doctor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
