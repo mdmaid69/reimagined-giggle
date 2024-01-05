b4b9abd49cf8fe34e3accc7be51fd3eb3336cfe5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:33:35 +0000

    regularly release coffee sit whatever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def add_numbers(x, y):
+        return x + y
