87a577b22d575ce670e39fcc945fe0f347ca2cbf
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:47:11 +0000

    plan mother wave themselves straight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+print(sum(range(10)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
