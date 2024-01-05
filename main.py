5ed1192a892ea16ed17d9644ec08065825e26035
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:50:35 +0000

    lock unusual bringup will myself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
