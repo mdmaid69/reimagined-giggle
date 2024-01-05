c639f54adb50bc751e2aeeea5ba8fcbb33c6a1ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:52:30 +0000

    interview with burst shallow proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
