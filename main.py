c7f021ef133d6971c1062bb203f77bcb5c840c80
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:23:27 +0000

    hurt telephone government success about

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
+for i in range(10): print(i)
