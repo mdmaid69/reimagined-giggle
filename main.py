ea61891dd158e5e13fa203c6cb8cc832fd03ab93
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:06:41 +0000

    exciting charge value involve grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
