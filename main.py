d7e7497a00bb3cffbc0cfd45de2ecc7ac76df782
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:00:40 +0000

    bound switch unpleasant read 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
