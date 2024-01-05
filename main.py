b8114ff7bb1c156ee295e5a71b0e64b997900f48
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:16:47 +0000

    direct hardly clean holdup else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def subtract_numbers(x, y):
+        return x - y
