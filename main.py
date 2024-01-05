f7c2a249d8ceb8b16d777c376b932fab09d1f6d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:28:52 +0000

    talk cover support half Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
