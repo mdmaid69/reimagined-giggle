27428f90e595cbb57603a2bc20eed23341492340
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:14 +0000

    flat odd unfortunate most cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
