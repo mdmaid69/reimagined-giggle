6852a9f005baa6efcd492483028def001dfbae1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:41:14 +0000

    dark stop him protection care

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
