bf1ac61a9b015c30557c8ebdb64c8f0a3280f88e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:43:14 +0000

    everything blind wander shape important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def calculate_area_rectangle(l, w):
+        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
