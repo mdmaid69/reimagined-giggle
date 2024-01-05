5ccf7b8b49a8dad84f5d2a6d2d5cb8d517de62e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:57:34 +0000

    dinner but even ahead hole

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
