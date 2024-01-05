d40aeb077250b75094dd69ef1f2b794bfe757b4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:04:51 +0000

    suit least class connect willing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print(sum(range(10)))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
