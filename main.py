5cdada5751f40eb9f802a2c19a95fa052b207e53
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:10:05 +0000

    remove mass yet set hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
