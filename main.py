5eb266eec962a2bf3c0762715f85d592e6075e9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:52:56 +0000

    unkind flow objection talkback will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
