3133afdd5bcd826836c899388321102bd8494ae6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:35:35 +0000

    instrument match finish nothing convincing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+print(os.getcwd())
