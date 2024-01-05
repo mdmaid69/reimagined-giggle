1f1a11930e9bad5e18e99e98a935e79683e421e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:44:46 +0000

    task model bush much now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
