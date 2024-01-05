c499177e502b9fd2399b9768f8e32f9ce1480f82
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:31:37 +0000

    crash building smoke could model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
