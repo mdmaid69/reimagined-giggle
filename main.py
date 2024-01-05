da9d1c56338b8fbbce56e77cf7a5e7360bcf7ae7
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:35 +0000

    clever keep feeling hungry let

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
