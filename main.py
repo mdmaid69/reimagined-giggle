fbd870b4a6fe9424e3ad6d0bc29f96484fe09a19
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:28:43 +0000

    several loud statement human inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
