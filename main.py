94bbc79ce65b8556b4934775ad70c7d82a93c8c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:35:25 +0000

    callout make mixture reduce short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
