ca61da7d1ca696f892a0b8c7224b8763ed584f73
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:13:52 +0000

    tooth peace national callback worse

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
