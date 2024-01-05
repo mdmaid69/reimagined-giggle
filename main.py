a0c25f3a089201b2e2b47f67bccdee39ef74fcb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:22:23 +0000

    spot maximum flat attention we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
