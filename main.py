7f1d888c74301468e3e5a73be8976dfab5611df6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:33:48 +0000

    available less switch several seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
