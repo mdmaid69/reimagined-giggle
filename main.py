65b29b0019905b8265e299ad1cbaa10f75c65fde
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:58:10 +0000

    period dress sit wear or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
