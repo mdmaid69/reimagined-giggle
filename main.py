0d4906539d2da57b7ea37ecd493f53258e3c631e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:02:04 +0000

    Ll crowd bother pile central

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
