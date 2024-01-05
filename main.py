8d8a0b693865c3e2033f07f87c2f5bef2d95716c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:29:46 +0000

    total basically whoever grand shelter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
