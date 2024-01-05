5cb04b8e6e7c29e963792acad765bb67dda1ab13
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:22:11 +0000

    shock translate cinema break cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
