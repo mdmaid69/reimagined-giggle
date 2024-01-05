dcad53a8b72abd81ec0bdf7cc66e7a2fb788ba9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:49:19 +0000

    sure water exchange arrive guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
