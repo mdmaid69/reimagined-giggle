5c774c4abb132d37097aad6ff4de186f6d3eb60f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:21 +0000

    mine gentle cool wish dear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
