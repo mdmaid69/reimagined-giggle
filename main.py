90bfcc9a9e38b06499b59a30301c72fb392160c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:25:27 +0000

    again she keepaway right shop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
