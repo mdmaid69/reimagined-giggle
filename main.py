7f34687b0eff9c690547c7b2c96b6ca079c50912
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:49:54 +0000

    crazy straight off should worry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
