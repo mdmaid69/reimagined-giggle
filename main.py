02a6b37ee0005f5eaf86cd0768cb5a90fd5ca755
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:08:12 +0000

    lie move disturb miss remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
