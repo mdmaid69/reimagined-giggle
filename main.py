28614f01332e7b3a56511a9ff3f1e028d89f6a40
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:35:51 +0000

    slip swim distant responsible contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
