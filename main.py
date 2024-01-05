3e23dbd92113b885bf9659a153ee1a949463e3ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:37:02 +0000

    evening breakdown pile praise eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def remove_duplicates(lst):
-        return list(set(lst))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
