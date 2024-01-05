8e43ff8995c69a6b5a16178698e6a507eb999ab5
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:55:37 +0000

    definite anywayanyhow approve bar turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_arc_sine(x):
         return math.asin(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
