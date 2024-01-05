d5f39c9a2d05fbd800d4bead80e8114228091ab9
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:08:44 +0000

    feel but cheap scrape once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
