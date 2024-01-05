2895f2d6f981dce86f6f90ecf9a45651cca393e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:25:21 +0000

    kind dry close several connected

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def square_number(x):
+        return x**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
