a7f81c256749e5d316925458013c73a26a2b754c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:47:55 +0000

    basically complaint roll world set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print([x**2 for x in range(10)])
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
