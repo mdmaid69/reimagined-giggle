d113931851ee44743c01c556fb1d694b20ac2bfc
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:38 +0000

    sleep worse freedom balance cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
