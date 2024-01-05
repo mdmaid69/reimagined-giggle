71e6ecb0f288d95a702477852210d446a25a73c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:39:58 +0000

    real respect peace sticky eye

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
