bc017c3fa4bdbfc04595e8d4cd14df6a38243158
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:21:39 +0000

    will glass play rub minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
