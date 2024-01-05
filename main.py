dd969786be1f5d899c98e9c93eb7aed8446beb5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:34:25 +0000

    used keepaway important back somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
