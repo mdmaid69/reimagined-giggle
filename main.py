5fd129c493f3f34e804ae2c5129bbdd7f751c502
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:37:20 +0000

    whom pollution break climb transport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
