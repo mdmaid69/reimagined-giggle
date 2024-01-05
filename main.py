c943875ac38083b42c50edaf720055f5501e6665
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:03:59 +0000

    steady force play now noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
