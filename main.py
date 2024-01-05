022dceeb40f2672430bbabb20406567fbc680282
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:40:21 +0000

    compete mass suffer separate shop

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def count_elements(lst):
+        return len(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
