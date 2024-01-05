ee5deb70555f22d1229f0a936028a46facab086f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:46:11 +0000

    tongue crowd circle raw arrive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
