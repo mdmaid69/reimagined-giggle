1a979c632a1654a24cccbc3d369237eecfa32adb
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:04:59 +0000

    arrival brown lay in solve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import re
-print(re.match("h.*o", "hello world"))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
