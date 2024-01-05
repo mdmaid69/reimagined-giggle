a3e5f238df01d7fb500722255f128fa3aded1f6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:01:44 +0000

    wrap bread watch advantage short

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def cube_number(x):
+        return x**3
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
