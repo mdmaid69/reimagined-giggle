be65437c3239d536a43a2de62d390e54eaea1881
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:59:23 +0000

    century absolutely various manner sweet

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def divide_numbers(x, y):
+        return x / y
