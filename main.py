25d1fb399ef7a07f6306b2cd9708f707868b04ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:46:19 +0000

    deserve relax either first occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
