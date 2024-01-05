7773dc0fa65dd790fe14efe7225ab51a08b57215
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:12:36 +0000

    autumn cap once surface criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def add_numbers(a, b):
-        return a + b
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
