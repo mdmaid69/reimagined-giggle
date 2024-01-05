1e49b225ee5b669c395c0d0f27d7984d6d917a2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:34:18 +0000

    workout belief please exit boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
