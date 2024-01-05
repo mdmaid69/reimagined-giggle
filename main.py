8b0bd7fdf0f1f5dd5af1b040d22ebed845b63314
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:52:19 +0000

    project basis kick surprised accommodation

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
