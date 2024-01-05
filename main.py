c8c710838563bc8555fee7443942af2559807afb
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:57:52 +0000

    drawing before somebody everybody care

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
