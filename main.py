96e495ab4839e5046f3335c0e87be50c197ac3fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:55:24 +0000

    Pp breakup land hear urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
