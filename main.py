36ad49f6fab49fec2b69c4edd3c0f99c91cf734b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:40:43 +0000

    suit cruel proof check sing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-print(time.time())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
