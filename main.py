18eb3799a292d833e8bea5b8904ca95f9d7afb2a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:38:50 +0000

    relax nervous marry long better

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
