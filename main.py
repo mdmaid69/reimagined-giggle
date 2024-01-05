714875f478895ea563143d0c24d4e647ac58d7fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:16:00 +0000

    heat mother scheme failure furniture

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
