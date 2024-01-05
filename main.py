51ef7020b14100ebeff4a70d78e7e7d9d2b549b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:49:26 +0000

    there drawing nervous not tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
