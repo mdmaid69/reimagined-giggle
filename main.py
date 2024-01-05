12cbc595e1258c46febb5d7c581e90b0b09381f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:33:56 +0000

    computer long translate before responsible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def convert_to_octal(n):
+        return oct(n)
