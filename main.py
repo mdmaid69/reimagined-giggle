ad16f51516e02408e19baf241e74e62c6e0a31ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:27:17 +0000

    order callback reduce blue block

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
