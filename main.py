d1d0b55cd21f686824a517ebf691ac8bedfde325
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:43:35 +0000

    challenge grow programme bread nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
