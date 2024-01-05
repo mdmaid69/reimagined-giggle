1883a0436365dba0b873f74748af86365f3ccff7
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:23:59 +0000

    watch bridge here permission snow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
