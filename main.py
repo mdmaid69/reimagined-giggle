68f69555fbda0e5f8c336a44425a4961c8fb5c58
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:35:18 +0000

    hate swing accident scream branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
