8099f96a29a226e2cef0f4a7b40078f72bbd58c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:01:02 +0000

    if control bunch drink side

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def is_palindrome(s):
+        return s == s[::-1]
+import datetime
+print(datetime.datetime.now())
