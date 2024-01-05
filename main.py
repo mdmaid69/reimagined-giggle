5b9b249e7d2388ec096d42a2d357e299a9cb8f2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:33 +0000

    crash behind children ready petrol

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
