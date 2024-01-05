faba92aea3a7469c52db9be3765255a19d42e73d
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:04:21 +0000

    no distant understand further know

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
