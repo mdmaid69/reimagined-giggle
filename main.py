0a54e9b8e9d55af92200abcfb4787e9a6609ef46
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:11:23 +0000

    behaviour inside dance life recent

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
