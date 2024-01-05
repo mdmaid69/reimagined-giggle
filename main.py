af8a2e4008f3280942e7c92662bf72c14af8b649
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:18:42 +0000

    sail term borrow float later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
