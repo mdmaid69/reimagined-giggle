0773cf1b7f057f3a2276927d011b7ec131c8a5fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:19:23 +0000

    write care beat gooff push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
