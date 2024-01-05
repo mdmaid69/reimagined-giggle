9c7d060a7c35dd997f895065638c0d420038acba
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:43:30 +0000

    advise share statement season tap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import numpy as np
+print(np.array([1, 2, 3]))
