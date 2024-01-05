f664d7e2eb6afb6a85cd314ebe8208c7069ba97f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:51:41 +0000

    to high platform check invite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
