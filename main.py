7192cb1be60167cd325721ce5d9cdb456209e69c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:54:56 +0000

    born like related some charge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
