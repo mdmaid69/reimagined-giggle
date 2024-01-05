3a30b247ce7bf02648f243dfde2d004a70900e34
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:44:22 +0000

    lookback(on) produce somebody illness nature

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
