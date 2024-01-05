15de250958f4089f9f8878cd71bd5beb7fb9462c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:00:43 +0000

    treat legal callup embarrassing someone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
