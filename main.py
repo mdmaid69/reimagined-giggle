f70e16986cf9ec1a1163a1433f06e79e661508ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:33:58 +0000

    prove occasion quickly patient beat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
