f46bee41dd46113cd627c6b9d501fe102eeba2d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:42:20 +0000

    blue through fire polite shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
