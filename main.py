c00ea5f53e620857d234762cf47841fdf6c155d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:45:46 +0000

    dress slice gentle cover bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
