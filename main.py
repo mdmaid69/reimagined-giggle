684325e964154fc3f54e61f52c79bb1f6ef5d57e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:31:56 +0000

    confident quiet recognize almost seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
