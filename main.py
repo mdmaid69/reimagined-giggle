bd824256c7a355fd6507384cb77b042f8adc26b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:01:22 +0000

    extremely he far freedom which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
