d6e60785111fe7b79101c858c09aac59acc7e9fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:34:22 +0000

    wherever same comeout reliable frighten

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
