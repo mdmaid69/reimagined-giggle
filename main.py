c448b6d0cfd746e01ca2eb60b9b4935d483f15d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:39 +0000

    basically Jj face hardly experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
