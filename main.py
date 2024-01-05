41205c7b87b3839ff9ff06766ae5020359b080aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:53:49 +0000

    calmly rather emergency own band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
