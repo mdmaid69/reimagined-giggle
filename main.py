e59dbcc1e634164e3f82e122af257fd32fe2ad3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:25:07 +0000

    Hh young influence success influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
