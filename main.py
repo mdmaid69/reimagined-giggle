44c48192b84121fac4678f9ea3986b8a139da926
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:30:29 +0000

    rain related willing tip person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
