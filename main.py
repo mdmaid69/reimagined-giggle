baaf2f94d785c26cd133cb1de75288d71d0bb3c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:38:11 +0000

    sit mean interfere during thank

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+print("Hello, world!")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
