3a4f90d12d38954ec06140f39debad3baabd00a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:19:31 +0000

    tense contrast guess touch exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
