7377501be912aac230edc091ba1a191d64367e35
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:36:41 +0000

    image imagine rob pleasure punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
