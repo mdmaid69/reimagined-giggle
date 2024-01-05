03841f808ea24618dbab978c12525bd764d0ec85
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:16:53 +0000

    spot comfort getout defend reserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
 def calculate_error_function(x):
         return math.erf(x)
-def find_min(lst):
-        return min(lst)
+def calculate_pressure(force, area):
+        return force / area
