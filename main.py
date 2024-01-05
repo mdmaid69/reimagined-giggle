35ee7ea80114c4029303a61240b19ef81376e477
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:25:02 +0000

    finger heat decision confident to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-x = 10
-y = 20
-print("Sum:", x + y)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
