28ea64015ea966ae8d42766ea519ef6cd828554c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:47:31 +0000

    list butter read drink meeting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def add_numbers(x, y):
+        return x + y
