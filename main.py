efa9ce7878d87a04cd1036f51cd0c21e58665b76
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:26:44 +0000

    like pain Ee although press

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
