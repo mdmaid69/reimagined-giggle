3e3cd9f4508c6c63bd00bc4da270ba3eaa168333
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:50:53 +0000

    conduct easy tip organize pipe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
