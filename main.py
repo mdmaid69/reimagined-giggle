7ca28d7fe9b1d570257b03a8a4eb72ebd86e97ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:49:22 +0000

    so would kind sharp continue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
