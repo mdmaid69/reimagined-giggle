76b97af444f32b9006b480a8db5b48d9041d8496
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:54:07 +0000

    sorry out habit start anybody

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import sys
-  def get_python_version():
-        return sys.version
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+text = "Hello, world!"
+print("Uppercase:", text.upper())
