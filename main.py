f9e383a8c844323b8c473dbc84885e01ddd0c882
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:02:51 +0000

    strike thank news late control

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print(sum(range(10)))
