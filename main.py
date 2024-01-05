8abbdf37279e7cdb4d4515b4f69ee1de851cbd2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:36:29 +0000

    tired rest sorry total point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
