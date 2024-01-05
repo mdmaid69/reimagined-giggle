e0a9b02ade79a9084b5fab93970982bc7a8a4edb
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:20:24 +0000

    patience fast lucky value tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_string(array):
+        return array.tostring()
