30cecffc3b996bec52592c6a4682575692a41276
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:16:33 +0000

    operation game shoulder interrupt male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
