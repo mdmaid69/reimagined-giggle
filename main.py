eaecf12506243832ec2bf4a57b058a5ad23a4cfa
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:26:43 +0000

    frame not special wide suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
