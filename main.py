52271a4b6689809e373ef3f17360b3cc023aa94b
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:19:51 +0000

    complain refuse this majority ashamed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
