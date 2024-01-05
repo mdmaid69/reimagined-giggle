487d70bc1e4881115c6c4cfc1aae1b35f130629c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:05:44 +0000

    attitude serious traffic importance it

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
