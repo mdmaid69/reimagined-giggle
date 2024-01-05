9e3b6145823e1a208de174ae89f86f38cbd2b13e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:01:52 +0000

    story carry minor weak charge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
