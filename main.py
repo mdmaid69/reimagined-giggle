cae45903c20a34a8b9aa619139d113afba41038f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:39:21 +0000

    complicated hello minority below famous

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
