8fbd2488dbb63ae1a6eba0935a7be713b2af26b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:59:12 +0000

    post worth order stare 570enjoyable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
