c5e54b3007bfab3a9199b9e6af20096b462dedb7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:55:47 +0000

    limit police from makeup somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def remove_duplicates(lst):
+        return list(set(lst))
