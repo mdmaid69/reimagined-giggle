265b3360417c646f8e8a1312ba7382934ce2ae31
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:13:00 +0000

    tip count anxious plant guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
