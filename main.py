5938ef5b076da520ca75cd69b64e601a2d8090ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:04:07 +0000

    clean callup brown run impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
