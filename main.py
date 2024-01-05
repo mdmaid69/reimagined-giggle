942db32e9b287bed22d4eea1c242615f3db40865
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:59:25 +0000

    bringforward impression relief warm by

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
