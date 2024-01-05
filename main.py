eba277db78bb2b324639fc9cf73a043306fcc20c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:35:10 +0000

    sticky science wind hate awake

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
