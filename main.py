259dc5c4fbc09bd1ecd4d6003d460f43767d4e4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:45:22 +0000

    divide old step present very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
