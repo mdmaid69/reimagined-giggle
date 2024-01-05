f702497c18fc5aa689af1babb0a5c80e113b418f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:39:26 +0000

    persuade regularly comfort far professional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
