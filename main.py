1d15203ea7a43671d3abbf7b76d5db156e7874ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:09:36 +0000

    gather criticism high with flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
