24366e83366dbde0b05978db974772eb6b159631
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:33:54 +0000

    along grateful attempt ago waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
