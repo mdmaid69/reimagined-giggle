223cfd6e5cd193a1b1f315e1351be4c55d60eb7b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:28:07 +0000

    raw back back your object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
