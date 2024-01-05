bd3ae7935aadc77e03ea1646ce55dc94daf59f0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:47:14 +0000

    false escape conclusion damp slow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def cube_number(x):
+        return x**3
