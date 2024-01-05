0f875f0a0574257b6cfefcde57ae7a484e3b9e96
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:38:53 +0000

    handle spend circle even number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import getpass
-def get_username():
-        return getpass.getuser()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
