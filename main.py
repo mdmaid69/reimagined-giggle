0f5900c2cf941e279eadca838449809fd0882688
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:55:18 +0000

    bright hurt cause opponent one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import array
-def get_array_typecode(array):
-        return array.typecode
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
