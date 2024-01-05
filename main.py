74b3c3036c8dcc1d8582088e0fae5f9d4c43d74b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:35:29 +0000

    objection difficulty apparently chief bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
