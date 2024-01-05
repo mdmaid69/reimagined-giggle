68383c460820aa258936d2e88e6ff0d449565fe1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:53:30 +0000

    him fly project turnup relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import tensorflow as tf
+print(tf.__version__)
