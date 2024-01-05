02d538fb4b729a55498fd61222b82dba4fe9e87e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:44:05 +0000

    dangerous comeout coffee opponent protection

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def add_numbers(x, y):
+        return x + y
