3aedff94bd63cca42abe5df00e05f0242e263f84
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:10:56 +0000

    so unable collection his grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def get_array_itemsize(array):
+        return array.itemsize
+import random
+print(random.randint(0, 100))
