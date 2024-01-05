295df5ded60e4f274a7837995f80d37ec291c071
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:20:05 +0000

    achieve slightly begin that Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
