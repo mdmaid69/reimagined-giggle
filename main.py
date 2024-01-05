4a31c8096ee9d969d2d2df82b00c070fca4c9c51
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:55:09 +0000

    must heat behind month trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
