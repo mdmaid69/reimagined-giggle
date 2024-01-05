a5234664575f555821638078e610a2d1e9ae3a35
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:14:50 +0000

    house Nn either material Uu

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
