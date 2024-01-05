06904a5fbcec172ca7440aa8615dc4dcc6e53fe7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:08:48 +0000

    unpleasant character congratulations regard us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
