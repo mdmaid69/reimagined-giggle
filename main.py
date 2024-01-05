68372ef31db6e91e954fbe49b375e8c7f04ad7c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:16:04 +0000

    smile wrap criticize about promise

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_user_list():
+        return collections.UserList()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
