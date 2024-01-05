c308d08f3e073a06ddf3ef33c2a37bc08e103065
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:26:40 +0000

    month sometimes mystery back brief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
