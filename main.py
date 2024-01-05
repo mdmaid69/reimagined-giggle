96549ffd28a9383fc63b1da83d989249b79d0ff0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:37:58 +0000

    Rr hour tend stage most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
