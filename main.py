1c8c13edb68f2fb4c56b17730a4ea544eb7212de
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:48 +0000

    capital repeat pencil during sale

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
