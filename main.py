492707614122cbeb97c273c0b8c94e62571988cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:24:54 +0000

    roll kick takeover check avoid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
