776f00d2d91dfa2de71e6f9c831f9ce3e58744c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:33:11 +0000

    machine giveup steady stiff meet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def find_min(lst):
-        return min(lst)
+for i in range(10): print(i)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
