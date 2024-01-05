ca956a1a20f5f7c3b31e775c6f6d7487f85ad99b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:14:12 +0000

    discovery personal thought oil damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
