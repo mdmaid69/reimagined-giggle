59233ddcb6c6abf4dc4dc217e3663d81ff0af949
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:04:47 +0000

    clue advise drive stay rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(5):
-        print(i)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import collections
+def create_stack():
+        return collections.deque()
