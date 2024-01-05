30c62a58f7bafc19c1c24587f71777fdbffbf425
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:29:31 +0000

    runover frighten collect worth up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
