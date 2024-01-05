93a8b50845065989541965783b19ea61216fbfb2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:41:09 +0000

    eat breath clear charge bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
