4de82cbd9fd1163f902eb4d955f4e89e9c9f6eca
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:38:49 +0000

    sad pour goback(to) square period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
