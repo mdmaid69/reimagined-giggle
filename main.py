64686a81193679839b29872e3f701b0bc4a85c47
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:34 +0000

    raw bother confusing suit uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
