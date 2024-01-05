2ce826e209a0a212753f6a5270251597617bcb4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:56:21 +0000

    all increase float once needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
