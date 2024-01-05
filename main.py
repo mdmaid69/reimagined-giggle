c4bafa2ff88a09b05b49aad4a64092c2d0e624ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:32:52 +0000

    music soak except battery public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
