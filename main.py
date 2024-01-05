02aae27e4003730749e579e067e1898ccbf302db
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:32:58 +0000

    where compare giveout smile tend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
