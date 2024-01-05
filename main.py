c7dc2949a779e5b9786cf1f4831edcf86b7f49e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:36:36 +0000

    some often still disappointed main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
