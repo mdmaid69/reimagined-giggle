0eb12f2d2ed86ee54fd7b03762f0fd1a7ca94d11
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:10:38 +0000

    contain yesterday pleasant spin chin

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
