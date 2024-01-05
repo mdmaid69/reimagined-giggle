7b21c07a0bf57a45fd69611ddb98a3d8864bdba8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:07:55 +0000

    secret warning imagination sit perhaps

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-print(sum(range(10)))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
