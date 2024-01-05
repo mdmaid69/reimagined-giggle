d8e54124e8d7e5f1ba4cce1f90dfbb84a8312e01
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:53:10 +0000

    bring fence mean weekend frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
