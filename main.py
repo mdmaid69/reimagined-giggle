9dccb94f23b0af43eba91a969b311dcdc70ceaec
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:33:34 +0000

    contact risk mark way brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
