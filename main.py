b041dc903a4994fae5e9cc47df96b2c179829e4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:27:02 +0000

    boil colour score address section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
