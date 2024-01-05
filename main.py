1b56644d54aff5675827429d08bba77e96f5a221
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:25:30 +0000

    set fill confusing indeed lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def reverse_array(array):
+        array.reverse()
+name = "Python"
+print("Hello,", name)
