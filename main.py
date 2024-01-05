5835261af1ffc4bb0061886daf423d350b35a804
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:35:48 +0000

    money marry confused very holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
