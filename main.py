686b2665bba186ad64bf5248b1eb8adcf59311cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:42:50 +0000

    contents themselves bank comfort slip

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def count_words(sentence):
-        return len(sentence.split())
-import time
-def get_time_since_epoch():
-        return time.time()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
