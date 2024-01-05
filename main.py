62f866626e8ff8a016511ad5df8ca98908a8ee5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:00:43 +0000

    modern table smile car taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
