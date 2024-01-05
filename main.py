b5e473f2714e13d0564a8a749c4397d5b0bd8e03
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:13:40 +0000

    concern attention I challenge somehow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-for i in range(10): print(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
