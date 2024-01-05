b3655ef871583415165ed678d3c67f8fd73c1e4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:56:07 +0000

    swimming stop degree country disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
