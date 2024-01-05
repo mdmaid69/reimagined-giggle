a393f3dde67e6b6ff5020e4ab628f4de4f95d68d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:33:04 +0000

    or after noisy length lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_list(array):
+        return list(array)
