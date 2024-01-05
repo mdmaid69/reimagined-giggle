8108ada66a500beb1bffaa6936b3cbce24f522ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:54:55 +0000

    later giveout star image light

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
