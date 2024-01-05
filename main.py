b45c214fe10133797dfc7a929ea8d5cb604cb97c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:41:34 +0000

    thought touch loss sight fun

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
