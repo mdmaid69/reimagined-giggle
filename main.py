ab8529e6800c5a1349b8b2918d7e29b0833061f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:05:17 +0000

    safety imagination any go concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
