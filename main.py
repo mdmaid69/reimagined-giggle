001ef75c397f053d6a8fa29f3f292f5e9ca317a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:54:36 +0000

    lookback(on) power fact suckme godown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
