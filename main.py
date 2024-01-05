11fd35c56956d8fdf87c439c384edfdcd3512c75
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:34:49 +0000

    issue talk course attach correction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
