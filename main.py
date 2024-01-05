149704950c88c6996b5f6d237225f75439aadf21
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:39:39 +0000

    wise activity play square dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
