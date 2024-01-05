b26c1ccfc03ec7500437b70a10842e6aee19096f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:32:39 +0000

    condition mine boil shout list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
