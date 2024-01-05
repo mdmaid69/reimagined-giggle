079d77087e41d13f34b6b17474ed6619046cb4c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:47 +0000

    shoulder rid bang upset row

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
