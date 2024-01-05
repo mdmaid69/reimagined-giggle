9ee92342d88c67c6129906460f4dccaca32c8052
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:59:13 +0000

    right fold sensible reduce dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
