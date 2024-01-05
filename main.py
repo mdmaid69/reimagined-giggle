6adb62ae8cb75c04d40f7b7d45d11341f40eef8e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:23:16 +0000

    wear drawing rest profit calm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
