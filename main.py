cd219859a2fb2099b996c6958bbdf4640252a64e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:46:58 +0000

    or pity patient shut shopping

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_work(force, distance):
+        return force * distance
