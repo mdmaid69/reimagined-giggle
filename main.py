55b9a56c0b02dfe72e122ef5ca70dce9a63f27f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:58:55 +0000

    complete excuse to flow play

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
