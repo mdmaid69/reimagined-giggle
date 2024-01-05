fc409417afa75308355e9dc003a4c2716155ffe8
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:29:42 +0000

    sense legal ticket OKorokay anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_set(array):
+        return set(array)
