56f49a87711208a3397762fd16daa0f9ea6b8585
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:15:45 +0000

    best cut impatient return Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_floor(x):
+        return math.floor(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
