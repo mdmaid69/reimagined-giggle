a8a3553427c642fed59972f54bb000e2d87f150a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:26:18 +0000

    team skirt group fear support

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
