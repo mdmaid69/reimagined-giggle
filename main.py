e0af0297fc1b3d1cab6b702c9af71f989d950127
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:59:40 +0000

    bus sort piece cause together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def get_array_as_str(array):
-        return str(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
