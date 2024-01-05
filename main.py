ea96ae93676a9ea312b57ec35c7d2a23269a3adb
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:09:41 +0000

    house connect no hate discover

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def get_array_itemsize(array):
+        return array.itemsize
