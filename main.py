c3af8c95dba85c1d2e6b3519f9bb52557350938c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:22:30 +0000

    ordinary fix view plan late

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def count_characters(sentence):
-        return len(sentence)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
