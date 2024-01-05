89ca99f3251931219557d4be17e4a21631068674
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:27:46 +0000

    reason separate throw disagree appeal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_speed(distance, time):
+        return distance / time
