668d05d6cb53249fcaba906f240b8f0be918b850
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:58:50 +0000

    mind workon behaviour love holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
