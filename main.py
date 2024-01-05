31676fa4b19031d3d8802dc2ba64671de35f8cca
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:48:30 +0000

    somewhere give today story design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
