c2a510bf945c7eecc8bac5be0f633068f29d79e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:46 +0000

    getback appearance ahead press last

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
