5c96c37f9bf2b3b4e0c12629a0856db5847d8812
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:09:25 +0000

    university order explanation tidy uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
