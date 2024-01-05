0df2eb83664525c22258587660787d281c540780
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:26:28 +0000

    defeat base reserve turnoff lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_speed(distance, time):
+        return distance / time
