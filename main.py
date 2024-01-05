b058b2690e4c9ae30c5589b8709e49b688094ab1
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:01:49 +0000

    date shrink science previous gap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  def cube_number(x):
-        return x**3
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
