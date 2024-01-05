ac7652ad05457981fc34391a4ad3313f192fa758
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:41:30 +0000

    professional perform check slight probably

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
