c481486e9a8f8a92e05473a61615a3dde27119a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:06:18 +0000

    discover so service they lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
