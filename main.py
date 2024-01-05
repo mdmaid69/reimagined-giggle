030fcf78a0eb25467f0f9428eb74ffa3f70dc8e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:43:29 +0000

    under marry shame smell to

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
