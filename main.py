3267f177bf463fca53abc3bcf81ba372d543df43
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:25:43 +0000

    gentle nor information stay best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
