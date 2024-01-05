fc15a91057c17c9a7c169a2de2494d9e555c83d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:02:41 +0000

    great alive would breakthrough content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
