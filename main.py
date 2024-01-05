b5f4060f67e1c4ba7ce9f30903695ba102e2b194
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:43:20 +0000

    record spite somehow to forever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
