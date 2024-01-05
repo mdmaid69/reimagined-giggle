fb61b8d0f9985dcdf4a2fe14f6c30679d172be57
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:03:21 +0000

    become drawing floor comeoff soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
