8507e70fc956bc573b68b230dd023d206aa12675
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:04:01 +0000

    brush yet replace thinkthrough hello

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
