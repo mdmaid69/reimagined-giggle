411effd6942876b1f5afbab2057f398fd5bb68ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:09:39 +0000

    plan maybe quit autumn pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def cube_number(x):
+        return x**3
