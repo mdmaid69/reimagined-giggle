4cdeb049f039e4f966279f32876b1b228e2fbcf1
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:43:13 +0000

    else suddenly will even man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
