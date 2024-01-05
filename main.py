ebf33ecb81ef337131213b9bc2e10c56bf2f25a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:04:26 +0000

    occur already solve education soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
