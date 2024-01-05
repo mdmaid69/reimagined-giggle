2ac37a7e2d34317524a9e7ddb21b6a1735a67974
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:23:09 +0000

    equipment piece look change thinkthrough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
