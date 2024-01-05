7dbb581cda41ee653720ff63e182e819e6aaa9dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:57:59 +0000

    illness unkind memory dish call

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
