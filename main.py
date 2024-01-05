cf87bdc6be3283aed2e66c182256e2191ae69f9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:04:41 +0000

    divide rescue acceptable confident wish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_floor(x):
+        return math.floor(x)
