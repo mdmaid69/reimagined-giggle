94bb953e9107fea556cca1d558259484a9fe089d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:01:01 +0000

    choose frighten guard voice appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
