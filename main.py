d61f99de72fcc7b16fee2631121dcbbd32eb1652
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:41:20 +0000

    affect care summer sing attempt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def convert_to_octal(n):
-        return oct(n)
+  def sort_list(lst):
+        return sorted(lst)
+def multiply_numbers(x, y):
+        return x * y
