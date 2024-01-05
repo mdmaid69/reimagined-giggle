427eb1fca065b6a1ad6d5cbf385f07328fb1c64b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:40:15 +0000

    both lean trade confused anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
