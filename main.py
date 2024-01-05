e5e45b15e98690b242c86484d136d3a20a457901
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:41:34 +0000

    conclusion surprising expensive price ready

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
