4289e785c3660a1861fce73dd996443365406efb
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:44:46 +0000

    crowded miss answer minor copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
