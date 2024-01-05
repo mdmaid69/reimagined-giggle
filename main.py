a73682aefedb2283219017c5cfc0d3f3a95c2825
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:34:52 +0000

    like dinner Ii challenge callup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def is_palindrome(s):
+        return s == s[::-1]
