cacd1c979e7f4afcc1fd39a4085ff96410b5b34c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:32:56 +0000

    list couple wind dry disapprove

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
