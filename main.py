ed5f3e20c214ca5215920854e2937035ab37e99a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:04 +0000

    key forgive yourself feature argue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
