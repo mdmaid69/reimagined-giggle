60c9c04dc825f0d4b08968ca4d4a6f317b3e9987
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:59:51 +0000

    balance cum minute on number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
