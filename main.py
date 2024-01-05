d848bac0c97e3c8bc4dd3acf97f738684b81eeaa
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:28:13 +0000

    tired suddenly live miss height

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_itemsize(array):
+        return array.itemsize
