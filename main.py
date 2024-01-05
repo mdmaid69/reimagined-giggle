ef4593abd00b0baac25640a1b3accba82ff2e2cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:33:13 +0000

    fresh talkoutof train pressure extremely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
