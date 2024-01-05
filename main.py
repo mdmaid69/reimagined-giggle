be4a447825d5d2ca0d095c318b34ad59ae3c2283
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:16:11 +0000

    estimate shock threaten talkinto stay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_pressure(force, area):
-        return force / area
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
