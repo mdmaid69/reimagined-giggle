2a5fe3d1e43700c2e10eec215a230ac1013ab425
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:39:03 +0000

    complaint if several collection dead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
