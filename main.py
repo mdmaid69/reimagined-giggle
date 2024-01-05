88027cfd9f9ceee8fc4c2d445ac47a71274fd543
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:04:41 +0000

    immediate target thorough fix possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
