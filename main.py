44b4ebd790652c898ef72e31f73da6c764ded4cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:49:48 +0000

    at respect drink tear turndown

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(5):
-        print(i)
-  def cube_number(x):
-        return x**3
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
