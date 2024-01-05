9af2af481d76818fedde71a8de30b2d1e989b796
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:43:39 +0000

    national shelter want under health

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def clear_array(array):
+        array *= 0
