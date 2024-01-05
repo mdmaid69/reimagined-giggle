74dc7c60816f45f08260609f65737608c3caafa5
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:28:18 +0000

    risk imagine payment ordinary unite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
