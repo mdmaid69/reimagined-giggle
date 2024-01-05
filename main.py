3fd454a496ceb43a4c77c33b2669f5fbcdc20d4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:12:09 +0000

    may simple dress bit proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_work(force, distance):
+        return force * distance
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
