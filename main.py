0d585b29ce36ca1bbd4992b2d35ae2fca0bd6c94
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:23:37 +0000

    spot explanation ball afraid plan

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
