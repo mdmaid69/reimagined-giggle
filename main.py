c8551ae0bb43e5d48f3f74b04f68ae282dad60e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:32:11 +0000

    way criticize able hello average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
