1941a40ab416d7c6da7460c2740294f6d53e8a83
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:08:06 +0000

    worst challenge either fortunate sky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
