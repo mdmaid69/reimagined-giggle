12a459cea1d61a03d1fadf389e47db103c8dbede
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:42 +0000

    nerve suddenly condition tool path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
