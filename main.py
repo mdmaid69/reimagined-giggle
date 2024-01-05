8ebfa438c062442b32a4837e95228b31b775fdff
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:09 +0000

    judge gate discuss lock cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
