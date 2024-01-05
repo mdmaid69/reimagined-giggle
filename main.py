45bca2a5549057103d69f36c5e25502ea8e21b28
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:48:39 +0000

    her I snow sticky accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
