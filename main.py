8726f5abe58965fcc3e752494917a43c328903f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:01:39 +0000

    confused declare plan material plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
