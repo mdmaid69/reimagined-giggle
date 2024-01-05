88e9ea724f7b1234fde03b566d51c341f3aed4d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:43:12 +0000

    warn city safe mess intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_acceleration(speed, time):
+        return speed / time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
