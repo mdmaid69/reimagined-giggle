33859539b7444201e23ee7427fa423bff2848440
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:53:34 +0000

    information suggestion difference power type

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-print(math.pi)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
