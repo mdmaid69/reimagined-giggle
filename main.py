5d356b8728f6ac43e23e089bde24069c0e7ebd5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:53:35 +0000

    majority crime necessary shallow dangerous

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_pressure(force, area):
+        return force / area
