0528e15433b2f6c4800fbcd43d51d73ac7640e38
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:34:06 +0000

    fly keepup arm brief fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
