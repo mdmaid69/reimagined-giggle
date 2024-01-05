0167e0443b564238a7fa6d8e68cf5dd22a703b7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:27:21 +0000

    concentration argue kick turnback slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
