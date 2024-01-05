3cd876317d5349397e45615916e3931cd12c0030
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:41:58 +0000

    Qq apply long best entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import sys
-print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+print(re.match("h.*o", "hello world"))
