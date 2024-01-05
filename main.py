43b74c73b7a8bd149731421cde956cb154fd07fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:02:56 +0000

    history only match aware drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
