934f698abe2cc238a73b3c30e525456ef7a2c729
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:30:21 +0000

    money real to trap near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
