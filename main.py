b62d9c87ae539f2f9dd834dcd50e7098bd203fc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:18:49 +0000

    strange exact period part reach

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
