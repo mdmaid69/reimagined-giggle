9da2615bcd1d778dd5be1cd4245096f4c7e97cfe
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:37:27 +0000

    father stop variety continue swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
