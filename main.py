b75d7152041eeb5cf01ce873fe286ac8c8250f98
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:20:36 +0000

    always 570enjoyable strict splash prize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
+def find_min(numbers):
+        return min(numbers)
