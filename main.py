8959a639a41a58507fa4e9c301ae52b0a34d3543
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:44:22 +0000

    fit to shrink few major

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_min(numbers):
-        return min(numbers)
+def square_number(x):
+        return x**2
+  def count_elements(lst):
+        return len(lst)
