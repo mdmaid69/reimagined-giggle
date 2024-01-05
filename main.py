fa633cafcbd7d5d7fd827f29fe278eea3ca734c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:00:26 +0000

    occasional risk Ii fairly side

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
