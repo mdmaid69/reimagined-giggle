9e93b02ad82deecb537ead06ad326398a204fb65
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:44:06 +0000

    do company sight contact broken

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
