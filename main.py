17fe4c03880ecb5289afb08b88f3081569bf7b0a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:05:43 +0000

    out trip neither mother after

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
