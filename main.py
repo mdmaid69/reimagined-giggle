5b5a9c0f461ac1e7374d57415d2c0071cba43b93
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:21 +0000

    breakin occur opposite ourselves bringforward

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
+import os
+print(os.getcwd())
