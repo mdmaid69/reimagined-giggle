49041ab6621a4e88b2643d7d1c1d091080cdebae
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:19:25 +0000

    fix Gg reply effect goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
