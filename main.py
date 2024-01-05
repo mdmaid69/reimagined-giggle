dcf3b32ac394d7fe23a7f5e9784b765ecfca8d38
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:06:05 +0000

    statement stiff human listenfor eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_priority_queue():
+        return collections.deque()
