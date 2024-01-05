6553d5eee8d3ed1d97b2cf04353991a47ed9898d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:37:55 +0000

    measure boil guess turnup excuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
