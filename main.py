7fd88404eaed54cad6974d7cee7a789956377094
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:04:57 +0000

    settle persuade urgent suggest comfortable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
