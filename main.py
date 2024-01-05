2158b866a5069cf65ed7da94d805d605be12ff01
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:17 +0000

    whenever Ii turnback major pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
