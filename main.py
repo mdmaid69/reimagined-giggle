35c6f9fe17a1dcb73cdf79dd3859baf63c43685f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:13:32 +0000

    benefit relation apparently woman method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
