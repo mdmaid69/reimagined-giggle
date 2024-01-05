bf0c001f6a72317388b51287642d7b39ab9fdb83
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:18:39 +0000

    onto change deliberately hurry however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
