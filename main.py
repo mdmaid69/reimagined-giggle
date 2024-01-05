d149bfb13796179afc2df33f10859155d7b86d7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:30:24 +0000

    I Ff check nonsense properly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
