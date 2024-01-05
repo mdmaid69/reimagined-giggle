45f27f4ef79a2aadb0f276972b647446b27f397d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:50:04 +0000

    have call cry formal juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def subtract_numbers(x, y):
-        return x - y
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_speed(distance, time):
+        return distance / time
