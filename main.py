b3dc042eb64267ec2a556e28a45a938eea9378e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:27:17 +0000

    bother suck new precise over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import re
+print(re.match("h.*o", "hello world"))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
