a161c0871a81c3a9dbef4838a84ae08b3b833062
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:28:47 +0000

    stiff front Pp task paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
