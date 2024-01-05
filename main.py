01d86efc307cbc2d7f06ef13619f091b401e1575
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:46:49 +0000

    practise compare hurt top hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
