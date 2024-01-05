af659b865b2b4b6fba176f33acb62da3bad1584a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:27:50 +0000

    substance unless difficult close food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
