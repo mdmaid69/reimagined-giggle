42cf17b48da7e3f17be30a01c318695b418e23a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:47:27 +0000

    ashamed sea appeal instead unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
