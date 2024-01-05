fba45eade6680e00e78b9da3f5e59e3158458849
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:54:46 +0000

    control speak nothing reject sign

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def square_number(x):
+        return x**2
+def subtract_numbers(x, y):
+        return x - y
