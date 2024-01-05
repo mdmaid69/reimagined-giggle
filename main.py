93c7655b1790476a1b25c1e73bf11a0a24056174
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:11:03 +0000

    fall plenty forever date shape

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
