4160c90f0de25ab47f91233983c699df13d80bd0
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:41:47 +0000

    home effect breathe independent wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
