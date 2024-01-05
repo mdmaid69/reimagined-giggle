af64cab257387dafa64b57ab445399d4e5d6655d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:06:22 +0000

    they either reason curious hand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+name = "Python"
+print("Hello,", name)
+def count_words(sentence):
+        return len(sentence.split())
