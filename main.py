3393edc79273f5aef9b6648b3006f687992e12f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:19:18 +0000

    keepfrom train importance bang land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
