00eefc08a3ad5115ad5216aec6996f98d532d1f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:44:01 +0000

    hang advice load regular lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
