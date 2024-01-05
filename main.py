ad269fc8e061a807df8bfc1873fb9b1c8ed41e97
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:34:15 +0000

    brief finally offer forget suddenly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import datetime
+print(datetime.datetime.now())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
