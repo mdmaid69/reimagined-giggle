3a08279e8d6972244dd9e46567bc830584955a93
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:49:45 +0000

    free mean how lot where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
