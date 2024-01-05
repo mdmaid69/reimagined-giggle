5e52fc2652505feaaa02221f4c745c31ae49e77d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:34:39 +0000

    on sting declare slice start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def cube_number(x):
+        return x**3
