b5f57641b6928b1d6b089b7ca7aa75a9d5b5adcf
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:02:09 +0000

    through consist ask know late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def subtract_numbers(x, y):
+        return x - y
