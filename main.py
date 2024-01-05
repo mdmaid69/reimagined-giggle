96db125b02d6ef6dcd6a4c2eaa62be440e475ffb
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:59:40 +0000

    shout characteristic afraid breakfast treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
