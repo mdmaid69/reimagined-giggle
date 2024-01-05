a0daefe9c19340787d88da7feb0872856ff217b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:44:04 +0000

    moral nurse black hit point

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+from collections import Counter
+print(Counter("hello world"))
