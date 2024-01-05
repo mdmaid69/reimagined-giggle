6ba2acce4cd67601e0e77bcadfb0d5c72588bbb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:17:10 +0000

    off adjective pass slide which

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
