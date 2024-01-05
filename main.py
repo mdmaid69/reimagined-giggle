9c068882b2e7f8bf1e4788f0ddd8b3903fb933d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:01 +0000

    creature round message Uu fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
