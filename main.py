f0e0817842d5e5fce1d7d0f23e742dec81062c55
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:50:49 +0000

    appear blow secret task their

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
