25bc27ca1b9698d96cf19e4c00d1d03b77eccd2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:57:36 +0000

    female bang spring goodbye reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_even(n):
+        return n % 2 == 0
