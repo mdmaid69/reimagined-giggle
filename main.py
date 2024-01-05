c1c813103203713f223ab75a528b1ceea2a9b977
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:46:00 +0000

    crowd beat finish value past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_string(s):
-        return s[::-1]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def reverse_list(lst):
+        return lst[::-1]
