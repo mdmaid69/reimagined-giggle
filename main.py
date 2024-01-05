2d192bf6fcff4a5ef7fbbcdd5dae25821497acb5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:05:19 +0000

    open health may rule equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
