4ea933dff4c60d7e84f4e28759abfeb8d60135af
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:02:29 +0000

    passenger accept miss little somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
