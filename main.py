c65d33fc6272b5da8e2fd453b18d2e59bbaa6b9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:38:23 +0000

    settle record sort exam content

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
