34763275a1155131682eca8fda81f20670706063
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:46:53 +0000

    pity callout direction ago ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import time
-print(time.time())
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
