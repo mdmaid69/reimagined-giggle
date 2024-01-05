4eb3ac8130bb26335526518a24b151375a0c024e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:17:10 +0000

    strong during eye book worry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
