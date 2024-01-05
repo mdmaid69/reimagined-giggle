907513ce67072d5f25e951dab73b5da83d4fdbee
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:08:02 +0000

    foreign content sense content same

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_speed(distance, time):
+        return distance / time
