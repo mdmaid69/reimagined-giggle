eece516a698a8e0eec031936c07db0f1e923fe70
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:09:19 +0000

    buy unusual heat advantage transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
