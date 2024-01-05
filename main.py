858b180edfec34526cbced4257b913ae7432cccf
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:32:22 +0000

    inquire simple good this phone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def square_number(x):
+        return x**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
