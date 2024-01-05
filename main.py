0ea6d2c6a8ee239399f6cae1aed2d9b96e99409e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:23:07 +0000

    lookup brave actual switch sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
