3712070d372d9f50cff2499930e2d9209c052175
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:25:13 +0000

    judge tone prevent give full

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tensorflow as tf
+print(tf.__version__)
