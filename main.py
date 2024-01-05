8645c07a6ef382623f90d19a8e0dff79ef117085
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:22:45 +0000

    consist custom size purpose trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
