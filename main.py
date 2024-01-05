934edd29154b6c448e0ee51e1c943a8d19a5f5e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:44:47 +0000

    fast summer guess repair keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_power(work, time):
+        return work / time
