5b0cafdaf998804766c524ee3ffc176ad9fda932
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:59:28 +0000

    belief formal positive interruption boat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def convert_to_binary(n):
+        return bin(n)
