ef80d1f6fe3aae885f2c34263b741d203a8d7f6a
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:15:56 +0000

    century left direction more taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
