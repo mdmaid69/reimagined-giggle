49b5d63288edf7f3b66322910f99ac34604d880a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:12:19 +0000

    ever memory section right geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
