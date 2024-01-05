883cc6c64a8373949b23cb4b2b9fa0148306e5b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:38:23 +0000

    drop please greeting pile only

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def square_number(x):
-        return x**2
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
