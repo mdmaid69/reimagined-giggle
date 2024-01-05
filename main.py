6b2e496501e375ff88d88736854014cea923f80e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:33:53 +0000

    correct space contact sting towards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
