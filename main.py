41fda7ae69a86f0cf5daa4a9035a0ff327d9f960
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:35:30 +0000

    letter business crash yes rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
