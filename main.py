801fcd848a7bed141e4cf29c293c06218bb3a8ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:04:49 +0000

    cap shoulder gooff disapprove heart

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
