ee45c6b0f19e5dc0d6a17cc65bde6dba56c88713
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:00:13 +0000

    report subject could leg purpose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
