b246f99f88d0c944cd2b8193b52c855d78349340
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:49:17 +0000

    reply begin they chief understand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
