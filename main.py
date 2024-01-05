628ea1374cfe40e1bfe090252ed6629336c48290
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:13:36 +0000

    heavy young study full special

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
