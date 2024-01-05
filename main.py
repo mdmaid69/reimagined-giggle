b5984e252eecf1121b875ee9d171b6d2c5e6f51b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:22:17 +0000

    interview close watch anxious copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def multiply_numbers(x, y):
+        return x * y
