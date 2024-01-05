4eb3d85b647a861c100d1e189f867cc06a621142
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:28:07 +0000

    follow frightened direct write laugh

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
