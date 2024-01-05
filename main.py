0f2bf270e0c5d4915dd3edb5f66608da6a3ffc0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:37:37 +0000

    rare typical usual rather flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
