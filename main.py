d457580d3202405d09fdd2ba617b9986dcdae0f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:31:41 +0000

    ordinary unless comein effect arm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def divide_numbers(x, y):
-        return x / y
+def get_array_as_list(array):
+        return list(array)
