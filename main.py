0ab20dbd70091194169be847237632fadbacb345
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:22:09 +0000

    care react total impression profit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
