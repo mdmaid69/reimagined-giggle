c1e6665ec06c25ba55b0f1f4e77cfd7d90dd907e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:12:25 +0000

    strike tour swimming owe pronounce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
