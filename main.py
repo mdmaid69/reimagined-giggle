15fdf81f4bd499922bc1078c073fd8b29e05bd3a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:10:05 +0000

    sticky tool respect curious it

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_power(work, time):
+        return work / time
+def calculate_work(force, distance):
+        return force * distance
