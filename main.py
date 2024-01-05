956d558eeb6fdb23249336d473997cf71b0b1000
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:32:50 +0000

    with admire drawing visit interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
