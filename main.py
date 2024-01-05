84f1342bc9904e2be45cbb33a9a4ce557727cdb1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:25:18 +0000

    they embarrassing reply protest offer

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
