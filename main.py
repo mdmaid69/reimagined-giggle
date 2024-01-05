8386e7be62f9a5db99a7e4c6d8231dfd65b007c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:25:16 +0000

    marry into mess left thorough

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_work(force, distance):
-        return force * distance
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+def change_working_directory(path):
+        os.chdir(path)
