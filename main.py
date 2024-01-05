e50c481af3f458214ebdb539c816d9d115df1bf0
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:58:12 +0000

    score comfort slowly write bridge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-for i in range(10): print(i)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import tensorflow as tf
+print(tf.__version__)
