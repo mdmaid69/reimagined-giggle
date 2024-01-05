d97242dd3e2d55e01cc1f277e759577fb8fe29d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:15:50 +0000

    wherever equal table information hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
