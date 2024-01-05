b9efe40726d1f78960576b032fe4600ce840d9df
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:56:08 +0000

    keepfrom considering government 569enjoy month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
