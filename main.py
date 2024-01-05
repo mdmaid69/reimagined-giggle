8c578537739d1867e15b444c530a8d1d2d3518e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:06:31 +0000

    share great try ignore buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
