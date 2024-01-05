90652a9702a0bbf0237eca3828b5be598fbbcccb
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:41:56 +0000

    physical calm transparent Yy area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
