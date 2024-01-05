040eca8b46a4a52573e143ec044ba93b1cb731a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:32 +0000

    we closely surprise complain matter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
