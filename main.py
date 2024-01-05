3b28e05735a9217b396900bcb9f03bda8a77e5bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:50:25 +0000

    decide completely grand talk Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
