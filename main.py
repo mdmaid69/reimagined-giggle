73486cd74f417b695fae4b35b4ec1e9a6f1ae6aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:56:10 +0000

    advanced if shallow wish clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
