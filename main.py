44ffe8514413d68bf102d5a9341c14203001337a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:42:50 +0000

    natural various turninto tongue complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
