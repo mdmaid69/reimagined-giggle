e818e9454abd05e6c3ab78fd7a93db459ba740a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:37:36 +0000

    total minimum unable meeting slice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def convert_to_octal(n):
+        return oct(n)
