0e3f471fe72c77442393e3db40bb1c1fa223c468
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:39:51 +0000

    fairly create glass fashion goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
