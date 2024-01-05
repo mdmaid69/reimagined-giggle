6219c5772d0e1d4bb44b195cc608ef2a97b376fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:51:33 +0000

    nowhere finger wash sensible through

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def greet(name):
+        print(f"Hello, {name}!")
