5a5d3b4c1fd50af8e9443d4b9a74ea8184419362
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:29:18 +0000

    criticize crowd light impressed heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
