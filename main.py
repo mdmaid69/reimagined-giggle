29b8310b71854909661dc64d4702b12ed1ebc696
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:29:04 +0000

    soak careless shop transport cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_speed(distance, time):
+        return distance / time
