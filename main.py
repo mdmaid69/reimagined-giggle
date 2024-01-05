e371c5cf84d1769922b0e129a1082b6483143f8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:43:42 +0000

    behind knock contrast cry find

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
