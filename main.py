678a9844b7548a53bb57781c11d1841dc8246539
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:59 +0000

    yet subject flavour secret holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
