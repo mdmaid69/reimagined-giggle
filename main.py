92af518015d8f6d44d9c153d0241558d5e9b81a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:30:43 +0000

    cry impression flight prevent last

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def square_number(x):
+        return x**2
