7d4efc915f9b52e681bd079a64d499b5e72ead4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:53:00 +0000

    paper environment left begin every

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
