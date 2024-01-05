620771be0df6577ab2bd5de6afc5774c31ca9736
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:13:36 +0000

    warn system hole shout safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
