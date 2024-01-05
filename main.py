45712200caa052936fe3bcc2be3cb91a518d3a60
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:56:55 +0000

    rubbish goaway live at pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
