9c6bb324921adc272be7fb66b726e6e829499c38
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:33:41 +0000

    public preparation towards suddenly see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
