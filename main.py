aabe287408c259e42ec133e94f48c1bf5710a111
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:10:20 +0000

    pay people tool unless never

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
