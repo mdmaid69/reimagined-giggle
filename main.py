ce976e1e2c74e9885528224f01476fe7c177ce28
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:42:14 +0000

    quantity score add giveup worry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
