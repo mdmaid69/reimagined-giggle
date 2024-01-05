c3d67e49b2326a304ebb9f2ce1d98c5963ab7dae
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:33:18 +0000

    570enjoyable fear make doubt conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_buffer_info(array):
+        return array.buffer_info()
