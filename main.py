c01582eb831b06cfd6bbdd35c76307d5d897d89c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:30:26 +0000

    world discuss reply new smart

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
