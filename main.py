7d4bbf994b9ed76b0c9c894f33db966f9d11ee61
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:45:57 +0000

    getaway whoever scheme succeed underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
