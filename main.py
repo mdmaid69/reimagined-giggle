a58cd5c85e398578718b2e6a0c5da681293378cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:38:27 +0000

    variety cruelty luck size cut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
