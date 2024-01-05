b9dc8ca565027a27bf88a184f7907f0005df2e08
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:16:29 +0000

    list pipe dark interest address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def reverse_list(lst):
-        return lst[::-1]
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
