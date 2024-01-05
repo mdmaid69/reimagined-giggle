5bc24ac37b098826de1affbd84582e530372680e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:41:07 +0000

    workupto tip half final early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
