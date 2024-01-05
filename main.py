387c24e9bccd2d0a4aaedcd5873e2eaea5db2eb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:32:46 +0000

    habit yes swing aware difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import os
+def change_working_directory(path):
+        os.chdir(path)
