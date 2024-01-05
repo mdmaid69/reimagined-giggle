c96f0408d4431c9faa27d5338786ea1b20fbbb26
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:25:06 +0000

    moment mine interesting moral excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
