81b7e83dcda006a414a9efdc10adc5b5bd3ee93e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:05:34 +0000

    science drink colour ago entry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
+def is_even(n):
+        return n % 2 == 0
