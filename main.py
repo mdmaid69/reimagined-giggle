c003a1ca7be4dccb36380be811fca5636368d52a
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:57:57 +0000

    really belong building than discover

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+print("Hello, world!")
