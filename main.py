05b262616e3d236575576ff6ca71441cbef0ff5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:42:26 +0000

    collection back table tense concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
 def get_bytes_from_array(array):
         return array.tobytes()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
