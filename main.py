b412d39c6618b57df19d1afd0849d5ba887b7e96
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:37:04 +0000

    mistake cover little climb to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def convert_to_binary(n):
+        return bin(n)
