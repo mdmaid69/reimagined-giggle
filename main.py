889d4165e63d947139fc1a4bd848b348404542af
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:41:58 +0000

    suffer herself embarrassing turnon welcome

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def reverse_string(s):
-        return s[::-1]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
