140c4424413c69edf403ad20c43aa694c40c3890
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:57:02 +0000

    relationship stop giveaway tomorrow mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
