1b187c05adb0473f893a5eb647e5c5b275cb2e7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:08:38 +0000

    hurt pause front development certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
