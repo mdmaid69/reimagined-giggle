d35623273cfa7ab5ec7dd7f3ceac96850b0350cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:54:54 +0000

    cool declare cinema land nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
