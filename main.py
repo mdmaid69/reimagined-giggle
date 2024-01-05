cd66b762280d5bb233166db937934a07ae8da869
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:57:39 +0000

    pure dream grateful attention move

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_power(work, time):
-        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
