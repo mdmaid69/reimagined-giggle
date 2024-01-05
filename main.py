31855554aca2135c1e1dc3d93a1ca9c344a8b4a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:14:19 +0000

    wake issue who attractive downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
