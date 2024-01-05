9b8c93884c8cc30e6eeea50f324a69f3ebb1529a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:36:36 +0000

    ordinary pick later care increase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_max(numbers):
+        return max(numbers)
