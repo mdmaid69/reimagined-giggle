ff5374e2ab73fe90e8aad3227dd37dae7c1eb6a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:39:48 +0000

    lookaround near truth line conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
