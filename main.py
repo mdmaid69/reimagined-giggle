9e9074a290b7e5a72091f44f947d828ab539d71b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:20:47 +0000

    hang last waste gas news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
