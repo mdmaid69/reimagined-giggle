7c9e07b0d6f5d6171857aab705d6a30dae86fb51
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:47:29 +0000

    none convince neck either pleasant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
