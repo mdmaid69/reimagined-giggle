fb05ffce6bbdb6b2f2810518064f0b4d082596af
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:08:55 +0000

    equal visit nice autumn taste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
