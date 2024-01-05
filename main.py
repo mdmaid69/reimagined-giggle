e6b331b32b60ff1e761860841e542a220763fb1a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:22 +0000

    if exist male division autumn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
