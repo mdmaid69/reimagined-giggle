6a84c6c2c312030163ba8e7ab3fc452d806de104
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:42:59 +0000

    terrible breakdown poor moment divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
