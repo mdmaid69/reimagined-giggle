cfc4bf3f2a2adfe2c7a7daa401ad1105c6f2d00b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:21:54 +0000

    forget short sky wire support

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
