d0f242b81f516c65497a6cf32b4f295b1d5e0329
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:49:00 +0000

    train plain instrument heavy complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
