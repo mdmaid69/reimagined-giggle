ab9f91271c80cc4968fc47ca2b13172f5fe774e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:52:45 +0000

    butter rich so method break

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def sort_numbers(numbers):
+        return sorted(numbers)
