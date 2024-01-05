bbe7d6241eb2009fac03b775e5dc33eef8394097
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:36:48 +0000

    accommodation around undo owe weak

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
