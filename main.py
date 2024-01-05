a1745e81c702f81119983bd0e1deee932f4f2df4
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:55:10 +0000

    violence character programme keepout hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def count_elements(lst):
+        return len(lst)
