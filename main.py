3c33c2e2ae63fc6fa07d053566056790f5190c6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:00:41 +0000

    approve ring fire children rid

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_directory_name(path):
+        return os.path.dirname(path)
