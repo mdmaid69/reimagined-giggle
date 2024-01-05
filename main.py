4fa36c9074cdec07bd9c18d9525b6a1716a8354a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:48:47 +0000

    brave impressive circumstance summer Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-print("Hello, world!")
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
