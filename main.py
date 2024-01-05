5f6efbb22e6e4cffe44165ff8f170b576c4cb189
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:49:00 +0000

    however extraordinary broad main spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
