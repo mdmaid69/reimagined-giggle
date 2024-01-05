cc0eba268f44df272aae7744993e4fcc1cebf6fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:36:02 +0000

    around quarter shade open refer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+x = 10
+y = 20
+print("Sum:", x + y)
