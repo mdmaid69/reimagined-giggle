056ce8dabc4b8e824fe3670e8505bf33e207f84a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:50 +0000

    regret rely but know careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import array
-def get_array_as_set(array):
-        return set(array)
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
