1b940d0aebc4ece4fe139d2f2bce2f60aa48fad9
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:19:38 +0000

    damage start willing cost basic

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
