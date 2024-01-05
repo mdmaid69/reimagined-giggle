2f39af60c346dae7fd5f783f72bdbce030efcb82
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:10:36 +0000

    health kind shock mind scrape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_work(force, distance):
+        return force * distance
