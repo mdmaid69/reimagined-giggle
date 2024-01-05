991da34f9c561a6d1adbba41b3a13525c5c6f976
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:18 +0000

    runinto beside hers schedule everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
