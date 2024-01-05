b66c1c18249dbccc78f0d55360166e86b32f4f49
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:13:37 +0000

    sister unfortunate confident comeround chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
