ba75919a494c8c4b021a22c95276b3a2b6dc722b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:36:25 +0000

    much circumstance use light alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
