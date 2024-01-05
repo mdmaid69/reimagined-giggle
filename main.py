125a8c981cfb46ef81eeaae53fff386bf089c61f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:05:32 +0000

    past deliberate front event proof

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
