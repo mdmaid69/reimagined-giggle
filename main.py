93aea1ad71129672457135cf7f16e7b1a302f71a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:51:52 +0000

    past earth concerned left club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
