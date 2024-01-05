43ed7c810fd02d312c961596fb56084e32dff785
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:33:47 +0000

    lookaround fierce steam artificial work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
