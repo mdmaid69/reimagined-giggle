5b106a0eb5ab084cd1d44e1ca5d185e7cee34b1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:09:08 +0000

    once lost sharp innocent whistle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_power(work, time):
+        return work / time
+def convert_to_binary(n):
+        return bin(n)
