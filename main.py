b51f6bd7fdad86ca5b61c31a2ac9807ddccfbbff
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:04:48 +0000

    difference better herself black improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+print(time.time())
