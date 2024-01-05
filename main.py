8c46dc64de4d2b07a0556925787c1831d0a966d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:47:52 +0000

    exciting flat smell name connected

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
