f151639f278bfe73eae1c13337cf4b4f2d2424dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:53:36 +0000

    hurt record any inside professional

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_typecode(array):
-        return array.typecode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def convert_to_octal(n):
+        return oct(n)
