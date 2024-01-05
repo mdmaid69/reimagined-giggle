4ffc9e870dd378e4d03dd48e611d6bebb2f095a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:48:25 +0000

    back field group skill them

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def convert_to_octal(n):
+        return oct(n)
