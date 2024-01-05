760c912a8f715741312de58eec6307859d004140
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:11 +0000

    practice fun separate accidental push

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def sort_list(lst):
+        return sorted(lst)
+import collections
+def create_user_string():
+        return collections.UserString()
