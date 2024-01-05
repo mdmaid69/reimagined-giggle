0840bf87400a6816b7c5b98493c2be67321ec60f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:04:41 +0000

    obvious slight tread correct passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
