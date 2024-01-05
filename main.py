3c6fe60fc2254129e2baf6260aa403a8549be232
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:08:02 +0000

    usual conversation your reason base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
