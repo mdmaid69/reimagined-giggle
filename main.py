3cf957becd17539e13630b7e241c6f14ed6a86ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:15:52 +0000

    himself hungry meet concern never

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import random
-print(random.randint(0, 100))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
