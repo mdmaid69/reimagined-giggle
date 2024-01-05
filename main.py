8bd99806a53789c50d3aaa309506601b542a8c68
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:28:38 +0000

    apparently anxious produce transport prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
