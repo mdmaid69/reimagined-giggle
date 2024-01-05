f6f95c33b319377203a801623db3926b066d0894
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:00:51 +0000

    laugh brief traffic unfortunate but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
