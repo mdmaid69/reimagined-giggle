137750aef172447d56e83eae43eeede72e06351e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:11:54 +0000

    morning out nervous strange wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
