ff5e699445351f6f019ae5a09a6e299797003064
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:06:12 +0000

    difficulty float closely brave by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
