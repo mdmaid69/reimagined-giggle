8a3e8593ec97c63f7175464e1016ab22e500c895
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:47:44 +0000

    argue sticky glass disturb result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
