3a79c66798cc8d8151ff516876f23f8421e19a46
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:07:17 +0000

    estimate stone mine pleasant persuade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
