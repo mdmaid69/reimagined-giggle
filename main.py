4caad2daef0296cb477e88027641846c40d20155
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:39:45 +0000

    confidence project fear produce fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_itemsize(array):
+        return array.itemsize
