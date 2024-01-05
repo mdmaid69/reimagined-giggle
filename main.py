0ec4c312d198344f6240b77a72d9db903faad474
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:13:23 +0000

    pile anybody either target bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
