bd9e2e22431793021673b6b58a9a27e3b6ddfa79
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:50:13 +0000

    base wise fortunate soon breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
