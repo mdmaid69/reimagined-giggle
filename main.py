a96997f83d6ebfa84dd3ac76516a1a87a4bb2d53
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:25:25 +0000

    medium cope suit far agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
