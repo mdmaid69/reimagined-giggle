8d101bc1a74a30145bb7372f9b332820373c45b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:09:15 +0000

    roof far minimum pot borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
