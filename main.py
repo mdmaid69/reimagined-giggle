f62f49cba9f18580ba3fc9d677b650dd9d501fbd
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:39:59 +0000

    petrol edge copy degree effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_elements(lst):
+        return len(lst)
