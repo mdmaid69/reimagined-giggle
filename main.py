cb15731c70b3b82cb2d56a59c94e1b17d1101e50
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:11:29 +0000

    stay father service earn whatever

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
