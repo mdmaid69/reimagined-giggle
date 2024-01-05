db559051d93ff6f8fb251711f93938f022f38fbd
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:29:29 +0000

    instrument entry everything ready rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_item_count(array, item):
+        return array.count(item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
