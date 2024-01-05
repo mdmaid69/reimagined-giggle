4b445eb322aaf5f8d26dee4bb1eded915ab4caa8
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:56:15 +0000

    company disturb fear born consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
