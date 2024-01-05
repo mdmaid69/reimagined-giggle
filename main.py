40145840ebc17f2c356858b7e6f339ac7389a076
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:04:06 +0000

    splash consider passage thing type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def count_elements(lst):
+        return len(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
