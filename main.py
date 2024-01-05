eb979ad1c28a720a27323de49e146053534434c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:21:18 +0000

    left culture inform team just

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
