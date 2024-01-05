3975ca76cd0d6cf4b16a740424a8da618feafa47
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:54:41 +0000

    conduct stop plastic block takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
