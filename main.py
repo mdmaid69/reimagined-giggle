86bb5ddb60615408be0efbeadc2df6fd2ebcf69e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:24:01 +0000

    section turn near doubt difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
