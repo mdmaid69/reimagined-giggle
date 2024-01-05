9b8793f6c386d4cc4162fb5d100536637635cdc4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:19:14 +0000

    direction estimate lookafter fetch base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
