594f28590e39c2ebca3fe47106be444c508d5336
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:23:30 +0000

    convenient its life reaction subject

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_density(mass, volume):
-        return mass / volume
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
