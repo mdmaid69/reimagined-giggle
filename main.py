400758feae36fb99a786b223ffd14888bb34d67b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:15:47 +0000

    talk interesting gather try play

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def reverse_array(array):
-        array.reverse()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
