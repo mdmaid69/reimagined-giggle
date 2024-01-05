dd574b69426ae355e4485854c9cce595ca38961b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:23:20 +0000

    open geton region rid attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
