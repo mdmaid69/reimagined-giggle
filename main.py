2012518858a2235f896723758658e812877e8008
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:50:01 +0000

    brush guilty before risk unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
