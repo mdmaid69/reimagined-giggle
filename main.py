c9115dbbb8548dc17ff0b002c9ba2af8f0417c6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:18:38 +0000

    less winter earth smart decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def clear_array(array):
+        array *= 0
