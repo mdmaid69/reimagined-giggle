468fecaadb7f779ad3dee565896ebcdafe76e8ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:52:28 +0000

    trial surface minute force key

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
