ad9722336e2075ff68fc335e3a867701057f6f41
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:09:25 +0000

    Hh keepfrom gather succeed attitude

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
