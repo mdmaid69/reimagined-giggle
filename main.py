ef70eaaf669c040add22961ec2fe3034b4591bf4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:01:32 +0000

    well once appeal reduce song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
