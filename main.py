4cbcee6a1307904911973c73998464a45015c0fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:03:41 +0000

    round along mess remember makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
