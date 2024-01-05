4005cf947a92b025a72e6ba3de8bebe7e8ab5587
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:18:59 +0000

    dull intelligent temperature town how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sys
+print(sys.version)
