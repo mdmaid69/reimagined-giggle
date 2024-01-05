553b678a57141bdcfb3396bd25e0630dd7a78bc9
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:44:10 +0000

    fail basic minor mistake cool

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
