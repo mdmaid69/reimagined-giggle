67cbe1c1d3182501ee74a657882a7f32da5276c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:57 +0000

    behind deliver wipe together move

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
