6280a3914165d9be8fb3fa47fae8048d140c7dbd
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:38:40 +0000

    to sorry future since butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
