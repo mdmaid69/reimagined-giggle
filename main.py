8f5779bd159caff2200f7e2cfd36e3c19a8917cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:52:29 +0000

    morning exist in reply fairly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import os
+print(os.getcwd())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
