583fff8f4539be116373af9cc99b0780df5556ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:28:15 +0000

    union note switch fork to

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
