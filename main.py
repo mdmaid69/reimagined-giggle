21cc724743e17ebb416295dbff2441cd3c05fad1
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:26:02 +0000

    rise anywhere refuse dinner snow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Characters:", len(text))
