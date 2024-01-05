825a911ad1f26795cfaa7632a7f4c2141bad41bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:09:54 +0000

    thumb flow complete flow pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
+import collections
+def create_priority_queue():
+        return collections.deque()
