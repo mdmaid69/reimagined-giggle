e72a84d93e7c4fbf582388b3431b51e9780075ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:31:55 +0000

    respect practise load push tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
