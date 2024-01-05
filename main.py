efa591508f6aed663726d83d24e6420165a71d04
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:39:39 +0000

    common comeround test before manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
