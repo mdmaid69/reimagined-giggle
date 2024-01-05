41486ce6432c36e99ae9e79c9f861a361b8f0f15
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:12:41 +0000

    motion forward than police design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
