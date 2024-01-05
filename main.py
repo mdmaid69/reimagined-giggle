85b2fdc3a88c3cc205d594e2d4962736b4cecae3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:06:47 +0000

    only bottom large peace combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
