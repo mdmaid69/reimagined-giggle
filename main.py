eeb66949e662ddca166a3c4cabf9be7b8e5b9f18
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:00:29 +0000

    beautiful comein left ready hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
