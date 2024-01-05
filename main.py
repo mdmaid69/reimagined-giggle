20af2824a28153fb64a0b37a509db73280f3efcd
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:24 +0000

    which hope ask after exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+print(math.pi)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
