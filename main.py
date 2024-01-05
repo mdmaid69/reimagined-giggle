c8241b791ebb1901e937153d5fee2ce629f712aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:33 +0000

    minimum label plain sleep Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
