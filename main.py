af50aeaad1692969252a9286877d73d0ae6dd215
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:42:30 +0000

    regret satisfactory normal false future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def convert_to_octal(n):
+        return oct(n)
