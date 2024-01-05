5a59a401d5de26e94ebbb0f7d4650142dccfc25e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:15:54 +0000

    instruction habit wide nerve marry

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_max(lst):
+        return max(lst)
