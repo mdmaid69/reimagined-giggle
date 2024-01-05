097537ae7ccb8e087f6b0664405d05d0a3e070e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:36:38 +0000

    play cook along name pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
