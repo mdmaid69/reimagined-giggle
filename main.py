c2135f729faa36c121aa710806f3e59d90c97c1a
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:56:48 +0000

    maximum extremely stare cause game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def append_to_array(array, item):
+        array.append(item)
