e60c7cfc077f40d6ed98f8758b64a84332e11513
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:21 +0000

    road doctor reach minority flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
