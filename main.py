755eaf1cbac4905b73215a781e348f559623b756
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:01:37 +0000

    spare distant share hurry burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
