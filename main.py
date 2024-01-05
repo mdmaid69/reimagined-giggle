a28b79795c6d4963ddbbd4486a2f206be22454dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:24:00 +0000

    doubt phone start effect thinkthrough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
