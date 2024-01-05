b7e121adb28e86c8b83b3ff5d7151c2fbc9983af
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:06:17 +0000

    male stamp warning basic exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
