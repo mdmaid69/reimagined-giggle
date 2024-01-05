eded0e90da40bb040398ef7849da375b16c1a81c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:48:39 +0000

    spot something impression comeup extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_length(array):
-        return len(array)
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
