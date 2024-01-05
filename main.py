d535400504bb723714ae903cab7c9359828eb9b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:57 +0000

    bang match talkoutof float murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
