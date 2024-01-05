787fe057c6b41eee508545d042cba7d9b2321cb2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:05:43 +0000

    someone noise sensible distant damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_combinations(n, k):
+        return math.comb(n, k)
