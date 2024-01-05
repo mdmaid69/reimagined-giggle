41504408cc2da83f34c23ccbf88ad546ea1cc0e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:12:37 +0000

    belong case scream wrong opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
