74ffed7b1968d1aa9ed2b5ae55a9f6415186d4b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:38:58 +0000

    beneath follow disappear many calm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
