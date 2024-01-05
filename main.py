a765804b229e1620af74f78be0c8c88aea8f9f93
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:11:56 +0000

    rise increase everyone plant to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
