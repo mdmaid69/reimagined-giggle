a54027821efe589ff2232c9c121595a03c57cc65
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:20:55 +0000

    scrap contribute price squeeze point

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
