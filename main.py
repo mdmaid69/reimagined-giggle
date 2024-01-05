bca7fa0d29862ffd5647748ab4206a32c71d5430
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:09:41 +0000

    clean flow pretty this roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
