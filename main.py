68e55124e800843c15a577d5d1a0162b1536d82f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:49:56 +0000

    permission none tragedy tap list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
