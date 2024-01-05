d2e355b2ef3eda0c148a7c98d894220764aab46b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:59 +0000

    shade share Uu summer fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import sys
+def exit_program():
+        sys.exit()
+import numpy as np
+print(np.array([1, 2, 3]))
