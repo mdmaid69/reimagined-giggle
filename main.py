c5968c6c6ceeb792a08514e45a82774bbf07910a
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:44:25 +0000

    tool study simple usually shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
