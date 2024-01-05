6264d5c8cd034cfe6369493ba1b0bd63c1ae28cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:34:09 +0000

    swing friend medium blind our

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def reverse_string(s):
-        return s[::-1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
