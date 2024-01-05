423e1dc557c0d9bab1bf494a5da99aaac57bad09
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:09 +0000

    instead professional temper tense deal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
