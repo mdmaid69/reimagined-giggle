d9866d593fd1691d83099591a5fa0fa952b3c4af
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:57:08 +0000

    finger hope pattern risk twist

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_density(mass, volume):
+        return mass / volume
