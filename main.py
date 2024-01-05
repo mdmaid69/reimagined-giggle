05c66da7a38d55576836a531a607def9e8350ab9
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:15:30 +0000

    dinner actual mend stay learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
