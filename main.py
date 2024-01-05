0225148ba553bcf69c7df738770823524eaa29a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:34:58 +0000

    exploration trust certain ignore round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
