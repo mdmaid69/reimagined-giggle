d829acd54214fcd3478a27c68024b74c35d46eeb
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:14:59 +0000

    reply care time cause shut

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
