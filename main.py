e0a67f2c7bcd72ae114fa2bac8a8b7506f2e2914
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:20:55 +0000

    accommodation repair certainly mean beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
