a12f610edceb51b3a743a597dc83851591f6a0eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:44:52 +0000

    defend right turn praise bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_length(array):
+        return len(array)
