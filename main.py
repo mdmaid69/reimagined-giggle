e1aabf0d6634f4c318a0be4efa6ba0c54846a453
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:43:43 +0000

    sentence cheek tax trick down

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
