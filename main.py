33969fdec70a923a5dd57ea7f14a6d5adb53a8ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:51:23 +0000

    manage juice usually dream against

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import os
+print(os.getcwd())
