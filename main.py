82945ce8af60bc07498cdc4697cb7a5c8ab10991
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:01:31 +0000

    unlucky judge terrible comfort back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
