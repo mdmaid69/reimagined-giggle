d9a7eb60a2fcad3985a6f138e24de58d2cab259f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:29:43 +0000

    love holiday every contact skin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
