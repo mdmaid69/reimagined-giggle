c50811a9beec2fab5b68e258bcebbfb2e40261d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:55:49 +0000

    unlucky discover train rarely die

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import random
+print(random.randint(0, 100))
