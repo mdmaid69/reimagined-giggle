4139b974e3b2ff5d25e19ede6821bf741336331b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:32:28 +0000

    hot turnaway pupil telephone hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
