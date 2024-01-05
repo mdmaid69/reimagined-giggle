a0d60ac5da197b4abc1edd85075ac3f0d395b185
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:23 +0000

    callup card exactly final properly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
