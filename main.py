87459105159abe916fe1dc7de89f6aeff2b56e2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:50:32 +0000

    free hand not lookback(on) interested

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
