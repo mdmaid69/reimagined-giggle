d74b092c91f3897f40d0bb54d9d63c11dd906775
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:58:11 +0000

    below skirt concerning loose drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def subtract_numbers(x, y):
-        return x - y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
