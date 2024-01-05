4aacb518d652d6052f55113fff538014f077ebd0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:42:33 +0000

    exhibition lookafter familiar both talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
