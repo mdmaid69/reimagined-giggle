3fd2cdb21217b0134fa81c28e3054562b142e775
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:38:23 +0000

    large plenty recovery while reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_elements(lst):
+        return len(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
