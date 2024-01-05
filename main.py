51f2ee3a9b4cd6d18bbf38502e28a067cbf75b15
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:32:05 +0000

    getoff claim disagree flood star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
+def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import random
+def roll_die():
+        return random.randint(1, 6)
