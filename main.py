b0034cb203f4ebd6e9cde0d49dced9969adca67b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:40:09 +0000

    share fold believe private common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def convert_to_hex(n):
+        return hex(n)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
