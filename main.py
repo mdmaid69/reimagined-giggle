30b2203afc950bd9dc1973fe517d29640556a40e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:12:45 +0000

    hide yourself lunch right frequent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
