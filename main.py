5b3f30bc8c6cf0db9bfa55a9589c1ddc7ab32ecd
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:51:25 +0000

    runover design study public sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_dev(file_name):
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
