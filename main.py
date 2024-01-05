7aebcfbec9a206de2e11127fb7c4aa4a7f5143ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:41:36 +0000

    boring offer movement minute load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
