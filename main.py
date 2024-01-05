98d2909d2bfc59ce998b4fbc858e1796d05ae41b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:49:23 +0000

    artificial underneath its laugh opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
