f26191cc08a9139ef349ba531182cd57a6e32327
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:00:15 +0000

    born spare tall towards right

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def convert_to_binary(n):
+        return bin(n)
+def remove_duplicates(lst):
+        return list(set(lst))
