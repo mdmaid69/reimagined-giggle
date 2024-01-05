d15927419aec40c378d6a3cf54679719df978923
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:03:47 +0000

    demand sudden become mistake whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
