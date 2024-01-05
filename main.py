27de12d0fdc92da18a4282bce5e7697245468fa8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:24:12 +0000

    report case disgusting lock aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
