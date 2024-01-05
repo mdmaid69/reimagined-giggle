1a12db582a9ad556abdbbc784d34ed1fd7013046
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:26:11 +0000

    abroad prove nigga commit tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
