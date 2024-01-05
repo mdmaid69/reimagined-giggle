5d9521ad8c29d324cedcb6a364790894436953d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:41:21 +0000

    school follow mass anyone raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_hex(n):
-        return hex(n)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
