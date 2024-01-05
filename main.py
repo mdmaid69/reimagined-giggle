fdf87e71e94e29b726913a057ae756bf7476663d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:22:13 +0000

    splash several flat fortunately notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
