9f267e8b7c91dd6627ed439506c68fff2c03a265
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:27:46 +0000

    catch want beginning come fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
