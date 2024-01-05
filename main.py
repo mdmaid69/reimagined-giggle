86bcc6d8a6cf4c50e16d9d81398efe20a78e4e20
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:24:51 +0000

    Qq lot hour but same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+text = "Hello, world!"
+print("Characters:", len(text))
