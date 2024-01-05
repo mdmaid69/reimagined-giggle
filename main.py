02f064231ad9f9c0c6a3c77056250b5ea26126d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:50:06 +0000

    meal light accidentally run purpose

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
