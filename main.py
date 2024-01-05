9b7de513620dd5a8e69384faab73925081aebae4
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:15:39 +0000

    paper object pride way tread

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_binary(n):
+        return bin(n)
+  def convert_to_binary(n):
+        return bin(n)
