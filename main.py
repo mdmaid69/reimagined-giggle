f86e47b1735c810f95a3dff12ea62eb44c5aa8a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:13:27 +0000

    standard Cc stage flat wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
