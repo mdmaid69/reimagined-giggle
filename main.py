07e1e5abb94c4225ed6a8337344010e1cc0083a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:22:50 +0000

    favour accommodation about soon start

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
