1c7d0072c9af1654c42bd5ae9b03a585547a3cfc
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:02:24 +0000

    round Ee move scrap hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
