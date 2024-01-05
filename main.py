f7d048e9bfca5646c6c9acfc54e27d1f9bd4c5c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:12:27 +0000

    method with good later ball

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_string(s):
-        return s[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
