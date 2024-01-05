95360dba215c6edd63928a19652a374613085543
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:02:51 +0000

    area kind runover marry next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
