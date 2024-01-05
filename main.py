50677cfa6a2db0c0948f2f0d382b5922db7dd735
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:58:58 +0000

    cause within keepoff direction now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
