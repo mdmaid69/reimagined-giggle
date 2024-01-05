119e629ba8c7cd9739e04dc0de67dca1ec98ed2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:33:09 +0000

    exciting knock below sight body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
