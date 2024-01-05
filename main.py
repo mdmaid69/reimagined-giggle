5a3a80715f589ec50e034270805ed9aee430db56
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:36:09 +0000

    top way emphasis strength boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
