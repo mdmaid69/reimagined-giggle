7276beb7060d90cd392f26f1a5a9620dca259c54
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:27:55 +0000

    wall colour fall they relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
