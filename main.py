1b62c6ec6e4ecbd9e9e88d6db203e55012dc38f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:47:52 +0000

    tell convinced mistake badly bag

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
