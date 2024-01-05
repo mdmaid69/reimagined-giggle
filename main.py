c2b0301a24c28b019b46d04bc2195f3d18428361
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:33:41 +0000

    same care question pinch habit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
