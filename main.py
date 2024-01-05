9f98ee22d770033984fcfc80e1703311946f3df2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:53:39 +0000

    movement beautiful relief receive fly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_as_str(array):
-        return str(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
