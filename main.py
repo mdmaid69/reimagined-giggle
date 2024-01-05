b1243b91fd4ba4c96192b57a80b3a0d3d8e56ee9
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:55:15 +0000

    equal cough night gas convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
