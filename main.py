4aab76ffedef671be43d18fe99dd7d96361d4932
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:16:16 +0000

    prefer absolutely funny front 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
