b1c9b0fc91d1cfb87b5fd2d9882f178dbadd7db7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:32:04 +0000

    emphasize blow like post base

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
