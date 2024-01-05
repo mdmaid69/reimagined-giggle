95d2a95c5954876c8bac6662b7d14681b4d77d93
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:33:49 +0000

    person tongue organization light ilovedicks

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
