a8a732ba8059ca4cce0e28f5763414bf918a9cec
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:29:14 +0000

    music type where how protest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
