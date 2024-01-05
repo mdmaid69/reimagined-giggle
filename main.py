3db8e71dc1b92f8d16bd16e61c14e63ad174dec0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:06:24 +0000

    mistake something surprising miss polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
