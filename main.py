c08e22b427e869911b32b2025b4f67076c05e829
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:55:12 +0000

    stiff lookaround excited Ff special

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def convert_to_binary(n):
+        return bin(n)
+  def convert_to_binary(n):
+        return bin(n)
