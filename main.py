22cfc63af61fbf0b33e5c951945f8aa4856bf4a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:19:05 +0000

    promise sleep sign care pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
