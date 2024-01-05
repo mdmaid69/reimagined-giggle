383bad631937171c2feab22d8eb6d00c8e2d3beb
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:14:09 +0000

    letter takeback similar confuse calm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
