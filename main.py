cc70598b60bbe2b20f7d6fc3b7a2cafdf17f9d6c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:24:27 +0000

    copy shine programme succeed yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
