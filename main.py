75b5574e016bb2b0ab9eb93b237e5e69ee1fc4b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:30:21 +0000

    noise though listen stretch ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
