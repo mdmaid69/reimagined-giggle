f8afa9db6b35a38e90bddf068d48c2274d6ea8fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:10:14 +0000

    act split grand arrange theirs

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
