035fdb7f14c6e63ed69e0caab891f54d4fc06233
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:19:09 +0000

    order comment touch trick point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_base_name(path):
+        return os.path.basename(path)
