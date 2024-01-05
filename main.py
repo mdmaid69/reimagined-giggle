527926c90ac3014270dbd99204b6ad39a7252bd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:44:58 +0000

    thinkof underneath science wish home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_density(mass, volume):
-        return mass / volume
+import sys
+print(sys.version)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
