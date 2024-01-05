28f034c57bc5b6c08c24b50f29243cc9d9f3e3d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:57:25 +0000

    refuse light shape cross life

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
