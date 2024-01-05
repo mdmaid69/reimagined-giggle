ae8c2876fef69b7e4069b5ddcdba1b08f1fccce1
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:47:36 +0000

    advanced still translate match swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_length(array):
+        return len(array)
