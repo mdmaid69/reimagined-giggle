8fd4a42f97b9182fecdaea167373ba8bee9a6a38
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:37:32 +0000

    trick cause go comeround pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
