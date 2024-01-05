cdefd08d63d95fe24c61de9d4586f67e4e5d0b28
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:01:50 +0000

    behave load opposite afternoon discuss

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
