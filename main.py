f8cd978484a5bbfa06263fe96616639ee9742e0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:49:15 +0000

    release university sea far trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_list(array):
-        return list(array)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
