ea051e71b940bf6c9e3e61efc6db55416db36cdc
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:18:41 +0000

    medium walk aim rough angry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
