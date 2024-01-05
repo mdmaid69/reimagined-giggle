fd0a6909f8ba0e68a048922f8db658a64fb0f178
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:51:15 +0000

    unpleasant translate but settle apparently

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sine(x):
+        return math.sin(x)
