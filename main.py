93afd087d4c1a5ac8cbabfb5d122d4a2fbfb358d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:53:10 +0000

    mark future fat patience lookaround

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
