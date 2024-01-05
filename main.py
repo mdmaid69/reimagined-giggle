a031fc14b1baff839bc805cb84e6f16617e5d7f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:33:07 +0000

    whom nothing more guard belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
