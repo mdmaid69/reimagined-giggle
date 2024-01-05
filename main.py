d9914abd8d948b65b0ac7594925dea3566d6cb0d
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:38:56 +0000

    pile tap lookback(on) thank education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
