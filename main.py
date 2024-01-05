0a752becedfb6c4398b2a1fb8e19aa6a52853b8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:47:28 +0000

    length inform mine knowledge treat

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
