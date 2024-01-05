0314f9a9049cfb031e5d3806df11b4533796add7
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:09:38 +0000

    lose lookafter deliberate note once

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
