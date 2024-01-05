da1227ce32e250290ec7d1a58f5df7c4264b8353
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:59:14 +0000

    promise please remain suppose happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def find_max(lst):
+        return max(lst)
