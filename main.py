f10081f617a9c689a654ce19b68df90f0d45efcd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:46:43 +0000

    calmly environment fit could talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
