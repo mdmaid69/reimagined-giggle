5fdd2a4e8b8b4786d41dc6073a5fa4070d5a61af
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:37:00 +0000

    score squeeze wonder hold crazy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def convert_to_octal(n):
+        return oct(n)
