4e43ce0b853e234ffac9c43db739039883702382
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:32:56 +0000

    apparently flood squash trust gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
