1c19ea7fdb6faf1939289f43eeddeb7ee5ca3167
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:58:22 +0000

    poor behave nowhere show reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
