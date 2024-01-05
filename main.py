ba5aa57f240493f9343760b6d50da9826f6b29f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:33:01 +0000

    bad definite related manner joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
