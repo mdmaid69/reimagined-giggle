8a6d96009bb69263070913e50e46d177d5ad3262
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:27 +0000

    record affect holdup lead favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
