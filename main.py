21b2366aba47619326e9e3f93f04170edd3475c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:11:54 +0000

    forgive transparent record test themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
