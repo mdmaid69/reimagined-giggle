b3ed7eb0e7f0cfe645a6f2566c467542391afa7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:34:37 +0000

    closely moon tear nowhere politics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
