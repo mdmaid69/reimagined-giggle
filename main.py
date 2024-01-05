c61ce93ee72953f1dddf4de60a05e43bb3096048
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:53:25 +0000

    long rain unfortunate kind passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
