bc4df19947b6308e08976d04d0e0c7b320f337bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:47:57 +0000

    occasion happy score interruption gradually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
