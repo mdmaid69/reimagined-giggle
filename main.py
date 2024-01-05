a184090d58d7d0cbbe99ea2b014f1adbea724b50
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:49:32 +0000

    tax aim Kk getout pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
