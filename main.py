34facb2a788734a65c3cb6d81bc04a03e6326e1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:32:34 +0000

    money key other below correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
