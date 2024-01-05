5bd9d38384f489c7fa7bbda4fbebcc9f2f3b0d05
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:09:33 +0000

    church proper prepare rank sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
 import shutil
 def copy_file(src, dst):
         shutil.copy(src, dst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
