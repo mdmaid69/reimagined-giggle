f0619d42ca4df4d2fd1a057645f46076a6afb5e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:06:46 +0000

    although careless arrive from measure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
