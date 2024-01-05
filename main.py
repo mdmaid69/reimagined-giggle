283109cae50f396af38fe284e3f8791bf65e1d62
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:23:03 +0000

    term accommodation note concentrate whatever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
