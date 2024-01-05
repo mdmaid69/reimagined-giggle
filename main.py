b0400e17376daef75a695d534e93a42807e81669
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:12:27 +0000

    violence normal become convenient carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import sys
-def exit_program():
-        sys.exit()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
