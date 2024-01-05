99a78ce33928d853bed732c602aca955dd755742
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:32:06 +0000

    runinto show hide bringforward poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def reverse_list(lst):
+        return lst[::-1]
