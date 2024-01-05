d1f535ceceb692121243d0033e77d772e67fa71a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:18:17 +0000

    prefer hungry juice head true

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
