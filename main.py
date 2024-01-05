3963a302232ddced72e6cd26cdd93c274f17b099
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:53:58 +0000

    risk breakin balance commit loss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
