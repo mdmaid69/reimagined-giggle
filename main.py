8c83222bbeb44ad96ddfe2cbb7e5a61a191ec6dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:18:23 +0000

    respect persuade hang further ashamed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
