0c60feacf0ae5baec88edb825c4f3952b8454630
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:34:08 +0000

    neck wonder estimate care familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
