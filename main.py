422159b59b5a6ce96e2ce22552d3e8ab85cf3bd5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:24:25 +0000

    stand wear free test down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
