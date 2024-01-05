f22d3c214b76843de561ffff3b1b2b2b2d689ce5
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:29 +0000

    them town under niggaacademy approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
