dc60e61b7c88804a6dd5cef808eb8a9f11f68d8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:57:40 +0000

    plate respect back until off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
