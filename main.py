8f1472ba9623acbdfaa83c5875211dc32389c250
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:10:54 +0000

    protection news touch Gg flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
