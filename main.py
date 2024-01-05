636ad5fd63490299d9ce11ff77f610f9c1312693
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:47:37 +0000

    study aim quiet escape suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
