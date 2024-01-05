cea6ccc1d62dc293e72730ffb0920ae8ec6cea4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:16:13 +0000

    car ball pocket exercise tool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def count_characters(sentence):
-        return len(sentence)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
