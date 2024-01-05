07bfddca111c0620f89606eb36617adbc27dc0b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:37:00 +0000

    food favourite soak shock question

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
