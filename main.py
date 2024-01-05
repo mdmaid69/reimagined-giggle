ad1279d5649752d18bb61cebea55367712f1520c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:24:49 +0000

    soft please knowledge Ii floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_item(array, i):
+        return array[i]
+import time
+print(time.time())
