28892ae5ffe213e307da8a187ea2944b1d1644b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:10:28 +0000

    goback(to) takeback butter together everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_counter():
-        return collections.Counter()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_max(lst):
+        return max(lst)
