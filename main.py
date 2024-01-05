44cd7894a753242c1d344f115e78296af0bb2474
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:52:21 +0000

    whisper side getaway sleep pronunciation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
