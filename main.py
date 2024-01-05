4f672d72bdf4465878bc68d687debb1c9f5f54da
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:21:52 +0000

    turn vote anywhere alone whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
