2696a868ecba98298fa10cb5e1e79c4386c80a27
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:03:52 +0000

    suggest makeup lookback(on) shine authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def reverse_array(array):
+        array.reverse()
