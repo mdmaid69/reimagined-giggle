7e07e36eb19a27b3824d901807f4e2e8ab92dfb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:04:28 +0000

    sink could choice sell push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
