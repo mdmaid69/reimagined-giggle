5fd51814dec7993580e29da0c44943ec1a664f02
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:51:35 +0000

    tour public Tt tend pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
