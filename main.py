0d6eaecfdd5d6b7915cb592b70f233376b3274c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:47:14 +0000

    stop education word plate throughout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
