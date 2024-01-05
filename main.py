f921d899009aeb755f2487dc48f66d04942b2520
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:39 +0000

    worried foreign roll gooff watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
