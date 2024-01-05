1b8fc53ee392b2a483b12fed1bcd155be84ec21c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:07:04 +0000

    wrong put temper command contribute

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
