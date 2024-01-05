cac08f2e7a8a7d4f7f2ad4a0304d38acdc30d0fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:03:35 +0000

    test apologizeapologise excited ring surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_item(array, i):
+        return array[i]
