3b2af31bedd4b172d489eaa3c060e8663051eb69
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:02:28 +0000

    knock sleep respect urgent organize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
