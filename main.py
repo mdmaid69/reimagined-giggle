bb64d54b273f6459d78c83074779cbe53d5cbc4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:27:17 +0000

    active shopping everyone ground pronunciation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def remove_duplicates(lst):
-        return list(set(lst))
+import tensorflow as tf
+print(tf.__version__)
+def reverse_list(lst):
+        return lst[::-1]
