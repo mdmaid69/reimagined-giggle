de1d1ba50160a6a7f1cdfcb97eda223db4df9f6c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:57:27 +0000

    expect stiff offence or tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
