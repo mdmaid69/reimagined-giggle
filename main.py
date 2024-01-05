8eef410627b5c1b9a105fcb089719e24aa916783
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:32:41 +0000

    account confuse wrap difficult pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_tangent(x):
+        return math.tan(x)
