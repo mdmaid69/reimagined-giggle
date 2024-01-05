c6710186002434dd9023bf22558cf086cbf1aec5
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:16:56 +0000

    push turnon comeround makeinto born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
