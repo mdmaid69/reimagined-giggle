09309a00a8dd344a5df95403e8b13c9f9eb60973
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:24:59 +0000

    correct priority hit no unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
