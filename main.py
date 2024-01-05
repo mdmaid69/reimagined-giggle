1afcd3772ec6dcb8816d706b81ee929ac1055028
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:38:14 +0000

    pile frame cinema company damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
