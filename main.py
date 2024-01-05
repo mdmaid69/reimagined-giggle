78e580f819be25241d9d76767a255995ddd124e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:23:38 +0000

    quit upset under hand mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def is_even(n):
+        return n % 2 == 0
