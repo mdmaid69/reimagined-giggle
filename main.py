71d2cf15e5d5a097d6b73732814f7a4268a1fd8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:38:36 +0000

    leave before matter relation nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
