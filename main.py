1ae8e1cbd6f5685667cab90e8bd8e1c971e73708
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:30:05 +0000

    justice opinion wherever shut half

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
