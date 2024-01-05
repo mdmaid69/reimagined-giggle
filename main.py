5585a60640e5af53473c57ad4d11167ab01aade2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:49:01 +0000

    chief organization breathe ride may

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
