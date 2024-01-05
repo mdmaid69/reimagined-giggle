db0946dcb272660635de782281b90a2710fcc149
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:02:35 +0000

    with experience half beat below

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def square_number(x):
+        return x**2
