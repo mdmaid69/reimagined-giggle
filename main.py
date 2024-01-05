6ba1b37953ed6ff349b30569d5bc760487848de8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:18 +0000

    say shut complain just makeup

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import collections
+def create_queue():
+        return collections.deque()
