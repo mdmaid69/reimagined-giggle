23d6d3cfb425311d89ba69220163c750281de41d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:24:06 +0000

    children top nice Oo exaggerate

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
