7e6fd9979ad0d285715c3ec4e3634051a0da5f7b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:38 +0000

    breath wander turndown star dear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
