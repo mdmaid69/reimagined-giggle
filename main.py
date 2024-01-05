08d64244c05d0d2bb2cef836576a1c9df4673a8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:16:11 +0000

    bringback cool wealth wide base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
