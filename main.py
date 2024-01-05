de9a5eb9a7ea1b780eb02af015c1c54147778ae2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:13:02 +0000

    fix work smile suit side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
