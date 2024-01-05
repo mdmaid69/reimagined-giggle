419e04f6450423c465c6ef4725f4f025d2cd446a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:22:03 +0000

    shade throughout course carry container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
