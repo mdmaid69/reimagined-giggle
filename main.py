e597172a0186dcdb937a114ac3cc3446b5615d93
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:43 +0000

    fast dream last visit complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
