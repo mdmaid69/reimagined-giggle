c6ff49f512e3436f0f0067242d6fc652cf21a2a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:24:07 +0000

    connected promise disturbing disgusting Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def subtract_numbers(x, y):
+        return x - y
