95d6d4c773db4f515c067d55ee81f90bed2d3cd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:53:24 +0000

    formal reduction nervous 571enough shake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import datetime
-def get_current_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
