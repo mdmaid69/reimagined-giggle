49d03f74ed05be112f7f2447dace1334ae885968
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:36:27 +0000

    suggestion computer of up my

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
