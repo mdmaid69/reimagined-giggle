1e72a883b600d6bcc640e46bd6231d7e9745ba83
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:36:59 +0000

    stone window rent stage report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
