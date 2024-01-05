ccd7a2a5888b9cb31c8deb615177f20a9b12144e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:25:52 +0000

    respect shallow deep cook drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
