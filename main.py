fc72300541ee2c2ec3a7905b43298bfe7ba5164c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:07:24 +0000

    programme suckme ball congratulate peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
