1c491fa2010c7868e32d123fe1534cb6002fe913
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:18:54 +0000

    equipment however poison represent so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
