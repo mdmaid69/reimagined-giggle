965bc547682a09ad8bc2baf7f286ed3ee67a780e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:39:02 +0000

    reliable sore sleep along workon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def convert_to_binary(n):
+        return bin(n)
