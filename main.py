bdf55b91a3fa6272680d95c2343f3f3e8032548c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:19:39 +0000

    press cough while damp else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
