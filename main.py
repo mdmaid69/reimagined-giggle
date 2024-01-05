db299f4c5c0fdec66508e061c04ec960b6f24d49
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:51:49 +0000

    to add whom along ground

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
