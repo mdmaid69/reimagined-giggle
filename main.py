0a8f2e2715af4e72792713bc10ea0608f88e2964
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:08:36 +0000

    weak minimum produce tired one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
