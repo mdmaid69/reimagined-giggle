b30a33c0112cb22d903f7ea1487e77b2068eee4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:40:23 +0000

    dare visit shadow get schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
