a6b65c0f4e74f56c9a41e92f2dc491b1fa236b2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:15:03 +0000

    family follow loss introduce stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
