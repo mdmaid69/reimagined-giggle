3266144092f25dfc1c248338688633e0c6c8362e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:36:31 +0000

    surprising concern move satisfactory round

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
