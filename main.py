70c899aa3734ff41925d5d1671dd03df27daf180
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:42:27 +0000

    call shape region guess foreign

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
+print(sum(range(10)))
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
