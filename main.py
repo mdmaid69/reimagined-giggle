e5511bffbca8985c13b257bfacedbfed561f6425
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:25:12 +0000

    memory interested down us I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
