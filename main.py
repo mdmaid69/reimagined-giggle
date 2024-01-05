b4cbc72d88c414537349ab7f578aea27c04e06dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:33:17 +0000

    count keepfrom claim clothes worry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
