b294d098c97bffa4aa62e6caf46a9bd3aab70ccb
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:43:08 +0000

    loose fail closed laugh pause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import platform
+def get_python_version():
+        return platform.python_version()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
