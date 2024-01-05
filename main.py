8a4a80c0ef61a22c755467e42f0f1ca07681a7cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:31:08 +0000

    possible anyone opportunity pen realize

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
-print(time.time())
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_palindrome(s):
+        return s == s[::-1]
