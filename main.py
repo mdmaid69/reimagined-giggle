7ed55b5b7adb39b196ab1956741dc9fc1c57f053
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:44:40 +0000

    block grow certainly difference fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
