36a5e52adcc1f1a9dd2df465886a2b8be81907d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:31:38 +0000

    wealth whistle head straight cover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
