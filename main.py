8404f28e119bef7e6c5f6cb0051e92f5701c3f56
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:10:12 +0000

    wait hear makeup muscle nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
