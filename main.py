9aef06cb1628d25ad520e5bf3ba428d180721b5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:18:12 +0000

    correction song estimate risk message

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_binary(n):
-        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+x = 10
+y = 20
+print("Sum:", x + y)
