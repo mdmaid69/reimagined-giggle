0a5dfd1189485c52f419e8497e51234b729bf0f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:49:56 +0000

    idea world like accidentally sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
