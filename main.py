c41212b7b380cd325fd59e3366c3636b5ed0cdb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:08:12 +0000

    door worry pinch attempt into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
