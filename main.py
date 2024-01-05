b08b2a8c6dcb70c5d13a0eb7f12a9a0ecb75fe91
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:46:08 +0000

    blue have muscle dry impatient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+print(time.time())
