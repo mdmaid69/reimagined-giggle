b01ce8569d421063d2d50697c82f751fa2ac5a44
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:43:41 +0000

    tough table valuable press themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
