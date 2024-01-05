f37877f7eccd0f5bfb44d310b0b10b4099ccef89
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:32:38 +0000

    bush power test independent direct

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def reverse_string(s):
-        return s[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
