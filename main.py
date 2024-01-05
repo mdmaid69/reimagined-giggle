162bb187deb6dd612835dac3b6873bc220245725
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:32 +0000

    bag chase centimetre leg present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
