3ef96ca8a4cdefd3923da07b394e6b08d3e793f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:20:17 +0000

    call border objection although accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
