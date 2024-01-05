d3966a949c59987fc41c38342edb8d6c023aa66d
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:53:40 +0000

    embarrassing direction callout thing bang

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
