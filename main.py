d31bdf159526c9b44603603a04563bc7dfe9cfdd
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:23:45 +0000

    lookup possible block advise wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
