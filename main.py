b1120dcb420281056fe4b9ebabab9ca74111f5ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:05:13 +0000

    ability wear breakup opponent handle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
