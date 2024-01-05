9547e69c381a3124bfc13505e09e9d27f430e542
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:39:31 +0000

    earn me upstairs myself seat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
