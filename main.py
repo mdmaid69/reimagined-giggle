6149ad385695daf9c701219953f93cf1f1cda99e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:53:10 +0000

    lot half slowly section open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
