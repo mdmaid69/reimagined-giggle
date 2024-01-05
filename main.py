9f18d4719656ec28d10b13c70b9df7b0b0336e67
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:34:35 +0000

    tall dinner likely fire room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def remove_duplicates(lst):
-        return list(set(lst))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
