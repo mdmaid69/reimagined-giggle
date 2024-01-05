4e52dd3ff82594ce482f9b4bbec24a2c1d05a631
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:08:39 +0000

    container broad tragedy language mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
