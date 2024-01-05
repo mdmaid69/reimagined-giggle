bda9d82ac01c7484cc9cc03ce8e0e313cb23d59f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:28:46 +0000

    affect open false thought start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
