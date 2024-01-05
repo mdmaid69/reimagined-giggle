b8b9ecef8d0e0351423bb7b9db29fda8a8859399
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:59:17 +0000

    card future tone proper either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
