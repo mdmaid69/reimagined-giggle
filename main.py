3da71d79e001f7a90024854e307e48734e554973
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:35:35 +0000

    march by experience temperature tie

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
