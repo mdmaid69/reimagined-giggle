0151d7eb0dac397865946dfed8e7143312b21d63
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:12:23 +0000

    trade fierce official shrink group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
