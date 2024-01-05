3e8ca1a0888305c36beb184e7de0e586cb94c937
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:48:49 +0000

    total right trick peace hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_list(array):
+        return list(array)
