cde915f65f316791faa321c824cfd37eeff63d91
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:57:51 +0000

    care complete guard introduce bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import sys
+def print_python_version():
+        return sys.version
