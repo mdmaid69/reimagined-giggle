8af01a3f58ce707fda04fd6b8df802ad96332234
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:54:20 +0000

    distance tough have priority expensive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
