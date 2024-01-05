e0890971e1fd06ff646ec112d282698bd97beeae
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:13:17 +0000

    takeoff break main rain free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
