adcd4eef66c82409de4760d854e21961c7eac897
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:39:12 +0000

    marry trial yesterday supply old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
