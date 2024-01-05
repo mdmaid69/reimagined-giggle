8a22b9186eb1228be5e28dd6fdc79ffffaaffc71
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:26:25 +0000

    backwards politics attract discussion callback

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
