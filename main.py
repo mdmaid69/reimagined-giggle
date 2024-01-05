8c1c17b1b997025a5a7c84f130bcb29e7b353348
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:28:38 +0000

    enthusiastic goback(to) surprise serious begin

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
