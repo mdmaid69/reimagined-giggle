0ad0cbf52d9f474ab2bc48d002ee8f416eb57448
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:14:02 +0000

    true old large slice forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
