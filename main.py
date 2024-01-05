4f1366c7139974ee06a146350bc9bc8b1b1e48dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:14:33 +0000

    figure reply smell surprise anybody

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
