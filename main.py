891c3e0fdf93ebd4d2a2f7b6aa9c80abd6595e15
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:08:07 +0000

    set future anyone before part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
