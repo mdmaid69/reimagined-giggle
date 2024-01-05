b541b072c2b2ebec1720081893c1fc490c89ae2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:20 +0000

    aware unlucky difficulty giveup stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
