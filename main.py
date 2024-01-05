8fa8425d92257b53c213caf930b428f06b4e7dcb
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:12:09 +0000

    set spring frequent how drop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
