a166860972e3aae5073cede421f5c9db8981a7a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:10:46 +0000

    low important greeting dish waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
