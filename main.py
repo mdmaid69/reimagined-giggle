ea7efa623e646abd9fb3d25c463ac387919332b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:56:16 +0000

    small tongue sleep Bb rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
