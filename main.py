03d6de7f8b9e15be275cf5f46f55936d2b344ae0
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:56:13 +0000

    risk empty prevent dry dull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-for i in range(5):
-        print(i)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
