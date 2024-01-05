3445fbf3560ea003b08f110508f46583f2983b53
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:05:55 +0000

    doubt gentle range display translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_list(lst):
-        return sorted(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
