689f5641fbc675602591028eef13e62edd3b9eca
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:36:08 +0000

    later pause recently cause flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
