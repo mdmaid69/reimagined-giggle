9631c4bf3aafa88e63d247c069bd51e08234e7de
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:00:09 +0000

    turnoff church prepare school any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
