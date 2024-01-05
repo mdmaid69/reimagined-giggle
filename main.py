eb2e16a8e73ec0da9be1dea72d2a35e1e52d0dc7
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:56:27 +0000

    bread please hate length feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
