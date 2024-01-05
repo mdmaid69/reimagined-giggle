3f5e19783165418b4e956925bd1a1c77aa559124
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:55:41 +0000

    pretty from fire holdout string

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_binary(n):
-        return bin(n)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
