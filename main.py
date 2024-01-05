efb304f16a6ecdafd04da5ba9dc3e167e43c015d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:59:17 +0000

    success responsibility spring guess interfere

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def reverse_string(s):
-        return s[::-1]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
