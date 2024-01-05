cc8f7c97d709686d3e48e5588dc8668a40b96412
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:45:00 +0000

    wait murder chase hope steep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def cube_number(x):
-        return x**3
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
