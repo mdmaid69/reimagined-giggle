fac320db4e0d91aaaecc15d55623ecf437d2768e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:53:33 +0000

    repair duty aim brush wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def square_number(x):
+        return x**2
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
