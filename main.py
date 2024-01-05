a4773a95c58bd2034a4e0d536b5812d7d66669c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:45:54 +0000

    typical alone free arrive wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
