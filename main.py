f9118722d7a0ac67db227f7cbc8ae73a8d6d3a48
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:16:52 +0000

    supply quality fierce impression hang

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_power(work, time):
-        return work / time
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
