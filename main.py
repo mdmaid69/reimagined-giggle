026e9e333904c95056ee57b130b4a34b30784297
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:48:38 +0000

    downstairs night into exactly geton

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_volume(length, width, height):
-        return length * width * height
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_work(force, distance):
+        return force * distance
