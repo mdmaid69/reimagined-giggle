c423c649982a040abf5bfb5f57d7e19167f96270
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:19:38 +0000

    immediate talk variety crazy official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
