543f8fd8708238ec76dd6f6df18ba29ee3273f0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:04 +0000

    near official car save hurt

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
