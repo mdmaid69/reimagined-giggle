65f7a8476ecef18cb471a8ac070c20a5768d151b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:33:14 +0000

    nature comeup clean raise order

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
