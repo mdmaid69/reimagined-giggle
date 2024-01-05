2a0dfb33887742230daf535532e7a01eedf25d01
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:22:11 +0000

    punishment rush use cover here

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
