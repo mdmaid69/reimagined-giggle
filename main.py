10c2f7e2d9fc021379715f7aa143fefdefa47ed4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:14:55 +0000

    comment free drive there do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
