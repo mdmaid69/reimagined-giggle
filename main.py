365b2eb10154efdcf37b1d538aab913e0feb0fa8
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:57:26 +0000

    ability uncomfortable spill criticism birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def find_max(numbers):
+        return max(numbers)
