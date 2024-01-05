114cb8d91d61dc1c29519aa6ac61db31d0546b3a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:52:19 +0000

    question command deep moral Tt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
