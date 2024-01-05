0fd6e02e30a1c794d922268982dc43018f86d457
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:35:00 +0000

    fetch upwards deep slice length

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
