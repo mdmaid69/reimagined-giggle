68fef7c3c4a5b277b5e53eaf38174ca978aa5384
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:11:07 +0000

    weak good total agree rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
