f8b90efa715adfb05315e8ecac370d4bdd852ad3
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:08:51 +0000

    turn sell waste surface will

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
