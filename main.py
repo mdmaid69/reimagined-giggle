e8d1995ce2437911077232ffb1ac95f870ca3963
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:24:06 +0000

    pure delight leg minimum present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import sys
+print(sys.version)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
