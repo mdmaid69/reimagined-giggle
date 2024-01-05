51edbe774745ddcfb0d45cfbcf77dd2841b58c11
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:38:25 +0000

    constantly view move forever on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
