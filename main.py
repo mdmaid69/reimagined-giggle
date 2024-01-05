cd076e64f5022a49b6657b452f6c45a9d6c14e10
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:17:31 +0000

    finally would protection deliberate cruel

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
