1c6440102359331c5ae403e99ae33546d9ffeb80
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:21:47 +0000

    light train sense stand bunch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def cube_number(x):
-        return x**3
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
