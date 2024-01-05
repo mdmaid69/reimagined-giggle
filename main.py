53f66065c139742dab4377871902767c189b9a23
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:40:55 +0000

    hollow exercise eager sorry force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
