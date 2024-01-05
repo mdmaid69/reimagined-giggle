036330da3e72fc90a8a15bd8127eaaf6d3ea1005
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:17:41 +0000

    arrival tune pleasure crowd list

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
