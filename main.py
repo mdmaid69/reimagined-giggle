6c9e979e5adf970ad2cc997bf5e79aed752299f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:57:48 +0000

    region possible interview its emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def add_numbers(x, y):
+        return x + y
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
