99114f7eb7b237c940208aefa963c182a084311d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:23:18 +0000

    stand keen ticket loan wheel

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def is_odd(n):
+        return n % 2 != 0
