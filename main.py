4b849bfaa9db5165c690d2c2582c17b30b3180fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:20:17 +0000

    promise story she breakfast wash

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
