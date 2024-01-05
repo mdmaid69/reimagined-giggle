2dcbcf7ce995df29911002cbb8a5a426f4616c0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:45:54 +0000

    frightened exercise smile between wash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
