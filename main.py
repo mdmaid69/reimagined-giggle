5684d9d7c753346296f30259b3c146805bef539c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:52:41 +0000

    shout loss under guess roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def cube_number(x):
+        return x**3
