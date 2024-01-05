1ca726eaaee5d38507f0377f38748d20e85ef3f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:54:21 +0000

    related contact variety children clean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
