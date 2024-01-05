f9119dba587f4509431e93b6f5edf37db29925d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:05:09 +0000

    design opinion name recover guard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
