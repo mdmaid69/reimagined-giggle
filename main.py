659605a36b7343c2ac72ac58796bd609cd6af9eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:37:46 +0000

    brief approve car search if

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_float(array):
+        return float(array[0])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
