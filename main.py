543bbee010664b9931ef46c2094a0969171bb786
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:21:37 +0000

    peaceful deal cool delight occasional

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
