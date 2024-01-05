987fb722559194fe4e0184fa460dfd8c4f59a087
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:42:42 +0000

    breathe team goodbye power free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
