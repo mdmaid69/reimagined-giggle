f012e88a9329c49d1b8d3ca928c4abdd5bf5bb56
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:47:29 +0000

    convinced star thankyou Bb total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def delete_file(file_name):
+        os.remove(file_name)
