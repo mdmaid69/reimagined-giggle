c51d3e8feda95a56b895aea9bb394ae8eb788f68
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:12:51 +0000

    human temper regional wide takeoff

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
