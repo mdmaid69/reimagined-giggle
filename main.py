8f66b0fef1c21b4c19f6b8e121e9f991e2afd525
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:46:32 +0000

    than loud seem become strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
