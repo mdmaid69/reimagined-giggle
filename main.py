e04f03cf87b0456525b4dfc5429f1834aeba921f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:55 +0000

    stay natural spot breakin Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
