9a2248cbf3b8da775543301b048a6f4a870bf418
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:25:45 +0000

    slightly judge push impressed opposite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
