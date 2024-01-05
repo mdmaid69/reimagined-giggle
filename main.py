a04d6b38102dcf32ab743184b1711f634a2d8187
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:59:55 +0000

    nerve transport come unfortunate brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
