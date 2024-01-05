4ab6da15dfe81cef484834cfcb6cb49b5e72411d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:26:56 +0000

    both possible over transport often

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+from collections import Counter
+print(Counter("hello world"))
