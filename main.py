b30570df6a18c3c0b920c6091ec633853ffb46ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:39:26 +0000

    attitude relax believe confidence because

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
