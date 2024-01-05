2788dfdd0aa78ee4484abc8f25f6b719b2353dfa
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:54:47 +0000

    judge connect unpleasant adjective search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
