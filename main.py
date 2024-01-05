15dabda247bbd7507b9071a355784e9303e66439
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:29:53 +0000

    offence find everything runout maximum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def create_user_dict():
+        return collections.UserDict()
