df974de2db085126c8cb9d66fa1183fd85698ab1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:40:10 +0000

    underneath unable pile turnaway pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+print(sys.version)
