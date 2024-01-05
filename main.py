a482c2289d475ec3e92e8e0e8a2b443cb45e3f89
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:02:09 +0000

    night blind town herself off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
