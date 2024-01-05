5712e4524244a10b44dd6c03753838cf7808d0dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:44:34 +0000

    float lower straight develop float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
