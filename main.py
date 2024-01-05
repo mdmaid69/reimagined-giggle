b05e271d6f0fdc0372eb763b02c2c1249c091489
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:57:08 +0000

    goout noone muscle television believe

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-for i in range(10): print(i)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
