b31f46021c6cf8bd23030a3ecc9a845dda9492f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:07:41 +0000

    add disgusting excuse possibility before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
