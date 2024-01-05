3296191ae1de3673a20950b5f5e88d2caa539fd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:32:43 +0000

    present mind makeinto backwards rough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
