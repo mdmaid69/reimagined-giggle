ce9d54577aaddfedb25a2fb05b10ffed0bb81e67
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:24:24 +0000

    pump goup draw generous especially

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
