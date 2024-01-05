60586ce9076542196a36faba56d0c183c5465ed0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:56:50 +0000

    last develop generous star judge

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
