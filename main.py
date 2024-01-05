05b4386fb98796e55c311bd509aabfb5f111d2bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:28:05 +0000

    thumb favour possession opinion rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_pressure(force, area):
+        return force / area
