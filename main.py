a94ad46ac92f47623c0a31bb09a810019dfc1b8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:51:15 +0000

    row down quick profit party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
