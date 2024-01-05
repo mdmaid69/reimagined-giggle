223201ddf25c5e8a70e8b97bd35885879d3ac84f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:57:20 +0000

    increase second Rr defeat provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
