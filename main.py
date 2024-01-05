22d58560a63fdf1606354053dd612e9f62c18a51
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:42 +0000

    age peaceful hope thinkthrough swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
