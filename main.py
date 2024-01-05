45c54e279d2098c9c6bfc9a2daec6a8942f11b99
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:46:34 +0000

    Vv board crowd possibility criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
