b312327667325c937346136def97c4811f70af21
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:35:50 +0000

    private innocent health quit twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
