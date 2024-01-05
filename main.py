5b0d9cdf7f5ed0a962c4808f81df6887c71a6e1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:52:30 +0000

    today typical go own colour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
