63253c9457e26c70e537518520be8e7b19d9754b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:48:13 +0000

    discuss polite rare mainly until

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
