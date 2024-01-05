f96fb384a1d457f018b69a5f9f54e743510b35e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:37:05 +0000

    far spin delicate notice influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_index(array, item):
+        return array.index(item)
