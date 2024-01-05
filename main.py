0fded75f469f0cac4e51f39910c5c030dbf7bcd0
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:05:01 +0000

    steer frighten scrape edge 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
