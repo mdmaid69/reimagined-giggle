cb03f56b2f4ca25e739b915fcc1abc0492ca4db3
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:56:30 +0000

    away distance bet precise wise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def reverse_list(lst):
-        return lst[::-1]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def find_max(lst):
+        return max(lst)
