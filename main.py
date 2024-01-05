a0b50b0086191f567ae1445f53ea7f04ae71fa88
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:18:22 +0000

    serve child shape small Ii

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
