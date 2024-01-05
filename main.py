c988e86be2d6f5e89536c0ce88bad2430b9d2dd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:21:41 +0000

    about anywhere refuse street miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
