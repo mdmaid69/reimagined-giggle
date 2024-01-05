a395ca5e61f8ae8b20e2cf53bf4c3b684507bd1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:41:55 +0000

    ring pure couple rough send

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_even(n):
+        return n % 2 == 0
