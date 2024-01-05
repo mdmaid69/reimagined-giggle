83af5ad8e3ead80742d2cc3389f73cab01cee300
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:59:18 +0000

    instant recover else side path

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import tensorflow as tf
-print(tf.__version__)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
