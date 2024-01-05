549435e1ef836563e9b13c38139765b33d2a02df
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:03:25 +0000

    aware getoff half takeout complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
