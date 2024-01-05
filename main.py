aae2b9e259681a721fd9317155e50c4e9e6cc63c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:50:21 +0000

    development mixture fasten copy rush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
