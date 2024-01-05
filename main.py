de5443097587deb441a6ce1ee7bff8ddffdb2ebf
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:35:45 +0000

    payment people pleased waste several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
