e923a3803b28b1ec78a75f7d8e59a786111721f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:31:14 +0000

    score realize pause food check

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
