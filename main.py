25e875eb57e22afc095e31b72c7dcfd762de3120
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:47:00 +0000

    judge breathe correct form exit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import sys
+  def get_python_version():
+        return sys.version
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
