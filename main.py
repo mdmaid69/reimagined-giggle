72c29c5cdee86dc00462e1aadee4d04f890ddca4
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:47:28 +0000

    surface back past traffic thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
