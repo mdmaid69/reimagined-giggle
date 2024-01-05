83804f20802f3c8fbafb8e9ac7757858e96d65df
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:01:48 +0000

    programme entry hers vanish effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
