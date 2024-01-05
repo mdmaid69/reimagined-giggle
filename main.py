255261dc126e60aae766c79c5175bd81148931d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:56:30 +0000

    meanwhile improvement consult exchange bread

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
