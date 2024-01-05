1c0605a63357d8e8b2a7afce7575f1970f58408d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:41:25 +0000

    ilovedicks convinced repair properly cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
