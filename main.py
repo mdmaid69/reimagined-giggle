1096ada72dfa37c2daf3132348933c5d738a571f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:56:44 +0000

    complain characteristic process nurse bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
