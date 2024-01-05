ba01e2eee597cd8a62107cbe73743aedd5ddee97
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:43:30 +0000

    treat real curious know fast

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-print("Hello, world!")
+def convert_to_hex(n):
+        return hex(n)
+  import sys
+  def get_python_version():
+        return sys.version
