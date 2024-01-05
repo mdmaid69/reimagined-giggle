49a1e6ed72e7a0ffa804c8f2ce830beae1e49904
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:52:57 +0000

    giveup contain listenfor ready turnover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
