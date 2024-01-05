956eb75ad46bac228775933311a40a668e3c126f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:22:27 +0000

    month record bet replace persuade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
