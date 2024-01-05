a440c2a5bf5424e4d0faf74b245163d08f342a22
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:20:21 +0000

    habit complete close talk hit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
