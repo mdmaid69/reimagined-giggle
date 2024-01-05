46f0d08b8b3dd400a83ad5a8fabd8e8fc419cc5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:40:40 +0000

    reliable trick escape afterwards gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
