520c5cdc304f465cf83ab4e4f9a028f758fcaf78
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:22:57 +0000

    letter cause clean land knowledge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def remove_duplicates(lst):
+        return list(set(lst))
