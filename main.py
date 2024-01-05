4afa316d5dc00f00b99df824bde621e708b1a724
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:55:20 +0000

    heart Hh expression illness furniture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def find_max(lst):
+        return max(lst)
