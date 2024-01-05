b282baf996e7ed380edd2691e222bc7a1865ee2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:08:24 +0000

    appearance build difference argument talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
