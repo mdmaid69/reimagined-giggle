a9df0ab9679346ff806206922d22aaf5663f4158
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:43:51 +0000

    experiment friend collect makeinto contact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
+import array
+def get_array_as_float(array):
+        return float(array[0])
