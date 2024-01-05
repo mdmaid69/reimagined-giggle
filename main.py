82faddffa6fb30c648d43f992e635352b93daa3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:51:23 +0000

    interesting present this pain very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
