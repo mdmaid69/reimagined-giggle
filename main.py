c91a19d330a19ef5fca976f6d628b2d1c7924334
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:22:54 +0000

    safe whistle stop temper room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_area(radius):
+        return 3.14 * radius * radius
