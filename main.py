edcbef7a3d2309a59bedc3d055911a47e6a13f78
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:10:18 +0000

    giveout but increase mention suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_typecode(array):
+        return array.typecode
