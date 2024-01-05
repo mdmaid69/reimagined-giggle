18e113b50da42b5735481f6ea29485fe917d65ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:58:16 +0000

    loose plan full care related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
