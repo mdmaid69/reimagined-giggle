64ce8654ed26010702e3a21018d586207c061f79
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:45:53 +0000

    relative spend tool quantity artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
