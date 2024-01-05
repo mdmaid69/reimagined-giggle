82e2bd303355cf902bf1a177151722113d73fe19
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:03:10 +0000

    desperate treat agree suit load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def convert_array_to_bytes(array):
+        return array.tobytes()
