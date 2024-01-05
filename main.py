87d5230e63d4b6c2742e6b3e282a342fea797ff4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:06:49 +0000

    extremely share tool giveout unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
