6ef4bbe5a97ed1429457894950201111bafbb6c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:43:21 +0000

    shut that male sudden workupto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
