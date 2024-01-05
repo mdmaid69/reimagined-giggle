bf3845c67457db2ebc264da0747f604e843cf562
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:23:01 +0000

    means down history mind deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(numbers):
+        return min(numbers)
