0d4d61644fae8b7586dc32995bba645a036f5e60
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:35:48 +0000

    approval design across later judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
