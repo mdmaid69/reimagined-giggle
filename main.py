71370c87c4599d21dbb6525836894a97753b59d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:38:13 +0000

    might keen count I high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_factorial(n):
+        return math.factorial(n)
