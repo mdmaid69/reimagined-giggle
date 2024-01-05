48bfbaa0ccfc4564e6402ed26ee36378e54c0f45
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:40:59 +0000

    extraordinary little display scare freeze

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
