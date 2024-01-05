875614425923f8433b325ef33044bd8fb5dab93b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:31:32 +0000

    suffer sweep indeed house nervous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
