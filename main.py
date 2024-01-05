92ef40f20ebe7666e5fe6f915fc3e356eb9212a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:17:09 +0000

    move table colour can who

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
