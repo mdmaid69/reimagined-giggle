202a112f55bd28278b2050e96ad591dc69199673
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:33:10 +0000

    right fetch number Zz subject

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def square_number(x):
-        return x**2
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def cube_number(x):
+        return x**3
