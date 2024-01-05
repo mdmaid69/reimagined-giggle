3dfb26e82d055a81be83bf636559baac35db7495
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:47:30 +0000

    distance pay tip alive slide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def add_numbers(a, b):
-        return a + b
+import math
+print(math.pi)
+  def is_odd(n):
+        return n % 2 != 0
