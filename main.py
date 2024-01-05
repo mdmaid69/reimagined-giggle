944267ae8b29769f8d41ad3397ad7001798d5c29
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:58:10 +0000

    sink either also firm honest

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+for i in range(10): print(i)
