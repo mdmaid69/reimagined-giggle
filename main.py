09331ec7be6729c44a56a0c9b745072b93df3830
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:24:53 +0000

    tap whistle lie talkoutof record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
