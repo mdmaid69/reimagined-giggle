1de979bbd52c2a4ae3038b00ca41387c06dfdcb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:34:59 +0000

    control phone open march need

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+for i in range(5):
+        print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
