7bef390560b5dab265653915d024d1feab9cf8b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:23:39 +0000

    shadow run dig reach regular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-print("Hello, world!")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
