e313dbd2ee9f211bdd7c7ae7f1d5aa7e7520c95c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:25 +0000

    tense shall picture each cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
