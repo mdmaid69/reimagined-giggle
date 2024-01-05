9e502e746b17d463e0fee60167b46ebaec5daaf4
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:12:40 +0000

    callup holdout touch bringback place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def cube_number(x):
+        return x**3
