1b71488f7d888c6c013a7da3c95a373bae15bb11
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:01:45 +0000

    decision fire gap quiet trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
