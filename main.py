e0399be93365da3803e0452184a7abdafb09522a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:50:22 +0000

    miss Dd round window cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
