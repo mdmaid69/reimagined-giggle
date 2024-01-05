e22857f6b927a778ef59e47e1e655389298e225d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:00:11 +0000

    smooth reach calmly tear area

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def sort_list(lst):
+        return sorted(lst)
