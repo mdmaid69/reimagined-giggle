cefe35bad605d32f634905527da2330d5c82c394
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:17:53 +0000

    dull plan beneath couple along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
