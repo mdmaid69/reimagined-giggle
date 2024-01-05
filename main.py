6464fe336e33d3f70c91c4a9016d1b29676bb479
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:29:19 +0000

    glass free development become kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def find_max(lst):
+        return max(lst)
