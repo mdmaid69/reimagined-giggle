e70f13f429173e66adac66a5ad0e2eae218e74b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:46:26 +0000

    frame fortunately Mm miss left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
