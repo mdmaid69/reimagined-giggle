3f8cca843c02013d038987b75765e05273fd6800
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:07:11 +0000

    affect family nonsense cool quit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_hex(n):
-        return hex(n)
+  def get_current_directory():
+        return os.getcwd()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
