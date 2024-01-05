81e888d33d4ce50f3eaec66cbd7d0ccaa9f20096
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:54:38 +0000

    his much something reliable fast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
