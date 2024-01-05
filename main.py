4c5be97da0a4e52ada17b406ec98dcce573de0e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:26:42 +0000

    sentence kick account but apparently

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
