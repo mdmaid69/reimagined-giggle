f44f0d8d074c9fa374374d57d4e991e616aab2af
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:54:19 +0000

    strike strike phone relative act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
