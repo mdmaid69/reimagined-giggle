f8cef29d8dad268155d329f939462edf09fe13e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:52:09 +0000

    sound plain special tone more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import json
-print(json.dumps({"name": "John", "age": 30}))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def read_from_json(json_string):
+        return json.loads(json_string)
