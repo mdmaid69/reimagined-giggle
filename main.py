04839730358cac3eeab82fa4435deaee30638695
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:02:16 +0000

    course profit mind share plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
