5f77c5a813418549ecd1efaab468f0e88b6f35ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:30:54 +0000

    extra chin combine runover stay

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
