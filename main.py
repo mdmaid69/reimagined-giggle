a2a9c768e7bebb24050442cfa02484316a115272
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:23:00 +0000

    complete keepaway wind limit through

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
