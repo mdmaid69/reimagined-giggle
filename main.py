81c9eecf532095211ba9c1c818b64a499b7b3064
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:05:55 +0000

    forget future regional tall street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
