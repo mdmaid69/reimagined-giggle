2d41d105af3e44407b717b42daeb266c140e8678
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:24:52 +0000

    smooth notice although way tidy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_sine(x):
+        return math.sin(x)
