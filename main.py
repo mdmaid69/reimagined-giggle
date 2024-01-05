c4828d4c04708b5259484c51b8efcab0332942ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:57:14 +0000

    grateful nigga maybe sit bear

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
