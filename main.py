380459c24e24e48ddfa728d7b8386aacfe647330
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:37:08 +0000

    declaration afraid bottom goup intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
