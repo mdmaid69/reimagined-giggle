767e2ba1763ab440f878a766a66b424b46556d27
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:27:53 +0000

    sound previous offend female doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
