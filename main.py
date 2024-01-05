27bdb8533f4749e68992090a5ae3a96a20817bf7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:47:28 +0000

    callup street cause release everyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def add_numbers(x, y):
-        return x + y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
