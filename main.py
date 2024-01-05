c847e7e5ffd5051275a403092f299fdb9ab91859
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:23:05 +0000

    tie break introduce major boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def get_array_as_list(array):
-        return list(array)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def get_array_item(array, i):
+        return array[i]
