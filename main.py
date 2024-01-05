d2738c24f2b5fa3d104c3ad815d34613cfc43b13
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:06:18 +0000

    runaway minority shut relative total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        return sys.version
