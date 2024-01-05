4fddad7709ebcc84632f5fc5f7d481064b8039ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:12:52 +0000

    wide survive prepare start money

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import collections
+def create_user_list():
+        return collections.UserList()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
