ff1dac3712acf53d7b419afa03afde1f6ab25ad9
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:28:44 +0000

    describe fold catch impatient container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
