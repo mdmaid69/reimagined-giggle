6f7c1fa75c984336bac80e82031475cd82437bb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:40:50 +0000

    visit free wave mend certainly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def count_elements(lst):
+        return len(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
