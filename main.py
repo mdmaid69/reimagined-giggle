9f6f3077aa6c735996bb104378c8080c8b61462f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:28:34 +0000

    otherwise effort whoever uponon enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
