236e491e70b66f576777584eece5d6b38296cdc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:09:34 +0000

    now drive buy refer calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def convert_to_binary(n):
+        return bin(n)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
