f73135c5fea35dd09d45b7f4da5b7bf7bc248ef7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:06:08 +0000

    late grow ourselves receive range

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
