95932a541322ff932a0fd338be4f94b022ee4ae4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:45:35 +0000

    classroom juice trousers borrow power

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
