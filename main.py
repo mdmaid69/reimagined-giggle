dfe7849a81b57c4d6d4e50c6218b34595a28c82b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:36:30 +0000

    sing badly still goout congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
