5c19347bf3b9423292cc2085187b585bf9857f3d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:15:48 +0000

    in trousers capital back upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
