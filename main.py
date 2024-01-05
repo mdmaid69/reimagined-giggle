e80c75a6a1e70785ff23bdcec5a13d767eea63f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:40:50 +0000

    hardly friendly begin single everywhere

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
