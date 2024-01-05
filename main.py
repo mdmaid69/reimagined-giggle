61c894b861da47ba8ecacc3f77feda01371908c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:39:08 +0000

    promise sore comfort interruption thorough

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  def add_numbers(x, y):
-        return x + y
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
