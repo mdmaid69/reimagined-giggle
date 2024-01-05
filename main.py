1a57848fc8bc9ad3d5566cd5bf5c43b91b381f5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:43:21 +0000

    face lean some Tt expression

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
