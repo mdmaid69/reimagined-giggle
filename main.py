7aee96e57a06e9a394fd5d2e75fc2c8df36ccbfa
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:25:58 +0000

    smell behind still its autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def square_number(x):
+        return x**2
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_repr(array):
+        return repr(array)
