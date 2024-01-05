a5925b9ba40a01f6f35ff60ca9b357f4309d10fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:30:41 +0000

    job taste supply weekend pot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
