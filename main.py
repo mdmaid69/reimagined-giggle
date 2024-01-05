2e647d47dd36fcfb97cbe1a39ddda1f2f8adc769
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:32:45 +0000

    genuine nothing little fit because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
