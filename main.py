2691cf6b7deca9f16e67ce904cf3653c4b22d07b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:35:57 +0000

    really coffee Yy spoil field

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
