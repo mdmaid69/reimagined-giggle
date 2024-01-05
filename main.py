50c0161b6d82a8a776c6f927f3818364e092404e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:31:20 +0000

    protest attitude treatment trust view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
