443584e2a79a7e80293d8a06c81ccda5e5fbd19f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:31:16 +0000

    boring backwards leg pleasure both

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
