b4f067b670ccfd44b583b8581561fb16f418cde5
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:58:17 +0000

    tape damage favour ever cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
