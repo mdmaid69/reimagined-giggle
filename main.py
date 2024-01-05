6c9421a72239389a16808a9364d4e71d01496d30
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:35:46 +0000

    vote embarrassing return imagine programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(numbers):
-        return max(numbers)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
