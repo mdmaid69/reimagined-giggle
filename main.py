405e21fd15e4aeec35706d0043d067fb1b531934
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:23:31 +0000

    not worry welcome estimate advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
