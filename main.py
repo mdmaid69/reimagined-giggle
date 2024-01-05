8a7ef3de79b1cc40d2b29e06f2868368c5615f68
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:27:55 +0000

    dull view Vv hard progress

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_tuple(array):
+        return tuple(array)
