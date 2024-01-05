53a66bda11742a6bd48cf461a5208efc5a64794f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:02:13 +0000

    considering think scrape know cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
