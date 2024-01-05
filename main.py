889a47f142f9d5cc7f6fd32ed29353d7e8a514d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:18:38 +0000

    scale worried fork exam check

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
