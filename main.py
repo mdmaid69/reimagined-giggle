aec0ef985859d8a92354028cd2b2ea1e2124b587
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:15:05 +0000

    process slice recovery sort anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
