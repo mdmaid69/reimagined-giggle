b58b47d98df3d0a9bf8230c5fc1917add19ebaed
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:52:12 +0000

    train furniture price usually deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
