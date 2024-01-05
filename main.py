f760c5c4ad281fed60242d1139bc3ef638765cd3
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:31:54 +0000

    wash right exact move improve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
