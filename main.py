598ae9ffb90e8f0cef6c7f8ca02446381f43c2bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:37:59 +0000

    add punish collect dead form

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_volume(length, width, height):
+        return length * width * height
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
