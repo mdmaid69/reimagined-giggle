36f4c284e37eeab739379ebc35f511357590af8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:48 +0000

    drop but fork blame neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
