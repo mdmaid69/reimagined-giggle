45d7952948f867220e11cf7bc7ee789e94cabf5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:02:55 +0000

    press view wear programme single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def delete_file(file_name):
+        os.remove(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
