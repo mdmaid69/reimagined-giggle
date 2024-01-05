1612617fd5c0a3fba402c38a4d63067588fd32b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:55:48 +0000

    might Mm travel wake compete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
