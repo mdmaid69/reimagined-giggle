81de32ee1aa8e7cbe2cc57b5c9cab2d38ea2ebaa
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:17:32 +0000

    bright unhappy false pay light

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_power(work, time):
-        return work / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
