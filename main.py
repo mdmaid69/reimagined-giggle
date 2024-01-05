531a1d238b0e6889f3cbcb99801f1e6385286fd7
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:43:19 +0000

    catch common rule possession border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
