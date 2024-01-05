79836775515ee96f9c2d4ddd47ad231718f28228
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:56:31 +0000

    people score official experience comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
