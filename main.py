6a0540cdbe688c9a8377dd33ced40083f8326de5
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:59 +0000

    wear claim me convince address

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-print([x**2 for x in range(10)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
