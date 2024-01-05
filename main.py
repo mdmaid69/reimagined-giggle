b9a51b1bcd1c4bacf12b488f5ca72575296f7b74
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:46:10 +0000

    mine burn catch before connection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-print(time.time())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
