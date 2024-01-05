0710595f94fe8ca7cfeebee2b2770311acf5075c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:56:32 +0000

    thick little shout sticky circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import sys
-print(sys.version)
+def exit_program():
+        sys.exit()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
