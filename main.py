c7fd9b56bb97959438333f9eae7b3f1553b55a93
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:10:30 +0000

    scream always guide there drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
