92a41ae59a82a582e48aa69f975696e72214e844
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:58:59 +0000

    I punish post sometimes journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
