f4f171bd5b71e9b0c4ed309f730857158920fd38
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:22:56 +0000

    blame negative getout need quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
