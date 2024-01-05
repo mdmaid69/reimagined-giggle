41c445b3d1e22941c909a1f63dc29f76d76b3277
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:36:23 +0000

    anyone certainly point shock bright

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
