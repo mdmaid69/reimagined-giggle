bfe69ad277db396f8a0fdcc4c7988e5d2e1d3004
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:20:03 +0000

    precise week thirsty fasten point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-for i in range(5):
-        print(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
