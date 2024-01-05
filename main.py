75ebbf2ef8b3d782f10dfafbcca7191d113f9cf8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:03:44 +0000

    damp plan rather either basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
