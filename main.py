afbe0d6517df346acc86976eca1a68bf088ef550
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:18:32 +0000

    why grand violent embarrassed pronounce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
