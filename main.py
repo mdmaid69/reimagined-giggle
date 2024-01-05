40309e2a2cf55a295698d93a62eacb3e04e871fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:26:36 +0000

    father discover cry tomorrow regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
