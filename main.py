904f1cbebaabe5da4d907c1a266789c0610fe0e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:34:08 +0000

    cut answer stranger remain find

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
