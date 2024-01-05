2db7c41d2fe8afa01211cb327ed7408dcec7f5eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:54:39 +0000

    sense gooff message speed spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_list(array):
+        return list(array)
