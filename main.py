919eff9752f32ebbbc7c500e22f1459f7c7b1b95
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:30:29 +0000

    thought beside little attitude hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
