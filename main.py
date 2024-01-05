02890ea0ef79f76961882bedc5fb4c0077785a6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:53:13 +0000

    understand workon characteristic fear point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
