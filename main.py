b7baec2f8a50c70b75954f31695f27acfadb4284
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:25:04 +0000

    opportunity right impression goaway strike

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
