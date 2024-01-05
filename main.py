2c49b2b1cc15045e0a889627cee95cdcf571b81c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:20:34 +0000

    fetch such hungry final load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
