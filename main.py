1ebaa0f1f93edd40fed552cf83e4593531111616
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:42:19 +0000

    office course opposite plenty block

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_work(force, distance):
+        return force * distance
+def find_min(lst):
+        return min(lst)
