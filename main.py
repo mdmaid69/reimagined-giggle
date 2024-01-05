a1d150d3d561a848685dfd35f7c853213e48982f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:17:49 +0000

    early smell pipe undo Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def sort_list(lst):
-        return sorted(lst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
