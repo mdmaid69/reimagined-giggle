7133a57dd446b1bfc30380b95027cced1dbadc1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:58:24 +0000

    score reasonable beat separate respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
