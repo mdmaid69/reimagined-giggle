ed4e4142f38d5a44d5f08d0b68f8b560afbb1a36
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:40:51 +0000

    border however wash height outside

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def add_numbers(x, y):
-        return x + y
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
