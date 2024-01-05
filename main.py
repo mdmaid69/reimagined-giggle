ef1f1a78815c292881eca4eb763e124cc82c5722
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:06:34 +0000

    trick anything somebody trick bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
