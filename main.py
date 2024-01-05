4d07fb7203bd66f114a6d29f4c700425f8092fc7
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:48:22 +0000

    way heat warning file but

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def sort_list(lst):
+        return sorted(lst)
