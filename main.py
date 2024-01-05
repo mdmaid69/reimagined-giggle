cb22f468c39d9cf9d0250464a6d803cdd23fbab9
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:15:20 +0000

    born exchange fail before generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
