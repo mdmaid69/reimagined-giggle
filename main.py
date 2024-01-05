adfa7b583107d6c1bb3013dc3ad137904a7275d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:38:41 +0000

    gas under system coat break

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
