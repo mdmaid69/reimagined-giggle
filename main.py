0605fa6c065eeac017835b4e6fd77be842b71b50
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:00:55 +0000

    previous moral giveaway bank soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
