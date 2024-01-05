8866f9fea37c2aac1d067326c40e71daac10ea35
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:48:53 +0000

    anxious connected film crowded name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
