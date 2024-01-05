f43480d04ea5760d20ec6193e78c0c3e1b07eb79
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:50:43 +0000

    eye arrangement Pp brief runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(numbers):
-        return max(numbers)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
