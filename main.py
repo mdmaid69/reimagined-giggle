2e763887170d3c33f266a6b0c5331029969bac51
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:14:18 +0000

    broad means direct appearance more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
