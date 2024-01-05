95fd3232b30f823f18b278a4cd34649856ce4580
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:02:54 +0000

    nigga me trip form spill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
