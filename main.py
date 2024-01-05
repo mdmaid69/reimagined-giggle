c443d1ba2766b64a47610d0c94ac0d7a982d26fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:35:30 +0000

    divide feel proof although except

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        return sys.version
