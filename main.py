016ec285a0f9a66b9f99f3e2b12a3ae924bb5c3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:08:29 +0000

    clear rain authority holdup voice

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(10): print(i)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
