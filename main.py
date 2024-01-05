5a7def094dfea2511f3ee724c54c2c2a0e3417c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:24:20 +0000

    advice arm middle maintain spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
