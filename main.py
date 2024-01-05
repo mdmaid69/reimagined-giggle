34b5b62b2a4f031df91cfea584583172503c14e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:51:08 +0000

    handle education worth container shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
