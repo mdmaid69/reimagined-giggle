1985e3a10781ec5e06d3a5e5ee2f1662cb12d7a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:19:02 +0000

    especially frighten rubbish future constantly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
