47f3b268a30532f6011a3d40a68386159d358f55
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:43:55 +0000

    disturbing score mass base mystery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def reverse_list(lst):
+        return lst[::-1]
