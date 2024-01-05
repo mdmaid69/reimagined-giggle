23b844a85ba5012f64c2db6afdcecebc930e7859
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:24:48 +0000

    shut happen share warm cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
