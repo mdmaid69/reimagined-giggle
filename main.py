57865c1a7b87718b0a2f8934eeb72739704cb849
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:51:23 +0000

    Ee store later suit protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def delete_file(file_name):
+        os.remove(file_name)
