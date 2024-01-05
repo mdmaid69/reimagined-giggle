c6afe5564184d0eaafe66f57a7917a2ffbe7aac1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:55:32 +0000

    related do correction completely pull

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def reverse_string(s):
+        return s[::-1]
