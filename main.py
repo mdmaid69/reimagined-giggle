927ac3db8fe04e305a640eab2545c67d70c6d6a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:35:45 +0000

    build easy progress block more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def square_number(x):
+        return x**2
+import random
+def roll_die():
+        return random.randint(1, 6)
