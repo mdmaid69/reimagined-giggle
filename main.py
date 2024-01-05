806b2e3fb56bb20f6493af2b082ad27bd5c8804e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:02:58 +0000

    wear lookback(on) keepout spite nor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
