b37b564eb25879535ad32025dc2e8de4c160711d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:45:11 +0000

    interruption claim during attractive but

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
