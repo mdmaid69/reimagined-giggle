4a8d88073b111663f3068d5c2de22f137759b303
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:07:51 +0000

    other bang late radio closely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
