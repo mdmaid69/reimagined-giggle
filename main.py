41bb68b756dc2cc4f6160e498e06d6e6e7d646b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:16:36 +0000

    protect dick long which noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import datetime
+print(datetime.datetime.now())
