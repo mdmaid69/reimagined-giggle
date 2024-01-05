02c8ebbef90d48f037f1bb70368cd7f76fe6dbba
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:43:37 +0000

    strength please ticket slightly bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
