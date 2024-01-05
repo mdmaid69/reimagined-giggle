c9dabd7ce9e1f0124bcd92aba6ab6c2afd1aeb54
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:55:05 +0000

    amount life train song left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
