9feb1f7c6c23a63e608a81a0cd4703fe9ed40269
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:50:37 +0000

    born begin theirs noisy move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
