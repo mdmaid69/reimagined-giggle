db816ca2d0ae011dfe9111c4422e72040fc93a0a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:56:59 +0000

    difficult lose think mysterious interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
