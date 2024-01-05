4b83f037a6e9db91dc2fd4a8ea8edac9e4853504
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:11:04 +0000

    partner embarrassed still loose threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
