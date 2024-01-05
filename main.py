daa0c86cbcbba09c81f335ab6d8265ca04a9426b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:15 +0000

    pair shake measure wing exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
