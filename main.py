0adc6ded91771768bc4a527804895aba3594fe46
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:32:19 +0000

    can sting slightly occur giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
