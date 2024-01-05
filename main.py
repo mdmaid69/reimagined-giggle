f2c6f51e83b263dee259799aab059f02d1efe932
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:43:32 +0000

    instant discussion degree represent obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import sys
-  def get_python_version():
-        return sys.version
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
