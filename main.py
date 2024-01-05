733ca3a0e28de1d824273c714f632b3adb274804
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:55:13 +0000

    thin control land criticize apart

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
