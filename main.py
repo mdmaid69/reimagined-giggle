8eb6757756d4380af03d8ba6d23d06b609fdf08f
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:28:33 +0000

    seat suffer gooff accidentally turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
