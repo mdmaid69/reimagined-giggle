99bec4a5763e01bebf78ed76a81650b0369f0a32
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:39:34 +0000

    pay cap wish black principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
