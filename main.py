62cbf041ddd677464ae0be3a0f3f726f3bc4528c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:02:42 +0000

    cap live among bus goon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
