cf746b455c64ccb95759636862ef92c1a6da4a70
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:09:46 +0000

    treatment concern approve future approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
