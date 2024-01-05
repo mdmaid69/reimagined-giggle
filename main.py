b91a99daad4e14237e7f57deb44b70f5e41ee41c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:01:04 +0000

    check home manner centimetre stretch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def convert_to_binary(n):
-        return bin(n)
+def get_array_itemsize(array):
+        return array.itemsize
