69d8c61095e2796e00105e96428e45db5f96dcfb
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:38:53 +0000

    strong top advantage off getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_min(lst):
+        return min(lst)
