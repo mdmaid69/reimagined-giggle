3cff52e45f81d26d269a9090aaa9fa6c87739065
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:34:52 +0000

    tip false previous truth trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
