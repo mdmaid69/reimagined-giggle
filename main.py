bdd8c1c2d0ded1dd8809c2ae1f3f9be7bec87e18
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:30:50 +0000

    take scrap nervous act friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
