29dfed2ba2a98198500269b324d87e84e03fec24
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:21:32 +0000

    hurry attempt each shape run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
