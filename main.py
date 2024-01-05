a5f1d106ee2e4106ee512331556b7741baf37887
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:24:19 +0000

    bite advise wait track accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_stack():
+        return collections.deque()
