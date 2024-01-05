9e345e84ea5dc98d4726843581363ac21b199d04
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:14:15 +0000

    separate concern activity effect bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import sys
+  def get_python_version():
+        return sys.version
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
