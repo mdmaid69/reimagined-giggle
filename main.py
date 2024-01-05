2a102dd708932650b81f5f8958a7a56a5117d002
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:13:20 +0000

    fasten slip model talkoutof expression

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
