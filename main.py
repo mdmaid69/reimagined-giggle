7ae3ad01da661b7c91452a21771e6dcd606a02ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:43:18 +0000

    need different ilovedicks trip company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
   def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
