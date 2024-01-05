b1f95722296f55c7b4ce2d689a090322d99f80f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:30:57 +0000

    underneath perform leg smell minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+print(os.getcwd())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
