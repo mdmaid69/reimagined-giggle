53792e568bf0a8ec61816ca774d195ef9c9cc9b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:15:02 +0000

    bin child read worry extremely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def find_max(numbers):
+        return max(numbers)
