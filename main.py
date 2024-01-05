d7d3de9cf06c57035ad419be44551a0995c24bd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:43:03 +0000

    round understand detail consider before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
