92ba88079f9f4915487eb635ce0beb8c27c0cb6f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:31:07 +0000

    wind seem bath begin reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
