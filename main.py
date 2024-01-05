6e6620dbe582fa55ba7fe65b102c53ac35be15a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:01:43 +0000

    all firm sick song consult

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+print(math.pi)
+import array
+def get_array_typecode(array):
+        return array.typecode
