dbeb445249ad9512e5543a4c503e7ee5efe2f7db
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:54:42 +0000

    stupid possibility habit wander left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
