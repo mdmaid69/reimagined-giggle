1e70e90b33641a0d3a8f6e6d0bbb7b90828ecd98
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:11 +0000

    profit ask money alive best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
