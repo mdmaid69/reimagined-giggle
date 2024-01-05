48db89746c6e627d3b0da0445161cf5a716530f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:41:28 +0000

    straight tough bet practical show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def reverse_list(lst):
+        return lst[::-1]
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
