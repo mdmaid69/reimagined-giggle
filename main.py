944cbb6d18c1d5e1a1a116f1f5157155f7d4d0ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:03:45 +0000

    Hh mine anxious want next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
