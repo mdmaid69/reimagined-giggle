ff63f661fc20f43e329b6cff9a1e62158961fedf
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:27:05 +0000

    hello together sticky practical drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
