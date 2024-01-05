6ec3710cb288e01a798ace9a5276845e7624a8fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:51:35 +0000

    different feeling hard refuse thorough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
