856c1b35ab0314792b8500092e46a4a27090f07b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:53:34 +0000

    finish ready life public form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_typecode(array):
+        return array.typecode
