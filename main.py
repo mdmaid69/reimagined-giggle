8a20dc0d94a40d3540e3b94041c2fe83bb643e28
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:36:06 +0000

    shopping yesterday OKorokay wave feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
