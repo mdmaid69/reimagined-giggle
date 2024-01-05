da56a7fe57e58c699322af77aac93d90468a6881
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:44:25 +0000

    attention watch event decision direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
