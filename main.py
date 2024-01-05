9f1e7942c1e8303b56c377c1c87d5f437541c005
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:31:01 +0000

    decide announce continue why accidentally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import collections
+def create_user_dict():
+        return collections.UserDict()
