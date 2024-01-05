ca1b22961227de0b507ff2c29097424103c4eea0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:15:57 +0000

    comedown slight little spite push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
