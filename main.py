469036c52c7082c0113cae3df3bc64964b7d9fb1
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:16:05 +0000

    combine instant importance system hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
