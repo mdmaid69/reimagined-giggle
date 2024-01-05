fb0464f652332022a402f23e9215b0a2e32531ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:29:58 +0000

    mother school further anywayanyhow wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_array_to_string(array):
+        return array.tostring()
