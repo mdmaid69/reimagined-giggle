b19aa546953086153a9c071f36c8691f069a3d64
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:15:46 +0000

    test gentle money relation offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
