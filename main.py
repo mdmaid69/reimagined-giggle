0f3184cf993e90a1ecd271dcc0283ea0fe741614
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:24:29 +0000

    none age power acceptable cover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def exit_program():
+        sys.exit()
