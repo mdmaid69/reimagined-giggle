d02ee539a9a781112c3b97c6427a4ddc8048a09e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:09:22 +0000

    live slightly model vision little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
