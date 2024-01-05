aa1e8aaf2fbe778759422e59b40f53d378135261
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:28:56 +0000

    threaten what case then disgusting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
