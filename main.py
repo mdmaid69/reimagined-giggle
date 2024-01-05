609bee1d3fa030ea78033e0674b440a18a89d0bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:06 +0000

    should willing cross disappear experiment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def divide_numbers(x, y):
-        return x / y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
