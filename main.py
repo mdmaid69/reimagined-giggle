fa4f72e03ec77124ccd2c79f4c4d6c4e68b631e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:11:27 +0000

    description awake manner whatever crowded

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
