361bd5bd364ca32f8f569791717a74e1a319c3f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:27:35 +0000

    mad rush nervous keepup shall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def log_message(message):
+        logging.info(message)
