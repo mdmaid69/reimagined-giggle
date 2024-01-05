93638fe8bd8a851491bcdf14eba5f08a715aeec8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:21 +0000

    practise behave rate sorry serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import time
-print(time.time())
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
