d97df961221ebc8cf83e37c92e3852b396eedcb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:27:56 +0000

    dark meaning worth concerning rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import random
+print(random.randint(0, 100))
