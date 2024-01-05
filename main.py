4d52e7a78a80bc2a7720d1d57d614417e5ffc5a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:26:15 +0000

    equal land together attach road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_int(array):
+        return int(array[0])
