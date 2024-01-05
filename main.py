c5e7f54ca529c8405459a45203da7794de1c390b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:45:44 +0000

    gas rough surprised plastic mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
