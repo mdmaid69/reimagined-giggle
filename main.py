156b0ebb98f49e06b9a577265225301f40c2e16e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:06:02 +0000

    fault every sad release keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
