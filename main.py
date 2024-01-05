5612b0a1bfe45033ef87e37dcfbf72ece5e52fa4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:31:34 +0000

    marry 564encourage other ago care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
