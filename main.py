95700b4fba1bf2d704d2acd7ee167520fbf67be1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:41:32 +0000

    doctor scene emphasize bread when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_item(array, i):
+        return array[i]
