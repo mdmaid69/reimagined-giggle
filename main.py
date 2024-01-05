2185c6ebfd7bcc88771ada566d47bdccbdd47d42
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:40:00 +0000

    main spring fall this right

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def is_odd(n):
+        return n % 2 != 0
