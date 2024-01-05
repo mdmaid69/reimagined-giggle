9d0605bbd4088aab705f36cd964933dbacc56da7
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:21:09 +0000

    principle hard achieve dare speak

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+print(sum(range(10)))
