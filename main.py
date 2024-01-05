74769c88442961df7f67ce9ec9af37d0edd4345c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:30:40 +0000

    touch system score tool breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
