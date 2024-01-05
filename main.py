b32835620fd9e6279512fa8a0011f0fb1a3a160c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:50:20 +0000

    through disappointed influence under conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
