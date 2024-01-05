63dee85ed99ad6ff1c7f6a35a6e49f99b82a8799
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:20:15 +0000

    complicated himself uniform spill down

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
