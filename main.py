a1a06c6d13e248673b8854263586a1911c93ca86
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:54:41 +0000

    floor deliver reaction test that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
