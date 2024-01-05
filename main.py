d26b80d7abf8a8d01d28456eb72cb783107a695d
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:34:40 +0000

    always suck left carry lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
