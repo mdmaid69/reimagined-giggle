cf9be50a2ee03396592a3e623815b4fbf18655e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:51:42 +0000

    unlikely slice share rest example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
