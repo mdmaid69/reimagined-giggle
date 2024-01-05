dcaed0cd7c7d880c582657138f0ba3b58daa61a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:14:37 +0000

    inquire hair sweep mention welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
