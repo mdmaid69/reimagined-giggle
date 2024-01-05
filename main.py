9c8e04bd27a0349e041b9c64496a08fe8f726c0d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:38:41 +0000

    wrap guess sensible thorough nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
