1bfc4e81c0a0701c341ae0371a24d4218f5718ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:50:41 +0000

    interruption fortunate draw problem worst

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
