1e0dbf51d9fb378b450010fc8b36a0af5ef3d92d
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:55:44 +0000

    swim treat murder ring declare

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
