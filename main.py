ceb393df3cf95732037f579f90528d65dbce2fc9
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:17:27 +0000

    run picture marry hungry pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
