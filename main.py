9ede4a52a0ac331633d56c27c7f02c61db7f9595
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:58:44 +0000

    juice urgent deliberate door muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def add_numbers(x, y):
-        return x + y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
