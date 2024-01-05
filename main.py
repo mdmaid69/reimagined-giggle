4ab5acd3bccdf30bfe48a831918c72b9b998976a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:16:29 +0000

    much climb so chance instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_as_str(array):
-        return str(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def sort_list(lst):
+        return sorted(lst)
