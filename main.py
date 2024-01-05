41747ddbced8f1976e359fbd7dae317b306944bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:07:08 +0000

    show compare send hope help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_repr(array):
+        return repr(array)
