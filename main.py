b7e9c75248bfe2964e28222ef0bb86bfccc512df
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:34:21 +0000

    feeling runinto contrast practise mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def get_array_as_bool(array):
+        return bool(array)
