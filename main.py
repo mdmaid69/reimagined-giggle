792e94747d3cea3297cafd412e66a3ed28d7c52c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:04:02 +0000

    powerful dream dig shopping loud

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import collections
+def create_user_list():
+        return collections.UserList()
