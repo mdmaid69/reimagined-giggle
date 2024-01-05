295bfbe3e86ee23babe97f15741143fda4bb9cb9
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:55:07 +0000

    agree march shopping pretend wipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def calculate_area_rectangle(l, w):
-        return l * w
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
