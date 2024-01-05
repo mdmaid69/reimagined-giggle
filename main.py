85043fecf1ab300a2c37c519e63ae694c5dc194c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:27:01 +0000

    grade crowd creature recognize she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
