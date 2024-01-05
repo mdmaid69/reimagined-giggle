0c79e7f52c21e13c3689bd634651ac83b9b9a1b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:43:45 +0000

    load send than independent break

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
