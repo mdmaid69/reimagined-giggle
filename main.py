b73fca258921414f0ca4bd0eee85905f256b785a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:15:51 +0000

    knowledge love either hour develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
