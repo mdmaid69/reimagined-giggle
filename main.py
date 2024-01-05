b647be43b2ebd7ad6968efbf7118b805317ae83c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:58:33 +0000

    wish mark related fit gate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
