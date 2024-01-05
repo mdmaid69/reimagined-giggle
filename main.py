d895831191b7de2ceb9b6141fbc2a55715f9fda3
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:06 +0000

    nigga lookaround my Ww couple

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_user_string():
+        return collections.UserString()
