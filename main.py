fd0d79b0b5bd39defaff2d661dfc8a7bd7b6c91a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:51:55 +0000

    pure getout realize surprised protection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_work(force, distance):
-        return force * distance
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
