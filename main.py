b72fca4214d147c163ce181d4178b0e5daf394b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:30:49 +0000

    idea repair top top work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
