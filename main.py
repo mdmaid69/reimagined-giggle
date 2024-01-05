fe615368a2eac62e236d2f9889d45bdbd50c25a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:15:30 +0000

    and reliable throw unit never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+for i in range(5):
+        print(i)
