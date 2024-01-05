ce252fa6e697c81830abb5286f481750325ae055
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:08:58 +0000

    and prove cope serve fall

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_length(array):
+        return len(array)
