123af72bbbdaab40bb117a59724bee4563d799a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:56:21 +0000

    like file protect beneath culture

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def add_numbers(a, b):
+        return a + b
