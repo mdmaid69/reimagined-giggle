0b9c1d351feac5c9b25af2dcb6f5ac392f13fbc5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:52:49 +0000

    interruption strange please breakup contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
