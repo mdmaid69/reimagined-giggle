ff82c8e9bde0fb93d0f0bafce60c3ab4ffdb5bee
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:41:59 +0000

    slightly suddenly hurt other separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
