46cfde33bde4c65fd4fb44b411073f79eb3968af
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:04:34 +0000

    children getoff he advice single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
