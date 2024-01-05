6a72bb42b810ac3050202a4c933f358b23fb69d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:52:53 +0000

    pick stare air back idea

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-print(sys.version)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
