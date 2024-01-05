a27767c768b89329f5f51d137e267759efa17231
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:03:59 +0000

    turnover some exchange patient between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
