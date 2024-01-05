c9715c657bf7a92699724dd9ce0fb219e0f6640a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:02:07 +0000

    bread or meaning cough touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
