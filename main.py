d04dcb5e4f55be6673b27843dbb305e06f52b98b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:35:20 +0000

    drawing reaction interesting minority thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
