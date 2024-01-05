3782565c295f19b0aecc17b39fa8591bb566757b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:07:51 +0000

    name obtain collect if confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
