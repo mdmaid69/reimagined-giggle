eb0c9535de9c854acc9b7d2f407f4df3a24eb200
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:09:42 +0000

    respect judge closed scheme pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_int(array):
+        return int(array[0])
