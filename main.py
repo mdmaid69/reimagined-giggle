0bb3e6f84aab8f6bd50afa768b5527fceef1cad6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:38:52 +0000

    delight cool freeze thought charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
