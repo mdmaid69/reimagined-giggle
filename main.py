73817aa370842b13644d65fd004cd427618f57c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:37:49 +0000

    delicate sticky suggestion play after

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def convert_to_binary(n):
+        return bin(n)
