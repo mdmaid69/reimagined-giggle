5805305197c976762e6574a43a40ccaab3e20e8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:38:49 +0000

    declaration all name funny like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
