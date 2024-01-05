73abdbae5c77be8f3b4f90a4878931f145184f4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:13:54 +0000

    we light hurry block entry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_directory_name(path):
+        return os.path.dirname(path)
