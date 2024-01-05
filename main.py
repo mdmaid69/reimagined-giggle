b7a3fd81f847e063c97b72b6273a635c80c8330b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:51:26 +0000

    die trick give name window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_as_list(array):
+        return list(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
