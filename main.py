7f3ec4810196cc011a0fc6cd69068febc303ee9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:52:40 +0000

    race plenty neither run write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
