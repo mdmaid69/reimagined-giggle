be92713a5d425a14af08d3f48f314bb088918aa3
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:30:51 +0000

    business drive bit or bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def multiply_numbers(x, y):
+        return x * y
