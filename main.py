eef1a280e7b6e6d6247e6152d89c4f5afbbbb730
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:01:41 +0000

    straight takeaway doubt office Tt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
