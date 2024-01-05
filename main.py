6d4d7f50aef5ab64f7f69e15f9061331af23e345
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:13:47 +0000

    takeback interview empty satisfactory style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
