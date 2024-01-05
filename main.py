25b7277ff249e761fd499d9430a5ee5403488c23
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:30:57 +0000

    help thinkof rid keen combination

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_set(array):
+        return set(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
