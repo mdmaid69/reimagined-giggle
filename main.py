811c8b316db503eb851adb393f0c6d2f4f51a857
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:54:19 +0000

    means naturally noisy protest floor

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
