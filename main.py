116313224e8f01f7dc14c1ce437f642d70a34416
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:36:13 +0000

    demonstrate fail board drawing enthusiastic

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
