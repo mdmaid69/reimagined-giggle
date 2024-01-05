1358d13dbc7b48f58e4f39386f72b464ac210535
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:32:12 +0000

    interfere pay consult anybody contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(numbers):
-        return min(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
