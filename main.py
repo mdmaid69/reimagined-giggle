336bb8a2f27bde43ecbdbcee5a0d3aff412df2fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:53:13 +0000

    importance hurry urgent thin wander

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
