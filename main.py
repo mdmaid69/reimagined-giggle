b8b641be0936fd35608bfb1a2575d4938c3d15fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:58:59 +0000

    whatever patient fortunately illness advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
