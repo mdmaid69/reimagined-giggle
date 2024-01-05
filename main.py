c6ed18f26ff7fb439edba51c1b8a14c01aea7bac
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:26:09 +0000

    statement warm vanish successful your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
