6350e40e344815b02523a3d9c7c09baf3796a565
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:09:06 +0000

    ring slightly loud lift fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
