48a26dd4d1dd35c1a43e235be32e18200d9f483d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:48:37 +0000

    interrupt get news Vv comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
