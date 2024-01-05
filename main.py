b7fdf088148c0c5fb2000921e10b7a6196b149ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:17:32 +0000

    connection improve race breakup raw

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
