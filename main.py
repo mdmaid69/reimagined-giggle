2ef65df1645dc673d3bb3c10f41c4479de1877b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:37:40 +0000

    hear business wise gather waste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
