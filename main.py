74d74a32c52a3e6dd6306f094fd89268b74a4445
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:53:40 +0000

    tear heart trap earth book

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
