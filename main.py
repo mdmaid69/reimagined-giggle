79896d0b0f5adbb262ab1684e93848002cffd359
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:42:27 +0000

    repair clear truth length turnback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
