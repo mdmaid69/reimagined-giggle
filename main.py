8a129f5330d45bdee4432fdde0afe7cd1c02a0b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:11:44 +0000

    connected approve practise office mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
