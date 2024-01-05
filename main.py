2ab666665fef68d0441e8f1c584458197a6debba
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:57:50 +0000

    OKorokay before disagree snow there

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def find_max(lst):
+        return max(lst)
