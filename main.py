f63e7edcc18b66979e95a81e20a0c1e1ac011f3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:15:20 +0000

    tell must stretch minimum study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_square_root(x):
+        return math.sqrt(x)
