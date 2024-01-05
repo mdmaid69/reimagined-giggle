e5a03be092eeab4049140487fd9411c51a60b1c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:56:33 +0000

    steady spray across Kk 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
