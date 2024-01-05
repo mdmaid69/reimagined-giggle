e884a713efc296664d7796c000b18feb7dd06934
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:57:48 +0000

    bread complaint second summer thumb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
