f2895d48d388dfc0e2d49d6e3fae914711fe1adf
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:27:53 +0000

    sweep agreement nothing influence forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
