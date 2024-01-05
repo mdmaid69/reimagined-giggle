369bde5175a23327e2259cfe3a114da41dfc68d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:29:16 +0000

    pool worth across process get

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Square numbers:", [x**2 for x in range(n)])
