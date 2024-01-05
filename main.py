c4ee577a37c6d5d3c3139317df4cd538eca62630
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:25:31 +0000

    middle compare I pause set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
