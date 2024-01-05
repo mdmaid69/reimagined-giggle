8994b42700b8d1072a33b8813d3ba3f233b954c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:20:13 +0000

    measure place sticky ride by

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_str(array):
+        return str(array)
