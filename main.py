531a7924960c6f84d95f1f804372bef2e891a562
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:47:02 +0000

    and claim excuse opposite science

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
