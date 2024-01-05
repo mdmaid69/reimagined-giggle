013afc1e408c3a9a0e161347df7fc2e328882b9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:38:13 +0000

    ago sign duty equipment ours

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Hello, world!")
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def reverse_list(lst):
+        return lst[::-1]
+import numpy as np
+print(np.array([1, 2, 3]))
