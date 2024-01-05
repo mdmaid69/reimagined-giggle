7ceacc7390df9b53d720f78d751082d1a57d1027
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:09:03 +0000

    increase pencil smart once but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-name = "Python"
-print("Hello,", name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
