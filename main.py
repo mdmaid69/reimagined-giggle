1f554fae4a9a32309f81dcd9814e18a4bdd0542e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:06 +0000

    pleasant divide comeoff cope wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
