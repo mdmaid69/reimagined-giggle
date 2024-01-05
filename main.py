8a42f90a98cc1172c18767aebe5075dd29f87a9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:12:36 +0000

    discover below will instruction kind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
