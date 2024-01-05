b938cea206ef080bb725d02ea52dc178eb1c2b45
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:52:06 +0000

    join stand give standard unkind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
