2c3a5f93f4ec9eb3cf4d2a9b54e195f5c7e96841
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:49:45 +0000

    pressure stay class comeup murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
