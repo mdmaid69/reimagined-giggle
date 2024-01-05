855961b7b612b533ab54cf4e34ab5248c6b52486
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:31:14 +0000

    steep software snow bother once

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
