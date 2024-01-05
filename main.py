a015437bb14dfd8137b657d486cfb24221b6eabb
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:21:40 +0000

    disturbing cope least pressure record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
