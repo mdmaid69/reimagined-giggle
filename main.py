c474588eedb2ffebfe21a9152e757430ee5ede34
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:15:59 +0000

    runover destroy complete vanish fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
