1844218c8608608c6f009c2f368d5fddfb680e49
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:58:39 +0000

    on wash cruelty respect 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
