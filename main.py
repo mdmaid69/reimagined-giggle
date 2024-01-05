78d3a90bc907747062861ca711d9558b8760a293
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:37:23 +0000

    woman I estimate fence ever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def split_path(path):
+        return os.path.split(path)
