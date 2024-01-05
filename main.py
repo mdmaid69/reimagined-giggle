4e811171769a062d031c188c01714164d8c5693b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:44:25 +0000

    unkind female steer twist temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
