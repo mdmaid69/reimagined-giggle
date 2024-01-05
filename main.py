92f3bf7f9fc2db0e9440e32e7da1fd8ec1bab64d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:16:23 +0000

    bottom talkinto lesson announce capital

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def clear_array(array):
+        array *= 0
