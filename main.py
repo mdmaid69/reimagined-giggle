fedcaa9a226c813c5e3c32b776433e231680d700
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:49:00 +0000

    recently centimetre sweet occasion statement

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
