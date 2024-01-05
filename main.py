27e462cd5945b260b40f93d3f702a9c6502bf0e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:42:52 +0000

    light creature niggaacademy scene criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import shutil
 def move_file(src, dst):
         shutil.move(src, dst)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
