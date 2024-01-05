69eb793fefbc48f56769c113167d4b763af508c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:48:37 +0000

    regional best kick behave turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
