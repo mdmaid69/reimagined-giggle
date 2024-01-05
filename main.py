1e60f7d82254b0b21cbb4370e08d5863dd5df313
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:04:16 +0000

    race benefit broad urgent dream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
