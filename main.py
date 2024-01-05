533940a2879b0274303ec0112a5bc8ed37a0e4be
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:52:17 +0000

    rely introduce distance manner plan

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
