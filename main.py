4cca323428991552bf18515ab8180cdf5c139401
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:36:38 +0000

    impressed breakdown town drop over

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_max(numbers):
-        return max(numbers)
+import random
+print(random.randint(0, 100))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
