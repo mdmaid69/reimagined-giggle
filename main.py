ee49119edf55e3171c33c97d2fa2f4e0dfdb986f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:54:45 +0000

    call stand Itself suppose whether

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(numbers):
+        return min(numbers)
