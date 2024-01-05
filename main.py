de8e3af4fdfc3d55843acf6cecaf09750a981a98
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:22:37 +0000

    priority unless team fall ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
