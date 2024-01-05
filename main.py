8da1d71debe7ac04e560ea8f7736e31a4fd86d18
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:09:37 +0000

    turnaway before invitation unkind machine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def count_words(sentence):
+        return len(sentence.split())
+def find_max(numbers):
+        return max(numbers)
