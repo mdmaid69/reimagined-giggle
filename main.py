8a16076474001e8a227fe659479efd9fed04ff6f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:17:14 +0000

    similar creature Ff party ability

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
