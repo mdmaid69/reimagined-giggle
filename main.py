aaede90c42e168fee3881a0a15ed170f763b423a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:47:33 +0000

    wish 572enter deliver trousers week

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_typecode(array):
+        return array.typecode
