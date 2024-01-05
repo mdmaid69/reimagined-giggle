0deca5ab1a26e5f791c1ee4eb33d2971418cdf3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:03:53 +0000

    question equipment effort mother win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
