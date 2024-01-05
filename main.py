116a11ee87b6154d6634c7e69fdfa896dd2466ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:10:46 +0000

    guide emergency post quality now

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_pressure(force, area):
+        return force / area
