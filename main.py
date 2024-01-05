6d62d05821eaa97104882d81c7b46e6a1bfa097e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:23:06 +0000

    short wave acceptable now target

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
