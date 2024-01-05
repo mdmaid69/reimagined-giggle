6774718d063840a39edcee97ee94a5da43be6488
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:54:12 +0000

    flight male circle unkind switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
