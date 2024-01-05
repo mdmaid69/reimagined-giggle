5bc1989ab9882d63b5402b4b991e9847c7e704e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:28:25 +0000

    natural contrast lost bath glad

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_to_binary(n):
+        return bin(n)
+from collections import Counter
+print(Counter("hello world"))
