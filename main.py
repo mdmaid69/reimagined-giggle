e975ca133b3fce8481d80594785f478b88964ad6
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:33:01 +0000

    part today sweet need twist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
