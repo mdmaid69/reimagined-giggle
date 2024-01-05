8248f9e19ffdccad41815e72c097efb7fe615b84
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:27:18 +0000

    price aim fine unfortunate peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
