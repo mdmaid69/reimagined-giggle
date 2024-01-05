728cbfcaed72fcbfee7f2270a42baf7c5a57fa3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:47:12 +0000

    remark bye thanks flavour sometimes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
