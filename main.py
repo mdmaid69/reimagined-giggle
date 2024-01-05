6ae3dd4c1e54cb2343a2a7e8d4d1332fc9ddcd58
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:17:25 +0000

    cry day protection scene delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
