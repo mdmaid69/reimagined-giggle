26b449b0ab298ad639dc9b8d65f698b702657d26
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:49:22 +0000

    circumstance sight store move tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
