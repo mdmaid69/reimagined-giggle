4ad29759acdf26b906c218b9a5777b0a4df99ebe
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:27:08 +0000

    reaction same theirs whoever design

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_speed(distance, time):
+        return distance / time
+def is_palindrome(s):
+        return s == s[::-1]
