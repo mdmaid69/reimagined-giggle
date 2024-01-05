f4ff1e41c7e54077afef5660320945057ebeeab1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:31:25 +0000

    close sign operation else radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
