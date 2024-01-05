369e4772241a53fbaf1056193ebf5918da6d3939
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:59:26 +0000

    total division lunch wall reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
