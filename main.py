b984794de552a6428bbed95176d21ab0197ccfa2
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:02:53 +0000

    powerful sure invitation float create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
