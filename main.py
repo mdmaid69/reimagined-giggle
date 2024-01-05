92fe1d44dd00a720d046dc83a79f3bd6fb538db1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:30:27 +0000

    port get type spring target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
