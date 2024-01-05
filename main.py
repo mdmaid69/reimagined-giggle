52ff17b2acd14234200292ce9675b24c72905b99
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:45:12 +0000

    destroy schedule remain agree today

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
