bc428bf7a35f03dd9cb0a27e769ca16e88e24a6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:41:28 +0000

    transport organization colour organize figure

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+print("Hello, world!")
