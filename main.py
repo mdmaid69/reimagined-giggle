14abe6a67fe934e136b21d9cd33924c4ba8724a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:27:41 +0000

    foreign politics your far prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
