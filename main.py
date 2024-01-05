e119e2aacc7517da74d7901f5f70841b59b568bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:45:01 +0000

    be board appear profit gift

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_power(work, time):
+        return work / time
