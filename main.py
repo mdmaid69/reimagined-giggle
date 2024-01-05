58a1d96bbc0cfc6cbe9088d8565c73e0e1e7f133
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:22:09 +0000

    dark thank someone neither join

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_as_list(array):
-        return list(array)
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
