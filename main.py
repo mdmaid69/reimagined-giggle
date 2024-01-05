abe1efda38168e7af734463980306076841a887c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:13:53 +0000

    ocean shine include yourself deep

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+for i in range(10): print(i)
+def add_numbers(x, y):
+        return x + y
