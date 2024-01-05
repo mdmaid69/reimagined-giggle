8a21aa75e3fab532540f5b92274265cedccdfbc8
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:08:04 +0000

    interested artificial involve language black

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import tensorflow as tf
-print(tf.__version__)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
