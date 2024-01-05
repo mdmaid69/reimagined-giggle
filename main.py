d1e875fa5578dc5823a289062827139701577977
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:05:19 +0000

    not 566end size gradually Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
