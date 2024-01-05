7d140d0ff9ed54310ef30184c8126ace9990b2e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:54:07 +0000

    preparation seriously no base me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import sys
+def print_python_version():
+        return sys.version
