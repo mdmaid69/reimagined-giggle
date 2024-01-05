6474c714b1d9b1d10b97c940f32a9398a9d0371b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:42:04 +0000

    practise proof real formal thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
