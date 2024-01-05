3a98e31807fa5f19a6ec1573e8cbfe7f17f4106f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:39:30 +0000

    unfortunate thanks correction alone opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
