513e0c190c15e04350a51ca75d062ddc222a683f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:23 +0000

    character human past present effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def greet(name):
+        print(f"Hello, {name}!")
