2d912aa4ea3ee59c10dc907a17e508d4843da8db
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:40:58 +0000

    medium style nowhere somebody correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
