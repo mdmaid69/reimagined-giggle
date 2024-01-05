6019b28a6ef30b8a10adae58da311f72dd7eca40
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:03 +0000

    getup wrong plant deserve centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
