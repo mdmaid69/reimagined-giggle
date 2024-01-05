3157ee1465d58d18cfd42a67e705faa6d3179693
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:14:18 +0000

    goon backwards giveback development remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
