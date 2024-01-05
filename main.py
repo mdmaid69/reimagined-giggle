8eb2d9a4754eceedc8507aad0a53e79acaa2c8d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:45:58 +0000

    test collection straight sure cruelty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def sort_list(lst):
-        return sorted(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import datetime
+print(datetime.datetime.now())
