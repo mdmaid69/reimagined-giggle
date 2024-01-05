353717e76ce759169b423a70fefdf0774c639b03
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:42:41 +0000

    every none talk whether spare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-for i in range(5):
-        print(i)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_power(work, time):
+        return work / time
