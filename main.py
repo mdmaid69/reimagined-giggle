11f18a0174b66a57a3a25db0ca6ac4d7177f8764
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:13 +0000

    conversation react further father trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def square_number(x):
+        return x**2
