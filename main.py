ab8f8c089728466a15c048614e496777d7651491
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:03:19 +0000

    central pleasure contact promise to

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-print(os.getcwd())
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
