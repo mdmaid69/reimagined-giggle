88d61fbcadae4f35f570f44b427f3eb36ae065f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:05:53 +0000

    another uniform build comeout exchange

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
