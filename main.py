5dfb9874f403af41a6a49e1645cd50f19cfadf64
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:28:40 +0000

    spread ride several check favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
