487d7310b4a96e838e6fa907fbbdd0fcf1690dba
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:42:10 +0000

    him computer distant how flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
