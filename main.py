c721c8770d974cfc3c5c1283dbf89599a617ae98
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:15:59 +0000

    workupto less in boring object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
