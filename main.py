a17ef3ec5efdbac068790e0531e55eaeff65cd53
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:31:04 +0000

    hard lot late disappointed keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
