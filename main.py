915d8aedc5c1f24c90fa259c05c1c3e98bbbcaae
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:42:52 +0000

    patient pen while 571enough warm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
