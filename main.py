705089b45a1e97c3a080dedbf0bc759d80cb3749
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:19:22 +0000

    imagine our wide fall defeat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
