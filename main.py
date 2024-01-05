fff6099c34e9228c2afc0819eb1edce8792b349c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:04:09 +0000

    runout sing rain ordinary paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_slice(array, i, j):
+        return array[i:j]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
