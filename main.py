0f31e0f34c60ba14a2aa614b805cc5c0e3562bbc
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:12 +0000

    original piece while press seat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
