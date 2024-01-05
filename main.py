185d7c99983bab2109e514ef24d6c18d850744f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:22:02 +0000

    ourselves wonder use sore upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
