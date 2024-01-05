1fca63b0edde29be023deda8d7ffbe3ff1d3f14a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:58:08 +0000

    picture backwards workout boring allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+print([x**2 for x in range(10)])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
