324243e12be2da011a8b04ae91f15814679eaf9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:45:05 +0000

    on handle message himself drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import getpass
+def get_username():
+        return getpass.getuser()
