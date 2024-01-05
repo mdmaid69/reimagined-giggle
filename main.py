d26381faad5b585ddc23e220590e17523fd3a2e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:23:15 +0000

    smooth introduce science dream emergency

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
