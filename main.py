d1debd4a43339c3a2a1790c7ce324db86d30ffcc
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:02:18 +0000

    middle try laugh course compete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
