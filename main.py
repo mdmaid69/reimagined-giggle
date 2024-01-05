25ff15ab52f46bddff9bb8c0a04179a98fba25f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:50:18 +0000

    ocean amount introduction tip shock

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 def factorial(n):
         if n == 0:
         return 1
         else:
         return n * factorial(n-1)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
