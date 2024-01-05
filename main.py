f89b4a0cd3604b178746d6e42eb40b97f3cab6e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:23:41 +0000

    underneath heart squeeze little exist

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
