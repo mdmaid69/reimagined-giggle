bf8963cad47cee8c9e5565bde78386736c04b05f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:29:00 +0000

    figure feed breakfast which communicate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
