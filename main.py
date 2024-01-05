b5de959f0ebad894c1f8e82c7d5c2b3fe05b63ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:31:04 +0000

    soft cause apparently target this

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+print(sum(range(10)))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
