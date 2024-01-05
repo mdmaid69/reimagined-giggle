b546e63635032c4416849439b13f5dcf9fe606c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:20:36 +0000

    yours have odd fork system

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import platform
+def get_os_info():
+        return platform.uname()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
