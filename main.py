baa7f076643e8aa40cc1559404cd71db95f91b5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:35:49 +0000

    kick sensible sticky steal scrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
