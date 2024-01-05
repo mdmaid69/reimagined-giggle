b312ddb0cc91aaaf9f77ff6ceb0d25f7fae5ba2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:15:19 +0000

    soon discovery Dd one shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
