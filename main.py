e377fa8ee4e171182d9f43186996fe306fcc5d80
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:40 +0000

    arrangement squeeze limit respect permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_length(array):
+        return len(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
