0c63373c3dc5b2adc2ee8af155efbad0e9c1692e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:54:33 +0000

    programme deep danger permission shape

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
