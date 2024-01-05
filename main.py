a8ac175c4ad0b042dadc6968429bba35fd2b9729
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:35:10 +0000

    responsible manage leave far breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
