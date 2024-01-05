995a4b026e8b34d7deaf6fd0040b88cd8b9eef33
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:43:53 +0000

    nigga turn patient right needle

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def sort_numbers(numbers):
+        return sorted(numbers)
