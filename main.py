cc1f9eba120f40c14033e5a1147f87f67cb73f1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:18:02 +0000

    disapprove pool offer sometimes wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import logging
+def log_message(message):
+        logging.info(message)
