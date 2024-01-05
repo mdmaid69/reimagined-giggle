fc18db5c5b471ebf4c47591eef811ca8131f1a94
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:20:01 +0000

    relative themselves lesson familiar formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
