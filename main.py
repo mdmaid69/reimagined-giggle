2371b24936ec855f41ff8d300449aa0031bd8e22
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:34:42 +0000

    together church us he downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
