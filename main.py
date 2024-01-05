516fd62b5f3cfde0c216e634b86849bd871e8ef4
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:40:06 +0000

    nice open open avoid effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
