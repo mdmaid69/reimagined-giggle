9b0f08c4de84f48bbbab98fbbf0775dbff71f31f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:53:04 +0000

    suit health this but pass

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_speed(distance, time):
+        return distance / time
