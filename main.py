fc2430f47af0dd5cc8efb9ea93354cb2bbbcf73d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:52:19 +0000

    round operate fierce private repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
