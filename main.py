0a28ccaa31a00f8f550b061f795ba46d6d21644c
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:22:02 +0000

    direct region pleasant tidy shadow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sys
-  def get_python_version():
-        return sys.version
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def convert_to_hex(n):
+        return hex(n)
