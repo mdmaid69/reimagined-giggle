15d0b33236fff18377000169a78be40827b3084c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:39:23 +0000

    right close previous quick fashion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
