15e991b221e41a86c54428c92a76a7d031c2d07c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:35:43 +0000

    creature Qq find Tt think

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
