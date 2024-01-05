240df246c1c2bfbc38c0c1fd5f0e1ab94990f294
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:43:08 +0000

    comefrom mixture plate holdup brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
