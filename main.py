e04301116c2e89f600105660cd8530aed19eb4a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:34:08 +0000

    today brief rain negative stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+for i in range(10): print(i)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
