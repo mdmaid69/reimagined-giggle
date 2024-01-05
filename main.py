35dd24e0266ece76ea0ef65b6957ab9fae79b37c
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:07:45 +0000

    fetch city reduction vote prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
