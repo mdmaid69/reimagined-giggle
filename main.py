c0eaf974abece2e48574f91c362fb4e49a1c9040
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:09:42 +0000

    progress honest steady wrong every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
