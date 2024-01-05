4a6dd470cc5903f12275c3dfc619bb81a5c6b3e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:03:34 +0000

    Ww nose comment lie flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
