c51bcf69138e0cfdf45880fddf6f706442126a8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:34:38 +0000

    sort name tonight where keep

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
