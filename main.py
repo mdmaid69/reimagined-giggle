9838383dbfaa1ef1589310188d52dd33e8531730
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:26:15 +0000

    period cheque soft judge because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
