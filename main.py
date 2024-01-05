ee067cda50ca77439e0e01476a96ba39cb28700f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:53:39 +0000

    that minimum fact design take

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
