c68f1e9942d077c1b47ed11969558a119b9467a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:07:28 +0000

    turn thing petrol comment strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
