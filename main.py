3a5a65b3f1d13aa81f5a8df50c8bf09f0d729ff2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:00:31 +0000

    trick more meet Mm pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def sort_list(lst):
+        return sorted(lst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
