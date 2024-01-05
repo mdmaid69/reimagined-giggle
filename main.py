cdbfedb615d33f87164aec075d41e09ef2c33a7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:27:57 +0000

    guilty reject community forward stir

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
