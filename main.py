5d71864b74cc3d11b8ba94261d9b72cd007185d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:23:05 +0000

    apologizeapologise right pure suit offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
