4490db2a8a33af6a4e7a3cfe6dfc2ac7a3673618
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:52 +0000

    base live react strength chief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def reverse_list(lst):
+        return lst[::-1]
