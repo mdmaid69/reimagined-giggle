5b1cf70015fbac1185c9f3810e0fcbf30be188f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:06:38 +0000

    rough strike process machine kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def reverse_list(lst):
+        return lst[::-1]
