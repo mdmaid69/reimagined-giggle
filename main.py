638ca13b6af164144dfedffde30125a94576f4ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:39:36 +0000

    arrangement mend tip prove steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
