b37b343ff99be8bd33643590deb0b37026fd4378
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:51:45 +0000

    clothes pool worth properly colour

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import collections
+def create_stack():
+        return collections.deque()
