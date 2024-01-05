2380db44daa8c4a82589881155f364018b716818
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:31:05 +0000

    surprise another hit bad cost

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
