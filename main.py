1df76eb3385326683d4382facca04b22dab0c6fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:19:04 +0000

    slight suit handle report file

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_acceleration(speed, time):
-        return speed / time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
