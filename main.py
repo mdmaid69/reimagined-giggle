d91eb0a1e532a21287aeb00d5d3aaae2fa89fbae
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:34:53 +0000

    speech so cheat respect knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
