919b83779f1395e3f7662f3a31cbcea2636980ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:12:34 +0000

    even common discussion chief share

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import time
+def get_time_since_epoch():
+        return time.time()
