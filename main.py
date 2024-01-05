66066e84449404882ce29be63cb9feace1c8aa99
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:57:22 +0000

    sore this load cheque difference

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
