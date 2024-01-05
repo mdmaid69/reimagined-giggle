c20566cf364a552ad2085999f5302bbba250a535
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:44:18 +0000

    pipe butter tax chance height

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
