c268d830ef7fe8fd5b4fb51f0907e9f5abe01733
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:27:23 +0000

    attractive force collection practise occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import datetime
+def get_today_date():
+        return datetime.date.today()
