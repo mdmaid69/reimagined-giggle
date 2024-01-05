64619d126d5ad0a62490c2dc5cd6db287f0e8700
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:39 +0000

    draw onto last variety paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_rectangle(l, w):
-        return l * w
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
