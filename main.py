62800f74faa1882f6e80b068e7fa3797368fe1b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:37:25 +0000

    crowded spoil abroad string light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
