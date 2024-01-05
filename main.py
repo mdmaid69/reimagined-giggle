05befc76afd4c2b8b79e034aeca5ee6c48a6f1a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:03:02 +0000

    occasion new computer matter store

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
