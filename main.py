9dd033805a32bc214ab05e0e69772a74d2638c6c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:12:46 +0000

    fasten degree creature arrival complete

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
