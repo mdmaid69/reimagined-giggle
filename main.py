2332a9988a4781ac7c433ba8491acaa6b6768a01
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:01:39 +0000

    anything desperate genuine same custom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
