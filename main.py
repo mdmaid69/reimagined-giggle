ff88c3860b0bf300a02cc2f1903dd5caaa66f384
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:16:48 +0000

    basis ball floor keepout nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
