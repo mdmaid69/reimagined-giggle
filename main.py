667bfa8dbe4ff6a3bdcef78054deb1750aebde9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:15:54 +0000

    means guess favourite narrow idea

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import numpy as np
-print(np.array([1, 2, 3]))
+for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
