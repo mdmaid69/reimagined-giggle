04321cfa78431a022a9ce2f585aa0abd673388a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:42:24 +0000

    fast increase trust left fall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
