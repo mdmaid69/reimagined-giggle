d1f6b7c24a7fc1b2c950684959b7bb132a1273e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:43:04 +0000

    shopping rush system key recovery

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
