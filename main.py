f89351ccb0bc4066b7aa355d018581127de69eda
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:22:29 +0000

    record climb grade upset receive

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
