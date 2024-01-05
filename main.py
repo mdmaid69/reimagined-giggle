563f8f128a83b62fd0f2c4463fc4239bfb0515b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:18:57 +0000

    interview flat partner pay meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
