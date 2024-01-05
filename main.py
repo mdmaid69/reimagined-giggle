bc4ed653ba06a9416013f1459e0d6a2ee85ae5c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:55:48 +0000

    support him chief dream repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
