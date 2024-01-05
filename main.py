eddec299f83b7bab145b8442b08ebe2785b4c985
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:12:08 +0000

    trip himself I risk something

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def count_elements(lst):
+        return len(lst)
