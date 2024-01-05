eb1d2feb8b2e3e60af6cfab8246ffa2b25a5ee24
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:01:52 +0000

    laugh theirs valuable lost fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
