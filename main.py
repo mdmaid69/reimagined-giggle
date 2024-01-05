e7d15b254c7b0227dae7dffbd33863f7facffcc3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:09:05 +0000

    old curious home excuse interesting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
