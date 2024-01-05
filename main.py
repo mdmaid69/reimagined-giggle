ad937b5ff60f42fc444a68a43400d1b6147c7b42
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:24:51 +0000

    this Itself stiff shade major

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_speed(distance, time):
-        return distance / time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
