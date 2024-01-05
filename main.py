0c0277d6b3c4e271132875fe170bdcfcbd47edef
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:13:17 +0000

    nervous deliberate feature key reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
