d4c16b3900ce33c0c170188d55000efb469d26e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:07:37 +0000

    when idea last comment goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
