4ec8534fe3b91e78b70ef7e99dce19dc9405830a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:05:23 +0000

    desperate doubt prevent 572enter concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
