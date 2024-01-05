444ea23fd53b49a9f0b88c753b0a910c5ce817de
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:11:41 +0000

    splash borrow us basic spread

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
