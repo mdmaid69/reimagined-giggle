e67b9492f0b8c9dc6461472310224b71ab3ee46b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:35:25 +0000

    surprising closely fall able file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+text = "Hello, world!"
+print("Uppercase:", text.upper())
