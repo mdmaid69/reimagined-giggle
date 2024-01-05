a8cb4a797910de220362d03e822031afb64d329d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:19:05 +0000

    study grand blue possible frequent

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
