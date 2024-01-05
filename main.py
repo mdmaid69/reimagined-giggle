aee1a045f5f4b0ee086abf68c1fe55eb4405546f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:55:16 +0000

    also team getaway OKorokay lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_typecode(array):
+        return array.typecode
