2f370bc19b81c0c4a78dd131ef6aa530ad51fdc8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:28:49 +0000

    board father hole shrink copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_list_from_array(array):
+        return array.tolist()
