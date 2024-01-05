bf6877d113c235a6fd718f44261ea323969274e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:53:12 +0000

    relief honest extraordinary reach godown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
