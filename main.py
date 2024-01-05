769756ee36a7ce3420534c7556f61dd816e444ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:03:58 +0000

    pair congratulations bush see disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
