fb500dd09f86e7f49f20fba2e9c93ac820be5bbe
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:05:32 +0000

    play pronounce happy win minimum

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
