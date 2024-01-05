310b4b03639cde9025fb1f68d075d47fd6613e84
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:16:25 +0000

    nurse dish building maintain keepout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+print(math.pi)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
