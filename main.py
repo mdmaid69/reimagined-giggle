5b0d5a4c4b91106fb36ed204db88cea150470da1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:06:12 +0000

    public special plenty borrow clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
