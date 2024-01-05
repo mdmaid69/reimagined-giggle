1552a996439376d221d9bbc5fa3e201349ee6570
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:32:13 +0000

    keen much beginning score pin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
