1a852e9c4b19d9f2100516d966ba3403069a0e40
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:37:41 +0000

    share name confident maintain sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
+def sort_numbers(numbers):
+        return sorted(numbers)
