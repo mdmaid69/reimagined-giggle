f897a4774f5734ca1f19aa3f9cba1fd7b932edf8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:30:44 +0000

    whoever view public approve already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def is_odd(n):
+        return n % 2 != 0
