adf18c0d20aa6cd5fa105521f4f4fd5988a9565e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:23:52 +0000

    stand influence catch month rely

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+for i in range(10): print(i)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
