5363c3395eeb9835a5be48d380c3dbf9694889da
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:48:33 +0000

    control responsibility respect heart back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
