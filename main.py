ab8fdda51afb7e095f0212f1971bdd20caf73885
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:31:58 +0000

    some trip division organize breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def add_numbers(x, y):
-        return x + y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
