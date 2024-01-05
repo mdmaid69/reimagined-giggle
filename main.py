d33006431e34967256a79af0a2224062112d2102
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:34:18 +0000

    bye person offer listen movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
