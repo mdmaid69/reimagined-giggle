99bd39869e32aa78a0ed8fc0cf23205bba608e29
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:02:04 +0000

    consult suck both wrong press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
