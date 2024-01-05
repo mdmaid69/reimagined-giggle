b07abe88556dfef35d0fc76b3c0ca29a3f6456f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:56:28 +0000

    well herself anyone Hh shout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
+def reverse_list(lst):
+        return lst[::-1]
