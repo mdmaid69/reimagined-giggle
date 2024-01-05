9cdb65803ed0f287e5ad8a048a13cafbe9cac1de
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:45:51 +0000

    smell state hope people exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
