2ef72bef780126b71a9dcbb48dcec5d650e621a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:17:20 +0000

    block spin search protect visit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
