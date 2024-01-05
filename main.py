8654dc65908d30df500f637ecfd3c8faf0369cd6
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:37:39 +0000

    awake consider responsibility acceptable even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
