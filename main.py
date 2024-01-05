6000ef11e071d13dfc6d9ffca80eacafb8edca1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:34:23 +0000

    follow interview admire increase form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_floor(x):
+        return math.floor(x)
