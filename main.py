438f19b56b4db6f1a688f640a066fb4fc499014f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:06:26 +0000

    question own live could foreign

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
