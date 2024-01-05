397fb64096370ca40b48180d752cdbfcd3fbf3f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:00 +0000

    fortunate fold responsibility runover storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
