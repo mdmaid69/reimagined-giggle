21ed7e64fb50ce9c2c911ef79fe090d336bf968a
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:21:57 +0000

    geton active basically interview wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
