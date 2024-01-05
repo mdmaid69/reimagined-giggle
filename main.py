59d66a2c9497e0c523b7e940ae8efe5f9cdbe87b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:06:48 +0000

    fire hurry bit window their

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_rectangle(l, w):
+        return l * w
