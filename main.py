b5b9ba6673430fbd4fb377b0700f1e88fd37c737
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:57:38 +0000

    communicate spend lean arrange needle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
