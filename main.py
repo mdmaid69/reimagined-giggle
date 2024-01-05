d41539b02a1ef0d6ae59368ef632d5e3deef7657
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:16:30 +0000

    sign mess general miss slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import random
+print(random.randint(0, 100))
