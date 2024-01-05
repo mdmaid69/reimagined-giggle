531e2baa0acedd1d4eaee90107166ae2f0e376a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:19:27 +0000

    hate shut performance tradition waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-print([x**2 for x in range(10)])
+import collections
+def create_counter():
+        return collections.Counter()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
