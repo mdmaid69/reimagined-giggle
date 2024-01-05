698b9cdcc6cf27a05867c9e204a1db1364b5c156
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:15:17 +0000

    brave clean luck often believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
