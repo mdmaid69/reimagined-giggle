fa6a250217c18510c588cb4db531c1d97861dd01
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:05:02 +0000

    compete tool either house brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def remove_duplicates(lst):
+        return list(set(lst))
