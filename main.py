1b5cec104a592d81bf8d9b5b281164059142bbc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:50:12 +0000

    organization adjective recommend unusual regular

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_exponential(x):
+        return math.exp(x)
