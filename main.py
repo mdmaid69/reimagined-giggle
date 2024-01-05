010b67556143c0ae79673ac23120ec00f843c921
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:40:47 +0000

    famous related task concerned squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
