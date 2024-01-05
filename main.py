0b853a39d0cd19997e04932eaf68c4cf3c602eb5
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:10:31 +0000

    serious train know impressed suck

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
