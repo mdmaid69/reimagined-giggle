092e9dc656975eb14a6eb64558569623625e3bbc
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:26:25 +0000

    love minor hardly mess welcome

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def add_numbers(x, y):
+        return x + y
