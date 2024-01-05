77aa1bd2cc22d18a3ce6ddd1ec70ea98e943eb61
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:48:26 +0000

    damp state free disappointed reserve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
