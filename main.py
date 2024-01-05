59f439064ac7b3c16d6b2a4ca05398905fd62737
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:59:33 +0000

    avoid time cheap star foot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def remove_duplicates(lst):
+        return list(set(lst))
