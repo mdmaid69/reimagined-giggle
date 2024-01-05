06d184c490d4951c5bb33ba7029ba4b0d49326d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:00:46 +0000

    concerning uniform sensitive lay comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
