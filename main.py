9bf45c07b55351b7a852c3c920dd4fd73d3e95d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:43:41 +0000

    open only rarely basis eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
