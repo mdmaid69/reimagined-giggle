7ce7aa1562bb80a5ff0936f1691111e44e98ec1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:11:06 +0000

    rest future father window heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
