b7be06db83c56f94253979943bcd561d39a81ac3
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:17:17 +0000

    later exactly cinema soak fond

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_volume(length, width, height):
+        return length * width * height
