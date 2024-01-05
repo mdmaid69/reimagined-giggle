44e449be4dc12e131b8d139b266ca74852773fa4
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:24:33 +0000

    criticize convinced many connected long

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
