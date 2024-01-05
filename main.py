5f5ab8f380838af73a9d6a16d8348b0f1cdceafd
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:42:36 +0000

    scare shallow appear stage better

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
