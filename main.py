7de78658c138ec1a95ebc5ac3a866611cef4f747
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:15:37 +0000

    frequent listen sweep mind contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
