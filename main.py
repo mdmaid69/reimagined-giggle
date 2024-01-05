d759b30220c99681d627085929190c26f10aae32
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:45:06 +0000

    line trap serve 567enemy bite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def count_words(sentence):
+        return len(sentence.split())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
