a2de33db9e4c83467f9606fd228363a3cc3d4aaf
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:02:01 +0000

    convince exactly deserve affect sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
