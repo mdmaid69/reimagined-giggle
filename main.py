3731f099ba326ea45e254292a2b49a7737ef58f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:37:19 +0000

    sentence smile speech smile suit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
