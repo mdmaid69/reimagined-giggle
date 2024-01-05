07a1d2fd5c84483730efd1c0d029fa9eabd9a5e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:39:50 +0000

    wave place rare vote there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
