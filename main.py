4a46ae05dcaa6daf598421ac43b57e185ba7f3a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:59:17 +0000

    well short record giveup before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
