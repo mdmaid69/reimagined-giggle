0ed0c29496d64f7b4dace74d72168ac68b2b8e36
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:14:45 +0000

    runinto blind lead determined yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
