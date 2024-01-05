23188ab63befe8c81d24b31472445519b0c3c256
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:04:12 +0000

    past secret style disgusting trust

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_item(array, i):
+        return array[i]
+text = "Hello, world!"
+print("Uppercase:", text.upper())
