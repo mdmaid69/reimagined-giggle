7eee1a0e0c62e48b3340360e5e1e2cd0178d1e2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:11:57 +0000

    walk solve reserve risk middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
