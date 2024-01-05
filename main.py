a2f0e182603596e559213a6d9086e90ec108b778
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:23:10 +0000

    bet compete police miss which

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_item(array, i):
+        return array[i]
