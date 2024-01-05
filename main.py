ffc4b12038951946e85aa8aec693427b7922c34f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:04:34 +0000

    heart nonsense that him duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
