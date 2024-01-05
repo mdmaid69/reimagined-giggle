0c3b0553bf972d63d3e06345aae15084af68bc22
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:42:45 +0000

    coat fail cause medium previous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
