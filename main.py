a3e34c1145bc72a269a60d2ea46c5511cd59f8ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:17:13 +0000

    lower doubt disagree raise list

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
