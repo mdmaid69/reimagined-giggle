1437ee9af25710886a1466642d6dc57a906893f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:09:27 +0000

    react inside our effect buy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
