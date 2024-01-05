89347b5ce64c0e9a1a79c79798b23e5a5a808207
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:29:09 +0000

    call mainly sensitive generally arm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
