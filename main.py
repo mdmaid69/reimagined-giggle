c8e2747f9e7e9210a9aea28fff3f6dced79be8d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:03:17 +0000

    friend control unfortunate settle large

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+print(math.pi)
+import collections
+def create_stack():
+        return collections.deque()
