2660df28fcc7e1c88ab385f8fd8d03d4896596a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:41:49 +0000

    file talkinto independent exaggerate fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
