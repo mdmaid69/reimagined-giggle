73baa8a71107d4897e249476dd7eb5a76bd9d346
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:53:57 +0000

    train difficult crowd fierce loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
