a4a3c36274ef73d9296e404b62423d541a6183d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:09:27 +0000

    see convince comfortable suppose regret

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
