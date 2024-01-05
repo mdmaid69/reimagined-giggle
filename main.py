5ea26d11e695564baa030e9026a37b4058cbc5e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:30:07 +0000

    stupid clear connect free upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
