ad4e8408b8caff322103250a3891eaa3e9f98320
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:08:13 +0000

    pair true could comment error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
