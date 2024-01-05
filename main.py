c653c4ba48edc13f98ee484a0d9f7dba6eded043
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:43:20 +0000

    calmly remain unable imagine people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def remove_directory(path):
+        os.rmdir(path)
