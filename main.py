6853d278bff9bf98bfdbd642beeb7d31b1e68ed6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:00:54 +0000

    contain greeting light involve very

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
