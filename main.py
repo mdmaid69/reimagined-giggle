8a980172fb95d603783d830cfe3da289a79ad75e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:22:20 +0000

    please really copy laugh properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_work(force, distance):
+        return force * distance
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
