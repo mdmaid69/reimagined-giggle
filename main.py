3dcad60a734fc82156d07b54ea8e79631697d9d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:09:23 +0000

    appeal range congratulate cope comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_base_name(path):
+        return os.path.basename(path)
+def is_palindrome(s):
+        return s == s[::-1]
