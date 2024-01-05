5109f088cf2310350a22985e87de5832beb21683
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:40:55 +0000

    clearly label before towards disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
