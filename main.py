eb2814264ea8de261c6a4b0f3896ee9921aae492
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:01:18 +0000

    regular pair great wealth ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
