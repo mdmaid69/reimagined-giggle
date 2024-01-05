876c346a16e1d8d128f0ec3eb90be9fe894013ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:59:35 +0000

    sell error middle record file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-import time
-print(time.time())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
