bb8836e5b8b953b8ebc3326b11e4c0646a61f014
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:11:42 +0000

    callup almost share Vv pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
