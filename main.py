ec50c13f0c29e2879e011f39a2380d10a90d14ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:36:18 +0000

    main child serious responsibility mine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
