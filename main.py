a336f27038c8d44939419646254b3ba4de1c94a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:04:22 +0000

    embarrassing comfort steady dry mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import matplotlib.pyplot as plt
   def plot_scatter_graph(x, y):
         plt.scatter(x, y)
