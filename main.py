f48b30517ca47aca02312341a8b4409a9e993037
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:56:58 +0000

    home whenever lead sudden appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
