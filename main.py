010cde32032bf210e81a49fc638ff90a7c4fa08e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:41:30 +0000

    your scene book shelter thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
