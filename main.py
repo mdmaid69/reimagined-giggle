048fd5c86c5e8265bb9cf00eeca76f44da2a3be9
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:26:38 +0000

    plate disappointed cheat this rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
