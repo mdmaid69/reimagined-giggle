65c6884885f164e91be18736e2f568a0934e7eff
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:52:41 +0000

    together manage knock at well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import re
+print(re.match("h.*o", "hello world"))
