689a1b401c398496785beecd1e9a988bf124dabd
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:55:57 +0000

    worst dance different squeeze check

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
