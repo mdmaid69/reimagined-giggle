3b77aa8f5e903ac53f77f924c0c4684bed83496a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:12:35 +0000

    capital size hope steady innocent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
