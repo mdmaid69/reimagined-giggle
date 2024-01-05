04eb80c3c8b96269dec9c9291027a4ecb2a180ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:56:27 +0000

    bet loud finish contribute time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+def log_message(message):
+        logging.info(message)
