1cc97eb208ac6773d492d0bc37467cc6603b3c01
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:39:54 +0000

    join wire list never floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def find_max(lst):
-        return max(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
