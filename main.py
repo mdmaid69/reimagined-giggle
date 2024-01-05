53202db185a30ed8c60eec3dc6bd7941e5e8ba07
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:52:13 +0000

    holiday negative decision gate nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
