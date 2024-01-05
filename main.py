9bd12e46687efbf43f9e21357b3dcd52f82c8eb6
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:13:49 +0000

    appear least play blame file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
