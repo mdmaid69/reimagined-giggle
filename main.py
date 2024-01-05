beed3f2a82281ab86e3289ff2eb73bb746c13445
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:32:59 +0000

    transport lead congratulations name shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
