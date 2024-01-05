ca525aea70d9e89258f0f279ad7a256f7a65f5b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:55:38 +0000

    central second book address cruelty

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def find_max(numbers):
+        return max(numbers)
