4d7f26d0ff6adca2cbf29dbf01075527f25d733c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:18:20 +0000

    collection family ilovedicks keepup whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
