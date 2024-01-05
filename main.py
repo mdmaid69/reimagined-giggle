6173e2a4506f2603ebcc2b8e0819d7ce5064b544
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:44 +0000

    professional single negative my remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def convert_to_hex(n):
-        return hex(n)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
