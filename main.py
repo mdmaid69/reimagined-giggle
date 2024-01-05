965b690cbcc646e125c0de834cac0d6133409028
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:29:02 +0000

    chance promise expect use visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
