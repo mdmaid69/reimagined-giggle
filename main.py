100a18aa0fa0bfe1bb7aa26476e6f43b31fb2647
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:35:51 +0000

    order shape refuse effort admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
