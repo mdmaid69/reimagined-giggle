21e0884be4ef4f7e3530731bb802582fb17bf93d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:11:13 +0000

    arrangement contribute across light compare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
