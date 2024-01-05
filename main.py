010f53e25ebc94cefbe8fb5ed8b7cf07b934f4e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:02:31 +0000

    worth mine future walk flow

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
