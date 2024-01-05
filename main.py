711ce524082e581e5d16e9f9b2028cb667df9b13
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:21 +0000

    accidental often sight breakup acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
