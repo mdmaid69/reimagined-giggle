3120724567b5405006b9bbad7eb23babf0f9d893
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:32:06 +0000

    instant importance refuse favour suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
