1dd085ba0c9c045ad9548bb0f04b251a2d398048
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:04:21 +0000

    tragedy sleep run limit still

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
