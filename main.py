133c2bf4ad2b85a0e06ecf4e460f8a9108c20972
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:42:27 +0000

    related hard nose work section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
