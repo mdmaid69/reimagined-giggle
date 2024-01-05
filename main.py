d9a6fa577abf6201f105223c9baa6bfea245033e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:57 +0000

    Pp slice underneath lie getback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
