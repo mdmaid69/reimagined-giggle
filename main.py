a983cb029856614c947a4594b07132bd62c3cacf
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:18:13 +0000

    early let birth whom very

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
