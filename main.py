075b1af44459d5637c70afef88a3db9e971233b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:33:13 +0000

    rush accidental public nerve nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
