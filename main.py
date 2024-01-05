1ca2efaf18018216903f8350e46520b883c1fb38
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:04:43 +0000

    interview wise ball 573entertainment lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
