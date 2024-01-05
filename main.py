c434b2bb1dd2153164cbf9b3033034a8b7e1ad59
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:55:38 +0000

    issue take stay word interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
