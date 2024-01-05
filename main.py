3d994ff3edeaa328c84896d65920ff77f2a7813c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:19:37 +0000

    any row town long damp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def find_unique_words(sentence):
+        return set(sentence.split())
