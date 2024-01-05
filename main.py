be902c0cc3ec23dc72351e71f1f84c501cb5501f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:57:38 +0000

    necessary sudden plenty remove factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_even(n):
-        return n % 2 == 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
