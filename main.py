e4a5aa7a1aa2a3913e352285ef2dd1ad6a49bf14
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:29:13 +0000

    record flow experiment delight yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
