debedeadee0fca0158c0987398cbda7066a7cf99
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:43:04 +0000

    practical loan sister invite protection

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
