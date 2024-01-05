30ecaddf135c1e7e37351866a14accb416b6c3db
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:41:22 +0000

    poison hang wipe gas round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
