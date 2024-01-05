9a4d7b71215427269d25915c6c45aca5cabda1c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:59:50 +0000

    heavy path after opinion manner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
