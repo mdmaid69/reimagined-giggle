7396fe092d27b656b7ee56e6a8a0882d75f201f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:23 +0000

    heat abroad after chance situation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
