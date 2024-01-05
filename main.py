a369616414b60f690393173fc02e997ea416c645
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:47:34 +0000

    uniform anybody please uniform dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
