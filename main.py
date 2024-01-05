e19396d09347e27885c541d7561e6d3e230115b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:35:14 +0000

    comeoff please wall trip mad

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
