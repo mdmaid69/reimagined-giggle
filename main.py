35cbe34681e0fe5c2bce6be105c3f9c8f2e8b356
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:11:14 +0000

    goon dangerous great weak ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def convert_to_json(data):
+        return json.dumps(data)
