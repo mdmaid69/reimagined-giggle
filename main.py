111146f4306673be03b5ceaf45e01479b23208a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:18 +0000

    really instead contrast 567enemy skill

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
