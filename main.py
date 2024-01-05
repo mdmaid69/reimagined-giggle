cbb3fe6e9ee6775b76b3178633f317de5a1551b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:46:07 +0000

    pull bottom rid split love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
