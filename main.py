fd24972d4f9ba216f5d8a5e2ea70abbc430fd5b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:36:17 +0000

    should section direct makeinto variety

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
