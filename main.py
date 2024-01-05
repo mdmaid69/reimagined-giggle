a73add1a65cb2aa9ffc3e615bff0f3b3cad1423e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:19:47 +0000

    contribute outside steam street bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
