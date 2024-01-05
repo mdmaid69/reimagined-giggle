2abfee306c991ff4cff34e73059a99d50bc507ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:30:45 +0000

    news protect generous slightly suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
