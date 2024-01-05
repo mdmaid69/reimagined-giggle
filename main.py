ee2ee2f1520399674dac6f3cccc7d7d690020ee4
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:08:52 +0000

    punish mess whenever bank spin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
