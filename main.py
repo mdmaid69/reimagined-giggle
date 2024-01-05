96aef7f711fdf5aca6fba208e4096a007aef4c7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:58 +0000

    wrong failure holiday official exact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
