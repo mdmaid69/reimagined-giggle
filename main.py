d724cdeaf536eab4d9b46cec0ee44acac3065e77
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:55:52 +0000

    either bored unkind port emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
