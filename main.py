bf600d5841c052eb43b617fee7437ab1807d1a40
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:29 +0000

    normal top way occasion ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
