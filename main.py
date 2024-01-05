7d633aafb8a8ef11ad2356891f971a42e23476f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:24:01 +0000

    statement custom several nowhere especially

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
