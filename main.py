bb896ad137ba9bbe8e05068b2d262bb9951cd7dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:23:22 +0000

    count Yy talkoutof town tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def convert_to_hex(n):
-        return hex(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
