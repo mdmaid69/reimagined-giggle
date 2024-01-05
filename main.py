3e4be923566d58222daff35129fa843ce52e8234
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:56:42 +0000

    pleasure cover exciting scare holiday

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
