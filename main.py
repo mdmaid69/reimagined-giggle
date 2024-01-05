6391facdf7b651539f36e8fc3cced948c0d9aa6f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:05:49 +0000

    566end weekend smile find sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
