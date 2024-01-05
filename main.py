673eb088a04a258466b1286596e1a433380b13dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:17:32 +0000

    amount attitude truth night meaning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
