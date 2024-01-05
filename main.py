a749754bbe993b28743f519bed809a09bd37166f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:35:22 +0000

    news odd shallow report stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
