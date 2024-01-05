de8de4b72c092189f3ebddaf5d98b6ae61ac1c43
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:33 +0000

    later yes plan deal comeround

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
