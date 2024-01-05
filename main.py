4db0b421bc25a2eec8aefb451d5fb7af592b3471
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:59:45 +0000

    whenever concern pump environment much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
