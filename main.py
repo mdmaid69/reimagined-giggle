bf02532c20161d4e7da2bad7d8a5125d918008ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:18:15 +0000

    contact ought will pin once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
