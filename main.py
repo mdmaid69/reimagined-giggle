bb865289615a60cbdafd717b4559979b5c950938
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:16:00 +0000

    last pollution still comeoff habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_length(array):
+        return len(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
