93fbc41c7d4acf5c20bad66a84abf3d2eecea27a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:38:43 +0000

    meet after plenty design search

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
