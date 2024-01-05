b9d31121956b92b8fc323928e68f79ac122dfb2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:35:38 +0000

    favour difference until another emphasis

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_index(array, item):
+        return array.index(item)
