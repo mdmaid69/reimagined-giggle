00beba7d257df7058383dc52db95f291b7c1651e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:27:11 +0000

    acceptable within loud conversation importance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
