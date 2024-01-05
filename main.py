6dbc2f96ed943ed7ff197fae6920a8480410c666
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:37:20 +0000

    practise high average difficulty else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_bool(array):
+        return bool(array)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
