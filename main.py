84e0838c791431f03c4d89523ca1016fdaa6e96c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:08:15 +0000

    feature gap operation emphasis bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
