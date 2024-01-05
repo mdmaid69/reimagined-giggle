360aac8ff7c97a24a02d1401f06eccf89e0bfb3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:54:24 +0000

    surprised out description standard question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
