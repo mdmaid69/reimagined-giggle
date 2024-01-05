de274ef66fcfc35794ee1d4783922842342ece90
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:53 +0000

    describe outside book scrap forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
