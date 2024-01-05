b15a6e6103f547d68ec842fd71abb924080bf967
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:15:56 +0000

    fix female patient godown busy

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def count_characters(sentence):
+        return len(sentence)
+print([x**2 for x in range(10)])
