98b2a7fb80dd570650b1948f38440cdc5140f7d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:53:43 +0000

    country past estimate extra relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
