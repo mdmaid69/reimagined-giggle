5fad9a074d652523925d6fc351fe925a6ae2e9df
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:20:07 +0000

    indeed 569enjoy shock copy information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
