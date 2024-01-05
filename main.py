0d9f1255b2af34cf2fad6974b7e186b70da8ca49
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:41:58 +0000

    schedule speak university how little

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def sort_numbers(numbers):
+        return sorted(numbers)
