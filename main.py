4119ad3a69ba1dbc1eff244c3e3a3ceb073a1049
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:56:38 +0000

    come pocket hungry borrow big

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
