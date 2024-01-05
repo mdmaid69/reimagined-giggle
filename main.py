813e7e1f1b68b449c083cd70f27329783be3b7ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:16:21 +0000

    target occasional mess temper spray

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def count_elements(lst):
-        return len(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
