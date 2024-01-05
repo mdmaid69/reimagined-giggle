dbf1dfb404d9b79febfde0947efed80f77ff0b52
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:49:44 +0000

    anywhere guilty raise lock different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
