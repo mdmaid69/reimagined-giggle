0876314106fb52aa1d2c05eb15732c94e845160e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:04 +0000

    Pp duty house charge war

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
