c93c6453d1c2b9fe96bf0f810ebf34c324b6791d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:21 +0000

    heart practice announce crazy own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import os
+def remove_directory(path):
+        os.rmdir(path)
