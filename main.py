895c721bbab142f2e44248f89ec3db643343c6a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:31:35 +0000

    bath information type teach accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
