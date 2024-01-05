ddff4f4c40cf4c6f09d29f3cebf2b16c3f21d356
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:54:46 +0000

    permission comfortable best Zz grade

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
