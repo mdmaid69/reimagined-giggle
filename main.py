5b2db75b46c9ad8b00e591433e44df2c8c2234fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:51:06 +0000

    petrol workout light chin sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
