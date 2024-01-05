ef5ea3a0f3498be899f7fb4fac51e31047b8d3a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:06:39 +0000

    criticism first back lean neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
