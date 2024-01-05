391691e7a07ab3c5261d34271d93f9de7ed18852
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:51:06 +0000

    before charge number brush speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
