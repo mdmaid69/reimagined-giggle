926bbd5963c6414ec2c123095ecdeedef3db9a28
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:15:02 +0000

    apologizeapologise careful exactly able rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
