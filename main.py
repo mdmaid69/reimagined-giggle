0a87cacc70108e574e27846f0b3cc9855d0f6092
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:23:04 +0000

    female cruel careful law be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_work(force, distance):
-        return force * distance
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
