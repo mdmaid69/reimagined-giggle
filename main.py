330dfc942ea1128b41f17f783a7777d8e0bc2cf3
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:06:17 +0000

    unit quality at spring opposite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
