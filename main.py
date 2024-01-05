7c4c3578fd04e62dc285a83949c45e2a019990f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:33:25 +0000

    try sensible bush tonight comeoff

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+print(math.pi)
+def find_unique_words(sentence):
+        return set(sentence.split())
