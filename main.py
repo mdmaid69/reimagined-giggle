3ef0bcd03c506e0fe38285da078cbdd6c6f17d68
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:43:33 +0000

    consist boring late turnaway lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
