f0748eb9bd53cab514d4d1e5089098c6505c14c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:18:20 +0000

    man attempt demand telephone breakout

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_max(lst):
-        return max(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
