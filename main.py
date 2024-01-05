62c6864257c13fa855127e564238401ad92b1d39
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:17:53 +0000

    slice next breakfast prefer your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_itemsize(array):
+        return array.itemsize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
