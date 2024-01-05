76771bd6a6c2adef475da9aae5ae016647ebe074
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:43:13 +0000

    pot string debt answer listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
