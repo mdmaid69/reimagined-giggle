fa548e009d692128df5f8a44a83ea963133eafc7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:54:12 +0000

    well Ii wash probably smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
