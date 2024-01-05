c861b057a3fcc25c1befe95af28b6369c9694f96
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:21:47 +0000

    record personal office genuine temper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import numpy as np
+print(np.array([1, 2, 3]))
