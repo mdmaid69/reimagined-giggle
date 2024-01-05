87998f03c43a9de642b642745b9aa5041d7b4ba6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:27:36 +0000

    demonstrate correct scheme interest search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
