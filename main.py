5f622a82469aba04100d6a443237781b6f6e9e40
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:56:57 +0000

    list characteristic mention pen chief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
