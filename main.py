69db0e4fcd5e2cf4efeac47d056c9221be5eeb50
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:01:50 +0000

    may moral nor sometimes loud

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_max(numbers):
-        return max(numbers)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
