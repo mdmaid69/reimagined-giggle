5333e48b9b14482289bd617f0898d5b31cc10d21
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:36:05 +0000

    cruelty grow during whatever certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_time_since_epoch():
+        return time.time()
