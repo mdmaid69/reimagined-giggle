86bcaa16c72a9b2e20caab13043bc269b6160268
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:53:09 +0000

    lock thanks warn because rank

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
