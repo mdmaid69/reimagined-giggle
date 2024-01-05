a9f05369ae0207b8e4aa798b35fe6a6feaf98947
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:54:08 +0000

    belief scare light appeal talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
