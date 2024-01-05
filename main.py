2d0583d7b1ca69c53da306dc9f1b46415f5743a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:27:30 +0000

    pattern lonely precise basis flow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import time
+print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
