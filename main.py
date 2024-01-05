0e891a74aeb2a3315c321c9c1d408ae4403ba0da
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:05 +0000

    left standard tread sweet mistake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def remove_duplicates(lst):
+        return list(set(lst))
