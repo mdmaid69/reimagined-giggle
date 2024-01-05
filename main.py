d3e215942231daefb43ba4a0ed80322b531c3108
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:13:34 +0000

    delicate turnover force art half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
