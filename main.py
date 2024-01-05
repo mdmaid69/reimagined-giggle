31ab2b10878ca92930fd9d2d3ad947e4fe220e19
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:51:51 +0000

    direct mix peaceful once holdon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
