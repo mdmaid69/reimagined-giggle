33ca2e7afba6dd93120fb4824dfd20223ef8b192
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:58:12 +0000

    tie forgive lose back various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
