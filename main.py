ce7f424675b5c6cc6056ad37d05d2953586a0d66
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:50:51 +0000

    measure forward separate contain value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
