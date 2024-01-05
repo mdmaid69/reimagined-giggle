a179f1684d5acedf09215eb8a6e21d01b2ec608c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:08:13 +0000

    last score picture return mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
