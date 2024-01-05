92e701536d7dee89d62bbfc13317f550b4f7b5af
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:58:30 +0000

    degree height pleasure lift ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
