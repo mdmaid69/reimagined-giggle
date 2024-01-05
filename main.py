af41f25e8ea028882fb250612ba00dc832331059
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:41:54 +0000

    excuse tend sudden fit recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
