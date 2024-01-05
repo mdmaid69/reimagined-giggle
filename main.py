2e606dc2a3a384e1c6d55c2ba896cfb17a4466ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:41:23 +0000

    hurt we lonely view exercise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
