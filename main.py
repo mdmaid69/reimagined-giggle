2d9c02674bec2f8fa02c9a0d368b12a7833e9771
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:23:22 +0000

    centre somehow pair except important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
