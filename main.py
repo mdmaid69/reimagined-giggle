3877e5f72ade68f5186852daef3acd9162bbd684
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:45:36 +0000

    light forever join thought official

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
