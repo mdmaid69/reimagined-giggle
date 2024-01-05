3862db41c9bf30431528ec1442ee5c83eb7e1730
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:38:27 +0000

    cope dangerous theirs invite window

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from collections import Counter
+print(Counter("hello world"))
