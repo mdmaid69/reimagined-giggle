f6800c04da67a43cb8fd4f82e6e4f14565b75063
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:59:30 +0000

    disagree teach crash reduce curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_work(force, distance):
+        return force * distance
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
