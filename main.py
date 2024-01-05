0218236e718e1103a4789aaee91a4051e1fe7339
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:07:00 +0000

    spin care negative central demonstrate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
