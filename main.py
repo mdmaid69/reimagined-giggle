6a729db830b8c1bfc9b0f28462add333cb6f0c6f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:12:49 +0000

    few holdon second probably book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
