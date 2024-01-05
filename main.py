ac5db5c5148dec320bcc75e1cdb5c2fe98f764bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:15:08 +0000

    pause union usually accordingto breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
