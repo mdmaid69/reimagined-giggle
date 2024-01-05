8631f73cea955aaf944de3120f8d6d974b69010d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:15:26 +0000

    suppose hope well throw unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def multiply_numbers(x, y):
+        return x * y
