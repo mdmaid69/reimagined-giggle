be7f2edb0b0004ad9f2293cb967a9059dfcb8d91
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:12:17 +0000

    complain I education bang exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
