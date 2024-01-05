e63ba6b976c3149777055317910c35f78586af8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:11:31 +0000

    unable bright city cheque serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import array
+def get_array_as_str(array):
+        return str(array)
