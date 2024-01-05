81dc57bcd5ea0c419edf8644315a403eb9cf213a
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:45:23 +0000

    worth feeling their fasten care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
