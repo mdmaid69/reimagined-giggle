ea2cfa3b281870b18527c50617e92fa847af9bfb
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:44:48 +0000

    dress need broad reach attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
