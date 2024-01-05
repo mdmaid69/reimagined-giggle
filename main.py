6abd8391752b87213c4ed07a175ace0206b20501
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:34:27 +0000

    search thirsty twist awake influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
