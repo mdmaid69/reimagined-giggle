37a675c46169155c95e29038e2b017033c8215a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:49 +0000

    flow no carry by kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
