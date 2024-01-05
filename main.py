2edcd7291d60e397b081d06aae8fec6a44594f4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:39:41 +0000

    trip least cheque time previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+print(sys.version)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
