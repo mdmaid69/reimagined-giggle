f3702a8d1d8a0e6fab42f532bb8e4ae9513800e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:51:10 +0000

    original remain rescue narrow importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
