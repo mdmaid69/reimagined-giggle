e5770221a44a3788f571380687555fc8fa87a532
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:43:56 +0000

    intelligent exaggerate Gg somewhere kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_length(array):
+        return len(array)
+  def convert_to_binary(n):
+        return bin(n)
