ce0b86c81c1d06fc217e98a2d3df219465b7b911
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:35:50 +0000

    change boat its already delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
