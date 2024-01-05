120c05bbdf0674f2fc8273428bcae15eef47953e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:57:33 +0000

    switch smile condition by turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
