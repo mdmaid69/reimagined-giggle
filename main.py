fe350c9686e3fbbd90107b89d94123b99a143eeb
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:29:11 +0000

    mind healthy improve settle conduct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
