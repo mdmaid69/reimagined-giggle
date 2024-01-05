cb58bb0e15739bfe63f1d4a380005bafe2b0fd94
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:54:41 +0000

    cum breakout wall probably brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
