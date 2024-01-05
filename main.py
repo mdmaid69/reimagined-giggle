65824fd12162412673d8e4f6cbeb05fdaa50626e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:17:06 +0000

    joke continue rare care morning

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
