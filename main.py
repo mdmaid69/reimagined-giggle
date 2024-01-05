ffc38632a4d73181777a3975e07adcb926bb5ed8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:57:35 +0000

    past under thorough friendly actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
