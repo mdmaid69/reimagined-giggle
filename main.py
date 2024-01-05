97fb68e016d3491b314c10eeb85c1a429df2adde
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:30:04 +0000

    unfortunate man prepare children bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
+import array
+def reverse_array(array):
+        array.reverse()
