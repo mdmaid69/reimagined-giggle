8cb25b2654a47120b85200f8e4664a911f8d534f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:55:37 +0000

    clue warn succeed cough wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
