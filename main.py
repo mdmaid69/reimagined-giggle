4dfdb0b7c31a9e9830f934d43d206376fa24f222
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:59:52 +0000

    music team clear activity admit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
