94a9da8c0f055759221f5bad45053d0ff15ac7c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:52:14 +0000

    she exaggerate performance create them

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_density(mass, volume):
-        return mass / volume
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
