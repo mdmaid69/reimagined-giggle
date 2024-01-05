543ce9c7b35c79d224c1545e01e5b7ef810de175
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:55:04 +0000

    part their teach ashamed style

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
