496edefcfe053f23f18dd89200f90ccb9781320d
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:45:27 +0000

    satisfied ear strange chin operation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
