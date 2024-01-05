51e9b11c89103b5bdb330a6cdd65d795e40dc5e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:18:43 +0000

    bring land pride argument ago

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
