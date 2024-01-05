7ca76001e17258139ac6341f271408527f92a500
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:26:11 +0000

    material charge branch post pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
