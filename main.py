0d57fbe15997538458582ce2f59da461795fcc2a
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:00:17 +0000

    kind open spring back belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
