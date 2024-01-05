0151c43f67cd088e17305665c5fe59eb4fd7eb51
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:02:19 +0000

    mind save quiet lookafter godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
