66c64121bde48ff0920668c999ada7b9e0ea58e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:03:35 +0000

    short mark otherwise manner create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
