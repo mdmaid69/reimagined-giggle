92eaaf1b45b1ec0571944c999cc6ca9ce275ff9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:12 +0000

    tape giveout bother process reject

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_power(work, time):
+        return work / time
