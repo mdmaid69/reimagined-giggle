e97bf452cbaafc49104f61c2d5d8bb0cbbc44050
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:05:00 +0000

    occasional uncomfortable lookup company base

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
