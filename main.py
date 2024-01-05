88f7ff1a24bc2d020a800d43133fa4915ba250c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:31:26 +0000

    juice reduce grateful regularly answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
