77adbfa10f921caa30542ec6b342de29f45d9ad3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:45:27 +0000

    most precise consult inquire discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
