0b872a47c884712ef49a8d529f73dc0a8d078787
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:04:43 +0000

    concern ourselves keepfrom road breakinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
