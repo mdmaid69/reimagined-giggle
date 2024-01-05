00c641cffc3a314c2679278c7d6c095872b08833
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:30:53 +0000

    weigh distant mistake correct fear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def change_working_directory(path):
+        os.chdir(path)
