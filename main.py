cff460edebdba4b0445385eb8e5fe6ff868d59a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:49:44 +0000

    close refuse disappear first especially

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
