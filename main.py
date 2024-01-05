07db0b198bffd704020a4620a99513b189210318
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:06 +0000

    favourite on suit Nn lend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import sys
+print(sys.version)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
