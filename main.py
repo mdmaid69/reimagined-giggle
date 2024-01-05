5f4eb44f2313347df48eb9c71c53483a3e73cff4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:55 +0000

    indeed run well exist can

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import datetime
+print(datetime.datetime.now())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
