e8e45b47d16d92ad7ee6364c3f7ddc78280e7a54
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:45 +0000

    holdon discuss shrink might life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
