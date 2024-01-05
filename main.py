84cfac79b95be9e866f8fbf94be6284fdc8e0baf
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:09:25 +0000

    quickly less confused union collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def convert_to_binary(n):
+        return bin(n)
