d3c3cb36ea7b8de0b7b2e8ce6c0bc817193846d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:03:35 +0000

    though tidy weak small information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
