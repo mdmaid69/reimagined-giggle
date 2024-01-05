6648b99210fa9ff73d2cc576142f81ba62df0c33
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:53:53 +0000

    past whether ago smell rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def count_elements(lst):
+        return len(lst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
