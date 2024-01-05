7ce7cc0017ad111f2341169fb53491159f9a8372
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:11:16 +0000

    approval perhaps constantly always cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
