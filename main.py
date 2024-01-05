224713c3b32c3400ee926bec2c40b3430f846c86
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:34:05 +0000

    backwards onto mouth telephone post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import os
+def change_working_directory(path):
+        os.chdir(path)
