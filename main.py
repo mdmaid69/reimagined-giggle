0e16a4a4e7025b2b7c07680641bb802062b0412d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:38:13 +0000

    always turnover move dare circumstance

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
