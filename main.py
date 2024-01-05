f34282be8e454812775041b24aa8fd6695d4b849
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:42 +0000

    safety block simple model constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
