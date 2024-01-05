7087b5ad9c1db64b939ef3a292a34fa8cd99a143
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:46:09 +0000

    by transparent careless pair behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import platform
+def get_os_info():
+        return platform.uname()
