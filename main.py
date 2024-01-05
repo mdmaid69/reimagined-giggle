79d8daa562bab7fc90e29ff25d6eec84a0ec13f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:25:16 +0000

    lend hot new train pollution

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_speed(distance, time):
-        return distance / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
