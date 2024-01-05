9114b9d4b930a878ace2b23d7a26c9776255b7d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:16:17 +0000

    world car bad Nn dream

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
