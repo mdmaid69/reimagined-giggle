e9b8ee29d2219e6b4da0f8f7c8a5c3c66074aa4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:09:06 +0000

    front possible brother influence sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import array
-def reverse_array(array):
-        array.reverse()
-import getpass
-def get_username():
-        return getpass.getuser()
+def get_array_as_repr(array):
+        return repr(array)
