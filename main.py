7b6e3cd1323ff75505551fb579017b3051bf2615
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:56:31 +0000

    free pen estimate recognize club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
