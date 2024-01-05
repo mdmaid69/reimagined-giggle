ad57674021e2da2d1736afdf5a2972c96ccf0232
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:36:54 +0000

    frequent mine container sure cinema

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def count_elements(lst):
-        return len(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
