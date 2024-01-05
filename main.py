ab843db25a0accc30ac7a3123e13f300da96d14f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:53:58 +0000

    target suit lonely relative crisis

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import time
-print(time.time())
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
