a9900161029f86c3e6b590d85f131c82712276fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:33:16 +0000

    wet score film correct lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
