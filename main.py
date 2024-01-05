f0f9efd30592879cb47ee9d3bf406db353a33e1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:57:45 +0000

    direction air swim connect proof

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_acceleration(speed, time):
+        return speed / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
