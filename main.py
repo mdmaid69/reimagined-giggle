4839350189b1e7048ba3234ddfa437f18fa5206e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:33:11 +0000

    match wealth description certain hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
