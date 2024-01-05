39bd9797fc234a1ec5853bbe3a4b5647232e9ae2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:04:09 +0000

    tune substance inquire catch other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
