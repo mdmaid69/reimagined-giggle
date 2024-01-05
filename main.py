1f91d0afb08f5375f9c84cfaea9df5929b45d333
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:59:00 +0000

    afternoon for true hand fine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
