c1198d3932361f7eb24b47141283b292a7c94a28
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:53:00 +0000

    continue stick other lost towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
