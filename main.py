ca2db42b88ff82498be919ae2dbd1de082d49069
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:18:56 +0000

    watch load Ss offer shopping

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-for i in range(10): print(i)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
