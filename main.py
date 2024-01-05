1b5ef757af60fb0e3efa90bb9035d5c63bfdb650
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:48:24 +0000

    mean clear nowhere provide whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
