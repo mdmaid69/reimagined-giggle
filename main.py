917c9dfcdcdaa9c6eb1fa6ea311449a0c645b691
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:21 +0000

    die accommodation congratulate tend tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
