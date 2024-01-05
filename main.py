3f8a019492cd1f855b537e95f215f7745363db24
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:52:58 +0000

    circumstance advise term difference spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import random
+print(random.randint(0, 100))
+import time
+def get_current_time():
+        return time.ctime()
