c36f5039453b35764410a1556fde31adb16c0320
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:34:17 +0000

    environment the tell correct wave

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
