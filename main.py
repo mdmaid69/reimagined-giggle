dae6b8aaad32b58ee8dc18763ab61e9b8be7e620
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:06:18 +0000

    lot cheque determined protest show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+def get_today_date():
+        return datetime.date.today()
