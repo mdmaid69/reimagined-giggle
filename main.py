dabaa5ad9b8e67b8631f6f4d27da147893d22369
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:22:27 +0000

    shine mistake mood of would

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
