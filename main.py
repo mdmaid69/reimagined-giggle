ddfbfea7bdac8cba2a30318cb93e784f793b0d91
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:17:17 +0000

    meet preparation him as display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
