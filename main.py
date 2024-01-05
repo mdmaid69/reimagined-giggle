0731fa3d603d9139f125f7aab58877d2bbc4b047
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:47:27 +0000

    her office politics criticize justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
