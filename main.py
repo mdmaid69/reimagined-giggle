2ce4a7e17b6da22ad619e11d083ea78b000ac22b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:49:45 +0000

    wheel fact Gg short sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
