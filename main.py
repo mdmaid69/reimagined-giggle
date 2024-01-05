4c0ea1aa5f499ee6668265f47ea5124706d32284
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:28:40 +0000

    sure dick regional favour low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
