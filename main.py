468a096679470ce7fac91250b9dc84f6c55b26f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:37 +0000

    emphasis exaggerate thank left water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def add_numbers(x, y):
+        return x + y
