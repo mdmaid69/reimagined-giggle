06bb763091f1841a3b48147a91409db4080a6702
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:10:01 +0000

    rob remain must calloff turnback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+text = "Hello, world!"
+print("Words:", len(text.split()))
