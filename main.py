de78dda3ebda679eb917ee025328eef44d966490
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:19:03 +0000

    generally classroom who charge instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
