32623b10c5916a3aebc2c56c44e4085b72669672
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:24:59 +0000

    limit notice coat take crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import sys
+print(sys.version)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
