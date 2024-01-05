d88710b3e8e7c7bf659cff33e198d0509e4b1e34
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:55:56 +0000

    pay spill out steal remain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
