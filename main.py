58a6da1114b015fca3f1e7b6637ebf08bdefd6dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:00:15 +0000

    mass second progress cause ourselves

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+def calculate_volume(length, width, height):
+        return length * width * height
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
