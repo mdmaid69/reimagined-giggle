79f62c8f31e66b7e928f95dac2efeee6d0a8ada8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:50 +0000

    sister deliberate ability crazy happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
