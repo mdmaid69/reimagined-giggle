352018d0332594187859809611a8cc45a75025e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:11:44 +0000

    willing suggestion important Tt software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
