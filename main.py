97cf1255870d5ad8f100b678908ebffc9ce7622c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:07:30 +0000

    petrol matter experiment string find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
