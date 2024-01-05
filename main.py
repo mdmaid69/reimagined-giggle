533b1630ec777b76b9dcda9eaf5d52d2683b5c56
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:32:44 +0000

    anybody uncertain flat pleasure maybe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
