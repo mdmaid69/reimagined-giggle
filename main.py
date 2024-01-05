2069a68c75932ae454ff7922affa3920bccae655
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:07:18 +0000

    crash perhaps natural character takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
