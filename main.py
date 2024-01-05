7bbe3ad97845f6524aadff457c6fa6ffdde5d4e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:58:12 +0000

    demonstrate comein arm peace otherwise

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def divide_numbers(x, y):
+        return x / y
