290dbb509ca3b987db3679f5700a08b76c378550
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:54:06 +0000

    choice shelter interesting thinkof continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
