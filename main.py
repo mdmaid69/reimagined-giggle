a4ec549c81e0b538115ad0fecb98b7d0e4b398c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:27:38 +0000

    find takeoff slide stamp awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import os
+def get_current_working_directory():
+        return os.getcwd()
