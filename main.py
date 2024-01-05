17248e308ca71bd8d75d116b00b37adb0312aa7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:27:18 +0000

    close work immediate wonder private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+print(time.time())
