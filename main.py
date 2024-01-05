6149948dca1c3f290582d63c458f2f4068688566
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:01:15 +0000

    human month cough demand increase

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
