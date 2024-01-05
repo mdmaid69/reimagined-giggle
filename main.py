7c7108b169add488a25a5059f4d2f97e57da543d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:55 +0000

    willing that failure stamp really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
