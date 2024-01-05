3f3e8c2b215191d3f9a76c6867c2cac80e98881c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:11:53 +0000

    occasional bored correct keep colour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
