ee1c39322b69191de601d120a61a85be44e19844
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:10:26 +0000

    main sorry choose impatient joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def get_array_as_complex(array):
+        return complex(array[0])
