3919558efd71663dcbd25ce3a898735bba508849
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:09:13 +0000

    left style manner water perfect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_as_set(array):
-        return set(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
