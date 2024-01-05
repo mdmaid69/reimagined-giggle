eef33d3f989ab7c4f3d7d5b1bfad8ba6245ea63b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:16 +0000

    confused murder bored flow charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
