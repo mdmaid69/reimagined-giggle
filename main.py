2816958fde94af90fab74e8453fb174422aaf35b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:32:30 +0000

    direction yourself rarely look experiment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
