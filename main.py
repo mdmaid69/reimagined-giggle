01faea15ee8d2aff09f6fd6cca990e707551cf78
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:55:20 +0000

    smile tape exchange effect temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
