00f179c5149c2a83939a9d300573074c1cdd147f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:51:19 +0000

    distance chin wise play conversation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
