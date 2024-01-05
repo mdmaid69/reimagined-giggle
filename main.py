6966271e51aa07fc86d5a22e4dbe228ad9cb4246
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:44:37 +0000

    action unit glass store sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
