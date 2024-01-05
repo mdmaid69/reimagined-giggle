5d617027c4f5c842e556dadd683782f90170f437
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:36:17 +0000

    but today bill yesterday sleep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
