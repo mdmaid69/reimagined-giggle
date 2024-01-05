9e4798d3534db29c0f8ade95f8dbeb25b2cfc1c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:26:03 +0000

    comeoff operate Ii soon practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
