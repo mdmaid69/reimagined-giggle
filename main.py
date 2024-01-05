3aef732e966e76afc23f9c492e86c450f767410e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:24:07 +0000

    favourite distant fetch guilty never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def reverse_string(s):
+        return s[::-1]
