bf14314237e957b3a0f63fa916bcb70958673477
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:03:31 +0000

    Ee forget well sure previous

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
