86f00cacdf53e356a33ebd0d412db244f7d74860
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:08:06 +0000

    mention careless common frighten respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
