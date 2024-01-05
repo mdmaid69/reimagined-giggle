b593bf5a4ab8c85a56fa04d3a8bd6da96c0a4e92
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:36:38 +0000

    protect prepare worried comfort catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import random
-print(random.randint(0, 100))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
