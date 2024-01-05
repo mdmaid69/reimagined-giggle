ff7d8c25672e2149b40e033a5ddb9aecffe3a526
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:05:40 +0000

    travel separate we sentence study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
