66be8511478f38a166b7f2123fc0081a069671e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:13:34 +0000

    lot talk extraordinary sensitive nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
