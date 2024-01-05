4ea3de156239a5019c8075bc6f1ff1c80f59d5fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:22:43 +0000

    573entertainment where recognize workout bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
