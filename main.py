5d50491cbfaea335b5649919397ce0f5cc144a19
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:55:02 +0000

    impossible grand runover round line

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
