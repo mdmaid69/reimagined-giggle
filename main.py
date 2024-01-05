212f8fb62ce869f2dc87724f7c8a694e593a9b4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:43:40 +0000

    divide town bush at feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def append_to_array(array, item):
+        array.append(item)
