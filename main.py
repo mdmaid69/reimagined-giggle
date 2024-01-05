f1ce7bcadbcf3f8395a62f1cc0adfd87c8b56613
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:20:43 +0000

    anywhere needle neither among backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
