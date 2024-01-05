eaaa390e26e7278fcd16e71ea7a2b4765455e294
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:57:15 +0000

    right something affect comeround talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import datetime
+def get_current_date():
+        return datetime.date.today()
