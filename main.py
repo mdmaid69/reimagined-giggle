64ee65c86db4d7661582654d0f04e2c4a3e465e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:06:47 +0000

    bin to any imagine ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
