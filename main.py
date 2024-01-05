ba3b37aecc27b7999c4c577b8851b6cdc65645b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:14:31 +0000

    breakin pile grateful far skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
