c599625cbe3bdb5dc331f815622d68d918ab431b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:34:47 +0000

    gradually cost brain male peaceful

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_to_octal(n):
+        return oct(n)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
