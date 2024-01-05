7bc9a52f4a81ffefaae4afe3d9b027923ef3af41
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:43:27 +0000

    choice use each tax takeoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
