0dce541fc025e1c7508ccec2a50f01fa0ee015ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:05:16 +0000

    mess dish 571enough entrance distant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
