6bb04207df29b6fdefb5e291677f2fbcea0ad011
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:05:38 +0000

    development exact during regular rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
