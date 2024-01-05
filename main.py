3f557dbc67c89d26b4a3f25b0490f163a7491f09
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:26:49 +0000

    dinner callup argument space talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
