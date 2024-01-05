27de76e4f349ab25a9928ee0340341b6c837a9c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:57:33 +0000

    clear hope pretty hurt train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
