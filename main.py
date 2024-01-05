3c60d964bc522e5a6638da079c05dcf91d03f1ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:52:55 +0000

    buy really on not person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def square_number(x):
-        return x**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
