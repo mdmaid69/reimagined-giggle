e705ca3a0547f70dac3000b15d107cf6f93d65ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:46:06 +0000

    worst terrible club explain powerful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
