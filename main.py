5c2958bce6dfee533bd07c7704ecd97baae4d26e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:14:50 +0000

    relationship extremely brave getaway arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_palindrome(s):
-        return s == s[::-1]
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
