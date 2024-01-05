ca53b9da5b6c4360d4a4bcb1574e450ca5a5c53c
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:48:36 +0000

    564encourage tear true makeinto necessary

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tensorflow as tf
-print(tf.__version__)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_volume(length, width, height):
+        return length * width * height
