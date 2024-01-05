3cde28586095b05e3ae40fee01fbc0b61f50cb42
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:56:59 +0000

    tough corner curious moon cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def remove_duplicates(lst):
-        return list(set(lst))
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
