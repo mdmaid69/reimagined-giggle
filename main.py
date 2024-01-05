3f04c280a3410520251507181737fa91b02a4884
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:50 +0000

    wipe occasion outside manage delicate

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
