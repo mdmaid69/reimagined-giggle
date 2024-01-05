f3e25554fc310bd39db9cfbd64b98b1ccc63ffaf
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:15:42 +0000

    split pronunciation crowd wind probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
