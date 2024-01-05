8d68b01534026550b2328a85e322f801f97eeabd
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:42:00 +0000

    row further uncertain watch second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
