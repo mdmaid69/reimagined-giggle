6dd9bd7acc0b3a80ab27001ca92f66910e90fb90
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:16:15 +0000

    human fortunately OKorokay draw goout

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
