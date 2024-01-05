1afde3393e2fbda28c9ab18e6fb6a175c614f5ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:10:30 +0000

    properly front keep go here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
