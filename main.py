551606060955ac9173d8ff4d6843680188650eb1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:51:01 +0000

    cold thick order job plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
