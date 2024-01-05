7ab7bd0ddec2202a3bf96efc9242f9d289850749
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:56:26 +0000

    event genuine suit exam result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
