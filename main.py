9d43a49847ce9d22c0e86b07874bfeeb351f2660
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:53:18 +0000

    foot possible practical welcome indeed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-print([x**2 for x in range(10)])
+import time
+print(time.time())
