4cc70552d24026db36113437b63e40317f4b25ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:26:57 +0000

    yesterday vote apply niggaacademy laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_int(array):
+        return int(array[0])
