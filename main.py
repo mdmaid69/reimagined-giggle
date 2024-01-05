07ba1a12bb32c5d98f3aa10f1379878036378b2a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:45:39 +0000

    scrap sale finish neither official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
