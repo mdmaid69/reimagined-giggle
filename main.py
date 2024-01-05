2ebac0d6f8a69de6161bb672439c55a7df326d9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:32:15 +0000

    callback do act meanwhile worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import platform
+def get_python_version():
+        return platform.python_version()
