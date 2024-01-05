d4910c14c4ab58f0582d61ddf5f3bfe2089d0f5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:21:12 +0000

    happy say once complete escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_directory_name(path):
+        return os.path.dirname(path)
