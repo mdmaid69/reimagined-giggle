859048c04ea94cc646831145172b03294a67c55d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:40:11 +0000

    limit town shame several pass

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
