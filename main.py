889e2ba3b4b40d6733b31f7b29666e3a010f7736
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:48:49 +0000

    child conclusion walk rarely deep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
