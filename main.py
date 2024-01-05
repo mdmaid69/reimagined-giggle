671cff4fa92e40334d0fc15db4e5c81e997b6132
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:14:16 +0000

    apply strike friend ever also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
