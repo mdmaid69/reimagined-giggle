f35ccfc106e39523312278bf5c82be5b9482c70a
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:25:11 +0000

    it belong attractive question bank

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_power(work, time):
+        return work / time
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
