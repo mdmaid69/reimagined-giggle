ef91864d3b3152ebb42f36f76a506a7a63554e67
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:31:25 +0000

    weather recently disappointed watch trade

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+print("Hello, world!")
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
