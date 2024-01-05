3b0429352d0238831a7b4f6e4b9e27d3ec29c293
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:17:44 +0000

    tragedy turndown yourself winter hurt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+print([x**2 for x in range(10)])
