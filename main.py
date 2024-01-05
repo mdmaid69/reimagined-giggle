0f9b262faf6b3d9aabc92c74f60900b0fe78060e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:15:37 +0000

    thick near where runover exist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
