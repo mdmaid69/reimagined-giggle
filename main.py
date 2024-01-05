6b9e1b0c78ad0b1d5424d434de2bdd2da76d323d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:28:27 +0000

    top lesson jump single goon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
