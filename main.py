f9819be4247902cf47e3109d0bb9c0697833a2bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:07:18 +0000

    mention depend vote afternoon company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_file(file_name):
+        os.remove(file_name)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
