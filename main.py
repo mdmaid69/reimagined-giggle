2d9d184d98d82386f5e5910ead81b693ace5912c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:16:34 +0000

    reply smooth minor central invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
