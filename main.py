db00b808286f5cadd103f2d70895ccf501ddaf52
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:25:01 +0000

    arrive send comeoff whistle awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
