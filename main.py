5e5e9ed20bcf9dcaa550721ff3349db066b720f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:26:27 +0000

    combination comment familiar arrange communicate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
