9053f866f7036f65997464f0ad33c6955923fff7
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:47:38 +0000

    watch quantity contact doubt watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_length(array):
-        return len(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
