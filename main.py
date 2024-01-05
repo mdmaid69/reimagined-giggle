c75e83be5f31573771d84863ca6cf76ff6672182
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:32:22 +0000

    most man backwards terrible climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
