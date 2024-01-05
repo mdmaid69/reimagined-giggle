cf3bcb9f27571bfeceb0fe1466eeb77509496f4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:36:55 +0000

    supply down remember yet collect

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
