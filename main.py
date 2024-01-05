fc56b36a80d4afb822214e81a19e4a62c81d4634
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:17:15 +0000

    swing address care class yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
