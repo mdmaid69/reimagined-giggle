f828d813556f00746b2144e518fffe7cde453e4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:30:50 +0000

    very suit closed load once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def count_elements(lst):
-        return len(lst)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def is_even(n):
+        return n % 2 == 0
