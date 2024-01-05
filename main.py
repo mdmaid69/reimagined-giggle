06c2e02bd54e5d5e6412aa8f20baed4169455a4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:13 +0000

    easy save hope simple effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
