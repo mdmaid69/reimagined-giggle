00651ac7798572c4b9ae2a2dbaf93876577734b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:50:28 +0000

    blue born to talkinto suckme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
