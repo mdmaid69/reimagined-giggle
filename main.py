406403a9de3e091da05352028935d55969e5df00
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:50:41 +0000

    giveout effort upwards kill crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
