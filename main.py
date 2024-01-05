5227f942bc78c1472db4f65b295b392e7a9f3f1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:32:32 +0000

    still hurt steal well never

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def count_words(sentence):
+        return len(sentence.split())
