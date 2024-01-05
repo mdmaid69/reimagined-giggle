df849a19e6d4c15f0ae17afc733783f927897ab0
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:16:21 +0000

    question right strike anyone lookup

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_list_from_array(array):
+        return array.tolist()
