c77d087441c8ec34c1d19dec4e56bbeeee215ae5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:07 +0000

    broad by plain temperature reasonable

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
