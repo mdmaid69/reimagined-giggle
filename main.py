d1a86cf7a425e52417b76917d09e9e65f4e23ef8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:34:37 +0000

    active dear circle division hold

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
