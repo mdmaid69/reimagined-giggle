4b27946854326f5e32db8dc7b08e61a9066d39f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:05:24 +0000

    demonstrate getaway again full city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
