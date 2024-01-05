f437720273fba301fda1cc00c695bf4f508b9ca9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:52:44 +0000

    hard body know lonely science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
