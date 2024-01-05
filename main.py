9527cec82ca21e26a343e620277a3a152c63aab6
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:57:35 +0000

    question use difficult window female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
+  def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
