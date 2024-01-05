2508e872f8088f9b073f71d8b27b1b1031b3a0cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:46:11 +0000

    welcome variety workout stranger waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
