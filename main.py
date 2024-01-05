e5f72ec87dddf0d12dac055e5cc8e35afc78b032
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:50:26 +0000

    guilty supply pump low tool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_work(force, distance):
+        return force * distance
