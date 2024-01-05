ed952fba77ee2e6c8c45eba4c3855b1f683e9b2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:15:09 +0000

    invite breathe receive back busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def remove_duplicates(lst):
+        return list(set(lst))
