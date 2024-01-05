2fa59911dd6cb842126fdf42a5e54d1e617373f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:03:47 +0000

    ashamed string offend tongue rarely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def reverse_string(s):
+        return s[::-1]
