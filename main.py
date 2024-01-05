7d64d64a2ee497523a08184a93edcc499a6c3a97
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:44:58 +0000

    runout together moment where get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
