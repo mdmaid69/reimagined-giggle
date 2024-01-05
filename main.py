9e0d1760a080c9347e5e004e8446829f171c2fbd
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:21:56 +0000

    567enemy bring argument prevent agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
