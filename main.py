73ad5cf41bf12b92540fd889c6c7137395664a78
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:28:19 +0000

    view underneath borrow separate taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
