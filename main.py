dc6d7bb258f7275ea31b047263cdce57d9351426
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:08:37 +0000

    throw wind add share kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def convert_to_binary(n):
+        return bin(n)
