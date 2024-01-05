e7f2943982d804b884d723d8a5de0fb0bda9e2e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:52:49 +0000

    language below reliable especially great

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
