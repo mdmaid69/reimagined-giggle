ec4b14d454291fb8c4b1ff7a1b632e947c7e3407
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:11:49 +0000

    meet tragedy fill squash chance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
