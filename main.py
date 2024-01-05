1adf9adf43cfeebd9b6802d3fd9ec351e2aa34cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:02:23 +0000

    tool tap platform bush all

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
