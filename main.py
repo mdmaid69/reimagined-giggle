c23ef210f9facda6fd53fab8d742737d3fb4526e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:48:14 +0000

    married tend odd temperature through

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_pressure(force, area):
-        return force / area
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def count_elements(lst):
+        return len(lst)
