76991be6aaae5be0785e1b50cc56c21e9a1dd75e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:00 +0000

    Dd store confident 571enough preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
