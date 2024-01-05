9bd303385455fd629f72474178ab77abd5c778cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:25:02 +0000

    country ashamed combine secret cold

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
