d8383814d39f33516452b9bcc424d6b5ce8216cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:42 +0000

    leg target phone appeal getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
