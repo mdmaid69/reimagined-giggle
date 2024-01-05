0d3bca6a7cc6f4fba8cb0f492a8fc13e3656d22e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:55:11 +0000

    never hit bringback naturally reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
