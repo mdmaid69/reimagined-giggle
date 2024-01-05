c4830eb7d753676756852486b4f8f6b3de632e16
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:08:58 +0000

    after mysterious chin listenfor maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
