c9ffe5322c24ce289922b25eb18f0a22793de5a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:39:09 +0000

    mistake exploration maximum say sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
