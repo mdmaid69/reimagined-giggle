fafe5a6497d5644c680357bd3ab1d59649f346b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:33:54 +0000

    introduction thankyou strong practice cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
