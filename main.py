cff34a115189712096468fcac83b88493ae1cbdd
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:37:17 +0000

    the tip brown pronunciation thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
