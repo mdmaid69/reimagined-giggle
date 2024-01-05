aff67e9eeff6804459d9be9f7423c8344e3ceba6
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:13:00 +0000

    I organize age proof coat

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
