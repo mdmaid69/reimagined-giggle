7af1ab83205b73994b5607f4d970cbd11caad0c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:46:19 +0000

    content load embarrassed interest material

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
