747fc59e082cf6b42018f07728c1fecb4dddb07b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:47:42 +0000

    all head help tune later

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
