47e4e6773b46c3ff09a836da9c7e693a62d5b671
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:20:38 +0000

    track ring container first sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
