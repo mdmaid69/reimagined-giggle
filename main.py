c1b6a9aaa7f3a369f547a3350a254b6b5484f202
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:13:19 +0000

    flood across freedom wake release

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
