af8a80723dae259bdf3d4b532e88f81eb647b6ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:54:14 +0000

    dare loan no protection wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
