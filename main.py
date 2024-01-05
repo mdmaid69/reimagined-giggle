16ddf68f94b27c8e36064cab769478f01945feae
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:07:07 +0000

    cheek amount bunch past punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
