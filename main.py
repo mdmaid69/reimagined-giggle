545431944fa53284122398e1a1c8e07c80aace77
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:35:44 +0000

    hurt broken principle marry mainly

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-for i in range(10): print(i)
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
