793c7d791b905ac812a404f2b61a1484ed9d9cc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:26:29 +0000

    whistle valuable anything fact prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
