2d3b96bdf0b57392af2dee13367ae5a001cbbe9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:38:36 +0000

    shame it accordingto cool shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def get_array_as_bool(array):
+        return bool(array)
