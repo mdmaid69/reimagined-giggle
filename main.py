0f63c9d2ac490e9ca3bc86781c77c2926ba41dc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:51:42 +0000

    class back part old force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
