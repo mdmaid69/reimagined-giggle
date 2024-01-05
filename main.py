aeadb26535ca242e59530957c273663a54dbe12b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:09:31 +0000

    spill unless count reply false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
