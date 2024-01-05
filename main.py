24482ff6bd4b72948b30a6bf17eea26813dd1579
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:07:54 +0000

    market sound address father owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
