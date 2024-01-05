f6a6769e161698a9089b14f5e743c70c88359ef2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:43:57 +0000

    rubbish attitude ring and his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
