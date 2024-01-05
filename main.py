04e6eda234ac9713684a86fe9ae92aa542140ef9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:19:25 +0000

    repeat collect cry talk callup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
