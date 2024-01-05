c1e76e41875c446a755a5198aea123b4253d7b38
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:05:51 +0000

    several arrangement force wind use

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
