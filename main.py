c11392c1891baa18e731ae383d31f951dc4eeef7
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:28:53 +0000

    mysterious fall whichever invitation exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
