f6b2846f756ede2e0855e840dabb66074e378f16
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:19:42 +0000

    else university factory centimetre tidy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
