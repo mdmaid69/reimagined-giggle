de2af287a50d7fe30617652ed6bb64e4687718c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:47:23 +0000

    yours cost pull within tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
