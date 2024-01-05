afca2e2c99a1cdaab2dc45d11c8dbc1e64dedc9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:26:02 +0000

    Vv pump calmly achievement invite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
-print(sys.version)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
