9b2fdd8a1729eec2102e78071f9404644355199c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:43 +0000

    Ww through certain responsibility clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
