f7a5855069622f846bc5fd55ee4c497bddea99f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:16:24 +0000

    promise avoid geton card trick

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import datetime
+print(datetime.datetime.now())
