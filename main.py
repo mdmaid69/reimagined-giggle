45b890ebe480344bc0c23271124e6b37eb03fdba
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:54:50 +0000

    move that determined fall thumb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+print(time.time())
