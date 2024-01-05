5ed6d318c8e7c853450d7327c0d3afb6bdfcbf80
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:04:56 +0000

    song snow tend water half

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def reverse_string(s):
+        return s[::-1]
