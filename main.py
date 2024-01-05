a37c4a05d760282471204cd804653ce68bf03971
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:12:43 +0000

    successful willing when smooth mention

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def subtract_numbers(x, y):
+        return x - y
