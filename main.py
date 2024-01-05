7d9482590a19f927b102dbf9ef42a0b6ae50e4f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:33:01 +0000

    bread shout history makeinto answer

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def clear_array(array):
+        array *= 0
