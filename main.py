3b2fddb4f83cc6d72161314b7d45a517be612652
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:44:11 +0000

    choose hold modern sort mix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
