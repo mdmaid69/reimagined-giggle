bf6aec6ce6f4f22efb1b1843d7d245740b2d252a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:47:55 +0000

    clever market least whistle quiet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def clear_array(array):
-        array *= 0
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
