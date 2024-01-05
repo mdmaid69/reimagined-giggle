52be31132962f8c415351b1cb85dbc7b7d94f184
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:32:42 +0000

    open spare whichever attractive guide

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
