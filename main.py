259f0b443515f47b6dfb4a8274eba452cfd96342
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:42:48 +0000

    ring praise official fat list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
