0fc55aab7286ddfb132fad8dfd510539365ce86e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:43:43 +0000

    prove hungry say fix advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_int(array):
+        return int(array[0])
