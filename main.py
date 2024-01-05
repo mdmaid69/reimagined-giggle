d4c4a9740bd01a99804252e888041dec56caeb21
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:29:43 +0000

    choose target exciting prevent beyond

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
