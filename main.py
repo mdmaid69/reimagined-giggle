efff2a8d462f32da8454799010bc013a4be636cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:47:05 +0000

    empty dream undo suck nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def reverse_array(array):
+        array.reverse()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
