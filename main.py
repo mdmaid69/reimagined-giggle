f0cf39d8b0046099ce266106d5efb24f4c51941a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:13:57 +0000

    past yesterday generous runout reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
