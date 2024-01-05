520bd7e3ff1cb355cc586c78745f5feed907781e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:59:43 +0000

    spin clear cheap lesson left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
