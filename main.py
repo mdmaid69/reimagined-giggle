d3b799e7706445936d15504f17059ee1d18e2cf2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:36:27 +0000

    body rub petrol pattern brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
