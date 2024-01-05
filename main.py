13b292e1b1ab7edb77ac85998d71bb2ef19475b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:44:29 +0000

    into star quickly appeal convenient

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+print(os.getcwd())
+def divide_numbers(x, y):
+        return x / y
