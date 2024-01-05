bd651997d6139acf6372bebe0707825b4ad898f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:31:49 +0000

    ago model produce case minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_length(array):
+        return len(array)
