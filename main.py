110e176d77f0ebd32450eb65de1a0486968c802a
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:19:18 +0000

    quick weak over have takeback

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
