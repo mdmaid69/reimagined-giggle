bfd01f75bfff6e360f24a85421b824c8624dc794
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:48:38 +0000

    combine course conduct practise theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def multiply_numbers(x, y):
-        return x * y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
