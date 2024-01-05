310916c0b6d52491cc061dae3f3371603a63ff27
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:59:40 +0000

    already comeup sting increase lower

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def convert_to_hex(n):
+        return hex(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
