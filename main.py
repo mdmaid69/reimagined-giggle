eaec1532acde7de8bf7cee592248ded0a38f9396
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:34:57 +0000

    for fix divide subject wind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def subtract_numbers(x, y):
-        return x - y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
