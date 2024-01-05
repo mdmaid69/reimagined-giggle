c70f993c36a38c5137c4cb799945f271fa489c0d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:01:00 +0000

    round before crime wild tap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
