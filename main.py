6723f1b0954f83bda5b095be8c71100b4a82fe9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:27:49 +0000

    project mysterious bet edge behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
