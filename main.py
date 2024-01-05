3544c6a324af11bf2ea92de6dce02fe329f26c17
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:02:08 +0000

    stop sister badly fetch fill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
