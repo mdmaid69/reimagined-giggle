1b0d6420ceeae9417634944114a6ebea2d8a1313
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:01:37 +0000

    improvement smell recent needle air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_acceleration(speed, time):
+        return speed / time
