d98572453bf7d8f5c6fcb8fe9c4aae0e32da1c4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:08:13 +0000

    sick shopping height wall area

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
