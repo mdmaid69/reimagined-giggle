698374490db40c7c99e57c607f421ff12680feb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:44:02 +0000

    praise drop gas joke rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
