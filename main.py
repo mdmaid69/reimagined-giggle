21ad7173e603e277434b0f61cf2e524e3e8f2fb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:01:10 +0000

    month anxious police far pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area(radius):
-        return 3.14 * radius * radius
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
