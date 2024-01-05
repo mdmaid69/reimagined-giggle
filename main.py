2a8eb3002462f42b0cf8813611f578be75728505
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:54:00 +0000

    boat deep race something match

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print("Hello, world!")
