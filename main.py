359e28727d8f624d8cb3b5b6b722ed95b9854e12
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:31:20 +0000

    only keepaway thinkthrough path explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
