465ae5ff13231b742d7641a52a9e4a921f90f337
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:47:13 +0000

    further recognize emotion along control

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
