c326303751a771dce56d26d1d911680b4ad8e480
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:36:41 +0000

    spoil place absolutely moment pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
