3358f173a92161c8cf1f4666d2468387acf635e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:21:20 +0000

    extra here win cool way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
