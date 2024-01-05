257e845b80318bd6d80dd9d56779149b99f675b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:35:29 +0000

    even slow stick peaceful right

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
