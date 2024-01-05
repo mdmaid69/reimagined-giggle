5eca599149fbe68cd14230610380cfd84bd1c175
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:06:33 +0000

    drop flood control hold friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
