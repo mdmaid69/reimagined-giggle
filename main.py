e413d61ab1ada622647144e5e79a2748475894ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:23:24 +0000

    murder letter interview union switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
