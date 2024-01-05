10ba03ab98a4e8acd82dfc13cabdddea9c91cfe6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:32:32 +0000

    apologizeapologise spring none terrible burn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import sys
-print(sys.version)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
