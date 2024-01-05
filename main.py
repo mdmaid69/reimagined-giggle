22321c7311c9e2240e86d8a03df35fe54258d3f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:55:31 +0000

    tend occasion giveup than steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def clear_array(array):
+        array *= 0
