e7c5a87f7209b6d88a2bb2fc35ba2941c5b12a92
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:53:30 +0000

    pressure fight bag create anyone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
