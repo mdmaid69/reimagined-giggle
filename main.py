116fe84a8c39c4689cf720f5320048b1c1bf0f0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:04:19 +0000

    example middle ought wing opinion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def square_number(x):
+        return x**2
