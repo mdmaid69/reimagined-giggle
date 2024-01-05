0588823288b2911f0821b855b5daec8482f6b237
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:52:05 +0000

    connected switch shadow nervous impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def split_path(path):
+        return os.path.split(path)
