d0ce98c830c2d60a7cc99bc2c81a20f997f6cd63
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:36:05 +0000

    cut treat age Jj shape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
