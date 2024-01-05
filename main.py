d8136ad329c04df0b48b739f1d6d8b02517b97a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:06:05 +0000

    one impatient lock generous cruel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
