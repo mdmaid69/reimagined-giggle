c486e06af61c3eb2fb01553c9ebb49cbee46fdb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:29:19 +0000

    eat make form send hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
