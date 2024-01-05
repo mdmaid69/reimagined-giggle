cdcafd662776ecdb8f63117c1dea72148ee66ded
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:37:36 +0000

    correct involve before capital square

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
