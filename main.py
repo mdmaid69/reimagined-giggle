8b7ebc8e8dddb566cc507037528befdef230e155
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:08:39 +0000

    sink pin peaceful store again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
