d314cd9cedc679108badc690e97b76997ea5a9b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:51:23 +0000

    computer appeal direct thinkof each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def reverse_string(s):
+        return s[::-1]
