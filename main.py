a1b882a1678dd22f625b95daa25b30e9edfc1dfe
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:46:00 +0000

    happy food cool design underneath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def clear_array(array):
+        array *= 0
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
