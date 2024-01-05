7d135fa7ae5ca8c48168ae8ffa8ee56555d51520
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:41:23 +0000

    surprise collection crime waste never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
