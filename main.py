de685e7a21b79d91ed7858ad13b6c8efb3f7631f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:00:33 +0000

    sign interest affect tour nothing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
