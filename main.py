04a5f71945e2f3dc37bc3b8a5df76b380cf1fbc5
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:14:58 +0000

    true difference true suit professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
