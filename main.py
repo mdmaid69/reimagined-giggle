bcba4593b61a6f1596adaf72edd34acc3c5ba3b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:40:24 +0000

    behind wire visit wipe programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
