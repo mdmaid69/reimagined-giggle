3748e5e5f8e0580687709893f2599dc5498550af
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:31:12 +0000

    soft mistake well meeting back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
