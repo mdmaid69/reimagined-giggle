d0ffe01d54d708ffd373b00f38b3f2ba220d7ab8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:13:59 +0000

    pain petrol to born opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
