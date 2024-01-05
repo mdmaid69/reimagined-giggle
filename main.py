a0b75ffcaaa7fec7ed0b3e9b3abf1daffdd46e5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:01:14 +0000

    confident place none minimum opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
