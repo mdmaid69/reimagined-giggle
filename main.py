8a631a1104aad0d9bae16b81bea751f33cd50886
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:05:12 +0000

    big new wheel aware crowd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def reverse_string(s):
+        return s[::-1]
