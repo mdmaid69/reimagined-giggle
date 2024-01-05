a48602f68edf25bab745f7b88303b667efd94088
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:37:21 +0000

    obtain convince control top man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
