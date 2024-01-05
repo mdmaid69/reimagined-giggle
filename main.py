8006085fcbc378c391384215a170a95158b581aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:09 +0000

    check gather hollow forget point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
