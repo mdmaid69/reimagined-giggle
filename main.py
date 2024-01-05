92826615b16c7e49e032d66234f9393ebc210bd5
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:52:07 +0000

    delicate announce land minimum for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
