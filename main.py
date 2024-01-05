c44e2453a66eaac62381d2d0c94c75fc2c6bac49
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:55:49 +0000

    many every gap reduce behind

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def square_number(x):
+        return x**2
