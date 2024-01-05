66bcc679e2a1d03eed463b3e025a5f138e592bbd
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:13:47 +0000

    secret impatient benefit sort statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def is_even(n):
+        return n % 2 == 0
