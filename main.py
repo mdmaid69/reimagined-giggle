a2c05f02c1990f940653755b1465ef825fdfc5d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:40 +0000

    clever draw trick during follow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
