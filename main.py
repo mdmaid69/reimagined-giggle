55bfbb83bd2420a1f466b0939e821db69db5a0bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:38:04 +0000

    definite only murder skill embarrassing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+print(math.pi)
