f7a11c52f8d20b51cfe360f0b17fbd4b34b21b04
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:07:53 +0000

    watch gooff concentration over time

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
