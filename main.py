269edd373a42d776f3a93366d22835b9e6fb59da
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:32:13 +0000

    finish under succeed cheap owe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
