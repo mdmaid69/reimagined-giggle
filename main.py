ac47286598faba75aac57a6f7aa795799be7096d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:41:08 +0000

    real narrow set unfortunate official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
