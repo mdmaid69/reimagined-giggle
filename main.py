0f398adc8ad876f2e3386f1742b20dc39a3e7b98
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:16:54 +0000

    trick paper shout up yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
