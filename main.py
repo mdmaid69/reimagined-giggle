8c64e20c1ecf27faa442f774b382f53736476f46
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:38:37 +0000

    persuade furniture sit I increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
