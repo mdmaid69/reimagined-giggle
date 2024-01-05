1b78138690ff57ca08cbf356495aeb8c6414e2a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:30:28 +0000

    aware reply shock story stand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
