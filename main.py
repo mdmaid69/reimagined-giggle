60b9accdd5a9d1f1d56ace756a30bcffcafcb636
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:19:33 +0000

    oil Qq Qq whoever science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_work(force, distance):
+        return force * distance
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
