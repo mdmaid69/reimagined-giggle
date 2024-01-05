a39cfb7499e15a0a9bc98c0b2740208c77b4e164
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:14:16 +0000

    calloff imagine deliberately hole flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
