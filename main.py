b5f3e970341154b70a5095b768649334b25a21d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:54:29 +0000

    punish question peace error name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
