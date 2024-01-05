aa1630639995640e3cffa62f1c4313f2667451f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:15:57 +0000

    mention unpleasant oil workupto cruelty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
