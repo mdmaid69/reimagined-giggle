496aae2458ddf7044131059f3c601a99c1101b8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:30:34 +0000

    fit pour pleased complete risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
