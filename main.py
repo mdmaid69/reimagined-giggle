3f3ce19554ea83408c8e0f2e4632317cb2737b7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:00:35 +0000

    breakdown flood possible disappointed comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
