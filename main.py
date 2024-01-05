c9a472fd4c4ea02382188f3287d19bfc5e6f0a26
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:14:22 +0000

    principle visit public less idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
