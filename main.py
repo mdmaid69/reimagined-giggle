a91826165cf2eba3764750fc46156b64000f68ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:57:10 +0000

    dig danger situation trade instrument

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import sys
+def print_python_version():
+        print(sys.version)
