c5215a3465056e786ce8fc8c0cdcb343191eb8b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:24:53 +0000

    reasonable afternoon nor 567enemy study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
