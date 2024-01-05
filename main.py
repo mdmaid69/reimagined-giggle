aecf2b126723b49378bd47f13dc5ed7b92f9ed21
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:31:18 +0000

    giveout eager pipe study uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
