7dc110c622d1f7799406e27d96fd2c3a09da1362
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:50:39 +0000

    accidentally wander into argument glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+print(math.pi)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
