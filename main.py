008facf7159b8bb5128ba8fb56dc7236f5ff4f94
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:33:30 +0000

    free production sad figure pity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
