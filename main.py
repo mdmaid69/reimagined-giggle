5ddc4f5bf72d11a5c1682b06323f110dd5d3d52c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:36:30 +0000

    giveaway sort male telephone hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import time
+print(time.time())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
