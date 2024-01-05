2ffc68a619323e81f15f79ad6032495a897d9ce9
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:53:32 +0000

    unpleasant stand check interview hold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
