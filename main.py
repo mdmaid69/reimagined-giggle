02cb64f13e1551c4355ec53497f83da6284edf4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:24:58 +0000

    direction complete towards experiment seriously

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def sort_list(lst):
-        return sorted(lst)
+  def subtract_numbers(x, y):
+        return x - y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
