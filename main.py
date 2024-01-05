ffbad007bad0b4ae89c9b9408b0f9bb5cbaf47f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:07:28 +0000

    talkinto new advanced their below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
