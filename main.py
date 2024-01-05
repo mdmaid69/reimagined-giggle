5c910ca9a0cfe611d619d331c0ebb3daf3c0378f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:15:45 +0000

    lock keepup might still learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
