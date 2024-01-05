b51c14f4929f69f3dcf8ffdaf52f36077ce77da4
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:46:34 +0000

    design judge front shade make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def sort_list(lst):
+        return sorted(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
