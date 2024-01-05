65ac0104150f6a25e3267188241e7bb868f03505
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:52:58 +0000

    discussion subject wake giveback inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def sort_list(lst):
+        return sorted(lst)
+import sys
+def exit_program():
+        sys.exit()
