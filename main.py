f432e343f61728f8fe5d7921eb4e466963c511e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:42:08 +0000

    turnout different board Itself notice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_even(n):
+        return n % 2 == 0
