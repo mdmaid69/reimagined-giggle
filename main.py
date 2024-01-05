cacd9b9f1416d36cb56a62e95aabf4226646a097
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:17:19 +0000

    beneath material precise back tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
