da20b121d6764a77837d780a8345e696795f8a3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:21:49 +0000

    aware write earth Ii suffer

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
