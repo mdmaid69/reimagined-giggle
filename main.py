3438019b5a4e93f88cbc974b6bd44e6abc511b97
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:47:04 +0000

    neither date pick light by

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
