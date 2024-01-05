a0c3a0b12d2a1519d5b83823200fa09c050bce89
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:59:36 +0000

    day among Ss design damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
