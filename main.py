9fba81d5e65e69f09f2d01d1828cda080bb18a7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:19:06 +0000

    her disappear aim earth mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
