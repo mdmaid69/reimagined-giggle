6ed40f691a78d4d4024441f690b4fb72625a25a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:32:52 +0000

    run occur home extremely past

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import csv
 def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-import sys
-print(sys.version)
