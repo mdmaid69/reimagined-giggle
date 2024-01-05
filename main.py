a7f24b4de41058f93ab1730e6b0b83f2fade9d1a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:54:59 +0000

    vote confusing field return record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
+import array
+def set_array_item(array, i, item):
+        array[i] = item
