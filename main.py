f18afed30a15f3462e31353277359b307d2bd0b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:03:08 +0000

    his survive runaway judge out

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+text = "Hello, world!"
+print("Characters:", len(text))
