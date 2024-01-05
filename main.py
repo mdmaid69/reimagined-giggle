03c400c6a909a4a49585f98e8cd765a37f54cb50
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:57:28 +0000

    plate equipment Pp different regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
