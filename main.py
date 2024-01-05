c0f0f7c4865ea2229ecf5da54ebe57becce57080
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:58:05 +0000

    interested unite whistle conversation breakup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
