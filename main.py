15c77fadc2325214f832d3e6802bc51bcba86da1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:25:56 +0000

    success steady full passage Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
