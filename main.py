d3ad95090fa498314a873ccccf4b8f08b661edcd
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:03:40 +0000

    side slip regular doubt advantage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_array_to_list(array):
+        return array.tolist()
