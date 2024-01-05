cc43b6c98fe692d2c2506849e0a6128567dfc3e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:10 +0000

    behave circle fortunately switch spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
