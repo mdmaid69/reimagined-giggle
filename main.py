876941601c645e76fa4660c4e3b08334caa15af2
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:55:47 +0000

    break gradually shout before cause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
