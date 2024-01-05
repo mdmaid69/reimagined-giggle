60b6bb07fbf5a2316432f5eae46db4fb95f8e633
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:58:03 +0000

    peaceful tape window beneath conversation

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
