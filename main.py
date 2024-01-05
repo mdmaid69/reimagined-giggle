afadfe8eef9b0a087d0f9e771567c56b92fd7274
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:11:30 +0000

    group niggaacademy statement star answer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def is_even(n):
+        return n % 2 == 0
