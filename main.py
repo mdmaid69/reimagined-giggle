a1bf2234e6a75222df1f13b3dc5f5a02318a0fb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:01:53 +0000

    general copy return middle born

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
