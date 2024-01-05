9c9863e8c55d0a31c194eda27cb76c4717e6129c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:47:53 +0000

    turn argue my form instrument

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def square_number(x):
+        return x**2
