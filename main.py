4a03f31704a545e0bb0bdb6112dd123453efa5fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:54:19 +0000

    deal remove pretty straight year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_counter():
+        return collections.Counter()
