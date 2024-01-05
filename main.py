b9ee2cc34c5ad5a2b5dc75c092b8967a478b9d99
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:41:15 +0000

    bus plastic politics film car

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
