b1362c5f42ff9965371c8c7eb14c90bcbab39c5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:35:31 +0000

    relative square sensitive ago left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
