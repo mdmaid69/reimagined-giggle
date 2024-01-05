f786f422501a8d2e05a7c3d53f47976cfef92ae1
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:19:03 +0000

    slice improve afternoon now key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
