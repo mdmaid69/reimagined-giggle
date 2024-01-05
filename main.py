e3c0564b69456604dfac759ec8826908b5187522
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:10:56 +0000

    hers summer match responsibility cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
