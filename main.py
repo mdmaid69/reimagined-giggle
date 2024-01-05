66b0ca1a9c9cdd5824cd8ea7a400ffb1907e116e
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:48:03 +0000

    pain neck trust answer go

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_list(array):
+        return list(array)
