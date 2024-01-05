c0a7aea76b8a65bf40e06bc30048836acb2a3e53
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:29:26 +0000

    sail impression flavour moral any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
