0892336f52b1f030840da40d47c82cafe67daf1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:06:56 +0000

    free control group target loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
