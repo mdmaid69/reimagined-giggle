f10a465ab793488a2a00a8f62599695c7e116dd6
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:50:53 +0000

    everybody describe fence repair valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+text = "Hello, world!"
+print("Words:", len(text.split()))
