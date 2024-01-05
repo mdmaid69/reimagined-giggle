c6213549112281d22e11f9f99249564d2a39c35f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:42:58 +0000

    take custom agreement lookup comeup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
