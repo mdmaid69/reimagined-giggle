93c6cacd6daed9b284510f915ddbe640f6b083c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:18:52 +0000

    almost finish bury mad start

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import time
-print(time.time())
-print("Hello, world!")
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
