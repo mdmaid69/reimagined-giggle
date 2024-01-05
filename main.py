deec62b4242fcf4541329eaac4f8d3cb14f43ce7
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:37:21 +0000

    traffic satisfied development guide yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 import array
-def convert_list_to_array(list, typecode):
+def get_array_from_list(list, typecode):
         return array.array(typecode, list)
-  def reverse_list(lst):
-        return lst[::-1]
