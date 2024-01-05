d3fcc976f8cf2df869b6588102270251b1485eab
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:06:31 +0000

    praise type whether back near

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def square_number(x):
-        return x**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+print(re.match("h.*o", "hello world"))
