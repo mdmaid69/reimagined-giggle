7fd81cf1e486fe78b708e227fbec97081a52b054
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:03:08 +0000

    fierce perfect downstairs available criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def find_min(numbers):
+        return min(numbers)
