a009aea93b918148734af444d563f76e948ff28c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:14 +0000

    dish visit concentrate correction because

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
