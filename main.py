34d3f3c57943d6f00833cbcbf6f58a0bf3536fec
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:31:21 +0000

    tip reliable beat fond cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
