d73b89edaf9630f8c70a15c67bca4a5208223fd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:44:55 +0000

    uniform lookup worth object careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
