cb2b449934bb1293e1aba08fd37d50bc982b14ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:57:50 +0000

    work place no careless holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
