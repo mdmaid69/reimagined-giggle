64f2c2d846248a84564cd876d922c8114d7de2ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:56:34 +0000

    ignore collect murder modern space

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
