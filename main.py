4f2cc70f4df1d77c6f0de26a0c2930fe8faf18d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:50:50 +0000

    quantity swimming introduction bringback hurt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def divide_numbers(x, y):
+        return x / y
+def calculate_area_rectangle(l, w):
+        return l * w
