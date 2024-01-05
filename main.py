27d71a64ce32b17863d19987afe0ee3e8403c465
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:55:18 +0000

    manage turnaway serve tense group

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
