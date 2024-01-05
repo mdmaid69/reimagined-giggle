e71f5b51c5dd6995b11ec54fd0ef7e4d0a09dad0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:38:32 +0000

    because embarrassed figure rank recommend

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_speed(distance, time):
-        return distance / time
+def remove_duplicates(lst):
+        return list(set(lst))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
