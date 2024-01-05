5a9392a457c6daacdaba10882cf69e09ac6d9838
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:49:54 +0000

    worried blame cruelty while sweep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+from collections import Counter
+print(Counter("hello world"))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
