437e1122e5c78a872ba49cea718404cee28b3a2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:46:26 +0000

    geton front support runinto design

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
