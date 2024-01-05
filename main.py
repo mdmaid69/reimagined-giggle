fd40d97be571bfeed2b427d0d13ca330f2dc3cc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:03:10 +0000

    arm contrast replace accident improve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
