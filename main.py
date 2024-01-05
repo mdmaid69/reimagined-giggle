c92749cd509b65991915a446c3c9cd90086edde5
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:36:11 +0000

    pipe disgusting giveaway petrol up

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def cube_number(x):
-        return x**3
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
