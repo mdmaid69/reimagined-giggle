3d632641ead2a35746baa08cb55566063538d579
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:35:55 +0000

    properly normal argue rain sorry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
