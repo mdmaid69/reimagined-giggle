726c1122ada99b2be75dc3c77c71a3cf2a7456ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:55:52 +0000

    fix shut contribute apologizeapologise fond

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_force(mass, acceleration):
+        return mass * acceleration
