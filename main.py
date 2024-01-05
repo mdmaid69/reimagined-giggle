5b5d6f94249b3c1586293760d4dd3edbf3e07b75
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:31:26 +0000

    soil narrow blue thumb by

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
