cd9f5396ba5c91375230730d6f3e8ec3337b0124
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:04:43 +0000

    curious good split practice display

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_stack():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
