3f85336734df7e4d29e31b531bdf9e44c27fe15c
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:25:54 +0000

    thumb lend store brother keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
