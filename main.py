fcffc9ae6c9e1d399574e1d33cad2439377883ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:29:47 +0000

    emergency mystery share behaviour wet

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def find_max(lst):
-        return max(lst)
+import sys
+print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
