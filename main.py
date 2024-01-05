d82d6bf239f35ec59db35f37109689e489591493
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:24:44 +0000

    material sign entry sink goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
