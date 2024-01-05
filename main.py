788e0cd3884617ca906c306198f3f228405b1bda
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:54:36 +0000

    bring block work attitude crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def get_time_since_epoch():
+        return time.time()
