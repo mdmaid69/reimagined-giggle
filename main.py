285a60916af48bde6e07d05274efacb0ebe7a776
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:37:23 +0000

    rush sometimes low him copy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Powers of 2:", [2**x for x in range(n)])
+def reverse_string(s):
+        return s[::-1]
