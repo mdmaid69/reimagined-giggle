ba79e711061a531338195a170e67f513617b59e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:26:15 +0000

    up join about consult definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
