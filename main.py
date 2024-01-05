3720a88dcb0bc579100a192152c13a83c6d12b74
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:42:08 +0000

    turnon term straight control comment

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
