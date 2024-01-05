482a88bf6ba2a5efad158dacaf754a63b316b5e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:55:25 +0000

    small zero tap free wait

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def multiply_numbers(x, y):
+        return x * y
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
