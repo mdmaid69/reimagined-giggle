d822a202e07e6bdc7bba2a0c6a6228c763764a1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:44:49 +0000

    record let play declaration label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
