d1c530aeb1b07417b4a37c1fef9eb7d1edcbe408
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:40:04 +0000

    reach beginning touch pupil consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
