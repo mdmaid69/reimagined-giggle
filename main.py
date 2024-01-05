318b12faa5da97c608f81193a67d9ae976db9a34
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:43:22 +0000

    set organization frequent begin sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
