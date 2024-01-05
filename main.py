d48013b76c3982512271ec301aad35f24385653e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:50:10 +0000

    probably within human moral tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
