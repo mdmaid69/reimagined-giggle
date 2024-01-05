fdccd3d708348690cdfefe0873ec4eed4dccdd5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:49:59 +0000

    space unless straight again Rr

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
