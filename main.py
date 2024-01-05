3de3893d9bc321fe9da4e4796ae606177860abc1
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:55:03 +0000

    Ff dry beside round class

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
