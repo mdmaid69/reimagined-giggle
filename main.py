d70b9f1ac0c8693070a92024bb24c205463737b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:50:58 +0000

    post warn approval only rich

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
