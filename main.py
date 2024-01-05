62db8d225abbf88b2274928abe4f4ac3fb89cd3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:30:09 +0000

    rob high wind original step

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
