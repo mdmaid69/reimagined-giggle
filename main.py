f5e82ba3293f433f946f5dc3e39790747a89b0ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:36:34 +0000

    which spite thankyou indeed style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
