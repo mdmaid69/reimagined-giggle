6623124ed4282f26b33d913604a8382c27a63f16
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:18:39 +0000

    top error tomorrow stranger stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
