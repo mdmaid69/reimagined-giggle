24ff4748863173e7bfc80bbacc8b47d60bcfc86e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:25:34 +0000

    turnup hope yes few speed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
