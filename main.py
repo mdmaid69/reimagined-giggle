0ec219160c955be3c3d1fc10465235996ecd783b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:36:08 +0000

    unfortunate pleased active adjective measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def sort_list(lst):
         return sorted(lst)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
