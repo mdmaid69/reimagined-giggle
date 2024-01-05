7b9c57dc465dffa2d4a527eadbe2279df8b80653
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:52 +0000

    star Ee tread size vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
