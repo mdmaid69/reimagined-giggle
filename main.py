f13c149acdecb224c095fed06ee7444db9cb9f5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:26:32 +0000

    such shop gas ordinary offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+text = "Hello, world!"
+print("Words:", len(text.split()))
