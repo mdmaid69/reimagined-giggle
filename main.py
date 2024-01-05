7876b2034bed3b6e4cee729c3b05c1a92ea8b577
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:10:26 +0000

    callat lonely pour fear square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
