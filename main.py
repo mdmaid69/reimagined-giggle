a57d6eeab7c31a121a7907ea810d4b8c64a520c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:49:11 +0000

    collection set physical radio consist

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+for i in range(5):
+        print(i)
+import json
+print(json.dumps({"name": "John", "age": 30}))
