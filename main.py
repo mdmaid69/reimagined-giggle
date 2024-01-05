7cfa0950564320583a82b2b5a555387dea04ad16
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:13:28 +0000

    doubt active breakinto takeback minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-for i in range(5):
-        print(i)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_density(mass, volume):
+        return mass / volume
