aa7d6718e63063570422a8e6c38162beff860f5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:38:50 +0000

    radio deliberately somebody show tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import sys
-print(sys.version)
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
