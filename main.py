651d09cd63c562ce48f79a00daa43e9be0ce0cf4
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:00:59 +0000

    familiar runinto whichever poison machine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+print(math.pi)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
