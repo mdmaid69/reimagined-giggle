18c94f8bc44f6518f8d30b3e5ec73609a84333b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:21:11 +0000

    official later special unless evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
