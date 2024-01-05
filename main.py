6d81454df042d9b46f3722dc9ce9a64963f6a0f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:44:14 +0000

    forward makeinto pretend rate carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
