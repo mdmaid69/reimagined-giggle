16afe424cf84dc0b5db0485396e9c5135ee56f10
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:20:41 +0000

    small tie several yesterday fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
