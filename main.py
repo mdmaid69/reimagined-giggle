2ecc81891d812a6c8c4b4e6d4246866e8d558e30
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:36:19 +0000

    catch takeback anywayanyhow discover action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
