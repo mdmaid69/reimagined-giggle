d5e4b7383b3656ca04a11ef1180c2e88b6983b74
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:24:50 +0000

    straight unfortunate fill wire sail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
