ce18989319a400d0d07f0e18b8494de89f286a51
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:33:22 +0000

    discovery vanish combination pot deep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
