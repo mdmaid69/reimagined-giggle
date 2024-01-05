281ec039cab835c23a7651bfa3d959595136504f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:17:37 +0000

    mass life official future city

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
