97d7651fc52fba93e19d19f8682fa3f4f89b5ae3
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:25:42 +0000

    quantity Ii dream maybe club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
