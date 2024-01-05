e64d3b5f7f0797b3a098df03484ea5008b68b765
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:54:37 +0000

    range nowhere study reject independent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
