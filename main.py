9a300a76639104201163fea6cc90a92f7ed1e5aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:18:56 +0000

    level impression excuse trick female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_elements(lst):
-        return len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
