6ea0c8c8268a03d18cd7486b21fc83dd6f09d0cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:23:42 +0000

    feel politics run fold pretty

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def sort_numbers(numbers):
+        return sorted(numbers)
