3ceb941f43f8dd0cb8a32dfef45545435c89ea52
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:59:33 +0000

    continue needle shop well advantage

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def square_number(x):
+        return x**2
+import time
+print(time.time())
