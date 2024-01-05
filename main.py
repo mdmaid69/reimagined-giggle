796a80db2ebb5c4ee733790da4d4338ac9443e00
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:50:43 +0000

    near born forgive unhappy father

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
