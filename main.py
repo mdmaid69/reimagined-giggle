d6b83fe5f90d0db52366293218dcebb0f7353a8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:07:09 +0000

    down dance proper school idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
