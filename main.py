ab1ef08ac8e3e6f5ad9a3891295e330c6ed415fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:48:36 +0000

    feature speed eye danger along

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
