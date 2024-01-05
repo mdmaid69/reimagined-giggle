ca1b23d3a5e8c8bc10262bcb6a9f906854eee518
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:02:07 +0000

    protection good impressed matter 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
