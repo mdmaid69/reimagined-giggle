61e42d0983d95a4ef02530075d212d351e8bd333
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:33 +0000

    tomorrow disappear light cum afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
