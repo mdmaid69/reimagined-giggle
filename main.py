bf2307a217cd3e171b9f4a2bfb627a9584e1452a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:16:19 +0000

    sport cause present she turnover

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
