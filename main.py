6d2f7fe646aaa68d2ac2247173dd39671d4c78bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:17:54 +0000

    gather greeting always connect pause

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
