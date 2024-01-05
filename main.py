d2f93c053a96a94687fe053f9144dd69cbeb6135
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:10:38 +0000

    guide square kill convinced relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
