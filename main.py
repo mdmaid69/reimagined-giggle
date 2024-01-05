4ec185a44192ad9946c29106b886f6d853e747c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:47:16 +0000

    believe giveback glass easy century

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
