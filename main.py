baff98127c1e3fc4a1b3b008df7b8bcc8829b7f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:14:58 +0000

    strength below high switch smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
