0f73c2f231af26eb7fbc04cb7f4f00368314c9ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:41:18 +0000

    learn lead basically succeed dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
