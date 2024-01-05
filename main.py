6bb6a838cf83aa6001b41b26da21bb5d57de2ddb
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:40 +0000

    anywayanyhow learn language radio confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
