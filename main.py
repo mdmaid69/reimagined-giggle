7cd05f5fb249dd941c92a655ada02f474c0288b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:15:07 +0000

    twist zero offend many demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
