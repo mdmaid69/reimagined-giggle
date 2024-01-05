ba3a0bb1c91a7928b31e72a9bb404a01bbdea9f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:51:55 +0000

    zero software fall this interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
