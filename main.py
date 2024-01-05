deaa09bb231d2dcf06e904844437d05f5a6ac426
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:21:24 +0000

    fear responsible design clever pure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
