552dff268276b571120c196ef4be0a6725e6f865
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:08:08 +0000

    part issue common disagree excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
