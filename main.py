ede6a681d49b9b2fc170a04b3f38c5c5179a532a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:41:24 +0000

    public about better touch possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
