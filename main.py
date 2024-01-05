b31e52d4037f52ceff2b2273b8e8dc99d11dd020
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:46:03 +0000

    feel right leg apart pool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
