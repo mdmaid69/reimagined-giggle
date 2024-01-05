460714518cc2128e284d3022a1ef77aa8ff66442
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:19 +0000

    table test hurry into ride

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_factorial(n):
+        return math.factorial(n)
