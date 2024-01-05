54d8201a298224a9130b31e016b06383685f9e9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:53:49 +0000

    everything kick people the uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
