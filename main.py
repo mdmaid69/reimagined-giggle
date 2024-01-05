3e621891fa07f88bd16a6513451c567b5054c90f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:34:29 +0000

    sleep ill eager left all

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
