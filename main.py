31c924ecfe60105b0540c18521eccc75aee6a097
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:21:02 +0000

    his active estimate pay Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import datetime
+print(datetime.datetime.now())
