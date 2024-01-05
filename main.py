f0999584b01de83096d4163bdbeef8fbaadf7fac
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:44 +0000

    age lead great block worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
