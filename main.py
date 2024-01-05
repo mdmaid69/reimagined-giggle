76896e172426595704f1527cacec9871bc4d1813
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:02:52 +0000

    necessary pencil period grand since

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
