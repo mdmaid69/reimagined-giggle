ebb248bd0836d354692b731fffd63ebdc9aa82a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:30:49 +0000

    sort food add Cc interested

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
