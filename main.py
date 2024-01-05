db5fd55f47fe09ad94186ac5dba8c51bd4a47adc
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:12:02 +0000

    comment store and belong summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
