e50dfe88f8d433d074f757674cb57d6dfa12e35b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:56:25 +0000

    apply emergency demand direction out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
