4909423f97d03b3bd95506bbd903ce705cd2af80
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:09:30 +0000

    peace rich university rest radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
