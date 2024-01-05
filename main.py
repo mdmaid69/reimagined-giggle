df1f71243319097ed9bb599b32d07dae2cd5fd33
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:49:28 +0000

    promise bottom up commit trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
