f2ed717dd4841792b5207dc6257250e2e57d8762
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:07:03 +0000

    hole strength will tread you

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_pressure(force, area):
+        return force / area
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
