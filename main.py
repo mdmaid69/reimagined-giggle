6b264bf59ae908e37dd54168ef4dc1c091734732
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:14:46 +0000

    getaway collect female finish rush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+print("Hello, world!")
