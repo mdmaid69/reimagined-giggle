7c17aeca34c40c5afc36d4f9586753bfe7b642ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:22:47 +0000

    introduction emergency right away quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
