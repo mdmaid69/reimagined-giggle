533f4735b1c5f370b9462d9c3356832056fee549
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:08:15 +0000

    getback quantity drawing chase address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
