131c7f337289215e89b8accaadafe64f7309305a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:53:27 +0000

    bright know hold imagine suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def cube_number(x):
+        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
