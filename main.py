04a6ba34ab66a7cc08670e2e194e9373eb865c49
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:42:55 +0000

    impressed rule without second eager

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
