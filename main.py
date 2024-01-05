7f14125109633823d303660ce16e17589897d75e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:50:08 +0000

    rest you concentrate measure game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(numbers):
+        return min(numbers)
