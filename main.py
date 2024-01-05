aff0baa2630fd08ee47dd2fc07c28e79e76371f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:40:10 +0000

    badly 567enemy note angry pin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
