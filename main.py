97e86def12398af5ef7dd89b189a0710045ea320
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:15:55 +0000

    port make afford attempt emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
