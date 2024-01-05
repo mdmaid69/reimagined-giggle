43d8d24585122fa5f4e081889fecbc1ce58b609f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:54:19 +0000

    crowded large note wise wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
