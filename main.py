2563c0a93b64fd3451dbfe83bd783cd6aadfbd46
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:00:56 +0000

    crowded turnon hard post expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
