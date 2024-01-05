d03da44458d88a383c1f44693c0e0159ca918ad6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:18:34 +0000

    fly precise simple more fall

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
