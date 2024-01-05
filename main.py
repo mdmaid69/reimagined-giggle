13f05381bc47bd107d8b5ff32e94abbae9f15b55
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:04:39 +0000

    interest intelligent breakdown wide solve

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import sys
+  def get_python_version():
+        return sys.version
