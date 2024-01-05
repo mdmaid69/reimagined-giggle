0d19583bafe41cbaaa85476fdc9c39e4396d82e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:59:05 +0000

    broken quick deserve be fit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
