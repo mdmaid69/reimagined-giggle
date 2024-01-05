ca459d2f7cfa05d5185aab488367e26779fa9547
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:42:10 +0000

    frighten rid blind fit speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
