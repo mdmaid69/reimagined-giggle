a45483ead2f0f267b93077467766e82fe4ae4c45
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:48:58 +0000

    type fix argue behind approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
