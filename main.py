6655f786234607416377e9edee465cd434dd06b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:13:18 +0000

    steam problem heart keepaway Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.time()
