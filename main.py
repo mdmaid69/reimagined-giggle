66252bf8be1042c35c77d321c17ffc8625a0b29e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:21:00 +0000

    Ll bit learn mark contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
