7fe664229e3c328b15ad43ff63268d4992890441
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:07:11 +0000

    inquire children valuable load majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
