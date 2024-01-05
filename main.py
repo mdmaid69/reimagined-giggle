961f4afa1c8d1e2e2b1a1415108df970283967a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:56:52 +0000

    slightly phone entry feature excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
