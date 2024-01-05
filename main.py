b26d699db5786b0c3a859588ac3b0bcaa06a1f41
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:35:36 +0000

    release certain your cough crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
