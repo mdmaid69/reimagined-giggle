ada145e3c0cc43b59fa8174714c850ca97356f5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:33:23 +0000

    accommodation scared hurt born feed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
