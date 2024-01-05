6a620b298de13dd74a4a993f87523ffbeedb4bcb
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:46:50 +0000

    getaway noone hollow poor afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_max(numbers):
+        return max(numbers)
