4f63c76154dddb5d25f6d7c13dc28f86574e2805
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:33:06 +0000

    clearly admit equal life music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_list(array):
-        return list(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
