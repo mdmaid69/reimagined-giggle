07a2ea9e85ba57ca30a0fc9d8ecd83b2a6d7a51f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:06 +0000

    classroom nerve no cap service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
