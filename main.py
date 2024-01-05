175773290ca747ee779f58b74f505c2ab03a6b2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:10:09 +0000

    separate meet alone fold needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
