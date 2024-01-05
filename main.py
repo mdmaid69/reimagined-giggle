2ec89802fd2794a36685d0c7907ce2a0de1c02d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:12:09 +0000

    avoid plastic attractive character bite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_pressure(force, area):
+        return force / area
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
