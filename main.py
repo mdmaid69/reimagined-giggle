fca4b013d79cb33ae375b6b083b8edb4a3d0a195
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:51:20 +0000

    rubbish comeout than proud night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
