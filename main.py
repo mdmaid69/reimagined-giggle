523496e8c1260b4f43a759b9c6daaa99c8eaf270
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:14:44 +0000

    speed style breathe truth meeting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+print(math.pi)
+def find_min(numbers):
+        return min(numbers)
