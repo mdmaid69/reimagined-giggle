e5db328614b1c20f6ae5ee259f2f554166bb5c23
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:08:36 +0000

    life border flight getout floor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+x = 10
+y = 20
+print("Sum:", x + y)
