b296e97fad6d7aede14414c10e8ef60842d184d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:00:14 +0000

    excuse again boat back tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+name = "Python"
+print("Hello,", name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
