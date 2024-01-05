07095bc90a1f9662755daef090f78fd975c2fb31
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:34:03 +0000

    grand goback(to) right hard back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def convert_to_hex(n):
+        return hex(n)
