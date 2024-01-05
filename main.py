90dc1bdaa348b9a8d9f438f7fbaf8a15a0a878d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:38:13 +0000

    summer embarrassed woman stare better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
