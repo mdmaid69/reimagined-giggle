2b20b25603d3f825ae253c3d2e3833cc02293164
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:24:10 +0000

    rid expression tool Pp pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
