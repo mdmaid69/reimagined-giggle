7b37e3dbce20c372055e78733044dcb3a2488f40
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:40:43 +0000

    food ride port conversation to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
