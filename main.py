c8f7cbc05093c821301832039f93a448725bef8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:15:10 +0000

    dinner explain tired comefrom play

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
