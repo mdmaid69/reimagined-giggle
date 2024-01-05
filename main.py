cd94df627943a10e4fd7bea0003d3e077aec9236
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:32:17 +0000

    exhibition payment getout insist practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
