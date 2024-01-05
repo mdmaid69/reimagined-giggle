039082fbe2375252e635d1dd9eeda0922968c85b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:19:25 +0000

    none clue sense Kk keepup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
