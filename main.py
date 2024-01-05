8c21f85be20cf072799a05677cf1cf4712d0b34c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:13:25 +0000

    port win way deep minor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_item(array, i):
-        return array[i]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
