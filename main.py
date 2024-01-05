b9cb55869f1cf508241b9887435c54cdd93511aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:54:29 +0000

    sore praise control smart pull

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
