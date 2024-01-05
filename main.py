786cdd4e44f6c34932084e1dea833a9467c4e24f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:58:50 +0000

    total then along as base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
