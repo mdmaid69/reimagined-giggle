60a87ae1b04363766a776c1bb5cdd7884d630b4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:36:32 +0000

    shout chief meaning look possible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def multiply_numbers(x, y):
+        return x * y
