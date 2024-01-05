67a092b97938670f3cd03399b9383deb56ff32fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:58:33 +0000

    contrast cross plant difficulty Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
