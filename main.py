798a9c309bb51992dde05bfe06638cb82fb08c89
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:00:16 +0000

    turnout high tune last both

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
