1705a7f1d3253abe918d4dcbe82907d51daf732f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:47:51 +0000

    towards tell shoot excuse slightly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_cosine(x):
+        return math.cos(x)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
