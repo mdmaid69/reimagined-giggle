4540af3d1ab64696b06a37bfccbc7fb155fe952b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:29:10 +0000

    ahead later especially stupid disagree

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
