9436dd999aaf48f65e28226892575c7aa217ac84
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:14:29 +0000

    think their never whoever contain

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+print(math.pi)
