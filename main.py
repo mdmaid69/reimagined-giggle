0cd72374ed57bb27b6412aa96261b942cdea1471
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:31:40 +0000

    nowhere evening human bend seriously

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import time
+print(time.time())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
