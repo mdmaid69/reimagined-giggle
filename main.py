7ff0e703bbf19f8c1571609ff8d21e9b787eb7a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:09:09 +0000

    sign benefit present Cc mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_list_from_array(array):
+        return array.tolist()
