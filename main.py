6cec677780a733bcd5fa3b5d25a9038c00410e9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:39:19 +0000

    decision concentration if project father

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
