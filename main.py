d26cc9354f6a6ed621313b67e2c0c6ccd0a3ae2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:41:00 +0000

    reserve first shock physical murder

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
