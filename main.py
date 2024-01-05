397949466e628bac98f74d9f9e9f671674da3a8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:02:55 +0000

    imagine flow sometimes dream dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
