ce75a6ecb8a1ce30f085bd5546a4685921d91993
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:58:26 +0000

    partner will order old sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
