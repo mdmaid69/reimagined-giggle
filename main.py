aaebbae8f216b87c732d5f3bc9dc8eeec23436b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:43:17 +0000

    rush comfort organize hard throw

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
