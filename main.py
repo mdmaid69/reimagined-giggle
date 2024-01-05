1d989186941f3117db8149d8606d2b7882d53fc9
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:43:19 +0000

    suggestion harm risk attract freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
