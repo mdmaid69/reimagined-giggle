11ea6f9c8d799ff920678460325e4226f06ac59e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:01:11 +0000

    search long control touch dull

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
