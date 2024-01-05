982b7b5e55370b3014e91477693fe8169a3e1c65
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:07:33 +0000

    meet extremely year traffic deliberate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
