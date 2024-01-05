52c161d0017fc5565dc8f3e8ce51773e7f00b7b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:24 +0000

    nearly its start mark 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
