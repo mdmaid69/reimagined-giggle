edb9addd6f09676398e251d783c5368befc4a1c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:46:23 +0000

    guard already ticket convenient cap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
