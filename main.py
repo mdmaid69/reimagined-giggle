ed2d9d89b6539686635512b7c47b7844281e460e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:17:01 +0000

    someone exchange bottom who slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
