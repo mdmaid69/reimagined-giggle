b8e9bcec08bc0497879c88490e369e61c75f60af
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:51 +0000

    angry ignore cap breakout exchange

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
