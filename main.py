948036ef60d28ee32ea439bfb56d14832050f2cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:31 +0000

    rush talkoutof judge oil married

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
