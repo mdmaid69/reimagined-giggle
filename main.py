d82368df8dfeb996060647c74932862eb9140288
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:14:56 +0000

    mistake OKorokay purpose hold scrap

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
