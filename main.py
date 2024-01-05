127ef93e995db169ec9f5f48afc837cea4856cf2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:13:51 +0000

    shadow shout out telephone difficult

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
