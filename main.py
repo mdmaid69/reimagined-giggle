6befd0a4a8d7f722aca902533b12d8b75f361f0b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:14:38 +0000

    spare especially clear only discussion

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
