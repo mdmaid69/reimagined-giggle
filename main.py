c8b562346848940bd408c3ec3e1400aa675806f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:44:16 +0000

    film successful night failure ahead

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
