325506cee479b1c65bdc22e3d3a37d9c7f8d8e71
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:17:31 +0000

    might round eat keep wing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
