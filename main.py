3446ba38832c55d8f46ccdb4aae1df774e303555
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:45:46 +0000

    themselves cry relax careless but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
