0d09324adf9d9f8d8b708302a1e0ebffe6c1e105
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:53:34 +0000

    stone recognize left history them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
