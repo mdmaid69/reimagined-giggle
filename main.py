7e77ed246892b520da49530046e5f7248606171c
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:30:36 +0000

    relative fear kill project loan

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
