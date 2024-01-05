6d34d4d2985ea42d1ccd2feed5a1be012795ff6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:56:59 +0000

    surprise shout personal foreign maximum

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def find_max(numbers):
+        return max(numbers)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
