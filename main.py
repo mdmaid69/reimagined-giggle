4b2cd88de2c46422936c165fd0f7dc1ace9a0427
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:57:37 +0000

    Ff evidence relation its ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
