aef305a8ebad550ff71ac4538b9b1fb3a7d8be1a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:58:15 +0000

    mean holiday walk prize shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
