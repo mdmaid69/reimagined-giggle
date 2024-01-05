933757937e0fb3b02246430d2cc86412e0e26bc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:31:06 +0000

    long lookback(on) read combine borrow

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
