e938bbd63f93dcb360b854f328b512fb9c1fd540
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:03 +0000

    murder dinner interview trap finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
