2f2e2a77f4d7355c6a80c0e4b16770cfe46ecb48
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:27:03 +0000

    fight tape bad sing Ww

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_power(work, time):
-        return work / time
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
