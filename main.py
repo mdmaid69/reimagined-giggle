d80e63530b44ed8ec35b808e3120919a17ca973e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:46:37 +0000

    responsible often ago shop kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
