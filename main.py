9a276d2a0b7de9ad3d0ff5667fae94f16b02d2a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:21:23 +0000

    now introduce giveaway her build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
