0413d45e96766e65d8a5f154712184a15d2f5b0d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:15:14 +0000

    proof possible tragedy breakinto actual

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
