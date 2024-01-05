24e83b7936b8014081f1ccf1073eacce8590f322
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:26:08 +0000

    through debt treat together exit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
