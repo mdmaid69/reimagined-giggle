cbe61cea63b10655be4673e9ce1b80944a96343c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:24:10 +0000

    where neither respect noone mood

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def multiply_numbers(x, y):
+        return x * y
