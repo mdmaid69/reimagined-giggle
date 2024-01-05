7a91bce1bcad6cd462f1118fe27cfc19669019c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:23:59 +0000

    whoever somebody drop check able

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
