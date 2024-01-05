d2dc863f5303537379468253b506cea1a6a10a34
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:39:05 +0000

    Ll sweet stiff determined sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
