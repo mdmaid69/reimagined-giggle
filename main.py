3398045b7e92069d066ce46b127fd8f04eebbe2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:34:20 +0000

    patient calm priority Yy type

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
