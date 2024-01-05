ea37e31725959b8c2230d326e9a98f5aec9715cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:13:07 +0000

    fashion refuse somebody grow foot

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
