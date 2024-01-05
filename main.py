ceee2056b6f5bd6e48e7118197412568cab6de8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:21:14 +0000

    maybe hello scene bound manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
