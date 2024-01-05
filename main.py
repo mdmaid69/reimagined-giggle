a0cad3225469396072f722900815963e894adf85
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:22:26 +0000

    fast please Dd please try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import getpass
+def get_username():
+        return getpass.getuser()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
