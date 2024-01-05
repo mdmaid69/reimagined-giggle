7349249af3f0c8bb48767495a0e542740c5f4a4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:52:28 +0000

    show form getout thankyou often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
