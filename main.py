872f46e287358053acc86e575bfbeaca72553553
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:44:22 +0000

    crash combine storm nature strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-name = "Python"
-print("Hello,", name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
