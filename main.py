bbcd2a42a3b05bef41f4a7e22689c75748980a05
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:15:15 +0000

    slip related test you reliable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_average(lst):
+        return sum(lst) / len(lst)
