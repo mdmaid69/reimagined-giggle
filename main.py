53d33e3ecfeb8c1950cb8db10e419e12af193f5e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:31:40 +0000

    fast habit holdup high will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
