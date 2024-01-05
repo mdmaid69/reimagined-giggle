5656a9d171d05f8204c7edf68d197770a12e18f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:29:17 +0000

    hurt line appeal loud so

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def subtract_numbers(x, y):
+        return x - y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
