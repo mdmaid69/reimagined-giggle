f1f1b5a72e76378d8560ee8ad26288178e3769f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:33 +0000

    pocket gas fashion aim correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def is_even(n):
+        return n % 2 == 0
+  def is_odd(n):
+        return n % 2 != 0
