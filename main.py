9a07b47ff3e2ef94889cf30ebe03c6e0a4c78d9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:49:30 +0000

    temperature Yy operate guess bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
