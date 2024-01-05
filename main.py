c910a09299e241c5b225fd6a5075bc010366d3ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:06:58 +0000

    hard shut main minute chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
