0584c76335dd43988c776dad1328484e1bc23cb6
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:20:40 +0000

    immediate any relationship shop related

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
