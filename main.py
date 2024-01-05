021d8f6f80b7992e12d3d3fa24a2dead87b1d40d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:25:21 +0000

    favour culture gentle this pupil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import sys
+  def get_python_version():
+        return sys.version
+  def count_elements(lst):
+        return len(lst)
