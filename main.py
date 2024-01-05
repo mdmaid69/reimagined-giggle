ff305751e8757aee4d9bf8fb126b497c02294d17
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:25:39 +0000

    declare holdon gate offence education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
