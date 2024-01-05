f82bfecfc8a2c327cb0f43b4c0ec4be60ed2c59e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:52:08 +0000

    contrast anywayanyhow past basically think

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
