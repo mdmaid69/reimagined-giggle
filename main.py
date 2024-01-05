1d5b8152837c355b4d06a1c7df774e26bb84900c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:33:11 +0000

    doctor union badly keepout ashamed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
