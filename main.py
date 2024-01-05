8df7b07423b375769db74b71c251935a81b6081e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:19:23 +0000

    picture cheque young nowhere lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
