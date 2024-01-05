49e37c3fff9d18e9bc7284730f9ddc113592d5db
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:57:18 +0000

    genuine when pour far meeting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
