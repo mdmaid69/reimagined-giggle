0083492586103641a59fc3d8cbaa3f563e3c6ffb
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:27:48 +0000

    accommodation coffee day peaceful born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
