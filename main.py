6b6dc6bf65f298785a817c52afe8157fee25d803
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:17:00 +0000

    fault copy become shake bath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
