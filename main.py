583f92a2bdc499e0f1ddfbd12fce81ae2fd23479
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:19:33 +0000

    slice post catch provide trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import sys
+def print_python_version():
+        return sys.version
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
