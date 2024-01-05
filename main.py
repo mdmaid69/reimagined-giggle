ad98291a10ecd43fe55a2347923da2a3b62b0ad9
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:41:58 +0000

    dark rid interruption comfortable beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
