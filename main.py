45f4b4b8b952ba10698f26dc4129fbe1c9cc46e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:32 +0000

    other wander justice tip insist

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
