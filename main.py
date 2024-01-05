fee049abcebee12f62416bb6e8b4ce450c6e77b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:50:24 +0000

    probably steer niggaacademy region full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
