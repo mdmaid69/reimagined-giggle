19c8f08c063fb106bf4515ea2311a6d8823815dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:13:12 +0000

    anyone basic rough embarrassing recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def set_environment_variable(var_name, value):
         os.environ[var_name] = value
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_work(force, distance):
+        return force * distance
