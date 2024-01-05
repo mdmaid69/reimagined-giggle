d1bca33fbed66570b15c9beddd3116d4d669c2a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:17:49 +0000

    measure proof increase cut suddenly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def is_even(n):
+        return n % 2 == 0
+import numpy as np
+print(np.array([1, 2, 3]))
