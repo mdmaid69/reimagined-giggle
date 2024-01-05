9c16f0b8ea1fd966be17caf8bcf6b1eba0d195e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:23:22 +0000

    but rob develop minimum fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
