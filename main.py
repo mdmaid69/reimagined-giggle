324fe72ee5f0c3b99bd1760108d4f44e7f9822be
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:57:12 +0000

    fetch cover try advise deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
