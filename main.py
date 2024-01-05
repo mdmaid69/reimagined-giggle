b2ab0d7e6624714b642306068b0bcc2447c89978
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:24:02 +0000

    claim big affect responsible zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
