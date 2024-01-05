f83484f964b7b68e68132cf3ce5c062b98e26bf6
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:07:25 +0000

    sail eager conversation 179bite off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
