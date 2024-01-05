549d29e3301a6c7183ef717725e1df34a97420f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:50:36 +0000

    pattern beside sort slice rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
