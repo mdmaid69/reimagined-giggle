22cea47a05c6de6751fde5ac9956ac36ebd5c8f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:38:44 +0000

    swing long trouble telephone table

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_area_rectangle(l, w):
+        return l * w
