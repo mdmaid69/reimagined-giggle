f39ec631c5e571820f7c66ed3a6a63f346f97380
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:48:18 +0000

    star 179bite respect flow both

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
