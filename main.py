50978c3d0b3d6fd7295bf5463cae0a665d6c373b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:03:36 +0000

    mass considering test plan join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def convert_to_octal(n):
+        return oct(n)
