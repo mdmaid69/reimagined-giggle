154f13dc9eb0f022583ea9c8d1797ec1d576f4fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:57:05 +0000

    pour shopping impatient offer teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
