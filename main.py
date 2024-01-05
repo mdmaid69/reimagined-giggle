48b4460aad6ab757878e87cfa5e8c66d4bf8efa1
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:57:12 +0000

    danger sink own winter there

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
