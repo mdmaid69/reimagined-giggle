a6a0c88497628aeb56e0f4f8b28ec1f4b1b26d51
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:16:26 +0000

    embarrassed large bunch create pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
