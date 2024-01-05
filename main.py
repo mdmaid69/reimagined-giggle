601e9e1d34687d0e90df1afea4ff0d72e424354f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:16:34 +0000

    joke project male owe bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
