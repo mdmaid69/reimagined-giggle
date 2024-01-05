b87ff5da6ecfdebc7032b5b8339088538830b35c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:02:31 +0000

    public familiar scheme best possible

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
