3d5fc17945c89a56c267d95b63513d78ab23a508
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:59:44 +0000

    responsibility trip card future shoulder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
