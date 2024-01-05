e31dc75d127f7f5d56f3fed126ca38abfaa66389
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:47:33 +0000

    cut ear paper question worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
