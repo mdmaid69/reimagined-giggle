5568499327e6b32ddf49ba423a38d5aafde0a6c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:35:31 +0000

    rubbish cap pencil on break

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
