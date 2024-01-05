f69969118b12c9b8d638ccee5b56e76cb8114753
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:33:18 +0000

    rich shake mine bin arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_as_bool(array):
+        return bool(array)
