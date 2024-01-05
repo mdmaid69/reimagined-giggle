0b55b376e5e9042c09a0ec7b7ffa4f4227817b2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:15:19 +0000

    prize move late yesterday drink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
