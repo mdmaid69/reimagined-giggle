d7f0c13c9c2c5b4070c66fef82b92933ee2d1ca8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:07:57 +0000

    swimming mouth job book approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
