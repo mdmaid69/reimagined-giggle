dab811211ac7d36d1ae3acb97e6d472e48ae6496
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:52:43 +0000

    fond boring holdout only threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def cube_number(x):
-        return x**3
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
