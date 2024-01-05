7ee3324c67c641e0a185c7bfe7a63edbdce27a44
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:27:04 +0000

    odd mother round enthusiastic 565end

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+print(datetime.datetime.now())
