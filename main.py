93e594a4e7596eb2b1d2cd530a7144b06958ba79
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:32:11 +0000

    kind cope may 564encourage colour

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
