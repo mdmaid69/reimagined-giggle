d46dcd1f6f5ae23dbbce98d947088cdb941e4c09
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:58:06 +0000

    godown so difficulty protest pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
