114089e21f250c5e11611b8b7c37459a3cb37907
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:59:55 +0000

    bush guilty earn remove suit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_power(work, time):
+        return work / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
