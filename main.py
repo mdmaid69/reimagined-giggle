160e859397a2102e7f6c3cba59a3830d85896d7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:40:30 +0000

    happy turnover force next sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
