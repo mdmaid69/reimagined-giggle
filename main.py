73e98bd0dc7477f3011e237ad39d772877225070
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:34:42 +0000

    mean disturb authority pure comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
