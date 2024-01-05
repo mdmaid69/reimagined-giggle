1f75aa5b9aa6d3617e0e4732f10c08f56858af8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:33:39 +0000

    nose crash tomorrow before big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
