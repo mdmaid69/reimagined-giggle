24f43db87ee63de03b45a2cfde0bab05895e90b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:44:04 +0000

    until be factory ago male

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def is_odd(n):
-        return n % 2 != 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
