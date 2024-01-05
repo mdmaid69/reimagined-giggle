adce51260a59c92afbe7931ec90ee282857c71a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:04:07 +0000

    freedom bye raw nowhere long

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
