492d35b1c33b25d52b4fd3a9e4b1e69cead3c7b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:23:50 +0000

    fit arrive scare runinto narrow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
