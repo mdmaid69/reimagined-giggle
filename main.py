891ca7542d5b77faeaab36be268e5d2ebb5470c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:24:02 +0000

    explain of light getback through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
