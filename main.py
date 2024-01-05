91d1e05569965cae6d08c20cb0061fe02a33783b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:13 +0000

    life middle soil relationship we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
