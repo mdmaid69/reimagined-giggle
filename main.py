e586980e6d286c839f950d2421804f2c06d68e1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:29:13 +0000

    control all apply it ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
