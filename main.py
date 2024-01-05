9d6b38f2c3e501188a6cc81a6f64c7b1e3300912
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:41:06 +0000

    reasonable time minimum town public

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
