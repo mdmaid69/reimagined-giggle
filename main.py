f21e32f18c055434a4eea6ecd5596253336477e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:55:22 +0000

    involve sure decision turnup correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
