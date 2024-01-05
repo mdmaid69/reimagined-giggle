62e87240f2142b611898f983a936f6331fd06d53
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:23:00 +0000

    positive close height against building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
