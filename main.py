faab3f6c1ba327fd71420f162c2afdc6179ef7c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:44:17 +0000

    turninto date ahead ring father

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
