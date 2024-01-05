0b1c4994749557ed8690a7ef6b221b84262e4368
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:50:18 +0000

    improvement round Qq sound total

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
