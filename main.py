001095960330bf614396f969d5971c7b420f5f72
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:13:49 +0000

    clue there fear grand weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
