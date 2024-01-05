40047b59499d6255a53b27d037c79e6c0c269dc3
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:32:17 +0000

    low decide exploration mass seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
