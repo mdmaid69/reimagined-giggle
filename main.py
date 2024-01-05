cb6e86db85c8eadff1d360371097d47cf0080fdf
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:02 +0000

    impressed private print mail curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_elements(lst):
-        return len(lst)
+  def get_current_working_directory():
+        return os.getcwd()
