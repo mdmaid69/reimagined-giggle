913b1d0f6784e59c07565f90acac0963fdb2fffd
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:49:00 +0000

    ability high bread embarrassed see

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
