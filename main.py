debe8008604fa447c100560250fad1bc214f039f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:10:43 +0000

    high point meet by food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
