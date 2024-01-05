1740a1ec3feb7bb0a60ce5a7cdadf9296b467dbb
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:53 +0000

    workupto old come sound collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
