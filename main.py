f9d23c01bcce4fee9052c4651d3be8c93da7d91e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:46:19 +0000

    identify change far strike silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
