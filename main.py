15a0d2950badec10f7b7e399b0db759d375984bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:04:06 +0000

    watch gather care form lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
