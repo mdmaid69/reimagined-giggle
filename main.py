48b2941fed5dc5f49d8e71c758ea34c4d3c904b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:17 +0000

    over comeoff when black female

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
