747d4ca5366c650f1d70804fd1560549041f3c13
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:25:43 +0000

    point wrong announce important window

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_list(array):
-        return list(array)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
