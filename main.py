955da460930eb6c6192092849bf52dcf79c2051e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:03:04 +0000

    explain born believe project worse

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_str(array):
+        return str(array)
