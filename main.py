3a6146452c033608d07f5316cf7b9c0063537ac6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:48:47 +0000

    maintain rest 564encourage plate nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
