d7b7a8dcb91cc2948d09eb720dae909c858394b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:59:07 +0000

    cause fight suddenly nonsense 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
