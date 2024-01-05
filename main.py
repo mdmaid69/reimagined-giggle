a6e08f772d56ed5c3f5149700c3cadba423dd9cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:34:59 +0000

    matter clever side callback demand

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
