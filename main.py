1b402f9bc24caab3fbdb98fcfc7b8b129dfe253f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:14:42 +0000

    bath original mail official expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def clear_array(array):
-        array *= 0
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
