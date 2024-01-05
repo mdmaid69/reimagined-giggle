0fd8a1c9bd0771aca14a8df26977f639733a8562
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:40:15 +0000

    dream once chin arrive joke

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_work(force, distance):
+        return force * distance
