ec273a78c07aa234458457904868fedb1ee91fc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:03:47 +0000

    used history regret if complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
