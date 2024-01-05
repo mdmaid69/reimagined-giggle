66b091c88d9e7cd3178b85b9cb6cace1285d1b90
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:13:43 +0000

    tone many slice dress nowhere

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def convert_to_octal(n):
+        return oct(n)
