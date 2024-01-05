4652af894998d487523cf3edb362ce36104b9f1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:57:25 +0000

    store check perhaps face until

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+x = 10
+y = 20
+print("Sum:", x + y)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
