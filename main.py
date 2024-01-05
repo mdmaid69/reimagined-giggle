7f87092605876bfded976df9d5f3165410fdf170
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:52:39 +0000

    wake brain stare tell from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+print([x**2 for x in range(10)])
