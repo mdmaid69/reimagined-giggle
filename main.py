0488b51325af6375702b1244091f140887b4e5fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:21:41 +0000

    feeling represent tell gooff handle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
