41358ee68d92b222bbf3f9034a33199bea866290
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:36:40 +0000

    ball arrangement gas shade matter

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import random
-print(random.randint(0, 100))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
