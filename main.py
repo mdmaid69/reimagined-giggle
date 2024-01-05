9946b243b1d5ff4bff7723f7a6d76655856ea0df
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:22:24 +0000

    small smell risk ask secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import sys
+def exit_program():
+        sys.exit()
