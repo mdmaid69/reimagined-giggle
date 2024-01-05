68c651d0497d6fd23acf9ff4c6086855dec26c38
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:08:48 +0000

    same eager myself trap imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def calculate_area_rectangle(l, w):
+        return l * w
