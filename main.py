e6314f6fbbe74c94dced5f69565db2c75f6b0805
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:48:25 +0000

    fine towards deliberately weak satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_user_list():
+        return collections.UserList()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
