281579ebeffb437282922e56d7e202317f4fe7d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:32:54 +0000

    suit holdout stare whole means

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
