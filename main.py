3e88d126156f3d376d6f581e25cf3231577a04b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:02:01 +0000

    pencil century mad left opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
