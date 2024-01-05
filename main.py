235dcea4bab656abe5b4f2fc60f4de45b93ca7a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:05:42 +0000

    takeback available call see anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import time
+def get_current_time():
+        return time.ctime()
