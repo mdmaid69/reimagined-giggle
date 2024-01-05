8bebdb5492b807d9615846e021e92974162bb175
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:21:28 +0000

    muscle storm nor hand trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_directory():
+        return os.getcwd()
