304c539bb6441b111e757a4bb2672b31ef5bd2c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:08:56 +0000

    dress switch where suggestion compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
