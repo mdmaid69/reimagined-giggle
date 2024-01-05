5e31a7527d8428209484ec06b052879ff035e463
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:18:45 +0000

    ring track accident regularly truth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Max:", max(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
