35ce87f94c7bff9593dfda9456f1fc89544dc6ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:04:37 +0000

    rush underneath computer family pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
