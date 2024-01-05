321f2125e345d3bdcd542495e74df3efdfc76dbd
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:00:26 +0000

    level offence takeback stage the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
