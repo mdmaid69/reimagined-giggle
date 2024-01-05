5354f11fc69ab4b57be9b5cab1cf56656b250474
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:47:20 +0000

    takeover exchange breakin usually black

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+x = 10
+y = 20
+print("Sum:", x + y)
