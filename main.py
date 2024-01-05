578ed691146774c3c8b78e926bda47c04d33f762
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:11:17 +0000

    regret debt pretty introduction ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
