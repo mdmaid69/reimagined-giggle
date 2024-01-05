ee7da2fd4a3228e51be4704abca306b586d35428
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:21:06 +0000

    means platform innocent traffic cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
