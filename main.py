14a895f9a96593165067890847b04781ed5e5c27
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:09:50 +0000

    569enjoy repeat plain 573entertainment desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_set(array):
+        return set(array)
