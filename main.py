ff4fc7fad8bd42b8e94c0783b820c6a36f4403e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:24:25 +0000

    light later thankyou crowd chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_queue():
+        return collections.deque()
