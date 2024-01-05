ea37050574ca505ad3c1a0b5012b1316a575f1ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:14:13 +0000

    pot equal trip switch shout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
