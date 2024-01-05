fbd0750b37adc45aff934e0fdc5f7b324cdf6e5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:39:47 +0000

    affect wonder soft solve degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
