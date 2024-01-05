8a7388ab48af946e0a06eb844ce21c332dd624ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:12:11 +0000

    match wish embarrassing personal ability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def clear_array(array):
+        array *= 0
