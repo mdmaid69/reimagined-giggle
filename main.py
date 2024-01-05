fd3436ecbca5efb109aa8b2d310c146fef3825e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:21:28 +0000

    order bad climb crisis emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
