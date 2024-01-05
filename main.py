180c976aac764f41989e4410ed3bf3e695037ed6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:39:53 +0000

    chance confidence quiet ear get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
