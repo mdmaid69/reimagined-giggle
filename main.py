0d8dd22b23ea8c3b0950a825e8b6f4baa6f16cf9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:11:27 +0000

    science tear answer rare hour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
