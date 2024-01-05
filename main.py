ff61912e48bf761a12f04ee5f8c2aea54bf414ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:32:37 +0000

    professional destroy mine polite nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
