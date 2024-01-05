3b24f266521096eaf3c669baeb1bcd6979b2e6be
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:07:15 +0000

    ring plant square wear promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
