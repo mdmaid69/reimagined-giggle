10954183e8c182ce41fb0567bbdc438c678e976f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:50:42 +0000

    lead explain deaf not Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
