bbac132160806bd21f5d43ab10ca7a2e305d924f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:20:05 +0000

    empty profit region shallow slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
