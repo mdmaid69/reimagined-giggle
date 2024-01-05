a2f12314aede643243868f6e39627a66e4a47a6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:19:03 +0000

    give scrap dance silent mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_base_name(path):
+        return os.path.basename(path)
