2ab6c2c58f1147ad7f6bdf710ff639a0f4e79a66
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:50:36 +0000

    busy television shut ahead pinch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
