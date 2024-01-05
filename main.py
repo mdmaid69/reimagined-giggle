571b435ff5a7c84b9a55c4197d978afd6b0d9475
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:33:02 +0000

    turn then meanwhile one expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
