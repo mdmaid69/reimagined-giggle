217f304c863ccef40e46b3ceb0b0fd1e00f3ae86
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:55:08 +0000

    false stupid immediately fire switch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_area_rectangle(l, w):
+        return l * w
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
