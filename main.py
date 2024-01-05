99e3566d17806a9ae22dbd41359ae9afa2a16d82
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:01:47 +0000

    very club contrast pretend marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
