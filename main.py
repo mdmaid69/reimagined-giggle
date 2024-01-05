e461620b510e42b684b397327114acb8d166d832
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:57:44 +0000

    shut always crowded make confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
