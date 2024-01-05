632ceeb2766b8834488c35d6f4648919c42488ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:05:48 +0000

    sad must by relation rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
