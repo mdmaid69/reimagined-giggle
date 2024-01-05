80abeb3dd0b670d91fc76323246a71adc38bcebe
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:04:26 +0000

    shopping makeinto build quarter contain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+text = "Hello, world!"
+print("Characters:", len(text))
+def square_number(x):
+        return x**2
