11f16c7283ce1604faa59ccafec6df313e226306
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:25:00 +0000

    roll comeover best crisis picture

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
