2347940b9745ed6e1035ad9cdeb976cb28a40967
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:22:36 +0000

    active trousers male spend reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
