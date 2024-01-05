6c846f3804028460eaca54c5f7d2f53d9ef8b064
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:21:02 +0000

    communicate actual protest project car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tensorflow as tf
+print(tf.__version__)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def sort_list(lst):
-        return sorted(lst)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
