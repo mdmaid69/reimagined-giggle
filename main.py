d720dd7bcd16b1b8cabac196a68b9a73f8008951
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:29:29 +0000

    ride shrink point comeround similar

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import getpass
+def get_username():
+        return getpass.getuser()
