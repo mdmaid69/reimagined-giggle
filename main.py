f6c0e5c5ffd6dcd9ae1e42621052dc6ace0bdc3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:05:02 +0000

    play whom hurt calloff sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_error_function(x):
+        return math.erf(x)
+def is_even(n):
+        return n % 2 == 0
