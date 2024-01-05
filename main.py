7ceca0f392b6d4b9e5f67707692d899f6cd90448
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:40:18 +0000

    tragedy violent child cheap reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_min(numbers):
-        return min(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
