f2286bb370703d0e9ee243f4a170eb8e978c2e24
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:19:17 +0000

    expression power home Kk could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def square_number(x):
+        return x**2
