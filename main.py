eaef14029bed7a775ad5bd82ad5fba88269e727e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:01:16 +0000

    risk into congratulate discuss store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
