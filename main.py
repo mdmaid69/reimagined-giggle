a827090644a580e91501586cebeafa29f97a393a
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:11:21 +0000

    Pp everywhere waste hate go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
