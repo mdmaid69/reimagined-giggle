f9942377db80a7c98b73af8b0f31a4af0c560e6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:52:35 +0000

    necessary gap worry stop somebody

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(numbers):
-        return max(numbers)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
