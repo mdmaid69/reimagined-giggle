8e8be80831f2cf668a2515a4919e03a2aba03615
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:14:25 +0000

    undo his poor destroy you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_string_from_array(array):
+        return array.tobytes()
