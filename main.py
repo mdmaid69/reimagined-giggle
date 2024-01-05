68922f46baceeaa461459ef7398d9d82a31c9e69
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:09:10 +0000

    either stir persuade sad trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
