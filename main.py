de27631a1a2633dc44ceb6b61e419c1fff695518
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:56:27 +0000

    conversation mistake defeat apologizeapologise possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
