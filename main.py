4e95f3653b55278cae56b3d99a9fae8393975989
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:16:32 +0000

    practise noone you shoulder lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
