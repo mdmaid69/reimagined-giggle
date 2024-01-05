ff27c715be10e995f306d90026d07500549d8cfd
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:40:49 +0000

    pot disagree bring terrible song

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
