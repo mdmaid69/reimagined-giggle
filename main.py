b9f7094d377bb52d95fd118991a262967d6879c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:53:01 +0000

    find choose tomorrow confusing threaten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
