630ad39975b421b30b46ee10a6684c679caa6129
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:11:41 +0000

    around exam anywayanyhow keen transport

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import os
+def change_working_directory(path):
+        os.chdir(path)
