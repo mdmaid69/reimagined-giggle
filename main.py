480d4ec2764a908d74f0b1c538192fa322ac3217
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:31:54 +0000

    healthy pause makeup impressed negative

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
