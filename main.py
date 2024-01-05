61103a87409efccb77b089d9ea76179dc64f5f48
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:59:04 +0000

    science sudden belief stop open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
