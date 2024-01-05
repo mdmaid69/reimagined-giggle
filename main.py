126150d163e58bd01336419674985e78f6dd1542
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:12:55 +0000

    same private desperate quantity trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
