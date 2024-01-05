610d88e63377520141f31f7fac48902696d7b4a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:40:23 +0000

    other sad evidence insist mixture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
+def find_min(lst):
+        return min(lst)
