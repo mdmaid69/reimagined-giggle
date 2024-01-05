0667c6148eaf4488b5ff49aebe32c8f358ed3d9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:29:23 +0000

    bank Ll join demand Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+print([x**2 for x in range(10)])
