84b319b5a3cf0918aed6dabeb3938eaabf01b542
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:57:18 +0000

    valuable use fasten reduction patience

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
