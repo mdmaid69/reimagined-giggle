23f4ed4467a74fb25304d4065a4fc0cd946c2a19
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:17 +0000

    any snow cheque standard freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
