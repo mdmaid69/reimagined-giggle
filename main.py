e14c26a0b8b94efce8218660601a1cc628bef8ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:05:27 +0000

    wide tune unless scream turnback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
