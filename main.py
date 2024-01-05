408484919e46c674c7c9fcada16eb2bf63ff4ac8
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:31:01 +0000

    taste count miss downstairs team

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import sys
+def exit_program():
+        sys.exit()
