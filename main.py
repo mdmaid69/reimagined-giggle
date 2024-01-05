84b52344b9d433ccbca42a7993ba065183946816
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:56:25 +0000

    set onto brother knowledge steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
