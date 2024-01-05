190ae48467e8952d79aeadf2449b079a4d7d6b94
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:29:54 +0000

    around deliver satisfied wall city

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_unique_words(sentence):
+        return set(sentence.split())
