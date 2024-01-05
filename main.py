5a8edaab690534854d7d7e6d8f21005651dd378b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:51:21 +0000

    dance fashion arrive nowhere criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
