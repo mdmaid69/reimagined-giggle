e9fefa87b3865b2019ecf7d2b163d4ea60cf754b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:38:44 +0000

    city form after shall breakfast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
