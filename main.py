e93b244662404cc7015f21aaad1b8d86d43c9bf2
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:48:02 +0000

    safety news its equipment rate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def append_to_array(array, item):
+        array.append(item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
