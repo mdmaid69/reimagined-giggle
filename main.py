cfa53ee940e6a33f74c0073fc19bc18078bf073c
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:17:49 +0000

    build chase formal left past

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
