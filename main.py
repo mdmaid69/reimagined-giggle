fb7b453941dc76d0152498f1d17aa48c59eef6f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:11:06 +0000

    Vv advice damage narrow straight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
