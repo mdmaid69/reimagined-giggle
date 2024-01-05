55eb5fb83f82125a8c06ded0f99a93adef6024be
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:52 +0000

    goup noise advise welcome healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
