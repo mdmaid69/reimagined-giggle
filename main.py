6f997d00faad9030ba5b73c326e645f20c218e09
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:39:15 +0000

    house regularly place neat Jj

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
