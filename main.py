ede67f70f86464897424c5b69914bcbc64405ae4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:19:58 +0000

    everywhere transport Rr considering comeout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
