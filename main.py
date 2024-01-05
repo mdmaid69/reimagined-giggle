08cdddddb2da8ed9ebd9eafef0799086b9b1a37c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:43:55 +0000

    bury female matter settle they

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
