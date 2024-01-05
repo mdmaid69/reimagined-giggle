b3156648d968f04287e4cd2852142e9d3033283d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:26:25 +0000

    crash mind under support famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def add_numbers(x, y):
-        return x + y
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
