b456ba29906e14cf35b0962cf979decfbc9e2b85
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:40:19 +0000

    permission beside wise 573entertainment public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import re
+print(re.match("h.*o", "hello world"))
+def square_number(x):
+        return x**2
