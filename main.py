3a221d3cd2ee7d8a49c23018e039f90766ff51d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:16:39 +0000

    hair proud sit common flow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
