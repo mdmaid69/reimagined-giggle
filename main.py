d49d58f98fcae7be7d99b624cd73717bd67dabf6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:31:23 +0000

    intelligent list wire space scared

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-import sys
-print(sys.version)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def count_words(sentence):
+        return len(sentence.split())
