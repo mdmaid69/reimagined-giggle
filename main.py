ea234af13c25cce7c33e2f8f843379f8886848a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:12:12 +0000

    thirsty dull turnout warning Tt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def cube_number(x):
+        return x**3
