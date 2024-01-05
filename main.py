ce14f72735c730d2349b053f30ac2d95e2c7576c
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:32:47 +0000

    squeeze minor present recovery chance

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
