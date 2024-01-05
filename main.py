7c5e8a677bc22b2af70614f14c6151a71f26b239
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:45:45 +0000

    risk broad ignore cap big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
