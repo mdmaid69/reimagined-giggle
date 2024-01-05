60fcbe816e3e0080961bc113092a7c16ddf49b4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:58:29 +0000

    trick naturally more kick dark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
