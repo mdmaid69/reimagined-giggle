ad17011c9279e946b21e7fbc6d128a0fd84056c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:41:05 +0000

    steep confident high couple suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
