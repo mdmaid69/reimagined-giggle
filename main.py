d74e6e542b480a3016ec9ca417848f035f89a25a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:16:27 +0000

    finish broken priority pile accidentally

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
