eda9f4fb0e96f5fcf434ccd19f521819f3b7cbc1
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:04 +0000

    interested capital proud fun feed

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
