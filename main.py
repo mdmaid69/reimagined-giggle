8034c326b5890a8ba0db0edf8f17b820c3b4af54
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:07:00 +0000

    back free scale cheque offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
