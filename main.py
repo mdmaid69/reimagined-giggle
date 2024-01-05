64a995b215785ae5d4b0b4f991268e5096517144
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:44:44 +0000

    law pull answer treat drink

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
