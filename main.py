0e9a0ba2de287d25753b1e357fd3b2325afffded
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:47:27 +0000

    spring used doubt concentrate declare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
