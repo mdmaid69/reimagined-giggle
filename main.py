a6ebdc9aa18d4a1eeffaec7b60da90fccd8a75dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:21:56 +0000

    suggestion similar flat smile brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
