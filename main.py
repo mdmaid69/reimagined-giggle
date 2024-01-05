37301fcdf1c6e8f1c61003553bf7177ea10c285f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:35:06 +0000

    calmly real unhappy eager push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
