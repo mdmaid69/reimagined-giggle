e321cff4095775ea1984c16a087a329f44d559a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:05:00 +0000

    treatment scream needle damage bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
