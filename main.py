bc785e53d97a29f1159622f2a4dab2ea2ee7ec73
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:35:09 +0000

    song minority another vision still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def convert_to_hex(n):
+        return hex(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
