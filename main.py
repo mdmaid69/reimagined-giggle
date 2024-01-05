ad60bccc2bf28fb1a4432916469a36d0b25ebe24
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:27:40 +0000

    show holdon expensive replace often

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
