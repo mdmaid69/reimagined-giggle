fd6fdac0d8218c253682bf5ec3ae23a7cf6ad19f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:36:26 +0000

    case before weather clean turnoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_hex(n):
+        return hex(n)
+def convert_to_octal(n):
+        return oct(n)
