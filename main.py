7e8aa77e1ab587cd37bb6c7ec567c755f59a2d92
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:20:38 +0000

    fasten lie beautiful welcome supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
