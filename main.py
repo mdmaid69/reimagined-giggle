e6134a17ab5f52641e46ebcf3de384b34a743e5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:27:00 +0000

    stamp plan turn further situation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def clear_array(array):
+        array *= 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
