83234850cfc3734e7c1ea3b7191be37c721027c7
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:27 +0000

    separate pretend doubt cheque environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
