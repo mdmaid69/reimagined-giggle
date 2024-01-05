abdd4ab8186f417dde31e0eee1b0fcc1784d28b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:28:47 +0000

    score shake disturb below must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
