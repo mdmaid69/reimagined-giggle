367147f00b0dea978f8014dcf1cce487752211fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:50:37 +0000

    at normal introduction congratulations getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
