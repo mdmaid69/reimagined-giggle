29c252945bf70fd6eabf535fbf19626a580b058f
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:08:39 +0000

    interview remember appearance problem between

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
