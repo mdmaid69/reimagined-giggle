943e7669e5ac0d454d0b64e24d113ea2676428fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:31:24 +0000

    anybody left underneath natural major

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
