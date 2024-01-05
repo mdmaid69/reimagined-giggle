eb38aecbb1de5e1f47b1e0877d8bfec839d9f87f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:16:23 +0000

    type secret street danger indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
