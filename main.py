0adb5c4bf8e6770200df85fb60a58bb2b50a8f67
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:23:05 +0000

    cause around bound comment close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
