2843bcdbf10ab0e8fea1ad1ae5085a28683c249a
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:07:58 +0000

    stand would sit introduction genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
