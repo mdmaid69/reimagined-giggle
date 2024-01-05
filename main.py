77ded4eb9cb055966046308babb196959abb478c
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:26:44 +0000

    cause laugh up curious previous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
