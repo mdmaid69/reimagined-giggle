afdb192349874643e1660821552fe8a0a0f8b35a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:27:28 +0000

    convincing should sleep bother wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
