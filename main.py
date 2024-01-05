fcc7d08075c17b8d3114b8770c28b2076dcb1328
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:06:07 +0000

    frighten definite learn frightened confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def square_number(x):
+        return x**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
