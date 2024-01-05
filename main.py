1776762dbefdc6a8b27f254937287d680544e561
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:54:51 +0000

    dark desperate evidence music afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
