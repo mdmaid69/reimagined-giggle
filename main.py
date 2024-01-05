b7e32c4528694983e36867ab49ed169e08796dc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:58:58 +0000

    police tape sell pause display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
