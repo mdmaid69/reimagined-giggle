b5531ab405fde65bd205a0b0bd5463d874882809
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:49:33 +0000

    own complicated deal unkind every

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
