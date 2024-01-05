117cce865eb2c9e79e280203cdd9f8509b256b26
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:38:26 +0000

    child closely shoot bush wall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
