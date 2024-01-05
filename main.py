1a97cb098c236c258855249fdc093a8018cf03fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:21:14 +0000

    mistake land correct please case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
