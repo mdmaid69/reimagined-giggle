86a65634ac2424d050c043135c781102c35ad57c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:04:56 +0000

    alive single face instruction hole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_hex(n):
-        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
