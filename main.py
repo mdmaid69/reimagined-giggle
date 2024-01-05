1db346b0ad02df49b270b3c823a8c442d0985b5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:26:11 +0000

    even brain along movement coat

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_pressure(force, area):
-        return force / area
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
