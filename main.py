aef8d808f6749b765bde81433b7a21c869e84cfb
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:28:23 +0000

    properly remark slowly mixture pull

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_priority_queue():
-        return collections.deque()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
