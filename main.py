3ac48ca72da07617237ae257594705c4d967efe0
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:48:06 +0000

    on explain cook little slide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
