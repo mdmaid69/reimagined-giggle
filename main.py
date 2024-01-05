793ee99bb1d5e25fddc053c7d3993e4845662765
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:34:56 +0000

    square respect yours lunch game

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import sys
+  def get_python_version():
+        return sys.version
