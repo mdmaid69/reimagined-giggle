117ed5408e0cf32f659f1fafe81a528aef822379
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:37:10 +0000

    provide calmly history smooth age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
