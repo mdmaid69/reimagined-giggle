cb3e53a52233247289b838f12fab7102386e79da
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:19:39 +0000

    quarter worth getup where lucky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
