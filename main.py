f35a61b54d6604fa558b9057ac2c1334c4e4c14e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:10:13 +0000

    season willing aim Ff scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
