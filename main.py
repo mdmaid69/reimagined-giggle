278e1691ea25096e7403b1ef429ceb37fbb3bd59
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:38:22 +0000

    more relation among keep even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def multiply_numbers(x, y):
+        return x * y
