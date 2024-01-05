f61eae67937e816847a7e5e82af72c50a693408a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:25:47 +0000

    564encourage off classroom run excellent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
