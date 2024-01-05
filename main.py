d6277f36253f9bd18991bbf276130cfa01376054
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:49:50 +0000

    job show arrange later disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
