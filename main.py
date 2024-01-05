c3653780400b54dfa98b18ac0791ac98fee2d855
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:48:51 +0000

    loan squeeze of life oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
