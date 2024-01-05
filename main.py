26ec0d780412d67cc0436639f0380cf137d399dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:44:02 +0000

    reject workout rid round swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
