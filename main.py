824af0c3d50adb5524aa66489dbd9ba863792d64
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:59:59 +0000

    Gg common remove hide often

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
