b6e600c231cc96471003fa48b369a6fbf2089545
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:50:49 +0000

    positive compare chase store cold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
