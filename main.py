ec07c0481dc2e516b2c3d66586c31e3c9591b119
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:31:12 +0000

    leave lower repair gentle damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def convert_to_binary(n):
+        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
