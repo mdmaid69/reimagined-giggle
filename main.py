a41cae03cfe8f6d5476ff96bcc46ff3b73a5d28c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:40:28 +0000

    turnup little future connection able

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
