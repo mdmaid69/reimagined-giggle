75860662a4d73841c65542aaa828f9e60c7fbb59
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:30:07 +0000

    keen collection attach refer actually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
