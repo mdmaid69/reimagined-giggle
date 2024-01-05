7788643bf961c17c63bea81fc0d4076cc6a7f0c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:04:02 +0000

    certain turnup Ll national translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
