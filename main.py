71cff2401977a6f5fb6d4c567e7dd7112f5e30dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:58:02 +0000

    mark undo cry sensitive quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def split_path(path):
-        return os.path.split(path)
+x = 10
+y = 20
+print("Sum:", x + y)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
