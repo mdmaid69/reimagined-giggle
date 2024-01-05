1e490f653ca98e81025a7190ea56b48cf0850e85
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:10:44 +0000

    contrast dull hard trap rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
