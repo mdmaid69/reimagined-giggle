8d068f4fba7597ad0c799af5e56b2ce4c2c7d983
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:28:27 +0000

    close born its dead repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
