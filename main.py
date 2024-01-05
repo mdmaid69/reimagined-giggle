9fc2fb6fd07f6cfba92d27820012054550d0c6ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:14:17 +0000

    physical wash enthusiastic backwards boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
