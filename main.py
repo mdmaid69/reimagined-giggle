940585d7e8e3562cb6e04ba19cd3188d803df249
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:31:19 +0000

    neck place bank rub finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
