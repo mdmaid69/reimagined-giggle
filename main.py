58af6de2ec6ee79b7db801ee816cd72dddee4412
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:39:27 +0000

    exact her preparation stupid list

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
