0600cc9de46f6ef644fe020b3d68a18ac9d6f367
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:31:28 +0000

    nothing welcome high determined discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
