092deefacdc9c566db984466b6c4513777b0f936
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:54:02 +0000

    hit underneath doctor one new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
