aaa78279000b2761d1db50c10c5ebbcc7827ed47
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:40 +0000

    shout permission bottom land can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
