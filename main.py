e972a9ac6f54c68de86dbbcb27c27f29561c2200
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:58:21 +0000

    pretend uponon empty frightened before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
