2a28e83fa3213deb680f35d5b60d1b39bebac5c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:47:40 +0000

    great seriously pure active fit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
