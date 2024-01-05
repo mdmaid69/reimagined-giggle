086dc0e351fb0fb73397e04793dc87a0b046d44c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:39:05 +0000

    lunch sit lucky pronounce confused

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
