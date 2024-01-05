7d03aaddeea7f01a31629b480fd042d5ebde2fac
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:03:38 +0000

    best convinced back feature attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
