bf3f7e8678bb974ef6cafaa48a877fec102464ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:14:00 +0000

    tragedy stay uncertain hurry awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
