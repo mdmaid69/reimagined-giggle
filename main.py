45064f995cf7f8a3ea90b7dabbd8c76bdf8c96a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:56:17 +0000

    tour house block reasonable of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
