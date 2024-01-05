431e908765b4646743389a7826031a4a03d4c77d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:23:00 +0000

    crowd up emergency shadow attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
