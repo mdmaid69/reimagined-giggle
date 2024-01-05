b30dd17ef0aeeaf38b919ab2c74cd1eeca40a058
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:48 +0000

    behaviour throw error bend pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
