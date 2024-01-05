0eef2b6901f4e49220e575fc70baf42e460ca3f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:01:37 +0000

    argument wind truth lot runout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
