fd7d5b504e590763f01f7cdfa113cb6aa6006e2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:14:34 +0000

    programme treatment fence certain mother

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def convert_to_octal(n):
+        return oct(n)
+import sys
+def exit_program():
+        sys.exit()
