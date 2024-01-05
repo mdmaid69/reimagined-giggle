997b7206c443f60f9e03a9b5b91acfd02f30b5f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:28:23 +0000

    Nn goback(to) period tradition belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
