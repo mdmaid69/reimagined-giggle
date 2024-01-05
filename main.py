e00118d1a4a55fb3622745fa6b37a874bf7e7dac
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:09:47 +0000

    cause wrong absolutely sleep will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
