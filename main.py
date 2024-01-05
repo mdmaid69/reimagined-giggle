b6a850614ad76da2c88c76d1a727f1aaa8aec347
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:44:38 +0000

    breakup upset agreement pupil as

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
