bdb139545607efe3436423fadb19bfb83e7992d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:21:39 +0000

    risk season flow successful hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
