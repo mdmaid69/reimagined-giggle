8c4a2e11ca598e7e5c196edb6c76a964aae02810
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:13:16 +0000

    keepoff happy nerve position downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
