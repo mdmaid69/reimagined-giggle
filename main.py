f144b914d85bdf54e77b8f8aa89ea01f51c597b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:53:22 +0000

    Bb rubbish schedule in finger

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
