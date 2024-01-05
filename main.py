72acbefc2bbc58746ff23064d5c30336a1fc978d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:21:58 +0000

    breakup sensible rate doctor careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
