73a7c93b7e31c61b4c5011b62f11dee3fa1f17e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:45 +0000

    fasten steady giveup include address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
