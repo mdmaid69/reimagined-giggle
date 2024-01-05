bfdfa7ee78b8625a23f75fe7e5d7d9a9d570e5cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:42:26 +0000

    run against sentence capital lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
