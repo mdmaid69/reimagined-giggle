0fd3091ac57571f951df985f11f13f23bd249403
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:55:30 +0000

    beside appearance pressure powerful crisis

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  def is_odd(n):
+        return n % 2 != 0
