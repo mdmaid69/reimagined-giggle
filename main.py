ab8fa0e0cdfde2cb00d8d103ccc665a863b3680e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:03:03 +0000

    rest cover unit frequent ordinary

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
