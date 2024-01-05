5b4d3855642482a9ce3a0adf42dc1f1d1a1a7ff5
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:34:34 +0000

    teach winter path fight comeback

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
