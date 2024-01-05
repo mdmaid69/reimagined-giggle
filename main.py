49ed65f4a136430cb11a93e275939e44534e0d99
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:27:39 +0000

    unlikely care round minute weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
