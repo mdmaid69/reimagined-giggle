1b1214b28cbb377d24973e553f07d66071779e3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:51:32 +0000

    though along centimetre best line

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area_circle(r):
+        return 3.14 * r**2
