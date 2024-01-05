18d3a31490e0b8c9137976d2e2dd8de790e99c38
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:17:40 +0000

    however himself know perhaps attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_queue():
+        return collections.deque()
