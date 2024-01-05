8f1378ce107da071ffe82ffa568a9db8360d75bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:17:21 +0000

    mind lose problem emphasis telephone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
