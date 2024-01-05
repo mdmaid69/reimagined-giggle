8f05ebd1fe6302c4d22b28192f50eb2ce3d887f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:08:29 +0000

    like smoke sister herself punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
