30d92a3680b64467ca1ef69b5a1c12834f0e1617
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:51:53 +0000

    pure along scheme mine off

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def reverse_list(lst):
+        return lst[::-1]
