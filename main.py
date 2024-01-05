35949325714577de8f3b3844dd170e8894e0fc63
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:34:45 +0000

    summer card away reduce visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
