7dba93b3d1668570ee57a084dd97a11a187bbeaa
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:26:43 +0000

    right more partner pronounce mark

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
