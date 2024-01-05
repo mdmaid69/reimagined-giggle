e2563c248a6882332655ce8a7ded2322ca76b80a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:54:16 +0000

    active boat getout sweet centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
