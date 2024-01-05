5a7ef1548b722a435fbcbeb56b5fe2b080b9a999
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:43:19 +0000

    wild slowly scream simple over

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
