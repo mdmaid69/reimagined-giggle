a7083ddd561aacecca68f65062c599492a36cd46
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:51:08 +0000

    performance before catch wind introduction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def is_even(n):
+        return n % 2 == 0
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
