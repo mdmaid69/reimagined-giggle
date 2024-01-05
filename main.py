1131c64c454cfa69fa523ae25dd5d338735d24ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:33:38 +0000

    aim container refer noone instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
