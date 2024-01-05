5ed113c8f43a4d551e2aa4ffc87544645341e133
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:00:55 +0000

    beneath dance service callout score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
