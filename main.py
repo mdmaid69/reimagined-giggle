17a63a851d843f971a488cc27172cc7259cb093d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:23:04 +0000

    organization uponon justice normal so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
