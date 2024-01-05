839c8c15b89edeeb97f633757c77948829c70f57
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:30:42 +0000

    takeoff common operate food cross

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_max(numbers):
-        return max(numbers)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_typecode(array):
+        return array.typecode
