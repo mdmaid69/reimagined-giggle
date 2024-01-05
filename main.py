9d94e5191f65c2f98734d89efcaa86a586bb53b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:43:51 +0000

    dinner clue sight challenge standard

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
