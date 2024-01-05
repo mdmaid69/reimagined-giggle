854f4574879aca9553ab4d56df0aa1464ad87b58
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:19:48 +0000

    reduction keen release play fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
