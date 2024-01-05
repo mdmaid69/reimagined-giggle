0fa709f7680022c7e68154efb93c1df7d2e697c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:23:23 +0000

    calloff so world workout crime

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
