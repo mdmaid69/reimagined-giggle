36e2f732e9f0d904dbd55dc0c2cf8df20750025d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:40:47 +0000

    influence scale broad pity mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
