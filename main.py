ecbd477b1ff63150f7e741c77e92d714a1041ba5
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:22:56 +0000

    understand light wing emergency top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
