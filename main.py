29473ca9eb08eef9226c090bb2bb2c43bdd7c443
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:40 +0000

    target friend bottom spread within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
