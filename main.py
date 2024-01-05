b7f65fd5973166310dd42b9115616dee245e53ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:17:13 +0000

    cope Oo afterwards makeinto area

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
