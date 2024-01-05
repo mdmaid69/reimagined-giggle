3f4c3183b64089b6db4a6d6f8cb4c30d8be44f7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:17:51 +0000

    ask inquire shine permission turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def is_odd(n):
+        return n % 2 != 0
