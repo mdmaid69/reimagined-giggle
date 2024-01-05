66a4aa3143e5cbf620aa1c0386c5710d9810880e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:52:03 +0000

    rest bite lesson greeting its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
