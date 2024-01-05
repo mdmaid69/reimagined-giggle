388ec43ba88b9e4bc5ba56af063e5c6f54cf428f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:07:04 +0000

    praise major friendly uncertain themselves

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import time
+print(time.time())
