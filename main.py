3bd2d91da74c472689655f4c1f11c453d193e9fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:04:39 +0000

    breakup beautiful moment complete nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
