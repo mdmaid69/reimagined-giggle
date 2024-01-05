f7ed68584f2b92dbfadec05e5ab8b92ea42bf54f
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:34:17 +0000

    comeup gather entrance drop store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
