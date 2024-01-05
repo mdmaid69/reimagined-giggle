19ac244b075495fe6109980d91162ddaa686293b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:40:26 +0000

    use complete order whistle formal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_min(numbers):
-        return min(numbers)
+import platform
+def get_os_info():
+        return platform.uname()
+for i in range(5):
+        print(i)
