745ace366054e36d671021b8b78fda38f7ce70b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:44 +0000

    recent easy getback easy smoke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
