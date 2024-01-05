1908c2a09721498b4f0ebb053772925c27cac261
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:23:10 +0000

    Ll pay settle lead perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
