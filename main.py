898c3b18a25c6b11253b4bd0812103d03ba6e8d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:18:31 +0000

    until in getaway limit rest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_stack():
+        return collections.deque()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
