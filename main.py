0d9be9d330d368ac423f026f63901f918295d67b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:32:57 +0000

    combination sorry future finger sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
