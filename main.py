5813a28288a8b96cf6705618efed02bddd904091
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:52:39 +0000

    list drive Ee bridge damage

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
