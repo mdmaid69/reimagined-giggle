e1449b89c1a2e0586f4d24d183dc4a08887b2060
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:34:06 +0000

    wherever alone whistle tend agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
