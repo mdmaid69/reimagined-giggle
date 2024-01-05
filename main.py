2a211b875f1dd7751e71357baffc1b7165020d03
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:10:22 +0000

    whom dull hope fact raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
