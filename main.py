d6b856972b9c860b8f8da6b39a7e147102a44b22
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:26:35 +0000

    oil extra butter however slip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
