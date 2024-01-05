b4ceb6a70baceff66bcfd696cc7f522db75c5603
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:39:24 +0000

    explanation reply mine hair debt

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_volume(length, width, height):
-        return length * width * height
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
