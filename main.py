512f88dc3d6f64a4465ede6c9e251b31680bdf60
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:57:11 +0000

    admire official thankyou holiday equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
