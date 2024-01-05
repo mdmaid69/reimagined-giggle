6805ced0cc05968c51150fc893c8364aeaefcc1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:37:48 +0000

    appeal character bet marry winter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import collections
-def create_user_list():
-        return collections.UserList()
+for i in range(5):
+        print(i)
+import array
+def get_array_as_float(array):
+        return float(array[0])
