c6ea269e9a46358a153630ddbf06a1cd40fb1de8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:24:18 +0000

    family what round underneath city

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
