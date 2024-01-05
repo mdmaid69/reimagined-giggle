0fa5359bc277d5f06f45fa5101c49fd04034f571
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:45:34 +0000

    pleased period may cover active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import getpass
+def get_username():
+        return getpass.getuser()
