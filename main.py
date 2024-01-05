49b723e3bcab8e108ef0686d28eb947e8468832d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:42:56 +0000

    false Ff wait deserve uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_float(array):
+        return float(array[0])
