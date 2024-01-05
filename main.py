d1d3d9de1691a283c37fa314283c5b3ae1c8e359
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:17 +0000

    their lower repair tradition low

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
