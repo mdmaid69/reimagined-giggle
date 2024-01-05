349371dcc69c29af6f548a988902c7caf94552b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:12:21 +0000

    feeling pile callout thin or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
 def reverse_string(s):
         return s[::-1]
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
