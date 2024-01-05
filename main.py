4c00b9969d35c68ba6df8d9da09ef66c0fcb47a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:46:30 +0000

    passage confuse terrible attention Pp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
