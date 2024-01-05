6d89a8b05f3c5912cfeb17466fa6631c776cfc65
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:22:26 +0000

    result demonstrate fight important play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
