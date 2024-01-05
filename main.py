23a2cdccd131051ffe31e402449108bbfd37df34
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:15:26 +0000

    smile 564encourage measure form step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
