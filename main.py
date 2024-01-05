3ed0bfa482df0e885e6b90e7c4399d7cc3df5cf0
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:53:17 +0000

    speed naturally regular eat fresh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
