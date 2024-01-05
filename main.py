9dd99b1a287b36a70c15a0821c9d4e28c378505e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:05:25 +0000

    tough wave shop gate perform

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
