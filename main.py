c09aad88e3788c1c54196ed7bbdfe3d3b0c4f018
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:40:49 +0000

    comfortable birth worth disappear influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
