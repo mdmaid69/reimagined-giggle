a8e322ddc14e87c4c785555efbfd6a4a5e3c4eee
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:03:09 +0000

    middle long discussion instrument play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
