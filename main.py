c4217dee288cb21b54753df47448f820c2861cc4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:53:00 +0000

    influence stamp fit suggest suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
