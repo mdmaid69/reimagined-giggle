9c035600c878e34df4091abaf081df16b915b406
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:28:19 +0000

    brave attempt who anywayanyhow willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def is_odd(n):
+        return n % 2 != 0
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
