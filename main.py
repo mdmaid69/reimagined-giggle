7834162c88a529fa7c5878acfb9b5d5f19787873
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:21 +0000

    Nn show wind pool from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
