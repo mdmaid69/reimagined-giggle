0fce0105c4c42bac15827cabd93b2d2876485a74
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:01:23 +0000

    heavy rather table sea translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import time
+print(time.time())
