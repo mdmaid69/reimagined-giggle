97ebf670cd55d9692e7b1cdecf96f98bccb51685
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:02:45 +0000

    experience wind fork soil rent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+print([x**2 for x in range(10)])
