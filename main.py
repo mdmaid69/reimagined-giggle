e03ffafcb2a48bda7f28adbde1dc24b5321f4e8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:23:10 +0000

    necessary far she exaggerate callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
