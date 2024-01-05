553639c6d2778ae348a7b1a519ca9eb442ff5370
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:05:13 +0000

    view hold acceptable uponon rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
