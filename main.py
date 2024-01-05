21a140e4c075c283630e1fffa4e62ad8bb7d6dff
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:47 +0000

    round set criticize cold correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
