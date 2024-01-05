4418d0a405b96c0a018788b9f5ea35a79e91b3b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:34:06 +0000

    here produce machine professional minimum

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
