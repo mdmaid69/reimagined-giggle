af1fe6fcf13ae583e7d5e4844a111d0f77f16f6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:21:06 +0000

    your work smooth holiday fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
