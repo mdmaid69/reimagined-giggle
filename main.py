fd13914cb29cf4ad526343d5e974524319748755
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:17:28 +0000

    estimate improvement crash dry speed

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def count_words(sentence):
+        return len(sentence.split())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
