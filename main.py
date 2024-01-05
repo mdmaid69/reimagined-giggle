4af9810207541cb5ac95b91bfc3cd2e9aaf3a0cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:40:07 +0000

    hold justice occasional noone pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
