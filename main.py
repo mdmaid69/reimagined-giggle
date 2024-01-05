25ae67eeeb5d6081c2fabdf14be92376794796f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:17:29 +0000

    over rate underneath sweep thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
