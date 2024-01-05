3757a8562fb2df0c41903027b7243147099fcc63
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:48:08 +0000

    funny pronounce now regular bye

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_max(numbers):
+        return max(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
