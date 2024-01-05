cb867fded1288aa0db4d3523b369e9faca7828fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:56 +0000

    through free force absolutely theirs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_area_rectangle(l, w):
-        return l * w
+def get_array_as_bytearray(array):
+        return bytearray(array)
