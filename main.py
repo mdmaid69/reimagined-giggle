5a3ef87cd6b7e25751dc9ca5ea4f2e1957bdd24f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:04:32 +0000

    talk plastic this fill modern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
