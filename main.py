73fe73b11bf8db3cb749e27acc168db944e255e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:12:41 +0000

    effort drop company stupid necessary

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
