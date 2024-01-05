e801e25ddbfc14f81f96badd7c401ef756e4608e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:38:32 +0000

    guilty unlikely tap and cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_min(lst):
+        return min(lst)
