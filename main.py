37a6fda2709511653f1381a378eaa860c0184cc3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:25:54 +0000

    turndown secret very light do

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def remove_duplicates(lst):
+        return list(set(lst))
