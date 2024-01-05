783c1252fea6eacff59d7ede41c974968f403f79
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:54:53 +0000

    fairly breakinto store stiff underneath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
