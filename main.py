3e623226a9089d221c91c5a3f56c882dbaa29d74
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:52:37 +0000

    570enjoyable year tongue Zz limit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+for i in range(5):
+        print(i)
