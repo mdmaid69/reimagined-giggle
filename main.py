d1189105efc00e3a940a06f9212228e75035e91a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:11 +0000

    actually centimetre evidence once quit

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_work(force, distance):
+        return force * distance
