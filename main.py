8481d623acb77d8539e5bc5fc337e2e5c04a3e22
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:50:14 +0000

    breakthrough admire threaten Ee achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_float(array):
+        return float(array[0])
