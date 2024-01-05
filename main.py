387a56019e332cf46947216075a2ad0a5b4b49ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:42:24 +0000

    check get short maybe press

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
