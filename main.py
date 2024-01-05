74b0777733245ec96c143a20f9797a82d7a2a4b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:09:15 +0000

    throw bringforward heart anxious view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
