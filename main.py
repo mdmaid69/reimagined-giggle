0200ad66d1578160bc604339b0338cb2f91b306b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:30:26 +0000

    yesterday serious remember present many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import math
 def calculate_sign(x):
         return math.copysign(1, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
