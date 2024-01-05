55c0cb4316521851f6d02296a91cdabe4ab99d2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:28:13 +0000

    secret punish lot chat callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
