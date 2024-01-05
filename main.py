5420d019d32e22b4705f62a7a32007151d6a1f2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:39:08 +0000

    roll bet content today tense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
