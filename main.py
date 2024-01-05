850ee70e1abc0bc59195217c526408594af55222
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:41:48 +0000

    whether home down brief problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import tensorflow as tf
+print(tf.__version__)
