e7fc8268cf696770c5ccab8920d26f198a2f9230
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:11:48 +0000

    rate mysterious short part straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
