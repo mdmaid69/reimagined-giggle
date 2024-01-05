495fdcae4d835f6d045790afd71f2a76d3a218f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:13 +0000

    common lean mark back discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
