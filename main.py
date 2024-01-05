57aa13abb2ed6ca33995d5f13bf6da4db4f91119
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:40:06 +0000

    abroad black pleasant estimate list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import sys
-def exit_program():
-        sys.exit()
+def check_if_array_contains_item(array, item):
+        return item in array
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
