6b777ed0eba8a09e6692e23f8ff0b1a22ec64d07
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:33:08 +0000

    confidence touch Ff Jj smooth

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def square_number(x):
+        return x**2
