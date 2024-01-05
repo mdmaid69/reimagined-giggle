cc2e73014ebab757cdb118891ad38ebb7b6a31e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:23:33 +0000

    rather runover speech interruption rate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
