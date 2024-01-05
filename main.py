23a2516a19a262c24898cabc7df6a8f1b7eb5b9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:42:40 +0000

    image sting reply family quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
