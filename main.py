2cc97243d30b6ef797b9627a03d6dc38e209199f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:23:15 +0000

    train pride along public personal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
