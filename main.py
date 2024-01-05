2ec037cca28729ba46f5abfd9671dcd6e9366d31
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:42:44 +0000

    emotion trick wonder include fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
