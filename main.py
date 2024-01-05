bb18e501deeb2739709f80ca55e5fd1cc7efcaf8
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:21:49 +0000

    break meet extraordinary post goodbye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
