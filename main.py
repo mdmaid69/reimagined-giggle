0b425a307a36c8e124d7b43bb0bfb2e3ec61c33d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:15:05 +0000

    easy still crisis influence hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
