cbb078fa5b098d1a0a2d2eed568127883fd16227
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:25:18 +0000

    passenger unlucky stamp herself throw

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
