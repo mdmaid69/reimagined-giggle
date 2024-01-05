8ccf0899c8f7e078bd43bbbd28797fff9193505d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:59:47 +0000

    Uu stamp evening real Jj

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
