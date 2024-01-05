98c9f5576ba51029097253547679aae540c3be3d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:53:28 +0000

    protest snow sticky promise wipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
