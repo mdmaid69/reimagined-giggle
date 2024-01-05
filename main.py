413b2a3d6dc4ce5cc0d925bee45c7af5df1864c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:56:46 +0000

    grade eager pupil average table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
