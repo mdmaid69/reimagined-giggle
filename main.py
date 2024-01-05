16ea5a9b7334baa15b4568e65d1c9b0b96a999af
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:50:17 +0000

    back remove advantage middle anyone

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_acceleration(speed, time):
+        return speed / time
