72312c137bb65fac017f861380ee2a60134b976f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:20:43 +0000

    aim prize afraid disappear handle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+x = 10
+y = 20
+print("Sum:", x + y)
