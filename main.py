c5a40a8d2da262a4d46d1872098e9b78f7bc924d
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:59:14 +0000

    try interfere nose deliver morning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
