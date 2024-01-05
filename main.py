bfc677d077fcf5dd250ed567b38bc86e96ee2c25
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:17:05 +0000

    level broken wipe refuse pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
