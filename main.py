5ff06b884c596787ba1f3e65f00ee13a1d050078
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:30:13 +0000

    none influence hand wire century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_bool(array):
+        return bool(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
