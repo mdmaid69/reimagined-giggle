efbb253d52489958d689f672b36650fe8b73d77e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:50:26 +0000

    cheque lonely beginning pipe probably

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def append_to_array(array, item):
+        array.append(item)
