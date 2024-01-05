ffa4c2587c3f3ed1a81f231d9222a2ef0520f81b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:59:13 +0000

    which rather rank excited better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_user_dict():
+        return collections.UserDict()
