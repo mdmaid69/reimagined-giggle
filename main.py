6770a04c26825dd193874d3289d8ea79a7e19020
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:31 +0000

    sticky wheel bit coat move

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
