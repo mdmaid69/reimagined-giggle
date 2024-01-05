48e5edc39e4822f2c50a3c030dd075f8f26c9a53
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:23:14 +0000

    meet profit indeed single hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
