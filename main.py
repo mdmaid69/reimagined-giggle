b2b69085e420b7d7675067e8a259466512b6b6b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:13:34 +0000

    claim bite describe soft hold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import tensorflow as tf
+print(tf.__version__)
