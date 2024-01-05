3a4a5923104008e1053693a51a535a322cf89fd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:40:30 +0000

    nurse wish most pressure yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import sys
-def exit_program():
-        sys.exit()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
