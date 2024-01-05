181981a17d71c50b1be1499387ed56cad4bf0198
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:30:59 +0000

    give giveup the event separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def cube_number(x):
-        return x**3
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
