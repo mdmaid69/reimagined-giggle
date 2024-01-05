c6e4e5824be683f2d1f88351be0da5a10fcf340f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:56:49 +0000

    let throughout aim help delight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
