381279e2f05b946ec3da2dc3384692124d08748d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:27:55 +0000

    makeup contact visit wave throw

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def is_even(n):
+        return n % 2 == 0
