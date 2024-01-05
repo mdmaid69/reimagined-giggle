ba50752431273bc2382983a8e619cfce35d0b96d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:19:15 +0000

    Hh strict size appeal modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
