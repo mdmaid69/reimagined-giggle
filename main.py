66293becfd0bff7f468c8e58c492f0d66d2c766d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:40:38 +0000

    occur command bill rub process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def find_max(lst):
-        return max(lst)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
