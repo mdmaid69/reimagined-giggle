bd3c4aa8387e5f3ab1e739e37b218661769f80f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:53:54 +0000

    harm replace comfortable in occasional

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
