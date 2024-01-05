316cbf1c9c3bb7d51bb1868465d1a81bbaa9f229
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:32:33 +0000

    wheel actual lookaround progress maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
