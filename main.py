558d65e387c80a89dbea210f41e092518ba35f30
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:33:09 +0000

    desperate heart like brief limit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
