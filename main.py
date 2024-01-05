15b60bfb4f0490007a5facddfa37215e63ce8489
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:17:19 +0000

    interview 565end book sticky bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
