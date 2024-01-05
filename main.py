0d808c7e4793d54e5d57f31121eb73b86ddef340
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:18:43 +0000

    look worry wash badly ever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
