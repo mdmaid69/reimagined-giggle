a2aee661f9c95b0c52ae25ea96f662c8c02533bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:46:42 +0000

    I miss disappointed thinkthrough offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
