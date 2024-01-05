2ab82871879382f4cce40e28df718dbd9ade9889
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:34:59 +0000

    wonder cold discovery reserve arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
