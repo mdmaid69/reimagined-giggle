0a3b9d5e8f693248a69b19a088462fd33d4b9923
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:21:14 +0000

    wise sale board mystery 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
