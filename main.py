16c82b666f820af2bbb105c604871eced239e3e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:30:04 +0000

    distant leg purpose game club

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
