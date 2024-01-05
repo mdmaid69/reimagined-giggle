415a10289a8b5f5011c55462811da90b3670acc5
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:54:39 +0000

    anything film address someone scene

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Words:", len(text.split()))
