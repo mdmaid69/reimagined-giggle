cf006677a31d1211b763f164c00bb483519e1283
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:15:29 +0000

    either figure section pause react

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
