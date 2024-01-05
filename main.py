0d49a5a443ef066e30e46b971664caeea83db34a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:48:25 +0000

    simple listen manner sink 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
