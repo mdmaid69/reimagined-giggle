6d334cd940f1582121550bde42e800db6dfa7102
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:52:33 +0000

    play scrap eye song run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
