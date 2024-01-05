046f5a6de1d9961b4e0b95dc90efea2ca5c5cceb
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:18:34 +0000

    radio with crisis emphasis agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
