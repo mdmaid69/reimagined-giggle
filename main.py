cbba08d60b4bd0e2872d48ba38a9ed034ea3a870
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:24:46 +0000

    shake professional combine my appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
