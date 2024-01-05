06fd4284cd097da0a3d8cb1ffb25696500a047d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:36:42 +0000

    matter accidentally Ff them Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
