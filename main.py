eb95707dad3af12c2e1cd6b55acb73ea0cf84700
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:25:30 +0000

    apart surprised edge track draw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
