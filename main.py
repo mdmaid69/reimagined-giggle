39e33fbf78367fc011200d2b5646cf95e2ab7392
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:48:17 +0000

    far justice rough possibly charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
