5f5f0cb1f971cc5570b943b62fa001fcdd120df9
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:01:36 +0000

    store bank inquire tired lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
+def set_array_item(array, i, item):
+        array[i] = item
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_counter():
+        return collections.Counter()
