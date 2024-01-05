72feb12c5818f75badfa5a86480bb75543df7b66
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:27:01 +0000

    height hand class comeup drive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
