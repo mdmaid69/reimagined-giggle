d063c57514276ea80dd93fac34de167d33ab22a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:20:55 +0000

    distance squeeze trust occasion house

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
