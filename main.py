2ef9c8d952c2bea4ad0fc577ac46c110793d7c5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:07:26 +0000

    sense belong drink block except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
