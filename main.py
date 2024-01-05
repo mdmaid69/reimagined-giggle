9a7dcded1c97ccdfadf5b2ae95311e238bd6546f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:05:55 +0000

    sore female test rub plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+i = 0
+while i < 5:
+        print(i)
+        i += 1
