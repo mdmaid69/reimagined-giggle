439bf55897d277a5ca4161ccbf42c2b824eba9f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:11:00 +0000

    front really gradually clear difficulty

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-def divide_numbers(x, y):
-        return x / y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
