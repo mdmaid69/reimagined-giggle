b4dfc68f471e30229798f44d3b1b16e9ad8960b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:27:58 +0000

    sing move action keepoff steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
