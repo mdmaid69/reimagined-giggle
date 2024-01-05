141b233138a2796a20560078c522f8c0afb26396
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:46:19 +0000

    565end goon empty clearly police

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
