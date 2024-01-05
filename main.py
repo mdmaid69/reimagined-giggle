836ce084c5729bb2929cf58ed99ee036892f764d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:32:20 +0000

    because figure left judge alone

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
