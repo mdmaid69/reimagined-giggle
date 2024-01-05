164ca3d29c03b393c0294a7a4764b918dc13545a
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:20:28 +0000

    tend address violent space true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_acceleration(speed, time):
+        return speed / time
