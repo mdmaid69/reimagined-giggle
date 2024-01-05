1ea9895988556274bd23b0c2a7eb25dbdbbc7979
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:02:38 +0000

    strike poison press business later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_min(numbers):
+        return min(numbers)
