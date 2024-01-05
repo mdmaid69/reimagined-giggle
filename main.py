f95403cc982d9a317db5c7a41fd38557b854f568
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:04:15 +0000

    will comfortable finish interfere talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
