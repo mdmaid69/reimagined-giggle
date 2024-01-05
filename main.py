49863de60dc45899fdbb18fc039532676db8149d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:00:12 +0000

    every weekend bill neither comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
