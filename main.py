6800bafeaaea8d3a5ec4a7db78ce71442f33d008
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:59:58 +0000

    build angry by ever off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
