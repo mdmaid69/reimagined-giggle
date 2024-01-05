4a56a80c2125dbb486479dcc49c9d12da0ee42a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:30:14 +0000

    however protest rate mail ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+print(os.getcwd())
