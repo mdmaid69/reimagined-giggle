922e1adf82af034eeb106ffc0808eaade54b49c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:08:04 +0000

    ball somewhere performance rely mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
