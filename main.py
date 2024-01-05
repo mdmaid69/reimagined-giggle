eb85f97297b9602e5d980de98cead28f91697969
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:15:50 +0000

    suffer variety middle repeat show

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
