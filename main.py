e83e4d07ea1cf8f3692791a0d7e91bc6a64517f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:30:10 +0000

    push house trade Ee shut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
