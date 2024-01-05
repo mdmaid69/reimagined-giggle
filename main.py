ad5a378ba2f2aabe08c8f9c27e9ddb504ef0d462
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:16:35 +0000

    fill turnup Uu few strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
