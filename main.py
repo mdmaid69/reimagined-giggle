601460fa984ed22264d6dc9f8a085d637519445e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:41:57 +0000

    scene persuade pronunciation load goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
