30a22991aaa4200df587c98a7cdeba621a0c8974
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:04:51 +0000

    concentration off find peace must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_density(mass, volume):
+        return mass / volume
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
