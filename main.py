a155cfba89be04a096e15b6db263f760ab4ece50
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:44:13 +0000

    worth culture butter nor polite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
