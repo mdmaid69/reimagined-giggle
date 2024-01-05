971506f55b90478c5c48ca75ad3f80ae25d1f917
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:28:08 +0000

    Ff probably careless awake base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_sine(x):
+        return math.sin(x)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
