a9aa4d2375ab9a83b6fcc96779f6254ed6973801
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:09:29 +0000

    fix new save destroy pay

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
