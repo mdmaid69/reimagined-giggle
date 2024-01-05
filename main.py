1a335941aa32900a8c0b60fb0ce37b70d7d6673f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:44 +0000

    godown smile medium part Dd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def find_min(numbers):
-        return min(numbers)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
