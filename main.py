0b689e416204d38c45766a318cdeaafe238c0407
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:12 +0000

    separate figure than shape closed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def is_odd(n):
+        return n % 2 != 0
