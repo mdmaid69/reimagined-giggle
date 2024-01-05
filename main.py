a0494dd14fab0504349a57f109a45010d19a4da8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:32:27 +0000

    boat limit foot understand fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
