53ebd162689e1adcd7f6a8dd84bf81e9e7ad07a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:13:52 +0000

    birth steal wide beat miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import collections
+def create_counter():
+        return collections.Counter()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
