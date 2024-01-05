65bdbfca1831e16d5340c9533088f646593ca6e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:53:26 +0000

    reach finish Ww slip funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
