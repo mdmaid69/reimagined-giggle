9aa19b0201d428bb3ff5970a1a1b26ef2eaa2d60
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:32:47 +0000

    friendly far key leave effect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
         plt.show()
+def add_numbers(a, b):
+        return a + b
