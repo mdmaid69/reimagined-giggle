e1629acab001ba4fa5b2a019cc628b023f7199b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:14:15 +0000

    problem where hardly intelligent city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
