a2189377c58ae013a382aa5c3a8858c2eb455563
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:38 +0000

    close try giveup receive well

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_as_int(array):
+        return int(array[0])
