e677ad11a1f7388ce5c92656c286b5029700ecb7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:41:09 +0000

    getback land earth union reasonable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
