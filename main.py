5f6913671b00366e262b412431d84dcc76e51333
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:51:02 +0000

    event fact debt bread through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
