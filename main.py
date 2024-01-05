82c35221a3fa4014b4511e964e9908e75ac23a3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:54:55 +0000

    complete to disagree apart consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
