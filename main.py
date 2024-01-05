64348a85471f14f157e6923809bf77eba3166f3d
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:32:29 +0000

    thank embarrassing building mistake stamp

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
