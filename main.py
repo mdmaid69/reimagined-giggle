751ba931124099399c333561b697783e5e91aa2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:03:28 +0000

    bill proud extremely eat right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
