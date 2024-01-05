4d7964eb59ce674405bcc9c4bc65d85da62f45af
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:41:22 +0000

    smell extraordinary be not last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
