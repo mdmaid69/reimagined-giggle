905b26e31a18605421fbda90b41859fb1481bc50
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:31:14 +0000

    club friendly concentrate zero improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
