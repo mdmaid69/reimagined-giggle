2f7efa4790b783463f726742688e1427d4204068
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:28:09 +0000

    squeeze hang recommend absolutely value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
