3c8f7f783f1a4fe88342a3cb9f7aa66aea839433
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:35:23 +0000

    determined union recommend illness design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
