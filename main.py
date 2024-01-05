ace1f0a028b06491b9746f7993a1171ca8e97e85
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:39:10 +0000

    oil dry declare anywhere return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
