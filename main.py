50d1fbaf723bceb379cdb7d631436b0c5ad91193
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:59:59 +0000

    blame contact comment slowly luck

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
