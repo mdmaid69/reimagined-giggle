8fd4d86ac4befa25a21a5ff03169967e1dacd428
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:43:59 +0000

    fetch cry change minor worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_base_name(path):
+        return os.path.basename(path)
