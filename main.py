57159caad727915a484b76a89234c5a6773b8f53
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:22:26 +0000

    wall preparation contact makeup arrival

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import sys
-def exit_program():
-        sys.exit()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
