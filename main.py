54eee730efd6d0b143dbc95ece592b54d8bb498f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:51:57 +0000

    Ii understand command familiar supply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
