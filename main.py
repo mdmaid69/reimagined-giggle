bf00e4c5d0e078ac8b13c1af8828b4e557799a6c
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:44:55 +0000

    makeinto calmly nor dance constantly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_pressure(force, area):
+        return force / area
