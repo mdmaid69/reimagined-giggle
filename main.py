91a36dc88e2466c8d7983d8eaadf9b2b1dab9c8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:50:10 +0000

    content underneath distance sort speech

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
