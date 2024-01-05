d2ce6e164aafc024370479262ce02e563b94400d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:08:20 +0000

    culture help rubbish detail suit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_palindrome(s):
+        return s == s[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
