92cac202a93b77ec3862d87b6f6f22a11b9e94fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:57:31 +0000

    thanks wind leave probably reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
