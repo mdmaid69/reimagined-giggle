0b5ba721996a52f59cd38d397ce61f43cc3aaf93
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:48:58 +0000

    size giveaway invitation shop few

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
