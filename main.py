7c1665589de7b2bdb98aa912f165a8364d304d65
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:02:28 +0000

    wind make thumb lookafter payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_acceleration(speed, time):
+        return speed / time
