043fcdcffbf23a63aea3affbd59d38d812f7a13a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:40:31 +0000

    fight hard expensive callout language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def check_if_array_contains_item(array, item):
+        return item in array
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
