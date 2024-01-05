2d7e950c5713e3121dc81ca37fa2044ef0343021
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:46:57 +0000

    against your pair illness round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_speed(distance, time):
+        return distance / time
