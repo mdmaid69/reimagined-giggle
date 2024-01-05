37a8999feaac09ead9d505babbd412b51cd58885
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:25:36 +0000

    search raw young carry Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
