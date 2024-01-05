5b921ad097ced9dc2c60b150fb51d4c8658eee8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:25:30 +0000

    communication pot creature flight fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
