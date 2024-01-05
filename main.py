8fe6aedfd8be7906ee3ada51e97408449c78708c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:32:21 +0000

    sister environment contain destroy guide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-print(math.pi)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
