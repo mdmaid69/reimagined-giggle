9e0fd20708df6b793a64b202f97b6086927c72e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:10:12 +0000

    pencil impressive letter away bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
