e7c0ea9c5a3db2c0895f9c5b2e7591d9591b852d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:19:12 +0000

    not otherwise criticize like hungry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
