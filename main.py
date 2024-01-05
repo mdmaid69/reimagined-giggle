50a75387777948b6ccddbd30b89b697e50832e04
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:55:44 +0000

    Tt onto mainly mine air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
