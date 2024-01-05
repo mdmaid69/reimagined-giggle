8e26aeb330e3c4f42fc3a787f8f2d8c53ffc7962
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:29:29 +0000

    transport comeover that satisfactory use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
