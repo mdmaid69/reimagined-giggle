46cdf4b5f9949c00004d0b02fc20af82a42c7ca3
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:18:26 +0000

    action similar song into actually

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
