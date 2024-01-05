e00a04951ead1811da51dc8e271525d5dceed4de
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:31:12 +0000

    display pleasant on prize game

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
