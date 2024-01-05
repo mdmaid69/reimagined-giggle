a378279554f5b5664c2b8718739f8925a1096e1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:34:23 +0000

    suffer century brush circle off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
