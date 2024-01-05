f0316d703fea87228a0329f3e8b9edf6e72f91f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:09:26 +0000

    stamp why medium increase read

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
