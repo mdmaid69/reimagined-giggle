be71ffef990ab7fde8df58f97e4f3038b5c40cba
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:27:36 +0000

    loose over pupil call lookaround

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
