ad419e025c0c1f5d9da94d8c0056120964d7106a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:26:27 +0000

    emphasize female summer night character

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def add_numbers(x, y):
-        return x + y
+  def count_elements(lst):
+        return len(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
