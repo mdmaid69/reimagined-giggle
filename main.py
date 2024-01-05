9d5f1e47357493385e102eecd9f6e5749f1b6968
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:38:14 +0000

    bang test exam repair bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_density(mass, volume):
+        return mass / volume
