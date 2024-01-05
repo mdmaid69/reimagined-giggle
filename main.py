f4eae6165596e95c1e59c1e5e293ad4783b89ad6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:46:21 +0000

    mind coffee remove equipment leave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
