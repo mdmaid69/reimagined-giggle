77a0beb044d88d6a66c5899d76e44b8fff5a13f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:48 +0000

    instant kind still present put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
