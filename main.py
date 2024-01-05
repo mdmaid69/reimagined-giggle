8b6c276a542176596f4515f1e959fc604fa360d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:19:15 +0000

    566end kind meeting go dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
