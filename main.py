0295a87f2fc5834e3c0eba1df9d256afcdceb37d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:18:10 +0000

    pleasant theirs remember lower gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
