e42f2aab5fc598a8fc839cb979ba7538f4b062a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:28:10 +0000

    marry speed strength false soak

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+def print_python_version():
+        return sys.version
