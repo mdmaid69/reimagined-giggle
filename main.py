30857f12835cc158172ac01a08e2a64b58ace462
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:26:40 +0000

    way describe cost sort pipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def add_numbers(x, y):
+        return x + y
+  def square_number(x):
+        return x**2
