52509bef23255ff2f090c5af1799c49f573239e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:21:33 +0000

    bringdown proud when movement unless

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print([x**2 for x in range(10)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
