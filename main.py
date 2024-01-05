549d368496d41e332a905d6fc32f0a1e4fe13f52
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:56:49 +0000

    tough season stone neat check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
