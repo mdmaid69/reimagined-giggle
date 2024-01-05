488bd54675738f1183d45474629bb2453a1def0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:40:21 +0000

    shop war relax stiff colour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
+def remove_duplicates(lst):
+        return list(set(lst))
