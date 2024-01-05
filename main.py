7491993a01f851734c3f10f51f5210b6f3f5b3c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:24:29 +0000

    close outside impressive correction sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import platform
+def get_os_info():
+        return platform.uname()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
