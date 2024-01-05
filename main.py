78995631fbe5055b5188dc80885f4adebebcde71
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:44:46 +0000

    butter us absolutely sleep so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import random
+def roll_die():
+        return random.randint(1, 6)
