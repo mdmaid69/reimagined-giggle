4261b5b7d44c7cebfdd41a8922a80788faefe04f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:49:30 +0000

    confuse order nature production well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
