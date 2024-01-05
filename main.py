da56354433ad68f96669d24548dd060e2aa195fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:35:14 +0000

    calm feature Ee describe when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
