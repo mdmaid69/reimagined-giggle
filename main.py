81ecff12959ab0e8bc4dc78632b4122527ab9555
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:28:32 +0000

    block discussion grateful hear guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
