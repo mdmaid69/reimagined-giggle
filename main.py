049bfefe2c187b3d4b32be4d3e4184afdeeb583d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:51:16 +0000

    payment experience act guess light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def convert_to_octal(n):
-        return oct(n)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
