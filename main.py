972fe8239b2c0b8d2dc6915de9e2f547a41ace1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:59:00 +0000

    forward because unkind correction question

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ def is_prime(n):
                 if n % i == 0:
                 return False
         return True
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
