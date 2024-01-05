de8f9980268b0111f52fb78f6c64dc78f9f73065
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:53:52 +0000

    rob ought about drive pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
