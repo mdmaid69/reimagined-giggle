1b96cba4c480e474e53763a6eff440fbb6362848
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:48:07 +0000

    powerful rush through genuine while

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
