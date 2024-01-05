d8bc2b84b758e35b8ffb299cd3c53d0c346ef00f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:12:02 +0000

    realize explanation where catch love

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+print(sum(range(10)))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
