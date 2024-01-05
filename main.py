c2d2b3286eeeb179dc0131e20490ec871c7ba9d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:05:07 +0000

    Oo public lead wheel nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
