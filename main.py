cdf8b5ec9e05beacf4ff8eb64cf8d160433b116f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:05:44 +0000

    in birth means afternoon fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
