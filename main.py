8df0862fc7e7c2fbeabef16c3834973585e6c68c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:02:01 +0000

    entry step lean attractive lend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
