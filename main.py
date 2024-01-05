c994ded7922212163c7d31705880dff2bbde14d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:21:50 +0000

    so hope argue worst same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
