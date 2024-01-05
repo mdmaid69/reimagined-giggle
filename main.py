a281725c7be21b64a319a35998b1181d434addc1
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:33:27 +0000

    tip zero permission slide community

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
