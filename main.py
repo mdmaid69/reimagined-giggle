89916158b777ce51b01c57edf6c91aa0bc48c7e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:29:54 +0000

    contrast report complain patience cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
