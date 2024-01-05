bb77509ebf66858bb52cd81d2a6cc0917a6be9a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:39:33 +0000

    dull display Mm light load

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
