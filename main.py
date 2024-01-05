475af78374816b30b23dec8b177b548b98c14766
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:02:19 +0000

    deliver seriously flight increase basic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_array_as_int(array):
+        return int(array[0])
