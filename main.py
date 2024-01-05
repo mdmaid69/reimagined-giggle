6e5072eec35a146d6c3157eacde759f9675ba144
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:55:27 +0000

    wrong matter separate obtain smell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-print([x**2 for x in range(10)])
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
