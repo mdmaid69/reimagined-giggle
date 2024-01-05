6136306f16f3e477bc9354b41f65ba7235d73e73
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:18:14 +0000

    confused lead blame tall general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
