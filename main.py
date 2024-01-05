49d35ce2d05f003f6da02a5200aead6217a3284c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:58:45 +0000

    design proof forgive doctor emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
