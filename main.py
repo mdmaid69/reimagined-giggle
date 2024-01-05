66210186f3a5b0a12c7a43f4aaf24235f2a25772
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:50:56 +0000

    unusual moon even run attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
