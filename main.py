4d5a4e55e06165c25be4317fa3c03a0a4666d01d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:50 +0000

    position crowd friendly match trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
