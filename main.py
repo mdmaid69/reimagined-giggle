ccef51864978d05d35355fd860cd8d6920b72a16
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:05:00 +0000

    bread translate how balance near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_work(force, distance):
-        return force * distance
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_bool(array):
+        return bool(array)
