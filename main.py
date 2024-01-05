261f3def3cb86635fbabd6885a3a17f32960b949
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:27:05 +0000

    confuse mother once wire quarter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
