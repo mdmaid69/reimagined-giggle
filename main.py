7161b158cb817798e49ce55b9c7234bebe26b2a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:06:24 +0000

    have wake trade interested crowd

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
