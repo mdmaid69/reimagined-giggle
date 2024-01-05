a8e2533e657b525b9a273ad6b4de5b70f3f7e891
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:59:55 +0000

    size suppose worst deserve goback(to)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_bool(array):
+        return bool(array)
