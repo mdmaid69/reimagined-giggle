4627e4acbb2597dbb11cdd408c2dbe8b4f34adfa
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:15:39 +0000

    section Qq side perform somehow

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.fromstring(string)
         return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
