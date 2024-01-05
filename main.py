802159c6cf85277dc8e122b21864b5de8449d9dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:25:52 +0000

    autumn switch correct complain pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
