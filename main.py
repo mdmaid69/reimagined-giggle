32578315ac6334377451b65514ecde03ae426650
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:48:15 +0000

    performance roof miss roof race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_as_list(array):
+        return list(array)
