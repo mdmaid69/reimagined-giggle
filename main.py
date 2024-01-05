0ab638cd55a4fd21049833142cb674e34c1c7961
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:42:14 +0000

    hit woman before slow basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
