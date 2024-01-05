88d06e2cad5e94c5fb5d7ca9b2c8977aa7723541
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:08:38 +0000

    sorry cold reliable nowhere interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
