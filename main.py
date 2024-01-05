c1a320aeeab39506f950f51c3b5e278008fa04da
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:04:52 +0000

    realize giveup compete claim combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
