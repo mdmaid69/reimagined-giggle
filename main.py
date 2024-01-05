30751bbedbdb3662e669c58bb5fe07ece35153c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:56:04 +0000

    movement towards centimetre belong song

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_work(force, distance):
+        return force * distance
