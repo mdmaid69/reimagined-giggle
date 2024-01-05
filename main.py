41e4c4998257c8c1bfdc2861e8ace3a6fa017004
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:01:14 +0000

    breakdown risk remember taste completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
