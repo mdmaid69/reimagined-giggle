c25eb3dee83fdf45590bd6a5355e790b24a23265
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:49:26 +0000

    shout window without match treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
