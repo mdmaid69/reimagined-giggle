ee90539c5f3a0bead824944fe002ebc6fe77878c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:38:25 +0000

    takeover wide expect expensive display

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
