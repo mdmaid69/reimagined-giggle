e95ed7bca25ed237849352ba25b614454b4469cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:39 +0000

    coat impossible slow produce bringup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-print("Hello, world!")
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
