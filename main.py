1baff3185feb7d02913a10e099c3d866a1ef14b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:00:12 +0000

    can who disapprove recover seat

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
