177fb62626929a6c8538b3c95ceda7e794b65665
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:16:51 +0000

    bang would best little grade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
