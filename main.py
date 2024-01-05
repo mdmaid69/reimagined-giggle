83b7dc3fa1ac93968a1d4ab99c7b6bc8f6e9c44d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:31:21 +0000

    or that stop dig measure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_speed(distance, time):
+        return distance / time
