e3f281680b9ce79f1c87d33376ade122e8381414
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:39:44 +0000

    treat warm both comeround strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def convert_to_hex(n):
+        return hex(n)
