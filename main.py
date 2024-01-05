ca7f43165606cce3e4f27a8ee3b6dff6f321df07
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:43:01 +0000

    announce lunch reach home sensible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
