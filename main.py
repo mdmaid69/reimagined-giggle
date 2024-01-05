10a996a8652cd18023849c6e7bd6eb05d0620fe8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:59:34 +0000

    conversation about win touch pupil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import sys
-  def get_python_version():
-        return sys.version
+print([x**2 for x in range(10)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
