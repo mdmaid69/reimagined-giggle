5d3c26b114c6f588be3e75ceaf246914da0bf200
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:52:45 +0000

    come yours block practical wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
