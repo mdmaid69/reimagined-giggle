1fdc716c3fe80548701bfaabe770be2770cc3cfb
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:44:32 +0000

    comeover emphasis winter trade lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
