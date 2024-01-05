b1de9bb0685ae49260c1870ea78587cbb8dac551
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:50:22 +0000

    silent pupil trial stop stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def find_unique_words(sentence):
+        return set(sentence.split())
