38d5808269bda4ac864c24e0d108636a01b58a60
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:20:11 +0000

    dream record love used soft

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def subtract_numbers(x, y):
+        return x - y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
