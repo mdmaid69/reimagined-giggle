ab79bbd7c5d70252785bd1efb1ca96ccae1a0d90
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:26:42 +0000

    act file raise yet off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
