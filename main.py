75dbe63fa68a35b1c3c0a77714e7fa8e18f91e87
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:53:45 +0000

    clever ignore protect innocent Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
