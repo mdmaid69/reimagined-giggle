ea262ea845ee3380eb480e899e90bd4fe72b6190
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:35:09 +0000

    might accidental steep afraid rely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
