b786ef7801c4a89d0314b5e5a775dde1f10742cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:50:08 +0000

    insist almost beginning straight blame

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
