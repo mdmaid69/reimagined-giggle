0b36588fcb28ed88f586b14936506b3887cf2337
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:25:45 +0000

    near them minute ashamed straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
