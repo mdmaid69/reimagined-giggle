10580c68d13493d223b2f02e93fdead1bb4058ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:35:43 +0000

    partner rent into treat uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
