a7efb6e9997016a64793a84c18d14e9da8f3fd10
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:56:55 +0000

    he lookaround demand deliberately borrow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
