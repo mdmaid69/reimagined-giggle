551d9b719d0571e866ae4b33a855137e1f376755
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:57:57 +0000

    far prefer flood persuade invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
