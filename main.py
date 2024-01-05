a1b1e2a5d15afd6400bfb33e89dda240eb98d6ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:32:54 +0000

    cheat realize bye set not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
