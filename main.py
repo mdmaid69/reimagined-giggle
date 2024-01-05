080aa9dbe426e954c231b55a98bd076d885ffae9
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:11:26 +0000

    bite identify object majority comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
