97aeddde57dc087f74bffe806a6b26bdcb39d97b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:57 +0000

    lend swallow obvious nor either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_power(work, time):
+        return work / time
