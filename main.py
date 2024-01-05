b8423c1e5bdb8e84664dd9a606f81e7106765f04
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:40:22 +0000

    strike female passage with angry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
