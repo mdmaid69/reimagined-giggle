40f14b13a9e3694255f14e15a0690657a1c59736
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:04:44 +0000

    eye crash broken blame emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
