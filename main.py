e1799a420e79aade53ad511e43552ca0fb6963e7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:40:38 +0000

    instruction contrast shape reject form

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def cube_number(x):
+        return x**3
