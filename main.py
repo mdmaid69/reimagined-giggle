14f7790770ed970afef7be456b94b6624dd36b0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:51:18 +0000

    admire advantage quantity amount track

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_list(lst):
+        return sorted(lst)
