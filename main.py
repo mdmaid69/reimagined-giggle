b4b4f0374d6e05bcb817377df8e6715f12a08018
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:29:07 +0000

    swallow proud nor still programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def convert_to_binary(n):
+        return bin(n)
