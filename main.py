0e2f1740fd9179df40b93d41378495a872e06c05
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:50:14 +0000

    taste minimum accommodation at tap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
