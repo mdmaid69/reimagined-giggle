42158ebd66f3b0066a4a93c74876f908f3586ea0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:56:04 +0000

    record trip cum occasion curious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
