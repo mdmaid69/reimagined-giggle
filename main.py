d65cb91c0a397769674fec054b7b1f51e8bbfaca
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:34:27 +0000

    funny big fix godown seriously

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
