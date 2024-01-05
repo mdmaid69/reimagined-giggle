369aef374825e04bfc23f36f3476842a6f2f281f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:32:11 +0000

    land holdup reject mix wheel

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
