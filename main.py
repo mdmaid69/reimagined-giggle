6e991d5395c11c784c41353e713f4d6da4905317
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:56:00 +0000

    apart intend dangerous exact practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
