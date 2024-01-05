773429f8feb42eb8458c70837e4b916ac37bcbd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:40:50 +0000

    race trap comeoff insist air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
