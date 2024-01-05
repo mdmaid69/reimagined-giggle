8f24b84f969c33e14cb063619bbe4bde4bf0be56
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:44:07 +0000

    teach occasion central tongue whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def remove_duplicates(lst):
+        return list(set(lst))
