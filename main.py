6e3b796889e3cf3b73a08e0bbb734a1acd98f146
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:45:55 +0000

    means stop permission knowledge unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def append_to_array(array, item):
+        array.append(item)
