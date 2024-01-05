6a3313f5127b714345164f6fad984488d7e6c55e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:20:25 +0000

    load freedom that bag best

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import datetime
-print(datetime.datetime.now())
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
