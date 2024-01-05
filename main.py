9a37c8eb0d96f1d752f997496d7a0ce1f848b7b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:43:18 +0000

    eye loan squash position card

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
