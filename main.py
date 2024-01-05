d16220c2d10ab8eb8ea01407b3e90b7ec2bf9172
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:25:05 +0000

    experiment laugh friend correct how

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
