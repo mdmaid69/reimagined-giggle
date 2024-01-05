1b92cd276fab5e7d88ad0fb24cdd47740b59c013
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:20:09 +0000

    fall boring comefrom reduce owe

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
