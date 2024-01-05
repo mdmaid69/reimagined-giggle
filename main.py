18ed1bd59f20ee75033d71c6d2892cf24084d89b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:07:04 +0000

    present wish last sad stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
