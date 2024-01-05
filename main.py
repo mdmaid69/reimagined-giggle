a27472e1ea7941102c42528a9d88494f47ad36b0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:12:32 +0000

    blame takeback concerned lookup thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
