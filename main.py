3a302d49535b2f09bdd5a48ca604c5d03b9d7d00
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:08:22 +0000

    neither morning as total harm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_odd(n):
-        return n % 2 != 0
+for i in range(5):
+        print(i)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
