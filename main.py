927d96c7bba6c132774b33847c8574c86637fba0
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:43:06 +0000

    once pain stupid standard different

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-print([x**2 for x in range(10)])
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
