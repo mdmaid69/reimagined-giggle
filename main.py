67c7e26deee3e51e16632736e24642366c507c96
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:07:44 +0000

    refer leave mend true much

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
