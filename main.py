8d66cdf8392b10fbcc10e8b4810690c047a3952f
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:42 +0000

    sore estimate mother live sky

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_bytes_from_array(array):
+        return array.tobytes()
