e284640d93101faa92c39280861833f7dca7a746
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:39:58 +0000

    trade drink speak refer lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
