b6585b36839d861ed3daa67d842be440772da1c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:59:24 +0000

    achieve club and question Ll

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
