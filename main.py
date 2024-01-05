213cbbf95c59b798dbe4eb3b26874b9515c1ddbe
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:14:43 +0000

    breakinto custom vote ought old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
