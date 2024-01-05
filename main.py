aded37dffbf98dd2ba127eb899c725094e8a895e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:08:04 +0000

    error score outside or build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
