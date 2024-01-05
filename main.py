6388c386488d229353e1dd6fb278ee794c1396a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:46:20 +0000

    waste experience smell instead especially

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
