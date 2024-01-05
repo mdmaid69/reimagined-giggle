a5808b05b6e91e5f1608392d371a7ffed0c25614
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:53:06 +0000

    weather worse Ee until generous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
