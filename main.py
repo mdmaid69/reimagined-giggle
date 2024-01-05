6e81659de9979f265017f6e95ff7ef874150757f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:10:16 +0000

    physical score either ourselves promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
