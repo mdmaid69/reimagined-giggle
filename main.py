509ddccc9b50fe0ed6709ef4356e449c6a3460c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:32:38 +0000

    ashamed measure fierce damage total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def convert_to_hex(n):
+        return hex(n)
