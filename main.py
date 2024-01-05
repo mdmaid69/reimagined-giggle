beab61231091cd455913b01acaa5e17cc3039b33
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:44:03 +0000

    pipe swim comment head him

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
