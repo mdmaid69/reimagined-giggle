313324052668420705727bd8f4af11acea9b6d31
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:16:13 +0000

    minority ring score loan serve

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_area(radius):
+        return 3.14 * radius * radius
