27b0b02ac50a4e349b741256283c7f385b829b78
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:36 +0000

    turnup praise stay extraordinary side

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_str(array):
+        return str(array)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
