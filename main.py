5a47d984c8494060425f43a8e4c91627366f80d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:25:25 +0000

    course importance top interesting bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
