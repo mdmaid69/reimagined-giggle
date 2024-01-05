c36081aa4edb9bbc94fb5ee26c4d30bc6b4d511d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:36:15 +0000

    no bored anyone foot keepup

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def find_max(lst):
+        return max(lst)
