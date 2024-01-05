095163f43a718be8e5f189efbebc106f8b3cff04
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:00 +0000

    whenever dress exchange show the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def multiply_numbers(x, y):
+        return x * y
