7bfc79037856ff72043fa34ab66b99eacc729348
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:24:34 +0000

    right region call thankyou generally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
