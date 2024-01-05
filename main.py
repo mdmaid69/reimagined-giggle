1e5875a0ccf8ceedc15d02509ef752680de52bb7
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:58:54 +0000

    neither organization price warn interest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
