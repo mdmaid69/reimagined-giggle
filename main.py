5bc47727da416bcc00ffbccb946071b9aea7016b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:54:19 +0000

    press force and turndown drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
