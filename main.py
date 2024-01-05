f4d6f1343d949fe4c863e27fbd41147279e69152
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:21:15 +0000

    paper reject crime job begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
