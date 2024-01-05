3aa80fd4f64b5ecd1083806e3bcd038c39ec9c2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:30:11 +0000

    passenger degree rate fortunately circle

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
