8e5ca1da3e73d048c3c4a06b003702bfd8db8449
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:22:04 +0000

    content speed then will patience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_counter():
+        return collections.Counter()
