d0a39e831da0386093a53e90dbb6f4710dca5a6c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:01:25 +0000

    squash treat study track famous

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_current_working_directory():
+        return os.getcwd()
