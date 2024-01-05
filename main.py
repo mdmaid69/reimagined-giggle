bd5560fe36586d4285c08907e135e820d96e93a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:21:29 +0000

    cheap late study used joke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def multiply_numbers(x, y):
+        return x * y
