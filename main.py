cb757ce99a1ce85bfce5b38570a97f1bb7933595
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:59:44 +0000

    sort floor look pay reach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def find_max(lst):
+        return max(lst)
