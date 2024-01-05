56c37e77326df4916404eeef808f13d5055721fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:57:35 +0000

    circumstance recent guess clean mystery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
