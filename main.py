5458c7d1622402c9c74e4794aa97f1b89d588215
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:38:01 +0000

    offer suggest workout tradition test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-print(sys.version)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
