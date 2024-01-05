4cc3d3cb0b0043364e13385386396a2aaf65a3fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:34:40 +0000

    thin model issue Gg consist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
