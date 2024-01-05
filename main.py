f3dd19ab5a1b8aea18c89fa331f636b51144b943
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:56:38 +0000

    crisis believe bridge beneath please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
