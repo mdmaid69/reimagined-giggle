806f967f5add17ba23a6538a592b6bb744c3dcda
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:26:15 +0000

    Kk passenger tip different cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+for i in range(5):
+        print(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
