7576cc79f0eb7bdfc4add58bc19daeff759a68f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:27:26 +0000

    rarely copy charge watch trouble

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-x = 10
-y = 20
-print("Sum:", x + y)
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
