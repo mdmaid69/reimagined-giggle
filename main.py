ae2e8248c1757d1ff3aa98ac5a17939a6578dbe7
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:16:10 +0000

    news understand completely other will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
