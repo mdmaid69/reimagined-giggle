74fb6f878bb243a2d09812cf19d897da979ff6da
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:22:52 +0000

    acceptable turnoff wherever write pause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
