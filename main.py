bdb9760350c126c50ad00324927fbd7a6547e4e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:23:08 +0000

    night sweep name there improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
