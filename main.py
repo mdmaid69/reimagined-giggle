7e7372d5d2a8a93b5f2ac7c423056a4178206bcb
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:48:04 +0000

    arrange treat impressive follow flight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def add_numbers(a, b):
-        return a + b
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
