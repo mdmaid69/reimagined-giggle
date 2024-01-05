f5d966e9a34148b18319f2653818a38a197459ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:04:04 +0000

    play another total practice raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
