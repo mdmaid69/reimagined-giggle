7ed06e17cd451ccfd249d550624af22ad1d95457
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:31:02 +0000

    can escape interesting empty respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def convert_to_hex(n):
+        return hex(n)
