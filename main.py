75e39d60a38f60d29112a315eff5fe84213177db
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:42:45 +0000

    bet mess 572enter walk Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
