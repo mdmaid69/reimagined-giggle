06008a5657a9d5a11cf077d7d79178cb159880f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:39:41 +0000

    pity real city hard human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
