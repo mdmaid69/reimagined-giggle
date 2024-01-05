67228aff04dd086242486bc7dc5965026c68920b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:40:02 +0000

    inform record punish way tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+for i in range(5):
+        print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
