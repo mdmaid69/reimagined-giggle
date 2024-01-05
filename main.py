c45fc94541e57471015ae619029d2fc73fa41f72
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:18:03 +0000

    principle describe mistake congratulations arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def divide_numbers(x, y):
+        return x / y
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_array_to_list(array):
+        return array.tolist()
