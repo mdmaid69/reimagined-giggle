37ed519da1e9a9d6f52faa2214fcbf3d36965ae3
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:54:18 +0000

    discover deep straight fierce small

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def list_files_in_directory(path):
+        return os.listdir(path)
