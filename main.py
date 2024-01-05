fa5e730ef3152282dc1b75ac93a402b1a62fdaf4
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:10 +0000

    comeoff weigh interesting he roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def split_path(path):
+        return os.path.split(path)
+for i in range(5):
+        print(i)
