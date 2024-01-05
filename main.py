f3b98f8848bd9464f93765fa0f4a0fc5b5f3bad1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:19:16 +0000

    deliberately boat Ll right be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
