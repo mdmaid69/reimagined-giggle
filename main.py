4190d84ba9e6831dc80c3b2d504c4b257c38d99d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:57:31 +0000

    scrap bet responsibility ready battery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_acceleration(speed, time):
+        return speed / time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
