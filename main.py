f1da133cc8c11ccf4566767ffcebdc4e6ed80c9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:43:55 +0000

    design number switch almost runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
