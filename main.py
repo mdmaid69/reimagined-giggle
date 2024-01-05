a76ada97bdd1059c88d755c66392a5373125e33e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:54:25 +0000

    suit improvement container anywhere speak

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        return sys.version
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
