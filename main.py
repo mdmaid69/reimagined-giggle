93043cec2c974e53dd9f1d3f3aa804d6756146fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:58:07 +0000

    however content tidy still getup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
