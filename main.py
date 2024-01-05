af998b140c4256cc3aec7fccb9813bff0c59ee4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:30:40 +0000

    none task accidental regularly but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
