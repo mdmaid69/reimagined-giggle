2d4202026ad881d2fdd90a8d6896bcd43386df17
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:34:13 +0000

    scale art but list edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_speed(distance, time):
-        return distance / time
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
