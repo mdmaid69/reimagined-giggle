5cc3c942f7c79411b6a97d8b6485e1de3325ecd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:30:34 +0000

    once 565end report size relationship

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def remove_duplicates(lst):
+        return list(set(lst))
+  def remove_duplicates(lst):
+        return list(set(lst))
