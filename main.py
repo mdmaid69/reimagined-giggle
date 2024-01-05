7bfc4d13e86dbb0d03694a90a7a023f9c403ac67
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:20:01 +0000

    make trap walk software characteristic

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
