2e3ba90cee830dc688b2e88bef061421e1df86dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:15:22 +0000

    spot software whistle battery completely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
