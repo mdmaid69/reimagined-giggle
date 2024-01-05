8731f0ba8c4d3bac514746ebc0091e6e39cd6729
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:54:29 +0000

    silent protest still pronounce order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_index(array, item):
+        return array.index(item)
