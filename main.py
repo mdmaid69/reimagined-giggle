4f26db399b143cbc58516a26336bf449eae9a47f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:14:47 +0000

    complaint problem shut little Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
