b398daf8050a75e405136fd39d81fd78a8950fa0
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:33:10 +0000

    demonstrate mysterious them takeaway probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_length(array):
+        return len(array)
