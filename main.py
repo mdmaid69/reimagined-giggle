e46dd7950e7d9a50afe9fd01fbf98a3c845bb799
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:35:21 +0000

    copy content summer play there

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
