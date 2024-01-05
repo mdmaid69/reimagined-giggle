3e6776ce83093a835caa72be381a0e533fd6aeb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:20:43 +0000

    avoid morning declare getback whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
