f41a65a54817de43c28e9ebdb4da8cde1084e709
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:46:15 +0000

    talkinto difficulty key concerned division

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def change_working_directory(path):
+        os.chdir(path)
