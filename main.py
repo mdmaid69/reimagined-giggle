15d70191a627ce8318722e9d5af05c94c19905ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:51 +0000

    unpleasant responsibility suppose quick comment

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
