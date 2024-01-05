0bf1bd452a01eee425a55dd1fbdfd854a3041e41
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:31:00 +0000

    wish rise news please comeback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_work(force, distance):
-        return force * distance
+import platform
+def get_os_info():
+        return platform.uname()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
