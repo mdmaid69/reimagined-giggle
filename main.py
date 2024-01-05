cd663b0b9864966a0dacfb7fb502dc1d4c7902d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:11:26 +0000

    influence equal pin before Mm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
