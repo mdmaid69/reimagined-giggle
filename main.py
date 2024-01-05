2c3b2cdaac5a841527e2aa9d22d9e25513b38e2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:20:33 +0000

    personal skirt interesting round shame

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
