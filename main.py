2559f6adddeb808e78695bfe883a352a110817eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:43:50 +0000

    splash possibility brush alive delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
