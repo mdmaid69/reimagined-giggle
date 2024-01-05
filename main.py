5a212d15903c13ccc50146735420355a75ca9357
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:37:50 +0000

    after complain threaten content first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
