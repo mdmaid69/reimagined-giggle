409b9413e2481c5abd1207448aeedeeb49a605ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:30:39 +0000

    after message outside before dig

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
