7cd9ee2376caf202af300918efa10b6c2eaa1b6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:00:22 +0000

    soak objection angry whisper opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def is_odd(n):
+        return n % 2 != 0
