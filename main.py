2f25d1520708881bbf89eeca04516b468a4c3615
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:02:17 +0000

    introduction regret tune limit yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def clear_array(array):
+        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
