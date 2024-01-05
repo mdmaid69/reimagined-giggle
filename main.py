5074b0eb89c43f2cf50ab0f672deca30f83bfbc2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:49:49 +0000

    must refer goaway test mistake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
