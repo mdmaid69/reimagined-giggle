e112fe9f7d4d92e027b4d47aa45ce2b002f4b45a
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:44 +0000

    hurry affect rough emergency care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
