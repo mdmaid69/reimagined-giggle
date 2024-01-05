5532fae17ac584148b44fb17ad88b54033e6a865
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:02:56 +0000

    blame flood succeed careless band

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
