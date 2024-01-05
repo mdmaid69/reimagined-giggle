1cdb8f4b1da3d23525f1016e3eaec56ce9351f8e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:47:29 +0000

    store why knowledge fat measure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_area(radius):
+        return 3.14 * radius * radius
