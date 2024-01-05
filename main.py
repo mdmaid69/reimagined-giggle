4740eeed7b6cb9faeb5f17c6b874e5171b6ac0a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:36:27 +0000

    under beside back breakfast petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item(array, i):
+        return array[i]
