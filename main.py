88b81009d76e10ce5413cdd54bb4c622b1062e56
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:47:33 +0000

    fire badly fat Kk trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
