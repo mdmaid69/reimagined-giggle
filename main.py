d7570c1701b369f48ba50f33bacaf31985e97420
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:01:39 +0000

    crime comeround do fear freedom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
