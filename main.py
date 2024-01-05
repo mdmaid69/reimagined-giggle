b68ae072dfed037ce80749a07fb02f19e98dc5ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:19:35 +0000

    doubt substance drive quiet heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
