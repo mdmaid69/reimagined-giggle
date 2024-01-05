41caafbefe273b16d771dbafa1ecb282524f2310
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:20:27 +0000

    fence manage over future Qq

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_file_size(filename):
+        return os.path.getsize(filename)
