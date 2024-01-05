041b297a71917faa182bf6b25fd55d30dc939ed0
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:15:50 +0000

    wise strike direct sleep town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
