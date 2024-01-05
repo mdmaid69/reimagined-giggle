33f89c690d890cc9c0f636557b9a9bfa80dfc3c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:15:35 +0000

    fork fault refuse rain tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import sys
+def print_python_version():
+        print(sys.version)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
