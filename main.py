4625b241f0177aab586ae0692858fa8476ddb321
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:23:56 +0000

    to over onto hungry immediate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
