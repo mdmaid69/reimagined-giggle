81c6d57d9ceca1db96487d67892930ca6df1cfe6
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:55:21 +0000

    sing throw feeling post central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
