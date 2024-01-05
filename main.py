6006d1e4d05c9e4c032b17c159ce61356a48dfd5
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:16:30 +0000

    idea print either hand method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
