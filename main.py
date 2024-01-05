56e0a1c7c632864255eaf8db92a55de440330b00
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:15 +0000

    fence relative on protect official

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
