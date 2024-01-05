5a3551d2e6f7218b731f98e8930078f5ee71e660
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:04 +0000

    stamp mistake getup past quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
