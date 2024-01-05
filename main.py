756b791f4c3e098692a41c8e2f7b832a810ca370
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:03:01 +0000

    shop spoil pronounce across turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
