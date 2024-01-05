7d8859625a384a4c693969015361e1454e4f46d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:03:33 +0000

    inquire constantly lucky cover mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
