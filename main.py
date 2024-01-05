942507d7c2110b2a1517815e02be4d0716b6f91b
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:27:38 +0000

    invitation clear tape answer exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def clear_array(array):
+        array *= 0
