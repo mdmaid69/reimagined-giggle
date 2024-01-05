f433d0687e722e1ef50120d1bd006d8358d5cd66
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:38:51 +0000

    leg trip ilovedicks fat tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
