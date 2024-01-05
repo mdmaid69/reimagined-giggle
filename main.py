ed1b83b8c951187484c5c1d31dd328d3752c39be
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:02:31 +0000

    mark trap best someone type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
