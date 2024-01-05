4e3e7a04c18103ee46f437f1c191f1a4e956859b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:15:23 +0000

    raise minor recovery still connect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def remove_from_array(array, item):
+        array.remove(item)
