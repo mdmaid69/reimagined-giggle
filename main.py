0f02afcfffaa2d6b57495250dee5be06e2624374
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:56:03 +0000

    block replace spread accidentally mix

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
