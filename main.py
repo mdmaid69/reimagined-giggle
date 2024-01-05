0e818cfb0a98c41128fba5868daa9937d0caef8d
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:16:51 +0000

    people finger him mean damp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
