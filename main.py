29295344af27cc0f51a4505e6cf25bed2ac3be9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:58:58 +0000

    lunch wild sense far plant

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
