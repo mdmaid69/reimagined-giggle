9a43ab06bc76527501d78d00e5dc9d2e91c070a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:34:26 +0000

    sister water getback ill habit

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def reverse_list(lst):
-        return lst[::-1]
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
