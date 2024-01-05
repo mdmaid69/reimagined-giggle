22b1c7c0954387d41391e8c839632d8268d761e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:23:39 +0000

    main himself swallow suck comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
