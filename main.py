076c66f69c158cbdbd4e09f31c1a4cd0d6636362
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:46:25 +0000

    become scheme much pleased niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
