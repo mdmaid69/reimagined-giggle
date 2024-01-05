f7ffa57c361203f5e0147ae5adeff1186a8f4f42
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:14 +0000

    street reasonable concentration Itself means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
