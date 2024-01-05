1c3d00ebf0b3f3883eec4108bc2879b755571f68
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:00:27 +0000

    herself sister occasional related lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
