cff625ebdc9e434aeb649da0d3f35df6ac1855e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:36:10 +0000

    opposite worth majority off otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
