940aa241cf6ce6e7fa09dff5097720e9196027c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:03:10 +0000

    funny above happen rarely none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.ctime()
