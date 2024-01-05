913f37952c159c014a5b6de7726829866df396bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:57:13 +0000

    benefit second choice too onto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
