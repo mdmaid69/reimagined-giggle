4c187fd800053cdd03ba4aab69df6c120e350515
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:32:47 +0000

    wild church object sail phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
