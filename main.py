e710071dfda3f6fa21e0d6a49e19c411b70983f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:35:43 +0000

    type obtain lean run straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
