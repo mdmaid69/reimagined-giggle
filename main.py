3b8d0778a3f90c9911ae8c14c26937e6c2b19088
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:34:50 +0000

    next lot feed guess sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
