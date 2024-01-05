2d89dce13a7d2e5ce35a90b1d19fa664f80f4ecd
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:24:18 +0000

    age test marry depend situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import sys
+def print_python_version():
+        return sys.version
