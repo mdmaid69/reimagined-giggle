d9e773449f11b6cd5a59b426f0754060cc273580
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:59:03 +0000

    no practical start above past

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+print("Squared:", [n**2 for n in numbers])
