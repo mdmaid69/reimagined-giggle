9dbdb9547f9a2fca46da7c88a2ca14c03770d27c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:46:43 +0000

    back judge cheque hard Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
