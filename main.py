f8c5d26e45b5f53ff222623c721ab99d58471eb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:31:29 +0000

    Qq muscle back square belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+print(os.getcwd())
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
