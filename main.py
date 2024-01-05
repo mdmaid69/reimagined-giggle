c298569f65ce90f24be85138c7b150cb0a7d1732
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:03:57 +0000

    feature finally custom accept pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
