f97b6daf3213b67119fa9bcf3be49eaa2f1aac54
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:15:27 +0000

    generally mad today building various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
