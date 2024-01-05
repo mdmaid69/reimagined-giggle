4b7cff485fd5aa3a62375e0a368c48ac2b026a93
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:36:30 +0000

    little everyone begin worth choice

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
