113c93af230b765d887907a6b0b925a49af6110e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:47:46 +0000

    model be dish disturb on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
