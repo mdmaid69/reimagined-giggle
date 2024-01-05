5419bd082f3674e3fbf773a9899cd71b6f984c67
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:39:00 +0000

    escape enthusiastic crash burn promise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
