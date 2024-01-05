0532addce775e187ef1f0c0a2a02ca2b7904c34b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:52:46 +0000

    difficulty count beneath on person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def reverse_array(array):
+        array.reverse()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
