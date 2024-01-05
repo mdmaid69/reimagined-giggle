fa8fbfd2c66c903510c2e740eeaa47d5c66199ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:24:38 +0000

    right bin concern recommend extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
