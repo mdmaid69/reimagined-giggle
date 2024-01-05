ee1363ee86976552f18809201a97977e80611364
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:37:02 +0000

    any car only each skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
