b7ab2b39bad7fe86f9c95ced75aa209638681720
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:00:16 +0000

    might trick Dd block unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_density(mass, volume):
+        return mass / volume
