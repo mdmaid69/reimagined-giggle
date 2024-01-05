ef544e7dba00c2fa35bdfaeadd07a62f997b1142
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:50:02 +0000

    worth further pity deaf embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def subtract_numbers(x, y):
+        return x - y
