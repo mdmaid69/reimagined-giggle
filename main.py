339189303ba9c9d6a0db89a406096910353bfbb3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:55:49 +0000

    goup total noisy up into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
