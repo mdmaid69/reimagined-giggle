67f11a159574d54a5f61da6695c414afbc1dbe23
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:12:53 +0000

    task hit neither direction university

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
