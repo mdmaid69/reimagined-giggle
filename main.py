71f850d25bd0e3bb4bd198e620fe68118061f8e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:13:26 +0000

    undo idea pretty off promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
