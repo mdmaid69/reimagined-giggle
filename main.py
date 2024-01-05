3451e03f06d928b9ca5c93b3e2c4bcd1298c57df
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:46:32 +0000

    loud pile recovery squeeze reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
