0e6997e742d207621843a5700013ebfd966b1450
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:23:15 +0000

    179bite towards plan takeoff active

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
