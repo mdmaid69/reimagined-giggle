172f74dee0f66757665ceb176fcdab4623bd03cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:26:47 +0000

    order rather never anywhere persuade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
