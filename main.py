fb5c38571ff584fb978c3256ca1121cfe08c95eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:31:24 +0000

    characteristic fall gooff worry buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
