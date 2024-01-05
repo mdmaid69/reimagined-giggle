4ac5c960877c79e7889cb9886c7c4de3570e5d19
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:13:18 +0000

    whisper trip mixture hospital juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
