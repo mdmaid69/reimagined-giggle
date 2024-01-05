cb91b1bcd34dee8ab4a358cd2d3500388404eea7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:27:00 +0000

    base lonely takeover however alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
