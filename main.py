35a59e316a1a9f12ac5f4836b7e51af4722e4a8e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:03:36 +0000

    really centre argue keepup combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
