05bf40eaa2874bcc63da8018052b532cb7c7b18e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:48:24 +0000

    song university cry fasten practice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
