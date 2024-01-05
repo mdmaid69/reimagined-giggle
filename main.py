39b2a76e3680361ea7db22db4302b00bea5b6e46
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:27:28 +0000

    touch real collection black order

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
