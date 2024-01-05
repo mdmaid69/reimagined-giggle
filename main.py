710767fa7cc458c1cfc03eae2b47624d94248960
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:00:58 +0000

    pass birthday far apply error

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
