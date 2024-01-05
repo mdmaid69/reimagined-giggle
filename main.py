4c1575d93a2adbd234af6b13280150a29388af90
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:16:28 +0000

    poor turnback commit its steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
