fcac951a76abb8bdffe8189496cd61ce9d5cdb50
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:31:31 +0000

    top solve rich even where

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
