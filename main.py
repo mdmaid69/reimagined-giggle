ce4720586f401f4d1f9cd30b870e7c0d00021220
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:20:07 +0000

    holdout Oo runout last complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def add_numbers(a, b):
+        return a + b
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
