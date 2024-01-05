07d831aad0989da3946eca4019d1490d29c521a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:29:32 +0000

    new holdout sweep content sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_repr(array):
+        return repr(array)
