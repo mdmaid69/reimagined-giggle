b5621b85e877c70d0af6b8c64d0685c88ee31dfb
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:10:09 +0000

    loose tip accordingto empty much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
