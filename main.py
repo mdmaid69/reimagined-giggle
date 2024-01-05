48be3751a8e166913f746570dba23a74f9927ae2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:00:04 +0000

    female meet closed talkinto about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
