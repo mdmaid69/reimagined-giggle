799c76289f0480df852443f4a76d8aa13b74797a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:57:38 +0000

    talkback fix professional still trap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_work(force, distance):
+        return force * distance
