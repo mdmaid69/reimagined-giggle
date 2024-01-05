1afe50ae8c36bb14c43f5540992ff951c32b0080
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:56:51 +0000

    trousers correct something fork fall

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 def calculate_average(numbers):
         return sum(numbers) / len(numbers)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
