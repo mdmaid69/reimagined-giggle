0eee333b26fe13a17c07003ee65b5f95b97833d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:57:03 +0000

    translate accidental now centre threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
