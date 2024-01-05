5ee6032c2d2aa7455fe3085033ddb175d987e059
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:26:20 +0000

    wear moral notice exercise perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
