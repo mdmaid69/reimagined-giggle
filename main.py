46c845326d48ab4dba5f4ffe86b9e5f4187cd694
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:26:11 +0000

    getoff 566end obtain common description

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
