59f0b3b3d0e403277e97be60eb15d7cad172eb9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:45:11 +0000

    rely pity imagine authority satisfied

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
