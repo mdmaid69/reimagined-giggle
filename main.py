a058b4e2ea3aaa396fa4afdb79ea9f25de3685bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:00 +0000

    believe pay pile compete relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
