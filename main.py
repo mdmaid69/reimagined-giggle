09952cb05fa35dae95203aa79298b73ef6bfcecb
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:27:45 +0000

    beneath tidy oil either straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
