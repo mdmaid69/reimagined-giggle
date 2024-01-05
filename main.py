31b224a26fe9e6ac885d2e6f538c6707a6503959
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:55:09 +0000

    completely able strike control bag

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
