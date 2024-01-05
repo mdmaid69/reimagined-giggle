09779b03a463413baa931a18a1fd41413b72f6b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:28:25 +0000

    worry direct wind easy human

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def reverse_array(array):
+        array.reverse()
