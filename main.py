483dfc8d6b1e4e1798035bc6380ce614dfd3ee15
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:59:51 +0000

    model unlucky form punishment cum

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
