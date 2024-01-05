3a122fd178966c6dbc1ac1021a66a67c5f69b417
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:10:56 +0000

    excuse explanation holiday pump such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
