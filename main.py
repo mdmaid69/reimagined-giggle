c17d1445017fcbf9ff2d78607559caf9d1f21e81
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:11:04 +0000

    happy forgive luck cheap certain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_str(array):
+        return str(array)
