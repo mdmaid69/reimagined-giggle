78d3cc7782aa01ac19327916f5b1cc3671cc63de
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:59:45 +0000

    disgusting fair public fall make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
