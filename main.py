ce7342c7fac6991ed003844705f2c794aca453fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:42:55 +0000

    rely health hello tend spin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
