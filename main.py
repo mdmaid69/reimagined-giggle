041fffc96bc14b99baf51d7cf5aa21efbce6846d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:12:52 +0000

    standard field exciting Dd police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
