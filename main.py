be791543afad0e44cddc5fbcbf4346b483499527
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:51:43 +0000

    little rest right couple family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
