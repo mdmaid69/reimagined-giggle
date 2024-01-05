6dc20b6e7a659f6bd279e299bcf6b318f86200ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:01:33 +0000

    disturb arm real positive scare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_acceleration(speed, time):
+        return speed / time
