e779fe485b2a62b69e9a8b09f192e3ba66898b04
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:30:31 +0000

    breathe anything far sudden Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import json
+def convert_to_json(data):
+        return json.dumps(data)
