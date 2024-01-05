98be34a3070ae27f6a1da49ba94e92ac4e3432e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:19:26 +0000

    chief reliable communication century clever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
