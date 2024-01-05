fc2b31b6cd29053c7c4ada3a9dbeee024ddc379b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:28:51 +0000

    himself label secret separate declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
