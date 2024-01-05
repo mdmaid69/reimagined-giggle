ad3d5bc2a24c69663c0367231299b2cb3ff3febe
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:05:55 +0000

    language inside perfect tradition most

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
