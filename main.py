0329c4078cd9f5c55168fe9f3fef008bab49461d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:40:36 +0000

    stiff area provide shock scared

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
