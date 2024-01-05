9e8ac8f0f846f9e5d6537fef3cb1c6c8b725aa85
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:30:45 +0000

    dance speed this confident estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import collections
-def create_user_list():
-        return collections.UserList()
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
