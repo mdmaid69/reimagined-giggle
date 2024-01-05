533102e1f59f7f1c501eda541308aa0f9f1f6f09
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:58:12 +0000

    worse fill below crash shopping

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
