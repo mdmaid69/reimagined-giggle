94bdfd302d6f341949fe129c0d3d13a9fe5dc5d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:22:58 +0000

    loose regularly pupil whistle unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
