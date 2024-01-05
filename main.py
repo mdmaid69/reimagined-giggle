21658180c3a78dc1d2ffbcb6ec2c690cc47ec926
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:49:46 +0000

    talkback church hurt poor takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
