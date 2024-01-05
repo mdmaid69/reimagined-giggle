4e5ada6badd0b3e11749b6ec68f6149a4713fff7
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:37:50 +0000

    certain year breakup allow fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
