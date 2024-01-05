1e9c552c716d3fe377a6436e955493767f8a88e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:11:21 +0000

    fix pin claim show discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
