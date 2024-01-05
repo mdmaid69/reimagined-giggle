53771973f10aeed18d06516325888e57344adf7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:27:40 +0000

    crazy afraid boil course suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
