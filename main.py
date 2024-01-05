db80a7febfa5eef6c7324f45450b71f26e08e0da
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:51:03 +0000

    clue mind comeround up example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
