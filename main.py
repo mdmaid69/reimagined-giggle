cb69ababf97e5e967d9a4259f9bfa43fae048c2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:29:23 +0000

    level recent demonstrate agree anyone

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
