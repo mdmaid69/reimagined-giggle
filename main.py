78395381db4cd374ef36ba76935a13389dc23d3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:56:42 +0000

    father back comfort pride listenfor

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
