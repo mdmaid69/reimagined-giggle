b4e3f7931dd2f86d40a13b13e690989f5f001521
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:52:00 +0000

    central part idea class interview

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
