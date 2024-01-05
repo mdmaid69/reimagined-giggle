55e896344645c52045253ba0267b44490d8a8462
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:52:27 +0000

    silent explain that emergency introduction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
