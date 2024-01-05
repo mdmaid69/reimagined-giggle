8f43d4f92eb76cac6331e6afb3a8194f795c64c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:04:45 +0000

    weigh everyone night impossible port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_base_name(path):
+        return os.path.basename(path)
