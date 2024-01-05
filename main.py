1316675204ddd76035acbe44c6ea3b18b030d87c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:15:02 +0000

    capital smile if callat pronounce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
