7e8c04b3c92cdc76bd70a61c3249d64143cdfe91
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:02:05 +0000

    share himself guess insist child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import platform
-def get_python_version():
-        return platform.python_version()
+def print_python_version():
+        print(sys.version)
