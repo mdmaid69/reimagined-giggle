1319d0f487f749147d9816f14aa4a565f81e0a21
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:28:36 +0000

    turnover mark pollution attach after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_length(array):
+        return len(array)
