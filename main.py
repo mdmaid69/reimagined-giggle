dd1f4fe49d910b6e24af73e2522ad980b9a1b41c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:44:59 +0000

    disappear emphasis prize lay offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
