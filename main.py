7b18174eee0ad6a4a078dd0b16c3ca63526fbb4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:02:36 +0000

    central final keep basic learn

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
