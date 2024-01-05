252c559fb7896d8726f69c4a219a9fa0ee4a1eb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:08:59 +0000

    name fill must occur border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_bytes_from_array(array):
+        return array.tobytes()
+def multiply_numbers(x, y):
+        return x * y
