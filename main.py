d970de66903c7660da65bb099ad9f6dc308b36d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:42 +0000

    large mention tend sell recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
