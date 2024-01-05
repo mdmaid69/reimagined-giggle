db73f1bc6961e8739c4f6d6736c53f7fbabae90f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:22:49 +0000

    underneath deliberately bread listen deal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
