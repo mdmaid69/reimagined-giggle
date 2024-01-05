cc2af05a3a79a31563ae90199e61ddc1331c5918
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:31:00 +0000

    light prefer thorough target delight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_item(array, i):
+        return array[i]
