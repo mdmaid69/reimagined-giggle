996fdf0ec6c2270cafa20ec936cb01395358d5a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:09:12 +0000

    neither appearance shade contribute takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
