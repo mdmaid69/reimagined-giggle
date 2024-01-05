4b649c79d0bf12a548091e6d55b79b47009d1110
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:03:32 +0000

    collection generally evidence Mm circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
