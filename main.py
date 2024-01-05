5ca7e689449f5969ee18475508ebb41dc01e21b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:47:16 +0000

    soil runover Mm upstairs personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-print(sum(range(10)))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import tensorflow as tf
+print(tf.__version__)
