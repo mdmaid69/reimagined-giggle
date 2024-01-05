07a40206c6126005e60b122f4110eaee1051a066
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:18:49 +0000

    course charge payment measure earth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
