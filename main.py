4c7850db37b247758dcb4e8e3fbb76ca9bc1494f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:06:35 +0000

    read trip train past area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
