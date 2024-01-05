22790ed6b4b3c7a587205f5adc7e71977e009688
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:42:14 +0000

    pencil runover press difference speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
