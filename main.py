cfa2e887939743d5d5c82c63c1557716805200ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:41:22 +0000

    turnback air transparent disturbing deserve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
