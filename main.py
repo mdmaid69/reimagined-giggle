40444978d3619c7a13bc222c9e36818d3faaee0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:12:00 +0000

    twist myself true thick keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
