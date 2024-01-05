e9465cdd90da463677f449bf3706ac5f2f5ee291
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:36:51 +0000

    typical chat think rescue scream

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
