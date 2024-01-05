b0eeff7329b25d706c174ff8716dc9fe157b2653
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:41:50 +0000

    firm sound clean must thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
