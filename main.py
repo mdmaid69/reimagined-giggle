d6d06c924e9d9e9c456562556658d7210b165a38
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:23:47 +0000

    mention get divide touch talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import logging
 def setup_logging(level):
         logging.basicConfig(level=level)
+def calculate_work(force, distance):
+        return force * distance
