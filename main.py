fdbfd35e04ab40cd85dde9221238384c13d21850
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:10:01 +0000

    compare truth depend closed computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area_rectangle(l, w):
+        return l * w
