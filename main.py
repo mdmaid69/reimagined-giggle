ac2be14830d776ba05754916b4f7854b0fae6b23
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:12:37 +0000

    sure spill middle rate yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_odd(n):
+        return n % 2 != 0
