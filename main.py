b8ca8ae6f4037b82558fa8f033587e808fc93af4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:06:26 +0000

    common provide impression language worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
