302bf4d9963bdad70c022628d0360fa2de22972b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:31:53 +0000

    begin pleased nearly shoulder comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
