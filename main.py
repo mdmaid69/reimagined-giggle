b26f648da22727d47cbe62b5c016529812ee087e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:07:07 +0000

    catch speed past yes fight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
