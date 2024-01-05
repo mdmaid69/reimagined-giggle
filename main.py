65a00de20ddcda32a997d8660de999090b90937f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:45:57 +0000

    invite principle petrol post hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
