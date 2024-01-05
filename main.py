f8964bce742e9db622c54837812eac084ff2bae7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:08:11 +0000

    force mistake subject now weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
