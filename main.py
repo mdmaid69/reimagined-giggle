7a9640a75c9e918d64c820e276a26ab8aa52142d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:40:53 +0000

    wear should themselves pool raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_current_directory():
+        return os.getcwd()
