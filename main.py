23aeee6eabbf0aff9b4008e2f0bcb9c5f6cfd102
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:14:50 +0000

    judge area all office unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sys
+  def get_python_version():
+        return sys.version
