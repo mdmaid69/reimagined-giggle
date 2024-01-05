b926cdee5353e670d2f9ea31c6cb6257ed4521ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:25:18 +0000

    describe instrument spoil business Ss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
