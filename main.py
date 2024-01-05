d0e2a7dd54e306e8f4d28d971c0326f17189c0eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:04:17 +0000

    fly comein border hope deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+from collections import Counter
+print(Counter("hello world"))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
