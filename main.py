931fdb78b9431f60f099f6142e76852d926d5025
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:57:18 +0000

    operate petrol extremely goon correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
