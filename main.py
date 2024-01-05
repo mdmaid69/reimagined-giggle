6542120f9b18e6585ed4564b347b9c30becf5e60
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:51:23 +0000

    exist tonight shoulder mad then

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
