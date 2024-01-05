33c1282999be9b669b16889d8bfc6aeff3452fae
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:22:03 +0000

    safe purpose hollow great joke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
