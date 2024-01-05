6c7f6cd8f6ba23a424cc7976be9e7e1b73670902
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:23:53 +0000

    cry thing drive it area

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
