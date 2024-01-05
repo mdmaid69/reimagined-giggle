4727b9a363a11678970df065b0b433b88ff2a83e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:17:04 +0000

    store interview date less least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
