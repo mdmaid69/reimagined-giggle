8a3aba627330e948d9a34f7f239afc748a5a0a94
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:09:55 +0000

    turnoff name recover occasion thirsty

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
