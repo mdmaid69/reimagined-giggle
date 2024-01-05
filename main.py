fdc62e3912457795efca9f575066c5ea3899bc9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:49:29 +0000

    tragedy tomorrow spite bright neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
