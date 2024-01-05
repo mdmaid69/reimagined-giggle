5b2100560ba7fd097e7f3019b64c70a6e5ee40d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:40:53 +0000

    remove rate interview around 570enjoyable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
