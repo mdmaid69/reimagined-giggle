74659ce9e13b34dc1f45e0ca2edb98d0ad325e68
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:52:53 +0000

    ring completely ready recent swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
