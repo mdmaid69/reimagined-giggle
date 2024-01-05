039e1c4bcea7801780fd820e93024ff22f55a71b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:50:47 +0000

    fairly stone fit match polite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
+def divide_numbers(x, y):
+        return x / y
