94bf8056e13e1beab27c6d6f43dccab44c8d6467
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:15:31 +0000

    keepfrom disapprove patience care book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sys
+def print_python_version():
+        return sys.version
