ea2d58bfe734cf02cb33fb863a2ce17e8cb89751
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:59:21 +0000

    air tool estimate information left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
