1bdd94a3637ed64f974da91540bac34f017844d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:18:41 +0000

    mix health match escape interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
