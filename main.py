cf8e3b9c036958b01ffe73472456eb1b741ddf66
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:20:53 +0000

    feeling ball present against guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def square_number(x):
+        return x**2
