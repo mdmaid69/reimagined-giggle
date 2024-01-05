c15919f084571c599813fd5663d95adb8672ae98
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:48:24 +0000

    after contrast relationship month possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+for i in range(10): print(i)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
