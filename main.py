f2bc20893fe347bdcf4999977c2d87b0f7f9839e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:35:27 +0000

    over burn square sad march

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import re
+print(re.match("h.*o", "hello world"))
+def is_odd(n):
+        return n % 2 != 0
