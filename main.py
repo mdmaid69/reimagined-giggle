69ecc2bb332c34421b5615fe82cf9f3d35183e03
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:57:36 +0000

    reduction demonstrate promise regret land

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_area_circle(r):
+        return 3.14 * r**2
