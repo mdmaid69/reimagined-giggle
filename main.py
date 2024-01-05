f7c93fc1b00183b2316750558c77e06281930ecd
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:55:15 +0000

    nurse sit to office plenty

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
