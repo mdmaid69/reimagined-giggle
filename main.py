3b6e44bdde4d3510e09c8ba7f5b4ec4234a9e303
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:09:47 +0000

    steam contrast wise paper plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
