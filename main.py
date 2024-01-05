563d3c5094959f59207bc8befed5d1cb3b008a48
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:37:15 +0000

    along eye regular sound suckme

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import sys
+def print_python_version():
+        print(sys.version)
