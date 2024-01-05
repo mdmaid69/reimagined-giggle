674ee42e6bdfe8a9c477e8e6e743806765ea3388
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:02:06 +0000

    conversation raw rush majority influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
