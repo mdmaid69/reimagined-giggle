81df72cd43f66710d5a2f86b50dbbf3875cf88a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:57 +0000

    scare command callout open whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
