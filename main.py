c1f98437892a1931313fddae2516c33061208f7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:38:02 +0000

    sky straight Oo mind cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def append_to_array(array, item):
+        array.append(item)
