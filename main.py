601d0bda1dd7d2acccc2ba3e18bf8ab382297fbb
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:32:45 +0000

    occur coffee accidentally once read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
