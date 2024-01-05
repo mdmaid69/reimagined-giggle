1e7abcaa27f61279378fc2279e9d65977e6eb08d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:09:48 +0000

    birth meeting upstairs recent plenty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
