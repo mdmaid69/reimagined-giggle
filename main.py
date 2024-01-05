3f1e206404c458c98a2aaa339f226b89f8f38319
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:48:43 +0000

    imagination unusual tongue there seriously

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
