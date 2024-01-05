3b9526c2e04b39ee91976c537d8960c79d3fc1a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:48:00 +0000

    from land murder mention classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_acceleration(speed, time):
+        return speed / time
