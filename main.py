51c5cf0f353333ed61fb9b3550bfc7d2f6978d97
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:06:52 +0000

    drive disappointed bang language typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
