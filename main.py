7426f742484d282dab7abbb1606ecf333634c671
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:29:25 +0000

    simple smell about post brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-print([x**2 for x in range(10)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
