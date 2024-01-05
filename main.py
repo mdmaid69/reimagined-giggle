245579a163d14bebf395d1b2a196189755e29024
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:19:00 +0000

    hear to several hot money

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
