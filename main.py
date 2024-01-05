a05e21a5b8c1201d8777d97386956e7abd7a22ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:58:50 +0000

    appeal fast comeover future responsible

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
