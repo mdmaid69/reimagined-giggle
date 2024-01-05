b570946ac2978cb17ddcde45a3b1a848b05b19db
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:13:08 +0000

    circumstance sure exist ring exhibition

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print("Hello, world!")
