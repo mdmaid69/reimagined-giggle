5518287a6161a0a30ff3e98c92673eb60da42cb1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:16:02 +0000

    offer shock round gather sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
