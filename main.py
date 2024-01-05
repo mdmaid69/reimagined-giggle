66b762e0aab0d75881265e5c68dbfb450bdf8f99
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:59:27 +0000

    indeed rush confuse address contents

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
