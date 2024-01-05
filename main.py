61362db53bf274931a6bff68a64d3edc594b3b5f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:42:32 +0000

    treat keepout eye hot offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
