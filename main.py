3717fc7149dd79eda641bf887637ffa3d3b3d589
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:37:19 +0000

    hour like uniform health cold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
