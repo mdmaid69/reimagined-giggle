cc357ae6a8c2c71e34e6345d894083329ec87f51
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:24:17 +0000

    disagree hollow clean think polite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_to_hex(n):
+        return hex(n)
