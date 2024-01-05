0fd76abb942f6618c08e6752c4509f3f28a35e41
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:37:42 +0000

    dream call everything situation lonely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_date():
+        return datetime.date.today()
