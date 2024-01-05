66c1f22f0c7e4203f152c39436898f5d02a57940
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:05:27 +0000

    hurry extraordinary hardly study foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
