971e79144acf2ed31dc27d1124df268280c4ac90
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:46:19 +0000

    takeover recover will their fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
