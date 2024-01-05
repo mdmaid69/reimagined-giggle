df921185b793ce4638a3af9ca196dfca3e4a8947
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:03:22 +0000

    roof disgusting nature angry land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
