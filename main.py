b7362e43fc22dcaae90fd730a3a25d69600a35e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:44:02 +0000

    dry legal careless touch rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
