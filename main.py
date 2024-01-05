b291bde788353b8d37850f39f4b2011eea2d8f16
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:03:08 +0000

    disturbing pause will city border

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
