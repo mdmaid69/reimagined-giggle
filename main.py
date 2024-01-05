6da4242ca50186b0c9de3312b73aa66f97c71b2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:51:11 +0000

    nose manner something simple commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
