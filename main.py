a6655e2e280ab32fbdfd9df4408fdf622c8120ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:06:28 +0000

    interesting deep touch grow interested

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+print(sum(range(10)))
