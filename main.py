c4a9f2cdc5898b6861141246ecedcf4bfaf14642
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:36:07 +0000

    stamp rush cover straight steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
