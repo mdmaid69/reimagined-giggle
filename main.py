a06f659d858fc91e2893611ebee815261eb3bfdb
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:48:23 +0000

    best system appear spoil proper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def subtract_numbers(x, y):
+        return x - y
