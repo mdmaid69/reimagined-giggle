18a1bc0a16544e0bbe975a3a8b3032cd7cc179f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:43:35 +0000

    soak fair this little turnout

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
