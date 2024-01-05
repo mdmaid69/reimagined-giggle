aa070a47000d924feaf8665bc61bca9c3d24cfd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:46:02 +0000

    raw describe cause top turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
