264475ba87be1a8528520ad0f62bed32e5fdf2dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:10:00 +0000

    care thanks rank opinion fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
