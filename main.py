91c364baf4f35262ca3a03b75fb9560ff56efc0b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:42:45 +0000

    journey urgent ignore otherwise dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
