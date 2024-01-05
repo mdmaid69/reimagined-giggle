97d5d531f468ac32ac6e0233911acc25c32e2899
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:11:34 +0000

    finally hate reduce disappear great

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
