d5ebd59a45b6579e288e665656c0f28b505d4bd9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:13:51 +0000

    holdup wash left connect promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
