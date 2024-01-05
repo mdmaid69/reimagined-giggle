864b9b14a7752bd41825a200089e2f4703b48a2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:56:17 +0000

    past right nice thumb furniture

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
