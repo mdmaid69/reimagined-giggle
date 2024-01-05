7e9338b707b327bc73f4faa427430b78ddd76346
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:30:57 +0000

    home change drive warm peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
