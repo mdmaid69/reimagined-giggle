63dcd8948f2e051eb697d88f6e061dc9915a9fbb
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:31:57 +0000

    tidy furniture idea miss error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
