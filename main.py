8d6b85af9a0c5f6c25b9fc43735f09c109c07a1b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:29:44 +0000

    poison flow stage exact stage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
