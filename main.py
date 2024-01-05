6d6543fcc0e5ec338187be02e67c9b6eefae21f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:22:43 +0000

    Gg later responsible something soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_set(array):
+        return set(array)
