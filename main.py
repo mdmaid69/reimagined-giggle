de3e6ac530fc0dde63441d3bdac7770741a73846
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:45:29 +0000

    order holdup quit from disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
