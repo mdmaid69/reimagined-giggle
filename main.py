0b5f0fd231d9641ccd430eec60b96d12282eed28
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:41:46 +0000

    check support bang communication teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.time()
