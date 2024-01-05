a6ccf41a25be5d15d5f99d03b3358f295dd9ac99
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:03:40 +0000

    confuse excuse main everything work

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_string_from_array(array):
+        return array.tobytes()
