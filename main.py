301fa3230d0f4c247f4a18b66aa0a44cd86d0050
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:08:44 +0000

    achievement nerve modern cry after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
