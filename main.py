153f8e9b6f4853686a1fc660734bf794f31d9376
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:51:22 +0000

    wall mainly seriously Rr ours

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
