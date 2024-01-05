7ce815b65f891116507fd580275bbe5db997813b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:54:12 +0000

    touch tight blue last Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
