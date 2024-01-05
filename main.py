1af830e5dba37feadf7283814cd7d84bf532f077
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:24:08 +0000

    some mad life mine arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
