273cb98ec03cb9b1a6c287916d3bcb8bda004e76
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:37:34 +0000

    almost glad happy close narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import time
+print(time.time())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
