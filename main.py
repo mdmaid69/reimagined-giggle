7a2e5aedc0596b16ddcaa9d1656dc3c71d4df888
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:51:41 +0000

    typical shade squeeze defend perform

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
