01723d6bc67ae27997209ff740ca7c1de5504919
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:35 +0000

    decide space challenge independent head

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
