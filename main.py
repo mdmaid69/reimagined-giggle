88493d3effaaf29bc30a333ff24fcff33cf3d976
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:40:51 +0000

    note allow voice also rare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
