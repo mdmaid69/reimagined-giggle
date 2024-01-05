6535c5ca5a863f8a8d387cbcc1588e252ce2a5dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:57:08 +0000

    break come free belong tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def find_min(lst):
+        return min(lst)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
