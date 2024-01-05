5a22223c81f9f70a008816a90b1a8878dbb64235
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:42:19 +0000

    lean damage money shrink open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def subtract_numbers(x, y):
-        return x - y
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
