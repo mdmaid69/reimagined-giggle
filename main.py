f53b79d53a198fbd63375506ff7c375a999c28fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:15:00 +0000

    plan steady fat help hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def find_min(numbers):
+        return min(numbers)
