40475abdea03b7939dc1dfc7506b121bababbd02
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:49:02 +0000

    myself big miss affect properly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
