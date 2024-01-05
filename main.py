ab6aa777b07cd4f3facd2d2eb9f09808af09c0e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:05 +0000

    speed different properly car comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
