f0e48b8a1014883c1d4dfc3164954058ae159d35
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:20:14 +0000

    welcome ahead certain grow basic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(numbers):
+        return min(numbers)
