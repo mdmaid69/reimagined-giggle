6cf8c9535956dcc9d6bcd787824b328430583546
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:35:21 +0000

    feeling brain pronounce early build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
