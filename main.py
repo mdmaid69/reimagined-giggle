e1e26db4aae8bb130ef98ad8f71ab9f1b209c751
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:49:49 +0000

    dance will excuse doubt read

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
