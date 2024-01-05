b97d7593ec2bd76d9bc78e31c4b87519a8e66e9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:47:01 +0000

    mistake coffee hers score into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def sort_list(lst):
+        return sorted(lst)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
