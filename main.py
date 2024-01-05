4c91e2063aeb5200809403d33af2daa9d8125ab3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:01:54 +0000

    basic row correction hold passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
