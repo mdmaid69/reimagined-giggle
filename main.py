c6de75e1b8839b26da9831a5561fe3e3f2e04ad6
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:45:09 +0000

    back boring contact disappear ask

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
