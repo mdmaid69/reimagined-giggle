8ef3dc5c2ef31914705c4639ca30df9a96162f44
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:52:06 +0000

    direct common special plate damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
