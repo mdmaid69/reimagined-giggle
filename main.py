26c4de945e38a584633b91b97cd6c3e0017ae544
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:28:52 +0000

    Mm crowded since start hold

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
