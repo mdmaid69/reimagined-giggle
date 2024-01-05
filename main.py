b6932cac3e07a3e4dd4d06032cf61a70d382853f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:27:46 +0000

    sharp bin cover own welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import time
-def get_current_time():
-        return time.time()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
