17c2d54b3947144a10d135a03e1492ab7cb80298
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:28:56 +0000

    since mix tidy formal breath

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
