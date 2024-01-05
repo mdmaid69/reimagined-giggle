9e8bb398a133a269c3b21e0dabba957f79220b69
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:18:39 +0000

    rubbish care shake peace beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def reverse_array(array):
-        array.reverse()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
