59616a2be79e68581f545d2b1fbeea4ebd2be551
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:40:49 +0000

    improvement foreign young story up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
