6f9971ffcc471e04f7eba11d9ede2df2c4285ec8
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:11:09 +0000

    reply period score weigh loud

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
