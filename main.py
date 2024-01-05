6be3e38baf45b33961f9d5fa57031f2dae0ef30e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:23:46 +0000

    rare taste language sky trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Words:", len(text.split()))
