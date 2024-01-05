89ae88471ba493707faf4ec0b9471cb9ee56f80b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:43:31 +0000

    minority side area tired happy

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
