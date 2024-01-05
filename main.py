f097600be7d96412e7ac9be5c9e0f856c0e8312f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:52:32 +0000

    unpleasant total quickly afraid strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
