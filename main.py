b6b71a270fa898d2eb8e5d572f49d35b23f158b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:56:42 +0000

    window our job place cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
