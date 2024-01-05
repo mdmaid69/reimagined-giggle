81bdaad3e501650b2f50127f1221e9ff337e06ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:45:08 +0000

    smell everyone old remove wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
