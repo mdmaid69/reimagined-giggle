a9bf33fe5035c47528861c388ef5293e2c36feb3
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:40:58 +0000

    last compare pot telephone any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
