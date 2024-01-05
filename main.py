41f0665aa53898a7479ba8868c6de307f8283417
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:26:39 +0000

    spin like row listenfor movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
