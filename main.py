2e00b11ca7f32bfff342bd3e2858b7063e48fc96
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:25:45 +0000

    charge chase explain famous else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
