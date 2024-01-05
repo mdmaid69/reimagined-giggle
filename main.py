0eb5330664daea1a4ec19ad4fa3cc52695c2e1bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:22:05 +0000

    go pick fire please sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
