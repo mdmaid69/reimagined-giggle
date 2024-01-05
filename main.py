82246fc59baf419c62d7a4a60e6192fcf934a534
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:12:01 +0000

    since month bread bunch cough

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
