86fe9b1667a213276a7a2f0a0191333bef6b1ee9
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:12:26 +0000

    less see exploration classroom goback(to)

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
