3d5bad175fac9ce2383bcaf1b59113ed2a3d48aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:36:29 +0000

    base outside deserve autumn compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
