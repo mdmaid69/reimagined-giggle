21821b94f05ec08fbcdd4f701e7c8503bdac6dc1
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:22:13 +0000

    thin number concerned create surprised

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def is_palindrome(s):
+        return s == s[::-1]
