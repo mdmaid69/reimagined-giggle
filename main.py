347f0cdda077030d8d36f53742246c6038aebf54
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:21:31 +0000

    rise play market pronunciation sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
