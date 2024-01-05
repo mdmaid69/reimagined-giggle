db3bdd13f354462fb1f3a6d40eb96cf708028989
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:22:57 +0000

    zero long another disagree ought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
