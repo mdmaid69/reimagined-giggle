f6e71ef61d864939545a2382e611494bf4a421f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:02:17 +0000

    nor not error yet Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_binary(n):
-        return bin(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
