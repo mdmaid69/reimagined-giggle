856a51c4fe3f6e3f187f3605c286ad8e2a1001c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:20:49 +0000

    between rise politics and after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
