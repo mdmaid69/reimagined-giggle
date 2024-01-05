8c4f4db5ffcd6f4a7f16975875be36a189c5f8a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:47:17 +0000

    near branch union service mother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
