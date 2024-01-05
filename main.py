971ded12860ce8bfc6cb9c7c99f6dfdd749f0604
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:46:15 +0000

    taste form suggestion description runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
