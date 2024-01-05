0a2847b9b80a988e978fe70d1e6b765ad3cd0a90
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:27:07 +0000

    neither rush deaf watch family

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
