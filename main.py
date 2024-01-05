a1c1bc7232880cf288a103dae5acec6f293d95b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:12:33 +0000

    advice left Nn exaggerate advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
