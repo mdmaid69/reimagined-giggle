ac027b3e277bab88db2ed2cbebaddf1768acf9a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:02:08 +0000

    opponent uncertain expect handle hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+for i in range(10): print(i)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
