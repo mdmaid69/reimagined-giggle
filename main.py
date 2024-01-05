7e12292c657a53ae86f8bbb0946c20a02ecc7f3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:57:43 +0000

    your practical smile temper breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import collections
+def create_user_list():
+        return collections.UserList()
