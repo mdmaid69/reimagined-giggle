498f656a2ca7d2a1b3e57c7e1ec0324e7ac70fdb
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:55 +0000

    else pass storm ground throw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def subtract_numbers(x, y):
-        return x - y
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
