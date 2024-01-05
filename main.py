d8d6ec34a9dbfead668cdba95a9e53d39b229c40
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:27:00 +0000

    accordingto leave leg joke seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
