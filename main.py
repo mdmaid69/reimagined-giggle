5b5b1edb32c9ae08a13db4aab42797eb613920d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:48:04 +0000

    near flow mysterious complicated forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
