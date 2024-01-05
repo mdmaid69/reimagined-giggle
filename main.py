8cbff1d2df21c9f535b7e0a83365ea52dbfd4a35
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:19:37 +0000

    urgent hang sort moon pot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
