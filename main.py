6fc80837c2042d362595d6ab1987c2d5c037e001
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:25 +0000

    organize length ever right badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_binary(n):
-        return bin(n)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
