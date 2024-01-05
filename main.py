9c9516de5ed5c319b9e2a65e30a73e34a15190bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:09:27 +0000

    public typical the judge jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
