86f991c78e88336a057ca625fb4f4d76072d6ebc
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:07:55 +0000

    unhappy address edge study strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import datetime
-print(datetime.datetime.now())
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
