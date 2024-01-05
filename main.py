532e25e91d1f5d0ef8787ba03800d8fee2d0d6d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:49:48 +0000

    equal busy job heart single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
