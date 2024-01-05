f6a7dfd22877ee09ab742122ab627a07346fef0b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:59:15 +0000

    card lay serious mainly plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
