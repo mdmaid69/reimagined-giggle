2ab509ac7a5c64058ddc83802d7ac54be064521d
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:55:22 +0000

    sting owe before behave plan

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
