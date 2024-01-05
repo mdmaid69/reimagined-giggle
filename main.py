c935e2a53f6a30748cb252fdf08db9efbe39bdad
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:45:44 +0000

    improve nowhere play since imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def convert_to_octal(n):
+        return oct(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
