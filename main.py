e840a48a3cd178829a35e635e6254eefd0b5659b
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:32:39 +0000

    air dish beat everything thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def get_current_time():
+        return time.ctime()
