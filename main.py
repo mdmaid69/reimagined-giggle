e6d9c37466a9f8b332d187baea740dabe0d727ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:34:53 +0000

    569enjoy book fork considering cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
