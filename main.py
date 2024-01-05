90e8de770ca0256c9eec1013a1422b55176b8e96
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:13:09 +0000

    load offer soak continue level

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
