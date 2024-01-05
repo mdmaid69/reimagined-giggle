06929c45c37ef9c6f583e1d7e4b704f90a6727bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:34:13 +0000

    glad which bang treat constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
