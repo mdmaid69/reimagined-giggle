31eaabfe642435bcf04a2383b7c919aafb49dbe1
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:24:21 +0000

    freedom instrument ground substance without

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
