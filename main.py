299299acb1d47a043efe8c1ff1e84cd965e22d0b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:34:43 +0000

    rely confuse Uu father language

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
