2736ed934278de93536fa09765cec40a6e8ffcb9
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:30:22 +0000

    active age anywhere table hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def add_numbers(x, y):
-        return x + y
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_acceleration(speed, time):
+        return speed / time
