c7f9ba38a98eca774ac10085518ed8a3e5bc0d8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:31:44 +0000

    squash light tour several rule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
