315e8d902a7a884d95243dddbee19bb54b89047b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:22:52 +0000

    gooff drop dance fortunate takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
