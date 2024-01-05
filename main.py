5680d3f98b94c02e721e1f5adb67754379b45f61
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:04:53 +0000

    confident condition once bringback slice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
