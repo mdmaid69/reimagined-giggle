61617f33b4cf69315cb03150e3e32dbb788c3e36
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:34:17 +0000

    effort of come remove breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
