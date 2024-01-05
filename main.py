39d9ac26fc891a22157609ba3dd1a7fb3e551f4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:48:50 +0000

    result room steep snow protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
