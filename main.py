9fbb41f21f4ffb0edf75ef0f01bf53466276414d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:16:36 +0000

    describe when fill famous thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
