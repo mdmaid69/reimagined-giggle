b07839e64f8609c592eaae5bc1fe2b56dd2aeabe
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:18:45 +0000

    smoke manner survive belong copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
