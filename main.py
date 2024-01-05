18a0780ae60e624d33fdf3e256b71926ced312fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:44:48 +0000

    right after moment cover bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def get_array_as_memoryview(array):
+        return memoryview(array)
