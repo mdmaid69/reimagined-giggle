dfd2b7f3e0ca93df91b6e04f5e36af723e2a11b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:48:42 +0000

    whenever intelligent his underneath remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
