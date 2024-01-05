41e9c1125bbb909bdb6799dcf782e3815f795659
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:19:29 +0000

    refuse finger maintain sad deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
