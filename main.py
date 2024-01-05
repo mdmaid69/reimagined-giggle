afe1122009c25417aa9a7e41789d80ffcf94d994
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:12:10 +0000

    care lift quit here play

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
