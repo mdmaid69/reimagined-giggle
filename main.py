5d32f90dc9428bb87363fe1cfc30e9c3453b63ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:57:36 +0000

    woman breakfast radio finally lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
