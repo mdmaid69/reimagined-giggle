11535477fcf465950947c6d16247ad4c83041932
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:32:15 +0000

    study cruel normal wait during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_str(array):
-        return str(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
