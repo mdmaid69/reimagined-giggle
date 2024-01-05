286cd8d4b6885485f02a7545338bbd5bd1265ca8
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:44:36 +0000

    sink shrink born move goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
