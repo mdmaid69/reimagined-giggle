ae51866d3b10c7066007f9a263d0b84828b0e10e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:07:31 +0000

    shake air pleasant many hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
