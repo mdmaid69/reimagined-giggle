3a4389c35200950db377f340f70c5316dc8be3b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:38:36 +0000

    block I read frightened no

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-print(math.pi)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
