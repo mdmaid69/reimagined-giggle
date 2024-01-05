3e990975d4c608916b55daf31ca552ba3dcb8338
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:36:45 +0000

    calloff niggaacademy correct ticket sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
