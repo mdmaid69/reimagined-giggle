e7658535d7de95fef85b17695d1b13760b71ad33
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:02:56 +0000

    walk mess account mixture manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def reverse_array(array):
+        array.reverse()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
