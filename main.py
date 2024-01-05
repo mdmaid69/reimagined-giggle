153b33f9919fd580ed0de937a9c0888e91c9a2a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:13:24 +0000

    reserve nerve learn transparent length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
