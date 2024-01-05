27b87c5348424cd0b3d0b34abae39125c5fd73fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:34:15 +0000

    worry something story quickly strike

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_density(mass, volume):
+        return mass / volume
