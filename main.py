21ce336ab617bee47a1e09351dc05a9cbe19b001
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:10:08 +0000

    die unless bring worth people

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
