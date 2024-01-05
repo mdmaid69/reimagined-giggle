7529a9e9205962ef0bd49e7cea25eea591161c3a
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:03:11 +0000

    collect still match raise not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_directory():
+        return os.getcwd()
