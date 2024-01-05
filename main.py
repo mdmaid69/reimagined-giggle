ed0a74a45f0d83a26204035d948eb2961a8bc3ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:19:25 +0000

    no hope object bury bunch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+def get_time_since_epoch():
+        return time.time()
+import collections
+def create_priority_queue():
+        return collections.deque()
