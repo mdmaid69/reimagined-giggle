2abbec33461bcbf2889fe7132d8cfdb4866370a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:02:23 +0000

    claim favourite mind some leg

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_pressure(force, area):
-        return force / area
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
