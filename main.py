69b6b68b9bc701ee55adfe9fd25f1f58aa372e8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:50:58 +0000

    collect declare add advanced pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_floor(x):
+        return math.floor(x)
