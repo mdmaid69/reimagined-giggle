be9ec3380fe97d6d0c979467351753a3fc10523e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:28:34 +0000

    fun giveaway disgusting callout second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
