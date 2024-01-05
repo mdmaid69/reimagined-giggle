32eba40a72f41c91fd48cd39ff64f9ccb837396b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:16:37 +0000

    face language advice human sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
