af898968963d5423a87aa4bc999ae37a14311c97
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:52 +0000

    produce uncertain plastic tear spite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import sys
+  def get_python_version():
+        return sys.version
