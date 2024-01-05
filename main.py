26b38c1ad3d14269f6cb8f6b06e643c9f29e7aaa
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:50:01 +0000

    edge separate set pride matter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
