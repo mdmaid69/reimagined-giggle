87213b92323abae0e0ee2a1ff141d428ea6c1d20
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:30:47 +0000

    date value understand person deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
