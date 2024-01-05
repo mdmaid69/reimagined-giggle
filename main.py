a695742143b5cc58ce10aae219779c3310cf95fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:16:09 +0000

    organize step start invitation tidy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
