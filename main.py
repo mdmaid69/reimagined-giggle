b58c33e3d35109750a2e45ff10584dd4624e4b88
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:45:06 +0000

    wear city breakdown stay mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_priority_queue():
+        return collections.deque()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
