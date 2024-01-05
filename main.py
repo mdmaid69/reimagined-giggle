49f623c876f80455425db7d5259523dadc55af22
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:21:44 +0000

    ride pretend order control badly

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
