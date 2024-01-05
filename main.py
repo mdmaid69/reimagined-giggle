140b07183a3a6f5c7af491fc57fa93744c45df03
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:46:03 +0000

    Ww proud on bank warn

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
