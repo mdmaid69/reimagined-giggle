e81b8da06a5f33192dda23362c75b726549d9ef8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:29:54 +0000

    month hear walk train everyone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
