cd0fea5fecf08cdd85fbdd5d5accef63840306ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:36:02 +0000

    strange popular improvement interest combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import heapq
 def create_heap(iterable):
         h = list(iterable)
