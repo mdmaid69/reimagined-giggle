6e5f12f04056277e7989f1f978c92194f00c304a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:12:13 +0000

    clue boring choose well steam

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
