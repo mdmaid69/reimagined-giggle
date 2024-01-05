a1108d7199a1131f2e0554e3897e947855bf127b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:37:12 +0000

    smell to crime best breakup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def add_numbers(x, y):
-        return x + y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
