9d1e4b515bd74c66546556ce7a4dc725d5f09eac
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:03 +0000

    ready minority much march giveup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 def calculate_acceleration(speed, time):
         return speed / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
