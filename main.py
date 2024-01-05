e39a017a64264080cd08dff24e8b5129a9f903ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:39:39 +0000

    soak later freeze dick on

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
