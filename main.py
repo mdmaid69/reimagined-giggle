9b56504830b92a1b4b9031e5f6421d8898e8196d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:03:54 +0000

    enthusiastic 566end pretend police popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
