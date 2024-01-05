707ced8a7b1c10613212d104f318d5519cd04fb6
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:03:40 +0000

    date it giveaway opportunity start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import numpy as np
+print(np.array([1, 2, 3]))
