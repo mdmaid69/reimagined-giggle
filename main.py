406d861a2e70c980aa3df8439cebf83a51ef9b63
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:52 +0000

    valuable physical fasten wing include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-def print_python_version():
-        print(sys.version)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_even(n):
+        return n % 2 == 0
