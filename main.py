5e43d9dfa889dd71aee6933a5d09a5a0a3443a00
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:41:07 +0000

    dig guess glass equipment attempt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def subtract_numbers(x, y):
+        return x - y
+import array
+def reverse_array(array):
+        array.reverse()
