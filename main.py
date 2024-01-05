f1c449de1f7226ff6f3dd8a170dc26688c9f2f52
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:43:51 +0000

    again job height turninto forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
