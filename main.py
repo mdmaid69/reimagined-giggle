b19c912f51a26fe56ae08432df8a34edee1e84f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:03:28 +0000

    part war confuse punishment tend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def is_odd(n):
+        return n % 2 != 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
