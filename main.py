800306c18a42772bf39dcb06208dcca16dd3467c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:04:10 +0000

    rid owe promise war fault

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import time
+print(time.time())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
