53908bfc6674779316936cda1dd4d6bdb4706f5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:38:22 +0000

    run me remain together worth

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
