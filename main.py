2f2669033a9b2e8a458c275b4d79634c720f0601
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:51:56 +0000

    post home load matter mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
