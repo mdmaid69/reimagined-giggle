b97fcf0663eb81dd93e811af370fd15c7cec5f49
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:32:34 +0000

    cruel blame main to lucky

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import sys
-print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
