4b1c34c72ea8434fd234b5d80f10b158697efc1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:08:04 +0000

    this development Nn mistake bread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
