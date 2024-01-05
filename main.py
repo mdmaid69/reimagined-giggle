dc2dbf190f3b1cb5740ee8b86f01e9b2ab46a820
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:27:32 +0000

    hard depend inform with hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
