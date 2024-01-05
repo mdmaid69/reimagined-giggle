a0dce71aa51a0357008e89f99ce0e7b738142f23
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:05:40 +0000

    splash example flavour nice separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
