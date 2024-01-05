bbd91f9ec661c177d96e3f8ad6101e1eb559f449
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:34:09 +0000

    independent whole practical approve yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
