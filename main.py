40cc73cea9714a33d1dbd7cc8341adcfba2402de
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:46:09 +0000

    thick yet slice balance question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def clear_array(array):
+        array *= 0
