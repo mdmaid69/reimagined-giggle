d5cf2caf8b5068fcbd51dc9cde7d80e3aca1778d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:50:49 +0000

    exam path Ee odd tape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_current_working_directory():
+        return os.getcwd()
