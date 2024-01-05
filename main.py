a8a29ae141ec59d3220fdaa2e7d140b949421d3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:58:31 +0000

    form impatient flood nature upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
