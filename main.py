742e5ed28ac35b794e10c3b687b468db558c8e57
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:35:49 +0000

    weather bringdown burst basic change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_counter():
-        return collections.Counter()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
