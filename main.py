1e72f7580ccd9aac28437b2944d1307304105bcf
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:40:14 +0000

    OKorokay breathe day occasional clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
