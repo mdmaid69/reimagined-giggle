68da5096e9d485bcc0bcb577e90431b3d29d7d16
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:09:48 +0000

    charge unit different cheat introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import time
+print(time.time())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
