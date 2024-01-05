9be82bd34e7b3b16a90955af4dc2937da713934b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:08:01 +0000

    face agreement neither fork report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
