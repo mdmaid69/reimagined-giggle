74d2534b16bf3b23f5905a38c3ebeba004c77ae7
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:52:12 +0000

    upset address dig satisfied exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
