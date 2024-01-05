81a8c5e4c7edc89f6d792d95ac019b10abf778b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:11:08 +0000

    Ll talkinto progress tongue seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_pressure(force, area):
+        return force / area
