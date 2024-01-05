1d955922f9481a936b9040c1f02ec0798a55c71c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:42:39 +0000

    drive far fat bang basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
