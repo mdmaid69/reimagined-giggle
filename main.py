330e0da9cbc939c8d22686f279a383e5bfeec028
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:04:28 +0000

    group answer blow criticize dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
