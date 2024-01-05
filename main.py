7602b6741e7111c8816f4d0a89ee126b642cbc15
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:53:04 +0000

    on summer capital repeat smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+print(math.pi)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
