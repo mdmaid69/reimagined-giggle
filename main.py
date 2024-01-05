37445276e52c416c25a1568fa41b012f8b17688e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:39:38 +0000

    rank stiff scene mark strong

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_speed(distance, time):
+        return distance / time
+text = "Hello, world!"
+print("Uppercase:", text.upper())
