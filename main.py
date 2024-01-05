5cf535e52518a1f1dac89c12c4aca9b440bc2b35
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:00:02 +0000

    row build figure behaviour profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
