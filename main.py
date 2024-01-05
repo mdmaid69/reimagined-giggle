468e7bdbeed0ae0bcc321511187a03ac7b4d9f66
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:56:22 +0000

    bye sort turninto forget trade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_speed(distance, time):
+        return distance / time
