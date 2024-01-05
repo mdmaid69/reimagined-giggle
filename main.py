6f9606fc8e8d5395a7ae67841ef4b8f2bd5306d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:27:08 +0000

    cry result fun considering who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
