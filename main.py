9445c9e7a188830fdb015fa515296bbb12dd476a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:35:22 +0000

    niggaacademy previous guilty Kk busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
