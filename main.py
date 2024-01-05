4706208feeb24e0ef88347966994ace44ed8321d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:11:40 +0000

    your ahead environment date bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import collections
 def group_by(iterable, key_func):
         return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
