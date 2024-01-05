8041da32e13463951c336fb45971a01bd62d1259
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:51 +0000

    average point later plastic needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
