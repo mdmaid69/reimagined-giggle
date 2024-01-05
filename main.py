0fb54ecd7c31083b88b5da5d36f0dfb19d6a63be
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:09:02 +0000

    order left rather prize task

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
