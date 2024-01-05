d559bc08c78ec789ccf7d90b5707ee235e7640b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:21:29 +0000

    roof match old neither formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
