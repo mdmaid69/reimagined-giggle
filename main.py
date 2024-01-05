6e91daf58353120676dd3270f3e9ab12f001fcb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:28:33 +0000

    deaf comeover regret world exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
