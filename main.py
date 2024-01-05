4fc9a09d57c72e2fc8a9351f44e426c2b65c77f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:48:26 +0000

    frequent offer nervous during broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
