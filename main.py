d2f5b2c4e98be1ca9ffa8ec236775cbc163c7c96
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:16:15 +0000

    form dress fork dance previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import os
+print(os.getcwd())
