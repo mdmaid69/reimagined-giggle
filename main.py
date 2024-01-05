77a0f0a150cf1a174d09594c575ac5b734b11bce
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:45:02 +0000

    spill dark top attract achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-x = 10
-y = 20
-print("Sum:", x + y)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
