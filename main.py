b85c4879980db6ff517c8df84121f7fd46977b2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:57:11 +0000

    decide black human same deserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(5):
-        print(i)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
