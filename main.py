0f66214c53588ca7ffa4487daf097025e56c03f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:51:23 +0000

    under glad someone brave quantity

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
