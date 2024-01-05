62edfe1f9db738ebf5328b1e7968afc26dce98d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:26:22 +0000

    appear pour certain beyond condition

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
