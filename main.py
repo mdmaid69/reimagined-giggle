89bef86642b3593950233e0290f75e6095774482
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:29:53 +0000

    conversation inquire pot beyond everywhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
