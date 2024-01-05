49b2cb222ee311d4cb507d57e0dd6b8f9768846e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:37:33 +0000

    worth material action wipe burn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def convert_to_octal(n):
+        return oct(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
