d5225ab522aa9c2befb171e9e5f0b48d1c11e511
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:39:10 +0000

    health bright admire owe once

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def remove_duplicates(lst):
+        return list(set(lst))
+for i in range(5):
+        print(i)
