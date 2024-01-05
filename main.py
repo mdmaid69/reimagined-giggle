6bdb7bdd6bc0aa812e3e9d13b22c683aee7d876e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:59 +0000

    about point travel food object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import re
+print(re.match("h.*o", "hello world"))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
