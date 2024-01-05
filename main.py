fc92a6f91b5ae7e6b8e74e620fc04bb945edc563
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:11:11 +0000

    any influence behaviour level greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
