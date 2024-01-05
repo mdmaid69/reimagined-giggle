d593734010bd9b081810aa5cf3b5d6de57105a72
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:24:08 +0000

    fast rise however description pay

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import re
-print(re.match("h.*o", "hello world"))
+import numpy as np
+print(np.array([1, 2, 3]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
