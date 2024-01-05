4e89b1c6eb81910a9f994e8d9fd1d78e70f20059
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:53:06 +0000

    cry concentrate themselves they test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_speed(distance, time):
+        return distance / time
