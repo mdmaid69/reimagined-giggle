2e7cc563ee35cfaee850c1576c210822c5d07fe2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:49:44 +0000

    clean safe negative generally ours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
