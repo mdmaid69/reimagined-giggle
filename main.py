c8680e4c744aaacecc0d06e7aeed33c72810df71
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:54:03 +0000

    from trap road offer burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
