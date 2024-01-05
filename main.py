f6ccbedeaff7da6962d7bbb7b69829e1e43e7a11
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:00:20 +0000

    remember afternoon takeover result bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
