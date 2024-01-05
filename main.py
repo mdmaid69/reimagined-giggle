f2efbe186b435b0eb51e3ee5a541bfaf4c89d2e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:33:17 +0000

    mood dear outside big rely

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
