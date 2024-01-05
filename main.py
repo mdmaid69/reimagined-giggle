b11847730fea8bae4447dcd82f2eda2798ca69de
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:34:24 +0000

    somehow public fine beneath tone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
