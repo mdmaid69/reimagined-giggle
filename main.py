7a791f31e5375901a1402f36a04ada6bb6d0d8ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:08:44 +0000

    breathe pride up means promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
