807b6df60ca1c5a904fd2112bff98fda054bb89a
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:08:39 +0000

    count politics join child wave

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import platform
+def get_python_version():
+        return platform.python_version()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
