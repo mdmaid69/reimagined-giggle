08d7346652904e8db28c53f894b9f1f31d0de284
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:48:41 +0000

    steady low unless suggest door

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
