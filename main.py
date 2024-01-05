a1c258511425dded1b573b798986bef1353ef16d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:28:01 +0000

    disagree test joke cry recovery

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
