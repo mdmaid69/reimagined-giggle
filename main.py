8f2f65e06780b4225ce5a173caf548d35f081469
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:20:26 +0000

    spot everybody whistle bus congratulate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
