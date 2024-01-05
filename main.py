f2a6946108890b8c813299688bd42c7b1135d2bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:46 +0000

    concern half represent exactly add

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_speed(distance, time):
+        return distance / time
