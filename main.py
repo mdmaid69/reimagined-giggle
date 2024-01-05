47e8d43ace51b7e2d084cbd01a30d49aa9a60798
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:53:56 +0000

    tradition his surface dear shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def add_numbers(x, y):
+        return x + y
