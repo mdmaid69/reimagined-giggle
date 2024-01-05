078dcb634b244f58ef990cbd6a27ce9a5cf1deb2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:20:02 +0000

    abroad breakup fence mark tip

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
