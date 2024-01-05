d4d1919af8c3e5b24b9cb378fbd8dc34a06bea8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:30:14 +0000

    dinner meeting turnout important anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_power(work, time):
+        return work / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
