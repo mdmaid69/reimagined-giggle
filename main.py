5cbe56ab3b032920befd968d95b25b92026644eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:49:05 +0000

    cold strike boring but no

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def convert_to_binary(n):
+        return bin(n)
