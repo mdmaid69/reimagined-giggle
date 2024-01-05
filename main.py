1a84baf1e3cb502415ea9cd534be11639a2f8350
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:26:05 +0000

    programme tense bother strike rescue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
