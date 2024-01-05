7c14111fb00ea2685e77867ccd1f275c143a76b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:27:38 +0000

    cruelty instruction record skirt centre

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
