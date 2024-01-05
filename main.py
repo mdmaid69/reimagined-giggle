7f3cd9505310e7e6b67aa70ef94853beaccfc3b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:27:32 +0000

    spring fight turndown suggest free

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_list(array):
+        return list(array)
