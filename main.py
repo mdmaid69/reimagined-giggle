e00f4ae41c74f669f79d143530c913b4bc48d984
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:05:34 +0000

    compare mess lead check bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
