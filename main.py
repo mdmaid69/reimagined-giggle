d5b1934dcd77ccd0817793b3142b3d8d6d11310e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:53:29 +0000

    accident one giveout tax unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def is_even(n):
+        return n % 2 == 0
