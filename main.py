a4bd1fa1b033997bea871ec9ca25f930a1c5188f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:36 +0000

    anywhere rush dare hit illness

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
