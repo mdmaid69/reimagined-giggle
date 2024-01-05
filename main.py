f7e8682c67bcc68b0583eb66f66abf10b516aec8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:46:20 +0000

    genuine false post bin wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
