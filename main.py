6bdca26a29a5fdfcb3d228a95d98a5ae8df60930
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:35:28 +0000

    damage upset whoever pleased pride

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
