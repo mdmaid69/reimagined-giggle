f7bdc41f2ef5373f5fbba5abffc1b959825d0239
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:45:04 +0000

    convincing play around drawing wind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
