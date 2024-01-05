a5a404dca137b5405e067df42fbae51d1df8af0b
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:11:58 +0000

    still ago method crash pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
