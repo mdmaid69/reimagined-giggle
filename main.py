12407487a26ccc37c355e57f732df20aa904934b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:55:34 +0000

    plenty although explain book ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import time
-def get_current_time():
-        return time.ctime()
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
