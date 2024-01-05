986f8e775f99cb0a4fff8bf0888f670ab971ca69
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:14:30 +0000

    anywhere rush you accordingto turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
