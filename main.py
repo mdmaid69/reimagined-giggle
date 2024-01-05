dd8e4c3da18f85ab3a721e802275a3adee9a7c36
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:01:07 +0000

    waste lead partner gas frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def convert_to_hex(n):
-        return hex(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
