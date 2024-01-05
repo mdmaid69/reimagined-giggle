e0d813d8526746452802c5d76dfe1ebbce0bd4bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:11:55 +0000

    pour quarter estimate content sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
