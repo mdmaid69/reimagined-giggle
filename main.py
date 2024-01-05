41a93625fb7075cef8a0035a6aceb23fe71129ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:49:40 +0000

    dress move use music fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
