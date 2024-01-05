4f64988aa371445242abaa31800f113cac4ce369
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:44:30 +0000

    whole solve dead introduce age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import datetime
+def get_today_date():
+        return datetime.date.today()
