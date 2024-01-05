e816bbf562af9dc39aa328ed7cbefc0881b889c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:10:51 +0000

    drive lot receive arrival school

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-print(sum(range(10)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
