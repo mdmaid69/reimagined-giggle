2234c7c49340ee47f581ff0871dd1f00337cbde3
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:44 +0000

    attention short moment unusual risk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
