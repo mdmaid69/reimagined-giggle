cf6f649e07380b283fdd1d1bd744c8c78bf09300
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:48:29 +0000

    dare cool stretch everything able

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import sys
-def exit_program():
-        sys.exit()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
