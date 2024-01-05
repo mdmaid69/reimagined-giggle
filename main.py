e07652bef372bf671381ae01d0cc354924f8e038
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:48 +0000

    stir broad flow emphasis field

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def is_even(n):
-        return n % 2 == 0
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
