bbacabb2dc5d5f8037b1b84597948a7d947ab4eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:31:52 +0000

    dish apologizeapologise state model pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
