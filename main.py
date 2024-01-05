266abe31c2246336fb5dec3b8d7270019cd092ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:52:45 +0000

    run apart declare rest pattern

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
