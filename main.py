de6b7c0b5f88edb557b65d8f41007c7d0a8920a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:46:41 +0000

    cry lookback(on) pinch seat trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
