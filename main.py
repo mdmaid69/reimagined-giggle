da602c335f80c1738e3023f0a3735742830a1b07
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:12:23 +0000

    history him course support clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
