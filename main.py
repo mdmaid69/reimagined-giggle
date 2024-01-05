0b77cac1941d1947e5230397104c86d04b4cacec
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:35:25 +0000

    179bite approve state joke announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
