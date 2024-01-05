ab2b13a5c9be6eda36445cc3a97a479596498b4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:02:23 +0000

    finger copy leg lookup bar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_set(array):
+        return set(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
