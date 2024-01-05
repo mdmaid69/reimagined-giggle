2a87dcb6c36c5434311a297583b9d8e1bca18622
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:41:39 +0000

    fierce drawing reduce sensible left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
