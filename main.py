fb063f535bd4ec9e3041a3dbb470314173a49600
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:16:27 +0000

    discovery world talk matter exhibition

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
