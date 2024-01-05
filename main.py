d5d28d9ded634f4c0bc9e42c862a737c2c4cf00f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:14:33 +0000

    cause ill taste drop apparently

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
