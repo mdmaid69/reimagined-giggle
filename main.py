4ec6bb406e8040fe4248417ada006ff3ab8ce939
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:34:58 +0000

    trial stretch switch length callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
