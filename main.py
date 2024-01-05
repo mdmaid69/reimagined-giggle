adf5d36434b8bbf3f03913aa91da9cbde89488fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:06:02 +0000

    giveout treatment fond quality party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
