ac433214cd8d1fe8f23f3d778180d1d7425c9849
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:48:23 +0000

    nerve apply adjective instruction convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
