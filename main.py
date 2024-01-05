538e2243ccc3025a69945c23ba8157ce2e26c3da
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:30:09 +0000

    straight which anywayanyhow care laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def count_elements(lst):
+        return len(lst)
