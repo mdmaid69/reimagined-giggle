bb47fc9e3e5d614e43f67a8703d411e93c2bc05f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:20:08 +0000

    yesterday cruelty other several behave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
