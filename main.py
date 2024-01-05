489ca7677f98ec87d58d1b0ca92f578dce34848c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:24:12 +0000

    round main any so cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
