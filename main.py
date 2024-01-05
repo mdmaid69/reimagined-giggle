79ec72cb08f2d332f32914fd3f4ac983e4dc46f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:21:46 +0000

    take old guess through account

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
