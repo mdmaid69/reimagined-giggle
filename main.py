9814c0872ca582507055add93a3352f79edddeac
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:41:00 +0000

    reach sudden point failure goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
