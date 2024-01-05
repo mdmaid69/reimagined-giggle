9c80943f660181a20a6457a9e0b43a8344d26e4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:03:20 +0000

    square fear gas finally thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
