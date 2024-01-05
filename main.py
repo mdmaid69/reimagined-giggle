87ce73ab180c0976198ee6f5a3c5cecc9caa16de
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:30:15 +0000

    foot seat wave considering middle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
