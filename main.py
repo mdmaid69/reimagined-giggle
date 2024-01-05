03bc5b468f2c1bd96ab9f4dfae4128bf4881ef88
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:34:02 +0000

    error treat smooth kill drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def split_path(path):
+        return os.path.split(path)
