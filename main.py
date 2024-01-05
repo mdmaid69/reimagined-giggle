18ef24f07fe20508b15c2ae6d05c06b6b36d5735
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:58:24 +0000

    good uncertain interfere us office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
