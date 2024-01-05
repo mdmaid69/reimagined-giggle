b4e2a4d6cb9717094c7779e5f82dab0c2a1d1eec
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:11:06 +0000

    line fine some complete yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
