22ba41aa150544abe9ae246429f1d82ef945fbc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:25:43 +0000

    level wire tread this sensible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
