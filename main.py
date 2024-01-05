efbe4844162e334852ae00b2fd15b15ebb1ff568
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:22:17 +0000

    bother polite late mainly match

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
