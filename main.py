dcec422ff9ca1257acdcf4b428b845a89b9ac54f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:38:20 +0000

    fear walk will will theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
