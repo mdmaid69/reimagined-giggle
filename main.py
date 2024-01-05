91d132fa619139e9c5f779702c0ab6a884057178
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:21:09 +0000

    where stir callup university discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
