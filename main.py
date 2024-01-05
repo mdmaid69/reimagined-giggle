29717236bb1b2eabc1c8d83157bafd806aaba33b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:07:47 +0000

    mind report it base cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def subtract_numbers(x, y):
+        return x - y
