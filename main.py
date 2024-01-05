69e7a015ff959912073e27c664903378fedcae22
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:44:49 +0000

    dark nigga loud wander left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_set(array):
+        return set(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
