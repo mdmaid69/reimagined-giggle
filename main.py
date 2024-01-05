00cfb064fd6851ae14a989ecf5c206940d8fb174
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:57:27 +0000

    crash rob already main long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
