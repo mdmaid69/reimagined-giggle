715e3c1c47c8cbcd049e42637a66321bb6d51d30
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:29:38 +0000

    takeout truth before scream spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
