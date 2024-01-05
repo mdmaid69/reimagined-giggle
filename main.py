4558a7252b071fc20449e753cfbfbb508495d73f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:05:00 +0000

    getup scale impressive hers community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
