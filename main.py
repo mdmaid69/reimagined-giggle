745dfd7a184486415767bf2ff53886dd77e7dd14
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:44:13 +0000

    pair away behave calm splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
