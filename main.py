8850e2a75cf2f1dad547684155c5ac656b35a640
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:49:39 +0000

    excuse on spare matter force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
