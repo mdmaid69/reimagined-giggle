e7ce39fcc77feea548e5577d8733f057b8f8c235
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:34 +0000

    shrink pure through muscle unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
