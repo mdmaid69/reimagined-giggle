0c1b0b879a51a2f7c1b36b94a60b02dc3b6a1a9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:20:35 +0000

    fence calmly car scrape whichever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+text = "Hello, world!"
+print("Characters:", len(text))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
