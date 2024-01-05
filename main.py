4719ea65573c925a21002b7da6609c4f5efb46f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:27:56 +0000

    cruel ago variety spare Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
