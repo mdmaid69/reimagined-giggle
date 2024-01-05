f98037342c65de46bd7fa44ae28c6a1e55c0dab1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:24:15 +0000

    correct pressure suddenly so game

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
