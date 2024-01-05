6368ab5c6d433f33b69b36cdc493be4a1bb638ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:52:57 +0000

    fit uncertain make there war

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
