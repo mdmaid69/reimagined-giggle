ef5c02ba9dbecb26fb3529fa1e0d56c51e03cb59
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:43:33 +0000

    bridge behaviour exercise undo pin

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
