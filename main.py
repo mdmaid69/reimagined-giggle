ca6c4e8cd9b128ae577614222e40e3a4b360950b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:06:33 +0000

    single than lookaround manner hardly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
