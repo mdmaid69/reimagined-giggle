3bbc555923d9c63d3a916b5b2739f4d41837cf47
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:34:32 +0000

    supply science lift song show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
