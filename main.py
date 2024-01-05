8b92155bebf53bc4f38fb1653610e74c04ed44dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:14:03 +0000

    peace break report bringforward flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
