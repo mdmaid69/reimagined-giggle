831fc9515e3bcfa118ff670812940cf9dafe13c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:55:37 +0000

    represent hand popular wipe maximum

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
