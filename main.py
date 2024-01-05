2e0b311e9c138a6e5ad1481f17855e244dd7ca4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:04:19 +0000

    collection welcome capital imagination makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sys
-def print_python_version():
-        print(sys.version)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
