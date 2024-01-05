e0b156ab1d6c81ee62491f49b41bd5911c26000d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:53:42 +0000

    popular nearly roof total regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
