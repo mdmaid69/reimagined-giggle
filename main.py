e4faace1d285de60b209ee82096bab6b900b9e57
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:20:00 +0000

    peace zero gate callout empty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
