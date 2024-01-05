d1a78874899f100d1958a8fcd8aea1c87f907e40
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:02 +0000

    suckme collection shallow valuable side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
