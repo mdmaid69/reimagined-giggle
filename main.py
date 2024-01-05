9bbb832f54ed37dbfe571be92cc93248a2a15062
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:41:08 +0000

    duty skirt lift food responsible

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import datetime
+def get_today_date():
+        return datetime.date.today()
