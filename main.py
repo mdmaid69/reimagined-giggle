07232dcfb9c102ee7e7c9cb4793ac80ae733d361
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:36:36 +0000

    spray conversation study minute lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
