272d1100aca1ebb6351d332f2f77a5965e4e3973
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:05:25 +0000

    callat dig taste introduction possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
