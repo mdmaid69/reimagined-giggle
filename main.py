d7d8ae39865e3611c8b2d46aa611b76e056ff066
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:55:00 +0000

    repeat leg several surprising careless

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_density(mass, volume):
-        return mass / volume
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
