bb70f4f8daf86548d5497dfd7c9c927e915ab762
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:14:29 +0000

    instead rest judge character Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
