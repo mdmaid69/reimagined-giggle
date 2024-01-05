10a520e35cfd7b24c184fbd26bbe9d89c95834b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:19:33 +0000

    especially separate above once experience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
