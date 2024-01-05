1f5d68c68fa8bc257fd81b2db447cee20d64f821
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:33:51 +0000

    shake once entry successful frequent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
