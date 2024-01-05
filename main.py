5057a3eac9aed7e31f4f99593fa1fc1ca5790a9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:00:43 +0000

    dinner realize patience tired gradually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
