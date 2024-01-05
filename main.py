b6d8ba184775f55e3286cd5e88c6b4afc1e6de4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:27:16 +0000

    dig yesterday somebody company neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_power(work, time):
+        return work / time
