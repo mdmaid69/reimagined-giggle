a29966b243a3ed9aabd162f4564359137e10f723
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:18:55 +0000

    violent always any shut relax

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
