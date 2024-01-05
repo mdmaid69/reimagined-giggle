58d396475513d62f87204cd74f39b4d9c2b2fcc1
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:07:34 +0000

    sight means measure dry disappointed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_area(radius):
+        return 3.14 * radius * radius
