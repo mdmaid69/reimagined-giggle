cf8f9ee099c8e4a3fab50d77d03bffcf51f61573
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:50:26 +0000

    laugh her raise set middle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
