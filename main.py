5ff2936ef8fadaf9a1a848fa93ed9d083ded9c17
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:30:08 +0000

    ask continue long call head

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-print(os.getcwd())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import tensorflow as tf
+print(tf.__version__)
