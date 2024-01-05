a94b2132a698f925bb2d565bb5518dffa2a33882
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:17:24 +0000

    back sure rob record practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
