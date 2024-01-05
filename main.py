4655088b9d6d67a80fac036eafaa6313a6b51b9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:01:11 +0000

    closely far love seat rise

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
