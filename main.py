cf96d29164ba4d1b51edc5ecd95f0ed38f55b7e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:58:42 +0000

    side broad unlikely dress peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
