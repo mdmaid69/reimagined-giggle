fda8b20c42a15b6b9ddfcc7cdfa63c1502326022
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:01:12 +0000

    practice raise strength object brown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
