c0d18eee746be10df4605d128f1a8c5940a165dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:10:59 +0000

    natural fire takeoff have consider

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
