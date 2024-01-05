6240efa33e160ae281176e19802de5261e909aec
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:43:54 +0000

    trap train sad good trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
