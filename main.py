43bb5d9f9a2986be247064cff7a23f60d38d905f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:31 +0000

    pleasant commit develop expression snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_as_bool(array):
+        return bool(array)
