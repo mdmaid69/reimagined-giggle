8c205bcbcd9048dd896e2bb6868e6777d0212b4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:36:20 +0000

    hand basic wrap crash old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
