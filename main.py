de5842bd5fefa5cdf63b7e512bdc3478efb108e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:19:01 +0000

    possibility if arrival young slow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
