cbf7c9ee1ee75f6456ddf2180a5b7bfc0396be95
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:24:47 +0000

    copy join season sit outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
