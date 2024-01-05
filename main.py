9c4dfb86fd1639861fc4bbf338c8103f05a7cb0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:18:18 +0000

    level about active regular instrument

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_speed(distance, time):
+        return distance / time
+def find_max(lst):
+        return max(lst)
