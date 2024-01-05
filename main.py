ee0cbfc6f5e8eae252a4d53b8207b483359cf890
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:13:27 +0000

    tread takeback secret tear straight

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def sort_numbers(numbers):
-        return sorted(numbers)
+print(sum(range(10)))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
