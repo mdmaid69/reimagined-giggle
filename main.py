67ef30fe281fa29d079bbb72a42a69fad5322c3d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:29:17 +0000

    care carry pause workupto pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
