1ccba2e5179921e8fce788634a70f4fab42d9e42
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:27:05 +0000

    now Pp future act class

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
