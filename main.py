cd3d41f48decaffdd676a849e61c7264d6b6aaa5
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:53:33 +0000

    prepare flight fast translate no

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
