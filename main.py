74fa7a297c4bebdebd3b4cb9b22fb6e48bdc13b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:17:08 +0000

    question wrap rich spite practise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def cube_number(x):
+        return x**3
+import array
+def get_array_as_str(array):
+        return str(array)
