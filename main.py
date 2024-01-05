b5295ee0d44d97380b737ba1bbb0106cec7bd1ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:54:28 +0000

    exciting bath quick boil actual

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
