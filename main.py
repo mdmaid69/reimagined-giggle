bac4f0004014b35de14a4b43926578a7b9c572f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:26:33 +0000

    key to fair all comeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-print([x**2 for x in range(10)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
