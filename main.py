f990df26a1de2b86c18d1f0bb0bd57ab66003ca6
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:13:51 +0000

    unite while glad behave worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
