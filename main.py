880f51c68f8c14a0e7495add95396e9fd0b71049
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:32:44 +0000

    exchange impressed almost takeout pleasant

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_pressure(force, area):
+        return force / area
