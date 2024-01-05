e0f7a6bd1522750fe28e7848a204e83f78cfa392
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:58:46 +0000

    piece regional back test same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import time
+def get_current_time():
+        return time.time()
