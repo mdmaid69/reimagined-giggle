fff9b61c62bee31f53153d758d903f03fbac42cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:20:16 +0000

    middle guess string choice measure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
