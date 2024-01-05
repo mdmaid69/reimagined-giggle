c2a7bd2d77ac533260559d483b14b0533573d7f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:25:49 +0000

    breakup father show rid beautiful

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import sys
+def exit_program():
+        sys.exit()
+def count_characters(sentence):
+        return len(sentence)
