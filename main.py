fe7f0fb7e28d2567ebae5e152b1bfb29cbbb41fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:25:18 +0000

    stick chat left blue charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+from collections import Counter
+print(Counter("hello world"))
