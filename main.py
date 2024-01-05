36fbd1ce2981d345bf8f0cc51d956a62f425c957
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:40:41 +0000

    565end passage blue bridge serve

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-print(sum(range(10)))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
