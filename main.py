7598abb92c3e8ede5c1a4d4e1cf4d9712ef5387f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:02 +0000

    shake inside nor patient light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
