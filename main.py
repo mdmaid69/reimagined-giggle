0a96746f05b4b763fd8721ad6f9a862a2f7cf49e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:36:24 +0000

    habit aware now tip mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
