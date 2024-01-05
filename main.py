89b47e86d177f9a8ffdc051513e50c1b765a8705
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:40:41 +0000

    mystery shoulder climb surprising calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
