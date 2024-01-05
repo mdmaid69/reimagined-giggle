037d5b2d51f85a283da94bc11b6d30c9fec57fee
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:42:00 +0000

    improve without repeat from without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
