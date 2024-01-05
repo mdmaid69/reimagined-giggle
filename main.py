1d9decbc8ac25c5ea730f11607b8a1616203d7de
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:50:23 +0000

    communication goback(to) course set later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
