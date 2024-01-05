b079de02ceb024976c253cd6910fbb0b82451fb6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:48:24 +0000

    believe wave interfere immediate walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def find_union(list1, list2):
+        return set(list1) | set(list2)
