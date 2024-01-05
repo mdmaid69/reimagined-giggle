e72b865a217e9c5f11f88107e4b8666b2ba33b3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:45:08 +0000

    owe adjective shake patience workupto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
