a80bbfa17946911100b0be984d05c205d924bea0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:33:57 +0000

    opposite shop bury cheque opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
