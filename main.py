24d7834f9f8066eb45706ce557560cbcf5b40863
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:56:24 +0000

    life become brief just one

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+print("Squared:", [n**2 for n in numbers])
