e446ca9ad312c697df43cb579173ddec7f2351ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:15:46 +0000

    finish surprising show wipe dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-print(sum(range(10)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
