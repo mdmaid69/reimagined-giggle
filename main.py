6678c768977ba9279aba4f64f97074543a122c8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:26:02 +0000

    plain shape free succeed supply

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
