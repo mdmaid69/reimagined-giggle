ab2fa7028789383321aab8e1dc1c3bc0c522c9b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:11:35 +0000

    unite serious time somewhere holdout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_length(array):
+        return len(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
