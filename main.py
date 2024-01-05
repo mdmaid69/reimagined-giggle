31f4c97331f057905273e7b7185f76f100376129
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:49:37 +0000

    zero harm square regret swim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
