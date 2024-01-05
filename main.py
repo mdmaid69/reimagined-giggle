3933353a3b28fdc97f169496951465313e242f12
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:11:36 +0000

    recovery pause temper quarter pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def is_odd(n):
-        return n % 2 != 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
