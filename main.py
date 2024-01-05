96fa68bf9e018beef11682c1690d8e48dc5a0744
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:29:35 +0000

    sister interview out influence fetch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_item(array, i):
+        return array[i]
+import random
+print(random.randint(0, 100))
