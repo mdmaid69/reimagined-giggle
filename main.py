a7174b13074445ec0b47c8d1daa917903d1602c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:31:05 +0000

    seriously again practical answer sink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
