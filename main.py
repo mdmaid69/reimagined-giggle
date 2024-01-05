0a2d71b514a654355dbe9e8b292d65b096174741
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:16:55 +0000

    remain steep watch shoot perform

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_volume(length, width, height):
+        return length * width * height
+def reverse_list(lst):
+        return lst[::-1]
