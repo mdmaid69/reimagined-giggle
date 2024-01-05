56572392a1fc070953e6e2ae5e77017f8667853d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:04:10 +0000

    lost large more usual his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
