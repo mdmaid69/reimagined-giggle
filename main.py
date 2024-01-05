1e1eb9a2f4b2d69615a9312e8573fd9db307dea2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:45 +0000

    easy urgent yes flow few

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
