c01636a50ff2ba1289c3c9ba670d7f1e94516e15
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:33:11 +0000

    clear getback wave word remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
