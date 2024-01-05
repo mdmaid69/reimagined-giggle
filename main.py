a9f5e4c350361dfe8ff68ec3b31a0e96ce04395b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:53:36 +0000

    wave thinkover interfere general Oo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def count_elements(lst):
+        return len(lst)
