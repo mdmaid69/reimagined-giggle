2e80c2e22e3aa4fc658e21382e4a99dd96cf7f23
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:53:12 +0000

    warning worse operation lookup cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
