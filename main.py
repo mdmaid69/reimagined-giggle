bad302fec15082aae7b2e3f77032e3c6c97b9d56
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:36:51 +0000

    cross university estimate many circumstance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
