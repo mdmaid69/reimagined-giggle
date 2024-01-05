ca132e894294bdde431ad95819d3fb2f90161e93
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:05:11 +0000

    change knock tip positive centre

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
 import collections
-def create_stack():
-        return collections.deque()
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
