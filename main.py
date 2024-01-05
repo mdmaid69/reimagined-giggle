3eb15396b4cbaa7dc28af151d09eebf9e59f4c57
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:39:26 +0000

    motion tie offer wave exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
