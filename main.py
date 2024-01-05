2abaa0d2d46084aa987a585d6ad6dc910b68b23c
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:16:10 +0000

    object by second embarrassed available

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
 import array
 def get_array_as_bytes(array):
         return bytes(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
