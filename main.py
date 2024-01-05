0b74030b79403dc326fa4ebed795b44ada883aad
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:54:34 +0000

    judge height certain stiff stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def count_characters(sentence):
+        return len(sentence)
