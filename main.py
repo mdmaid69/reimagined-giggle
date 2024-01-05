19dc10a00445d6af5506dfaf35419a254ba621a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:53:45 +0000

    fire offer funny 571enough tragedy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
