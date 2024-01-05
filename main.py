e78cfdc51ca4ce47ce44e03581facb215875ff05
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:12:28 +0000

    mean rest work talkoutof lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import sys
+def exit_program():
+        sys.exit()
