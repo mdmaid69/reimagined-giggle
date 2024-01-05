381821042babd48bbd9c3007c9f3c481a92dbf35
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:28:20 +0000

    produce turninto share culture comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
