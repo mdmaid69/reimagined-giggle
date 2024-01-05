4964c09bde5bd916d60e1daa149d95603c380a3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:49:17 +0000

    sure cry release mass cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import csv
   def write_to_csv_file(file_name, data):
         with open(file_name, "w", newline="") as file:
