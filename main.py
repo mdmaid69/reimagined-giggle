e371f5e9408df573298ded7675a6ea5776e64126
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:50:23 +0000

    busy high balance well quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
