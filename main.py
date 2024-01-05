abfb34190d337faf2e315e0d362b9d507967d40e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:06:19 +0000

    throughout amount without sail uponon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def square_number(x):
+        return x**2
