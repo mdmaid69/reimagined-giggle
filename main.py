faa08938175b6684278ce5bd60e124d68667f39c
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:21:29 +0000

    read demand short embarrassing steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
