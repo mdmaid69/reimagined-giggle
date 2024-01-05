f075e3aff28211718205d62f6bfe3dc9e6ec7cb3
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:32:26 +0000

    but by whether past bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
