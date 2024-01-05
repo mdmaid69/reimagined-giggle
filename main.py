39e123086b5d9a26d76b4ae7bfd782d768dcd51c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:48:59 +0000

    Itself least towards in show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_user_dict():
+        return collections.UserDict()
