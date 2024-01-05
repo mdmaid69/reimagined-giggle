513a7a87434e9c08b9d252197670b4f198c0dc2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:42:48 +0000

    indeed sight snow rain call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
