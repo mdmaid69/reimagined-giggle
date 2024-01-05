26e5f8e2c9b6cbf683daec04c7b4a50c41239774
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:33 +0000

    present their surprised discuss break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
