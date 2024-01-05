4b8a8563723b7e0ca608a729fba37d7ae544994a
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:03:55 +0000

    bet brief keepout disgusting fact

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
