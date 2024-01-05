8b036804bbb620ad4c268c1c5c59949496e78405
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:32:28 +0000

    form point switch chief trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def convert_to_binary(n):
+        return bin(n)
