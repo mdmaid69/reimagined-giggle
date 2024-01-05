e9409b9ca48b0f73970fe464f61bdd0c541383a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:52:11 +0000

    night short modern operation lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
