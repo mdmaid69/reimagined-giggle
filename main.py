e0871051d4f010e32dc69bdab818644c59439525
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:14:08 +0000

    callup test dry interview set

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def convert_to_binary(n):
+        return bin(n)
