cc0103b176da7a12e835ec7de0f1b10bb98e20b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:56:20 +0000

    immediately wet storm that pronounce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
