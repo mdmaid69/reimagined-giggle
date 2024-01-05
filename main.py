941a5d7b6cdbd7f6e768c9b49324f0f6acc3162c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:07:25 +0000

    pronounce occur instrument battery like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_typecode(array):
+        return array.typecode
