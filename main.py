805241dced97f929fe14acca16d12f692b9e5d9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:17:15 +0000

    and march cap control somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import platform
-def get_python_version():
-        return platform.python_version()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
