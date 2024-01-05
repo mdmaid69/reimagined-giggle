c5469e3843476946fad603b48d15caf5b52de32d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:49:13 +0000

    gentle period watch draw upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
