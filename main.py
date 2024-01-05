ac12f235ab316b7a54e171ac917b640f8719c627
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:08:53 +0000

    needle fast house wash stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
