02ef2035213f697fef2a5209dbbdb460a117d27f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:15:39 +0000

    run suppose need whistle sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
