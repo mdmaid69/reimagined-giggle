68e18be1304b9e69e17d34b25f49817128460abc
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:56:31 +0000

    occasional goback(to) either tidy action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def square_number(x):
+        return x**2
