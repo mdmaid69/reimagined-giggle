eaef737e049889af9139825694e0b2e20649f598
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:29:32 +0000

    quarter information catch afternoon around

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
