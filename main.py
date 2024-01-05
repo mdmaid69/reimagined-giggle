0a49024f592f3b7277759777231bae35804e8a97
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:00:41 +0000

    plenty save very centre winter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def add_numbers(x, y):
+        return x + y
