a0210f749ed4b98a89d5333328968034a4d315cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:26:31 +0000

    once right see benefit herself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
