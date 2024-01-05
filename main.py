cc3a78ba7216e0e72c25fed33fcbf022c69eb08b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:23:11 +0000

    late full fault stretch please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+import sys
+def print_python_version():
+        print(sys.version)
+import platform
+def get_python_version():
+        return platform.python_version()
