8bf23fca063b2cbe3054ddbeb25e5880b206e979
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:30:05 +0000

    destroy face rarely just noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def subtract_numbers(x, y):
+        return x - y
