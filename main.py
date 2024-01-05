550c271bc1fe9b71ba11b3623009f7701b373e58
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:30:34 +0000

    sound protection wealth general any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
