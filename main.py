24d925481c4ac472119ceb5d5dfc5f40031782ac
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:51:45 +0000

    this fill away deliberately behind

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
