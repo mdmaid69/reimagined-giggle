068f6c9e3d6b760218d22ca2208970077ca8279f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:40:04 +0000

    turnback disgusting education broad delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
