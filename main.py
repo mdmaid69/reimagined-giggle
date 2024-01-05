909887bb372659e0a29726898fc56c90f9f542e5
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:55:15 +0000

    breakinto Dd base introduce manner

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
