7dedc803dc3a8292a3d11d80a2fef09f0eefabb3
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:25:35 +0000

    apart after effect quick number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_item(array, i):
+        return array[i]
+def count_characters(sentence):
+        return len(sentence)
