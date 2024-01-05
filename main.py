aaab2102d9f426c80a4de42f7afab60c5514b81d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:50:11 +0000

    then rely common instead pollution

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def square_number(x):
+        return x**2
+import json
+print(json.dumps({"name": "John", "age": 30}))
