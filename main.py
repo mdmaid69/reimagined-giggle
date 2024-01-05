9429c4f9396562ec4f81bf5f9cfe03109ad3b659
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:18:07 +0000

    narrow concerning comein building whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
