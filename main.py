fe4ca00b8b77a825fec2f8403172a8c11556f33b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:47:09 +0000

    cause whoever about string piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_stack():
+        return collections.deque()
