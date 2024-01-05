cdddbc0955dae8726acebaca044140915801bc53
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:13:42 +0000

    secret too scale when grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
