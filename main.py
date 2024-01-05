12c2be74efc5a5aa3ec00524d3ff34e644afb23d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:18 +0000

    find ahead situation height block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import collections
+def create_priority_queue():
+        return collections.deque()
