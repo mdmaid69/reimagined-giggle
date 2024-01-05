71a0125581b187ebf1064e5ba28031b753f503f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:10:43 +0000

    deep mean suggestion follow Dd

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
