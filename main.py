6f00e9e7fe911c8a97b5bb7302591cbd7ab07fda
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:43:53 +0000

    quarter circle upwards dream Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
