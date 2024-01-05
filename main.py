fc02aec2d7cf0ad20b519ca655899d8cfbb67e12
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:57:34 +0000

    nonsense deaf smooth receive criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
