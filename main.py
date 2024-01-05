48497986dfe8b400b0515cd46892429f0a739619
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:29:46 +0000

    turnaway soon lift along mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
