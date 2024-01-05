f613f7ab719d4d906ceedcf468be3fbcdf8e1a1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:25:32 +0000

    mail takeback any interview anxious

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+print(time.time())
