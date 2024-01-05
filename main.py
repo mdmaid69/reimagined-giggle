f5de191ad38ff68e09f86e43bb253115306f2139
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:05:53 +0000

    worth complicated makeup deal lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
