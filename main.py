4a1aabcfbe26350fdcfdc3310289089c4be484a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:44:03 +0000

    steer 567enemy getback however spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
