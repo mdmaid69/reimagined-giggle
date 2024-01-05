c7315ff5f39390892a074a27f59aec8e30eef429
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:44:39 +0000

    communicate boat platform score congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def square_number(x):
+        return x**2
+import sys
+def exit_program():
+        sys.exit()
