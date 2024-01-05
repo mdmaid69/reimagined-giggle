d533d7d5333af9667397751d64579fbbd63d3f66
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:03:34 +0000

    long can model meanwhile use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
