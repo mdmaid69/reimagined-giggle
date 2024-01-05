0e58640b709140afb3b281a9216a1e0526bf2d15
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:25:51 +0000

    can capital Dd turnover by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
