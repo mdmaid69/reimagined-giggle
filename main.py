3d4727a9b68f02caeee63effd2f574ba5dedd528
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:55:33 +0000

    comeback away square fierce anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
