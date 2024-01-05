d831fecca9e790730df15118c278f515a77cfda0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:39:24 +0000

    radio within turnon strong equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_length(array):
+        return len(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
