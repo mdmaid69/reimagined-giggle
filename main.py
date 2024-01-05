72c55b100f41003f723faf48f00d67c32cf9713c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:16:32 +0000

    later if sale shall note

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import datetime
-print(datetime.datetime.now())
+def get_array_as_bool(array):
+        return bool(array)
