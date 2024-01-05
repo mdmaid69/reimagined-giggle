3bca7e1be54ecafe976a50af6fb5534f1eccf400
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:56:39 +0000

    nice talkinto suckme continue slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_list_from_array(array):
+        return array.tolist()
