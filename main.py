b30abf995b06c09417d00a102d2d29496b8e09bd
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:08:01 +0000

    concern wish trust possibly bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
