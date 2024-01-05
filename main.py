de1e637169a38694250e7c38a97c58e813357477
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:14:31 +0000

    blame excited with exploration share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
