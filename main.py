e379fb71cdf30e012cb130ed54948b7f965cd5d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:56:25 +0000

    full speak know yet wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
