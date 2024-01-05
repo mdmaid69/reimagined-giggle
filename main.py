82f02e2bec8c0a4add7a1dc169c7410c0909a577
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:21:48 +0000

    only consist please available event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
