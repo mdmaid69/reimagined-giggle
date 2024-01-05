19b7091accc4f8202b9d23e584b022a0c4f55c50
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:18:44 +0000

    keepfrom narrow possibly workout flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import sys
 def exit_program():
         sys.exit()
+  def convert_to_hex(n):
+        return hex(n)
