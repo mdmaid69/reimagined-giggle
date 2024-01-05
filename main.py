9047b186b305e76a45716ad8b041247f1524cacb
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:08:35 +0000

    strict appearance nearly spring first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def change_working_directory(path):
+        os.chdir(path)
