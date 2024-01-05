cbbf8855b3c92d7e345dffab7c60ff419a539ffc
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:13:51 +0000

    brief shoot even quickly declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
