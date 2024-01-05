25eae6134bd8714f73e795552c4b0cbd07cc13bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:14:58 +0000

    chief blind charge afraid tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
