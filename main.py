02a51c6ff8859c365b4ad30671fda709b19117f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:01:07 +0000

    pick official but breakout command

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
