685215627fc8b098708fec84b7efbd2fb2888e47
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:10:43 +0000

    react position wide deal tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
