7a3b9f28f5908f5fbdb05d802ad5e0e08da06b00
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:34:21 +0000

    upset information praise style stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
