15a1019299a5fef30553a34c27d1996231384254
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:37:27 +0000

    congratulate total grade start grow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
