58a236fdea55e80b723177dcb211c694c3e01874
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:48:03 +0000

    hope peaceful shop welcome grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
