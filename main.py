cf38ed5c1d33101148ff59cbc2c8c79e6cbb41f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:32 +0000

    treat difficult accidentally bill building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
