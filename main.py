bd56724d4838e6dd75e61c9409e2e2e7272b7d0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:15:22 +0000

    organize behind true from neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
