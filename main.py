fc10feebb45d722e8dbb70a5e0c329c88a528da0
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:38:11 +0000

    survive patience he project full

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def subtract_numbers(x, y):
+        return x - y
