e5cf458ca79751fe079aecc35391f19b1ece6551
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:53:35 +0000

    work kind over passenger wheel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
