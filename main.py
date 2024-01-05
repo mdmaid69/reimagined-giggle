49fbb8923fd0f5da1afaefb29673facb21c41efe
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:52:03 +0000

    nose crime disagree correct accept

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
