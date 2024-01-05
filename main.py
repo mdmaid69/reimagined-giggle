f68f4263f768f8cb6def3a58911a315130e85e4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:29:14 +0000

    part pinch all free accept

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
