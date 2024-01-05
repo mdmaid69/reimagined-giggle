f19e6850c072116e6dccee00f0bc074b7e6ea14e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:51:20 +0000

    Itself close wait pollution store

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
