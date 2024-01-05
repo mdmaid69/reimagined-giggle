97a3b731b8c1750d46dfc77540ca325be4be94a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:19:28 +0000

    thinkover road similar tap workout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
