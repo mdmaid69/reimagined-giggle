cbe3c49cfc4f5a9edca384ebb0e1a2082d47ce20
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:37:41 +0000

    unite lend manner ought combination

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-print(math.pi)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
