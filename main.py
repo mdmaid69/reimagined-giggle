f1f2c68bc55ec5ea8489ecdfea93fa39e1445a4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:58:17 +0000

    smile load storm real sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
