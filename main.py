6b21149bd48c39409423724da42a978480240270
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:38:05 +0000

    repeat neither myself develop cruel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
