1359c5cad3e504cf0441056b1e29a829b4bab99e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:00:33 +0000

    sort cough furniture combine deep

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def cube_number(x):
-        return x**3
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
