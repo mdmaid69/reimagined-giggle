46dadd0404ebdd1ce0a2e3b3fa099725724bd105
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:17:46 +0000

    reply most means regard measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
