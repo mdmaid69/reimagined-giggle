d06554091a51902cf177259c10fc14572e26067f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:59:16 +0000

    normal solve exploration smile appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
 def calculate_gamma_function(x):
         return math.gamma(x)
