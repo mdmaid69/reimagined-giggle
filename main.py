ef3e55866d2332a9f85d128e5f04f5e2a12faf5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:34:26 +0000

    figure woman swallow mind get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
