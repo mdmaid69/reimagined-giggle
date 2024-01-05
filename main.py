999e2b8f9091f9c3c8119a06bdfbce35293bc483
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:06:25 +0000

    store sorry degree happen balance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def count_words(sentence):
-        return len(sentence.split())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
