e335bf948508a69d1bb0ae757b51528e4d19169a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:18:22 +0000

    let flow fix thanks makeinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
