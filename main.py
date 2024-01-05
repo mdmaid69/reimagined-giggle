b16f110dc4ec66ff1b009660fe149af59a8f6148
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:29:14 +0000

    love spring determined weigh late

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_int(array):
+        return int(array[0])
