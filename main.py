5bb51405464f6f9e25bfed66be6850c9e7c2361b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:56:21 +0000

    definite common attractive broad long

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
