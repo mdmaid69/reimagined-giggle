8cd3adbb9e41da6b0644f0936d17ef6c176b56b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:02 +0000

    wind one stone sort boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
