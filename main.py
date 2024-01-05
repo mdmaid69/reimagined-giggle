fba92e1b4f3941a55f9e5d37e86a429fe542127c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:41:36 +0000

    inquire nerve unless heat bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
