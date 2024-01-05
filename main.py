c7619d2c7bd45720d3d8a70104cfebd293325dbb
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:33:53 +0000

    table bang tip yes negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
