9eb5ac43f0a8aea99371d771ed345b08e8c93b63
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:31:54 +0000

    love advise impression keepaway capital

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def convert_to_hex(n):
+        return hex(n)
+  def is_odd(n):
+        return n % 2 != 0
