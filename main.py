bec1bc4c6f0f1c53ddba0c069a3a9a8ef83e3db2
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:22:42 +0000

    average catch urgent authority suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
