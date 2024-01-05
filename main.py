7ceba3f8acd3877061c133f279fd158384f72d91
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:06:10 +0000

    Qq cover national expensive rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
