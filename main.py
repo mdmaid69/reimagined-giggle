6326f5db0518e3fb867eb54bf64ec26c2def3856
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:48:20 +0000

    wish address myself lower string

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
