0d4b110132ef98263199e1a17b11f1a27195cba1
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:17:25 +0000

    suck ability thick defend drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
