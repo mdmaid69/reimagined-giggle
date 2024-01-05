9467b76fa869c093f1f1cae4323e1c8d6fa06160
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:41:34 +0000

    short everywhere his Mm lift

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  def reverse_list(lst):
-        return lst[::-1]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
