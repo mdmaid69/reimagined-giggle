0ceab84ec08d7af0f858e2e21e25c5ee442a8630
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:30:58 +0000

    divide Zz rain phone skirt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
