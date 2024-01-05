315c5a29da6966bd305e92de1cada9b3fedc3a7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:28:38 +0000

    fold attractive black save luck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_area_rectangle(l, w):
+        return l * w
