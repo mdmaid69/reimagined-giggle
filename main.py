a177bb54895c8d45127f5258f00de7d3de002984
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:38:12 +0000

    fresh delicate check attempt definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
