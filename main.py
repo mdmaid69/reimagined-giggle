6252d21195c1f4c115648aa2e311932fc92ab235
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:58:19 +0000

    Dd disagree protest band same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
