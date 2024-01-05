3113944afab2ae90eac317d40b8550461fcc775e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:35:12 +0000

    otherwise rich lost raise quantity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_volume(length, width, height):
+        return length * width * height
