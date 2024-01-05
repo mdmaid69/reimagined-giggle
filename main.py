6b8f8474720ff580ce2db13d1d4a00f79cb9e399
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:12 +0000

    still wear honest single present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
