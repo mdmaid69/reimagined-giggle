ef2dcd19de61ec117b52e90c9895f8550d3548ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:00:44 +0000

    later please exercise club office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import collections
+def create_counter():
+        return collections.Counter()
