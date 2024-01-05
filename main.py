e704044a60e5c395cbe7ca5b037269830429a42b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:17:42 +0000

    proof shout consider rid recognize

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
