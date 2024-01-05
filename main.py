154459298363a27b0bd3740437c695540c868335
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:34:11 +0000

    roll callout feature table state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
