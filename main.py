3f3350b5d762590130fea322a49e177bfe061540
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:56:07 +0000

    wash rain steal any shelter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
