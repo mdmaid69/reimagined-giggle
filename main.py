482fcad4a5ff3f19b19f13eb367db5d8316442df
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:54:44 +0000

    lunch another beginning record sea

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def convert_to_octal(n):
-        return oct(n)
-def count_words(sentence):
-        return len(sentence.split())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
