4c77c9b4f845b77f7a1844b91c2c2daaea8693a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:48:29 +0000

    bringforward pretty loose consist responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def cube_number(x):
-        return x**3
+def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
