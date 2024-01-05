b3ba4dc4478202f34cecb353b8618cc669a14df9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:02:25 +0000

    wonder lean follow deal breakdown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
