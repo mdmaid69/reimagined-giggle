d17fe405425244b0ef9e2e11012fd9ad25239300
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:46:13 +0000

    coffee depend keepout but brain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
