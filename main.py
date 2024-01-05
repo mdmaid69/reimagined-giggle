0f95dcc87791cbdf755c28a8d3e3c37beca79e43
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:44:44 +0000

    commit sport character pipe violence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
+def calculate_average(lst):
+        return sum(lst) / len(lst)
