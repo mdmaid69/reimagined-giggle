3f62f4cfed494c75d5c24e0cde991fc24e043254
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:51:11 +0000

    unite uniform used strong breakout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def is_odd(n):
+        return n % 2 != 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
