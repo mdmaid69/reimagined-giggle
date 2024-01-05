9eb3d8968aba8c4afef4f18fa276559b9253810c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:49:37 +0000

    person tongue train light commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
