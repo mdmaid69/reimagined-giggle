1e6edb50363064a4cdbc7a13c7263833ff78bb02
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:35:39 +0000

    middle him in sound again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
