75f8b1dbd7906e1305b2acc644ebd7c13ef4f5b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:46:38 +0000

    quit concentration worth nervous introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import collections
+def create_user_string():
+        return collections.UserString()
