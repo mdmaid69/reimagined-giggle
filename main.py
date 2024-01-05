5d6939e5b779fda021055d8d9d1d0725ffc686ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:57:42 +0000

    instruction union justice everyone half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
