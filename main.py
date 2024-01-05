db922c3e573453759ec76d120b82b4a63fa57c4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:11:44 +0000

    mail then instead new expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
