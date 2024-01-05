341c320d42ae2471eec861219ac19a7277ff9b29
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:04:24 +0000

    future congratulate result back callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
