954dcd15a51710a505cc75d8ac801d54adad11e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:25:31 +0000

    basically married gentle small relief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
