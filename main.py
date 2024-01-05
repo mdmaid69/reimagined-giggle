c769cf3fd1de4269742c144d7be64803463c298b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:29:04 +0000

    switch bridge whether down platform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import sys
-def print_python_version():
-        return sys.version
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
