93e24f2e3d6498283ab39c2ab3c9a0457f56f14c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:53:07 +0000

    tooth scrap border ride clean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
