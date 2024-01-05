8fa4e2cf873733cd5e5377897bea648ae3e9e98c
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:44:31 +0000

    temper float dear many pretty

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def change_working_directory(path):
-        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def add_numbers(x, y):
+        return x + y
