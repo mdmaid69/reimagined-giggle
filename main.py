fd6be4b548e1048a3b6eab779665c2dbffd982ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:33:28 +0000

    children mind part limit wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import collections
+def create_counter():
+        return collections.Counter()
