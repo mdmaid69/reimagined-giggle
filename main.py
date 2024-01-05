9ec9c8ee9de1d12b97961f94212d727626c70506
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:37:53 +0000

    dry play fire break person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
