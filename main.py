ce57bb7b91299d7bd8d181e00ba32fdc1ae5de80
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:46:58 +0000

    blow translate shine could clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        return sys.version
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
