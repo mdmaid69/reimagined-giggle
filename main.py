aa5b4b8ccd57199cdafc80da1b854a68e32deb67
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:42:56 +0000

    murder good book giveup old

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
