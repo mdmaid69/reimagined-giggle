7fe3625fd03c240c16e7f5c074aaf5545bc6f052
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:36:37 +0000

    wind remember noise serious friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_typecode(array):
+        return array.typecode
