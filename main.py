1d74151ae7fcd727e439118a441120b25c291aa2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:24:32 +0000

    dance damage cause paper outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
