7231a4eccfa8cfd75bc6d3362cefb557692538c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:25:34 +0000

    direction indeed past ability warning

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
