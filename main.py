42164927dc097f7b26aab4de46cdfdab95b6e8c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:27:51 +0000

    operation mention later scheme influence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
