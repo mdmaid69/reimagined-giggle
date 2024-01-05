1d7300a4398b3228d23c4079f9ebed38a5417526
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:33:37 +0000

    goback(to) style often knock truth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
