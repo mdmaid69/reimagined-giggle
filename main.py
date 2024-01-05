fab1a5324a66d5c4e83dc9a1753d1696e19e421e
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:02:27 +0000

    teach nature mend apply grow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
