783f128e798ebd4bc933abd9120ff28511b6a681
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:17:29 +0000

    crowded raise thankyou trouble disappointed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
+def calculate_perpetuity(payment, rate):
+        return payment / rate
