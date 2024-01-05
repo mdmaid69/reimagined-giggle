462a01ce6134760c528316eae5cfca8a978bb823
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:53:05 +0000

    and accidental bye read like

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_max(lst):
+        return max(lst)
