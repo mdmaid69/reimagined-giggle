45c3d105042eeb420cf020dc94bd1eea4fe2d71b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:23:37 +0000

    interest arrive first underneath class

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
