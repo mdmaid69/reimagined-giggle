8dc907aff9199ec567e5d90ae7d994074856e2f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:03:57 +0000

    shout police favour field do

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def reverse_array(array):
+        array.reverse()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
