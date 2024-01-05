290d7e5ec84dcf5f2ccd50bc55697f4a86f9df81
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:47:34 +0000

    at lie love seriously stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
