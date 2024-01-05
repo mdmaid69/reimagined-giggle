8f34515b7d963e3ce391acdbc7400dcaf48840db
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:01:48 +0000

    scare brave yourself cry more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import collections
+def create_stack():
+        return collections.deque()
+def find_max(numbers):
+        return max(numbers)
