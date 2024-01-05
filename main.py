339a9db527199d824cd43822a79af04a1dd340c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:10:26 +0000

    glad warning hole journey shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_repr(array):
+        return repr(array)
