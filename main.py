d74070c1e69a1fa4887f6440e683c94e9dd11610
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:40:14 +0000

    behaviour production with behind pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
