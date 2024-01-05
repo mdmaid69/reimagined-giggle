c129cec77619ac8f8d43d913a1eeed8cabe5b4b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:17:23 +0000

    frighten hope steal high maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def reverse_list(lst):
+        return lst[::-1]
