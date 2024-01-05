5bc8b3d5af2ea0d327453d2a567ab397ed9078ff
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:42:06 +0000

    invitation care fear turnon perform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
