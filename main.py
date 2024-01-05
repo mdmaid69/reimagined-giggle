7a6fbdd12d2e3df4583d5caec83b6ffb966a5dce
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:56:50 +0000

    getback door because now loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
