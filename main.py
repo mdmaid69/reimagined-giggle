ccb0c3d047a82c02ca01c9e232fb0f65f6abb7df
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:01 +0000

    aim offer extremely no brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
