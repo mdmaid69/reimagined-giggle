90683d85ef26f37f7d40da4ffe8308635e215181
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:56:12 +0000

    software along begin as stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
