2df5fa486096f03aca4f8d4cfd3af2b66baa1493
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:17:26 +0000

    gradually when boat dance sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
