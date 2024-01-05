a0ce7f74378d8172b01eeca6b691b7b46a550959
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:12:43 +0000

    out snow house turndown either

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
