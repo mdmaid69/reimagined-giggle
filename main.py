54d087bd55b6b8e78cb05696c7fbcd8baaddfd69
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:37:42 +0000

    thinkthrough silent workupto getoff silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def cube_number(x):
+        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
