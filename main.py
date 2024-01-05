48d61ca94635196fa69c6a47b290fb9936901284
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:45:42 +0000

    means cheque round music not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
