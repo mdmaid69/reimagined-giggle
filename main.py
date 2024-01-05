dd031afd2728deca49f9c61b045e4472963c83b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:26:02 +0000

    back telephone once note drop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
