19b2ac9e53a1b8306ad2d8fac22d0deb4dec20c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:39:42 +0000

    same still Ss everyone later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
 import sys
-print(sys.version)
+def exit_program():
+        sys.exit()
