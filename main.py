d2195a7100dc4f81eb5095fdb179e9eefa358d87
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:00:07 +0000

    satisfied Rr why responsibility which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
