fab02b937fdbffb3ee227837e99b7a1fb1fcc0b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:16:47 +0000

    advise Ll excuse emphasize return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
+import math
+def calculate_exponential(x):
+        return math.exp(x)
