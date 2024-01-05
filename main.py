25f0adf05152617e7063f8fee3c6694af76d7136
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:39:46 +0000

    but inquire knowledge bunch hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
