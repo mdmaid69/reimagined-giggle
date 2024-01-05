9a5ea2772c6ee163950fb058670680b7e629d815
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:16:26 +0000

    poor train odd couple definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
