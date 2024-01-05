b4c7b187f3ac3d7ee587c8c817b7f984fc08df52
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:53:41 +0000

    stage follow frighten takeback damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
