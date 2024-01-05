755f02643599957b774bccbf98e9c92d2eac7b09
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:33:49 +0000

    name piece speech coffee head

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
