d0edf713dfe48b440d0b7fb135f68b7f904374c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:22:00 +0000

    me bring minimum lead regular

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
