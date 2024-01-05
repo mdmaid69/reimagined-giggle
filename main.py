65a92a3d01236939a032e4c7e19532d05d49765e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:20:26 +0000

    mess popular exam bringforward instrument

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
