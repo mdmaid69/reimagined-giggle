4d41abf6d341c9f101bc72e812f63adc4568d3ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:23:42 +0000

    polite uponon straight interest summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_length(array):
+        return len(array)
