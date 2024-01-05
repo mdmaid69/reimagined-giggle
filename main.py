80bf69db4e8081cdd1fc5c28a3865e7409b996ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:49:19 +0000

    tense cost somebody think though

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def subtract_numbers(x, y):
-        return x - y
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
