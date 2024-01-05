0318d581c773cb3bb4d95d70828f6ad8ea6c6c9b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:05:53 +0000

    still health extra set basic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
