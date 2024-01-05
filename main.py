2c50c1ccebea8660ebcc11405616c995a79d5e1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:42:08 +0000

    bored public great reach smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
