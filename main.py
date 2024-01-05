ca490b3bbaac66906a9c0033d60f3bef9f113e7b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:06:26 +0000

    near way underneath road estimate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
