333a995cc1391c533335baf55b207770cc2ffef5
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:49:04 +0000

    hard twist giveup connected suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def get_current_working_directory():
+        return os.getcwd()
