5b86dabd37fa7081f84590f3230421e1fbfe9c9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:25:15 +0000

    contact limit sign gentle programme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
