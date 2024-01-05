0895df4d79591cb11de565f567023a39310249f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:29:06 +0000

    Hh total idea after break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def greet(name):
+        print(f"Hello, {name}!")
