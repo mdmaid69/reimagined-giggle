13640da094cc36db02096735b9b81fd5b23db1c4
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:58:52 +0000

    ready good fear his clear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
