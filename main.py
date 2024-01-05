e17433d0aa207d22ae73c5d761f49e68dbd7ba25
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:18:06 +0000

    still acceptable what funny claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
