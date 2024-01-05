006a3c98d14edfb42cc6e947554e373d7073e7dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:21:32 +0000

    cheek present explanation begin exchange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
