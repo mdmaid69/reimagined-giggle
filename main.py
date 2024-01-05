78762ef8c028c98081ce0739448820af49246265
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:24:37 +0000

    concerned accident surprise herself popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
