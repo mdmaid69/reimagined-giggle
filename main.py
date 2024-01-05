78a9b83a05672c782478146c438da54387e0eb5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:20:49 +0000

    defend suggestion or language odd

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
