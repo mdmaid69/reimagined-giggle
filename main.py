e99452be626c01afdc29621d84ff1b25e242c92d
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:41:49 +0000

    disturb production exam sweep score

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
