f2d15117261869cf90da45277d7c69c9a2f1ff52
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:16:53 +0000

    range bet ready container phone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def get_current_working_directory():
+        return os.getcwd()
