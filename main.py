fcae01c3ad95f408912ed36cf0fef32006c00e3f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:12:34 +0000

    unfortunate check getout let waste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_work(force, distance):
+        return force * distance
