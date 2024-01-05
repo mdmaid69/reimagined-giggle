bbad91178b31da19ccc8deea5d47427b6912cab0
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:29:29 +0000

    hurt set cut allow thick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
