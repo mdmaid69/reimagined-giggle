cc3c81379bd9e2ff8a0863e04991b5d9469fc498
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:13 +0000

    contact free lookafter right large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
