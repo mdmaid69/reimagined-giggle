0cbb874e7fba842f689857f5833bd1b2342524bf
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:08:28 +0000

    wild back operation spite back

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_as_list(array):
+        return list(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
