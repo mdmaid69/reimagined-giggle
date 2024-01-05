9ed458e00607cad4ffeb90c40906c8e3af387d4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:16:28 +0000

    into instead listen view transparent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
