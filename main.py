e51309853d8af00ac92feb01ea3aae2875acccdc
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:23:49 +0000

    government computer stamp achievement slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
