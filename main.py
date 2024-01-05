be56ef985c57d60f2883165d44f314e2cc169959
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:40:50 +0000

    excuse obvious scream take worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
