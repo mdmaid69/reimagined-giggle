fc02603a02d3e56ca613d0b89674fe794008d8a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:46:15 +0000

    down nervous moral post week

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
