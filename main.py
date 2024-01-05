761ed6dda0ab6abf9d3c34d01b00e4eb4f783a47
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:28:21 +0000

    muscle term sting describe suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
