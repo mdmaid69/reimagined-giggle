42ae08970ddcfccd5cda7102a79793ca5451450a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:40:09 +0000

    name borrow godown visit dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
