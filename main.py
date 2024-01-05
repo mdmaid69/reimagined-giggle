c47a88f96a3c3cee1723ff06dacf28b4a26dac83
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:09:05 +0000

    priority consider appeal effect list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
