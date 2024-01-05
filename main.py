339f031f6c73f00390063cb2cc92c9929eafc242
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:30:51 +0000

    principle instead run move naturally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
