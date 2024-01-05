036474f2cb49ec54c2139fa23a5c89cfee3493eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:45:18 +0000

    familiar rescue circumstance remove mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
