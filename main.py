441ba1f0456e95088ef9e049b089769da0934bb9
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:21:36 +0000

    owe myself sorry confuse travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
