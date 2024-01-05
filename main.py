a32ee5db9fd0ba9ccc3decd0a066ab52053ca8aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:44:27 +0000

    study blame various buy intend

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-for i in range(10): print(i)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
