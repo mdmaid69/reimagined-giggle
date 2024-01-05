cae68cc3bc531419bffb78edd2027606f220ea11
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:08:20 +0000

    someone beyond profit frame tidy

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+for i in range(10): print(i)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
