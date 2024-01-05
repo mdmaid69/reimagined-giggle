2798f2faf40d6f94acf3204f4fc5e77efaf9b909
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:33:12 +0000

    after store content mind compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
