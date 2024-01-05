e351e2b03022f6522a7c00f9f1fe23b5a7c97d1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:32:35 +0000

    used receive sentence string again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
