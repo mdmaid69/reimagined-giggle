fe28cdc4bb75bf8d6e4adb8f98ec61cda772662e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:08:28 +0000

    split contact idea chin bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_typecode(array):
+        return array.typecode
