8d04d7cb3960344ae3eb45c30c0333f022352f94
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:19 +0000

    getback artificial adjective relation advise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
