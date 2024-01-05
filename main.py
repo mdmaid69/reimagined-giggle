3c2ad806a21371b3c704bd06a5e5cb6b60ee7fa7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:46:34 +0000

    workon trap standard past keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
