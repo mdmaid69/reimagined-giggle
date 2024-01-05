aebc8dd9b282fc338d37d1bd672413ab50e5b830
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:31:58 +0000

    goaway decision usual prepare Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
