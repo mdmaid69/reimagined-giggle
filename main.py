a685468b50c50628764e6be407d09e7b7ca235ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:01:56 +0000

    once back pretend hole description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import sys
+  def get_python_version():
+        return sys.version
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
