ce2710dd12327e2af44df5f4da2fd75297fbcacb
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:36:58 +0000

    listen move cook thinkthrough care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import numpy as np
   def create_numpy_array(lst):
         return np.array(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
