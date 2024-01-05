fdb07ff18708ec6cd8121d0e1e4519061e318719
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:11:03 +0000

    raise sound tall oil genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
