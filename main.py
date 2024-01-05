429ce9b0c06ac450a599df0a1c270e919b091560
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:44:50 +0000

    slowly design backwards sorry rely

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-print(math.pi)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
