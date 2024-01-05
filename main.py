a5765794685f79ed802484a20dff0c4b30b8ca52
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:29:08 +0000

    gift failure brush heart sentence

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
