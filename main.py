a857fbe67bcbee800f0d9d12ef147407073835e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:51:26 +0000

    safe inform behind fill ours

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
