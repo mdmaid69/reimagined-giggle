f220225ef2fa1da06c3ac1f8fb34498f8f224da2
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:15:29 +0000

    wander pick collect rain high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import time
+def get_current_time():
+        return time.ctime()
