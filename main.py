21f25dfd640e0d8ea1af53f723856e451de60709
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:22:43 +0000

    success separate smart value impressive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
