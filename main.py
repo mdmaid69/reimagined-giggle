6889ce3769be2ccf7d46ff274fb9a1e501a7ad79
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:19:22 +0000

    illness display several worth touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
