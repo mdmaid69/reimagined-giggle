5f2847e1ea96fca1e89beecf5cbc11d583852981
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:50:41 +0000

    blow bus imagination unlikely content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
