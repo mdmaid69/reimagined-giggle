73f816884a103640b41dc414152d28292a2bd604
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:50:00 +0000

    properly shake television respect praise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
