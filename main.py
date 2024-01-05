44235cdde7e6432f15ad06bd04b96b545a779f9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:29:48 +0000

    become breakout friend conduct describe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
