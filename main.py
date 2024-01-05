b61b677f7b76102491c0e99a24889585ed79e61e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:54:42 +0000

    memory rate cruel let better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
