e9d74f7d1b8b71122abc41e521b8766aa27b7c1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:25:58 +0000

    dry getaway sure drawing bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
