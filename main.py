1bef696a4e5554f656dd7e0242bb1091de419fe2
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:13:49 +0000

    well bottom woman sight run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import platform
+def get_os_info():
+        return platform.uname()
