f15e34c47fa1604ff460897fc65ea34dd7c3c488
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:53:23 +0000

    bag unlucky window slide achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
