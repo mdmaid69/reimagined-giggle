c31c0d1b76558c18347632b4749c4117da4b9e66
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:24:10 +0000

    style cause public share kick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
