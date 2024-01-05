77cbaaff780b6cef45cd299585edef76c66be9cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:44:49 +0000

    health new face flight trip

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
