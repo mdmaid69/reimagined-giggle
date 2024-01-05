f8f770cf61e476c3d4f50c0263cee9b755a16b65
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:54:20 +0000

    fine part keepaway wall design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
