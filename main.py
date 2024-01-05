788eb574b9c3754772a4c2d37c749b78a24ccb00
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:24:35 +0000

    gentle slightly order accommodation wander

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
