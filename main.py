04e51725595fc4ce175fbbf23719d03964d56073
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:48:07 +0000

    scrap contact exact typical father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
