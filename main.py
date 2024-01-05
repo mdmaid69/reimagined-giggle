2244be88a46fcf074fc6cea43789549e3c54a865
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:20:54 +0000

    goup terrible trouble sport thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_item(array, i):
-        return array[i]
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def get_array_as_tuple(array):
+        return tuple(array)
