48285cf1d0ed655f435480a6f2e94003392e5d7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:56:32 +0000

    turndown holdup disappear wake agree

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
