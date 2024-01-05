9c11b363b9bc973460c7814ca9a36db5efcc5b6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:42:17 +0000

    badly run drop boat cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
