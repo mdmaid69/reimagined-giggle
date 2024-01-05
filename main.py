9c24ece3123d487b0a5f0c833d27e109649c59a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:17:32 +0000

    measure demand consist care estimate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import time
+def get_current_time():
+        return time.time()
+import time
+def get_current_time():
+        return time.time()
