240acf34318ae9d1a68f84095559e5aefcab4b3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:39:00 +0000

    lookup begin fast spread closely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 def count_words(sentence):
         return len(sentence.split())
