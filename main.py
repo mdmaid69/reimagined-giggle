5d3183a7127f598ce812e55ca57a46e53aef376c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:27:48 +0000

    frequent introduce continue wipe crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import time
+print(time.time())
