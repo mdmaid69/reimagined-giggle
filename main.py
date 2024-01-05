79689488d66f0d916921c2c84228f3eca2a1530c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:16:27 +0000

    improve dress bringup step mother

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
