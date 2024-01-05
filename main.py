f320e771c07019cc8e341af1d24617b32568ecba
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:00:19 +0000

    shout cheek estimate row machine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
