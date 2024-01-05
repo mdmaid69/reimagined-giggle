8b0f9971f14b2753f8a4d31cb70613bbf1b69034
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:30:46 +0000

    nearly this no flat offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def subtract_numbers(x, y):
-        return x - y
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
