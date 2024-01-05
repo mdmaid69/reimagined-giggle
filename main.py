e1f2bdbf31c45953f49cfdad17fada61463fca28
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:59:37 +0000

    squash tense later substance chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_acceleration(speed, time):
-        return speed / time
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
