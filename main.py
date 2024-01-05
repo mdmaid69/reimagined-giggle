63bdd5275bf1874e970e52e34b07652e4e6f3d7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:08:07 +0000

    bus bridge cum story nowhere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
