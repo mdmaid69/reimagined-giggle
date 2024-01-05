8642bc08aa538d91c7c6dc5e45f7918b6041ff4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:35:31 +0000

    means wild compare length brave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
