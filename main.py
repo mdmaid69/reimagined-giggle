4f37366bf2289e7732b97a2bc7fafe66e1ee008a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:40:20 +0000

    season excuse principle true herself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
