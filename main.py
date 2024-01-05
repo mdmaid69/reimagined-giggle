513ecaee4ef4c953424af84fb6b099e43cdd9fb9
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:26 +0000

    about country add valuable Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
