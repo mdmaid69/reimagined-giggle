9a7408f42e625f1fc067daa283f0f50b43e5177a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:09:33 +0000

    load catch border tune mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
