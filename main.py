aef1b91550fe564143fa9772a02d76ddecfcaf38
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:57:46 +0000

    wake break age fit lot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
