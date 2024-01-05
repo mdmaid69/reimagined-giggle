80db2a07a3c72cd048269a4b046cb8d71e359804
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:44:07 +0000

    separate sort goon firm first

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import time
+def get_current_time():
+        return time.time()
