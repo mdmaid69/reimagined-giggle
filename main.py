03a89cbea7356b3dcf1c5d4bf52bf04ef6566bb0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:30:03 +0000

    notice otherwise confused choose disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
