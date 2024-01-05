f10f0b2ac472bcf7ef79c8da63aef5309028586b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:08:53 +0000

    tear deserve recognize accommodation busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
