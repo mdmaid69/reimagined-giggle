9bed31ac3efc797f0597656ea588c7d126212e85
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:09:08 +0000

    mixture grand satisfied raw giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
