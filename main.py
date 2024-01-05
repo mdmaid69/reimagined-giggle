9e17d0e0fd028915d0893914c2858303a70b9212
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:50:16 +0000

    shut wide responsible rest tone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def cube_number(x):
-        return x**3
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
