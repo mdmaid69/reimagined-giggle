78d2827e9bfdd865d66b2346d17cba28c759346b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:49 +0000

    his price shall behave journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
