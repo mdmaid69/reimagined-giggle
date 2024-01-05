7e2a852e4cfaa6ea1b0720df5b8cd20eecd32bec
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:25 +0000

    frighten everyone rest set charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
