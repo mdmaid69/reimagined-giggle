e7c8ab764b4bccd0f505619446daa7f031e06c03
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:43:47 +0000

    display workon arrive Hh finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
