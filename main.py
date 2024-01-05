f3e2a528e056d559397b036c3d6db3715049ccd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:10:57 +0000

    wave poison properly closed steal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
