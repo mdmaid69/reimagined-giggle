490a007048dc5b1be7cf9ca1358b74667fb88276
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:57:45 +0000

    persuade chance power now remark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
