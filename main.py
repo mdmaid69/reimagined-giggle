2db7b05ca044980519fb9acaf48f0828dbc64923
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:23:29 +0000

    capital of software minority pick

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
