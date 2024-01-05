6320f732d5701c1acd0a399ca4e377b8f6da107a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:05:23 +0000

    wander continue street bang action

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-from collections import Counter
-print(Counter("hello world"))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
