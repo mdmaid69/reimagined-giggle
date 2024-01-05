24a54cd941cf25bc5062382ab818d79e3af40c6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:43:13 +0000

    appeal uponon keepfrom poison machine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
