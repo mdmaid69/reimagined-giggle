9e0a35b96798b6e061338c260d9db57be56b1bff
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:06:07 +0000

    moment criticism hit whenever bush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_as_set(array):
+        return set(array)
