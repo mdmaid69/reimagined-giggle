f18a819340ad020207288ebde94b948b9ae05efe
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:16:00 +0000

    kind strict hit OKorokay proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
