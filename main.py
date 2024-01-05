edaf9fd9160786fa44e8dc8b692e7fd209be6b1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:47:54 +0000

    think near bringup grow badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_palindrome(s):
-        return s == s[::-1]
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import random
+print(random.randint(0, 100))
