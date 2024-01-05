ccad1b99560434fbe5ec87ea18a8c681e35dec24
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:38:18 +0000

    nose need let after woman

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
