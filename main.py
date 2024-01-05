78bdee048e4ac05d83d4bf5c68aaf430d7ea9795
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:21:18 +0000

    loan wealth behave fine entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
