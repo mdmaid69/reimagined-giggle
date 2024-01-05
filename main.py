ee4ec23d7ef38ee2454a599db005106677d8a411
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:37:00 +0000

    cross only safe mark remark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
