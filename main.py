49bc701baf0d850682d138200c2cba843036d790
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:12:46 +0000

    custom complain neither child show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
