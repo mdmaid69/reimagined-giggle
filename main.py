6cb542f1779ead7924d80c6ea1e579367b024ec9
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:51:22 +0000

    trip Hh next new begin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
