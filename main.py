0305275ce83347d8269246a9b5c8e00318058d2f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:14:20 +0000

    strong make lock obtain protection

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
