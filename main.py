a13d9c1ce64366cc3a29dcae70d2d34a103f7bc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:09:33 +0000

    on disturbing dig table demand

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
