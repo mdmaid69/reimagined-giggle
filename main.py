60ec430e843098f81e0b6cd660fd79df890efb2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:22:26 +0000

    genuine interfere female cross warn

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
