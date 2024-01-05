5ff9a02fc8c2565ac31f805942dea34950e803a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:45:06 +0000

    shame bored not thirsty die

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
