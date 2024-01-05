494c67e83125816085c6799b384e075b6d38ba43
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:19:39 +0000

    thanks main evidence mess especially

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
