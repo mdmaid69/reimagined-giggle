633d4620f9d8d6a7b432a8b8ae018fe3270ab936
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:52:39 +0000

    deliver somehow confusing statement exit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
