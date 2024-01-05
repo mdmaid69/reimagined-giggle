723b72a7382667a3ab5cd1a55ba9923845e1f92f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:14:09 +0000

    exciting smooth but bringup late

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
