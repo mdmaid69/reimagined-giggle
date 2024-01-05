3c73e00cd91d353a8df1223a275ec876da664555
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:15:28 +0000

    embarrassing face must because either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
