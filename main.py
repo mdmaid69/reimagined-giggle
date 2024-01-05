e146281b4f7604c32b212bb6c2971e6741098f4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:21:57 +0000

    doctor opinion deaf tax everyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
