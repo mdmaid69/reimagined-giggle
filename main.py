1fe47633d34b08c45e34194fc2571b860a69cb55
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:03:02 +0000

    bath immediately waste disappear mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+print(datetime.datetime.now())
