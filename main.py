efb31205645945ce0cb48134de3b557ddab350be
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:43:44 +0000

    score below accordingto the content

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def is_odd(n):
+        return n % 2 != 0
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
