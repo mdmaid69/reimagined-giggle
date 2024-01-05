c7ad33f86d5e77dfcbfa8d96941fa7c7ca9fee52
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:55 +0000

    hope laugh forgive promise change

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
