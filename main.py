9ab85e9adec2ab7eafe80a2c44d6900d0a12921a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:41:03 +0000

    special accidentally depend ours game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
 import array
 def get_string_from_array(array):
         return array.tobytes()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
