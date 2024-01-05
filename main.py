be5d32b0c8f7295f2d3aec89e6c8a0ab71dc2896
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:16:22 +0000

    slightly fond bound brother impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
