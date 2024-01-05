95f84aa03a456a57006e5eba4a238326bd46532d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:43:32 +0000

    fear system weather yesterday separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
