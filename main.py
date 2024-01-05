a05466ad431d54ba61e478cb3f9cc1c096d006bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:47:07 +0000

    mystery minimum safety truth round

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+from collections import Counter
+print(Counter("hello world"))
