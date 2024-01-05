f8bf59996744dee7530739c2feea009fa753b447
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:40:01 +0000

    never basically wake angry cough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
