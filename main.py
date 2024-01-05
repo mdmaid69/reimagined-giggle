5e89e783ecae378f4d0c8c02598e0a5d078559a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:30:08 +0000

    rent dress content prefer first

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
