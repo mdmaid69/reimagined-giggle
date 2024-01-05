a6ef3c0cdd786fc31890c3d8d64a05a4ddec9396
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:33:24 +0000

    objection soft form round fix

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
