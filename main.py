3940c880434ea310fd9178dbd96bddc28e13c376
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:06:25 +0000

    add far holdon entry reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
