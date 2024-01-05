622cfb01487ee094cf459808c88898a7edae0c36
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:15:16 +0000

    morning introduction patience disapprove bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
