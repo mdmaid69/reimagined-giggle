7deed4d6e4c6f2d0b69b640cd8b5d688f7c3891f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:09:41 +0000

    actual child pencil lost direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
