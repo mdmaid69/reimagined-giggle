88809f1f2e45f585560015dc1f2b1cbc9a408306
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:37:41 +0000

    light number save complicated characteristic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
