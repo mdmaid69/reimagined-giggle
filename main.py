3db5cdb5b175fde9e46da704d4619dbda8a37d11
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:34:05 +0000

    Zz brown send ignore awake

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
