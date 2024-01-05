cbb93ee339c763471d2a244c88e9eb3f1b626866
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:56:40 +0000

    entry swim mine complain sing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_power(work, time):
+        return work / time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
