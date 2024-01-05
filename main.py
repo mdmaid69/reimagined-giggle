544598b29b28968cf07aeb29425076ee059a928a
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:27:50 +0000

    seriously swallow clothes soon face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
