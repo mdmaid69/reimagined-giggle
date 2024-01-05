c1800a05d7e1bd737541e9204380027ef68c369e
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:17:06 +0000

    greeting wake breathe bored damage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
