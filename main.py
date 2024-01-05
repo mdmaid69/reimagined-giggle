64373a37372834b2aafa0f82a723881947c502c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:39:43 +0000

    objection worse sound together almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
