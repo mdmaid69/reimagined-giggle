983f609a650a6d40c1d40d2225c3ea028945fe23
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:58:48 +0000

    sister centre replace strong injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
