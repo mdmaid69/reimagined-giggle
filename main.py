2a1fd6ca8bcc31ce266aebd4b37395719de61c93
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:01:10 +0000

    murder lift reach sure charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
