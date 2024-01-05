9d06299ae5037aa834ad078d65e0599f33387ec8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:40:13 +0000

    firm Yy shopping ring print

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
