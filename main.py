4919ef4d490b71cdaf5c05ebd9c79c6a340cdfba
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:03 +0000

    forward none mass listenfor thumb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def divide_numbers(x, y):
+        return x / y
