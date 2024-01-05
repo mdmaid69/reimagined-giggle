3673266dddb06fed9dbf8eb69a095f27a0c9a071
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:49:36 +0000

    poor surface middle progress print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import os
+def get_current_working_directory():
+        return os.getcwd()
