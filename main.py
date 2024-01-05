5a6949ae69bf72c09ab9281effaae284bd9bf122
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:17:22 +0000

    warn public course treat programme

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import collections
-def create_user_string():
-        return collections.UserString()
+def count_elements(lst):
+        return len(lst)
+def is_palindrome(s):
+        return s == s[::-1]
