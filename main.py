d96c7877fa57032cc419eb08a0a74fc893590c56
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:26:53 +0000

    note interfere confident mention responsibility

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def change_working_directory(path):
+        os.chdir(path)
