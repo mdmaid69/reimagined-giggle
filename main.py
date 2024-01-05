92a7510e9750bb3c6f85c0df26a5a5e249da45f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:39:31 +0000

    information Qq report clever funny

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
