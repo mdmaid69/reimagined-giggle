05c9a73d5036f479159a4fdd60467d437ec18b18
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:03:59 +0000

    permission arrive unpleasant vision illness

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_set(array):
+        return set(array)
