d697b1d43428fb591778fdbd40a4ef5e759579f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:30:31 +0000

    convinced rest wipe protection over

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
