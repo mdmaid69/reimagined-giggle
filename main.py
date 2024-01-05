c1d73364e77675638215c4723eb4d99d560a8872
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:17:04 +0000

    load bath reasonable attractive trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
