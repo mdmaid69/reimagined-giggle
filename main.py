164eb0c4e65827bc1819a9345e04c23e21f2b89f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:51:25 +0000

    visit unit close prize lead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+print(sys.version)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
