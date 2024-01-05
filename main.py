b183aa52728941de42432addf2baa53073f8dcf8
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:07:58 +0000

    profit compare Ee period while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
