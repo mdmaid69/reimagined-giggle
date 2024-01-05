2dcff5422a314cbfd2e2fe7603e6a2f7761b3f9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:52:53 +0000

    sick earn turn comment fail

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
