c663213149904d1cd2240cfa5e208a249ef200a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:13:37 +0000

    partner challenge clever female simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_logarithm(base, x):
+        return math.log(x, base)
