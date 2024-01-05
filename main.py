735673778d746403f84bcffc84b1aa83bc3efb80
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:04:06 +0000

    curious slightly worst command container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_string(array):
+        return array.tostring()
