c382ef3754a14f63594369e071ee17cc131e2c08
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:15:06 +0000

    run issue rush trial vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
