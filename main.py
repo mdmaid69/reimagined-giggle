fbc062181c9e174559fe55917a6adaf76ce94ae7
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:38:33 +0000

    tooth stop rest noisy evening

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
