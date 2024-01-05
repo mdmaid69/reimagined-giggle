8a7706c98dc4c4bb1a896c0b48cd2bf6faea461e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:06:58 +0000

    little business mess pump nose

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def square_number(x):
+        return x**2
