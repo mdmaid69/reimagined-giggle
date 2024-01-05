fa0afea2baeb80319ca024a2fdbe4b26bc9fe861
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:24:46 +0000

    Ww damp lookup unfortunate there

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
