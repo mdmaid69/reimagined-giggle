ab70721959f421feffb31bd07bae6dcd2949868b
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:13:30 +0000

    exchange afford can extra across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
