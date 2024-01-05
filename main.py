ae214f71d2faa29efc4c026ba3dbd1b4209d3294
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:27:20 +0000

    collect how discussion intend substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
