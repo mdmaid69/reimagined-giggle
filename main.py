4dc5fd0f24639576815015d332a28b1d5caf0a1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:28 +0000

    duty wide pot love both

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
