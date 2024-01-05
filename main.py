b37f5a371bd9600827143bf39a79b08118530f46
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:51:04 +0000

    one here shrink bit until

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
