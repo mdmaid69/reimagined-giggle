72c2f4311be6ea290706facf6d5e34481e986048
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:46:57 +0000

    match vanish sign most maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
