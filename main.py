e1c0f84a1f3b1fe30e55702118f1ade63e316ac5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:37:25 +0000

    win kick branch properly left

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def reverse_string(s):
-        return s[::-1]
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
