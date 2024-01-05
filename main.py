2e9e1e7ede84473beb9b6291d5129de8e18b9a42
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:01:32 +0000

    foreign flat brave breakin naturally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
