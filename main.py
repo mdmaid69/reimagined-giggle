102c6fd257ac176a1976442d8370e59164253ede
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:39:20 +0000

    substance side splash against secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
