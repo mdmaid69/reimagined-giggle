40008080d0958956b9002c3c9b1280e598a842a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:43:22 +0000

    shop away giveup limit squeeze

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
