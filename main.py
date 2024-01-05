e909db48485853ad4979487af8f8e149eba5d449
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:01:52 +0000

    bringdown lower win necessary meeting

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
