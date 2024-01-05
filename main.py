c7bbf8c753cd9c68d91340e49d7fe5ca13ec326d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:39:11 +0000

    slice advice extraordinary swallow area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
