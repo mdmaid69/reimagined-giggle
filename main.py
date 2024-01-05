ef0003a0ed393dcc89b8d407b26722d6785d631b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:57:29 +0000

    minority argument to beyond getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
