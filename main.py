5097efbd0f650598887411a00782fdfe2c9609b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:35:39 +0000

    line ring introduce deep factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
