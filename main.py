84d7f51601d802b2a2b904f221917b24122dc539
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:20:05 +0000

    naturally means ought godown square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
