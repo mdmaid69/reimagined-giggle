7754e2ef86b7ee854bf592bc11ef70cddca1b68c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:22:52 +0000

    rescue evening protect serious mixture

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
