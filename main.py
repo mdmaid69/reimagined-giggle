ecc291387c6011ba0dcbb5c987812cfb88f7e460
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:44:10 +0000

    field takeback considering except bite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import time
-print(time.time())
+  def cube_number(x):
+        return x**3
+def find_max(lst):
+        return max(lst)
