41d3ff7d69ba88117b15455c782bb8c010ca554f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:43:02 +0000

    criticize myself night field can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
