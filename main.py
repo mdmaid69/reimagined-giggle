1a5a992ed522d9d3d0ed8eee02622916ed4f373f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:22:05 +0000

    note debt either that along

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
