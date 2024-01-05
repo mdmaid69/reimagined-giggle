2afe915d663ef7f72ea16bf68c2a62fb86afba76
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:16:42 +0000

    surprised rush argue impossible exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def convert_to_binary(n):
+        return bin(n)
