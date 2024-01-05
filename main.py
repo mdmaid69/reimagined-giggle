1e5387ccd4c331260a77e72aa6d80a4cc6cf215d
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:15:39 +0000

    criticize whistle nonsense 573entertainment support

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def clear_array(array):
+        array *= 0
+import time
+print(time.time())
