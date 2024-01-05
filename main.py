639acefd667583ad47e20cfb7ad7bcf8283d8495
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:51:28 +0000

    young window bread country 566end

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def is_odd(n):
+        return n % 2 != 0
