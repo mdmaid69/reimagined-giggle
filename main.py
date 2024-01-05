244ed5e361e298443b66f0a01a0957c425082715
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:19:37 +0000

    pinch factory choice shout rely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
