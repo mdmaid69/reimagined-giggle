b2bfd7d9223ecc4b97ff1db7d6b23ae70bce54a6
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:43:36 +0000

    education bang all often estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def is_odd(n):
+        return n % 2 != 0
