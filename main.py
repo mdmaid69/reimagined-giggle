9ffc7c9cdeb0c1c54ef7ff48e899116c83496205
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:06:38 +0000

    pull communicate black tape there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+print(os.getcwd())
