da0c845cc8c2638826bbc36c6f69b1ff2bcd36ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:49:58 +0000

    tax distant sentence remember across

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
