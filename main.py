5145adb0324dc5c0cfa067f0af37141ce0377d0a
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:59:34 +0000

    public than majority large blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
