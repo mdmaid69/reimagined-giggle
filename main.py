27e08f1f5ed69e96a4a3a12f2c337883b69006ad
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:01 +0000

    suggestion insist worst float measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
