a85a11eef9cf6070e0a3aa909f67db4ffa29eedc
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:22:23 +0000

    poor free print apparently taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
