3ca8bd7ee4a0e8595f5e1df7599780d6124c3d01
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:47:21 +0000

    loss Vv already except rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_octal(n):
-        return oct(n)
+import collections
+def create_queue():
+        return collections.deque()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
