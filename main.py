47d9c25634723500eac7792189493410ca24f9eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:12:54 +0000

    realize onto several method very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
