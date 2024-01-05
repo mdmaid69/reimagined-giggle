f26d1a30fd009dd221009f2c76cbb3fd85df87d9
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:19:35 +0000

    back cry stir scale invite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
+  def add_numbers(x, y):
+        return x + y
