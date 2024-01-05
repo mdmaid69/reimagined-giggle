dff0e0bb41f02690e892a4c126930f2366de6667
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:35:27 +0000

    glad laugh imagine typical tour

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
