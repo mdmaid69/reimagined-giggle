58522370619d78c5ef5b09cc1e29fe098bcdac87
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:56:06 +0000

    let share activity famous mother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
