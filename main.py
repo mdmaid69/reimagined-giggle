cdd65b070d5531824d118fc96a3eef496b71bc95
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:30:33 +0000

    commit collect calmly window turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
