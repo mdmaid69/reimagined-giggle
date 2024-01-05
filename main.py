0c1d6751913883a27f76c5ed4e64f03b5f1bf218
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:06:14 +0000

    pronunciation convinced mouth fit step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
