c700d9c96da45e8669e7ba1024c79a12a24422f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:58:48 +0000

    spring measure there turndown home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
