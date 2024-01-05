6f0c436150bb99e3c2ea7ad921b8d96992f8b2a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:22:17 +0000

    either represent than offend call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
