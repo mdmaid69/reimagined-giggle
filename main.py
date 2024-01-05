bff788281dd4c3e0806f843979d4ccfe1aaa74d3
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:00 +0000

    generally low correct care imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
