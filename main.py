5a7a2ba9bcd87a5a33886b5945c93e697d7a1d59
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:42:41 +0000

    recent blind explain nowhere turnout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
