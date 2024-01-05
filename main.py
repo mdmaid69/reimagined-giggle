b26bf872389a31d057cccb66b02528c12cba2ebe
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:20:44 +0000

    proud uncertain through likely steady

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
