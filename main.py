7cad00c8c4504e544d8541c8a958bfeefc1ab864
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:40:21 +0000

    size guide person original but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
