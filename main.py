8d86ae05083e8917399f4de8c1af1a7cabf4b258
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:28:43 +0000

    powerful discovery impossible rely pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
