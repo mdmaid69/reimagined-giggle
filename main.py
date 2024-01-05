d6c207b34ee7175c668d38915604ee783b009343
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:38:28 +0000

    lonely view brother lean father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
