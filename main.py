8a5bff27e1f6708e5817106eec18e057534b4abe
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:15 +0000

    tight pull interfere lookaround whisper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
