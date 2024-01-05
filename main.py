48f74070cd6702b0f841aad7d003285465265dc7
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:39:33 +0000

    call he I concerning prove

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
