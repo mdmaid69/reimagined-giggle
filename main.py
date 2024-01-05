3c827012c79cf6be016f43a2e1019dfdaed769f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:59 +0000

    slightly motion funny sale instruction

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def find_max(numbers):
-        return max(numbers)
+  def count_elements(lst):
+        return len(lst)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
