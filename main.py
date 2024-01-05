5c5e46c97df580db7c68beb2c54b19c546672270
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:54:07 +0000

    mess black image whatever me

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
