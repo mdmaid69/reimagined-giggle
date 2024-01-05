00bc624788a9462c3e4cc5c4d9bf17c352c850de
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:06:20 +0000

    off close evidence extra rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
