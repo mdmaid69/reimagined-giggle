f5a63ff570de20e5f3d495cf3bc6a9a7c6d9bd9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:50:31 +0000

    frighten alive contents pocket polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
