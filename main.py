8c2425e0904ee18d7afe133dfb1b48767a160735
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:52:16 +0000

    suddenly comeover every high tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_octal(n):
-        return oct(n)
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
