97d5470d9456033f8ece4bb749ec52e2542f1216
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:08:20 +0000

    cum confidence final recover beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
