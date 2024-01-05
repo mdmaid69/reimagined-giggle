fd06756277880aaeebebd5182e20ff1f11509661
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:11:24 +0000

    neither school software artificial shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
