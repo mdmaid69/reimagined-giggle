50dca9d7d1dbea85eb45f9940527d6e5a470ad9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:33:03 +0000

    dry either earn ours this

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def clear_array(array):
-        array *= 0
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def is_even(n):
+        return n % 2 == 0
