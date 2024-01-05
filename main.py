44592133d9dffcac3c2eeafbeb82fc147311c2ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:56:27 +0000

    still advantage judge switch around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
