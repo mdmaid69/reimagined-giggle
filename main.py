483a711cd0faab8538b2b1d1265b58b6dac9b34d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:26:57 +0000

    start luck convince wind bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
