981c17119328b92ed59c3511771b22a02e650247
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:37:42 +0000

    when sense food actual programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_complex(array):
+        return complex(array[0])
