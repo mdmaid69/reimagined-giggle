d55066932a3381adda1945aa66f3e0c9e8253698
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:21:22 +0000

    sensitive such gap visit regret

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def is_odd(n):
+        return n % 2 != 0
