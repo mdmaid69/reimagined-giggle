6d304d2af872fe9b79adeb7a712f915f3aaa19cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:37:43 +0000

    bury turninto arm contact scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def square_number(x):
+        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
