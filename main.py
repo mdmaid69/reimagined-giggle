9d0f78cd9642472789fd26fa26a66a4684170a9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:10:24 +0000

    silent interview deep suit opportunity

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
