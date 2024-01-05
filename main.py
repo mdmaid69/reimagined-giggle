d4f59e79789197aa510b240076a6d238dea3b547
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:40:05 +0000

    poor kind information business guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
