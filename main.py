b881f0308d73ed331d9667ee46a4a1dc5dcf2c72
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:21:12 +0000

    wander breakout keepout joke release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
