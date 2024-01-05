43ff75a0f2d7664265ce308b3f1d9d6750cb0274
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:17:06 +0000

    awake until like ground education

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
