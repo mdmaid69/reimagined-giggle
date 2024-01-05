78faed86f8635a3cf7b8321eba132f35c5390495
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:25:02 +0000

    difference job cheek wash car

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
