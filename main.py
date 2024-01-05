20aff8b7e212924652a10de915f716a0344f106f
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:06:50 +0000

    regularly finish still bath transport

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
