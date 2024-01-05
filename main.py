fe8f8548bac4165b4459fdd1ef52a7e82e4ad440
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:10:40 +0000

    ought pencil wear task importance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_length(array):
-        return len(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
