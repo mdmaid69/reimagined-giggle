708402df702a55958e470030abeba465b16adcee
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:30:57 +0000

    save basically classroom goon line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(lst):
+        return min(lst)
