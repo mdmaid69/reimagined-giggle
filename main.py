f0783bc2c2174f75699ced06ada80af250aa0937
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:33:14 +0000

    walk original listenfor fond breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_item(array, i):
+        return array[i]
