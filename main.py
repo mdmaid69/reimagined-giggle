13e1e2e5ac02f95cdfb3ca2660dd2eb3717bd09b
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:22:48 +0000

    ground keep understand race achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
