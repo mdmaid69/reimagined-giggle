dca41552fd0ed9a9b371570a2bff46ba55845b9f
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:21:10 +0000

    round concerned company which control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
