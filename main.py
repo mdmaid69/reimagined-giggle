ca114e2fba0bc9d335429069baede85a842e348f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:16:10 +0000

    snow temper first tone be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
