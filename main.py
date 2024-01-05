ed8df7da4b2640fbe1aefcfa3ba39ccd2fe89f93
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:48:00 +0000

    stop build mess left flavour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-print("Hello, world!")
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
