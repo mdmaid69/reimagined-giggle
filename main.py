6c7fe3f164eb8ec998fcbb82dce6dc503726de3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:23:29 +0000

    straight pride direct keepaway most

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
