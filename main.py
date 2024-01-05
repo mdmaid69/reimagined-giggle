ae2149e3a6737c95d21ba3e61c34e22b97640dbf
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:08:36 +0000

    wipe neither turn stupid rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
