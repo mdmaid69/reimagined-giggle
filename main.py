1d07729a5d64634fa2fce1fd935981ce6be0ec7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:40:53 +0000

    getoff swimming step company season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-text = "Hello, world!"
-print("Characters:", len(text))
+def iterate_over_array(array):
+        for item in array:
+        print(item)
