bb071d5c970b61e68e611ca05bf676d557e933d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:50:18 +0000

    catch period comment comefrom on

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
