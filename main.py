bb01f83341a571b228a60f10ee2401e752193223
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:15:20 +0000

    conclusion match service true season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
