0480ce0db08bc9b58d59c3475736593734d704a7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:16:58 +0000

    calloff drink sky goaway advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
