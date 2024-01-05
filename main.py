c4900e0aff7a2cc209b67fa0b523f50f2e26c6a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:25:35 +0000

    top experience comein snow ready

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
+def remove_duplicates(lst):
+        return list(set(lst))
