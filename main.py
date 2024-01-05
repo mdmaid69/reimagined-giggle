235ccd55781b8bea4ecddc04e0cdc351114b737d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:41:59 +0000

    closed skirt nor disappointed window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
