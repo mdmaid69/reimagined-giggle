77f709782a23e439ead513ac05c02a4d1839a424
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:56:36 +0000

    thick gather family reason form

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-print(sys.version)
+import platform
+def get_os_info():
+        return platform.uname()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
