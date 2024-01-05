a4d1275c7fe09310817a813938b38c1c9a3f2b0a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:31:16 +0000

    combine godown tape fortunately business

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
