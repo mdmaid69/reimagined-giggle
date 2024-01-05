097ba9ae8b1fca6c50c88ad8cde98b910a71d3a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:54:29 +0000

    finish offer right usually plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
