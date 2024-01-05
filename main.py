61d5e8014c3024a0477beae84793ffac984fbd0c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:29:44 +0000

    possession nervous winter actual clothes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_max(lst):
+        return max(lst)
