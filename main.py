661ec244b301c57a0aff685975b0d0cae37af264
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:27:48 +0000

    interruption willing wrong art film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
