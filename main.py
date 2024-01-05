a36cc70bf9baa7fd459bafb371c9f7687a8d58e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:40:40 +0000

    basis trade amount surface runout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
