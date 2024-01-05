277f7d0c09ceeadde426884cec9f968782942dc1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:38:15 +0000

    careless responsible much rescue together

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        print(sys.version)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
