bfc04c8f70268e765e8b19adc087b40eec9df717
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:47:55 +0000

    alive charge maximum by tend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import sys
-print(sys.version)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
