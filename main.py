ec603fd6d11080ddf0d1fcee79ead98ccfd3ad64
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:10:44 +0000

    attractive march instrument achievement we

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
