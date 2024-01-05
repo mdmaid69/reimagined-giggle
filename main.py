324d3139f974bec0022999050db1c9d31158d9e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:39:50 +0000

    keepoff breath exciting bottom ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
