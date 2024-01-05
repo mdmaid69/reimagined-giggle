baf14897b60dec884f9c2728522b47bcaeabd016
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:29:59 +0000

    experiment rub field onto argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
