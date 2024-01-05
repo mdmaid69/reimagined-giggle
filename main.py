a8fcf1b3b8564cc68ca402ccdaced722d75d95a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:19:57 +0000

    whisper fall last fresh sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
