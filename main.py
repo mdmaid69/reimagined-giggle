5ec57583c5025f644a5dad4210d96a12d350c6cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:20:43 +0000

    morning warn unpleasant quality than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
