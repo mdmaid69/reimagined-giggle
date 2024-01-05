686e7a7de2e30d7feb55d9997d2491ce50701d5b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:14:32 +0000

    service tall right usually whichever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
