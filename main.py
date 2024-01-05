02cb8188ffce7830085486aac225e1d5031df22d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:57:49 +0000

    Ii needle finger awake realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
