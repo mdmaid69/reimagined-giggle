15a890bf304e317b063daed4e3a39ec83f03b84e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:34:30 +0000

    priority ago exchange considering afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
