f9c1e692c2e3a83b4e3ea17ba752ade3c12365af
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:01:54 +0000

    spray learn vision custom temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
