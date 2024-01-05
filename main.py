88ca9c5c545ce36e45a5d5c3a6c6e29f27352f2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:02:40 +0000

    move flat smoke close man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_length(array):
+        return len(array)
