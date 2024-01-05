f2e7b2792af85a395faec51e56ceda6693ed8118
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:36:58 +0000

    post climb runover by lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
