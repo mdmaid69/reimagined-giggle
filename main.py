9cd736f765f6df09ce1b3a002e24603063783674
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:39:33 +0000

    news unlucky different Nn journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import logging
+def log_message(message):
+        logging.info(message)
