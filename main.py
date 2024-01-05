7c6e1d89502d8370ca32e40826fbd53f6cfee402
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:36:39 +0000

    wide partner lower squeeze far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
