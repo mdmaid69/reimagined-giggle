8929d01fc10473f4b1eb95f3962e544f024ae6b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:25:49 +0000

    upset regular front brain scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
