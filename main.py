229b0d35f76073c162f6f71bcb1dbfc74d4a2ae7
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:50:58 +0000

    discuss alone aim burn their

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
