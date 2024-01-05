3cffa9a3fd8191d9574edf3e7968033f310b1067
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:39:46 +0000

    design suckme square deal actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_float(array):
+        return float(array[0])
+def is_odd(n):
+        return n % 2 != 0
