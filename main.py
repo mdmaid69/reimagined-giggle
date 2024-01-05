4dd447515d6ccdf23a600bf12886edfeeb3cde75
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:17:18 +0000

    pay wave single bus club

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print(sum(range(10)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
