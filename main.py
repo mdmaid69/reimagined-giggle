c9a464db994c5dbeff1ba5bbe607920f24dbbf24
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:15:05 +0000

    sound smell again piece smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
