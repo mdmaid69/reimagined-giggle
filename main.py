2c1b61679db49bc23ac08f17cd8b1893b507cf47
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:45:02 +0000

    include object neither rubbish receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
