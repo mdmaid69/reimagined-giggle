ef496c48a406617741548ce5952c0ff1d2609fa6
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:39:47 +0000

    confusing next sound travel plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
