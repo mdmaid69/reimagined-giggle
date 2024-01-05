2bd6643b44355fd06b50476a64caa63c68dfc3b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:23 +0000

    long runinto together cut release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
