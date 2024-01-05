8637430d07f6591cca5672715378586ba3bd8d00
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:50:31 +0000

    thinkthrough music spoil round plastic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
