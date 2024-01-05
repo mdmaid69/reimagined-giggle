88edf2ba0540a7936edacf915fca2facd61e429d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:15 +0000

    justice television everything might exchange

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
