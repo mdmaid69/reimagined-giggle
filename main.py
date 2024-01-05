8e4078f732911c3c023f66ecae6fe3f2491af3e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:55 +0000

    decide bet modern slice sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
