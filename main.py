0a61db587e77ac0934a31d9bc08fbe3c04162ee5
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:51:38 +0000

    workupto own bet event content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
