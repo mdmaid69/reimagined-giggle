b965d6182b04fae26f0acd38f37b3fed3bc3132d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:11:38 +0000

    lose rather used settle genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
