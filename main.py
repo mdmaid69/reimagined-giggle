9e81028a11c0ce0fd713184c50af2e7141baf66e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:38:30 +0000

    balance motion Vv fashion any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
