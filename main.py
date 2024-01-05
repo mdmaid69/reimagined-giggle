36a20f2203bceb6ebc2982dad231959e878aeef3
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:54:27 +0000

    attract valuable funny first question

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
