72cd7e8e9c8d771d39125d59ce9fac4009adac4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:11:04 +0000

    sensible have home part reduction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
