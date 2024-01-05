4666ad1faca8091f47ccad92a387cee4e736a247
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:14:14 +0000

    fear miss pin further normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
