ea2d81a5634e7bb9940b425166530c5aff5a9f98
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:42:09 +0000

    stop practical release education turnout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
