ceed1c860413c83a0dd4fb9c074e02ec59246555
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:06:40 +0000

    hit pool body doctor damp

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
