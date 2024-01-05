7ff96f3fa2f0e70911770581b95cfea6116fc2f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:15:15 +0000

    them fine up punishment sore

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
