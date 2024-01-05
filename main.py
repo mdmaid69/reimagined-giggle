a25b10837b8d0bb8643ff0d2315c115f916a21fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:02:11 +0000

    comfortable should share interview study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def subtract_numbers(x, y):
-        return x - y
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
