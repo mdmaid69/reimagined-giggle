f4cbf01b564759d54fcfc18d847f9eb36770365c
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:04:23 +0000

    both loan advice advantage live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
