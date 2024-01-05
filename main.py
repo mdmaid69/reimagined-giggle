4f1806c42ccd543db4516987868995b514183a88
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:52:09 +0000

    add split pretty for however

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_area_rectangle(l, w):
-        return l * w
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
