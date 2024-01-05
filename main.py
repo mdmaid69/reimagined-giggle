9f3d6e94966ba9b4b92170f524999b8b7cf5d9ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:45:32 +0000

    collection unless force 572enter nature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def calculate_work(force, distance):
-        return force * distance
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
