908ecfe107acb7d7f11c14e77c8716d46dca2f99
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:07:26 +0000

    please full pool flow workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-from collections import Counter
-print(Counter("hello world"))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
