2b884815b8ae2e7ebaac555b3aa85bd878b7861d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:38:48 +0000

    half especially my key sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def get_array_as_str(array):
+        return str(array)
+  def square_number(x):
+        return x**2
