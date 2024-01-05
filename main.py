1f23ade26ece7fb363ffdec8d337d9cace57d85b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:52:42 +0000

    hole sharp impressed bound interfere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
