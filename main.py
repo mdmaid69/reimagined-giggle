068ee453ed3a58bb297b0e460255141501fe56b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:34:25 +0000

    protest goback(to) remark report comeround

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
