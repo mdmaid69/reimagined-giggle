1984cc788da69db13e86df93da1a7fb61479d3cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:58:01 +0000

    exactly themselves cinema unkind human

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def reverse_string(s):
+        return s[::-1]
