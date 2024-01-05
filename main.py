2d369929e428989f130dd35cb02bcb0ff4afecec
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:04:20 +0000

    error trial blame occur base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_repr(array):
+        return repr(array)
