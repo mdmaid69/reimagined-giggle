4c74e37a35cc4aa0822e5fb421b14904adbb8947
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:52:14 +0000

    hospital trick permission switch trousers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
