c8facfdc3e4c6f0eb936608116abeca5f563d8f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:42:41 +0000

    mix brief leave turndown exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
