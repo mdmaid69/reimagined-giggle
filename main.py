42de12e7aefc69268e03e9ae553b2625e49ef2ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:45:19 +0000

    count noisy giveback say breakinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
