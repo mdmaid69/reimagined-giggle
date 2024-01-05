d8df482c22aa3a2f998e9e95eb74312b8aa06939
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:52:48 +0000

    ours fly quickly cut adjective

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_user_list():
-        return collections.UserList()
+def find_min(numbers):
+        return min(numbers)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
