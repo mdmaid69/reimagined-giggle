4ba007a955af1a1cc80adc157178c7fa47705796
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:33:42 +0000

    false ear quick brush punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
