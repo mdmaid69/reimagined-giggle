3208c36ad178d2acae9308fd4523c00e1df5245c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:34 +0000

    cold shock whatever book visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
