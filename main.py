7dba7a088232e6186355b36b049330984ba73054
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:12:26 +0000

    evidence naturally glad succeed skirt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
