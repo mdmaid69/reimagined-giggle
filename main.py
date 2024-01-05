5fca2b3393578f627d2dc76f6b0571e5929544f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:23:52 +0000

    fast giveback praise side happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
