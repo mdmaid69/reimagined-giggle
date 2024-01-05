76438a226f8b2b540b2a4e40ad18021586eaa7db
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:40:31 +0000

    wet set idea road mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
