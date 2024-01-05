20752561374b85093cbf357cf3908a5b09f67649
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:41 +0000

    drink shut sad trick union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
