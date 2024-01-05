ea62ec11b55039f96494291723f8f054f609c0b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:53:15 +0000

    error develop fresh raise afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
