9a5323b2c6c9da71ca21effed4c97d2c27d2943c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:53:38 +0000

    computer who pay thinkthrough until

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
