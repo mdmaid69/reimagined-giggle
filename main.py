9211dc6e341db3ec1e7f8512066eee992136fd07
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:43:04 +0000

    put relative increase thin interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
