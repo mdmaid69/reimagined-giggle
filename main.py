21c3068893375d6dbd9fa7fc80e23b20606e6ac2
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:51:55 +0000

    factory list flood cause exercise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def reverse_list(lst):
+def count_words(sentence):
+        return len(sentence.split())
+def reverse_list(lst):
         return lst[::-1]
