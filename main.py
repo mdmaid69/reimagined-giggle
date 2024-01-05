19276ccd59207ca872e03363a2e5e1a0576da467
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:33:06 +0000

    reserve may remark stamp occasional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
