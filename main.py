df6a91c1f08fc1b6804cfb438b5ff2a5122d8a4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:46:53 +0000

    strike now prepare sale neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
