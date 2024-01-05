058b88dd1bab70b89f45caf609203729106add4e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:50:34 +0000

    port spill feel joke ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
