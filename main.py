735987d7b9f9b19619530f91301caa74a3749b6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:50:24 +0000

    whenever study address match party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
