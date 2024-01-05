83a98fc83615e5711afedde2c9087d2c1f2a99da
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:45 +0000

    evidence instead justice unless meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
