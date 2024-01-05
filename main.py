e6fb611ed6d4cdd316f70886773e4fa6462b926d
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:00:08 +0000

    freeze determined minimum market image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
