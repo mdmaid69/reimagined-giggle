7ac72950b3ecc0d83394114e1bd39807a05d4493
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:47:30 +0000

    mend ocean trousers sleep pity

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def convert_to_binary(n):
+        return bin(n)
