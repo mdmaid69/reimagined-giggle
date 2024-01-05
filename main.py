4f4d71b41b273c5a424b37560ac3c82b08396004
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:43:24 +0000

    lucky ourselves getaway miss tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_user_string():
+        return collections.UserString()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
