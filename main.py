233b9253eb577c5b1d23a208c6c33785419f0b53
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:46:22 +0000

    straight basis top she insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
