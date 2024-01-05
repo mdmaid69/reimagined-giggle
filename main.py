e24d5cce0f0e4f1594cb5281747b3b267f2ab21e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:06:53 +0000

    cause bottom yourself provide tonight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
