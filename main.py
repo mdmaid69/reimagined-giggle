b750cfcf040fc263546ec9f56caf4c31cc03a6af
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:16:30 +0000

    tape ball permission corner Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_as_float(array):
+        return float(array[0])
+import datetime
+print(datetime.datetime.now())
