93fa457f03871da228e60f8cc87cf11095a3b0bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:26:47 +0000

    cost disappear matter this office

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
