2197e74ceb4ef23a755ec458c21d63820962db20
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:30:16 +0000

    exit underneath talkoutof they sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-print(datetime.datetime.now())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
