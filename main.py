6c4975c71ae5ebf7d234f6388422608cda412840
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:15:07 +0000

    hollow equipment giveout conversation exercise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_priority_queue():
+        return collections.deque()
