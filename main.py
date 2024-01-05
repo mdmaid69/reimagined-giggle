70b46c9db1d1ff3f9d06080cf4d41b6dc8c25216
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:48:53 +0000

    name confusing mass behind dare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def remove_directory(path):
+        os.rmdir(path)
