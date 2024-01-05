160736e1024b378d99e0d3254285cc5aa269f6db
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:38:27 +0000

    bar present opposite little slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
