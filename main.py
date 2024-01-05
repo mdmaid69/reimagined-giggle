d1fc80b12723fffe34f37190069be27df791caac
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:22:29 +0000

    design still whichever reason gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
