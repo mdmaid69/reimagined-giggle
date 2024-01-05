697b2eb053db85310701441bcef48da80d07a674
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:02:26 +0000

    alone guilty beyond dress ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
