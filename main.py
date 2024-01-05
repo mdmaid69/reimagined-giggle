01324f645e20a3acda71496e6a9ae94ab0d16f11
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:13:54 +0000

    turndown pleased brain play prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
