779e114abaed0fa50858a788a2cc0d8aff9afbed
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:33:34 +0000

    lunch minimum breakup behind dig

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
