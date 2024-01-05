c6264ffbbd1deb8e88ce8013aeedbb5bd48a8a86
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:33:29 +0000

    bright protect introduce pretty so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import getpass
+def get_username():
+        return getpass.getuser()
