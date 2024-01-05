9259877f47197bde4d36f34475fd645ec1179d33
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:53:52 +0000

    ilovedicks well store worst us

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import datetime
+print(datetime.datetime.now())
