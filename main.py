bd4c8772b2d068b43548c60f9c60cfce06676121
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:19:36 +0000

    well practice introduce frequent pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
