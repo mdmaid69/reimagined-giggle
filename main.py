a8eb7ba9d068195caf7526e3e8c3e9677ab6748b
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:36:02 +0000

    thick shelter cross organization badly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
