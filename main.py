d2343a185f9923798d67f30d1ba1cea01c860980
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:15:42 +0000

    deliver risk shock float operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
