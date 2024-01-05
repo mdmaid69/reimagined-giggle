80635ba02da140ba5aa63f235a363475ed5a8b8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:25:39 +0000

    sign getup backwards whole supply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
