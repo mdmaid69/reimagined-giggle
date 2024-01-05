b61ac282f5c53bb44b4516864fa3e3066cf4369b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:40:28 +0000

    unpleasant gift wherever taste priority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
