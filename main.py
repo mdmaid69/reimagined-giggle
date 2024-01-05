d2706a321bc8a371743e97f80360593ec082359b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:46:30 +0000

    ago exactly firm journey top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import sys
-def print_python_version():
-        return sys.version
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_stack():
+        return collections.deque()
