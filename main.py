e2ccaaef05bd4ec825491a52203180594f4dd8e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:09:08 +0000

    importance impression nowhere receive laugh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(lst):
-        return max(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import collections
+def create_queue():
+        return collections.deque()
