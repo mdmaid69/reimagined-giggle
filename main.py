93265e5d9368b2b694bb04eac9373ef74edd4a2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:02:26 +0000

    demand callat description free gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
