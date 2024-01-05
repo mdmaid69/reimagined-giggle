485395424289f35dd655e74fccce972b2e324ffe
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:15:30 +0000

    various backwards declare priority serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
