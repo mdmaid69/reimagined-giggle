0e0f5b42f07d04c5cce3c2c6ab44a722b722cd4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:38:55 +0000

    hit minority turnup scrape mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def find_min(lst):
-        return min(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
