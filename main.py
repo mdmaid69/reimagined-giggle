96153f67541f9917e0e39b8d6a350b4a6ebfb0e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:05:45 +0000

    apply present famous godown sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
