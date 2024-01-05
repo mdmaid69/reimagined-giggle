0ff7a685241a7686f56e5dc42617942687952d6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:53 +0000

    comment OKorokay far estimate cut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_rectangle(l, w):
-        return l * w
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+print([x**2 for x in range(10)])
