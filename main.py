9ed3f5145a5f236908e23256dff3c3d474ac34a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:58:39 +0000

    mass load Ff before report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import collections
+def create_priority_queue():
+        return collections.deque()
