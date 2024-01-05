3f157394bb6a56fccca5f7c7e6f45de0bc567cff
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:12 +0000

    letter though gentle run admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
