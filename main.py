56170f2fa58e810787c68cedde1516f2f4c7f531
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:46:17 +0000

    pipe shadow special wall prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def convert_to_hex(n):
+        return hex(n)
