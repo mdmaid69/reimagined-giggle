85c832a0d0f415160e9a75a7adb4c4e310e16113
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:12:19 +0000

    breakthrough damp movement present thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def reverse_string(s):
+        return s[::-1]
