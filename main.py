e8fc5462c1b934277c479097c4b9a58ac29c191f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:11:21 +0000

    report sad tell force suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
