0c12a5d2a95dfde1432a5d6d39c08b2795ce64f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:31:41 +0000

    spite keen splash fold smoke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def subtract_numbers(x, y):
+        return x - y
