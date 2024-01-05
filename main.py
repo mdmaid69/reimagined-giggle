751ba8e19cd821bd49c7a99eaad2d50c16a9aed7
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:50:16 +0000

    union breakout even design deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
