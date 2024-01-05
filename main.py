d3c0be2ad920d3a0ac8bac4f63cd8b8f48adc2ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:07:09 +0000

    important post quarter perfect gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
