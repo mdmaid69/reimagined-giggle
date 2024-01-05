d1e2981d0962474d8bb8a8c589bd3ed8e9a54c1e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:31:01 +0000

    vanish manner information formal wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
