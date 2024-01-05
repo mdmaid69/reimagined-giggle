53c46f60d7e2b6c1f70c740f99e6fb05b4c501c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:47:28 +0000

    string light birthday substance reliable

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
