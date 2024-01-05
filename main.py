42a7b45fbcc57b5fc91411f87426d97ad851e963
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:53:23 +0000

    active well government win exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(5):
-        print(i)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
