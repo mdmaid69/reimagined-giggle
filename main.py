10fcf08b04a8203c11e14c9d40a53512b62c5dde
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:01:38 +0000

    anyone off several smoke realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
