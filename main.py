4bed346f238f078426cfca79ecb62808c74edc7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:40:54 +0000

    occur bang total cover example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
