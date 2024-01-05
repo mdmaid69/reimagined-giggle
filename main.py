7e289bb179a52796788d3896e04fef52e4d1ea9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:22:06 +0000

    survive refuse duty collect nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
