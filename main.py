5133b408c0b37ff789ef8d98bc86b499499ed557
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:08:46 +0000

    anybody bang final distance phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_odd(n):
-        return n % 2 != 0
+def count_words(sentence):
+        return len(sentence.split())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
