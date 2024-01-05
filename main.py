904cf651372f8de3986e4fbe9c594fd94f36349b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:46:45 +0000

    first lost whom basic borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
