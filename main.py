2cc90ed747cc0da696a61f442fe3bd8f1e647088
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:45:57 +0000

    shop Yy transport building help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import array
-def append_to_array(array, item):
-        array.append(item)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import re
+print(re.match("h.*o", "hello world"))
