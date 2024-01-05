a0b5583898aa1e0fa40a6b52ad60f321efe63680
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:42:16 +0000

    world watch weekend ring charge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_elements(lst):
-        return len(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
