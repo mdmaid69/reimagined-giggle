d85e08ab939ec44e48cd514240c9cb316927b248
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:02:37 +0000

    enthusiastic know substance deal model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def square_number(x):
-        return x**2
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_work(force, distance):
+        return force * distance
