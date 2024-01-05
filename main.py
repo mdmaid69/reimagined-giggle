df7581405cac9e7863eb412111155b405ea48f6a
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:02:30 +0000

    own wrap action benefit wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def find_max(numbers):
+        return max(numbers)
   import os
   def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
