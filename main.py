7f691e9dd990844f87f2d931a101ea118559d125
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:04:54 +0000

    draw thought land instruction sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
