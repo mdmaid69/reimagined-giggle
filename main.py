6c8ac32c2c5917e2da69e80e5de50cbc4a3286ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:31:49 +0000

    sink pleasant brush bound exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
