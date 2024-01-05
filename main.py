ee6b64ce8d751f71cc8f008e15c31014a25f2969
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:25:22 +0000

    important any advanced mine argument

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_repr(array):
+        return repr(array)
