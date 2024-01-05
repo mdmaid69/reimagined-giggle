8b476503cb33d45976a313f07c6214a2cdad6634
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:03:23 +0000

    present change refuse emergency feel

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
