90de39eb10754545457822fc0f95ce5939271a36
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:19:43 +0000

    usually fond repeat together area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def get_current_time():
+        return time.ctime()
