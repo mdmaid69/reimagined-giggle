734a3076d2e1afb5b8530b4c0a0a90f07af6dfb3
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:07:20 +0000

    different height official music rescue

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import random
-print(random.randint(0, 100))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_speed(distance, time):
+        return distance / time
