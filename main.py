98cc32379e048e1c54e9da8fd500c1fa581e80a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:52:07 +0000

    less fold scrap relax responsibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
