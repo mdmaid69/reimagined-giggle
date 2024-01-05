8d3cf8c49046d6c4c4e24967df29b150fcc9e99c
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:22:17 +0000

    shop gas point succeed Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
