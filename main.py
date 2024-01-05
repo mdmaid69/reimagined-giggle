66cf9726048683b32c4c22bb8317286987742e23
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:50:15 +0000

    wake professional shake step secret

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
