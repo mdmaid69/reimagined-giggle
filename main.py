93582cc7e57673a0e05b54d69b1981e2c13376ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:48:33 +0000

    skirt yours reasonable software people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
