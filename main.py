dbcf42f15701ed8e284b180a0d000abf4336bb84
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:14:49 +0000

    like cheat steer can through

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
