673e374860c2d6b359be81b9b80126ebb91da943
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:38:14 +0000

    hope apologizeapologise bored spring thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
