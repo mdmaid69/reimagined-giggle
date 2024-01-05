6f21351ac95c6abf3f58fbe3f9c351d2b1cf2539
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:02:21 +0000

    considering odd bright talk interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
