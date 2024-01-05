ce6c77812c971d6cce5bd0ea936a5e5d85e4df6e
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:32:01 +0000

    row drink there soft step

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def calculate_distance(x1, y1, x2, y2):
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
