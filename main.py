e726922cad4b5ba50237dfa6e8f9fa24e82b4c6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:57:43 +0000

    pain separate practise empty mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def square_number(x):
+        return x**2
