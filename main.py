e5b393f48ac1010ea8a57efd48dcb5de1b152624
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:21:23 +0000

    room left meeting payment can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
