728d987f602f7b102cb7a4770d56fea5155b04d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:07:17 +0000

    branch positive to run band

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
