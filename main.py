f0d0a66d6cbcbec17fb67b451dfa079d433f62b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:01:14 +0000

    reduce rush earth cheque steer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
 def calculate_speed(distance, time):
         return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
