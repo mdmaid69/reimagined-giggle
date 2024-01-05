343214b32192acc1fc3f53041749eb4baf9afbb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:50:01 +0000

    kick over wonder group art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def cube_number(x):
-        return x**3
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
