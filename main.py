88acdb2c1175ece69d06450688e1fa026999a94b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:57:14 +0000

    tight sort welcome catch pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
