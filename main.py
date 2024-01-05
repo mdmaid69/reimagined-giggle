d6b7c6243afa785e718fcabdb1cd0f61ea23fcc1
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:42:26 +0000

    pour majority apparently amount finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
