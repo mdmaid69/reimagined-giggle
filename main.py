3b091ffcc43a4662ab0174cafc45bbac7400d6d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:35:19 +0000

    tradition strength convincing artificial sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import time
-print(time.time())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
