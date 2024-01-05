67c50bb20dfc8982c4855f34587fe6016170ad78
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:18:07 +0000

    certain university pocket arm shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def find_unique_words(sentence):
+        return set(sentence.split())
