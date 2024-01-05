2a1f7faacc47e5c198afe73eafc03b92e951f797
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:48 +0000

    matter sink slice part software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def subtract_numbers(x, y):
-        return x - y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
