228864ab2f85ef0104a6ee6fd9628a2cf3140242
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:13 +0000

    practise drink fear print steady

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_priority_queue():
+        return collections.deque()
