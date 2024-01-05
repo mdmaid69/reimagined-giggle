0922d39b2889a923091ace7d4b7721dd6d0ae1e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:35:31 +0000

    children distant balance connected dry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_average(lst):
+        return sum(lst) / len(lst)
