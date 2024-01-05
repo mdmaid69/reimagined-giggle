69a29adbb45f39c658e946e5d24660d591319907
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:49:06 +0000

    quickly consult and calmly main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
