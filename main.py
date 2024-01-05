525516dc9d73f1868d5d0879b9c1919861f5643a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:49:15 +0000

    guess reply sweet what along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
