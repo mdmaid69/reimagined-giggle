f2dfa6e94fa036a5f5d96706628c18e22ff4650d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:43:25 +0000

    along draw onto over shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
