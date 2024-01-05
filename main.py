7c15d1824ae4eda0412d39b53d88ac0b48472bcf
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:08:51 +0000

    whether moment repair succeed justice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
