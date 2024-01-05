e7c4fc8407490828cf64e95567b82e412dde1a5c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:56:36 +0000

    display skin play do say

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+print(math.pi)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
