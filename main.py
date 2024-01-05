134017551ba20e3b095f05d6617a6b44f0a78073
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:33:55 +0000

    stop important soil complain extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
