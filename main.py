3499af54fc78158e460849512fb1dcf77b8f65b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:47:11 +0000

    bored soak offence peace meal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
