8d4f9e94b5b1955faf55b9efed1c780ee4dda54b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:51:17 +0000

    destroy fierce this sound substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
