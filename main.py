71cbe4a685f21b85bbbdbfacd559193af73d56f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:39:59 +0000

    forward compete callup sweep friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_list(array):
-        return list(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 def calculate_circumference_circle(r):
         return 2 * 3.14 * r
