a8a07cbe6f9df5ea8d9e4eff0decf88873c15784
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:29:49 +0000

    challenge comein march teach wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
