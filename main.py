315b35942082827919f74b7723617e4b55865701
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:32:11 +0000

    favourite cheek complaint fine complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
