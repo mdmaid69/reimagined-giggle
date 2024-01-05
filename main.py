d65d1cb258ab96ad13398c4e1ffdc2a82aed6176
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:43:11 +0000

    callat art live concerned accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 text = "Hello, world!"
-print("Uppercase:", text.upper())
+print("Reversed:", text[::-1])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
