5bedcc26034f82813f90b456a43bfe208d2cf614
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:06:56 +0000

    help store care unusual unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
