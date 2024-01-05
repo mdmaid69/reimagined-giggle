c1a1f7c97d1559d61eb54f51d774874083c9acd2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:58:37 +0000

    meal while speech fast trial

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
