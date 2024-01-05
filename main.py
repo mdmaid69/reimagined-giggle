4119ee30cf8ef73363e3852ab634d76727ff8158
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:26:10 +0000

    crowded dangerous you emotion rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
