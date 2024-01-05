2a371868bae69f934ce37a63c9bb726ef41ce9c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:29:51 +0000

    confused practice method false perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
