abe40d260bfdc251a893cb37d3622767c73dd439
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:04:49 +0000

    continue bush behind impossible cut

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print(sum(range(10)))
