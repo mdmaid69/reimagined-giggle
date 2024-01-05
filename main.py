be72cc599da1d889b8532fd14b3f801aaf013fd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:46 +0000

    skill shock lead Oo brave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
