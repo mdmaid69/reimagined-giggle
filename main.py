65ab26752237476a53222b155a20d7e08a3224f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:34:00 +0000

    holiday tomorrow entrance board how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
