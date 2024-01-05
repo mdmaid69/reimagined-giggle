f5e44734c663633ef2914c02509a236d5cea9ba3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:11:29 +0000

    phone soft present yesterday smell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
