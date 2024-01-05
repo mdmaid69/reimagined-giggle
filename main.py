138aa90f99238596cd5cb9eda064d2f5b163978d
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:38:59 +0000

    noise Itself artificial legal sale

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
