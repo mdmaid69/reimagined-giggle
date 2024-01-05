e0a094bb05380da8f006796faa0151e5299c519a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:45:54 +0000

    match bite couple firm possibly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
