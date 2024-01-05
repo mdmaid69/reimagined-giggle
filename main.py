4aff13d3de99a1840a5a893d0a7aba7153d2d7d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:43:45 +0000

    car discussion wear indeed night

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import os
+print(os.getcwd())
