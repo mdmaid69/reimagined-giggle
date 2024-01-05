302f5412d764dce694f7328e86436973ef6656f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:06:24 +0000

    calloff worry great seriously add

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
