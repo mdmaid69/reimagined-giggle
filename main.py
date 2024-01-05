329728448fa7c10a197c2d8c1ee2879fb705d8bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:26:48 +0000

    square brain rub spare remain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
