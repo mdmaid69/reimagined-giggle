143b0a7145c5be3733a1754ffb92492811c5867e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:24:44 +0000

    own honest we club heart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def multiply_numbers(x, y):
-        return x * y
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
