5b321e6da83d33b8534502e997f7f1b1252d825d
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:03:51 +0000

    react exaggerate copy completely comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
