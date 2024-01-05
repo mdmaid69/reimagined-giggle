26ab9f0f8d95df1a5ac51676b1aeb525b99cb58e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:37:14 +0000

    future hear suck mine accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
