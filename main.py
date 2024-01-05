02f6c08e5074444f23252b2d5d905ccdc4619f39
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:07:13 +0000

    then worried crash post drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def convert_to_octal(n):
+        return oct(n)
