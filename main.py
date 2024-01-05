5ab8910287e8f10ae39776ab088c3e806a6f37aa
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:35:35 +0000

    conduct still key female recommend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import numpy as np
+print(np.array([1, 2, 3]))
+import random
+print(random.randint(0, 100))
