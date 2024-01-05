0864c282ef2642e947f716af8f158840cb620d7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:59:23 +0000

    badly excuse shout type outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
