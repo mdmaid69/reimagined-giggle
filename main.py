50c8818f55b8c2c84259740d659d708bcebd66b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:47:25 +0000

    fight by interruption walk dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
