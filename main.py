2ae9825e42c68dd6f68f3d942f2a846338cca184
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:37:48 +0000

    square climb day wish breath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_min(lst):
+        return min(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
