3949a0e251137ed2528ce03fc05d6824a969bb9c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:41:55 +0000

    owe loss justice chat nerve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def reverse_string(s):
+        return s[::-1]
