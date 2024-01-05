2ab1da706a861ecd721f1af022c19c286f432a76
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:24:08 +0000

    dance together rescue expensive anything

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-print(sys.version)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_str(array):
+        return str(array)
