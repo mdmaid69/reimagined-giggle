f3a30590a5220c2b14990fc3a18b342304cc7963
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:46:30 +0000

    sense order always book region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
