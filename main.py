742517d39dbb508d386af7bbbc3a876e6257ca51
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:32:44 +0000

    programme tax gate so mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_speed(distance, time):
+        return distance / time
