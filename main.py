1267f076a96d0a45e8f18fd7b1669a9a47705830
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:02:40 +0000

    gift hospital rank neither serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
