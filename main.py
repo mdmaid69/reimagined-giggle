f2de116a176b0821cfb8930a3ee926e5f6dacdfe
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:41:36 +0000

    square nigga space 565end separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_list(lst):
-        return lst[::-1]
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_int(array):
+        return int(array[0])
