26414ce61a91b0a9ba371a42de70b4589454fd0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:34:42 +0000

    round treat accordingto gradually left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
