75377404de48da40c9a2163a6c95f126d1367b87
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:31:38 +0000

    greeting piece indeed beat sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
