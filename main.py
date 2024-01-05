29aced861512d8ed2df2159919f1176057c43cc0
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:44:35 +0000

    education move prize accident without

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
