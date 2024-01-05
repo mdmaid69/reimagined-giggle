3d857b581e14eb99987d21b9ef6bf5fbe25450fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:07:20 +0000

    rubbish accidentally march wish shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
