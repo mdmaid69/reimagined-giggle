56803ebce9e128046e17d5e82a6b03a01d68c1e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:58:46 +0000

    callup value too want breakin

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def cube_number(x):
-        return x**3
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
