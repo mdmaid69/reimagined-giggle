3afd6d69218f25807d0d0817e2cb3662c79a4a1f
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:29:33 +0000

    talkback straight first same no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
