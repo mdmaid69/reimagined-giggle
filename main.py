fe2c5dc6fa1658ac7d59a08aa89319d597374bda
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:51:06 +0000

    slowly fortunate major place stamp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def split_path(path):
+        return os.path.split(path)
