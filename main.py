bcada448c7fbde2d316f005b70a5de9ce7fca7be
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:40:43 +0000

    concern Rr mail comeback force

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def convert_to_binary(n):
-        return bin(n)
+name = "Python"
+print("Hello,", name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
