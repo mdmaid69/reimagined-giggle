e950f5f31244fdebddfc3c291e6dd0cfa3985caf
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:22:05 +0000

    vanish instant marry fix bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
