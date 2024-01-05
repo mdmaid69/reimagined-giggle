a666ecdbf4d55b71772d92f268b18ef4a100e41e
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:20:28 +0000

    share sick pull improve report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
