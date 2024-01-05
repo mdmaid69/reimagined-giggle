4333d479c1665d41a1cbab95f9fb8ae0ddd669a5
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:05:22 +0000

    ilovedicks place shade reject really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
