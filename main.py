07fcf9a2eb57a4a357f1cafb0322a01b983525c9
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:36:56 +0000

    trust curious unless place steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cosine(x):
+        return math.cos(x)
