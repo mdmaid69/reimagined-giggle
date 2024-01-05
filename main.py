af95171bee72827c541ceb67bc8fbc1e1c108dcb
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:43:44 +0000

    legal run charge put giveback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
