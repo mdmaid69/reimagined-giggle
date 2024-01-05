6857363f4c840849bc61c7dc2bdc6dc6d6a22dd0
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:00:01 +0000

    bit lock before arm safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
