6e8f80497b206c9d7a644fe0301203768c127a4a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:12:22 +0000

    boat legal up butter shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
