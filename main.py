b82b933f5e60dd7cdc78c029283432e7023745f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:59:53 +0000

    disapprove Qq continue but smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
