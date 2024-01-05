38a934a260a275e9a46bddda4b3a8edde8349ed9
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:06:08 +0000

    quantity telephone shall might shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
