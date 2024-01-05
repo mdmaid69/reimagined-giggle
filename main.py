46c8b3856b16bd495543c1e85375f255ffad6c94
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:50:48 +0000

    love dream yourself club declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
+import collections
+def create_queue():
+        return collections.deque()
