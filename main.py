0d51231080666e34af59c22024cc7a95442e3656
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:37:05 +0000

    drop flood tour our nowhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def count_elements(lst):
+        return len(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
