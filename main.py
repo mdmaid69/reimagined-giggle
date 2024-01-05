24d0f3e2d3d1ec1357691d16d09615f240995a48
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:50:58 +0000

    band wipe model love same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
