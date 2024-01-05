c9b99dea756d2c7b366330a4a8903bf5366b3d14
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:46:59 +0000

    holdup passenger open down corner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
