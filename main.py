29bf62de97ea9bbfb7f72d70fb4716aabcf7dfaa
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:33 +0000

    refuse shallow where breath respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_pressure(force, area):
+        return force / area
+  def square_number(x):
+        return x**2
