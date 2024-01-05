0b8c6a0e69c2e834771120ea465f561231c03f4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:47:28 +0000

    loud case through plenty quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
