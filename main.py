89c257970c83296a3c2b28ecf4218a15e70f918d
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:04:23 +0000

    used skill choice coffee your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
