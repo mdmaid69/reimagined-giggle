6ac51bd5789fc19d735ae963351bbfd696f3ef2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:33:57 +0000

    fact goodbye once fly successful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
