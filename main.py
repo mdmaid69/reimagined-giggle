f03fb43744635e737c134c665c761938236a0568
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:18:20 +0000

    curious belief fasten angry attitude

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
