52eec4a5f99281e1387abd0b3f947f481fa01a6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:24:31 +0000

    makeup both object shop unfortunate

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def check_if_file_exists(file_name):
         return os.path.isfile(file_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
