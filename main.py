371a7f58c6c2cb1f8cf6897edb40151ab041e8ab
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:01:22 +0000

    world left violence goodbye gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
