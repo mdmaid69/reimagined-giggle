e8aa1b60e18c950870b49385498e2892134d6e54
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:00:01 +0000

    everyone bringup while usually undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
