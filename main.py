f7f11791104b206dc3eed83cb40f223619ab9ab9
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:08:38 +0000

    talkoutof system judge near successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
