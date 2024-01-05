658fdd49c1372efd5ca5edd972b00f5f5ea7d987
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:35:53 +0000

    handle possible regret listenfor petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
