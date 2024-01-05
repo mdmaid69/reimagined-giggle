bc822e1e2b909be66d365eb18352b47334c4f7f1
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:40:39 +0000

    answer the away stupid most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
