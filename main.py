04229e375f18170fe1366b3e66b0ade5f48182a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:56:15 +0000

    if general holdup sorry corner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
