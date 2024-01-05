cc4ded42aeb693f6f9a5fb706d1e99addcbc5130
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:31:14 +0000

    form lift minimum level warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
