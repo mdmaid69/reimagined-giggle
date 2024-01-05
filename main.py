45f3eb05e6e658144f4312c4d6bfdbec00909c96
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:35:22 +0000

    state probably strict seat hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
