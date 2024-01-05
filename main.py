aba3577bfb52d618241f2a20cde251a8f9ec78f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:24:27 +0000

    problem mass rank interfere goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import time
+print(time.time())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
