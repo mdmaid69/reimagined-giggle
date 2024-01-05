5f227c07c17a1ddbefc3940b00f148a6859ec84c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:44:16 +0000

    runout contact use challenge stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
