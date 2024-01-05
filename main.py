f4f1539e7b27846e74761b292e3081a066611e4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:05:49 +0000

    moment travel spite goaway wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
