a6986c0f2094602ecc04c77317e398641d1c65e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:19:11 +0000

    hospital body excellent persuade flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
