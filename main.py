e9dbe473f4a40caccd4c735a070f5fc2a4ad40ae
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:39:18 +0000

    Ee special safety reject month

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
