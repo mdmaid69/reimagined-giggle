08330ac91d39f20915a50d89372025df4e931814
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:40:34 +0000

    wave worry split course down

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import tensorflow as tf
-print(tf.__version__)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def create_counter():
+        return collections.Counter()
