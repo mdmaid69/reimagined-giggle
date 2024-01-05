b1d8e8841dbf1dbb0cf8873fc8fa6d1e2a7c007b
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:11:58 +0000

    water gradually present sweet man

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
