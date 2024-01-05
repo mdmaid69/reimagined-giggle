1047c936331db5aa836284e09b96be17cf80387f
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:48:26 +0000

    tour touch earn hurt should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
