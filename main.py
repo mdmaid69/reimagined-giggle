25b7bcfb37b11ac4a72c743e85a624e087fdf5c7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:20 +0000

    criticism distant quality afraid pleased

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_hex(n):
-        return hex(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def reverse_array(array):
+        array.reverse()
