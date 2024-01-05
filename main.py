70540f6a5cf4e2fe18db24ff1c61a9ec96226f85
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:18:24 +0000

    long several runinto wise worth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
