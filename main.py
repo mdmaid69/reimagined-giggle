e47c249db17f5e48806ff3d953e55c4f6025797a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:35:07 +0000

    shallow effort own uponon Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def get_array_as_list(array):
+        return list(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
