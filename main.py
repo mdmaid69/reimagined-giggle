3dc25aaa57365f2087d7a18c557a555bdca9e0ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:34:19 +0000

    mark punishment emotion poor twist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import collections
+def create_user_list():
+        return collections.UserList()
