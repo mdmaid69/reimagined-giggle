25565dd105fb59fd46487aca54d799f4adcd4bf4
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:38:02 +0000

    round funny tough me simple

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
