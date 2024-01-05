47f795ccd92f946dd31d285923668a75492ddaa0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:42:48 +0000

    concentrate comment view final line

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_power(work, time):
+        return work / time
