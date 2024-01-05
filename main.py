8de72df2c4502eb0c2495d994fe676c93b8aa47f
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:07:31 +0000

    belief cope needle summer quick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def multiply_numbers(x, y):
+        return x * y
