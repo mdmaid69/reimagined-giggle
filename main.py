b669b2ab75f99d6395e4c446daa5d6df8653be28
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:28:00 +0000

    act properly before somebody march

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_pressure(force, area):
+        return force / area
