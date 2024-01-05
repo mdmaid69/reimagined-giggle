68c2571cf12f72c9b1cc4858634b219e13ded779
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:57:55 +0000

    disapprove scream pretty task comeup

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import collections
+def create_user_string():
+        return collections.UserString()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
