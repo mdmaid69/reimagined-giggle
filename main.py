42d5a2a5adb472c028de2b3062c8eec1dd5440ea
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:37:22 +0000

    change workout know polite touch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_bool(array):
+        return bool(array)
