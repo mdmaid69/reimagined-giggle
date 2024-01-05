35b82b79a4c0a3640f1f0ba666c75600a0bb2e51
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:46:42 +0000

    vote contain Zz offer minimum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def multiply_numbers(x, y):
+        return x * y
