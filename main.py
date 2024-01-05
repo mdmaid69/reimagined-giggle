e8a9385f2c1678a5227b4ea007da7f1f38c9dd9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:56:18 +0000

    why die 179bite soak ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
