11aa00ecd13a170a911672c1d38733fae5656e75
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:16:26 +0000

    shout value passenger intend save

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
