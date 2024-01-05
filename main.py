94a2a138bbec933278e62753ce0a65c67866efb3
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:21:22 +0000

    nerve achieve contents position tax

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
