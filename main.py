04e1c35dbd841e5e45840d52a40ef39a44224433
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:23 +0000

    container embarrassed keepaway fight curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_octal(n):
+        return oct(n)
