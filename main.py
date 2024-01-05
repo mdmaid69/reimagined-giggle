05ffe7b4eda56ebe0128beebef1c8169eebb147a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:45:10 +0000

    bet characteristic other store field

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
