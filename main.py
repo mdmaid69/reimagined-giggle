53e1adb0a30d3c3be561762ed54bdb88e80d3c70
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:04:36 +0000

    correct poor order belief knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
