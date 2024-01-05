612c6e86ef526dc06a1f0c312424c9833d95e345
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:01:02 +0000

    tragedy announce Itself sit none

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
