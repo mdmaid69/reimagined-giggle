2d169865ebe5cfb6ba029f9006a4c14cab153877
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:30:36 +0000

    float occur mean hospital meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
