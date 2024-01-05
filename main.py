0ddc569bc851db56001f244bfc68cadcbcfd2c5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:27:44 +0000

    themselves display bread test break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
