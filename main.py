1bf44ee549b68367c481c4a09b1f054b48b90827
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:38:31 +0000

    collect exciting public own say

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import tensorflow as tf
-print(tf.__version__)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
