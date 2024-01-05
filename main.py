03c7653380c7058268caa7d7539bda638262d3d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:42:11 +0000

    more fast occur characteristic thin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
