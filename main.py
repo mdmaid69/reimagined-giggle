c6c0e6449f0b520e18f3a4f3120d1815da67612c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:02:33 +0000

    Bb onto change tooth result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
