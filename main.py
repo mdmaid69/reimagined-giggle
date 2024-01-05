f3f7831d56df8dccc28e6efebc69728e6a59f3f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:16:05 +0000

    figure last quick sure neither

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
