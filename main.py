d0e3e595a7faaad7877d6ba628c9cb4649fc3c1c
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:23:30 +0000

    thin person fit straight need

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
