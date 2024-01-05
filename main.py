ea1b4432bfc9310d02c46ecd4cc78df40ce0b604
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:59:38 +0000

    private length flat consult importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
