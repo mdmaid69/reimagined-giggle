7f371075f69c2ecac088b5c819b1a1492dddccad
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:48:49 +0000

    hold payment position damage form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
