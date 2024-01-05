da8fb15d971c69a8bf297e751d18b4d6ebc613a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:35:34 +0000

    loose father tradition delicate callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
