e008f89cee20463ab7a5b2357356486eafa431d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:06:05 +0000

    hospital new whistle permission regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
