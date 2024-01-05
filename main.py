0a9a321e1063bbcf04bd36bfeb13bbf0a6138ff2
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:36:01 +0000

    back frame final report thanks

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+name = "Python"
+print("Hello,", name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
