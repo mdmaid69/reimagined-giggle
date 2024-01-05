a74025595816f2602706983d4af488f4f6185d67
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:26:27 +0000

    preparation charge activity moon doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
