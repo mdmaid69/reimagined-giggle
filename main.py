62a1e7889c0f4fd7600fdf25ae6b3e54f0fad696
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:32:47 +0000

    getback weigh factory accident grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
