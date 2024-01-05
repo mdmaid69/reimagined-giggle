29ab24ecccdd24e54fadf4f68cbd5d8e98e5f0fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:28:24 +0000

    fly thing step deliver giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import collections
+def create_counter():
+        return collections.Counter()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
