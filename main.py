eb0b2a9fe54cc3a91e5039ab6c7d70808f408ae6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:45:32 +0000

    behind support tongue dead holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
