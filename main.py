71ee4782fa933392858a5aa4fc32a803fb6480d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:35:33 +0000

    limit adjective repair down rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
