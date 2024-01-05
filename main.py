c4eab63b0b4e4594e3950b370ff05d080135ccb2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:48:35 +0000

    involve instead write performance possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
