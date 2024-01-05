abb3003a58460810b514d1999438b71c11ef237d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:09:48 +0000

    right pinch strong pin air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
