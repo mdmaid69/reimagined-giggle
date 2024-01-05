3bbdaa7610229732389f32589a695b60f64d4974
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:02:34 +0000

    aim key light low range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_min(numbers):
+        return min(numbers)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_length(array):
+        return len(array)
