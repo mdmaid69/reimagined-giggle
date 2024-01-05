9d48f20590b3aad14219b74c4b95ce137bae1023
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:40:11 +0000

    scared turndown grateful factory expensive

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
