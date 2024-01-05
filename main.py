a49868122fa35b0e65b0493cc68f4b332f0498de
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:11:59 +0000

    glass arrival thing hang increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Characters:", len(text))
