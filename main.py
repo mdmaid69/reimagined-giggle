7d9c146032bf2df74b4d6b6e3a490dfc010cae13
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:34:49 +0000

    deal fire business past mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
