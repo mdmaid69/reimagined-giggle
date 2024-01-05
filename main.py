f8679732be13dcfefcb2a9a183ed89d5e218e37f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:55:45 +0000

    cinema afraid strong we possibly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
