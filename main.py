6cea3ca764adae9e1a38170b31eb7246189ee8ee
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:56:46 +0000

    reply afraid thing partner Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
