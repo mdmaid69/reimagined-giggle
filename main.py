a7365ae17d723c32d87d2545e63023ebb0351224
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:51:23 +0000

    bank aim favourite careful goback(to)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
