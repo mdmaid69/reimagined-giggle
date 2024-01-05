47fe6880a5e03f4094aa1c76fa686c6492a9d364
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:25:08 +0000

    central activity recover in culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
