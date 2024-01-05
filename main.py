12dab7790a0c514a3b0e41db15c0e86da4ddb860
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:57:41 +0000

    picture equal for whether common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def convert_array_to_string(array):
+        return array.tostring()
