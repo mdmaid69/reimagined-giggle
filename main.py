34ae2c88f5d7e7dbc56817aa98e31e838de5467c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:40:02 +0000

    throughout unhappy agree out smell

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 from collections import Counter
 print(Counter("hello world"))
