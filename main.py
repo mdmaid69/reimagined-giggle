4d6bbfadcca3b302541c65b0b161b7531de81ca3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:56:46 +0000

    port 571enough pipe great Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
