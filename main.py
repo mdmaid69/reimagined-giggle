a9d0a67b7fb43ca57c9990c9ecf962db3a73bc38
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:08:13 +0000

    negative permission body hang translate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-for i in range(10): print(i)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
