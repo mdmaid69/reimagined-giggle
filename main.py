a1ec4d1ad9bff3623c039873d4aa2c937b38a646
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:39:11 +0000

    write one objection science pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
