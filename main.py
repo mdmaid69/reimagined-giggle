c13ec6e6d6ce6430e03ae2f2de6cd9bf4621f8d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:06:38 +0000

    brown Tt 573entertainment protest warm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def reverse_list(lst):
+        return lst[::-1]
