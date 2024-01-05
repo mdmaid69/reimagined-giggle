0059ba67928509de3ce498ebc6d763b893ed411d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:20:26 +0000

    never teach to visit small

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
