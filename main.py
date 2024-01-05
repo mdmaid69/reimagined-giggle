6510f381712a09e9e85c373c4c4b4b7696c42de1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:25:42 +0000

    call contrast match dance make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import os
+def get_current_working_directory():
+        return os.getcwd()
