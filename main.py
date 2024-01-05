88323b1d417b8640df609b32a52ed10f60d0507f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:04:01 +0000

    heavy or least influence opinion

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
