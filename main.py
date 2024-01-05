faff0b2d89c822046103de910e07e609b6c9c600
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:40:20 +0000

    manage collection keepaway loose godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
