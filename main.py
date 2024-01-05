bd31416444667eded45eb400b1f2ea4457fc3c36
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:40:49 +0000

    physical peace whistle scale circumstance

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
