60f845db425da5e4663dffbf0831c67bc97763c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:06:27 +0000

    that accordingto journey smooth steady

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
