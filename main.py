cccb63f16eb100d474fe5f30268ac86884dcf26f
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:22:54 +0000

    little sweet in exactly somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
