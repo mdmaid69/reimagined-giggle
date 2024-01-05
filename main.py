a9dc9a4ac66881de32cffc561b9825aa2f92df7f
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:32:01 +0000

    snow film behind seriously continue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
