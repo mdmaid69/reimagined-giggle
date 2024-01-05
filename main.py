1a976cc866a48b697a7b97e828d1e4ab83fb80fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:10:16 +0000

    noone reliable meeting rely little

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
