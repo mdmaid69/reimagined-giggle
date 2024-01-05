24e0d3396eb4e9b0b9e09e0431a6468d3ff566ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:13:03 +0000

    wave brave bottom keep fit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def square_number(x):
+        return x**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
