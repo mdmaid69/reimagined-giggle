aa06a5e037245c756a23d2b0c617ade643e80981
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:03:34 +0000

    thirsty whole kind old takeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
