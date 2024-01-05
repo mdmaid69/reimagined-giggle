45d96e557eeb2fc462a4bc0e12556b1c297873ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:40:56 +0000

    rob cross matter power love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
