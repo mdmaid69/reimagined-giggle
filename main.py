6fb47c2ec8a2ab82a1052b861570c549a97f16e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:43:15 +0000

    community round callback dry importance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-print("Hello, world!")
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
