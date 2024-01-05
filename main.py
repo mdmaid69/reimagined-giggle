e59f3f6ad93c7606972c522a279d8d9b2fb004d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:28:21 +0000

    now increase difficulty seriously brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
