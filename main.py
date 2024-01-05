41dcf698aeaa7581212698a59a3b527365400abe
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:57:53 +0000

    treatment turninto experiment promise comeoff

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_user_string():
-        return collections.UserString()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
