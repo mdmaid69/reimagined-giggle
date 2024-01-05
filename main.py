66b0107b6c8b1181adb94d88be4e27141e310d8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:25:03 +0000

    apparently mood understand century give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
