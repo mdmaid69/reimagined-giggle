96fa0c326fb678ecfe4fc00e302902b447ea61dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:22:51 +0000

    declare speed repair touch waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
