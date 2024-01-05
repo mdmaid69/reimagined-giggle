08b3599a434b1e5c8c95785f75484aa73364f1d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:06:46 +0000

    bringup worried breakup regard famous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
