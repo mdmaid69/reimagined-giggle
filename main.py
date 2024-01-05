3c30f90eaa53de1aa8872b075c8ee5e471eb45d2
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:27:32 +0000

    car attempt special trap sport

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
