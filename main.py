5c74a0ad6f125defa4d128dc98bbba7087eadadd
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:56:41 +0000

    crazy service brain need wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
