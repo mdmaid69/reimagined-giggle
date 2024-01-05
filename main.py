56992aaa50a99cbe186743b6f7b1b546513c1ad8
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:14:31 +0000

    emotion operate tall frightened connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
