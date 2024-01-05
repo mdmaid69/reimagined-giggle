0f4620c60a7e0bbc517c4ad86a954a6f51430c16
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:33:38 +0000

    base demand scrap bad air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-print(math.pi)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
