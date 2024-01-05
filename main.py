49badee8b4b125f1b0707e501afd89f658d59a86
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:44:52 +0000

    definite Dd definite climb still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
