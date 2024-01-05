8aacda2a79188d834d1bcb4cbb63bcda984493df
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:01:00 +0000

    Cc answer spring favourite confident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def multiply_numbers(x, y):
-        return x * y
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def count_elements(lst):
+        return len(lst)
