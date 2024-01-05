d3229bdb520c96a29a0e270aac9a8b05becf10ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:19:55 +0000

    tune question flow picture lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
