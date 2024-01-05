1ad6ead0a286d92d58f826310385e40899513ed4
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:03:50 +0000

    wear turn law circumstance big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
