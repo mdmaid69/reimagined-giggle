87047ab69baaac4592bbe2ab016811eb27b82207
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:40:52 +0000

    bored today introduce lay doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
