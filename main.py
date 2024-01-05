e779022e29d6c1e5798d7aef9e02c765709f2d97
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:01:43 +0000

    post computer communication show general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
