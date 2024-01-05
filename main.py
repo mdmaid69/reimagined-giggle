d54d2da7c1cdd355e38acc2c1d4b63249ee7a1cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:28:03 +0000

    mistake tomorrow wish fit very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def subtract_numbers(x, y):
+        return x - y
