07dc6e279a07a0f10dbbaec02ca0f699be4d259f
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:22:20 +0000

    shopping stranger exam unit keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_length(array):
-        return len(array)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
