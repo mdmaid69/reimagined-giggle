2fecb7100a8ef6d4d4585fe42e37597e7c3ef9c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:12:46 +0000

    Jj act bit instead there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
