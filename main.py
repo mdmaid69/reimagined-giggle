6d76b83b4a4131675a42f279cf7647d52bd0eaaa
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:08:21 +0000

    obvious before radio getout style

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
