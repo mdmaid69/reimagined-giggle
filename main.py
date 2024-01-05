9672a6f44f86c7b4d15900bdfe875ac3e439c40e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:55:37 +0000

    compete true pride bottom concentration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
