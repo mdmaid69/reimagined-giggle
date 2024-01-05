966e6acb77b3f2dae96001780013fca01806cb25
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:37 +0000

    belief job emergency travel movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
