d2b9f6a00133eda1eec360630a72150c95b50ba9
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:32:58 +0000

    make sting country tour tone

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def convert_to_hex(n):
-        return hex(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
