c12d0a871ba1dc7d0d9dc878aab7f9ae62bbbc03
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:40:44 +0000

    information study Ww like lose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def add_numbers(a, b):
-        return a + b
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
