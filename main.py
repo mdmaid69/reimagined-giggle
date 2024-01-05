6b409d6be444111a1e94d869f725fd077e466921
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:20:09 +0000

    comeoff group occasion journey culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
