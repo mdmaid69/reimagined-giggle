8c84042b513607e16436e8f84cc027f4f643df5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:34:30 +0000

    ashamed listenfor at giveback surprising

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
