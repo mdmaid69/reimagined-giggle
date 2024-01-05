84f61ccc9c1a18f834298e4b25bf82ed19e61861
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:19:16 +0000

    regional dangerous fortunately project runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
