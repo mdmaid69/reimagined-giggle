fc127d40d23961cee070c18e4b45548cdaa040b4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:26:15 +0000

    claim matter us preparation judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
