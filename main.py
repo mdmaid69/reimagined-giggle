62bdd23ae6e6af604a4940fddf8117300d429007
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:45:03 +0000

    love pinch this lookback(on) affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_power(work, time):
-        return work / time
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
