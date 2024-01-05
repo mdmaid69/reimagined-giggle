e76d26d184a26395d32f6c4b0d54ddb8645804f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:21:06 +0000

    ground will refuse sale comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
