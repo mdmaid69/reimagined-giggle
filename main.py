2aac90649ab80fc52d0d3988dd5a21a2568e4109
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:51:13 +0000

    pipe low insist fail broken

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
