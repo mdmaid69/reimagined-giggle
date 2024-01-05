4dcc29ee578af9b74fa556119aaab42452a6a79e
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:51:21 +0000

    skill concerning downstairs quit creature

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def subtract_numbers(x, y):
+        return x - y
