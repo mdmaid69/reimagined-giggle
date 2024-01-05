cc95492935ffd0757650fd31e78a14b7b5f5141f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:21:31 +0000

    workout region advanced previous Bb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
   def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-  def convert_to_binary(n):
-        return bin(n)
