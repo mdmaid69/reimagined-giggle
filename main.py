692e75c32601cd12a2c47ef9519798befcacf8e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:42:16 +0000

    yours score guide what achievement

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_acceleration(speed, time):
-        return speed / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
