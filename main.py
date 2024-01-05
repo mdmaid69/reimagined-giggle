360e8f238c9b7cfb4b8568d67d860c6ed7f0f9c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:07:41 +0000

    mind thing several advise first

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
