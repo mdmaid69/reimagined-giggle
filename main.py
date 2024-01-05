ae41a91a5aa147cc7a3bdb2ef6a01cc62bb5b37e
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:25:58 +0000

    work afford to shoulder scrape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
