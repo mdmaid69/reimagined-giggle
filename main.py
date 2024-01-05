284d0f30a55828847bd62359498a48c7b751bda9
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:25:04 +0000

    activity know keepoff pencil narrow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
