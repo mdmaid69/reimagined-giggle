4de8cc27516c46f32320d3a6d7ed50dce93fd384
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:59:28 +0000

    me include behaviour police listenfor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
