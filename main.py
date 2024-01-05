a459e2b32258300344376eb7ec4fd479691813d8
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:03:49 +0000

    imagination detail prevent voice party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
