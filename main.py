03631ee671f6d71ca7c329d2fddf12844f9c6819
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:16:45 +0000

    wave about shallow pressure belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import platform
-def get_python_version():
-        return platform.python_version()
+def get_array_as_frozenset(array):
+        return frozenset(array)
