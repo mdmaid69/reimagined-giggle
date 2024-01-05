acc95d1501324048861bdfc10b81265626fa835a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:09:35 +0000

    gradually moment search into week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
