442ab9def91b9ba33c2231db2eeea0de846b43f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:01:46 +0000

    back study whole wind catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_pressure(force, area):
+        return force / area
