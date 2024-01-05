4de11a53cb1fabfba39d0c85438acd7295b67f5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:11:58 +0000

    arm either may church report

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
