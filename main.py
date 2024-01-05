18ac03013fd8a6ac0fe520b62984ddb52a26659b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:39:50 +0000

    part corner giveback criticize murder

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_string(s):
-        return s[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
