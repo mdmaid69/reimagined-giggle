7690f7a52ddf8789f705cee22b0f89f87064968d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:48:38 +0000

    loud month mouth boil cry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
