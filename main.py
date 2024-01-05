db9b485a2adc6b2dcf289d28474461941219e1f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:58:21 +0000

    pause exercise protection against them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
