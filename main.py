cd25a9fa870e0b2a19dad8c19ddec14990c16b98
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:32:24 +0000

    hide conversation reply meanwhile active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
