ece7823b52b96d9436b24451ffde7dd26633681c
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:31:03 +0000

    either into stare shake give

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
