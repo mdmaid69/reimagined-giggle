8cf6e0b7f4988ceacd6dae612226440c9dfe0be6
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:20:00 +0000

    though shake convenient anyone used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
