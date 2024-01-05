4cdb352c487dd884f228004b637e8f0dd750a138
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:28:58 +0000

    over twist turnon powerful day

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
