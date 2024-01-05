91619247d94d44b19af1599fb255840e40f92d49
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:04:11 +0000

    must replace meal believe want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import random
+def roll_die():
+        return random.randint(1, 6)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
