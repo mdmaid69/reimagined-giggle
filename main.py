3bb2996fd697e2df3a5372875048dfa49d062bae
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:56:49 +0000

    care freedom image woman act

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import time
+print(time.time())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
