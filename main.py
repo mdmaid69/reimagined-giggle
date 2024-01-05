353ea7db18f17d3125fe8aa08359593466299b17
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:25:26 +0000

    black off lonely suit so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
