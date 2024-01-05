cdf8f29cd36e42abf35dc93535f1006270409ad0
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:28:19 +0000

    include post post nerve wipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
