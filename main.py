f89a6bb13c8f565523232328f70b0529a56d4e94
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:33:05 +0000

    bank lonely port along later

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
