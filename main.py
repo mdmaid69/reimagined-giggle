9150912aa370db48b016336a29bf2e4ca92166fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:25:02 +0000

    drive check behind gas might

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
