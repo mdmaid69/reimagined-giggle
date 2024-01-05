d3015dc6bb53a4e79fa5eceecd8f6abf91388935
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:59:14 +0000

    reject praise measure drive speed

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def convert_to_binary(n):
+        return bin(n)
+print([x**2 for x in range(10)])
