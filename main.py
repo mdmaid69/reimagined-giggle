68d3f5bcb1bf8cf4b5c4cdcada5868f9e336abcb
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:44:21 +0000

    impatient environment around favour by

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
