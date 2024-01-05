35248c01a5e4a4f38b95c4c69b33ec4ecbb70526
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:13:39 +0000

    sensible shop walk carry dry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
