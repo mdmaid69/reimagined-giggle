bebd53f2fabcf16d2d01f44e80543346af9413e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:49:05 +0000

    dead turnoff respect nature fence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_item(array, i):
-        return array[i]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
