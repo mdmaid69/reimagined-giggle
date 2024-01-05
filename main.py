ab91ffd2788c82a762822ac9ede8d057713f2780
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:17:58 +0000

    proper sweet support spare whether

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
