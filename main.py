c32002fc115063de7bf59b6a854ddfc654ea3345
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:02:06 +0000

    claim everywhere illness smile programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
