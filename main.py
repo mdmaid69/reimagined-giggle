b6fda1d388c46628ade7e3dbe3ea4966298c4c34
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:44:48 +0000

    home Pp bother somebody complete

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def square_number(x):
-        return x**2
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+print(math.pi)
