e64a8097d3ec07438387eb615752ac5ca4a9a6af
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:52:06 +0000

    birthday have slice generous steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
