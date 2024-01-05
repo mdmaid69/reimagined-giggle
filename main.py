2aa5db349001a903b84f72f40ccf2d92bd4b543f
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:17:06 +0000

    create soft protection original slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
