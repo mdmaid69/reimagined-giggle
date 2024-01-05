d7aa144eebf4ca609524c8dc07fd0867673e404a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:13:51 +0000

    snow point worth eye head

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
