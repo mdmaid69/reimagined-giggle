31c698a025f85e399ff1c0f9d57383998435b429
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:37:46 +0000

    ago wave great arm division

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import time
+print(time.time())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
