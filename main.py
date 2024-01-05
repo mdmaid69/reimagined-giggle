38914fa93111a58cb553f860b550934aad6b53c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:52:32 +0000

    sad ashamed confuse belief common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
