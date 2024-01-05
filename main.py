00e7353bb9d9dc5167b1d1df1c36e4fa1323d82a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:53:01 +0000

    opposite carry contrast get note

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
