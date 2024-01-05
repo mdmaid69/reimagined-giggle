e061ddcc97e3617479275a69a8efd645b6a2997a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:36:33 +0000

    home valuable rescue feature funny

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
