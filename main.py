9f60331ed8d59f95fd1709ea2f1c90388e059fd7
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:55:02 +0000

    activity spring replace female service

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def print_python_version():
+        print(sys.version)
