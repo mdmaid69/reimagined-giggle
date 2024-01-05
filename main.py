bb861291cb5e2aa8371e4df2f83377ce394e3f3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:33:41 +0000

    port suggestion clothes persuade apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_index(array, item):
+        return array.index(item)
