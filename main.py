4734e22ff8c5fa44b8037bfed08fa76718e0e2f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:55:28 +0000

    getout disappointed upwards guard glad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
