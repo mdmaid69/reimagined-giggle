f41b2c8fbae9602063acd871c6db0d3471dc17fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:36:29 +0000

    label keen shop swimming behind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
