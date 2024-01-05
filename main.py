9f50fc5be5ee36813c91b089b279f455b59252b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:35:37 +0000

    consider bet fork tool order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-print(os.getcwd())
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
