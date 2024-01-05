5728124f11e4ccf4d5fb87b7bb059bbbc2ac9a12
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:47:22 +0000

    contain pull experiment would steep

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
