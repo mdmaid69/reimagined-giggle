3c2c632ede8d44fc1699a94b0fff4d6af68cae94
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:53:02 +0000

    goout correct shout nor yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import math
+def calculate_floor(x):
+        return math.floor(x)
