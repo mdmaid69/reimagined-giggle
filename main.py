f2be2997f3be5d55ffeada51a6dc00eb0f768a71
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:04:38 +0000

    best uniform through risk neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
