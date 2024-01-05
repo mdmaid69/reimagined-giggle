fb64e451096ba6d58bc36a5d3497e3a3b07d49c5
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:17:25 +0000

    pressure hate tap tool prefer

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
