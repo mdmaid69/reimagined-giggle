774a49213d3c131f588ea532bc227b97ada068fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:04:04 +0000

    confidence flat congratulations under likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
+def subtract_numbers(x, y):
+        return x - y
