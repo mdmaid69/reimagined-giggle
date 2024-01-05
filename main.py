e3d8b5c35df3856c6b6ba9689481e7d562568b83
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:42:08 +0000

    height weak skill label tired

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
+def count_elements(lst):
+        return len(lst)
