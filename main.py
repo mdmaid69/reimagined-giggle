6daff555df2df43e37c16e0f915fbf99f45ff7b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:02:19 +0000

    Vv time building activity letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
