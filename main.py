902180fe40cf5bd2ed69754be63f7a5924363605
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:09:30 +0000

    friendly negative nearly behind disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
