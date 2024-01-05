6ed615c3f6226d082bb31057cdb3661f423368fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:59 +0000

    decision increase course entry watch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_density(mass, volume):
+        return mass / volume
