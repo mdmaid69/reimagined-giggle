d5d2ecde44d2a28a8338269331734776893cdc78
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:26:27 +0000

    surprise fear further combination getoff

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def convert_to_hex(n):
+        return hex(n)
