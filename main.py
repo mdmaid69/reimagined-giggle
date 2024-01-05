d801f7d02174d370ba9855c95ea44c8a0c81a8dc
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:49:34 +0000

    appeal rarely wall over because

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
