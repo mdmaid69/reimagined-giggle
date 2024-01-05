a5ced5bf727fba450e076b9bdc57e3e1bda8b75e
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:17:50 +0000

    early trial towards pipe lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
