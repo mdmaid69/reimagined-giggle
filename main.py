82eeaa62c93a0e16ef9aa16144e0d6a907458218
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:29:39 +0000

    hope without plant suit love

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
