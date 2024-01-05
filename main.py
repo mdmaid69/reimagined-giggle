5d92ab53aaa2ea065de37ca3b6a8df5d2612f972
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:27:11 +0000

    power present pleased late naturally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
