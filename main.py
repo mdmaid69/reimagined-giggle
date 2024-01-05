83b4572d49c4d139cf1fa27f53f8149e0b5cdbaf
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:31:12 +0000

    choice fair emphasize minute fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
