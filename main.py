e970f610c5b0bab2e185505d7fec5ff57d1d5ec7
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:14:04 +0000

    bringback crowd fire character blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_array_as_list(array):
+        return list(array)
