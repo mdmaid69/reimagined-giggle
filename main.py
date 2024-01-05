39c45c94e644e8df7434359405b6eb2af482b1f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:19:59 +0000

    properly night smile poor OKorokay

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
+def is_palindrome(s):
+        return s == s[::-1]
