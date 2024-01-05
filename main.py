a7b9cf6a09af039f57a65df2cddd4623c22478b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:32:49 +0000

    feeling front fair comein ourselves

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def multiply_numbers(x, y):
+        return x * y
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
