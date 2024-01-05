a36a22741b353af445a4cc7b07cb21137bb18f72
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:12:23 +0000

    later special live dare confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_power(work, time):
+        return work / time
