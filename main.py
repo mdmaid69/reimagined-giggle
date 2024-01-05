61677657fde754d0ff10beaaabfc95ee24d1e862
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:27:24 +0000

    mail principle enthusiastic platform switch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def reverse_array(array):
+        array.reverse()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
