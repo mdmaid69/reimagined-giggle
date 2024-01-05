c224e5f56a68f03cb903264229e1760a3522e84c
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:41:31 +0000

    connect definite decision classroom respect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_repr(array):
+        return repr(array)
