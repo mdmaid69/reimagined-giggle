9bf715bc7cbc4be7d17072b58a6f7460dbd55909
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:58:27 +0000

    cry beginning objection because medium

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
