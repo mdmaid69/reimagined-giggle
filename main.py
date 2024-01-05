86a64a9640a1e1266d96a0df7f0799927b2e1fb3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:29:12 +0000

    bunch welcome succeed doubt exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def delete_file(file_name):
+        os.remove(file_name)
