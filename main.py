e1936fde5b8cdccc4f7abeb464474a5e49b490fe
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:38:51 +0000

    pay shopping rent cover takeoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import os
-print(os.getcwd())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
