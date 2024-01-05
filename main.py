0395ec76bb4c19ba1f1898c0db5eb51c52ecfcba
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:56:14 +0000

    once deserve sink estimate scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
