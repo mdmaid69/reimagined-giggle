3740393556e9a17e356754fd8d4e6a8ae8c5904d
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:08:42 +0000

    congratulate favour near someone goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
