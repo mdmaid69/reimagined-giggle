c0606d4af6e23a38220e62817a18abc975409b15
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:33:48 +0000

    separate top stage afternoon represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
