c0e709efd6024a1b5808182fb1e5a459d23fbd2e
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:19:52 +0000

    fly tap example conduct set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_int(array):
+        return int(array[0])
