37ea56ccb5e063980948013a231d05fb11e01414
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:05:28 +0000

    second whole release steady behind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_work(force, distance):
+        return force * distance
