61b51f8997d2b62577318a48173fb121b91931b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:12:53 +0000

    572enter far control recommend space

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
