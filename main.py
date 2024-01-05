2966361b18293313fb2fd572246dff03ebb25b2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:11:00 +0000

    number gift seat earth occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
