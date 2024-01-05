382245a87de9279cdd009a4d7bb3e4adc6ba9e3e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:04:26 +0000

    content little whoever unfortunate large

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
