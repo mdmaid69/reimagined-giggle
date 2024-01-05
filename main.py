1ef86c8f6ba1caae0bdeeb26a3641d1badc71fda
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:28:30 +0000

    moon talkback anybody lay lucky

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
