23dee7e1d8bc8338a41f896e348aee5741dae94b
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:59:21 +0000

    limit discussion pair grade breakinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
