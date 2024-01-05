03a0ee47ebe21120d60e80850b0a40c70804d840
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:25:59 +0000

    come bunch finally unite makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
