b3ba8eb4abb493aa772f82925360379cc306067b
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:10:06 +0000

    plain talkback unless correct invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
