f14b69d6d470ac3eb830eebe9ded82feb65acfea
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:23:42 +0000

    best little style colour talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
