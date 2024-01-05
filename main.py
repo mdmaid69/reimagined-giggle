c211c1b3405af6629e86b038244143bdd6a10edf
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:18:49 +0000

    funny pump sight regular wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def clear_array(array):
+        array *= 0
