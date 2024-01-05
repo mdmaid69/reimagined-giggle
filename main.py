5e845238dc79e155764f9cf5e7737a22cc9bd40c
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:58:39 +0000

    say recently hope fond crowded

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
