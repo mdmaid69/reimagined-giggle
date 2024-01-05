233a9eaf4b58c98fd6b456a7287291e79a5bf018
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:13:00 +0000

    sort hide possibility pain hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
