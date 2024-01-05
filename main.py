c4a7a32ac307cddba2b9524f212b25c831219fc9
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:11:51 +0000

    development order field pen keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_volume(length, width, height):
+        return length * width * height
