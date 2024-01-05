706b0e5f6c5fda6ae988f5746b71edf9d4a0df8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:49:23 +0000

    normal harm consist appeal goup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def add_numbers(x, y):
-        return x + y
+def is_odd(n):
+        return n % 2 != 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
