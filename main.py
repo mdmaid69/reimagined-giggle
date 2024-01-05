d394492225c72ff5c5992fdad18576e80670d335
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:45:47 +0000

    obvious takeover please contribute defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
