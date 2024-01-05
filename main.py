9aa7ae4c1df8428becbbc82140e8a5e0209e2031
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:22:21 +0000

    communication every thin answer across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
