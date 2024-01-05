fee4fad902574d76207f459db6d087dd6ac9d3d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:00:11 +0000

    point early admire sale satisfied

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
