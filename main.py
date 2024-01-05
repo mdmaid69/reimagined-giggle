ae87c3962fec82677caae9e71a9371ef244383ef
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:13:54 +0000

    slow region into other tense

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
