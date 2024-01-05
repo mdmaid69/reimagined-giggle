9d705fd09808bc6526c31e6fe349e06abc69fbf4
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:31:37 +0000

    talkoutof criticize collect own play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_index(array, item):
+        return array.index(item)
 import random
-def roll_die():
-        return random.randint(1, 6)
+print(random.randint(0, 100))
