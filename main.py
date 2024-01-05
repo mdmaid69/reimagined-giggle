66dec513632dd9f971ad890bd869470e9224b973
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:48:55 +0000

    strange protest left characteristic favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(x, y):
+  def add_numbers(x, y):
         return x + y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
