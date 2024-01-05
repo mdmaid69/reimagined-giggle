2903b8c98f0501e5028a272c0d52bdcf9d5f96e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:25:45 +0000

    actually children injury action record

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
