589e9a73180fd37e7b60141f44ff7852b068b0c3
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:22:02 +0000

    about rough cover moon die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import numpy as np
-print(np.array([1, 2, 3]))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_itemsize(array):
+        return array.itemsize
