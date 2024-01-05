6fa1c2d8384fd16a48078b74d9e8756dfba030f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:22:58 +0000

    female yesterday need object number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
