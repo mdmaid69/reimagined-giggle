6cb425ebe9fe405e198c4652fc20e372764b457e
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:52:23 +0000

    raise wheel artificial interview mood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
