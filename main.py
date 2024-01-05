c55e77524b7b5c01a1e9bff75fb0d3d3202506fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:32:03 +0000

    ordinary interested visit male common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
