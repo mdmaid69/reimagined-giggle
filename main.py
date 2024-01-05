a25e555c0b68cc2260f424bf027d620bd4b18e02
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:53:50 +0000

    extra far century passenger frame

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
