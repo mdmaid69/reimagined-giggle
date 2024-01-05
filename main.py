297674cf4ff3f58df1522df7b6b1c3aef8c1aa75
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:19:55 +0000

    achievement creature actually muscle indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
