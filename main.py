e4e5c684f072244d715bdd018042b568f2c77008
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:34:58 +0000

    design near die definite organize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_power(work, time):
+        return work / time
