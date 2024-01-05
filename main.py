b639ca64faeab50cc3499f27272f7459543571e3
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:16:39 +0000

    top Yy professional dig desperate

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
