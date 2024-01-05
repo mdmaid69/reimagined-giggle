05e4842a727a4392078e0e37320c17926a678522
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:23:47 +0000

    near pure surprising system live

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
