be95895b6636bc286889be50b2539f26248d176a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:55:44 +0000

    abroad quick splash holdout leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
