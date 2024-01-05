6952fe12d36aca78e74f65666b3b57afd9b1ffa3
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:38:16 +0000

    away problem write breakthrough stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def multiply_numbers(x, y):
+        return x * y
