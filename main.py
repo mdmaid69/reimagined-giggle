621f7cf322067d9726eac44cc6e31fab9891c9e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:05:51 +0000

    year crash official temper expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
