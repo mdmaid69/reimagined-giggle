5feba438c9553d448980842f36e1676f83f9502c
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:17:21 +0000

    case sing 565end Zz listen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import collections
-def create_queue():
-        return collections.deque()
+  def subtract_numbers(x, y):
+        return x - y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
