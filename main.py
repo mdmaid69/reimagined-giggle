52a31ab87f252571aa3005ff3373f4f52df83b7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:50:03 +0000

    fall scrape gather touch path

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
