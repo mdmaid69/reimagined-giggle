d6d6fc8df124122490b1df3c8e8f56607b614c68
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:50:39 +0000

    list cook plan constantly smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
