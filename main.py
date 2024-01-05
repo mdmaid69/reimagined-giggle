e37f8a5d3297715b7c0847412564d09c5f13c0ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:42:47 +0000

    software wander plenty business understand

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+print(math.pi)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
