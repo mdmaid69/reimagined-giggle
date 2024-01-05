26a6ea09e6ea48530e93777725cb356748e03237
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:28:28 +0000

    ignore experiment much dare substance

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
