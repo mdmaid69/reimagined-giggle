a368933f5355bceb999b4aa26f98e22df1d9b14d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:12:52 +0000

    now mail usual contribute bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
