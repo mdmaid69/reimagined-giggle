8f3fac71df412ec57b0d09e64478746aaf467c9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:44:47 +0000

    neck hair safe unusual nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
