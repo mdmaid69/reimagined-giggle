c7dd349d038beb595fd81a9094f6654cd0c17f86
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:14:13 +0000

    plant possibility arrange lookaround face

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_palindrome(s):
+        return s == s[::-1]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
