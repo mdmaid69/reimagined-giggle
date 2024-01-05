620f221cd887c05a92d4afd25c348e94b4b159f4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:23:40 +0000

    drop book justice basically common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
