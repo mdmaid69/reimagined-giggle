3e3e485cf4eba9bafa328e2114b2460b495d2f79
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:23:26 +0000

    point genuine emergency depend nothing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
