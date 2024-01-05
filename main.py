0c3f948f58caed8b9a68f7d705000f507c5231a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:31:09 +0000

    patience narrow trap little actually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
