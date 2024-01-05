ede807e419f45e79793df9966eda6f93e2e4fd02
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:37:36 +0000

    pleased juice steep further physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
