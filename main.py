34b0aba29e5831cbc22f6a5c217d0db53e4b3df2
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:35:43 +0000

    town create answer cheque suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
