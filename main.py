c78e084aa17134ff746ed4f817fd91cc82b07401
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:02:12 +0000

    community really wise independent prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
