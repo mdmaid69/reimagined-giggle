eec9f56842c83b8814029a883a5373dc4dcfec67
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:31:51 +0000

    agree right could characteristic forget

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
