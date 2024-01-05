baa71b399477ca88330e2715272370ef54c11dac
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:45:21 +0000

    hate arrive plenty later middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
