ef8a85c11cf7e3ea1863d8636721e255eb93bb2b
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:28:15 +0000

    exam wake contact send duty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import random
-print(random.randint(0, 100))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
