01a18bf9f2806f47b956fef9674c1445d1e36a08
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:21:40 +0000

    strength lock fierce comeoff snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
