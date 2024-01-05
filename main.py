0267f3648bc266944f7ef331cf5d431f40fdb702
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:54:55 +0000

    sensible suffer gap survive tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
