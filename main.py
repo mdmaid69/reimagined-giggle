3898e0993c0f29ad940774720009028cca469411
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:30:28 +0000

    apparently greeting brush turninto better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
