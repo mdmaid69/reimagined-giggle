2761fe3066c03d69070c9cc2367d04fc83728069
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:55:15 +0000

    whatever alive suit new aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
