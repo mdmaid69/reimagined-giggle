43410912b14bdfef874f657fbf6b15ea0912ba7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:55:12 +0000

    pot there container than only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
