4493309b194cd691a4557b39a65920a42fa2ffb8
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:28:39 +0000

    come means line negative alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
