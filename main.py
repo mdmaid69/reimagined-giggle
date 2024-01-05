9555d48234570ea4bf4af4e6be99eeebc25e3c82
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:45:09 +0000

    blow apart pinch dress still

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+print(sum(range(10)))
+import array
+def get_list_from_array(array):
+        return array.tolist()
