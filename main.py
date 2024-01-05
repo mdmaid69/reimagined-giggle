04b045c6244e57b497feeb11d8b869947abe1c40
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:23:27 +0000

    discussion satisfied back Ii development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
