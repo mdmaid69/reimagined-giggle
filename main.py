e6789c4403f19b1df6ea5772aa3e745f613c615c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:44:50 +0000

    sentence respect sweet boil I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
