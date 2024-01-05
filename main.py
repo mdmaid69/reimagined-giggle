a37b431bd6fcbb724eb1cd05e521344d5702b412
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:06:46 +0000

    fat profit pair alive what

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def cube_number(x):
+        return x**3
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
