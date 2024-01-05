312d111a8568a4fb743509ab03048d4d9b08e50b
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:08:19 +0000

    happy taste embarrassing reasonable tend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
