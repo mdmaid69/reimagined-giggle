7b26b5ec4d6abfb4d34bbae4f52387a1894b2887
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:07:04 +0000

    sweet store acceptable form though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
