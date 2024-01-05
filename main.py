fb8174ceac9e3d5fb6d8bf71f053fa1681537b00
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:13:52 +0000

    water water hard praise event

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
