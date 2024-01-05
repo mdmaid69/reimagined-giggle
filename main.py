0c2888d6ef6e8158bf1d26105c2f274bb7ce3a90
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:02:15 +0000

    description with top contain position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_acceleration(speed, time):
+        return speed / time
