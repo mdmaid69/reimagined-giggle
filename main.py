9d9baf9e45c8405cdd39405a83f65d2905a17518
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:44:31 +0000

    safe far community confident comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
