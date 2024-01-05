0385e65b1bbc3d739c87d93974ea67b39556a11a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:08:13 +0000

    gap connection otherwise before base

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
