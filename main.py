412edc6c3b931ac75e23f8b5570417098b3322f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:28:04 +0000

    less ought rain waste pile

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
