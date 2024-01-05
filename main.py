a31c897c10ffdc49a63e67cb3c70a60554de8c0d
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:47:51 +0000

    warning close genuine thankyou lucky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
