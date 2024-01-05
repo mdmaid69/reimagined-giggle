65ccb8fcb15ec396b73ef98453e0917a799ddd01
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:14:18 +0000

    whole neither agree write point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def find_max(lst):
+        return max(lst)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
