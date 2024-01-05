b7ac694cccf8d74476b48823301588e62a64252f
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:37:33 +0000

    must connection everybody suckme slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
