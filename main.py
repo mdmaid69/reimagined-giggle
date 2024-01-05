d5b19ab2295c6f58e344698df97def9e5df6e8f5
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:17:15 +0000

    sad sting about equal material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_current_working_directory():
+        return os.getcwd()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
