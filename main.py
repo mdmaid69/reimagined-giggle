7ba52c287e38c7b6b303f069fbb6be692eba95bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:33:38 +0000

    intend system all go uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
