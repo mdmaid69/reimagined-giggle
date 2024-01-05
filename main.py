0bf7a3f27c52403fff9380b1c2eadeaa6b1437d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:38:21 +0000

    computer half row scrape wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
