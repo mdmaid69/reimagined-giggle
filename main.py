3305508a56e1142e03639820f0f8887a356dbd88
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:57:39 +0000

    she question return development silent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
