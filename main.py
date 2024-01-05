462a29f7f4177ba05309238a43f7136763f92894
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:28:37 +0000

    swallow boat yourself basis mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
