8579fa8cb0223bb58d7abccf2520fd14fb884847
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:31:07 +0000

    sing wind sit discover clean

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import datetime
+print(datetime.datetime.now())
