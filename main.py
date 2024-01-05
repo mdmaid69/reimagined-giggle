724cfcfb9915da289d4551f16579bc0d4ab4ec7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:30:17 +0000

    hurry material ours evidence steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
