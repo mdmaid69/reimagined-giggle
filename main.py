e36beea37612f2bc0a49c97565b6a4b810bdd596
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:48:34 +0000

    apologizeapologise bath force danger scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
