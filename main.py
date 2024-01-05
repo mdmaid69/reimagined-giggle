fb89b3a2932e3beefbdc9402d8004f9bad65f5ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:23:28 +0000

    count opinion organization prepare strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_queue():
+        return collections.deque()
