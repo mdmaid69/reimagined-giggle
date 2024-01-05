849a03a51270b32d578fc458d430b31fbdd124ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:44:44 +0000

    may stand contents reaction gap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-print(math.pi)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
