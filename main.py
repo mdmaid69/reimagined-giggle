4fa7d933850992c8674b1dfd99c2e9586177a7fd
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:14:59 +0000

    communication commit around suit punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
