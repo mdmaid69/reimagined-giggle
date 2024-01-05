18d098dce3dbf0a0f50aee6d98f5b7e907cba6df
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:31:23 +0000

    direct steer pay shock embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_area(radius):
+        return 3.14 * radius * radius
