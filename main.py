a7b534121b7b28f9c48620a321340bb4f4197d4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:03:02 +0000

    sky practise once to favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
