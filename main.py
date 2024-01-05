446a7e6b463bb5d385873a6fd1f82bddebe337a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:53:12 +0000

    project callup city failure seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
