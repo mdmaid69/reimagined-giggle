21cdcb735e9b8cbbf2b76bedfb46b6723638e1ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:55:04 +0000

    back its smoke grow schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def count_words(sentence):
+        return len(sentence.split())
