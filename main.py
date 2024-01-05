85fbdd7842c4c60e6d2e26209b0390c77b91f99a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:17:25 +0000

    trip almost war need amount

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def cube_number(x):
+        return x**3
