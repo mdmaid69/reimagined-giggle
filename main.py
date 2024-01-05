09458da4e2476fc6ec58cfd49e6b12f287e8c23e
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:32:59 +0000

    mess name effect cut flat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
