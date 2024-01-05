53fc0c4e8d46492f4104a3e35d389cc550926a0b
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:39:44 +0000

    happen smell Kk stiff fear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
