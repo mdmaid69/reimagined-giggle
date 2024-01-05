17e0388ad41f012f914e40b469c74fc31fd970e4
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:28:23 +0000

    sink impression anybody or top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
