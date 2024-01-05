d8a5b0d3bf0d6143f203acd92cd93f567b0855cb
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:04:02 +0000

    space work react waste account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
