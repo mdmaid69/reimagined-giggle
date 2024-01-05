14bcf533567961baeecc8f775f01e2b21dd57cff
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:56:22 +0000

    count warning independent force improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+def get_current_date():
+        return datetime.date.today()
