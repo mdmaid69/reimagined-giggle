5999e0bbf5302f0a7a2c0eed9f6b15bd0fa9947a
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:33:16 +0000

    thankyou fit could nor motion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
