f4d7e8a4b75a9f2cca0ba5d32bdf4200ad3b02c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:21:40 +0000

    form order cut other fine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_array_to_list(array):
+        return array.tolist()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
