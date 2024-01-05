d0b3627414ace7641f519b1f047fa508f40ba635
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:20:55 +0000

    likely interest bath favourite previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
