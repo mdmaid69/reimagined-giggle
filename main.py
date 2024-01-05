fde36df8785f5050136f0408e939422616b69692
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:14:21 +0000

    cost ability unhappy keepfrom anyone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import platform
-def get_os_info():
-        return platform.uname()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def convert_to_octal(n):
+        return oct(n)
