0be0e12e26916ed7dcef79196f117c70999abafe
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:06:16 +0000

    near urgent sound please hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
