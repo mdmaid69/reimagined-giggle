9e32280972ac6f883917c892e013c4dc6b93643d
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:44:15 +0000

    comment fear safety take on

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
