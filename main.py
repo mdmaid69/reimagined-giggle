72553ef6cc0464a42fd73e6f9d32f32ced42dee9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:26:25 +0000

    dinner far part Dd clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
