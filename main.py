309cc65d3fd6288a135ed801f03510dc5af3bd28
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:51:30 +0000

    satisfied degree pressure crash aim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
