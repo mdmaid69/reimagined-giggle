4605cf91449ceb8e30a86db4570ae4e45e492a55
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:51:33 +0000

    board accommodation violence bridge meeting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def add_numbers(a, b):
-        return a + b
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_str(array):
+        return str(array)
