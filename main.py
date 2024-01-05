367bf563ecf21c32b899b20c51016dbc0a03a099
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:40:38 +0000

    border several spring comment steal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_volume(length, width, height):
-        return length * width * height
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
