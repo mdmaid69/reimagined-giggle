edd5de1558a62e73c1266005f11c65a74da21b4f
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:00:51 +0000

    that can fork float power

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def append_to_array(array, item):
+        array.append(item)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
