e72d373332ee73beac0ff859d4e05beadc8078f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:12:42 +0000

    guess sore trip rush dance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
