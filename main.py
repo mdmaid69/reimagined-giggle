f66eedc49772b3aedbfe57dfa1fd1e73b7c3fe9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:31:15 +0000

    urgent less news sit affect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
