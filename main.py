dc5f97202626248a72adf3d4015f365b2e0afc6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:25:49 +0000

    flow judge centimetre lock mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
