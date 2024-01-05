2fa4faa3186e088a3993c26bf71b9b30aa1ef732
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:05:28 +0000

    war breakin conclusion wave entrance

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
