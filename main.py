150aa973b0f94757912a8058f11831f2502f6520
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:28:41 +0000

    impressed supply progress sting married

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
