3dabd6d099e1d9e33c9e3aa745dda4db0b032acb
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:24:28 +0000

    strength tidy comeoff long fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
