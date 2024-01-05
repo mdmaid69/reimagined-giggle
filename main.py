11c83bfce070e99ba1eef5d89af2a5f2641feefe
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:18:44 +0000

    somebody Nn subject suggestion journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
