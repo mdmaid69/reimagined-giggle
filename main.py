d7cb478250a43d7d68e42b74cdb0bb9f303ae947
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:09:55 +0000

    related probably goodbye game judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
