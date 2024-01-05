85b02bec1614ebf888a4f2342ee6956e4f4c2f22
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:08:14 +0000

    564encourage comedown comeover comeover scheme

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
