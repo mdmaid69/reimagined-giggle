803a46f701fd78a81fef4b20b37f184a4f1f0884
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:01 +0000

    reasonable every apart free Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
