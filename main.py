8883bbcdbda83180ccdd2494a6a6e295e6d4b5bc
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:34:23 +0000

    prove moment dream fight myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
