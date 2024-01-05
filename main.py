6d723297fb0009e52dd18b9072db93aafcffecf3
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:23:00 +0000

    label mixture comfortable standard tool

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
