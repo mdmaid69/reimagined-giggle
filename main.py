6214c7c559c23d072993f7e57b5c9e97b5f62de5
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:17:44 +0000

    summer brush safe freeze violence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
