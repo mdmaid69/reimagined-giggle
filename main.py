35231dac9bd449fd9dcb6b57d3a2c85ba7716ee7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:31:23 +0000

    sail more weather drive wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 def calculate_speed(distance, time):
         return distance / time
