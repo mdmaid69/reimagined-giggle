e031f2ed2b102ac60768f08229a81e707258fe19
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:15:06 +0000

    school open familiar warning also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
