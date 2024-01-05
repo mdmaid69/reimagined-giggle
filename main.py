370483ae4c5f0c880bb6bde32b3c4a8f2d7f2732
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:32:23 +0000

    top prepare afternoon chase accidentally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_work(force, distance):
-        return force * distance
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_length(array):
+        return len(array)
