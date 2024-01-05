2430c5e7b7b0bed454ea2b7e479c354f8c753c91
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:10:18 +0000

    way beneath wealth basic turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
