af4d63d954c281aa121873fad0c2e67a2dd6e684
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:50:16 +0000

    Tt Vv justice scale work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
