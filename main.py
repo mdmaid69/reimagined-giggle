0d7c6a65f7a115c01c0e5730a9160dce59853353
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:34:08 +0000

    swimming path keepaway rain pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
