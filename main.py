07cc00cf13355aaaf04d3cbc19d14cf973cd9679
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:31:14 +0000

    direction such just agreement picture

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
