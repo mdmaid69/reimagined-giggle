bf2d927f47d07ea22ea3299abf4ec74fd8451bb7
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:56:25 +0000

    great hardly behave total female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
