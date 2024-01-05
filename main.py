0ca4160c79cf6a592aacb1f3ddccd7caeaad247e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:17:57 +0000

    hurry invite immediately over order

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
