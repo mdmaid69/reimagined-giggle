1b763cae8ebd4decb9182578c1edd4a377632ad3
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:52:55 +0000

    complete suffer forget wash person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
