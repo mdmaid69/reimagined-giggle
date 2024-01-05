601e867dcabf2c59a98337496b16dcb6be7041cf
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:01:45 +0000

    splash arm shape across urgent

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+print("Hello, world!")
