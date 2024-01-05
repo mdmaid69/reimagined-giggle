ca98a5b61df99b619b14b2148bdacf6889c40943
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:56:45 +0000

    scene left instrument fasten normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
