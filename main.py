abc04b2f9dcad89e139eae6fd8fccc0a8718b180
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:56:04 +0000

    thirsty confused help right church

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
