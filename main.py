bb176deef58951adf219b3210a4f7199a475d279
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:40:21 +0000

    lesson generous tell brain drop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import time
-print(time.time())
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
