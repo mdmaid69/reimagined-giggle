ec70871c40028040c3a0923192038b01a6467d4b
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:34:52 +0000

    our tense safe even tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
