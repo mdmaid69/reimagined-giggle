b5dec532415e589a848d0c08b8b5e2bd21165d6d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:13:31 +0000

    vote feed ball born discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
