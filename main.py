db07669cc811aa504b9790f7ea67edd7c0cdade3
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:24:30 +0000

    surface apparently respect explain declare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
