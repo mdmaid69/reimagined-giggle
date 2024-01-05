a33cc71bfb03a456cf32321c852e999909d3392b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:25:02 +0000

    give whom lean mend tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
