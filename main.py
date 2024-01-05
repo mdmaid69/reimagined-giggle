951f18a76423bbea7bcc2a1615ef35787fb6981f
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:17:16 +0000

    probably scrap carry still soak

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
