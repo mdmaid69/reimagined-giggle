8a09326afce072e8cb557520ec302a5383c4fd83
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:02:24 +0000

    slightly least least uponon always

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
