07286e594aec96d6919ea728b9e280eadc148116
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:49:32 +0000

    fire quiet surprised they bang

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
