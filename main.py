970f5a33b7196ae43e44e80856118c215a05a970
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:42:54 +0000

    connection calloff pen division possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_complex(array):
+        return complex(array[0])
