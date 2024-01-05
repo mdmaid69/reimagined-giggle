eada8b8034157459ecba92966e3dd07a67d1194f
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:23:50 +0000

    object bottom age drink performance

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
