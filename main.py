6d82ab460e6cd27cab151638a187a31552304204
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:50:07 +0000

    later pattern smart quantity rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
