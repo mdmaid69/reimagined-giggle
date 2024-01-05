6744368c78af00a9c3f00441e36fb22ee39646e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:23:22 +0000

    I lookafter fold relative hold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
