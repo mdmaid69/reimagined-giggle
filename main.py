28f25ff0528c5684b47029a497bd302cff29917a
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:20:41 +0000

    spare active flight month safe

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def square_number(x):
-        return x**2
-print([x**2 for x in range(10)])
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
