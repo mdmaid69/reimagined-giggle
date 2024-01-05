70f9fb37660466ae9ed740e2736e5f9ccb7b9e4c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:30:38 +0000

    attractive smooth holdup fierce beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
