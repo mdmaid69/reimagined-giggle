48262dca08fd7243e89f4cb7114dd416e58ef6a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:10:53 +0000

    dinner influence Gg impressive fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_itemsize(array):
+        return array.itemsize
