c630149ad1b2db3d08f6220f1dfb189389e79a80
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:13:53 +0000

    sweep emotion hardly patience consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
