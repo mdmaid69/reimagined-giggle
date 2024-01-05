786dcf71d287ba08c98b107f1a3984bc7054dd31
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:43:42 +0000

    so cheque strike cheque they

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
