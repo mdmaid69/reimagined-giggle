63aff50523f495e049398c92f9414ae1faaa9149
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:14:55 +0000

    argue cope lock appeal blame

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
