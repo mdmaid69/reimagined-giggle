8a78ddde4a81a3835ca0d23cf12d6a11ae846f23
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:33:53 +0000

    charge just tone depend furniture

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
