3cec833bf428a6d7bab8a255b5907a3f37b24386
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:50:32 +0000

    sky rain pin brown have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-print(datetime.datetime.now())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
