eb1791724b6620deb75069f2cb99b509283228c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:41:48 +0000

    mind pleasure mine eye by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
