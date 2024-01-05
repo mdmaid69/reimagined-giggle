256b9e3754fcbbf895d6fb14f514f4058c6237e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:48:27 +0000

    distance adjective die godown perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(10): print(i)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
