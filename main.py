b2e2a7de3671780ab5b8855698a8f1619f5d2cec
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:48:45 +0000

    godown wake instant follow or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
