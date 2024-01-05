58dc9392cb8430f4ba1e78aa53ac2f313ca06005
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:54:29 +0000

    organize strength murder live shout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
+def reverse_list(lst):
+        return lst[::-1]
