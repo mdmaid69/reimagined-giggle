50102e50463d13ee47c6ad39a3a88328bcf2a81b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:22:57 +0000

    change without steep interview full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
