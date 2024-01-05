a0dad9ff43ba893df3bba98a854bd1cfc86f1989
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:17:55 +0000

    fix blind tough Ll bit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def remove_duplicates(lst):
+        return list(set(lst))
