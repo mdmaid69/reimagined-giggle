4d07a1e81ac2a11cc5afd42e8f68003a7b7de262
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:02:05 +0000

    regularly suddenly connect together sail

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_pressure(force, area):
+        return force / area
+  def remove_duplicates(lst):
+        return list(set(lst))
