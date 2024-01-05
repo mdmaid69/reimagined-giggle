43671e87a7294db3ee9753d2e0f757f010a9465f
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:01:22 +0000

    card gooff rid discuss cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
