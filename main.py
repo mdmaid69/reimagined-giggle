39165edafec75946912449e9e6134f4ad818ff57
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:39:20 +0000

    wing damage once person fire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
