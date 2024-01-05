6a237985b862a8832a8bc608b8de7b02f1ce38ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:04:41 +0000

    declaration bend ignore content independent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def reverse_list(lst):
-        return lst[::-1]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
