a26fd91e0a60a09a922b05deda0bb90ca26834bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:18:34 +0000

    yourself exhibition produce dear that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_typecode(array):
+        return array.typecode
