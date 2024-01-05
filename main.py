fa43076100852955fe8b6d8f2524bc27e7aa8ba0
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:35:21 +0000

    before considering unhappy share himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def reverse_list(lst):
-        return lst[::-1]
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
