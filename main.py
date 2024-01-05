2ecabb710e405c740e05e0ce37f0b0cb5ece55c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:58:23 +0000

    Gg stiff letter 179bite accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
