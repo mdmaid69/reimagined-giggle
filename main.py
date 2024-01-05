46965cb628450e4ca4efddcaef185f671f217615
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:50:08 +0000

    traffic refer thinkover mark injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
+import os
+def remove_directory(path):
+        os.rmdir(path)
