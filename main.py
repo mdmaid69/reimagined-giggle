088588efc0280580efb836b9e6b99c830e4abb9d
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:59:16 +0000

    brush its many admire lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
