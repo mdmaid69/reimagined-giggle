f4f6dc60de9fcd73c39388fc15d650c29d963310
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:17:18 +0000

    yours bringup runover central soil

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print([x**2 for x in range(10)])
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
