bffb711b0fc0521fc9ea12655424603e308c3a19
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:06:37 +0000

    bye excuse both deal deaf

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def convert_to_binary(n):
+        return bin(n)
