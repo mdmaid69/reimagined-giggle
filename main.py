2262aec620c8f542128ce03b521e0f600b52d2cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:28:23 +0000

    narrow can kind the combination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def find_max(lst):
+        return max(lst)
+for i in range(5):
+        print(i)
