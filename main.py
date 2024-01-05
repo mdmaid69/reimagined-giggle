10475d1eff9c368b83396339d43a8af5056163a4
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:36:21 +0000

    exact hour remain Itself bush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+print(sum(range(10)))
