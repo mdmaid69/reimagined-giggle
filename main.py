bc1971f989ef1735f2b68158e34de2f7c37a58d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:34:11 +0000

    middle represent breakout occur list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_speed(distance, time):
+        return distance / time
