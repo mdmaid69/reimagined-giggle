19b57bc8e8311fbdf1b6f7eff1d2ea804999118e
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:55:13 +0000

    exist table clothes flood turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def add_numbers(a, b):
-        return a + b
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
