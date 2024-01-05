b659d81d7f7edb6158433dacf999c9a6dc2d4996
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:26:19 +0000

    habit female funny open vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_directory():
+        return os.getcwd()
