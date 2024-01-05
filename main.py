b51dac2d16bde99f48856c9baa60fe59eb440585
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:20:07 +0000

    sure disagree city fear fence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_power(work, time):
+        return work / time
