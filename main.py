f180b8fc38eeb4a80d733069d0f75fdfc72120b9
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:36 +0000

    walk reply illness seem health

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
