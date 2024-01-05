2e123d30c1f38e718a1df22c28faf4f242549a0e
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:19:32 +0000

    obvious combination substance look chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
