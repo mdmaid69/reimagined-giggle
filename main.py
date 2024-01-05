7f140cbcfcc88bc3b1aecad9e76e7da5be44e5a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:08:00 +0000

    tell strike between Ii standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
