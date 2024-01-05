6e91a1a5b05fdac046b42b40707e85bc01112c2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:41:54 +0000

    order piece top strict develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
