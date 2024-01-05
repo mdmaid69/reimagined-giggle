a231717be4b0b310bbb85065686a93efa49d2a46
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:11:11 +0000

    hardly service Ww interesting independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
