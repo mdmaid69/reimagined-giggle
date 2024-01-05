6cb67a143348d9ec851279fbc9e329df26bac7d7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:39:26 +0000

    callup Kk yours question original

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def square_number(x):
+        return x**2
