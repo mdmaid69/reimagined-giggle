8a6b5a83fd1e312ce69b39d08c5b2e7cf7db9700
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:26:07 +0000

    estimate appeal have mess uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def append_to_array(array, item):
-        array.append(item)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def insert_into_array(array, i, item):
+        array.insert(i, item)
