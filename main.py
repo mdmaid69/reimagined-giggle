fc5240ea7de5816e518a73d5a690cded6c5f05cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:30:46 +0000

    treat bet plant tend art

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
