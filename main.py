e0815301a2d12d2c52d0866a7067c0215e65b0ce
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:19:21 +0000

    comeout situation attach unfair pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
