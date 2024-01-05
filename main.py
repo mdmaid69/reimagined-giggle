ecb74a9a3e55ac8c3c72271078d0f5d61756ef3c
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:16:48 +0000

    bang history charge hair ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_max(numbers):
-        return max(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
