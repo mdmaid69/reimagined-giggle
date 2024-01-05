05be8aa0815e831991b9b4a9db04f84f011c8074
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:55:15 +0000

    dress accidentally charge touch far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
+def count_words(sentence):
+        return len(sentence.split())
