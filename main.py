fd766dd23fecc48fe9eb3254b8fb060489f5be56
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:26:19 +0000

    voice all relation great speech

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
