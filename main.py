add0cb096bd8f2da1725bbee80555a801144e880
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:33:25 +0000

    market let exhibition wake port

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_length(array):
+        return len(array)
