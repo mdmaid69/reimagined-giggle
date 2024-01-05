168e872049413e130e6e155f2d8cb7613dae5c39
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:01:46 +0000

    small whisper seat benefit their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
