cb590f87d50a2f6b8defb289bb1026533f898ee1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:37:18 +0000

    slow fold wonder thick concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
