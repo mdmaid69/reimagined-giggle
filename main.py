5af4cea562d433c0e60f76d6ae2ca452ad150346
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:32:43 +0000

    cough fast Kk joke 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
