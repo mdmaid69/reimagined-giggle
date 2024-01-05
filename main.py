65ec796528ae3237e1164ff9a4380b934da4f597
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:49:16 +0000

    wonder Ll pour part great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
   def get_file_ctime_ns(file_name):
         return os.stat(file_name).st_ctime_ns
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
