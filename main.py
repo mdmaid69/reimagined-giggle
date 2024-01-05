36668f9d0479108eb83b092de2b0e60ea2411c54
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:31:10 +0000

    ilovedicks accidentally classroom trade bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
