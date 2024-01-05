687264fe395533b79c824d6d7446810e40638291
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:38:30 +0000

    excited expensive save owe match

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
