ee5d01c99965701d3eb8a09a7b050d89f8e5f6a3
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:59:35 +0000

    present narrow rare possession ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
