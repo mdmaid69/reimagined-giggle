7c067f6147f31051e216371f31c08a6fd289f540
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:02:23 +0000

    peace how hurt takeover please

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
