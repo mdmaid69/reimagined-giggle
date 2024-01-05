f3cfb8e86967d60a96629581f24fd48d0e82a4f8
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:39:18 +0000

    expensive bound butter clearly frame

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-text = "Hello, world!"
-print("Characters:", len(text))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def convert_to_octal(n):
+        return oct(n)
