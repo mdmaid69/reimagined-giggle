75a0ba34759eb8f8c7195db74dbfc52264c06498
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:27:51 +0000

    regional condition cross peaceful knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
