8f5fc6f6495351f59e0914efb5b17fe2bd40501a
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:50:45 +0000

    will official sort shop difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
