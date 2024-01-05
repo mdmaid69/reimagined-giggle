a9cf7c3647e2a4acd496810d36b4bd668e543006
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:17:17 +0000

    makeup performance run callback control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
