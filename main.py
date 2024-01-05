4efba1fb965e432eb598d0473de1801a58ad9851
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:18:55 +0000

    space spite we tidy look

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_odd(n):
-        return n % 2 != 0
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
