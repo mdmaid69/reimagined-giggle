38231d547620f7d8637568c91abeef388997074b
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:08:11 +0000

    team organization label sudden willing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import sys
 def print_python_version():
-        print(sys.version)
+        return sys.version
