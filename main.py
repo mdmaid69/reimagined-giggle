0e7e87203fc879f4c331dd59b98e92350a0004c6
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:42:45 +0000

    gentle niggaacademy past Vv downstairs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
