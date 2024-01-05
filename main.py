aae3034786ae1c52efdbdc84b82a00a42c450635
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:50:30 +0000

    anyone study table independent century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def find_union(list1, list2):
+        return set(list1) | set(list2)
