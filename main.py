ac72ca6081ef079d63b27fb2f2179c943386692a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:38:05 +0000

    OKorokay long out study club

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def divide_numbers(x, y):
-        return x / y
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
