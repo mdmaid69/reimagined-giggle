404fdbd81b927adf8ad021f923518b5917c9e228
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:26:02 +0000

    generous waste benefit wind thought

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def count_elements(lst):
+        return len(lst)
