5128f5f459eb32ca44def74d9d83cba1585a9853
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:41:46 +0000

    calmly wave attractive idea objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
