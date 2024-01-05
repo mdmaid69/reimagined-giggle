cc910f1bbf547b626beb18c086194fe24c16cbd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:00:53 +0000

    concerning rubbish innocent comefrom strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_current_directory():
+        return os.getcwd()
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
