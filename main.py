48c4bf7b11787d70f8524f8cb7a768cd5979c460
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:28:42 +0000

    room trousers cross contrast eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
