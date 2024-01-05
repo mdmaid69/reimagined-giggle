9add773f34b69b61b2a4335efe0c1bf9bbb64756
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:52:10 +0000

    balance hurt surprise pot kind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
