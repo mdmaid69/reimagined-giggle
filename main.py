7f18ad5e0508f2966049135b25adae0082e72665
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:07:19 +0000

    compete maybe 569enjoy meeting skirt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
