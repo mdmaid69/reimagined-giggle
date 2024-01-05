6516299a5673d705975dc38035e98c6185dcfa8f
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:05:39 +0000

    advise unlucky on joke fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
