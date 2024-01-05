108a52a2da7e00c7f067bf37691db2ec70383308
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:09:02 +0000

    drawing determined human corner polite

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
