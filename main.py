3264ba0c3f73283b4447fa73605de5d02598c176
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:03:29 +0000

    note small react exit risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
