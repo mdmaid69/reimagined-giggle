e875d9b14234914781a388bde84cad2664f0cbc7
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:56:08 +0000

    thick discussion moon visit increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-print(sum(range(10)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
