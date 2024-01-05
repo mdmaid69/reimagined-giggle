eaeee2b7289f39e5b080c775490950fb5c48afd7
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:33:54 +0000

    walk standard drop contents hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
