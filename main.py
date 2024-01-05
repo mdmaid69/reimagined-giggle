b4129f6165f000bf0c98e02e442a6b49ec211df2
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:52:09 +0000

    instead drawing thin difficult mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
