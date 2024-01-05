ab7fe4f141f52d6a67a40058e2a008e25f513b7d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:12:40 +0000

    closed chin shoot gooff town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_bool(array):
+        return bool(array)
