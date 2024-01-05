78515dfc63a14c8c231d68847aa73ce63007d9d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:29:31 +0000

    enthusiastic always begin takeout wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
