2cf79d1799770aad728362bf96910af08b850185
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:45:00 +0000

    calmly thumb talk give shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
