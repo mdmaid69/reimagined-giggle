9599fb7cff31577901d4b401d7546baaf3d624df
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:07:07 +0000

    inside market little floor as

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
+  def is_odd(n):
+        return n % 2 != 0
