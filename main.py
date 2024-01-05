2e7db4ce53196f52c9622f076c81ac0f65add871
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:10 +0000

    carry church again supply no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_floor(x):
+        return math.floor(x)
