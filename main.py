6b148744980a268cc3cf53ad425a82314e3fd0b2
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:28:32 +0000

    generous machine car base temper

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
 import array
-def get_array_from_bytes(bytes, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+import collections
+def create_priority_queue():
+        return collections.deque()
