95bb7af4d3012b12cfa6d925b97aed771720a930
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:23:08 +0000

    lost space plan sweep breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
