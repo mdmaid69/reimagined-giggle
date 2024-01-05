ce1ef889f4ff8c09d6a40b1de50a56c51678b602
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:10:43 +0000

    treat cover satisfactory responsibility workon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def cube_number(x):
+        return x**3
