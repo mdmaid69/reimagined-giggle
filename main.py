176c0f9d6db9a896f9d12fa1007ab22125c0977a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:02:40 +0000

    protect match believe clue flow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
