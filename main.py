312a7998d5b3a6becabdf9a05d19a7e776993bfe
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:52:14 +0000

    plain noisy argue belief interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
