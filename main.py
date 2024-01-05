5539ce208ffc04b13085aa67c68f5bc0b0b66b5a
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:28:47 +0000

    make sport apologizeapologise conversation force

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def is_odd(n):
+        return n % 2 != 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
