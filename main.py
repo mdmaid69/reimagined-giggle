a2f712c383ca123a1b5b17ef16aa92677b1eb5bb
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:06:45 +0000

    complain yes while skill attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
