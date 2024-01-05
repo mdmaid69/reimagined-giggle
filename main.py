2de2e681cfbdf4c1e85534296345ae322203eb6f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:29:58 +0000

    evidence truth explain punish past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
