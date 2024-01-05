e10203cff4eb9a5ac5417c1fdd05738077e3bcb2
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:38:16 +0000

    busy provide official tidy start

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
