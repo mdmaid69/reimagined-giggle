83662b82d1d5bf35852a94bed4be1ae35913bfe7
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:14:11 +0000

    punishment Kk answer wonder interest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
