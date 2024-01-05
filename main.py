40b3ffa91b317ef3b183554c8a86f2a48e9ebd8e
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:12:26 +0000

    interview warn attitude pin unable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
