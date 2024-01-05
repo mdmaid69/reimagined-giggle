d6be7b1d975a3590ed50e80922b289d60e02c5b6
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:52:03 +0000

    by character stay through several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
