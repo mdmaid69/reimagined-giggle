329012ee9a5524a8a520508b7aa65d86b8af18b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:07:10 +0000

    patience secret breakup turnaway purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import platform
+def get_python_version():
+        return platform.python_version()
