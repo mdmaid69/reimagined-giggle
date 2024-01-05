cf8d5ecf6d1c63b8c48d1504e4eb258d0ce5d4f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:52:09 +0000

    emphasize large manage instruction every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
