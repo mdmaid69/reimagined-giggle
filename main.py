297f0fe59fed324a552a8c68950f93e3994b70f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:38:37 +0000

    fun land wait waste search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def sort_numbers(numbers):
+        return sorted(numbers)
