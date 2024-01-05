728e0cb59dd7d2aa96a03fa6b8f9787be179db94
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:00:52 +0000

    meaning rid care total stick

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
