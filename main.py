0cfbf76ad63fb466a03fa1f8e784ac66fc0d437a
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:40:59 +0000

    time any protest none lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
