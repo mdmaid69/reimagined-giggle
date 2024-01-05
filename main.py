2e8d9080288b29ebc15fd5877a913f4a7fbfd737
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:59:44 +0000

    train everything workout before frighten

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print("Hello, world!")
