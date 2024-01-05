2f1a5f96f4b3e2d13bf8707acd84abf0b67caef2
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:05:16 +0000

    list suck guilty test normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
