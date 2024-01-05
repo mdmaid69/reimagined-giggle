45e2282648e3430cb61c0cd5c937a0091150b62c
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:22:16 +0000

    forever normal piece really expensive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
