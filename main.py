ff426f961d6f407ae940a8aa5a10aa9f051b4f83
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:21:21 +0000

    university large sign bang tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
