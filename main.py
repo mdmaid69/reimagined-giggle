ab5a374d5f473e704e71cac215de12c45bcaa708
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:23:55 +0000

    call rubbish different within onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
+import array
+def clear_array(array):
+        array *= 0
