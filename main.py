b156ca838328122ef0140fd440b27741d7e002ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:59:52 +0000

    many anywhere refuse sound confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
