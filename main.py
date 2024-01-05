0bef472f2777dba4f9b225aba48f64d745100c6c
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:47:58 +0000

    afraid allow talk takeback live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
