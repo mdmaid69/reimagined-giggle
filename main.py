6e366eb08614a40b45751caa0003d99904a33230
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:50:28 +0000

    company connect smell way destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
