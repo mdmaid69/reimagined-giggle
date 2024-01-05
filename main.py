fc407e1ba6963053a8dbf6a1f2922278cd90453e
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:51:21 +0000

    closed keepoff thanks order march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_max(lst):
+        return max(lst)
+import collections
+def create_queue():
+        return collections.deque()
