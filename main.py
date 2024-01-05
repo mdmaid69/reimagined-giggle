6789d2a5773923edbc357ec7cac64bf29f82d986
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:45:53 +0000

    send rent smell too stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
