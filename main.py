e498a539786507ad3906979d99c7b3bcb4735b19
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:24:08 +0000

    show Mm strict such patience

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
