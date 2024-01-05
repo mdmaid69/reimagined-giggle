c5b31265c00c658e1f275ddfdfa13f8af83802e0
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:34:38 +0000

    home land bridge arrival neither

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
