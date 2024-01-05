da37cb3f40a76fe9fb633d4747760409829656cc
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:00:52 +0000

    comeoff limit thank length black

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def reverse_array(array):
+        array.reverse()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
