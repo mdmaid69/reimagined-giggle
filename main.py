f807e3a737b85a78ff6a7a15a26d9cc373f517f7
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:58:02 +0000

    shelter pronunciation treatment immediately surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
