8e2582de09beb7e6c9774a86188fec974e69054b
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:46:55 +0000

    post meet breath matter announce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
