b3a1280608739abd1b6df82a2916c6ba0c2a173b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:57:31 +0000

    undo block awake change float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
