868e1c5e4271e762e9171d781eac877f409d4993
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:10:00 +0000

    influence herself write control indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
