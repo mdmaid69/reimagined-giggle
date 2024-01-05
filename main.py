e419fee5397a749eabaa4ad5e33d2c5131772306
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:33:21 +0000

    computer shake needle discovery tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_binary(n):
-        return bin(n)
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
