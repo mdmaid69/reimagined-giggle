640b8f0d83ca48aba0a96d180eb70b8dcf7f9598
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:02:52 +0000

    hello anything attempt hit figure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
