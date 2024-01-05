0b0d0cae20fa3734d8f19fe44c7b3b1bfe841d52
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:26:50 +0000

    race increase cry present central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
