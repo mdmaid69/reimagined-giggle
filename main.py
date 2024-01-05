d7886138b6780009c91615a3dd4a3fe38be615b8
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:08:42 +0000

    late lower store Ee come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
