09674613cc44f78fb7e61d7d79107b17ae2a9213
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:34:23 +0000

    bring 564encourage relax mood limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
