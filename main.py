3b7b95a1848938892ae3f2f2cba0afc0a8d2d69a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:22:21 +0000

    let exact floor clear rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
