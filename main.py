15fa9cba4102f780dbc81c161e06b3314b743ccf
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:06:58 +0000

    past emotion play bad dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
