69861858d29ed641a0291a5b3e598e2f89c860af
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:54:29 +0000

    boring easy dull pick comeup

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_work(force, distance):
+        return force * distance
+print("Hello, world!")
