13c724067fd5b03263fe48d992f88adff3f55c34
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:31 +0000

    straight goup shine sure insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
