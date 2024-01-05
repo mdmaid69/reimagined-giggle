08c7c1e7b84f0a6db758a02169f0c7583816f202
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:56:36 +0000

    stick safety none division little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
