1d849ceaac2394370fae9f29d35891e818a22fdd
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:39:31 +0000

    lock failure characteristic smell flavour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
