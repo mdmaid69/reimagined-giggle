57fa9d971bb51dcc740a93b998ed684892280725
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:07:54 +0000

    manage company makeinto drink begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
