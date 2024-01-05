6ed560c837b0e44a84366c8ccd5d759567be618c
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:01:33 +0000

    or us scared air thinkthrough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print("Hello, world!")
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
