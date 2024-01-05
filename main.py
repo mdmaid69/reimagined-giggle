012f24f5d7e9e6b610bc905e486ee825bc5eba94
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:34:42 +0000

    block little your kind get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
