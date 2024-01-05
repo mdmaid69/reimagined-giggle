d98c549b7abbf732f79eb9c0b8c7b399747310e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:24:38 +0000

    inform forward subject possession neck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+for i in range(5):
+        print(i)
