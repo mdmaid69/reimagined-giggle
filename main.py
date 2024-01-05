47145bb4a5a60ff218f1aefd629da6beedfc8bae
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:42:47 +0000

    the division curious when chance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
