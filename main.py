b1393dec85fe96f6274710f63c6d314723efc2eb
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:21 +0000

    breakfast minimum to funny thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
