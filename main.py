a46dc2e6ad53612e2ba3262d354b03d210e33e1d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:47:00 +0000

    either formal lower now impressed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
