007b083577018930c7f15e179df1f6b0534d3712
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:54:23 +0000

    bother intend smoke hole whichever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
