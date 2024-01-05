12bf95ece1cb8eedfe3cddf961fcf93d50a1ae77
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:26 +0000

    language own look mass single

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
