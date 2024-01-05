8af0a10201ad6f669a64fbaa32b42993bb2f7c25
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:16:14 +0000

    branch round confuse rush receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
