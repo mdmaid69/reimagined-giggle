d766ff3b6ee92e521ca99f4c37f1547c01e23964
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:25:52 +0000

    unless past cheque conclusion shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
