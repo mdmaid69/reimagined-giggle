006bd05a7cabda03ae0e695f1299b12cb4b3f4e8
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:53:10 +0000

    roll could fire combination bridge

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import tensorflow as tf
+print(tf.__version__)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
