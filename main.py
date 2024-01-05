91fedcd6568df2a1cee75fe32dea320a894bd1b3
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:09:31 +0000

    trust board whichever child style

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
