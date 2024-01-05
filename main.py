9b0bb1923491045d75a024fefabb0767b2970e2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:52:57 +0000

    Itself reasonable female level introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
