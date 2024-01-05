7e1f27db91545c08580115526fc718892277cb83
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:19:20 +0000

    provide noise proof safe medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
