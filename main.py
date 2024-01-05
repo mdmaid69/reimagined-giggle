f29d28b323740c178c30536a96c38ded0b3c4c3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:39:04 +0000

    personal defend 564encourage yes right

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-print("Hello, world!")
+def get_array_as_bytes(array):
+        return bytes(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
