bf0ed9dd3bcd6bf592d76e88ab76ee141e0c4b41
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:29:58 +0000

    shut conduct spite 571enough sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def get_array_typecode(array):
+        return array.typecode
