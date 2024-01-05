51daaf07486c9cb261624b580bcb29ed363e24a2
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:12:18 +0000

    needle regard sense beyond callat

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
