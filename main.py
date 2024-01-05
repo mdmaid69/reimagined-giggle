5a914a0823d0d76f132713cc8094a74a51be3664
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:37:51 +0000

    study dull face your juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
