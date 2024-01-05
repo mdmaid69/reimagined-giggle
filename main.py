10d79f26e435897185db6a0fe9240425c0b359e2
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:39:45 +0000

    offer quiet equal yet relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_bool(array):
+        return bool(array)
