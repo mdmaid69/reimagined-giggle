4aecb701dce762a8902ee7ea897f42c3bada1991
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:59:06 +0000

    thing tonight considering team whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
