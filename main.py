5706bd8df03d8f7e07f428ebb3f049601c1b395f
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:46:13 +0000

    idea language deliberately achievement refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
