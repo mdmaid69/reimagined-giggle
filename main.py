073d90a5e18bf3ae988d94ef2ac7543a46d9eb58
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:48:44 +0000

    workupto transparent mystery act paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
