921cbf02353f3a090d664dc86594f544dd3b601c
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:28:01 +0000

    continue slide need sharp office

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
