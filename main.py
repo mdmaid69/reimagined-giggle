dcebafb0d488759fb61673c02c614cb3eb236f40
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:33:39 +0000

    choose priority choice name artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
