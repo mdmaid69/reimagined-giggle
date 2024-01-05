9f1f0dc791700bbb48fc88d7a9f0c152e9c70033
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:34:34 +0000

    organize size outside straight usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def sort_numbers(numbers):
+        return sorted(numbers)
