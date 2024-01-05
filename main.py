2c1452605ee716abbdad917ffdbc0bc2fcf667f0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:01:08 +0000

    over control colour dry race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
