491278bd704475f919004fcdc06d7e3953485c02
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:48:52 +0000

    zero government image half complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_set(array):
+        return set(array)
+import array
+def clear_array(array):
+        array *= 0
