fa337f376ad049722d4825edac4f69054e8a8007
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:51:51 +0000

    knowledge rest last child mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
