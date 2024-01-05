3457c94a11d04f6eb169875760eaf717a3767157
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:27:49 +0000

    past move regret takeback comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
