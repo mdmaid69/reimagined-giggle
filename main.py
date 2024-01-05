406a3c5e53fd291480c0972ea485f181152a7b99
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:18:11 +0000

    brief undo couple translate spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
