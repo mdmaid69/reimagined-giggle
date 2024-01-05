b6673035c148ed6c959a6fc297403a680016e1df
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:50:12 +0000

    share she front talkoutof whenever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
