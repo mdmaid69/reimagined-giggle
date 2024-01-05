0896103d3a4f739e02b76422ab35ab1f98b0cf70
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:14:13 +0000

    sail brush term picture case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
