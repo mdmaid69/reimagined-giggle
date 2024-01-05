9df5c8301c19e3bc579832dac05a3875d7e172d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:27:30 +0000

    much name translate possession glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
