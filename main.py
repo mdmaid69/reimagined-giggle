b52349053f2a56e7b8a91a6c588c125d131b3cb4
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:49:30 +0000

    actual tend 564encourage contents nor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_average(lst):
+        return sum(lst) / len(lst)
