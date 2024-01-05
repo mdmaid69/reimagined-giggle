a236649b5b0c7b3f16d80d31f752f6db5cb5a6e1
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:23:17 +0000

    burst slip seem stage swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
