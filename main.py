90ef3b3c41a9540519af4b866dd9dfd50f79a813
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:25:23 +0000

    let dance talkoutof this getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
