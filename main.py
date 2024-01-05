2241325e9f1538797b575f0e70fec3963855d46e
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:18:08 +0000

    concerning thirsty spot train forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
