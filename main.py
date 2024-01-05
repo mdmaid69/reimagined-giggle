e9b154e9ed635bf743d035e3790b542ce2dfa97d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:15:24 +0000

    attractive lonely region scream miss

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_float(array):
+        return float(array[0])
