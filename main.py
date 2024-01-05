888c1717a7b990291f68f5ee4b9d531b20550fa5
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:48:33 +0000

    tap probably surprising cry reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
