c617c69ea4fb5de078d7cb4de3faddab472410cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:23:15 +0000

    collection dare education file Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import os
+def remove_directory(path):
+        os.rmdir(path)
