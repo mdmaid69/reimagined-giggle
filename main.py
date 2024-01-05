05a90c8a6de4efeb6dac375ecd6a663ea1b6c595
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:38:30 +0000

    failure statement mix get get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
