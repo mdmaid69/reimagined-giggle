12427421147e570709606f559b3710e4bc09e3fb
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:16:59 +0000

    classroom arrival window bite cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_pressure(force, area):
-        return force / area
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_work(force, distance):
+        return force * distance
