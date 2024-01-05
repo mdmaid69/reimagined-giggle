2a1166864c9d36492094b9c22be8cc6fa87f1821
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:59:13 +0000

    backwards ill lift interested know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
