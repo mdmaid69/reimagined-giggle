ba07c31bc65aaed5b6e6271aeb4048109945110a
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:28:07 +0000

    round hope trade house pleasant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import sys
+print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
