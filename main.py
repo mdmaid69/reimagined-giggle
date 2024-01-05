9b05a81a9555047ec1f719fbf9625d720039fcf4
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:40:18 +0000

    money machine scream shelter duty

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
