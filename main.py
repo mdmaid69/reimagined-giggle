4ecf3e3c9496baf5fad4be89d51459079a090897
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:17:41 +0000

    yet have shall themselves star

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
 def calculate_cosine(x):
         return math.cos(x)
-def is_odd(n):
-        return n % 2 != 0
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
