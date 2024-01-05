e43cc05af8e6496ac20218b21be71f9596b6375d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:44:46 +0000

    runover card play moral over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def square_number(x):
+        return x**2
