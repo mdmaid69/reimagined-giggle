3ca5da7e9b555c205168375d485af41ab133affc
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:12:38 +0000

    care sit action drawing plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
