eee0625e1ecc79c72958c9edd2688d7b2d1b6de8
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:42:51 +0000

    communication spread surprise him advice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
