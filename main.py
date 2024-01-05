ef6e3d3738e380d542b6df682da7860483dedeed
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:42:21 +0000

    high strike noone pity band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
