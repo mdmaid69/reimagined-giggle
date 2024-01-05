943156a8e662500962858a436de2f7aa968c649a
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:04:58 +0000

    lock floor bring fear takeaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_even(n):
-        return n % 2 == 0
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def remove_duplicates(lst):
+        return list(set(lst))
