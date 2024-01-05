f279c447abaec1b7d6c512dc98152f306f6c5f7c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:51:33 +0000

    Rr comfortable relationship correct case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_circle(r):
+        return 3.14 * r**2
