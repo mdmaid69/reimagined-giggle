54b6cbb89b3b1219259b6cb65e3226d5348fe24a
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:22:27 +0000

    scare impressed passage holdup regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
