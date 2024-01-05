79aa87189e35be517c698329e9fe2166d2edb2d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:38:16 +0000

    lookback(on) crazy respect performance cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
