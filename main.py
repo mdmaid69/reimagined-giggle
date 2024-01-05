11c9bc6dce13b0c3ff42acb9b79086f6de7bcbb3
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:01:50 +0000

    take suckme music laugh flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
