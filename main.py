e8151771fb90428758d1793b74cd9a6edcae96ec
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:45:24 +0000

    attempt reply increase anywayanyhow constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_stack():
+        return collections.deque()
