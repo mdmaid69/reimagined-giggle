eb82bc4946b25edebfdaf265b8c9a94cfd2bac87
mdmaid69 <robot@alexsyw.me>
2024-01-05 15:49:46 +0000

    study wonder table nor shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
