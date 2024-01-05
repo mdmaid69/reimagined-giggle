98339e86fe54ed34e155534e62e4790bdba46145
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:22:10 +0000

    skirt 573entertainment score office border

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
