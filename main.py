af66cb4c3a14425563fceb9eef1a9f2583d33297
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:55:46 +0000

    describe downstairs imagine problem temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
