d37f59f04949c4cbe7509582da8a490403eb30a1
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:43:16 +0000

    protect year guess constantly motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
