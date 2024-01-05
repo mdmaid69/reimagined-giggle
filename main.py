bb3f810e89ff5f45c5d8e57d9feade57b58b61c0
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:50:50 +0000

    somebody crash before custom branch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sys
+def exit_program():
+        sys.exit()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
