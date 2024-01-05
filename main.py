2723eab55c907474b1c9efedf32b8f0b9b6cb9a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:12:31 +0000

    target skirt approve opportunity damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
