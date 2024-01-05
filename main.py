2456ca0047d2ce7fbb7ebdd936e41a6d11ab1933
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:59:44 +0000

    evidence contact wide rare pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
