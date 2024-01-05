bd045d1ec99822ebb9d0a1cfcd901a7ee7a0bb7a
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:38:05 +0000

    stick dare official card worry

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
