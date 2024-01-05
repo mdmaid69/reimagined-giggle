87e094fb5239d91f0288e9a855172b8064441582
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:39:06 +0000

    leave star another Nn reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
