a4ac4c9e92a4991b3f470dae833f22fce69c25ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:24:31 +0000

    snow crime arrange price stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
