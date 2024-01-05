1262307229a34651b81808428828a47063b50899
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:28:55 +0000

    fork operation few slip share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def is_even(n):
-        return n % 2 == 0
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
