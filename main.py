ecf57adc6c7649a33cda080599e8e40bbedeaefd
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:13:06 +0000

    raise picture over lock smell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def count_elements(lst):
+        return len(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
