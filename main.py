06e2fe21722e67b819d8f1cad9cc2ea301e7ba8b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:47:56 +0000

    mark pretty note interested you

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+def get_current_time():
+        return time.time()
