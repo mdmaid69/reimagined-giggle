302ff81f5595929b955ad5bc05c00a2564980b8c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:38:43 +0000

    rest man boat afraid tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_tangent(x):
+        return math.tan(x)
