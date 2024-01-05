cbb7a30e7e0aac4d8cba377bfd4345d3b950425e
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:24:23 +0000

    disappointed boat compare when moral

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+print([x**2 for x in range(10)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
