54370307982e55ab782cb5f46bc9621e167c5502
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:22:37 +0000

    spread record criticize absolutely standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_list(array):
+        return list(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
