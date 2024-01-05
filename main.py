1f37ea3094e93db5f7165f3ece6ce7e0c2e1e2a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:58:32 +0000

    slowly cheap lookback(on) positive workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
