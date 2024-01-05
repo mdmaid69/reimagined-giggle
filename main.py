13832c03adc2ca916505e80f5fa20febe9d9f529
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:04:38 +0000

    turnback rely split grateful exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
