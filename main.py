24bf9aad6fea3fe15b4d91835973a3a6b429d948
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:40:22 +0000

    information keen professional over harm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
