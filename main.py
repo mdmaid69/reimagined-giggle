5fe68d93a940f6d8722d3b79bfb20e324ee39091
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:50:30 +0000

    possibility inform suck last team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
