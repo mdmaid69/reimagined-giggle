a2c736392f274a1dde96196833df7e0f28450f90
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:26:39 +0000

    pause interrupt indeed least than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(a, b):
-        return a + b
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
