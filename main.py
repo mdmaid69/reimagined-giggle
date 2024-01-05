99593c47fcb8d8f6a9bfadd30e3c4a5358c7c2dd
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:18:12 +0000

    study on duty present ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import array
 def check_if_array_does_not_contain_item(array, item):
         return item not in array
