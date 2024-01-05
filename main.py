57b57e14b1c965dd76791dcb952868b776409a2a
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:01:32 +0000

    Zz turnout priority happen act

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def is_palindrome(s):
-        return s == s[::-1]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_area(radius):
+        return 3.14 * radius * radius
