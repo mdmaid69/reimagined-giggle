9a75daed0b49804512258c0214a434c1a8ce59ba
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:17:41 +0000

    exercise Oo blame education drive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def find_min(lst):
+        return min(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
