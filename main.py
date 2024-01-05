77b1527dd26b7e52d7c2abe242fa600a1b547698
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:25:09 +0000

    bag pool anywayanyhow communicate arrive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_priority_queue():
+        return collections.deque()
