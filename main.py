2c24e41184a1d181c79ae61d01b9290598d9c906
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:57:01 +0000

    seat always design ticket shout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import os
-print(os.getcwd())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
