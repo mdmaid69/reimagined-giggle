7cd89853cb71c7fd13b21ada669942b609866b71
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:38:59 +0000

    giveout sea area any poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
