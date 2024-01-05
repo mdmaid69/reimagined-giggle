3059148d8ab538955045e5ded2bcde4131a6660d
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:59:11 +0000

    finger late relative picture our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
