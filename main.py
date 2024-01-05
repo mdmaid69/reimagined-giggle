7966e6be0de41906214add7042c7c21f2031db12
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:22:17 +0000

    work contact pleasure offence main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
