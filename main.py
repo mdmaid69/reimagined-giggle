55393ab3a8f1e2b79e244be6f95dfba2b76973a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:13:43 +0000

    example pollution behind spite part

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
