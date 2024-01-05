393a73e68207c61a993d9d4f385b3434be098fd1
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:44:25 +0000

    cross post thanks represent pleased

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import random
+def roll_die():
+        return random.randint(1, 6)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
