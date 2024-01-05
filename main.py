5e0dba254230974691b5b4e66064bec1ef2db916
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:48:39 +0000

    lend downstairs somewhere valuable pool

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
