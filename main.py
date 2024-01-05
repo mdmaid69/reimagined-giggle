14e41157188a9e59853d51564d04094fbaf50dac
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:08:49 +0000

    completely tend cruel anyone lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_str(array):
+        return str(array)
