4fc6146b09add3bdbbfd73e90aa943d44ed500d6
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:50:14 +0000

    various ball pin surface money

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
