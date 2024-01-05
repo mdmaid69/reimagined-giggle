b6c7fda4cdf83775d5cc0012984d3fa3c68f5167
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:16:03 +0000

    guide sometimes bet final leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
