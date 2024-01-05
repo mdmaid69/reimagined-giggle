6569bb687e40adf12c59f6e77ec7481c672696d0
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:09:15 +0000

    luck unless bored address copy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
