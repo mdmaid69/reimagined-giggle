532af6e0584f635afb402c9c5d8a7e6f70dbabe1
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:40:51 +0000

    broad relation willing objection silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_itemsize(array):
+        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
