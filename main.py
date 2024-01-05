8ba198c5043f9393e0eaa7348eded5a1d5c0c47d
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:32:04 +0000

    criticize welcome cry check tune

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def count_words(sentence):
+        return len(sentence.split())
