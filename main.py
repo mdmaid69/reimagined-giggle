933d090d5fc0905a7d60e290afe833e939b27f13
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:34:17 +0000

    own fat feeling correct unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_density(mass, volume):
+        return mass / volume
