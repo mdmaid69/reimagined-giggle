e971b37557d8e49775e1bc40a6d1b9203fcaed2c
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:23:40 +0000

    software there well slow ready

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
