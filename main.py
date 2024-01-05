698a06b1d5de55662ee14921075e7ab70e251c02
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:46:31 +0000

    admit recover open house getback

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
