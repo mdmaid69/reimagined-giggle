4154be3aaf1ed9806c60fc0fb33db3e1e57ae5cd
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:08:25 +0000

    vision single unfair aim persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import time
-def get_current_time():
-        return time.ctime()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
