0e7d647e89923329944db19af34f6375ade87d20
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:50:21 +0000

    production balance contain speed many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
