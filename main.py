770e2034e4ed488452147a780ec28474f0fa78b1
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:38:53 +0000

    proper me know crowd include

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
