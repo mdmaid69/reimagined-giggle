a334aee0ff067fbee9de18ab691243a90eec16a9
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:17:58 +0000

    above ahead port comeover getoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
