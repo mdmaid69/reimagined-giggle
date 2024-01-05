c6d4f21c516be370bf42c8ce2044c25277de13a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:55:55 +0000

    sensible smile person repeat whistle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
