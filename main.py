8beda4801fc717bb6d18a19da237d16c4cb7a0d1
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:50:05 +0000

    naturally basically sudden confidence office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
