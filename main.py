d793b5b935629a74652f89e6e3b1b9e5b8768010
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:11:51 +0000

    me except appeal normal will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
 def check_if_array_contains_item(array, item):
         return item in array
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
