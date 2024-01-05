f0614e4165f87f91f20880e51c4f5b3dbf3a8f15
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:59:59 +0000

    connect part relief float traffic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
