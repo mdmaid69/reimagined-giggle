ea5d08790c44c1ea275e28b355b65b047e79ad6b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:37:11 +0000

    himself confuse address reduce press

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
