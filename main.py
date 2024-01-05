560741b442603bb03655139a110b373a37a8c305
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:08:41 +0000

    anxious bound stay wise system

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+print(os.getcwd())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
