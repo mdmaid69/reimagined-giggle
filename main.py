2d56cb673552f2a5c0c7a1d1ac2f3e8ee27a0681
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:06:34 +0000

    rubbish difficult eager one joke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
