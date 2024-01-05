ef64034aacc9614a58f5e34ac26b1616c3d6e84b
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:29:57 +0000

    show snow cinema comfortable between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
