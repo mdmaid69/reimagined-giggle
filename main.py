1dfedf9e1c9d97658b0aca02b7edbcb6e3a5ff63
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:22:16 +0000

    right nonsense ill middle speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
