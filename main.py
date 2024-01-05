6ff6cec76d6978ab659307c052010981b2f4bfba
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:52:29 +0000

    speech shrink arrangement appeal face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
