dd56e1a57cd1f52053b5aad5f6014d639263c46e
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:56:23 +0000

    unless rush region separate share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
