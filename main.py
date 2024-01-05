de1579f47463701273c766c2c0ac32882f124279
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:11:48 +0000

    breakfast bend finally doubt ticket

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+for i in range(5):
+        print(i)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
