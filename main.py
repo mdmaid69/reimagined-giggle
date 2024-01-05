de4d6c68dc8575c549f1b8cdea94796c13274f80
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:00:51 +0000

    fear church Zz theirs immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
