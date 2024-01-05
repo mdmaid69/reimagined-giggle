e037ae2f3a545736d077fb907d3c4669924baf93
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:55:33 +0000

    doubt sweet prefer minor permission

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import re
+print(re.match("h.*o", "hello world"))
