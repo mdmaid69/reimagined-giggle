f2d52bb357632987c509e241d32ac97a870f6e75
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:35:03 +0000

    spray project lay range proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
