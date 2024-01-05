8b5a07c78efc305db0cf2de68beb1d8a07749429
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:59:50 +0000

    go ocean eye muscle married

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
