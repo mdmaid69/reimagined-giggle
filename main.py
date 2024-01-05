16a73e633838cb02efcf07edea37b83c414d5615
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:14:42 +0000

    file plain bar goout foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
