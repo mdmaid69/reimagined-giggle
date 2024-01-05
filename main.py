c1f1c4de44a4a6dbd8d788a1a11dd9b41cde227c
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:06:31 +0000

    before smile any my bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
