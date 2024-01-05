d85fdf03d6d53056dc8c7095d5896aab498aa8da
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:42:43 +0000

    disagree scream scene personal hate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
