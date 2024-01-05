40b944ec4e3227e2cb0d4defa55e86ed3d5b3181
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:43:43 +0000

    cry roll soak cause paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def split_path(path):
+        return os.path.split(path)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
