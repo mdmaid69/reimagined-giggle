5d9583fa48b6ebd7ac785d51010c5fa58059909d
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:49:06 +0000

    call now comeout right compare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def convert_to_octal(n):
+        return oct(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
