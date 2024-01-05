6ec7b6cf0fd41044737a09d8f88464dd172b01c2
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:28:07 +0000

    hour nothing development keen hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
