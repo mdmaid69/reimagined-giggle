fb5a66d5e02ae1c693c79bd6368591cac2d4ab36
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:23:30 +0000

    geton unlucky thought trip mysterious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import time
+def get_current_time():
+        return time.ctime()
