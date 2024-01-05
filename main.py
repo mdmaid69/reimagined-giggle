e5be0ae8574d607cc979bf6e009ea4d0235bb650
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:14:27 +0000

    giveup interesting hang yours tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
