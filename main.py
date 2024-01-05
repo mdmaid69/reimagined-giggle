3f947698a565f35d3abdf014aa1e8499000fe668
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:53:50 +0000

    neither divide takeover Tt crowded

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
