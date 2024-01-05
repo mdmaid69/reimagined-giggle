5516b7a96040ed4cff76d98a98741b02484ef500
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:33:21 +0000

    breakdown fierce cover of cause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
