84ebbe584d32098ec3e78117317f6e4af1824689
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:57:16 +0000

    compare ignore buy play suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import sys
+  def get_python_version():
+        return sys.version
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
