543ae7dd65d5e14bc882fb19713f9d4b6f13b0c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:34:40 +0000

    best together wide pinch friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
