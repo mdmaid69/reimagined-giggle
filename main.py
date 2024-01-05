ded0133fd4088ce986bc1a82ea4fdc7faf19042c
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:51:42 +0000

    our lookback(on) young cross plant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
