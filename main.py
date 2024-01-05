5ba1ab17792d84b19382574b91030d84780f3a71
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:35:42 +0000

    wherever cover loan to compete

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+for i in range(10): print(i)
