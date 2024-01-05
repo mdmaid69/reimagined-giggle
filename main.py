0f9aaacb9fb79235755e734120aace11fe2805a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:39:13 +0000

    race terrible difference watch test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
