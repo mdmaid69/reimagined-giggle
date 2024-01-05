4f57fc4d9dc91e7192456303b90c40d6cc887be5
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:45:02 +0000

    cross bear every injury part

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
