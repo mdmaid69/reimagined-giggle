4ebe57e1f4133cd3f14a0bc896005ecf2b1423fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:39:22 +0000

    performance contact taste their or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
