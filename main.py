5c5282bc56c2116c81391386c99a4c14f7de53b5
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:29:30 +0000

    turnup available important complaint bread

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_item(array, i):
+        return array[i]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
