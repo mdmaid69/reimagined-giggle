3e18b67460e02af56f28510f60237a89abff608f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:16:02 +0000

    feature human fact deserve pupil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
