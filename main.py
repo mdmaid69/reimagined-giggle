5786f5b6ad56cc74fb9b464e962fa497c24c7fc5
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:05:24 +0000

    expect obtain remove bread first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
