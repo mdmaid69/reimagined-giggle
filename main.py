d744f9fc097758eb150ce3c4d8de6b8075e4c89f
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:47:37 +0000

    character offence share stamp aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
