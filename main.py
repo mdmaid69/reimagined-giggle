377ff350dfaa8cfa86759ef1cc1af3a1ca2148d5
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:58:26 +0000

    quickly through desperate game fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
