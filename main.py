286a189662033372a909836ac73fe63b39c4540d
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:26:56 +0000

    hide ride appeal building aim

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
