0165ba833ceae4e71c954b72319e3dd3d2aea611
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:21:37 +0000

    service copy please damage hard

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
