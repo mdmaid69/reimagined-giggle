5f1ecc2628b5e1519534a774b400b0da84e457ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:39:20 +0000

    visit ground ourselves determined healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
