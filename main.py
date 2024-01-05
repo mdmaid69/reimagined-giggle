908ef409f0440c0f97935bb98b3c25691ce1b958
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:09:53 +0000

    fix regular smoke scrape complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
