f45ff67a19a074590702a93639ff22b64c477c8a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:41:18 +0000

    near Uu father error before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
