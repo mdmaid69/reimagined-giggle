a168a11a74aadb0506a6e91ae74a9c40408d72f6
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:08:12 +0000

    turnon side who almost offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
