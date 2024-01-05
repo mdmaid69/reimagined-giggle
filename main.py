d0c6c9f43506c56a706f4c3f7e1d61d93bdfa807
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:24:48 +0000

    dead brush wash trick finally

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
