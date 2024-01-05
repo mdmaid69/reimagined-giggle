3a310f2340428f40cf33338aace3747908e2ccd4
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:46:08 +0000

    lay convincing pity activity division

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def convert_to_binary(n):
+        return bin(n)
