c12814f2a196e21887ef5f0264a8429911b4d620
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:38:00 +0000

    control pipe feeling complaint goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
