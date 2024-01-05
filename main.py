74a6c8a52bf450f238ebba9baff82d2546412534
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:59:10 +0000

    woman start when suffer even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def count_characters(sentence):
+        return len(sentence)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
