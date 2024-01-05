cbd4b24060a5547183787c3dd68a326b106b35ca
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:05:14 +0000

    ignore uponon discuss trust whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
