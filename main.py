014337920fc4fde9113c7e478de0b11692a03be1
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:10:37 +0000

    interest disapprove might limit sound

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_work(force, distance):
+        return force * distance
+def convert_to_octal(n):
+        return oct(n)
