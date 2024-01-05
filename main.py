80b66ff5ad0315060b903ad7a07970b91a1f734a
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:05:29 +0000

    behind goon fit pity line

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+x = 10
+y = 20
+print("Sum:", x + y)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
