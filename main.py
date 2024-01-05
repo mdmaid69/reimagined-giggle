7b5b543863992ef623d24e8ec482e2c3a9fe6475
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:39:02 +0000

    judge progress rain Hh though

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def reverse_list(lst):
+        return lst[::-1]
