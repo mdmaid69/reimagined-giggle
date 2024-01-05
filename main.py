a82584457ad6323a74fa1442d05138026dc5a74c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:25:54 +0000

    limit guess piece independent brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
