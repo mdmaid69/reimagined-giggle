f85d348b3665c698de7388f6b9a64fd0f628e8c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:37:37 +0000

    bar seriously on close plate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
+def find_min(numbers):
+        return min(numbers)
