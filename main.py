2fc95442bf347abb42b4c8b03e3c603daca21aa9
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:17:59 +0000

    spread hungry himself fairly balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
