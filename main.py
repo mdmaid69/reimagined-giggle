7405af3e70f50a394a07eb12c87c60885a7a7771
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:32:19 +0000

    snow steady touch turnup may

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
