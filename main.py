2211dacaa17f14bd51f24bb0c81ddc0f1b3befb2
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:27:03 +0000

    aim war calm section down

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import getpass
+def get_username():
+        return getpass.getuser()
