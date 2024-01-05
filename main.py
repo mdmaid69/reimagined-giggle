56a378929f30910989ccd0a8f922456023f6c0c1
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:13:49 +0000

    makeup suffer brush train immediate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
