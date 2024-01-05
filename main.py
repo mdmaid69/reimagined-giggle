ada4516052f864202334193b2413d5249cdd631a
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:38:16 +0000

    like Qq excellent knowledge sound

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
