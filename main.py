92855fdc3d4b886837d7081fb484481a58400f5d
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:46:09 +0000

    cry someone appear prove point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import sys
+print(sys.version)
