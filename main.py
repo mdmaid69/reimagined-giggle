87f8902369d13fcab047721b3ff0da56ba2b2fd8
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:26:51 +0000

    fail hard past nonsense march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
