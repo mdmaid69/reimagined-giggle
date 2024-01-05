b13a7cbb232934db37bdc37ff57e88e0cd8a58f3
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:43:08 +0000

    beginning raw small control if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
