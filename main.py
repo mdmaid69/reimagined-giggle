bf0aa7e8fb0d6fbc8f334ac9577f87580fcee968
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:26:09 +0000

    by run take base comfort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
