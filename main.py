3c6988a8f434bb907b2931f7d99f47b9d7992cd0
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:25:43 +0000

    brief bring intend pin emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def find_max(numbers):
+        return max(numbers)
