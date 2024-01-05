02d6c37869007081d34164c93c85dbc0242eb5b7
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:40:59 +0000

    either apparently be claim need

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import random
+print(random.randint(0, 100))
+def convert_to_binary(n):
+        return bin(n)
