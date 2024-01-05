57c79e513d98a4e169a1a515341e1cb4767d8160
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:04:01 +0000

    increase announce temper congratulate fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
