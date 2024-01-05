b3c4a18943b42f6856e20eeda1228c47d71b0b23
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:18:11 +0000

    match welcome me will holdon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
