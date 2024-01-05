851fb2324718a5261cfeafcb7a1983c5df49c19b
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:02:00 +0000

    bury bridge hard brush now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
