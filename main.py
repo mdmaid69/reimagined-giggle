89114a6d254ab5dd6bfe3b0c671e40cabfbac5ed
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:20:32 +0000

    kill smile summer need bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
