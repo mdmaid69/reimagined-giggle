2346f5ed6da16416b16aee47a189634379fc6ce3
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:38:32 +0000

    far sight suppose at office

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def add_numbers(x, y):
+        return x + y
