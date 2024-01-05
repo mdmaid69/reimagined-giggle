a12879879340f9dcc8060c9160cd5ce9f4e648f2
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:30:11 +0000

    value brother evidence pause drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(x, y):
-        return x + y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_list(array):
+        return list(array)
