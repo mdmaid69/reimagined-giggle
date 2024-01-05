ccefe3feb1f402a42e52d513639368f8e254bcbf
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:39:37 +0000

    trap taste cover national flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
