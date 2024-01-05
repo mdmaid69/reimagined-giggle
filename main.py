0da22d962d4d486433fd9db256b1f880a6460a95
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:39:53 +0000

    poor onto might generous complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
