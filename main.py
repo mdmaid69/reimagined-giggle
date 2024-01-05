eff9bf1dd2952832157d12ebf789d206128030e9
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:10:20 +0000

    school goup comeround hurt disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
