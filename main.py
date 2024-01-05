b18e788973aa2956712b9a923471927e02a1bc70
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:33:42 +0000

    slide curious cost human Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_volume(length, width, height):
+        return length * width * height
