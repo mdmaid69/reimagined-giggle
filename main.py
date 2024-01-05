5d4fb8705ca39752d04473600b1a30a7720b4fc4
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:46:42 +0000

    likely unusual familiar below back

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
