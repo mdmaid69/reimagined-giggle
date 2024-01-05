69c44c9437f0d84deeed2a439ee83afe5466ccbd
mdmaid69 <robot@alexsyw.me>
2024-01-05 10:47:24 +0000

    before name false geton summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_list():
-        return collections.UserList()
+  def is_even(n):
+        return n % 2 == 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
