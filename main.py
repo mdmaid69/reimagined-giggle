b7d6f739e35acb10d0df03017187194c4bc80fc3
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:17:36 +0000

    display ring respect lesson smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
