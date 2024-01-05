05068ef72cd116b5a40c7d1e00dba29a8a5a28fa
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:37:06 +0000

    godown greeting uniform rely food

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
