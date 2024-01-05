667fc2ab1a272b0f73e0daffa6dc2b8443523557
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:16:11 +0000

    shape walk 564encourage somewhere careful

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
