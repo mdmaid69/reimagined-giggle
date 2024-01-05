b54913ab97e6c5b9bc93d9364f1a90416609a3fc
mdmaid69 <robot@alexsyw.me>
2024-01-05 21:30:49 +0000

    file fill plant but flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import array
 def remove_from_array(array, item):
         array.remove(item)
