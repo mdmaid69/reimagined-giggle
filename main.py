2c293669cdfe28c43b8ebe82d5077d36ca64c35c
mdmaid69 <robot@alexsyw.me>
2024-01-05 08:52:30 +0000

    return joke workon flood plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
