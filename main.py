0e4708d0d664e9c39845ab0a8f6f6d138a31ed9a
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:14:45 +0000

    object some father match upset

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def create_queue():
+        return collections.deque()
