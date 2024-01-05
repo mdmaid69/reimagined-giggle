fd2a6edae3238812077d908775ea28aad66f0652
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:52:01 +0000

    doctor tip impressed convince early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
