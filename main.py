a47082b0fa1f81a41b32976f30810db662525c0f
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:38:17 +0000

    tell keen interfere moment rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def convert_to_binary(n):
-        return bin(n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
