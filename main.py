a89914c28eb1c85e2373892d36249fe0b4fb31a0
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:50:06 +0000

    improvement swing city crisis drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
