4d1329cf37a7232961fade9eb345e0ec2945bf26
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:57:48 +0000

    callup while life pronounce simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
