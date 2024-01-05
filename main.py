8444f0c6935fb66b960603d3c2b3c8c3d61489d4
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:43:52 +0000

    either OKorokay switch doctor similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
