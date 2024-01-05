71a505b738f41feee5fb6e80712a44296f9e006b
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:54:49 +0000

    offer system sharp along cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
