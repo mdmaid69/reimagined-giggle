bc6cd628df1dd34cd5e4c5a06d4c36446608f612
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:34:38 +0000

    round trial easy rich platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
