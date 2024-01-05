74b08f9dad451f1004c82ab7fb8e697518fe5466
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:10:16 +0000

    apologizeapologise mail nerve future it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_time_since_epoch():
-        return time.time()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import sys
+def exit_program():
+        sys.exit()
