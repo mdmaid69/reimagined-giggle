34e4d9a41411ebf7c8294da3ea447ebd2b249cb1
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:09:09 +0000

    anywhere plan closely split light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
