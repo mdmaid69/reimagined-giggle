49468d82d2a32c63d9d9ac099599596979c10ab7
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:44:56 +0000

    young fork new advanced worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_density(mass, volume):
+        return mass / volume
