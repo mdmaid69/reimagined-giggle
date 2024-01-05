b0d673765ee4a7d8e9c3c36da2fcc584b16d3c64
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:48:36 +0000

    idea have left cinema freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
