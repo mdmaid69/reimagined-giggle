f1072760a5834f6d1f5d55ab2811a325b5353bc6
mdmaid69 <robot@alexsyw.me>
2024-01-05 20:26:11 +0000

    not draw frighten full maybe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
+def cube_number(x):
+        return x**3
