b4701e487739bf6f6d5b723d028e849ae6a4462b
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:26:31 +0000

    ahead treat either love fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+print([x**2 for x in range(10)])
