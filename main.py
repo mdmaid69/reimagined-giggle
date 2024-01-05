c903ea3ab4badab92212188417bc74bc31baae42
mdmaid69 <robot@alexsyw.me>
2024-01-05 12:02:12 +0000

    breakinto about there race crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
