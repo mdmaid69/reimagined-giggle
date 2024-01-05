13ece406e6b967305000b2cc89a32e0f4fe33f36
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:04:20 +0000

    sometimes child bridge fashion excited

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
