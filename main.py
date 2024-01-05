d82f1a9217ac5681ebf23670d973654ccfa4d401
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:13:01 +0000

    mistake freeze high computer contain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import time
+def get_time_since_epoch():
+        return time.time()
