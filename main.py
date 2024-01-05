8727b6322705b7b8716c50a1bcfc268b05c30ac6
mdmaid69 <robot@alexsyw.me>
2024-01-05 09:29:49 +0000

    joke pour achievement run Zz

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import sys
-print(sys.version)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def exit_program():
+        sys.exit()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
