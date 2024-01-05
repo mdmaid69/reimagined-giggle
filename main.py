ef01a59cb749cb65cb4b341325001c5a489d1788
mdmaid69 <robot@alexsyw.me>
2024-01-05 18:13:22 +0000

    convincing search room slice often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
