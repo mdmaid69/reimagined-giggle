deef02052da2423fa1c6db14dcc9bf41c2dac164
mdmaid69 <robot@alexsyw.me>
2024-01-05 16:41:42 +0000

    storm little process absolutely tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
