b984d1083cd0b6692b2dd1125d228b7aa52b4b9e
mdmaid69 <robot@alexsyw.me>
2024-01-05 00:12:48 +0000

    large and reply disturb beneath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
