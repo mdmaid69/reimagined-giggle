e24bc0523f1699046aade37de16514f96fb67e3b
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:08:18 +0000

    embarrassing car thin rather any

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-print(math.pi)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
