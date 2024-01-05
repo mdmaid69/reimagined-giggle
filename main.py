b67976e58c1afe4fd9412f8c171107ed43b344c8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:24:23 +0000

    instruction her break himself fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
