3b88820715f4186c40b7beec792b9a18f7722a7e
mdmaid69 <robot@alexsyw.me>
2024-01-05 05:14:39 +0000

    turnback bye help sense trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import tensorflow as tf
-print(tf.__version__)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
