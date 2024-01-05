189db9c13ea7fe06d8be2daffe1c27058f28e4a8
mdmaid69 <robot@alexsyw.me>
2024-01-05 06:00:25 +0000

    steady lookback(on) definite downstairs perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def multiply_numbers(x, y):
-        return x * y
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
