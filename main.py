d644c81a0c4abe7d0ca17725a8b29bfb80dfc913
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:38:09 +0000

    live value throughout tough sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
