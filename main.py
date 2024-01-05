30667b09c2e877fa54c4287b52e6cf5bbb3acf31
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:08:20 +0000

    care make sign explanation empty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_work(force, distance):
+        return force * distance
