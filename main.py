fde57414e342193ba55a98338b09d5580adb4a43
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:14:37 +0000

    no separate lesson instruction spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
