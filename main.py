9441a9af8d35b9b7da7ce795607ce51635af5768
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:20:28 +0000

    evidence fast welcome bet married

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
