e32f941e1fb961a0161b66999c1ce45fa5c9aab9
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:22:36 +0000

    tap steer every hate scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
