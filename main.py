864025f7a4dac0a8b596e320c8b8d2e629ac9b4d
mdmaid69 <robot@alexsyw.me>
2024-01-05 13:34:26 +0000

    event plain confident personal through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def reverse_array(array):
+        array.reverse()
+import numpy as np
+print(np.array([1, 2, 3]))
