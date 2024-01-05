75a8d6912bce6f169d303c1c27ac112266bc88e6
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:21:06 +0000

    war full back shop spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_queue():
+        return collections.deque()
