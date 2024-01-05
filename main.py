728a1285736f25d0f8d40002784b83a41adceb2d
mdmaid69 <robot@alexsyw.me>
2024-01-05 03:59:59 +0000

    slice meal rather dream feel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+name = "Python"
+print("Hello,", name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
