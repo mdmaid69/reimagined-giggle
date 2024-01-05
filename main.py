a9e2714f8f22033d66e4497bab89c785080b578e
mdmaid69 <robot@alexsyw.me>
2024-01-05 17:54:01 +0000

    recover whether far port receive

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_index(array, item):
+        return array.index(item)
