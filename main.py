17da02044f676fb6af42f2b5aa5b51c716d5192b
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:28:39 +0000

    release major sail seat comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
