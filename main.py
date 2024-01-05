94a0b01a10cf7285b1c8d67ecc2733679c535af0
mdmaid69 <robot@alexsyw.me>
2024-01-05 04:01:55 +0000

    takeout sudden country eye visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
