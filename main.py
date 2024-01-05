4a7fc2e24a0225a96933d24767a2e6080ba3202a
mdmaid69 <robot@alexsyw.me>
2024-01-05 11:45:15 +0000

    constantly before instrument flow brother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
