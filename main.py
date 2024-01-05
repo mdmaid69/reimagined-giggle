9cb7139a7ffd0a827d2f8439819e222f58336abb
mdmaid69 <robot@alexsyw.me>
2024-01-05 23:32:27 +0000

    perhaps scrap steal laugh apply

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_user_list():
-        return collections.UserList()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def remove_directory(path):
+        os.rmdir(path)
