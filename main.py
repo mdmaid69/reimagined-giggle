856051c6fbffc437c82335a34715096724419dd6
mdmaid69 <robot@alexsyw.me>
2024-01-05 02:04:14 +0000

    rough system include blind central

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
