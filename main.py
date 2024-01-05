6209b69c6e1753caf863bb78609a5c28baee3b20
mdmaid69 <robot@alexsyw.me>
2024-01-05 14:25:56 +0000

    exchange dig peace famous step

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import collections
+def create_user_list():
+        return collections.UserList()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
