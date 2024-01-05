1f89c08be0c2eb237b9152ebd2291ecef1536f90
mdmaid69 <robot@alexsyw.me>
2024-01-05 19:14:05 +0000

    name nerve grade possibility ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
