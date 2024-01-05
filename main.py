31ba0aae7e890563e47763b1cb6caeec920efb83
mdmaid69 <robot@alexsyw.me>
2024-01-05 01:36:17 +0000

    threaten agreement rid mother dish

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def sort_list(lst):
+        return sorted(lst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
