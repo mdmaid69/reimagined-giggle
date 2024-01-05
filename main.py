f9f6602826b56b87358688d5a86d297897343291
mdmaid69 <robot@alexsyw.me>
2024-01-05 07:04:51 +0000

    post fork thing count between

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def convert_to_octal(n):
+        return oct(n)
