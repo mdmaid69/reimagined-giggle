ecf7f040c3b1cc3fd3ea148a5041b30a9b1923f9
mdmaid69 <robot@alexsyw.me>
2024-01-05 22:15:33 +0000

    hard side wander then nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
