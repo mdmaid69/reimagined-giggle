e109e18bde06a098815480432203c4d049db21ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:11:09 +0000

    badly sensitive off black judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
