0d9f894cf7b56da4bd168772ed51e3cb17af6ed6
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:06:52 +0000

    only joke breathe minimum position

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
