def8c60360669f94c9c592285ac834f3f6dbaad1
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:35:38 +0000

    never goback(to) out float find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
