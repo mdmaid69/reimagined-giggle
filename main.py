c18352d5adad8bae0c52f1f202f63e0da57ecdb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:40:58 +0000

    telephone begin pattern concentration provide

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def convert_to_binary(n):
+        return bin(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
