393d0dfbfc63cf0975d0d93aea9a9ce2e83ef560
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:56:08 +0000

    deep shoot owe exhibition 573entertainment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_max(lst):
+        return max(lst)
+import collections
+def create_priority_queue():
+        return collections.deque()
