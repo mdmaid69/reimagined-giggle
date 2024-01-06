5b9d465390f424ffce1360f67ef617ba6809fea4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:29:49 +0000

    knowledge worse attract region murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+import numpy as np
+print(np.array([1, 2, 3]))
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
