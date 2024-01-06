2fbf55bea12b96fd36ed24ccfc6394f4ff42ec38
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:15:00 +0000

    offer birth suggestion feel the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
