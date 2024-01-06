7fb2dddaa634ecfda980f8c78eccf6d9ff387f34
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:27:03 +0000

    splash warm whisper many suit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
