5b9eb14a4a714b3adc9f8319b5bcf9d4a1c9dd8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:44:20 +0000

    journey early sharp them wire

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def square_number(x):
-        return x**2
-def find_max(numbers):
-        return max(numbers)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
