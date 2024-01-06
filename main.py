b4dbf3fbb5da9fe870c30e6259560f775bfd3bb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:36 +0000

    block ignore fortunately slice embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
