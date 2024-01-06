324d84bfbb406f5aba8652a4ddbe411038a69724
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:55:26 +0000

    desperate turnover challenge perfect temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
