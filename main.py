63b4819770d7b0cc05ac880424954c27584944e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:41:36 +0000

    actual borrow everywhere consist separate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
