bbc29b3af4a7d99e9beceb596ea589f5283cb9d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:55 +0000

    lose waste unlucky impatient idea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
