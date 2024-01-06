41a8ab63349627161394e542c922124c32957e3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:50:46 +0000

    my furniture regard tense firm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
