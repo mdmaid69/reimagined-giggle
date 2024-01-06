30cbff0acdbae2556a7216d127cd37d1090be9e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:47:09 +0000

    fairly set over interview shout

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
