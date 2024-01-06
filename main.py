c5a3a09a976c22fb5ff93ecb93033d47fc7f7f75
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:56:39 +0000

    degree favour female bringdown runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
