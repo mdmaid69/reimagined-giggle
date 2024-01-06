408461f8435eb418a5c8e99df01cc46acddec14d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:23:25 +0000

    which 568energy rub embarrassed temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
