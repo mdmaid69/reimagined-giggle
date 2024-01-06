3ba33a424fc166213f09f920248d2014ff65da94
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:06:33 +0000

    basis give since powerful behave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_volume(length, width, height):
+        return length * width * height
