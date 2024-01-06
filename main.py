21366d130345eb13fbb5c96f1c5c3075d4e9f371
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:25:32 +0000

    add general left bright occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
