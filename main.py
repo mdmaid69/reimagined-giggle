b2ffb19bfe3a06e82353a6ef3cbd9bd89cfacec0
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:55:56 +0000

    sensible situation passage may complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
