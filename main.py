2cb212e0b8393868e0a1078557ef633f856f78e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:10:38 +0000

    gradually 573entertainment uncomfortable natural train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
