0dd35924af6c516fa1861e4946f26df921e82c3f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:44:36 +0000

    tour cope own theirs owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_list(array):
+        return list(array)
