07d5797d52d142ecc9c36b03774150863c81d6c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:13:07 +0000

    search slice tend prize scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
