3d10a6cd0246e81cbd781cea6ffd31ab071ac1f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:15:47 +0000

    regional mine regularly alone spite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_power(work, time):
-        return work / time
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
