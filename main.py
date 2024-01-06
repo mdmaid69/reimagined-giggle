9dcf8454c712d10b1c4d2b6a65b11a48e3329656
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:45:56 +0000

    turnback subject afford wave act

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
