b7cb224f56f03bc93037cc6df7e949a1bf94f12a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:29:20 +0000

    neck maintain afford goodbye murder

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-for i in range(10): print(i)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
