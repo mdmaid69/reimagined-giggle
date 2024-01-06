73500c04d25b742f8cb2fc6347d763ae9bc42461
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:48 +0000

    finger whole together dick Ee

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
