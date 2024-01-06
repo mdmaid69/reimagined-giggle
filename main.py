429def07099573be64d8ec48c6cc5b80806efd80
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:42:49 +0000

    wish where appearance still false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
