97e72013e382333038f822ea67de92aa093aaa1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:21:01 +0000

    travel slow 564encourage before subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
