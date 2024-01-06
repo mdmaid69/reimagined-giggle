e19c136785f7a8059724ca625ef219d32e983926
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:58:27 +0000

    threaten Jj cheat get mine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
