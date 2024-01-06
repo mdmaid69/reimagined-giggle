ce778071e380b5a88a8f0d12d46ba411a99bf422
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:09:16 +0000

    conclusion sore trade hit lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
