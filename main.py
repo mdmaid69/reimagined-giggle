092fe065321d99ab40787710388b42a9c89dbb47
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:12:20 +0000

    introduce visit in keepfrom complete

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def sort_list(lst):
+        return sorted(lst)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
