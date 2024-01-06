203e373ca9beb29b181027b02658ffa303ce541c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:01:19 +0000

    excuse wonder interruption so hurry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
