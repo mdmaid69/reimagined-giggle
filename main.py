a59de551f6c675905af4d13f0467c09bb6b0b262
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:14 +0000

    share lookaround tip hour learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
