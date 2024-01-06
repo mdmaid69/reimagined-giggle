28704b53813814c91e0a4d52cf50d1c766d321ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:45:36 +0000

    understand total apologizeapologise valuable fortunate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
