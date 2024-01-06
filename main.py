eac9d76a23e54230935f387b71cf607e9835d841
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:01:13 +0000

    discover careful about look shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
