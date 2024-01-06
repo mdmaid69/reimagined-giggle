4234502dd0f752332add7f863e96c86ced316f75
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:40:19 +0000

    share chat nigga slip solve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
