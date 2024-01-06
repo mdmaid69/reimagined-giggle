a943434625876c7e1e6244d1146e5ea153b58612
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:47:48 +0000

    fairly early cough lookaround rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
