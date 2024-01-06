1813e9118caedd19308a5ec37da97275acc9ed11
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:18:38 +0000

    related walk react mouth ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
