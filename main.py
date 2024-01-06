3c3cef3509a5069b33aa49f9bdc57e3ba860d532
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:50:05 +0000

    year wash since destroy attractive

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import array
-def get_array_as_list(array):
-        return list(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
