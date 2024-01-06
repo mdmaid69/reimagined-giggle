16ae6996f576d49571f7168e8bd163402622635b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:47 +0000

    gentle life prevent conclusion our

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import sys
+def exit_program():
+        sys.exit()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
