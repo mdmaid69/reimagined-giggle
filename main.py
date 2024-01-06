8f52f32a1f1d53a23a5b306edc6b3519db7fd198
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:27:19 +0000

    burst false hold phone floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
