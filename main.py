f65d4938d3f817170e5d4b92786842593c0fe0b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:12:05 +0000

    sweet all substance glad floor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_float(array):
+        return float(array[0])
