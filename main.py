9569d4380609b4494a311486b4b37197ef924894
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:45:50 +0000

    false hurry child at warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
