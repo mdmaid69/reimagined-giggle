6c5d71c789fc91339cf229511afb37b8e828f73e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:14:28 +0000

    Ff become acceptable its suffer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
