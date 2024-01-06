e4618128cb89bb61921020a15916c1fdfda6a5ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:33:42 +0000

    in light anything hole little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
