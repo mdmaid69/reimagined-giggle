25bd3520c4d2609890a5c2d4be88f1fd63e3a847
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:29:56 +0000

    interest bye view claim attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
