4727974bf7e5978286cfde486d8cb86af61c8c84
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:11:03 +0000

    confused hard left miss suggest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import random
+print(random.randint(0, 100))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
