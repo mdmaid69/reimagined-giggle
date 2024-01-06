718af5fcc61c47b7686987116f0da2cc25124cdd
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:45:47 +0000

    squash pronunciation cum song event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def reverse_array(array):
+        array.reverse()
