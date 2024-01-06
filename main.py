013c8aead865859ae725aa0eb9891327f89f3ab2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:16:02 +0000

    connection that loss difference to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
