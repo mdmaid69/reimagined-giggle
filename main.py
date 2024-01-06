32be50668dad37b5d608eb04f6cd54a10a65ee19
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:13:27 +0000

    determined occasional behind have could

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
