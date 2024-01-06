52993444a90a149b52cbc628c97369d4d6e700fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:51:12 +0000

    suppose telephone change perhaps week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
