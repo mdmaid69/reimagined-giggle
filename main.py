bcd93f395bcaeed25196e78b84877b9f5081977a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:57 +0000

    low mend ear in runover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
