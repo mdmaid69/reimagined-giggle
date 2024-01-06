e01daa1760c47ea94939e9ce853b97e755add5d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:41:52 +0000

    ourselves love tap best otherwise

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def reverse_list(lst):
+        return lst[::-1]
