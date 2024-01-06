50a704592c9c81796c146400d0ee3ca2c774280d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:33:49 +0000

    quality bye crime call base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
