d00b91079381c87fd490a4abb3f3beab8aecedd7
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:30:22 +0000

    model evening threaten pocket complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
