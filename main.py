dbcc2f3ee25aa6ca8feb12f8892134d0e758ccb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:45:12 +0000

    finally comeover matter their Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
