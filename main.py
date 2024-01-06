6423046d42e8aa6ac440d531c4733fc0eccdb391
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:59:33 +0000

    loud admit left no regular

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import tensorflow as tf
+print(tf.__version__)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
