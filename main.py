2ef200dfbfa44b657f8dc8dccd9b6b562588708f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:40:09 +0000

    ours fact ilovedicks attitude listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
