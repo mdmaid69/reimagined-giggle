40c6a3f412d2b8040b17be8ab058a7a176439c07
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:05:52 +0000

    exchange OKorokay movement form her

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
