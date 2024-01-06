5b61cbaf62e15d5ab7ff42726d3b1442062c798c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:41:19 +0000

    formal flood wash side show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import logging
+def log_message(message):
+        logging.info(message)
