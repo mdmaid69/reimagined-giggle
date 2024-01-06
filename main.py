c7f5ce3cb6706a0c22ba25deb0e479369d4d8544
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:39:27 +0000

    sell lookafter influence chance determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
