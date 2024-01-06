24d4a18624a6f8b7543a0fa943fab3676c54a642
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:03:06 +0000

    press couple left quickly matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-print(datetime.datetime.now())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
