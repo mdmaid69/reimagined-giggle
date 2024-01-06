c419ac7be895cc090e59d79a605cb0cc41291d7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:47:01 +0000

    also content advantage treat amount

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_list(array):
+        return list(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
