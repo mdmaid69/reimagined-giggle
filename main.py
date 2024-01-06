978214a36b6272ce16369545ed4a9b7dd0c15659
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:10:18 +0000

    surface anywhere look right mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def count_characters(sentence):
-        return len(sentence)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
