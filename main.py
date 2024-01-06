8c0b42ecf7f110fc8f7f0fa11c390ca613a1ec3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:11:27 +0000

    pause division blow both breakfast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
