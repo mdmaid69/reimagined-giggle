6bc4893858b0b2f52e82db5dc82e26bbe05b0f51
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:30 +0000

    noone getoff adjective shine before

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
