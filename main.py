5178ba8563b5037840b88fb2267862726be84e47
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:24:42 +0000

    suffer please empty choose describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(numbers):
-        return max(numbers)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Words:", len(text.split()))
