3d330dfc513ef1b09fba5d75623ad6028d9f52cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:05:45 +0000

    thank woman object introduce among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
