7c66829a46bb12bb750b9b10c1849af14e4c41e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:41:41 +0000

    keepout go possible downstairs generous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
