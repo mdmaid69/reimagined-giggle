37d9c4fcef278a0fbc23db0c0d09939f857cc13e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:00:32 +0000

    cheap top claim belong thank

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def sort_numbers(numbers):
-        return sorted(numbers)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
