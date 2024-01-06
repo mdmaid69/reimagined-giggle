b4f5acc452dd66e38160460bfdb2973e7888331b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:27:12 +0000

    mean single turn war when

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def count_elements(lst):
-        return len(lst)
+def get_array_buffer_info(array):
+        return array.buffer_info()
