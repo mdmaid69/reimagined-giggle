1d219622587801114b46c9177e24115ffb35a5fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:54:43 +0000

    deliberate generous bin delicate full

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
