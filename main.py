445b522d8838f91e40c85709e81e034c9bedf324
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:42:58 +0000

    nowhere expect clever hers closed

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
+import math
+print(math.pi)
