51d3b8c4a65e9562cf975ee6b8a690e354dc9188
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:12 +0000

    trade passenger country change star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def is_odd(n):
+        return n % 2 != 0
