b6b119028709fca798eaa6cc88d13b81935b0268
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:15:56 +0000

    part divide excuse instant round

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
