6a23d95e873a5c4d68f07c985a26b78949a2a509
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:20:48 +0000

    anyone constantly shallow plastic crazy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def is_even(n):
+        return n % 2 == 0
+import time
+def get_current_time():
+        return time.ctime()
