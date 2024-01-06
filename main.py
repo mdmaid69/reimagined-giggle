a1b91c3d066ee9f52bd9f677bcb982177b896078
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:57:59 +0000

    yourself hardly finish part ever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
