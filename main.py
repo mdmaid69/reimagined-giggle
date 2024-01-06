8a93c5b81b4016655cdcc43754ab9c8a1d91f547
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:36:48 +0000

    invite wealth food declaration support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
