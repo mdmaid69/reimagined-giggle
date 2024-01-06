9bbd17420d4a087240e5d147fb0847853f025ffb
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:17:30 +0000

    tie concentration off bury increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
