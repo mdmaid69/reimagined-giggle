15ddc39b8cab6c1ce20939dc94af6311d2dda368
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:58:22 +0000

    turnoff word thankyou benefit suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_int(array):
+        return int(array[0])
