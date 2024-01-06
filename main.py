99da500fc4ddbb09fb98496fb6f3c15b088142a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:09 +0000

    feel lookafter leave their just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
