aeb0ec80f919535827f55be77bf8f10de253ed02
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:34:41 +0000

    worry difference can worry wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def square_number(x):
-        return x**2
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def convert_to_hex(n):
+        return hex(n)
