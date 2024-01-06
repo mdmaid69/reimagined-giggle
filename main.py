8415ffb052f587774049ae65ae2becb553fab643
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:05:21 +0000

    print deliver shock position getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
