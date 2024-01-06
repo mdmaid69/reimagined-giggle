0bdcb4fa4bd33904573cb6a55577503ec6360a01
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:40:07 +0000

    as scheme food pick cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
