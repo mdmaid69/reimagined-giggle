f8e6dfa08f1740f71413b6243ca3a93cba4ea630
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:26:40 +0000

    claim character yes power think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+x = 10
+y = 20
+print("Sum:", x + y)
