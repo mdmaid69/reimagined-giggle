5f82850437fc8556d83d0f202d55a5b61e461530
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:35:55 +0000

    breakup serve block promise stranger

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
