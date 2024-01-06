84b723373043da9ab6e351bfd83344f00d67b6c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:00:51 +0000

    wind boat interview unless nervous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
