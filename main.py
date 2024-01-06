46447f67787c92898ff4ae498c31dab7cd159798
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:41:04 +0000

    embarrassed possible come feeling pay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def is_palindrome(s):
+        return s == s[::-1]
+import sys
+print(sys.version)
