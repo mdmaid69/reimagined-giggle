359064e671369a3d8fe64a1c828baa99bc4a5161
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:40:57 +0000

    fasten accordingto morning wind culture

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
