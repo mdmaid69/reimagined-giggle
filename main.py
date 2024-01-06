b9fc8ba11dd6cfc63180e08cf717cff4f96be966
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:49:54 +0000

    confused neat prevent middle union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
