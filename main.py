2fda7b307c20d3aa401b2561bc01fab6eefc63f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:22:28 +0000

    provide proof sleep excuse independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
