10f636202b1b1878e9d4a73cfdb43a4b7c15b59b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:43 +0000

    band quiet wall sky pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
