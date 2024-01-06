a428c11c042cb7aa00e0ec15df9598cfb6ef06f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:24:55 +0000

    married Qq finger reason acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
