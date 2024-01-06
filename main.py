c28364ffea533b82f7b6eb028111501988cc15d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:39:21 +0000

    rank me quality manage bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
