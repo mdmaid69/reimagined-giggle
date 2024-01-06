72d377508f99cea24f9027585bf3072457de3c07
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:00:21 +0000

    constantly such bringforward light there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
 import array
 def get_array_index(array, item):
         return array.index(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
