cb89ce4ced62a3b63e6f92efc634236ced4b9dac
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:18:55 +0000

    fire sore speak spot after

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
