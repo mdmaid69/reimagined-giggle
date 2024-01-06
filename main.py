02238ffcf1a101070b0509e10238c616cbd5d328
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:36 +0000

    present know know pretty lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import time
+def get_current_time():
+        return time.time()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
