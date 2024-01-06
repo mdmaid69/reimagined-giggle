94dea32a78244fe5ac4bc60a8acecbbb1cb74bcd
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:36:20 +0000

    makeup sore talkoutof protect arrival

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def convert_to_hex(n):
+        return hex(n)
