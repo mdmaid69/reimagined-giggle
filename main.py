3622fcc8b926057a0b2534ab1ec298695d5e9285
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:50:56 +0000

    prize last giveout declare no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
