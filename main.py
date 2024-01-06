0ea8e9331421b2092a8ba5e60b8e062f1fa742b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:30:12 +0000

    come mainly preparation refer help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
