e6c3ae35ff26c740a5bf620ec78cd05091b02c02
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:14:05 +0000

    future common name country blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def is_palindrome(s):
-        return s == s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
