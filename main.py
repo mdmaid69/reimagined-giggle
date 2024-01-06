0f9d98622eb7e49bd9546ca4010090ec2860627b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:25:48 +0000

    dance circle prevent platform in

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def get_time_since_epoch():
-        return time.time()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
