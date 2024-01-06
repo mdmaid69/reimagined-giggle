c6d84d9d191b6cb9e00142b60fb617a9c0442e25
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:57:32 +0000

    combination bother illness complain calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
