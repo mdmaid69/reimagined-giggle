d34c6cec1423e0e682ce63b11e06b6056fb6db2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:43:28 +0000

    geton limit must mind bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_time_since_epoch():
+        return time.time()
