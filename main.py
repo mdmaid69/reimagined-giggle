62f391eed99eb64dae80135c9afcd4ca0dc5d182
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:53:23 +0000

    apply murder matter Vv comeout

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
