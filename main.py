aba0feff90771dcc5b77f6f90db72464a8517ab1
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:26:31 +0000

    nurse him wait crash chief

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area(radius):
+        return 3.14 * radius * radius
