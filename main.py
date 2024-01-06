5e823a76362f596410db6c3c342cebfe6d8fd108
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:49:26 +0000

    subject Ff land male live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import datetime
+print(datetime.datetime.now())
+def sort_list(lst):
+        return sorted(lst)
