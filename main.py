b29b1adf9ad5667b61ebc5fc03286551d7e5f2dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:46:23 +0000

    capital type tap means minimum

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_index(array, item):
+        return array.index(item)
