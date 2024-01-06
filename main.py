d9de61ff63052a478c61cc85800954477a53168f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:48:00 +0000

    season score whether hollow main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
