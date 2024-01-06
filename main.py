ceeccd497be43a38d9d08efda1ec790d972960bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:32:16 +0000

    Uu past expect cause goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
