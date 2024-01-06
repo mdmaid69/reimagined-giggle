823d9cdad6d26632602b629d2ec4430048638621
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:41:46 +0000

    while display stand job lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
