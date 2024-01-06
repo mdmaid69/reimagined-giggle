3d98831c6cb3dfe048b73b2b566f5b3943c4d547
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:54:23 +0000

    board road total peace difference

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
