a4978d1392e88ea15110bd2ff29b9721d37725f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:14 +0000

    interview stranger target bang interested

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
