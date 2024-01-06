6565129193da4ebc2c2b2243151c6a6ad5c7088f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:17:19 +0000

    patience spring quantity world borrow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
