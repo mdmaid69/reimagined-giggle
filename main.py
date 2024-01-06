03739227650dd7ba0bf5bd05a3b3872a63a4750e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:20:26 +0000

    duty wonder disgusting artificial steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-name = "Python"
-print("Hello,", name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_python_version():
+        return platform.python_version()
