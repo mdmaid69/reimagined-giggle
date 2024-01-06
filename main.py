76edddf4d2e78328717460415543cbfb1b21ff6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:15 +0000

    which certainly fun fast remember

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-print(random.randint(0, 100))
+import array
+def reverse_array(array):
+        array.reverse()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
