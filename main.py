3b9d617b1dc5367acc79206a6945d7d068fb6c8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:21:34 +0000

    explain custom copy suppose brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
