8c95f24affbdb051216594e909f4255c1c221eb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:18:27 +0000

    both circumstance broken cheat discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
