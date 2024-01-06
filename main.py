d8d8f5c074b6daf1a98f72b44ab17385b6bc72a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:57:02 +0000

    office concentration thinkover improve pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
