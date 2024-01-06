26cf3296254d345faf75b81cf15bfaa2209ca144
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:35 +0000

    press nigga lookafter flat suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
