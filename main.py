c1952eda14d58706e8a92f882e5e5b269264dd69
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:21:05 +0000

    hope fall lock embarrassing ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_typecode(array):
+        return array.typecode
