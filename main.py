b41243d12100193dfae6b6557c6a12d66b4c2ce7
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:07:35 +0000

    concerned simple no hollow advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
