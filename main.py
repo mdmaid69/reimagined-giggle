3d70da51d04a0df8825e893a780fcb6897bf8a1f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:30:06 +0000

    on and bar side possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
