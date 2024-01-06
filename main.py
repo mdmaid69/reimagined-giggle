4e795c199e3f364ff8c73c224e7b959e8c1ca32b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:31:50 +0000

    increase whoever answer hurt personal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
