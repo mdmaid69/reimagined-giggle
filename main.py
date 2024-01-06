ecc8010c856e1a2c446f501cfdaffd33962c5144
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:09:12 +0000

    software hide account cheek public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
