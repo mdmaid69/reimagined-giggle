7809aaf270274eed49cbd00d953718edfbaa80e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:08 +0000

    movement zero fond cry after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
