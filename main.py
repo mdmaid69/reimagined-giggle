407f8acbaae76001e8ae139afbecad25adc2ce3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:27:42 +0000

    report neither field almost swimming

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
