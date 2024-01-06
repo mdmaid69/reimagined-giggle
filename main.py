6fe48e45fd9d97b0858033fa69db7171a7978b75
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:24:22 +0000

    such black debt attempt basically

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
