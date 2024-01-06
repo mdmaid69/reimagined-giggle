6401868d48506a3dd5a5a439dde478b87faa7540
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:27:26 +0000

    somewhere treat average sky turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_array_to_bytes(array):
+        return array.tobytes()
