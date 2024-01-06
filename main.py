28b5ada1cbcc0370dd18e8735b9cf2c39baeb197
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:01:19 +0000

    computer should central programme worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_string_from_array(array):
+        return array.tobytes()
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
