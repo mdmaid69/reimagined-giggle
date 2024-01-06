0129d0f14e6cdf5bc85e7ea95f311b7f5ed0ffbf
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:47:50 +0000

    medium objection refer hard business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
