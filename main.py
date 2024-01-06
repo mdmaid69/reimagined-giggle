3c77d6dc55cd7de92322a0e177ca62b566bb9fa6
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:22:27 +0000

    frighten circle talk deep breathe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
