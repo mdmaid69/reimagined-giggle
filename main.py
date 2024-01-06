3d69b887590a10c00e176d8e4f818839eca37c43
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:22:40 +0000

    foot holiday contrast generally comfortable

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
