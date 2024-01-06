315020e1bbc1a1e5b022edbcd9cd49f7b117e5a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:38:35 +0000

    extremely count sweep window talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
