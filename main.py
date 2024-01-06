8cf6371b9ed400088448d625ef00c5e88213ff93
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:00:53 +0000

    clearly official limit 573entertainment pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
