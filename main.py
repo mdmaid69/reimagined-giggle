0b40e3c100913e0b5ab477e5e5d11be703b38cd7
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:47:59 +0000

    bury field advantage grade considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def remove_duplicates(lst):
+        return list(set(lst))
