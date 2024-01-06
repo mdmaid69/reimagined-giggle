4c26e6519df3c68ffc37edb77068c8ad33439290
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:59:06 +0000

    pipe move neither yesterday cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
