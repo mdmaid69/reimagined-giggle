d52c01f8913cac1e87764151bb9111fe0320b51f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:08:14 +0000

    post amount choice circle basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
