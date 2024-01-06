47e6e02733046117198f26d03cd7ef083a00d377
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:57:42 +0000

    frighten I fire impressive fence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import os
-print(os.getcwd())
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
