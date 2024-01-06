e729e43cefd1e581ff53d780d43490b25d07ef3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:10:29 +0000

    branch wall except strength dig

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
