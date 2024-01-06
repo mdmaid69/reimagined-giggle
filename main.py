09d305c94b984cff5a80871a3ea4274f87dfe85b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:42:31 +0000

    speak his frightened mean read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
