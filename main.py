8d36bac0c3cd5e4a2dc8a8e176dc49d3a9bd05fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:43:12 +0000

    pump save pretty attach concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
