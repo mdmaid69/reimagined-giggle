8ac67111497621e5055ce0e92b2d50fa84768f60
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:58:42 +0000

    false soft properly popular turnoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
