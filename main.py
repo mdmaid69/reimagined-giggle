7a97fd87c98cd61db361e5ac78be400a95b9945c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:55:04 +0000

    fix invitation get stir model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
