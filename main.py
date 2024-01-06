b7d240747892a06c7d440c2e2ac64e63c33d3169
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:05:23 +0000

    fix wire forever punishment passenger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
