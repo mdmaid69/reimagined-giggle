3bbb91ce90bf071c08e048cb83bee6ab0953f3c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:47:12 +0000

    complicated tip damage cost cough

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
