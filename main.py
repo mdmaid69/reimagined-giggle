e1e83f5669ec28df87b53ddb8164e731cec39d98
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:34:12 +0000

    exam boring interfere broken way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
