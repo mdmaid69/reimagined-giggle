f640271210bc82adbfdf9bc31447faf20ded7660
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:35:28 +0000

    bit spring who cheat space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
