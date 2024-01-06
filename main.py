901654f35fe1fe1ae51bdb28d1bf8f0b41bad0fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:55:36 +0000

    bit wait aim could means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
