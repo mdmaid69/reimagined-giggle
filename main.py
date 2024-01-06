def7f34cdf4ae765611c4ede3e13e78ba3d84d30
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:44:34 +0000

    among aim day than unless

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
