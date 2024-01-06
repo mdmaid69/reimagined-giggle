6fc203545974dd28b7365142788ed05927a461de
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:13:56 +0000

    call collection pure convenient flood

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
