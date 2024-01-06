edf80dc2839fc181f844b2962bff7b32409ca48f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:11:30 +0000

    nor bus dress light trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
