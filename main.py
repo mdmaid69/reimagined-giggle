f82cc70ba8f545df7a6ea2cf6b15fbbb9b261bce
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:29:23 +0000

    score print special strong chance

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def convert_to_binary(n):
+        return bin(n)
