1eb9706d4d983977ef22d5db295a83c53ac6cf5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:49:19 +0000

    doubt content best previous workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
