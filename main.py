fea62a5ed5cfbaf81cb9c985f2e58b7d2f348a0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:53:26 +0000

    world act shall spare bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
