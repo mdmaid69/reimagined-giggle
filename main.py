cb7fde0f85242134e72847e2e91dfe77717ea80a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:35:53 +0000

    ordinary spite pleasure manner prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
