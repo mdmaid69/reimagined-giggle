98e7bc87603a4ffb7e055f6935c111e67e57ef64
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:40 +0000

    plain last ability prize holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
