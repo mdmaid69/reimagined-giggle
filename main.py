1bd90eddcf9bea4dc9e690e54db6f96088b82cf1
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:22 +0000

    appeal loss pocket godown ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
