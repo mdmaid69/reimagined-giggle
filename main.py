1ce83bc2ce38e55f1c544f41410c7b0d63aa6f7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:21:24 +0000

    run development push nature desperate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import sys
+def exit_program():
+        sys.exit()
