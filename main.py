6f2bea5b2ea9798d04fb7b51b043ea293e05e2f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:50:02 +0000

    chance would rank trap window

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
