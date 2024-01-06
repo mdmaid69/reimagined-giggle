5065705abc9b412a040dc5fa83e99fea3afdc9bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:14:18 +0000

    chance wait illness sweep practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
