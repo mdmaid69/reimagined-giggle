dbff1d992f5220c685b62b3f90b95b0ef185843f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:41:24 +0000

    remove shop afternoon live loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
