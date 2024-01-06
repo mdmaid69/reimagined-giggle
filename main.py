22f526967a945df2bd44cd4d92685aa1b272e352
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:02:26 +0000

    mention sweet certainly gate unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
