add7b6dcdccb4bd0b1ad770bbb1a9b9c96a2358b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:23:00 +0000

    thin way recommend earth win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
