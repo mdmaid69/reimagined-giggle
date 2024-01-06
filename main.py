5d2736e8f29513da524fc1d7f37fddd640ad8cdc
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:23:07 +0000

    practice choice responsibility trade early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_speed(distance, time):
+        return distance / time
