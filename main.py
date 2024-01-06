e99177143af730f64d010deb0e797b7d6d8e1ebb
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:30:32 +0000

    meeting actually journey lucky seriously

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
