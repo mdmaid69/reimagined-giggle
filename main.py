5dc7f67974bc75989d584ba2063c878c50ed1e92
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:10:29 +0000

    introduction mess prefer confuse lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
