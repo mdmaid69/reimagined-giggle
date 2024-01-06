7e61cdbf20b5fc0d42faf3b592a140995a0a77b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:31:38 +0000

    stamp achievement answer together emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
