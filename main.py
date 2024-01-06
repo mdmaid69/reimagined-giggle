45241534908a321cab1ce5c17d4b604d97c8d9f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:24:55 +0000

    exist rich different face brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
