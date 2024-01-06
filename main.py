6d3d2281aadb27a9445563149ee3a976683ab16c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:15:53 +0000

    sea maximum aim original find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
