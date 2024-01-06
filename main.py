5ac74957ad6c40abd4ab16c8d6d7093db5e19b1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:20:50 +0000

    action extremely watch lookafter just

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
