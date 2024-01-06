1dc40b851aba03563abd4d6be25bcfb53f86c544
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:56:28 +0000

    guide reduce turninto release flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
