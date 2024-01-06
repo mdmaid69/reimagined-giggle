6ac60d731d04db5f476aa7c2a9847d61fe4940c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:13:14 +0000

    please follow mistake watch bring

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def get_current_working_directory():
+        return os.getcwd()
