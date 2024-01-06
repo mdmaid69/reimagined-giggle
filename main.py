32e873c48df4cd3e4cbf6ffce91e4a5189d4b3f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:26:32 +0000

    pencil form contribute another there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
