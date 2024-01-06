30df40ef90c9de35efcad3d99283ffea74d6498f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:01:49 +0000

    certainly decide put match change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def find_min(numbers):
+        return min(numbers)
