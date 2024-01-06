96cfebbee1c310d189ec7a321a3dbb8e867c18ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:54:05 +0000

    impossible able fashion make twist

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
