a32554c3b5fed2f6dad589e2334790f3f39ac6ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:41:29 +0000

    bread live main worst next

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
