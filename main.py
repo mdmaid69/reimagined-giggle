a23c4d3bc5b5c11dbdc931b88dbd796d9b47e49f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:01:21 +0000

    male tip actually satisfactory real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
