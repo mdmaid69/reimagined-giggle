c63632546af225070eea82e63968982a4e567840
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:32:02 +0000

    treatment now apart mail everybody

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print(sum(range(10)))
