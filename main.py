93ceeab72ea9399e4fa19e6e0067c7da6076dec9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:34:49 +0000

    question agreement sudden behind matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
