369b72e48f51b82f4bc4fbef0a10ef3f76529f38
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:22 +0000

    fill music relationship contain certainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+        return 1 if n == 0 else n * calculate_factorial(n-1)
