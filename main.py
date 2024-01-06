98861b96dec0265a058694912f3f52864ecd9040
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:02:10 +0000

    someone knowledge table hot experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_set(array):
+        return set(array)
