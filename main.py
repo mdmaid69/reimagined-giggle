0427bdc75f405e5c178d8bd9adb125b7f7c7fd72
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:52:04 +0000

    whoever sad her many off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_list_from_array(array):
+        return array.tolist()
