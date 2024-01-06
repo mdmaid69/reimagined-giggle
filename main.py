106aa52f8e8d60bd8679f0901288a71d961463f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:58 +0000

    difficulty stamp swim limit distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
