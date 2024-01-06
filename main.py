2e72d1117a3cb1145b0472dc21fa6a4bfcfb948c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:46:09 +0000

    blame machine mistake agree slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
