304615608ec42a4054204fb8de0a7a3254ddee51
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:14:55 +0000

    bang between that emphasize discover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
