cbf0e729f6f700cad8568ec949cf34374e636ffa
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:46:08 +0000

    curious concerned young near that

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
