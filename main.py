f28006c192892b5b1b3af3b03c3f47e7d987e24e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:22:06 +0000

    measure culture store need outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
