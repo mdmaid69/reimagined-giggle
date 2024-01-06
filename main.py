4153340f62e91bec579187a3306069eb0745b934
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:30:11 +0000

    honest strength unlucky progress rest

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
