e3a88895a9cedb2b0d69c536be8021fecd926fd7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:20:23 +0000

    now price crisis talkinto murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  def cube_number(x):
-        return x**3
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
