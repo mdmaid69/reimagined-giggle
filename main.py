878bb274fbd0d2a15cfc3964cd1894acfbe65d8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:06:28 +0000

    on single statement great yourself

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def convert_to_hex(n):
-        return hex(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
