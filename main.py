e805a27ca737bb58c5428a6a46d761f3ad5b80f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:09:29 +0000

    hide remove surprised kill interest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_min(numbers):
-        return min(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
