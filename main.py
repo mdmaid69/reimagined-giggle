905a8a577b89b23c1d3ff73385bc52588c096384
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:45:43 +0000

    moment neither shoulder suit win

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
