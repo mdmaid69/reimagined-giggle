f1de70154968a9a630315f19d48a26d6b6a784bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:32:54 +0000

    trade serious congratulate regional ground

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_odd(n):
-        return n % 2 != 0
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
