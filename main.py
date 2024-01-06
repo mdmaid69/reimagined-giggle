a421e0cac20e3978fceef4e297bf6a981253b2b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:05:17 +0000

    settle once trousers replace television

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_volume(length, width, height):
+        return length * width * height
