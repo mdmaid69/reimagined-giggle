3f1e8c595611ce7889eef56bff4e24649c17077c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:54:16 +0000

    steer change talk juice cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
