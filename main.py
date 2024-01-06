1bc5bde1c6039d116ec71abbc47e9b53cf31310c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:23:13 +0000

    hole dare discover uponon interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
