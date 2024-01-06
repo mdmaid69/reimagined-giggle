e2ccefd72a3eb5f4fb628a6908c0b32f344ce59b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:41:09 +0000

    morning up dance mean store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
