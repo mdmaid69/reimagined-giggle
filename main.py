1240838878a7c4028d73fd7fb5e01c159a026ddc
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:00:54 +0000

    later short authority tall complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_list_from_array(array):
+        return array.tolist()
