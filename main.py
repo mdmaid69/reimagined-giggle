441650d5c5654e6ecf124985ad5f1810e8fe8d11
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:46:45 +0000

    back word burst bye steal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def remove_duplicates(lst):
-        return list(set(lst))
+def add_numbers(a, b):
+        return a + b
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
