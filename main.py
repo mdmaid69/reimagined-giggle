8384b788fb1946710cc5ae15756216a36256ba2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:17:58 +0000

    murder declare hope entrance remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
