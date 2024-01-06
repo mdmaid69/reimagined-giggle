0f7f1f523b05dc5092dfe02f7a3fd41b62b508ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:14:22 +0000

    why such hurt previous secret

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import platform
+def get_os_info():
+        return platform.uname()
