ad4e957f95053fdbc07424d69d7bf30591e04e2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:18:29 +0000

    tread not niggaacademy boring thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
