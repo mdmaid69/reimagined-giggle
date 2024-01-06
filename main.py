ca0e91eb0d51ae1935eb63198450e9a91f7ce50d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:16:10 +0000

    wet answer reserve star estimate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+print(math.pi)
