e46958e345af7439e0899d8c2d26400291b511fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:42 +0000

    round settle challenge admire left

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+print(sum(range(10)))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
