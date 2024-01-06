ac2f4ee301c6234995fae5098ba71b1f724634e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:04:57 +0000

    poison influence turnover meanwhile still

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def sort_list(lst):
+        return sorted(lst)
