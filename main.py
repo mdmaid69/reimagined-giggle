f4abc47cb8b9d8c963ecd277fb4bea17bffd84ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:56:09 +0000

    however wind rob doctor experience

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
