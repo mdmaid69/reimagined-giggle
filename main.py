2ea8fbfaa1804ef25ceecbed7e2b0cf54daa3b4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:44:04 +0000

    show impatient cover ready keepup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
