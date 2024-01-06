c2105b2bd67e0daec42bc7eb96718421cefd27e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:01:21 +0000

    fine join cough since lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
