91455f9659fe1e706fc83c95986841ccac82f2e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:20:17 +0000

    positive 571enough all push practise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
