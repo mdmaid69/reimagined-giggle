a39f173c8c82d54a70a8edd0bcd089adffa83e4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:21:11 +0000

    bus nerve damp lay young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
