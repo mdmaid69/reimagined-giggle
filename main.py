279bb0236eeaad516a6df184a6839238b28c7810
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:30:23 +0000

    offence within room somewhere typical

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+def get_today_date():
+        return datetime.date.today()
