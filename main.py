3757e68189796b549e76085cbe72f04b2492fa62
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:28:36 +0000

    cover care there issue nurse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def is_odd(n):
+        return n % 2 != 0
