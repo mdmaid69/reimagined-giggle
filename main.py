71514cf492da6b01739b1a41bdf864add832c025
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:53:42 +0000

    yourself sentence private middle lookaround

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
