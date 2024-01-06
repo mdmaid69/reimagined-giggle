77178f21a5d9989ed2e84d0973f60ad5f6bc69fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:28:20 +0000

    once detail agree spoil ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
