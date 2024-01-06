6b130cd4a26fb14aed0e7ba74cc7a22461c38c13
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:31:37 +0000

    bother long down anywayanyhow base

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def add_numbers(x, y):
+        return x + y
+import sys
+def print_python_version():
+        print(sys.version)
