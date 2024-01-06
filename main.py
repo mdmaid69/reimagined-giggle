896a687d4f29da9467c14303c423c738187a4cc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:09:51 +0000

    dangerous emphasis flavour foreign choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
