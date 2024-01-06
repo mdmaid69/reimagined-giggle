2692cafb6eb564c51862175bd6f4c0441b3e2462
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:20:55 +0000

    relationship off give thanks pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import numpy as np
-print(np.array([1, 2, 3]))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
