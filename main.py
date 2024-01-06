71485efa1dbf340f7b5f983c7d9c53d301ada65e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:57:13 +0000

    through beyond morning worth whichever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
