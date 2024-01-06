0f8d47a34a57ed24834b9c3c1dce71d2a10b3c9e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:55:57 +0000

    advantage go cost connected upstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_floor(x):
+        return math.floor(x)
