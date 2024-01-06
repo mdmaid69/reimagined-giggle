ca1c17376648dbbe04db334f48af469dc01e71d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:22:48 +0000

    question border spoil pride row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def reverse_array(array):
+        array.reverse()
