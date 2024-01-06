a2417798e5fc87dc5cfb959036551d082ba8b42e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:50:23 +0000

    tough pour development test region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_density(mass, volume):
+        return mass / volume
