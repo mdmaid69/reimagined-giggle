a6e3d3492ef863a9acb1cd7c3f40f496927a6b49
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:50 +0000

    city or health persuade tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
