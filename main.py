bb831daf229b1d233eb1c7ab2239316d1e98d6d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:10:31 +0000

    gate responsibility achieve nothing protect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
