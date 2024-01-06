f80fd002028ea85b235f7384946c3635bd16d35e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:24:31 +0000

    obtain apparently action big holdup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_max(numbers):
-        return max(numbers)
+def count_characters(sentence):
+        return len(sentence)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
