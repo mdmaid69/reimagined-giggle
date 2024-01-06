ce55e95eb88387ee2b0a24c305303deeada27729
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:59:00 +0000

    spend alive soon hardly exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
