eaddabc1e9371a135fcc888ea56c2f5efd73186d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:53:45 +0000

    they late best journey opposite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import time
-def get_current_time():
-        return time.ctime()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
