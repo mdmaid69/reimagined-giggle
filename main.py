822140a22021e014244fe7b05ce98dc4edd57b3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:19:12 +0000

    pile fly attractive wait forever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
