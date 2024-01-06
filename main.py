da27cf5dfb6d7b3c7afd7a207225c1dd985df7ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:12:34 +0000

    style basic change consider the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
