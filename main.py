1928c4e78a975e25ba862a16fc66aeef0a5b17fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:15:16 +0000

    closely mystery risk next thirsty

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def reverse_list(lst):
+        return lst[::-1]
