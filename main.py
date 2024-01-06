66b2e65e79b6a8c3464c459e8f0b401a6b4c4767
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:07:43 +0000

    guess uponon slow uncertain contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
