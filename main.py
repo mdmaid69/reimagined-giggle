9847961aa2a523bf065341c8ec16d59b61ade87e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:15:31 +0000

    how mysterious name conclusion with

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
