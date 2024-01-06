94eae02acffae04bef443f067a04b32ce353ef5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:21 +0000

    check out inside fine pump

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
