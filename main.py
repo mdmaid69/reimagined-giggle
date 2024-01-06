96770c1a7ca6f7fead0839639da01fdd4842e41f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:42:04 +0000

    round hurt attach include which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def square_number(x):
-        return x**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
