bb701e64e7dda5abea011079dcb9ee95a62840cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:56 +0000

    unite somehow lose example chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def subtract_numbers(x, y):
+        return x - y
