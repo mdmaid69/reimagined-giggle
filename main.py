01f241483c7df56e810ba137a4ff7de3ac6f73f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:44:35 +0000

    whole punishment stop street shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
