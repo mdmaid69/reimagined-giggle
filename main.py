063a82195269afdf95dec4f72d268f81d16f2050
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:23:52 +0000

    shut punish support keep bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
