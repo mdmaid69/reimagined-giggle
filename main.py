6a454d923ed46e8380763804c6faa7251e421f5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:55:26 +0000

    excuse shout light health sea

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_repr(array):
+        return repr(array)
