95347f9aa6ddcc164aeb944b54ce017e736e85a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:38:17 +0000

    improvement be recent gap embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
