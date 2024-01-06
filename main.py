453ee3e8c7eee3c0abd2557bb544f93741c15245
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:18:35 +0000

    damp threaten conduct unit neck

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
