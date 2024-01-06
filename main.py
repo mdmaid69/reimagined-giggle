037cfedc03e797a8092c527da2fb358327483d5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:05:00 +0000

    deliberately do loud familiar drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
