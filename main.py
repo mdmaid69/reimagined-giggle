260840d4d7ad9423b65f431402599f2b4a6c33d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:12:20 +0000

    winter frighten national plant extremely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
