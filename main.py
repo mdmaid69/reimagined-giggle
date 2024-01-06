d20ef11b3fdd067d127b648a1c448a44c3b2c46e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:13:17 +0000

    ordinary straight able condition refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
