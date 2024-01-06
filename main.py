a0b720107be6f963d39913f65f8187afca2f9c01
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:08:50 +0000

    far fill match firm uponon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_item(array, i):
+        return array[i]
