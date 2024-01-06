70a6a11c4250f174dfdaa32081ebedfc6bd2672b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:08:26 +0000

    fall record within whisper seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_array_to_string(array):
+        return array.tostring()
