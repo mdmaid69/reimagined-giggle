85735aac048f01da2b8c9874fcd13552eb9d0e48
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:52:48 +0000

    child nerve spill post smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def cube_number(x):
+        return x**3
