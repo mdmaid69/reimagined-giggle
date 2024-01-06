64b322912d9137114eed9656082bb04c4bbbe255
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:28:40 +0000

    can all remember pause stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import sys
-print(sys.version)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
