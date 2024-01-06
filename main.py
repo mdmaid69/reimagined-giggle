b153cf6dcfec68ab6e5aaa191cca082ed8fc9425
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:42:59 +0000

    fashion lock rain steam excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
