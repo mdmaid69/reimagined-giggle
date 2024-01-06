a6598a2dd1c17838b7a2068287d2639456b8e901
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:34:30 +0000

    here physical beautiful petrol nothing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
