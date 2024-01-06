85bf641a4ef8d4eb81f2cf6fbf65decacb039b57
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:30:49 +0000

    project to regular smoke towards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
