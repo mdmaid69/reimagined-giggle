c68974625ba1dfc08b032a61a630635e6d4f096a
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:33:13 +0000

    reach although tune father attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
