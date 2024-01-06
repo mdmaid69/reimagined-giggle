0a5c42ffa63bd2e99fc5b33b3b8ad0ff4197cfa3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:58:45 +0000

    along lookaround instead matter distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
