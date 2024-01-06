9fdd157e3d35988680d915d2ca04235649a9b867
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:12:20 +0000

    song themselves never sign noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
