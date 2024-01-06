e49b123d9d137f71fa491e90fb9be262cd4fd090
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:19:43 +0000

    famous in repair night string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
