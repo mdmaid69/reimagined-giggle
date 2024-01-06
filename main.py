1f66eba95f46f463ca03ad04f78779de4f7b2bda
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:14:30 +0000

    dear careless less lunch variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
