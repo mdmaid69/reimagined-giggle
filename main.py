bfda7885eb43d22d5c354e931055705cc776d781
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:50:54 +0000

    entry twist brush split order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
