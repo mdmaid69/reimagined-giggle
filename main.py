c8c8a13008abdbb05ac34ac60bee3e2e78aa22ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:53:02 +0000

    dinner course escape laugh dangerous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
