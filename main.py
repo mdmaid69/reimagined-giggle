a9fb873dcdfaddad724a54bf70ee48c45625c1db
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:18:12 +0000

    Yy understand round Rr original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
