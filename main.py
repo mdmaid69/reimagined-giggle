cd10498ab5bd73764f0e57b335900be2434a88a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:38:42 +0000

    illness godown professional some method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
