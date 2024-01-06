36ca2deaa37dc9be98aa395df78fca084c0a0e86
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:57:21 +0000

    formal under prefer violence love

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
