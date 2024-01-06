84d4b9b8949209abcb21fa9fcfe913d20d8cbdb5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:00:55 +0000

    herself recover breakup peaceful necessary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
