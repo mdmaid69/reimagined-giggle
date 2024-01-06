cf96a32158946a414f766ab9b6517534daca4d64
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:28:20 +0000

    idea complete grand achievement hit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
