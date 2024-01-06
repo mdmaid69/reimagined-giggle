5b0d760ab5087b2497c1296267484d9984df28ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:45:33 +0000

    we different search on process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
