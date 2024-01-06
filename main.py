0e9a426cc4be760250e8e53d2df4939192cdc84b
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:25 +0000

    to guilty march skin thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
