272946518ad4621d899378de639ea327f2ea5bdb
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:14 +0000

    warning before worth crash shout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
