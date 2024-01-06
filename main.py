295e4c62d3034fcc9b2f919e0a8f1c2d9a27b286
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:47:05 +0000

    stamp fortunately after cheap cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def add_numbers(x, y):
-        return x + y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
