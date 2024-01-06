bb39edfc3487105a97a5dd88949b073e8bf63453
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:22:46 +0000

    still around surprising determined small

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-print(math.pi)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
