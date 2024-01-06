cc9953c2a40c841c327a289d5f024c615a80a1b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:00:52 +0000

    general so act tired hold

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_username():
-        return getpass.getuser()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
