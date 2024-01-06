c950ecb4870450cbbf2df0790cdd3d2870b0b452
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:29 +0000

    black worth record skill sport

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
