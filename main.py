f863d5eea23ae8aa58bbae7cee52a7230bb06c3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:22:35 +0000

    dare forget day limit giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+print(math.pi)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
