306954249d6b87d7deb5ccb26cba2ef4d518fac2
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:23:03 +0000

    press responsible play safe sink

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
