5466915a691bfddade387ed9408bf2830945977d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:40:50 +0000

    564encourage holiday again period slightly

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
