a362dfc57a87035db2fa1068a06fac3e824006a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:32:13 +0000

    formal complete near shut principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
