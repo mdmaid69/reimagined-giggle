f9cabc168afb6045c1323725b3a99e301b22802a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:01:33 +0000

    this pleasant behind accept want

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
