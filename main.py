9de7219cd7187e12d55ef75c5a0bac6a58f57425
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:04:00 +0000

    expression nose congratulations alive free

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
