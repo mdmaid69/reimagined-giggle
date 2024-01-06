338f1b637cb2b3b49abeb272f35a9a2a050eccba
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:34:59 +0000

    themselves murder increase born gooff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_repr(array):
+        return repr(array)
