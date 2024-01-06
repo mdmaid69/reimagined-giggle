ce5d23e9eac90e9bff21ab45ba835010350143aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:43:44 +0000

    time getout wave watch agreement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
