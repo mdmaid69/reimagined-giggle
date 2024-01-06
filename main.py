ac277df153d7de17813ab11fe1e3fea5cc6e6356
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:31:11 +0000

    emergency usually rest needle suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
