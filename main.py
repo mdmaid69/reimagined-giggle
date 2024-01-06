0353c8cb6bb950602eb0fb309e5bb4499f57a58a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:03:35 +0000

    community stop lead clue trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def count_elements(lst):
-        return len(lst)
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
