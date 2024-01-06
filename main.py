2ac47017b79da450fb5f22b83aeadb4dc05dd3f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:40:23 +0000

    hit strike crash grateful rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
