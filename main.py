5cdb6c243070b52863fae6ce6d8839c48b84289b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:21:46 +0000

    place priority combine difficult wing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_power(work, time):
-        return work / time
+def calculate_cosine(x):
+        return math.cos(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
