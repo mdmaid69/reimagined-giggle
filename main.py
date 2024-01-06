26c5bcf04dc42cad47973e14246631a608a2bbcc
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:36:20 +0000

    friend war straight juice tooth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
