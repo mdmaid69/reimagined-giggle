1713f0d025f5b7142636e2322f0905517b0246ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:26:39 +0000

    calmly unless effort culture second

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
