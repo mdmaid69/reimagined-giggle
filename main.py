881b1c7bed52b4036eebcc7931c422fcf2a5dcc1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:39:48 +0000

    Ff middle enthusiastic object sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def count_elements(lst):
+        return len(lst)
