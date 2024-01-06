5366c545f28ec7ce52e29733e9e7b8453a610e1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:51:38 +0000

    getout family straight pronunciation spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
