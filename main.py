2f3c352f48775a02ecb267b70a34e0e7f2b505c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:20:24 +0000

    duty term ill keepoff protest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
