a7e614e1a437e80432bad99851c60740255ba4ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:09:52 +0000

    gather company mysterious least she

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
