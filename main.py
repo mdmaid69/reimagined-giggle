6a6959663bae52d0ea1181014a8cc9ef9571740d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:08:14 +0000

    centimetre disappointed keepfrom wing scared

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
