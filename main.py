bbdac69a8635cb0f31233895647f87940d9df7ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:42:40 +0000

    combination degree backwards 572enter beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 text = "Hello, world!"
 print("Is palindrome:", text == text[::-1])
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
