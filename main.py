53e4ab0bee1cf1cc086f13bfc47b321aac57eaae
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:28:44 +0000

    odd surface answer fat means

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
