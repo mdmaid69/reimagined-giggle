af7a728cd789decf5b3f0b0b764d8857bb578945
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:15:49 +0000

    importance please call rule cough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def is_palindrome(s):
-        return s == s[::-1]
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
