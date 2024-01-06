19abc1743d832e08c17ea0c6ad21119d1247db16
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:50 +0000

    this round factory bank respect

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
+  def remove_duplicates(lst):
+        return list(set(lst))
 n = 10
 print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
