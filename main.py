e0d5ff6cd1030ed541f310ba66111d61a0d9b542
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:06:53 +0000

    convinced half bear many do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def log_message(message):
+        logging.info(message)
+def calculate_density(mass, volume):
+        return mass / volume
