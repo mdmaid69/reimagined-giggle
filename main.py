08038c6c3fddbdf283b6528d43005b22240ec7ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:06:54 +0000

    correct danger call reduction perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
