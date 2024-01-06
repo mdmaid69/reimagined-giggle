e0c356f847b9a71ca40894e0f21516f16861e032
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:50:48 +0000

    instruction tread cheque lost destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
