a5621bdca4d670562046b73a937cb6b67044a3b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:00:42 +0000

    especially spring emotion male part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def exit_program():
-        sys.exit()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
