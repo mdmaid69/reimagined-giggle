cdb4b0d5a5b846892a1e61a2ae3e0b2f5bbd9644
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:05:30 +0000

    stupid book walk read healthy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
