378df88875641e68928130d014fc0fdfd36b745a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:09:13 +0000

    really patient turnup shop needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
