18a3697dab1408e85070d0e36d628609bb4dced6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:15:26 +0000

    people far power else ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
