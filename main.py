59c14380c9f9631a0956910562576a8e95b6751e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:57:36 +0000

    inform turn need single lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_hex(n):
-        return hex(n)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
