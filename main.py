a188d5238f02a0042bd616d15d47afa0c6eb4ac9
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:29:56 +0000

    clever middle only human slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
