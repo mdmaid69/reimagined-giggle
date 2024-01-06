7713ca808b64329c32f8ef4c3be735617ca2964b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:20:09 +0000

    bet television anywayanyhow here accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
