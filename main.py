72c29c71eb95ae554083c72cace03ce83d7e8336
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:39:26 +0000

    exactly man doctor add possibly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
