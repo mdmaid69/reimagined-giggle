bf2ea8a7a25de007f17eaef8a6c549bb4a12ec5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:16:53 +0000

    flow formal fire foot getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
