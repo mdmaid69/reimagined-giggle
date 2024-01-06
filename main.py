8094d2e2a985c1069ac100af93ae2335aa66e3f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:34 +0000

    lead freedom represent physical only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
