e2238ad964dccba183edcadbdbf12703bf3de3f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:12:43 +0000

    influence guide character home rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
