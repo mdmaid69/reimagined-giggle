f8fa31271f6fb78654f6397000bdb23ff22fb896
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:56:15 +0000

    food permission thing custom criticism

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
