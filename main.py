1644e8153b2aa71120c29b1ea06d597456dc3949
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:13:50 +0000

    chief murder brush progress wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
