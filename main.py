cb9e8aba6b643944ca7cc454b650542d524af798
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:53:04 +0000

    draw fix partner preparation emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
