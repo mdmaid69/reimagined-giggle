5da4d65b936ef700ca842a5536e76543ba28865c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:32:50 +0000

    if passenger crowded square sore

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_pressure(force, area):
+        return force / area
