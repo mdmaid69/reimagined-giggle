5061cbc1615b38f2f7eb8b59ac7ba8dc8186e8b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:11:11 +0000

    offend show male job though

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_combinations(n, k):
+        return math.comb(n, k)
