b9a3c1640ae769be570b02505a8b448a4db219d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:02:56 +0000

    accidental behind take tense fill

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
