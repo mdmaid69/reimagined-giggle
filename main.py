0fbecaca88fbe2a02350da629235422b0e55d7e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:19 +0000

    university goback(to) effort this reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def add_numbers(x, y):
+        return x + y
