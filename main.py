2098f3f7b7179733cfb35fe9067238c9b1b88fb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:19:08 +0000

    fear press ever address up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import datetime
+print(datetime.datetime.now())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
