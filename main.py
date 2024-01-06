872737d4e753c9f446aa8cc29b4027955c0d6a35
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:59:00 +0000

    reduction scene female objection characteristic

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
