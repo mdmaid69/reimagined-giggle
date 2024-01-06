83369516c3374a727296f9310f5ea1f9fffe720b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:17:46 +0000

    aim cheat punish boil some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
