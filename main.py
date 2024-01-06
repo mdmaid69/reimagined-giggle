f875902e216d7da0ac43eeee620ed98ed2244d98
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:31 +0000

    sell ready official comeback invitation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
