3aa61be70ada57e9adbee091a1278d77ab30bafe
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:21:11 +0000

    flood colour tonight most couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
