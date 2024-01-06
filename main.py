f33f761e80a2266070049a691c7ae89391ae4bed
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:11 +0000

    university wash considering round yourself

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
