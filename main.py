54a57d9251d27e901a99b54237f65a07697d9098
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:46:00 +0000

    before size continue welcome soon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_force(mass, acceleration):
+        return mass * acceleration
