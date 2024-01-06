0a8e7b2da814474e7bc4eca724a0c22fee8ad955
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:39:28 +0000

    result entry bound through broad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
