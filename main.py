3ed9932940dc19a7116735fed6fce3c3a602a9c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:53 +0000

    suddenly manner personal cook intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
