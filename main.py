8c1c079620f4a110218873c70701832882085bd2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:02:52 +0000

    statement mystery comfort disturbing runinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def convert_to_hex(n):
+        return hex(n)
