507193cbd290039cae85bbfd974853577403e869
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:51:51 +0000

    across perfect suffer innocent club

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_binary(n):
-        return bin(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
