c9c80728804cce45d394e50824f7447da61402c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:12:27 +0000

    pin target direct independent tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
