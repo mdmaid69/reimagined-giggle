48544208fe250ba5e3e7dd6585dde7f4bc681464
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:24:02 +0000

    principle wealth now hit smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
