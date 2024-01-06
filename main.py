e6df7fafcd3a56ebeda9ef1ebcaa623fa5e5ac39
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:58 +0000

    comein careless solve mess anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
