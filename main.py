8cb19a1559367715cb43b3cdd7b8a58554fd4b1f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:48:50 +0000

    accidentally behaviour believe responsibility fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
