c2b03091743e4955895492258c180fbcf25e771b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:14:48 +0000

    alive mine unkind shallow hollow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_work(force, distance):
+        return force * distance
