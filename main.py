8c435b64ca3c6e213bb0be0003846e2218bfe5f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:20:52 +0000

    friend apart little interesting kind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
