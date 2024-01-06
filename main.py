efc4191ededae2f12bf0f143b12b4266f8e52d1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:02:09 +0000

    convinced will reach very quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
