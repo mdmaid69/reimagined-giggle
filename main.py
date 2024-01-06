0f7bd6b8c5871001b55bd618cc61eb706f2bc58f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:38:07 +0000

    large search treatment runinto breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
