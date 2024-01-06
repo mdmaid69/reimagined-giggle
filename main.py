10b09cf0544469aa2a3e3dc8dee3f574b83b4d67
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:42:24 +0000

    towards worse argument moment ashamed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
