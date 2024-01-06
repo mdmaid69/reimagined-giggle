68dbddb1581e91ecc09de871c9fe58826d993b3f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:16:44 +0000

    file save takeout 564encourage near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def find_min(lst):
-        return min(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
