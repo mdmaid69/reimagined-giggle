ac6cb9547e2f89c1469f82ffec6a2277f585a835
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:19:54 +0000

    welcome spoil drink taste desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
