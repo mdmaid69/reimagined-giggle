73c8f5baaab05edbe62a56b2879dde59bff019d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:23:32 +0000

    talkinto bury off put spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
