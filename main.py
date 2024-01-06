88af9d3c19b1fa5a8dfa7d6ec0a54ec4e9de1586
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:28:12 +0000

    some home attitude buy ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
