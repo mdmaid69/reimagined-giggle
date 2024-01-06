6964d4249cc4732323a163e8772811e32fab17ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:26 +0000

    outside voice Gg round strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
