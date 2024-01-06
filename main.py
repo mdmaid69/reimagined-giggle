7b357b97ba502ebbf3bf360e2a761ef8e4463bf4
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:27:00 +0000

    justice present instrument border undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
