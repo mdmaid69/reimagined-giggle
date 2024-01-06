008c2fbee7ab29a373624a00b10f10de42d2e12d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:32:05 +0000

    tread giveup crowded amount clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
