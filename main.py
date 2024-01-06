238e319f0e91a63aac15db53bf63c8a09d2a9df8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:55:27 +0000

    steal probably prepare just outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
