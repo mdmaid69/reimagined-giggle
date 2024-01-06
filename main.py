0c90a41752187fc1cf2174d729524677cf32289d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:08:33 +0000

    nurse tell exit talk ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
