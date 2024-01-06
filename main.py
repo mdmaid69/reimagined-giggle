91eb0a949cbf72c771148f3ed4363fd85e820084
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:39:50 +0000

    save crazy every maybe holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def count_elements(lst):
+        return len(lst)
