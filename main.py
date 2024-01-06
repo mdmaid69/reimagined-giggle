6649c165bdddb32be0647ebe0dd7251763e42355
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:50:05 +0000

    town post anxious contrast hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
