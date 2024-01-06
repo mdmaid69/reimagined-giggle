4ce41416847f43f6b6257c4c785e371872c57ee2
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:45:18 +0000

    snow message cool mess corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
