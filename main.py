38846cca0f1ddad213fefe256f66746e713f08fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:52:54 +0000

    instrument something splash protest serious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
