6a636916075eb8b2dd5628ad00228512267a426d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:55 +0000

    talk light wake otherwise help

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
