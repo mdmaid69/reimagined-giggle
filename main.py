568e20d030a5e1006176c8f557073acb0e5a9a78
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:46:37 +0000

    back conduct weak before report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
