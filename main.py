a77f03ff12cfdc5f57e71332d3e0e33fc8a07b3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:28:47 +0000

    fly shut which switch schedule

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
