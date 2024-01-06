b3970520a492f1a72314d69fae8ff62bb99fa492
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:48:05 +0000

    discuss sort rest risk sleep

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
