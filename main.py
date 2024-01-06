234bf05b26084dd4548472f2d576c8efb02838f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:50 +0000

    majority getaway relax pronunciation desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
