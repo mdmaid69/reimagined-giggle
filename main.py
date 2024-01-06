61c76aa56d8e46e2fa4279803dc30b727e4163c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:06:32 +0000

    age burn turnback battery Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_as_float(array):
+        return float(array[0])
