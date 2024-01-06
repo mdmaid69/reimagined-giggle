dc4c128ef36848f51b9b42d07cb91dc5b4a308ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:30:35 +0000

    collect either official suddenly reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
