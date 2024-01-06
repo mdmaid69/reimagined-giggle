f75f758512163efc13c9ad802553148e36fe2ac3
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:00:09 +0000

    under force long hand continue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+x = 10
+y = 20
+print("Sum:", x + y)
