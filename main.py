b8bbc6cad7327171ad817caa9ee37da95528557f
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:07:10 +0000

    determined show attention pair keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
