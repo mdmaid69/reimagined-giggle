46128d03ce14133a500b49b74d9cd4013943c896
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:06:27 +0000

    actual hour everybody getout activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
