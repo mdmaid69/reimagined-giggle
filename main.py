5d2c582451311c5fa38e3a2b719724223e180f33
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:49:31 +0000

    achieve environment case hard repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import random
-print(random.randint(0, 100))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
