3ceecfb40fe57408a057071df9535ec2fdb92a25
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:06:15 +0000

    ignore fear arm side believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
