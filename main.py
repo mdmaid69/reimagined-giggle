43567fe428a9a9b00d034a3a71350ba012086707
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:42:44 +0000

    event defeat forget neat soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
