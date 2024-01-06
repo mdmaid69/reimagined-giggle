3c344c94ee3a16a5542dbf1e209fbe2aaf49521a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:23:22 +0000

    unpleasant easy include anywhere interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
