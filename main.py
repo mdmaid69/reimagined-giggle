6e0325b89e8e613ceaad6d8f8f40b98f5f17795b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:11:54 +0000

    quick objection accidental send boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
