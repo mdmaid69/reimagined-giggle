c33b33fe4bba5a655051ebd2aeb043a6fb0001df
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:46:02 +0000

    failure reduction exact brown wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
