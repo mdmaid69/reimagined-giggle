e661434d21c7f5f22d93583f96afc6f2a0a2dfd4
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:42:07 +0000

    case roll create exchange variety

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+print(sum(range(10)))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
