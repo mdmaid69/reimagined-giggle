87d3346db42cbc3a5a063263a7b0304f4f1df374
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:34:03 +0000

    them ring dull increase their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
