5f95988cf97f4a7067f4c46d08d45eaa07194685
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:48:31 +0000

    rain condition object swallow cruelty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
