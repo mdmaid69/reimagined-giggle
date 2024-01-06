0e747c4d1fb84ec8f530e7a45b17f93abb4b2e22
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:15:58 +0000

    duty add cost awake hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
