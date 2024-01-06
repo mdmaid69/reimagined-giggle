d52b687c48f9bba3127cdab14b19b46dceffead4
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:56:31 +0000

    repeat great announce its with

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+print("Hello, world!")
