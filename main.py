356d0e3aa6a819858a5012058d2d6b3d1e0619b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:19:38 +0000

    coffee comment petrol know trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
