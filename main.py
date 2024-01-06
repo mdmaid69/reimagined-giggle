13a571ecf2425c0273bc1d2ba73379b571d13cac
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:24:47 +0000

    badly present neither bin nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
