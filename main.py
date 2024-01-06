a8657b07e03d4c7e107c4de93d3e71f8effed810
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:40:54 +0000

    Ff become point push breakfast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   def find_min(lst):
         return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
