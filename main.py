b8e46f50737fc33cc7aff10c31f8aa2a2828cc37
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:58:44 +0000

    criticism getoff probably murder drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
