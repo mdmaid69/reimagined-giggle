aead481b15de177a124c5e22b99aadfc13019352
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:46:44 +0000

    excuse since environment brain use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
