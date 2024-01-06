b4441b968ba90ff87b834014ea57d6c7342020ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:52:43 +0000

    sink limit wealth shop guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
