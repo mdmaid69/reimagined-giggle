988c0d3450fede289ec07632874408efa3f9e7b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:57:24 +0000

    sensitive but cheap unlikely poison

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
