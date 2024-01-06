1a7b31d74754767be204b76750a90553a546d51f
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:20:11 +0000

    hurt fail deliberately exist fierce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
