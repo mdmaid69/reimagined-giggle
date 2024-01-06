0f0e78186f23935ded3628b73438952aac30449d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:23:37 +0000

    achievement doubt embarrassing compete have

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def add_numbers(x, y):
+        return x + y
