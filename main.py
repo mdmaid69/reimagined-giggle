bd28ba476532c4a88af1616a5bf3603f0e1c442e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:55:50 +0000

    convincing fly border meet together

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
