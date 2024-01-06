66a2e2521c655c34f056d8b4bd9677e82b7b97d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:05:38 +0000

    conduct use piece country tidy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
