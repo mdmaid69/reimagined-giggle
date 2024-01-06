c3a41edc2f81ca50c68c4a7ff647328488c2c90a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:35 +0000

    press risk brush correct food

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
