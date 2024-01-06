5d7ca2bcca4200bffc16d2d276fb7b85fe416ac0
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:27:27 +0000

    come trade emphasis ill copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
