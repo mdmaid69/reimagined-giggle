f3a118d22c263f7af51c39a89855918344728099
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:58:14 +0000

    listenfor makeinto blame crisis cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import platform
+def get_os_info():
+        return platform.uname()
