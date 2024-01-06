d19ba4b82c487a3f6c3661ee920e89a4dd70da35
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:06:14 +0000

    hear suffer inside fence weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
