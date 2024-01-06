0f80abe638089ce12be32bedab798440dc4dd8b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:35:19 +0000

    set rescue behind office favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import datetime
+print(datetime.datetime.now())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
