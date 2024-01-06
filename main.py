b6db401036699cab7c9c5c0a1e8f9d31dcdc6c78
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:34:44 +0000

    boat community fear hurry however

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
