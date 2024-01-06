57e54fc751445d2218a1386193aae710dfb2901c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:44:06 +0000

    friend rain shine professional jump

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
