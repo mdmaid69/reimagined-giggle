44e7977cc923150015d3720c7bda23c2bae81f5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:46:16 +0000

    this cry length runaway ignore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
