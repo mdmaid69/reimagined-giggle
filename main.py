21b458471788c9131f7e38a42330567817242bf9
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:04:42 +0000

    slide smart plain interview there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def roll_die():
+        return random.randint(1, 6)
