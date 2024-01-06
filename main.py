1851886cc498a4b14f4e3700084e2a20e2b37309
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:17:38 +0000

    holdup precise Jj steam here

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
