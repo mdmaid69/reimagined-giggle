6a55a648843cce220acc97b74fbe08578cd123c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:43:57 +0000

    represent or speak reply blame

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_current_working_directory():
+        return os.getcwd()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
