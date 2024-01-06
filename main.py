c10b2e8fd7c9a72bdb645aa132ccdc352d5dc73b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:16 +0000

    flow trap goon tape difficulty

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def convert_to_octal(n):
-        return oct(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
