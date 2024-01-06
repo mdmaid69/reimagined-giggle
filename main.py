c34bde5347e8291ea6dcf3de4e2fbade4351c72e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:13:34 +0000

    angry nearly realize summer night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_list(array):
+        return list(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
