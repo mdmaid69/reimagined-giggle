6a2e078acfca7e79d55b6268921c071b1bebfb71
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:37:23 +0000

    thinkof noisy spin blame big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
