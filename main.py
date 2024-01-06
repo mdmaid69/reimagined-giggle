4d90645a063b481ffab1cdd48ad182205588be83
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:44:37 +0000

    without bear instead front possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
