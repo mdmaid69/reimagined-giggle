807066937920a77bea6a0442d860e0cca3be48d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:54:58 +0000

    none expect preparation measure purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
