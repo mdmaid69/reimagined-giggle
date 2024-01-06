b7767702fd7c3c0fa13132e15d711dcc573f3982
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:07:37 +0000

    wrong fortunately here geton lonely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
