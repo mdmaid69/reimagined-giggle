e7033b898499cab347f737826cefc568fc0d0002
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:20:02 +0000

    hour possible mean negative relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
