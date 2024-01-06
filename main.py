5c8c81a0e2511bc2cd1890dea44611f3b70c4633
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:44:58 +0000

    influence minor impression return seat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
