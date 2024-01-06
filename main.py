4e7c86ea3df40dd44e6e82c2f3627b95ef970ce7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:30:18 +0000

    related family ignore responsible round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
