2685421bf3f13e2a02a509e5750993cbc450eeca
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:03 +0000

    injury steam child peaceful fat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
+import json
+print(json.dumps({"name": "John", "age": 30}))
