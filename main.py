95dd04d3c49b10d890bdc4ef65c35c12a6b01bae
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:51:55 +0000

    admit lower battery few level

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def count_words(sentence):
+        return len(sentence.split())
