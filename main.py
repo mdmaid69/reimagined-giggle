01af8ae0eca22f1fabb14b395a77eb3e969825ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:50:40 +0000

    throw excellent as face runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
