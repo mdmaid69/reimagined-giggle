732fdd241e5ea3ceb888529b403f755bb1b1173a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:44:54 +0000

    important mouth clue intend splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
