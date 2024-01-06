3aba69e918f31cdd62227b5b71b417525f18a410
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:15:12 +0000

    basis stranger transport keepup damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_length(array):
+        return len(array)
