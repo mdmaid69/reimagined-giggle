c94fcfa3f877fb0e003d84ba22e0682a70599376
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:48:34 +0000

    arrival whisper sore impressed stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
