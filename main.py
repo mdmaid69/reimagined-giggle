2a84e4f142c72d41289ccf352245c83141e5b514
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:38:54 +0000

    Mm stiff although physical fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
