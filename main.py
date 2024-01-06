4a0e9402ca4252b117c2460638491abbc1489180
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:06:07 +0000

    of risk flat invite father

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import os
-print(os.getcwd())
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
