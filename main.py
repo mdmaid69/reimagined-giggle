92b098624b854f21107a0b397b465d8dad9388f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:52:33 +0000

    petrol go touch hurry war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import getpass
+def get_username():
+        return getpass.getuser()
