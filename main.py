8e685fbc63425bc15700e4bdbc7cf4379d28de76
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:31 +0000

    surprise burn workupto top wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
