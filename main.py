f57503129d8909ba92fab55c2683f246dd849ee7
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:46:08 +0000

    price imagination some neither scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        print(sys.version)
