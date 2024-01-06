b19714d4bbf491dd97cf645b2a2467eab40889c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:09:04 +0000

    power concerning obvious dream sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
