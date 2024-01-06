ebb6584d9f52a1221e0b95a7ec510a660945f647
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:00:56 +0000

    machine everyone disapprove pleasure get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def sort_list(lst):
+        return sorted(lst)
