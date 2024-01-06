5802529030a4321dbfcd968dea5befa6d0aa3af2
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:29:26 +0000

    operation present sort draw port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
