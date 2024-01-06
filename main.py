250d4b7caa29ade980db91b6a43f9e6ed8826c69
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:47:25 +0000

    564encourage pleasant shape upwards room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
