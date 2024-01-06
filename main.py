dabb19f53a46a63c346efb54a24d86f36b00b619
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:02:55 +0000

    accidentally serious calloff by estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
