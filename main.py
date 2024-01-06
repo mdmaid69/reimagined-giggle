e5c920859317375dddee3b3bf15e7661f23b5b41
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:24:42 +0000

    such wealth wander rush desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def append_to_array(array, item):
-        array.append(item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
