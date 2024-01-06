266d5abbc1eb8be3d134a301d4eb995b5bf9a025
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:27:15 +0000

    condition harm slice during strict

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import datetime
-print(datetime.datetime.now())
+for i in range(5):
+        print(i)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
