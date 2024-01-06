9b032173615d833c77e715ebd98f96e681785a0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:36:05 +0000

    wait give ocean follow achieve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
