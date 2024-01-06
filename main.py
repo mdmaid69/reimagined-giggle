a90dd08301f4fe156a7158dfc894f531612060eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:52:19 +0000

    politics 179bite getout usual capital

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
