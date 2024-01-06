6edc5d806b8c81ca0a558f2a9599a4bdcc2cb140
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:43:38 +0000

    comeout suckme confused season summer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
