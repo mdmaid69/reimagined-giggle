07d9b193c69ac3f5f70ac054629295b6b9267e89
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:58:28 +0000

    success breakfast late reserve ring

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def greet(name):
+        print(f"Hello, {name}!")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
