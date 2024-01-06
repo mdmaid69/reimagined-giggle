6d8c4264daa49d9fd14311169eab4dfa3980e6be
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:16:32 +0000

    goout anybody uncertain crowded compare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
