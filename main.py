6ac9e112f36c2bcf263332a4a21b12ec2066dd18
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:16:34 +0000

    bringup repeat usual whole stamp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
