fd1bec7ae8f9a424bee7780b03cbc2bbc03fd33e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:48:52 +0000

    friendly exactly thick to splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
