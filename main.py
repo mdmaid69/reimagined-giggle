4b84a7289eed367426e70227d7945dde7344f2c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:45:46 +0000

    afford related save violence real

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
