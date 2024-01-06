d87e863c4fe0332eceafe387f2b60e14cea30271
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:39:35 +0000

    shock recently distance talk contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
