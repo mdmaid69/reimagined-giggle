01783efa7a961e7f86c8465ad915efcdb6f42141
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:08:45 +0000

    use no return history defend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
