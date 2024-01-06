97900d2792e64467223f5696bc3a7abb37244002
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:05:10 +0000

    age bringback leg secret only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
