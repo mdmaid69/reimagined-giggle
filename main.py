0f7298eb6bc9a9aeb19ab3480ca69ff6e2b4cca7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:49:33 +0000

    cheek 566end his uponon since

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_rectangle(l, w):
+        return l * w
