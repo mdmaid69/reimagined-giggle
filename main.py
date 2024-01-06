f53134120046f2ea1b4cd7f4a3a33312a993c784
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:38:34 +0000

    mark band school might cruel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def subtract_numbers(x, y):
+        return x - y
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
