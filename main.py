b109d6fb052fb0c712dd443fce1b84007b448124
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:21:52 +0000

    recover nearly identify receive advice

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def find_unique_words(sentence):
+        return set(sentence.split())
