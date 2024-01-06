34c677016ad23d06b3dc20e7396c4c0877ef3f6b
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:23:30 +0000

    go stamp pay consult bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import array
-def get_array_as_list(array):
-        return list(array)
+def append_to_array(array, item):
+        array.append(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
