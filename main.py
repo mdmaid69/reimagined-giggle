30c97ffa5d9048a1d783af5e694e780f9029c548
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:58:12 +0000

    loose permission become tool tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
