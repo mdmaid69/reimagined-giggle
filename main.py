8650a2275a0b75936fb478256892de3940f756f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:14:51 +0000

    excellent sit her walk call

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def cube_number(x):
-        return x**3
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
