1740db95edf5567035f5b1bff78a35f3138fab6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:56:40 +0000

    comeback takeoff window fetch tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
