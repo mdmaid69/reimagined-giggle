e6c5e4c5392ef92da005d250a971bc039f3c24c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:56:57 +0000

    sometimes connected confusing argue start

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
