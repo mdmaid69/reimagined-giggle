308808d2be62c1a9cfb790152f9cbfc4f1249a9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:51:25 +0000

    afternoon he murder straight range

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
