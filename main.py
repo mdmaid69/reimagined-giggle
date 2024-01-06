b49b2ddefed9a0762b68c2a28f8741edffb09449
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:30:08 +0000

    whom comeoff roof lost swimming

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
