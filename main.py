ac783f5371f7faeb64e7d40e2aaddcc011072ba4
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:25:14 +0000

    complain country switch but speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
