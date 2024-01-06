23077d7185e44481149c72a4974cf0ee1eb0d267
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:19:41 +0000

    sound interesting organization community actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_current_working_directory():
+        return os.getcwd()
