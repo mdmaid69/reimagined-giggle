3671096601cba9426902a19c5e78ac9841c86ca6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:46 +0000

    noisy hope basis stare importance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
