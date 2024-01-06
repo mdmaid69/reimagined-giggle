1cfd749b21a22c5f40e26c582dae30b8ce790256
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:27:53 +0000

    study tie escape distance sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
