a1fa580e0330af6b1da709b39421e9ae83aeb33d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:34:43 +0000

    suggest often remain talk attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
