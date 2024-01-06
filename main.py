1b45e05b490c4e7cde2e88171b1844603b4a6789
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:54:59 +0000

    term common reserve finally song

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+for i in range(5):
+        print(i)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
