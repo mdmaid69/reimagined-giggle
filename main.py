9c09488bd5a304ff241bad8369941003b630c9de
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:02:17 +0000

    injury together point politics learn

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
