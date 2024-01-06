0aca35ed370ee475eceb952aa3401b591db29954
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:07:38 +0000

    pen content clean transport same

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import datetime
+def get_today_date():
+        return datetime.date.today()
