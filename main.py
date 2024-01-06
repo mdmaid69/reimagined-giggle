3f2d9af359ea0ebf473ba196eab9f5bbc1d10ee3
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:43:23 +0000

    smell will around something slowly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
