4fc8595901bd1caccc46948ae3a3bf5448c57618
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:46:59 +0000

    you above force read turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
