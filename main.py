ae4e470d2f7b59eb2adc7db1c98ed7e59a54272c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:12:15 +0000

    circumstance confused fire ever although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
