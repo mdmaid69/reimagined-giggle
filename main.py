c20dfb0b33b0da36a0acd1dee081e45c981dbd54
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:44:18 +0000

    company shade another mistake mystery

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_binary(n):
+        return bin(n)
