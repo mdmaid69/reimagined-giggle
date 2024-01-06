2372059a53c9538e69aae04b8387140db089f137
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:45:35 +0000

    man perform suit occasional love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def create_queue():
+        return collections.deque()
