c13e603b1cb81ff9057b17b5aa65795453a9241a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:29 +0000

    concerning responsible poor play phone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
