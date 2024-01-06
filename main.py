f9f5449dfc7340dad7b62bed9dc0414ba9c80d44
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:36:25 +0000

    aware reaction knowledge much manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
