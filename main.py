7ae215b5a08f82edd4586533f88e5362ad8591ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:21 +0000

    confusing attempt quality present kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
