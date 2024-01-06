704cce47df3ce53cba6b4bc47d90a3f1a9003746
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:16 +0000

    less fasten maybe introduce tap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_int(array):
+        return int(array[0])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
