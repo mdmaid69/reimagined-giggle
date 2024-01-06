47298ccef4bb88c5c31fcab9ac0fbc3bd0d75be4
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:53:37 +0000

    phone healthy nor situation fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
