bb3066e98d39821b5f55fa6c9fe465329afa1afc
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:55:01 +0000

    back generous blue chase getback

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
