c93950fdc6f1142c325a8d3833b3ee3c2a5f0f99
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:17:50 +0000

    only voice address claim culture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def square_number(x):
-        return x**2
+import array
+def get_array_as_list(array):
+        return list(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
