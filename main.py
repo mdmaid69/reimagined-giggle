dade53a609b5031f28a99cc64fbbaa7f9c338e32
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:39 +0000

    rain stop autumn scene charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
