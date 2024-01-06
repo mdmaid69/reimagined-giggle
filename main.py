7560264a261c50e392a1430a0b6e4b91be0a0253
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:15:27 +0000

    put decision getaway lose chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
