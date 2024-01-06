dd2d3408a11c0151496de8607df16ebefa0c1f98
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:16:57 +0000

    takeoff probably begin must quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
