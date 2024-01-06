10759facb99d0cf1865d72b6d227a078ffa21489
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:15:47 +0000

    sale before brush concerning quarter

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
