6657fe298f0d955ae3662ec8720ec8b89ef7131a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:58:02 +0000

    big invitation nearly congratulate rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def reverse_list(lst):
-        return lst[::-1]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
