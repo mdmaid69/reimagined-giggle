baab8ae0956f485919d236e8215080b917f77a09
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:33:14 +0000

    fly quantity noise prize behind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
