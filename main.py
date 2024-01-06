0dcc5a4f1e04bcdb9e06e959345e64909851948a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:23:03 +0000

    pleasant measure keepoff spare runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
