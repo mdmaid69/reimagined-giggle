825e88cd7c888e58f85b5cf2a146f234040643fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:04:04 +0000

    who replace equal below tread

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import array
-def get_array_as_set(array):
-        return set(array)
+def clear_array(array):
+        array *= 0
