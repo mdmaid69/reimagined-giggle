230fdd2dae27bd33d91d9c106ce6fa85dfcccdf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:04:17 +0000

    lot public scrap further near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def split_path(path):
+        return os.path.split(path)
