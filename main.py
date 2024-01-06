67170098e7aa7a2fca6b1c9845d2b9d8e43d4f20
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:06:08 +0000

    honest combine limit ground print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def divide_numbers(x, y):
-        return x / y
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
