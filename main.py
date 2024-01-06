7b58a0c1d2d6b909f86d0dbf857ffa0e8e4758f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:27:03 +0000

    fond freeze mail us habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
