104776500bb9c9f1400b1f08892ddbfb99155b85
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:33:15 +0000

    measure family introduction object steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_palindrome(s):
-        return s == s[::-1]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
