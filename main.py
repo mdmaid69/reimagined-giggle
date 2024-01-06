529bd49d17954da41c2f326bc510edf1c3383f6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:36:19 +0000

    someone lookaround stand statement weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_unicode(array):
+        return array.tounicode()
