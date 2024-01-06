b9b7ef85b5f6fcd36e5b2f9f25b34bec3106db9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:14:25 +0000

    calmly dangerous my field cough

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
