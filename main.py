be1ab71701aade43cd61787323cac40dfbcd6917
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:13:16 +0000

    measure total piece square general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_acceleration(speed, time):
+        return speed / time
