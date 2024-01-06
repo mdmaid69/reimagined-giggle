0e5702935f598755b7932c6c3350c310cd2d2488
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:03:08 +0000

    shock clearly stupid build ashamed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_today_date():
+        return datetime.date.today()
