129f1b4c6da1291f7c4e73e8f37fbf51a9055c81
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:36:09 +0000

    temperature Tt develop exact happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_odd(n):
+        return n % 2 != 0
