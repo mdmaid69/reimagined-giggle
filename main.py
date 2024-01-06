e8956c15feb6e002a1dbb33e9b9d56738fd9b7bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:18:25 +0000

    occur breakup perhaps pretend breakin

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def square_number(x):
-        return x**2
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
