74419e1e0649436beff8cb76784bf570941d06e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:25:39 +0000

    land breakin shoot tax vision

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+print(sum(range(10)))
+import array
+def get_array_index(array, item):
+        return array.index(item)
