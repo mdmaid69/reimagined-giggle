dadeb2beddc3032fb568f7e419ba3be9f6ba9c1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:01:34 +0000

    television copy feel activity result

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
