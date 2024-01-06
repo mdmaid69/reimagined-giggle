8c059cf21f0c89294865cff6ccdb14eea3797e01
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:01:55 +0000

    cook health than trap lookup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
