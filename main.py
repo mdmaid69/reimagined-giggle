2f62efbaba3fd5e71b522ac300dbc507a4a50961
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:52:05 +0000

    poor nonsense decide ticket drink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
