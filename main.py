3f6fe4bd16bfcd6a5026684142ae08d353265d47
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:26:25 +0000

    study hungry flat convinced appeal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
