c10b9ef887f05fd85062dbe6ff5f373e5fc6af73
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:08:57 +0000

    sit correct mixture avoid file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
