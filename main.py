16b2cc2a8fdaabb3f3b8a8816ebba147cd7d77a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:28:46 +0000

    tidy consist taste lonely force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def is_even(n):
+        return n % 2 == 0
