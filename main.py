74ced2a095f6ebc28127103e1b03ca92008c469a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:12:27 +0000

    book shine wonder talk factory

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
