f234208139295884e5d30b3cd1ea3eee6ff9c296
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:13:01 +0000

    illness disapprove own compare close

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_density(mass, volume):
+        return mass / volume
