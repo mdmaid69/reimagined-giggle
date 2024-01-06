daedd4b7999d4c9ce395b0ad06038311e9369395
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:03:47 +0000

    makeinto fast keepoff best minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
