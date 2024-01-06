ad131d0400b00f39397ec1f0cea9eafdcf04c416
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:41:23 +0000

    obvious block everyone regional interview

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
