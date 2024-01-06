f16ff10b222b859722337e17be23f099bde19a2b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:25 +0000

    run tradition rescue thinkthrough name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
