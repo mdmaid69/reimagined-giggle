8222d3ca128888efe0af980933adc937f3ad01ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:27:44 +0000

    OKorokay complete everybody dare far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
