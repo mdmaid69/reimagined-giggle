c2472292041c09ad38896445e978d9d93ae89537
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:08 +0000

    open flow complain rush score

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def sort_list(lst):
+        return sorted(lst)
+def subtract_numbers(x, y):
+        return x - y
