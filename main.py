c78229fee616352f8ac6618a170acefe6523d227
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:06:53 +0000

    strength basis her noone splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
