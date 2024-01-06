e3d8f26487a02cbfddd03df1ee48a89a2fb9235d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:19:27 +0000

    upwards lock memory hand experience

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import platform
+def get_os_info():
+        return platform.uname()
