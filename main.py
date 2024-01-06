f7b35def8d49789d7399fdc3d23bbe50051dbdae
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:21:52 +0000

    pleased over whistle eye book

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_power(work, time):
+        return work / time
+def square_number(x):
+        return x**2
