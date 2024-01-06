951184eee55f3bc1cc3e46b613932d5d388fab24
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:29:45 +0000

    bend profit invitation present exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
