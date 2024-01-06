85cf53a49b0d651fa5f25b5271fdbef86f2fe337
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:28:13 +0000

    it breath lookafter nigga clue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
