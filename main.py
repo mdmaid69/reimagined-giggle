76598af0b4fc7346eeeefb17fbfa8114f0ee72f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:34:09 +0000

    pretty lookback(on) degree anxious set

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
