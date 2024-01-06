2c4672615b615e608c3bff21caa0b9630a87cbe5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:18:29 +0000

    cook mess official chat notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_max(lst):
+        return max(lst)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
