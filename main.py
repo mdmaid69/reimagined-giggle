d4c4c716b1a2d7ab9941eb8cfdc22eb33de6c75f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:05:10 +0000

    Jj bottom teach runinto seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
