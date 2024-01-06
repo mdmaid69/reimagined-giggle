ddffaae1e68512e4cd015f22b01b0550a1bcdaae
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:48:11 +0000

    straight once difficulty shoot praise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def square_number(x):
+        return x**2
