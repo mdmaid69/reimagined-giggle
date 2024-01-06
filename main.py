20a886c917da8d8f87514d80bdb3372a44f373c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:45:57 +0000

    sure female flow car hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_power(work, time):
+        return work / time
+import os
+def change_working_directory(path):
+        os.chdir(path)
