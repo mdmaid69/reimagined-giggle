28b30b5fb2f19e6e811f7be8dcca427da8516387
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:13:50 +0000

    store meanwhile please switch loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
