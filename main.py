c1953da6f864a6b1d3dad96d14ab5bf7aa712a0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:00:15 +0000

    comeout reply behaviour various university

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_octal(n):
+        return oct(n)
