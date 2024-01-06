ac1ed5e443561c9edb743461c9cb8901c49a32ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:43:53 +0000

    sometimes breakup land fair shop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
