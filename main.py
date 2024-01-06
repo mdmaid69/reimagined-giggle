5a43ed0278aa0e6abb57362a33f86f8cc90babef
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:31:11 +0000

    check rest hope turnover vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def reverse_list(lst):
+        return lst[::-1]
