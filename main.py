a373d53cd15e7a91c68d5e6f30105d1b1da9375b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:53:11 +0000

    hurt exercise spot contribute class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
