69db619b186a345e6287bd28a045e746adb90fa6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:05:46 +0000

    police downstairs earn across negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
