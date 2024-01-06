670a728065c99b8f9caeffc59ccc7ebfeb4f14ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:40:26 +0000

    rate formal century duty want

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+print(math.pi)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
