61e0045ed97c0ad1f9da844ad16a90d8a66befa9
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:28:05 +0000

    mine keepoff tap month actually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
