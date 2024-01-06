ccda6027eab42ba90326462e48af083c9eb292c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:27:51 +0000

    underneath again snow period foot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
