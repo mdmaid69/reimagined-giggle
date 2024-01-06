0adfbe6ffa0b74588c17482b572e6b7e80f106a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:50:26 +0000

    now several survive concern practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
