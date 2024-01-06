5123b84691d84aa668611dc045ca9c0226c6fbe2
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:00:17 +0000

    anything nurse clear trial few

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-print(sum(range(10)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
