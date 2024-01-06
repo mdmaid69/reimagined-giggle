dc8787294632f7985a30ef4f0c68606aa3ffd4c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:16:26 +0000

    level depend getout OKorokay half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
