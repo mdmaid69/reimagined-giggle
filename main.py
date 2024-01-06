ad18d3d1b94e46b01be607cab4956c59e73e7471
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:58:14 +0000

    lost weekend influence consider pull

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
