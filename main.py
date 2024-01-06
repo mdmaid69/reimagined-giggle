aef2b2d898ce112df5722212e07173df90ec8ced
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:13 +0000

    sweet experiment stamp never clean

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_max(numbers):
+        return max(numbers)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
