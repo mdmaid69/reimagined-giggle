bcde302933193b60a5547376c37a8b7d11090891
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:24:30 +0000

    see band exact good lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
