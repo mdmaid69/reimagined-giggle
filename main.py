9e37c971de4eba6eb8cb99d7c9a7b15ad19e2ca5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:31:41 +0000

    commit situation strike study breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def roll_die():
+        return random.randint(1, 6)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
