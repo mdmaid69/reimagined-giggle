351506727f00f8ca1b124358471e91e31b6f9ab0
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:14:02 +0000

    level sensible prepare mail expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
