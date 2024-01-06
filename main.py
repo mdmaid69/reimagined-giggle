7e1d9fcafb1fa33a2eeae2223bc31affaf34ab91
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:41:59 +0000

    not swallow boring sink radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
