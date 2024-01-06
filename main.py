da77a98230b7278e5c35db261bcc0bc257f20a9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:27:07 +0000

    low calloff slight identify 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
