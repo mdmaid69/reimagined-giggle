c67fae02b6c3b5fe6b3b300113d6901808ca6565
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:29:18 +0000

    price wherever habit speak yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
