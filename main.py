0a216a1761424f0fd4491b88910be889e00dba35
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:50:02 +0000

    create intend left side listenfor

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
