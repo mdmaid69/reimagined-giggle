5055a1bcee0c94e5bdd0f7290b82d1939b8d634d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:46:51 +0000

    ground 569enjoy ever nigga everyone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
