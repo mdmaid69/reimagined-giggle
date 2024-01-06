7dfdce01e25684ae4c52abf51306e0e18f5cfd73
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:26:47 +0000

    flow cost bringback spread flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
