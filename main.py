fc24758b3ff410e039bf0d615d2e0b5c55ba26a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:16:50 +0000

    surface disturbing runover rubbish he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
