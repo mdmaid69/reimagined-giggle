69efd32e0ca2ab62ec88d7251e747059c018114f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:26:45 +0000

    activity accommodation another spread bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
