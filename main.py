dc8ed9eeef9a8212d12ad3437e2473c256a7481a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:41:23 +0000

    mean turninto male exist appearance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def clear_array(array):
+        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
