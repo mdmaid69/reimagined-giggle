f8fd28fc78b60c0e82ce1241e3d3ea29a81ad8e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:14:52 +0000

    tour train side excellent pile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
