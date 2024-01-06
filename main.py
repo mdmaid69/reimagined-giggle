da167ada584982b090325bfd302ed23299f6c9b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:00:06 +0000

    colour pleasant however much address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
