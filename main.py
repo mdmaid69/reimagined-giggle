fc62c985355d1386ebe2d6610c25d9f4c908d095
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:11:54 +0000

    song connected complete afterwards smile

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
