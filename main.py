4f87647a488e82721c0bdc578cead497a06b9f85
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:52:26 +0000

    use constantly holiday look confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
