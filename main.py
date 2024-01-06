0ce8ba08fbab0bf41a2ef45852c29bfcaf13fc40
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:17 +0000

    plenty geton turnover comeoff nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
