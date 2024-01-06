906379c73064f75611a03f1a81d8ab5a0f8d163f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:47:57 +0000

    begin towards label comein complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
