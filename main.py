f29a56ecbff61ca98ae930eb9ad4b687b5b3c9ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:20:30 +0000

    break behind shade next treat

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
