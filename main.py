b215b8407ef5bcf27f73b0b761dbcdb47265141e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:49:51 +0000

    blame sensible human waste responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
