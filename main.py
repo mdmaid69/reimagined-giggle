38517e9a54822e1770e848a52d76a201056932d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:56:28 +0000

    pronounce level fault enthusiastic active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
