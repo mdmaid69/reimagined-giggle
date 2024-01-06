083d492f4d35da419444a988f1f75c3e42e1fd2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:47 +0000

    regard choose married point intend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
