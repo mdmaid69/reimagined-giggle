ac10e271b1ebba9d8ced06a052b23f6c87ebc688
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:46 +0000

    proof gradually plan sink far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
