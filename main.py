ead094dc67333a56ad98dc4eb64f641c04399ba2
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:17:12 +0000

    muscle bright stupid amount worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
