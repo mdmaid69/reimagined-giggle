8ace7664da5d808c325051c5d6674e13c929709b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:49:34 +0000

    classroom workon freedom care month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
