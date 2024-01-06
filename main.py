71fcfeb41bd7f521420b13a57f958b11b9ea0faf
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:11:50 +0000

    big impression into whichever niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
