2014a049c1c0d1de2b29a38bb556abc3bc4534ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:09:02 +0000

    service comeround fit unable sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
