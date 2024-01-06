964e8736c3456330f48f83dcd19b77f5f0912850
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:45:47 +0000

    throw wall expensive train command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
