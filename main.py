bc2d28bebaae827dde11830d57d46f78eb420cec
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:21:37 +0000

    themselves cheap much crash sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
