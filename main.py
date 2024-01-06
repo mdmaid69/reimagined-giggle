dc8960d2a68fb6e456c842a90ca997b56830d929
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:36:16 +0000

    software relative imagination goon laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
