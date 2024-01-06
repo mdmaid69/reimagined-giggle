213ec009f2296aa118837f97ede64c348b508df6
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:58:39 +0000

    sure backwards permission trick quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def subtract_numbers(x, y):
+        return x - y
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
