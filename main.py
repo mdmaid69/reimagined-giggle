b70c24173d0c9cb041866fe38ae7196e48a660d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:41 +0000

    fun here minimum danger snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-print(math.pi)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
