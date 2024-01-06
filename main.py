655c3f2516dd928bbbd61acd5d2db2b3491e8c68
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:19:13 +0000

    hollow belief grand cause gather

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
