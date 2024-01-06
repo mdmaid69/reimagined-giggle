6e3f3b747f6f677231c83df2b5dd1a9db5c24ae1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:27:20 +0000

    shade move wet somewhere turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
