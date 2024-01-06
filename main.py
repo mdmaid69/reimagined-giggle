03a18c2df2a98289276ccdfcb6980aaee5bd3ab0
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:58:46 +0000

    567enemy deep section offer bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
