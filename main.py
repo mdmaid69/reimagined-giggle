43291698f2074daaef33e1b47d5fe5ac3fc9c246
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:38:38 +0000

    shut interfere prize shopping partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-name = "Python"
-print("Hello,", name)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
