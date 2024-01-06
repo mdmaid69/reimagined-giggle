4aa22e1da8f4d29eb59b515e4c3a461170e978fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:59:40 +0000

    tidy delight listen high doctor

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-for i in range(10): print(i)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
