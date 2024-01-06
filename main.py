a3956baf1e2d6e8e7952641f81c3ea6b184ebd27
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:37:43 +0000

    food illness concerning save bring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_set(array):
+        return set(array)
