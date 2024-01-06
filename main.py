60baa5ae112d396733d58d0e5a2ca5bade75c1f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:50:33 +0000

    bin brain head edge punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
