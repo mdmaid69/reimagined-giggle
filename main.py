d3eaddbb13be46f1cf8ab91af560bcf6349cc003
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:39 +0000

    distant shopping spite congratulate accidental

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
