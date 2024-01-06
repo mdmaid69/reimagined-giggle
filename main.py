dfa24933600e3e4da5f50f1cadfc2df4902353ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:55:02 +0000

    road height unusual compete against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
