7ee8d13700fa9245572409488dfe766f935eba52
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:11:53 +0000

    murder hair secret sound famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
