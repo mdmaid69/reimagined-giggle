326c3cc89e33fe80a1db9788b5238c083a2533b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:07:13 +0000

    close fast difficulty off offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
