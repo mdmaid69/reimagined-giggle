bbac12f4f1dc0ba0c1f8c31c41a6474c714d0f9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:43:52 +0000

    building issue store home card

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def is_odd(n):
+        return n % 2 != 0
