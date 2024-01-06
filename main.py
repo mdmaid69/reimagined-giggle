c618b6594a50f94e0f9734ca948455438094a79f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:54:25 +0000

    fashion clothes black cum scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
