8b64baf15700dbdf662e56cd856343b47ef683e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:37:16 +0000

    power convince while fit vanish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_cosine(x):
+        return math.cos(x)
