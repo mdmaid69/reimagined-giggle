d60e135bb112924cf7cd7eab7e80ba7c03525c0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:32:19 +0000

    total blow risk flat contact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
