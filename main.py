ba799b28025d82c8f05ce3bdad83d4dc0ad1a1ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:18:48 +0000

    boring argument message once shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
