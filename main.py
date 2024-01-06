9093283cb6cdd25a0e97c0b573e85e28a863234e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:59 +0000

    poor often area office public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
