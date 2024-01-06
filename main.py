f47521de54ed9c77127d492bef6715c211790605
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:55:25 +0000

    bound quarter prevent deal remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
