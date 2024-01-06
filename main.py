786e3b3e6601d620f744ef00016c2f1a73e4abb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:55:16 +0000

    careful pull woman wealth ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import sys
-print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
