542cc885a23fc6edda7f21040b16b687db6ab6b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:26:07 +0000

    until path tall stupid rush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import sys
+def exit_program():
+        sys.exit()
