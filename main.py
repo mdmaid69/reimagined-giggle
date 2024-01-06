1d02958e0660f6679175db1abaee522b2ca6498e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:11:34 +0000

    transparent turnoff experiment getback study

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
