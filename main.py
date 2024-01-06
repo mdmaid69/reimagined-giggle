e2a9d9fbd273d11c7c34f3ae91c3840cddd665af
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:03:51 +0000

    error event neck news push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
