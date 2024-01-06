52b8b51fd0eba1788ac0abcf08701a02ef9eb040
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:36:25 +0000

    as defend block strong exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-print(os.getcwd())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
