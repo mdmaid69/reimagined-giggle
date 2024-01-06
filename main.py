235b1b0c96f70b1f27093c6eddf0ae4da8dc221b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:30:26 +0000

    print heat save whisper now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
