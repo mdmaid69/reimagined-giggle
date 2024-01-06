3d32cd1895937e5d7b1500be74a07846a68ac9eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:22:31 +0000

    breakout best beat force brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
