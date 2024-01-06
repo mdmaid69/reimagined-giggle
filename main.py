8879181780591d7999dc25ad06483ffe9b5d8b20
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:52:30 +0000

    allow spin hide string all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
