b7427afa81ff5535314eaf5c8ff11a73bfac1bfe
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:17:35 +0000

    teach blame keepout holdon equal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
