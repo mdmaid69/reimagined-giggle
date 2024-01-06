d57e6119f02d6f6941d770d1090e645343aca930
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:12 +0000

    report bag bound turn bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
