602647a1a61331db6fd5366041b48d9e1381cc7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:12:14 +0000

    repair jump guess why agreement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
