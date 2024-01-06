ad3e44a8b58589bbd3f52d93c783d0503f1456e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:44:08 +0000

    once worry motion shout hard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
