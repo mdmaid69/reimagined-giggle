4fa9cabfb60d2c69b00f647183d8333b49a91f86
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:45:50 +0000

    hear arm divide calm pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
