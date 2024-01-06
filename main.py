cc3f57c0da0d2378fd1c5d3d6cd07b5841d8e8bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:51:04 +0000

    shadow far while Jj average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
