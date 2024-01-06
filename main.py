cdafab5f42223da38ab41a8487bdd0ae9f3831d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:05:55 +0000

    future search raw minor typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
