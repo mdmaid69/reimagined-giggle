ec2529cfdab161e166a9ee61814341b3d06e6248
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:23:13 +0000

    now pump hurry slight leave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
