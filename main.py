c66c9b28e922d5100b3d5a96f0ecb0ef3db011a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:59:09 +0000

    sticky goon bin breakout cover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
