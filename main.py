5ae799b774f9f2c8c4fb92990e4fd47dd363503e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:42:43 +0000

    talkoutof minor compare same callup

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
