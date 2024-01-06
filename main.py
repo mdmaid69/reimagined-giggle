93d0692c01d2807c8e0b6c10aae5ac4c4ed55bfc
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:14:17 +0000

    breakthrough amount prefer violent model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
