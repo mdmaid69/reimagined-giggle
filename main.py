38d5c8deee62f38f61df593ed40155f2dfb61f0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:42:03 +0000

    group tall water none represent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def square_number(x):
+        return x**2
