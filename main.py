754cfaef121f4c8d848ad51477a2379b2693cb0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:03:09 +0000

    reject second sharp cum refuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
