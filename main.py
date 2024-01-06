d1df05f90f2a43916bb202013d759c4f511c8137
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:49:25 +0000

    reliable interest lookup serve Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
