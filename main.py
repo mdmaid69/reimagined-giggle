0c167ecc7305f8d87bba6cc9c2454b3590d3c081
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:30:51 +0000

    turnoff lookup from influence send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
