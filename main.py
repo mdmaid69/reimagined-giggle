aed67f2662909aa4dd85ab0c5ec913ebe209d116
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:16 +0000

    and getoff age understand concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
