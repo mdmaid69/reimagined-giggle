eb056dda9f84221342f700056af168f01559f88e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:34:57 +0000

    Mm warn join represent plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
