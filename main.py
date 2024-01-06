d5eddb659b29f19e5d054312b107638a22590b84
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:24 +0000

    doubt lock opportunity spoil makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
