5ac169e94313ab34955421eebedc1a6edace544b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:52:36 +0000

    square 571enough front window face

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def convert_to_hex(n):
+        return hex(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
