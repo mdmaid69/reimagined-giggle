a4d02a3cd0b3b4a8b6699ae3b0ad83f1e7272160
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:37:20 +0000

    declare connected size quickly degree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
