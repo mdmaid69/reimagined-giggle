84b859f87df2b6576dc322c0cbf4701a668c9233
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:34:54 +0000

    might certain bet object use

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
