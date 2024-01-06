1da4b9745e51fe4e2f49420868c8e905bad120de
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:30:55 +0000

    vote away show or keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_min(lst):
+        return min(lst)
