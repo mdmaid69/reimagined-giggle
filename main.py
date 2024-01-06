db96e6116be7e41d249dcf6eabd79a52f75bd22c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:48:43 +0000

    bringup lay anywayanyhow string round

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
 import math
-print(math.pi)
+def calculate_cosine(x):
+        return math.cos(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
