407669633260bd7d3928da31daeab0a5b5f15a26
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:23:41 +0000

    leg hit word right principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
