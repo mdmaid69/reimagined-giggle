f946f0a8d7ec9b45da2200a9ebe6b844af963452
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:09:19 +0000

    book best conduct interesting one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
