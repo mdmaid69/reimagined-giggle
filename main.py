537e8bdcaca11af6ab177504dd8a2df2df19a79e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:20:45 +0000

    skin send stiff health name

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def square_number(x):
+        return x**2
