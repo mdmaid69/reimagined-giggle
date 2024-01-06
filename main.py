5fa71c9b8f2b9b08be60cfc81faa065fb94e9a1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:53:54 +0000

    571enough kind child frame loose

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
