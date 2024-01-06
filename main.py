6c6ff278433edd09c1d7d7b8c5afd095bd3f5771
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:27:08 +0000

    area joke mind definite bill

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(lst):
-        return min(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
