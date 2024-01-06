48be27be6d09abdc109496eed58ea5b86d9c0008
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:00:02 +0000

    spot birthday squeeze area afternoon

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def cube_number(x):
+        return x**3
+def convert_to_octal(n):
+        return oct(n)
