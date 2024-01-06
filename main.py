d2af060ff0c4ad120a746c5b47ea01792a6d5b86
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:15:30 +0000

    scrape error consist we giveout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import sys
+def print_python_version():
+        print(sys.version)
