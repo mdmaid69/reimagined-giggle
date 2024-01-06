4b01c28b82911968488a722f82ff9e4603370241
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:44:58 +0000

    sing strong film shut maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_pressure(force, area):
+        return force / area
