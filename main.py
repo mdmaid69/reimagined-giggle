809b00e2e425ee727b3a36ca4aea122c4c9439ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:29:22 +0000

    which start whole cry material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
