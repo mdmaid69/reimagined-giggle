aa54ce38654f3ccd09617d397f669a10506ed683
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:42:26 +0000

    roof upstairs popular drop pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_even(n):
+        return n % 2 == 0
