3b665405b6dab5e6d3f939d7e675479d910bc475
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:32:20 +0000

    greeting getoff confidence field environment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
