6944b7674ce065d4074220231d0d028281779f58
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:49:35 +0000

    bear confident side deaf car

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+name = "Python"
+print("Hello,", name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
