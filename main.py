c1732a6b4ff0d9c2219e53e776bdcb1f86f521e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:57:25 +0000

    worried means rain breakin charge

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def find_min(lst):
-        return min(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
