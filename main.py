51ba940377432ecda802dc3485996bd02c379711
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:50:50 +0000

    build help describe surface battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
