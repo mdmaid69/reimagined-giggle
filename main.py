a38de730e1cf8e3dc38ee52d190cd27f307f8b38
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:09:46 +0000

    stay average breakup shake for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
