6866221dac150b943b090389dd9a06d2a3a82452
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:31:56 +0000

    keepaway direct major getup pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
