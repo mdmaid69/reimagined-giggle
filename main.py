87b233b190a46dbebffc8b2805ed4b8b40a6e18a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:43 +0000

    mind key limit any whole

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_speed(distance, time):
-        return distance / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_sine(x):
+        return math.sin(x)
