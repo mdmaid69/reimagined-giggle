889ba2bbdd61f173d58d58973fae6b1a398719fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:45:06 +0000

    obtain meaning leg basic square

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
