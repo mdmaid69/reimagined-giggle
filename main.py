f6b060df223d9e6102dec1ec2543ea65b1091db2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:24:41 +0000

    play schedule invite uncomfortable cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
