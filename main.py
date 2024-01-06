030a7a5cbb729c1e19084bc4cc50ea1358130ee1
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:06:11 +0000

    rush drive book job beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_index(array, item):
+        return array.index(item)
