270806a7f973395959692c227cf3fcdbe42c7a48
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:58:41 +0000

    part intend ground tour pour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
