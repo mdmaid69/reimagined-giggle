4831e6bfa72fe7940117e8fd61edf093c8d2cd13
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:19:49 +0000

    embarrassed undo petrol wheel skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
