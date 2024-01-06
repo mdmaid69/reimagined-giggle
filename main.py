dba7a75ef03bfc0ab058dd207dcd3b905caab57e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:20:08 +0000

    arrangement everything share criticize sign

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
