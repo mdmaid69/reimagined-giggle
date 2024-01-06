20b97404d872f3d4beaa0854bce451beee360309
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:53:03 +0000

    far danger sleep passenger straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
