d31c905ed28be1c10d49fff6326d044b7c7be300
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:20:14 +0000

    everywhere estimate live especially fault

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-for i in range(10): print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
