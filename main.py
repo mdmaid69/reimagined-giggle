41e8fdd61f6ce67fdef686a73262e25bbe048424
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:49:46 +0000

    national battery burn considering wrong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def count_elements(lst):
+        return len(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
