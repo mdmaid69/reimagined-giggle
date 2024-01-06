79ac028f0dd9800c7680be5d326234d60d916d6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:44:51 +0000

    skirt importance heart fit delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
