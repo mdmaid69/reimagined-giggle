b1861132d2e74a5525aebcb931d8085f6a401ed9
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:51:09 +0000

    apply 179bite your smell warn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_elements(lst):
+        return len(lst)
