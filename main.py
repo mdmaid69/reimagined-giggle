441348afbdc2e9eab7cedc298294db8ab6434680
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:10:09 +0000

    fair happy tool tongue correct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
