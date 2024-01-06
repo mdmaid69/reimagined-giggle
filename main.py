0c96c1ce0b6a57c0de2ab51ee309d56d10403965
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:09:39 +0000

    scream sign address present search

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_hex(n):
+        return hex(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
