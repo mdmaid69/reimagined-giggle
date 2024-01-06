6426612a8b12b670bc2a020c79a7449c93923065
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:56:17 +0000

    mystery explanation miss active when

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
