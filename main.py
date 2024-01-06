8345aa2914514774569adad92fdc4686ed82d2b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:41:41 +0000

    tidy apologizeapologise interruption pupil experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def add_numbers(x, y):
-        return x + y
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
