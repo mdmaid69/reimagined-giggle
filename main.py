3ca29baa646ffbd12edf95344c90547bbefcdb76
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:16:39 +0000

    real return regard people situation

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def convert_to_binary(n):
+        return bin(n)
