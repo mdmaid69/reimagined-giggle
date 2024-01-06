84e5f0f82630062b04e11f2fe11852152933e894
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:14:49 +0000

    somewhere basically criticize undo comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
