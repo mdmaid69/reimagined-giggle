6dd5ebb41127512594832b57610cae2cea6302c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:36:46 +0000

    callback patient square breakup action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
