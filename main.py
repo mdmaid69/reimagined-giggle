ef34982f233d154e91992d9c9ef4f4c9878eb999
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:22 +0000

    similar home switch compare wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_time_since_epoch():
-        return time.time()
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
