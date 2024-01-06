b461f60ed5d4cd573284e182d6dfba80236a7aba
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:22:11 +0000

    standard really round second grateful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
