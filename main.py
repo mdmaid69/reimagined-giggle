3df8ead7a3ef3d051b4ced7de8ecd49cc61ce814
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:46:48 +0000

    tread tragedy contrast support no

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
