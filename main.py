ef4d010e451d14fdf3cca8e5b92f6f5892ce7be3
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:20:02 +0000

    sound break far Cc comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
