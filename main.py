ab127da67f05df98308a49b97966fbcbee054c83
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:03:47 +0000

    classroom callat bend mainly nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def cube_number(x):
-        return x**3
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
