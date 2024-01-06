afd5ecdd481206be54d48e87dc5793fa3ab075cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:25:11 +0000

    keepoff its sleep know tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
