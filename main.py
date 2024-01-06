4642566a0255b7f45378129da084d9809ff192d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:24:03 +0000

    often there move chance hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
