8362cab9085a92167aa4dadfd79b9359b9147319
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:30:55 +0000

    letter scrape confidence voice properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
