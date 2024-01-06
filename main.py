e0856e0c0c47f6872b0aec2e60ddc63c20b670bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:49:31 +0000

    question much whenever combination workon

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
