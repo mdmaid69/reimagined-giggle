d3be312fd94f2d3447ef66702d516f0ef651372e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:34:05 +0000

    process bus own far rest

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
