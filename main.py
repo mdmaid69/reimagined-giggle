913027daa85f306fbd3139135499c2f4c78f9781
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:05:56 +0000

    without free cap scale pretend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
 def calculate_arc_tangent(x):
         return math.atan(x)
