02da91e928658f1345b55a4da3851907da652a38
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:59:47 +0000

    similar himself journey air grand

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
