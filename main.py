7b96702ed82f705b994ae7164d53e20d9155cf89
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:57:25 +0000

    male shoulder comment body Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def add_numbers(a, b):
-        return a + b
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
