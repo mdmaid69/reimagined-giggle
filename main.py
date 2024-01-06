976820f20ffc1450db25c6e3d9b3606dd534f8d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:47:07 +0000

    turndown crowd bet it weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
 import re
 def replace_pattern(pattern, replacement, string):
         return re.sub(pattern, replacement, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
