d2beb5818adc591c6acc5a56cc231015fe52bfe9
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:47:08 +0000

    face have post rare chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
