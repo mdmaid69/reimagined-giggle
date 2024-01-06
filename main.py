0a595b63c024d9835c87bfe2ee484cce84a14c94
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:17:05 +0000

    work greeting born car feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
