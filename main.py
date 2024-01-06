a3ae2d1e7f03bdfb341cf1c11c9126bcbd601b73
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:01:43 +0000

    there general field operation gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
