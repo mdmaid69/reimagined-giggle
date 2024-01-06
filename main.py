f5085456aefecb786f9622059f212607c51599a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:19:20 +0000

    on way there money pretty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
