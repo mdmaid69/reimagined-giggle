8330748fb22cf9070150beb8c467105f801f5420
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:41:48 +0000

    scared game impressive block crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-print(os.getcwd())
+def get_string_from_array(array):
+        return array.tobytes()
