5bacff5fc36765bd03e541b5aafb15cc792125ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:18:32 +0000

    poison legal lookup jump improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
