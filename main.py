7fc017ac3a9a6d4f41b249272e7ae52edfe3227f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:02:13 +0000

    shoulder score compete estimate pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
