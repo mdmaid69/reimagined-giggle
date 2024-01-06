9869f6935f74eccd381a2e2e451ba3447f639d10
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:26:36 +0000

    sea with fault offend hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
