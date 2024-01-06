db1d613dbee8eb6b9580a68d67a1d4f5e46218b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:52:21 +0000

    sort play throughout door already

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+print("Hello, world!")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
