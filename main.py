2c62a0c2d9f2cfd6d312cc920c5bd5200726939b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:52 +0000

    boat act majority spite back

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_error_function(x):
+        return math.erf(x)
