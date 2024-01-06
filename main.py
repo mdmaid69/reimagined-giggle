4db3be4f32c4920d206a13690263dbed162fd8a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:00:01 +0000

    soil smile reason ring go

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
