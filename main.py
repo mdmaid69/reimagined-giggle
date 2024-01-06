1e8d206656268ce0cb7a4160ed790bb2e830b6ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:25:56 +0000

    fire order once public matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def convert_to_octal(n):
+        return oct(n)
