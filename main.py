6ce5ca1fd9f757e2b3d91d501722582fa4acf402
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:37:20 +0000

    pass still funny workupto bring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
