6afe2f55fa0a88e30d915e0aea68aee2c7812c3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:25:28 +0000

    behave beside thin forward sad

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
