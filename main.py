f10eb738e382d63a90379ee4ca134241a9d762d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:19:51 +0000

    difficult level advantage however rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def remove_duplicates(lst):
-        return list(set(lst))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
