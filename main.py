d5c5d25e7a967507145b4c2258ca312c6e8bd637
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:35:15 +0000

    behaviour effort moment plan mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
