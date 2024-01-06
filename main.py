e5873e3b5e2ab08723134451be1db7679e6547df
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:23:02 +0000

    introduce accidental workupto geton path

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
