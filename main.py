1d49c4aa420d5c814e66ac77bff8531e3393b462
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:07:21 +0000

    belief error that noone comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
