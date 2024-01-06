ded8dd2f3a4be78423c5a87076eb7963e71f7efc
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:57:25 +0000

    please possibility positive school push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
