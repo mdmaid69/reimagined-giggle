df4a774972be5b025717ab4b426908cf28de5478
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:24:30 +0000

    talkback until pinch mark large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import time
-print(time.time())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
