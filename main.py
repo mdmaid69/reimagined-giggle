43912e40c7f221d1dab1c5f432a63aa7717cca3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:07:50 +0000

    unlucky computer far neck sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
