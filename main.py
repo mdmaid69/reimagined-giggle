e0b56675eb30cf31ca446b14d10751ac718a0353
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:49:08 +0000

    country pity either criticism pen

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def convert_to_binary(n):
+        return bin(n)
