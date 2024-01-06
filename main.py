0f3ea54f9c2b6579308cc6cb7161c7640b4aa5e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:35:05 +0000

    dry later punishment escape also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
