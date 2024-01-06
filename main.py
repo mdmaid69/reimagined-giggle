7ef07b2fe1005ef720989ae814df5b063c8feca9
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:43:15 +0000

    please dick connect child among

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def square_number(x):
+        return x**2
