860dfd56f61878f2cc13cf1f960b21f8fbbef018
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:12:34 +0000

    ask long swing brave arrangement

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_volume(length, width, height):
+        return length * width * height
