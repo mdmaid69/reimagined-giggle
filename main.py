e611421dc43738ecd2a704c81771550acc5398dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:56 +0000

    his protect environment record refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
