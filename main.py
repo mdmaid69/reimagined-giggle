36a1e56850afe97c69343c93c2d6ab4b4b06422f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:02:47 +0000

    wish television hot tradition total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
