bce515e8330802c70b3a2894421325deb23b643f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:34 +0000

    what major bored achievement Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def find_max(numbers):
-        return max(numbers)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
