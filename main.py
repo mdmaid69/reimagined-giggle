aaf8a10a8701875e6e7edc69aed93aa697eb2156
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:09:40 +0000

    turn pride takeover before thing

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
