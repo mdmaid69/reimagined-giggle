e19e8251bb632fe9bd24e9a55861aea4e7ff3827
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:39:53 +0000

    takeout me responsible drink fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
