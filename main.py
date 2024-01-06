1981bb0a6aef759208af1c32af4383b50a780bde
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:11:19 +0000

    ready before blue body office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
