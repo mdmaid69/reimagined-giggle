85a3a48b2f9aad1b897b31a3f7629a8f80a32b69
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:54:25 +0000

    pencil wipe different hurry last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
