b0290d7f789452a5bcb449e52e9e08d017a3a897
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:59:00 +0000

    pay away calmly appeal official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-from collections import Counter
-print(Counter("hello world"))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
