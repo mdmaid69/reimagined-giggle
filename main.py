03c51af3b681334904e9f8659b0cf1c14ae14499
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:54:21 +0000

    write place concerned goodbye lookafter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def sort_list(lst):
+        return sorted(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
