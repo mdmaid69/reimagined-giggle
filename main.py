c9f5e9c2d000a8f8b2404f736c83a4b0ad0c2fcc
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:12:17 +0000

    law include all speak nonsense

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
