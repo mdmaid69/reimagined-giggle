244f0d4e852a7ce9a10dab826380670380991af3
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:26:02 +0000

    upset wrong movement priority office

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def divide_numbers(x, y):
+        return x / y
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
