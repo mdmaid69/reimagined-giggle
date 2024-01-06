86fc8d5af15a60fd557436c9c5642e3dc4653073
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:13:39 +0000

    climb dry offend chief comfort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
