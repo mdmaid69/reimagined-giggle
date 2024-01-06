643a496eaa08e27ef4d764d4a9a7a86901911714
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:29:25 +0000

    negative slowly trick actual finish

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def convert_to_binary(n):
-        return bin(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
