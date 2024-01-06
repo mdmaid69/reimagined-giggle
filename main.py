2e6e84809ba26014a7499296f2cdb2fe869b15ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:42:58 +0000

    profit exhibition tape plain join

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import sys
-print(sys.version)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
