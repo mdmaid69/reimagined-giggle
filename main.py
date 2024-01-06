34af05363e7b2ad8fafc8e4d3502f3078c7a3c72
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:46:19 +0000

    different pronunciation bored age getback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
