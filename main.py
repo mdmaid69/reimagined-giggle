30ac92df3bd80381eeceae672b7800666860f417
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:23:30 +0000

    determined have complain be arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_density(mass, volume):
+        return mass / volume
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
