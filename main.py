e2f56f3b0e32ff02c132aa80f8ac35711fbc52cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:14:05 +0000

    step accident official rule wire

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
