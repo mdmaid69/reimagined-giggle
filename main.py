d766b7f5ae9354010fed36a9878727908ac62287
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:08:58 +0000

    callout none 564encourage performance plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
