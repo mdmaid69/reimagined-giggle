0cefced56684769914915ee8e6a41b878aac2e3f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:23:38 +0000

    stand look broad war across

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_list(lst):
+        return sorted(lst)
