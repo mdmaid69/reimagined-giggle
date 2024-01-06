9dca417547f9a6f6d7a48fac73104f45aec61faf
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:40:47 +0000

    hide while failure walk wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
