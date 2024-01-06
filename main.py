61b10a2bef7d045ece48bd806adc98c2db2ca6d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:14:00 +0000

    care difficult risk picture agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
