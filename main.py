4799ca7f634925a3420e92cd571a48b5dbfe3fbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:15:58 +0000

    threaten otherwise without material refuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
