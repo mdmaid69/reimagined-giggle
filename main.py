cdf3f4413bef5dabab7191c6b2f920e2a8b00f57
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:23:07 +0000

    recognize with twist Ff estimate

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  def cube_number(x):
-        return x**3
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(10): print(i)
