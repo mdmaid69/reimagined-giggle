6eed43555ae38e042bd23633d43c5511c6d919c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:03:32 +0000

    unhappy few corner challenge affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
