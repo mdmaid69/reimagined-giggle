03c27c7383eefe7626aaf3d7a77da6fd5b79b8db
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:56 +0000

    Yy careful opposite custom nice

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
