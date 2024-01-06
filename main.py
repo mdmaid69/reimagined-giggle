90ecc947ee26e78d9a35ddd61eaed7e629891631
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:57:51 +0000

    suppose just pinch terrible strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
