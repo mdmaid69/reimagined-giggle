22ac3c41b0bf6fb3f9f93760a3541bbc188e78d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:04:05 +0000

    in point fine base ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
