ff96020a2dfeb464327675b5be2228a326c4ef6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:55:17 +0000

    wave bank country raw substance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
