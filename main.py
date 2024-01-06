d3096bc93ec7ba6eccb11eb7a0aed8972ee534b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:11:26 +0000

    would swallow fault impressive community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
