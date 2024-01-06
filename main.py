bc46ac5abcdf9102e56fc65957ff52f04f426c9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:46 +0000

    question race wise or neck

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
