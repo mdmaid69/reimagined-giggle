ab74297ebe70f2f269080a540ee2d511ef0cdb19
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:06:35 +0000

    trouble share trap family spray

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
