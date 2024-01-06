c11a1f884f9244977729f70768cca8fe9c97e22b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:32:06 +0000

    pure degree ours collect answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
