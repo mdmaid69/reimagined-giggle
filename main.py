247f6ffc95e627271d537a2a88d68fe025c758bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:36:45 +0000

    expression that tall exit part

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
