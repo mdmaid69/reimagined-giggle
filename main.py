daf137a3725c3c3b656ab0963ea60cef56ea3603
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:09:08 +0000

    joke workupto simple uniform breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
