16dc841db940cdaca0f4f6cba64abd823629c9f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:11 +0000

    freeze past offer sign study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
