0dfaf803cacd647c92317f64f37ac8d30b28c1b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:57:09 +0000

    lean discuss treat improvement programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import os
-print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
