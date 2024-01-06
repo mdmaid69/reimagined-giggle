b630a079f664edda21ada5e5311a7d31da95ba96
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:04:39 +0000

    suggest such beginning runover frighten

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
