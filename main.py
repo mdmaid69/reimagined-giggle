f314ecf4036086fb65c81b6844196f22ab2390bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:07:19 +0000

    only definite share usual season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
