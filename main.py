37910fe611ba40ac531e547e2d3accb5855d3872
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:49:57 +0000

    different refuse sing develop crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
