f01890b4cbbf0157d1f563f4b19cf8e25c0baa88
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:37:12 +0000

    join practise pot scene meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
