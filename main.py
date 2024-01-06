f52efdf4e318524578830769412339481deb3445
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:50:35 +0000

    recent shut sound copy like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
