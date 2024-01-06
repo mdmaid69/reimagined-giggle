af4af9473e782763ed578173a9d391ca978944e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:35:42 +0000

    right true deliberately giveback workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
