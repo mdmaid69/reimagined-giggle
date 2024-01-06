4cb73b5bc9247aa7c19f30af7c5c0a16b183cf90
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:48:13 +0000

    couple secret so partner leg

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
