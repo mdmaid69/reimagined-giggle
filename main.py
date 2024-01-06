eed8e363eed3d7b98f018b66d36d4660dd4212e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:50:27 +0000

    tonight storm light suddenly fall

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
