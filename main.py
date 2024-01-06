2f5757cd4b1a6116d1ef7dcdbbb916680ca07d3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:34:57 +0000

    line today miss quiet bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
