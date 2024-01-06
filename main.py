d19f8ab739b44a3ab2ab21fae8e92ed258e97d98
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:51:26 +0000

    strike push thumb key by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
