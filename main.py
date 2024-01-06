11ba89a7b2475a92c6f0299e1f662b97a625a5a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:35:16 +0000

    store laugh during organization class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
