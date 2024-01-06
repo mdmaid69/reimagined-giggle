d8cf61735582141a9ff29fabd25dafed5e97a4ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:15:45 +0000

    rate people sense willing comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import tensorflow as tf
+print(tf.__version__)
