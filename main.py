67ee739176609df5372fb71553e3734e53a5ec3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:00:17 +0000

    willing sign unhappy represent speak

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
