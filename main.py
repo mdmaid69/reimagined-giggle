9d15eccd76007aa7ccaf4f68918698dddac68479
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:35:29 +0000

    date unhappy inside object goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
