17d26e05ddedafe74260c0a5d087aab9d6fc2058
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:08:12 +0000

    room sky neither theirs unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_pressure(force, area):
-        return force / area
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
