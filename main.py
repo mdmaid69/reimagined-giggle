b91debb0c583769edd2e56e9b06ec5783ee094dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:09:02 +0000

    mine precise goup increase comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
