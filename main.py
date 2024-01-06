17a9f84fd945dd469c52b3e601034b742d2d6e5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:05:42 +0000

    steer strength partner Ii post

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
