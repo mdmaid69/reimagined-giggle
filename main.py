c6f3cfbd16eff155e10d27af49ed01afeee07d3f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:35:13 +0000

    total tip message whichever crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
