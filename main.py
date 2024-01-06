4ff364f78538ec6a9ed322c2e91159eee9c0ffff
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:21:21 +0000

    hit against question another still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
