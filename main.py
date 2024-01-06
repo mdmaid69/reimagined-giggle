d8fe3e8f32abc08f528d6940aa08efa9adb975f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:49:31 +0000

    no protest collect it lead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
