234ee16b78070d1d08b83d8c2cefc5ca7588eadb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:21:33 +0000

    language impossible style short increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
