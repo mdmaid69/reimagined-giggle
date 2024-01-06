1d3a4cc70ccc4ebcaa81a2e863914a61e85c0b87
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:26:17 +0000

    transparent care cover trust anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+for i in range(5):
+        print(i)
