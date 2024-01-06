fadaa516ce94ca02b6d3cc9f391f2530ca818b6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:03:24 +0000

    praise dig several attention uniform

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
