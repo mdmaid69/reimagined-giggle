23deb81eebfb8b29ad87f627825d20e28163e31e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:55:43 +0000

    reasonable hurt just kind type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_area_rectangle(l, w):
+        return l * w
