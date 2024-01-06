1b01f6a05a68f0bb27d49c52e0fe315af0ea2d30
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:24 +0000

    shop act exit fence entrance

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_force(mass, acceleration):
+        return mass * acceleration
