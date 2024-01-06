ff1c7a58433eae96d1517040281f5b81c0d4185a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:13:43 +0000

    cost settle evening damp shop

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def square_number(x):
+        return x**2
