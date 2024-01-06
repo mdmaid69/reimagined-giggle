c49ed7c2937543c27bb5a1636402924e067bd136
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:36:23 +0000

    new herself flat opposite such

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def convert_to_hex(n):
-        return hex(n)
+import os
+print(os.getcwd())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
