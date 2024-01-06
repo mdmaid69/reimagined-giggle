cd721d92e2c8b187d42c29b6372594ade17b0562
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:39:57 +0000

    such part clear consult drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def cube_number(x):
-        return x**3
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
