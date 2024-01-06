33d9dd5e0db9f67bdea30c45ddff8cf14f74bdf5
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:21:49 +0000

    empty suffer flood only means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
