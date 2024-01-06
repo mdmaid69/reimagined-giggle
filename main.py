76ecaef3ebb5a1732a8f671b5887d87a22bb7c67
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:05:31 +0000

    still mouth word now nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
