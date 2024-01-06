51d02492d682d302f3df3091a87895ab9cfc8aed
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:47:49 +0000

    bury lend follow partner meanwhile

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
