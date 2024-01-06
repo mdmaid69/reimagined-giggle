75ba3068e6ef353f30f914f793aeb6dbbc446cf6
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:20:24 +0000

    true holdup trap often paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
