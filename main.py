b0c51ac06457612139e78bf5367e12f88264785e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:13:23 +0000

    sell year pass example dead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
