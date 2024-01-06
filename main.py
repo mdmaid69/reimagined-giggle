038a798e9cdab85e589a50fb1f2bf5fe73b93168
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:05:56 +0000

    summer past object talk see

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
