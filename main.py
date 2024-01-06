98706825b197e9c01f5a5f61a7de2d6ffe743713
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:28:24 +0000

    team design practise proud callback

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
