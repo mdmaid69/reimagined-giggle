f63a80394b705879cb90f78c19669cd8da56fe55
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:56:07 +0000

    child food whoever store flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def clear_array(array):
+        array *= 0
+name = "Python"
+print("Hello,", name)
