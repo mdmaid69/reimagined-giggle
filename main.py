d8130cb828a745ad3f6a4172e522aa72e5687cd5
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:25:02 +0000

    edge bar family season like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
