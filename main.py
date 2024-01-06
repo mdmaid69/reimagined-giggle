070e3eec375a5aae7d2e83fd2ef250ae10538620
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:21:45 +0000

    reduce nerve will mean ordinary

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def sort_numbers(numbers):
+        return sorted(numbers)
