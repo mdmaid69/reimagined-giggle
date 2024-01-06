f85c543dea40e1e06709f8c67037109b1d08fd86
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:17:19 +0000

    matter hate intelligent home goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
