082ee59fd05f00e7737ad499dc9240f640b9b8f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:41:19 +0000

    charge section club tone mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
