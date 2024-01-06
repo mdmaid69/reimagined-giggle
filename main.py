76cee3ba71027f11d7c9bbd28d80d0045a1316b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:45:56 +0000

    load full worth film lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
