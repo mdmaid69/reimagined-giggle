902fdc2d3003156c9cf2b91a6e242fa1eee9f30c
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:27 +0000

    possession large exhibition hope foot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
