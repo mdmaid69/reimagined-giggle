e017f7aaf85774282149b736516f6440f8f802a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:16:40 +0000

    effect area stop past unkind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_current_directory():
+        return os.getcwd()
