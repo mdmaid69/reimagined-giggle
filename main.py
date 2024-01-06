445a90ace68cd970f0c26ffab42e5d64bc55618f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:27:02 +0000

    date take concerning result needle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
