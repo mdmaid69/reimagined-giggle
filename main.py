14db08e597dba37af78c79e1dd8fc246dc5b04ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:31:01 +0000

    rich progress delicate swallow further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
