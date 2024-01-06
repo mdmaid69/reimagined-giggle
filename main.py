e218973f9af1ca824947b31c7dcc409370ebe26c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:57:44 +0000

    shallow part awake stamp regularly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
