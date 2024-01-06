23874069a7b0d4f54ec8509032f01d66931a7d19
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:56:17 +0000

    into odd inside bus embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
