e1ca056e2f7ed99426a4f5486a23785c7c4b2399
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:09:54 +0000

    loud people basis pump aware

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
