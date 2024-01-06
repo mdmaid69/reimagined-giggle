db53f5b7c314b4ade921cacd48f1cb53edb59bd7
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:49:33 +0000

    appeal claim company long off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_speed(distance, time):
+        return distance / time
