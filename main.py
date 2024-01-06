3cfbbe2ca105c4f747bbb4fd1bb7d11e8e6b0143
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:57:21 +0000

    when rest strong century Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
