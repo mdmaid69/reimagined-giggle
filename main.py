0e776242a40b5b043d6c169ee9469abec6e67da6
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:09:47 +0000

    dear neat software yet degree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
