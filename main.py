23c4671b4835f5b3f565651111ce9c64eefae138
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:48:26 +0000

    getaway hide disapprove one fashion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def is_odd(n):
+        return n % 2 != 0
+def sort_numbers(numbers):
+        return sorted(numbers)
