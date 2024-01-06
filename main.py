af2491ab1fc822e77734a62675e6fa399cd5dd86
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:51:53 +0000

    business their excited quarter slightly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
