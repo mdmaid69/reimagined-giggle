c74c74bc2af6facae08bbfac314c207ed9204673
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:28 +0000

    strong react home challenge board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
