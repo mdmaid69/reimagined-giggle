7ae53eafee9dfd7e14bfb830f1575a9d7ba72ec3
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:54:47 +0000

    air character petrol weak spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
