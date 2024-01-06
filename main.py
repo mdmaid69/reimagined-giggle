3c56a960112854128335d4882acbcacb4d01f25a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:00:04 +0000

    height moral bet fill runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
