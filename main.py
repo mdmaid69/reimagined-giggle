294f1ad94707e41afcc227b8cc6710323ea1f548
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:09:44 +0000

    last ride free stage contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_item(array, i):
+        return array[i]
