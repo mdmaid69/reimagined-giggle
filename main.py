4dfc2cd7e1d55c50dde6941e8fcc90dcab6eb432
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:58 +0000

    rare mean limit reply train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import platform
+def get_os_info():
+        return platform.uname()
