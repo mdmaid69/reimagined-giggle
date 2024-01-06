6d05de4fb3d335d1046a58f717ea6302df7b983c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:18:02 +0000

    base height half ability open

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
