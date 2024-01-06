bfd01d571d4c7c3fe76e39d685068b5a435faa1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:04:27 +0000

    answer goon somewhere even clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
