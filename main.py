f697df9685c00ffdec97ad89790bf73f8e3e606f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:04:44 +0000

    chase proper fence lift separate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def reverse_array(array):
-        array.reverse()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
