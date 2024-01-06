ebc16b1c4e6fd4dba7d75da998e859a6164ccd0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:16:42 +0000

    on makeup jump material please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
