797d884db04fda95c1ec458d4d2f394b38d5a398
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:52:17 +0000

    name state light waste basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
