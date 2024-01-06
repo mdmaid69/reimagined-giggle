1af57d76a9d5fcfdadee53c34b32b117ffa810da
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:59:47 +0000

    listenfor long bound car workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
