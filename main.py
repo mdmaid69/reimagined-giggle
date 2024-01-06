9f1a5ea4e77c4def2aa91148799670b312a52363
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:32:22 +0000

    its relation touch nose guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
