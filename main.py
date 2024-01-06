8e18dab8f4b04168ba1fd5aa7f4ecc1ab19f5854
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:45:20 +0000

    disappear exciting spot bye finger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
