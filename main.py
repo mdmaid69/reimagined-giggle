07a7f7f4a5afa05b67418af7604ef7e79ad066e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:03:31 +0000

    workon acceptable Rr rush confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
