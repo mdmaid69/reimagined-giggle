4fc2d7360756766e67b6212d016119bb413c49af
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:06:45 +0000

    remain culture neck knock fine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
