d91a379f066b9eee50093ae1e2a4e868ec45e5c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:31 +0000

    steep finger for by usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
