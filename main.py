2379ea56dd05cafcd4d23ff0e84da9ef4c5292af
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:53:33 +0000

    friendly this unfair settle in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
