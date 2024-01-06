00f1d32e54fa675d65f3409dfee4d8daf16ce987
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:37:23 +0000

    imagination exhibition activity burn display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def get_array_index(array, item):
+        return array.index(item)
