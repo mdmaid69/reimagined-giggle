5282a10e36f00f69a3b7e7808e28b3d7a2a22afe
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:37:56 +0000

    choose shop priority opponent reach

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
