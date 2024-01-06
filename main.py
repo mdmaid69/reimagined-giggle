f7b89262535cd707b66ff3d4c9d1daba98ccc28a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:45:29 +0000

    abroad adjective learn rate protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
