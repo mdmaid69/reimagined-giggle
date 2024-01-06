aef7af7620f453bef16083961bb8939e36cca77f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:45:21 +0000

    body extraordinary serve minute freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
