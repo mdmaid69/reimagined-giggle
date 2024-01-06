b7f2c8064e56066f8e6afd073b70101bc9a98ed6
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:55 +0000

    impossible rather bush which through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_volume(length, width, height):
-        return length * width * height
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
