16a06e8a0254916aedaea53b1ee8b121a1699f74
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:54:40 +0000

    invite front woman who live

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
