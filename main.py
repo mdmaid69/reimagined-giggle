d91b5180afce4c0530aca675b2b81056b9c15b78
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:37:22 +0000

    skirt unusual previous effect contents

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
