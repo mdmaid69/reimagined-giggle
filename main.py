7686688db2d5bb04e0f5b12b323ec920d841efeb
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:23:09 +0000

    chin possession area object hard

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-for i in range(10): print(i)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
