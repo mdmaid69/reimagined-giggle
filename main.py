afbe6b4b74afece45abd5b36bb599ab9a4693773
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:35:45 +0000

    noise nerve solve uponon limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
