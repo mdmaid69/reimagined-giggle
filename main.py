9a7c3741bbc9990f007b7415629cd0811a0ed3e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:22:41 +0000

    true thinkof hope road combination

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def remove_directory(path):
+        os.rmdir(path)
