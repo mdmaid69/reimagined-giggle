c457bf1f58d482e265317f3745728fc1b8c21a38
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:34:52 +0000

    run although guess brave frame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
