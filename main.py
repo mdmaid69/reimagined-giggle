53b9a44bf29e2f112cda9138842da5974163ed3f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:55:40 +0000

    everything calmly trip comeoff tense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print("Hello, world!")
