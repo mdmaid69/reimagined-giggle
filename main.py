734c1321eb87ce0e25d6ce67d4fb55171ce07f3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:33:17 +0000

    thought fetch round remove might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
