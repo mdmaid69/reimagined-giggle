fa08f5cd7abed11ee3312c70bdbc642c20d84da9
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:42:53 +0000

    frequent entrance polite defeat 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
