78b9fbc40292ce16203c975ba350a187b651b348
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:28:53 +0000

    keepfrom usual before put oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_as_str(array):
+        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
