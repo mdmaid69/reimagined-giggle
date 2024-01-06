4b15cae9784fe38d321054df47ecaefc3d5e3663
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:42 +0000

    space interrupt takeover rent prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
