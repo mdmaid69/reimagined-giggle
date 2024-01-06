650aca89dfd6500d236924e55ed9240a92c739e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:00 +0000

    danger Cc outside system favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
