2daf43750d4c8c51b78b0c233c79dade55d1fb66
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:44:16 +0000

    tense quickly school section value

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
