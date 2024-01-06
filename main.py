550a79446a1d2ece7412d7f25f6c7dcc4e19b37b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:24:30 +0000

    relation along so miss visit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
