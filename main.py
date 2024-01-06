c232c04c4110340b999433a03649e22ac12a2b94
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:45:32 +0000

    tired speak doubt steam crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
