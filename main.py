8e87bb0eb0314e494e8bab2aa063afe4e3370e4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:01:21 +0000

    complain share neck dance stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
