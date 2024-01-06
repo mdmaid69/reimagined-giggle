8e0945045e65e005c5db40ad6486d24afa22cf54
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:43:53 +0000

    luck join suppose murder business

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+for i in range(10): print(i)
