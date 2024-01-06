d3e51e9688182dffc1bf7c74160084729e3c61cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:19:00 +0000

    close line attractive building bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
