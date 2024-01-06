f2d008298560ae77170c76d412ce78198b424d94
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:37:54 +0000

    call agreement movement open major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
