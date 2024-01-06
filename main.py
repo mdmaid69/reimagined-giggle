f3cce8f63f6a308e53d65fe729d76cc22b982a30
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:06 +0000

    interrupt run life clearly wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import os
-print(os.getcwd())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
