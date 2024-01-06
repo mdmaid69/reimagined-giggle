d106165597a17b13f49a9fb8ebca860602aecb7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:05:46 +0000

    Bb land thinkthrough usual wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import tensorflow as tf
+print(tf.__version__)
