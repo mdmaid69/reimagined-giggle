1f44d1ea2efc90f86362a6be02b5257cc06abf5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:18:39 +0000

    feeling turnaway splash matter himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
