16b434b3c045ee41cd528a830475e1bce0e56f18
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:43:37 +0000

    keep treat comeover loud gate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_array_as_int(array):
+        return int(array[0])
