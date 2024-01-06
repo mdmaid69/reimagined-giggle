a60ab0c415b527a7d4488b481a2e406232c85955
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:19:47 +0000

    wheel act runover protect unless

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def convert_to_binary(n):
-        return bin(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
