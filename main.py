948fafe94f79e744b82e9d7387e41bce2566b8c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:55:32 +0000

    busy comment low certain scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
