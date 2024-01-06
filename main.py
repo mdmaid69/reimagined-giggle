92219a57cfa72ea7f183a1b7c30c9dd571aae171
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:58 +0000

    flat listen pupil politics possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
