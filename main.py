b6738b959ff56367a34a0395dedab2a4edf93ac6
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:43 +0000

    label most boring century success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
