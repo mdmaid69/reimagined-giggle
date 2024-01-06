a5e11c974f4763401ab31ebe3eb95730f2c7134a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:23:11 +0000

    along sweet happy mainly live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-x = 10
-y = 20
-print("Sum:", x + y)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
