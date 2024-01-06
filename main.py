b0e51f1c35a63ffe65e9023489bc59fb869ccabc
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:22:02 +0000

    national shock if with arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
