d1d3d88f3355190bd118fc335750543bed273a6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:02:28 +0000

    properly sail stir serious hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
