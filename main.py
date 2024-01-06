d5a2dae7d2400f607c64844273f2f84a0bc4ae64
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:34:47 +0000

    its start excited noise priority

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
