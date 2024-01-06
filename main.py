6455e9966deefe05445b4a0c0588ca8852872bef
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:34:17 +0000

    extremely know hurry die whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
