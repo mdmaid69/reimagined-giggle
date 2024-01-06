deeedd6cc3f5cf1b6ad507855447c3243fc81f47
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:31 +0000

    service tap unusual pencil tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def reverse_list(lst):
+        return lst[::-1]
