50432d96d02892e0c41ed90149b3bf13610d5979
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:52:43 +0000

    heart city block 569enjoy next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_float(array):
-        return float(array[0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def remove_from_array(array, item):
+        array.remove(item)
