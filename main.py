4680645699d95c5b9a4ddd6bd85e2a319968f35f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:22 +0000

    so drop set start result

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
