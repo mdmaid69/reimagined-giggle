00e91ee7f854f4f3b96fd2207c466a46902ff58c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:40:14 +0000

    offer seriously term enthusiastic black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
