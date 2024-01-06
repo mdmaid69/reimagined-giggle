72adfa6ad33f8d3577d78defeaa3a2ca3abc4c28
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:17:16 +0000

    borrow rob report neither condition

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def sort_list(lst):
+        return sorted(lst)
+def convert_to_octal(n):
+        return oct(n)
