b4fc2bcc2ae437661b882a25fbdbc6fa2fe757fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:06:58 +0000

    foot opportunity once young city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
