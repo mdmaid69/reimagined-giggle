0d820056cc869b924a45b4f2b0f1ba8a14a31549
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:53:13 +0000

    consult successful next month woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
