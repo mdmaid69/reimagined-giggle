6b1b87b5d1be1907b5204cd88ec8be72d5cf7829
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:52:49 +0000

    long change design anything ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
