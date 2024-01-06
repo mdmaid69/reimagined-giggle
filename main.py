e2870a935b96b51dc086e47de276640f11d10e38
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:24:12 +0000

    necessary number we male quit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
