2060d568482b6afb2de4c1999cbfc0324f7dca5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:47:42 +0000

    join noise lift offer story

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
