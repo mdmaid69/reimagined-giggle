0099ad15288e3415ca7cc2daebad2982694fb246
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:26:13 +0000

    reply difficult high bit keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        return sys.version
