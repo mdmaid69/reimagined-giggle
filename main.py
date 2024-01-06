6c53c9eb0f19231a242b34d82140f8079fdc2d0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:14:44 +0000

    bringback exchange scheme remove giveup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import tensorflow as tf
+print(tf.__version__)
