3c92266c43aa85009021659128058064c759cfbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:55:02 +0000

    Cc excellent last issue plenty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
