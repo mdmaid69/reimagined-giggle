2a6bd92656734cc1508f675dc01940e153c872c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:10:27 +0000

    sort beautiful hardly street nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
