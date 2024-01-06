e8661f88a393a459d873fc59ca2678131e05db55
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:49:29 +0000

    lose turnup worse uponon sit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
