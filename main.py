872d9c38d0f5b45eb89c4ad3773aafc8c8a6f901
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:07:32 +0000

    gas switch mend morning unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
