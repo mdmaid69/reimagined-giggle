b7aee68ce1dfd28c92cb9f4ca132e36608b5166a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:19 +0000

    shout send runinto bang position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
