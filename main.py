3367b94fb7e111c3d3e98845ed95e641b39fe858
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:40:08 +0000

    reserve fix science every discovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+for i in range(10): print(i)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
