bc9f814eea10adb06b428f075b800c26045f9952
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:13:39 +0000

    calm on raise boring even

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
