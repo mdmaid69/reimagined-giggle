44a4efd896153703f351ccb7e8d4dc00097a9bff
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:18:48 +0000

    length belong there sister old

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import math
-print(math.pi)
-  def count_elements(lst):
-        return len(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
