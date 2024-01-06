213bb8507e2db1586680edf21a5b1826f4f25edf
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:59:23 +0000

    best Nn until type possession

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
