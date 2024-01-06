80c1cf9a646b6f9aba0ccce009ed9504901f4719
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:41:15 +0000

    quality large quit develop machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
