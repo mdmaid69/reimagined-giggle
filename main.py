9468c3707acaf8af2cd4f5bec317367de852ebcc
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:51:45 +0000

    affect mind figure neither game

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
