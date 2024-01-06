aae43cfbb6a2addf4adc2dbb9d157b3ddd6941cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:41:35 +0000

    comeout nonsense help slight sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
