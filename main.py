9cf18bb5354ae3930b55de37193fe8d066bb51ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:33:27 +0000

    bound bite deserve anyone home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def multiply_numbers(x, y):
-        return x * y
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
