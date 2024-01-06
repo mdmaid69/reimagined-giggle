b9640c072c324e468ecf814db5ff0f29a90a0233
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:54:03 +0000

    when steal variety copy sorry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
