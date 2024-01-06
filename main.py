d5c7021ee21ac2d8bfdafbc886bb664af5c607cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:25:28 +0000

    lucky worse catch thing hot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import os
-print(os.getcwd())
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
