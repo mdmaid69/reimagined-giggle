a8e2ad871838b62d8760681df39e4ed8f2b9b273
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:03:34 +0000

    goback(to) sell direction occur telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def convert_to_octal(n):
+        return oct(n)
