dc422846c529c5b7390d0ab8d03246d8750acd54
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:34:39 +0000

    whoever bother sweet priority rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
