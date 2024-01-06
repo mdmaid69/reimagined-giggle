569e490957506bf111a86944e2d7d772dfddf540
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:01:42 +0000

    pot anywhere forever embarrassing immediate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_density(mass, volume):
+        return mass / volume
