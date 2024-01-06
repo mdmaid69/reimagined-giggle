d5c29b694dacff3e97bc6b3b2935fa57d9f19609
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:06 +0000

    song hollow illness to subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
