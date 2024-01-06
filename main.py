3fc3eb83fb9d3f7848f755437955e6ef9402c48d
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:23:53 +0000

    cause scheme neither aware stage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_stack():
+        return collections.deque()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
