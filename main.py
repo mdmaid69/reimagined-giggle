f042c6f6bcad172e4e573748d3551990981c8b00
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:53:04 +0000

    continue know slide general approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
