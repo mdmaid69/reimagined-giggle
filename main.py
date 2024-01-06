575e365f8e0d96c9d3e4919893b51f36ffea529a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:25:40 +0000

    may opportunity else platform seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
