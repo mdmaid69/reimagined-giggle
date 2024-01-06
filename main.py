23e6014464adc67312f0b22e6bf89a1c82dfb98a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:12:26 +0000

    music touch aim record awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
