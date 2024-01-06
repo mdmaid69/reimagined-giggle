5e0b258801ec95454eaaecc4e0d9edb9fae0c75f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:58:32 +0000

    yours bye personal her trust

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
