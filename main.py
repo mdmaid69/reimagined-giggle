7a53bcdda1ccb39bf4650edcf5f3b11557d09b39
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:41:59 +0000

    appeal ocean open point under

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 name = "Python"
 print("Hello,", name)
