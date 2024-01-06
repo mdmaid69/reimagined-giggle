83a69fecc8fb86c03e45be1823bf29d6fb65deb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:41:03 +0000

    yet storm suckme weekend wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
