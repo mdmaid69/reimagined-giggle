7fbb02f2afa86d8f3738d32d4dff5809c8fe781b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:45:26 +0000

    cool weigh announce before sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
