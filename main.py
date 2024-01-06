ae427eca7e2b193785be40c3fbfc6c07ff33c507
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:31:17 +0000

    564encourage pick reject edge less

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
