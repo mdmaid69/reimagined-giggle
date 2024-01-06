dcc9caca1d7e27672b2011edfa169b004193f530
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:56:10 +0000

    spoil walk giveup realize seem

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
