04368be4fbcdcd788d9ea453d5cbfc73deec9520
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:08:31 +0000

    bunch quickly shoulder machine month

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
