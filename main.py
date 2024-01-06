21073493dd496244ac4092b23c9961917c8815be
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:29:23 +0000

    fear instrument wind mine fear

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def append_to_array(array, item):
+        array.append(item)
