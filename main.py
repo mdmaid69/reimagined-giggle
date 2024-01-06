c046c7c08bf49346c930e41d9c8c6476d4b5f3dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:08:04 +0000

    Ii believe hardly throughout think

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
