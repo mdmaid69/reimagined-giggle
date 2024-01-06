07d63066509f1ed01858d1f444bd65936cfda7cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:00:12 +0000

    turnback he pick human experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
