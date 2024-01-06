11eec24efcc25b61ee6c48a7159ac8d479bd8891
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:26:24 +0000

    offer after error dull excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
