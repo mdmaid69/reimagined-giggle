bc30139bb2faa9a2524270937b676de642b4024b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:04:20 +0000

    control raise favourite interesting float

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
