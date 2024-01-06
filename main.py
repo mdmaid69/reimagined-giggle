2d3580210d439f9d08b90570a07c1c831f95e2e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:07 +0000

    breakdown petrol for final put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
