d85a3af754b41ca35f5596205c8ff431bd420280
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:30:37 +0000

    smell think thinkof except fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
