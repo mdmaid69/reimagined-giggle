e941be3192a09bbb07c49d9f11117f1f2f4db618
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:05:48 +0000

    pity truth design continue team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
