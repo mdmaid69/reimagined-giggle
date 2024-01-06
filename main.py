e09da8422fecd0341880cb936f1629e139af2c2b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:45:28 +0000

    stand pure design hospital behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
