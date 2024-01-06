9523ae97b34c4d3f8db3d52284fbb7335c0e5888
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:05:37 +0000

    equipment aim sort whoever open

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
