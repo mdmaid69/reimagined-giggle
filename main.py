a4736c53b51bce030cd52049e04e88f15898c7bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:10 +0000

    clean by suggestion space brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
