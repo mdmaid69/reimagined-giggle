9534024a43c4a3aa339ca8923baf910d87d27ba3
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:14:27 +0000

    that speak achieve dream invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
