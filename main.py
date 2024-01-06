ae13876cadc3902885952de02c5c68099cc8d73c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:50:00 +0000

    book shock community consult carry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
