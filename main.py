813393f9de118d072a7af07d5fcd41b0d03d65ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:13:17 +0000

    whoever surprise quit spend reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
