410d23e34abc333671ffd573c1d693d91aebebaa
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:01:07 +0000

    honest pick extra away border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
