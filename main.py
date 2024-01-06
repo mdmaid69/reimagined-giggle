dd83ae47d5c5dbaddc907f58d6c07d7b0e665c64
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:26:23 +0000

    watch turnout temper find crowd

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
