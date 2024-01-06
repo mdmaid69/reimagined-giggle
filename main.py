e9a1411a8ee153cade16f1e5070a6f60dbe8ed7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:15:15 +0000

    dream mixture plate afford print

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print([x**2 for x in range(10)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
