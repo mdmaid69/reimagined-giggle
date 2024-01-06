0fb7238e2887dc8d3dc252bdb8f8705c1b539ed0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:32:46 +0000

    score language advice fasten cap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+text = "Hello, world!"
+print("Words:", len(text.split()))
