02fce00d67a385258e1b081de814e857dbcb2a91
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:44:07 +0000

    spill lend before blame suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def add_numbers(x, y):
+        return x + y
