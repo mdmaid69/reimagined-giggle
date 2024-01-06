d2185d0a49c6308da54c0dbff8e49ea5304e8956
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:37:33 +0000

    object quiet distant tip lucky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
