36f64d92f95775ea4b84648e4c3b0752c1fe5576
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:55:52 +0000

    drive cook murder ask service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
