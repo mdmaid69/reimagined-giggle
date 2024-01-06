2d530aefee87633e50086107749b7980e5c6ce8d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:54:29 +0000

    miss phone cause doctor after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
