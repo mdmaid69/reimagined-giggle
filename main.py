712c8daee50bb4a7276b44be08aec6f9cee16959
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:13:20 +0000

    home especially real valuable among

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
