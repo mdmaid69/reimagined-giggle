15bb5e6d8db41e8c76c64ece57fd6e3b8495a4ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:02:38 +0000

    along exercise press foreign cum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def reverse_string(s):
-        return s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
