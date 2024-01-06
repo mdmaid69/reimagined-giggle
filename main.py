bff27f2f64e6a1a2a8445f29a304774b05ceed88
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:21:59 +0000

    system place mainly custom confuse

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_stack():
+        return collections.deque()
