cc8207b4218922dc63dc28d48c640b28784a8790
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:00:18 +0000

    become over run land bringdown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
