727cdf7eae4bd6ae3bf671c25209942d4938f621
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:09:43 +0000

    tie occur getoff hold opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
