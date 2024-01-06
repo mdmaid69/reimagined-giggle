93a7ab1203eb87aa2e7c4c9b85422ae49f42a428
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:49:21 +0000

    change odd scream ahead smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
