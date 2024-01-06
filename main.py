d2268251619734b2447397c54ca8a4f5e35eb617
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:08:01 +0000

    squash behave father emotion public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_logarithm(base, x):
+        return math.log(x, base)
