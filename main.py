c1000f0a64f7ebf0dd22ccffc0326166424f78bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:08:56 +0000

    wait Oo carry miss children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
