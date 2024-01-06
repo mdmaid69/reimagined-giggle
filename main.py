d6fea983e570af0e099efb7b5bb4657e91401cf5
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:42:18 +0000

    party criticism fight innocent laugh

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print([x**2 for x in range(10)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
