306f889cadd9733fa2048a0851f195d6dcc171f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:41:01 +0000

    tired regret sort advanced cruel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
