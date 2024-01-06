828069fc493adf794e027c61bc092d265ae6f4e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:21:07 +0000

    company by impressive sleep polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
