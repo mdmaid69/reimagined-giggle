94a3169cad96b88847a67e4c0e23480a4c9f01f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:44 +0000

    repair successful rent arrangement murder

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
