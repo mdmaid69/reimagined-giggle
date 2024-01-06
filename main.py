4877bff7690a632572920fd4333aa09434fad766
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:53:37 +0000

    fairly favourite communicate town will

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def remove_duplicates(lst):
+        return list(set(lst))
