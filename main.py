d8a40616d0b79ace262775d0721c4409584527f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:45 +0000

    side meanwhile lose acceptable makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def print_python_version():
+        return sys.version
