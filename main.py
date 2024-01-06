92adb56925b9be16592e128ec7d5c1011209a9c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:36 +0000

    shoulder they criticize shout borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
