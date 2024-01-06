b33c23667e139e36c81d11669ff7bda8b8281e21
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:29 +0000

    consider giveaway row matter subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
