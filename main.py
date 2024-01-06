1e7aa68e53a351043b9421e24c2d73cbb9f2b390
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:30:50 +0000

    firm ever cover funny skin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
