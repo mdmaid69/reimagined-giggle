4ff9816bf4819351742d9fa50b58d0c5e9f5caf2
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:11:24 +0000

    acceptable shut quickly clean sorry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
