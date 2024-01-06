a5f22246b97060773773cf31d4dcdda823a4f4b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:08:39 +0000

    game available shall attract notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
