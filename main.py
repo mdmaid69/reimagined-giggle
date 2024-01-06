b230c5f459f89c21351c38a8b1afc96a55e6360f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:27:02 +0000

    protect bet recent exercise my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
