5f66dcc03b13fac38b5f18a06697a8bfa0a8a4b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:19:51 +0000

    back permission deep produce burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
