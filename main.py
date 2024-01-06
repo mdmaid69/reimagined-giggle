5a4c98e3f92e7f8afdb25d154db8d37cb541568d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:40:10 +0000

    suddenly check congratulate left shine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def is_even(n):
+        return n % 2 == 0
+def convert_to_octal(n):
+        return oct(n)
