ab99877b1446e23629ba3e8fe3c659da98e7a649
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:51:14 +0000

    sea quantity environment effort uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
