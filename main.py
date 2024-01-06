d96e976c5a46bc7a6a7b87492e0251bac328fe0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:34:53 +0000

    567enemy child quick community react

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
