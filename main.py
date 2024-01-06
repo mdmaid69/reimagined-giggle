f8f2cc4e43246f11d808182c22d32e8a7957594d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:44:52 +0000

    gooff lot scheme character middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_index(array, item):
+        return array.index(item)
