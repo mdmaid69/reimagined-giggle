974587f8a1e0efae60c6db52e8989bd2bdeec7f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:42:42 +0000

    extremely emphasize increase what many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
