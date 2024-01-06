9b0fa56d412eacde2dfd5b76ef775a752b4e9979
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:05:15 +0000

    near consist pretend because female

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
