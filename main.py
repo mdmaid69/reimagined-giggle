e7cc4a2268396ce4396ad235bd40b879dd3ec00d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:39:55 +0000

    sick pile burst skill loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_circle(r):
+        return 3.14 * r**2
