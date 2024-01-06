70ad7f3635b1844f8aa7a9ed2e0b44089eb64eea
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:35:06 +0000

    embarrassed able drive father operate

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Hello, world!")
+def square_number(x):
+        return x**2
