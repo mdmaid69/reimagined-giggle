d6746f2add403a4a0078a424299c52fc77273799
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:38:42 +0000

    man refer need Uu precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
