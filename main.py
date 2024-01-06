96a8360fbd63158fdcac43a3c784e903d1f82920
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:11:28 +0000

    before cheek lonely behind protest

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
