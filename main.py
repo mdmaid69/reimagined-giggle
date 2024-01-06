3cfacbaca40ba0d51eab8e8f446115eab88b7479
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:39:29 +0000

    charge clear middle collection big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def square_number(x):
+        return x**2
