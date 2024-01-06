867f3178c2174d209208026198a5d138df743577
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:19:30 +0000

    characteristic lend off present practise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import json
+def convert_to_json(data):
+        return json.dumps(data)
