68917557eb4f6be80723ff38f0b951e050e876ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:21:39 +0000

    powerful Dd represent lean dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
