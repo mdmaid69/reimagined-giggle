8a7f9ebee6eb45135b49de3dbd96830eb8d2656a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:54:31 +0000

    divide before petrol exercise complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
