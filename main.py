2db54a5adbec715c66f6c6b2338428cd23f47192
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:01:52 +0000

    freedom again unite street impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
