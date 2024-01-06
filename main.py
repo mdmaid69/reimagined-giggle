45ed18499f0a01ca24d1ded21d14c7bf6aebbf42
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:49:11 +0000

    trust polite that satisfied watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
