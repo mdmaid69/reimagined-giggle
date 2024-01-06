5f6e2ceafb79d8facd81b67af646d3c2ada9eafd
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:08:33 +0000

    fix please negative company left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
