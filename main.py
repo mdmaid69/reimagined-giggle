c8d70a3873c1acf8f4ba7b6145bb21d367dbc007
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:20:37 +0000

    thick yet spill thanks best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
