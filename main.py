d3157e0d7aaa27bd038a55b596b57c18adaa5118
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:21:05 +0000

    transport pronunciation damp bill big

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
+import os
+print(os.getcwd())
