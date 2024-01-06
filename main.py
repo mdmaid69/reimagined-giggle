68d71eabc1ef48b9bef46f419f12a0e34eabd7ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:19:41 +0000

    send active achievement middle crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
