9a557ab0a781ec1ff28fc451dcedafecc847c778
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:50:09 +0000

    generally claim funny mistake urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def reverse_string(s):
+        return s[::-1]
