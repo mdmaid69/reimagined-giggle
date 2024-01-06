c16f2a810b37320d505760e8379e11b522267bbf
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:52:57 +0000

    ignore travel high 565end understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
