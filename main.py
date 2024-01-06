1aa058af4d81ab2c62740666576ddfb950ddbd64
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:10:02 +0000

    upset shrink Yy flight slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_set(array):
+        return set(array)
