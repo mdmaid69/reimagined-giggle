0ba0363d63fb5a21e2291213de2312c8a403753c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:05:05 +0000

    announce grade upwards during big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
