0f21338de2f0e20bc54fe850be05976baef7f1b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:40:32 +0000

    child solve waste mystery congratulate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
