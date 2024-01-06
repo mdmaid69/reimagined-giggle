6f39de1371c67fd06a00cd034630fa5fc8e9cb98
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:23:10 +0000

    this sight wrap sail know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def count_words(sentence):
+        return len(sentence.split())
