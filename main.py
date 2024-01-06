27bd2b73769e65cb30b205673010b3caadc8ff60
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:27:11 +0000

    comedown zero ignore use play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_current_working_directory():
+        return os.getcwd()
