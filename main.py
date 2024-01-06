87f598501b023902996c0ce5218baa87efb305d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:19:22 +0000

    content little goon accidental long

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+print([x**2 for x in range(10)])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
