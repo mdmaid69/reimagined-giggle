bfa7c41d49540081c738adb5b00fd36b4bd49942
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:16:20 +0000

    Gg born either question condition

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
