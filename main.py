76903407090281549acf6c09250dbf02251d1fb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:40:34 +0000

    mistake hurry gap scrape harm

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
 import array
-def get_array_as_str(array):
-        return str(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def pop_from_array(array, i=-1):
+        return array.pop(i)
