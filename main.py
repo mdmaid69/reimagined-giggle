b2b7412adb4c1303c8adb958a1d89dfa3505acbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:13:05 +0000

    maintain word moment face often

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def convert_to_octal(n):
-        return oct(n)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
