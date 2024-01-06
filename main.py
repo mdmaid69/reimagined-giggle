6bbe6aa56996cab14e8940e22ec00b098342b8d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:51:34 +0000

    quarter definite Dd however warning

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
+print([x**2 for x in range(10)])
