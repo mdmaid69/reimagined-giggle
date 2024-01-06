4ee1aa026c3ceba654f977aad347c382d25e0ba3
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:47 +0000

    authority occur dark back cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
