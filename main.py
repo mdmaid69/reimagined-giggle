1c86b235ff6824b18ae57532791a1c2fc5962b13
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:12:55 +0000

    its would thought forward shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_counter():
-        return collections.Counter()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
