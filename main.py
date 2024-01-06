aa6b444a58977572670ca9c81ccf52a613dddc30
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:28:51 +0000

    respect confused mad welcome tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import tensorflow as tf
+print(tf.__version__)
