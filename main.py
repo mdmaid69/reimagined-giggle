6ebe1ee64d4381cfff0616183557530758d01ffa
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:31:43 +0000

    majority rate passage succeed warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
