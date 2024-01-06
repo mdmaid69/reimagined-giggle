2898c091d3bb37ba81817805f6180aa207143583
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:40 +0000

    that issue nerve shop bunch

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def greet(name):
-        print(f"Hello, {name}!")
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
