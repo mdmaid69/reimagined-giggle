9c9da30afd7aca43e0e49c44f35d3fe12ebc5e05
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:34:42 +0000

    forward walk no square worse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
