5e5620325f0477a2ce29bb9d89abe7d248c07b43
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:03:30 +0000

    talk bringdown it top ill

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
