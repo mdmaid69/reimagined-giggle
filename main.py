56fa7e46198fa37d79936ca11613a28ba6e6c0c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:41:21 +0000

    rent acceptable exaggerate meaning meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
