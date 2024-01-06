205ada2245e08271d00cc8cfab408aa7ee7b041f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:13:24 +0000

    luck admire situation violent before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
