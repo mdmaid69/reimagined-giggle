da72f1a341db4b4acbfbed3d4fc123a113176deb
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:13:42 +0000

    concentrate high some teach them

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 from collections import Counter
 print(Counter("hello world"))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
