13b644f668a49a46d36b1348318bd19699778a25
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:43:24 +0000

    hungry suckme over clothes mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
