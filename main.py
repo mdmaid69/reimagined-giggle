f607fb193ea5627d0d7ad69484cfce4c8a94ce05
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:55:59 +0000

    square too alive tread warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_speed(distance, time):
+        return distance / time
