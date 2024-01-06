3ed3db3de6ac9a2109dc67c87882c0e6975829ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:38:05 +0000

    finally wake stage stamp theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
