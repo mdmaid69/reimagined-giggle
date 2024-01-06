54d0b2a6701161771f18731c37671060b71b4045
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:09:30 +0000

    hope meeting hair backwards turnout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
