3757e6aba0a2e282d48f7cdab02745b6d25be6aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:53:03 +0000

    health confident dry offend disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
