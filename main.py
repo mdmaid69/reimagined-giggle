58d2e4125f33d6897d4b458306b9347a5742b992
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:41:05 +0000

    roof stay and takeaway lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
