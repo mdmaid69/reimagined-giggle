ff798338415d6d450cb91088bc50c5ef81b1c9c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:09:18 +0000

    unite avoid friendly kind bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
