377af88cc7765f9770d502e84a5cee242551ff19
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:00:33 +0000

    next condition juice his callup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
