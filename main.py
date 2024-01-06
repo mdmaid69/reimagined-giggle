ab15a502aae20d4380dfda9f5e84e96abc4f7c02
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:38:27 +0000

    effort second miss recommend usual

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def count_elements(lst):
+        return len(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
