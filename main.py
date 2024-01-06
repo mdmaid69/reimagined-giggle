db6303377e8b4e7064a223a76cebe336e1e1bb1d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:46:51 +0000

    tend family factory surprise memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_itemsize(array):
+        return array.itemsize
