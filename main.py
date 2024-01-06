cd455d151201fd15424ad52d87890307cc13d162
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:48:49 +0000

    life exploration agree sweep tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
