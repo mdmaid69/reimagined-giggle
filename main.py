fbd7c4f2fa373b78c7f6c06b723fbf15aabaa889
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:01:49 +0000

    exhibition deliberately lie behaviour divide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_volume(length, width, height):
+        return length * width * height
