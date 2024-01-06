6baa635a8004688faac906a867d0a2c98b82b741
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:56:54 +0000

    improve gentle interview feeling course

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
