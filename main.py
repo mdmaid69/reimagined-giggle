b08098939e355e3e562ca00ccbd652facf6993bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:28:31 +0000

    improvement strike stone woman react

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
