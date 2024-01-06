ed5640c16d028a9477acdbbe20081f3baa62c234
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:25 +0000

    sign excuse mention sleep mouth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
