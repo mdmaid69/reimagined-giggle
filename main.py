298694f9cb2879f73a7ce9e69652b537d689fcb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:39:50 +0000

    basic fault period must keen

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_error_function(x):
+        return math.erf(x)
