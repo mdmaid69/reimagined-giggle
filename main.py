ac9d87aec650315df3bb146397828b6bde588111
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:55:01 +0000

    blame clue commit problem other

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(5):
-        print(i)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def reverse_list(lst):
+        return lst[::-1]
+import getpass
+def get_username():
+        return getpass.getuser()
