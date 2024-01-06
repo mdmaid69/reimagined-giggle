8e2847cb942dbc49a2cbdca97652088c6e377359
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:22:04 +0000

    aware refuse move today level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def is_odd(n):
+        return n % 2 != 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
