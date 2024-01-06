5348b952512fb3bcc8deb6d227d189198c8ce36e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:21:39 +0000

    roll hollow offer brief apart

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
