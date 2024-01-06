7e981bbc9ab14a4e88da03f39cf822cbb74561ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:09:56 +0000

    partner lookafter flood talk damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
