2395bdd0a85f6a6845763b2f2d37e9e5b02fcfc8
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:19:51 +0000

    interruption exercise precise history cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
