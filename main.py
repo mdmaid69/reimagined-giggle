84d6815a9869e9647ca97b87f3b5f0049a1cc678
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:35:15 +0000

    quickly deal otherwise turnon use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
