f7f59287376130a8569d063f8f5fdc935d2542f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:59:11 +0000

    drive accept building pin to

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
