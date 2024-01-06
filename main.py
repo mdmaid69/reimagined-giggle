3b315630b1dc853b6d1b892261cae862da549aed
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:04:18 +0000

    long congratulate dangerous spend sport

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+print(time.time())
