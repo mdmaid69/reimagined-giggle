8b0e35820edc4870a7d8f3eda0f7305049898c57
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:17:03 +0000

    little low must tough release

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_speed(distance, time):
+        return distance / time
