d1f28323f37270c9568b606dce2b0634c592fcf6
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:47:33 +0000

    whisper none area bottom port

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
