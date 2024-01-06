f4bdf5d90b923eedb23039be3680d29e3de7dcf3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:21:05 +0000

    soft shout know hit protest

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
