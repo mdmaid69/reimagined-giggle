318bded594ef96241e23b0d975d4a042445e5e1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:28:26 +0000

    front expect average sorry far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
