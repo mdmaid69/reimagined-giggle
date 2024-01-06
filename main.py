0e06953dee6f6441ca14477fda3a9d525b5dcb36
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:04:30 +0000

    blame classroom badly careful list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
