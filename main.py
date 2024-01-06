4ea46eed484cd6c05ffd58209de196f09012ec95
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:17:47 +0000

    bit bus mood fix less

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+print(sys.version)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
