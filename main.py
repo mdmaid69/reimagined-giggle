1006c9685f68043a4196280648789f162dd64250
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:56:26 +0000

    sore yes accordingto manage keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
