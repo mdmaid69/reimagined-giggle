8d88addb88e7d0ee051e6c7275a35e984a975f74
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:46:08 +0000

    sometimes crazy talkinto excited holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
