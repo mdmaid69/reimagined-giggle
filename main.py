aec631a66786fdaa8a2ad6d72281634caa00241e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:12:21 +0000

    separate shine notice black thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
