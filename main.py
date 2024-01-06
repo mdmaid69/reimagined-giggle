e4e72ed17d6cb2eaf2318fbb0c271d75f2b1f726
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:17:32 +0000

    private peaceful describe explanation issue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def greet(name):
+        print(f"Hello, {name}!")
