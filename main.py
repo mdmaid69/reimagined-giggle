ec3acf65aefc1be8825766defb24f8e2b86724a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:45:38 +0000

    small surprise condition scared uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_list_from_array(array):
+        return array.tolist()
