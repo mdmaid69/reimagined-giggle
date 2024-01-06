a0176fe971cbba54a946715f02f6547bd919ad13
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:29:08 +0000

    careful drive miss broken raw

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_pressure(force, area):
-        return force / area
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
