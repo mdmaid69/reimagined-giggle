b1978322fc25cd0aff851016d5d8774fbefa9b74
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:29:15 +0000

    figure hurry pretty each children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import os
+print(os.getcwd())
