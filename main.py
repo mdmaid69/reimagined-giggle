4e1121377c51f4d94aa178240873c87d7eb34533
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:43:06 +0000

    dare mix everyone mess completely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
