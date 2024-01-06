4bcace211c1343b10ad655a690b8f0d899700a6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:02 +0000

    sorry century passage receive flat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def reverse_list(lst):
+        return lst[::-1]
