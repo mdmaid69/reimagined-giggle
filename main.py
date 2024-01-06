f8397758112c3be3376a5fe6a3d58ba1b3864515
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:22:48 +0000

    be advanced badly patience onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
