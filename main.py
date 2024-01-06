0e54e631a62aa15a6a66178de7872a9349a84900
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:33:43 +0000

    guess just shop cruel wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import tensorflow as tf
+print(tf.__version__)
