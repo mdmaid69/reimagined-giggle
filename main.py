fc8406739ba2ceaefb230c04dd37d9b915e8ee7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:24:54 +0000

    wish bored whistle well could

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
