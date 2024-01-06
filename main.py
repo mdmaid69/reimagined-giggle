f32012cda5b035358a72bb1254728c445d3c0e90
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:28:24 +0000

    achievement dish before smart careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_volume(length, width, height):
-        return length * width * height
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
