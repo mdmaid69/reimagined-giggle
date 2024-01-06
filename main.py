98b2ca760830e382cb258e3840cc33da3b66b221
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:27:20 +0000

    reliable shade war petrol dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
