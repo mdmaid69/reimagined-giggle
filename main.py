f074674d04c241b3123a2571e27fbd4495abfc8d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:13:10 +0000

    point before underneath number judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
