9a3615be51730bd1b79034ce90674199728ad38a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:26:58 +0000

    coat me various should official

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
