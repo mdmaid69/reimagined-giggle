cd39da565e63b90dbe6d158df5c423117de1c77a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:01 +0000

    drawing fly official do entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
