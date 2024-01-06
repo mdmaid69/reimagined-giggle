260696f75a323a80c2ab580ffaa66b739f9063a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:55:21 +0000

    nerve advice admit city round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
