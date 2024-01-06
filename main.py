765f0d2832981f52b4c725c09732c525e5111653
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:24:26 +0000

    moon admit big luck attention

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def convert_to_octal(n):
-        return oct(n)
+  def cube_number(x):
+        return x**3
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
