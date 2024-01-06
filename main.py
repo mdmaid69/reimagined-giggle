5f80cfe0285f7a08d08f995eeb02c44dc65b7fb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:48:59 +0000

    class like marry shine understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
