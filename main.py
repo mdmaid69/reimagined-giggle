c97d6fb78fc52e1b142f3de37c9a955de4637af6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:11:00 +0000

    share exercise goodbye 565end improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def clear_array(array):
-        array *= 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
