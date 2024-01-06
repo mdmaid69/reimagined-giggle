e24f008cdf0cdd349a48dfe6cc8f716884d44e4b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:19:35 +0000

    occur sign ring none during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def count_characters(sentence):
+        return len(sentence)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
