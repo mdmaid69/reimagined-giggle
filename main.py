cf57ab00d2c4b1cd271cbc70bdc43e12bfa7b8ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:12:05 +0000

    giveup turnoff scream empty happen

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
+print(sum(range(10)))
