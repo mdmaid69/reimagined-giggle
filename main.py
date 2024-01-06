385e1b598d1a8f703c4d6a5aa6bd21d27e040303
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:10:42 +0000

    underneath pay sad suit certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
