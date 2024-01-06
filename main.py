f428945543d71adf2f57336d78d9fffbfe1fa741
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:05:21 +0000

    big wall offend vote point

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
