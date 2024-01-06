67af8f4cac0ce3c876dc424327268d532d2b8864
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:10:00 +0000

    mistake design gift common must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
