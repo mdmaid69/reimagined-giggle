d1cd79c89db6f1408cbadf0d42bb6b3b227451a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:23:40 +0000

    runover thinkof complicated fit good

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
