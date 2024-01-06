033f6ff7b4effbe8614357993290941fa8f1469c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:12:40 +0000

    light sure save public violence

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
