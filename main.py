f7e7c7ae48fac72d8b3ec892212aa513971e9ab7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:30:40 +0000

    minute curious description refer difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
