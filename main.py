5251ab60c0fc1c0950c583d911e9b8e836891f2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:54:34 +0000

    straight lesson occasion open turndown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
