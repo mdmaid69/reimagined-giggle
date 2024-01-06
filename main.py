b7c2924aa959a5427d1f363aebd1f93dd4b055e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:46:11 +0000

    pollution match steal black scream

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import array
-def get_array_length(array):
-        return len(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def append_to_array(array, item):
+        array.append(item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
