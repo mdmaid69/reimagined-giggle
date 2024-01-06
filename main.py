c2614b94290c9f4fb824fcce925453bec423e35b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:16:55 +0000

    deaf lean manner view treat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
+  def convert_to_octal(n):
         return oct(n)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
