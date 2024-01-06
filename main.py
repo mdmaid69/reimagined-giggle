3f5fb0ca98e25ef32eb34912c936e473c8b646c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:23:27 +0000

    people much steer help spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
