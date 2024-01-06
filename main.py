0079bbb86c22dd682ee222e198711dcdf356f9b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:36:05 +0000

    risk slip fall workout through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
