6575f5847ce85a82a2ed5056e44496537241e74c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:58:24 +0000

    exhibition purpose brush 570enjoyable far

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def sort_list(lst):
+        return sorted(lst)
+def convert_to_binary(n):
+        return bin(n)
