080d0e5a1a300436334cc4b93b9c72e7178a781e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:33 +0000

    project independent instead holdup slice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
