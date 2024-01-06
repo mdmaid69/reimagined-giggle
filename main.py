ad37de375741a0781c420b4354e383df335c3197
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:23:02 +0000

    proud risk rain noisy whoever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
