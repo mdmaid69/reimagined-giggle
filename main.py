4a040e28072dc346b3f3e9a28c30f8689619694b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:55:39 +0000

    would pain mood pin programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_repr(array):
+        return repr(array)
