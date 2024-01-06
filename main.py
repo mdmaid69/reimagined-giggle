d667df5f2805b8736e751b815a42e248513284a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:14:17 +0000

    steal related pay involve hang

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
