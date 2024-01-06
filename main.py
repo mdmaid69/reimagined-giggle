f8f121073ab761ef761085c9af0915cc6af85aa5
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:02:18 +0000

    load load activity importance great

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
