37015cf060921b74e5f8761c474630b42916a942
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:42:18 +0000

    correct division nature difference in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def sort_list(lst):
+        return sorted(lst)
