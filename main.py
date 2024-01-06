42afac673adffdb078fc8a0e43f77cc714ba1d0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:51:08 +0000

    frame foot holdout popular praise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
