f1bd02f5fc441deafccd8b0cb5dc4c62ef00722d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:25:17 +0000

    refer affect obvious organize term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
