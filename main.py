011d5321fe0129486adbc2b15a01d02f2ac451d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:56:17 +0000

    drink view price mend moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
