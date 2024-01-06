60bf36f1f066410f858186eaaf084585f87db3d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:47:13 +0000

    recent turndown relax every finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
