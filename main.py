68e890caba1fe1d717afaf6e3ef351e496171742
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:26:29 +0000

    independent tough aware complicated pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def get_array_item(array, i):
+        return array[i]
