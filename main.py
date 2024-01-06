0988843fda5249d0771f90bb96dfda516e4dedd1
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:24 +0000

    trick boil accidentally stir measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def set_array_item(array, i, item):
+        array[i] = item
