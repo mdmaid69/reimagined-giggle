8fc8e8e595dd67687e0b61f9b333edae9e9c539e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:15:15 +0000

    tone exercise develop very never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
