157144e09e9af2be33ec0320a2d13a54293419e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:38:13 +0000

    company choose reply Yy actually

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sys
-print(sys.version)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
