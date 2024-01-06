3cc0d634a843d333da3ba83a2fcc1811542ff66a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:35:24 +0000

    protest after direct cruelty song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
