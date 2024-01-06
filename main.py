40fa53180406d743b9331816d0e4b50f3c9b5633
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:43:39 +0000

    polite responsibility new cross herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
