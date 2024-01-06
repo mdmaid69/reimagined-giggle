1bec6d0d454dc67ed705e3c5950cc92305a6c3bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:07:49 +0000

    ring bringdown tend history harm

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print(sum(range(10)))
