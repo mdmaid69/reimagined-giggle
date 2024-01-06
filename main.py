390a0b96303683be2eb854717bd3d2a2fe91daa3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:25 +0000

    sail recovery rough image let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
