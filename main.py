fb7906bc72b192be142f0651b393f07142d35771
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:43:27 +0000

    behind division warning sorry plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
