bc84bec878b05cc691ee4778ade91543db7fd9d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:50:27 +0000

    worth contribute abroad upstairs choose

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
