e753dc8530073624ea0eb32ddafecfcbc6bded6b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:37:35 +0000

    comment tragedy grade anywhere Mm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+print("Difference:", set(list1) - set(list2))
+  import os
+  def split_path(path):
+        return os.path.split(path)
