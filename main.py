207aae8b9e7cee93874341294a312deb6f91d32a
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:19:36 +0000

    crowded trap doubt careless fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_circle_circumference(radius):
         return 2 * math.pi * radius
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
