160266be619ea0dcd7105cbc1be9fe242b3ed90d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:00:41 +0000

    same quick turnaway over open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
