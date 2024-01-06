7bdcee8e437434f00f6e75316016eca3b6cc5596
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:38:13 +0000

    wonder reply bringforward bite great

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
