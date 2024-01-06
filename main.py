92d65b7d51a30e328eb1f4155c6384577606aa95
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:13:11 +0000

    full seem glad wind turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  def sort_list(lst):
+        return sorted(lst)
