08e51dd5b34aa2700253e7493a2b6930005b7f2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:46:36 +0000

    odd colour curious matter course

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
