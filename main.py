19a02d2ab556c11126f85e9c8aad8de519552e48
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:27 +0000

    win act get remain instruction

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def square_number(x):
-        return x**2
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
