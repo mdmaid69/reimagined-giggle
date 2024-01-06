1c15f79d0e79dd244111332a09a3e0d5eeee37e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:45:52 +0000

    warn man in lonely importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
