a825cbc1c49eb79f4cd304d54616f3dae1ffeda6
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:18:37 +0000

    react practical later official empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_length(array):
+        return len(array)
