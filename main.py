021667a6280eec7a4d13faffd1de81c2b6f750e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:44:22 +0000

    murder nose order mix beside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
