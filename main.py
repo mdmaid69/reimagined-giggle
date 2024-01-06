e87f6b1132cdf4e03a52a3e8e1d20044b481c08d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:27:29 +0000

    ready getaway tooth plan careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
