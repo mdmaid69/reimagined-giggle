cec67bcb0910fa5a23f930414d3fe9baa2d190a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:14:49 +0000

    consider tooth rent usually shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
