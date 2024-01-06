b1c3e2238a84f301f7e97c871711021a39ab495d
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:33:06 +0000

    list whisper goout university pretty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def reverse_string(s):
+        return s[::-1]
