c5eefc3aa69b1a433c6477a96888c1fc7315953f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:32 +0000

    completely arrive shrink intelligent print

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
