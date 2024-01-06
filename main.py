100f73617ccaac6202d98341f3ea135052187af5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:56:33 +0000

    powerful stamp strong limit failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
