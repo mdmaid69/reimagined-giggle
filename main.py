25fe1a36b017fa93e8fcd4490c3a6adb5e1f3b11
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:46:13 +0000

    shoot issue class ordinary upwards

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_area_rectangle(l, w):
-        return l * w
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
