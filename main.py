cc1c6b7149be6b4514896afb53c26174775946b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:31:01 +0000

    substance whisper concentrate fasten say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_numbers(numbers):
+        return sorted(numbers)
