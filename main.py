747a814cc49284e36eed9e7d10a06a0314903f39
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:25 +0000

    square bound OKorokay stick determined

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
