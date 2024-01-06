6dd109c3bb5c6f594d72279398ccc273c143d107
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:47:01 +0000

    come task smile like begin

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-print("Hello, world!")
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
