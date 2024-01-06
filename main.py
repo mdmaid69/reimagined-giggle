cdc5a49aa13290dd9b9aa1d6fa5e2e3292a696b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:02:47 +0000

    left calloff rough peace treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
