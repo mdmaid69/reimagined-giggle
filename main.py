db3c752bcc385d859034978393514c2a6ab9e016
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:16:19 +0000

    copy favourite first please pattern

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
