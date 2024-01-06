df68ec907967215d56b9d6b67fb2b5d9ee47fb02
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:57:17 +0000

    tomorrow carry address dress various

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_directory():
+        return os.getcwd()
