23f27e2385591950561eda8516d768718c446213
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:20:29 +0000

    coat healthy now clever firm

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print("Hello, world!")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
