86f81482de36add8b0e7afb4c81a54be549a6abe
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:48:57 +0000

    problem include one under ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
