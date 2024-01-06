58fdabdd0f4a6bdfffa0a3b330127a3c54cbbcad
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:37:48 +0000

    impression choose rob challenge deaf

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
