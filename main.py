32e3958902a8794c3c92bb63d6e7c3152dea0ed6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:42:17 +0000

    shop damage happy fall lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
