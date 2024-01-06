59cfc1dac63c61527dae5d80ea12217d8831f17e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:35 +0000

    bringup strange disturb black swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def append_to_array(array, item):
+        array.append(item)
