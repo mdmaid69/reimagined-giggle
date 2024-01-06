6def20d4c520e3b247002475cb2941d5c0652d3a
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:26:03 +0000

    generally bright listen worst gooff

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
