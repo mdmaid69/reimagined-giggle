e424d7e80e18d6b84b2ec69b368ec469780936df
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:10:15 +0000

    again happen previous probably big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import os
+print(os.getcwd())
