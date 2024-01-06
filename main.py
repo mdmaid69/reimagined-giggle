857ee9ca301400d4a534bda8b264079212017f1f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:30:05 +0000

    emphasis medium Ee turndown pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
