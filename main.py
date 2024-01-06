5e2c4fed0935c0abf6b78af37e0e59d4ceeb2a56
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:38:52 +0000

    point scream chat rain same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
