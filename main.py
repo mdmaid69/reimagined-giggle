3b2291b1d328abd4aafd81e8ccae6c4abbbce9bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:31:35 +0000

    fork range break short exact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
