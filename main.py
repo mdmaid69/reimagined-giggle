d2c7aebf547f7dda400a80c505a01bcbd5e25a8d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:52:18 +0000

    breakfast she stamp detail market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
