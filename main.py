27c60b8f7fc448da1cf8ba45e8d3e02dd2e7fa2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:01:24 +0000

    average keepoff basically above far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def convert_to_octal(n):
+        return oct(n)
