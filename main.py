a05e697e646a508c59d238319a07344d5df650ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:47 +0000

    hurt and plan guide hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
