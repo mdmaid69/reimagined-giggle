d1ab39007462affdbbe87e3e0da6d55aa57205ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:23 +0000

    positive similar below hardly change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
