9be975b74768b41fedff0324bab9139e71c63a43
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:55:47 +0000

    method turn might loan she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
