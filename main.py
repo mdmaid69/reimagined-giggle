9cd411fad1beae789cbb9e6c759add727f5bcb8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:47:13 +0000

    something less match recent equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
