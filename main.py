fea6358254c3b0c0117db222fe1c88378dccad41
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:33:00 +0000

    direction summer direction comeup roof

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import sys
+def print_python_version():
+        print(sys.version)
