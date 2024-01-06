2776869ebd7764ad40e6e89073d3e7a26532fe3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:22:01 +0000

    grow mystery convincing form nonsense

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
