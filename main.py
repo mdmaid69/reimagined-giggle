942bb80eddc94bf4a4829da1867000a09a412ee9
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:38:03 +0000

    present communication use fierce step

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def square_number(x):
+        return x**2
+  def convert_to_binary(n):
+        return bin(n)
