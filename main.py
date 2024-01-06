94d45ce3ffae9fa5b0b0f5681db779a6ca63ac2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:10 +0000

    offer turn really move laugh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
