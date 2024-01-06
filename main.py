9cd471804508e9e4ee36db1c1b728771a95c4493
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:23:24 +0000

    long escape gooff understand single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
