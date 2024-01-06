4e65fbee855713c3b5ff6e246acc07da991bb7e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:34:42 +0000

    correct copy straight claim should

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+x = 10
+y = 20
+print("Sum:", x + y)
