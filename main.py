6d9cca694de980094b2ecdbffe602865ee0feed3
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:26:27 +0000

    make wake admit there trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
