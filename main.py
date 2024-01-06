752e04feb00d0f6e7a104dbf58c691758b734cc4
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:44 +0000

    lookaround skirt apparently quantity remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
