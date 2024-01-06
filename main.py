35ce4b622384380b58f3df4aca42c7451fa5288b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:55:36 +0000

    above beginning surprising imagine sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_area_circle(r):
+        return 3.14 * r**2
