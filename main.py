dc20513a59f4964a20eb6f098067095eb481b93c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:37:52 +0000

    company design play phone unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
