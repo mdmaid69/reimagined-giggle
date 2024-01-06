2754c90b2074d096d57f2cc49155904971e02c5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:38:06 +0000

    curious deliberately pencil base next

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(10): print(i)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
