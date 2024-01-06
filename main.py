05e4e1e447559729cb1b6687d5867022bdcb0947
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:41:37 +0000

    happy accidental work divide new

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
