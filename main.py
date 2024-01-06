67fcb33ffb290b2cb051dbebb5869361c394cd82
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:21:01 +0000

    operation disapprove add repeat weak

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
