559cdf31f59f3963e6290ebbc327f73fb4301a4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:14:25 +0000

    negative happen money beneath fasten

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
