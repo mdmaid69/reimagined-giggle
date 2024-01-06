302c4a3d710a576f098a3f0d381aa447e4f1fb1a
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:29:47 +0000

    relation floor top necessary anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
