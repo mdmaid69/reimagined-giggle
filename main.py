428924b35e2188b36be3a46f9fbb3977d6c96cae
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:45:21 +0000

    similar loud minimum answer build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
