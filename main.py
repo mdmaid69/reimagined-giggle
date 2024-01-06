1c1b8781d3a6701170204b45e83a9f46d46bc595
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:33 +0000

    comefrom level spare dinner work

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
