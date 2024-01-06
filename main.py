e292cce42760934526c219b5cfe420fd97789965
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:55:18 +0000

    period knock complicated furniture thumb

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
