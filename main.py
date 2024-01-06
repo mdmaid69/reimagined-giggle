a5c3e5bfc1cb632b679eeb0851d2d7eb47e02dbe
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:19 +0000

    break improve serious legal myself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_binary(n):
-        return bin(n)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
