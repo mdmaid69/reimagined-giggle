8370301d816ac2e1ba4d69d4004b672e16ea31ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:28:44 +0000

    we cold stretch culture joke

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
