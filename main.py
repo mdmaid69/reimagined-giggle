54e139e3339e2379f56466ba54e6b2d604e84a9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:05:17 +0000

    open exercise description lie getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
