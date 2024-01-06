f9ff8972fb2dc9a1390b466e68e4cd69de13a8cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:51:29 +0000

    certainly do taste famous smile

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_current_time():
-        return time.ctime()
-def count_words(sentence):
-        return len(sentence.split())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
