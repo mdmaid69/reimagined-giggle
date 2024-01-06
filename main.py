2c9595b7c2314fbb7087f2008f7ed2ac10c31274
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:09:47 +0000

    prove answer odd think now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sys
-def print_python_version():
-        return sys.version
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
