5f14b5e96180b9dc6addb7ef7ca11d1d28df1483
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:39:55 +0000

    drink pity fetch Ss waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_to_hex(n):
+        return hex(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
