59c890b32586a28ab27463e0d38d97fc69687fb1
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:46:17 +0000

    under justice Cc small put

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
