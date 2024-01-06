c12a9ecdfd0291de8b6025dc850f8c161d32025e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:03:17 +0000

    centre door worry someone unkind

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
