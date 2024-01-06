fcfabdfcd3486ae24c2901b1e1eb1c6c9af5243e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:32:57 +0000

    mark fall quick round past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
