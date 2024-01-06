8899ba40289bf3fff444df7ba81e2ac7e7fe69c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:55:04 +0000

    interrupt interview stiff strange unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def convert_to_binary(n):
+        return bin(n)
