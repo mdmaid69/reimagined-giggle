b90f1659804db9e624a3ab2de206527f6421a7fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:12:40 +0000

    slight say reason guess temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
