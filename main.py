b01c288a34461cb227a6ea407f339bd833f489e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:03:46 +0000

    let sorry education shoot miss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+from collections import Counter
+print(Counter("hello world"))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
