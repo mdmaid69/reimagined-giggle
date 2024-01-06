2005f8f8af03a8a42cba4e90c445d0b47e2e105c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:26:43 +0000

    mother print taste high interrupt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def find_min(lst):
+        return min(lst)
