6215df8266f01bc9c9a73fbe1bd31eeed924fcdf
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:55:00 +0000

    plant leg put off picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
