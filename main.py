12e4505c32bcf0fbfecf1227b6b84a8bd6a0ab7c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:54 +0000

    scene same anybody hospital measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
