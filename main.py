02aa74b5f104e378d64dc7f0d8c4b38f06d74764
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:57:53 +0000

    our pollution press time wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
