00d18fc72ecab1f6364b934b7089d743cfd8dce4
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:39:17 +0000

    close rain mystery payment zero

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
