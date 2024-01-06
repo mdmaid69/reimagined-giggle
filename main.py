aa056fcd7bb7c40fa3e18eefee66257ca45447d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:00:16 +0000

    worry pool smoke drawing borrow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def remove_duplicates(lst):
+        return list(set(lst))
