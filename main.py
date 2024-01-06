316711e57cb49c64215b1bf99ac7e2f1f159dbae
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:03:27 +0000

    later there polite method path

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
