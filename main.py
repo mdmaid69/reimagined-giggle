36b96675f8b11e8b8c13190655113456a244423a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:00:26 +0000

    now pollution when risk surprising

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
