975959de3564ad8e18c638810d3831b4e37e7989
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:01:32 +0000

    afterwards meet church land family

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
