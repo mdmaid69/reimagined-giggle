493739b2069943428724f7070555201f708cd62b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:13:02 +0000

    arrange key summer round human

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
