6c9d6dcf77c32f19d13f6ef549a3fce205edb669
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:19:13 +0000

    neither danger cost idea guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
