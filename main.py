feab8864f9824a91b06390cd5946e645d2e825bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:45:38 +0000

    round skill low child charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
