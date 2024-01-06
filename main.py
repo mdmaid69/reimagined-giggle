2773d9dc4a572c7a6f3d9299f19a55466055e355
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:52:04 +0000

    turnout rest satisfied answer getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
