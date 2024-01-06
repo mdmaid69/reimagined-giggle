79089544157440e4a93a48c49e3fe545b943e377
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:34:30 +0000

    exaggerate border confusing sleep still

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import json
+print(json.dumps({"name": "John", "age": 30}))
