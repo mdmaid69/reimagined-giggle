74a368f17e902f1a8c8cbf233688ffc7d1d72b61
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:52:39 +0000

    block birth goup year single

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
