15ac1116e7ce1d508e56b858d323a3fe1a644118
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:07:30 +0000

    delight underneath gate rare tear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def divide_numbers(x, y):
+        return x / y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
