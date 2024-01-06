e09d5c61e70e57781e1ba84cbe0dea11472171a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:32:21 +0000

    sport accept drive colour common

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
