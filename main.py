cc8bfea6ef6a3feca4622f2ac3f50d5da5020d26
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:50:41 +0000

    small busy preparation seriously top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
