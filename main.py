24f8e55e814d33f40a8ba54c169631c8407de996
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:28:23 +0000

    print free pass affect pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
