15de50cf3207c39b8736e73770d98dbd4c944a84
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:12:45 +0000

    hers history used teach fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
