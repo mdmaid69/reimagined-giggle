2f839fb44c17c4187ad7251b7dc463e68b1d5482
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:51:56 +0000

    equal bring Cc port thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
