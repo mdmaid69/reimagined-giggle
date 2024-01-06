1f5b1f29063bd63c84f1e411aa754c3d40a13c69
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:22:02 +0000

    opposite talkoutof great enthusiastic represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_density(mass, volume):
+        return mass / volume
