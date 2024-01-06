20d2a5b0277d63f78a95252cdaaab339d1a6ea35
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:13:55 +0000

    crash along badly pretty appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
