c4f1fc1bb1068c7d9813dc408bd3f625293ec30d
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:08:45 +0000

    visit distance comfort cover raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import datetime
-print(datetime.datetime.now())
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
