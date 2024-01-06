23241c11dd12407837b8fe7a304299ee2fca6170
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:34:24 +0000

    recovery feature 567enemy achieve allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
