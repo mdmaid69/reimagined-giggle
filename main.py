004449c321e5401afaa3d8f4972c915024978517
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:34:29 +0000

    further action use information kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def square_number(x):
+        return x**2
