7a2a6e8418f26d1eaa34b58d52981595f09e07a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:35:15 +0000

    joke nigga bringup deal left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
