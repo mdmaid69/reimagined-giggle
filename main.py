abc2271ff95dfd7da3a4f465dd90c18a6eea2a2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:51:29 +0000

    dry giveout passage wide seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
