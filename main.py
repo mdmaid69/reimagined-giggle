1b160fbad47e7f750f7c4cb40dc424344fba175c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:42:35 +0000

    arrive stretch flat lookafter strike

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
