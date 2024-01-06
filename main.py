e89c03f5ab461558fc8baad58199ea4c3a7defb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:21:42 +0000

    hit hair bound last whole

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
