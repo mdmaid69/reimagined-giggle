04688b8552987899ba91eee948814cc2f98668cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:44:02 +0000

    sure mend pen workout hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
