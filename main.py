4937a6688f42128f4dce0bb0469f32f9c7f6f1f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:24:10 +0000

    community yesterday soft point morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_array_as_repr(array):
+        return repr(array)
