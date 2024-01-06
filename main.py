9319342ed3c01e6a0aa8a0d86f3972e9fbe45638
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:18:29 +0000

    alive deliberate which private someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
