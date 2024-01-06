f239c153a32f238e533ec3e9024a2e9c838d15bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:01:14 +0000

    impressed emphasis receive bear failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
