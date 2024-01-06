3ab63e57102b980e26fc7addd94a6ef392d82e44
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:15:20 +0000

    usually face this tap callup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
