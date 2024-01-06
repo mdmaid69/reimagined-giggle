e2307e004f010283cd15b2cd9172c72df720d9db
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:42:01 +0000

    early smile talkoutof beneath ill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
