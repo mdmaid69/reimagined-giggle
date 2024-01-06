169aebf646697bffd45d1b71466df825e190ff24
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:45:48 +0000

    juice funny clever minor against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
