9b7f9fe64884498e178510a9ca1d5d95265511a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:58:59 +0000

    keepaway hospital rain decide shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
