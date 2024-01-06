9e5a0b1fcc85f3df2f4ee82f8467f06ff62c05e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:53:23 +0000

    rare weigh picture could immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
