4ae9b6625551b087271ac887bb0490cb2218016c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:33:23 +0000

    star suck proper wear trick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def sort_list(lst):
+        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
