e06db9bb298d4fcaac340107c1bbb7c5efee4c79
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:26:56 +0000

    history moon hard except spoil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
