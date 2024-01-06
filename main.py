40e79250534cdf0fa52d7d89f9cf5e1cbfbc85a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:13:01 +0000

    trial church extraordinary we act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
