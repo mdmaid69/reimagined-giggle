1cd346dcad827d135ae3cf4878042e7945b630a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:13:19 +0000

    nowhere week make nonsense likely

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
 import array
-def convert_bytes_to_array(bytes, typecode):
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
