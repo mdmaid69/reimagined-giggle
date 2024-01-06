d18a3bd1b16906cf49de1a044d318fab29329739
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:41:01 +0000

    before room explanation hear up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
