450b37bf72f6b4813c49d8f18a01024363292660
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:26:57 +0000

    slice country shoot of development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
