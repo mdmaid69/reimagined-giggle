6f9feddde418ac236ae21014a9423aa6ee664174
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:46:17 +0000

    thankyou delicate unkind bad upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
