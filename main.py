cc211aaed750c81d5a1477a6e49096e770bc0790
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:57:53 +0000

    hope unfair smile window worried

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def clear_array(array):
+        array *= 0
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
