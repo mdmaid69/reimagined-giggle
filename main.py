c5a17aa1f15f6e6fbbc8261b763418c3a81df344
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:50:45 +0000

    murder until confuse responsible frighten

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_as_set(array):
-        return set(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def get_array_typecode(array):
+        return array.typecode
