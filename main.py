52bcd873d440fda20ad17290d6f30a56ab8970d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:03:48 +0000

    off male job grade best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
