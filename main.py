8fc562bf67ab07194bb26015aee4b69954559238
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:14:45 +0000

    exit prove shop punish breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
