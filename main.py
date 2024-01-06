f452d2912572ffca8d42133415850564a51174f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:30:46 +0000

    slide taste takeback rid urgent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def count_elements(lst):
+        return len(lst)
