8ab291e53c2327225b1ee6307a2244c9801c17d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:36:58 +0000

    finally recently will judge takeout

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-x = 10
-y = 20
-print("Sum:", x + y)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
