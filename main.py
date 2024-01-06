057756afdb910197e7be7d7ba309cb25258d8422
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:53:02 +0000

    peace sometimes happy wrap defend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
