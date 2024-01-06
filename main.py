822bb2349174fda69805fd59276e0d64c3c72101
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:18:33 +0000

    stop pipe matter until usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
