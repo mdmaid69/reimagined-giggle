7f08a406bec3e9e8ef349437a1fc84b2271bb967
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:14:54 +0000

    listenfor scare myself prefer cruel

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
