5320dfb51907552464fb9abac54b8045df43f34e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:14:13 +0000

    unit pool mad politics news

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def convert_to_binary(n):
-        return bin(n)
+def add_numbers(a, b):
+        return a + b
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
