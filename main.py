cb14e7508e4ec5677ae324c637c677f56186609f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:34:51 +0000

    increase snow leave rush doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
