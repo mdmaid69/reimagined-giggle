173d25ee494401a9b68af7aecb29002080f708e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:42:33 +0000

    butter congratulate wander thankyou practise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
