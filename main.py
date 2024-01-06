18215b6e2eae6aa8fe13a39a459cd3e882e8a268
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:05:52 +0000

    politics every throw government shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
