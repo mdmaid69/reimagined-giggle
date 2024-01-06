ce201faa496e14ca7c37589e285a3837b9590fea
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:12:39 +0000

    sort dance threaten exploration stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
