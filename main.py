481faaea486b7ceb28a1cfa7152c141d89d67e97
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:35:54 +0000

    mouth shame time band sick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
