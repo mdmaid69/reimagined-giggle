a61e4bf584f48c9f50642b7c60e984ce92be1c7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:57:43 +0000

    judge long believe callup worse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
