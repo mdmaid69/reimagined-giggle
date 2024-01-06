1429d02bde397dc277858fce368e8007a8202808
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:06:28 +0000

    dream foot introduction still possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import re
-print(re.match("h.*o", "hello world"))
+import math
+print(math.pi)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
