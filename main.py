1f60074d1a11a5a5555b3a49222a0d8115b11bd8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:05:02 +0000

    effect murder receive deliberately movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
