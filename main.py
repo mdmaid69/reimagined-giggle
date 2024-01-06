c32ee18ef9e613f80bbad173230704bbec1b107f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:51:07 +0000

    variety label fork comment level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def multiply_numbers(x, y):
+        return x * y
