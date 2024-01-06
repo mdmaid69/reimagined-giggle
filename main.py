3426d14fead81b6ad54a9b96717d042dd30a2ca4
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:53:01 +0000

    factory busy disgusting serve number

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+print(sum(range(10)))
