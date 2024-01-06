0fcf0b4343b34f11dd69c0f68dfb733dee64489d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:45:37 +0000

    oil comment shame minimum skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
