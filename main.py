e4a8ad0c62888d7247ec0fa0da7a932a376e033f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:43:52 +0000

    course murder contrast whole callat

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_list_from_array(array):
+        return array.tolist()
