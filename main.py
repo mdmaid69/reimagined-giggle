f9788d3af3736a289c3b2116ff1a4f77012fcc6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:33:47 +0000

    copy burst activity order instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
