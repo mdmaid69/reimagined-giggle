8f4f805af1777c7bfe56992b6330a73c86e9cb49
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:44:09 +0000

    scheme flavour separate front getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
