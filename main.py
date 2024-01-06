f647c04974ce62a38e6d903dae20d26029494ebb
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:46:41 +0000

    mend communication stay explain pocket

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
