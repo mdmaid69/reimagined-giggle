1aca0043d4e83253f2987e11119359cc447ecddb
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:04 +0000

    towards sorry here steam estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
