97396a7a791a5f2d7b2ab52d500b269e90f76264
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:46 +0000

    disturb sea modern reaction disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
