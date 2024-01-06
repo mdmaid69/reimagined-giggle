b40956bea0e260ff67f67e4b532e6c06edb0db2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:24:49 +0000

    regret fall when radio before

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_hex(n):
+        return hex(n)
+def convert_to_binary(n):
+        return bin(n)
