82cc8ab07c1c53088270f1425a783c0648606fd6
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:10:39 +0000

    take balance up constantly brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
