be42c5e3c786017510cda34ce3653464798b6d34
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:10:56 +0000

    calm earn 569enjoy Vv keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
