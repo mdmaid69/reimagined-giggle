aeabc6ee0efb942ad20d413596c47238fe7d220b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:24 +0000

    bored perfect wait ticket chance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 for i in range(10): print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
