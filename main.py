6b3485501d3ba8f2da916c7697894fb1c0e1cd90
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:29:18 +0000

    pair reliable church learn knowledge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sys
-def exit_program():
-        sys.exit()
+  def subtract_numbers(x, y):
+        return x - y
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
