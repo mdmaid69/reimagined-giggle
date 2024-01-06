4d32256137eaa06651ec8b60ad4aac36eb7fc280
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:29:28 +0000

    organize active birth tap getoff

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
