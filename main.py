f602ff2883a972d9e94e9ec85c93f7176deb6702
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:34:50 +0000

    whichever breakfast slice 568energy Itself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
