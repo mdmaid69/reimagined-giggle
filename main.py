464edd520ed056e4f0f713578b94d67ad2ad4f43
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:43:01 +0000

    cost hit close sharp past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
