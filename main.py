84dad2f3a0bfc0663c84c36617936feb81c24251
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:21 +0000

    start lean ride equipment Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
