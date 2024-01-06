4f4ff5a548b671e258e43379d82a97078fb4c93d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:39 +0000

    crazy cry hope turnoff fetch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+def get_current_working_directory():
+        return os.getcwd()
