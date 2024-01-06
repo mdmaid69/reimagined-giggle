d66b0ccdaa1832dda5cbe80e44a007e8646b8c89
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:49 +0000

    easy up enthusiastic beat rid

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
