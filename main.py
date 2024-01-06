8ecab446ba101a8a22c02da86b8d284a0a40ca17
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:11:20 +0000

    expect or develop if the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
