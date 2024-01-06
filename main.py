5ce983b8b53e99385f6fa2615ba2ae878f75e4e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:10:39 +0000

    problem buy our gooff expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import math
+def calculate_floor(x):
+        return math.floor(x)
