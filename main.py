bcd4d12882a1fad1a6de0b8a75e2eb854dd69d78
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:54 +0000

    stiff hear either not consult

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_error_function(x):
+        return math.erf(x)
