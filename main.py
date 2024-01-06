8b34a5630b4b297cdce4b340468fdeb2eddd10c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:55:50 +0000

    lower face then land time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
