2cedb41ecb9dd1525ff9e5dbee79d9367b2855f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:45:40 +0000

    classroom road gentle fond sudden

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def convert_to_binary(n):
+        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
