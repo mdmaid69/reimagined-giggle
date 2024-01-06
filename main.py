4621776fc8613cae677217eac8487de0cb2211eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:17 +0000

    under again fork minimum I

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
