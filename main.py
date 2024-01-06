32b4cdfe2b628c80c2e1fda3bf960254e4397e3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:43:34 +0000

    error final stare takeoff card

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def convert_to_hex(n):
-        return hex(n)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
