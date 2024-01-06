abd0e823d84330bda8741bc8e882c536cd869a23
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:51:19 +0000

    heart plan slowly give shock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
