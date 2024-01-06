cbf25474d36df6773e9778cb90a79e35f12612c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:47:50 +0000

    around hear over doubt confident

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+for i in range(10): print(i)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
