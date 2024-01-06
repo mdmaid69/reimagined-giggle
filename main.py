c15d0275392562a7bab8d257ff7858dd448a003c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:50:26 +0000

    long towards fire plenty tooth

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
