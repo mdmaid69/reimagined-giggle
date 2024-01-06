2c9c1237732d03d4753b3845f4713189ca1d3e33
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:18:45 +0000

    square towards nose train long

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
