68ad33926f7fa01c532830f651b6143378c8e5f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:05:35 +0000

    visit interfere capital 567enemy attract

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_max(lst):
+        return max(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
