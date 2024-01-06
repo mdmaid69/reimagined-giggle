0cea24e88892395ec8f003bed61a8ba8f9bcfb21
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:49:06 +0000

    attention bye beneath danger afraid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
