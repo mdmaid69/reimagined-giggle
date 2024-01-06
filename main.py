d4668d9dbe419feb1b352c5b973b499a1cf1c5c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:20:07 +0000

    come string rescue weigh exam

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
