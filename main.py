16803021b12792ec211c328d96879f7058651f94
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:45:00 +0000

    weigh mouth factory period recover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_power(work, time):
+        return work / time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
