cec08b95a96e0d8a54174697cf9fe962583a7c4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:53:42 +0000

    neither plain deep section open

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
