d0070aa9b512b38fcb706753e15fa1d2db74098c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:54:47 +0000

    would region 566end criticize none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
