1db1117f4437776cda4369f7a76e5d2fb5961bbc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:42:34 +0000

    welcome unite touch original display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def square_number(x):
+        return x**2
