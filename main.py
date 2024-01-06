e29ab318194d8527fe3b05f82efdfa9cb9a0deb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:18:05 +0000

    talk drop close young drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def reverse_list(lst):
+        return lst[::-1]
