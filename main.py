99651755738c1e35658ae850aa2e94e7742dc9bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:56:30 +0000

    error there pleased shut cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
