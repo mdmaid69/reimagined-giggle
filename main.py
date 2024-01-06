66e62d7df87863195d185d03f923f1ae52c94d5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:24:13 +0000

    certainly profit violent provide so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
