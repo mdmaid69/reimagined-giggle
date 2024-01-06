d2067046eadbd3d18132720d4d6f5060919eed02
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:35:46 +0000

    goback(to) will gooff imagine top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
