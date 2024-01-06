a97fb23899d377d8233748af576afc659dd99bde
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:41:55 +0000

    complete image man land ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def subtract_numbers(x, y):
+        return x - y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
