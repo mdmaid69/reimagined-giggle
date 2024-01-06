e70e6a5cce7da03903570ab92c12eab7a5a141db
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:50:01 +0000

    wonder 571enough needle survive beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
