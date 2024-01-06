1f8e23b73b3243ddcbbe43ecabe44b14bcf066de
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:01:35 +0000

    immediately judge protest although bound

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
