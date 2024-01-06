3f7191689f202720349974093e7677a5bf2cd9e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:11:08 +0000

    poor quickly music tough before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
