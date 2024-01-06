5825b88bd1dae3deb09dcb5668d9b873ae11d6ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:55:00 +0000

    stranger belief negative false decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
