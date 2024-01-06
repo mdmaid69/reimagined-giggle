73462c4afe41773edcd00c5d1a8a97c07c9dd148
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:32:46 +0000

    connect supply damage disappear juice

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+print([x**2 for x in range(10)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
