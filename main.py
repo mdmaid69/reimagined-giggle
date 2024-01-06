06fa1106b5f9d0c25d0f87e37e069f8afc74e192
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:35:36 +0000

    print careless start night lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_floor(x):
+        return math.floor(x)
