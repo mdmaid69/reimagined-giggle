597a0556e20ba6cb412919c5daed9ebf37f8b14b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:49:09 +0000

    bored uponon machine behind situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
