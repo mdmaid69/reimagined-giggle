3040321e19ee6e418b91b4a89e87ea48483fc58f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:19:33 +0000

    dig list basic present car

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_pressure(force, area):
+        return force / area
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
