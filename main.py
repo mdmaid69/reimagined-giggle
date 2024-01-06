06a606fa2e9348094bac0f05fcc8451c0f9d1720
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:34:59 +0000

    without pleasure the to belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
