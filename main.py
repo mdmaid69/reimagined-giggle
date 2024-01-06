e0cff734d9f16897471a4ec2948f1f341ddd76f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:13:06 +0000

    recent fit change lift clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_user_string():
+        return collections.UserString()
