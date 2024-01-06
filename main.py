e66f842b1ec10a8ff73af81c854876db4efecda6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:08:38 +0000

    home judge criticism arrival mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
