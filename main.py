bee3d0e8bfcdd18f6b6734baa7e6ea304a6390d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:23 +0000

    day fast evidence evidence about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
