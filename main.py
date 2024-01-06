3562ef786151567160f55304402eaaa64b778bf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:10:33 +0000

    bite issue wrong ordinary pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-def print_python_version():
-        print(sys.version)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
