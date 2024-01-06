73290022acaefa1d12f7d2b874dcc8ee10abf1ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:03 +0000

    system independent service letter world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def clear_array(array):
+        array *= 0
