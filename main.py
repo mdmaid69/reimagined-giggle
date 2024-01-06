a9444a51daeb7ecd4e1b456a3599483d54e4b804
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:26:43 +0000

    experiment region moment Yy worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
