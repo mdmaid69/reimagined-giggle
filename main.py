fabb08aeddf14ebae44a88b61a437ef94afebd33
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:22:33 +0000

    nervous benefit turnoff wrong basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-print(os.getcwd())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
