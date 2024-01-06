d98b1a588d1986f558cbe809b49d3f40519bc543
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:02:55 +0000

    disappear argument splash join house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
