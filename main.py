ceae7fe36689fa740f8986ee106d7b2959b0174b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:53:39 +0000

    lookback(on) delicate give his air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
