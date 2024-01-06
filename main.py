95b4dd3985f70273a28fedfcf1b7a0945ed78cd6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:23:42 +0000

    reply spite custom hurry curious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def create_queue():
-        return collections.deque()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_min(lst):
+        return min(lst)
