8b2b6db458f1b553ef50c5b5944e212ee833ba87
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:56 +0000

    actually lead cost man please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
