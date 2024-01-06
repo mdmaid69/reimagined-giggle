00b66f2e012cfafe27bb5799e4ff1f67268ddf72
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:41:41 +0000

    government trust accordingto pot herself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
