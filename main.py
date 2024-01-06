e46ec44a92c1a1386a0421d2048bf9e5d200f20c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:13:53 +0000

    mess narrow push possibility church

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
