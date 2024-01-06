d7bd4b6d2cbe696a3fff417bd355a3c71fc5c308
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:45:58 +0000

    around common university hardly rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
