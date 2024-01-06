f112bc659843f9ad49cc48fdf18d52f94175e6d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:21 +0000

    moral earn hurry public thing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
