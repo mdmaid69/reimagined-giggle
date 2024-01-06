98c87cfc61a8830aaee1d2e57ddefcdbe104587d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:45:18 +0000

    effort convinced your study season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
