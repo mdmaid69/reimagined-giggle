f433decb042a3f482a0fd969099ab332591564b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:57:34 +0000

    admire call share confusing add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
