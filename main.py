1332182a9f5bdb9a904ed5829c23877eb0afa884
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:33:52 +0000

    shopping country punish although throughout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
