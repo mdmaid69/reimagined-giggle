2b1dc1c854ea6f39b66031ec09dc87e104fc504f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:39:31 +0000

    hope advise improve book wise

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
