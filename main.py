66d98b20f87761ee84644088f88ce45b3ab739c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:30:43 +0000

    soak tax plate aim draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
