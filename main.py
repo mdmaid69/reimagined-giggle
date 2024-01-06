5a3216cbcf743a47793076b77eefb4c056568094
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:57:21 +0000

    whole last under moral meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
