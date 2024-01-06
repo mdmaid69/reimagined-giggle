36f70fbe77912ad97a4d4bf630e18554da437c55
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:33:11 +0000

    twist radio your loss pencil

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-  def square_number(x):
-        return x**2
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
