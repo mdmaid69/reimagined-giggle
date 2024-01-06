512f3a44bd9f8422875f998da3d6b1a4fd4b63b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:53:00 +0000

    bend male and Uu mistake

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
