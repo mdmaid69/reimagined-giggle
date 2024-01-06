343a4b87997854aa2d24c5628a89694a1a0f41a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:31:07 +0000

    makeinto knock disappointed hole error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
 import sys
-print(sys.version)
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
