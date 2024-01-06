7a661dd484fa0f69de0e00deb017727d64e5db4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:00:51 +0000

    Tt programme behaviour regular often

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_words(sentence):
+        return len(sentence.split())
