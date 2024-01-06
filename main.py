7ad1a0c3dd4f544c0ab3e1bc0ecdb05428cafc95
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:53:09 +0000

    argument teach empty workout keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-for i in range(5):
-        print(i)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
