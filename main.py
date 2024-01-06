e1e84f37842bff28fbcbb0c19e33322570604fe0
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:45:23 +0000

    combine wide lucky hard honest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
