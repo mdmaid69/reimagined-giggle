12f4310b9b4ab1a093900fed6725fc649460a0e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:24 +0000

    priority somehow period survive connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
