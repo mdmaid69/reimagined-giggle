ecbd92df6decf4d438f76d6e9f9409c2a740a13f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:13:25 +0000

    wash opportunity hurry match ashamed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
