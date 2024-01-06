9decc6fe17e2b48c6274aa9d5fbccd22abd74097
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:02:27 +0000

    play indeed amount single suggest

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
