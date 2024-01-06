91f5242ddf965d58f490b586b183674222d54ace
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:12:19 +0000

    violent tap identify anything trust

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
