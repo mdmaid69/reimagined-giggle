9f2b27d36d31e2190768c9fc47f00e1bb7b2f673
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:45:18 +0000

    suit well ocean satisfactory Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
