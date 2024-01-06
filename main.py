bad93d646b645ac9155ee07e7b041d60cd14046d
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:35:40 +0000

    exciting switch maintain because definite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def count_elements(lst):
-        return len(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
