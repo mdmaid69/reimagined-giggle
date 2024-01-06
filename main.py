21b418bcaedb742cf196ec96ed175a537c6e6dbc
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:13:12 +0000

    lesson programme police behaviour bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
