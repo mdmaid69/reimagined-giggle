a673b9274af49b823026b76398ed23077f5cd3c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:27:55 +0000

    cross between lock fact cope

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
