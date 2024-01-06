dd10b68438ff6a3fb08306c76fe9126531ae2c9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:20:57 +0000

    front bush free stretch takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def subtract_numbers(x, y):
+        return x - y
