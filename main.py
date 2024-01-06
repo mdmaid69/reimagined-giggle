0e15f419531e5bc70db6fe168e45085e4097ccb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:43:34 +0000

    fact opposite negative take welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
