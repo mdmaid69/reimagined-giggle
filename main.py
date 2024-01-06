338e396105537593303c527fec5038a1b2dc95e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:19:40 +0000

    switch type travel connection regret

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def convert_to_hex(n):
+        return hex(n)
+  def square_number(x):
+        return x**2
