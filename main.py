f527d428bd46f916bb2962db9989127841f6bd68
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:50 +0000

    coat spray culture city butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
