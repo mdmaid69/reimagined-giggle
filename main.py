0040f631b564adefb8dcd1d0e8a90685b867d5d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:53:31 +0000

    combination sell combine disapprove love

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
