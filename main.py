df436fb5ba1eb18a9f0874843ed554b27aef4cf1
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:46:47 +0000

    vote moral success statement healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
