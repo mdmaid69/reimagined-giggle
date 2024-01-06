08d012ee774b5fdb92db96ab5e828d46b88ef9b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:33:58 +0000

    geton woman do marry survive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
