dca7aa0c58fbabdb582263f55c09a8a6611d47b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:52:46 +0000

    anxious mess wild issue usually

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
