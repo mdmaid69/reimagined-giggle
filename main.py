cf513fc494802191c7bcfc24871486ec7c106597
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:14:02 +0000

    nor meeting concerning deserve situation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_set(array):
+        return set(array)
