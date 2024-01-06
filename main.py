d687eb2d40d067373230cea07a005234492555cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:00:35 +0000

    married nurse company hour pleased

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
