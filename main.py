7fa8e22b68d4d5931b902f6e8256ba520a6db0c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:41:50 +0000

    watch steady giveup thankyou pencil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def split_path(path):
+        return os.path.split(path)
