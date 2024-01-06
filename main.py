11911e6ada114ea8db0a84134ee4b5a414b10d5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:35:09 +0000

    still forward room juice trick

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
