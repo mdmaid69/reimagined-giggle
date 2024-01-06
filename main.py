52630f61d362234ab5e761323319388bef104e06
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:28:34 +0000

    favourite field battery certain behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
