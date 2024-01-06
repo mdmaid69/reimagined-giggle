0edd58ab674b5bb9ee789475f2992ada32b1b417
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:32:36 +0000

    tidy either carry process attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
