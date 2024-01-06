0efe165b4178cae976de7bcc3c9d8c0ee8b58827
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:13:44 +0000

    actual abroad open form waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
