ad98dbf50e30e859076c60051008d180dd97763d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:30:02 +0000

    cover heart search come eye

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-print(sum(range(10)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
