5629d0c9eedd3a1472227a4893300b952f99156f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:05:34 +0000

    criticize worry mine mark brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
