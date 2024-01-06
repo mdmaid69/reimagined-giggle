bb3ebf9d7c3280b3c55b5a6f6af2f3d1d0a34765
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:45:56 +0000

    recognize trap space walk callup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
