d9c73da7f336db92fac4f4f5e1a280d07c7a6b2b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:21 +0000

    walk fight male 568energy walk

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_array_item(array, i):
+        return array[i]
