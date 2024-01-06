10a0dd04c21c47c9e97da39d33c12a586c21d300
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:19:28 +0000

    hardly towards typical future practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
