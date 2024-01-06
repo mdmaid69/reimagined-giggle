325b115971f0f7cc450d21627c1afaee3b5904a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:48:57 +0000

    whistle wet scheme swallow everybody

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
