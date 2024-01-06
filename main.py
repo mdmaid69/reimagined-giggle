ca82faa884bd32046c6974585e4e8ab2d0df3888
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:46:44 +0000

    goback(to) commit quality introduction quickly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_density(mass, volume):
+        return mass / volume
