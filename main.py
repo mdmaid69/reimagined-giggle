70d3e2eddce1475ec53a6ee9246223888c5050b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:55:17 +0000

    lower character keepfrom shine temperature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
