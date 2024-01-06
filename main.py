da349e2aee3b4de9988603b368dc64348f27cb1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:50:24 +0000

    hurt admit effect best determined

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-for i in range(5):
-        print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
