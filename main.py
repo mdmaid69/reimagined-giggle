3a90e3304d5376b5773f5edae91f3b0b669ca12b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:42:23 +0000

    settle day demand keepout shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
