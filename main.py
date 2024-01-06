2ec091440bff7f9a715e725914fb60db0b17a55b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:49:58 +0000

    him afternoon Uu surprise benefit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
