e89a21aa50156edae2b598a90a131fc17f2d2d6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:07:58 +0000

    announce tell truth goon anything

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_factorial(n):
+        return math.factorial(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
