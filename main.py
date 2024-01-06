366a495564dc367470377cc63eabf612fac864cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:37:21 +0000

    apart last environment wait spring

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
