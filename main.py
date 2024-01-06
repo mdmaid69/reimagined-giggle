2241e1878c952fc99028b862d1f845bfeca4c8e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:14:29 +0000

    mean dress early label flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def change_working_directory(path):
+        os.chdir(path)
