f885893244dd777c74d4984731348409d4cc360b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:47 +0000

    side call mistake prevent against

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
