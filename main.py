720a7f449fcd6147f31a0e4c175957eb683e56c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:02 +0000

    condition effect instant number later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
