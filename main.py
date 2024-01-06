b794996052910cdb04df8d072989e0adb9e8e6bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:11:43 +0000

    fun expensive secret blow OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
