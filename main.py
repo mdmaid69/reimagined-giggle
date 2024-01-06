85e930d331c97269dd739d3d66ab813afe84ec66
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:08:59 +0000

    everywhere transport straight tense passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
