65bcb615b04f7b4c5ff0b6badaddbefe246d357d
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:34:22 +0000

    butter hurt television without healthy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
