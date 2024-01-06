aecdb8f7cb5d3844b0c7523cc3d8b24a79e59009
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:17:38 +0000

    while positive flow fold there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
