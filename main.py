b07b597c5e46b5f077eb38cd3f73ba518258c624
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:03:49 +0000

    intelligent ilovedicks slice extremely comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
