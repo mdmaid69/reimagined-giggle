7bf6f286a82a2e87efc346d9c5db6c8827800a3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:26:51 +0000

    wish Qq objection like hear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
