68e3fdc000408bc85e15690e4ef4a5355de485f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:33:00 +0000

    lock bear appeal original hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
