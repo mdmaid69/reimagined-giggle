37d98d0d7ab3af4705674c74349ba1af30c7c420
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:16:16 +0000

    mood correct day cold cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
