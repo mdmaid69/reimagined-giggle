70676f78fb8b70070869013a7dda38d1931d0fe5
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:24:41 +0000

    listenfor turninto hit business sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
