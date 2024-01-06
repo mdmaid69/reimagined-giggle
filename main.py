dd0c7413d483e7a2bee7a0ae6761f908d935416b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:22:15 +0000

    Kk rate my skin accident

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
