ad457f28b3291f0864edd7bf53d45d281d366455
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:41:51 +0000

    afternoon cheap future breathe muscle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
