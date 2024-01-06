9f4a1d1b8995c3a07f9b02aa99de460c9ac37257
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:22:12 +0000

    mouth fashion join whistle crime

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
