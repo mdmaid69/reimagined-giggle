b8cba6f88c98ff680ca82cffb654133c35b1f60d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:48:51 +0000

    add thorough trade joke Hh

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
