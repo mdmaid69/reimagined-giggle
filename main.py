f781a24c7f7a2645eb9c38c94c013c5f419ca28e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:02:18 +0000

    pencil such shout ear common

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def count_elements(lst):
+        return len(lst)
