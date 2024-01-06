9f3526954b3c993b2a90490dcc959d945099e0fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:10:58 +0000

    wear damp no succeed 569enjoy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sys
+def exit_program():
+        sys.exit()
