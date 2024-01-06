9be309a9d57210ea60a134481c08c8ba93fb3667
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:45:35 +0000

    flood workupto post declaration ocean

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
