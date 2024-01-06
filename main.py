6b5ed2ab438e93b65ddd7d51e24715bbb5fe5b41
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:05:36 +0000

    factory clean distance ear smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import getpass
+def get_username():
+        return getpass.getuser()
