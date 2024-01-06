1254fb7983ac018273586ed4c9a29e219754aa25
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:21:09 +0000

    getback radio us realize project

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_string_from_array(array):
+        return array.tobytes()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
