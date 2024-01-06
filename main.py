12962f4c76ef532149358df23488464ba066ccfd
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:09:23 +0000

    concerning shout start breath meet

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
