1ff60573cc5339af07c6e40f076ae5a1226ce117
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:20:19 +0000

    plenty abroad violence teach borrow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
