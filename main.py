f5635a50db2c5f9c85735a4b28e900f37d2061fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:39:27 +0000

    do pay exist murder street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
