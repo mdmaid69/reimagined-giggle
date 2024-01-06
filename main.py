c220d57d2e10c420d16c31b12a4d9b686f5341f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:23:47 +0000

    coffee fine shadow favourite pencil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
