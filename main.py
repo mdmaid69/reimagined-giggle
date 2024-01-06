1e2508fd1523e11f11411fed2d770e447cb90171
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:52:07 +0000

    rubbish that shoulder borrow best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
