9af792dd8fc46e19caf2ad0818fa18cc84b1a591
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:29 +0000

    store bear perform face lower

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
+  def remove_duplicates(lst):
+        return list(set(lst))
