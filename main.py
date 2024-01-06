1b2c7fdeb42d0f2b471e8234fc439669f1474b58
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:54:33 +0000

    truth pleasure ought shut possibly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def clear_array(array):
-        array *= 0
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
