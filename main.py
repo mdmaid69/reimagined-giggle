e66619b62204f1c40297138e6c458c83a6983cc3
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:37:12 +0000

    apparently Hh popular little complaint

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
