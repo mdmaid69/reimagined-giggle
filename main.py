9c13b594d568aea8aa7d3a195ea95fda6b36cc64
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:37:30 +0000

    tooth Jj language holdon suck

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def square_number(x):
         return x**2
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
