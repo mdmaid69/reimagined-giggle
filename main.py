8b95e42e29249982f64cde43e43c2b047020d6fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:08:39 +0000

    on thorough physical actual uniform

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
