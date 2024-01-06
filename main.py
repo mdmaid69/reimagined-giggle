7b7ac0c8463ecf0bea88750caf52acd9f791b83e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:25:06 +0000

    behave shout bringup block weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def add_numbers(a, b):
+        return a + b
