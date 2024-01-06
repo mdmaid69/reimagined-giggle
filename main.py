c07a3560188fba8d2ce316dd68da68ab59a04bc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:44:21 +0000

    keepout centimetre finger takeout forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
