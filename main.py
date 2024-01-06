978f35d4a505dd0a259b263c27a08cc484041309
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:34 +0000

    run object fat worry run

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
