2a8362d5aafde1ffb8c5c8107c715fc4abb4b7f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:16:15 +0000

    majority test ago watch complete

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
