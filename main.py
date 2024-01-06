475970bc21cec1cd10b37d4cd07e9cc150e211b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:15:55 +0000

    view gather interview suckme low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
