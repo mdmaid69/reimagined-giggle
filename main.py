48ba071863331c0c4a44cd693c0c2cd8bb1831d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:54 +0000

    correct save tour cause walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
