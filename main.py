6fecbc77e38bf5e2617b73b14f3a881d77833096
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:47:15 +0000

    rough rob shake start Cc

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
