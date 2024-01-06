18a65db276585ad5e281c3047892e7797e68613a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:15:33 +0000

    runover introduction convinced taste quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
