5640a5ebdb53420cc633ec54e05a1d8868ffeadf
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:33:34 +0000

    across offer other round doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_user_list():
+        return collections.UserList()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
