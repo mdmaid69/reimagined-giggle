7df3e5cb4837bdc184276312bce611dac0cfde06
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:05:39 +0000

    similar pity frightened circumstance favour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
