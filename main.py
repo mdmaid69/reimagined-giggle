972eaf32c5b930c04fa64dad31c16f749a52ec60
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:50:48 +0000

    quarter formal cut imagination sound

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-print([x**2 for x in range(10)])
+def get_file_size(filename):
+        return os.path.getsize(filename)
