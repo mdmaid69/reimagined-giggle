09a15f9d5ea06d0e935639b99eecba02f14b819f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:47 +0000

    star or pause challenge declaration

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
