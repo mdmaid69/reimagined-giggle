cca1b51aad30e77e3678a90d3586971f588928a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:34:33 +0000

    will equal attract square crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
