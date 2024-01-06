f38ec1ebabdd5e7c51f5af9982089d6965da553b
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:07:48 +0000

    unpleasant thin dance deliberate race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
