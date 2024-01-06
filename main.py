5b2d9f7a5e68ea5c12e8933aabf9877de0e95695
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:37:17 +0000

    performance tall getout wait matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
