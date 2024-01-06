ad2bc3d748e949035357b20dd9c6a0bcd00db821
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:45:05 +0000

    climb say pull ticket last

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def is_even(n):
+        return n % 2 == 0
