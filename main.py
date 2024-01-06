cb5ff798ef122ccda424bd3ce7193b3edf81034f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:01:57 +0000

    date file second ahead call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
