a67d1d2c1769a99148912fa3e8ff0055c0e39abb
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:03:49 +0000

    attention scared peaceful Pp uponon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_power(work, time):
+        return work / time
+def find_min(lst):
+        return min(lst)
