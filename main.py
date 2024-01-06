8f28b8ac3c45d050ffb71eb1d1a939ec057318f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:13:03 +0000

    justice claim support estimate agreement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
