9921d1524d2c83f9db61c11659b5459aca4f6384
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:22:58 +0000

    wire glass next circumstance serious

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
