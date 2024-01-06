1318837e1cdf105471825e2ef74cffe190255768
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:47:32 +0000

    radio duty turnover dig return

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
