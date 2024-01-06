0c8fecbd5fcc0ab4c6f8bbb3ae1db683896587aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:08:17 +0000

    project impression see sink beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.time()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
