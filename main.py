8587c249df08a57b8973b213f6fe7e444fcf67d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:57:30 +0000

    since already expect save Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
