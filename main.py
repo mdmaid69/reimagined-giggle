4be503318ec87cae3527fac8d48fc88116e6a69b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:31:31 +0000

    school threaten takeoff left love

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
