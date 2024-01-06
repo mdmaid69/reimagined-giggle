0fff39378438d59d985e83caad16938b96df50fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:34:11 +0000

    hard machine action its giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
