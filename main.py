1423eaba6a1cdb0142953949c98513bcfde9efb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:04:25 +0000

    lookaround apart majority this lose

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
