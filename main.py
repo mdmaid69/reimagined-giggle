46e901a81de5d4508d5e7d9995fe538b0c2f5602
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:00:06 +0000

    cheap round police yours get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
