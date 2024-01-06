1b8269905c9a118268f15a8b2d5031265928e840
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:19:45 +0000

    takeout sudden recover risk yet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+def convert_to_binary(n):
+        return bin(n)
