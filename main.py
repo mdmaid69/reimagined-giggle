639e7cda3de48a47ba12ceb68efa94eef5587da2
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:56:59 +0000

    target agreement care see invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sys
+  def get_python_version():
+        return sys.version
