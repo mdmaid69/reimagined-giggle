0603c23a42f694cf17ca89e27dc1d1d8b1bc3beb
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:56:34 +0000

    happen nonsense actual on will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
