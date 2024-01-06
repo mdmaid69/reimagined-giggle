5884c9e8bab007e3c7d6f4b7d4644370b6f253f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:48:46 +0000

    available ever frame shopping frighten

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_odd(n):
+        return n % 2 != 0
