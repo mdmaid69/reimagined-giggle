2a71472f9f87cfe9ad1d2790638faf3039c811b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:02:14 +0000

    letter story recognize back loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
