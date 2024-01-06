7174e3085b549c3173300918e0404d60b5e67d94
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:28:44 +0000

    feed platform anything telephone downstairs

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
