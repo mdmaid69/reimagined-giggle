1258099d553cf04c9bbdc641f2307ac0791e76cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:57:39 +0000

    worry disapprove pupil bye speech

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def find_max(lst):
+        return max(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
