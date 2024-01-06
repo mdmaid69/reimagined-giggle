5884336a80d48f79ec6b1df4b207f1dc0e117b57
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:26:21 +0000

    bet weak after danger know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
