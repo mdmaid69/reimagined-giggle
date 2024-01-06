3013b6b1e1b40f0a657b6784eee76c8beb41d54e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:26 +0000

    together deal embarrassed front already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
