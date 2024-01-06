a89c7408e57ff5e4521fd9181d4711fc30fb1573
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:18:50 +0000

    count rest above accept entry

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_list_from_array(array):
+        return array.tolist()
