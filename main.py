6a7f75f25ceec0b141295e67fbb5fbb6b6add85e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:22:04 +0000

    break mark quit medium frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
