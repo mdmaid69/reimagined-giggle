127e1abfec0a7a2f3d06de77c963cc859d7f4227
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:40:17 +0000

    far carry brief objection whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
