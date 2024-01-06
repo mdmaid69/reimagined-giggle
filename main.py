4d0b8120971e2c0db386cade6aaf05425011ae3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:42:03 +0000

    at war giveback disappointed pronunciation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
