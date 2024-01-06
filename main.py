6f2edef7e6b5d1ffe6906c1f5c6cbb057327a9b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:19:13 +0000

    not impressed child office handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def add_numbers(x, y):
+        return x + y
