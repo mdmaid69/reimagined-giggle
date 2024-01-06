e95bd63419982b82e698e7ef8a13b8e4e2610aed
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:23:13 +0000

    another outside over avoid state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
