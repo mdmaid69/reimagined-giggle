f50717721a79f77ed37d7343f5f84182036ebc06
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:07:43 +0000

    come knowledge prize immediately he

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def extend_array(array, iterable):
+        array.extend(iterable)
