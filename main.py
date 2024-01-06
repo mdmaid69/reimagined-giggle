547b277bd8373fd332578b68d760c7a1d193f968
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:00:04 +0000

    emphasize list trial book young

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
