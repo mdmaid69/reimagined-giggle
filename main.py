3242bd62241339aa30e6fc8ed539832c165ab8cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:03:23 +0000

    sing personal noise turninto blame

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
