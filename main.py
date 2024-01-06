d5a44872259fe760db6756c5e71e72ae932741af
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:44:29 +0000

    best reaction live pleasure fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
