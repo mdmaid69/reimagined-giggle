db8a786a5b5a24d6cb264ef5830eab3dfc749222
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:44:40 +0000

    design walk admire kill boat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_max(numbers):
+        return max(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
