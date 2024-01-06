cd3135610d54819b56d0d4ba032db7ebaa50f723
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:51:30 +0000

    even bored provide needle Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
