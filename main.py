fb6e5c343b5922f25a983251f4e5efdbe780c219
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:44:56 +0000

    possibly amount 179bite proof safety

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
