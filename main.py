e7e589917895614ae66a7282e05fd42ba764aa2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:58:38 +0000

    possession concerning gift shut free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
