bbcf53ee3ff5f235542b8d08dd495b58c2339814
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:13:46 +0000

    the time touch figure Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
