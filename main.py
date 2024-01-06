e12c321be47a91e7c315e70e9964d4ef78e17ac5
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:16:25 +0000

    defend crash print alive pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
