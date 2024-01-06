bb7acb532b30eb41f3c85bd6769549c6a1e42925
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:40:27 +0000

    miss rent getout wealth line

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
