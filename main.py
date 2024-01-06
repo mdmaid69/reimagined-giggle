685334f4e957230977b488c598108f14faf78008
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:01:44 +0000

    fear compete smell relative total

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_as_set(array):
-        return set(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
