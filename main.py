eee4fd0a323088857c2558226cedf683dacc9399
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:43:38 +0000

    within noone book record scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
