76ec9ee2d8e3e92ca77b12e2d3a44ca71ed3e45c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:43:55 +0000

    drink offence pump lost post

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
