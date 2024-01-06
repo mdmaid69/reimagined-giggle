f1def5895b40c1f70b632655b0cb612698b0a82c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:11:03 +0000

    attract successful connection suck professional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def multiply_numbers(x, y):
+        return x * y
