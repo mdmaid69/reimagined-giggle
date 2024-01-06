5d2be65544d5700ec67895f36c950daf4f2bd421
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:51:40 +0000

    successful serve mother agree steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
