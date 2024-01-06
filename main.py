6eb8ecc46c1ff40a1d8ac29693eb57ec1cfdcaf3
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:46:04 +0000

    home content cinema show worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_pressure(force, area):
+        return force / area
 import math
 def calculate_ceiling(x):
         return math.ceil(x)
