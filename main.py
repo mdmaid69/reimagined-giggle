bb4f2264435fb47ff965fe6d5175ea1de720d10c
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:15:58 +0000

    passenger whole fear content free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
