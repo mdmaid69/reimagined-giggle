5c13ddc3154bb391278c9917384720e8231ae1d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:36:15 +0000

    funny rush nonsense tap confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
