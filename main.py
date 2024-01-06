b0e069a8fa37f4a8f69b4c223214ead1a296c777
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:44:21 +0000

    snow average task mark along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
