4fd4d5149f354a68479c1bb0518dc393b0c09a75
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:04:53 +0000

    receive glad rarely ball fine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
