c4d84ea942595f88cc1328ed01da541e7ab80aef
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:15:57 +0000

    form bored cope neither rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+print(os.getcwd())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
