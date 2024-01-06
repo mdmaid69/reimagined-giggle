ead58c6a0b32b970dc5dfd87f1a7f6df6b191bf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:54:11 +0000

    evening experiment odd switch still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
