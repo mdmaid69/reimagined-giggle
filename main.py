686d1d648b10ee7dce4343a1716c906bfc041741
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:24:24 +0000

    single praise game unhappy speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
