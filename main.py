326b7885eaea73fbe90383101ec14df2b971b733
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:05:48 +0000

    success method choice mess beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
