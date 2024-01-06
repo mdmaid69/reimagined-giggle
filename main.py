d0178d2c03d85aaf883fe8068c7fe1889a68dc85
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:23:50 +0000

    busy real noise long gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
+  import sys
+  def get_python_version():
+        return sys.version
