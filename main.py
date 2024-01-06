c417d4b23f4ff9ee85a0cfcdae041d811f7d8c14
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:42:22 +0000

    cut today my loss calmly

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def count_elements(lst):
+        return len(lst)
+def is_palindrome(s):
+        return s == s[::-1]
