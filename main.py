7d1bca68efb0d9817716d37491879d37cb920966
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:49:51 +0000

    hope crowded sudden ought ability

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
