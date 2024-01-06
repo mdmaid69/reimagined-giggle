62dce321946a5110ff58ad1469b3797a682c0fd5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:08:00 +0000

    pay pencil shopping introduction moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def convert_to_binary(n):
+        return bin(n)
