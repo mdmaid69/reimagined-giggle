5802d0dfa8a8b95e3d18d4f86f4af8b376f987b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:29:37 +0000

    size cost hurt key attract

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_circle(r):
+        return 3.14 * r**2
