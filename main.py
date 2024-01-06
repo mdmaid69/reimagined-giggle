c6b0ac2b25aa67cadb74e205a98278df584849a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:20:04 +0000

    finish write pull runout clever

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-for i in range(10): print(i)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
