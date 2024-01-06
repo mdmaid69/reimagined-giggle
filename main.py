4606561b3ff5b2c81a75a7fb3523dc1678553811
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:33:08 +0000

    disappear early thinkof left still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
