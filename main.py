fc7e96ca78b2a9accc782c971e316504a113be1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:18:55 +0000

    contact description nowhere company rule

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
