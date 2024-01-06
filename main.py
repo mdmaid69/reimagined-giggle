325c11a4b2c152470069aa9be964b36c6a5cc9e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:30:14 +0000

    well practise spill strong star

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
