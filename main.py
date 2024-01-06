c309d103d5e87799fb15495082db263c52a7260e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:19:54 +0000

    wait keepup still brush as

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
