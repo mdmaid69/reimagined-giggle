0a6c1314a5e32498288f9a7decc7763d07d4f592
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:17:52 +0000

    practical switch sure tell hit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
