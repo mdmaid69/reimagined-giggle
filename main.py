b7cb5b2ec0bcd4ccf913984a8ec3d21f80e271e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:13:48 +0000

    little abroad sticky as trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import collections
+def create_user_string():
+        return collections.UserString()
