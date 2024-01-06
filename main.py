82cd08b02de16362644c4c3d09ce2cbb42a61e10
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:29:20 +0000

    hurry dick something year travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
