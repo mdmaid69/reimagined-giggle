d1dab8ab91987471720ea9942b5d6d9d74a173f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:42:52 +0000

    lost maybe lunch appear wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
