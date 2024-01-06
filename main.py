3b901664c974d9b5b5f345d866ff3370877a670c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:41:17 +0000

    try lesson past pause left

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_sine(x):
+        return math.sin(x)
