98aadd8d848d60c3b7f44ca2b3fbe2ea7ec5b283
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:14:27 +0000

    trick whistle not loan teach

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
