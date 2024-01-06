2a0c2591131c293f53dcf6b91b70a630943ea8fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:42:41 +0000

    brown sensible custom damage supply

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
