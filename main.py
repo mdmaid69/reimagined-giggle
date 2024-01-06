92037380af56e0631086b4c1eaac50e5b51b379f
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:20:27 +0000

    sting point ride future score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
