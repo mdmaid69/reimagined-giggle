18a7865b54a912da43d6d8870e487d8354066216
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:19:13 +0000

    cope time task keepout tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
