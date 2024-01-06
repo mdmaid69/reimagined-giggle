2842277539737a2ef5a984b56473fdbce20230c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:06:27 +0000

    ocean develop blame move contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
