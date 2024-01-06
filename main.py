c4189846a7c3dfa553ee4085c70f24e6a4fb9a2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:04:02 +0000

    rare opponent until skirt pity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
