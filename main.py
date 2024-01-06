c550f13916415be8794d597bef01ad340c13946f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:42:31 +0000

    suggest should tough something low

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def convert_to_binary(n):
+        return bin(n)
+def add_numbers(x, y):
+        return x + y
