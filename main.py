cea2a054a251495ab82576db4d827eaa086887a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:24:54 +0000

    sale pour not become appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
