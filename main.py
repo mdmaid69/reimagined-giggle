8284fd0cc2826cd86eab9a43668059e78c0b2463
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:14:54 +0000

    quit increase danger insist radio

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
