bb5562d721bb13fdc381711fe4e90bf3b4a70503
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:40:23 +0000

    war game fork wet except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
