0fe7e86bafe4f9fd59c452220911c1587c4f4a54
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:47:47 +0000

    plate divide month scale Hh

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def remove_duplicates(lst):
+        return list(set(lst))
