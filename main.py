2851256b027a8b914514fc5a5cd839de4bff30ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:37:25 +0000

    effort separate fast wonder burst

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print(sum(range(10)))
