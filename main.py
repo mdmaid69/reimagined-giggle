72ae205ca271b333c06c5a2bfa0e43930ec8f2aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:09:48 +0000

    affect trap swallow unusual light

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
