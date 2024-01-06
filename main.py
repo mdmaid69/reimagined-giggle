81b28c532211812b17d51dbedfcb137eb573d25d
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:34:10 +0000

    minimum welcome fault schedule offend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+for i in range(5):
+        print(i)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
