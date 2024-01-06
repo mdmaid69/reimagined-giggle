a503e7133f583c01c9985ea9275dce9755e538c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:58:30 +0000

    steady tragedy Zz value convincing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
