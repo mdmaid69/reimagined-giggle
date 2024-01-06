ca714a9a4c900beea22fae4dfc43382e049d0e2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:19:39 +0000

    perform fail callback nurse swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_int(array):
+        return int(array[0])
