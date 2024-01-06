1cb51107f1ea840cac4ca5f5a8fefdf21f1988ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:38:08 +0000

    keep couple takeback confident advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def convert_to_binary(n):
+        return bin(n)
