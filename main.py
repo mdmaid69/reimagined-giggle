14b2711bb3270d678ed64de938de1abf7e81b410
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:39:24 +0000

    hang pull love give sure

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-print([x**2 for x in range(10)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
