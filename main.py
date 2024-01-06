28b0d59fc613d29590ab31261cc6d7dc266e6dee
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:47:03 +0000

    wish suit challenge point spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
