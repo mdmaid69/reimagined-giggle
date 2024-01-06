715ac48d65683a5c43190c64a70537519fd48689
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:58:18 +0000

    necessary seem cost store makeinto

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_power(work, time):
+        return work / time
