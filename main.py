39a6f2354a4552c40e2c58b2d311d6f6824133bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:50:56 +0000

    shut understand crisis wide crowded

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_current_date():
+        return datetime.date.today()
