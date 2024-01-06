890406bf082f4e87c5b2bb297df670f353164761
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:44:56 +0000

    such close desperate pool glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-print(datetime.datetime.now())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
