72c624445e8cc8cbfbbf160c0428d8a8a74e15e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:11:35 +0000

    deliberately fit pressure heat appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
