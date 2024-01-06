ab5b72205e60d81cdaf6ceaca9f6fac76e1fc939
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:30 +0000

    godown bag official noise try

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print(sum(range(10)))
