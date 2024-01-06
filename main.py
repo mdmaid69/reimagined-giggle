d1b9509f88a9d591dacfb84d73209861bc247e09
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:51:55 +0000

    corner anything hold pause national

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
