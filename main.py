12f92fa626fd8cbc86b75d62d051e436ddf0c23d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:17:24 +0000

    not this goaway print safe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  def calculate_area_rectangle(l, w):
-        return l * w
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
