8c466070af7fd0fab3629b8a070d7ef1088bfff5
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:37:33 +0000

    print meal importance practise future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_as_float(array):
+        return float(array[0])
