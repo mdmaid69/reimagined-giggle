ff6002978c3e0649a323fa3761bec2857bcf263d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:59 +0000

    insist something fly support maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_typecode(array):
+        return array.typecode
