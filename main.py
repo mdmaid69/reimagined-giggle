de86bd5b2585946d1a1a0a2f5706e1ba4826b14b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:59:49 +0000

    habit sink shop refuse declare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
