186460fd7d7d775985b7bb1b6c5dd66e788f13d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:48:33 +0000

    call card protect it interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_list(array):
+        return list(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
