075368832968ed3f1f12099073626b80b8806fed
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:14:54 +0000

    guide usual wait start we

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def get_list_from_array(array):
+        return array.tolist()
