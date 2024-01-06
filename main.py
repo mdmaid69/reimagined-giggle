7ef7c9ddb4786bad096001d95a4b622a71d912f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:04:13 +0000

    ring fault fashion getaway feeling

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
