afac0b3d2e56d2e38992b33e7d9bbf01153cb1da
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:35:07 +0000

    truth stand compare my doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
