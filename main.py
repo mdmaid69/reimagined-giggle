8957baa31aaafdf98a436506d7c58f842a97d686
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:04:01 +0000

    frequent part shout on along

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
