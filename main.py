d8b1ce429f07cf9119a553dbb55592348b798444
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:00:10 +0000

    plastic why stretch substance century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
