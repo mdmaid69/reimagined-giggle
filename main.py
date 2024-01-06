90ace91e91edfa28a12f3b6207687d48b04c8e6c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:19:33 +0000

    special consider for top completely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
