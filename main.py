d49c05fc0d6107a97213debe30daec5f043a535a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:25:06 +0000

    argue radio value disappear deliberate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_max(numbers):
+        return max(numbers)
+  def subtract_numbers(x, y):
+        return x - y
