9fda719f5872eb08b18416a00cceb2301badeb91
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:52:53 +0000

    stay mood and wild rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
