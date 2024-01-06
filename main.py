e9206a87ef1696fb137d178ddf02117ee4aec646
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:58:54 +0000

    workon profit up smooth congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
