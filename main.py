9d26780c36a8720eeb1676a3e468bfdfc6c60426
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:28:49 +0000

    news different believe journey account

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def multiply_numbers(x, y):
-        return x * y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
