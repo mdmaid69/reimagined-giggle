fa6644f1e4c6e9bf7f4e358b14f530e8761da9cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:49:37 +0000

    571enough division tight stand his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
