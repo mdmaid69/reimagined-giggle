97b26d811906c83118f070d6194c16044f0f44ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:16:26 +0000

    arrangement lost talk conduct get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
