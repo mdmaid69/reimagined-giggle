ec35e6f09f134c66973cc92102702d09ba814fe9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:08:49 +0000

    spare excuse gather spite runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
