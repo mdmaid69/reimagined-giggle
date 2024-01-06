fc1f6dedc1b6d6654313a488d835793d6110526f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:17:05 +0000

    capital less further actual replace

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
