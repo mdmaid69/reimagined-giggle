b87fc8f4fba58532736fa286d715e58f7975491b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:17:01 +0000

    business actual culture thank plain

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+for i in range(10): print(i)
