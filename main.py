9139df84f0fe6b7e31a12508333671cb791ffb18
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:31:19 +0000

    joke partner cook scream mood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_factorial(n):
+        return math.factorial(n)
+def sort_list(lst):
+        return sorted(lst)
