96e3f0f55ee2ca02697c6a194227fcf8c6082888
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:21:45 +0000

    challenge likely opposite public Hh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import numpy as np
+print(np.array([1, 2, 3]))
