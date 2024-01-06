22c5dd2d1624766d210896f7203b8fb92dc7d28a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:39:13 +0000

    because fetch obtain which black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
