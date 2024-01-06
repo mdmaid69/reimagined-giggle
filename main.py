e01d0e79132974b9663acaf5f36eb43371dbf4a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:31:16 +0000

    uponon slice waste interview path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
