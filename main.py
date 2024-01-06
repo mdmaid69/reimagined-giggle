d569069983868f19bc3da473afaf1a0b555b733a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:42:33 +0000

    skirt achievement relax rank rare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
