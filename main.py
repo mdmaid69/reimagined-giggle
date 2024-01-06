cde32b8f6badfc7caae3394eae95ba2a4b8246c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:09:42 +0000

    behave abroad wise illness contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
