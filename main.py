9d76678f4486ef9815b7ab080f4f03a753a43d7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:42:16 +0000

    speed world would copy my

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def reverse_list(lst):
+        return lst[::-1]
