14c7d4ecf4688b8f6a390ce90a757a1d3435948f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:27:38 +0000

    summer dare belong club shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
