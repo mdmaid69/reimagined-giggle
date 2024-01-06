823ac71f52998100898daf55f11dc3380a6275a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:54:08 +0000

    her thanks nervous serve record

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
