6e0ba7615e06149efd43b3293bc9edf634df29b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:23:23 +0000

    turnup buy belief lookup exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
