1cd2e6132942e9dd9223cfbad09cf66ad2b71d08
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:27:27 +0000

    bringforward into suit usual wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
