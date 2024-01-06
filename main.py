41ba4b76eb48f705d9ce82d6d06f1e6ab28f5708
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:03:00 +0000

    smoke birthday concern probably grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_tangent(x):
+        return math.tan(x)
