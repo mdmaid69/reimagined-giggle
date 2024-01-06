99b3120eacac8e617b887668a63fbad45402fec2
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:37:14 +0000

    nice minimum see refer hot

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
