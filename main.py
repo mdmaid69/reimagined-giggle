149c66c6f755af244d0392bddd040a138695b1ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:57:35 +0000

    569enjoy praise glad unfair weekend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tensorflow as tf
+print(tf.__version__)
