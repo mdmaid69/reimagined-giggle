457db0afeedd9246e0076a9fe08979617d9fba08
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:02:41 +0000

    alone long emphasis steam old

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
