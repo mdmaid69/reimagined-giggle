04999df781ee44dbb0b18e1300d179c15053a7ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:14 +0000

    towards behind hospital raise nice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
