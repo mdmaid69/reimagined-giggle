f6ac48df8dfa4d19eccd84f6618e336b87235ce4
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:53:29 +0000

    kind smile cap cry event

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
