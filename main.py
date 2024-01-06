6573dcb4fce9292e753565693bd2f1b9f6112131
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:41 +0000

    up today table description ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-print(math.pi)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
