f851173bbe87bedb07c8e231e0795d942c2a0cf5
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:22:01 +0000

    aim doctor through whisper real

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_stack():
-        return collections.deque()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
