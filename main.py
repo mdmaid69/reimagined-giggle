591673df4e86ea99df8eec4318feea048cd17ef9
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:17:20 +0000

    bringup centimetre stick hold university

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
