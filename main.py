61ecbe9e8925eac4e5fc358ae927a508728c3f1d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:51:50 +0000

    club chase address point comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
