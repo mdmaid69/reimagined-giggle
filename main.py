5b2dc052989ef87a7837febe805cc3c5988c1490
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:21:09 +0000

    team shoulder boring police new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
