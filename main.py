29d348503a9573bd9e69e510ad89f80264b4ba56
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:55:15 +0000

    among calmly cut turnover service

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
