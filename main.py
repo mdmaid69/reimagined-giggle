61395251e111c5b0b0a5dfeee9d01b71afee17bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:02:58 +0000

    not spray giveaway pupil book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
