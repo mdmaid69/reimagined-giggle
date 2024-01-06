408ec52bd46cecff59f5e3718dbb0021a0ed7c5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:57:30 +0000

    vote hurt attitude glass post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
