1603e94afed42768079dba25ac0d003bd8ae29ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:03:54 +0000

    gather sure mystery hospital both

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
