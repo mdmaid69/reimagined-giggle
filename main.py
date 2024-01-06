b31e94698e5731addad764009a5b9ff030cddff3
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:20:04 +0000

    escape comefrom health crash indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
