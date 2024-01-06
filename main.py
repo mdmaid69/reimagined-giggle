b46edbc6f208a37c3f07be580a8c33c9380f325a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:34:19 +0000

    wing already address delicate love

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import sys
-def exit_program():
-        sys.exit()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
