cc5cb2a35fe1186f264557c673e15dab5fd611b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:24:58 +0000

    bag relax fair back freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
