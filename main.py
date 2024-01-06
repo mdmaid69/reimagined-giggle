63799dfc42b1de65a14c11f5574cb13823bbb482
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:04:03 +0000

    freeze determined plant weekend low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
