de175de574967f69c0a1bd8b4d7ff3dfe90a58a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:15:09 +0000

    city attach lookafter this paper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_floor(x):
+        return math.floor(x)
