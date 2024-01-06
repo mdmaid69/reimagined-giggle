7834ddf4129ea67b37376ca197423fd7ff373534
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:33:28 +0000

    weekend could actual strict bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
