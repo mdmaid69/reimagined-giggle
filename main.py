e51def09719523dea1044ccf4c5d41c111a055f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:18:17 +0000

    workon relation dig holdup beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_stack():
+        return collections.deque()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
