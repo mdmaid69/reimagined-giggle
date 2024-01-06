5825cfeae792b61116499b15f79b0218d3016791
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:05:10 +0000

    card job whenever train edge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
