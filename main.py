fdccfec9348ef8a0568e01d9439c6cf3f0750dbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:18:27 +0000

    car job shake mention afford

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def cube_number(x):
-        return x**3
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
