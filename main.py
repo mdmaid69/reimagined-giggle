1c14a2c9c219cfa2586b311f8d92d5fd0fa01cd7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:11:22 +0000

    whoever level bus software government

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
