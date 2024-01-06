289372465bed10057595c27e06167df784af5c03
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:25:13 +0000

    law form moment coffee hard

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
