b8a59d25d36f0dd22432b3e9bfabe3e096d28e6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:30:29 +0000

    oil term spring squash learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
