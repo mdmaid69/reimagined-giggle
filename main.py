1248ce3cf75f3eb1b276ab369b0b85e13998fc27
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:43:22 +0000

    race underneath explain bottom eager

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_as_repr(array):
+        return repr(array)
