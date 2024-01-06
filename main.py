22841e3659d41fe72fa44c2c3b0b8d686d0ed4e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:49:44 +0000

    study system important fetch rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
