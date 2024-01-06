140a50fd87f992fddabbfecbb1e2cc7da5985a5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:17:13 +0000

    battery practical Jj ahead family

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
