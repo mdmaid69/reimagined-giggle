1a1984fd719490d0af7b4b2cddf0f5434e23c03f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:27:21 +0000

    protest school share hair language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
