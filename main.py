fd87def6ecbec2bcfa1f0623bc36ee8406e72206
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:05:09 +0000

    surprising lose correction activity criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
