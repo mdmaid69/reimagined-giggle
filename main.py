5c661b6699794d0eabd79dda29757a3748988b6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:17:23 +0000

    bus talk dance bridge translate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def remove_duplicates(lst):
+        return list(set(lst))
