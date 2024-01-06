31323fd7ae721a75680fef662b417d62b1b7668f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:19:57 +0000

    control exchange accordingto though honest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
