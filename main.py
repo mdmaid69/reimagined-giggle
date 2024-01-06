e291a0978d514578f49772697e9add7c016d9080
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:53:46 +0000

    unhappy visit communication lift fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
