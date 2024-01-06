f4fe9e22bf2c0daf98b8365d828557a6e05d0157
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:25:47 +0000

    brave gradually discover form breakout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_work(force, distance):
-        return force * distance
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
