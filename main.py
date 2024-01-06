5178bd0b9386423bcf557f638fc03f27f9f46e23
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:43:51 +0000

    slice information trouble trial tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
