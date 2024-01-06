2e7aa827cd8c678fb0096545c473e59ea15bd626
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:41:13 +0000

    unlucky sting neat any right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
