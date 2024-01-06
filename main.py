dbeb47ae859e449a4644b42d9114c482ee02b869
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:44:22 +0000

    meaning sit know hardly friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
