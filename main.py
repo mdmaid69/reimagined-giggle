350cef6b5da7f2531f6f6ec32f8e34ff4d514c2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:03:19 +0000

    level suit interrupt closely battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
