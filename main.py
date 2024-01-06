65e5ec7f71372464ebebe2ca76ed0be6b2bedaa4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:16:28 +0000

    study interview lunch bring combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import logging
+def log_message(message):
+        logging.info(message)
