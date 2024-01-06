c31c9f67626582883464bf50538ff9babfc9f35b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:03:16 +0000

    claim necessary fork doubt land

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
