7d6230d1f90e7b57785f825181603bef05d12487
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:26:13 +0000

    rely border total turninto direction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
