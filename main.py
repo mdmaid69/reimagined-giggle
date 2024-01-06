347a8acbdfc93bc8451dd203eea76b6a9c31f72a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:26:12 +0000

    urgent anywayanyhow 179bite art wise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
