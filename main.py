a2b785249563e4d99ad3a61b19365fc285e0c003
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:35:20 +0000

    year cause choose regularly programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
