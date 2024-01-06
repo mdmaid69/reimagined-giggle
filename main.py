4f657c31532789e3b55ab148bcada3b3de708ef2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:37:04 +0000

    scared interruption shock figure season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
