2f0a0a042363b6b9944b7695cca67584264b3491
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:14:09 +0000

    place fire price that beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def count_words(sentence):
+        return len(sentence.split())
