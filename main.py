cfbc2b7b2ce7435dcc8c3c816fe51bf6455e72ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:18:02 +0000

    passage thick up tough thankyou

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
