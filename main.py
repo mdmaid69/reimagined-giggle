fd5ae0cd7894b815f0fcdef83956cc7ee74bbacf
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:52:44 +0000

    tongue justice confident neither tragedy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
