f992ffc9d67d10704486ea6f0309bd9e6e9c7bfe
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:19:11 +0000

    second admit comment possibly there

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def is_odd(n):
-        return n % 2 != 0
+from collections import Counter
+print(Counter("hello world"))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
