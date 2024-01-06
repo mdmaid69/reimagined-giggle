adb1a75825fc30848098ecbecdda598f975cb680
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:46:14 +0000

    hour solve information message exaggerate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def get_array_as_float(array):
+        return float(array[0])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
