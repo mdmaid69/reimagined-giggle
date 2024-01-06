0b27a677bac31d40661c0444893f27fbb3085a98
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:47:44 +0000

    test send below everything such

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
