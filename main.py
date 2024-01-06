fb257163b5d556b6b5fd7c9762a891c7958b91fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:44:25 +0000

    Bb quarter underneath left drive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
