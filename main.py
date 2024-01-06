09c9a4b4ca7d39baa6313de2256f2fbc84fee8b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:49:15 +0000

    meet over return plant before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import os
-print(os.getcwd())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
