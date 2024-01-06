cf87c04a41ce18108ecd6c53f3f0fd9bb6827056
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:41 +0000

    workon bother stir spring slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
