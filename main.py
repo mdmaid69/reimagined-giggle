e6d6703a077524da94cb4c58162f134faf81e3ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:28:46 +0000

    connected impressive rough test lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
