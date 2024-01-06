efed702e45c311ba69556d541e3cdc1b7cddae86
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:58:01 +0000

    tense family reply bite thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
