e9e00905c25e472b449a71f1b60d8f53974f0992
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:10:05 +0000

    road do face terrible reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
