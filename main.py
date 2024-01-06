96842f6c6fa44280bdd6cc24043dd4055ceb3718
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:47:21 +0000

    young production top special ring

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
