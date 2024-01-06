bcb0972e8cef0e4428f5c17303d05d537c02e071
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:25:59 +0000

    unusual sleep dream light still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
