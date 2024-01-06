05e170e06f277821f381ed9edeba4e8d07747bc3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:42:03 +0000

    pleasant high power desperate thumb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
