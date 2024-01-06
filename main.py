aca789f29d6c0eb34e3f149771ac9b65c88245ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:52:31 +0000

    tight add stand hard me

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
