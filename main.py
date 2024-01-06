8290ff5d8cfeec6f8b70606ee3c80ab55f6b958e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:19:08 +0000

    she impossible positive unkind shopping

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
