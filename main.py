74b0c70c5bc115a7dfd2560b5eb31eb685023e9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:47:39 +0000

    hope hard damage murder long

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
