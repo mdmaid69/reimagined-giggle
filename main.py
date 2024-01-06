50b899f2985845b2b116eb1797a643f4407597d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:12:55 +0000

    amount destroy myself sensible under

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
