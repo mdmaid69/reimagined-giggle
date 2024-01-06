b6fbf4744f7660766be30d0b0f0e5a630bf44986
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:51:20 +0000

    plan university still public factory

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def square_number(x):
-        return x**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
