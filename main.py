5dddf942dd43bcb7f6c8e15860a40d2cc8ea8839
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:20:21 +0000

    exactly comefrom Gg why speed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
