3e8a2ce62dab2d2aa434639e8978e1ad4b723db3
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:34 +0000

    realize reliable dream angry window

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
