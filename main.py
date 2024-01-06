f337dc35bfb634226d223fffe755cadb61c1f9f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:19:52 +0000

    politics imagination surface feature control

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def square_number(x):
-        return x**2
+def convert_to_hex(n):
+        return hex(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
