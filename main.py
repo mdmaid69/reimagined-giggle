249a24fcb09ca0fd8d1ed09e200127ea2543bafc
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:02:30 +0000

    late country whistle left past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
