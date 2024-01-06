807555f1289d85f3ca748f26652bd70b40927c56
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:10:21 +0000

    now breakup office model instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
