8f174f248bf65bf5dd29f6f0ced58180de5a9c01
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:22:23 +0000

    anywhere regularly weather report apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
