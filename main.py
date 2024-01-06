fb9aba0ee976bde05a266fea6fd5dab62cc46f66
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:36:09 +0000

    measure or steady stupid point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
