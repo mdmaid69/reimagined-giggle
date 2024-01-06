056462fceec9e022b678a7c63dbbe62c2c272a92
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:04 +0000

    steam married idea later eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
