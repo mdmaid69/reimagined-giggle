105aa9be8b387a8a6cda3f37b09074213d43b791
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:19:20 +0000

    claim price imagine love tone

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+print(sum(range(10)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
