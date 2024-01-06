b3ae6865b3084e4e29514e30e393815a6860a22c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:19 +0000

    ear tone coat well police

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sys
-  def get_python_version():
-        return sys.version
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
