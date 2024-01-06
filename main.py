a48bb1b86005583f36437122a51554a7070cb9d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:53:46 +0000

    awake takeover fork journey might

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+print("Hello, world!")
