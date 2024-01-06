b5e73925fc75bbbb4708114bc395ccb0c452d0f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:14:16 +0000

    occasion by call takeover hit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   def calculate_average(lst):
         return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import platform
-def get_os_info():
-        return platform.uname()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
