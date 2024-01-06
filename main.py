86b70bc8192f56165404ca26830be2b453f5c6a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:22:12 +0000

    nurse morning along interesting approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Words:", len(text.split()))
