cf7350414fec9092537a4abf01fb6cab066cb650
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:29:10 +0000

    lookafter noone crowded convenient admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
