ca4d04b02e848df53bd058d88f3409b75befb441
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:27:46 +0000

    Mm most goback(to) regular favour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
