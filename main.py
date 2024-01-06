ef9f22f3cde0b03bb00d1a92ffb6fa2f2d74cbb5
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:02:40 +0000

    child may smile tap inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
