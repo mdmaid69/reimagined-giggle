66bcc331625d24f323f4ae2d19e52304dd8f33a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:21:34 +0000

    twist compete ring after giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
