d15a9850c2b507ef18645ce3d18705c8a565f9b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:34:45 +0000

    tough listenfor believe joke motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
