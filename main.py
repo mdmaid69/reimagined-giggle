fa701e465633dcae7ed05fca93dda4a70ee8040f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:33:11 +0000

    lift disappear everywhere slightly sharp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def square_number(x):
+        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
