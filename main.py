094ed44c6c7096d7096e41eb8bea8b6fc2aa84ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:55:29 +0000

    pencil control draw famous tear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_set(array):
+        return set(array)
