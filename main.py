2f228103e3336b93477ed2a24b31f2706de70c39
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:02 +0000

    will last kind wealth moral

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def divide_numbers(x, y):
-        return x / y
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def calculate_area_rectangle(l, w):
+        return l * w
