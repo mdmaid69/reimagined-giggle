c5fe76f232084d048e052b9e78f7d788c00806fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:09:32 +0000

    centre everyone book beside patience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
