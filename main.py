3d7bb0c5658f23da9a2b47bf090ce7e80629ce18
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:46:45 +0000

    march tell visit field guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
