793d0ff439fb05dba1262998e509e803134f9e02
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:22:35 +0000

    moral about matter would seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
