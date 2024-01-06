3079721baa3fcc489f990eba0cc8a5aff4bbe212
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:57:57 +0000

    bad dance balance aim season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
