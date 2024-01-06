fcecd252ac3eb69104db96249633e8fa29cfc4d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:49:19 +0000

    visit indeed travel century trip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
