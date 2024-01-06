841080f8ba5e023979b612ca668000a98da02d13
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:29:57 +0000

    regret near like at belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_length(array):
-        return len(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
