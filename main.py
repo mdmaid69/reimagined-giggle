a8be0aa290816740a73ec26b5cda2a4340b48805
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:00:55 +0000

    Nn image phone intend pupil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
