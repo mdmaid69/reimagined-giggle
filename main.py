3151135bb77f3251901ad2e619ea4e8345e88b83
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:28:27 +0000

    answer model fine standard sing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def convert_to_binary(n):
+        return bin(n)
