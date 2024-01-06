c2b0a35e08df5293e0b664f52ac08d439dc10458
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:10:43 +0000

    handle special expensive only compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
