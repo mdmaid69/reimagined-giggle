e0bfabded5a865815fd0438c10d79c3e03705a97
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:07:05 +0000

    how meeting team thumb matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def is_even(n):
+        return n % 2 == 0
