763a4ed485dc8b3197827ae782da540526146ebc
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:08:27 +0000

    defeat lot attract hurry period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
