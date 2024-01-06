2e9fbd1c7d0a2413ef62aa0092da97a395e035fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:05:21 +0000

    permission damage present contact smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
