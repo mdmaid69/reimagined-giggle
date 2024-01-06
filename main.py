0724097f1b08bca9eafcabc9dea912d79464cd83
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:29:47 +0000

    circumstance since confusing reaction hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
