a072df014c6fe1afbd85d7ea2259c43f829e492c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:25:08 +0000

    trick shrink reaction disturb welcome

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
