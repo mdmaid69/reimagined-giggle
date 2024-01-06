dcae2ef0374e8d6970de48341fbc237f30adc19e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:22:12 +0000

    test 571enough preparation hers base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
