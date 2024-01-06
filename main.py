f56dbd25d32760ad3c41225e5bc167a8798fe184
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:43:30 +0000

    man pressure home in substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
