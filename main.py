9d42088c7861744f6291bee47642bc9d3aa47841
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:46:26 +0000

    peace might lonely basic possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
