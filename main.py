d957e3346065e1bd8d7866e893f8f868349f7fdc
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:32:56 +0000

    improve fight height past goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_item_count(array, item):
+        return array.count(item)
