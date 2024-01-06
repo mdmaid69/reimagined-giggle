bfa43f9cccdbd18a690d6f36d9cc2828c79d1430
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:40:09 +0000

    control sad mood direct experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
