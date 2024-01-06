39913200574e9633b90a06345694313724b8dbff
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:38:55 +0000

    make home move lot company

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def is_even(n):
-        return n % 2 == 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
