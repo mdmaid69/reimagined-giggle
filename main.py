7e684ceeca401e91ef1551d168fd09d9d6b9a768
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:10 +0000

    calmly meet fly instruction cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
