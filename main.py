254bededcf8899ec394630177a669c5c40e13116
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:29 +0000

    hurt great soon walk tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
