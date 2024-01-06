35eb256c765c963e6a35e3d16d2bb5c14bd7ce0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:06:40 +0000

    job much estimate full table

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
