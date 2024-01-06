2efa4124018663e41fbc621a6a51ea9e9613afa4
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:10 +0000

    mood bang born centimetre concerning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def convert_to_octal(n):
+        return oct(n)
