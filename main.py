e09257437e5efde30491e97f7e9573308daacc3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:06:20 +0000

    visit visit child catch fasten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+def get_current_time():
+        return time.ctime()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
