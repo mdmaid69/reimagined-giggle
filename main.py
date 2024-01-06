bac522dff59b8141fede43f294636ed28e72ef07
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:25:04 +0000

    seriously allow Ll unable danger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
