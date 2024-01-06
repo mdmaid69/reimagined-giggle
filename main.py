adca4d56f0893dd641b8d33a420dd0cbe643f9f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:15:28 +0000

    fat poison inquire steady mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
