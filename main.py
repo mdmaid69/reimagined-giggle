dc9b5a5664d1607fc7e5d32a5c7899daf1d3f428
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:55:14 +0000

    position exaggerate quickly clear lower

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
