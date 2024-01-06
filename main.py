9a737929b2e8a5ca384ca7f071a39f970ee67e86
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:58:21 +0000

    across I equal speech callat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
