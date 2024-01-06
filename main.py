404b91fc82dcd1e10e9e2b2f4ba52ca0b03e4582
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:55:38 +0000

    trip check floor also embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_as_bool(array):
-        return bool(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
