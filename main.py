9de8a287d90338d9d0a0743f8c7390cf68763caf
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:44:43 +0000

    dead rarely area lonely lost

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
