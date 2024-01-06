81dea24d8dde8488e3a245112cf80b50b5c3daa1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:42:02 +0000

    show human sound run least

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
