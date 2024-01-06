43f562b79ca2931fb857dc91438a7f3095022ed9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:22:45 +0000

    treat book wash difficulty judge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import sys
-print(sys.version)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
