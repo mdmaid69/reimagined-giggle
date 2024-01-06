e095cc56a2fae017f682453788b886abd08f0698
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:05:19 +0000

    mad test sing fear let

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
