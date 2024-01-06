1164c726db1f888613957e650b90a6bc3e65322c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:28:15 +0000

    comeback declaration tone cough convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
