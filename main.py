d2956a62b22d758f00752139748e6210728a6316
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:50:01 +0000

    Hh exam crisis Mm suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
