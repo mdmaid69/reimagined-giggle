5fa498b6ccd8029380b65e988a91099cc56d05fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:44:33 +0000

    impression callout chin wave forget

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_binary(n):
-        return bin(n)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import datetime
+def get_today_date():
+        return datetime.date.today()
