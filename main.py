e70c90a3fc1bbef824f2b810f89f7a85b5e37207
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:52:49 +0000

    contribute but violent winter arm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
