f738432ffbf94e2e001c9b7a4aeff8b86b1889f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:03:51 +0000

    towards rely tap worse event

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
