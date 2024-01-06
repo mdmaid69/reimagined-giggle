5795f96c7ce772c53f384cc9d8a22078c8ae15a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:47:02 +0000

    poison case comeover 564encourage kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
