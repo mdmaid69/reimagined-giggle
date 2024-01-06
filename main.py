c24ca9c30ab040cc174349b138545ab16f550232
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:43:23 +0000

    stupid her contrast speak adjective

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
