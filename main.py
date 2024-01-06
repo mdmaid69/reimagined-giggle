7088e7b539da622ba4a9472ef1626825283ea76a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:33:12 +0000

    straight instant seriously lesson arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
