95301372cd6adb4f130156292bdfe4828309465d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:24:18 +0000

    pretty pretend rank freedom whichever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
