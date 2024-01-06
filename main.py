4b8d61422e2f32a8ecc9111f34ee36df455fb16e
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:45:39 +0000

    adjective original quantity pool tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
