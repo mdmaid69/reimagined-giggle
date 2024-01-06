e9a45451e651d0e6074df1454a9c1e47c06c7dfb
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:57 +0000

    rank contrast morning relax care

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
