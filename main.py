f46329f8887907d7c20adece6ed803fbaadaf86a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:32:59 +0000

    city mean stand same fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def subtract_numbers(x, y):
+        return x - y
