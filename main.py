cb247109f2ac6b1bf51d040fb51343f7968bae21
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:52:56 +0000

    persuade heat stir swing crash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
