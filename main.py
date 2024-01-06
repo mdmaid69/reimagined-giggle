22a7e9b872cf29f8e6508836f1fbd0885ac71138
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:03 +0000

    arrange preparation turnup surprising happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
