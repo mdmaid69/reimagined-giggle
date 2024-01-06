be261c6248a4f2f7cb71703d07ec401e1adb99e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:01:30 +0000

    joke play wide similar bit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
