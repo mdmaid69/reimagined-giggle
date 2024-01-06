331afe0b40f47806592e7b0092cfac6728eed433
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:48:49 +0000

    flight example together explanation tidy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def find_unique_words(sentence):
-        return set(sentence.split())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def add_numbers(a, b):
+        return a + b
