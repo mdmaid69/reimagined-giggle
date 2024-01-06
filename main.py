53796bc108103b6ea52c670f42cbe072e3ffcd0c
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:15:56 +0000

    its trust prize male seriously

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def count_elements(lst):
-        return len(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
