f4936a9458ed5d001542f782e523406b41648b46
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:22:56 +0000

    centre ignore reject that then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
