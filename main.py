56fe0fc3b33987db3ad1dd6d01261974a1b0c9a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:38:40 +0000

    smell sleep pool fast insist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
