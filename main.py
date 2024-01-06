b5bdfc211e40595fec5af6c876585f8fc4912fae
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:36:17 +0000

    direct lunch breakdown plenty wake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def is_odd(n):
+        return n % 2 != 0
