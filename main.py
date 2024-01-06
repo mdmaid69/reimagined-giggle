34f26abbbdf268ae5f50d8e7691ba933c9d456f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:57:15 +0000

    one keepout exaggerate well away

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
