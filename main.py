41360f79a48a366b10fc656bd6fb116aef2670d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:25 +0000

    forget quantity means sink sore

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 def is_prime(n):
-        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+                if n % i == 0:
                 return False
         return True
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
