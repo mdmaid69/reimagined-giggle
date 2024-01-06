7e3aaea41fbded019f092a755bff7362c095c333
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:20:59 +0000

    Mm breathe 573entertainment matter both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def subtract_numbers(x, y):
-        return x - y
+  def square_number(x):
+        return x**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
