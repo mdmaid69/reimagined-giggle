cb78f224dc9e2c29cccdec201dc2015e625336d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:38:09 +0000

    fit start poor arrange certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
 import subprocess
 def run_command(cmd):
         return subprocess.check_output(cmd, shell=True)
