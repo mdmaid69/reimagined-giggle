43d4e86ac7fd20db12e8d6c12037a0d4be57ee21
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:46:15 +0000

    bringback line up combine either

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
