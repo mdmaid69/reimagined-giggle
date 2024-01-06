fa90f5fea1fab25177be4145a590e4db2309c984
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:36:25 +0000

    stage back past habit trousers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import os
-print(os.getcwd())
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def find_max(numbers):
+        return max(numbers)
