ac6bacdb08778582c950f661c3e90f60ae266139
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:47:43 +0000

    power name world convince hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
