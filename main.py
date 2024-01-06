e8aab8d15b8040976d0febb35393c00b3979a752
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:27:53 +0000

    language address means part bet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import time
-def get_current_time():
-        return time.ctime()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
