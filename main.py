31eef822ed15e45804f0466a253897ae6c04b410
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:13:54 +0000

    set hospital modern reach dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
