a6f9a1675e0995be41817f5b845821711d887277
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:37:50 +0000

    pour date not fat odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
