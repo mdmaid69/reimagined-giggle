55358c585b82226204fd8381f80b361155621677
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:19:39 +0000

    wall hit Pp steam dull

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
