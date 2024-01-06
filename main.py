cce5d090881b7b9fdceffe96af321e7b94a81658
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:27:46 +0000

    foot zero disapprove backwards lift

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+text = "Hello, world!"
+print("Words:", len(text.split()))
