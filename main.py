89838a4dfd7fd57ecf7bdc62705a9a4442d0458d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:30:30 +0000

    talk modern sort consider touch

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
