26e1149e22ae861cdafb5817733c508b7a2561e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:27:50 +0000

    fault stop confused lie objection

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
