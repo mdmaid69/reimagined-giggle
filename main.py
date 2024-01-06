e8c3c5d65254b27f124490b13b3deae59e14fd04
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:21:05 +0000

    nose represent row memory stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
