3a95d6637dfe9ca69242eafdd02701b287f63607
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:17:36 +0000

    freedom after pupil friend makeinto

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def find_min(lst):
+        return min(lst)
