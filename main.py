9995cca0790fa7b0125a0f367c0d7610cb12e12b
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:43 +0000

    afraid every true well angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
