20b45b0f8b66cfb79e766de330b0155fa8fe0778
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:35:01 +0000

    takeaway test store difficulty sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def subtract_numbers(x, y):
+        return x - y
