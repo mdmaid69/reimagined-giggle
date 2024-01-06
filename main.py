6c5949d46cb50b60a01e485f7a4b059726dd1786
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:31:18 +0000

    different copy operate crowd chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_index(array, item):
+        return array.index(item)
