8f3ad68c5b62e251b1bc4c8a0feb9e175d3fb75f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:46:36 +0000

    uncertain match university cross regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
