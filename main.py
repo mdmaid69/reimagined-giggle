a63c7c476e65f2e0c95849ba20d7d71ea8c3ab0c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:06:30 +0000

    food able runout command church

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
