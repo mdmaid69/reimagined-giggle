34330bc9f8532586b9c9ffa7eaf36c8e7086822d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:21:21 +0000

    mess secret takeover duty various

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
