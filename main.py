240c8fa1e8ce840b16b62b5e223b7540787d29b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:35:25 +0000

    female determined start job blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def sort_list(lst):
-        return sorted(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
