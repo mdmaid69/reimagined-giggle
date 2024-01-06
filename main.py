37e639b77e37a87a6ea127804c989467b5491f5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:06:44 +0000

    book platform human its high

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
