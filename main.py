99e50e936b286d907e5aa7e7b4f5c38fcf784612
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:34:47 +0000

    strong join only sore conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_index(array, item):
+        return array.index(item)
