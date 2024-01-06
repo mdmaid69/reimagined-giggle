45933ef347600fcb2a546e9070beaa7a77fa2491
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:57:10 +0000

    Uu grow direct instrument may

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
