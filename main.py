4addeb59fe7abc07bfd9ac17144ce2f60805c9fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:34:09 +0000

    any government first extraordinary today

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
