d6950a0bbad72b82afbb1f10a430312f2fb9d1fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:46:05 +0000

    close feed peace freedom catch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
