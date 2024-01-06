70d8fa64d751448cf9ae453f27cb4d916bfe1950
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:52:07 +0000

    grand happen arrange treat loss

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
