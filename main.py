e505ec8f9263ed94779897a222927e3ea9dd5cbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:34:32 +0000

    what wave ever start butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
