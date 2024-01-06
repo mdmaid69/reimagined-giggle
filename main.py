ef38ee6813a5eb25240e48a7706d3aba3ed1fcb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:25 +0000

    lean try female fit away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
