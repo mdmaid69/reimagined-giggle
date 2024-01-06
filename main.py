5471687ce417d26fc0610d34dc3162f87c6ae085
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:06:45 +0000

    popular match tell wave gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-def log_message(message):
-        logging.info(message)
+import tensorflow as tf
+print(tf.__version__)
+import sys
+def exit_program():
+        sys.exit()
