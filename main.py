d49ff019bc685c07c8700c9ede9ecb5129f6d662
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:48:47 +0000

    bite advanced pressure eat price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
