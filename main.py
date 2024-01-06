63a39a3908bf2f8b1383a7118ea6ff899e218f81
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:12:18 +0000

    lookaround any dare song replace

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
