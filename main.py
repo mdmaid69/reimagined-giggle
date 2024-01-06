1166508fd312487367f2cc0ea1ee11513ddc77dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:24:37 +0000

    getback admire star art now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-for i in range(10): print(i)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
