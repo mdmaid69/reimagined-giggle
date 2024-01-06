ff43ff335c1f4be0eeb5905cf9f513e219e0126a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:30:30 +0000

    undo press supply anybody recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
