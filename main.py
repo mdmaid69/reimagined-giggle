2e6b994d03c7c7076b71d413daf0116c69b3d408
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:12:59 +0000

    just space activity influence provide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
