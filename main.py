f0e0f450ed370bb376387c48b3e18e6d922eddf8
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:37:59 +0000

    waste noone middle serve too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
