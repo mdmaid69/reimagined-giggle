3796261148b9a50c9601255d751615d67287a223
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:53:42 +0000

    key pull professional breathe takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
