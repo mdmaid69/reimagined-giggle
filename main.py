d0bc115f8163f6f432c7b40cebfd64ab38240d11
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:51:47 +0000

    store kind force crowded decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
