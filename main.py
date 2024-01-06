9b81ab9843313854e2f835f63311550b27575e74
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:32:38 +0000

    but bottom promise wherever now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
