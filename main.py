72026f555f127cf7f6d5bbe5204bb3d024c90b70
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:25:13 +0000

    pain protest later lonely goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_list(array):
+        return list(array)
