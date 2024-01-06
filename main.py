533547079294568a80235b4ed0f9481b950efd49
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:34:42 +0000

    breakup me ground share characteristic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def convert_to_hex(n):
+        return hex(n)
