7519a09451ce48800b99849c9d89a3b0cb129602
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:06:53 +0000

    any cost maximum warm estimate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
