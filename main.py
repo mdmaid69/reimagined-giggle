7044daf834b32cfe65a05992a20950789927be2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:41:38 +0000

    worry pain attention bother keep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+print("Hello, world!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
