dbbb5ad8cbb489291625f66f6d7053dcd0c1e3fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:05:37 +0000

    wire public concerned between Ff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def cube_number(x):
+        return x**3
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
