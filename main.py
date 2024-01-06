7e5c342c19f318a8aff8ada5ea9be7fde46f0f04
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:03:23 +0000

    today protect too keepaway break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
