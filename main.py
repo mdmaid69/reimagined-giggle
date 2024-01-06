0a351af8d4868de5d32aa37ec938c4345ddb161f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:08:54 +0000

    embarrassing recover recovery recently strange

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
