1fad7cb909a36c7d9e921f8d9ef33968ba30d556
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:40:29 +0000

    personal do lucky cheek hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
