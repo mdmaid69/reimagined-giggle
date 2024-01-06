732d3ce809629a4e1cce4a32f7d90945a221b8d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:04:12 +0000

    official opposite store comment late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def calculate_area_rectangle(l, w):
+        return l * w
