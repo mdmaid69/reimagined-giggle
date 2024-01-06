5a81f38ace54d5c22319b8cc12a3388119e02467
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:48:36 +0000

    game rescue violence hate much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
