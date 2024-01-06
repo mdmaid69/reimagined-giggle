d9a601c474a4eb9fc15549850676739960873253
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:00:10 +0000

    geton nigga point tax convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
