29d38a9def0f80330426e06175a7c13f8e7fd0bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:33:42 +0000

    Pp Gg consist so without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def create_priority_queue():
+        return collections.deque()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
