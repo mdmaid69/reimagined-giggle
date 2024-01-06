b8d838df65a938c81d3017b800dd157f4dcf7abd
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:58:43 +0000

    attitude uniform force replace goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
