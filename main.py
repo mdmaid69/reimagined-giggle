13906dc41f34c3a70a8d60799f7336cd8a3624ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:39:03 +0000

    warm wear fight emphasis split

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_power(work, time):
-        return work / time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
