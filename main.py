5d3282355822a15418a1c1c8a2ab99772f5b9aa4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:19:11 +0000

    future feature deliver test reduction

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
