5e4f1899a1b8698b60bc7bac914d31f70c19aa51
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:37:29 +0000

    often limit soak rarely cheap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
