23d0e084b9d98776edc93769bb6b8fab4baa29cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:48 +0000

    church room week day decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
