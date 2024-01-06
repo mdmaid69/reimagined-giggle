1fc2b84d35537718cad6124ba59989dfb10eccd9
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:26:39 +0000

    train breathe pair disapprove weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_current_time():
+        return time.time()
+def remove_duplicates(lst):
+        return list(set(lst))
