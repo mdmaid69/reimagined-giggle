67c9b2b0ef466f6ca173ef16013b2530109c759a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:16:28 +0000

    I dry loss fat definite

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+for i in range(10): print(i)
