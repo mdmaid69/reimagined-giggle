4987335f38e363d28d1cc07700fb4b269cca07bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:17:44 +0000

    whistle bank us protect view

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-def is_odd(n):
-        return n % 2 != 0
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
