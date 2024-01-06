51be353872c3071ff59a5f71a8d75e0caa86c002
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:05:28 +0000

    pocket earn why quit gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
