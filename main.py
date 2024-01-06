106c1d0eace6e38d9ecc2b9ab1c0ae9bca49c217
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:51:30 +0000

    building early recover morning skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
