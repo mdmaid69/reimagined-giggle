d7daffd49bf565ad9e72f644f3bcf3586334e163
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:03:01 +0000

    past circumstance area violent convincing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
