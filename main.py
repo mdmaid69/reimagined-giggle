e95e38bc189af6fe23927cff7408d919f3f4e4a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:24:31 +0000

    consult action room target interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
