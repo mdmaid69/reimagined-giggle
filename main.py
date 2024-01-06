047a77324baaf5c4ea7dd9c2df06732a51259ee2
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:29:07 +0000

    straight giveback communication know drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-for i in range(5):
-        print(i)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
