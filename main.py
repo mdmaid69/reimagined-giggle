d0987f23ccbc251827e854e2c9639e55d211857b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:32:01 +0000

    genuine draw tend Qq study

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
