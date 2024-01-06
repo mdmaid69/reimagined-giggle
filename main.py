0e5bd4f4bea46887be186a9160359e5c5a39b8f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:50:09 +0000

    accidentally food loss war comeout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
