d3a8b413e0f9528e57923564fa8c922842d5cbff
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:05:14 +0000

    morning save ask permission comeup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_index(array, item):
+        return array.index(item)
