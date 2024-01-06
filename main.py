2daf4b5f923c8c1503db91d3fb5ad0fd9b39dc5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:42:52 +0000

    bus quit bright respect anything

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
