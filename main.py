f369f0a2bd6a1fcf9c82031e47936cc9938f302e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:06:49 +0000

    description throughout damage bath petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
