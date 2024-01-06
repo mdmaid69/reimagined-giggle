3262a0b4e685af1511a03ad0afbccc30bb13abd6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:29:51 +0000

    over fairly burst everybody unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
