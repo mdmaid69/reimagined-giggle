e61285c22243271dc73c37ea6e25b9a4a70eb7a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:25:25 +0000

    upstairs high fast subject support

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import platform
+def get_os_info():
+        return platform.uname()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
