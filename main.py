379e80e9c2c7e0233dde873a4c8ff6ce48550ff9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:28:18 +0000

    guess minimum small if number

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
