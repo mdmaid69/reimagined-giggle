ffeb97715593b8f2654f380411a29d6000874205
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:17:04 +0000

    mean fact sound mixture press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
