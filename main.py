ef82777f8eec1b9975080b7200216072f54dfb42
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:26:58 +0000

    shallow control above television shine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
