d800c485d03544bc0c2103980f1b696d7ccd00b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:27:23 +0000

    stamp crime between getback breakdown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
