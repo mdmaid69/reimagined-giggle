e3c6ddc1b2d9a65a2fca382db6293c7b3a293da7
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:08:28 +0000

    complain talk afternoon afterwards flat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
