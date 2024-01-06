477590f3ee567ce87d80d8a5d4e9845190fc894e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:46:45 +0000

    harm whistle luck mood defend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
