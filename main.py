aeedcdc4ad9f806fdabc1b4b4d15e39564a03d65
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:05:20 +0000

    light human seriously call trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_list_from_array(array):
+        return array.tolist()
