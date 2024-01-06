c86106231a294b6121df9423f8aee492bbe5e13b
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:38:55 +0000

    receive language black tap memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
