11dfdc8d4c104888e0677d6528d584bb442cd4a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:00:05 +0000

    cost seem absolutely become except

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import datetime
-print(datetime.datetime.now())
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
