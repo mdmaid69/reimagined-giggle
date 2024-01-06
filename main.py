ecbe68d3b22d767117f15f6460f1c75988dbb738
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:30:30 +0000

    contents remain start move mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
+import os
+def remove_directory(path):
+        os.rmdir(path)
