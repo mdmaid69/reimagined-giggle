a0061b691b64e89fe2a5ea378aec9c1684732cf1
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:05:01 +0000

    glass fall dish middle expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
