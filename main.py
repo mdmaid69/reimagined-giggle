4f43d6d68093e71fc5b88be57498b78c82a941ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:24:15 +0000

    cum special runinto notice will

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import time
+print(time.time())
+def find_max(numbers):
+        return max(numbers)
