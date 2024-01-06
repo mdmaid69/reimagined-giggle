77c85efab62ac6a0feafcf15e572b16eca020959
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:10:05 +0000

    detail hope target man month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import re
+print(re.match("h.*o", "hello world"))
