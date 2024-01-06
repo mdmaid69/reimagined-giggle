9c78dec312fee0d665e76ebb92bb930105fa68e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:56:40 +0000

    married pool ago spin priority

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
