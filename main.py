786e9b6a8aef01a17187546468e9a5ba3b8783db
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:19 +0000

    Bb unpleasant cap shoulder list

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
