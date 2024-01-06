055af8e590b9da79f267b6dff4624a775afb1b21
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:19:40 +0000

    hour whom contact traffic hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
