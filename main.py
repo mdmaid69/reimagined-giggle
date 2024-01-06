0c1638de33a871d4489f2fbae0bc3326b313beaa
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:13:00 +0000

    or near invitation too forever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import platform
+def get_os_info():
+        return platform.uname()
