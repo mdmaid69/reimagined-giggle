7b6fcecc36caf7033ba29c11a7153ad6ee48ab93
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:07:57 +0000

    modern science estimate backwards pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_floor(x):
+        return math.floor(x)
+def is_palindrome(s):
+        return s == s[::-1]
