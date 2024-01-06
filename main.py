177b1a6e8fcfdbbc4a6a50df730788fc3d64d35c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:33:37 +0000

    insist practise Rr report lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_odd(n):
-        return n % 2 != 0
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_username():
+        return getpass.getuser()
