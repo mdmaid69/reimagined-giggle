9d7ce969aa4e7a849fc8bf5dd7bd1574168cb3fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:19:46 +0000

    there happy urgent offer thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
