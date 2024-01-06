745a8658f8bb76d5badf939be4231e27ce4f9312
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:45:09 +0000

    point disapprove stranger detail pot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
