484ccf0a81f7c32aaab6c18dee4ed55888c00121
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:04:00 +0000

    kill contact explanation reduction tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
