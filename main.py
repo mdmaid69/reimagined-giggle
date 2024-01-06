5ddc8064fbdfc0fde754c94ffd3f6f883420c107
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:25:23 +0000

    finger form pump partner tragedy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
