22f32be5824ebcbb5500502e74fa4f0c8e71fbd5
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:10 +0000

    confident plastic quick unit roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
