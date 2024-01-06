41822fc65fcc290cf5fbb2f7bfb93928be86f82a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:29:55 +0000

    ashamed evidence pollution far part

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
