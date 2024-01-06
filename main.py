8383669b7c8601d69c773a6248f09a00b52244b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:39:52 +0000

    flow knock mystery mystery side

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def add_numbers(x, y):
+        return x + y
+def find_max(lst):
+        return max(lst)
