576d97c05211845738ecffd93c5871689eb624c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:00 +0000

    ability home vanish score friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
