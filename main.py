6d63600b026a3beb8e1151d9d61664453a21c09d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:16:09 +0000

    among tend but pause fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_sign(x):
+        return math.copysign(1, x)
