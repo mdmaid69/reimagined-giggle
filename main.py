01d3b14e956db86f2ffc916101dfc6ec03d2735f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:05:59 +0000

    bring slip match getout chat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def convert_to_binary(n):
+        return bin(n)
