7d16cefd2a00b92d2278bb7453882f1f03f07a3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:30:34 +0000

    road swallow immediate Ii lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
