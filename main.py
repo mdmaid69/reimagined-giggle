62e5acba872cde153f8d972e0a2e6624571f7252
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:16:00 +0000

    any however generous born surprised

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
