73237070b5b52b0c3c1cd78ef5c7ed39fde7d2d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:19:12 +0000

    regret answer project condition split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
