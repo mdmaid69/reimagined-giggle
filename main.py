f9ceb2801e3cec3731758b12ea47bb2b41165c1f
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:55:10 +0000

    shop can hold independent activity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_power(work, time):
-        return work / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
