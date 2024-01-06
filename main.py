4ccfa84f66e469b4cd20ef82de8e4532b6087fe1
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:05:48 +0000

    outside myself law plenty warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_density(mass, volume):
+        return mass / volume
