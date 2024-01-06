6547af7afb0c4e805d4d6aa258f27048d78c740e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:35 +0000

    pass sleep should regular expensive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
