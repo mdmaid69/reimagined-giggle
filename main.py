70d65ff405cc9dec97fc17af44ad45bb6d03018c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:25:56 +0000

    air attempt match now pleasure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
