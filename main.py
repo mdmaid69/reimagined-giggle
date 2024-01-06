f30443ebc9cd73ea9d9eb1f9d2b3a4084ff8e51c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:32:27 +0000

    other free certainly excited wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
