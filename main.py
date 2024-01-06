fd21db579dfa7f683fe5f7a667902812672e21ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:47:34 +0000

    getback store especially ought deliberate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
