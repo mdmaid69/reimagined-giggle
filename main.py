b33ffd70e09ae8b6afb4b3ce1b5a471db9f37b3f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:52 +0000

    more quickly sensitive advanced love

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import time
+print(time.time())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
