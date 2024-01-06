0fa3fc549f89aee30a525bcd082fea5a40468a3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:41:43 +0000

    interested out relax union one

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
