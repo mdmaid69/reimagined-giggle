3f3c150c26439c85c778587a7109f656398b79ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:31:50 +0000

    sore height later knock rain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
