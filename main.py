a91d106bdeeb66ece1c2515d69f6538ff5d922de
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:29 +0000

    sing vote dark runover report

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
