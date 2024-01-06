4e06f5f17aa4fc46cf9b44149a1d33d362e50d73
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:55 +0000

    phone slide minimum opinion operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
