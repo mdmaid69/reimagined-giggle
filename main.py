140a4c4b724e873678b4c28cb0a7f385a297cba0
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:35:58 +0000

    lost line report characteristic action

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def append_to_array(array, item):
+        array.append(item)
