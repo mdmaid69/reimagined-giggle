dbde339feb5dd8c14865a6099f6cc7a3464fdab2
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:07:03 +0000

    corner fairly slip computer chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
