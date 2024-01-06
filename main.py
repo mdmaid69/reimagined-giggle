7d16665fe01597dfa7dd25e25555888d7abeb776
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:42 +0000

    neither motion contrast place getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
