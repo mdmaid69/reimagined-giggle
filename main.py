395d9dcb475545069426b4746c132bac5e89fcce
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:46:06 +0000

    murder eager branch tie dream

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
