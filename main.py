0762e429606fe89c779b6ab25bd48629bad3e7fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:52:47 +0000

    noone authority like professional bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
