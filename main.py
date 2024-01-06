8e816c365e0b61438949d6700505185c2bfccac2
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:36:55 +0000

    then ashamed please education forever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
