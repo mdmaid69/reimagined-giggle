67c845be0562a5e95ad0e2df528e4815144e022b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:54:24 +0000

    outside admire insist weigh complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
