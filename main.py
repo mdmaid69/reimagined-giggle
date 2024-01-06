ce3c5d5a991775e7552a475ecf5ec12bcca539bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:14:16 +0000

    tax sensible hate black anybody

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def append_to_array(array, item):
+        array.append(item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
