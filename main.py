f4c57f9e89ec42332f9cbae674a636edae8fd4b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:06:02 +0000

    obtain delight majority secret soft

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def get_array_as_list(array):
+        return list(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
