9b5e860db7a6a0fa40c5f0c461e71b6f42c40d31
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:18:59 +0000

    lunch chief ours sensible game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
