94b6005d489061b970f1e5206402c7c2fb9ddfda
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:09:17 +0000

    cold bank couple onto spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
