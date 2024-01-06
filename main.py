42ee0880550dafc0ababc2e99da308aa85323935
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:17:59 +0000

    forgive wander tough house sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+print("Union:", set(list1) | set(list2))
