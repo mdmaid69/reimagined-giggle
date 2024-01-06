789a0c5eead2ebfb9db0c77f512deddb38964d4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:53:26 +0000

    how swallow pass goup muscle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def is_palindrome(s):
+        return s == s[::-1]
