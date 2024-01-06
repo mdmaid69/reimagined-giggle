a921f197f187884c96cbe82b77c5a33fa0533699
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:16:03 +0000

    keepup answer talk abroad way

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
