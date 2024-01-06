12063a01268d9d7788657c6a1e2258549ce135cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:09:09 +0000

    have male base lean rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_directory():
+        return os.getcwd()
