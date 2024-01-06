f3c38f09d9ad49255808d90d0f2b14c71cd97c47
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:05:38 +0000

    image wise order workon anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
