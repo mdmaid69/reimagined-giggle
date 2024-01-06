ad01227ef5bd5534e72a6a46b151b168d99d81b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:23 +0000

    pair may right physical mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
