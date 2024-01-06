c52017b80f00b71a7a3e7980c417b54510f0ac2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:29:44 +0000

    immediately near lonely wash communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
