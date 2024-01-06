ee52f2fd3a2d7efa2352662871e90543680903aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:14:52 +0000

    gate trust yourself change polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
