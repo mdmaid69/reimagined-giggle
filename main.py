e494f22be31ff7135679463e335716f2a0560129
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:43:56 +0000

    up strike keepoff advise air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
