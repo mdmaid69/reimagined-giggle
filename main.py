d2ae4ccdb75da2e1efcfe4b95decedff78a66259
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:48:03 +0000

    approve private turnup likely free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_counter():
+        return collections.Counter()
