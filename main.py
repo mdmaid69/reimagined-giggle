55e16623b60a4e6219ada3bf4d088c2d7933356f
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:07:14 +0000

    later squeeze left any natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
