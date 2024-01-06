3dc112ff1eca4ae6b408bb59975edad82f633b3a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:42:17 +0000

    warn claim importance visit flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import sys
+def exit_program():
+        sys.exit()
