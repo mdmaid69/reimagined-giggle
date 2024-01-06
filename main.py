03da8f634c196a43b0fb8cd142e454821752f8b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:03:21 +0000

    few chief imagine coffee high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_current_date():
+        return datetime.date.today()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
