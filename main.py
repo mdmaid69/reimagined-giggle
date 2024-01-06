f4d1ea03d5642ab431c1b8f3106c368c88de6658
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:59 +0000

    bottom skin eye neck defend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def find_min(lst):
-        return min(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
