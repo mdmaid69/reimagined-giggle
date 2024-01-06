6f8105491c2c05c17c5b5c4d27bc22eda0684f7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:20:45 +0000

    ready keepfrom quiet ground in

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_length(array):
+        return len(array)
