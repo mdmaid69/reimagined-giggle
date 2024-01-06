d3ceabf6c7952a5f31f5218474da85152f9305f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:10 +0000

    above attention turnon hand suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def sort_list(lst):
+        return sorted(lst)
