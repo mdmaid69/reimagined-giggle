4b109956ffa4b34b4e9ab823866a1c6898df6991
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:14:55 +0000

    late sorry mistake immediately reach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_list(array):
+        return list(array)
