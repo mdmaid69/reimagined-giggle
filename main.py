6366c5a64e0c6ac2d2603dafee8e487fe97f5fc8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:18:39 +0000

    behind exploration question start eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
