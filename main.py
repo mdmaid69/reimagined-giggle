cd07e7e97ab9ef12749bffb91a66c3b8f67bcb9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:49:20 +0000

    male care past town guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
