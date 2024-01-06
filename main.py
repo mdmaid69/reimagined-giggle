636d70bb0cb6453b890c98cc0c2ef47cf9bc4438
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:01 +0000

    female reliable ago sound smoke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
