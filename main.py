a682c4fa871a8b6b9c2bf588201a1a4235855c45
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:08:59 +0000

    stamp exercise holdon fairly our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
