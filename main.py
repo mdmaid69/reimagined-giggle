6271b8aaf9951a4d889ccf314c80c0891f40eb9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:36:27 +0000

    emphasis form will ought difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
