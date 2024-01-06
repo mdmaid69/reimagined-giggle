64151d765f64e5f4e4a59e502f509b0ac638b529
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:32 +0000

    shine point be free fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
