655dcd2c17ac39c1dc92e82c2e2c42530249f096
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:51:43 +0000

    expensive direction live wonder shall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
