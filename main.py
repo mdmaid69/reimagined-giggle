84349214deef13680a3cab44903f740f7dd4c8b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:29:31 +0000

    achievement dry noise form stand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
