56409192e2e4b7563f6c4b635ae007994c252df6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:48:51 +0000

    spring community breakdown comment path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
