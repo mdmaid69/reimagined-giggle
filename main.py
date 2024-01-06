b1e8ae28f9452cb4fa928cfb27e64bb96f6bacd7
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:56:41 +0000

    prefer fast involve season funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
