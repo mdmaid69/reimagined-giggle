1847f0993b9c780f3a52c23e62bd1e23756de2de
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:39:53 +0000

    sense fun less difference violence

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
