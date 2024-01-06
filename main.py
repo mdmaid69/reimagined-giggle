fd1360bc9d8e1f70f840ed4cb39bd4b959d48669
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:56:27 +0000

    about ignore nor track cost

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
