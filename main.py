31913fe8117b167ba267c1be99dfca1e73b05fe9
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:10:34 +0000

    room he future badly protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
