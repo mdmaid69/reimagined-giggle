512ca148899c32273cfff1022fb5a5362a4b5a91
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:08:07 +0000

    change apologizeapologise oil compare rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
