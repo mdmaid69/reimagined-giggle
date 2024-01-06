2e3aaba0e9448abfc7eebb4eaac18926e487acc2
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:22:36 +0000

    keepup dish bill minimum risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
