48c81623fba1943e6404e2dd4927cadd04fe48e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:36:44 +0000

    kick rather yes lost until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def change_working_directory(path):
+        os.chdir(path)
