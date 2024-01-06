194187192731470adc0fcb6923bacbeb00e7b4fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:59 +0000

    plenty frequent tear seat welcome

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_str(array):
-        return str(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
