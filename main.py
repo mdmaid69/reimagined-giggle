246ab48fbaa410f99fcf2a39b9dd5fa8d791081b
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:18:43 +0000

    566end beginning serious work tune

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
