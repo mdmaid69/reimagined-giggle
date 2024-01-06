071f24e3ebc1e3dd55cc2bee8ac30be03e776c19
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:31:27 +0000

    telephone free style lot shut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
