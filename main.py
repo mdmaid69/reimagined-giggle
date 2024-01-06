0ae16c1d25ae8c48118b83609207e308edf47b1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:52:01 +0000

    where tired violence healthy nonsense

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
