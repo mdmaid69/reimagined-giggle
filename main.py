d4fa658ca00c7aa564266bb1b1dc093e11af8a68
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:13:28 +0000

    action open sentence breakup shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
