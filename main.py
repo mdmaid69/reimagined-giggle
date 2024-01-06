b817db44270b4ff1180f17044b8b1a236a86af91
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:36:01 +0000

    on century image base mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
