55995981912eceffd8834332420edae287a30fe7
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:33:19 +0000

    exam cook fetch race once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
