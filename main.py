1afc03b29460a5ddecea284964db287d6e559b56
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:33:13 +0000

    legal burst scrape careful broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
