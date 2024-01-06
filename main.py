0837f1451b8b88bbf6181bd1c97dbbe79e7ba7b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:30:25 +0000

    doctor exhibition before thinkover everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
