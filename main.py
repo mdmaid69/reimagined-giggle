0ba065ce2e23da61276db72d5b48892aecb67489
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:52:04 +0000

    whoever fetch drawing goout number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
