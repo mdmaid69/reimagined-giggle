a0e6cf1280b966261e7aa051c78e4ee7d59e7cfe
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:12:59 +0000

    tooth how bank seem tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def is_palindrome(s):
+        return s == s[::-1]
