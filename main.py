14c16d65543b49fe96a557251bde689cce684c89
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:15:49 +0000

    test copy Nn wealth skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
