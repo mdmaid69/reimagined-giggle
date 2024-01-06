1ccfc3999b671f19eca744ac7936c0b75bef4a16
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:15:58 +0000

    notice flood remark runout season

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
