9999527b9aa3a6474e0ab121fcc06103abe3f0af
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:04:54 +0000

    minor wealth welcome bank offer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
