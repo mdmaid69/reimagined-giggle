2c8d1f25c174b45353acf5dcffcc727d57d45cc3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:29:52 +0000

    which whistle represent mention instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import sys
+print(sys.version)
