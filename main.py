260c193e9936d66e718f5c74e5aed54107e5a25e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:51 +0000

    ignore inform stupid store scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import sys
+def exit_program():
+        sys.exit()
