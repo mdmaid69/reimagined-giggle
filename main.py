9895fafbf64590ffa0a9a9c83f5e4a34ccc59c71
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:54:24 +0000

    embarrassing behind half kind permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
