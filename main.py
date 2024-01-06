adbb286999ffeea68788d905fa40ca010c296289
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:19:26 +0000

    flow soak suck relief stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
