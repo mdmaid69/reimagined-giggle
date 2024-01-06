86027c9d4fafd197e7c7290730549e59b0a979ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:22:47 +0000

    stage sudden getaway body store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
