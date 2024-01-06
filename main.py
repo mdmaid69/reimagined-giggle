35de210aa75f7054724f5a643f08cfa477491674
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:52:51 +0000

    second music close slightly dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
