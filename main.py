bb914205009a7d77ec642998ce40f4321f1effdb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:57:18 +0000

    Pp out light possession public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_exponential(x):
+        return math.exp(x)
