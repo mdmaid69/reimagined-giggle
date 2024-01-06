32d099491fb98034e6e3a54975d352f4f64b7c32
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:12:19 +0000

    shape personal all as now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def find_min(lst):
+        return min(lst)
