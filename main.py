0dda31f098c75904147d0cc8f9fc6448e4adcd54
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:25:53 +0000

    safe woman form flow her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def find_max(lst):
+        return max(lst)
