013e5fbabff3f11c14fcb5308b4e648c9d34c408
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:59:08 +0000

    history Nn street month level

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
