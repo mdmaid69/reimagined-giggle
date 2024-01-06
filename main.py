a4fde9b2d76a8c2515ea3cb5a62b509c8cfb2619
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:45:23 +0000

    there abroad identify one 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
