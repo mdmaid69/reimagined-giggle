55c8a7f518ad73186cf1a2aa663652ad316a8ee2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:06:12 +0000

    suddenly window path us way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import sys
-def exit_program():
-        sys.exit()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def add_to_python_path(path):
+        sys.path.append(path)
