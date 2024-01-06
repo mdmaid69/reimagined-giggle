087d6ee6401474af16a5ff26fa8b2ddaac989d57
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:42:02 +0000

    freeze instead present glass tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  def square_number(x):
+        return x**2
