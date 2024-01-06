2fecc00e9c3372f01521f0492040cbb43bf38598
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:44:19 +0000

    project afraid know content tired

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
