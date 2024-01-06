a5d4bc46e8f2c9698173cca6b15c20e1860a4ab8
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:55:55 +0000

    whistle shoot minority belong transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
