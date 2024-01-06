6ac9bf126717f46b4967f4b297712128a73e2569
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:00:54 +0000

    crisis shut stage past wealth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
