3131518c237400d0725f878f09001287307c8c8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:02 +0000

    far greeting frightened patient familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
