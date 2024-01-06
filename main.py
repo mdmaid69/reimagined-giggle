45c232bed1d3d257a4b0479646f476269c0f6c89
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:41:35 +0000

    blame deliver declare 567enemy dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
