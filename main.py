6f240b1ae727fa941dc6db8cd19a4edef04685c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:52:22 +0000

    until swim damage willing young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
