aa725db99032a973f9124e8e45dcfa98d8b14dfc
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:55 +0000

    somewhere with search quickly luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def count_elements(lst):
+        return len(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
