ccc39e4ee77a4fa9456be37021ae456b790c416c
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:58:27 +0000

    opposite city sometimes fill kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def reverse_string(s):
-        return s[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
