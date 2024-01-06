508c48551a6f7c8308e4cfd02b05d4d109c2f71e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:15:26 +0000

    evening sometimes because mass common

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import logging
+def log_message(message):
+        logging.info(message)
