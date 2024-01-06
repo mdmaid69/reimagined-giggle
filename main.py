9d54c1234737155f733de54739178be65cfb7a1a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:26:18 +0000

    flat press only temperature picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
