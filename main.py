9d5fcae39197ea402e8595326774cbc208067650
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:58:42 +0000

    getback unless test block keepup

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
