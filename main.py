0b86aa1a5dc7b621f6f9d173f7f5acf467ce0784
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:14:31 +0000

    Kk needle bringup attractive declaration

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def calculate_area_rectangle(l, w):
+        return l * w
