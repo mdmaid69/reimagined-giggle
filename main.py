570ff7d08495186cd505d8c3835915851959ae37
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:38:09 +0000

    recent free vanish important jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
