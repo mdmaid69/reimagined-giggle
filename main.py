216ff9770c3f72e48282679f1ea8b772dcdfa40b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:24:21 +0000

    bush spot gap rid shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def create_stack():
+        return collections.deque()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
