0b467899d93efd2e7c20a1af94c5c1735cf558b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:42:39 +0000

    cut callup finger pleased breakinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
