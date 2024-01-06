91829d5f17f3a9b40ea67f4f2da63f579226574f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:17:54 +0000

    wrong up empty clue definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
