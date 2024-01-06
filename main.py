6337e3ba7b56ccd13eeb1f462def8e06ccb831d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:31:13 +0000

    cheek getoff hope put burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_current_directory():
+        return os.getcwd()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
