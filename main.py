64f6e31724baa0425c19f46d2b8dc06ef094f5d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:01:02 +0000

    short wave lower flavour container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_as_set(array):
-        return set(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
