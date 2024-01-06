2ac99dd165a170200186b9d1c809d935cc61b29e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:14:48 +0000

    eat present truth shock takeback

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
