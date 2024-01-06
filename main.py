813f3b3294eaa695b368680c0c6a24931b83a286
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:43:38 +0000

    dream claim tone need memory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
