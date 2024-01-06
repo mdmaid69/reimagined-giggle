6209703515455c066737996a2948e90581bf7f5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:02:12 +0000

    taste mood closely now past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_max(numbers):
+        return max(numbers)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
