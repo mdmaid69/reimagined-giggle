f492ac395ee131ddb152b1ad8fa95ddecb766965
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:16:09 +0000

    scream computer mean poison hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+name = "Python"
+print("Hello,", name)
