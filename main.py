fab65b6b8b2f1bfc39a50f397bce07ed65ea9f95
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:11:40 +0000

    leave music bored sore entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
