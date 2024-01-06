8bef315e2f0cb009de8f8b8b96f30ae867f5bbb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:28:13 +0000

    shock politics need side sell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
