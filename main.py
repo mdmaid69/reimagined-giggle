aca54a89dcedeb613be576a68dec81aef631e63e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:50:39 +0000

    wise material fire serve sister

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
