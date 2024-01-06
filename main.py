318acef95dddadf2fa3d2de56e64a9a7701318aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:08:34 +0000

    top thing skin beginning thumb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
