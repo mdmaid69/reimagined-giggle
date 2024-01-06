7dc73e9c2bd79c0b059b66a8b3bcfa25995f16ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:54:03 +0000

    suggestion fond hurt thirsty way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
