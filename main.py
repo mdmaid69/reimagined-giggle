7227143a64f28ec356b264dd56148fd869b04b9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:43:50 +0000

    order do month as interrupt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-print(math.pi)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
