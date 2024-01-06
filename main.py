e2bb70b7cb17fccbb71fabab9f242f9ac114a440
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:49:31 +0000

    service face patience drive voice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
