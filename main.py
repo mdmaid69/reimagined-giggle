c2ceef6a121023b4fed3b4837113d24014409077
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:38:34 +0000

    dish petrol profit delicate stay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
