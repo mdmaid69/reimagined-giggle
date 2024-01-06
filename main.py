1db00379e1250a39b7b590032e3ad56d409d18ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:32 +0000

    introduce young across smooth listen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
