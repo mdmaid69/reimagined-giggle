c43a7c40a2bf7133f519463ff70b9b7e13a01150
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:06:53 +0000

    condition come grateful image criticism

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
