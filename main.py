89b5761735bf64439b3e13a5b54306edbf6636a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:51:47 +0000

    reply square quit yes unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
