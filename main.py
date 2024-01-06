c0ea6c66256da9a54824728111df4b83116e5b02
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:56:48 +0000

    best far limit connection Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_stack():
-        return collections.deque()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
