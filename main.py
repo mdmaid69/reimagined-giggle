e7ab06a9cd3b395bc96d370f9c05d296378dcc73
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:48:43 +0000

    reliable smoke air autumn funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
