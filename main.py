44c60cbae063b09b979852db36d9e78c6cd211dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:37:11 +0000

    better related expect promise sleep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
