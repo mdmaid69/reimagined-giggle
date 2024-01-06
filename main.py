bf53851776e41cfaad18fad918a065a43ba59c47
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:35:52 +0000

    peaceful attempt up practise nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
