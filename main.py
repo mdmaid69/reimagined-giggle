8ec0a037beea60da0f8a80f9bcf9c97c81729b7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:08:51 +0000

    scare nor through worth flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-name = "Python"
-print("Hello,", name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
