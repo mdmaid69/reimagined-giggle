b5d70de400eb2799e41a039094baf07a4ae7c8a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:01:36 +0000

    standard admit never rest trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def clear_array(array):
+        array *= 0
