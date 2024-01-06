310cdb1208c97db0894a481f705fd9fc33f9bf3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:38:06 +0000

    part fetch imagine usually cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_tangent(x):
+        return math.tan(x)
