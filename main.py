e1e96d875499ebf9443a3f41cecb36fe6873025b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:05:24 +0000

    carry post comeround professional whatever

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
