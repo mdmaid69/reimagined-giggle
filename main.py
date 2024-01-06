5c61208df2f91d62f7f4f3810914c67d1acbbea8
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:00:55 +0000

    major Oo company thing strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def cube_number(x):
-        return x**3
+import math
+print(math.pi)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
