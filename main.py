9cdf3b547a273f28c9b2f9b53aa946c3b87e5821
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:42:58 +0000

    within line coffee sweep Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
