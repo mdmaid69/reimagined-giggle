4f4ba8d22bd68efc1f4aeb16044e4c1af0bab521
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:56:02 +0000

    illness convenient urgent aim wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
