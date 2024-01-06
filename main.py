b914af3ce11bbb43cbdee7ebf8f7f6f15d0f35c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:20:43 +0000

    mystery classroom firm borrow balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
