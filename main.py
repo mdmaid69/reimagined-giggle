35ea2991cec8c04b8e21520197d628de10f56cdc
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:02:55 +0000

    upwards shoot send vote cruel

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
