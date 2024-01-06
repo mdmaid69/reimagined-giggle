c595872ae934b40cc5b29dca751f98e1e5cf4ef4
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:52:17 +0000

    music bath force spend copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
