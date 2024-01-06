6262440f8795ae82bfb6227a46ec2ed33f9b4a31
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:49:37 +0000

    trust carry thorough mean claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
