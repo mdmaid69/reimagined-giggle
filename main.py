c29fbd16849edd94195d6a03b2d18069993c7e40
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:19:12 +0000

    narrow earn development soak book

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+  def convert_to_octal(n):
+        return oct(n)
