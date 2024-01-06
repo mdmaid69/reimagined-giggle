0992d95ec07010b30d3cfd7b0460e456e4762f76
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:00:50 +0000

    couple switch would inside lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
