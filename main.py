fe605d29126a0d4365e56dbf9830d13ca57af2b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:56:05 +0000

    morning draw motion frightened me

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
