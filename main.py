8dad1ae898887dac3e011e65b65cf2ba3c4f0313
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:36:14 +0000

    once secret little plant uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
