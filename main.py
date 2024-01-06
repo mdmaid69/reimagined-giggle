2cfc8ef8bba62bb1261ffcb148218a0cde45d78b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:32:21 +0000

    listen sensible area Ww copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
