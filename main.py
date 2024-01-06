7f39e804ce396a314e930cbe37908e6f67ec6ae7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:36 +0000

    grade emphasis name pocket factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
