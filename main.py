e40c7cb8cfdca4d662ee23aca1772ee8c37ebd19
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:36 +0000

    dry perhaps pen difficult shoot

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
