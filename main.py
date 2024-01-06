c7db81ed8cc038a7fe34b293eb4155b10ebda466
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:31:40 +0000

    cut purpose confident joke exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
