f04857a5f1f31054392b2e1c83f66dee184e0bb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:21:43 +0000

    branch more scrape short they

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
