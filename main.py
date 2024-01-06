e3350183e7e480eedd6f5161dd029cfbd81b8799
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:00:36 +0000

    forward manner excuse downstairs statement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import numpy as np
-print(np.array([1, 2, 3]))
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_pressure(force, area):
+        return force / area
