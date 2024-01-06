271f0235e1d71099f3bc2b437619cb869126c524
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:05:37 +0000

    soak game mainly you spill

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def count_characters(sentence):
-        return len(sentence)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
