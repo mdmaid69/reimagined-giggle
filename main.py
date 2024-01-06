5e6e304f8aa3562c06513e9595e1a079a9f9f1c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:19:21 +0000

    really female century course takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
