65831f5e1a6caed836ac84b6325f4b1e21231bba
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:02:43 +0000

    comeup family organization uncertain job

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
