85ae863eb83e6133cb786e1f85b7869af2203d37
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:00:15 +0000

    learn action sticky fasten know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
