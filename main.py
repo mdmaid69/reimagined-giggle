ae3d0eed8b94ace3df4cc3fd33b97f5805225ed0
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:58:09 +0000

    grand drawing improve team shelter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
