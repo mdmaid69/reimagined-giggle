89bd32ccd1e7799f91548bccd88f843992755704
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:12:26 +0000

    complicated tool bet although pile

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
