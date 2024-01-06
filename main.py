f7a035773d3e2df49e6b44aeffd839d8b9b052ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:55:31 +0000

    approval obvious hope late contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import os
+print(os.getcwd())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
