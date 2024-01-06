972af781f1e245bd7debbd060c486a7848eddd5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:57:57 +0000

    age book welcome level special

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
