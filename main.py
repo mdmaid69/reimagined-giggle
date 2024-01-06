85fc0568e3cb96379dd4dea6a287e9131687807d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:59:36 +0000

    let language feed community politics

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
