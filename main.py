5be541857c6d7b0b7260bb40218a8a99aba2f47a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:28:16 +0000

    narrow dream remove comeover switch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
