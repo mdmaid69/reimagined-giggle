d7d971b2b526461e6defee78ffdea44ebd20921d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:56:44 +0000

    confusing rarely story burn first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
