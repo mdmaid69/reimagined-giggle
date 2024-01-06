eef1c04c4893cdd91979c0fdd7ce7475e6956820
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:19:35 +0000

    get exact himself store extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-print(re.match("h.*o", "hello world"))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_odd(n):
+        return n % 2 != 0
