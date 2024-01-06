e5ddd1b8d7dadca0e3ce67531f654f40d4e173ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:58:00 +0000

    knock low shut young bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
