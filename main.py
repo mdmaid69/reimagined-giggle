6ea8080443f999570ce35b186845af821143b64f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:56:51 +0000

    anybody afraid attention rise look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
