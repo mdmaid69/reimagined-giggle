c8cc6399c4312823a7065e012810984d4660c657
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:18:35 +0000

    carry relation fat tough Ww

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def count_elements(lst):
+        return len(lst)
