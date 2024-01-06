b5eccea7966ab987a27b1642b3cbc872766b864a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:37:15 +0000

    comein proud come talk boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_list(lst):
+        return lst[::-1]
