1f2253764da2e11668ad8df2d587280651ab843d
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:04:31 +0000

    few commit pencil car sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
