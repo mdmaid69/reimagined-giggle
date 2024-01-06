bb8e634f80455003c7d0fcb7988db8c5ec9ffb07
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:46 +0000

    over aim explanation knock large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
