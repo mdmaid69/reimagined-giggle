7be4d90ad31d560055dc0169102510eb34b94538
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:32:46 +0000

    quick lead comment load pause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def multiply_numbers(x, y):
-        return x * y
+import re
+print(re.match("h.*o", "hello world"))
+def find_max(numbers):
+        return max(numbers)
