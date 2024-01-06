353a65da1efd56452685dce52f409f75742933a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:44:03 +0000

    plan tradition case comeout fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
