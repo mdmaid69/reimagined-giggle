553abb094f46a30409de35c8b7e26cf202969e11
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:09:40 +0000

    date wander fall pronunciation minimum

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
