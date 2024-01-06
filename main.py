5af3b7c921bd97bc9718ae392ef337e1734f5235
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:46:30 +0000

    here dream entry lonely proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
