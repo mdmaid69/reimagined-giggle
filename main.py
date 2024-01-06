6a653054918569b96d505dd6e781cd5a2e787704
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:24:37 +0000

    father when professional Ss smell

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
