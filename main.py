76b8eb5c4fc7db890f3b3cf62b37d9b5db1daff4
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:25:44 +0000

    clean interesting rely count funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
