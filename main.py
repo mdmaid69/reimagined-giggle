963146ccea0fd72ce718c983de434c74f15435c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:58:29 +0000

    truth sale ticket size tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
