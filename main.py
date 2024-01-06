6b525da26b14190db8ddf27862dfba239704bb5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:29:13 +0000

    Yy damage fierce tap influence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
