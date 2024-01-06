3ef27d5eac6374bad70c561ac73f481a3fb95968
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:13:35 +0000

    there stir spin to born

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
