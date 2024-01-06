b31a4986fe17439f1647ded42864ef51d025db4b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:03:58 +0000

    ability 565end actually tap sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def split_path(path):
+        return os.path.split(path)
