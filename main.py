a6e1b5634f9092b1f7fae51382853c8f9ee2ba78
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:07:29 +0000

    deep Uu train emphasis confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
