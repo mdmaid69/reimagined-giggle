1055180a1a6dd286cf0b07aefc00fa0fcde89b52
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:52:59 +0000

    smile letter window across reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def remove_duplicates(lst):
+        return list(set(lst))
