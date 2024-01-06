246d08e8e73152c3ee191c305b2b0a343a6f7c68
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:23:11 +0000

    trust runaway burst soil trip

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
