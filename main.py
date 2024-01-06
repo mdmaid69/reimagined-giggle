cea76f51777051f9cfb117cb51671edb58a280a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:58 +0000

    exhibition gas wire fond close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+import time
+def get_current_time():
+        return time.ctime()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
