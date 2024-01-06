28887073778f50aa6c27743153995f07137cc5dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:09:07 +0000

    everyone represent complain listen conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
