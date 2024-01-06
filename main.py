ac2b0541493dae7db47e6ee8c6f5e0ace5169526
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:14:56 +0000

    delight simple there possibility upwards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_elements(lst):
-        return len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import platform
+def get_python_version():
+        return platform.python_version()
