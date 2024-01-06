2b119651bc19873094dec16900554de2849f3b7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:28:20 +0000

    some season pencil brush climb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
