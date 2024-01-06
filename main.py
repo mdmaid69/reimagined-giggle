2e81acd4ec5a24a733ba871078fccec17f928277
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:58:23 +0000

    moral tight regret money react

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-print(random.randint(0, 100))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
