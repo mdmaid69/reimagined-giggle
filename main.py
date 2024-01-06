1969b9ee3b028aca62c423f4fa6617f11d54650b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:00:32 +0000

    complaint minor pollution with allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
