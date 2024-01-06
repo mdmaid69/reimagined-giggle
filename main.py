8ac2038d6605b303f71678b755fcd9a8417a4a18
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:22 +0000

    goon basic information general scrap

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
