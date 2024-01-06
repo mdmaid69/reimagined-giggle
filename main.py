e616a4394aff69f28253befe5971242f166af9a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:05:21 +0000

    soon sick recover equipment juice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
