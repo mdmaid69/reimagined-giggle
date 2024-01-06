74352c9211d27c6c52b060871741632be989fcfe
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:12:33 +0000

    back release may Ss cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
