fddca128757a4451d3208f2f673752bbf4613292
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:43:15 +0000

    correct train type want wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
