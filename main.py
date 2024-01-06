b96872aa08cfdb79fae98062896dcd6918fbda46
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:27:38 +0000

    through have pleased operation air

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import datetime
+print(datetime.datetime.now())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
