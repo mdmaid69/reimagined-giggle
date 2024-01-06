2d9750e8f45be7f4199d8f0cc34495ffbb4047c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:44:50 +0000

    town fine juice false passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
