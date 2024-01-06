df9895bab7101e0eb507e963b566d1d771151bb5
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:07:07 +0000

    history worry frighten 564encourage regret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
