5b424ecf8d5f894fc4823f454f52e1883a80b814
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:26:23 +0000

    deep to try insist once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
