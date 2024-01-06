dd994239cfcaaa7904454b6ef256cba5b7b3990c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:20:19 +0000

    thing nearly punishment question string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_repr(array):
+        return repr(array)
