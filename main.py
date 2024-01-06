e89ab44a473bb7850650b83f2074a07ecdfc47e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:24:52 +0000

    insist exam getup row impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def get_array_index(array, item):
+        return array.index(item)
