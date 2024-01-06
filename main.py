6e11b5ca6bc8c03e32a0c2c56a02d4e4a0ed7774
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:15:00 +0000

    fast back provide tone treat

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
