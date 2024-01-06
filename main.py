7c631259168392dab5acb9e4a2c52d70560ae299
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:40:06 +0000

    juice ear hand act laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
