b1a80e1181903956e044d3cb19ecb0284839334f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:30:38 +0000

    judge own how freedom takeaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import re
-print(re.match("h.*o", "hello world"))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
