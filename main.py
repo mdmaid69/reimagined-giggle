a9ef506d8e2952dd00ea39ed1dab3ec7abba3434
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:46 +0000

    line charge test cross grade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
