f79d23e3ae87843b74cecf98176e234c90c6ace0
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:12:21 +0000

    drive week this boat turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(x, y):
-        return x + y
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
