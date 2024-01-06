26ec3a09af4218d92c014656e9e79c4f5948a1a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:27:51 +0000

    present breathe classroom centimetre tip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
