97273479f031e7eb13d7cb1823830ebd799c7d4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:26:25 +0000

    comment off general difficulty opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
