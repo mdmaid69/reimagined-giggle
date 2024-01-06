731ea4e6ae6370fbd9723c66c543dd3071b265ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:07 +0000

    react production permission sleep profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def cube_number(x):
+        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
