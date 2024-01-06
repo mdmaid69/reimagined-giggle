9c22c16d7e074ddef58be6aff087777181cd9c50
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:28:02 +0000

    compare back separate run effort

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
