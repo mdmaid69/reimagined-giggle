2cb18725fbcf6f08c1da1908517e6fadce4d6d90
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:14:54 +0000

    control flood sick soon strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import os
 def get_current_working_directory():
         return os.getcwd()
