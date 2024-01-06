addb6c24369ad0fbd1c30c55d8201f0d19d5ccf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:55:11 +0000

    sight lay rule minority think

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
