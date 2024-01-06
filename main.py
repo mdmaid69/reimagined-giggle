2ea165d86cd40e4d27f1df02598895110a4fe71d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:21:23 +0000

    community mail tune holiday holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
