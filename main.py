0c86321e88c1841cea48aa3f3e6de20a8c87bec3
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:21:09 +0000

    holiday flight mistake accidental sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_octal(n):
-        return oct(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def change_working_directory(path):
+        os.chdir(path)
