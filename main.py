2f6262d397e8e10fac696a0284b4579c437ce326
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:51 +0000

    workupto debt but sweep description

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
