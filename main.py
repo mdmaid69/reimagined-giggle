f8ab239ea089371a866069aeb7ab3ca1166a8a45
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:16:41 +0000

    simple confusing which birthday comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
