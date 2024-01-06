19e352d020f133d7de4215884f66e1a875c0671d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:21:05 +0000

    record behaviour medium whole address

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
