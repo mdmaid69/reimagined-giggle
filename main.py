9dd3286e93ece61e748ae37be215b05123c4dd49
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:31:56 +0000

    trust increase Ii takeoff pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
