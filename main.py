9fe3d1ee51fcbbc62f7078e92b2f890b7a09b0c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:37 +0000

    murder wide trick winter large

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
