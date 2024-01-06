feb6edba6c8d6f7cc6bd7ab47757fbecc07bd9d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:31:11 +0000

    pleasant damage explain confusing convenient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
