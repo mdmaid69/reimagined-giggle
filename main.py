e820112481d744b790e8f584785dc59a50431acc
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:57:40 +0000

    lift study centre truth communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
