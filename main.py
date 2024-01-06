aef63cfba34691134e0a03c4912f8a31269d8a07
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:56:35 +0000

    peace stranger team swim wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
