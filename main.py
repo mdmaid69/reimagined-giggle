61a7045a46aecacc72f28d35911ead4bb9d63033
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:00:02 +0000

    way suggestion disappointed guard describe

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import re
+print(re.match("h.*o", "hello world"))
+import queue
 
-{body}")
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
