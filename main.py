9faaeb44af1d4dc6afc6438454f0d9132f15466e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:28 +0000

    face shallow unite under concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def reverse_array(array):
-        array.reverse()
+import collections
+def create_user_string():
+        return collections.UserString()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
