683536085826833026034f685c20407ca97ec4b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:48:14 +0000

    makeup weather print down Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
