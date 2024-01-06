37e043a86e9d93eed3063736a363956493cbf450
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:57:42 +0000

    unfair recovery eye unpleasant suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_acceleration(speed, time):
-        return speed / time
+name = "Python"
+print("Hello,", name)
+import tensorflow as tf
+print(tf.__version__)
