7160e50e5cbe1b6fb67f6fe3ee51880d84adba5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:50:30 +0000

    hate deep take eye in

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
