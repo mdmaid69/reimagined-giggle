a9d956d0f443f08e719e1bffbff40f89f4a2b23b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:58 +0000

    travel need promise care tape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
