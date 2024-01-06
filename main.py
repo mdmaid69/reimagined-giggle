76b3ab7333ea4922e591a6c1e988f34c720d8aca
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:19:46 +0000

    grade set angry our set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
