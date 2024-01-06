239e4ac692017ebdd1e571fa97b71bb066c2a13f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:07:45 +0000

    bunch smell famous actually past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
