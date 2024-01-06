e2c1d44d0002144953f805869854082c0614264c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:14 +0000

    regret lower couple nor reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
