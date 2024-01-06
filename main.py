ed98faac322febe1007d8f4ba80e3b1eecd9f7f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:19 +0000

    emphasize between appearance along telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
