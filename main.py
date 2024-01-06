f5ca60e2e825149eaa1998fc151af36e7ac3f21b
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:02:07 +0000

    operation only address sale section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
