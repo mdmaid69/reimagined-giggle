e4d8476d2f83384a5c3ebd0ebdc917f48a590914
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:03:45 +0000

    deserve everything care confuse concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_float(array):
+        return float(array[0])
