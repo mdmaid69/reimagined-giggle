3d294a6374747f7c4363d67c2b7d01d9990229ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:09:55 +0000

    unfortunate regularly come gift convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
