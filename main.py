9e66b88f201bcfdd5137b7a4b4a0addea682285a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:05:46 +0000

    benefit treatment part round difficult

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
