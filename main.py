de44ed04e42f9fdf438993ca6f39f11ac896ef35
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:26:16 +0000

    give yourself repeat offence equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
