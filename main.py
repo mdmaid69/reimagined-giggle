09422d680ff8fcef8398719d55690731d488f07c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:18:35 +0000

    advice close this completely beneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
