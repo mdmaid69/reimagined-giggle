9dde2715e71dfa61a6e937f314da2d5479b59612
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:31:01 +0000

    own slightly office needle steal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_power(work, time):
+        return work / time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
