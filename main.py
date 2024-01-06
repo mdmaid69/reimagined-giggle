895f10c8bac74b2bc12263ec75c9f3a138b89b6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:48:33 +0000

    example shall cinema science leg

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import time
-def get_time_since_epoch():
-        return time.time()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_octal(n):
+        return oct(n)
