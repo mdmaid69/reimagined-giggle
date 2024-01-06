03d401ad4527bd3d72eb97f2cfd087fd0f48fd54
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:52:23 +0000

    if stage sharp open permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import time
-print(time.time())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
