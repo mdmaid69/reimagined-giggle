d918aa543fbf217312c6b1b482243b6fdb701fae
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:03:43 +0000

    estimate guide university gas heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def split_path(path):
+        return os.path.split(path)
