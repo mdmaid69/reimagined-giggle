5c1598d792805d4374a44e1086d5193b3744f5cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:05:36 +0000

    objection mistake away mark style

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import time
+print(time.time())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
