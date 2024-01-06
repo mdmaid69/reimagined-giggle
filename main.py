0bf6e3572ac04c063516ef13f0a722aef108384f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:27:24 +0000

    fork there equal bother communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
