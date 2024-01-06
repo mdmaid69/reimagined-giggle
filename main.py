781da3e1cfe811dccc272baf654c413acab2f9df
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:10:05 +0000

    reject frightened comedown congratulations formal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
