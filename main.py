cc1acaec10b9893a446fcc1ebeb4e6da6d7252cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:33:17 +0000

    visit rescue hear turnback another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
