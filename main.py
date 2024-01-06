059f7fab0e617b3eb2a2fce6a212bc1eea617066
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:58:30 +0000

    expensive welcome previous nearly wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_counter():
-        return collections.Counter()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
