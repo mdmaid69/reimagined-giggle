9683fc01a524cc025fd52902270a19cf3753d3a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:39:55 +0000

    imagination bridge group complete sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
