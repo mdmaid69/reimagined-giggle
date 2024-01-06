f1477e6db1089d8c2b6555a3e4d829b86ad7ec6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:56:47 +0000

    comein behaviour what unit circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import array
+def clear_array(array):
+        array *= 0
+def calculate_speed(distance, time):
+        return distance / time
