a8c4734c7a5aa6000b2a6cd28dce0713d6b4513e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:14 +0000

    must turn terrible mystery state

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
