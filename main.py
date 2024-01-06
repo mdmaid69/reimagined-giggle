9ee1ac4c407d3c873abf12aceaa5f85c91aa354c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:47:08 +0000

    over might opposite cope neither

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
