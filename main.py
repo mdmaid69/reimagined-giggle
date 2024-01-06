21a8561aed915897f05d0c896b0e6aed890a8674
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:37:16 +0000

    nowhere prevent taste hospital travel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def is_odd(n):
-        return n % 2 != 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
