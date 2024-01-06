b6c301d67500a0709bafdf167a75a121465b8bce
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:15:26 +0000

    beginning somewhere exchange calmly great

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
+def reverse_string(s):
+        return s[::-1]
