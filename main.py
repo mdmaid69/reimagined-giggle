ed828343f9f6e1c0d122aa38731b7c2558757b63
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:36:11 +0000

    public quick store public straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
