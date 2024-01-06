2d868a8edefc263d62519e499085622789503e09
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:23:34 +0000

    breakinto apologizeapologise laugh doubt morning

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_queue():
-        return collections.deque()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
