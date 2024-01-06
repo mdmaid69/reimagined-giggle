8b54cb60d5575a76597dc75553ddfa970571d6af
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:22:56 +0000

    home gather at his breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
