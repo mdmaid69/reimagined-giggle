c60df30074027977b01e92045bbc00b9d91aa534
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:46:20 +0000

    butter whom plain smile grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
