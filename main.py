2cba22e51b114865ea22918ba35622ec432d421f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:18:07 +0000

    know wear apply hard compete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import random
+print(random.randint(0, 100))
