7f1dfec2044da473752b995e7fe2eea5c0dd7587
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:11:10 +0000

    thorough settle carry division accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def square_number(x):
+        return x**2
