746f34de54ff8d4f0fc7d294aac3840d20590216
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:29:09 +0000

    provide bend kind different shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
