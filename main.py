ba7a13cdadb2ccd29e51fc82431cdf5b556ec36e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:27:44 +0000

    sweep contrast fair depend generally

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def find_max(lst):
+        return max(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
