7d21e71c574fa557a377b8ecb858df5f89cda4c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:53:26 +0000

    at sale under study hollow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
