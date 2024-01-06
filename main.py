f5431483fa67b7ce4ef62471f77b6a277c5f3997
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:16:55 +0000

    tread back lesson oil up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def append_to_array(array, item):
+        array.append(item)
