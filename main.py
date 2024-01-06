1e6a2263734f99f4b2ffdac6dcebf55bfb8cd09b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:24:35 +0000

    callup soil permission burn fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_power(work, time):
+        return work / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
