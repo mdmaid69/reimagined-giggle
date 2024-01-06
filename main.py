e069e1c26d441db743a9d2c50389b19611cf6775
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:50:24 +0000

    part lend join meanwhile already

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+print(sum(range(10)))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
