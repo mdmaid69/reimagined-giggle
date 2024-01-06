4ea5e8b04666c9abffd8b29f4dc489f33fcf3934
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:25:04 +0000

    demonstrate fight be face answer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
