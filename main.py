c1faefdfc1dec3ec1cae7480accbad8a83d561f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:15:41 +0000

    tip part delicate lost chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
