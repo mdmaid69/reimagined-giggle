ec2bd29d989fafb12f1241e15431f8bd94be4ddf
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:32:40 +0000

    thinkof smile argument hole like

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
