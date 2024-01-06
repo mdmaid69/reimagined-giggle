aba882a3ec96cb55021c42518a001db5420c93e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:42 +0000

    measure Zz central change anybody

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import json
+print(json.dumps({"name": "John", "age": 30}))
