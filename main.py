c230ff171a3bf3bbb5489eb968189cdb950ddc02
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:10:59 +0000

    bringup equal keepoff plan want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
