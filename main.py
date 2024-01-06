c3d81decccfb8efe560ee1684d38c1b8ed4900c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:26:30 +0000

    measure immediately touch form equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
