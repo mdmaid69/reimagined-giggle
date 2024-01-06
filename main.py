e37ba191fd3ff1eb702e247e9534e63f66091d85
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:37:08 +0000

    related turnout lookback(on) than maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
