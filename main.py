6a96c80ee18afc66dcb40107c50d8a883e7f06de
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:13:10 +0000

    mother second score return speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
