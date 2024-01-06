65b6f9925fb992ff9f8576d2e2e4be5630c2dc6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:11:15 +0000

    tape other begin speed with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def add_numbers(a, b):
+        return a + b
