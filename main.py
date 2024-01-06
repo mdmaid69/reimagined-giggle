4a160035174cfe1dc2ab62f5fff5b94d46aec6b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:24:10 +0000

    follow course approve Mm bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
