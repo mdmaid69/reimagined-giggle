d1ac6839b9b8b7dd934febcd495776eabda2fe6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:06:46 +0000

    seem nervous commit loose benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
