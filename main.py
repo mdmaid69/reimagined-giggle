33abb8f3ab2ccaeab5fd0cedd09738f49c106d07
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:17:37 +0000

    easy pump breakinto over creature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
