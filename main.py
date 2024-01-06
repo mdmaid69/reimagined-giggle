65ff6aeaae19529d4f39ac2b2a2e3f0827f4dc32
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:08:49 +0000

    fond rest experience under throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+name = "Python"
+print("Hello,", name)
