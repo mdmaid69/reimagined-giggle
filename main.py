50ed2ed686bf5120969dc849cba8e737cfe2a4d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:36:34 +0000

    recover correction plate rely getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def convert_to_octal(n):
+        return oct(n)
