b142c924853030b096b39a3546df8f95a4eb9194
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:27 +0000

    mood under object mine tomorrow

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
