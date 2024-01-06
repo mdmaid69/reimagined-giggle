a240bbbfd22e031b26e122f30f710ac3a095bf66
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:44:54 +0000

    score should part 564encourage level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
