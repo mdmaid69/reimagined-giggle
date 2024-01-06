2e9a46c7242a4a98c0441eadd6ab0ff291f83568
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:10:33 +0000

    runaway importance return load appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
