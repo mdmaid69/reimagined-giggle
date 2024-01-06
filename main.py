343c039035a6992aa0f07d93a66efe0c35f08d4b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:33:58 +0000

    value occasional especially slowly memory

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import logging
-def log_message(message):
-        logging.info(message)
+  def sort_list(lst):
+        return sorted(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
