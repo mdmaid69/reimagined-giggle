6a4afe75aa42ca22576c1fe4b4c0ca68eb46c124
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:16 +0000

    leg behind best usually question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
