3a11139009f005591d5590314e407f7d577df5c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:10:57 +0000

    Hh refer weak supply constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
