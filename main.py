261013cf6ad698925ed1df875083a339838efcc0
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:05 +0000

    prove on form contain string

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def square_number(x):
-        return x**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
