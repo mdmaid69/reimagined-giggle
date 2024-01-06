4d1246bfa27aeba74fe36b25e8c6cb42c28749fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:06:04 +0000

    picture yours satisfied turnout press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
