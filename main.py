0a0d3821a56057b101bba4dcbe75750e57bd37bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:02 +0000

    object just person wrong summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
