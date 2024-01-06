4a83dceb8393284a4e4015e5f2a4aa3cf864551a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:38:54 +0000

    bother giveout who even further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_max(numbers):
-        return max(numbers)
+name = "Python"
+print("Hello,", name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
