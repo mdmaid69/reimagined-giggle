861e00027adfd162f8552fd6af2cbd202d05f23a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:03:30 +0000

    grateful past bound back police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_float(array):
+        return float(array[0])
