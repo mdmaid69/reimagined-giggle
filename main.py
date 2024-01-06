67f660a9127064c31a20b3fea7eacab3180d65f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:49:09 +0000

    stiff handle pull abroad odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
