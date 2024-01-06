8b9be89a9ce4cbab60d4a239620cbc7a0b996675
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:47:50 +0000

    strange start bad translate objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
