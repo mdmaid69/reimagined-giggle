08a939c8a53aa04b7bdb74d5671b771cc6a63af5
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:49:44 +0000

    train bottom content in later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_speed(distance, time):
+        return distance / time
