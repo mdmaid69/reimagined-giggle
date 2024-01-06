93b830938a0d781732ea06e92170aea198926864
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:27:16 +0000

    loose past catch speed mail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print(sum(range(10)))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
