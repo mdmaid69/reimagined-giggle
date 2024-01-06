7f67874072d941d63dcc0a11ed5c0944db6c52f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:21:45 +0000

    or crowded almost very close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import datetime
+def get_today_date():
+        return datetime.date.today()
