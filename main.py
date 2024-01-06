8a4851b70e40d121fad0b46b4812e383a3cba445
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:03:47 +0000

    add proper wide bag quarter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+print(os.getcwd())
