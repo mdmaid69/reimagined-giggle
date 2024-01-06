e7fcdc3c4bb89e4d39e81b5500c6e1072e652812
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:34:17 +0000

    escape spare heavy satisfactory minimum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
