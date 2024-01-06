f37804c6c468f9d0c90490eaa87b458164b43ce4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:00:15 +0000

    on among Cc clothes car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
