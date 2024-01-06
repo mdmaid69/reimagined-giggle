0afb4cceb3d31631eaf9da6a336bff025135babd
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:07:52 +0000

    basic beginning corner organization angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
