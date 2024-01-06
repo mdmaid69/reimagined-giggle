7fc358e5eccd96185c2e096f2cfbc4ebf68eb959
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:56:42 +0000

    nor normal cover thinkover pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
