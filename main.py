9e80f15877ea4708f0e7d3c16eaa04deb64cc01d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:15:42 +0000

    takeoff intelligent opportunity smooth excellent

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
