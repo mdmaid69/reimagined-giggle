2286ff62bf52ffdd62c6590351f9ffd2141d1b8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:18:25 +0000

    almost division to genuine address

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
