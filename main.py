cee3a8a3860a5a76253068243e20af535706aaea
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:41:58 +0000

    look sudden declare identify talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def square_number(x):
+        return x**2
