c8a805182f74a0b88c118fa4b62ac01c0b5a594d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:32:35 +0000

    smart by choice rub whom

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
