d3a5cce831a7e75930e3959beacfffd224c0c4b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:15 +0000

    joke hello list nowhere nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
