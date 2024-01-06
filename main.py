231d03a98ac454a1bca6c80e11e9866691351015
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:46 +0000

    pipe likely related equal clear

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
