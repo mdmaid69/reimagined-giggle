6038d223e0c789c93f57f38675c04067a513aa71
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:26:15 +0000

    invitation laugh leave high dull

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
+import sys
+print(sys.version)
