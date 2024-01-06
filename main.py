d78f48902b96c19cbf0c5aa39131c3bad8d5e4c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:09:21 +0000

    comefrom level herself share tend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Words:", len(text.split()))
