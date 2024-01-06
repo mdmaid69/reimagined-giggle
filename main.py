c6b7e65fd66c3d6cc296d85ffb64908b66eb1893
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:55:27 +0000

    shape prize broken exact become

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+print("Hello, world!")
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
