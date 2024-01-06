68581562f95a57a973998d5e325a03ac69b26a07
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:36:58 +0000

    bring produce short way habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_time_since_epoch():
+        return time.time()
