b888b0ea320429d9d35ff5ce796cc85ad4a571f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:15 +0000

    sound moment advantage Ss wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_volume(length, width, height):
+        return length * width * height
