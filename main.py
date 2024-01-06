9352fb599df1c4d4d9e4766b912c6fb825ab4785
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:30:11 +0000

    visit notice comeup treat really

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+text = "Hello, world!"
+print("Words:", len(text.split()))
