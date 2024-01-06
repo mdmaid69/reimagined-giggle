104c1158fc74623b21dfd028c0fceb40c43b9412
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:25:42 +0000

    squeeze mood night call report

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
