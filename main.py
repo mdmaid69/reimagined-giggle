52403c2aeaea97dde53af39caa7f1734645da356
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:17:31 +0000

    lost especially professional height attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+for i in range(5):
+        print(i)
