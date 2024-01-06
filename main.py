4e00bfb90b023e227dad14796f3db3b1b58f5273
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:15:28 +0000

    fix move get head age

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def reverse_string(s):
-        return s[::-1]
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
