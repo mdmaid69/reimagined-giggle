4e8b639b9519d0028acac56c9ec02bfa06c49ab8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:47:38 +0000

    how trip community handle dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_counter():
+        return collections.Counter()
