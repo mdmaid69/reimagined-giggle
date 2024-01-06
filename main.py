35fd7e9628bb0c6af72dc2eecb7842b2da1530e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:44:18 +0000

    pronounce down before description song

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def square_number(x):
+        return x**2
+def find_max(numbers):
+        return max(numbers)
