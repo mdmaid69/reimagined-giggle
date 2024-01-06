87d9e5e2e4c7f6fb26d4552b017bde0a6242e232
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:41:03 +0000

    edge comeback dream rubbish rely

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
