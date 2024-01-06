e4f82553be279c7eaf37a3315f5e025a6e59fef6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:57:41 +0000

    turnout criticize image note test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
