eb2a66f03e04665bc2e3d97ba0a0fff93cf3ebd5
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:02:54 +0000

    decision expensive holdout side man

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
