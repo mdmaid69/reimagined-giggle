7a38b0dab2f074094603baa97709332ff308ad35
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:34:11 +0000

    street similar band summer freeze

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import tensorflow as tf
+print(tf.__version__)
