a630c6b5fb76f46106bdae7a71d66dedce66fab0
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:59:28 +0000

    impatient nearly hold trap holdon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
