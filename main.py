bbe552c9a704ea55e04f78fbf9511b17a2de134c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:12 +0000

    unhappy replace shoulder bush firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
