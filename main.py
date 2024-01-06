e1ed43fb26e8405759d72ee58c4c116a93127885
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:08:54 +0000

    drawing pay beside hollow bored

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
