09903f519540d201cec8bfe4428c98db804fda7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:50:15 +0000

    report terrible sweet flavour black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
