eff24994e304590548dbf3f3b7e45d1075179bd2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:50:22 +0000

    gate heavy then tight conversation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
