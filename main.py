1bc07b307b60f82d581e2f5b566b79005491f0c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:03:07 +0000

    damage receive Ww Ll town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
