612003c2bba235012193868486f456d0d917c7b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:07:20 +0000

    experiment neither check middle fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
