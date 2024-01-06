0b3a8b22b1323489d20314e71b655afb8f94d9df
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:13:17 +0000

    move basic disapprove float goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
