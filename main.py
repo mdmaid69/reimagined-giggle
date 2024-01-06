c3bed94be27d713be929c69e8571ef90df78b636
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:42:37 +0000

    weigh admire minute big contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+for i in range(5):
+        print(i)
