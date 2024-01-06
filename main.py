f75bef6f490b79632f11000f42aed5ba802e878c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:31:34 +0000

    powerful limit mystery apart bringdown

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def convert_to_binary(n):
+        return bin(n)
