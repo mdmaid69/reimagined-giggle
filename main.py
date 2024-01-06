737afeb5951d563af27a3380c6c4b747f0368508
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:27:29 +0000

    accident problem prevent throughout arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
