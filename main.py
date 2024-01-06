0d0b6348f1f57b810f0aa7a34567841db1c13ca9
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:47:49 +0000

    harm mend shelter intend claim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
