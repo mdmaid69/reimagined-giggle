ed33a5229ff6b4d7325af1939bb26cbd28bf8c6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:16:16 +0000

    culture stage protect Kk name

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
