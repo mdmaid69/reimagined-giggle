1a2bf9f751eda29624788df9f04453f45685eb38
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:36:01 +0000

    scrap pollution generally earn Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
