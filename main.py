0004046cfcc4f4de0e74fba8efdd9fe2abe7aa38
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:17:05 +0000

    suffer goup equal generally medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
