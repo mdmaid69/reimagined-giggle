a88cc0d966024d4791d571507b58f93d20fdbe12
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:05:47 +0000

    community compare brown used contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
