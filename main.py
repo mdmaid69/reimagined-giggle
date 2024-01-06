2c2357d21013b0cb0184ae14c55c04c6a159aef2
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:44:21 +0000

    free century question trial correction

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def cube_number(x):
+        return x**3
