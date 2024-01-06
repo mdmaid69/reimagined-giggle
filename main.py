da980f9fc0a711a5e3687d96a41543647fcef65f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:07:55 +0000

    share control interested shelter approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
