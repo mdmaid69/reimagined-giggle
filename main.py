ff61498bbcf81d9181f7cc537bb7ab280d87c8ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:58:14 +0000

    tongue certain follow area beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
