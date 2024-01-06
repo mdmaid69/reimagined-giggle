07677bd6bc584ec78e3321795796d8d009678486
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:09:02 +0000

    bridge beat waste education bottom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
