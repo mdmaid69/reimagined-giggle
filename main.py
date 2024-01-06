b4ef339d03f1c82b3c1b389703b3c545b4e19034
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:54:18 +0000

    become section of coffee fence

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import tensorflow as tf
-print(tf.__version__)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
