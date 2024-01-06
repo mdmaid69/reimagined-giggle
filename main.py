fe642e150865ee1f657c6f3420d3c34ac76db719
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:47 +0000

    achievement major goout disagree bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
