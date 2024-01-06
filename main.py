f3c12d15e49c679284115acc62bf58793996c3ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:00:34 +0000

    maintain smell angry race violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
