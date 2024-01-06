5b398aae4ff9c4c5c022fb8f9aee147a9827c9ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:48:32 +0000

    book pattern water tool play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
+import sys
+def exit_program():
+        sys.exit()
