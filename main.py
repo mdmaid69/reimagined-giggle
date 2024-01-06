31d7a4ef6909c163ab0814e45305242fa6ba06b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:33:54 +0000

    declare bin town bear near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
