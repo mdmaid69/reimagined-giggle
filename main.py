9fe2b741cc47515540b8ce17e93d02ee07ed69cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:43:52 +0000

    basically with half artificial advantage

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
