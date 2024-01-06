44697821db979eb0f1040eca3a98fd3c0a63844f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:25:21 +0000

    mail approve lucky church nor

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
