6216520ff28a03a06af7e6029adcbf91f6e4ab8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:57 +0000

    ours Oo anyone I fact

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_floor(x):
+        return math.floor(x)
