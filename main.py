5fd86a053fc987f3c799c16465d43e3cf3856211
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:22 +0000

    them support best nice settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
