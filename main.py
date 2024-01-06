5e69401a87127619a1cb7eb45ff0314258d38a71
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:55:56 +0000

    beat later dig slice switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import sys
+  def get_python_version():
+        return sys.version
