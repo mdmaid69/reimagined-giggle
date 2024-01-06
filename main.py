0e4e5edd6f08d116b256a0ca0adde201793a592a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:51:58 +0000

    public house giveback taste passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
