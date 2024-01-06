68a74235367bf288e207b9ce237f2081e8b31eea
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:04:34 +0000

    ignore professional declare failure sign

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
