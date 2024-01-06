67ac4380f10d20b83dda4cc3fdba32e7ca39a96d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:31 +0000

    shop space count stranger available

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def square_number(x):
+        return x**2
+def convert_to_binary(n):
+        return bin(n)
