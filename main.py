ac8b57041588c798fb413d35972461590e54e0e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:08:43 +0000

    organization comfortable well seat describe

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
 print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
