9ebc94d489cbede45736986fcd1b489a735b4834
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:52:58 +0000

    matter bringdown middle freeze calloff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
