66cd53278c5f7603438b3701e58662751193cb75
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:32:30 +0000

    somewhere possibility job serve development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
