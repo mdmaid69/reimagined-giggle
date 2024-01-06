77bd3810c33a36c55edd291f7b7ae5f25cbddffe
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:47 +0000

    card arrive limit nigga offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(5):
-        print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+def get_current_time():
+        return time.time()
