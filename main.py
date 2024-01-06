b8db828c804002ee5567a38605314e7758abb849
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:34:53 +0000

    break basic bet drink container

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def square_number(x):
-        return x**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
