ffc211f0705fba147e0beb65ad498e15d55a791f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:56:59 +0000

    reserve involve fact issue young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
