6366aa65bdd9f003d43de004be24ab9d3cb6a510
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:45 +0000

    stretch fat share risk well

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
