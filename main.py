464c459dc5f62fe0d90282cad812f1aed51441b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:03:30 +0000

    health from each yours stamp

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
