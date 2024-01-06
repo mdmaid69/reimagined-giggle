1fc9398055d744bca1f3bbb6ceac6c59708d4178
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:25:03 +0000

    busy teach report improvement advise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def is_even(n):
+        return n % 2 == 0
