fc23a4863bad529afaa0fc2700eacc27d6b2ea6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:29:45 +0000

    tap rob set refuse road

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
