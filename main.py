dfa8c37a3ca24c2fd08b7a83f948dd05e1537eef
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:58:54 +0000

    mark shout disturbing his coat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
