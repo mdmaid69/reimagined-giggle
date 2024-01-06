a4659ec453785fc636095ac8df4fa718b19a9f01
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:34:10 +0000

    left need ahead project usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
