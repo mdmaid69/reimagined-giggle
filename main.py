a4a60c6669049bb222a667ab4ba1898c33768eb7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:32:18 +0000

    do centre probably deliberately change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_list_from_array(array):
+        return array.tolist()
