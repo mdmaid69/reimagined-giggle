c09645eea215d90286de4c4c6e127d0324bf5a7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:45:07 +0000

    difference understand guide wipe thin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def is_even(n):
-        return n % 2 == 0
+print([x**2 for x in range(10)])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
