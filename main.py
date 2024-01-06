2f26b88d8ae0419f2d348c1ea26ed4c679289507
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:54 +0000

    limit free clear common comeround

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def multiply_numbers(x, y):
-        return x * y
+print("Hello, world!")
+print(sum(range(10)))
