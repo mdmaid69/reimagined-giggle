ab3f1e93acdd7c19d59b46cdb3290f496054f3e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:00:36 +0000

    squash active pot emergency squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def is_palindrome(s):
-        return s == s[::-1]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
