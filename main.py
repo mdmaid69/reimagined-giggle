b6abe666a9bf5d0cf907c653202555112c1d190d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:13:31 +0000

    inform happy feeling step in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
