849301baa7b4c74b3fbec3cf1d06a35665df4cb7
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:11:32 +0000

    arrange home polite belong breath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
