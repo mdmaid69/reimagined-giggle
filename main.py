e8cae79c053cdbcf68538040305bf279427e964a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:53:02 +0000

    scale explain underneath string rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_queue():
-        return collections.deque()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
