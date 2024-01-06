0fd7d41c6daec146058fc338340a84a002e83628
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:04:37 +0000

    reasonable size square bar much

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
