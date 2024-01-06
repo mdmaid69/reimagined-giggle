6922deffc75a0883547d93fc798507a3e449a24e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:09:53 +0000

    declare workon purpose raw fun

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print("Hello, world!")
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
