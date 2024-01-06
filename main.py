3572460e0e9f67ac539aa9aa72d4ec40d5befaf2
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:00:22 +0000

    stage extremely book hello apply

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
