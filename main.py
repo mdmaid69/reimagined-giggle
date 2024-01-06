dbfaf6b5063b15d816a6cec2097799b7c17e90bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:06:27 +0000

    holdout give surprise kick official

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def remove_duplicates(lst):
+        return list(set(lst))
