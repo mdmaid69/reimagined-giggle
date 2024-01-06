21aecf8ded72f0f79c0ec753d3a16ddfb73f34cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:34:31 +0000

    original sea borrow I fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
