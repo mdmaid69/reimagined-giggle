d7c5c16af7d3c9e069f32e32d761449e9cbe201e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:49:08 +0000

    need steer night watch factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_density(mass, volume):
+        return mass / volume
