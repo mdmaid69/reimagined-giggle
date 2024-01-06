f4e71253cc748c3441b0df11c6a65f134f7fc187
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:04:05 +0000

    relationship debt last genuine unite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_max(numbers):
+        return max(numbers)
