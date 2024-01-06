560a14b9de20ef1d13ffd294e4083593e9d8e753
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:34:59 +0000

    duty equipment maximum enthusiastic rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-print(os.getcwd())
+def is_even(n):
+        return n % 2 == 0
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
