72a56ccfbde516b03e3a6a0c4898ba364fcea1c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:32:16 +0000

    for buy circle table single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import time
-def get_current_time():
-        return time.ctime()
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
