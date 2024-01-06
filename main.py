3cc06849d6a4b7bcf1412b9cdf607919492d2286
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:29:24 +0000

    little journey mother send bringback

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
