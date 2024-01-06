5f9492af0419323fe2a6d4027d2404779b26e5c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:57:47 +0000

    create minor else back silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
