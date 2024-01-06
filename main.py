3ce3f0c60e4e15da0613d8f79784203756baff28
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:22 +0000

    thanks trade OKorokay comein our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
