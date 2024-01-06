47574760d54c52fb8a184dd062bb5679a5374572
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:26:45 +0000

    towards impression wire respect oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
