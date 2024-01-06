41265a2dadb11339b3ca669bf6819e9a645a55b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:14:37 +0000

    past window pay into realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+def calculate_acceleration(speed, time):
+        return speed / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
