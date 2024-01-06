6ce29c3e0a4f7d936d3c6cf510f1825482a3badd
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:06:51 +0000

    cheque taste quit know trip

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def calculate_area_rectangle(l, w):
+        return l * w
