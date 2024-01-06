e2928918b7c141db30e3f50217d33a686a66bc47
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:17:25 +0000

    bet figure rank main powerful

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
