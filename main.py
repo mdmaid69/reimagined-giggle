18844bd7754cc33127dfa0329956b8b80ed810e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:39:24 +0000

    message holdup brother life replace

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
