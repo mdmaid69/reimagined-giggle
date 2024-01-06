b5c24fae8cf000105ac7dcc49f863e1e75ed4076
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:02:22 +0000

    boil nose next complete small

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_hex(n):
+        return hex(n)
