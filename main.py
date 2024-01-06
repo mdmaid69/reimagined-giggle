5501395a19b75331aef2cb0773dc7df0b0c39593
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:43:45 +0000

    achieve successful want seat design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
