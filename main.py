8dcabf24599f3999f36161759bd8723dc773460d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:38:13 +0000

    direct authority in good platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
