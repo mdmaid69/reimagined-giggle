7b115f8fb655e5235ba6344be3d2f8b762152fb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:28:35 +0000

    debt kick underneath everybody border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
