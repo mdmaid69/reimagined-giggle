5124de33cc1e9d0b5b3668160d758b4cae385adf
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:17:38 +0000

    interview plain their ought wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
