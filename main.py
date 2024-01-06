ae68b96aedd3c410085b4abea346555ea450e265
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:16:02 +0000

    tour goup introduce luck wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+print(time.time())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
