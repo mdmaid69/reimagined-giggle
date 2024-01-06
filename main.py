3a0c4079241f61acf0d4090ef886223871144e51
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:27:25 +0000

    getaway modern seem remain test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def convert_to_hex(n):
+        return hex(n)
