493e1e91ebbc1afa1a1b7b2c765f83613a1b4cbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:27:56 +0000

    snow makeinto extra equal bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
