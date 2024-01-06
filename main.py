839de68b3bc2df7edd5dedf09280ab79ca99e126
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:28:39 +0000

    behaviour dark community again important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
