ca62554b5ceea9aa77e205d8564367bcd3190b9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:23 +0000

    say crash blow noise safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
