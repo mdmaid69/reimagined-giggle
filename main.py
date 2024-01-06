c774b71a001c1bb84525e3881cdc84518b5bc1e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:56:33 +0000

    clearly comment save fond rest

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  def remove_duplicates(lst):
+        return list(set(lst))
