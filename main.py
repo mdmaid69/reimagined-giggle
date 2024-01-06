054f632ed5aeb71665af66bc0a3fca7b39e011be
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:48:35 +0000

    petrol anybody mind how punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
