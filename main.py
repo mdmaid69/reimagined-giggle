7ddd66d208345adff8170dfeed9b873585379957
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:45 +0000

    upstairs back delicate instant cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def cube_number(x):
+        return x**3
