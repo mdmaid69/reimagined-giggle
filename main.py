56545d8a089f452880b4b7b89ff71f2d77bb25cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:48:08 +0000

    mad consult test face police

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
