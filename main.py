df7a270fdddf6f0ca74b156db61818ee02a97af5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:29 +0000

    in will they badly sting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
