523e0a28366d0502ce64a2769df5518e2fdae26a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:21:36 +0000

    typical store usually ours Zz

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_exponential(x):
+        return math.exp(x)
