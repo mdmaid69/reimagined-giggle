45f20b7e3f7cb087418be264582f45f0b3a90156
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:12:07 +0000

    spot better talk meet breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import random
+def roll_die():
+        return random.randint(1, 6)
