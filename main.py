04748b51fd39e9e69fa36c3cad6fc0e5b5e6d896
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:11:34 +0000

    basic tragedy responsibility uncertain stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
