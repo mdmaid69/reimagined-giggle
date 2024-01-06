4ee2d8c79a82016000f5b74e1a94caabcca43cc2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:26:16 +0000

    generally skirt pipe telephone can

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import sys
+def print_python_version():
+        print(sys.version)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
