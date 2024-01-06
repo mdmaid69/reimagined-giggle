0394c1e58e8f18476dfffde5bad3ecf9f3b02792
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:49:47 +0000

    ill protect force profit regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
