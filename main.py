72f680a73a07ea83be0e69effec72ae40a57209a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:22:38 +0000

    violent desperate earn own study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
