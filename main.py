c0deb16b1b95fa57c3403c3d874e4130394f6394
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:34:48 +0000

    crash past react represent announce

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
