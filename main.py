529ffbe37dc7efc9e22c39027a62cce7278cd85a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:21:24 +0000

    ask either summer wash steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def remove_duplicates(lst):
+        return list(set(lst))
