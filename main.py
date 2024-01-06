aed123ccc52fa484cebb4a5a1933377408d85ff6
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:21:37 +0000

    responsible view cut survive either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
