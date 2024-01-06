5113d1063ac482ca9568b1c644a10a73457c6b33
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:33:41 +0000

    guide visit fierce criticize popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
