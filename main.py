de9128d9518871c7cef3caea17988abc04680a42
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:50 +0000

    present early emphasize 568energy patience

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
