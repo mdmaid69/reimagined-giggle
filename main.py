ceacf2643d607b7981b57bed8bc21eae69034b0c
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:32:53 +0000

    chance interesting chance truth survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import time
-print(time.time())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
