4a12b1c721ec393aeccb6bae608ee8b68c8fe039
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:02:48 +0000

    chief minute schedule murder result

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def remove_duplicates(lst):
+        return list(set(lst))
