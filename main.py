1b7cf942cc5262dc6d5a7abd64cf329a5de36895
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:38:14 +0000

    discovery late giveout drink rain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_index(array, item):
+        return array.index(item)
