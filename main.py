ed6a4875a38442b2ecea5dc19297d4f785f2589c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:03:51 +0000

    wander later doctor cruel prize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
