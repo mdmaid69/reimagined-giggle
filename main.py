1b794fb98af64fda24792b05ff2106832c36f3e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:42:50 +0000

    read change story healthy edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
