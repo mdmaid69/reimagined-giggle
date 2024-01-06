0f83e49904d20d3d48e3ca0c4eee8a2ff0206c07
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:22:19 +0000

    copy close exact warn reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
