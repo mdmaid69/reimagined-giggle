2406cd35449f769be2afa043ada9cd8373730a4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:57:59 +0000

    part scheme bend afterwards pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
