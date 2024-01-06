a20c15704c3660349de1e1e5603557ace2ebcc7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:56:49 +0000

    thankyou opposite round say answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
