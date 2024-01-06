55ddfe0f310c2e90e5b33b7a94e2a08ef9ccc07c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:07:09 +0000

    recognize of ring progress smell

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
