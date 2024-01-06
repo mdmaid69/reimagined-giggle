c8d756ada055672f25b2b4c9b7889ce9f6bc1670
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:45:01 +0000

    Kk fit silent thanks talk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
