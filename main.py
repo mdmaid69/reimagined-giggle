f43a02a7450f5d24b2ccf5fc1ac23c6ec39fc004
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:15:29 +0000

    smart fine path violent forward

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
