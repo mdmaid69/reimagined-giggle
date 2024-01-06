d20eb3c10040b7fce50d9ffb6f9646ac15da86db
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:30:28 +0000

    behaviour fit Pp operate path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
