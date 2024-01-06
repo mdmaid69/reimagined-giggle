885c3f11016d55f471569b187ec2b4334d802104
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:30:10 +0000

    early mark standard later university

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def get_array_itemsize(array):
+        return array.itemsize
+for i in range(10): print(i)
