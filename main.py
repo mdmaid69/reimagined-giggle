f63ee7b74c48e249525ce04301f1fa5880da889f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:40:53 +0000

    transparent illness guard science treat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
