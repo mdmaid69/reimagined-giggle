19a105105c524d3d46eb19a8aad9bbc98fc70562
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:52:14 +0000

    573entertainment trousers harm talkoutof break

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def reverse_array(array):
+        array.reverse()
