b2c48b31ba91ddd2f73645b447fe1667e51703e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:55:49 +0000

    fetch cook dick bringdown gate

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
