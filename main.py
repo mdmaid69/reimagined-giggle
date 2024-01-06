fd95efea0958c3f3db26f56ee4689c1166c8af60
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:27:25 +0000

    kill grade scale situation thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
