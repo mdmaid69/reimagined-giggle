cb51c69e5d0c7159c2a9b68c871cbd449f34bc61
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:57:38 +0000

    flat Yy cruel rare ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
