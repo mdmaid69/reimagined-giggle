a5e9d127c8e4d42a886d678c843d82c4ee87f007
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:39:53 +0000

    grand bored find catch excuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
