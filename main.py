3cdb2ceba287ef861ed8bf68112e37ac81cfcc64
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:23:11 +0000

    listen beside one Ww greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
