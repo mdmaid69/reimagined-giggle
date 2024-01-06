55baa1846b213002064dd8cd519857743c790fb5
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:16:53 +0000

    everyone tour mistake beyond clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
