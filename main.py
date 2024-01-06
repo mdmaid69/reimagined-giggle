dbae234cabfd2d141baf3e5e3aba7b532671599c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:19:38 +0000

    base organize find attempt sleep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
