6365241d54ce2d903f4b190f4f7a635b20c2a07d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:18:22 +0000

    plain Ww even figure provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_even(n):
-        return n % 2 == 0
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
