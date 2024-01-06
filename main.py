59c19630e79165ab728a61d60049083c0bc5e45f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:49:43 +0000

    whistle sister trick introduce goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
