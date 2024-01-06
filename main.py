2b2259aaa547d587e16f9fd74d32591c436916c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:13:05 +0000

    cap 570enjoyable positive probably ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
