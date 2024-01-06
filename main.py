75b43400c2a5cbdd34cef7f20ecd2ba0b237b12b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:46:55 +0000

    hang illness burn force advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import numpy as np
+print(np.array([1, 2, 3]))
