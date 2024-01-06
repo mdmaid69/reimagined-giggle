f3912da9c045adf7bff440e6dc8e1fe5ac8eee67
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:46:47 +0000

    left zero software involve easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
