a4a216371075c6c3068eca121d86904768f5e7a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:32:38 +0000

    stop deliberate trade closed direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.time()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
