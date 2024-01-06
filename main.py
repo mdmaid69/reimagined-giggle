1111d75af7dff21edc0b1fae2e9c04aa78378ecb
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:37:22 +0000

    software hurt business before move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
