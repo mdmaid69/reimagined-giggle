074e42f80f024e16859f6c258328ba147df7b802
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:29:18 +0000

    arrange speak praise talkback trap

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
