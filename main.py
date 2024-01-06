ce9ce89f71d8dabdb445a3a24340d9058974faeb
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:44:36 +0000

    character will afternoon fond Mm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
