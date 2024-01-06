e8931f4855e83ddbafbcfa3a6a6eb25e096a6e1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:43 +0000

    organization flavour attractive like shock

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
