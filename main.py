1b54c02727ad8ea6743bdb332c3fcf9e49d8a925
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:06:14 +0000

    favour nice task trick today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
