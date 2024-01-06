cc014014d789346ee23f4d647f310fa66c52df3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:42:05 +0000

    makeup let happen explanation across

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def square_number(x):
-        return x**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
