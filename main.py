30a0c911f74833e71553eae632564132400144fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:57:04 +0000

    pattern geton disgusting correct journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
