3fb9a511ccae572b49a1873ccdb1ee7df7402882
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:12:28 +0000

    waste shout group lift takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
