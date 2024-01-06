843726acd4ff6059f5fab64c3dacda3265871f2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:52:03 +0000

    frame drink shallow punish average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
