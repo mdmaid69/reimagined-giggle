2a19413e1f5ae9632d9ea0db412739e70fa99700
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:28:15 +0000

    noisy brush again previous brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
