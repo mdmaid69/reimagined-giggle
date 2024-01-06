34b3da06de4dbccdfd741e340cdb032f4361f37e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:30 +0000

    push question film song now

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_density(mass, volume):
-        return mass / volume
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
