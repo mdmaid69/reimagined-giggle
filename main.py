ad59b0981ea80c8c35500811057964d3e0324d62
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:15:23 +0000

    string sharp smart right side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
