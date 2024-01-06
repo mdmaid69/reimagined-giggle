5115623a6b0a8f74d75642e7173f26f3a743956e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:22:16 +0000

    wear attach sore sail runover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
