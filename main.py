d0981334b0a88f10cfca99d9c4f15777f92c0ea3
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:50:45 +0000

    beat have holdout content software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
