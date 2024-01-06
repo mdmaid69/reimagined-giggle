47c9335e692bb8eab11274a67640d788e9f4b163
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:57:33 +0000

    home pipe instruction away only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
