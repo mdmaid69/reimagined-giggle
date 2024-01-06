ad2f3938b1ff2d652bf3ca8c505757baac1e961f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:55:16 +0000

    introduce bin freedom active much

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import re
-def find_pattern(pattern, string):
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import re
+  def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
