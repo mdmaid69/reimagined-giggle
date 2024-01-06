64ffe5071cad729346a3c85f5ee3aa084934d73f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:33:22 +0000

    flavour behaviour interview there reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
