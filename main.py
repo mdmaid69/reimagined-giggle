226fab311a7930df360549ddeced3ba050b9f577
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:24:28 +0000

    opposite popular fly ignore neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
