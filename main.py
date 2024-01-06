d8c63a43bbebf4e77db8af952b73044a4d6a7607
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:47:36 +0000

    cope home speak act fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_work(force, distance):
+        return force * distance
