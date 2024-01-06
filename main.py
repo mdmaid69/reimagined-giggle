6cea03f909fd841c17998eeee226ba8400034b15
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:01:44 +0000

    straight band nothing possession pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
