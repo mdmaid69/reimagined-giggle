859450c1bdfcea098bde10057d144f5cd7126fe8
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:51:32 +0000

    platform suppose rise shame runaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_work(force, distance):
+        return force * distance
