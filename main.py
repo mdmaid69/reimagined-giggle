09ed43b4c9a4ad9ecb3840fd206bcace162776ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:48:13 +0000

    yours deep breakin different police

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
