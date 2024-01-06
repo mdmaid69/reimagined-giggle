b952ef483133738a4b114e12099785b5283e49eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:51:42 +0000

    his boil cover interfere right

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_even(n):
+        return n % 2 == 0
+def reverse_string(s):
+        return s[::-1]
