8a147c0bfb6be4fa6868e1ac8b45f00d81454711
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:25:36 +0000

    calmly comfort pretty house know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
