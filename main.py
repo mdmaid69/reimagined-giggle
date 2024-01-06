c5d6b23bc8681022724ef645cefcf33e732d611e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:01:05 +0000

    dark 569enjoy coffee race however

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
