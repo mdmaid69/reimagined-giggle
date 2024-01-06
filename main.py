9a57bf546d62dbdab47a3a0b4b66a846ed6dd455
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:07:33 +0000

    cause law lay air live

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
