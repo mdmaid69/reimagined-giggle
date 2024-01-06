fa184c81fbac70722cd0047f80b8e00fcd68756a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:41:44 +0000

    challenge vision excuse slide fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
