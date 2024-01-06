ae680e664df304535fed876ebab4f0a45d1c3098
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:38:42 +0000

    maybe cut field manage heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
