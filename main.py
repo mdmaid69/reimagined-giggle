259fd09162671ad366fd2a3eed9b1a23b3e4dc96
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:08:51 +0000

    needle worth cheat mad post

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
