0b28ced920dd6fc0fe63d7546832fce2d2d5d1bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:55:26 +0000

    what case figure false test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
