10e7d3d7c005411f702195dc46178bceb528dba7
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:13:20 +0000

    likely walk learn innocent search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
