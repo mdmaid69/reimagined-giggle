057a4dfad4c4b72bb777638114d76c3e2f4d388f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:01:03 +0000

    sort defeat substance clear wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
