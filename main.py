68d1e0377b53edff9580273670f3ce76e23fb05a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:22:19 +0000

    punishment head sharp forward breakinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
