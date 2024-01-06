482a97daef6c718f9efc5d5f80d61227a057aa17
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:22:42 +0000

    into discover fierce ordinary Yy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
