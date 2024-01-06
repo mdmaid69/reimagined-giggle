e3feb2d84c2a9683b9c7b28c77123cebaeee901e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:10:00 +0000

    base convenient funny performance arrival

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
