bfe8330d1b0787a27ebfba3440edc5206cfdb53c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:19:03 +0000

    everywhere classroom carry thirsty brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
