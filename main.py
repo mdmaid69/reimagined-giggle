ac9c7b5a6b9b48ae083688a52f0329beaaeeffa4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:10:39 +0000

    turnback alive afternoon brush sometimes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
