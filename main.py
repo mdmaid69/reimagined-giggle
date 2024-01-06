cbd1576ed2e5dee85fd710b20c64393ef286ed41
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:07:59 +0000

    load battery arrangement ear comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
