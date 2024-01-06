7f8b59560a0c6cb9cd5a5c9daca5b741c6cfe2e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:41:13 +0000

    couple respect sink succeed up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
