f6d304b3e377a02f9b923ceecf88bfda560873d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:18 +0000

    hungry suck borrow brave succeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
