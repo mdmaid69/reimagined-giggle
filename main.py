1f2cce6693e547aba216cb58bfebc57ad52f5e11
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:11:43 +0000

    interview safe holiday natural trust

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
