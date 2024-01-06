de2eb066a7233f5881798c9fdb9487308f2d16ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:08:35 +0000

    negative freeze why meaning copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
