6dce5624fe167111a4040f77411481d5f70e98af
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:31 +0000

    visit hurt far blame repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
