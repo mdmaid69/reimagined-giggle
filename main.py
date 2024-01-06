ee7fca951efb5e44afc63413481a5c107552dfbe
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:07:36 +0000

    illness port ever police bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
