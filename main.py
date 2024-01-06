f9d08fa6dd9f3870c5e9d7dc2ddb2703d8824d87
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:46:18 +0000

    prefer escape satisfied side lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
