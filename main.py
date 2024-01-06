805a0201b41bfc293d8db714ab9634c70e330637
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:56:12 +0000

    slice tidy forward phone content

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
