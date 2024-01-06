55a95a4dd0f917343eff331f2309b84ca41a81ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:55:55 +0000

    able doubt but heat possession

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
