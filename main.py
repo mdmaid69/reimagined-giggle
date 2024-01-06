a1cc2c13b61053dc3923883c27da6f959e524233
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:01:19 +0000

    shoot with high profit capital

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
