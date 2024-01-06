d32f520563d4ee3173f391103cfa6b828584a4a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:50:32 +0000

    how no guilty nowhere raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
