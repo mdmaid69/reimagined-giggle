ac40b7c73c471ce8f110f40f855420d62d70a5ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:45:52 +0000

    feeling sore during conduct food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
