33ac6a594f4ecc0100431a8fc69bd141e88f0b73
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:40 +0000

    public throw shall bye door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
