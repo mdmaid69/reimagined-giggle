007f06f2f0ba2c035904e0c8765dfd81aebb12c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:34:11 +0000

    equal behaviour gift exactly come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_min(lst):
-        return min(lst)
+import datetime
+print(datetime.datetime.now())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
