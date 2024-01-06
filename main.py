d490652349f6565c44a438041efa8ceffbf92b37
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:17 +0000

    activity best roll problem strike

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def add_numbers(a, b):
+        return a + b
+import math
+def calculate_sine(x):
+        return math.sin(x)
