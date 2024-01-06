d8e447a9c639febfbf763fcad5d400b2872f4356
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:19:29 +0000

    tradition smoke bend part nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
