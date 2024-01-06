fce7fe696d6d2bea439df76036c82c4b1ee8d0d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:47:17 +0000

    unlucky comein land journey go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
