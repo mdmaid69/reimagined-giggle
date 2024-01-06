0fe481a6b54cea789c337953ecfb5c0c7982c068
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:35:18 +0000

    trial rent convincing completely murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def greet(name):
+        print(f"Hello, {name}!")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
