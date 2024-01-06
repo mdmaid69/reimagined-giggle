a63feac83ad10baa1e4fb9413a215c881d32c20e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:22:52 +0000

    sell thinkof line wonder touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
