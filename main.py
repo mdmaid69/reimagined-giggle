3fd3ef571ebcf3eb37344fee08d47c4f2de703ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:46:10 +0000

    regret vanish breakout lucky class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_ceiling(x):
+        return math.ceil(x)
