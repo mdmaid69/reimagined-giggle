768a9cf46d29ac48e25370e9724891e06f38d903
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:40:33 +0000

    scrape apparently callout instrument never

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
