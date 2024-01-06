8ecfc6e107be51165c42ee3fafb6a717318e450f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:07:19 +0000

    recently attempt succeed tour present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
