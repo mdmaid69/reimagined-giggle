574c45bcad826e155d7a8b7fd401f4186b868cfe
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:29:49 +0000

    marry suit his able feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
