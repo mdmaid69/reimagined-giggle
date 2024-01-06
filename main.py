474e961f7772f641f4e49a181b3ba71df85a9895
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:26:44 +0000

    bang turnaway convince opinion reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
