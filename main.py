8d7997c3f72731b365c66d1c67c6760162b6bae3
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:33:01 +0000

    loose knock emphasize hang used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_repr(array):
+        return repr(array)
