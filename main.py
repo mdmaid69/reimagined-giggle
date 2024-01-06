034154e1e86758e9b0b2352f811286026f44821b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:20:01 +0000

    embarrassing start create slide schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
