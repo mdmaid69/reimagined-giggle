3c61f06cf3b4457626eba5ac529e8a6ac7f9945a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:11:48 +0000

    study pronunciation limit thinkover later

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
