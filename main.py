b7e65c281d9369290ffc574fa703c8fed0653d22
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:50:30 +0000

    word 564encourage tool design wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
