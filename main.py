08ef664fabd832fc51a612796756baabe78a1740
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:55:07 +0000

    comeover scene knowledge land fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
