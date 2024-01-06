50eac1470f32c62c817f856a6acb68d2409e4ccf
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:16:40 +0000

    standard either slight little shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
