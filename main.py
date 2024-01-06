a8e0f6a134901429f19f388f1c9e16d39e870306
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:04 +0000

    shock proper before Ii level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
