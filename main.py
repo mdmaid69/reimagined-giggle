d0b37a944a0ae0d554c717e0b80d6edd6a5ece4b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:59:14 +0000

    afterwards murder gooff politics charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
