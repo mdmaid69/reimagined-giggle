4158e78aa149c418e987e8d1f6d2d1a8303fce01
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:07:14 +0000

    succeed century certainly congratulate collect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+print(sys.version)
