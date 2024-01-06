f1dc5fb7c03f2bed5ddb740ef74964a4948e77b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:00:04 +0000

    condition advice nervous week decision

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
