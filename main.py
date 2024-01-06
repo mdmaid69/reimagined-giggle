4bced81ad0d7d808dc13c920feb2b62e631cc1f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:00:30 +0000

    shall bush wherever consist nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
