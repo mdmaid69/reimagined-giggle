fd33969255541c247afd5916d15f311a25328ccc
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:24:47 +0000

    mistake battery tooth burn dull

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
+  def convert_to_binary(n):
+        return bin(n)
