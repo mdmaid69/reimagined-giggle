6590ef8862f5c9f0afaca6a98f6ce1142910de28
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:38:24 +0000

    advice favourite fairly teach unpleasant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import tensorflow as tf
+print(tf.__version__)
