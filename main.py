0acc4a603f89eb8b869279eacc19cada237e18fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:06 +0000

    wander interested pipe round popular

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+print(sum(range(10)))
