0f49ee7b9998a4d932ff9b7daba0f4e31cf464fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:49:18 +0000

    certain appearance realize cause thin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
