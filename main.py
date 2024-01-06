960a898f6ef5e0ee90413348460a62fd9207b15f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:27:42 +0000

    interfere glass Ll recognize fortunately

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
