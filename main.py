218a6ffc3bf9d361bdf34e2c4685fec9e13524bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:26 +0000

    loose perfect remain big content

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import platform
+def get_python_version():
+        return platform.python_version()
 import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
