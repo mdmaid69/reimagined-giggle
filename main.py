4b361245f5e898a4e255d76785eafaa22269182a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:19:52 +0000

    work Ii touch Zz he

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
