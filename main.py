7757a559375d2ad33d5a94d217842850ef7cdd7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:20:52 +0000

    legal pass neck idea score

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
