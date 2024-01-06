00e50807c4e7155a1cea6cce46f9af750c8af5ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:36:19 +0000

    year bringdown chief communication search

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ def create_thread(target):
         thread = threading.Thread(target=target)
         thread.start()
         return thread
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
