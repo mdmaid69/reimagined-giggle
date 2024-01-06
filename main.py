884b0d316be481a5abd514d971b7020d25347b4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:54:41 +0000

    meeting please pen post behind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
