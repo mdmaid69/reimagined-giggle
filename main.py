4197357433fb2b9042c1003aec39eb12752b97ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:53:33 +0000

    underneath several memory represent present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
