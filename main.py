27dc65ce41f21720631c304888a95069c603e515
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:04:41 +0000

    wherever free near job dead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_list(array):
+        return list(array)
