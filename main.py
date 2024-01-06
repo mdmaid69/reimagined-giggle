a01c69c31f0f5cfbb534745ba1975460c3e89213
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:50:50 +0000

    section church circle sensitive pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
