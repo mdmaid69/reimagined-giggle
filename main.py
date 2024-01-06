3f0a4ad550af71bc5f5238249c1e0f2c3067c409
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:10:20 +0000

    pleasure apart stir excellent treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def add_numbers(x, y):
+        return x + y
