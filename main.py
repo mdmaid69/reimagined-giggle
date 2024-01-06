1c86adf3875d39c3af8914822839fdbe45c4cce6
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:17:10 +0000

    band climb 568energy switch Tt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
 import math
-print(math.pi)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
