facffba8708e36f02deb53b4b4cf15ae1b7a453b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:49:55 +0000

    emphasize confused character ashamed left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
