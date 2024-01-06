9780eeadd7956b579e4af0bf5d38063ca368276f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:02:40 +0000

    hold slow runover may post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
