def591637b2978ae1d04152d9457d274e4d5ba28
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:14:09 +0000

    damage experience herself expensive recommend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
