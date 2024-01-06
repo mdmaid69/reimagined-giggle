eb3d7339fe1fab8c6be3599095755b64f6437fa5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:25:35 +0000

    fault praise interview lift damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
