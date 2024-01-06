921d7b9d9d9d255d9f76332d96aeede10b41cd8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:03:38 +0000

    defend show want consult land

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-for i in range(10): print(i)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
