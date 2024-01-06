8659cd521f899dd5a7d5d3414944f9cdd5f6e004
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:00:43 +0000

    meaning operate ticket smell often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def clear_array(array):
+        array *= 0
