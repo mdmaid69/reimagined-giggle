cce992a0b3336ecc14414a1e7f87b76257d69bf6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:56:24 +0000

    aim free scream brush duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_current_directory():
+        return os.getcwd()
