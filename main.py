a93f1c8cdc681dd70a15ee2344d4601f9334e05d
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:35:21 +0000

    argue place nerve emotion progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
