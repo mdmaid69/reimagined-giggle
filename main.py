9cdc681fa40f8e93348d1558cc188a6cc9f7f7af
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:09:13 +0000

    harm sort get sit prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
