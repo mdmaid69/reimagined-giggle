b5743ed42fa72759704b63dbe253297b85dfd593
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:50 +0000

    each pause there ahead maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_python_version():
+        return platform.python_version()
