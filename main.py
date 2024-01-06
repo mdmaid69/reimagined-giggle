a4c7e59ed693d576f36a5e1aab55785597751c64
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:12:45 +0000

    mine loose clean whom properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
 def find_frequency(sentence):
         from collections import Counter
         return Counter(sentence.split())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
