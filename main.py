78d5dbcc3ae8f8762012d719b8b5606212251b07
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:25:47 +0000

    hate fold emotion dear pollution

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-for i in range(10): print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
