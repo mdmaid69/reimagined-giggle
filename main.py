1c43b85d812bc7839eb4b69c5e8af4b52e89f521
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:01:11 +0000

    pleasant should bottom theirs will

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
