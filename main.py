506dc0aa4a32240fdef09165b040ad17887499a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:04:23 +0000

    present trousers argument claim stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
