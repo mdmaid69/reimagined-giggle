1115bca947f428fd3f16328846c4b4fd638781f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:44:22 +0000

    precise behind burn ability visit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sys
+  def get_python_version():
+        return sys.version
