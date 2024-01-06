77b45dba701d9f35b79038446b73bdc4b6d648a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:59:03 +0000

    below live account distance pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
