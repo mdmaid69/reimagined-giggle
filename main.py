c4f464102088e88efa329c033caf026892940736
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:49:12 +0000

    kill in turnaway exhibition ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
