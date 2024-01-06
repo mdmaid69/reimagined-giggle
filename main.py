d7addfe151f9989316102cd3b78ba913e1154c67
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:29:56 +0000

    slide exactly emergency detail face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
