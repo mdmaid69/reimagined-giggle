cb1674a04be5e982a89f8ca42e035a6f15bae5d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:47:50 +0000

    comfortable boat innocent may speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_list(array):
+        return list(array)
