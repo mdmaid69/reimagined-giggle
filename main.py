3fb56aaec7ede9f2f1059e42148e3dbb4e1579bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:18:55 +0000

    education steer smoke delicate attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
