adcf065c124917f479bc6b9dadf423f8dd1d1acd
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:35 +0000

    shut transport ground basis pull

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
