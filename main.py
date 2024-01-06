9e05e88edd91958f9fd45de5ab6ccff30078ae04
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:46:57 +0000

    positive way later fact praise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
