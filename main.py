89b5e8a046f1aab01b581919bc11e99738f8ef8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:36 +0000

    Vv success separate section round

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def reverse_list(lst):
+        return lst[::-1]
