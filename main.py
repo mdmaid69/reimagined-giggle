fca00a1f103a0cb2011f1eab1fa13ccf9e108c7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:05:05 +0000

    other admire below club Uu

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
