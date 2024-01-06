76b2d50f42b105595530769f405bac555761671a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:01:20 +0000

    turndown raw plenty talkinto block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
