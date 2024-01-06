03473ecb86355924b8523d2c0689f665d527b989
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:08:13 +0000

    offer wind his surface ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_work(force, distance):
-        return force * distance
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
