f6b520f5b0ff8ee0024945ab1945b601608aef06
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:53:04 +0000

    home post allow question once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
