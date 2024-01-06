9ba737432c2de0bbc9e0a049269a5024faf2b756
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:36:18 +0000

    argument might pipe but turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
+  def convert_to_binary(n):
+        return bin(n)
