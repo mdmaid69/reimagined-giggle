b2f5e7564f09979dae9726a55c3d186c7cd4dfcc
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:46:55 +0000

    bit drive seem home whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
