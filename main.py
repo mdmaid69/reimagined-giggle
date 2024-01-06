91ffe45fb3ff5183a13a6423070ae177c819b251
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:49:17 +0000

    judge university wet talkback young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-print(math.pi)
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
