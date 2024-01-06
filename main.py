faff26cdd3f7cf5104929e8789b4e33e855084bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:38:25 +0000

    rule constantly immediate outside find

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_pressure(force, area):
+        return force / area
