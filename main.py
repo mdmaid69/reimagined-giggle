7115f35e6a8d9e4011d051e668e54716984c2ee7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:18:15 +0000

    shoulder whichever well wall roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
