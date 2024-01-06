b47495482b6c5580ff5c354477219b3b49dbcf94
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:32:07 +0000

    chin heart fold open wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
