204233eddcfab85803ab953e167269091f75183c
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:51:01 +0000

    sale set full benefit give

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import tensorflow as tf
+print(tf.__version__)
