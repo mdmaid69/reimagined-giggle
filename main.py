c03dac4ff8bcc1ebe4f5449dcddc8d741cd8a726
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:26:46 +0000

    reduction square deep confident dead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def find_max(lst):
+        return max(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
