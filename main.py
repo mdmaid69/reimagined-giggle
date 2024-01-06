f9315c1acf0c9faa751e6d4ac26a916b782ebcca
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:10:02 +0000

    read comedown uncertain satisfied nonsense

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
