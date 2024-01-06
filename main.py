1e5ba9bfe541b25c2555dd6be11d1f35fbe9b519
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:06:53 +0000

    go fast out paper sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
