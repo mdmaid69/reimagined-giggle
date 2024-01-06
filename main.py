7eab5927594c7e61629dadd5bc909669f0dce0d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:30:52 +0000

    school woman else Rr future

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
