3ca659a5ca903f9ab6a2083f2c422cd48a59c3e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:24:15 +0000

    plain do wrong forgive glass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
