349d6818884ec6ce423591052f9c8b7da75b87c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:22:55 +0000

    then amount joke unkind slow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
