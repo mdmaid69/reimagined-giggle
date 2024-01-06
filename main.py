b0ffeb9000952503b4a3e9817d5b2fb1ebfa1413
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:08:36 +0000

    holdup communication away result satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
