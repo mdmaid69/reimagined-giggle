6a7a459efec98a8f794946d81ba1e018ea7fb139
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:12:17 +0000

    turnout world place post slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
