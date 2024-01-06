9f2d4de72a84eda2eb081e1ba9346611f0398c34
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:51:55 +0000

    chief contain place trip muscle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-import os
-print(os.getcwd())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
