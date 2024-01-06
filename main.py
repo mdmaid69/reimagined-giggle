299b2a093c0b30c583e47a9f5b92d85dc351f5b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:35:35 +0000

    way though project form never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
