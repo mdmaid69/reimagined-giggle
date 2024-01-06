f0dac4cf8457c586d60c2147755c5b4f6dd9e940
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:43 +0000

    thought please below play test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
