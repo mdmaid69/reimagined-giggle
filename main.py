531f76d1b40e484de72ddf18c1d3fc1d47d80cc2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:30:23 +0000

    somebody sink worse sometimes yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
