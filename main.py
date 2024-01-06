1e26763b64e032b65d386e454588ffcf8b77c1a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:41:32 +0000

    day possession commit worth risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
