ef6602d3b884804af91f933c7b007ab6664c5342
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:21:46 +0000

    unfortunate whole target understand wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
