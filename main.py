087382f3d0f3e7ae14feb0d3ff5ccc5cc274df93
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:38:19 +0000

    bin produce sale everywhere correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
