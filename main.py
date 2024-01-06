5d49b0ffbb74ddeef13b1b5b387d6182b2735f19
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:24:19 +0000

    by thankyou hard blind concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
