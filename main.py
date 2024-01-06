10af1c761a41e3fc1251131f9e7639b26492b851
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:57:48 +0000

    pronounce computer eye defend odd

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def cube_number(x):
+        return x**3
