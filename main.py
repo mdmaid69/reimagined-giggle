7e75d9df3c1a5ad0508ebd3533508ed72f6dc145
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:13:59 +0000

    rely workon rub as theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
