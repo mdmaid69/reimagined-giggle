4332dd8396d4a07bb5fb7abab11e43709d80c899
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:49:14 +0000

    company willing stupid tip model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
