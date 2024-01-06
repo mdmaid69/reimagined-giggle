514e4730efb69db9d52013c44ca3854e7b322970
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:45:07 +0000

    run rule measure reasonable hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def sort_numbers(numbers):
+        return sorted(numbers)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
