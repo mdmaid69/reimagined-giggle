e7adb205bbda2ae486762a94262521762cc83812
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:10:31 +0000

    acceptable aim do runover fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
