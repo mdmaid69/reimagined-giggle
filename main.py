336deb4605115f9b1c685e37fc31348415204a94
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:10:29 +0000

    decision breakup marry satisfied to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
