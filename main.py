998b851d5638eb1fda5c4167e3f4f944073ff9e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:04:17 +0000

    comein shape visit bend game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_pressure(force, area):
+        return force / area
