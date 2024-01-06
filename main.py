b375ed45c35ffbb10fda355a7e3340d940ebdede
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:10:10 +0000

    talkoutof especially present clue during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
