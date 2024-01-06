f331fc362a5d1dc93ade786eff456b5ab010e57e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:49:29 +0000

    ahead breakout issue round secret

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
