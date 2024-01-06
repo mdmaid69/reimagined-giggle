862ab76215b6627dd88d3e456471b2c163d5cac8
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:15:21 +0000

    passage nowhere much estimate guide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
