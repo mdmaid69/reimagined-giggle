e80a41a8cd08b199954ea2817936b2c269ac03e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:11:08 +0000

    conversation hold music excuse stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
