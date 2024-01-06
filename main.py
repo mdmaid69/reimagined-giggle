952fa8123af89734f5caead34d591d7d0826528a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:09:32 +0000

    holdup share top avoid attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
