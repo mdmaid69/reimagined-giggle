665b0af4f2f79f2999049f3b73b3e9ee2b75b6d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:58:38 +0000

    ring skin call news home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
