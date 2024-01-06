bec91edae37d23106e4fabb0e9de7e7ebc171e85
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:18:48 +0000

    happy thank search object tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_user_list():
+        return collections.UserList()
