44f06754b8200606b6f37ffcae31c50698b18e82
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:39:24 +0000

    unkind process stranger crazy godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
