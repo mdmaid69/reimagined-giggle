52e8e9339f7867762d1e25738ca0291ab0d22f6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:56:01 +0000

    slice holiday later polite run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
