6feb4931fad63ed7cf55d64f4e8878c715a67b69
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:07:53 +0000

    doctor glass juice abroad shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
