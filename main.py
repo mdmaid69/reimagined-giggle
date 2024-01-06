e06ab02c021f0321d4d7c615fffbbb773d1c98a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:28:56 +0000

    shop visit table thing treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_string(s):
-        return s[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
