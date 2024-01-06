1cb34101278eb3203e003222526062b32081ef63
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:58:40 +0000

    possibly free 568energy Pp admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
