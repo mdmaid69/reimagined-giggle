f4ba26f67493ac060d4161f39737a144fb483a99
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:05:50 +0000

    minority news emotion used direction

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
