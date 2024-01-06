0657dd6dd7f3806efbbbbc9b129311fee51f1b2b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:25:54 +0000

    trip needle turn complete bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def get_array_as_tuple(array):
+        return tuple(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
