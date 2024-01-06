6c7a8c2cfe43345c269c632e88d80b8af49f607d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:54 +0000

    plan introduction suggestion only will

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
