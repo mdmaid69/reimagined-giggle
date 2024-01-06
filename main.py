5a29f2357ccb241fce8a2b34e9908b707f10b73d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:16:24 +0000

    minimum nearly joke scream play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_set(array):
+        return set(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
