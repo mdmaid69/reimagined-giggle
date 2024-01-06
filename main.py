7be163345d0edb5b03188987fee2c1593a5b6f85
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:39:39 +0000

    actually find high succeed tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
