be5e4e8b3104366e4c9c3224e3fa68f6300b984c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:23 +0000

    shut calmly borrow fire object

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import sys
+def print_python_version():
+        print(sys.version)
