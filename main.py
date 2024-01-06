c670e0d9ddd6171141d9af54a57ec1cdc5bb690e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:15:18 +0000

    weigh fast drive piece furniture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
