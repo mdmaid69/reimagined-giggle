50ab3d69fd88253a9d7d0ef00caeb2d634492c7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:31:10 +0000

    basis crowded hurt light mistake

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
