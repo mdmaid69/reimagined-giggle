0f08ea18caa04a2412d335ef1e999bf59f17b137
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:35:29 +0000

    breakthrough shop vanish present case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
