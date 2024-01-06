3a1b5fff4fb75d4a828b4fecd3ba3e5a6c5f5435
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:45:28 +0000

    exactly notice round match beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
