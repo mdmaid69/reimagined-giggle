440cd9f04e575cdac57905db0c9a2b211e8c4ed5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:00:48 +0000

    old moral duty scrap noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
