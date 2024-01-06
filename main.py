02418c532bc9417ef572a16e1c649ca7fbf2cdc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:28:49 +0000

    bored matter spoil time success

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import tensorflow as tf
+print(tf.__version__)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
