5ce1f1baf45b4da21f852a0578e0265237d4490e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:17:23 +0000

    middle stage future mark feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
