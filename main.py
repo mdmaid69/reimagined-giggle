607b9b35fdcefd042bf0a1f7f6095ed5473a8031
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:34:21 +0000

    bill again freedom Kk complete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+for i in range(5):
+        print(i)
