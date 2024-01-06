fc490a71a83d828c4808c797b1a0b3e2e9915a33
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:58:53 +0000

    dangerous post scare treat upwards

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
