5ec7872b1382685df3ccbc8d7d9b35d40868cdc3
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:00:13 +0000

    fire impressive reliable accidentally hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def reverse_string(s):
+        return s[::-1]
