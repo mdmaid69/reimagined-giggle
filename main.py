cfaa7bbdf7820c3cb3d50c94586eb88c8de77abf
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:53:29 +0000

    unhappy steal accidental brush catch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
