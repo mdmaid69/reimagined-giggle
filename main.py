2e2bcc06f452864daf2c3ca8ba07fa507d410707
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:54 +0000

    recently show tall we alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
