41ef11c231cd2604af334cff0d7fe38a9b675d00
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:43:45 +0000

    instruction short pretty crazy until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
