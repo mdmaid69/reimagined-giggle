46a12d7f018d256f466e512322ad72d6201e69c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:13:46 +0000

    behave sticky special extremely immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
