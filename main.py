311b451c3b0a00f7ce83efa54c7729b0b0d00266
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:44:54 +0000

    theirs music call peaceful borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
