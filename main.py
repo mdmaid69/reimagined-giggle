71fcbafc63818bf80b1bfa1404905c0ce7909e1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:15:20 +0000

    consult sign forget reach real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tensorflow as tf
+print(tf.__version__)
