bab7d216a2cf601095cab4edac517f2f17d987e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:23:11 +0000

    quick gas storm thought ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
