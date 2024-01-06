fcbc23aa5e9ee9858657c5653d0ee92fdc17d4a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:24:37 +0000

    circle strange among hello exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import time
+def get_current_time():
+        return time.time()
