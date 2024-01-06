1ac1ce16ac01b481fe12e92ae0144b1507e118f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:48:55 +0000

    but along rain take rather

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import sys
+  def get_python_version():
+        return sys.version
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
