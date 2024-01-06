98acb61b0eb620a694c4da815b7cd378d37ff9ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:58:02 +0000

    each not standard call wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
