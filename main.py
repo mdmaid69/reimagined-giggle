a1f546c13acd9b989736d7699836cf2938d624df
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:59:19 +0000

    pain comeout choose centre attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
