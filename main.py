34e95192706db1932f4d49c19dc12f323695e4aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:40:33 +0000

    exercise bang fault getaway spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
