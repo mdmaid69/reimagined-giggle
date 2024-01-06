9f0b01d2d83eb8a2797982ae8eaa3a257ca88d7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:33:25 +0000

    damage exaggerate fail hit change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
