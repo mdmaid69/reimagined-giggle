83bb1ac9d090daab0be9d120556f2707100d8682
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:49:10 +0000

    delight understand somebody sea hardly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
