2d48618691a58f11bacfda9a0fc93834e3491d6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:15 +0000

    concerning sensible close hide marry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
