9c6ad1aeba2a54d863730f20b72a0ed88bd9466d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:17:09 +0000

    include keen key song duty

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
