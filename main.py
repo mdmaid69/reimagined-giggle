a381a10ba14f2bfa57f60c45a1f8969baf3ac8e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:27 +0000

    set whistle impressed owe breath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
