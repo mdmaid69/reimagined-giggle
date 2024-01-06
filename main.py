4239af4839ff113c315fb1a3160906ca7a45a528
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:59:19 +0000

    feeling possibly burst accident communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+for i in range(5):
+        print(i)
