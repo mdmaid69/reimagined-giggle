430a52e05bb05e6227fbe4f5f6068fae52f0d459
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:16:18 +0000

    shoot across dress train note

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
