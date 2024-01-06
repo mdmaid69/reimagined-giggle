6c74a893c06464913edd435d245ad931d7cb5754
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:14:26 +0000

    chief you spread attach dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
