ffbd0418a3f07cb98930430de09c89a04b91bdea
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:56:15 +0000

    pure worth turnout react influence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+  def sort_list(lst):
+        return sorted(lst)
