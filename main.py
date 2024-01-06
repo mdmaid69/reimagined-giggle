04d91d2a890862ad5d208b561295a85d11483257
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:51:26 +0000

    approve find grateful effort slow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
