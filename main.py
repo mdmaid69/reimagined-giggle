360361272294f06eb3d92c2734b138463e314c5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:46:34 +0000

    pin available child find belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
