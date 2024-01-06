f2fc55f5c43b0a7c9c04a7bf343559fbd8d7b120
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:48:45 +0000

    hurt city dare to Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
