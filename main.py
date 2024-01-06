f7b2d0b46a50cb0ed6935c183aa85beedc389c45
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:30:45 +0000

    maybe doctor name pollution takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
