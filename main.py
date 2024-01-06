d6dc21e789a7fe3ace8dca2a9587b8954cd5e9de
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:50:47 +0000

    try boat evening attempt hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
