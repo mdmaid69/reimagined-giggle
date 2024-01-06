262300004dd14868a487a22a53eb920012503d33
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:26:49 +0000

    design own shop compare surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+for i in range(5):
+        print(i)
