6a08b3f92447cf6752eebd69519090f0716b7feb
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:15 +0000

    foot easy once dear delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
