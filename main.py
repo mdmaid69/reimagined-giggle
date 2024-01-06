408403396ba8abf8b31edb1989f4a56a7ca4244c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:25 +0000

    peaceful expensive special space see

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_priority_queue():
+        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
