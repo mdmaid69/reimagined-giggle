5c20039b827355498880a036562d8f7dee326a8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:02:52 +0000

    supply eager up yesterday hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
