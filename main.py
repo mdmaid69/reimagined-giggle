297f71ba5fd4e3e90cb43d1d04c54b76d7641bbe
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:03:21 +0000

    mine reply tour wear weather

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
