58e90c68ec01b16d93c4c40766f39e67e75e9ed9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:34:27 +0000

    heavy talkback raw round smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
