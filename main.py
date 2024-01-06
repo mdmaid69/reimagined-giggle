71da9bbdab8ab90c73f78a240bc4ebde3fecc646
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:01:46 +0000

    detail up secret mass underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import collections
+def create_user_list():
+        return collections.UserList()
+import os
+def remove_directory(path):
+        os.rmdir(path)
