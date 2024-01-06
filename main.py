2373acc76294d28f331692778ea41006f1fc3180
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:19:53 +0000

    wonder file education workupto squash

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+name = "Python"
+print("Hello,", name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
