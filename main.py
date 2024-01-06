e3623fb8bb360148440430ce799280ff5031a28d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:55:28 +0000

    mail lot attention whenever mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
