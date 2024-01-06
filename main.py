993e45d6d7d3769cbc506341c5eeac23b03e2941
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:47:28 +0000

    think dance somehow range care

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
