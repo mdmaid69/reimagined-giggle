53443362688892a13aa9d3aac8a9c824f98b8585
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:19:58 +0000

    exist dry power breakup detail

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
