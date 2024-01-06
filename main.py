1184368e2fee501d665dd4ca2d7d174d7123193c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:04:01 +0000

    concerned change valuable agree buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_area_circle(r):
+        return 3.14 * r**2
