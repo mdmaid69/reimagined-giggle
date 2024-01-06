05c1cca4085432da79593158424bc06fddd7ef7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:03:17 +0000

    address underneath tour fresh ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def convert_to_binary(n):
+        return bin(n)
