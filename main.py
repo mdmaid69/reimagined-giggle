3adfb5f2a02a1e035770935b8519741531a955dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:01:22 +0000

    score problem image area consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_pressure(force, area):
+        return force / area
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
