89bbbb42e92b622a6d49408c517fe36e2973cfab
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:13:58 +0000

    exhibition frequent breakout vote guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def subtract_numbers(x, y):
-        return x - y
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+def exit_program():
+        sys.exit()
