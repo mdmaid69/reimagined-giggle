b8ca58f52a3b4021306b43f1080b612e0cb83c67
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:56:57 +0000

    callout refer for habit bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_user_list():
+        return collections.UserList()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
