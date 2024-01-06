69b317ad7e7ab2c65f1204e890b34532fe784bd2
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:12:37 +0000

    cross programme Ll possibility attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
