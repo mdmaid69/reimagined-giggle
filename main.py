4b8dde0cf591d341914ceae0c74399fb8969ec09
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:45:55 +0000

    platform among right while year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
