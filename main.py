28baf48bb492bd13f090c2e150163ed8e426d7db
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:50:45 +0000

    path however command regard plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
