80d900027c9926968b20365a8e44c67c49c29645
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:22 +0000

    definite sport mean perhaps more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
