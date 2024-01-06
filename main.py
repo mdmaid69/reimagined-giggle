1fbefa9bdcb314207a98d6bdb0f56f154572dc49
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:45:03 +0000

    wheel admit no describe guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
