e57b52ab60a7a4586966cf567f87b75f458fb346
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:44:37 +0000

    time average mess worry ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def get_array_item(array, i):
-        return array[i]
+def check_if_array_contains_item(array, item):
+        return item in array
