f689eef0e8e6ed20dcc9bf570df6c7115558388f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:35:04 +0000

    deep store claim admit certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
