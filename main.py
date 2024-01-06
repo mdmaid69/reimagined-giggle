9866b2ea0578a00082da95d61efa07459578dcb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:57:14 +0000

    bill mark mail accept bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_odd(n):
-        return n % 2 != 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
