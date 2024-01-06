dbb9a3a6e562745fb15751fa985fda9db99b3784
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:21:39 +0000

    send plastic although tough thank

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def greet(name):
+        print(f"Hello, {name}!")
