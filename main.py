5a8fce274c04bb8b309f011c9e36cee959a13a56
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:22:24 +0000

    across anyone ring say sharp

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(numbers):
-        return max(numbers)
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
