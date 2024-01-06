7db2309f5bcd09ee964abb5e120fadbed65ecb6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:51:19 +0000

    natural dance loose how front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
