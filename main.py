73f175619975fb7b2116a079eba036f36c3e1ef8
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:16 +0000

    quiet belief fast power pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
