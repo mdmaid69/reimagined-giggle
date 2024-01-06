0547f7bd958fa3e9690b80af2ff63cecec78e2b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:40:58 +0000

    sense confusing wander ashamed born

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
