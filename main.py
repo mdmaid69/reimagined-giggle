0e8a39ad928c921873d15e057b72691a270a9238
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:38:36 +0000

    convinced healthy to past most

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_speed(distance, time):
+        return distance / time
+def multiply_numbers(x, y):
+        return x * y
