eebeb15f7eb6d5efe31ca6fda8ce91d78f204b6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:05:18 +0000

    fall deep statement from classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
