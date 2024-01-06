95aa6cfa65ed923c7fb1b4d313763f2236ef3907
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:39:51 +0000

    after entrance original me factory

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
