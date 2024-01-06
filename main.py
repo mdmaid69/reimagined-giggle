c26b7265d40084b8c43c08632a3aad243e80f982
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:41:29 +0000

    vision thin serve properly any

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def reverse_array(array):
+        array.reverse()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
