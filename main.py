cb2484e75f7610ff4f8640c86ffec0bc1f7d7c10
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:58:37 +0000

    peace please education forever off

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
