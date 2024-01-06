89438e6b7f3451c89171d6bc4c7c5b8242613cfb
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:09:59 +0000

    ignore object level please absolutely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
