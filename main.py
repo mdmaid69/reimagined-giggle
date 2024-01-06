0175778b52ce1e853c8a2887f52bd221b6a2871d
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:46:43 +0000

    cheek poor yours ashamed sort

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def remove_duplicates(lst):
+        return list(set(lst))
