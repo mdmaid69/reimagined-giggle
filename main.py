2ed52ffd855de4e3fd6aaa6d55518f980bc33cc1
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:16 +0000

    fast already fix lend force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sys
+  def get_python_version():
+        return sys.version
