877d2c2d04ee55ba7d7c491cf68bb19718c44480
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:15:01 +0000

    true turnover straight together fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
