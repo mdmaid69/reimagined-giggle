bd74296c39a5a5bae5a410aa71fbb6ba44ea5bb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:44:30 +0000

    control shake beneath silent hungry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
