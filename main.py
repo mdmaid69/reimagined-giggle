79bcecc80534547cf9b3450f772ae5f3c513fe02
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:46:11 +0000

    pinch turnup risk vote for

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_speed(distance, time):
+        return distance / time
