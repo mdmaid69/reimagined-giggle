c72300d0c25d0e1438ac4f835d8ef78cab58b49e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:00:37 +0000

    takeout copy amount deliberately enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Words:", len(text.split()))
