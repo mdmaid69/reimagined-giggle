7e84e93173b62f9576dad98805765100fba18104
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:12:11 +0000

    back movement apply movement statement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_array_as_complex(array):
+        return complex(array[0])
