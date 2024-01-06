09621dff509231cae7186f366cdc31e9990dda6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:58:58 +0000

    could punish upset shade apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def divide_numbers(x, y):
+        return x / y
