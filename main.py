5fd0c36b08b592aa56cb931131facd1bd03ea879
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:00:11 +0000

    creature moment drive neither but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
+def count_words(sentence):
+        return len(sentence.split())
