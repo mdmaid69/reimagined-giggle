5e0c52a29f533720d401e4fa9928f8c30cce51d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:35:52 +0000

    change attach twist unlikely speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
