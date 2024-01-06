28ab7bb85d01674ea9cdee354eaba5a72e2451af
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:27:13 +0000

    play slowly contact bin pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
