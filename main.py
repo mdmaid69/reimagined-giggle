be75c324c96db9abb7148ca87d6248574178d9b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:15:00 +0000

    cause as drawing thick fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def get_array_as_memoryview(array):
+        return memoryview(array)
