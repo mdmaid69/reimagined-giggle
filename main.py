2f5ad84e0616754a3a3d99647a9437c9fe32ed61
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:25:35 +0000

    also disturb attach around workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+print(time.time())
