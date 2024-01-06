831b524d9e62bbd641faf3c62d2d292a417d0c49
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:48:04 +0000

    most experience issue or benefit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
