64b7872e467b5904cb1687bca422c1b52c1045b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:04 +0000

    alone theirs clean Nn look

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def convert_to_hex(n):
+        return hex(n)
+for i in range(5):
+        print(i)
