ed818ef715e18c7057e38dbc1405bd0fe98879ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:55:08 +0000

    strict declaration wild shopping stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def convert_array_to_list(array):
+        return array.tolist()
