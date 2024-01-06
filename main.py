02a19e5e55d1747d8c5a30723c0fed4339f74c3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:56:08 +0000

    important suggest badly regularly guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
