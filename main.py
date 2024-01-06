75039d92cbc57b1f0dc95ffc669f214c3c2aab03
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:19:18 +0000

    thinkof soon squash together yourself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_area_circle(r):
+        return 3.14 * r**2
