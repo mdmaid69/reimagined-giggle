bf09dca190fad2119bb2373722695851d6839b63
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:11:41 +0000

    second whistle task twist never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
