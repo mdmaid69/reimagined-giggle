1a650a88ae445a534da902080efe1b8bf4ffc355
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:19:39 +0000

    push wise basically easy calm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-print(sum(range(10)))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
