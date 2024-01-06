bbc7058199152a3f60ace94e17a37637c7dec795
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:59 +0000

    Mm create country since have

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
-def convert_to_json(data):
-        return json.dumps(data)
-import time
-def get_current_time():
-        return time.ctime()
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
