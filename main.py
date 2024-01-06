cd270a3ff82353491308bba9cc06cec53ed2a53c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:30 +0000

    mistake switch recommend mention criticize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def calculate_area_rectangle(l, w):
+        return l * w
