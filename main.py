4308da7a5d4ea5e011aff7d36b8d8176c828b7a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:35:23 +0000

    mean form boil start book

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
