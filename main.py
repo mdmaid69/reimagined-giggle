73588eceaa609d0579892ac012f09665cb0eb6a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:49:56 +0000

    before travel frightened joke part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
