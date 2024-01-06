5bde3d849fb02b839f226a8e2852d4d603f1fd10
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:56:37 +0000

    anyone juice goaway their continue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_max(lst):
+        return max(lst)
