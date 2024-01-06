7f4b34f2e7caa854affa446973d88378f3b16175
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:41:53 +0000

    square money there burst dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
