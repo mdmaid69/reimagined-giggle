24e15206844b33b7a9ec0668caf6b7ec9f202eb9
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:20:14 +0000

    workupto reasonable split raw operate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_max(lst):
+        return max(lst)
