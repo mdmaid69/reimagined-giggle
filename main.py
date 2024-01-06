77d52bb2f2f2edc410ed8eeb3de2f1a10735aab8
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:43:51 +0000

    risk circle private seriously frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_pressure(force, area):
+        return force / area
