fe9777b37496e5d3c1c32132358c05af42834a33
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:34:48 +0000

    for hard artificial runaway rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
