faabb7609ca8bfaa5b60866d0d9eb1c9be0de3ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:17:24 +0000

    laugh for against raise total

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
