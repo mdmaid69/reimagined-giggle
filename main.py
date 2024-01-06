528a3cb46dc9162f7de5363d23af99aa8dca31f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:54:51 +0000

    somebody advantage cap slip scare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
