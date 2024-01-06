699102aa3bf4e20a9de97b9b539b5d6ad5e60e55
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:30:16 +0000

    welcome set port connection free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sys
+def exit_program():
+        sys.exit()
