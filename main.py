8482b9117cccc1f026d23355b6473fa776ea9328
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:13:25 +0000

    squeeze warm machine cost practise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
