68a72cc1b93df18eca64897cc15220559832edc7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:19 +0000

    programme development floor just pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
 def get_bytes_from_array(array):
         return array.tobytes()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
