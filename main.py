2496cd7b5db39ae8adf9e18104693addb69b3016
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:05:43 +0000

    transport about just lay suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
