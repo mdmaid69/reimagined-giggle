77551e63b66ca7637462cf75c24f2051a184914a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:05:18 +0000

    average damage old Ss treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
