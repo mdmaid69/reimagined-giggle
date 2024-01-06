cb28d40b28307e9c13bcc52bfb4de003ff9ce769
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:33:00 +0000

    middle taste bang interruption satisfied

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
