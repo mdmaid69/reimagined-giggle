a7a28e4b110f0850e07b99089cbfc155da83e8e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:10:03 +0000

    getaway dear necessary proper beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
