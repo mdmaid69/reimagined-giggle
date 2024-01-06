61be723622d499bcf3cbf602b7f1cb7d11a87931
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:24 +0000

    Rr plastic movement sore responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
