e46717271f2ec49d571804d31ecbebce882a4f9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:13 +0000

    Qq open sleep price warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
