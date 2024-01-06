a58f1bc556292a68140ebb115c6e8b46ae795f14
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:42:24 +0000

    straight plenty description decide excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
