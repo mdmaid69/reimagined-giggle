afc8dba9e094676f6b712f8c4fb1a4a1d06d7a83
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:48:26 +0000

    Rr plenty murder region Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
