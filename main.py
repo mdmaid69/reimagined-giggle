bf084713d2d9f333ca494175ac6f4a9e18d1aac9
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:35:31 +0000

    brother date convince shout further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_memoryview(array):
+        return memoryview(array)
