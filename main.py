7f3c4a16d286891434ab7ae0ede270fdb6804efc
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:52:15 +0000

    feature false unit give last

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def cube_number(x):
+        return x**3
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
