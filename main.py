d163e79bb842f6b63052041fe31e3ede1198b430
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:11:32 +0000

    be injury before whatever high

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
