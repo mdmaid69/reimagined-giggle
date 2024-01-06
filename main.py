8d8caaf99bb2e886e1a6a8496a7e2d46705acf37
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:45:40 +0000

    deserve authority front usually across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sys
+def exit_program():
+        sys.exit()
