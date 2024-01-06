1c3ea4f5527d5435a769cded0e632846c2ac36f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:04:40 +0000

    occur hide recognize certain relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_set(array):
-        return set(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
