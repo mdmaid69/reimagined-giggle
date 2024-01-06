c2b2d56c9215966b21920f7217b223e44988d044
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:56 +0000

    570enjoyable away uncertain serve middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
