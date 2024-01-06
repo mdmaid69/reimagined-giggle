1fb7d098add972eb4df3811d8c6ddba36ece2fc4
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:41:07 +0000

    communicate patient judge lean complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
