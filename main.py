4e705ed73f3c24e745cae8e07d190a84ecc8c80e
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:29 +0000

    do port fact talk occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
