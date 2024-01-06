2512c3cc776f18b0183d60207e07e0f09a893575
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:20:17 +0000

    bringup law satisfied trade switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
