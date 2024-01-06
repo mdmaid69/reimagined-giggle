970147d1918f3e4660a2feafc78e63e852e82b65
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:23:53 +0000

    old round definite influence debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
