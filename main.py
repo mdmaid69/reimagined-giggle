02d6fdcde76ff56413400067667af9e16f2f23ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:48:59 +0000

    false so poor offence plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
