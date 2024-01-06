cfc44391e0adda2e1b372a73121fd60842186c82
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:30 +0000

    cap part disturb hole slow

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_area_circle(r):
+        return 3.14 * r**2
