1a0257dc9ad2828dc0d2e3a850bf13fe25205f86
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:34:53 +0000

    tap upstairs careful could furniture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
