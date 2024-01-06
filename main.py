012744d2d1e87c902cb718f1e673db26db693f28
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:56:25 +0000

    among ours learn unit second

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
