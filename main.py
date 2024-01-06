9f0d00d879a4bf89d7235da74f8a19282e55732a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:49:57 +0000

    confused getaway laugh chief abroad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
