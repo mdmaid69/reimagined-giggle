de6588433db8d6e3af9fb5391a9bc1d4cadb3b89
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:16 +0000

    touch through thought performance spill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
