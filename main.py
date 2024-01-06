52a488945e55621f4c132acc0ca490ac375b596d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:45:55 +0000

    tread over someone persuade hard

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import time
-def get_current_time():
-        return time.ctime()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
