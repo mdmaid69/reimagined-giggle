07e2d6a886d98bda3bb2406f3e7510ec5b29b617
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:51:13 +0000

    reaction though cope children fact

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
