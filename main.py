aa8d64f4a80a2a6d58b051c557d97989fcae185c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:57 +0000

    promise wish touch party means

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def find_min(lst):
+        return min(lst)
