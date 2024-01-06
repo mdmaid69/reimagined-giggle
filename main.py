8b45856a70157581080b6b29422d4b6c3c283430
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:51:59 +0000

    holiday occasion finger call plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_stack():
+        return collections.deque()
