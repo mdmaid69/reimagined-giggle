e70320f8ac0a3b35c7a33483b6bcf1067f0ab3f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:08:54 +0000

    ourselves sure out evening recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
