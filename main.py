5288b67aeaef6b3a5ef41347ba491303b78c57d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:49:13 +0000

    die careful compare scream then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
