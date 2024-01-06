46eaa097d7319168ea3e7f689df3a35aacd235f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:17:18 +0000

    something describe reasonable connected separate

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
