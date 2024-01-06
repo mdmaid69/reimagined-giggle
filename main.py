350d234fa372611a4c1043a7cf4c20639dc3bbd1
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:23:59 +0000

    tall decision because offend peaceful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
