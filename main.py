f2856f3d4fca4d16d7e03776e2520bf2b8ce68cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:38:04 +0000

    provide wish total 571enough hollow

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
