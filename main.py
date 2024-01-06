f64bcf2001ffe2c1f408660f2de8e67c6d738c11
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:02:37 +0000

    workon performance wild major use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
