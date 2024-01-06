6681a31ce611c519c7e8a2e4b30321b965d81f4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:48 +0000

    true awake walk regularly black

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_list_from_array(array):
+        return array.tolist()
