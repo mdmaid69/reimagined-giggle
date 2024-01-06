2a4987db4c49bec92c4aa5d9b2f350af0c9ad1ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:03:06 +0000

    bit communication major runaway music

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
