40b8eee925e999cf34c058a1fcba9747204efa66
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:47:11 +0000

    experience serve police stare kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+  def multiply_numbers(x, y):
+        return x * y
