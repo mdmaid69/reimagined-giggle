d561bc1f8efba02aa3a75f46d8d294ad5e57f7d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:09:21 +0000

    damage interrupt cruelty pencil appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
