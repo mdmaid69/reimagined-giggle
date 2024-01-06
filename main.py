d7916bb81d049953498e2023fbd157a6dfbb35bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:18:16 +0000

    point exact stone value right

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
