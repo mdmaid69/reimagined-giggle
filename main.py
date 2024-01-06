6ea940f42acc0d994e5c994f8021dc180a8fa680
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:32:47 +0000

    rank result either course port

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
