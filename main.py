52030c7feb4f26a63dbae5b7f90beed022142aba
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:34:13 +0000

    door passenger throughout attractive use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
