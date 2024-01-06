c54776ced27fcd8798d9c41dba949bc0d89011fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:51:05 +0000

    such news born character fresh

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def set_array_item(array, i, item):
+        array[i] = item
+def convert_to_hex(n):
+        return hex(n)
