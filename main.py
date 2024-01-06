ebe9c06731f6b5cdb7e21c49a97011c8726e7e0c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:53:04 +0000

    straight ground vote ashamed joke

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
