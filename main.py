94d592c699fd8dda776c3641cc7e427d53b4fb25
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:42:32 +0000

    match how add breakin guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
