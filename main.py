a5e82973762947b80fc51b7796a2f3670d9b1e83
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:08:26 +0000

    but indeed argument unhappy impossible

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def count_elements(lst):
+        return len(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
