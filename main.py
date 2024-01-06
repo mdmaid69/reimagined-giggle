b63b86c740c4a9139c6e15681b8556a8c6d4f20e
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:03:47 +0000

    autumn from noise soak they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
