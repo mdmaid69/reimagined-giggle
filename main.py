9d339c2bf0457e9fd91046da6e36f88e2249979b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:27 +0000

    contrast law Yy divide laugh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
