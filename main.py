e65081b00d9124c7685a37195ef1ed23ba2a716a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:12:13 +0000

    approve error fence arrange Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
