f3a0fe218d8195e55d5683c3e7146e5e862a00f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:30:48 +0000

    lead pretty scream dish figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
