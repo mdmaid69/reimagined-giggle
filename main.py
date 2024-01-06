942d728c0396cdab8623ee82fce32aeb47cc0e99
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:42:32 +0000

    majority open blame clear 566end

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def add_numbers(x, y):
-        return x + y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
