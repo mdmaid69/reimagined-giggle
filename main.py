9510d7dae8a1323dd0aaf27179d4a857bd14688a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:02 +0000

    music worse near cool well

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
