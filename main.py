e14a32bc37ccca07be696a0c994d1bbfa9f7d37d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:43:02 +0000

    laugh bread tragedy rough scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
