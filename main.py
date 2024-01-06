86369c47ad8b657812c1691bb17c63c70c0fac67
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:14:46 +0000

    conclusion narrow common fix tear

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
