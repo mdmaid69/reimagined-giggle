611ca005aee24c57582e574c1b5696828ea860fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:55:19 +0000

    finger bush wipe control comment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import time
-def get_current_time():
-        return time.ctime()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
