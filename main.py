b1938e226b56a63e538cbdb1ffa0b6d7ef2da6a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:41:28 +0000

    operate ride 571enough upset content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
