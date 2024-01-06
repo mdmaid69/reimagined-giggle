15375d1792e560197cd3e14bfa505a4fa471c46f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:02:24 +0000

    post music extremely people live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
