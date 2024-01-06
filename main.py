2518f7e068c32ab12644a2e56df9638beda14d81
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:16:15 +0000

    crazy for cause between imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
