2fa67154c6437c411b97c0e70315edf2fd0eb7a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:18:36 +0000

    typical must store pure flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def cube_number(x):
-        return x**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
