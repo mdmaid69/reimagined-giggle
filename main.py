0291e091e86ad01917b3fd8b69d003d9ab3a7bab
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:17:38 +0000

    experience familiar true false confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
