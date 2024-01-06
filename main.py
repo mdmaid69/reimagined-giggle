f388a00468346f6f3c24fb66b834fc0a62f7f87e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:26:57 +0000

    include equal miss improvement connect

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
