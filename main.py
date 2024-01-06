1a852de7b4a1355bfc9ef097aebc8396c85eb280
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:49:39 +0000

    comeover emotion brush geton could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
