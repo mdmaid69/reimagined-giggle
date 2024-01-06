dc51929ad6788fb581383de4ae0f497268b1fcc7
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:31:15 +0000

    takeoff near emphasize see benefit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
