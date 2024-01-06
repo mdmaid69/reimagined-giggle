713bd15237991cacf609a207a94bdabf7c39556e
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:01:18 +0000

    wave national under unpleasant relation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
