5e7226da9f9ef2a24864169ca7203aa764237b76
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:59:29 +0000

    border soil soon giveback clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
