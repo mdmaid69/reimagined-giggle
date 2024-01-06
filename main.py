84439c8eca5d0efb5042f37b56a0f1903b68aa5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:49:23 +0000

    brief own figure like lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
