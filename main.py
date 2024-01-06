2a5784d3194aec0980cbde881fa6fe7e7fed1705
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:00:58 +0000

    send on thirsty loan shame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
