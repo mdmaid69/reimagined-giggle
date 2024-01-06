c1620e7f02204516862ed63ae00d69a7a41f6130
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:05:50 +0000

    unfair family hard win plant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+name = "Python"
+print("Hello,", name)
