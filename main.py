301bf102086b43eabdfec63b86522e18bd2b20fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:54:40 +0000

    supply use history pocket leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
