12640f0d76ee6fada0ece2d4403e96d46bcdfcc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:49:23 +0000

    people crowd pressure long practical

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
