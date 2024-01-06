812ff415864c75e0711a9700bedff2984fd022ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:54:04 +0000

    frame exercise value Ee afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
