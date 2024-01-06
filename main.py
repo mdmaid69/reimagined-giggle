7911e7e6989fae11848c44a635ed6ccdd850fa15
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:51:11 +0000

    outside steam fight touch comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
