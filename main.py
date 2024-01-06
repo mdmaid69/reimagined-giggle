0e033dd5035fbec7be2efc9e3a2cef1c9e376fc4
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:04 +0000

    either war do thankyou trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
