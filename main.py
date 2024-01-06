a4f1a9ae280ff1d41682dbb5aaca2deca5663109
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:14:36 +0000

    definite grand discussion arrange scream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
