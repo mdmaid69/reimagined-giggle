6b240445e1afe1c7ea162376b993385e8663464e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:26:01 +0000

    fight work common declare again

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def find_max(numbers):
+        return max(numbers)
