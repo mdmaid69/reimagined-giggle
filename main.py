86f035e3776224af2586b0ae56c189ac47c018ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:41:14 +0000

    then century region account save

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
