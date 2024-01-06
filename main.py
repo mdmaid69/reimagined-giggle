0b8a20bd7d7e01121a942eb0a08d03a1d262414d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:23:20 +0000

    burn plan lookback(on) kick knowledge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
