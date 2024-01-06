1c59c7280ce8dd57198009520e81ef493597debe
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:03:37 +0000

    gooff forgive go rare he

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def reverse_string(s):
+        return s[::-1]
