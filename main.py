2f82467803145a9f7e1ecfc5b15af5d871ff1369
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:54:41 +0000

    announce negative exchange improvement listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
