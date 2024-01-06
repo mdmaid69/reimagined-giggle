42872e1fbd98847398279138d96312ae8d4a9643
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:03:59 +0000

    Nn cut but edge name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
