dc6f73ec4ba7983ed41f89e400cdf127de15faf1
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:46:58 +0000

    extra produce comeout left speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
