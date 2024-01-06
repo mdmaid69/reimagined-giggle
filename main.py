88ca00fd64063f7ef4e1887bf713970e16c82217
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:14:03 +0000

    neat unusual knowledge waste freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
