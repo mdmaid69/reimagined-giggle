74b8b0370002cc0f4001024d81bb8378d3a3767f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:28:55 +0000

    strike steer score size deliberately

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_speed(distance, time):
-        return distance / time
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
