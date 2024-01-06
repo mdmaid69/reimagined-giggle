e7cbfb9605c94650bba1897d1b65802de0390160
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:42 +0000

    organize friendly machine bad colour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
