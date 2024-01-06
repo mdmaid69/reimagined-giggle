dd4fd632d4d8a68210145231f15f2bf2eb904446
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:44:42 +0000

    soil desperate cap bet back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import json
+print(json.dumps({"name": "John", "age": 30}))
