61dbd03c98227c760fccb3683035b2c8c8dac621
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:05:22 +0000

    free shade unfair shout book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
