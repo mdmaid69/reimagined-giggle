90f0b6f8314d72ab0b790cefd2c80d6646155287
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:54:34 +0000

    makeup priority noone before hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
