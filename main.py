c3f14dd2d3da625fdd38b3417fb2f84b0a94ca65
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:00 +0000

    regret and however unpleasant deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
