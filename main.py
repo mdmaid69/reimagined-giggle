5e19572d37bfd32a32ce713e426327e168201559
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:50:28 +0000

    evening easy combine beside preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
