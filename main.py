9ef1fa4e3bd247ab1c5cc115d25d3418ac3fc47f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:28:17 +0000

    demonstrate Pp trade formal conclusion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
