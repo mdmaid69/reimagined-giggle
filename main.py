7722b475f394541e276966a1fcef2516f0a42005
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:30:35 +0000

    story final copy slowly deal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
