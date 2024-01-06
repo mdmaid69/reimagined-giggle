e7ac94c8fbbb1ba24d48557aa1aea899d9bb4fa2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:35:29 +0000

    ring human all passenger professional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+print(sum(range(10)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
