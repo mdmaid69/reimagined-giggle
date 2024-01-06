e952c20cb72bb4596736576a96715a399f7d000c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:53:39 +0000

    rest maybe everywhere oil threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
