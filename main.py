b4ec735c1241c686cf6270776d38051cd01ce68e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:23 +0000

    possibility length Itself alone exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
