098f4b1254b4ae7a5896f35c8df08867b6bc5154
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:44 +0000

    fairly up 568energy extremely the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
