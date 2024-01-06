d7130e27f5fb7a46b2ea743815587775a520c0d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:57:19 +0000

    crowd idea nervous favourite performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
