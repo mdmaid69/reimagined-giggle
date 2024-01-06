76916f15cd8e2dba1c24d9f2177651d8bf0667e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:44:38 +0000

    away release rub face disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
