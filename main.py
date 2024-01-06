072ba8bb9f6980a90be234e3cf0094b6715456e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:11:05 +0000

    seem sore sport Mm false

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_typecode(array):
+        return array.typecode
