a023ab2bb13a62be1cf28bfc632c49f637895ca6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:31:09 +0000

    getup win experience believe spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
