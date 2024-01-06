cee75a3f1d415862958555927d9133b4f65ae6d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:31 +0000

    comeround push mine warm matter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
