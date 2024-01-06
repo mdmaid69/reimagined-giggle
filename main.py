d4eebfa0bab10449a0deac1c53fa79e3cb1017cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:42:29 +0000

    mistake compare equal close slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
