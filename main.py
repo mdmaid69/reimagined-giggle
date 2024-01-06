d09b17a83b1ad564e137cc9d3905938d384525ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:18:16 +0000

    likely ground shock with join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
