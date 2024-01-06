022ab36173aa676971366a1d9e00749c37c37829
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:34:07 +0000

    hide to dick such set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def reverse_array(array):
-        array.reverse()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
