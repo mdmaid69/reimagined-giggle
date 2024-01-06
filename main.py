58c58cc2b43a454e93191d8ccdc5b1027e197a34
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:11:26 +0000

    job long say pleased before

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import sys
+print(sys.version)
+import array
+def get_array_index(array, item):
+        return array.index(item)
