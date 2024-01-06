4eeb8cd52d022c63ff488404c9ab164028a69e67
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:02:52 +0000

    moment Ii emphasize friendly safety

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
