afc841ac492f0039d4a6def174eaa14faabb8702
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:57:53 +0000

    bath glad avoid with life

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
