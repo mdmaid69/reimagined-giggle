22ff93ccedd789fd0b7755359e7fcb748ce7ef2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:19 +0000

    early offer record impossible see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def remove_duplicates(lst):
+        return list(set(lst))
