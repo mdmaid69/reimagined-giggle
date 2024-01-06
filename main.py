fc2ecba2e69a8e44f2f999d85c0e18dea6cb9464
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:28 +0000

    thirsty lend past future let

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def is_palindrome(s):
-        return s == s[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def convert_to_binary(n):
+        return bin(n)
