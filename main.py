c0787c57c6584134c6673c4cf2b0bb5d0d8edeb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:57:45 +0000

    satisfied smoke basically narrow pronounce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def print_python_version():
-        print(sys.version)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
