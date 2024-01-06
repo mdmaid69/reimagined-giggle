c594cae6728886c9c67346eda91b985dfd419d48
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:25:00 +0000

    on principle eye cry seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_sine(x):
+        return math.sin(x)
