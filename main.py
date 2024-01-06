4dd8329efa2a7fab0bd341d81201fa42470e33e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:13:14 +0000

    she few regularly plant Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
