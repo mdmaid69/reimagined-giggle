907384f72ef0071b5ed7060ca280f848074cfc32
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:16:28 +0000

    shall about attention basis description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
