6801cee6e935f6c0c4f8af07eea1708c40fd8721
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:48:25 +0000

    heart earn name plenty crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
