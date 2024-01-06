007a18b7806e05f25a6de4ecdd8a78a4f60a96a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:29:30 +0000

    seriously alive one sign he

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
