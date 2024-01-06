67990fe523d21f89fa6481eb4cbae01825959ac4
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:17:36 +0000

    above fear convincing nonsense plenty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
