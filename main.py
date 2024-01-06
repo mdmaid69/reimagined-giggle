dcce5e9bfa6e264ef495789e9c90fde7017ea501
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:30:46 +0000

    risk into make tone crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
