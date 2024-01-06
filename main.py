19c8af340d30e04fe611fd5833d8a634627cbe3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:26:29 +0000

    shall idea city confident determined

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def create_queue():
+        return collections.deque()
