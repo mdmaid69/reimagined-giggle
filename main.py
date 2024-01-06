3f5553836d826cea9b2aeb47d4918951f2ba9432
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:01:07 +0000

    scrap waste still clever meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
