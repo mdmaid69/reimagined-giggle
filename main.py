427e391194d1a8442744b91bf6e153d06dad8138
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:31:58 +0000

    coat suggest treatment building twist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def is_even(n):
+        return n % 2 == 0
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
