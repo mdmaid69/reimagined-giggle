80eb3d0c8a338c5bf118cf428bf8609068aa3c05
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:04:07 +0000

    wipe neck start ahead takeoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
