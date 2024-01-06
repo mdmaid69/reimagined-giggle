6960c9b56def3012bcb421d80ffb4684558d831c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:08:47 +0000

    term complete interfere introduce wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
