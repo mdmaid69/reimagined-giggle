1e7251c4973773801cb037f2e492c94b7e50eb35
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:54:47 +0000

    wander weekend truth conclusion independent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_power(work, time):
+        return work / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
