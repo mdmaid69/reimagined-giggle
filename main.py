b8a7a1872cb8254d9e5a9f2d0dde476d8ae3a808
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:23:06 +0000

    change shop sell building pull

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
