e3b2293a386ce271f9881d221b270b9555832630
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:54:22 +0000

    never company female nearly Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import collections
+def create_priority_queue():
+        return collections.deque()
