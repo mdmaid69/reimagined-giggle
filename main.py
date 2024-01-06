e9adfdcaa0c8024e1668aa4fda6a7375b8dc361b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:39:19 +0000

    goout scale brush maybe fear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
