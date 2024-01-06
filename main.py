eec5bc006d9fae8b886b1ee82845329059dcd344
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:24:14 +0000

    exciting afraid traffic shrink point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
