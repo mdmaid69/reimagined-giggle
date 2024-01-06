90136a96f1e0081fce834ae48a5f767de1a57840
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:22:57 +0000

    succeed comfortable family building plenty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_area_rectangle(l, w):
+        return l * w
