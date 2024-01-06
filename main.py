6d683e32926929bea097a0b72e1fd1686646263e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:57:42 +0000

    turn workupto connected yes leave

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
