a5c57891983b450d4d9139ff482e26c3dfd41682
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:28:10 +0000

    lost sort scared beginning receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
