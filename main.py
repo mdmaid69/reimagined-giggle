9ca84b47295acc893fdaee55954b0662350b944c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:54:42 +0000

    congratulations exam slight roll stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_array_to_list(array):
+        return array.tolist()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
