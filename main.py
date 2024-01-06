17c82aa78b0d1e6bea0235d7c9922a4e1ac3e746
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:27:48 +0000

    delight 572enter birth eat identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
