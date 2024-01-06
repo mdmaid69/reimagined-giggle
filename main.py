5985a74ab3bbf0e74b04643a303a1f9bad4744b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:25:14 +0000

    nor special callout entry bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
