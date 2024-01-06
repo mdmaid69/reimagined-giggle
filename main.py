6222cb0f404eda58456d214cf0a03905bddec3a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:08:05 +0000

    hope feed over recently spend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
