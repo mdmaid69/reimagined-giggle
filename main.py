aa07da8d8942585193d81fbd98ed4f66a549d63d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:39:15 +0000

    model list left otherwise accident

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def is_even(n):
+        return n % 2 == 0
