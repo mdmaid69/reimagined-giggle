08f2c07092b6984ae919f228f493c55279368d39
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:41:30 +0000

    whom wash mistake complete year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
