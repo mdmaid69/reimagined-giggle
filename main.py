27b213e7bdecf95ab58dbddc462e0ed4234edb55
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:44:54 +0000

    free afterwards next send cut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
