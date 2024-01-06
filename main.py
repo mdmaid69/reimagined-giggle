b05cf376eb3624dbbce51c2596911174834626ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:33:39 +0000

    interview rent near leg now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
