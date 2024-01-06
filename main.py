93b1ad99722b26cdfa504a21375022fe0b3c39b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:36:30 +0000

    report other although call begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_user_list():
+        return collections.UserList()
