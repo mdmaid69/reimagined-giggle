5656083e6677653c534809bac80356d04d643973
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:28:20 +0000

    tax set offence sick 179bite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
