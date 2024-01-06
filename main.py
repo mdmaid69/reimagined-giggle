f4405115a42ac3688f04bfff8c0d2d2b59c36eb1
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:04:26 +0000

    worry grand dare strict worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        return sys.version
+import platform
+def get_python_version():
+        return platform.python_version()
