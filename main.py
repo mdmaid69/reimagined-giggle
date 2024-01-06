998dbfed2ad07bbfe6c2e7242711aabeeb1d87d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:08:29 +0000

    lay flow emergency consist legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
