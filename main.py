68088d7560b7312fb8887d19d45a9f3cef4203ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:29:18 +0000

    dinner own figure crash catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
