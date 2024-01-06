2936e2b2e8c8fc33c2ac37640a4b987df513732a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:45:41 +0000

    rule trip substance take please

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import datetime
+def get_current_date():
+        return datetime.date.today()
