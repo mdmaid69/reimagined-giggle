9aa1da9801b66ec61def838869d78b197d20823f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:58:20 +0000

    involve central borrow crash lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+print("Hello, world!")
