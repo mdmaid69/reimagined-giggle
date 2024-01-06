ff61d708a990fcd4dfbb2da1c7a373f6f982c262
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:15:31 +0000

    fill Oo first equal or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
