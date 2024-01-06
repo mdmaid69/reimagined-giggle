d1e64f2fe06d70840031e34028b869b8bf4a65cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:56:39 +0000

    hurry before tradition sharp tidy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+for i in range(5):
+        print(i)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
