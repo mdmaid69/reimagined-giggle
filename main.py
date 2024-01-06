6619c3d76d9a28341acf45d990a5778c531eb154
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:41:45 +0000

    correct news string exciting lucky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
