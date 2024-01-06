6bf431da85073a0ec08039d031713189b5eeb38d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:31:39 +0000

    load whistle label their same

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
