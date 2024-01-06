21613ddc284418464828bfe07818363af6fa4b33
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:53:46 +0000

    sure print fire 569enjoy promise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
