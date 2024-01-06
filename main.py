3a8cb14ae7463513f0eb03506966477fa0281652
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:39:06 +0000

    finally also declare live wake

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def log_message(message):
-        logging.info(message)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
