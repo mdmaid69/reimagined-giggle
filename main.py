b892c92f5d7334409e6ba0fd5fa965a7cff4f50e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:26:08 +0000

    original tooth private gradually she

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
