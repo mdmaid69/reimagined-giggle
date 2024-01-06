b2323ddf4e05195ef32d2702a0d3ccc7a14de940
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:05:40 +0000

    wear use do anyone last

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
