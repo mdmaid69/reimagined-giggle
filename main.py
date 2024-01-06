1f4982415967d561961d7d4a00f8bc556064e205
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:40:00 +0000

    praise past urgent boat fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
