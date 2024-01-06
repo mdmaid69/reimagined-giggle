46db81e1a96439f30ba39b572a003ec06622db60
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:22:42 +0000

    unless rain blame pay fun

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_floor(x):
+        return math.floor(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
