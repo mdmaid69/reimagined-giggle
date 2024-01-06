e3174780efcfd60657cbf095ea62de932f182090
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:23:56 +0000

    correct criticize will field difficult

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
