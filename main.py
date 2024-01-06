15586f9c55e91fb6e6c36878a4fc0304b1dfa21b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:42:13 +0000

    label uniform runover behaviour gap

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
