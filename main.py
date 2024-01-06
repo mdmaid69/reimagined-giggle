5535166c18ac6bc86cf4dcdb5d8dac9de4c72d0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:03:21 +0000

    by question theirs part fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
