ccee3cc0c6fff4cc7dd4435817216060b0291baf
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:29:30 +0000

    hand swallow past makeinto slightly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
