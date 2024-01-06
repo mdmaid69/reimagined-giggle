4dee37d223b253f2a5836307ab63d17c672d4c0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:08:45 +0000

    lend mixture sight attach else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
