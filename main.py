33eb23b1e0d37c83036281e5e3716910671dd9fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:06:12 +0000

    remove regret rescue cruelty simple

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
