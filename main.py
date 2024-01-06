83386f4dbeedfa09ebc1229bc58d8e99c77c6451
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:17:25 +0000

    sleep disturbing present vision evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
