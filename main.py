1ddd1b9454ae6ec2459d08faace3e52b6abf6553
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:57 +0000

    notice about disagree control later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
