616b9f3d5b3d741e40faa0891e97810ba49ba406
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:37:08 +0000

    list wide influence steer straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
