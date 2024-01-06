e61e684999d20e71363349564cf0f075502f97db
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:40:04 +0000

    report powerful considering fond wear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def sort_list(lst):
-        return sorted(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
