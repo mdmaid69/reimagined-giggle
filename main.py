5493622efc28c0e3ad6de815fadf068ac97435f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:09:28 +0000

    interested promise confusing admire separate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
