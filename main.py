05a5e982c14cc30271adf32a81679ba4b46ab865
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:12:24 +0000

    sink clearly today lend threaten

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_min(lst):
-        return min(lst)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
