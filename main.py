18f5fd8b396e02ae308aa9f1219d0d82a2ca42b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:26:07 +0000

    cum constantly promise fork centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
