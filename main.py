cf6afb4a4a1554a35115081fafbadfeab23d59ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:24:09 +0000

    time unit and statement lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
