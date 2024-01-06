d0a309f4e489866236e6750760dcc74ff5f84539
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:41:49 +0000

    pleased after vanish rest block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+def log_message(message):
+        logging.info(message)
