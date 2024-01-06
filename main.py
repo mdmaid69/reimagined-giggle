bf0237fcd0117c99443050f91bd6c4fc340f9fc4
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:40:14 +0000

    steal hers bar consider division

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def divide_numbers(x, y):
+        return x / y
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
