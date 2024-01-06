d6ddd0988e5f872d1a01873fb1261fc30600f6a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:49:41 +0000

    dish company arrange black meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def square_number(x):
+        return x**2
