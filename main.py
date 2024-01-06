248e17f4b809781e4233726071cadcf8bd9d6f8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:43:53 +0000

    now never shrink backwards problem

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_words(sentence):
-        return len(sentence.split())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
