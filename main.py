b5ea957e1d3b8b97d360c90b0bc26c727361c65a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:28:09 +0000

    before suppose match silent task

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_work(force, distance):
+        return force * distance
+for i in range(5):
+        print(i)
