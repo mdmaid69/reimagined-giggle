98d7b71225316236aef29f0b3fbdedc004c58fa4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:10:55 +0000

    passenger apply stamp afternoon mother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
