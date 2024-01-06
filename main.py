8db69ea1451e76329135138300c7a887dc1ae48d
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:47:11 +0000

    welcome Yy interview goup street

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
