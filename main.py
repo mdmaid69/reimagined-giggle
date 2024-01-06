3c1d57257efe39f5b95b7ae479680d8912579483
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:49:08 +0000

    cross laugh join fortunately long

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
