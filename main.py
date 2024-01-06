24c6f5bedcabe952fa12ffb93960470ef0fb3994
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:38:37 +0000

    run accidental any experiment party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+name = "Python"
+print("Hello,", name)
