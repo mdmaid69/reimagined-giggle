e40763d651549d6a489a69f84f77654f876dd411
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:37:56 +0000

    exhibition excuse type fall behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
