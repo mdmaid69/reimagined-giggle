ca5846cc7ff5492d875b6acf6e672acd768992ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:52:22 +0000

    new rub else married equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
