aecfb360c3b336a858d1f2a15c40fd71e74905fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:18:52 +0000

    shine store provide beyond cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
