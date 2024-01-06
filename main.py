71b25ccb821976fd115adfeb44a09b8f96d5a480
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:25:40 +0000

    pass spoil many catch exact

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-for i in range(10): print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
