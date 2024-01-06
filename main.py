376d2c934949ee16bfc1fdd071184be414d0de95
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:00:19 +0000

    support contain rely concern hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def multiply_numbers(x, y):
+        return x * y
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_current_working_directory():
+        return os.getcwd()
