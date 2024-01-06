32063064197c58b68c541385a4ec74f1c3e0798d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:14:53 +0000

    brush influence dangerous comefrom famous

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_elements(lst):
+        return len(lst)
