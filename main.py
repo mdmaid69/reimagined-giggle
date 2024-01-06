3f4c1cec13920f87caae23013b4728d14a2d9aee
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:08:42 +0000

    impossible himself mouth appear steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
