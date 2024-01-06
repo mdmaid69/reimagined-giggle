15bd5afa430e0077b0da425a97a5206e2286f6ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:51:30 +0000

    sentence address sad confuse wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
