4768b9b1edc6f5deaa81faae2294e2bb3f40d1ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:20:21 +0000

    deep takeback achieve ear fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
