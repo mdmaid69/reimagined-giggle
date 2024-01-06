e9f91d032665c5919c3ef5a909a81abc6c0f1c97
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:39:37 +0000

    closely bus determined supply treat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
