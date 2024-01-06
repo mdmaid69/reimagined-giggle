ee885a19adb695e78827a0fdfc7cd8df72a59f1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:09:20 +0000

    term giveout ask emergency wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def cube_number(x):
+        return x**3
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
