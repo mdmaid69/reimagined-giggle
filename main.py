c48d410eb535f13954e3344a3c17d9ce130ad06d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:26:33 +0000

    closed image bridge niggaacademy usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-print(math.pi)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
