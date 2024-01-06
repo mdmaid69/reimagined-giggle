d5381d85f46f57fc9b521e4771122f46426932c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:54:44 +0000

    pull point Bb radio difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
