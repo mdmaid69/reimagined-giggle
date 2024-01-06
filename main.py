25d02622bbc05e976525de550226e7d3b75b8116
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:20:08 +0000

    show during turnout even battery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_tangent(x):
+        return math.tan(x)
