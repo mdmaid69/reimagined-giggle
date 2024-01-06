599c656e8a4da07125e6ec6c9cf38ab8658a9ef2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:38:47 +0000

    spill fall few meet original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def count_elements(lst):
+        return len(lst)
