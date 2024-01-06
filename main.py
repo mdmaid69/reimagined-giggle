d57ab01c64f399c25a41b74bf1708e1792fd46e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:06 +0000

    shade game brief hate star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
