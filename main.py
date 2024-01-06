a6afcba5fc822ddcadfdf014be1c5c441b12663d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:16:37 +0000

    draw air dream interview hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
