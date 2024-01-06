e3b83edf0fda2a548f46cd4111c8554d2e64475a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:20:19 +0000

    tip turnup day weigh dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
