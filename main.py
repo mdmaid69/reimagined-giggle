6abb4ae5f983e3eaa2387f66545c4ee9b729b544
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:35:31 +0000

    offer part personal then performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import time
-print(time.time())
+def get_current_time():
+        return time.time()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
