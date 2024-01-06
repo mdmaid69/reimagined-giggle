66585da22f41f1e51e9840424c516b86a4fb6ad5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:15 +0000

    harm meanwhile improve road she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
