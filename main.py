aeb32e2d3aa3f7a61f1107002040301ee6da370c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:02:41 +0000

    enthusiastic promise ago illness thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
