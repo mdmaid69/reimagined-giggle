da526e9860fda8bbb49310caccef8259784aecf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:07:40 +0000

    education bridge attitude wrong bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def greet(name):
+        print(f"Hello, {name}!")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
