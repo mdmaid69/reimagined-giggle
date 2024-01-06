3a122d45a522de709c89383edcd2fc16a884fceb
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:57:14 +0000

    decide cut partner sleep inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
