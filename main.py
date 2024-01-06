19039dbc16abef498cd3407afde483044a1316a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:01 +0000

    someone cinema born silent pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
