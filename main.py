b270cbb82ac21c18eb54128a934c27b92193e5a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:30:21 +0000

    wind everybody underneath otherwise whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
