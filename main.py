5214f7ccf286433517bed1aa702d2887999cbef8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:37:18 +0000

    trick back feed insist bar

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
