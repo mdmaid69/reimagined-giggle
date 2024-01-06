c7967ae1a60b6f544142393f175acba27b458b47
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:46 +0000

    trip live next relax neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
