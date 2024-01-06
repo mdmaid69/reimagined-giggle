982f1774f2ba47ccbcf453be1c61b0d7edff98f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:01 +0000

    hospital except without whisper recent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
