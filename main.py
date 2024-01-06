ac952999b725e56374d7c4a68f7180a71a10bbd6
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:30:52 +0000

    mainly half care colour scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_item_count(array, item):
+        return array.count(item)
