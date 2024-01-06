8fb00eaeadff1bd55219851e867b4705d0869b74
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:31:13 +0000

    constantly mend but beside it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
