c26f5fb43a93ac270d16306b973fa0bd337b7943
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:31 +0000

    crash principle tense worth discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def exit_program():
-        sys.exit()
+  def split_path(path):
+        return os.path.split(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
