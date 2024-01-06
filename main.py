8144a7820db0b58915317678e6d1aaa017108fbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:14:36 +0000

    blind talk Ff you still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
