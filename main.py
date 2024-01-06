626183cb1c3a8d7df01f281a5f5eb723dd206ffc
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:19:28 +0000

    compare since godown way realize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
