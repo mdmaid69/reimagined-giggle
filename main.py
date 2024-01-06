8616481ef6370e77ab66a59b2b09ba243d0280c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:59:10 +0000

    cost lucky float sport matter

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
