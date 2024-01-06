9747aadb929b3087639707654831cec1cd5ff0ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:05:23 +0000

    right thanks boil press accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+print(time.time())
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
