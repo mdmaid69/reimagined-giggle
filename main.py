eb2998f1fea6d59bc47c93749774a1bc537bfa5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:02:10 +0000

    floor keen slice crowded edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
