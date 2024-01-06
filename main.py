73415cd808b6c4ef7fc0804c1ca02bd2ed2a1e74
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:39:53 +0000

    together both so base Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
