caffae917a99f532854b64cd91395c0cbaa3630a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:02:16 +0000

    lonely computer constantly responsible 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
