81dac12df42790d196f1c73874347035f275c4cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:52 +0000

    maybe runinto development sport steer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
