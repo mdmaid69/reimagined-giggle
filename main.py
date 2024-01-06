ec40e0791234f26b10ca018fe5375387d2db6a83
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:05:08 +0000

    danger wrap before roll lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
