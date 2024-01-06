d5c7242ffd46e90b5ed0f8f62dc790751925a3c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:59:12 +0000

    owe woman bank fear frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
