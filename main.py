44e3db2702623f6b1891da39fbdfac76fbe483b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:21:30 +0000

    bag turninto hit dig impressive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
