7aac050ddade4af95c52d41b868cfb4c13fed634
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:47:02 +0000

    sorry birth replace sleep show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def split_path(path):
+        return os.path.split(path)
+print([x**2 for x in range(10)])
