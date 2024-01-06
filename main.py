23f61360dd9b58e15c48ddaba93454bf4822be95
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:56:11 +0000

    minor stop long wonder regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
