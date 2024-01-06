3021d75bf1eac545c54262c6d07220cd400c3786
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:32 +0000

    late mood store exciting breakinto

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def convert_to_hex(n):
-        return hex(n)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
