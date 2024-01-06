28bf5242879bd1a59326e81699c24c4494a15d74
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:38:19 +0000

    none represent brush guess mixture

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
