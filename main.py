344b430fff3235806be5b51afa3baf1f2c867282
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:51:10 +0000

    us dare agreement treat warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
