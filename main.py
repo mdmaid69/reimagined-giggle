b384fd85b6c8d6fbc7948023f00b4251874923b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:08:49 +0000

    section exit entrance can test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Words:", len(text.split()))
