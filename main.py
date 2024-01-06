1791dbadcf030e58aef5a8608b57735b40dd4ee1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:27:54 +0000

    finish makeup flat fast report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
