04a02bcfe9adf2ae76eca2ab87bad7c7f653dca6
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:33 +0000

    destroy friend drink narrow scared

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import sys
+def exit_program():
+        sys.exit()
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def reverse_array(array):
+        array.reverse()
