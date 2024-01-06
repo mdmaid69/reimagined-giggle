7f271fb3ca9ae85ec08dfc6539bd18fbae3ce92f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:28:30 +0000

    watch base convince runout better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
