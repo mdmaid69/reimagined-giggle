d2cdc522c85a4da5143e0a833ee8ad126efcaf6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:06:15 +0000

    union spot really sell hole

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import sys
+def print_python_version():
+        return sys.version
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
