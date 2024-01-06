2e72e2a6b082de9fc67626e3a0b6f48ab5eaca2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:01:43 +0000

    save mend include old OKorokay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
