c478e8118afe4dc7a7567d77ba87a2dd7f4ffcbf
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:43:23 +0000

    awake track sleep interested affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
