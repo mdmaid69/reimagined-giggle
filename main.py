418091766fb24b6812c699291c674c52b282c75a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:15 +0000

    nature deliberately circle basis anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
