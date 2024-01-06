b1de5e38b0fde5b53ab71403d04c3fe2145c8320
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:57:20 +0000

    suckme separate promise watch sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
