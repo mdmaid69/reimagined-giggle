26b2af2ec5f6f7c42e1b7d9530540cc8df493ab6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:43:57 +0000

    sink takeover fly office react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_acceleration(speed, time):
+        return speed / time
