da116a9efec11aef46e8f6665a03dfa89b184886
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:48:53 +0000

    song description rub turnback satisfied

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
