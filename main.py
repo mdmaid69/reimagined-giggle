2af611c4be6dd527ab80c9cfd033ff9a8f3f553b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:01:57 +0000

    majority pin careful kick statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import os
+print(os.getcwd())
