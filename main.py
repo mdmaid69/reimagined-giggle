f469e42819a47e3ee6c83866186d7417ec29e931
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:25:54 +0000

    anybody become duty sell unlucky

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
