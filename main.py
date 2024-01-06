fe9dbda1d2e683c8a7ca92558a4cf7797305fdad
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:30:33 +0000

    convincing smell zero part content

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
