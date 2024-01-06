9d6f3320b6773b14496086b872ba3ce3fc1ca0a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:30:28 +0000

    appeal claim performance ilovedicks key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_max(lst):
-        return max(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
