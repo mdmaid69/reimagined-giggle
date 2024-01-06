35c24f98f07c52ee1c1fb942cab5284a64f09e03
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:48:31 +0000

    increase talk race urgent push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
