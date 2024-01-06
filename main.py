e097965b94ccb37c1d2928caa7af4c6018a8a626
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:26:22 +0000

    dress influence head giveout history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
