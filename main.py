d06d66a76c34ffb946e99c5188e942bec6b753d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:37:50 +0000

    joke straight blue upstairs better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
