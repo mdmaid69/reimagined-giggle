3bea742f9b6611b2682da43a107a6eed3c5e3a92
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:56:45 +0000

    name thinkof shape which perhaps

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print(sum(range(10)))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
