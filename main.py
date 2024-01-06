6780b1f53aa4080138ce85eeeae714de86aa3a9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:58:47 +0000

    strange very side embarrassing sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
