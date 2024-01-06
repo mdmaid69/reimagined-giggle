c2e30c7cd6223ffa1a0432ef49ee703b1733c5fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:05:46 +0000

    female turnoff themselves shop constantly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
