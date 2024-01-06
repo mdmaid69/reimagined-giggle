2746ff7c4d59da9074d3dc93db17796101597657
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:50:30 +0000

    note interfere report nonsense feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
