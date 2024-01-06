a7ef90491e608d47d5e5bcdc4a0ef57212d2e88f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:19:37 +0000

    few talk common interview carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def is_odd(n):
-        return n % 2 != 0
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
