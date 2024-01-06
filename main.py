0623da1f9b036b25ffc9bbb2305ba61a4fd55e16
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:18:58 +0000

    frequent keepoff both one present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
