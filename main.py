b24d156104f56b6fb8e0effb725979a34f1e57ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:05:34 +0000

    young worth confused scream arrangement

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from collections import Counter
-print(Counter("hello world"))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
