ef83d5574f849616d647dc9b06fbcfa449458d96
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:34:34 +0000

    comeout off fit say shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
