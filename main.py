5a3d3fc9f6206de4d5dc2390863962dbb7e85990
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:20:30 +0000

    pipe mad guess well welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
