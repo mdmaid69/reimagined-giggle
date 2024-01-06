b6716f0ad72cac0c9e4aa75bff7569207401078d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:29:41 +0000

    obtain measure behind light objection

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_work(force, distance):
+        return force * distance
