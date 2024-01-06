aa07f7e7e7931743a9a9e7c8311c52494456e415
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:25:54 +0000

    description record design kind thinkover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
