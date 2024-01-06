877bb95945a7b7b1ebc35d5dfef015bda877a38c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:15:01 +0000

    makeinto path 179bite keepoff turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
