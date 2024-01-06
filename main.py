b9f3df038e31898f28e90e5ff54e27fe2cf0772f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:36:23 +0000

    passage steady proud taste environment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+print(os.getcwd())
