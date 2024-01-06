57a8f39211aa495f69ce18c97c451c8100872a4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:29:34 +0000

    tell long rub rarely back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
