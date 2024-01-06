64b5326ed69f79aff490c88b0508d004cd52caa8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:24:14 +0000

    plate apologizeapologise awake so sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
