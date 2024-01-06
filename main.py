e40885dd2bd9dae3d195bd177be6ac2687cbaeb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:00:37 +0000

    stand can responsibility standard spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
