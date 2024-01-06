508b1a7c0ea170204ba2a98b68d1cf71b9ddc0e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:52 +0000

    breakfast cause thorough film mouth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def square_number(x):
-        return x**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
