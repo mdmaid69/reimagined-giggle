c964d8cbd648f078ec59aaae0482c4d539278572
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:54:16 +0000

    although ill mend thirsty shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+print(os.getcwd())
