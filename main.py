c9b359f6d1a3e84891803f97238b1cf7ceb110f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:08:33 +0000

    quit operation line limit direct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
