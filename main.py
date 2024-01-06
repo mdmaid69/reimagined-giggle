a325053ecc4d14574baf9f59ff4c6544e24e0438
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:36:52 +0000

    our battery study spray regional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
