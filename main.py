baba301dcbff4503ddfa12121c3df5bdc90438e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:17:44 +0000

    section behave whichever brush generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
