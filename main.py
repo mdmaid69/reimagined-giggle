ee57596309b87d7263e2c17472df4ac966c009c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:39:14 +0000

    tough remember sale connected day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
