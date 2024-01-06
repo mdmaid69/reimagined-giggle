ce5f3cb2352c21e88a5cc0432137eaac23ce6b52
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:24:19 +0000

    over command side reduce without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_density(mass, volume):
-        return mass / volume
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
