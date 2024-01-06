fda9b345a2d4c5a40d2f83fa0e441a7eb224e651
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:18:53 +0000

    duty bread spring size hardly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def cube_number(x):
+        return x**3
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
