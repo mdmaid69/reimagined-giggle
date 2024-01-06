513fd227e1927871dfcf00cd91c2f2d65adc9b11
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:30:15 +0000

    who meeting hold suit everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
