3047087946d2727969d30a1edfa9289841f0b4d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:28 +0000

    fold feeling complete cinema during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
