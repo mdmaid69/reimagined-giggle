2489b7d72f555e47df7d87fd206b1da92b074e40
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:05:18 +0000

    inform shelter reaction callout pencil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
