50559bac45b00954ce450befb5259d16dc67561e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:25:38 +0000

    culture urgent set swing pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
