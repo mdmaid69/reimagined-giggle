b9877776d41cfd55ecb0544dba97ecca9c424a90
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:22:50 +0000

    bring straight beyond like plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_array_to_list(array):
+        return array.tolist()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
