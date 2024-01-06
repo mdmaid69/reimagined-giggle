1f0c721ec3b6ca14b545c0456e31e7eac84e4cfe
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:42:43 +0000

    hurry funny excited giveout reason

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
