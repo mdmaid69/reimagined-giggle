e1c637e4e47beb99bc29dd0500027026fc2af031
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:11:49 +0000

    572enter plenty sure demonstrate please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import time
+print(time.time())
