262ca30834aeadad23352122b5114e2882cf15bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:55:07 +0000

    fair available tidy nice earth

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import datetime
+print(datetime.datetime.now())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
