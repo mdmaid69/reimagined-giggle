cf30ee11eac337b6ca1f7e67e3a3a0821191ceb9
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:11:34 +0000

    loud mind hate exercise move

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
