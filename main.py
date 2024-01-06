4eee12a2057bc3bef5eda96f477d50387156f489
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:57:44 +0000

    certain attitude child busy commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def convert_to_hex(n):
+        return hex(n)
