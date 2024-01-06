3d5e1f30763d00d8637ddf69b4e264060dad66a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:55:50 +0000

    past curious cheek excellent must

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
