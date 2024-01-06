17ccd932af05c0aa1250cb90a766cb674b89ed65
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:29:03 +0000

    bang petrol performance criticism smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
