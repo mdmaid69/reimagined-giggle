c2ca7c41a870668fa5826cdd8108eb15aaff60ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:25:44 +0000

    probably nor everybody broken behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
