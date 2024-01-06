e5106967a3a86ceaa2a1df7df13ca3f8448292f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:29:13 +0000

    movement rate child central simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
