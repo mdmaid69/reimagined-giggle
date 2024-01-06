9d8e2fba2c85e74ddc28993d83df602a3c332dcb
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:00:21 +0000

    traffic demand start strong everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
