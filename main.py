b60ebc72b93955c6b07ad3041046cbc3fc96cddc
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:31:30 +0000

    downstairs pool job shout arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
