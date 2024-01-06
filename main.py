25f71cb4ea96c99bb05729c0bf72858293cb3bda
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:42 +0000

    smooth call characteristic chief relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        print(sys.version)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
