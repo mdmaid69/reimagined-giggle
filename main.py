a75580e46a4b31b941932f1ff9c065f070db3b07
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:07:45 +0000

    person set decide risk very

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
