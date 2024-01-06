e81a9db78f25b8603662e76c50daf84a36bf9bdd
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:35 +0000

    cause holdout cry below possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def square_number(x):
+        return x**2
