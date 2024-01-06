9fdd735c36c25b206189c274faf07a1ee1c4c8e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:10:18 +0000

    impatient some instruction flavour turnover

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_min(lst):
-        return min(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
