1951c9ea83f168c76b52fb8f6e399fe1d472ea1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:49:23 +0000

    comfort clean various spring collection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
