4f72c695ddc8aa0f49ebc7b814758e3a615133b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:09:13 +0000

    home squash dance interrupt among

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
