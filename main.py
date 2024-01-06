dd689ffeb400172131a8fc72e231ad0383b12ec2
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:21 +0000

    cruelty burst butter street serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
