470874bff650691261b02dc1c11a6a21b759cb75
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:08:32 +0000

    cap score with judge violence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
