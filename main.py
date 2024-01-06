a6bd4139ff03f948caa8bf6690033104465f4a12
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:33:26 +0000

    clear substance bear unable special

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import sys
+def exit_program():
+        sys.exit()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
