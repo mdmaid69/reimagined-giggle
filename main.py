1030cd5b07cd9ef4ed7ed1cad342ceec9604dada
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:06:44 +0000

    almost protect put it word

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
