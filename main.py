e948c9d0546bb395e13229a1b203916462572ac2
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:58:48 +0000

    actual sit official by attention

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
