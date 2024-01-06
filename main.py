7435728041086e496cbd95b8ca6bba9a33a6e348
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:24:43 +0000

    broken wide space nigga grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
