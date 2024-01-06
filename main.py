e6c0a9a38f5502c86b622e25925da2b97a97a8e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:44:02 +0000

    explanation forward step callat home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
