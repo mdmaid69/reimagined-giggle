05b8728b5afa890f36ef911218ea2977477633c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:06:08 +0000

    better old taste circle past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def get_current_time():
+        return time.time()
