1d29b11f43b3d7f3bfb8d7b0557b96e37e101478
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:32 +0000

    tight expect below opinion trick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+for i in range(5):
+        print(i)
