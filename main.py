bd58389b3a611b9ecfeccaea8c609d868b935383
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:49:03 +0000

    low damage street century store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import time
-print(time.time())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
