e179cd6379b3949f2bae8f70eb76ceec38331fe6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:10:30 +0000

    tend meanwhile autumn us something

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
