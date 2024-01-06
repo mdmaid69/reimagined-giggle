e0c5947d0182320e2d5e994ef6c29833f6d22b2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:58:21 +0000

    far future once spot change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
