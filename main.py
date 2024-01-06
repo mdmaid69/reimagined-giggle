7d83156494e65bb89f44642c3bb5073a06beda2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:59 +0000

    crowded might building hospital scared

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
