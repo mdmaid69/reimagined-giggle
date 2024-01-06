4e69b525cfa76b085281e43a9bd7658928dca1e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:08:30 +0000

    secret without interest shout issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
