bf0980275e7d0b6c48302455960187ba7b99599f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:49:17 +0000

    opposite become Ee talkback content

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def find_max(numbers):
-        return max(numbers)
+import numpy as np
+print(np.array([1, 2, 3]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
