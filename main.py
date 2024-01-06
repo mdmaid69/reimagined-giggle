d74668a619f5336db7fbf78a5039de388981411a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:51 +0000

    might congratulate boring result pronounce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
