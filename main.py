b7e5b9499a3edfc68f99f1e3e55fb18578107468
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:45:33 +0000

    mine except take damage swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import sys
+def print_python_version():
+        return sys.version
+import json
+print(json.dumps({"name": "John", "age": 30}))
