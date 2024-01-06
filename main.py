794acdc162329d0817cb9b1401c2de34aca2756c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:11:56 +0000

    sleep last connect swimming Yy

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
