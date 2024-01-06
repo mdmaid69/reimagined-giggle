ef8b6ea931d0449770907d23539cd46fd4a9fe08
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:41:45 +0000

    tool safe takeover pleasure getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
