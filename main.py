e47aa408a8b04c8bc809aa2037ffb258fca07c3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:18:42 +0000

    exact frame write declaration children

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_union(list1, list2):
+        return set(list1) | set(list2)
