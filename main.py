856a36db75f6fe790b65510fa1b2f9125311385a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:04:02 +0000

    guard Ww cruelty reply education

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
