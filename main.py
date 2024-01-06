f560cfd6c5522312d6c2659ce9f04fc6a09f8e1d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:32:13 +0000

    ought speed so drop press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_area_rectangle(l, w):
+        return l * w
