02c0bfc1b122827a782d49a8cd563cce5be1db89
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:31:19 +0000

    unkind geton comment emphasis rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
