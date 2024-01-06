6e55e2ee53b7e1cd3f5af11ced6038ef3785ad78
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:14:08 +0000

    drawing float match level market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
