f35cd9151221085d9443aa571bf0848a8517a32b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:04:15 +0000

    hate sense length space some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
