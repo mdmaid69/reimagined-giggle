a48a1e4f18fc0c6c5bd26b91c18addf270e99f7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:51 +0000

    aim drawing brush usual fair

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print(sum(range(10)))
