9d061701a6b6c9977bee2b55c1c6d4c3f6cc1e6b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:41:07 +0000

    mainly sister war pollution broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
