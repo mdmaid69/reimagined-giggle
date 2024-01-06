5ba4d39e7a4fd4a6c4e0c32b7a46a4bbd02c4229
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:37 +0000

    neither announce match manner chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
