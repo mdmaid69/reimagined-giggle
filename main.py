c6c0708de0daaf407e9d16e42b894a03be0b54a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:48:23 +0000

    ever together march doubt lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
