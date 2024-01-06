9d2dabb72f972e45cc131177878f9c28be400e4b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:49:12 +0000

    neat maintain guilty sight connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
