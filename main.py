780d564a945fbf0f5a8b4f41aa1b621aa82d6379
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:12:51 +0000

    tap female battery act intend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_simple_interest(principal, rate, time):
         return principal * rate * time
-import sys
-print(sys.version)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
