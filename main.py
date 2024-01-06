2f9c0b4d7a0a572eb3c0ecbe33402eccbd19ebb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:03:26 +0000

    draw thumb whistle previous single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
