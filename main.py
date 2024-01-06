2417af36430d95a7b10df441b80fda30478403fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:53:21 +0000

    talkback active lie impossible immediately

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
