1634a3afb8c5c194c0b2423ae5f25d9f3e4223bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:47:37 +0000

    hold drink have switch walk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
