bd5629745efc5c99606ba0abad035b86fbc93a45
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:50:30 +0000

    introduction square danger pleasant wealth

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+print([x**2 for x in range(10)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
