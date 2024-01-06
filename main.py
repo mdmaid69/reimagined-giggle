4d78067ba5aae6f11b4c96ffd6372e88741baf4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:42:41 +0000

    row spot mention talkback attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import sys
+def print_python_version():
+        return sys.version
