4795130a0533558c809d7d694063aa93fae88154
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:18:12 +0000

    cover authority emotion maximum waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
