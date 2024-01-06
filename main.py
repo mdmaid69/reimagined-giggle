e91759d3a99d597648c48329c2ea2c33e445a56e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:03:02 +0000

    contain person give bunch far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
