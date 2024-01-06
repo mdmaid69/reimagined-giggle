8c279183ae04bb8a59b1fb66a61d7c3b3a71bb71
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:46:32 +0000

    partner clean character rich complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
