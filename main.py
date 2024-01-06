8027c46e71cc815566b5624c8609d270f5078adf
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:22:30 +0000

    print yourself there much note

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
