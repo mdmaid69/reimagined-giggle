8bbb59e9cc9b64af5d5354eedfedc30c13e9335b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:48:05 +0000

    trap flat knock impressed quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
