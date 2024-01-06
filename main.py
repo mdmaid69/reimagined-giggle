7c6c6978e6caa4f342a2d91d9a6f36ab4bc3851c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:25:14 +0000

    boring food hurt sweet shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
