e2efc26221df19ee9e03a60d365001f84927b016
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:01:22 +0000

    person before safety finally trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+text = "Hello, world!"
+print("Words:", len(text.split()))
