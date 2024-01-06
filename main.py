299ad59899d8586ab2a56edbfab2b055639720cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:48:51 +0000

    separate sentence country refer city

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
