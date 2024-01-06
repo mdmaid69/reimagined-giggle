a56540494c13df9760ef748d2378d2e561efc8fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:42:22 +0000

    ourselves complain approve mend cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
