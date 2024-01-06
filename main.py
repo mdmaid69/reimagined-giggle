30996373a0604f29a9aeb5f7f17a526b76131896
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:03:44 +0000

    edge challenge discuss regularly train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_bytes(array):
+        return bytes(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
