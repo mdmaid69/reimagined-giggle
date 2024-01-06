d3f2c46d748eba543a35952b39d6108c564f5d86
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:36:57 +0000

    accidental speed love comfortable combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import sys
+print(sys.version)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
