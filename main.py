6411c8c27e65af2764f4f2bc8cc102ee021db512
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:00:11 +0000

    suggest possibility stamp this introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
