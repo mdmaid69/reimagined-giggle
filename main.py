621f9436a05c424d9143d1d07b6ab9c4d8b18bac
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:13:06 +0000

    feature flight worry depend into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
