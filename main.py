16eb6ba68576fcd29e14226dbcf06ecf9207ad29
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:15:39 +0000

    interruption move minimum easy force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
