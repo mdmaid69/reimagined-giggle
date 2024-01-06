b9f78bdb3e622b64da79603a920378fd1242be9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:38:21 +0000

    bring marry disappear benefit tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
