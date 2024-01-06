0ab79911c845dcc094beadb68a3a6733d8ab3581
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:17:08 +0000

    single could squash answer married

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
