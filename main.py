f9184289a0406a9607445c3e8cb6adcf22903b2b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:10:31 +0000

    wall split different convince fast

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import platform
+def get_python_version():
+        return platform.python_version()
