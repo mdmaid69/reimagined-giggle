a15519fff9b56fb3d8a934d83bd1fe292fcff01f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:20 +0000

    fire average hide complain hide

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
