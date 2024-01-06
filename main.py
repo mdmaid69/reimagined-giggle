a02657b052d89cdf76040f6acadf3e1857a3d76e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:56:34 +0000

    almost autumn steer period work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+print(sum(range(10)))
