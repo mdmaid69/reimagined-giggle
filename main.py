093fd013162f2a87402fd08d34103065f2e86f58
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:32:08 +0000

    place old manage slide godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
