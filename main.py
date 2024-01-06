2c88009075d445db461a266bd48533aa43fc24d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:55:13 +0000

    war insist right image goout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_acceleration(speed, time):
+        return speed / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
