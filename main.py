234dca9638b06f9c65dac700c2edb83126598119
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:28:17 +0000

    system speech show Kk factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
