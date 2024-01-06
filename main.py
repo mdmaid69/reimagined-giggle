75536f0ad7edaa6932e6306e9e733ea4583cfc68
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:39:54 +0000

    switch lend season understand badly

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
