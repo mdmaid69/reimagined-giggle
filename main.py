2a2824b68bccc271f87ac623b6d28e8163e1c06b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:46:55 +0000

    pair walk wonder firm earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
