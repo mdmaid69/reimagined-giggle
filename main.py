7b33cfd35a251dcc21d10914adbf31e8fbc9f144
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:44:05 +0000

    benefit question treat gas ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def find_max(lst):
+        return max(lst)
