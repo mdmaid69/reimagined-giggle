467e17a857166b2627cb4007b1d71782157b6daf
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:11:25 +0000

    favour fast could rest would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
