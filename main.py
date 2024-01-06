8f64336a5998931dfbee96d58a7da519bde07515
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:59:22 +0000

    underneath confidence cook deserve couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_as_repr(array):
+        return repr(array)
