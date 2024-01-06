9502862c40f8bcc15134ec949d240331a4a8f11a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:44:39 +0000

    violent dry mind exhibition everybody

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
