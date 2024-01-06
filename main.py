18b1c08fd2303eef010881d4a68246a9595b1a50
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:43:55 +0000

    really action pool convince secret

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import numpy as np
+print(np.array([1, 2, 3]))
