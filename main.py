a233a3b9cbe972eb4304ced59c3bc7241cb1fa0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:55:03 +0000

    score tragedy extremely in relative

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
