32c7879ef122ae31f543495c7dd350c39610044d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:27:29 +0000

    copy relax stand young direction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
