7435a68833c1439efa97e5194fe53f809dd4440e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:39:10 +0000

    let bunch collection safety cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def set_array_item(array, i, item):
+        array[i] = item
