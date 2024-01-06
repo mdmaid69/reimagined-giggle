f05d92b1da0b632575b9b205161a5e569f336545
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:32:12 +0000

    attach port fierce identify length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
