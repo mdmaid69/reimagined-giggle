5e3766c4e51cc8659b48028a30ccdcf91c535ede
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:14:27 +0000

    squeeze turnback hot service neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
