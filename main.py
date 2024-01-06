454a40eefeefc5b1b9f6c66295bfb66874bc3744
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:31:12 +0000

    turnout different entry share story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
