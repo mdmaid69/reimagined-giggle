838350dbdc3a80ce0c0e6960a36d6dc312876c19
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:40:25 +0000

    suddenly conduct shoot different inside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
