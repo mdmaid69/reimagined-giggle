721f2f1d42d8835739fd19a3b994fb85822822f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:43 +0000

    responsibility meal effect thin prize

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
