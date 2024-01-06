360ca32328617d76b0026253bd849555fe7e0e80
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:45 +0000

    line deliver carry workupto ignore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
