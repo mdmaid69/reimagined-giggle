c8100ced499c8b5e80dc5105c86006a915d1368a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:38:58 +0000

    little drink against dear marry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
