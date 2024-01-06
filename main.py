2f0a28340fd71efb8e4ccc15332551e20dc0fac0
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:54:25 +0000

    runover thin find habit find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
