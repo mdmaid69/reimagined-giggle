beaa0fcc2900c433fa3fbce123eee585b8ebfd7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:59:58 +0000

    sticky still worse feel drink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
