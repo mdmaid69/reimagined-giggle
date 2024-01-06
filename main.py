641cd2ed16344ad39aaaff872b19e4cbe1db610d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:38:49 +0000

    exactly thumb allow secret match

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_odd(n):
+        return n % 2 != 0
