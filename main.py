e908933b270c5dab0a3a3090d2148b8617591e92
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:11:30 +0000

    warning understand gap apologizeapologise physical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def sort_numbers(numbers):
-        return sorted(numbers)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
