7b24e0e65fd8df525a54e2585c62ceff16d3079a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:25:06 +0000

    dry cinema price crazy throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
