14c5eecc7d1c101192f1ccec97c59d4568095a51
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:11:25 +0000

    lonely holiday left risk ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
