c7b6babf0c45ca80c3335059ee2966f9f45e7d9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:05:37 +0000

    escape admit getup glad ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_speed(distance, time):
-        return distance / time
+def get_array_as_int(array):
+        return int(array[0])
