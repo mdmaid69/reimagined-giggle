e6dfaa398cd979b4f47730be5c18a1c1a3e8a5e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:44:00 +0000

    use confuse wealth small breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
