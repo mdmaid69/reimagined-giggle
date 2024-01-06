f0a9bb14fa1173d8eb53110735250ee6a9cff8b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:56:24 +0000

    kind murder complete themselves plastic

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
