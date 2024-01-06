6cd36b09b104b66c5c782aea3a78dc2525931a9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:25:13 +0000

    heat any reply pour insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
