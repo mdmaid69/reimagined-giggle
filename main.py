bc40852baf717ffe9e087166e937708e7bbe71e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:46:10 +0000

    regional loss alone sink comedown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
