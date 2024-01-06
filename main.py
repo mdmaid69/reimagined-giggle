25a8a7e2b45aafef2f86761a9a5868b28f98c7f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:24:13 +0000

    juice wake ilovedicks ill wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
