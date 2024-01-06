a65051f037aff5ec209c3f4816fa8caa713a5e07
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:56:47 +0000

    freeze inquire medium standard related

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
