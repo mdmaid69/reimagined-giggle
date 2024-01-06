3002b653cbf11386eeae049bc7e1f9bfe3faf527
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:24:02 +0000

    teach age but great book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-text = "Hello, world!"
-print("Characters:", len(text))
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
