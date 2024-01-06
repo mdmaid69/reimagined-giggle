d21de12b6c48949ebe7645562dd64fad9257c60e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:17:42 +0000

    drink frighten poor desperate air

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_str(array):
+        return str(array)
