fc6ebeb050e54a7c08fc42c34815e4ca4b365430
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:45:31 +0000

    warm fill ocean transport win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+for i in range(5):
+        print(i)
