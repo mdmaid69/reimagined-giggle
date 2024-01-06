0132c484cf8c830146d9a7843dba73dac2f9a6ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:35 +0000

    repeat win our frequent cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import time
-def get_current_time():
-        return time.ctime()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
