1765b9d60deb41535f56f11e5b9b173c5b06ac59
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:48 +0000

    return pronounce what turnback pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_repr(array):
+        return repr(array)
