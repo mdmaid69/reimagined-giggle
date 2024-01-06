7e294b6a5e9823d50fdc3988ac26760c2b8b95b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:21:32 +0000

    example store fire sea turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
