5c5b3606ee698f356228b8623e412d0799903f9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:48:16 +0000

    interview home introduction flat search

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
