494b8633ccbe8997330e144e88a4e24e648fcb4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:38:06 +0000

    desperate bear myself whistle wander

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
