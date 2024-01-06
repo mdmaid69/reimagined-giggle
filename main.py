b392b0087d37f6ed88fc6d4ad7bc73d30d0e540f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:06 +0000

    pump neither bit minute available

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
