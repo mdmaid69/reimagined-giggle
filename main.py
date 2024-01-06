dcfc287567c8e7a38339ca89bdcc575bacbe46f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:45:43 +0000

    mixture shape human generally cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
