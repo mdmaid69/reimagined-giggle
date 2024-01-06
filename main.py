071e75e7716f1df073872176ba91fa1703f61095
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:28:23 +0000

    past advanced introduce beat back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import sys
+def print_python_version():
+        print(sys.version)
