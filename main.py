efb551f13416a15a0123f6fa26272c96616634b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:48:27 +0000

    environment scare interrupt home I

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import collections
+def create_user_list():
+        return collections.UserList()
