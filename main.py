0f6977379c521909c12b85b294ced231c7afc90c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:39:38 +0000

    certainly certainly squash clear nothing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
