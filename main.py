6a7bb463a97c76791befdb311019d612bf521f6b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:38:55 +0000

    however so pretty regularly factory

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-from collections import Counter
-print(Counter("hello world"))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
