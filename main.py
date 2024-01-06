a32c1092e572c54c34ff17a3625ff3e63c239735
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:08:45 +0000

    birthday various drive belong lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
