0f53fcc29d9843acf1d3b9e4abfabc3ed0402aee
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:13:43 +0000

    simple day dick ear failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def is_odd(n):
+        return n % 2 != 0
