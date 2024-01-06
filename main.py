5f40363ddb262937e7e82f3eea440c6200f44ed9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:55:55 +0000

    out under cause shape round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
