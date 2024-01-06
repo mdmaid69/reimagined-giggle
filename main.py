460c9f2b32b237d09bc061cf2ddb965b2c91d059
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:03:37 +0000

    comefrom win disappointed black tape

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_base_name(path):
+        return os.path.basename(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
