800a4d10981ba135c756a7f98b4dbae753cdec36
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:27:40 +0000

    whom gate secret wipe identify

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
