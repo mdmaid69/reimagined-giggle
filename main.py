02d6380a0e68caac41ffcdc75af2c856efa75788
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:08:19 +0000

    noisy defend intend without why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
