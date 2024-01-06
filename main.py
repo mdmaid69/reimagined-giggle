8842263c6f7991b934d7427e19f031500681109b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:08:12 +0000

    explain style pleased ahead protect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
