cda67aa638a810ca30c0b98b27d855418b7e611a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:05:00 +0000

    harm pain Ll popular related

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import platform
-def get_os_info():
-        return platform.uname()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
