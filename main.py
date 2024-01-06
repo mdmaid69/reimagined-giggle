340c71310e0377d74fe0d4842abf3f5de39f400d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:58:49 +0000

    reach beginning permission nigga wish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import collections
-def create_user_list():
-        return collections.UserList()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def create_user_dict():
+        return collections.UserDict()
