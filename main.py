7f50fbf09049ad62a77d9db6f211bd227c82f151
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:14:11 +0000

    instruction squash whole food display

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
