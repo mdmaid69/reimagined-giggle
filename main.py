ac12e26858ffaa916a95acbc50f51edd8885f414
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:46:14 +0000

    roll switch joke keepout direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
