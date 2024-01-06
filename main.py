40a0cd75c195d397f6cef64b676194e3a5b21903
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:38:38 +0000

    behind cheap himself majority 179bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_speed(distance, time):
-        return distance / time
+import numpy as np
+print(np.array([1, 2, 3]))
+import os
+def change_working_directory(path):
+        os.chdir(path)
