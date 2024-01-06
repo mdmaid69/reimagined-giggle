59397f38fffd41a1b2bb5b4e693296ff36654429
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:16:32 +0000

    gap intelligent free character wide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
