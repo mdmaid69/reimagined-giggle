3ceda07af5842e0c36ebbd14b4879e06ada54fb9
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:09:28 +0000

    scale build seriously point sale

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
+import json
+print(json.dumps({"name": "John", "age": 30}))
