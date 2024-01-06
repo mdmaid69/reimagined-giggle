b8adc68bd621030f60180415e45f967957cd882b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:19 +0000

    exist breath car challenge firm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+print(sys.version)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
