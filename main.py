fa65a8f0cdfa8903f9f5510ef4efa049d4c8e74b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:40:44 +0000

    imagine probably laugh lock embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def clear_array(array):
-        array *= 0
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
