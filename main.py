938f63e3721f9d1ff672b5b5a71e6dede23949b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:39:55 +0000

    share runout play peaceful make

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
