96f1465d239f73dce476e7289fdd5bc58e3b47b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:27 +0000

    scrap danger border goon contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
