274a661c20caebb05ab51364cf7eb67b0caa9f39
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:28:23 +0000

    out breakout chat fold 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
