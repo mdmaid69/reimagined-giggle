3d7407a33dbfbd8c7b00ce2750ace42b4a8cdbcf
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:56:38 +0000

    ashamed drawing future total cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
