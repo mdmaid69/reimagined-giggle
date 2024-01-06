c3187a2b63f1f61f05c58ce0eb29d1706ccbea6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:31:05 +0000

    dry brave feeling let lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
