5d1e1b366a1865939d26d01d9b2f5c3c638b11ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:30:44 +0000

    performance sense bored cause suddenly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
