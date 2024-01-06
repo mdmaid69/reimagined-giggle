67a6b9172368df4e47a561833edb7502168aee74
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:44 +0000

    turn tomorrow noisy national godown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
