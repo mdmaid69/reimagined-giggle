095ca701cb15413fa16e83d91fb36a1451c1b141
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:36:26 +0000

    wind talk organization position image

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def square_number(x):
+        return x**2
