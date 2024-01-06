7308b13f3652f1ecc1a9c6bcc458b2ebd02b9b10
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:31:20 +0000

    lock badly reduce weak time

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
