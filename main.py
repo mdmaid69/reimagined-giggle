cb165232e81296fde6e7d1501ec8727351b2442e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:26:12 +0000

    clean unable correct duty inquire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+print([x**2 for x in range(10)])
