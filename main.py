27fa3410d3cf06f50c046e2a4f0d9dd24d5bd07f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:04:47 +0000

    consider complaint but where change

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_cosine(x):
+        return math.cos(x)
