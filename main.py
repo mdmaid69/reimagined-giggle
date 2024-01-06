ad195d470355ffdbd367d7492e56092897f21295
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:23:23 +0000

    popular standard floor telephone string

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_speed(distance, time):
+        return distance / time
