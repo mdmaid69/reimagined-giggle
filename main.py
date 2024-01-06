b9958ac1fa2ed3d3bba76016d902a5839901d85d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:42:12 +0000

    market can second violence joke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
