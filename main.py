da12500e5683a20d1635c8b3d04d68325b29709f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:50:50 +0000

    authority stone 568energy complicated government

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def convert_to_binary(n):
-        return bin(n)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
