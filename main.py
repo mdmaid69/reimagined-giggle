94710e0a07d50d54fab07437faef70ba5394b832
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:27:58 +0000

    571enough close thinkof measure day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
