b90c1565ef10a5b2f68cdd213f92612b804b4865
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:51:35 +0000

    famous reaction exaggerate 571enough blue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-print(datetime.datetime.now())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
