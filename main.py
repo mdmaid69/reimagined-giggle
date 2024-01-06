646ab0db756d23c7e6fc875a4dfde121fb338073
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:35:26 +0000

    trouble pour artificial crime describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
