2dd3df51b364c8aa93256ca6465a2c79212ea8d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:24:50 +0000

    film value burn centimetre common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
