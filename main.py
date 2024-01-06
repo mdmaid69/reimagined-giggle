84e7a0c37dab3953b9cfffafdfb7daa1bd32e6b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:42:49 +0000

    charge continue win height aim

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
