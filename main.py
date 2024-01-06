b63992dad3d9a73ba9526cbf3845573d27ad05ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:59:48 +0000

    change opposite burn butter claim

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def is_odd(n):
+        return n % 2 != 0
