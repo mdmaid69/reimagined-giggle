f4971528f9ac168ed566e9defda8d58d1459f62b
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:16:53 +0000

    talkinto rain idea save murder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_speed(distance, time):
+        return distance / time
