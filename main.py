9bc511a8d3a4b0f383eddcac7eecf8d4adc9458a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:30:41 +0000

    goup breakup contribute maybe floor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
