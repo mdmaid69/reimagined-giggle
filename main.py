c2427ae3ca63f459f1d7c0bc44711fffbcb8567e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:43:45 +0000

    positive instrument for impressive apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
