b15546eb1611e365170ae25b808a85a91baadf55
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:35:34 +0000

    government mouth system save pollution

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  def cube_number(x):
-        return x**3
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
