bbb52b11be99e275b738649bb4afde8f68c39998
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:10:29 +0000

    cheque prepare behaviour remove touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
