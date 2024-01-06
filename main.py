c1466f7d21c0027be39c2b80251ee2d1afa3825e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:28:57 +0000

    open life exist closely goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
