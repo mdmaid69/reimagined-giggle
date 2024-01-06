fda37e24f469498c0005ce29762cfe9231ed7046
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:59:00 +0000

    hollow nature mean if noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
