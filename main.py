ea8f93ef87eabbc2d7082b3e17058f84dace5692
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:53:44 +0000

    plenty passage post book snow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
