13a4ead7bf5c52cd5d53abb06f63ab93af94df12
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:12:23 +0000

    wide school strike naturally today

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
