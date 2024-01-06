7dfd847147b97bcbf9d120384a8ca9f2fbe48223
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:26:08 +0000

    accident authority lot care label

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
