a031df0914da6627e812cb6bf73a904a284fc4a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:56:15 +0000

    street declare handle to period

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
