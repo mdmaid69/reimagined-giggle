5391f7550cf816c808d7a27e7691760f17d2b1bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:11:49 +0000

    load connect pause at message

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
