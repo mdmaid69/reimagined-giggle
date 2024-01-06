027710c4e08500040188fc66be9c421d2bea8f50
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:02 +0000

    project turn measure down cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
