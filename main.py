2088faab74827a7bd21d14f1a52f779823d9bc99
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:01:01 +0000

    rely trust comedown positive minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
