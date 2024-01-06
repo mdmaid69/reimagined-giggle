579ff2311d145b1d94ef5527c58502791ffe9376
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:01:18 +0000

    past present brave belong pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_index(array, item):
+        return array.index(item)
