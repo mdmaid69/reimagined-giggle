ec014c41c089be1f49b7e9c0a39304d4f65aefa5
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:36 +0000

    ilovedicks better birth hang brain

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
