005fb3c47c7370c825906a73f79e5b652cb15567
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:40:23 +0000

    study frame hurry pocket you

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
