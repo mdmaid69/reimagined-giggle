c53cf77f4a19a89f22c1e5c9f4009dd25f9a528e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:35:46 +0000

    one runinto carry tear free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
