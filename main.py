72de752ad5cf53dbe32c62c202ed8cd8e5e24871
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:40:40 +0000

    let maybe own ill doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
