199e458bee5e18de3414dc5880b4779ef49f7649
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:35:56 +0000

    bottom wise please moral Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
