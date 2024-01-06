85c36ad7141e58c1a56b095ff4b5374257b9f4be
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:11:59 +0000

    surface mass base dare brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
