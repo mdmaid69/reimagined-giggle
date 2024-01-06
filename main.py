b59c9b67976be390ad91e7c42efb8a86720b61f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:42:35 +0000

    nowhere cover uniform open active

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print(sum(range(10)))
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
