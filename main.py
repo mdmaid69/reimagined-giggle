4329cb16780be6c6aa980d217f21b06f0f56d21c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:24:35 +0000

    throughout risk remove weekend precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
