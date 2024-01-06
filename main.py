745e5cf2d32d515d68ef4a7fc68458f4c3ccb88e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:19:09 +0000

    hit contact dress burn woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
