321800586ee20cfca5ec13d36942d2a0a86707c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:20:02 +0000

    truth word pretty probably flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
