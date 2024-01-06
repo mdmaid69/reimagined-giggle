c83b1a93db8c61a36e813839e385dff4713131a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:59:53 +0000

    turnup proof smooth chin quality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
