49879ac26df38e8c879583615327a8f1c888b395
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:52:22 +0000

    hollow spot shelter burn wonder

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
