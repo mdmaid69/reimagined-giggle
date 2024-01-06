f91fd1e8452f5cf1ac98a9b366c187473928bbb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:30:39 +0000

    border arrange inside bringback air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
