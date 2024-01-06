4db0ec02040861dbc866b44bc64676e14da3d203
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:58:31 +0000

    related Hh talkoutof find police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
