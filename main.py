f6277b41802d58dcb66a3ca6b2135c8b0b067b31
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:24:04 +0000

    design project slice downstairs forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import collections
-def create_stack():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
