32d5a4cb3f0b2053fe159a17239add34ca529f71
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:31:24 +0000

    event justice now after wander

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
