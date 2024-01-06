9c38f98bf55ee1f267108733e0f0afb1a73a0382
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:12:19 +0000

    describe through everything value yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+print(os.getcwd())
