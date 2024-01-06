7eb4910210f69b2ac96767e5f629bbd5c7ac28bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:21:43 +0000

    thought step shock wall exchange

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def convert_to_binary(n):
+        return bin(n)
