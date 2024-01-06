aa532c7ae5a62f5ea1f74907f9c947550bca0fff
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:43:39 +0000

    unpleasant plan case swim present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def square_number(x):
+        return x**2
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
