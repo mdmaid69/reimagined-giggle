97c89981187a871e0688337685a6f5b1cfea117e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:07:40 +0000

    history error experience unless do

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def find_max(lst):
+        return max(lst)
