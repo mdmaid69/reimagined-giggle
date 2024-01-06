7bf6efb3330679585d4588d52b08450c60220dff
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:23:01 +0000

    thin common love morning track

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
