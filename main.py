c8759bbd47804ecc81fc94ef82837a24b7d91f64
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:10:22 +0000

    succeed stranger pretty climb confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
