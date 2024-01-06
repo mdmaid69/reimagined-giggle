653990140d7d8a5e72ca4a7dd725855f4653fed1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:23:58 +0000

    meal Zz hello edge convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
