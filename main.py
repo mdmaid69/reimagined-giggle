4d8ff3a241a134e911417175b6714c423ab8b0bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:27:27 +0000

    top storm left tough black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
