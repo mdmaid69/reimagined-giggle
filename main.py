5eb99582358d79963c12f988fe31acac1fe6d8d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:54:32 +0000

    pressure crash smell level dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
