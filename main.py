b8cc92dd798b62efd472e388d82adb66d0494c0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:59:25 +0000

    hide home unite switch opposite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
