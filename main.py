0a8d8d7a9087a04fcab0a94122bc7d3bcc134dba
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:16:04 +0000

    tread plenty black stick swimming

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
