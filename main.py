9fd8e0acc7f076b99bbf58ea54cc18c5b37a81e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:00:37 +0000

    extremely Ll share party my

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
