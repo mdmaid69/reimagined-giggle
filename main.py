6f8c1a7eec0635c6153233b0fe23adb6cd1d74ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:15:55 +0000

    fresh powerful office scream soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def is_odd(n):
+        return n % 2 != 0
