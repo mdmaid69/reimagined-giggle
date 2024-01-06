6c342e903ebf2cd2b04049bd7dca7db5cd129e2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:57:45 +0000

    total through wall drink collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_length(array):
-        return len(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
