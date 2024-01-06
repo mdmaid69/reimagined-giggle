c52d6442e1846c925d28b6fa06892981dfd911aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:25:51 +0000

    rely married mouth aware light

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def square_number(x):
+        return x**2
