336e061086617dc8afdaf1ac399027f3125881cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:29:16 +0000

    drawing care Ll coat politics

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
