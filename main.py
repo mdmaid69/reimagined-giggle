24031beaf82c6de61a9dc148249ab4b7292f50ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:03:39 +0000

    closely radio disappear child though

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
