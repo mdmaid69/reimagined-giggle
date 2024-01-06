3c637f923ad0d7e018cd448d5758c195c1e61a53
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:10:37 +0000

    spring comment walk injury contribute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
+def calculate_force(mass, acceleration):
+        return mass * acceleration
