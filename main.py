e017d5fcd1f2358fa1fc47e8700357dec5df8a1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:46:17 +0000

    mouth centimetre keepfrom he cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
