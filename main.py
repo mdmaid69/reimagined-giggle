d1b74082b59631c73ee5ca53fb530a6d43ca46e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:46:51 +0000

    loss polite lead eat clearly

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
