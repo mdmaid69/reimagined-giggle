037cdd0d3105e80be1224051f3552e26c6176177
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:43:58 +0000

    joke report Tt exaggerate land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
