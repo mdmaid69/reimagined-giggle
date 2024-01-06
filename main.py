9a5117f022deeb2208efa4a7c04f502c3b61cb0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:43:20 +0000

    fight 573entertainment embarrassed welcome opposite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def remove_from_array(array, item):
+        array.remove(item)
