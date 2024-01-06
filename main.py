cd7efc5a01fdedbc5f26de979147d906320e85a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:50:46 +0000

    turn furniture corner tune wild

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+print([x**2 for x in range(10)])
