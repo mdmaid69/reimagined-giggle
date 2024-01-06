57e48a3c93d77fedbce9253ea0870eb69bec0542
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:16:26 +0000

    true hold confident bother maybe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
