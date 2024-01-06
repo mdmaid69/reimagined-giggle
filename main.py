c2a463a877f165659889b6c540ec3518786e8d08
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:32:50 +0000

    round holdup other substance challenge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
