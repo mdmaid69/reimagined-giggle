cda5655b7ba584040d5afa591fafd15ca9ca1c3a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:01:47 +0000

    relative laugh shout tape deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
