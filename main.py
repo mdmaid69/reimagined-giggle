a3f2d80c275ff9f2b31873218d17c0261d5212ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:55 +0000

    independent excuse used centimetre wonder

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
