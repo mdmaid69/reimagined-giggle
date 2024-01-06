997172ebd31887ee0731525d26ceaee0ace94aa2
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:34:39 +0000

    brother meal sorry comfortable soak

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
