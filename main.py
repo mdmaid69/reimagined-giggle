0cf5b26b1dbf913a0433197dfac7db804f2a1101
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:16:36 +0000

    divide politics scene film pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
