52a8391e1ef83bfcb1729e4df10056fee2bc63db
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:20:22 +0000

    be lookaround slight wave sight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
