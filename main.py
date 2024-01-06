279e7fdf2be0bd90c4817b37b323acba3ecf7369
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:16:30 +0000

    part consult tooth hard fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
