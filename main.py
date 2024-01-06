21c9bc43b0f09491b5367311c2d00eafe3edd82c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:27 +0000

    related mess 569enjoy rubbish track

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
