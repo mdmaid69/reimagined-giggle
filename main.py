3b71d141051981ccf25cce92e041aa11975eb997
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:37:37 +0000

    able regional connected rough book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  def reverse_list(lst):
-        return lst[::-1]
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
