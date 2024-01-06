321c7f403d1662c53f1425f6becc8c5089a80b21
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:16:42 +0000

    ought club copy chin prepare

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
