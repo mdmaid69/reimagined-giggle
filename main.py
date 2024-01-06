aca848589f16c52d74b8d4cc5fea3e1391faefbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:37:21 +0000

    571enough weak old cum case

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
