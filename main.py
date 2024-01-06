e5b5148351d72a69f01e4598a4cd872e0beb2416
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:09:37 +0000

    takeoff fault collection drink again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_list(array):
+        return list(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
