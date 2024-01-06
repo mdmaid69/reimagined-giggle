7d0b4dadd086f8485bfbafb86d6b2f76935d5c5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:25:57 +0000

    see bringforward powerful list loose

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def delete_file(file_name):
+        os.remove(file_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
