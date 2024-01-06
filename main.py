55093676410973531223f7bc1ceef4c56e6a3251
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:02:50 +0000

    secret long speak bringdown thick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
