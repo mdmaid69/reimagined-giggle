ac0b011f4c1a3edc45d7e84bbbcf541020611b84
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:02:53 +0000

    tidy picture sure careful intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def append_to_array(array, item):
+        array.append(item)
+import sys
+def exit_program():
+        sys.exit()
