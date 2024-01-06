e8883e047617493fe99dcbab3b6d527b4c46d339
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:42:52 +0000

    aim hurt slip artificial suppose

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
