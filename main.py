482bf01df72660c3b989363f4cd6bdfba08af33f
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:38:25 +0000

    fix contain distance care measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
