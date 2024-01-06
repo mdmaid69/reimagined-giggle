d4fa17eec9a49bdd343ed0dbe70368b89faf65a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:15:40 +0000

    interview Tt Oo worried stretch

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+print([x**2 for x in range(10)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
