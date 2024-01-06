eefde415099f34988d605a07c3494e773e7c559a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:58:55 +0000

    ask friend until new film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
