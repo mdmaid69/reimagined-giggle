24a0b5953072a8860edc1b3fa9c8d89a52eb898e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:01:54 +0000

    argument crazy anywayanyhow cool body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def square_number(x):
+        return x**2
