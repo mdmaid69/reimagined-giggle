3786dcc4263ac31cacb23c2a340b64b2781911ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:42 +0000

    surface check buy gooff stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
