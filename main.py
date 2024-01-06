ceedea57dc567f03e1d9c2d501139113f148c430
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:40:44 +0000

    neither big answer catch whistle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
