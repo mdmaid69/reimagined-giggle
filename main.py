df996dbf6e103eefb4c47d784199c8258a9637b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:38 +0000

    direct possibly feeling same door

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
