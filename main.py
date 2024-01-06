2a55cf391aa1b736366092ff4fddd2fc7a0e08af
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:09:08 +0000

    field exchange stamp university broken

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def delete_file(file_name):
+        os.remove(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
