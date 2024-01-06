c4be29d37d5f11f43ba2349108ba7466e5ae34ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:33:33 +0000

    punish who any lock hope

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
