a8c959bd335ef7cae69df55d205b737baf31ef7c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:37:09 +0000

    take terrible evening stay area

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
