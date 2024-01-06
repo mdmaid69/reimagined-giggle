da11fe0496e01b32fbc1f7002c5458d784e6fdf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:45:40 +0000

    damp weather as store slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_base_name(path):
+        return os.path.basename(path)
+  def count_elements(lst):
+        return len(lst)
