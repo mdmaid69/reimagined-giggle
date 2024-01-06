3f28b42a3f5c2f7012345970d0f939fc0c2b0c69
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:03:49 +0000

    bet maintain regional tear carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
