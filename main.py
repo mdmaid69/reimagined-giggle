ca1ad16b83a2ec44c86ba26bfad1c1ff49ccf9cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:06:25 +0000

    such direction frightened goback(to) bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
