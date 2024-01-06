a2d7ef04df7bb24d9d97048dde500ee5fd33c38a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:35:36 +0000

    Kk join view tall underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
