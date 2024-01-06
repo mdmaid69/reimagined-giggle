82676aeebd3af7491c41646339983ba36717f1c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:32:25 +0000

    accident expression consist flat squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
