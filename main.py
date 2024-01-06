94a439e9f5c703f4166595c7c9a056b0c79058da
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:44:58 +0000

    bother danger path now part

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-for i in range(10): print(i)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
