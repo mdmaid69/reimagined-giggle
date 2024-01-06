6925990b8f568cb31b42eaa407e7553ebbd52e78
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:59:08 +0000

    shallow memory discovery real classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_item(array, i):
+        return array[i]
+def count_elements(lst):
+        return len(lst)
