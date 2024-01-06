9ba0d0ff2d5f7424115246d9dc378f8d6e0ac293
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:09:13 +0000

    face careless bus plan statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_repr(array):
+        return repr(array)
