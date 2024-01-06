26521a0e65b6e1eecc13a9d5721dbd996309563d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:25:27 +0000

    dinner become shop usual government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_power(work, time):
-        return work / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
