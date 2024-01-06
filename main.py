fa9e153a3c244be7fec92bbc3024659a5cd41295
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:03:41 +0000

    smile us arrival manner so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
