2929b78beddf030a79ca3bd0ea959266d9b2a0ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:11:07 +0000

    no familiar uniform crash course

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_octal(n):
+        return oct(n)
