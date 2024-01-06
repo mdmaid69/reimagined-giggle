f93bd5c72211966f22cfa705ccbd1cdeeb02dca6
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:37:26 +0000

    several fresh getout pause advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
