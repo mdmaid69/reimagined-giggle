869842fb204a9ffc946a9048b27bac9a4ad797b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:11:17 +0000

    low block sensible tell pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
