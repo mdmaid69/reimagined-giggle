80ea65448844da92765b66ed476dee1268a597fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:13:32 +0000

    fortunate complete dare much female

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
