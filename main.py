80f8c86ac825cc2d72ae3c01afe2aa959b22a047
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:31:28 +0000

    mainly habit blow blind home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
