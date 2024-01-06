5e8199a2c32d47155f915536eecff9f20584f6f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:33:25 +0000

    564encourage understand creature cinema behave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import sys
-  def get_python_version():
-        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
