b6037b87896ac9a3edd415f71ee8328e44f9407d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:48:22 +0000

    proper sign type anywayanyhow extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
