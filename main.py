60e8c7bdf4e6c038034af5fc99969853eb06ab8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:06:31 +0000

    field speak sort school shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import time
+def get_current_time():
+        return time.ctime()
