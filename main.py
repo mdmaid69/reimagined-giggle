63477d96cd6b9c83c47c892a07fc87f3cd86bcb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:39:13 +0000

    yesterday taste thank shoot never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
