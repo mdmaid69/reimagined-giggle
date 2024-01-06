dcee29f82fd9cf0d5399e82b6a57d03cab8efe04
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:30:16 +0000

    strike sorry loan straight mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.time()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
