719fe11559d768a79ead9947e1ed89ff884d44de
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:29:40 +0000

    edge private pretty squash climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
