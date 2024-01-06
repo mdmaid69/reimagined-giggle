965adaf42547642312dcb1f5de0bf13e2edd3615
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:59:35 +0000

    abroad pressure hungry consist road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import datetime
+def get_current_date():
+        return datetime.date.today()
