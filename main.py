90901a0e06c8b5f5433404cbe556fa285649ad76
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:54:55 +0000

    giveback emotion and middle appeal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
