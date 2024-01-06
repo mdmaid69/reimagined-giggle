f11e6d5a5144e6ffb8399a9ef8f2e713e8cee46f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:01 +0000

    noisy kind down pay air

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def get_array_as_memoryview(array):
+        return memoryview(array)
