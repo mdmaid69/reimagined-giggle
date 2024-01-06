0558a79c7e2c0cf2f53b49ef8e768200c3907971
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:36:51 +0000

    thumb card ought surprise city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
