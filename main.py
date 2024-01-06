623ac43055caa2398700d2fb103484f5830d4604
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:19:09 +0000

    neat Mm by top something

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
