06b66dd9d4c5969627bf939c6942adff20c60c87
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:55 +0000

    identify food warm wander satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def count_characters(sentence):
+        return len(sentence)
+import sys
+def exit_program():
+        sys.exit()
