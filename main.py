ff74de14ac4cf51f0f00a9299d5274da7d9696b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:39:57 +0000

    bus weigh flavour centre sport

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
