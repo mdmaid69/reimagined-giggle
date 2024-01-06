7e0244ebb5f13a1d22592a8483d49a55c603483f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:31:28 +0000

    repair then thorough rain commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
