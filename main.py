ab75f056cc97b6d699e60f1f83bc8dfd43f7ed30
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:27:17 +0000

    theirs lucky friend direct basic

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
