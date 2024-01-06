99d088e40c2c7331f04c10b76e15e622f6ebe059
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:39:31 +0000

    normal stage rely false maximum

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
