0e3108d31e82beedcf554cb7d0e7a772a0d39866
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:02:32 +0000

    platform Ii workupto operate afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def convert_to_octal(n):
-        return oct(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
