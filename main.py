0119241b94519d782da91c7af92ac9df91a336ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:19:22 +0000

    any protection thinkover wherever impossible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
