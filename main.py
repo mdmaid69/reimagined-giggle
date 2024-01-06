7412ee64aa41364abd38b97e1bb0be0d8d1e3f8d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:22:10 +0000

    tradition project moral difficult upwards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
