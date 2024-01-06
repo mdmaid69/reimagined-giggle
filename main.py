0b0ad23cb6831304190390bad9a73848bd1c4aab
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:34:04 +0000

    though feeling plastic allow here

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import time
+print(time.time())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
