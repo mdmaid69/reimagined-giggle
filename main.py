84bca5dccb2fedb83c278de77a8f84979f9e3ce3
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:23:28 +0000

    declaration trade same attitude drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
