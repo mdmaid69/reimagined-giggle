bbaeff021b228039ef567e901871d11a1ef9aee1
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:30:09 +0000

    squeeze deep most style hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
