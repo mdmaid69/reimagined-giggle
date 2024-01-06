7abc60411e53db7eb5ca073b2071dc31371b24bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:09:39 +0000

    exchange close grand win spite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
