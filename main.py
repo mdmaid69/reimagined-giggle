6d4490c2b9c724acb1fa90b1e406763acb624cac
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:35:01 +0000

    worried door identify minimum heart

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
