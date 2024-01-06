6d204a97eec8142479dce1b39e9a2c2241c4d374
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:18:29 +0000

    land indeed over goon put

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+print(sum(range(10)))
