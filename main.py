c3d1dab960987975c3821bca9305c4f452ecd945
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:32:20 +0000

    like wide our bury disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
