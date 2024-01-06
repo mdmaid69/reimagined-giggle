62bd03062ad08ec441ce45375fc1cd3b0b4d9741
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:47:56 +0000

    warn workon suckme above tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
