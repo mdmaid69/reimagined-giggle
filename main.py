60dbfc84b29aff6bb5b6a02fae829227a3972c61
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:37:28 +0000

    fight recover start high rank

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
