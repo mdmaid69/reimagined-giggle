ca966cccdb62621dda8f3e90050cef8f6bc3cd2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:43:32 +0000

    threaten wipe pinch bored hide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def convert_to_hex(n):
+        return hex(n)
