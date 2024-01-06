9ef4624b6fbc4a89daf63ddc65bf982ffc304fd7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:49:21 +0000

    Ff light shame splash fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_binary(n):
+        return bin(n)
