8ab4e9321ed0ebd70d9af1d0ac8e5cd09c249383
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:40:54 +0000

    embarrassing forget shrink provide generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
