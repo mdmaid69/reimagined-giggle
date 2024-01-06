57cc41de6816fab5c21274556ad37c5fd52ef89b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:40:51 +0000

    wrap cruelty thankyou peaceful content

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
