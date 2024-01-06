f97ecfbcdc14be8d9feea71496c3cab4abeefcba
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:50:39 +0000

    century suppose tidy choose confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_user_dict():
+        return collections.UserDict()
