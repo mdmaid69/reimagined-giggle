4fa9b43097e071dec5450611f53df68fc3605c7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:38 +0000

    press able inform dangerous prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
