d9b32c4b838c217302db719d3f7ba95f1fd5a1f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:34:59 +0000

    also powerful direct sister preparation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
 import csv
-def read_csv_file(filename):
+def load_csv(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
