1e80efcf37118c5c5a62852931feebbc39b19b2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:40:29 +0000

    whether sentence reason ready exactly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
