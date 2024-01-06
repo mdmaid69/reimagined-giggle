27c80ae62025ef6204927acdb81eeba91130e9e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:17 +0000

    cruel divide reach flow excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
