600c997769c5dce43734a5f87ad8ea4bff6ed876
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:12:32 +0000

    wind commit disapprove talk sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def square_number(x):
+        return x**2
