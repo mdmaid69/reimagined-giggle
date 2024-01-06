6d0388ea47467c2bb2d101aa793d8de0c59f6253
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:19:49 +0000

    language rob instrument sound wish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
