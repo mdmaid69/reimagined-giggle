dcb8b9f8a5e828902247799f0f888536be7e2809
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:29:37 +0000

    convenient meaning cause trip similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
