1faccefc6b13a665363b6f08d643b32b93739bf4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:51:37 +0000

    search capital cough join lean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
