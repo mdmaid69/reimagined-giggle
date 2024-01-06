9b5a00c9aef2b0883fdcad1bb11ba80559fd5909
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:13:12 +0000

    fold seem cross goon house

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
