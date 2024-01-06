dbb3fb4c35f4d03349e765e82f0e468e602b3e20
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:58:53 +0000

    nigga well forward force persuade

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-name = "Python"
-print("Hello,", name)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
