4f022a4bcfe78ec2d95be4ee99271796ac620518
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:34:45 +0000

    impressive vote seem rush then

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
