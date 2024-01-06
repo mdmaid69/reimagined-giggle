cd4d3b755b14e61314ea2126adf9c344f56405d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:28:01 +0000

    wash wild pride divide cut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def add_numbers(x, y):
-        return x + y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def count_words(sentence):
+        return len(sentence.split())
