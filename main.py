fdaa16f26911a71ab29515d125563602076a8c06
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:28:56 +0000

    approval sing shop disappear possession

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def create_priority_queue():
+        return collections.deque()
