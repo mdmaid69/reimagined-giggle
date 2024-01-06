16a974238896b30d5aea3dab62cec8665c5c4906
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:32:34 +0000

    question deal famous properly tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_density(mass, volume):
+        return mass / volume
