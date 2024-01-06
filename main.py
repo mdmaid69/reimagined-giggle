55be5656c05ed67ccaffea7457dd3c242b62ff90
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:27:18 +0000

    measure deliberately place answer pronounce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_speed(distance, time):
+        return distance / time
