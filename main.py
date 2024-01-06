ff9d5ea69052f8c54fe2a6153bcac09629758f93
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:54:56 +0000

    makeinto corner cause temperature feed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
