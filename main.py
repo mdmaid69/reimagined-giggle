7e0951cdbf40040f3120989bd2c1ed9280b05046
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:29:37 +0000

    lookafter become middle tend confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
