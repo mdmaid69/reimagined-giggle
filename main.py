9c1b13b1b48e50cd38e4e124d0f3526bf8ed1956
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:51:55 +0000

    show relative when rule cum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
