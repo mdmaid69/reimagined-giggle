7f4b0f29b53630ce01c15323f9c975bd664d7773
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:11:47 +0000

    drop alone involve unite above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
