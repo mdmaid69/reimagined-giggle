8c0884bac59caa4fd81d86de50a767fe4d9e98d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:28:05 +0000

    ear Nn whistle advantage there

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
