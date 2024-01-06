f2d744de114e47428a1cd5955e7cd3c4b46535c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:12:08 +0000

    maintain give godown Vv degree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+for i in range(5):
+        print(i)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
