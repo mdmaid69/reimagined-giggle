9bf38ec33b8814dbb5546644d7aeb9b867c0191c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:25:41 +0000

    ordinary police test cheat impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
