024157ec3fb4684aeaf473599127c3906e7d3232
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:22:49 +0000

    yet close dark past keepup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
