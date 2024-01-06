8fb6b96e93650d576763b57f27bebcb417e0c8c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:51:38 +0000

    soil both oil lucky study

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
