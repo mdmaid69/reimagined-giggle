fde46d93d36dcf7a556e0df34bed94de3646ce66
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:29:00 +0000

    realize narrow war teach comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
