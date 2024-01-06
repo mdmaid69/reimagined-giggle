5aba6f62c55a8fadee17cfc5dcf9f689415aad7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:39:02 +0000

    mysterious dish negative yesterday makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
