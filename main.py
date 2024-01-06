610b8e9124ad9d4944a5fd6a27a61a9d169b8635
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:39:19 +0000

    oil track probably skirt lose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
