6ba9fbc9b55048dd551377c040984142786456fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:10:37 +0000

    fail science correct down watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import sys
+def print_python_version():
+        return sys.version
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
