2496b60febde1673c65792fe28bc10aaac6c79c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:27:54 +0000

    supply rub unlikely coffee noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
