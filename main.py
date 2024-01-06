9b98f4651bd9b62642787fdc714d67e63bdd9ca5
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:12:00 +0000

    through surprising fierce fine normal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
