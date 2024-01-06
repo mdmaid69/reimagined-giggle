4dbc6e672cebdc869b9383882cea1b7cb4679efd
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:43:47 +0000

    drink develop brain instrument defend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def subtract_numbers(x, y):
-        return x - y
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
