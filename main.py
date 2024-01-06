aadf03bb9a9c117e876b756baf81d5565d3a7774
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:26:26 +0000

    protection ignore authority drawing exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def square_number(x):
-        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def sort_numbers(numbers):
+        return sorted(numbers)
