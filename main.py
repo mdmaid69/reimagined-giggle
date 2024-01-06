2a09970046b852de8a2626aa8ce2a54ef29d08af
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:23:46 +0000

    accordingto time character ordinary tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def subtract_numbers(x, y):
+        return x - y
+import os
+def get_current_working_directory():
+        return os.getcwd()
