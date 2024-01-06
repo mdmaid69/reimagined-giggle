9c403705da80ebc475800213e54f65470fcd49fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:10:45 +0000

    pure about my love goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
