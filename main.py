91616a14b4ffca41da02dea391577a48c34fb7f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:12:25 +0000

    getoff comeout delight machine belong

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
