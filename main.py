15ccfa742e304cc6a51043196c7a36ffe07759c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:29:43 +0000

    wrong respect arrival aim real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
