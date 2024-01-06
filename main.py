28f8517bbeed797817542ed46aa3b9c6572a50df
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:04:40 +0000

    large be transport subject cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_max(numbers):
-        return max(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
