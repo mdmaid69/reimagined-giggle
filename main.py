a4d273385e36659ca95b1f25a9b3792702502b15
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:37:28 +0000

    consist well firm proper out

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
