cd1a90b40adc9fc2806ea97a64810a8bf4c54e52
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:10:12 +0000

    fill straight afford holdon runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
