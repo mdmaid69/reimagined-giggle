2ae159ffa8808391b1794bcdebf95454f9889f07
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:04:49 +0000

    after bill combine image risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
