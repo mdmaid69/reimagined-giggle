367b8a0d0cc2e602a5f374d9adf666aa61dbdc9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:50:13 +0000

    father mean real Gg order

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+print(os.getcwd())
+import logging
+def log_message(message):
+        logging.info(message)
