bcf8a386d2865b11870f6be58956eecd90a386f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:12:11 +0000

    absolutely thing software ground authority

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
