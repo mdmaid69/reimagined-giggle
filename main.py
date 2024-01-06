27d6c409a7411de7768f38011aecd76e773d3f53
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:16:32 +0000

    lookaround bored nothing almost present

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
