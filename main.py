1df2ab27a38e51b884c1a0481798fed823de665b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:15:13 +0000

    offend soft image yesterday affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
