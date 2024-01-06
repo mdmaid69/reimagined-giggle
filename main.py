9814a99c0e79af4c9eac6f96eab3601e52380a50
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:27:39 +0000

    welcome allow shape offer place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_area_rectangle(l, w):
+        return l * w
