661e7f722e87a691d61709258a47bb072eb717b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:05:05 +0000

    woman search but you score

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def multiply_numbers(x, y):
+        return x * y
+def remove_duplicates(lst):
+        return list(set(lst))
