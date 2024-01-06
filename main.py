4dd6659425fcaf7c474adcb2f00959f8c55dd773
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:01:42 +0000

    tidy hand involve argument polite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+name = "Python"
+print("Hello,", name)
+import array
+def get_array_as_set(array):
+        return set(array)
