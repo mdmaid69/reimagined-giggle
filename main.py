5c64b32335ec8319d27609ada9a7dc66a1c354c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:02:03 +0000

    extraordinary recover hole mistake taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_float(array):
+        return float(array[0])
