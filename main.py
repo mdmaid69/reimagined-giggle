240b18e86d40d86be7e825073c9666000e657147
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:31:29 +0000

    eat light open fairly direct

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(a, b):
-        return a + b
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
