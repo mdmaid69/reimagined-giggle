2ecb88e8c7634e901d2c23009cd8d29e667a87e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:19 +0000

    wish music state study handle

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def convert_to_hex(n):
+        return hex(n)
