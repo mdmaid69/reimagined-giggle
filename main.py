c9c475436b80badcb9c1de592f2fefd7fcc7f91b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:02:39 +0000

    guilty regional control comeup apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
