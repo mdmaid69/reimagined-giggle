7298d2f911c3d7fa7d4299ea3d4c1a41effa3a49
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:20:25 +0000

    reject negative public container refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_stack():
+        return collections.deque()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
