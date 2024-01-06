f39ed3c53549c032bca18a97773757fe1871ad78
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:19:51 +0000

    favourite proud pin satisfactory water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
 import time
-print(time.time())
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
