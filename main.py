83f876a64c3a2ff2b49cfb1b57311cbbaca8baf5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:20:16 +0000

    concerned weather apart touch regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
