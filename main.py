8b9548788d01f3fa60656938a1a4ba89d43b2653
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:36:56 +0000

    opponent copy slide trick present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+for i in range(5):
+        print(i)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
