410110fd7fe5da5bd948a4115d5f410a05f70c80
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:13:19 +0000

    delicate show future connect moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
