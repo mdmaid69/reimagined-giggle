42c562203902b6c8dfd4387e334ae31d4b3a77ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:52:12 +0000

    nowhere television attract music division

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
