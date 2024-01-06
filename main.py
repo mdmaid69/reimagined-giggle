88b4942cefae0d40b4d4fa692cbde879965b5382
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:34:44 +0000

    bottom offer Kk shoot cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
