bd9034f01497da5cb30002e09077c7e75a932be6
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:45:56 +0000

    nervous scare tradition confidence style

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
