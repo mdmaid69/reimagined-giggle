434b38d53906fb54aa826567129b729f8a6a1062
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:02:46 +0000

    among cheat cheat Ss classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_current_directory():
+        return os.getcwd()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def convert_array_to_string(array):
+        return array.tostring()
