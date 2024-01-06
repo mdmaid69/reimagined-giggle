b9060d4143fa8b11d972ec4c7f7a0bf5279143b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:18:26 +0000

    married disappear otherwise estimate help

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
