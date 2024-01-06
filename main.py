340589523d298163b185685e93fe42b58ebb810b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:01:15 +0000

    along needle cruelty mention confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
