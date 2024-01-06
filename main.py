00b58bb5bb373252d6499af3bf770715f0ea147b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:33:19 +0000

    welcome idea suit congratulations write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
