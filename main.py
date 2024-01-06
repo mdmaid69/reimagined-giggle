110ed076d2e027991e523788ad7d0a3d359c8517
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:45:49 +0000

    see hungry reply long lay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
+def calculate_speed(distance, time):
+        return distance / time
