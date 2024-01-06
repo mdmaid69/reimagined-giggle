a9d5f1e279bed04a51aaaf994d2a7d3f605b7b1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:33 +0000

    Oo after failure middle power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
