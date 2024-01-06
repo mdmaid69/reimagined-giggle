5a3b2542861bb336e5a44be5e443ab826b05a6c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:42 +0000

    day somewhere wall dull walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_index(array, item):
+        return array.index(item)
