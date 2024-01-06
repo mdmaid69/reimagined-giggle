6ca164b1d2ca31134c823add8634f3edbaf8b478
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:19:24 +0000

    date cheat begin bath maximum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
