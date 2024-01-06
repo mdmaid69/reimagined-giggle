4b842176e70fa475cfe49dad993fc753f98f119c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:11:03 +0000

    for breath ticket physical workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
