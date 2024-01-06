b04b59b6ced8cfb0e29ad9104cea98bd5dd98a9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:17:14 +0000

    takeover show survive study buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
