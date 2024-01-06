32d0398a31219219259f63b6852a80959fcc0fb1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:13:28 +0000

    film scheme one rest pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def convert_to_json(data):
+        return json.dumps(data)
