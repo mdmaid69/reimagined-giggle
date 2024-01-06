f3eaf87d5494103230621811a23b1c7ee27091ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:06:45 +0000

    section honest respect exaggerate bottom

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
