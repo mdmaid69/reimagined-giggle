ac140d552f9d59024eb759325807efc5f68d3fa3
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:04:00 +0000

    goup study badly load communicate

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
