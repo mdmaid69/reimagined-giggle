128ff5e150eb2bde77ee71780f484a158a974934
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:48:03 +0000

    means night personal top calloff

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
