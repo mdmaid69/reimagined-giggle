f7f4ee58465a237d8d37c80c0e17c5313afc4a19
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:06:30 +0000

    main tear factory table intend

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def find_min(lst):
+        return min(lst)
