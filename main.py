2ec14f9e2b3310e56d87d325fc62aa1d721a5453
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:31:16 +0000

    than die uniform perhaps health

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import sys
-def print_python_version():
-        print(sys.version)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
