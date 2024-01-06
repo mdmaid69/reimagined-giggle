9ab9cf14464fb8ed2ddf10749d9a1231db05cd1d
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:40:04 +0000

    grow blame there entry bridge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import math
+def calculate_floor(x):
+        return math.floor(x)
