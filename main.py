ea37eed1dd221b4c22e165a7d7f3749dfe2f2958
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:53:12 +0000

    afford his quickly love learn

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def square_number(x):
+        return x**2
+def find_max(lst):
+        return max(lst)
