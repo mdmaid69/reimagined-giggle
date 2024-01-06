5a76470b70ddd3c8cbf308fcb2c20ac1550d46d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:28:39 +0000

    belief plain tour square bother

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
