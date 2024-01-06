be44bba303c3863b28da6f7552aa412cf170431f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:55:40 +0000

    flavour quit whistle light house

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
