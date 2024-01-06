097ed97d9cd306094da00d88accb34879b29dcf8
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:37:54 +0000

    shock argue clean neck train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
