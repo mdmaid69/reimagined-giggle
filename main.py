4dda2d705f294f8b9a81a8a2dd786bf5cb9d9693
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:28:53 +0000

    truth runout admire responsible guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
