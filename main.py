b577816f0119e7d47911192dd14da74678bb1450
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:53:27 +0000

    guard any noise brief side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import sys
+  def get_python_version():
+        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
