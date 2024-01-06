e566ddae2a7767b1028f917635c3f4caf2aa7e2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:28:50 +0000

    either accident mine frame everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
