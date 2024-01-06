082bf0d3350604903fb2e7d55bdec6c88018526d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:55 +0000

    male wear set she result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
