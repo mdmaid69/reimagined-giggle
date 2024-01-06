1d023b9b0380d410b5288ff8cabd3d8138cb3b76
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:17 +0000

    considering replace sound destroy negative

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
