0aa7a95cf4d8cbbba72ad21e57f8702223f639c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:08:27 +0000

    basically advanced image combine trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def cube_number(x):
-        return x**3
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
