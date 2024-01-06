aff7dc440162ed4198b000f247477c0c5e8c95ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:51:57 +0000

    off today professional permission mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
