8bf5ae0ef1594c4c597c16e858c0bbf2fc497418
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:01:24 +0000

    tool swing into fit dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def split_path(path):
+        return os.path.split(path)
