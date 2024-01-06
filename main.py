a5505a7e7dd610c28c5985b576233cd33c15edda
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:42:53 +0000

    comein weigh wheel be official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
