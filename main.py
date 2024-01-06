01f9c6fc3178135b8492a81c943aec31fa73625d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:41:54 +0000

    badly side extraordinary aim fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
