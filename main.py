ae648c971ce7d00e124a9b166ba10c3d9e4796c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:29:51 +0000

    shopping relief until apply help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
