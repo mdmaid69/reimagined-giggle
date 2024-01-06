157dd9ca4f1b66925885c32912dacd21453264e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:59:30 +0000

    glass nature bridge 568energy instrument

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
