9329d5d6a2ba3fade4dd47adf8c8a9e28b593480
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:22:43 +0000

    dish last protect direction dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
