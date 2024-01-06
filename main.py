56dd58d78b98c50386c9663cf54408613e019e5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:55:58 +0000

    cap fairly male matter loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
