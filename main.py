1ac2c2210742244d773425fb7acdedfa0de56229
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:07:13 +0000

    like something way measure bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
