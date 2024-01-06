f9c1c8b966345f079666e41c73d9bc4556f54a31
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:44 +0000

    real plan shame answer tone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_acceleration(speed, time):
+        return speed / time
