9f70a7379558545a75092dcfebd7fae59e21cef4
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:04:46 +0000

    can deep same tonight quality

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
