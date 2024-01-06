d3969dd307678847ccb7727c64312517cc297294
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:02 +0000

    runinto wrong cover muscle get

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
