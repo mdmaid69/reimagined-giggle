8684576ce7dd666784c0073eb8d5bd2a6800d72f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:59:07 +0000

    central force reason impression theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
