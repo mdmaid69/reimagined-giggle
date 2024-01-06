8f983e326417935695a139f66c3b2ae770a1c859
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:36:41 +0000

    match low weekend everything aim

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-def reverse_list(lst):
-        return lst[::-1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
