c9488c37025169e7759f8ed297865b813adde41e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:44:43 +0000

    rarely another include pattern friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
