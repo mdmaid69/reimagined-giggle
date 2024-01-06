cabdb5e93a3ab1d724abea9e3ded4b36fb416e31
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:57:00 +0000

    name border undo left rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print(sum(range(10)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
