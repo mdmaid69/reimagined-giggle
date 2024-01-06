5767940f3181c21f7c299178f43bb26c9be36519
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:48 +0000

    turnoff thinkthrough wall birth play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
