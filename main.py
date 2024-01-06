64b575f1de5976677ffb91417c276477976b9c26
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:26:24 +0000

    correct life fine rid justice

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
