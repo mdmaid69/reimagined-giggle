41426a373168f17b8bcc01d3cc2b803abe495a99
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:40:54 +0000

    sore compare short bringdown up

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def is_even(n):
+        return n % 2 == 0
