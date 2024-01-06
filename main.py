b4d438661d79b9d7f9a130b60fc2ae6713c5a327
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:56:55 +0000

    issue by mind meal regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def convert_to_binary(n):
-        return bin(n)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
