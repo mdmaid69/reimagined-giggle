f4903a9ebda563299ab67804ed1e0c9bdaa7d650
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:31:29 +0000

    brush equal condition frighten criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
