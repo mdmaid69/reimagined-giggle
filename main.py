e7a96895c7f75e74c990d6d76c8ab5784662e2f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:40 +0000

    private mouth consult cope receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def append_to_array(array, item):
+        array.append(item)
