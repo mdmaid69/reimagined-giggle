1b474467665f5873e657327cdd1c4f21f1366a66
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:13 +0000

    form colour put smell opportunity

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import os
+print(os.getcwd())
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
