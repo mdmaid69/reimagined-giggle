b5b26ba3e48b1a417c21615ed700f74da577e939
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:22:57 +0000

    excuse rank complicated related since

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
