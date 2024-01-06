4967881366502bc24a73b4459bde2a61011c5962
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:18:33 +0000

    winter accept explain subject condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import random
+def roll_die():
+        return random.randint(1, 6)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
