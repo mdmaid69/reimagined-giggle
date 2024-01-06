cfe3d89d3df3dcb186b829161486f6f1278a7b56
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:23:09 +0000

    sell Pp separate right far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def reverse_list(lst):
+        return lst[::-1]
