0dc19a50e64b6ae3485ac4819b49b233aff39b82
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:03:47 +0000

    burn prepare shout convince behaviour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
