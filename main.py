0dcfa928108cf7f6ad0e16e3757a3e9adc63042b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:45:46 +0000

    flat average choice news opinion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
