3e9bced7aa121ed99ee7146272cfe4776471c4d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:55:07 +0000

    reason however advanced worth talkback

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
