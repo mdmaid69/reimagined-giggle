59c42e054cc3229ab4b74e8df3794573986ed688
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:40:54 +0000

    high hand injury performance range

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
