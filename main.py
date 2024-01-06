27cbfef753d798f10a994d66ac5f75b34f85f8b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:41:55 +0000

    water bridge translate cross basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
