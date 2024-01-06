8d3dc7bdea4b5cd41664e410f62b114d3c25c848
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:49:44 +0000

    cry shade container discover by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
