b75eff8515bfa4a444b675218dd1c84f05765e56
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:20:40 +0000

    final wise comeover answer physical

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def add_numbers(a, b):
+        return a + b
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
