7517011f923fd76838c1893e11826d64db7ff79d
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:46:35 +0000

    about spring gift it near

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
