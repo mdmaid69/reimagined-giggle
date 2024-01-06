7752239e976abaab1adacd613a6d3d5552be1970
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:57:02 +0000

    tired label shut thank accident

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
