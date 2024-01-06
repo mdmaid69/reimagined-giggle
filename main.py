b0db2109c796309c1216df7e684e942332adc547
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:02:37 +0000

    gift owe feeling direct march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
