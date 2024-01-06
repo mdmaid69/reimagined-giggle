2ebad652083678a6f553e693fc5de8f644822e38
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:16:07 +0000

    pot police once general list

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
