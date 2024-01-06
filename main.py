b2ef2273530fe5ae2f7a46c44375011f0cb61c90
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:17:50 +0000

    frequent apologizeapologise about miss ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
