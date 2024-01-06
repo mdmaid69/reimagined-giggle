bb9ed6cdb1791498fbd478763f43a1d4116c47b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:58:47 +0000

    it display available vanish even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
