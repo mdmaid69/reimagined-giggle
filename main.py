2f98c36ab99dd6dc1825a8f4671c8d4ba553ba96
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:53:27 +0000

    noone stretch proud free temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
