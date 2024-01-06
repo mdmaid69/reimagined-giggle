f6c9f7e4bd3505513988bd381a6065f36507aef2
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:16 +0000

    interfere confusing takeoff closely art

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def cube_number(x):
-        return x**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def count_characters(sentence):
+        return len(sentence)
