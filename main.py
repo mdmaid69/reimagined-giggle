e12ca832a84602fb7e1f046e4617491eb517a3b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:17:30 +0000

    weigh involve brush steady turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
