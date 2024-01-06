0027da78c7cb52702bda1ef53cb45dc872cd2cc0
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:20:52 +0000

    container waste save bin clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
