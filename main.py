73deb87f2873cd874f726e442b52ab5fac69580b
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:13 +0000

    stupid legal mail be conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
