e464cfda77be62d94a9425c2060e741516a32fb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:07:06 +0000

    round pure proper exploration trap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_sine(x):
+        return math.sin(x)
