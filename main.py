1433a600903723cefc675235b2331ee70f5784c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:00:23 +0000

    welcome tight clever dream connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
