a670a5a62837e5f87c1ffd8cf0239d36a47cd5ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:10:12 +0000

    skill twist uncertain second score

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def reverse_list(lst):
+        return lst[::-1]
+def find_max(lst):
+        return max(lst)
