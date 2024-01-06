c55d858966da36463f57b40ddb5880296423a661
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:28:46 +0000

    contact tread children prize perhaps

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def clear_array(array):
+        array *= 0
