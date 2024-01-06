f8f91a068651fafd1bb761a60ef73742cc39941d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:08 +0000

    new why suddenly maybe tense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-print(re.match("h.*o", "hello world"))
+def is_palindrome(s):
+        return s == s[::-1]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
