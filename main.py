6c2f9629d00e3704f0fa80ae95dc2777fd1ae308
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:08:14 +0000

    cry matter urgent which accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
