ac079673017a45a6138ee0022751b66f80d3a0d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:18:40 +0000

    swallow test thin mass treat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
