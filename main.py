ef07193a0fcd9ebc1eb172fad961fb861e0d9e38
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:23:58 +0000

    whistle generally agreement amount programme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
