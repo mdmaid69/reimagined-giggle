2634a0ba2251cf649bb7d80624bb336a375eeb2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:22:17 +0000

    different mend child set deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
