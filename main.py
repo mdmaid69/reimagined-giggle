9a8df38728a5fc9a8c7909bc9e5e742458c4deb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:09:30 +0000

    conclusion post single turnaway afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
