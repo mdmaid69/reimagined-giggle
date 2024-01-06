2f6274f2fc55c4fcd149dcb510e57c540eb5e07a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:04 +0000

    door talkinto grow ever relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
