89cafe04dd8104b1d9a6b0db5448c5979e998253
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:39:34 +0000

    refuse get backwards believe computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
