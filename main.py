6bce1403a708543b15f3e3c148204587dc783f40
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:55:26 +0000

    getup practise rub possible note

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
