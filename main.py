d194b291f5514f3d187ec9668048d41c8e502c8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:39:59 +0000

    discussion reliable ours occur age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_set(array):
+        return set(array)
