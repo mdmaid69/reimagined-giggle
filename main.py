dc9d46baab39384e65f60e6e66086e6e1cd7f644
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:02:18 +0000

    list bottom pinch takeover left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
