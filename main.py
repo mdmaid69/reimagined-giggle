5ae7f392a5df615ec434e05bf4a651214461f7aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:59:40 +0000

    round married system correction plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
