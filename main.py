f969aeb7a56ae0fae7ba792f8ea9a1fba4e44369
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:32 +0000

    pen want someone month attractive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
