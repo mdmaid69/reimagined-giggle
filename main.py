29e73306ec2591af0802e92c5da96a9e8992249a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:37:03 +0000

    land bang thank plenty especially

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
