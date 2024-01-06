8c33b06bb6427fffa9d7372b5db1d2aecea9d495
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:58:05 +0000

    free Nn estimate spite crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
