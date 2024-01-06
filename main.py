ddfefe14bc1a9a5bbcbbe0f217e482dbb41d5574
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:25:39 +0000

    gather bar touch lost compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
