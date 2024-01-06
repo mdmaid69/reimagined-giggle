d60c9de1bf3a9d31d93c08872659c9ca9a384558
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:42:02 +0000

    science pity somewhere agreement fasten

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
