d543b6e22c03c6a564edb6e69aa6527885610523
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:16:01 +0000

    pollution live approval light 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
