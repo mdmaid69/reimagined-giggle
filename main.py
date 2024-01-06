98588774a0380a46b3769f11b85580504702158a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:11:58 +0000

    nurse other concerned find somehow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-print(re.match("h.*o", "hello world"))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
