09316139de83d0cf42c0be1838ddfd09ee7be016
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:43:54 +0000

    separate responsibility Itself disturbing extra

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
