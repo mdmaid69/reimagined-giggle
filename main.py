f1daa576a38026df7f30577da7b30655ea7b8ba4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:40 +0000

    sit tall suit strike sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_min(lst):
-        return min(lst)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
