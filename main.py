57c58a510d0c95427c6cd80bc2488db0f17da039
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:29:38 +0000

    tax rubbish discover effect yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Reversed:", text[::-1])
