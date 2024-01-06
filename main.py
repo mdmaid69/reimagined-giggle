9df85d5317ee580f60a60f7c7c697b12665a7bef
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:46 +0000

    secret future walk protection week

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_speed(distance, time):
-        return distance / time
+def square_number(x):
+        return x**2
+def calculate_pressure(force, area):
+        return force / area
