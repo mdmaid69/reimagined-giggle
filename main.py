c0e094ae74372019f871e5136c1362d8c2a3ca3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:25:13 +0000

    container avoid border Ff thinkof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_power(work, time):
+        return work / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
