cebbe1b4bfa908f77686d9150365cf29478d2a0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:28:26 +0000

    meal reject inquire estimate refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
