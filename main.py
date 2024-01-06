832508d1ed9a2fb9c45407aaa16332bad2bef710
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:26:29 +0000

    acceptable official last statement correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
