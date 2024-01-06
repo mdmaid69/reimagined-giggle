ac0f9a2dd1cd5af68e507b175645391c4d7be8f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:18:37 +0000

    care nature switch clue picture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def convert_to_octal(n):
-        return oct(n)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
