2e1ab8a716fda4d46fce70ef6107f6ae2bf46099
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:04 +0000

    behind stare strength ready enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
