3731db71c7d8e03abcbb031ae552a0727c28fa2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:08:56 +0000

    who question sensible flow knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
