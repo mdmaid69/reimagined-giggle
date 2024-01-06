c6c5f34743819d5a6d45e4a43b18dfc7798faaa1
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:13:45 +0000

    pain dark land top nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
