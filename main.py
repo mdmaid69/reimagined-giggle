efeaeac51d7612670fe409038392465a61cb86c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:25:26 +0000

    plant original ashamed round history

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import os
+print(os.getcwd())
