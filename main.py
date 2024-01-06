48f5f9ca92689ce23f3621bb31147c5a8f2c1542
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:42:10 +0000

    fork wonder harm avoid concerned

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def print_python_version():
+        print(sys.version)
