b0fb89b542d91e9dc95107dcdef6cac917f9745c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:20 +0000

    spread of declaration decision bury

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
+def is_odd(n):
+        return n % 2 != 0
