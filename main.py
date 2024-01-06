5ec88b30b7cf75defd2bc0d1e0ab7cccc1a6e805
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:21:39 +0000

    recent summer regularly nigga tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
