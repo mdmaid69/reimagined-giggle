b7b2b033ebbbffbcae6905d463d1f40300d97799
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:52:26 +0000

    scene free deep appeal account

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
