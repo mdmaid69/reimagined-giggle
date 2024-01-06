d987af2cab90f2c9e552d70a0826c23c2edecf50
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:16:54 +0000

    cheat exam prevent circumstance hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
