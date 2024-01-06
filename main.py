28c016c754c8206ac33096fa3d41a5816ec95bf7
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:27 +0000

    ride scrape jump phone cough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def add_numbers(a, b):
+        return a + b
