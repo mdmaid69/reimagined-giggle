697d8b16d7d31fc045fec9c0c8c6584015d30b94
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:36:46 +0000

    tomorrow exercise date whistle fast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
