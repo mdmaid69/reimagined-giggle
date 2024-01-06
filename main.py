783640da370d45eb0433534459e933a16dfe55d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:45:12 +0000

    deliberately flow safe thirsty theirs

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_power(work, time):
-        return work / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def clear_array(array):
+        array *= 0
