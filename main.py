035dbb0f58709379ea9fa6cede3a62c430989601
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:32:37 +0000

    storm show guilty also play

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def divide_numbers(x, y):
-        return x / y
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
