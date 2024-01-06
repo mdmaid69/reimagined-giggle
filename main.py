fec56a39f8840891609ffa5c2973b00a92fd5fb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:53:51 +0000

    settle bend hate wealth class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_int(array):
+        return int(array[0])
