353746401077d36ac7e746e2be8e06cb3dc176d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:32:20 +0000

    rather dance advantage send famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
