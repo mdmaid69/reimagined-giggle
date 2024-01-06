89a49331ca1031d311bb22dfe0d82c3879487c2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:27:02 +0000

    will takeout stiff agree lookaround

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
