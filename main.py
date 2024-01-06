104c459671b334fb688f7981ab14dcf95448ad4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:54:34 +0000

    better bother cross live weekend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
