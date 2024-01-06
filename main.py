5028132c9a3cbde079a60ab239c3779b2ed7fb38
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:30:51 +0000

    swallow independent suckme inquire date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
