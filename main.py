162c95adda765fb665fe0ef70d8afc2876214a3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:43:16 +0000

    love true pain fight season

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
