17bd2d3f05c5ef046f7d2753f9d5f70d667577b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:49:10 +0000

    argument identify sink splash vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def count_characters(sentence):
+        return len(sentence)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
