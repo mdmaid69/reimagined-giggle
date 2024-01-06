5d5ac565751681e42bdbdf1cd07b8c9847c2feb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:42:28 +0000

    deep thirsty uncomfortable forget bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
