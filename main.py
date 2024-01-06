6dd9d8dee332c4de4778b4954fdc22c1eecb5b56
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:13:11 +0000

    finally takeout taste stand doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
