55462bbd142dac84ba7b6e695add570d64a428f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:15:13 +0000

    lose narrow attach Gg takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
