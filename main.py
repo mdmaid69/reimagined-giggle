96f20b40f17982a697fa2f19b619949c29bd88f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:07:06 +0000

    weak address active able early

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
