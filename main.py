cd3933a53f0310dec7792e4e03cbf2cbd56d8b2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:24:21 +0000

    follow confuse Ll hit appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
