435d7310fdebdbc6c1a406d3493cee48a64b54f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:42:56 +0000

    face glad surprise card fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+def print_python_version():
+        return sys.version
