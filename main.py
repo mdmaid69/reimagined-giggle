93ffef2af901f961f2e623fe08c22c519a3ddad0
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:38:33 +0000

    maybe marry soil simple arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def sort_list(lst):
-        return sorted(lst)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import platform
+def get_python_version():
+        return platform.python_version()
