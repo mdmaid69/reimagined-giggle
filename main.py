f666e52458de40ace570c62f9b2d73acaf1b97cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:04:08 +0000

    light her call you objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_typecode(array):
+        return array.typecode
