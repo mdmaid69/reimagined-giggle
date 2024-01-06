a57b91af56f1085ceee6f2bce26eab6eb314e234
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:16:00 +0000

    rate spray stone dream wrap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
