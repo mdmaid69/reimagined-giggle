358695874c0cfe784ddb337774647c524f1a96e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:27:14 +0000

    song compare no recover various

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
