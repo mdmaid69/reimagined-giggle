2df7054357bd0d32a6516a77709f59e01653d31f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:37:48 +0000

    compete lose pride change record

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def subtract_numbers(x, y):
-        return x - y
+def find_min(lst):
+        return min(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
