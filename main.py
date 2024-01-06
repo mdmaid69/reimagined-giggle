30479a079555fc5c05266020fa42e58b548c29b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:50:13 +0000

    nigga deliver list solve dream

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def add_numbers(x, y):
+        return x + y
