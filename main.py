b713b6dff64c80d93fb59e610286f325245d352a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:19:24 +0000

    world company travel quantity accident

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
