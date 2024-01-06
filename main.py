fd34f6eee2403872d1ca18808c7b985cbd7f438b
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:48:30 +0000

    announce little appear used count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
