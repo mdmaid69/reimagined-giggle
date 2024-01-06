5495d9cd888e13c57dd01f07bdb406a70db46b60
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:01 +0000

    murder miss claim storm hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
