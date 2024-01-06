92617aa446eeb2c605b1afd5a26a9e289314c48d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:17 +0000

    getup along hour bite complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
