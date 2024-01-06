39dc998f7aa9c3b8e63cd7d9550814fd9a1ad0f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:18:05 +0000

    say report peace doubt land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_max(numbers):
+        return max(numbers)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
