53d645eacba7beb4831072f8719ce056086d9cae
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:38:39 +0000

    experience undo tonight breakout rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
