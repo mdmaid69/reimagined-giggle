3cad3437dcd7b6469fe4c37b73e540659987a48a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:06:06 +0000

    plenty crash Oo expect fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
