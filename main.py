5b771c8345eb6b376bc71e10873261ec184256cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:59:59 +0000

    team remember base sign breakin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
