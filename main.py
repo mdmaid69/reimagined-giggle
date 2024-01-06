dbf6d0c47fd1d76c5470287ee56dc909fb139989
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:16:39 +0000

    content pleased habit compare turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_list(lst):
-        return sorted(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
