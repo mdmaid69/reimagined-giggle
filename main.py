ee698833fd8b9f235a504530119512644126c98c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:12 +0000

    club bush actually computer trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+for i in range(5):
+        print(i)
