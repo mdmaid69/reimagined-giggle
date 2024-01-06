67256a2ed7b6c7df3e18e4e4be299255e5a5bfe6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:40:44 +0000

    winter reduce theirs recent better

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
