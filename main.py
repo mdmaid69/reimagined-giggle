2223b98a5921a60a18fab2a00d370a8693a93a05
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:58:41 +0000

    disapprove scared nor frighten life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+for i in range(5):
+        print(i)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
