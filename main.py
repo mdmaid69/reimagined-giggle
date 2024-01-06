d74b9e2bbc523abf2c0c21fbff770182ccb43d31
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:24:20 +0000

    raise release loud poison possibility

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
