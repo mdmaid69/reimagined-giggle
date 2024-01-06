c9876f7c7761b7aec0f7c5412fa891846507a01c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:47:33 +0000

    impression year conclusion probably table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
