d5e1e8652eb4904f11fd25eb11c24337196dd7cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:16:54 +0000

    prize second female shall father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
