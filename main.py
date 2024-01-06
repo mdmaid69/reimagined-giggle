20d6a30022809fb5b8f7dff65c40d7a04638e6c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:47:04 +0000

    tear rate further film 572enter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-print([x**2 for x in range(10)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
