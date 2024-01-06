9fafd5229d310e69263c0d8b15547aaf210d225c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:11:55 +0000

    regard gas wipe tooth moral

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tensorflow as tf
-print(tf.__version__)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
