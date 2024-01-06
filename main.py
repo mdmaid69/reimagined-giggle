44838f363d836656436950393629da302f99709e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:21:55 +0000

    nonsense computer prove direct disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
