d9c6db4e4dee29eccce9fc5f03d7cd81f5702309
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:48:52 +0000

    clue joke problem single real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
