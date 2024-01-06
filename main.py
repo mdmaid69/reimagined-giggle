2e8eb9af8baa9cf8d7b91010af2665c36b49fb50
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:23:51 +0000

    both far prove print odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
