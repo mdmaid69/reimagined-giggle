11bba99a41675a721857335dcdddd0194c32a68a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:48:36 +0000

    total record important start goup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_tangent(x):
+        return math.tan(x)
