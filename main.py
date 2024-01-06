a25c2f0afc4205fb3ec18088b72b51f853bada72
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:22:33 +0000

    television build male none tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def change_working_directory(path):
+        os.chdir(path)
