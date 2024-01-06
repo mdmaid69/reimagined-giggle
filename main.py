ed3c96926fa4977dd5e774142c68956e20c165f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:33:37 +0000

    attitude congratulations news temper whom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print(sum(range(10)))
