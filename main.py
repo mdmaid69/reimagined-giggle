8bd519e16e0dd58b5dd539292e8fd408bf492fff
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:56:14 +0000

    failure uniform majority schedule clever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
