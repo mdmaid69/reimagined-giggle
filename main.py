5a6a6241f634278522e4349d2fc483ba69ac704c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:45:34 +0000

    in road above exchange design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_pressure(force, area):
+        return force / area
