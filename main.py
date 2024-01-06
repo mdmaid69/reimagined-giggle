6cd58fb8f9661c175f98096f7aa2ec65ef4b17ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:50:13 +0000

    watch last likely comment hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
