f55a723a6cbf82709db1325c6dc590ef7a2b4ccb
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:20:15 +0000

    glass breakin hit damage defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
