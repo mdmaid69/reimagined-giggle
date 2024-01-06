6247fad7275ee201d3d0d0e652e892e8a93a3e48
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:24:13 +0000

    punishment trust bang brown my

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
