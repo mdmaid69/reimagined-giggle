7062736ea5fae831f52e4d718f607d85201faaf6
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:56:41 +0000

    story command rush fine suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
