0298528c7859879736c472561d2bcf79800d2abf
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:24:13 +0000

    Tt slip company attract minimum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
