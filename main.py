e5bb02fc8fe61fd68dff8059c779d7d1e52b8e4b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:11:02 +0000

    nose still matter share scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_pressure(force, area):
+        return force / area
