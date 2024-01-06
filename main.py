daf2c2e502eea2be1b24cba37d67543da7888301
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:19:15 +0000

    anybody beside community appearance limit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
