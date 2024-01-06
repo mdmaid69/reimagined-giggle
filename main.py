f7f8840b7416340f8fc8ee42f978dfbfc4c04514
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:20:35 +0000

    dream it medium everything dangerous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
