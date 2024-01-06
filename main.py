fe1bbfc027407186bbc9483f1d43994ab38616a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:49:33 +0000

    pronounce will lay father against

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
