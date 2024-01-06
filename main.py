0681c62efbfaeb9ad568430da4c111eda07b0154
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:39:18 +0000

    burn freedom whether look take

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_speed(distance, time):
+        return distance / time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
