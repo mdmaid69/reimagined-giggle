5c67de53819d7cd5f545ffdedff988d34d67e264
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:01:47 +0000

    sure he wander heat shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
