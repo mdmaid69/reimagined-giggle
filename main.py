c719286273d22e49749d16f6dc2d58eb1166db1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:21:26 +0000

    sky wet really divide information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import sys
-def exit_program():
-        sys.exit()
+def add_to_python_path(path):
+        sys.path.append(path)
