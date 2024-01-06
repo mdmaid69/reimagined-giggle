5dc927e6106b2b43e505db440d3d4132da7dcf17
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:16:41 +0000

    reduce home trip wide present

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
