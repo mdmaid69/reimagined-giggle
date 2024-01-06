d14e2fb682f1c5e5e5a1dd80d7b6599090ef2c78
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:49:46 +0000

    anybody fairly need shine how

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+print(sum(range(10)))
