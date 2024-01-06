1f4e4bb84981c5eda87f226fea22071c4db05a78
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:12:12 +0000

    suck conduct little listen bother

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_exponential(x):
+        return math.exp(x)
