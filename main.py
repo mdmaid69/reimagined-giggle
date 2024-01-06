146bfff53beb16c799c4cc5d356a3ce1e05722af
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:15 +0000

    hide choose birth sister bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
