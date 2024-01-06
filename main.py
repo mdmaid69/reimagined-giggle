c79d6cd3f9477b59c18fffb47261d2f9ea938ffa
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:27:01 +0000

    thinkover hello excuse morning proud

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_index(array, item):
+        return array.index(item)
