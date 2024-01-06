a967bc12a1027c888327831a293c26ce24615c58
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:39:37 +0000

    sleep design house lock partner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def change_working_directory(path):
+        os.chdir(path)
