0d1b0c21aa5eea1ef898975bdb4022ec8fa3a2a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:54:19 +0000

    Ee calm comein form suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
