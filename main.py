bb3934c9f14e7741c228cb9264a780dde1f1868f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:23:30 +0000

    control central love sleep out

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def add_numbers(x, y):
-        return x + y
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
