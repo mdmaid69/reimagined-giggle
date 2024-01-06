3434a55591777f93c8fdce2a1bdcce0b24753552
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:36:22 +0000

    whisper effort in name row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
