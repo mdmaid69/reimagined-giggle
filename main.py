d31e668027cd4b7f5c5400071ae3914db1787c34
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:23:27 +0000

    somebody grand clearly write draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
