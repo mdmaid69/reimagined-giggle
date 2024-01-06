53c0c6196cc512f807a3880a36eab57464c3445f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:35:17 +0000

    advantage destroy live thinkover guess

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
