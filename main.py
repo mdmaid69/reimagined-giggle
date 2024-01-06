f13b8a72e57233ca2b86a3071d11234c6bab8677
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:13:50 +0000

    pleasant mine maximum general tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
