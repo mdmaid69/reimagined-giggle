00647fcab419c7689f86e1cadb2420ee28e619cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:29:05 +0000

    common die home exam rain

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
   import sqlite3
   def execute_sql_query(connection, query):
         cursor = connection.cursor()
         cursor.execute(query)
         return cursor.fetchall()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
