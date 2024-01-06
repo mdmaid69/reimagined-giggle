80a45ac021444ec15c7dd73f18ff67ea62fdfd53
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:17:06 +0000

    callback bread plan afternoon angry

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
