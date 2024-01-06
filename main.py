8fe3778f29bf15c8d6f4d80317f417a271249b02
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:51 +0000

    up abroad loose before week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def square_number(x):
-        return x**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_item(array, i):
+        return array[i]
