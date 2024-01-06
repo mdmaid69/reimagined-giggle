ecc87320ab56dfc2087a3dcde42aa691c22515df
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:17:29 +0000

    worth and secret damage wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
