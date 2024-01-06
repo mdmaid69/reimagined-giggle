b0b5f2cbd375145dc254e17bd20211e3a3718ad7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:45:45 +0000

    answer fast secret someone future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
