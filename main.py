73e585341c5d2eb7f8124ff85bfecb560ea33585
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:17:07 +0000

    impression mystery paper practice line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
