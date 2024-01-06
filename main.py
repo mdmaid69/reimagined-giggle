60c151481341ea216e01a6b179041744bbee794d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:31:07 +0000

    believe goaway many cheat calmly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
