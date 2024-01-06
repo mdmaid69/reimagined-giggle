e9192ee80309fec26f9c6fcbcff8b3413327a62a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:01:16 +0000

    town ear together that yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
