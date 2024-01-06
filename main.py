c604be4a7d295e5a68ef8afe710ab5fade1a519d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:42:42 +0000

    gap takeout argue aware contents

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def convert_to_hex(n):
+        return hex(n)
