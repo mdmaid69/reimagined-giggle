23ac968f0a4524aedc8dc259ebe409578b6c1a7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:46:45 +0000

    nature certain so break wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
