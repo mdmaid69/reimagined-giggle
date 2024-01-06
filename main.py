70247c14a20fc1d9b4119c26b0d74dafdc97b0d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:55:55 +0000

    common order joke sometimes all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
