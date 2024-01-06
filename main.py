e8b18fef8d7fb45452cfd0aa162d513dd5df155f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:19:18 +0000

    completely fasten board another behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
