ebb206098cc3a12cb38510c95135b21bb779c763
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:20:02 +0000

    address frighten command play married

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def square_number(x):
-        return x**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
