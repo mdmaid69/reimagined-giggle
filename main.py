990c6046724861e31c1548633aa22ee5090bc9c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:24:32 +0000

    common Hh allow half health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
