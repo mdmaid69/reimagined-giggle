50fec2bf729c0b6e8193d36cfb2030d4cf1bf257
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:40 +0000

    certain still practical everything friend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
