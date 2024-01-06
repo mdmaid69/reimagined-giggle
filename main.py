1df5dbf2e0257908068d9b709b32f6fdfac8e3c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:37:22 +0000

    actual wire hello obtain sort

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
