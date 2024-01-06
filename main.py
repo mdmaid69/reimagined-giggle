b841504840f3c0545859609b7024a06e81dc7ac4
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:21:13 +0000

    note holdup sense determined join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import numpy as np
+print(np.array([1, 2, 3]))
