9c3a25ab5745f9d8bb98dc4d0dfa4972ab624c49
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:33:02 +0000

    greeting lost awake letter only

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
