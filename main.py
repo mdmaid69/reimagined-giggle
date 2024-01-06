3917726288d40bb933cc08c277e5b4edc99fe58e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:43 +0000

    future familiar past shall delight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def greet(name):
-        print(f"Hello, {name}!")
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
