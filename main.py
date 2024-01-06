7a3df858c397fdf5aee6be4f057b2b5b178cb8a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:23:19 +0000

    should reduce towards slice favourite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_bool(array):
+        return bool(array)
