0adfb9e073bef086436bdb4c844fb761151e8ebf
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:26:47 +0000

    tidy work content sick wave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
