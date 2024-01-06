b7d1a72878f137d719792b00dd9787eb7325f172
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:57:34 +0000

    raw age arrangement maybe even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
