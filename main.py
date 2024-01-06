68bdfd02749de47ee68b044779572f10c41c98f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:44:23 +0000

    stop standard belong Tt perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
