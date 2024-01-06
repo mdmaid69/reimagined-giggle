9d83f53d0be658d6a63a7e01147cc95dceb82afb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:49:09 +0000

    war children book estimate long

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def count_characters(sentence):
+        return len(sentence)
+print([x**2 for x in range(10)])
