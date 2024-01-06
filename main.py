31869803d890552f7fd3f6e528cc3eeca2945c0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:06:56 +0000

    arm judge disappointed plate high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_list(array):
+        return list(array)
