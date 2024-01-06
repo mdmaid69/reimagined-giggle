42180941cefb5663b37410a37661626dbcb5bd51
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:32:51 +0000

    stamp discuss sort why dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
