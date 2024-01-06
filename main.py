b7aacc77f376ded6f00daa0b79e767201efe63f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:39:49 +0000

    relief opportunity sensitive wide innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
