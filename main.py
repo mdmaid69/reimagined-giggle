64dbf061f5861104d5c0658caa81c3489ee87d01
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:20:47 +0000

    direct shopping list office country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
