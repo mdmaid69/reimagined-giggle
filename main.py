ab095e6638c1add0baf6e64db3aafd6bda034379
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:59:43 +0000

    dare once slightly platform pen

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
