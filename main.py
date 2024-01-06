1cfab3859021c97d6e5ac5a6ee9041edd28b2747
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:13 +0000

    basic giveback poor rush beyond

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def square_number(x):
+        return x**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
