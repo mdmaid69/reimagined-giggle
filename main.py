417705aca5edbb59f411e5a9d1346a4d46d3b2e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:44:46 +0000

    clean late typical high level

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
