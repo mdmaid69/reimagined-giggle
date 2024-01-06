0de625a136352d9e933f342708989d5a021ddfa3
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:01:08 +0000

    drink debt quality avoid case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
