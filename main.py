7c5cb22b74200aed0d4b9fa69235ad6df1c306eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:46 +0000

    complaint talk direct loose attention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def square_number(x):
-        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
