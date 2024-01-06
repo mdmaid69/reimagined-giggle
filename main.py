fa586b2bff1db9a752875589c36e33f15bb22a0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:38 +0000

    key kind great floor judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_min(numbers):
+        return min(numbers)
