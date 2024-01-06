bf905681547cc1f02cd7333c8b56f6f10accdb87
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:56 +0000

    please offence hard win disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
