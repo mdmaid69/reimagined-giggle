a467eaa758fba4c07385333c8f5a6a313c15aafd
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:30:16 +0000

    advanced distance declaration dead up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
