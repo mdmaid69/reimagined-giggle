517714c70b7df037a2273bf1d16249d31199b19d
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:46:08 +0000

    guard false turn believe future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
