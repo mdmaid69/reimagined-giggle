dbee0d706c33dfe9c5e98402e72d294e64951002
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:13:49 +0000

    funny 569enjoy copy press corner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
