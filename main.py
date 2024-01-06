42827686124886c90d8988d95ab71621c4416332
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:23 +0000

    raise why necessary star steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import sys
+def print_python_version():
+        print(sys.version)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
