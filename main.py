a65d0e46ee83cf1c5b63051651404c5093309ca4
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:20 +0000

    gather operate beneath print level

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def multiply_numbers(x, y):
-        return x * y
+  def get_current_working_directory():
+        return os.getcwd()
