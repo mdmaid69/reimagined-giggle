d2f6e531e74efe843fd69c8dba273e0a21b3f3a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:16:23 +0000

    impossible drop under disappointed travel

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def greet(name):
+        print(f"Hello, {name}!")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
