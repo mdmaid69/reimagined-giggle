539a973332a9fd97bc2e1e17b9b4152a3ab46b29
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:51:23 +0000

    flavour share turnup criticism door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_max(numbers):
+        return max(numbers)
