6e9594cf583e1a58cd685d1a4061da6c82820750
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:07:59 +0000

    ever long mend mind accidental

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
