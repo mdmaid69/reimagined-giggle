359841be281b28f3df3de54fbb288e05ebae28f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:40:56 +0000

    narrow country bringback score ahead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def remove_duplicates(lst):
+        return list(set(lst))
