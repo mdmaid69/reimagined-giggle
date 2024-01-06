f4d49886f327e291c9100e9544fdfb3e78ee9e0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:39:53 +0000

    complicated smell shout safe help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import time
+print(time.time())
 import array
 def get_array_as_float(array):
         return float(array[0])
