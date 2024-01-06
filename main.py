b06a01c63628f99473f099e02384fd8e361d8ea9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:27:57 +0000

    number once bringback computer translate

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
