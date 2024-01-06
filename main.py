ef6d7fa4cb6a2391dec09beb9051608413b74d8d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:59:39 +0000

    copy wet worth hair charge

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
