efd062cfbecfef777389b4dd123c1e7e8e563926
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:53 +0000

    bag workon again 570enjoyable rubbish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
