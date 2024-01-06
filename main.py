9a59e277e5259c922a5948be7e651307f2254bc0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:35:25 +0000

    recover cinema declaration neat nothing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_speed(distance, time):
+        return distance / time
