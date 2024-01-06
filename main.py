f90b7130e07aca6856766cf4a856204eb1334179
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:46:15 +0000

    cap callback herself major certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
