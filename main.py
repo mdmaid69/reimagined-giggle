0691c49fa21b536dfd7b035b25b119384f90326f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:35:36 +0000

    comedown herself whoever sure reliable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_power(work, time):
+        return work / time
