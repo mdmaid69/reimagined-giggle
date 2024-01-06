dbe0c19ab73d08ade5636a8bdf00da8fe607e995
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:35:15 +0000

    report obtain charge hold downstairs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def clear_array(array):
-        array *= 0
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
