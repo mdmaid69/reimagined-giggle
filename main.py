69b1cc617fd91575c479257a47ac6f3fbdd93912
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:44:38 +0000

    letter related serve story mind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
+for i in range(5):
+        print(i)
