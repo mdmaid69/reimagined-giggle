ae7c3f869a4fdcd9d12302cf18a18ad208cd2584
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:38:00 +0000

    formal address shallow crime keepoff

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_power(work, time):
+        return work / time
