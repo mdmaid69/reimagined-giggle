eb8098de7a62dd0da0493ae3f02b6c26d885b4a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:34:36 +0000

    burst detail argument fun tragedy

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def count_characters(sentence):
+        return len(sentence)
