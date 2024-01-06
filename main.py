ea8a99b1a56dfe1be7c3e9cc2fe3377e3a754c25
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:05:20 +0000

    amount city ability spite else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
