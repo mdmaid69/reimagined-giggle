b04dcdfe503a97ec734dfe8c7f94d73f12ce880c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:03 +0000

    interview check off complete see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
