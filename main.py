4393157c6f13f47ae097e0ec63adea8c49656d51
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:56:33 +0000

    range bang point instant someone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_work(force, distance):
+        return force * distance
