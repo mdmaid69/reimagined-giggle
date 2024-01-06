887d1a4cfa7cca81fc3e551ad00c7d8a16023a23
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:16:16 +0000

    blow clear possession think loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
