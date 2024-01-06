9857e6623ac3efa44bc3dc240cd09b60ac9f9c4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:31:19 +0000

    no Dd strange collection tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def add_numbers(x, y):
-        return x + y
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
