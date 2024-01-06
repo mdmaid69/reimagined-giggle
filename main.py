32b704698cc4b203617b3abc136f07fe6b56d862
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:29:39 +0000

    572enter thorough onto would joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import sys
+print(sys.version)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
