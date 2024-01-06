197d6185c96702787539f9914abd160bb89d865d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:31 +0000

    seriously contents Ll question limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
