43bf5b758ac5f17a1f439e895dac9312f622c44a
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:22:13 +0000

    development part probably lot average

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def square_number(x):
-        return x**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
