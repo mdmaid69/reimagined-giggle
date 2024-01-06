aa1938efbdf8fd2f66fed06d2cac2c57f802f419
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:16:30 +0000

    organization important state sensible criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
