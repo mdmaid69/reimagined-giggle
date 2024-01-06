1f90f46a52c39fca1603f8fb978939494b1b1048
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:51:23 +0000

    spend lot will secret professional

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def add_numbers(a, b):
+        return a + b
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
