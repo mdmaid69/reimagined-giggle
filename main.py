15da6f61c4cb629f14d7dc2bb861228b0628d1eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:40:07 +0000

    skill situation uncertain will imagine

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_array_typecode(array):
+        return array.typecode
