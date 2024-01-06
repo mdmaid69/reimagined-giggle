898532e9071c1417be8a359e837a6bfa4929ec46
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:22:46 +0000

    note like illness tomorrow reserve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
