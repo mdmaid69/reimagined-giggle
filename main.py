255c354d412860ef8f5988994da984e361ca68e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:38:55 +0000

    length fit fear project game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_acceleration(speed, time):
+        return speed / time
