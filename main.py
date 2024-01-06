e1d9cfee41cf3a0915f79eb5648dc6869ebaeea6
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:26:16 +0000

    be 565end ever turnaway course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
