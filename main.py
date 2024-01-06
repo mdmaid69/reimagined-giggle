4bf4e925b47b9acb11f5a962a57628569b66c5b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:18:10 +0000

    niggaacademy future quit classroom boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
