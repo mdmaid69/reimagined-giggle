27d8d00279f3859524c0f2aa6f8fcba6bad67394
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:01:20 +0000

    vote afraid nurse today Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+name = "Python"
+print("Hello,", name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
