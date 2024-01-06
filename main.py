b82cfb998e5ec2ef489645026cd96a58b6843ec5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:36:44 +0000

    such responsible rarely middle beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
