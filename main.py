58c29afdf40a9d6199baa83d5771e1cdda38291b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:41:55 +0000

    what rescue join impressed thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
