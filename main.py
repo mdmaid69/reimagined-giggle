a2ad145a5c5a8b80187baa1f09d58d50790456a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:06:12 +0000

    unlucky fire exhibition direction worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
