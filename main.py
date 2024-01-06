9d6059c66813d9bd02a76dc29dd9876297564472
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:20:38 +0000

    shame container talkback normal fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
