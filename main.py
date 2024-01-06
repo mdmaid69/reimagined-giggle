18f3d3d0cb6aa2fdd6e833f3292b63ea073a0835
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:37:22 +0000

    talkoutof please surprise squash ask

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def sort_numbers(numbers):
+        return sorted(numbers)
