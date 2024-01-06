eb95d2c088b378bf05d2ce013ca25fcc34070118
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:19:10 +0000

    burst advanced air performance involve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
