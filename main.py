55851a0ce462586883cde42faa9a53cff0a36327
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:24:55 +0000

    pity listen undo law bush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sys
+def print_python_version():
+        print(sys.version)
