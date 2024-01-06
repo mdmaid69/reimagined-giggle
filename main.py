64ac78278cac9de3284d628ec646478bc6fb8d16
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:02:52 +0000

    promise Qq relax sick obtain

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
