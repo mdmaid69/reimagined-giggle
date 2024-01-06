7218ff97c065875e4d55a48837093b8ab0cf5f82
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:13:59 +0000

    goodbye can hurt slow poison

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_power(work, time):
+        return work / time
