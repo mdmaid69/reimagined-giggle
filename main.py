70a09ff5bf092041ea2121b2542cdb2f789a4c5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:26:47 +0000

    damp simple risk damp mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
