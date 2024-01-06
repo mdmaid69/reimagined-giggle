3a385c72bf7a1d75c5063369c84758ce0114f283
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:06:38 +0000

    eye own normal rob feed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_str(array):
+        return str(array)
