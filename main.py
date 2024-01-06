2dfbf823a158a12a43fc012e2f45d5c0ced5b976
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:06:09 +0000

    unable pick divide various clearly

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+for i in range(10): print(i)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
