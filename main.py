6a70e153a1f6238b363318591e4fc5aa406385d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:35:06 +0000

    design drop except edge bus

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
