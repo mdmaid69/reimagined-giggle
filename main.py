86e616eda2d127673457b7904d33957eb0b3f651
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:28:25 +0000

    under quarter rush especially exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
