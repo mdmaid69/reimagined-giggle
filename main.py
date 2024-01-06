b288f75939d0b0688e1fa80fc769e2af2224eb0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:16 +0000

    house speech plant lower question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def greet(name):
+        print(f"Hello, {name}!")
