258319fb40b48339c74e60634a4d1b7ac1c984f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:55:27 +0000

    professional platform reasonable boat joke

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
