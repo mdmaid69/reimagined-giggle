bdb557b0a733fb5ec2d38b76ef730c80b2718778
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:33 +0000

    block rank battery yourself attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
