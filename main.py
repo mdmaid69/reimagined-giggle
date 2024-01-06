14e32b1e93b132125082c31e8752b7da920f1b0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:05:12 +0000

    deliberately tense stay Tt ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
