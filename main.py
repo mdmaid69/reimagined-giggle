265193e632518c0ccd3fa190ccfef2860048ac94
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:47:00 +0000

    freeze interview tradition how ordinary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def reverse_list(lst):
-        return lst[::-1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
