e123a31522116221b450e5a07c0b3e8e19cb40d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:18:38 +0000

    bang love run poison trial

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+print(math.pi)
