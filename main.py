fc577067fab389e7dbd9afc98bcae8b0f1143f16
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:56:07 +0000

    scream guide read I decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
