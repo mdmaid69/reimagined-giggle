74e11a95a10d6392e9f3212600e224f4b2b3c518
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:59:05 +0000

    cut answer both deliver period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
