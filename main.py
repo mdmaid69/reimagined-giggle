85ad11abd31c4a286c792bd81cc6324deba2276b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:53:25 +0000

    middle back circle age interest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 def convert_to_octal(n):
         return oct(n)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
