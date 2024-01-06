0da84f8faff0ac1d2a1b2e3f95db96f0e47313fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:05:28 +0000

    get tight real sight error

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
