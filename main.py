6bb6925bc4c39af4b4f2caaf81a785a711b18fda
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:19:16 +0000

    class class consist name machine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
