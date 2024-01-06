3aaca1f9a8706172338984c9b75671625106936a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:58:36 +0000

    trap talkinto everyone keepaway rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
