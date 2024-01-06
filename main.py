85d9fee46a00d199cbc956bc1a0df54982fbb20c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:57 +0000

    lot home rich invite right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
