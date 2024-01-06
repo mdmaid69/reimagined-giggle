9468a7063ffd5bafd7557c8a29572618a1592136
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:21:28 +0000

    meet sort joke whoever juice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+import re
+print(re.match("h.*o", "hello world"))
