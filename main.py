baeb062ba8a233b0adc14a5c7e67df9671cbd1ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:10:58 +0000

    priority talkinto connection heart tradition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
