77d6a238ad3ddc6338d56aa8dfae53d6f145958a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:27 +0000

    defend hello sister Ii want

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
