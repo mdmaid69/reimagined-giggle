4e5ebeaeddd39bbcca37d03cc8f76fb65821dcf5
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:28:40 +0000

    attention lookaround by control fond

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
