7dc7cf562fcac758dbba506d840afb225eb5054f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:02:27 +0000

    straight ahead grade poison gate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
