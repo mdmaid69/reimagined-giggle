67af7ed2f5664e9ea8a5c51a89989554ec0e347b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:56:28 +0000

    control voice cold goodbye brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def square_number(x):
+        return x**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
