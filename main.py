d8d6a06c777a61622accc70ae9296d3ae075cfc8
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:25:23 +0000

    force noone tend correct around

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_string_from_array(array):
+        return array.tobytes()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
