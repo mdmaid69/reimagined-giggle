caaa94b1ea4509bb1f60073102d94930289c5573
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:58:30 +0000

    fast worried plastic of practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
