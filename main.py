a662fa165cc671622834dbe7c4e8b5aab012f0f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:05:19 +0000

    concern mystery coat fence concerned

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_string(s):
-        return s[::-1]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
