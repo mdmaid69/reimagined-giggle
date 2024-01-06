aebff0d88e29d3e55c7294255ee026d90611fda4
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:02 +0000

    stay relax appeal comeup contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
