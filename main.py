b1f18168582d56614ea4a85a63d535eb559916e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:57:31 +0000

    embarrassing project contrast constantly comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
