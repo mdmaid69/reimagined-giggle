5e1f6ef333c6f0057f31399964d40acacb444e5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:39:46 +0000

    satisfied snow accommodation Nn any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
