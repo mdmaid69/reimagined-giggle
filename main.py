512cbbec1d7f8ddb5f5b8fe624ee8aab14dbd992
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:08:06 +0000

    write very top before peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
