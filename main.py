4cd0e81164dfb1954f61a96043a65a2f694f41e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:29:52 +0000

    doctor sting purpose such gather

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def reverse_list(lst):
+        return lst[::-1]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
