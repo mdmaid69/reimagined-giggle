4f2a62c6047f6ba58c23b1c9475317b4afade084
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:26:29 +0000

    keepup pinch Ii answer transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_queue():
+        return collections.deque()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_array_to_list(array):
+        return array.tolist()
