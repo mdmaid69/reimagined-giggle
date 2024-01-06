962252e9e416b4ca03475cf080478af30214ed2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:15:01 +0000

    politics might full should part

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_unique_words(sentence):
+        return set(sentence.split())
