a1c21ab48670d402d449c0af1134ffc8d24520c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:24:58 +0000

    unless trust born back concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
