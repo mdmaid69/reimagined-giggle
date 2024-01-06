5d95d58b0f36504e831349e62d20630610fd1e7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:56:30 +0000

    design hungry beside anybody mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
