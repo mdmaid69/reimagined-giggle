380938534573f0f4198a76b1bd2422b3352109dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:43:38 +0000

    deep characteristic dig thinkover mystery

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
