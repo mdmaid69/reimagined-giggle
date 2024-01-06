3e97b902f161f8bcff6eda3070bf375588a9efad
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:41:49 +0000

    deaf matter post holdout tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
