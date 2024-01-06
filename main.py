8aa169c40b5768062c0b8e00d3caf6a6b4bc34e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:44:09 +0000

    gate pair common maintain destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def create_queue():
-        return collections.deque()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
