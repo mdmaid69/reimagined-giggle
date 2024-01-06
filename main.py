ea8099b02198f4537758349c0f0dae258e578ddf
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:32:33 +0000

    charge marry shopping curious watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-for i in range(10): print(i)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
