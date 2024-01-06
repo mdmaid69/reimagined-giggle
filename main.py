9027e98c33843e748e0b9cfc80493bface756f15
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:00:36 +0000

    interview high friend crash road

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def count_elements(lst):
-        return len(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
