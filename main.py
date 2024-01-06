16ab2dfbc7fdb523366c08263082c552e70fec4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:23:13 +0000

    right prove nearly value disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
