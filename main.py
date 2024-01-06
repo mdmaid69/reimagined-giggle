27f842315bae764b80b3010d0a066e1517c9a32f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:51:21 +0000

    better light admit stand pipe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
