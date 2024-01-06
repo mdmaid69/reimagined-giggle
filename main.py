205b7269894bd132ad0ae4c00398e1a90b691228
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:13:07 +0000

    do suggestion dull will paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
