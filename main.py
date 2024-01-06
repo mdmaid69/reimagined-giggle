5483aa3f969635ebe6d4344176bb509f4a78e249
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:17:08 +0000

    bank thorough move last schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
