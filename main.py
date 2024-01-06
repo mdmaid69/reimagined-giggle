37f01f5da3c9805a61ae76ae03903b789f4ff497
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:29:15 +0000

    importance imagination breakout can calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
