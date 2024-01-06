b42efc373203a5f94ce15187aece899106f92620
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:57 +0000

    otherwise comment hour meaning wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
