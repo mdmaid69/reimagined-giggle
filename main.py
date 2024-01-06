48386684552eac8fec5158fcd12f81aaa60bb1d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:59:41 +0000

    university development shop sign definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
