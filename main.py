b301db79175877f1cc825e9d2eefd70ce4e8b7c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:45:25 +0000

    cheque oil unpleasant Bb at

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
