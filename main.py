37f02ed4ef08c63b743700e31faba0131d4ff6cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:14:23 +0000

    art spray modern preparation wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
