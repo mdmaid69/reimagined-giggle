2179ebc7f89487fdba1f917acc62b6ceb821d1c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:45:49 +0000

    pot profit Ll achievement achievement

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
