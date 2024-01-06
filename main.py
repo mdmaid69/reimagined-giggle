d7ba9d3b32b866e09f3b68b105685795bafd6d05
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:25:47 +0000

    everybody getout imagine spring quarter

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_counter():
+        return collections.Counter()
