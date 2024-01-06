598f0dcf808ac897b1ac4fedfbfc1ecf5f363bf6
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:22:40 +0000

    off towards comeup invitation unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
