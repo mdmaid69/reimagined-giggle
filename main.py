c535c59e1433ff69137a319ebc38a5f522201186
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:53:30 +0000

    nowhere tough special week Pp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_volume(length, width, height):
+        return length * width * height
