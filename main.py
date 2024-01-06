c5f0beb15f0dce2eadc5227fa34a6c35e5b8d98e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:37:10 +0000

    pity pipe decision speech adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
