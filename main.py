691459bd18fa5b796e16cb7615f983d4402695e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:49:17 +0000

    past silent shallow word upstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def square_number(x):
+        return x**2
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
