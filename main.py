41fb353821ec425423260e963ccdb9e55bb31443
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:30:03 +0000

    now curious born me lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
