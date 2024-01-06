f59ee78e18e9bca663ccb8438c1be0ba3fa5d434
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:00:06 +0000

    name noisy or their fear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_speed(distance, time):
+        return distance / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
