2f1f7f424836923a453e82c80995c8233491345d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:27 +0000

    cinema plenty love positive pour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
