e9a716cec73656b828fea21d9012d097b7a64039
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:01:17 +0000

    stupid whoever attitude emphasize clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
