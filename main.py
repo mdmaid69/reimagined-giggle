5cc6258d9fb0878f83d0d070e0c380127461a3f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:51:30 +0000

    whole even hole thinkof undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
