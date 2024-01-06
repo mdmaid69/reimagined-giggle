b42f3eb88665ee3a74b61984612822d1532104b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:12:16 +0000

    by common anybody feeling total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
