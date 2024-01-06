bd8f5bbad10581f9e2b89a10e0ef6c1ce7834a21
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:26:37 +0000

    whistle which combine cook capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def count_words(sentence):
+        return len(sentence.split())
