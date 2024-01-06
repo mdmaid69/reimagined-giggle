ae3ea862497dd8dbe60888cb5db9668f801b583b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:13:00 +0000

    skill crash fill high badly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
