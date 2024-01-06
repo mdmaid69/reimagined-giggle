5e565c10c370bbe151aa47d9f0829e8cd5812427
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:09:42 +0000

    mess OKorokay slice quit accordingto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
