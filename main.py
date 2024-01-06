c9ee2537df92402778046684fab4401c2862b1ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:41:27 +0000

    buy base extra memory flavour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
