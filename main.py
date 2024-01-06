71cbcd28d1d17c81ded04d3ad701bd1ce0648249
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:59:47 +0000

    block seem mother promise size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
