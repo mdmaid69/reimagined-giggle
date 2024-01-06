91f2dafb1f9ee502522a128841f2707989698922
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:15:44 +0000

    task willing stop operate friend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_length(array):
-        return len(array)
-import logging
-def log_message(message):
-        logging.info(message)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
