c28fcee7b563d2cfbd8bbe8a54d6ba2fb633152a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:28 +0000

    almost second under tomorrow fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+from collections import Counter
+print(Counter("hello world"))
