3b29e2f2539ecde65e4e0acc2a325bdc582800db
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:42:33 +0000

    shout man stamp week rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
