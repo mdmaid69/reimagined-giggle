8e08d978598c8fc5686c826d5f6e89f9e738baf9
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:09:50 +0000

    flavour famous bank off progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
