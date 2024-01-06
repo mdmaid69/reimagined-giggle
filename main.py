4258dd3a2c3597203e92c11af46aa09349a16113
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:15:21 +0000

    tool rent common anxious wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
