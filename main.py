0a9322e40d65455d5aeba8f77aeb67d7827795f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:08:18 +0000

    positive possibility measure heart comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-for i in range(5):
-        print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
