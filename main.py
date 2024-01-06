e4c420861bbf05539b2e174d11a35eef4bc5f09b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:11:33 +0000

    wall belong common especially comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
