1963653b564f1b1cb4436d123b5a5c886b3ba415
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:17:46 +0000

    frightened whistle task break side

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import platform
-def get_os_info():
-        return platform.uname()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
