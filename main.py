a94e7f14e62ac9bd10d1d73e512b6b7645e0a1ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:53:44 +0000

    invite type yes shoulder hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
