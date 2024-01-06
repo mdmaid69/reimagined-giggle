af0eab223785b22ae3bc3eda4b507c30eb95ab48
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:14:33 +0000

    meeting matter forward before workout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
