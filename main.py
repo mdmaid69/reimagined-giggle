51a3aabbb15c60bb7b2e016393b81778a2ef61ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:14:48 +0000

    hungry sometimes mix single pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
