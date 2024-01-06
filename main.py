b5d71493cc9f60e0c290b01e9b70aa9172f66e28
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:41:41 +0000

    increase maybe possession sea good

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
