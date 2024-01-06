ae5a8e4445d079800289b818226aa5400a0b18a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:59:10 +0000

    lock belong manner flight strength

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
