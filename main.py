5e92971006bede13396e5c422545b9849961e208
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:41:07 +0000

    judge add affect delight scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
