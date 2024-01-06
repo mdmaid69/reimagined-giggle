a4677ac6c7cefe853a1eb62914a824741a54e6da
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:00:45 +0000

    switch today cheat tall chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
