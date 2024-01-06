186e013ba174546e51cdd6c64817880a8ad484f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:29:26 +0000

    achievement contents think hair intelligent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def convert_to_binary(n):
-        return bin(n)
+def is_palindrome(s):
+        return s == s[::-1]
+  def convert_to_octal(n):
+        return oct(n)
