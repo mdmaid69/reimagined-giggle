9386b0a7a0da340b31c8f6b263a565cbd4a7c11b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:52:06 +0000

    more limit stand great welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
