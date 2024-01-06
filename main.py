55b0ae2856c49e78acb63bca2cf41381bd1f0242
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:21:50 +0000

    soil concentration step careless interested

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
