11ccbbb098812c6e7e27b93365fb382f35321905
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:10 +0000

    trouble wet grow matter separate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
