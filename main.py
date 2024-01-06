44aa2a221f12af86b330398bbab9a1999ea265c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:00:25 +0000

    away breakfast thank work unit

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print(sum(range(10)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
