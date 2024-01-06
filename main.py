b0a17c473e63a89dc8cbc1b82881b3ac66f6b856
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:38:43 +0000

    them burn as zero city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
