fbbfbb689fa08b68ee7c1cfea2370c98304bcb55
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:00:23 +0000

    poison such wave door appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
