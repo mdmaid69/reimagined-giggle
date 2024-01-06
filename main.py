a2e6e3dfa87a90336eaab8f69f39b4df3edcfa80
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:08:30 +0000

    choose less dream past consist

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_density(mass, volume):
+        return mass / volume
