8041a1ac4bc92468590836c9ef8d1b5e50e076da
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:31:46 +0000

    breakup build here ask success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
