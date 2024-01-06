7ff0637e189e2d9c1a5bdf5eeea4213b94df61ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:42:54 +0000

    sad hair dance open suckme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
