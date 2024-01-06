cdc2d30e248ee3ffff2d9cd5d031d8b8c36a7855
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:33:12 +0000

    up warm answer slip getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
