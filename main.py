64e40b930c50f2fbcc14965e99f0a735b0dd8fc8
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:42:43 +0000

    feature fortunate anxious fear actually

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
