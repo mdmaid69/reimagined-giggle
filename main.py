ea9260dcff93265faa8c84ea7b1a2cd6fc9a4907
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:25:36 +0000

    ride watch bang area mess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 def square_number(x):
         return x**2
