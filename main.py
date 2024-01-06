d21232f50c19bf96cc192b80d370a03cd5ed6c32
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:05:55 +0000

    speak clue family show plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
