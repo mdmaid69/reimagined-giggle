dcadd1cce68f42efeb7cdea7bfffe3334ae38d29
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:43:57 +0000

    whom deep check gradually Hh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
