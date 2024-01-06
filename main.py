93c648362ac6a989049e35b8ba9a413280c042d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:31:36 +0000

    few avoid recovery water agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
