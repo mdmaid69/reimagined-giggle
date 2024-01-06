a2bd4b8b4017c2fc2a1780265e90e19b6481f565
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:42:45 +0000

    office naturally behind tool concerned

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
