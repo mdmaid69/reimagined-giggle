00913760f5101e80c7e87d73834c08fd9f5e12b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:45:55 +0000

    choice method shadow talk would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
