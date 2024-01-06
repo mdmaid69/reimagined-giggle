814a1a6f2de2de6e3baaa7dd27d6f330a205b562
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:00:14 +0000

    further apparently hair then store

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
