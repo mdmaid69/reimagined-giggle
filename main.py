5f89c69b7e00ddcaa482bfc79d1241c61e8e37d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:05:15 +0000

    early rush worth scream hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def split_path(path):
+        return os.path.split(path)
