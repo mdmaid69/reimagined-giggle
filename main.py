f03682991950ee6c402bf1c902bf855a45fa7bc8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:38:27 +0000

    flat complete near bet gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
