39d5cd284d97ead7d736c9796b2fdda1bc598850
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:11 +0000

    position hurry interview or total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
