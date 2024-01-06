420d7427e3320d9df40d20d822f99b1dda0c589b
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:11:06 +0000

    no carry explain copy makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
