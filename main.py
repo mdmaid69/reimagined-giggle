7ed93fb9a60278600adb1775d855da595ec2f659
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:18:35 +0000

    keep display me taste wind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def find_max(lst):
+        return max(lst)
+  def sort_list(lst):
+        return sorted(lst)
