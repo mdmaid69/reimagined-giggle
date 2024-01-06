c2eaa4450da26ee3cd949b667f5e65f5017725b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:33:49 +0000

    sure lower cross bar weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+def get_current_working_directory():
+        return os.getcwd()
