ad74a7a973f72a0fe9d875403962b7b4e90a5734
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:09:53 +0000

    sister unhappy deal back OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
