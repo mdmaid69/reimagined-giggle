b8e423ee8febaf699686f20a9a5f2d9588a67ea9
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:12:20 +0000

    real receive so sound computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def is_even(n):
-        return n % 2 == 0
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
