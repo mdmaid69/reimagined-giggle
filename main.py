b4aa32be417a1e0b2a31a2028d1f3bd1c30f31ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:39:15 +0000

    head immediate confused love spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
