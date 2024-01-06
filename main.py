ed1494dffe05247496f201b739105001fe944b04
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:29:55 +0000

    pride towards very clear doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
