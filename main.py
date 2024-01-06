8db85a364e98d2dc44c0846c9069b78dd17269ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:57 +0000

    attempt combine cinema keepoff value

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
