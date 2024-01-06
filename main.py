01aaa80eee67dcf9febf0675075a2500b9c6ff71
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:22:23 +0000

    hollow total war will related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
