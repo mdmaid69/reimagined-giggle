10073c9cb53a0424fa10ca5757f82a039792db53
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:06 +0000

    tap bury run bend watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
