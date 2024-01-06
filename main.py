a767449b2b919bec054907cb546d7f1cde743f49
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:37:01 +0000

    Vv part dance collection name

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
