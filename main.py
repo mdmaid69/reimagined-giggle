307c5118270169276674914f3727046280ef74b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:47 +0000

    must goodbye depend callout sign

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
