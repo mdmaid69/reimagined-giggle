e6e8a4c5de369c43fba899002845ca3013ec94e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:26:51 +0000

    bill file begin early possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
