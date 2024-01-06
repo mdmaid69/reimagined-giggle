125c4da181e0d7aa14a9dcb6cbaf25dd0acadfe8
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:40:59 +0000

    brush yes sore open keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
