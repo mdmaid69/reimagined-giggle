a2bfc1eccf825779ec76b328826bd98b051ca089
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:42:53 +0000

    improvement lead less callout stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
