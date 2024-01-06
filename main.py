6c0d9c4adee32ab3a6da2986511e812dfaff175c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:32:31 +0000

    most follow ago suddenly influence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
