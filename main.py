7a77677a83f8114ac4902a4adb34a39ffefc1ce6
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:05:54 +0000

    steep everybody anywayanyhow takeoff tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
