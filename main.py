840822baed0f02e910e2444ef3692e1fe75f90b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:56:30 +0000

    Cc happen gift away add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
