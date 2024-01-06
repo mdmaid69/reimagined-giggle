f5be1e39b31c464fe5d7b3ef51b08d7748a1bca3
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:26:07 +0000

    pencil Ii ahead taste dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
