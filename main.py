57e792186e69536bde07f45c549712bbb2914083
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:47:19 +0000

    hour closed dark sure someone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
