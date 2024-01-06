3600c7a38f2d4aec711745215ff0e8238f0aa4d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:10:41 +0000

    remember treat broad fight come

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
