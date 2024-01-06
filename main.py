9d6bb684f2af1a82a1932dd55838e6dcbc44e004
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:04:53 +0000

    tall ride laugh neat past

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
