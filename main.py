7f91d51b9d6aef6d1eea676be9f1b45f15c7fe69
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:13:49 +0000

    combination apologizeapologise last trick trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
