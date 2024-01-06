40a1dbb306f3ed95ac9fa80f0731cedf12c45002
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:33:23 +0000

    fight operation pass challenge point

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
