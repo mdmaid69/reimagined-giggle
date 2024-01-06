cdb898745d67d288ab95113ab1a5ed923f7a7dcc
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:45:56 +0000

    short getup neither contain corner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
