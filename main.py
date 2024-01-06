69d5b8b9efbb1549dc9326dfd3058ca491afa018
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:00:39 +0000

    both practise praise style patience

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
