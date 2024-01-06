28971855853a89f69d3ffc10c2c74d45fbf13b1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:41 +0000

    insist right burst belief please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
