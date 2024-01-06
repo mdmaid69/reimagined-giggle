a64b5b1f6bf590ee728105c1b617146a8603937d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:08:27 +0000

    trust argue bring image correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
