91f64f9b893288c8a5a8fce6bf14d9cab22436cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:24 +0000

    shoot cry forever equipment sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
