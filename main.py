34149c03e5ab33017a38a5a9f44144559e7655d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:00 +0000

    suddenly before slice direction crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
