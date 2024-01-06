afb9cf115667a227392db76c3166e6acad4a83f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:41:48 +0000

    do main 572enter quiet image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pressure(force, area):
-        return force / area
+def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
