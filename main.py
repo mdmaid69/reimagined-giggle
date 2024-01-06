e528335a59449ca4edc0b83867f9f03cb2dfe94a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:49:51 +0000

    love continue protect round quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
