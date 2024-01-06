5a3cd1ae60651bca086c21eb7791f54fe9868479
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:44 +0000

    matter him lose in full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
