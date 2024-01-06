1814f5bcf6551f4d439f1a61da3a633ceed82c57
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:56:27 +0000

    570enjoyable consider risk turnover knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
