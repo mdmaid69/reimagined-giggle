238f4511729b8a02599eda45be7b63df70ae3b00
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:41:40 +0000

    extremely possibly smile date introduce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
