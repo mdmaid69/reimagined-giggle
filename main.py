63b338b5cfcf953ad51cd1c4e466e1b76d6f04be
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:42 +0000

    either tidy moment journey callout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
