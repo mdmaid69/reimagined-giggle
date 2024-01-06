6a384a8a2816270b8eb2403e58d5b99f0cc5d25b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:19:31 +0000

    neck actual alive very interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
