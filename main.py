50b49e1216bc44fabbc7ddf7e54c282d5d5354c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:06:34 +0000

    sea perfect estimate consist argument

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_speed(distance, time):
+        return distance / time
+def find_max(lst):
+        return max(lst)
