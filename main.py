967c9c0fb99bc78489f601d82c2462d5b59b8f91
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:09:25 +0000

    all estimate shock point free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-print(sum(range(10)))
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
