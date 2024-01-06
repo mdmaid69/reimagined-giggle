e0277f0c61505e5f613381f437586e89abdc9cc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:17:36 +0000

    turndown add maybe tend custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
