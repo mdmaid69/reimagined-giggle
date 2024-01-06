a2e33ded2fc35226cf855f56609ea57d875d99a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:45:24 +0000

    mystery limit night offer arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
