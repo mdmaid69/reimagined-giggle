7b3454d22e3c15aeb4851e33f911df752f5da433
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:05:03 +0000

    callout deliver declaration accommodation hers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
