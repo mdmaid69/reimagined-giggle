fcf786745f325a9923f65fe1a38ead6e94e0d4b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:25:03 +0000

    any bread unable move carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
