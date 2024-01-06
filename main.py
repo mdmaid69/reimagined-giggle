b09153eb967e3bfac32881e563ef3c1fe9cc3008
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:26:52 +0000

    beautiful praise ear suit raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def read_from_json(json_string):
+        return json.loads(json_string)
