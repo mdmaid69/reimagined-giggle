7e60c327ccab82e2219cf36a64f7bf5f3f40c5ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:15:26 +0000

    convenient Qq correct turndown suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
