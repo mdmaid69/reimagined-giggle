dd1a73c6a4499bc499b82c5728674fe1af6879e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:26 +0000

    spite actual callup suckme country

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def multiply_numbers(x, y):
+        return x * y
+def convert_to_octal(n):
+        return oct(n)
