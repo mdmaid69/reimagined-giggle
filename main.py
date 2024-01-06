659b2733cfcbcca1d29db550372053754bf8bf17
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:45:03 +0000

    round connect left thinkof unlucky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
