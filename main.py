6579f725dec401bfd3534aba24c4e99748c83b7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:06:13 +0000

    science behave freeze back study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def cube_number(x):
+        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
