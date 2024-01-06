892dc9a276e733a406c3ba8e35f9e5b40d8914cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:27 +0000

    opponent towards judge over interrupt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print([x**2 for x in range(10)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
