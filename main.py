0ce07574e2621f1c6273a3a1f9f30e5b1ec286e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:57:55 +0000

    matter release minor wear pay

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import sys
-def exit_program():
-        sys.exit()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
