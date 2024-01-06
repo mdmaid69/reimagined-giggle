ece1487842fbc997a13e4dd7ea32f7a08be88e23
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:54:32 +0000

    break now term wait prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
