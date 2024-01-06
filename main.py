c76bebefd17952e01d7d71e4d92123bd915f5cc7
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:13:08 +0000

    breath brother load fire relative

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_string():
-        return collections.UserString()
+import random
+print(random.randint(0, 100))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
