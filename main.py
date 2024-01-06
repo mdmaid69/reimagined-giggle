9e274fc255d758ca22d7b5e41777e67fb0e6d271
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:25:02 +0000

    maximum finish tidy talk neat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_item(array, i):
-        return array[i]
+print([x**2 for x in range(10)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
