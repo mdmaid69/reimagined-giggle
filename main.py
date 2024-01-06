064e0008884e53e78a0247aad8e8127e96c40dbc
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:34:40 +0000

    group road slice borrow geton

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import numpy as np
+print(np.array([1, 2, 3]))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
