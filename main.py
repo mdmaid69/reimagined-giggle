85c3625c7b053028e46a9305dd1ae9964d835d5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:24:10 +0000

    home deliberately battery this centimetre

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
