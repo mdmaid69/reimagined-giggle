04a126cd3bf19d0b6524e4149c456f324e816f4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:20:12 +0000

    comeoff may wash wonder habit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
