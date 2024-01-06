c956b26f55156d282290b24040b8074171ebfd81
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:05 +0000

    information occur loud which safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
