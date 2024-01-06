c4ce42dbc0024d6f0e84e9a07e7c6a8029f91497
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:04:03 +0000

    frighten before valuable body bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def convert_to_hex(n):
+        return hex(n)
