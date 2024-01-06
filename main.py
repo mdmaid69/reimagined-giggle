ba88d2bb55b2b71db32b62629d605207be0dc646
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:52 +0000

    can sorry stop aware platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
