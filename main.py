46319fe76bef2b5e5b461225fc6c287d60b8d771
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:10:43 +0000

    spring herself harm later have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
