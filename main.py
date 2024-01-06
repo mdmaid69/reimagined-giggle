d0bd358cd8bedc4b22eb3b4e28b79bebb98b2460
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:59:15 +0000

    check scrap hurt speak before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
