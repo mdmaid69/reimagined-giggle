1822f1ddd096c374db54263a09dd4de9e72d72ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:42:15 +0000

    nearly supply future interview escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
