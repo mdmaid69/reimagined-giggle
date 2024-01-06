49d7b17dd3fbd0018fe5f07206fdef4fcc0845d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:51:11 +0000

    middle dull settle nonsense throw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
