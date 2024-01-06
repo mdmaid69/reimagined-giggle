eea0635add3fa37eb83f3f549a02ea972ba4922a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:47:51 +0000

    through ourselves turn doubt entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
