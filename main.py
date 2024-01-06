1b6a37c9074f2a450beecac4ac6bcd732c071fbe
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:03:01 +0000

    note among change run wind

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
