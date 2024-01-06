cae54911c1c96444bdff110125d895a9e544496b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:23:30 +0000

    spring advantage punishment typical quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
