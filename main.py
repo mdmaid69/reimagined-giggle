dc3ea15e1ba02bc610ccb51852232da7de5e60f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:51:26 +0000

    interfere card unfortunate programme bye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
