775a6c5abcdd017309461c80eada799a77d2c6a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:12:04 +0000

    careless shame trip share other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
