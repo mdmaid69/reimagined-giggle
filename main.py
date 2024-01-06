3fe5abecc05a833445f169fcb3c2a0567400429c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:17:39 +0000

    guess concerned sweep black unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
