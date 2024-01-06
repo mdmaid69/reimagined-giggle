475b26dde9cc30d429f400ed2fe276a791462071
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:51:45 +0000

    dance reduction contrast discuss trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
