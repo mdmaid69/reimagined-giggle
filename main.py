4ecb1de5f7a58a47139f6fce17c4c9a8881c27c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:35:07 +0000

    measure important quickly perform take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
