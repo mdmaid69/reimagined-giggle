74a62a07cd91f732393f81744768caa62081bb83
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:15:06 +0000

    swim control attract ocean sound

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
