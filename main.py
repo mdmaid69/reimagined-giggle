ba679f30325e33086ede1f4ae5655f581dba79b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:06:40 +0000

    off table complete friendly win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def convert_to_hex(n):
+        return hex(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
