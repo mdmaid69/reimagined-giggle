bf1e888f7ab3961e7f5660a34e5e63f41cb41e1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:39:57 +0000

    region every question harm breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import collections
+def create_user_dict():
+        return collections.UserDict()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
