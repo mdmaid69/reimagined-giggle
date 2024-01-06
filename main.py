4d7e4f1a89b14ba086b25f0ce0e3d43991d859de
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:43:11 +0000

    opposite stranger over some quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+for i in range(5):
+        print(i)
