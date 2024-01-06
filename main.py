24cf9dae714cd973694c2899d8d7cb3bb1aab378
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:34:50 +0000

    print serve complicated mistake less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
