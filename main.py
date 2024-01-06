9e2993764fe043e674c8f87bf844c5cbe0a919a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:29:08 +0000

    organize influence raw meal excited

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def reverse_list(lst):
+        return lst[::-1]
