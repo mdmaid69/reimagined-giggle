129822bceaba55b369ac87e40dff56c2f82a847e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:50:31 +0000

    same ought 565end giveback recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
