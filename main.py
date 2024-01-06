fae60cf6c69cfbc080e52b20c46c1594503d64a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:52:39 +0000

    powerful OKorokay naturally guess set

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
