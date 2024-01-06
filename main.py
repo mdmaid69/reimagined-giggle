5cf5f409be42ba56c5d084bf10f797392fc685b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:36:16 +0000

    free sore scared mend club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
