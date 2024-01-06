5c233f6e308db41825d07912568fcf53b4b3485d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:36:05 +0000

    familiar cry even sign grand

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
