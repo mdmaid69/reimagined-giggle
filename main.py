62ac41d2048e07fdc3d126e11239c423e70953c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:19:06 +0000

    suit not tear sort exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
