208e6115081e8ed0e0a7c32d85675ea5a735a8f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:06:45 +0000

    defend choose dick have certain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
