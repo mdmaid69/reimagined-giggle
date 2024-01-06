ad3d23b2f4c0a8058dab514f5eab87658db17add
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:36 +0000

    quiet memory party influence wrong

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
