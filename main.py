587ce5a050792fde16e2f557c3ff1994602eada0
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:21:24 +0000

    nor fear heavy Ll geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
