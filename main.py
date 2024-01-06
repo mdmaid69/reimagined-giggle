7bc255d5a63475917de9284266c9c464d51cf1a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:34:22 +0000

    handle plant pronunciation afford afraid

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
