80f533eb6401bc7192b0eee518143328cf05583f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:18:56 +0000

    easy decide shock safe fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
