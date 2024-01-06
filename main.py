0771d54600554d56fde7325102507bbeb3fea66c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:29 +0000

    imagination reserve truth small surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
