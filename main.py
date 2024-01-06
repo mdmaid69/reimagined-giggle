9c2d9d11a8d6c9ebc4c4e0ae38ecb61f9ba4c23b
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:13:53 +0000

    least shall keepup unlucky mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
