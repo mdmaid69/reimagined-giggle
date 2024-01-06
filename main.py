232ef58026a99a729920783023639eae0a884e40
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:46:03 +0000

    plant short calm cheek doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
