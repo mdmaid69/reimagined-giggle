6895c08c8efbddaadf416617fc3c71c1bd8355fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:22:14 +0000

    still top receive read solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def print_python_version():
+        print(sys.version)
