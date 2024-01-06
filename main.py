3277de6ffa0651b12270433090000d58b5fe6189
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:59:10 +0000

    help party badly promise unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
