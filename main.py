3f57c2362a6991750e861c2cc08f23eab3d91f09
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:52:36 +0000

    survive lend slightly cry hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
