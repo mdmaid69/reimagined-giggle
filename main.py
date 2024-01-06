07b1e5cbc4961ceed6c9efbb2f7af875e987ed0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:06:38 +0000

    bite convinced city temperature float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
