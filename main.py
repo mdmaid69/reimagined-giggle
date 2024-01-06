bec5b817891fd28019d94e0e3d30451a5f4def1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:04:48 +0000

    wherever interest calloff fasten creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
