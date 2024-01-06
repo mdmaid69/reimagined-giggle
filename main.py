a9ba50be96d1d090f7e94d10aed17beb7f590602
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:36:59 +0000

    brush coat snow treat recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
