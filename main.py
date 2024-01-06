b822c771e4ede1726d6245109d50fa7f8dbf18d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:15:06 +0000

    emphasis that type law neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
