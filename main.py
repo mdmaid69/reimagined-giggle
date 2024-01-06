1e97999bf9e42be41b013625f994eab5220032a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:56:04 +0000

    question ask extremely tell let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
