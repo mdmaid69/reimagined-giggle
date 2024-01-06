0acb8572f4894b23b2fc9a5c8ae1772ce46b6e10
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:41 +0000

    major track impressive thin state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
