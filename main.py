36663bbea7e7394eb27c3fa1bc2e33d30d44c7f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:34:30 +0000

    convince extra wave influence regard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
