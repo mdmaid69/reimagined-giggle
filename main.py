9f1396520557d8af4eefd545649a298befa260ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:17:47 +0000

    hospital shop detail minimum battery

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
