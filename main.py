e480fe271e52333f604a4398def469f0f1cfe253
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:57:03 +0000

    succeed fence unlucky complete or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
