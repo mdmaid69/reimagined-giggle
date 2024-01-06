883d093f2b85a7ebb953946ec4912428d192b9aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:47:57 +0000

    half wherever uponon smoke coffee

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
