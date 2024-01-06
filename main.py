b557a402f424b691b8628b102bcae5029b0ca2ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:43 +0000

    sticky any grateful attention question

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
