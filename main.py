a3d8b17d03a61cc53a4751998426b2ed088d0d1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:51 +0000

    public birthday advanced cry warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
