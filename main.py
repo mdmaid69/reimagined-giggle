58459a1e6e9c9c76c19590c512682382c17210fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:12:39 +0000

    somehow limit rain will silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
