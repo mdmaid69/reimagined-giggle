69e0b9e1056321d457b7aa4a07f15d814bfc62bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:01:35 +0000

    building drop raw good nerve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
