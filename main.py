091c53b93a2cc3994a34cd72648d5dd014e28e4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:55:45 +0000

    high almost suffer bright operation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
