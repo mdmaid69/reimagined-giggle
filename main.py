6068eedef2a45118408f13d45b378b9608d124d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:47:44 +0000

    influence when break peaceful value

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
