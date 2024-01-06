93381b690de08fd18866554836796207962cff48
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:53:39 +0000

    Kk accordingto satisfied flavour Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
