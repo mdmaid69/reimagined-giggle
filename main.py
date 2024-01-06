97aada4522f2e6ee8130ca1638c334d907363287
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:53:07 +0000

    comeout Yy Jj free church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def remove_duplicates(lst):
+        return list(set(lst))
