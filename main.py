26e42eef30a5d38d40424815a74726a5a4ba30f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:41:10 +0000

    personal centre serve splash complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
