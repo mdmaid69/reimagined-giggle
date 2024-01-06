6318ba745f24d10c6bddd765bd07457493fcac51
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:22:06 +0000

    disappear present waste still hang

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
