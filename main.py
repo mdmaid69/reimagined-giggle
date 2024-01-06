c177b1b6d83e6fcdd87ab20025486fe0e25d9347
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:13:37 +0000

    fortunate beat perform wrong again

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_work(force, distance):
+        return force * distance
