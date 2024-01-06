37e92c089b798538cb0875deb0abc75eb26594fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:24:47 +0000

    better grade Vv remark post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
