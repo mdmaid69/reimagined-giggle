4dde1ebdf1028de36b6eedd08a0e18ab1b7dc105
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:59 +0000

    damp colour measure protest grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
