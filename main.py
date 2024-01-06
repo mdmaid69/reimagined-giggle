b09f05d82360bb027ef81edf2f71ba7708a79eca
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:36:11 +0000

    above reserve arrange hand smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
