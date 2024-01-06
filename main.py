7da3ec8efaf9d94cd53028c6a62c25101bb1ab75
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:21:43 +0000

    theirs software mark law ago

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
