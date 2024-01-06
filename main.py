6c7ad5d254c4ae004def2f06daf0b16e5a80accd
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:36:06 +0000

    turnover fierce shade principle 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
