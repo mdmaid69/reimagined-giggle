32539fa3f170b7999b9b28f8b7b69659226e165d
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:35:42 +0000

    intend seem impression drive most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def append_to_array(array, item):
+        array.append(item)
