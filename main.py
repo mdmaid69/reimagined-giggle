816e146d10e8ffb2fa33f2f29df8b1a8c7a7d9f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:14:00 +0000

    179bite dream announce rest discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
