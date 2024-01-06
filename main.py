596535fc642a0f45cb0b40377a0c580890f7807f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:07:17 +0000

    movement name treatment free as

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
