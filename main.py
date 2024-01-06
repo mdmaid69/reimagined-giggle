0489cc6fa968eb739fbf1bedb68e98833eb6a76f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:08:03 +0000

    afraid basis marry bottom clever

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
