df81be36c32c60531ce5cdd9bc364de7d7928350
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:00:13 +0000

    proper concerning hers correct colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def reverse_string(s):
-        return s[::-1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
