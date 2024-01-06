fe06915e742bb4362b7bf8b369bcd46ecfbb31c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:04:32 +0000

    recognize even dream agreement organize

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import datetime
+print(datetime.datetime.now())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
