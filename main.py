68785bcbf33e5c06b5546c216008b3009f6badc8
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:01:20 +0000

    callup Gg further skin it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
