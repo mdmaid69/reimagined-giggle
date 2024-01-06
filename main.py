ce7a868d64b4556a484c62612263fcea45e12059
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:37 +0000

    sleep avoid radio low introduction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import collections
-def create_counter():
-        return collections.Counter()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
