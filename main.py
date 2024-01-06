d2b598fb0f7667fc553ab1ca17b76aa61e21392d
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:30:38 +0000

    health except trousers properly dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
