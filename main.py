430e9578db14f813fb800a7925eed9c98877d481
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:19:30 +0000

    exam disgusting comment march pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
