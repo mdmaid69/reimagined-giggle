8d3f43efe6001084f75025ee675de3387fe25fe3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:18:50 +0000

    clear should petrol radio hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
