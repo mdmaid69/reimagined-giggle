4241d6dba3c74a774f97ee33d56a344f7e22d292
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:40:11 +0000

    chance comeoff voice music on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
