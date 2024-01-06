36f63dfa09fa98ca97458c34d8d83c8c6e11398b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:02:34 +0000

    laugh for vote final after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
