2f9ade4bc0af441d6a018c6a37956d2bb4dc9a05
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:03:39 +0000

    everyone bridge black in broad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
