d53000f84fe6f31563b223bde3b3f80951b341e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:48:28 +0000

    amount promise what impressed form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
