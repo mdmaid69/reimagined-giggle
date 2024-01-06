1119d373c7275e8d5f4b31c5e65878c89f144e0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:27:26 +0000

    who wherever flat OKorokay entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
