10e05ec93622eb45787b3bded526ff9a0d62c6d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:59:10 +0000

    forget taste see comedown cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_queue():
+        return collections.deque()
