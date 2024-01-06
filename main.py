59e68e3edbbd443e8029398733960542853cce01
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:50:23 +0000

    often add goup true tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(numbers):
-        return max(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
