b82becc4cc43f5279c914dfc895dcc685526f945
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:53 +0000

    minor adjective instrument every have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
