e6b5e2fc134680f9d49dd53544d43594b05f434b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:31:13 +0000

    best would scream wish guilty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
