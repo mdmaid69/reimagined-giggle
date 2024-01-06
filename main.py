b05608a270b5230fed77f98b3d4e04af6324de63
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:33:49 +0000

    strike innocent flow country off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
