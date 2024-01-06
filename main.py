07fbaa545e01926d4d7750c5cea694b0745123ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:12 +0000

    however bread throw 567enemy increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
