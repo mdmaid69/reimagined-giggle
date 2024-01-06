cc1795c0bc55f6125d5536ae7ce91ce3a2f3c4cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:56:15 +0000

    thought skill Cc track cover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
