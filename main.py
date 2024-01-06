28ac389964e346633e95f9afc3523950468286f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:10:06 +0000

    minute permission disapprove transparent glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
