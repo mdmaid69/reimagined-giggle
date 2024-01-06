33fc362cc940b5e5184d2adb91a011ce8a9fb542
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:04:32 +0000

    thin guess sometimes OKorokay suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def sort_list(lst):
+        return sorted(lst)
