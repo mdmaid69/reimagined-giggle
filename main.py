194d69af3a97c3306aafbaf08ee02b94ff7c8bb5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:48:21 +0000

    precise dry pair soft wait

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print(math.pi)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
