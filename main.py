9768135be2a676425854ff3af0eb417c121af1c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:38:20 +0000

    march can challenge chin born

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import sys
-print(sys.version)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
