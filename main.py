c4e1679250007adb4813ce15df51c42a9f8bf7b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:31:13 +0000

    satisfied along will treat occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import sys
+print(sys.version)
