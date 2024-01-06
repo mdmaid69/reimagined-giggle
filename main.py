cf2af75d56126b264086a2a0d9e4628c367857e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:02:09 +0000

    date declaration furniture suggestion whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
