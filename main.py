47abfae20089c4bdee26a5c399494895a21e5281
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:47:56 +0000

    beat arm cinema fine Nn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
