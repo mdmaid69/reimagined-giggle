e4f6da291c72521962a8508d3ba54d6df49bd93c
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:07:25 +0000

    ocean prove apply although loss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
