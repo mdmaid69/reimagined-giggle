3986194f97d12e49a33301b2eec5c43a80fb736c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:59:38 +0000

    refuse objection fly takeout theirs

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import platform
-def get_python_version():
-        return platform.python_version()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+for i in range(10): print(i)
