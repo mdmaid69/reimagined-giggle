9e58424b892eccf3c970ddda153531571eabc319
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:38:40 +0000

    treatment considering service progress ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
