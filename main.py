79b270674111449579f18e21afde16f7b75740d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:57:16 +0000

    splash yet shake neck plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
