7559daf6df363ff9fba3b097e650654e34ac00fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:49:14 +0000

    related arrange certain meet spare

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def find_max(numbers):
+        return max(numbers)
