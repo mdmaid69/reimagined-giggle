e13cce0c3fb07cb645876af06dea6fa809a2ecf2
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:25:24 +0000

    confusing excellent mix uniform string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
