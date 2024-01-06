17d2f144b4c85e99db4f26f6718d2c11f3c678bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:28:30 +0000

    bank system sale lend hurry

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
