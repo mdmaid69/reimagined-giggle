cc52923134f8b513b7418e4eb64f6b4f44893e5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:13:19 +0000

    walk skirt police while care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
