71d94b1c3751fd37fbc60c07122b3d497cb43ad8
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:34:56 +0000

    secret think abroad match that

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_min(numbers):
+        return min(numbers)
