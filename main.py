08bc1f3da390b44cd93ac9fe483a9ecd5b52c9d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:09:02 +0000

    formal spend side birth clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_octal(n):
-        return oct(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
