66a4acaebf6988cf56c9c22cdd257a0785c4d265
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:12:58 +0000

    workout open whatever welcome Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
