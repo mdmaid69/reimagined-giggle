ebfb64b1aa622e3135a68d6d4361af977639738d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:29 +0000

    everyone Jj talkback city beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
