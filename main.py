d77dcaa101c46af70f772dadaacc746757783d69
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:04:02 +0000

    raise or ago 565end stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
