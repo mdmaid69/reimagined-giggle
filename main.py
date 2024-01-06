c3503c75854481977b5bd6ebeac18de872f12c07
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:15:11 +0000

    tax mysterious name no suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_user_dict():
+        return collections.UserDict()
