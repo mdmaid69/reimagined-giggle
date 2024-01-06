2b2b66444e7010b4259f77c7c458c8b120a321a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:44:43 +0000

    lesson favourite miss count best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
