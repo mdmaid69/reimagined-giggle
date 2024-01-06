0f0bf3926fd7e9abcfad18b26298d959cf4c2d95
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:34:00 +0000

    wheel enthusiastic trap brain clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
