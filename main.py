6f7deed1776790994be77a37564ff6eb312470fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:55 +0000

    flat rub improvement help love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
