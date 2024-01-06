cbfd7f715bb89aa28c02a7a0f48984dfda5b9e5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:21:04 +0000

    offer order represent load impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
