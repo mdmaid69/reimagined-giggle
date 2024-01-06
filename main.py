af73b3d9eab12003197e991514bbc2e205c272fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:25:21 +0000

    home comeoff find approval name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
