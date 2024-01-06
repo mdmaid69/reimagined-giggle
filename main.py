8535e0122c0fb65c4e27eb3a1011551f718f813a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:53:34 +0000

    will callout reject pin hurry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
