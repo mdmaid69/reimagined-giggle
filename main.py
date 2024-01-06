1002c7f3e8829d3a4176798602e2de1d113b0307
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:01 +0000

    by its behind they professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
