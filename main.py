033135bcb85f5962c89b36cc2c73fc05fb9f584e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:16:49 +0000

    Ff petrol indeed story bit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
