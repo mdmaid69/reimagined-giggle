8f3b8cadc3cbf42c3a62b426aa2cea60a167f912
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:48:48 +0000

    between word design whisper through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
