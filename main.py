4b3b7af3d0cabc6a4a5f5f8785595ef2a61893f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:03:14 +0000

    Yy report prevent office intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
