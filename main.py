d7b7feb095fb980f0d0fa96948e0814abc293afe
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:18:21 +0000

    571enough train real choice trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
