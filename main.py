689b991ed9f3658b3ac4be47243e3f7c8a3398e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:23:21 +0000

    stand where range slice badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
