281a5d93c76e39f48af7e30d29bd4960d8fca434
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:10:34 +0000

    gift turnon exit Zz check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
