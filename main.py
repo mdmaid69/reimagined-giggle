77b30b23f11cd830b8abfea40d089453e817b9ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:21:06 +0000

    anxious Ll stir clue doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
