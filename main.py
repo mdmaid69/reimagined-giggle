6548f40bcb7af9213383b4f9be3dd347cd550647
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:09:19 +0000

    yet stretch moment wire exchange

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def reverse_array(array):
+        array.reverse()
