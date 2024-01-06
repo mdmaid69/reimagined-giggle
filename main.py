b9fed76323c576c0616b41373e878c54cadfdf71
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:06:24 +0000

    interruption kind actually hide naturally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+def change_working_directory(path):
+        os.chdir(path)
