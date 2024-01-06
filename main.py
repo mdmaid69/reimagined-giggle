d8770a623b86382fe4a56af2c9ee20880e5cb6c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:32:17 +0000

    send bang doctor comment pause

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
