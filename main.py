186d01593c8f2b0b5408c4ffc969ed417af6ed40
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:17:38 +0000

    modern reduce partner clearly laugh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
