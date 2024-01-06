782391a920443f5ffe8f838ccf3342b02f737006
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:10:49 +0000

    mixture doctor by guilty chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
