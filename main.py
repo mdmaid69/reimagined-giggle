2ef0b5649a246abd42f3a783e0eda50f099e5545
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:13:59 +0000

    activity fresh meeting makeinto steal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
