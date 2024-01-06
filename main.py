e031d77024e7fa2379c73a33c95971dabb2380d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:16:15 +0000

    reduce after from wear violence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-print(sum(range(10)))
