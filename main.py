c07fe308cee68b6e4dda73609ab33faee55ba3f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:12:34 +0000

    contents stranger pool accident careful

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_volume(length, width, height):
+        return length * width * height
