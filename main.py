dcc31b629f38206422a92930b2ca024d3a11a051
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:46:21 +0000

    tool big listen spend connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
