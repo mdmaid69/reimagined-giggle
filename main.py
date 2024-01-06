2c6d5bed1566ae3eb54e3c23e51104ed9262524b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:26:14 +0000

    friend sometimes beautiful market couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
