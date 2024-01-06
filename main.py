7f49b69aff77566f32a4833d7af98c592e3906e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:29:53 +0000

    relax base break world turndown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_circle(r):
+        return 3.14 * r**2
