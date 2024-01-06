274b67dc437a772dcdf4da6530ce6221d8a76c1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:02:01 +0000

    stir holdup of excuse makeup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(numbers):
-        return min(numbers)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
