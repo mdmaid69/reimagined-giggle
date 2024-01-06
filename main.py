74f1953c6d366115d9dfeaaffc42e5ef411761d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:54:57 +0000

    back willing honest describe born

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
