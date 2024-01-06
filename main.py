d8717169a15cee6a43cc1752249d08275beb43ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:27:44 +0000

    smell exhibition tread band mean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import time
-def get_current_time():
-        return time.time()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
