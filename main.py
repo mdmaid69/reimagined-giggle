6c7cf1a4e518d41f1d3abb8b275ffcce7b948f4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:54:14 +0000

    fine will pour burn receive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
