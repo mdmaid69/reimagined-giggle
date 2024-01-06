9b71c8f12d12dfce1e776badde1e05f430003783
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:50:54 +0000

    break tap speed 572enter lot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
