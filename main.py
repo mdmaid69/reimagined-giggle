c2a2fccb5b894827e9b9fabb070c46f51d7a97b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:30:26 +0000

    her organization thumb means event

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_average(lst):
+        return sum(lst) / len(lst)
