2f76cddf260a4ad4b75aca7856918286f34937c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:25:23 +0000

    through bring imagine meeting sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
