86227d389f476e3d1bbbe0131028983c90e74ebc
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:43:18 +0000

    from turnover front splash club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
