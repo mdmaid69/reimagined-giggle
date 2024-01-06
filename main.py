3a9b0c7922caeb66457d4b5f3533d5a1efee8e25
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:07:38 +0000

    voice platform fall trust peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
