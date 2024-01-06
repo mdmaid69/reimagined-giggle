0574db4be81fbd781a62f5c806f9e991b42091ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:14:21 +0000

    wheel imagination praise least argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
