cb59c51573b720f1708193399af5a26abfb1509d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:48:18 +0000

    top probably row play value

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
