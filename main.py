d4a2847e84faaa95c09210396f6c0a16e3b62b6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:36 +0000

    identify price order circle sometimes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
