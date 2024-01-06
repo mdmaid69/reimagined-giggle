ed0f3cd6b909ba550b2d64f598e5ffbd9041498c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:08:37 +0000

    hang nearly to appearance rubbish

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
