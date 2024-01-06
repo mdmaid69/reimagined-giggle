cdc9a53972b76421e9249c7ade4f5f56e36caf52
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:13:32 +0000

    term building meeting adjective continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
