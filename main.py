37b74506b81f4c2b5505539715abdb5c39efcc53
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:53:04 +0000

    sensitive apparently black identify change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
