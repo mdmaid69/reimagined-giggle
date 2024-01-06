91c5b44da6715b0e5a47b18324afd83aa981b272
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:33:46 +0000

    ilovedicks split inform famous loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
