713a55f3bd624f2114814aa750304c59403e4008
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:27 +0000

    major plenty contents flat through

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
