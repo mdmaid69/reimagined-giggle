22c1b4bbc6b3f9d8fc9a2b85da6e739d42680e31
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:10:58 +0000

    ago disturbing lend centimetre cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def clear_array(array):
+        array *= 0
