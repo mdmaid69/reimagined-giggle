78d4e1fd831e556b34befffec5752d76d7cd26d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:29:24 +0000

    delight like quality fetch pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def convert_to_binary(n):
+        return bin(n)
