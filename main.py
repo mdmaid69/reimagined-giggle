d88e0ec0aa27271f38af710391b689916c62acfd
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:28:21 +0000

    split nice bang easy tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
