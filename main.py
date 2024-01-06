b8f6d3100d6dc2c40136d580c2cd926f4f2e35f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:57:19 +0000

    question ring serious people mystery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def convert_to_octal(n):
+        return oct(n)
