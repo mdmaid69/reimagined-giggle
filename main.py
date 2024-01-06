2075d764d386db5fc507d4f9d20809dbbd255b68
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:26:10 +0000

    crazy meal trip wash police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
