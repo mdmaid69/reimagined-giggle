6a02450dccc9ef3650499d6f1d6f863c1481f39a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:32:42 +0000

    hope afternoon fun official upset

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def is_odd(n):
+        return n % 2 != 0
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
