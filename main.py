3d4b186ec003997cb408e1390e1b891b86e062c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:01:53 +0000

    lonely justice turn mainly down

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_min(numbers):
+        return min(numbers)
