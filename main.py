471302d1b576ae34389bebefaa6f96b737d82239
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:59:35 +0000

    personal eye sell ring fashion

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_repr(array):
+        return repr(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
