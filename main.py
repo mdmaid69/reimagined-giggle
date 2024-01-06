20da3f313dd95b98e144a44dd11d1e2eb59a1afc
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:59:20 +0000

    anywhere arrive wise brush from

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
