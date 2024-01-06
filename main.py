3b38ee4251800394b596fcea0cdd516dc09df480
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:56:15 +0000

    supply early complicated than turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
