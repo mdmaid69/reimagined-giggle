1050d522991b2d5b36d7c409125ab399baee10bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:47:11 +0000

    confuse health near performance no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
