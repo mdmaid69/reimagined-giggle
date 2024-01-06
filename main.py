10b24fffd6d86ea88bf741770d3528d9c1d87d2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:25:35 +0000

    571enough ignore steep day confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import datetime
   def get_current_date():
         return datetime.datetime.now().date()
