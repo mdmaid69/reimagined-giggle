384f993cba5679010714d53ee60c2c0d6174e909
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:51:05 +0000

    judge race action tall touch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import json
+print(json.dumps({"name": "John", "age": 30}))
