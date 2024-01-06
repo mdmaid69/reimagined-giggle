369b09696655ab9ff910a541dfceac9616467f0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:14:16 +0000

    demonstrate takeoff exciting regional book

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(10): print(i)
