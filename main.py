4e90ca91cd3b2c28b3e664d426388b542964ceef
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:46:25 +0000

    sink concern arm refuse separate

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+print("Hello, world!")
