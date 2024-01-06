f1370f8fbfdb4e866bc5252998e9e08660436250
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:54:58 +0000

    turninto beyond seem trick song

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
