c6d2cdbb531e81c0eb5ba99427e420e4fb34eb46
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:26 +0000

    bar holiday market comeover Gg

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
