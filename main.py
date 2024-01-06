fcb33a48671720a8d554bd641149530d7a9cf97f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:09:02 +0000

    nervous answer standard slice Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def add_numbers(a, b):
-        return a + b
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sys
+def print_python_version():
+        print(sys.version)
