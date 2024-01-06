e59235b85c5de1761f1be5951206f1e6c9648c9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:22:51 +0000

    software hungry safe surprise much

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
