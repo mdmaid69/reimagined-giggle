276e0452630201a3932fff89d2239db6156fca45
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:49:34 +0000

    accident large thin throw priority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def sort_numbers(numbers):
+        return sorted(numbers)
