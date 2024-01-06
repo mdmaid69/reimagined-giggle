32eb3afa3cf29eb80501f75f71a1663b7863ac02
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:14:56 +0000

    weather apply need must push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
