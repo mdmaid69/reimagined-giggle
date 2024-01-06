9cad10dc089f44685638f5353752780489d7cde1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:50:42 +0000

    embarrassing bringdown sleep nothing direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
