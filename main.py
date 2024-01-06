6be930b51ee0da225d54e7296d16205063f701a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:05:26 +0000

    worry remain bringforward treat light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
