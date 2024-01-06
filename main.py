21edd9b963e48a64f3ae7dccad9aa0d87dd90d10
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:27 +0000

    raise bother fast quickly that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
