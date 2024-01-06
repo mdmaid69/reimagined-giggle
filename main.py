fb97aecc0d8ad3a9c59170a6f35f0f94d9ce2b17
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:37:03 +0000

    disagree contribute getup genuine comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
