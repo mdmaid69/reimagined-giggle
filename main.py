7aebf565ea2248cd55c26d95891b47d05fafb559
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:56:00 +0000

    late drink exam sentence closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
