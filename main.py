bf9529a2ba50f7566c304d478b3a45833f673add
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:14:31 +0000

    now split about fine determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
