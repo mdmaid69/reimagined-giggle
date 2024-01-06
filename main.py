1ab1c0ae22d7ca4b1923d0124513e17163625fbe
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:45:29 +0000

    branch Gg anxious ill friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
