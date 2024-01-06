70aeb05c72cbf37dfd9639313e9abea0e8c3c825
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:25:30 +0000

    form common spill demonstrate breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
