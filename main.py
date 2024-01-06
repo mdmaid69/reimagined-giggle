f81cf6d6762a47e8585259088ed19acf72826ad2
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:05:26 +0000

    practice mess sore brain you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import numpy as np
+print(np.array([1, 2, 3]))
