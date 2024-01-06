11e16e1f267dd497e8e6bbac3e0a9e351622f002
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:15:32 +0000

    accident bin delight of less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
