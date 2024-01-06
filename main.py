9d27f4e9b20e0a4b23d382d3fb4d57c6a0dc9566
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:15:44 +0000

    although Uu variety train lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
