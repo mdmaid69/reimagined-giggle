5a496c5bbe1cb1d39b41afc85886ee172b5b1e19
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:40:28 +0000

    personal convenient level usually promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import platform
-def get_os_info():
-        return platform.uname()
+for i in range(5):
+        print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
