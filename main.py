39ca40f44686bad3855a5dfdb0a9386afbf798d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:23:26 +0000

    stop crash herself car mistake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import collections
+def create_counter():
+        return collections.Counter()
