3dd140f3c42a4868eafdfdcde40c5c9047b853cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:03:22 +0000

    confuse advise holiday centimetre along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
