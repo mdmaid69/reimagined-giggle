ef1da11dd0e08ffbd388222c47c8117edd4c332e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:42:28 +0000

    confused deliberate shame stir take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
