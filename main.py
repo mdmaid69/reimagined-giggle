b6538898fe8a84a1a405172c606e7591e0af3e24
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:23:31 +0000

    roll great direct sad front

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
