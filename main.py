2bfdd242a81ff2e5ea8f9d3ffcfcefd90dc2c398
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:54:47 +0000

    physical shallow set shine range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
