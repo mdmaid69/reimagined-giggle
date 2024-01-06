61e0987782f454bbd3cd991fc67c335b6e892d22
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:42:54 +0000

    bite smell recognize against design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
