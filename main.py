63a4665f97ce29a48b05d8b1bbcb797ad8051e6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:54:38 +0000

    advise black hungry grade Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
