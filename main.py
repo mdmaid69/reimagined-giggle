e38f5aa60341316186c89a620cec35488b98f180
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:17:16 +0000

    great great remove replace you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.ctime()
