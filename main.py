869bdbbf4fe66e11cd66ec53ac656dac20ff3673
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:43:10 +0000

    better should marry how brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def roll_die():
+        return random.randint(1, 6)
