4a82eae3c0c7fedeb49fdb63da485c911055a70f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:53:13 +0000

    return Jj join brush message

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
