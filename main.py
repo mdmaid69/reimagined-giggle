c34199c1d820279f3278d669a1d719fd707c8adf
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:24:19 +0000

    meaning feed possession wipe joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import getpass
+def get_username():
+        return getpass.getuser()
