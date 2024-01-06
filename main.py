37b4020394287b26fbb53ab0a22fff0d6c2837d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:07:46 +0000

    bit odd nowhere mother confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
