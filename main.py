a61b78319654fafa43e55b00bac059b9cad6fcb5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:41:08 +0000

    afternoon question Hh fold least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import random
-print(random.randint(0, 100))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
