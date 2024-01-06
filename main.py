3accac7946eed10ccbe0e896a978631426b76669
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:23:09 +0000

    admit aim properly either block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def square_number(x):
+        return x**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
