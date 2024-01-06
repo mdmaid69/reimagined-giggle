7d0576bb9186de37cdd3edc53687044d20aadbe1
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:45:32 +0000

    among top unless along any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def reverse_array(array):
+        array.reverse()
