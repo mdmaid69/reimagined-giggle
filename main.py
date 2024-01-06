5ee342db3693c326025d099c4f56faf27292f411
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:15:02 +0000

    but care scream because spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
