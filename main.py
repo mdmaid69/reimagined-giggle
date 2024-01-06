ec46f1223859bcf7ba7e13bf31021996c4b9658e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:01:47 +0000

    value pen unable evening form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
