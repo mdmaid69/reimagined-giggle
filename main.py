e82b940f6b88a4f1742a47804e93b85835accadf
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:24:59 +0000

    here close repeat quantity represent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
