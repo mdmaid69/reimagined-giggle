1ef078413e51b12febd13857864e55645b8f0a67
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:24:17 +0000

    behave lay swim mysterious mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
