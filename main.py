ee7c86280554dc6ad8c2d5330a8ee4b35a04d50c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:38 +0000

    care thank dance tomorrow cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
