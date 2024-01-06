1127d69f23df067b7fa0db2e6ea367e5503eab20
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:36:47 +0000

    idea friend call entry spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def count_words(sentence):
+        return len(sentence.split())
