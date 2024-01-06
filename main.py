51817f2833fb10a41d718a57cc61b83735c8a0cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:51:39 +0000

    hold silent pipe married describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
