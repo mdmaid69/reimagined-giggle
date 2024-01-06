6087791f5aabb3dfd02d86b7b23d0572952cb163
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:21:03 +0000

    except famous convenient makeinto Ww

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_density(mass, volume):
+        return mass / volume
