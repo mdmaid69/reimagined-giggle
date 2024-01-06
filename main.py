6fc40e4f5ed72d2331fcaef49584f76501d0f436
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:41:52 +0000

    begin hour also image task

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(a, b):
-        return a + b
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_floor(x):
+        return math.floor(x)
