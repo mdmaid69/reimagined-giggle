3997a0da67bedd968f7051f5d13ff6cb8428fc18
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:02:57 +0000

    breath sky entry show giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
