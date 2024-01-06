9fb905c58885ed8b7542d62d475fb17deaafd2db
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:09:04 +0000

    ring ours radio you object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def find_min(lst):
+        return min(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
