8c3b3fd4d933fb7e87ee7f96554cf7aa93cec685
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:08 +0000

    thinkover up demand set shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
