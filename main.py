f3dfe29f5ae1c30bc17bf1c3d5693850bfbfbd37
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:44:31 +0000

    complicated also practise suck beyond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
