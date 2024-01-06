9d9614ed1be97915dd527e55433b07880a3dfb0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:42 +0000

    off pity name jump already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def square_number(x):
-        return x**2
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
