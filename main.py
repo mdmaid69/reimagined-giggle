0a83ed2c062b462fc491fc09ac04068734336282
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:18:28 +0000

    back organize could watch picture

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
