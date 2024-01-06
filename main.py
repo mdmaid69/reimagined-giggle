47a868527600bff1e9a5f6f7680a39849aae94f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:13:30 +0000

    quick wander dry call danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
