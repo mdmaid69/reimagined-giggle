8b99a9323c8c84a3f22a74e66b1c77b221d09fb1
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:55:46 +0000

    teach injury write custom sensible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
