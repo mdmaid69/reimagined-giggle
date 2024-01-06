7ec954526ada943b728b4f1a63a404a37f883e0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:54:55 +0000

    179bite space stare introduce plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-print(datetime.datetime.now())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
