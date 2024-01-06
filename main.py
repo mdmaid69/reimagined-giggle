f5a2830e9310c3adb77621850aa75fbd5cda3c92
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:21:54 +0000

    supply approval godown Itself left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.time()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
