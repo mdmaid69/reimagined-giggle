82c340602a5d9a8fc1fc64b16bc9ad5052663842
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:05:33 +0000

    charge advice war disgusting support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
