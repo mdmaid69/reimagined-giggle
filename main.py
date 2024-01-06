fbad05dcf0b21bf3a380ceac0ebdae7dfbe52947
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:50:12 +0000

    this design motion now possession

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
