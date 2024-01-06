a2d96706d2606b58ca1c4e4476669972392a7d28
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:31:06 +0000

    wet eat active final concern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
