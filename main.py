9f5ed7441739af98253f98a4f7c3d4cda7bdf734
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:55:55 +0000

    correct nature detail centimetre receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
