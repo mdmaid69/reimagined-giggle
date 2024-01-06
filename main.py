2adfcdfab890b918ecfac42b16e03670b721f557
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:55:33 +0000

    breakup worse leg tense replace

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
