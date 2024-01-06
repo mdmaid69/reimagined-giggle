fb6666c0b6c8bbc68672b0bf572f9d3a6182e5c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:53:48 +0000

    back wish Yy traffic declare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-print("Hello, world!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
