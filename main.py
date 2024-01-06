7d1dad44b5f8fc43a73e451d3e0b94365e1a02fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:15:53 +0000

    suffer return result produce extra

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
