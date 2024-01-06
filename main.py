386187aa11428b9a3412ab3e2042616ad7ea6d75
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:39:55 +0000

    catch shame embarrassing division town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
