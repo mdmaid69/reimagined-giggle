6d4973fb9fa505ee67b32d195c610ce462159b60
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:27:31 +0000

    snow value preparation pride phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
