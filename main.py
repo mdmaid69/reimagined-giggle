d7bb3b557295868b41154b98fc465de264ce352d
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:08:31 +0000

    hole lucky takeout giveup dress

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
