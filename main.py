451526ecc53c4e2c6b0a021aaa49f83c3e464ac0
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:28:45 +0000

    tell female range display nervous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
