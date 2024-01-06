42f181a2ee7fb4d07912076b166dcac125e8b30f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:44:57 +0000

    duty produce mark name use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
