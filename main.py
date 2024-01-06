773b5cf0fed681208a00b1c10b9d87972901496d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:29:57 +0000

    job work soon unite word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_item(array, i):
-        return array[i]
+def reverse_list(lst):
+        return lst[::-1]
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
