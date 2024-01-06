efec7ce942d4ba536440df8949747655de65471f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:17:49 +0000

    plain telephone possibly aware shelter

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
