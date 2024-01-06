3f83313421bb83a88c1d4e7354d527aaf7f66668
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:14:59 +0000

    gas conclusion drive back movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
