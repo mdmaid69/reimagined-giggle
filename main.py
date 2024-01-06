c3cdda196af2343edb21b8389a14c3c10ffba9e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:36:32 +0000

    lie declaration instrument thumb yesterday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+print("Hello, world!")
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
