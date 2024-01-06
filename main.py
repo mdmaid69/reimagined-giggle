ac8ba2f73d6c6efbe46de21c222abdb23beb7961
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:21:00 +0000

    tough time available will situation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
