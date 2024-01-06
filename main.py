9e95a55ae001fd270fc1ecc368a611788f2a62a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:53:22 +0000

    block upstairs after must butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bool(array):
+        return bool(array)
