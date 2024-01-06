648ce505b71bcb1f10adcb76336a189081177e04
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:42:51 +0000

    ask before grade clean pile

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
