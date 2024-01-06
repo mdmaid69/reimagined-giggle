f039fc0f0de9b05b1baf18444a0f5e46fd1f68f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:17:27 +0000

    contrast wash leave 179bite balance

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_repr(array):
+        return repr(array)
