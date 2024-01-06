bd0f0c1384cbcc414bc744222624cad5718b2235
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:04:40 +0000

    question bit unpleasant value sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
