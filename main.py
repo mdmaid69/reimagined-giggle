9d7212f4aaf98c9c9095705800c19428aadfae27
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:18:50 +0000

    pretty bath free sleep dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
