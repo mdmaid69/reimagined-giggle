88c0e2991b7b616f14acfb044beed1ed09096faa
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:07:12 +0000

    limit represent extraordinary success blind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
