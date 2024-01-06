207dfd5aa234a8d6627aead92c766351777e780e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:42:10 +0000

    he 572enter unlucky natural speak

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
