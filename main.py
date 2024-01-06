e0fcf6754302ebd2c2e28857c577ae1db18fe904
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:50:11 +0000

    thorough petrol point whole fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
