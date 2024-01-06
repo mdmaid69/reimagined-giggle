aa4319860341fd9e4389a4259ffce3b0d05dabbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:47:43 +0000

    turn shop callback command smooth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(a, b):
+        return a + b
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
