20518f837d9c26fa0cd61aec01b1d4aa2112fa9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:09:51 +0000

    fine of set runinto priority

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def get_array_typecode(array):
+        return array.typecode
