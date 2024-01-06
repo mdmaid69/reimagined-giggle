825b8ef339c2b85d8643b572210259d15b8821e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:28:51 +0000

    manage carry explain business eager

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
