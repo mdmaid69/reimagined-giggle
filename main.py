60b5924d622ea8335bb1b0e5b2248e88ca6a5ca3
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:19:49 +0000

    depend run entry influence entrance

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
