3ddb1d2c75e8586f1e95899eed1594dd67af26c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:23 +0000

    feel value separate stare consist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+for i in range(10): print(i)
