adb62771c81ad73911824fdbad3f321ec83ce6a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:04:36 +0000

    team birth lie roll school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
 import math
 def calculate_permutations(n, k):
         return math.perm(n, k)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
