7657110c066af6364e8086a63e383cd7ab80104c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:21 +0000

    swallow hard permission touch design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
