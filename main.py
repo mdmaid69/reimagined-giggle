cc39c87f0baf7de8ddae1de6a861b9a9d8187176
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:18:41 +0000

    shop noise plenty attempt thirsty

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-for i in range(10): print(i)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_str(array):
+        return str(array)
