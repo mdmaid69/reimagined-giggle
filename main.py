5924861a88bd5ae3c6c34eef34ea724d2c276d9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:32:16 +0000

    anywhere yourself birthday harm drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
