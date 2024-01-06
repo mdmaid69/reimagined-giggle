53e5ffecb3f0c92d7d6c32ac78aaa51403d0bb0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:51 +0000

    breakinto measure schedule grade seriously

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
