d2f9a70760496880726e83b3ec0a89f4eff37328
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:05:24 +0000

    chat pretend claim arrange estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
