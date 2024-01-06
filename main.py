5d3c6454dec0c337c384df51fcbe8fd31a5c811e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:03:05 +0000

    measure snow blame reduction Ee

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
 def calculate_perimeter_triangle(a, b, c):
         return a + b + c
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
