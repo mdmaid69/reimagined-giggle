d64fce154d4465cf6af581490240bfaf188e1a65
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:17:02 +0000

    unlikely spend turnover important defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
