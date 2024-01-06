de66252d69cb5291d1c9f0d9fccb2563f05997d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:05:41 +0000

    rarely communication surprised already wake

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
