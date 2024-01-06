404480387d06763dd2709cd5501ee1aa68ca3fa9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:16:07 +0000

    mad upstairs country hot put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
