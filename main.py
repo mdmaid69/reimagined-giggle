508e04b15b50879e35475a9ef4dc55edf78df5fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:51:37 +0000

    high mess pause mysterious chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def reverse_array(array):
-        array.reverse()
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
