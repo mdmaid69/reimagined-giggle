a42aae077cfbf2257484de81241c3fa316d95749
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:16:15 +0000

    may about transparent action transparent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
