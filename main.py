cb2ba56e58912ff2077bcf6a27bb41eccdc4cfff
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:47:16 +0000

    note runover thorough generous because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
