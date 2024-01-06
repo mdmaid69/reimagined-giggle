72917611c4b7658cde470fb2b3884bc77f4db964
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:10:07 +0000

    real sink funny pass immediately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
