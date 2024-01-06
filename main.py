1c7b67dcf3da0458b6fd60bf4c47634a3e41edec
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:42:31 +0000

    guide card base comfortable passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
