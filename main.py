5216811cb371055968ea9dc25e1c1e56650eeb91
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:27:11 +0000

    poor rent realize sharp cook

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
