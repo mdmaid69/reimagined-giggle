76c1701535f21f9672097a8d962fe3f78f87e2c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:43:00 +0000

    abroad roll many success continue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_str(array):
+        return str(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
