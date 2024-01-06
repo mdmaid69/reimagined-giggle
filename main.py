bbe4e09394f8d76dc228f3ce4ff28849ec6a7d7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:05:21 +0000

    century switch light copy depend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+print(sum(range(10)))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
