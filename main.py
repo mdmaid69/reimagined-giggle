daa3ee227d9499091ff93f2965ce2a1426122a46
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:49:03 +0000

    history match talkoutof Ww sure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
