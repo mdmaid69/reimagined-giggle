e11c92cb977de2b98030cc6e3224f72cbf050823
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:36:07 +0000

    shrink file slightly weigh keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
