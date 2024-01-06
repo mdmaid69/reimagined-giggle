215169dbc338a8d536dbf30c8ed6dd142ff81277
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:14:01 +0000

    swallow machine ours so persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
