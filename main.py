0d45050d25556bc218d2dca9aaf6707253083794
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:28:00 +0000

    environment have Ii general lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
