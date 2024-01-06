fcfa2cb7fafb04cf7d1553361e6d775375c9e6b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:37:38 +0000

    such legal act book large

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
