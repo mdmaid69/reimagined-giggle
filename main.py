a2d880aa7f24be0e3b4d3e67d417960b1d89d6a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:48:22 +0000

    some pretend bored dig bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_length(array):
-        return len(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_floor(x):
+        return math.floor(x)
