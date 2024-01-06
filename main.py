90245a810837de7fc69615c07680221708cbf56a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:00:31 +0000

    sweet fat advise mine little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
