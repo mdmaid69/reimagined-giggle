b0848bd152956feb8bed6caca489e7df16ea37ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:01:52 +0000

    base treat order instead anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def convert_to_octal(n):
-        return oct(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
