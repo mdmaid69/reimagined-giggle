10a3c909ba9f5f20906e77205baacfdbe93fffe2
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:05 +0000

    week image lookafter broad compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
