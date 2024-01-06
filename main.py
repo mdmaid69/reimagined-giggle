6f1b1b4e8f2267811cfcf8a9190c2091c121eaed
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:15:23 +0000

    science convince favourite block today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
