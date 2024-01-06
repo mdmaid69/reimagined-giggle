04855b58913b333ef7ac4ffad31c0e93ebf4b093
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:23:09 +0000

    can wash suggestion 568energy price

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
