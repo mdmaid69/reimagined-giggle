ff0508c18076fb810a23d0f4718a597e0129e0f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:35:05 +0000

    care return safety container copy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(5):
-        print(i)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
