9ec8e65ebc7d163680ca74df270b355388c18ae2
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:33:35 +0000

    happen dance arm region wrap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
