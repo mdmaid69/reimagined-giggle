a7f176fc69686bd806efa6ac4303d53225ec2df6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:26:03 +0000

    previous description relation nice however

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import datetime
+print(datetime.datetime.now())
