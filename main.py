8dd6bfda69cb29263b2058e85986e7f726dfbe39
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:42:08 +0000

    discussion say that marry unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
