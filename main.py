6b91676c65198c01e91b75114cdb5fd22e23fb21
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:45:27 +0000

    treatment bang unless wish generous

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
