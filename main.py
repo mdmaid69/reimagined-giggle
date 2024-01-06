88f50283181c6b1b37675e2ad87d0619e2ef7766
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:44:58 +0000

    scrap attitude respect bound Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
