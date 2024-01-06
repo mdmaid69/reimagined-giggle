696a5fa0aa59e2c3a9ff9589233cfcc451add7a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:25:04 +0000

    hate previous bang impossible thumb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_stack():
-        return collections.deque()
+import os
+print(os.getcwd())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
