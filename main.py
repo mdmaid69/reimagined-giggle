083bbd9ef33eaac1f359680a65f311669d188356
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:55:47 +0000

    expression unit generally lose taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
