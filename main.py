a542e04d44cbab7e53f0433ed4adfb0187e35193
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:53:23 +0000

    lead fix just city ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
 import sys
-def print_python_version():
-        return sys.version
+print(sys.version)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
