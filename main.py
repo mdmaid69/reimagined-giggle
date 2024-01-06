aabcab56b79d8cabf728175d004fbd59e735ce90
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:04:09 +0000

    see produce her centre mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        return sys.version
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
