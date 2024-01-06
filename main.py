53c1fe73f554a3c378d4285f75d834a1f2fe4bec
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:38 +0000

    stand principle dark direct everybody

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def convert_to_hex(n):
-        return hex(n)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
