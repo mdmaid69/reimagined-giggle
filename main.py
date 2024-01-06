12ff85c7e6880e189da449219d6e001159e87121
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:09:28 +0000

    sale maximum see home brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
