561f0cb009f3ab955ff0213d61752052862cc45a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:13 +0000

    obtain tired disappear nowhere perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
