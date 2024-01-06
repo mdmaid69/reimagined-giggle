72f8ef866dd537e607ce57383dbcee7a7bed21c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:52:38 +0000

    minimum eye passenger from Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
