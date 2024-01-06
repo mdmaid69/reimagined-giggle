62b04dbb8d2883efcfed495ac95fe1eab58a3e78
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:21:18 +0000

    case spin ours discover tradition

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def is_odd(n):
+        return n % 2 != 0
