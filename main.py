336180574742bb0bbb7f25f7fad8f7536adadabc
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:42:58 +0000

    foreign absolutely bus string behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
