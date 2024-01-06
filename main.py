b7e1b25b038ebf31224c7fd24279058ef2195050
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:54:08 +0000

    stone Zz nice birth callat

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_str(array):
+        return str(array)
