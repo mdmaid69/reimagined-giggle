699703f8740c0c4e4fa64d0175fd76ae424ba477
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:59:29 +0000

    but lock worth judge disapprove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
