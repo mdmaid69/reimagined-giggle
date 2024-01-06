5cc59220d175b6c698888e5e1ad9465590840a09
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:44 +0000

    scheme cook proper natural lower

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
