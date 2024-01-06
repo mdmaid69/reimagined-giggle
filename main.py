acd29c523ce14e9febc228c308a306a482314786
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:46:02 +0000

    lost respect present interrupt look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
