e39196970d08e1ea581677fd1ea810932b313c11
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:29:40 +0000

    foot damage relative appearance improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
