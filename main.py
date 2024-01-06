acaa45502fa72eb7cf76ee668d537ee3411b219e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:30:53 +0000

    makeinto rich pinch scare environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
