9e2c6944c73d90acbcc78b90982d395f5bd5eecd
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:57 +0000

    blow sting begin crash undo

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
