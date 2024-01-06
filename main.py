72e1b6e9c5ae1b86b5e37ab159fcb35adf53d47c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:21:14 +0000

    wait deaf comfort consist apparently

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
