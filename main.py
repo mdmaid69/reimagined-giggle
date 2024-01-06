5c2d3e79d815af80f0d9df47a5ea53d38e57141d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:36:40 +0000

    smell tap hers threaten heart

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(a, b):
-        return a + b
 import sys
-def exit_program():
-        sys.exit()
+def add_to_python_path(path):
+        sys.path.append(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
