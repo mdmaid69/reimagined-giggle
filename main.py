3e877921b319ce9c6665c47485db3060a754883c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:48 +0000

    breakup even expression transport relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
