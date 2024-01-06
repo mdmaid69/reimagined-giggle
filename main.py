2ad13d4f713197ee3dd9ded716d22b375550db38
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:13:10 +0000

    quantity object inquire spill smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
