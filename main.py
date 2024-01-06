cf06d498edcd4455949f7361a373c759c697c434
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:16:51 +0000

    everywhere rub journey row indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
