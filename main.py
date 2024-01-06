7cf546fe6964af7467d3e129c4238dd446e2721d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:24:49 +0000

    about key steep sensitive succeed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
