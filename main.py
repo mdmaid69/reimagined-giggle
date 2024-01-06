0f90ac52b79b5c96fc20985f1cf1fedc012259b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:35:44 +0000

    raise remark blame entrance makeup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def sort_list(lst):
+        return sorted(lst)
+def calculate_area_rectangle(l, w):
+        return l * w
