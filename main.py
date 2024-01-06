85d8d79aa9138211327a7cf7a4741e30f9c8f676
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:18:56 +0000

    dare community fit about alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
