0699a9b3c82c433c6f516f0ef86ef1bfe930ed1a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:11:03 +0000

    guess shelter fight flow culture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_power(work, time):
+        return work / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
