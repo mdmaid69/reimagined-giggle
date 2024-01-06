8fb610398e6ca72d359e151607212841bc66d22d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:14:48 +0000

    bunch soak almost report anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
