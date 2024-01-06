d73a98cbab8b81c50b4b74c6a2ff419fdc1e9334
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:59:08 +0000

    when mainly thumb tense rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
