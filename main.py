ea76f1e690f2a0814ee7dd80b2907c1ffdacf26d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:54:51 +0000

    behind strong person treatment consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
