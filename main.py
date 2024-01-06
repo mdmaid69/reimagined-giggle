a38b28e66d334b50926e6c3b996360b5bbc222de
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:51:16 +0000

    least anxious match sure heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
