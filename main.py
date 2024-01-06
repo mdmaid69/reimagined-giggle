f199201622858f7f6273816c51a0fce832faf530
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:17:34 +0000

    file speech back takeout company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
