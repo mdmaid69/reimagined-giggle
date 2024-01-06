0de81a991c70dfd978e0b77d3e33ba331a78be47
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:16:35 +0000

    bunch bear warning lot arrive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
