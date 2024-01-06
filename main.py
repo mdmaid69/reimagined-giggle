8008a02123b89a70424b1b61726a43b45e6ae5e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:52:55 +0000

    wherever meal full recover damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
