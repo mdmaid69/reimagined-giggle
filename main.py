1eec34b5a3b6a5484b30244c721c1d134c19dd28
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:55:52 +0000

    lead back comefrom blame sentence

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
