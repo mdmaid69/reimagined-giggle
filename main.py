f3edc74a6ea32164382994534fc48b0ff0e0ab49
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:02:19 +0000

    couple immediate sick Oo comedown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
