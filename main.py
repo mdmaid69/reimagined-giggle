0d17e9a678c7de4a9f0f0434b3e520cf660a597c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:40 +0000

    after male mass sting range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
