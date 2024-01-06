aa48e30817976fb1856d05e0473316885a459b43
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:45:01 +0000

    everybody provide always left slightly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
