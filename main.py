b1a4e9304bb73e4b44d0119b1a7af40ba96cf224
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:04:50 +0000

    turnaway strong keepup interesting centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
