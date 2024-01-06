5f7604c38b8f00122453a40e0190e64e6bd3dbf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:17:11 +0000

    but thanks live in share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
