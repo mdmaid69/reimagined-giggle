a19e481371f10c79df01eceb1994e6b0093a2d66
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:54:55 +0000

    well that mark even warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
