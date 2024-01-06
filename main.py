4965332d5e4ad33487fcbdde9fb0f52cd31b6f06
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:48:36 +0000

    night careful holdon sort sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_typecode(array):
+        return array.typecode
