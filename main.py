a2598f934274adc55936672c40e0613ee88c5b16
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:58:29 +0000

    back section float rarely Mm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import random
+print(random.randint(0, 100))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
