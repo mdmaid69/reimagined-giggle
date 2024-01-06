f67a2f4dff93f168e6beaf45ba5ee0a77e7c1973
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:37:00 +0000

    judge theirs cook your show

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
