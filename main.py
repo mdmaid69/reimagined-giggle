cc67c8a3c3b9eda87ccc798c2faf4bf7ff4b4a4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:17 +0000

    over suddenly coffee wire pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
