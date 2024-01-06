da21587b587bdcbce82156f6529087374f60b716
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:52:09 +0000

    consist rush finish excuse border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
