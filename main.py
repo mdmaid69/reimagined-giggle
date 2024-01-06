d6ff120f8865a5fc6c2fabfd4c4521ede5f38c63
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:54:12 +0000

    pour discovery know runout loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
