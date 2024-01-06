6086f4ff9436e31d627285304dcc4b9e708ca260
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:59:38 +0000

    total wake several sure follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_power(work, time):
+        return work / time
