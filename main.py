5f02c3c2a288e162909118d8968c8f1b1d6f7768
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:53:06 +0000

    importance control your turnaway control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
