24d16850ca6256cbaafbae45182105e35cd3c051
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:48:32 +0000

    attractive best lead blow deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
