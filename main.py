cc198b70cdf9a373eca9ce741b803ede325b3b2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:04:37 +0000

    brush avoid band address patient

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import tensorflow as tf
-print(tf.__version__)
+print("Hello, world!")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
