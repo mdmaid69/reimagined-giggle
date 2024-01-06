35a5010c8e290596504374c6506e8ba87833ecd4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:52:51 +0000

    bright brown thumb turn crash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
