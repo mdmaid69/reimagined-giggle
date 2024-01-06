149501859f11727451550f88fc4a84cf9c297d99
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:59:51 +0000

    uncertain tall pleased anything suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
