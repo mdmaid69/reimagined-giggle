7943a893145f1a3274c621b2ead1aa72d7f955ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:40:14 +0000

    protest dig circle many bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
