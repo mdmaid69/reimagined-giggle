def29bb79054958aa08dad8f88e5c72ea4f149fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:00 +0000

    please onto last cause bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
