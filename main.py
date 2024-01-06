942351d1224d10dcc55c0456f0831db5bb2b7bf5
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:11:29 +0000

    ring anywhere else callup must

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
