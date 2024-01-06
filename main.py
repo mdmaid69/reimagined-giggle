a8d4f0db6225afc4dfb41b45562c2382a596c18c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:20:50 +0000

    rate bear content hospital pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import time
-def get_current_time():
-        return time.ctime()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
