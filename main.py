4641d439ae7a7e225dccdd14bb1ed499c3acfce5
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:00:11 +0000

    towards season miss better sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
