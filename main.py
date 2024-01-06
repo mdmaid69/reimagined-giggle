4179b4096236800ec9dbb42fc5f6eb2288410e1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:49 +0000

    estimate freedom hurry push damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import platform
+def get_os_info():
+        return platform.uname()
