7796f47af21cea920d50d5c836f99137dc709d08
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:05:13 +0000

    myself typical whichever quiet occur

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_unique_words(sentence):
+        return set(sentence.split())
