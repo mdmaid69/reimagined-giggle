5ee4bcdf4ee1221c6ebd8727ce673c1b1993a7dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:04 +0000

    yours guess near rob drawing

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+print(sum(range(10)))
