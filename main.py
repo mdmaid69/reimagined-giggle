af5a61770d010e99c02608216fe7f292aaef7983
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:13:50 +0000

    blame speech behaviour uponon play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
