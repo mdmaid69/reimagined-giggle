45c7d2dc94998da33d047d715b48a8e2e8ef4722
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:22:10 +0000

    large wave nose top glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
