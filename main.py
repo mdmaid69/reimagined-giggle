4db0ff3a36ef39adbca7db07188386ad1d8fd8c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:54 +0000

    suffer suddenly Ll fail collect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
