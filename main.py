41d14f71e3c3e545cefbe3aca9e3dcd08f0f9e20
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:56:33 +0000

    lend achieve means nice aim

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import json
+def convert_to_json(data):
+        return json.dumps(data)
