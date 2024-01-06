460701dc229fa392db1c55bfefd4f42a4c85afab
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:45 +0000

    save level past bye belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
