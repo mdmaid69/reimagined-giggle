8922167f13c0fae2db9ed53b4b0a906081f64bd6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:05:11 +0000

    excuse punishment importance report load

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
