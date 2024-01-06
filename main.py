a4bac0d5ab4becf81a8105491cfcf655728ea448
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:28:44 +0000

    film interfere deliberate about whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
