d38689b4073fa8772130612983100db0b3db8ccb
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:10:00 +0000

    operate start opposite avoid available

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+for i in range(10): print(i)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
