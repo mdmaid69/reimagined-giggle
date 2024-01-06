538543d785064f6809a8df0a59a1dce300a228ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:58 +0000

    we neither professional even fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
