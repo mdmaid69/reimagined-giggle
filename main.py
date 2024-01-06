401263293764e60878e34154d7ae3202457c1488
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:39:08 +0000

    tear keep usually runinto rubbish

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_density(mass, volume):
+        return mass / volume
