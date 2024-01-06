94fcc128daebb3518092538f327b3b1333a5f6f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:59:48 +0000

    else clear close Ee shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
