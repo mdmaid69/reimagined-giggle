7a93883d60f41cb84b3aea5e6b89c08190e9a26b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:28:21 +0000

    combine favourite machine concentration expect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
