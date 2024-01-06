f17c684206f01e0051e16be1828a5ca8cbe8b843
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:23 +0000

    develop majority contents floor design

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_typecode(array):
+        return array.typecode
