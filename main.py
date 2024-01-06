1324958c1653407564bc64a308c78a0067b2c68e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:09 +0000

    sale mean best seat tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
