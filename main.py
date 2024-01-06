5196f6cf19a71e098f27f5e8f99b9ac7ccadbf8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:17:31 +0000

    cheap tense brain hold post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
