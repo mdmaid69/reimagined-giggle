cd8339b674e3f524c86866f717d513b0fa8dee92
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:06:32 +0000

    instead small sound will wipe

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
