e847182ab2df41eb5f38bdf14feda09467dc1187
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:40:16 +0000

    consider emphasize young steam boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
