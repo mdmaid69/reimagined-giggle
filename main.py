93e81fb3ba3aab8c9dd222cce7a42153950124f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:49 +0000

    stiff city transparent wide understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def divide_numbers(x, y):
-        return x / y
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
