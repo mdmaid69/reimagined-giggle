ff0def7386efbc04dc6d895a18be5f945df51f9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:26:02 +0000

    mine used food solve win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
