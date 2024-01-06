48c56fbf8697545b5f23af4c20f8f43c0b318185
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:16:54 +0000

    unite brave characteristic book chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
