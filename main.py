cca9bc30df73afbb331d4439078910c225c8020f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:32:08 +0000

    save spend cross Mm relax

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import time
+def get_time_since_epoch():
+        return time.time()
