8ef94662bb1bb5c526c44a6752946a3a081b6c01
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:03:27 +0000

    standard little many public shame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
