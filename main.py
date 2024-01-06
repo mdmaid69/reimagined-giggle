24a987b1fe6110c3a4ae86dacaebe4c556c73e4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:31:58 +0000

    frequent fall worth empty reason

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
