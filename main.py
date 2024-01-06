0fad02de95deaae89d6d26fb66e14af9ef76024b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:58:32 +0000

    finally bite belong change stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def convert_to_hex(n):
+        return hex(n)
