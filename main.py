bbf32982f34b9e025a2d60284d7b6a7cddae6fe8
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:15:53 +0000

    century swallow plenty criticism keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
