58599ffb25c5fea481a3471c4df89991f878c712
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:20:25 +0000

    smile callat guilty nearly difference

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
