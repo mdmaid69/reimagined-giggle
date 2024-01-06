debb5463cf8f04270f745877452639481e4efdd7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:40:12 +0000

    drive part reply surprise connection

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-from collections import Counter
-print(Counter("hello world"))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
