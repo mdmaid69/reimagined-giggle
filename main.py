96831c18a1b2655cdf89005c827854338b37dcfc
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:20:40 +0000

    share familiar ahead care fork

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
