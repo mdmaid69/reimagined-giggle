9772975270c88a85f5e1a2670a496659e618fb0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:36:31 +0000

    win there plenty from steer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
