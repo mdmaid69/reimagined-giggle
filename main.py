d7ffdbbfd21497350d74297eed341a0dfd494736
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:11:03 +0000

    bridge recovery sharp meet downstairs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-print(sum(range(10)))
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
