3536e764ffa5eaa3a7d42e176f22a4cfb49ea521
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:51:35 +0000

    sweet history reduction steady callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
