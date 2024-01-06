cd2ce201992f51ad102aed147b718988b7550cc0
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:15:03 +0000

    bored any trap trade offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
