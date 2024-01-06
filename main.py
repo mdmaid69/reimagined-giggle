f2f2bd8ffe185ad511c9ddd9c060a69c283a553e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:34:34 +0000

    near prove body explain share

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
