1d50bc821c6f49052d1de7f69391e45b6df3cc46
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:59:08 +0000

    tie bite cry Mm start

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_density(mass, volume):
-        return mass / volume
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
