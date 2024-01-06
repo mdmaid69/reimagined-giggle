a5d6fa58574d5404678a79e032aad398e4f79789
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:02:22 +0000

    only respect chin earth supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
