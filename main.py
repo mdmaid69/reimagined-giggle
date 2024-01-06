b981cb3de07ebe68b173b09cd4c55773a49a33f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:49:11 +0000

    spare Cc week steam stop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
