70c64e11e36a97d1462c782c665b10bf7bc2e412
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:58:37 +0000

    if injury skin trouble advise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
