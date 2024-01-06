c38de90d7b848cc8bb5ff4286359efc578c43941
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:46:56 +0000

    wealth organization sight identify tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
