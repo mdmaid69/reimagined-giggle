0aa462bc97b740a0a91150356925b19601cf39a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:30:49 +0000

    less divide fail makeinto race

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def convert_to_hex(n):
+        return hex(n)
+def greet(name):
+        print(f"Hello, {name}!")
