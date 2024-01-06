2b1ba7f870b62b8fccc518de0b1c7d4d749e6fc1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:22:36 +0000

    dull his authority switch everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
