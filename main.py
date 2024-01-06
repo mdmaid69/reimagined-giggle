2bcd094b1a165a9661a02b527459fcb209199eaa
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:39:28 +0000

    promise admit left union switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
