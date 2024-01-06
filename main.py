1665a8517d52cc76aee06371c8617a2597cbdc5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:37:37 +0000

    brain use skill sting yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def square_number(x):
-        return x**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
