69bd9eb55c5f3654c81e61125a6dfc621eeef3c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:55:57 +0000

    quickly hour care advanced container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
