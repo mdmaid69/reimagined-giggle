44fd16c032b0ae64f14223ff5acc58ba3bb964e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:28:12 +0000

    female mention 567enemy breakup event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
