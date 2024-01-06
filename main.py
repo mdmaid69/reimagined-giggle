4d85cfc6f8b550ca71d1ea1d54f603b0bf68c2f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:55:41 +0000

    advice range innocent distant distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
