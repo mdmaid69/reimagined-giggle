ca3f1dbe1605a61cc98a01b4db9b73954a74ad64
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:29 +0000

    unkind narrow total section bad

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def split_path(path):
+        return os.path.split(path)
+import numpy as np
+print(np.array([1, 2, 3]))
