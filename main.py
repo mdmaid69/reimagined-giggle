38490618079bb078d53625b931954af5fcdcd385
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:42:47 +0000

    process voice count takeout quarter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
