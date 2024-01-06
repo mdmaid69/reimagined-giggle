dae7ff0d318970816b7947ce70a326a7daeb1457
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:49:35 +0000

    journey string shock without increase

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
