f90587348cdd62d83ce61d9ee15eb9a3db9b1ba8
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:32:30 +0000

    later morning catch explain onto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
