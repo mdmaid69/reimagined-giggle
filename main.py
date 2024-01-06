3fed376e08a8f44c977b52fffae13e7495371ba1
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:38:06 +0000

    stay scale Hh recently bang

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
