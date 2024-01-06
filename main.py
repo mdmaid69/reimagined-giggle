164e6f8c86439e05556b69c1ace882a06ab0e47c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:05:58 +0000

    foot front earn justice ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
