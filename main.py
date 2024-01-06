ba51a3fd75073daa8ed4c82fdb7aa94bd9684230
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:57:59 +0000

    early ring any tie capital

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
