d530021544b0b22e62c4aa9b0c08ea46b951ae67
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:29:24 +0000

    preparation attract carry hit trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
