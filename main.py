87852e6ff00000ee11463e7309fa3539260e16a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:55:34 +0000

    approval early used remain get

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
