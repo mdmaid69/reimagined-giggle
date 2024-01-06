2fcb31734971744faebf05cd0fded6df62988400
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:07:54 +0000

    fail goout tall instrument comment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
