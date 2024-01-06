5451c7a64ff4561b4b97c2b2fa7aba2e42dfd277
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:35 +0000

    range section display Ff birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
