2cf6a45a06b19d1ae1b09587670a658fc471f552
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:02:21 +0000

    face concentrate upwards chance claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
