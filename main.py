cf3a7d52f5eb1f2fc65b70806aae9e6f91478c1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:33 +0000

    temperature mistake yet capital makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
 def get_array_as_format(array, format_spec):
         return format(array, format_spec)
-def calculate_area(radius):
-        return 3.14 * radius * radius
