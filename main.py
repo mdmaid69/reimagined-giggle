9ed580185736a04231a1b03c258ef3569c22dea1
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:52:43 +0000

    slightly fortunately instrument unkind house

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
