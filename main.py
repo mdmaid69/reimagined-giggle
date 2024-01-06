3aa7491f19dacf84db4b62a42677ee4dc6180058
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:03:32 +0000

    patience even strike problem treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
