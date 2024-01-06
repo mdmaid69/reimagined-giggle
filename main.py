d6a5b2edcceff8323414e17bc7d83cd2224cfbad
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:11:09 +0000

    measure communication now could example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
