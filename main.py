e17d8be7ca22dedb43a35dc47c53fd17e4f20de9
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:21:06 +0000

    improvement getoff label nearly exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
