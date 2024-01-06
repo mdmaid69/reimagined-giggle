c91a9240857cc4cd1cde85485402b46b682dd019
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:21:16 +0000

    money hate Vv university minority

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import sys
+def exit_program():
+        sys.exit()
