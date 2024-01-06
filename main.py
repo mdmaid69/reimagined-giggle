772eeb10e06e73659042c8c63bf066a36702cc45
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:40:58 +0000

    emotion bunch goon call score

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
