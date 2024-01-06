48a9e87287f91e7e0070b8fc58847b0683414aaf
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:21:18 +0000

    contact frighten have keen lesson

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
