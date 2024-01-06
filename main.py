f642986dadaacab61e3d7a8251884d8dcdc5e448
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:17:21 +0000

    every wipe rely approve majority

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def find_min(numbers):
+        return min(numbers)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
