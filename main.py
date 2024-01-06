e7bac2b9af8febee0f5379462c400b1e889c92ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:23:06 +0000

    attention job throughout answer matter

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def is_odd(n):
+        return n % 2 != 0
