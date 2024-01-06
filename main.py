acaac99ab878c27192583c05e77b48989552befb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:10:09 +0000

    advanced forever blame live now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
