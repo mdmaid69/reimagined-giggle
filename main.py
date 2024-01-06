76808c0fbdf3052ba36fb5acfb12a09d48cae999
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:33:18 +0000

    cheat collect spring key sky

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-print("Hello, world!")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
