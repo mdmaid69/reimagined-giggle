43206a9c72dc56cb12b61238eab1708edef8755d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:06:28 +0000

    recover drive forgive unable frequent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
