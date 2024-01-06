5d997183027e54e585f1a86c2aca0de86752bf55
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:01:19 +0000

    keepout public worse describe add

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def sort_numbers(numbers):
+        return sorted(numbers)
