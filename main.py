dc807d6f625863c5d533a2cfcf56131e31c20b1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:49 +0000

    pollution separate separate stare support

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
