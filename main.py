18a4024aafceabb15f0e1414678e365bccabe51b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:25:13 +0000

    blind luck sound drop consider

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
