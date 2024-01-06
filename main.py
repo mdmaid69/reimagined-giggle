c8290be3d8219d2d69882e62d8b845fa30560238
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:01:48 +0000

    whenever top pattern somewhere female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_combinations(n, k):
+        return math.comb(n, k)
