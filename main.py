d197517b4d4d84e4c39c4edbf2ae11b2f06c69dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:49:10 +0000

    trick nor arrangement thing standard

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def find_min(numbers):
-        return min(numbers)
+def calculate_speed(distance, time):
+        return distance / time
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
