69607989b3764ac5ea44b840c6689acb7b99ddae
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:47:02 +0000

    fierce pride speak quarter kind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
