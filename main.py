114381c2a93571ec6ea3229a90518f1c9d0001f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:31:33 +0000

    callback someone but together opinion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
