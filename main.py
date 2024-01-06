862013b2950106aadce5b918cd3ce49eb11935d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:14:29 +0000

    sing 567enemy climb reasonable goback(to)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
