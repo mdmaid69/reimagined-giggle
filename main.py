675e1979951abb869ee3a84ae9e51139bd6116fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:06:40 +0000

    tone inquire getaway rent getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_str(array):
-        return str(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
