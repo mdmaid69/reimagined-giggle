a99510d84adf04b5f5f713472ac02b53f4a2168d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:44:13 +0000

    turnoff dream brother shut control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
