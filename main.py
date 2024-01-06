1a3a9e73afba3c630250f51c1eb0b8efa6244bdc
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:34:05 +0000

    violent concerned pleasure later excuse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def cube_number(x):
-        return x**3
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def divide_numbers(x, y):
+        return x / y
