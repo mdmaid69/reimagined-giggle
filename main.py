317f1b2de7d2938c69705fcdedfddf6d3cf7f212
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:16:03 +0000

    busy good before maintain lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
