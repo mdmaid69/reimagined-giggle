c1a5c361540bf4725b75bc88eaaf698a7cb739ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:22:53 +0000

    hold shallow otherwise fairly mood

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-for i in range(10): print(i)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
