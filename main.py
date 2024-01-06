66def1b809a2be186ad269d0bbc45df2c997dcd7
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:42:49 +0000

    equal passenger gift rarely Vv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
