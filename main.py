5bda6ef5200b086db7f60882205f567b5ffb2a47
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:26:14 +0000

    free run attention minute miss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
