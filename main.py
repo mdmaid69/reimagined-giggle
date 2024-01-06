ef0bb56a2867ee52fe9658f2eccc3cb85d44f534
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:15:42 +0000

    spray silent say lookaround nothing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import sys
+  def get_python_version():
+        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
