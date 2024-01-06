b5cbd5b66854cac7b0db74daa9a979bbfd8fbdf8
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:15:37 +0000

    climb goout soak time grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
