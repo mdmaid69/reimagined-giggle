48c069df7fd1121508352310602334cd11065347
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:39:32 +0000

    pretty pause pull pause thinkover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
