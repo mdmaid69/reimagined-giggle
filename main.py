b4af7be480dc1e63aeac9baa8dfa28cb047811c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:40:00 +0000

    run event comeoff drop event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
