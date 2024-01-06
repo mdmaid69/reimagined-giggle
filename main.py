894a3f0a6543e03d2edca56cc9e0c91ebb0c2b4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:23:30 +0000

    well soft century knock rain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
