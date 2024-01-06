95f70f4c19d2666a2760a675aa2b879b1cc66658
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:02:08 +0000

    bringup start better file read

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
