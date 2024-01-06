803a069015429914b947bc2e82eb63ff5976ae04
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:16:36 +0000

    hold circle old weigh skirt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
