4ad28190e74ce80420a801f5c3c60ed633248768
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:28:37 +0000

    film shock weigh wrap address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+import sys
+print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
