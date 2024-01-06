96f32f69668429c4b89a0fec760a61ea694b725f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:33:23 +0000

    pass classroom mistake open last

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_float(array):
+        return float(array[0])
