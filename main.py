2fb106f5bd454454a6a745fd35821971e18e3a1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:59:26 +0000

    description entry comeover picture 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
