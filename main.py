a608433d89ffcd72a6d642006cf8447e9a886968
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:53:09 +0000

    count public 565end record learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_max(numbers):
+        return max(numbers)
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_current_datetime():
+        return datetime.datetime.now()
