296e2d704c55e2145e7384edb27324860f2a1125
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:34:20 +0000

    mention valuable pressure basically scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
