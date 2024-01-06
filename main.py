1d4b8c981066b4d40f2533df53b735746886c500
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:39:05 +0000

    balance nothing correct appear advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
