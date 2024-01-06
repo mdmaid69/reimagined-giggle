befedfe149aec0dc5a41ee6415dca071232e223c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:40:18 +0000

    group risk equal trouble another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
