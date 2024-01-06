d09b7160c771ebf3804595a268eafa316e0a0b54
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:37:23 +0000

    face hard calmly rain benefit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
