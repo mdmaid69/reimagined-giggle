37cc4677636bb038d1e272aa853342196fe62a01
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:01:35 +0000

    note run measure underneath bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def is_odd(n):
+        return n % 2 != 0
