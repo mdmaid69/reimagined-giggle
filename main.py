92eca6795de5d563cc52a006d1bc3a1500d66d74
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:37:13 +0000

    cheek autumn me whoever sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
