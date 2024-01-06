f528d6a5bc278b69dd70cdef7d4306de2182dbeb
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:16 +0000

    shout satisfactory time imagination Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
