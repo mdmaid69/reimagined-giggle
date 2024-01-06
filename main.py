3a1d1b252a429c30ac6f762a4c97185d554840ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:42:01 +0000

    breakin right lift people everyone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
