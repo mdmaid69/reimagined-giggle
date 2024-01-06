2be8771b39ffbe0af87021cc95fff72b8f2ab774
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:43:07 +0000

    belong later they very whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
