c1bf0eee33a99bd1b1991373749d940dec6aeab4
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:23:38 +0000

    good beside Ll worth together

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
