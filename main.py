e8658b656a9584a1baa62adca99d5fffb4cc499b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:50:05 +0000

    team middle nurse by load

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
