21488db3d9ddb5b1ccdf3695ee4701faf2a275a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:27:21 +0000

    handle opportunity problem report lesson

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Characters:", len(text))
+print("Hello, world!")
+import array
+def get_array_length(array):
+        return len(array)
