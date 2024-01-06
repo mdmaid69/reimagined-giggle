7100f45806fae5494de1f1839505c2a5ab38db35
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:35:04 +0000

    unfortunate much yours accommodation mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
