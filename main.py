5d98060f1aed853261aaff8dd89f67de11a09e17
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:45:09 +0000

    however dry continue available familiar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
