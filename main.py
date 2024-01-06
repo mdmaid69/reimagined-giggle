56c872e795bd48b882e1e85032fcbd6617339c3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:35:07 +0000

    far such off force bunch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
