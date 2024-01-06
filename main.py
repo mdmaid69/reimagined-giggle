8be5e8dbd36cadbfaaccef78621ee755a99fb2a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:55:15 +0000

    wherever live disapprove accident agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
