8f87df8c95b7b9a05974cd01f1118fbd498c6641
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:05:30 +0000

    power union declare unkind already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
