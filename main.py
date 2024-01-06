c915f81f7326be88117983e356ac9a3516e07084
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:55:59 +0000

    design cap continue tape embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
