efcca9bf91c95a9763933d7757db248f31680dd1
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:32:06 +0000

    tongue uponon grade store play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def get_array_as_list(array):
+        return list(array)
