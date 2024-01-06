b6205b7326c19049582fd12872a8f3c138ea7214
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:25 +0000

    takeback rely car sticky trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
