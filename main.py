35f8b0d4f7e485895adaf93419ed69f4ee31e8db
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:50:15 +0000

    fast building minor look priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def find_max(numbers):
-        return max(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
