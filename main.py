7fd1028f0ad4e3cc84173fd4d96cb75f586c2787
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:42:22 +0000

    later worst rid exercise cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
