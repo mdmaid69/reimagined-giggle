67d7a00930547acc19b7c6f67f27d79f0ad61446
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:33:56 +0000

    relax number pay computer fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
