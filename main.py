7ec94c8466cc753cf1e3d9a0e4c31ca2887a356f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:29:00 +0000

    probably beside another waste take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import datetime
+def get_today_date():
+        return datetime.date.today()
