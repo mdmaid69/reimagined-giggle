3763eb397893ad5cb5de04872ba17d15a149ac9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:10:05 +0000

    safety march branch cross reduce

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import os
+def remove_directory(path):
+        os.rmdir(path)
