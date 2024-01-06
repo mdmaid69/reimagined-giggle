a682f9d6001f0d6b576e940716033dfd757b244f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:31:03 +0000

    burn past calloff thirsty zero

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
