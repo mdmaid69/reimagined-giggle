38d165620642bea2b092a9f6ebee27f8d76e8f4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:58:35 +0000

    schedule about stiff grand unit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
