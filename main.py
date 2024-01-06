58a4d9628562df32f6900de9af4febec165f5f9e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:36:50 +0000

    bunch there comeoff but word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def is_even(n):
+        return n % 2 == 0
