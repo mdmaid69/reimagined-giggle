d0a41770fd1ad38ef890276acf0c7ef46bd6f3a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:13:55 +0000

    outside able excuse wing buy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_length(array):
-        return len(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
