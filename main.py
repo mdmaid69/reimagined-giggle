6eca4db0fdeabe7157ad2a6659408a958477cbc4
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:08:28 +0000

    nature material supply cross yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
