fea4aa9e06232ffec37efd7369f6dddabf37c605
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:38:28 +0000

    slice wake police wherever upwards

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-print(sys.version)
-import time
-print(time.time())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
