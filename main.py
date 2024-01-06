a791374e8ce027f2974b4760d6b42b9da7e604f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:28:09 +0000

    patient strong few hour intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_current_time():
-        return time.time()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
