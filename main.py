f41191a0f7b88c3ac0323ddfd2acf700de4cf8fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:46:24 +0000

    change today 566end mistake operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
