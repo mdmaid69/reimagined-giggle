06b3b0aec006753a2243953efd3793b9d5678577
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:04:14 +0000

    event wild I worry centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
