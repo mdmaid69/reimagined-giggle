a90420d4c82ab47e706e36e6364f87c540063415
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:45 +0000

    true open wash female service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
