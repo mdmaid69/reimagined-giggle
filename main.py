b89212b4d8c61b0cdc3bb6d08b6a29f8d500545e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:48:56 +0000

    complain least grateful runinto care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import time
+print(time.time())
