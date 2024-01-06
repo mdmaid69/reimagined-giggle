e0a9983179e0df1bdaa48b320fe0e20eca7e6fc7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:40 +0000

    runover as comeover inside telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
