3ff4d0bdd527e01be211ab2e47b7c0567457b77b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:16 +0000

    then message lower field make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_area_rectangle(l, w):
+        return l * w
