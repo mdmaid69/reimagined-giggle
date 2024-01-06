d4d1a2c48cf1a5fd7f0e2a168068664477e7e437
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:08:15 +0000

    plate children rare someone people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
