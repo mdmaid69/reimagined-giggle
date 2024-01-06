e243f4d6c62ab69da6b9b9e8bb637f43e2403c1a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:51:53 +0000

    open late form union mistake

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
