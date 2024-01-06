fc2ed2d626d16283b8f91931dbd644f064b3248d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:35:42 +0000

    please write mend lend group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def check_if_array_contains_item(array, item):
+        return item in array
