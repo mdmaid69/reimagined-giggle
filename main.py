88d9e3cb2f27ea6258446ac6322b1fb30d819819
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:18:39 +0000

    refuse Uu round read cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
