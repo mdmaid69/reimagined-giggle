c3725ea50255617125b9a6e97effff9720df973a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:11:15 +0000

    somewhere vote small transparent later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_area_rectangle(l, w):
+        return l * w
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
