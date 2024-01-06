afa5518a7c127b8bc6efe894211b8e74a7278d2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:39 +0000

    discover glad present soft but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def reverse_array(array):
+        array.reverse()
