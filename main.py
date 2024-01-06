16158bc2e2950b30020766ae6e9fb2afd03966c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:44:39 +0000

    attract phone bringback clothes regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import time
+def get_current_time():
+        return time.ctime()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
