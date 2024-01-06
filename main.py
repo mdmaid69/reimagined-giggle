023d76218f43d35b62c4a74ce935799880575bec
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:27:39 +0000

    intelligent art fight deserve keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
