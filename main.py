5c97366cda89955f8da42c1823ad8d1a3b440f1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:23:05 +0000

    oil trip join boat upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def greet(name):
+        print(f"Hello, {name}!")
