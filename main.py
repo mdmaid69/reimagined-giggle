129fe1b00c4bf9aa6ddb9179738df3e129b8700e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:02:46 +0000

    circumstance they some team minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_working_directory():
+        return os.getcwd()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
