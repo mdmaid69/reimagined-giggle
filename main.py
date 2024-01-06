59fad3bd1e0488db1ba8ea91b44cc81f697c6f07
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:00:08 +0000

    stick clear loss announce Kk

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
