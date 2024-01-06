86d4a09fa5615fc89bced05955598d5879dd85dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:02:27 +0000

    operate please shelter hide gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
