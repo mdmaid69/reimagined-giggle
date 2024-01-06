1635f88cb4921ec096e93ddb1ad56fc55a0a09ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:27:51 +0000

    sky confused sweet among release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(a, b):
-        return a + b
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
