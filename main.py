5a1e3283f0e2dd3b1d978a83df42605ff62a48ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:02:28 +0000

    promise listen snow loan again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
