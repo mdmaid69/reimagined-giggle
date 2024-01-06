4832b7be52e86f1ecc0ab2e03ab31dc7e0733cab
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:46:00 +0000

    often regular stranger wet trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
