63a2eed3e624c98c5d428fc52efdebe22672d9e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:26:54 +0000

    share rubbish direct at movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
