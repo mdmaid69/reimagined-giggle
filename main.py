7f7e4dffd01bb3c8b4c3c87866a046dbff82368a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:33:37 +0000

    majority stage pool close art

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
