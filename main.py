d2272f40b9fc656120c6d101de03dd9d9fd87b72
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:56:13 +0000

    cough somehow grand problem produce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
