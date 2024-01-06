11ed6713d4e5b634db60273911147bf890774e72
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:56:26 +0000

    translate Bb hard turnoff live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_speed(distance, time):
+        return distance / time
 import collections
 def create_user_list():
         return collections.UserList()
