9a15247926645be6b2b5fee95ff76f5a28cae7cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:28:28 +0000

    Cc expensive accident wire fall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
