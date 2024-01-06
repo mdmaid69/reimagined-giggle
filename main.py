e4634454abaf59d825fde9fade7062eb99676efc
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:27:13 +0000

    tour near set draw keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_error_function(x):
+        return math.erf(x)
