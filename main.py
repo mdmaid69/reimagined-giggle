1a7bd680d3711181bcd4d9e8b89d37b6b853a17e
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:54:23 +0000

    cost possibility must gap they

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def count_elements(lst):
+        return len(lst)
