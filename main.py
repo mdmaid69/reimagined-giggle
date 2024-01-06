bfb11c7a73fd378c608319343a64eb2a997574c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:35:02 +0000

    pronunciation cover phone constantly risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
