fad1fbd87854a41e322a2e0011936ffc9a0c943c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:38:32 +0000

    downstairs point hard contact honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_acceleration(speed, time):
+        return speed / time
