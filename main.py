e5d076eb5f1f15e753165c802c2b50b38e996abc
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:07:10 +0000

    rest uncomfortable birthday fasten cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
