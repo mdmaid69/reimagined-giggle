d4942bf8651b3a08e4a0ea082b330b10574de57d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:10:57 +0000

    stretch expensive market child pretend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
