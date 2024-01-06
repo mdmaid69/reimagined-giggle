a34e29f3fcd9968e1e071688be5c9f322a93b875
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:48:21 +0000

    perform start tie receive flat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
