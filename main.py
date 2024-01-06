fc17f3e4289905be1feaedd75a07884cc72a785b
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:48:07 +0000

    noisy cheap few somewhere autumn

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
