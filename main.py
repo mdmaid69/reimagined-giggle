31c102b43c808985d850d67592462cf24a4511ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:16:08 +0000

    brother owe flow plan race

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
