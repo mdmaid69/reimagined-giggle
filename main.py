0a524963ef3ce586a13f45b4813db1240ea6b50e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:39:43 +0000

    skill wait anyone basically Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
