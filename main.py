c9b48e2055e5ccbda51afa389acc593bcd342ec3
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:02:37 +0000

    night hole smell error border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
