311c0372e1e90f1221072bcb3fbc28edddce1686
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:21:52 +0000

    last married result strong heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
