8021385916ef55d8538eb54b505ed0a8fadcf034
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:30:18 +0000

    squeeze back offer create call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def square_number(x):
+        return x**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
