6f1ab5d0f78ea034ba9763c049fef3b765932876
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:34:35 +0000

    fence fortunately week chin colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
