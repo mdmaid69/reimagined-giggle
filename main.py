d6e5f566c34b06f0c548e27d5e9e2665be060810
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:33:09 +0000

    opinion even sensitive stick love

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+text = "Hello, world!"
+print("Characters:", len(text))
