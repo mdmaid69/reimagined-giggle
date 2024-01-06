e4e72f31087669936a7f6ca22711da073c633760
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:04:21 +0000

    juice keepaway blind learn compare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
