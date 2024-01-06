7883d7bab1f0eab5ca479bd42d958454910252af
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:07:49 +0000

    fact approval must back expensive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import time
+def get_time_since_epoch():
+        return time.time()
