4c35503d5e67baf723f6bd039cfb2ed1cd935fd3
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:18:21 +0000

    with point exactly sticky calmly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+print(datetime.datetime.now())
