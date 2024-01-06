b7d97af7bed17b67354656c12ed2901ac173789c
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:42:14 +0000

    minimum bath instrument gather free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
