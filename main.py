f8a8391550457902d12e42bdf3b8c9363efec964
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:32:00 +0000

    how soft estimate formal sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def divide_numbers(x, y):
-        return x / y
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bool(array):
+        return bool(array)
