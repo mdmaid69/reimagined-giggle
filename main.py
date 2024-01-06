bb250f6255a2ac164aa5e66974c45871c3f9bb7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:07:33 +0000

    any model for blame show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def count_words(sentence):
+        return len(sentence.split())
+x = 10
+y = 20
+print("Sum:", x + y)
