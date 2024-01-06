ee4745fc4899ebbc6e47dfaee35edb72b89eb0a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:54:17 +0000

    beside quick squash replace meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.time()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
