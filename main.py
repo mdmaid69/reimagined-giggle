69d3a3edfeed6ef18462b7390baf24aaaf39fdc0
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:37:30 +0000

    dare private surprised love wall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
