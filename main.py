6df393809ea1bb3bcf45af1335b6215b676b7381
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:27:34 +0000

    slow discussion turnaway throw turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
