61d63b53f6552537ce4cc59cdf0138fed0b04250
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:38 +0000

    straight instead fast maximum give

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-print(os.getcwd())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
