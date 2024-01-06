9843e1a9401c9c0780cf6c38efb03eaef3d639b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:21:03 +0000

    contents within doctor agree succeed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_pressure(force, area):
+        return force / area
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
