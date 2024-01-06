95c5e069d8ccabb09252de43c45e6844b04b3c12
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:51:23 +0000

    build how minority our rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
