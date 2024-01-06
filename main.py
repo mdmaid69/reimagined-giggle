e0ec42602c86fac2be2bd1d41cab4e3ef06d3eca
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:04:21 +0000

    risk closed cool only turnaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import time
-print(time.time())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
