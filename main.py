917d8b9183cf933198141a42196de72667214f57
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:03:26 +0000

    hand near lower plant shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
