d26ddf9afaad526711b5f295c41ade6ef94c332c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:07:32 +0000

    argument guess night vote just

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
