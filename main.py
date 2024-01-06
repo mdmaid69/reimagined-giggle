a13fba8f75220163ee413df4120703365f150429
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:47:25 +0000

    sorry project favour know sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
