4122c095b19522315fb28c47bad254d4e7d4c936
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:37:35 +0000

    wrong imagination knowledge gather height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
