b4b8b1b8f0bc1699ef555d32f6b4adde238cf7d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:06:44 +0000

    sore doubt secret thank them

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import math
-print(math.pi)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
