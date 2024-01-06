447f29c8637de4dbda0bee3ed754be0f88e4eea3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:47:25 +0000

    degree colour Dd voice plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
