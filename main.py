22d559e730a7faf04bfb35826f784986da2e6701
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:15:11 +0000

    suck tap niggaacademy chase quantity

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
