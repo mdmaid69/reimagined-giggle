48c5f9f451ba64e36df4a0660b495fa7607b6f57
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:26:25 +0000

    giveback regularly attempt limit do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
