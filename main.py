24a7d577625de1866d4bd92d76a79bf8d0cb0724
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:53:38 +0000

    mix process size familiar usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
