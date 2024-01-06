608359a8643416faac724174b260df50e0f7e912
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:15:46 +0000

    circle demand head accidentally climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def sort_list(lst):
+        return sorted(lst)
