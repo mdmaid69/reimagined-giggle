df7d99d86b4d9545176881ce8f535a61d4307569
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:15:13 +0000

    ocean cheat tradition immediately better

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_list_from_array(array):
+        return array.tolist()
