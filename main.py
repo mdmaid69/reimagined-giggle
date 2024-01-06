64ad6725c5f52ad665c30f2b895726b906c482dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:44:31 +0000

    record cross direction beneath seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
