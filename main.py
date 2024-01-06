fd3787655a77c4d41779d648664f6d6c24378bd5
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:54:24 +0000

    public port comfort uponon expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
