158008d5d587dcf8bea28e3ba4dac736b44cccb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:09:51 +0000

    begin sort present knowledge form

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
