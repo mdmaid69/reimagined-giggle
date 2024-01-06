ac57ffdb2af53e400f3ea138eacc372b995d2b81
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:49:25 +0000

    convenient final watch standard leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
