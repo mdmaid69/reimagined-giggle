7432cd6d5fe8193e5b22a451c37d0ae286ce3448
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:55:21 +0000

    ourselves bad crazy information convinced

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
