81305f219ac0d78a821dbd86cf88ea62d6df9eae
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:50:03 +0000

    then pay fall branch danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
