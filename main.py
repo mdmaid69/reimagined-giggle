589d1c0231721ee4a04781f78446f3b4a10766c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:30:59 +0000

    disturb really walk measure left

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-text = "Hello, world!"
-print("Characters:", len(text))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
