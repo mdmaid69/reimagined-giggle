dac8683d64eb33256830751f7f3291ba04ad691b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:13:39 +0000

    slight around single introduction hour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
