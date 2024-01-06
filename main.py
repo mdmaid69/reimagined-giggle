93730ea68add293b901199898391a1d0e3a3ea4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:16:22 +0000

    train trouble nurse fond concern

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
