f09a6b3dce9ee79555546fe52ca5c0f760198e57
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:58:10 +0000

    tip murder cut about company

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def add_numbers(x, y):
+        return x + y
