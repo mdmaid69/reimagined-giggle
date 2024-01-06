522333a31549c9ca6ed0e86160942e6ee2b48613
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:49:37 +0000

    bottom opportunity opponent delicate theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
