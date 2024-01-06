1baa060b42a72cd4fb4000d294c69c54f9a5800d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:46:34 +0000

    tongue ready condition nearly trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
