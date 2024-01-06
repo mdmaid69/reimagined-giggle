6bbd55b48f6d0cdd0e1dbc9cf02d6c2b21a2d70f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:32:01 +0000

    clearly report catch comeround experiment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import time
-print(time.time())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
