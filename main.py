846bc56be4d9040dec25f7d2a042636f02391b8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:00:40 +0000

    brush trick damage question bringup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
