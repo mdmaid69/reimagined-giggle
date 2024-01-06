3a08ae0325bcfcae2bd1e88e084184c8e32f7c96
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:31:08 +0000

    achievement suggestion regular mess agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
