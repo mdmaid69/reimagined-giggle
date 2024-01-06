33aec4216da20d13f82d23875130599055dfe37b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:20:46 +0000

    amount stand somehow dull pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
