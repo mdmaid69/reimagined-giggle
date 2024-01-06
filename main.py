4a851b54ebccbfdb187bf170bbe7a7c6cb007e35
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:23:07 +0000

    admire powerful everything evening shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
