15e1362e1763f0cf8ccedd6bcc4cc7ac3d4711e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:47:37 +0000

    cut 570enjoyable hospital all rain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
