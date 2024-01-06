e5ce6c4e02669fbd18dc9be05c02542525469200
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:06 +0000

    advice my university suggestion produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
