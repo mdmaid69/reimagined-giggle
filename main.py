fc911bd7521f1ff57d3b517cc9315bda7fc2063c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:38:32 +0000

    create weekend guess judge eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
