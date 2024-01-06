19e66e784721b8a4862b70a77693bb0f4418826d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:22:02 +0000

    safe towards rain keepfrom mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def count_elements(lst):
+        return len(lst)
