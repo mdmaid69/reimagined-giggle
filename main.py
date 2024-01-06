c1c2ad612e42ca97ef18305809defe9b7e2e1cc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:34:24 +0000

    grateful perhaps tip introduction window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
