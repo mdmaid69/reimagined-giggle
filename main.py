f5a632e87942dd559b573d95e90d0833805b810d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:36:34 +0000

    acceptable late right marry earn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
