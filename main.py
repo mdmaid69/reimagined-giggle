cbc73c1843be84838f048cf689d29cc7493c6bbf
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:54:18 +0000

    watch board ocean area believe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
