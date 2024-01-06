8310ca7b86e7cc6f7b62bf43bf92b1d131fb02b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:40:01 +0000

    stretch pump nor lonely factory

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import random
+print(random.randint(0, 100))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
