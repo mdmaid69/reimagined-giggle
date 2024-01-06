1da15a6a437625704970e794001993dfe14298e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:35:38 +0000

    workout hang cruelty record be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def cube_number(x):
-        return x**3
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
