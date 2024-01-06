cc68404301f19e0acee4c2ec6a3d38d19548ef82
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:21:52 +0000

    border like unable skin behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
