4f2a995448e6bd1e3cb113f44daec6408f67b64f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:23:07 +0000

    over prevent although repair used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
