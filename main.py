26ea660f6950c5393a4b4de8c38a67369cd02201
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:16:20 +0000

    criticism of rise legal man

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-print(sum(range(10)))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
