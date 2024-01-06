930573f4776a318c34061ba7733722a9931ad5ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:42:56 +0000

    noisy Nn runinto run thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
