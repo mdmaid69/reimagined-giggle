78604343e28e23bec2e9e364d958ca8380d3cf8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:29:41 +0000

    talk keepoff ignore completely community

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
