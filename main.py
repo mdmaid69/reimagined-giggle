ddca7bcebfeb16041271aeed6bd64baee2d1e5be
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:57:37 +0000

    nice road cheap backwards blue

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
