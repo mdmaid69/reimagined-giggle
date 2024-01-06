157dcab9ac6f16e9c081f2fefe80ad502f9e8449
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:36:05 +0000

    crash other sorry narrow wonder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_pressure(force, area):
+        return force / area
+  def reverse_list(lst):
+        return lst[::-1]
