d73e86af1dbd3f4caff4f8ca07b3bcf2e18a55c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:56 +0000

    absolutely cope convince treatment push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
