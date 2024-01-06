9284fe41b21212a734df4f42036e9d615f0abf76
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:31:55 +0000

    connect minority breakfast it stare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
