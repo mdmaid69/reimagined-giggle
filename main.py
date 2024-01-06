56baf15af18d58a907283f5d4a36cf8bc3cfe6ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:58:39 +0000

    mine mainly class shall beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
