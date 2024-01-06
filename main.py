a827245c68421eea7f05973173b4f9d4ebb8e284
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:33:21 +0000

    needle Ss come aware claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
