02b5360074382e267086e2a4fb38c9b95220b9f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:04:51 +0000

    refuse blame soon far further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
