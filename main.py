55a81e405de292123469d24edbbefeaf5355e9b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:01:48 +0000

    lock available judge neat natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
