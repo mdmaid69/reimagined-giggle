f05d672f7e029b0c3752318bd19288910200c4ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:54:04 +0000

    collect sure care 569enjoy low

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_even(n):
-        return n % 2 == 0
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
