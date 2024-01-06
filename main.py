6d389946a999c6f129cc898dbc96b5b31ff2b337
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:21:15 +0000

    warm band speed crash Mm

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+name = "Python"
+print("Hello,", name)
