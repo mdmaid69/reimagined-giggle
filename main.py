a7e64dcf9acb895949e76a1298ffa7402818c202
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:00:42 +0000

    flat grateful dick plan snow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
