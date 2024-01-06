14548f72a858b3b27ecab859bf6623a82108e11e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:33:36 +0000

    claim recognize cheat Cc sell

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
