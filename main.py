578c7c58f10d4a275da539d4322979046aa26d9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:08:01 +0000

    complicated kick over model remain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_acceleration(speed, time):
+        return speed / time
