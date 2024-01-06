24881c50b9959a07f5c728b342916a4e386defe9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:10:43 +0000

    hour announce round crash holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
