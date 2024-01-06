23e0f9589bd95d7acef7f16becd4d986b0fcda75
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:26:37 +0000

    contain risk branch freeze use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
