8700827bead4d2ce20a177ccb71ea436ea2dfef0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:00 +0000

    seriously allow wrap care keepoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
