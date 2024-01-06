df68c173c745404100ff3610407ca7adc8551e4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:09:37 +0000

    escape unkind spring Gg performance

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
