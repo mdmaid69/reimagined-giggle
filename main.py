e801d19ce0edf5c0b7a85945b154c497ac0147c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:27:50 +0000

    Ff with drive mention mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+print(time.time())
