547b2d26fa2e0abce782e6eb1ab101ee3a94c6e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:43:22 +0000

    long take convince get please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
