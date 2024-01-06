ec2cf3b8853d282e9fdafbbf9934bb55f997a7b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:07:35 +0000

    hard sale giveback either schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import time
+def get_time_since_epoch():
+        return time.time()
