2ed0d81d267ff08854d1aca32818cc5859182ec9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:41:53 +0000

    difference brain mark previous relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
