ca483be0f4599763618c6928d399386d4249bfe1
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:55:06 +0000

    fresh foreign possibility lucky welcome

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
