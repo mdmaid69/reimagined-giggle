ff5f3c38747451b642e7623d8dc1c354d22ea352
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:19:48 +0000

    gather understand guess before long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
