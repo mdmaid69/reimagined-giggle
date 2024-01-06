4fa0a1586c369453437c93a518e7efb26869407d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:25 +0000

    except pretty doubt dare watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
