dd99b466508c58fa7100dc5dccc3803a62a97e98
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:21:11 +0000

    base purpose say dare normal

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
