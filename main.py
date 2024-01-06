e729997e89392cf93feeaac8f4c83334d163072b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:47:27 +0000

    where mainly uncertain fun sell

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def subtract_numbers(x, y):
-        return x - y
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
