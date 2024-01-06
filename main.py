e8aaac7dfa18cf114eaed71d1f8a6663f342e923
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:32:35 +0000

    climb appeal rub about wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+print(os.getcwd())
+import array
+def clear_array(array):
+        array *= 0
