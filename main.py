eaa6022f182a06439f12453625b91becfda99465
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:01:53 +0000

    prefer club valuable pretend whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
