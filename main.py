d4a9be9953e9c3d5665640c536de9f6a50aef7a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:38 +0000

    hot keepaway television exactly squeeze

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
