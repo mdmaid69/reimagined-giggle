a1c98c0f9901ef146b63410ae710dcf2f2fba06b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:45:57 +0000

    interest tidy say city lost

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
