bd25ffcd56af280c01413ebffaf549bee06ebb87
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:26:22 +0000

    he willing anyone necessary new

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
