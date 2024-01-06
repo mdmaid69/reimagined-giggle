e2887b9eefaedf2429f7c3c505b60ae15fb65bab
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:09:48 +0000

    invitation yourself exchange blue pile

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
