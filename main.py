7e21730ea0e70fcf77a85a5092018e8e120dc0e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:22:52 +0000

    operate guilty means one runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
