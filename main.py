94afe5398e467f7c56cde195b032cd9f04171bc8
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:00:20 +0000

    ask carry together chase snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
