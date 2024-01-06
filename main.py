b4572651377977d0da353220980fc8a6b374d2c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:58:27 +0000

    acceptable far ready rid do

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def greet(name):
+        print(f"Hello, {name}!")
