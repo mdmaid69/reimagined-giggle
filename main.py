524a14e997c8dadd233392b5361d503c2e71359e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:26:56 +0000

    born miss lower lay male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
