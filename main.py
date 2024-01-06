ec6a6a80c3624a5a8d4e9b86d2bfaebf4b0ea6a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:26:14 +0000

    sorry impressive upwards remain immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
