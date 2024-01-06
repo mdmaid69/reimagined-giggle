44c64d3327c63b3974372977aa8db0d0f0d3162c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:14:25 +0000

    usual consist wish slowly give

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
