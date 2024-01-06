f02ae21b93f3d3d50af27b2c5f6eb09541fd7f27
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:14:47 +0000

    minimum her guide straight close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
