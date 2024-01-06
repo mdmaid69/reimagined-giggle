4dd04d1e27ebb150fb14d10a5098accb79c28852
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:58 +0000

    shopping rush stare juice previous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
