3a229bb2d836553d10203d76925943c9ccd284d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:41:34 +0000

    will keepfrom brown swimming slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
