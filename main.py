627591d424fa762661dd57f74cf2ffa457fe486c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:37 +0000

    so close tend split keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_acceleration(speed, time):
+        return speed / time
