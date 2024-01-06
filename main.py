c15b9f3aa2eb45df000e5f911210525dc1e8c730
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:43:16 +0000

    than similar lower temper morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
