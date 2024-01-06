be89c6c0c334a68695212737f37080fa6441a4d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:04:33 +0000

    mine date discover do pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
