0eb7705a3388173310fe0e603cd368ac7d222aac
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:31:46 +0000

    area hardly case odd frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_union(list1, list2):
+        return set(list1) | set(list2)
