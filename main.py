1618c90cce88120c3f40cddbeb81614e8bf42d97
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:33:27 +0000

    bill tread control great care

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
