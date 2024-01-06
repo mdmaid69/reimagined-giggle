95bd9a598fd499ec8b7ee221f76434b1d677b16f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:21:31 +0000

    hope owe sport performance issue

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def calculate_area_triangle(b, h):
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
         return 0.5 * b * h
