90675e82df4c8641d9b8d30090a78fff86c7b26a
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:22:35 +0000

    maximum flavour greeting too city

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_min(lst):
-        return min(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
