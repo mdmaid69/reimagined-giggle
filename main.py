08120e71078bc37e4fc5d08306f49d04fe89ff27
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:56:08 +0000

    set regard fail uponon remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
