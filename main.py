89379972ab6736407ef855c7cc622040c432dd39
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:51:47 +0000

    scheme Ww 179bite account whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
