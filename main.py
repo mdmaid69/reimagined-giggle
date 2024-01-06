efc0c7bfff1c2414cc11f6211a49600a711932a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:05:20 +0000

    succeed still never anyone rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+print("Hello, world!")
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
