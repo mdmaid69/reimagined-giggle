fc918ea8e65dabb5ac22c7b8986943b44c72fbc0
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:29:34 +0000

    letter sudden interesting communicate and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
