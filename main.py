1a69e92f7b15c5040a8f1e44c80cf3f185b3ec23
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:40:50 +0000

    impression record young phone breakinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
