c348dc7d9d91f8b9fe0cb0eb681a9757f72a9104
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:55:23 +0000

    figure use bad smile comedown

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_pressure(force, area):
+        return force / area
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
