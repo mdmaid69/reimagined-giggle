1f8276b4a4bff00a79f57a90b64d987dc0eecb02
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:19:36 +0000

    cry age have cheek experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
