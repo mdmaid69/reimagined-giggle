7946b5d579c875e41854e3f34c0f8bb87d9672df
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:18:33 +0000

    circumstance bound previous modern post

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+from collections import Counter
+print(Counter("hello world"))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
