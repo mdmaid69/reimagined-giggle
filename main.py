682adf4a2a59e582b2c542f582cf5503ecc8699f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:35:38 +0000

    carry far minute wheel explanation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
