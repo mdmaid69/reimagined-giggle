46dd9d0ec40c3bf05c7ac26e057998664795f733
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:24:47 +0000

    authority acceptable threaten welcome genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
