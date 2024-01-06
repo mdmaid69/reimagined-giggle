95cd0d9b8adc1cea6313c9bd643419fdefb3ea71
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:57:51 +0000

    intend fence charge welcome report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
