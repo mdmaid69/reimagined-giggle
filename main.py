a2a07966067063e031f5560761bdce1940d06c75
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:04:39 +0000

    begin sort blind leave dry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def reverse_list(lst):
+        return lst[::-1]
