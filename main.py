369a1d92d3117beab80ea480980a27efc4b39c7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:20:44 +0000

    bend probably break 573entertainment identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_stack():
-        return collections.deque()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
