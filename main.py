8478fa1de8ab8bfadb693c949d91158e3daff663
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:31:59 +0000

    this makeup height bar fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_typecode(array):
+        return array.typecode
