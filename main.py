62837c79908b21854d737b515f08d20a8a00d366
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:05:21 +0000

    separate little pretty fasten but

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
