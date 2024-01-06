0a8bc25309b180f070378e42ebfcc9a3b3e99373
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:05:16 +0000

    letter special protest perform stand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
