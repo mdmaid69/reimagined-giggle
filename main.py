621ad19dab2fab793723de51cb5adf1f0dc7f6f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:58:38 +0000

    benefit ought cruelty morning show

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_work(force, distance):
+        return force * distance
