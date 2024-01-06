3efce39dfd76fdef59f6ff3dff561ecc81fdee84
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:25:30 +0000

    lesson hour ball maybe along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def subtract_numbers(x, y):
-        return x - y
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
