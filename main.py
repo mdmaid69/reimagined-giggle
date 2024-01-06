a06b142bc3fbcf83ac7d86cad0d5b9aa390d6325
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:39:29 +0000

    model rob repeat part throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
