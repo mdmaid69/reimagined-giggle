27c1bf0a5229a52a4fd4d9581c20c7086e1ff6f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:40:48 +0000

    beyond exit imagination hold adjective

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
