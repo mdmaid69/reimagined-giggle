13d7b371f4441b7985c6c681000664f19354c67a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:52:49 +0000

    now lend contrast plan you

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
