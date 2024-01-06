7e4fe4c543884fda7a38092b786a7dab6aa85bda
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:17:57 +0000

    fairly squash cost bang among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
