6bb29a56fe10522a863bd64fa9b8ccdf50c627a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:16:59 +0000

    sting child meaning famous challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
