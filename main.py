7233e14e3bdf63e39639d103eca8ecbc3354fbda
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:35:01 +0000

    replace confused must some morning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
