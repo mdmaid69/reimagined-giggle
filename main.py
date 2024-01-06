64c79cfc21a6ff755c53779afd33f35751746164
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:36:42 +0000

    question guess onto until obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def get_string_from_array(array):
+        return array.tobytes()
