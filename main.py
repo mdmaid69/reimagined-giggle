fcf94edac67fd6c18a313f9329a47e6b9fb4fe5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:54:09 +0000

    Vv frame love nonsense he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
