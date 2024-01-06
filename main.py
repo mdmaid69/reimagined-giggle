18e3f38dd7ecce8232706562361875275fe874f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:59 +0000

    enthusiastic appear so send have

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
