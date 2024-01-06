117f6e6c1ad7ec8f54de551fe95d67458f277b2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:48:03 +0000

    trip spoil tax guess turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def convert_to_octal(n):
+        return oct(n)
