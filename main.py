bccddc5ff34386c47455788d08dbc21d436c4b61
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:35 +0000

    minority safety without interested spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
