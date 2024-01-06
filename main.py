099e69163a5cfb72b5539373ba621d681ca0a2a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:56:51 +0000

    circumstance sorry switch bag telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cosine(x):
+        return math.cos(x)
