5abbc72b42d1ecbeddf9ef6f24e266912bbe05c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:02:04 +0000

    offence upstairs thing yours mainly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
