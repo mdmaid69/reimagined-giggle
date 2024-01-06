0036697181254707ad8550f744ebb96654b21c92
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:34:49 +0000

    tight responsible traffic quantity Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
