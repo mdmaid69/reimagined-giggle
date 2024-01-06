647e3abf862db2fef94e68340e87918079e02ea7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:27:07 +0000

    much below shrink relation exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-name = "Python"
-print("Hello,", name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
