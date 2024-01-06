0301632c9d798e6286e6818f8dde3e9e21e4e49c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:00:32 +0000

    feel original roof skill border

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
