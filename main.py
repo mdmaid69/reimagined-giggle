1a2771b2d95e5541c5f7fbc0d03e436d3a0397d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:00:54 +0000

    contact by they bet switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
