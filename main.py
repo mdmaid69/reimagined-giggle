ab26db0f31df90888d43ef6ee6675efa5b2726a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:44:15 +0000

    in university plan 565end characteristic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
