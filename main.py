6f453260517663156f415416344ea3b839ad0e3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:58 +0000

    threaten attitude freeze regularly point

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
