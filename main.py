c712e6cdbf97256c745bdd69745a600bc42bd99b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:04 +0000

    boring by wealth in seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
