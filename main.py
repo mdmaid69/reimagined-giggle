6c9e16d3386b7b64fa0b7e5cdbdb2c42329f7b36
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:20:27 +0000

    justice complain study calmly wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
