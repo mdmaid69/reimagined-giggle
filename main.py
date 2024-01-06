4f38e1107cd98578bf74e025ef9fa8322ee0eadc
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:25:50 +0000

    prepare reliable flat uncomfortable argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
