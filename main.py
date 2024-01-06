693421cc199cdb55fb165720d0ef5409ff5cf9a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:52:00 +0000

    popular dry nerve another night

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
