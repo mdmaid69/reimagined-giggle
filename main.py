4757a4959392ebf21616e86736c6b585f1eae2dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:00 +0000

    number long shallow clearly effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
