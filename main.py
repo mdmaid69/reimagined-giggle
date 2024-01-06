94210ac1dd6943a9cfde5cd5b9cc6fa9ff471b7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:48:14 +0000

    welcome anything bother door tool

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def remove_directory(path):
-        os.rmdir(path)
+print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
