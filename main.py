5c61ab24c8aa7c12c2947c5044f1362461e27af3
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:04:54 +0000

    reserve scream arrive sticky apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def count_elements(lst):
+        return len(lst)
