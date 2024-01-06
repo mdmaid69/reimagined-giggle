0c1a9be700f85f85676a17d1a0540ef438cf6ac2
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:42:41 +0000

    winter forgive message since free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def count_elements(lst):
-        return len(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
