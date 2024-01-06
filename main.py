16e94c563071d69c53f57b389386628dc9edea31
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:57:17 +0000

    even project run miss throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
