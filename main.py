a10f12bc350a6213bb2e32726f1dc30c9bd0064f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:07:26 +0000

    morning fun already gentle second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
