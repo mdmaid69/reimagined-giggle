eab71bd83fff2fff5fbc641a8bee67e71f7ec444
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:45:13 +0000

    set contrast ahead rain frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
