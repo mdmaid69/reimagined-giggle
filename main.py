4d6c6b60794411b00176f1b1d548f07a79a43279
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:34:29 +0000

    another operation 179bite talkback giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
