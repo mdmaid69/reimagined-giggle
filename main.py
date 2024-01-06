96ca6e6777ea012d32a511eaf3ac2c1f69014e81
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:38:55 +0000

    lend disturbing turnup risk nearly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
