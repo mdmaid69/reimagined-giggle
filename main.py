1eeb5ab041c4246d1bc7f15761882d17bdba9123
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:17:53 +0000

    century always geton bank burn

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
