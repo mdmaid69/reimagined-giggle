159f6f4a1da7f929a0586bff41000de13fdbcb4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:07:13 +0000

    hot substance rarely success meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
