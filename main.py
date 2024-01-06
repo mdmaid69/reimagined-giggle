ccd7fe68649f0cbed72b09232faa673c2851f5c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:08:31 +0000

    organization hello strange plenty Itself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
