2fab01c5ecde80c885a8408e536adcce31420d9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:19:19 +0000

    busy model centre pause hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
