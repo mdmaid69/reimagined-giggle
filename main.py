5fdf004a77af20feb73171feaeb9032b8b4f061d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:59:22 +0000

    thin excuse future act pump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_length(array):
+        return len(array)
