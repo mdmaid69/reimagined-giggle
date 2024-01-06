555c68b9ba93f2bea9d5917ef09261e79fe613ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:17:35 +0000

    day either tooth mistake ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
