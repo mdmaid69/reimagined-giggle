80b59cfff886ab3cb9e696271f0bf9e773a5cdf1
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:30:56 +0000

    only bet getup sound preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
