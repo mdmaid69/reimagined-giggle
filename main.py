97b776b75f16f7606126d81677c39e2e3c8d485c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:31:53 +0000

    but authority story sting extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
