8f7bd9e965a1c5adb285d6dc42ffab9d27b9fcf7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:53:48 +0000

    disturbing practical further cover tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
 import array
 def get_array_as_str(array):
         return str(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
