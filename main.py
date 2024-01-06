1f37ae8d484893220ee06a4aa5a924eadc3c2b34
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:24 +0000

    570enjoyable reduce appeal form passenger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
-print(os.getcwd())
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
