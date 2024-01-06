db406c2f6b8589b9e1977e5a87f6db2b42f7d7f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:28:55 +0000

    niggaacademy visit murder breakthrough line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
