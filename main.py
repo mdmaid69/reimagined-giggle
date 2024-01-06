c87a6920281bf025d48165b322e141ce3963af6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:45:14 +0000

    split rich secret pain dead

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import array
-def clear_array(array):
-        array *= 0
+def get_array_item(array, i):
+        return array[i]
