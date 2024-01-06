85aa26fdfde6da3db21311eea45a2de23bd5a2c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:21 +0000

    switch fail insist capital impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import sys
+print(sys.version)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
