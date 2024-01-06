a89c170ee544ac6ed65dbebea66b6a0dc9b3d0d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:27:21 +0000

    mine pronounce month dangerous mood

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
