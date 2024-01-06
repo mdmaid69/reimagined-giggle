a54c1c4fd61e1e6dff575f1fa6a08ba61203f236
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:12:23 +0000

    shout thinkthrough calloff transport sort

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
