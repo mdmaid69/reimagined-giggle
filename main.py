cc15c65e9a036a4bd7a3d5198f9a3880d3e624f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:47:30 +0000

    wish then half original recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import datetime
+print(datetime.datetime.now())
