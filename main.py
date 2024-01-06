df102226f9e9d0bfa250ec562637f016a6060ae2
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:08:36 +0000

    cold steal Rr apply mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
