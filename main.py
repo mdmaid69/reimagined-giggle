87170407453b57cc31271020c54f046ff1d009e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:26:05 +0000

    bear hospital switch broad sport

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import datetime
-print(datetime.datetime.now())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
