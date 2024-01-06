ddddc3edb3a7d4528549e312a7b440aa5ce3cd6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:21:18 +0000

    talk needle calmly teach alone

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
