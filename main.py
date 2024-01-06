263f3a9045c215944f898006964a673485184fdd
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:35:15 +0000

    increase rob 570enjoyable complete black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def sort_list(lst):
+        return sorted(lst)
