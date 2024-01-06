ec00bdff6cb41907e31704af8997febd215e8600
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:00:08 +0000

    closed plenty of book pump

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import sys
+def exit_program():
+        sys.exit()
+def calculate_acceleration(speed, time):
+        return speed / time
