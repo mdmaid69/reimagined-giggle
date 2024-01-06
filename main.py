d2103ecee973190e0197f0c1fad1e929571e4f8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:57:07 +0000

    illness dinner breakinto plastic tax

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
