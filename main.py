9fa2d8879fcedf1f3140ccdd0530f7198bb77bfc
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:04:55 +0000

    spray mistake hole obtain sentence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def append_to_array(array, item):
+        array.append(item)
