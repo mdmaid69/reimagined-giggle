2a6f5d851d7ffa1f1689bc79bcbbf469c1fbf76e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:18:20 +0000

    study further certainly official difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
