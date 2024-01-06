b43a6b67f486fe85349358f56467dac1fcc858b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:50:51 +0000

    speak rely turnout provide score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def square_number(x):
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def square_number(x):
         return x**2
