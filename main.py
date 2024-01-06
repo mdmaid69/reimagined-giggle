81a2213088a94c3a770f8c7098186603f56fa39e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:38:04 +0000

    simple fetch fasten abroad its

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
