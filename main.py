344419f0408f4d33afa188220e39cbfbc30a5881
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:00:46 +0000

    pattern minute tired hardly shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
