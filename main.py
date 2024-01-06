4fb84aa8b0e15ffe7e0e030ce25ace087abd2032
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:53:19 +0000

    hate nothing claim hot divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
