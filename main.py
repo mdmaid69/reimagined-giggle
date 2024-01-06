7c2157247087e74d2603d87708dcd898628c473d
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:05:52 +0000

    by half describe class dream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def convert_to_hex(n):
+        return hex(n)
+import array
+def clear_array(array):
+        array *= 0
