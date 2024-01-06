3053b8598d3612f2cf5c449b501df5aad509d32a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:56:25 +0000

    also interfere note band street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
