e9b56dbf2b01853b2b50ad7cd73be128cd0d0062
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:33 +0000

    post minimum less strike bread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
