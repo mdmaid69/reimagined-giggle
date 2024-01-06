0c62a8a7134b54fd2878470f93bb8e2454571283
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:42 +0000

    nothing rid means bunch seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def append_to_array(array, item):
-        array.append(item)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
