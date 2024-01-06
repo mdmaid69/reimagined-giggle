dcd7d0651e7ffea6353afe37b0202a7cbf489af3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:27:53 +0000

    admit combination goback(to) spray intelligent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_hex(n):
-        return hex(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
