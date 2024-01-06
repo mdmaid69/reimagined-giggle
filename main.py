d4cb973c479cdf814bcd3f4102691e4e93b2e140
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:48:23 +0000

    Cc maintain cut turninto protection

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
