4742ee20c8dfab1e4147a97534f2d7f4da161b77
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:38 +0000

    safe free replace care realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
