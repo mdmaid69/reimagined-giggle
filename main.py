1fbf79937662b530053e1a62382a8f16ec40f676
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:47:40 +0000

    spend underneath common slowly the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
