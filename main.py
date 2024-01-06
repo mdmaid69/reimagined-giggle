6c8f2f55a25db53737ba8db1789ed1c338951db9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:54:32 +0000

    mother rank destroy this yours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-for i in range(10): print(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
