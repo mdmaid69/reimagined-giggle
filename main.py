5ed917c73e308325ec6e9bfa561426ff6bb5b210
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:47:40 +0000

    shoulder bound practice worse doctor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
