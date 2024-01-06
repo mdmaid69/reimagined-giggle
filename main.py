30500704331ddf3bc9c3a5da07d73cf677821363
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:55:03 +0000

    container want uncertain storm actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
