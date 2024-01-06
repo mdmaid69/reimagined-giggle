2e5d30bb4b00ea8480bdc3d5878be7c1e9f37786
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:12:42 +0000

    doubt transport mine bad size

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
