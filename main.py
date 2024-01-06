179be0a6f9312f9f8a5bbd703498ec75060a06f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:36:20 +0000

    return business undo path responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
