e01174f508c255723b8d80c71687f945b7ce8639
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:08:16 +0000

    surface deliberately properly both scene

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
