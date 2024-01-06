e5748ce8d775c2256972689c725e237bb1b1054b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:35:54 +0000

    pinch fasten example shock along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import time
+print(time.time())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
