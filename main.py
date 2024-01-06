e012b887763293f73ac7197d511139dd2b0f7680
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:53:56 +0000

    heat hello hear tired miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def reverse_string(s):
+        return s[::-1]
