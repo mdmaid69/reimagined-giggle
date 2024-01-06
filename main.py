2f2861b15f1d63b33dd10d3fd2353acc13f64c77
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:20:26 +0000

    forgive tradition organize group runinto

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_acceleration(speed, time):
+        return speed / time
