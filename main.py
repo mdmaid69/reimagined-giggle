0c35c842c46b7e3b6f3b309dcd8ad283ff36beb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:26:13 +0000

    crazy everywhere over gate interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
