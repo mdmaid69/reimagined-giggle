e199ef11e9278a2ebd7913ef3473ca10031fb203
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:19:32 +0000

    underneath traffic near freeze after

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
