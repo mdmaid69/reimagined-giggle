03d5cb228c319aa9fb2b95223219f56f3cb16b1f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:51:24 +0000

    naturally beat breakin wise stir

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def find_max(numbers):
+        return max(numbers)
+from collections import Counter
+print(Counter("hello world"))
