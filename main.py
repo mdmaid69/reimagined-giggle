c09ea07d01d6371bf29dbd996041f444510cb731
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:00:19 +0000

    cool rid invitation problem ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
