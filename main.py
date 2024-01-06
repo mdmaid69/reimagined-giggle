373f1659eaccbef16ba7eef38974d17a85907ca1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:32:12 +0000

    custom something confidence offend cover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
