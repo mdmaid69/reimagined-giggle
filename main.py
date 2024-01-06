5521b4737192ee172dcdd433200912b6b9a61bb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:51:26 +0000

    wind excited workupto goon important

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
