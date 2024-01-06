80ec3faa2e3ae11a68627fa884787f8e25c38b8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:36 +0000

    market possibility talk trip mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
