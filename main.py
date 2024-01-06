fb71e87e028c0809c2e205794c9bb15348c90b56
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:14:08 +0000

    track small top speed limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def is_palindrome(s):
-        return s == s[::-1]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
