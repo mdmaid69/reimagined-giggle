4c2e330b963a7d2050fd8f82034d3ff85be0b9f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:43:10 +0000

    fear attractive cough priority party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
