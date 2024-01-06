f5bf288b95da6902787d08ce1f3c804280b920c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:07:18 +0000

    talk turnover experience march until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
