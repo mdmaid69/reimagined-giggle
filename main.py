ec259a49e15b98a2c8cee93d4ccb84bdfa7be084
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:20:55 +0000

    classroom example bringup rest gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
