57725ccd6c96520db905cf5adc38a9b2da3e6521
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:12:21 +0000

    welcome last language hospital pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import os
-print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
