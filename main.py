86450728c489e7b57c739076b56ee2c6758d0c65
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:05:08 +0000

    boil operation on best further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
