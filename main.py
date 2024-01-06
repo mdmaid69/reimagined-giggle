75912767ced483cf413a6c07701e206c033350e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:58:22 +0000

    water thumb now tax slice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
