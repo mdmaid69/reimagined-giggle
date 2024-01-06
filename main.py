e045bf768c706db9d48b4d2316726adcaa8eabe8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:44:28 +0000

    way club interview education through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
