a6e7562809b21077d05f12cd58a310563e9fa16e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:07:20 +0000

    congratulations reply confident handle grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
