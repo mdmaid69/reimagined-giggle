76e02b73c15fd8d2c14cce3951edd8290da449cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:05:47 +0000

    otherwise little calm direct example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
