23687b7c02716e5856b7f04e195c61b1bad6cca1
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:44:06 +0000

    mysterious famous mood commit scared

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_density(mass, volume):
+        return mass / volume
