569a9f84ab633e4fd87e3b26efd12b3caebd62ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:09:30 +0000

    female march combine wind now

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
