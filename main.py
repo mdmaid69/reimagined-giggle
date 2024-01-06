b8ea54e9cbf6a5c600c24de789bbd215f65fd833
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:09:44 +0000

    wide in strength choice worst

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_str(array):
+        return str(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
