36faf5373f5e4cc275cab767a0af414678e3c2e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:39:31 +0000

    dull introduction chat pleasant slip

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
