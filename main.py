2cc785ee5ae54bb7baadddfee0343cdb048f794e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:54:29 +0000

    straight duty top health once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
