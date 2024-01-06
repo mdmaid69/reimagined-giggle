ad8df68b6d2b943e4b0a04a4395a6e47b759ea60
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:10:51 +0000

    ilovedicks expensive criticize neither message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def reverse_list(lst):
+        return lst[::-1]
