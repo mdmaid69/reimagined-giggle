ab98c81853ee992d30732de19625302af8522163
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:10:58 +0000

    work today present prepare machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_factorial(n):
+        return math.factorial(n)
