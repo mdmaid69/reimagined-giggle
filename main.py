b41ea2ddf3f7dd75471be76cd97a1fe6be6c7327
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:35:04 +0000

    whole guard clean amount legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
