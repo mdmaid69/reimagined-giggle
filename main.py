d706fbc00a3f130697851eb6ee4f32c916c011e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:22:06 +0000

    that admire store trade thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
