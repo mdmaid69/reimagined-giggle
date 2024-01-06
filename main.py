5453a9e409d288a2dd9a52f79a55bc2fba425b83
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:34:51 +0000

    punishment natural secret fall distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
