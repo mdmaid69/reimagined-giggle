b742bc7f7cd09eaff17650fb164eb973672130ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:49 +0000

    shake but model strike ride

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  def sort_list(lst):
-        return sorted(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
