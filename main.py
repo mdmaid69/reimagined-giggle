12f4319159adc897e3224ba39b21c714b905f731
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:47:40 +0000

    all beyond union rid majority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_area_rectangle(l, w):
+        return l * w
