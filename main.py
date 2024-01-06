bf9f4b34b31e09ad3b16d9bd5ea71393f0ee35f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:33:30 +0000

    heavy abroad else class female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
