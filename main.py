a22bab082d4d551969a093ced381b496be00acf3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:24 +0000

    union sensible past expression section

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def convert_to_hex(n):
+        return hex(n)
