a88d0da37d48dbdfbf8cf94d3e6b969c73b769a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:41:33 +0000

    object terrible trap break visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
