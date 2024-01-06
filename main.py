baa4a9df7f1609143d41e4964decc9c2205833d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:13:06 +0000

    repeat worth rank rub action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
