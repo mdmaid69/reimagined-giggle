a28a626f3ac37348383f8bf41f426a7234b89d66
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:11:22 +0000

    field noone phone profit something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
