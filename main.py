ec06a1084a8867f7a756e491d43e0b36af517171
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:00:48 +0000

    exploration choice bite exist shame

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def is_palindrome(s):
+        return s == s[::-1]
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
