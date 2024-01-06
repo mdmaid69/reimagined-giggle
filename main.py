4a09fc5ea19a7e8accc9596f8269a89278358d6c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:58:09 +0000

    town whether important drive block

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_volume(length, width, height):
+        return length * width * height
