50cc5aea2b38e2ea622e64772e02862fa30aec1f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:26:07 +0000

    male report inquire band expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
