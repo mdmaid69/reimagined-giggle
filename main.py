95c00425b45738aeff586ef49e3a6bf325fbdd44
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:30:11 +0000

    slowly nigga age strike deep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_array_as_str(array):
+        return str(array)
