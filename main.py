a715a7335f0a8252e1af89642ea342bcf9da0894
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:59:27 +0000

    spend become throw hold realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
