cee6f103aae0b37bf226f1833f98160e42cdac92
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:05:09 +0000

    classroom indeed shoulder and Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_list(array):
-        return list(array)
+def divide_numbers(x, y):
+        return x / y
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
