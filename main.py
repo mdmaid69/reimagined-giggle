abef9ec5ee7568de22b6de3bc5f75de0e01aea94
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:40:45 +0000

    outside roll spring spare edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
