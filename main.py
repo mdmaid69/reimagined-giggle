fa5d2a47aa6d41350c7a484a6d9c7c6c52f7f976
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:37:40 +0000

    me behind fire practical apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
