0f1b81673a4841a06e3d7f9380495726cf2fb872
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:31:19 +0000

    welcome lesson safe climb sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
