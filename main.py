673a1ae64c884bdaad995a3af0c7b6564eb097db
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:20:38 +0000

    surprised promise family effort strong

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def convert_to_hex(n):
-        return hex(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
