8ad016aa506b67036baa3411146946db8ea90416
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:00:50 +0000

    proper talk knowledge careless balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
