046d51bb6f97e2796c30a675646c69f2eda9cf37
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:16:43 +0000

    rain difficult band really taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def sort_list(lst):
+        return sorted(lst)
