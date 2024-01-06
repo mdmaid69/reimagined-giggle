36d798feb82b45ef9af3da3a0a656ca1a271d749
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:20:18 +0000

    Gg legal foot air curious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
