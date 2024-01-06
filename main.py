cdc5be53e39166a7934e87566e66371863cab579
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:25:10 +0000

    official pity lunch to comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
