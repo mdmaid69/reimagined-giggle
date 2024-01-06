ccad57e108481c2faea3f3913cb2523eb0b78f23
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:28:47 +0000

    quickly move press thinkover book

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def multiply_numbers(x, y):
+        return x * y
