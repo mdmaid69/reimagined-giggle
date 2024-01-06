42b52ddd506a65120f2e54e3f9e7a20369f70afe
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:13:25 +0000

    by uponon rid 567enemy turn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
