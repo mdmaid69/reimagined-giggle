b1dec3a73c6f84f116d2ac062f078b80a598e43b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:49:43 +0000

    Dd form future family off

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def square_number(x):
-        return x**2
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
