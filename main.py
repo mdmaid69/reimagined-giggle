02319469c9f8036a8f0371862ba5bc98ffab59e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:17:27 +0000

    pencil honest sore owe good

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import sys
-print(sys.version)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
