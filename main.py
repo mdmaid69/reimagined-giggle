af4c551b75911934cd98e2848b0431b8db414f18
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:02 +0000

    accidentally plastic reject new pity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
