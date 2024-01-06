2f18e154a11cd9ba2597a6b5a3792c73ae8c99d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:47:50 +0000

    amount trap fierce complaint reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import time
+print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
