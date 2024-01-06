70fd96b841e8abce258b2eb48ef37c8c20ebd590
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:51 +0000

    callup whoever Rr mind second

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
