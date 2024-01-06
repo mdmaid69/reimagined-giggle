08ea722b77d4044afb60d10e7668067b7f86da11
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:40:41 +0000

    responsibility may best wealth open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
