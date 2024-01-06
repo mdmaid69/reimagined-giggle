f88a96311fa193b08aa6e2908bde41285b1b6540
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:12 +0000

    inform care here crash turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
