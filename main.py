d852e004bb57fb8095cc9dbe2dad8b71ecc564d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:31:57 +0000

    expression talkinto field thumb give

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
