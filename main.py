34d346fd6c4f610e516bc5298b30cc8888ca91e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:29:07 +0000

    interrupt reasonable keen suggest comeback

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
