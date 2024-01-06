0e98b19b0b00ed79fc8e6023b93a26ea1063ebfd
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:05:33 +0000

    fast disturb blue frequent except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def roll_die():
-        return random.randint(1, 6)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
