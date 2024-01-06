2a82041448833c7aefe752693f85a8a7394f1e86
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:56:12 +0000

    soak similar live side suffer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+print(json.dumps({"name": "John", "age": 30}))
