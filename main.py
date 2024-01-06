0ebb278881cd30e5e011c628553f5a5b0b0c4b79
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:26:41 +0000

    comment idea ring off your

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
