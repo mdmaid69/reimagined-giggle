02192b08b28bb755d680abce351efd0af0968953
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:29:26 +0000

    increase cheek pupil calloff murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
