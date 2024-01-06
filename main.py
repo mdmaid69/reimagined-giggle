3ca69eaddc283d7bfde55d62a9400faa81249887
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:49:13 +0000

    plain person mother ourselves door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
