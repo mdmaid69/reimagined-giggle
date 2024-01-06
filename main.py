828e3db4375b03830aa2bb36f626a4cf1ad20541
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:19:21 +0000

    greeting whole occasion eager why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_as_str(array):
-        return str(array)
+def remove_from_array(array, item):
+        array.remove(item)
+def greet(name):
+        print(f"Hello, {name}!")
