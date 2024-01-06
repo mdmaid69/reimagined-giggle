6cdbda7783b984c2a04df34110988cba26c23d80
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:06:57 +0000

    wealth eat tidy cough impressive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
