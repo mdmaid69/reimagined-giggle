c3328d345868fa1a80b9ffb433186ecc36cc3852
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:36:33 +0000

    hour hurt suggest exit cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
