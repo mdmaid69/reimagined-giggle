183eea1494343eb22f35901efa7f7aa36bc0e99a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:19:23 +0000

    priority draw sore family split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
