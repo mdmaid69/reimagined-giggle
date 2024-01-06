8259b109832ab59414321457c8cff771e8d9f6cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:38:44 +0000

    translate drop off side fortunately

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
