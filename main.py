0465a45d5e9502ec0d205d81ed2fc3305b9d7b11
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:08:27 +0000

    accordingto neck bury appear stone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-print(os.getcwd())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
