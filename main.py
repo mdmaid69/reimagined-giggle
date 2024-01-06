2a79b8c69f277c678f3467d6a222e81c490dc4dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:38 +0000

    general prevent take popular shock

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def reverse_list(lst):
-        return lst[::-1]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
