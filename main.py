ebcfb341b216cb088941332a88390baba4bbddde
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:52:16 +0000

    nurse alive steam male responsibility

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def reverse_array(array):
-        array.reverse()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
