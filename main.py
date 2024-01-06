1b0418b4aeda3ad6c601a22c7155b549acff34dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:21:36 +0000

    condition desperate comeout expression confident

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
