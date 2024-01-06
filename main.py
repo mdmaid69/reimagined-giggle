05e6bf4c42b9621dd96d097b238afb3518530497
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:28:03 +0000

    busy balance holiday play rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
