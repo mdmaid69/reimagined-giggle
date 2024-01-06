66f6cd6b30d9ab95c420d996c1bb790a6520c113
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:44:46 +0000

    protection comeover afford Gg thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
