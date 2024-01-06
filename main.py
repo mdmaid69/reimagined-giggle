431e61b5591197727aff93e32f78eb1dde03d039
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:08 +0000

    cause bright object damage rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+print(time.time())
