6ea88d0449f3c59c09e78bd9fd2383fb231988d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:02:38 +0000

    punish visit share takeover slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
