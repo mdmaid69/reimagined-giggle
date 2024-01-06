8e4d04fe6c941692f1560e19eab124102c318158
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:21:30 +0000

    scrape dream sound left cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
