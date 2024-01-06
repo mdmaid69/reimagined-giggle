6310cb5877afe49255b8646dcb80ff56348e8914
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:42:01 +0000

    change determined speech eager sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
