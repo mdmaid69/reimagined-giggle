8821ca57bdd19cad9b6ba8e287e3bd0d4547eb2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:10:37 +0000

    even when flow spot aim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def convert_to_hex(n):
-        return hex(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Uppercase:", text.upper())
