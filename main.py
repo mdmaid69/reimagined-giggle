0c5258ae3e6290f72ac0bdb4e38b35bb771e470f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:13:17 +0000

    name Mm goout hers share

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
