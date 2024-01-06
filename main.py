384bead11e8a63ddb9aff05e47ba2a3fbd7b5956
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:10 +0000

    apart because rescue instant rob

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
