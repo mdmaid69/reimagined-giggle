b2f9d7afb14ce0b0af2c71a7183f36333d2c501e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:18:31 +0000

    difference interesting build light sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
 import datetime
-def get_current_date():
-        return datetime.date.today()
+def get_current_datetime():
+        return datetime.datetime.now()
