a7667da74cc8afa80b4bc25f8c2b985cb88b7d9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:53:07 +0000

    avoid dress appearance performance lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
