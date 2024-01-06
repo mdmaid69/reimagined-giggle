bebf3f03a54ca2d199588449c71a3b20a83e72a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:28:39 +0000

    recovery butter patience fence extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import time
+print(time.time())
+def find_max(lst):
+        return max(lst)
