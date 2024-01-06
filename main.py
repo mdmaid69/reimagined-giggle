b377868a312b2f5c8a4e92f47e842f1fc5020591
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:16:32 +0000

    drawing play exchange man square

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_max(lst):
+        return max(lst)
