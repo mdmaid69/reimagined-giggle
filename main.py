66b4e94711a172fc9e288df05bc7362e42684dfa
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:47:49 +0000

    mixture right factory worth divide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
