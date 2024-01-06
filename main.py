46059b50017664ef68014a882d80ff9c2414da1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:18:28 +0000

    full ocean mess radio comefrom

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
