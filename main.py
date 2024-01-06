f2ff8229a77c10d270ea34a772f88beb7a7edf7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:38:51 +0000

    clear stop just matter hand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
