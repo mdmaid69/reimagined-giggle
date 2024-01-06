c2798e4a6c27d2e19087d3cd4ee5bdfbe98fa255
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:18 +0000

    guide noisy continue comeround lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
