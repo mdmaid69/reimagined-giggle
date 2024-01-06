4b6f196bd2b9eafd5ff1ce400ccd84661675aeae
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:30:21 +0000

    round big ring game use

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+from collections import Counter
+print(Counter("hello world"))
