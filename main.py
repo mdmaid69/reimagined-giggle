2a35f798380d8191cabd4526383c05f0fb0acc72
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:45:42 +0000

    opposite no tell mess getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
