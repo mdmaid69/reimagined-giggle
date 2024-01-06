fdc8a19af95a38e4bf19a5fb9f461cd5f001749b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:58:35 +0000

    juice bring acceptable next pupil

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_speed(distance, time):
+        return distance / time
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
