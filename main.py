e126466e70b076a70b0da4b3a41c61458fd57ed3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:52:11 +0000

    Oo pen opposite ring motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import os
+def change_working_directory(path):
+        os.chdir(path)
