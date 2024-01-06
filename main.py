2fd433472148c189aee20792f685d9b6bd9878e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:10:13 +0000

    surprise popular criticize join legal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
