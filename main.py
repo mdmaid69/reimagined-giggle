6c2a56e236e04a092eac9227c86e2a0d8188668d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:47:31 +0000

    shine dry force advice attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
