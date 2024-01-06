69e0a20758d3bc690df7629443dffbd8bbb345dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:23:13 +0000

    gentle disapprove rob stop loss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
