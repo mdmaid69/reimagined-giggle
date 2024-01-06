2e1fac3c779531f4519714ebd3a4b32407f30542
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:43:00 +0000

    watch voice attract dull unkind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_counter():
+        return collections.Counter()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
