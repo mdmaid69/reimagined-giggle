abeae9335a4c89cc8cd3c3eec90b60154b79f72f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:54:41 +0000

    wise crowded bridge spite lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
