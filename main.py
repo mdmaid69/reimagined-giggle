4882b018bc208d8e196c041561305e3ccb0be919
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:14:47 +0000

    contact advantage common better brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_length(array):
+        return len(array)
