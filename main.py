084c8d8fa6862847a21b6ee9e76e2f6d1204f6b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:40:21 +0000

    sort breakinto popular flat cope

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
