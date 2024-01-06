1bcf55f911e70dcbf0ace3892a76b74500f5900a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:34 +0000

    need prepare thorough collect fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
