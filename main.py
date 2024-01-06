393b6e97ef0339c4a0359e45118d8bf1cf43fb90
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:13:52 +0000

    too before star drive shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
