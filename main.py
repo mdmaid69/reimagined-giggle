fface3acd7cf75e414cb72c3d9dde1fa2f3f1d1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:47:22 +0000

    artificial content sort somehow fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
