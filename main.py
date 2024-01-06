46fb62e78e90aa898d42f214efac525312ec53dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:17:53 +0000

    material combination marry would valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_length(array):
+        return len(array)
