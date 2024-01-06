e94eea5000476b53be63e25acf69a75efeb278b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:21:13 +0000

    admit sensible vote Yy person

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def multiply_numbers(x, y):
+        return x * y
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
