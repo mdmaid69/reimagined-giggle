ae77a36d9c88ddebaa2c3c66ab3fd7a90e5e623a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:58:20 +0000

    angry loose fast change city

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+print(os.getcwd())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
