929348db8cb279f2d09522f8fde1bd3c4a3e3e6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:12 +0000

    cut loss cruel goout produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
