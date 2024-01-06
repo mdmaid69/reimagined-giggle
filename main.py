be48923956bb2633deef06b986ebeb79bdd42d7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:06:39 +0000

    nurse immediately increase set shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
