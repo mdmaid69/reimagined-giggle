94579ab9d8035fe07463a47f512b424129186bfd
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:53:00 +0000

    like in nowhere degree protection

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
