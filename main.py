ca429474b6fafaafd066583011ef440099a1c4e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:11 +0000

    approve children similar base Rr

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def convert_to_hex(n):
+        return hex(n)
