ce8dbd0ff26b6f085b8b537b1a112779da4d4945
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:20:38 +0000

    worth repeat depend like and

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
