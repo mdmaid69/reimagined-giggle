c42acbc02bb02b9ac823b12591b980437c5b7fb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:10:40 +0000

    plate shade visit pour drink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
