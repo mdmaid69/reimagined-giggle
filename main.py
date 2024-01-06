0a7611cd921e2fb69728639d6ea00809fd1daae6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:53:17 +0000

    chin maximum original term bother

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
