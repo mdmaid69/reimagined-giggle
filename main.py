7e05e193ba33f9b1f22a3ebc1359ddcac9d84d1a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:12:24 +0000

    damage on programme cross repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
