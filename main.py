29df0b97602aeebf42d4f7634d728abdcb0b96c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:01:16 +0000

    drive but pick group front

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def greet(name):
+        print(f"Hello, {name}!")
+def find_min(lst):
+        return min(lst)
