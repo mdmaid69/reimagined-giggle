07f6625a1b27638536fb9d34db9669d508c2629e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:16:36 +0000

    shallow improvement personal joke entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
