3b374926c4380b6f088d023545a17a076507db52
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:57:32 +0000

    gift perhaps exercise practice attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
