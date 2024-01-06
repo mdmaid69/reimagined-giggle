618780aaab54d6c6533831575e446975e4d43781
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:53:31 +0000

    content interest unlikely normal paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
