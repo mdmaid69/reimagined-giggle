d4f9ab0e4725660e64297cc263caa28109480bd5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:05:03 +0000

    theirs wing last runinto importance

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_hex(n):
-        return hex(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
