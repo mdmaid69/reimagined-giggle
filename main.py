2c39a03c00581334e39a21453672e01c37625e64
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:13:13 +0000

    straight stir narrow programme behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
