ae475e478515a544aaac00979f95ae8e60d92896
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:22:22 +0000

    real turnaway leg Ww frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
