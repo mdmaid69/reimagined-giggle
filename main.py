6a36ee8eca1258219d075964400e16e7fe780737
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:25:50 +0000

    trick yesterday centre wind sweep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+for i in range(10): print(i)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
