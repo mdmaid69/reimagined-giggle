a3f9725c337c802c794c6020f2bf7b2ec96c1cea
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:47:54 +0000

    seem provide impression head bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
