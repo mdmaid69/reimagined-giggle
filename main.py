3238aaff5c2948f1191af854d4dabf472ad0bd6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:02:28 +0000

    post description full put artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
