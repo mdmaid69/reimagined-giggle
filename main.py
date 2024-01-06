d0ee22f7b2cbca04c809e80768dcb1a2193ef79e
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:57:04 +0000

    party line want deliberately embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
