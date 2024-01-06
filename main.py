eff2d11c34881f7eb29bf5d000a0f0c7258d8734
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:19:38 +0000

    fetch used term complete report

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
