6c14f2a8b2523cc750dd7ba04b799810613fdfa6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:47:05 +0000

    habit apparently key profit step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
