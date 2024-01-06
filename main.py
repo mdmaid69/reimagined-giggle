0f753a7b4ced5a9db2e9812cf08de4ad41d97ea3
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:24:50 +0000

    cool clearly Mm find mend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
