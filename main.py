6e9e7c626b2102a0e07a49df6a44c05ef4ee094e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:06:15 +0000

    order storm office national lookup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+print(os.getcwd())
+def calculate_work(force, distance):
+        return force * distance
