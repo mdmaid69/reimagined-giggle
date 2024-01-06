ba1f07e0bbc352e90131b192cb0ba2b31736c530
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:59:20 +0000

    project freedom neither worth opportunity

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def multiply_numbers(x, y):
+        return x * y
+def reverse_string(s):
+        return s[::-1]
