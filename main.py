cce3e784a822778856a654661798254655a61bd8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:32:29 +0000

    clean friend neat ring close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
