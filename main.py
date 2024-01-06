9d98b73ac48945924d20c26e0ef4aeccfc66c739
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:46:46 +0000

    immediate sell reasonable contact date

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
