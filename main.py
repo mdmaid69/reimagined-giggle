2bdbdf50b332220083c03a967f08d8ef72e34618
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:46:29 +0000

    human couple feel fast deal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
