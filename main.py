f841fe9c9821767b6dbd866cb1343397add911d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:31:22 +0000

    opposite form goup issue makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_power(work, time):
+        return work / time
