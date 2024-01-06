8c4a184c94e96ed613fa8cd0494d0f06dab12332
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:41:32 +0000

    deal month clear beautiful instant

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
