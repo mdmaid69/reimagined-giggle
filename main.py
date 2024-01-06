1888282c1ba71ef7aafa372ed6e4af4ebb46d6a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:11:32 +0000

    label scream measure agree statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
