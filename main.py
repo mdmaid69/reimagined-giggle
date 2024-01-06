e171e02c6d451070b32d6ddee62c4269df56fa97
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:23:14 +0000

    price recently quick influence shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
