4b35042064c781f980d388b7165a33f254ef1503
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:45:12 +0000

    Pp limit second drive cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
