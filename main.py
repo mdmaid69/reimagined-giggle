3c62bf15de78585c3db4c23c0a988a385a43e999
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:18:26 +0000

    scare gooff Pp forget lift

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def sort_numbers(numbers):
+        return sorted(numbers)
