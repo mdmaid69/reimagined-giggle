9bc7075cc0a9282072e1644809ee3a09c6cb00e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:31:59 +0000

    central example where possession write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
