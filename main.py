f669a2e7f71d445e84c7effb4cc71097bce7fb16
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:10:10 +0000

    fence jump solve under under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
