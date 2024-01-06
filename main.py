733a902e62991fd873da90e700adc9d895c5fc16
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:36:55 +0000

    exactly goup Ff poor breakup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def divide_numbers(x, y):
+        return x / y
