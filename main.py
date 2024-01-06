43d0212a0a0c3cb0b6d463980dba9738eab2be61
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:47:06 +0000

    bet over region illness impossible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
