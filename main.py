720b110a6e8ae35adf80c79069faa295c6a98c17
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:41:13 +0000

    load both bend then exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def multiply_numbers(x, y):
+        return x * y
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
