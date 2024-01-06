12938e6736610c2a323fb490229c876d76591fa7
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:07:56 +0000

    value respect head thick action

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
