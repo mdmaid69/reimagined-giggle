074866617c606185d2718595622b43cac0420a16
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:56:32 +0000

    arrival communicate dress gap sing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
