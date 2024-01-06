099f11f5014d6e219cae98f2dadd075fb27a9692
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:43:00 +0000

    mind argue body them flat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
