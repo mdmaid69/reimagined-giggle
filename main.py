0134f8e7aa9e520585158ce84f95b9587260cc3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:01:24 +0000

    sink emphasize myself lead uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_characters(sentence):
-        return len(sentence)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def check_if_array_contains_item(array, item):
+        return item in array
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
