8733b58a314084f2629b6617d7011191e948a2ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:35:35 +0000

    runover road commit sit wind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print([x**2 for x in range(10)])
+def calculate_pressure(force, area):
+        return force / area
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
