eea4765b692af3c08a421c0abec60d465be7ce7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:07:59 +0000

    again total deep sign common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
