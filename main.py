ca2242dff59aa25be2449747e3b343e981419ecb
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:49:17 +0000

    mix proof apologizeapologise that tap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
