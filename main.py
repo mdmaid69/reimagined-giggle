11d1a0d2e4735b32a1d1cc6bcb340b09f7f78f96
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:24:46 +0000

    like detail table error situation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import collections
+def create_user_dict():
+        return collections.UserDict()
