5e1e602fe895474208b3acd10641ea1f415ee8e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:04:27 +0000

    wall favourite capital praise imagination

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
