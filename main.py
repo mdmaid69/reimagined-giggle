1ca4b68a5899479b9672844547117f28ac3febfb
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:39:24 +0000

    flavour world height think direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
