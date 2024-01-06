c63fe1ce19ec6d2c34439eea76b230f89d7f9ecc
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:00:14 +0000

    steam worth paper cover alive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
