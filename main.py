5f77dbe086999a2eeaa41ae8895e3ca1d86e521c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:53:08 +0000

    cook his correct mind exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
