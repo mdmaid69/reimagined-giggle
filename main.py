11c7d5ccc217f7a9e0a99b8d3885d57e29b81141
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:27:37 +0000

    must distance lift fashion wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_length(array):
+        return len(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
