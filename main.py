2a4bba6075346caaf13e9e96da42ac418416db04
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:45:22 +0000

    mark leave last then poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
