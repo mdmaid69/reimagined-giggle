dde4e2112c215fb6fc5e5bf8b49bd1d1c74d4540
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:32:29 +0000

    talk light few identify manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
