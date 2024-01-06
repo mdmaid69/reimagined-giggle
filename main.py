d258696a8dcc8af421c894061ab26005f7319b08
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:35:52 +0000

    answer entrance crime uncomfortable development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
