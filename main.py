10cf16fb10cc591bdf1e547d2dcc07892bf8ed8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:15:19 +0000

    hurt calloff somehow soil date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
