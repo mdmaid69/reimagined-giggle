16f44394dd2db08d65070ffd68b735257d5837cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:01:53 +0000

    reply bus stiff keepup town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
