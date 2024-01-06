250b721ec36584ca75edde59a20ed08570c12443
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:46:31 +0000

    drive fashion guess Zz unlikely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sys
+  def get_python_version():
+        return sys.version
