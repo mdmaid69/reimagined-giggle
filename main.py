ff80ab521e52e1d7b942946c03769b049279924d
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:07:00 +0000

    repair father relationship since platform

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+for i in range(5):
+        print(i)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
