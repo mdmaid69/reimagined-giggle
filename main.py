ceccf6f6022bb42cd3b2d5079ce2d47e2397a554
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:02:25 +0000

    loud accommodation attention holdon pump

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
