89ad1d201a369b5491d19427f62a3f0992e4252d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:05:44 +0000

    guilty dream left cross fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
