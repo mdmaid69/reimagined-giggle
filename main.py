afe70a13c68efbe60ddb7a10cbb13b57a39d0dc7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:05:55 +0000

    only she set absolutely bread

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-for i in range(10): print(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_power(work, time):
+        return work / time
