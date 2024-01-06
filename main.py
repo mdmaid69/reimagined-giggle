444778545c96424d53287ffcaab50e1d6552841b
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:14:44 +0000

    seriously waste say thank busy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def subtract_numbers(x, y):
+        return x - y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
