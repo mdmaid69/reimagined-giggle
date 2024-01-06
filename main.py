197a55ab2d7fa2e0c6a28170ece3454c5b9d6dc8
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:42:20 +0000

    schedule contents sign hope long

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def sort_list(lst):
-        return sorted(lst)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
