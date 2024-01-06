0fc3a3fd6e9d8454a0c38384a7711cfd22501870
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:01:47 +0000

    taste achieve abroad pocket accordingto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
