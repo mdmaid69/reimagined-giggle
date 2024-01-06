2b1b75907bdbe6ab23542ee703fdd9962be52f77
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:45:38 +0000

    wise fit wave over neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
