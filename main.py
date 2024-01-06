3f689d2960b44dd3a4f77b9c81687148dd755b62
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:43:03 +0000

    stand march operate come just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_itemsize(array):
+        return array.itemsize
