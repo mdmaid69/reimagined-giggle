aa004f181b409397bb6e6fdc02476cc56f41f638
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:39:07 +0000

    turnover 179bite teach still quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
