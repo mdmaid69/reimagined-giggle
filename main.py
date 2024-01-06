742ef44512c44be05f7450f1e3bf3a4b94c350bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:40:41 +0000

    base actual bang accident attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_set(array):
+        return set(array)
