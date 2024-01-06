3665427f1c4edf2673f35dcbbf51103000620e2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:59:03 +0000

    rule communication so guess bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
