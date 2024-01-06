7b3b89aef4fb545240997f4f3f59aa2163b7796f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:41:51 +0000

    gradually close preparation tidy through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import logging
-def log_message(message):
-        logging.info(message)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
