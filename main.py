94e1bc7166d0939511ff60ee42c6cd8d0fd67826
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:19:02 +0000

    against sink world shock apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
