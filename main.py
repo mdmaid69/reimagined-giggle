9578f09a722c92ac7b92dcb5115f4fafffd51a83
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:11 +0000

    small tune visit load test

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
