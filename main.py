c6d175b57069b0b5f15a77a62afad3a387062470
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:54:10 +0000

    frightened indeed after comment habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
