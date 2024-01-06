972c73f1431846e4743258e6d852d18dc29585e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:24:56 +0000

    future force stage public score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
