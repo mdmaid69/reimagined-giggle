4c5ab08ad9248ca69d4731cb21d6f4c1285909e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:50:41 +0000

    interest start push pour drive

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import getpass
+def get_username():
+        return getpass.getuser()
