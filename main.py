210262fb0e577f08883a1dc9454e7c9ab16a321f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:46:41 +0000

    available shelter beneath ring trip

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
