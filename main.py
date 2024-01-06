57d06d8e7d93f2bc861a919e83f7b8183b1c7783
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:56:53 +0000

    town bringdown sore sense noone

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+print(time.time())
