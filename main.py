fef469f7f443e1589f866ddfef60e1a78c680748
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:02:58 +0000

    rush general last under attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
