97566370dcf02be7de5b6bebd44c77d48050ade1
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:54:12 +0000

    advise in back holdon sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def convert_to_hex(n):
+        return hex(n)
