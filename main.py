546ddee1c70cf1bbc44de8ca6ea47c447eec4073
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:30:25 +0000

    amount shake passage sound quarter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
