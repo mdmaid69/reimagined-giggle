c78db65033c9c0050a443b4ba50d58dccda49751
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:42:20 +0000

    ability spot dick clear comefrom

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
