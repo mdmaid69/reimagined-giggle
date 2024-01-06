1fd7d077bda988955efc2b862a29aa5bc61a4d28
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:19:24 +0000

    view be description pinch sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
