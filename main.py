6cbc0cf8e38aceef85456c04f709ebd5854cd29d
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:36:07 +0000

    then several Uu damage 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
