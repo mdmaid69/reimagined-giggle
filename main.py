039bde1d559fb293fd2bdc37630386f438ea7ac9
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:27:22 +0000

    regard declare dare fetch rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
