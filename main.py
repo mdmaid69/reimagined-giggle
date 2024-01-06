77a1c1d5df49be693834dbfb6557524805bc454d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:04:27 +0000

    contents long interview stand experiment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
