218a7d6656bb7b2d815898123e6060f2939d825b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:58:53 +0000

    spring watch hers sign traffic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_typecode(array):
-        return array.typecode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
