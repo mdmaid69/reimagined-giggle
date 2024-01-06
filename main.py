28b1e078fcf9609c9a3b2d2509a4db5d8edf73c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:09:19 +0000

    check defend variety object morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
