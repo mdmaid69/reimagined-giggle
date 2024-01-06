311379958edd3353a7e9c39fcd285f7509390f2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:02:48 +0000

    copy constantly mind interview pen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
