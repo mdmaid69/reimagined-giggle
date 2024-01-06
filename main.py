bf9dcfe2d26b3821974b669f324749a71ca0b27f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:48:01 +0000

    call talkinto welcome ours drink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
