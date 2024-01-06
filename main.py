3ef66bf02b377110a174eda69033bddb606c4358
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:19:14 +0000

    child achieve ashamed challenge figure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
