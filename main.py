de4b202b69cea84f48032737c54a9170c6966513
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:49:09 +0000

    over drink ticket hope conversation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
   def split_path(path):
         return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
