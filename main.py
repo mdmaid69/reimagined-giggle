b28298a415e83a9e1f1093d90eedcc6b89e4b134
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:09:06 +0000

    mark flat honest everyone distant

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
