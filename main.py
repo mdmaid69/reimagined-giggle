ce411bf1c2a8747b09e0238c882a8d82a6e11a2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:40:19 +0000

    feel twist hurry popular piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
