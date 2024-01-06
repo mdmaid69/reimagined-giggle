46d2aba19badf42fe0d78cb6e2e41497e0a21043
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:48:37 +0000

    lost future programme bang fashion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
