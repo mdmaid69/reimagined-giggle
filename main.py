d89877d1800efaa8bdab8a48146109e9aace69cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:51:06 +0000

    fond size square raise opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
