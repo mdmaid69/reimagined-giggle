6c605eba81729cec64408447c3bbe5e54d7db0b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:17:48 +0000

    large steal include content impossible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
