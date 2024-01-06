2f47ac1e213f125a5f330e55b67d487b518d8dc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:25:00 +0000

    organization blind formal song seriously

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
