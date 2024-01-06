765cc3abd231dd3da2acc254eb9ca8e4fcc95354
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:04:23 +0000

    church inquire lift concern sorry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
