264f85e5e4b14ca29f9c9b549f677eee93e112a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:04:57 +0000

    door into punish there Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
