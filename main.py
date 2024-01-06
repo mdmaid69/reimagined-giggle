4d31d167f6b3e398d8f1d1b34b21a833ffe4e79a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:37:22 +0000

    claim need then zero to

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import sys
+  def get_python_version():
+        return sys.version
