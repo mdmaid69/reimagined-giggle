5ef6540169b4a9672274e43581804e618c7de70f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:35:12 +0000

    ordinary attention ought exhibition callup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
