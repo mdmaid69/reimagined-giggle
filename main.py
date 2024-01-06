ed37f9bd97d31db0a1deda503bc7e90b185bd43c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:10:16 +0000

    back wide old reliable later

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def count_characters(sentence):
+        return len(sentence)
