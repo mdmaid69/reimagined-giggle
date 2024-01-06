9763fc8f5ea1fa4f3e2b7a7d1b375007daeb520a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:21:49 +0000

    freeze interesting telephone shadow Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
