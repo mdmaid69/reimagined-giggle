4ded10198ccae107178146d6e3f67fc4e4c2f59f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:41:58 +0000

    refer winter uniform suck advice

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
