8fe3655e7f46d096bf2d83c1de310730fcf2773a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:53 +0000

    mine shrink chief culture charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
