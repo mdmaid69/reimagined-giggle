88db82f1a876db955bee8a2e7806008c2f58dfee
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:58:06 +0000

    key stick sport unhappy confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
