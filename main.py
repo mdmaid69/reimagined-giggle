999847f52bb876870e7c0bf1e5b79061f746b970
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:06:57 +0000

    obvious curious once cry offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def sort_list(lst):
-        return sorted(lst)
+def reverse_list(lst):
+        return lst[::-1]
+import random
+def roll_die():
+        return random.randint(1, 6)
