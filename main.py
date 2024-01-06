a5f31586a8f473a9b8297b0ab48c4ca138f62b64
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:11:06 +0000

    public expression people work there

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
