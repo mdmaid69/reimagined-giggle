fe35a363e4b3f443f2117d011090e967eb5f473e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:23:52 +0000

    time view game dead confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import collections
+def create_user_list():
+        return collections.UserList()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
