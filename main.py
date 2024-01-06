997294e04178e303ba65e57d212f1721806fa643
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:18:50 +0000

    our like calmly permission union

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_string():
+        return collections.UserString()
