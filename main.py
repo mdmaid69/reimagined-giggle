78e1ff5b86f4f1187e2a0e8f838d2ab03c576dda
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:27:03 +0000

    stop handle common corner connection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
