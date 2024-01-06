ff756c9664217b2e181456f5cd34ce33dd102c81
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:54:20 +0000

    practice otherwise far prove smile

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
