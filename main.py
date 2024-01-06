338b74350f05307c8add3f5c09d42da565cf8428
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:30:34 +0000

    badly stamp appearance notice show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
