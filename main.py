152aabbc7dcf6ab51e1d4e0f1e2a27baa613bf4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:28:50 +0000

    actually comeback software food character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
