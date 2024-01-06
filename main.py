32ce3632b65de90c068c3790397005ba90548e5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:54:54 +0000

    test principle everything middle same

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
