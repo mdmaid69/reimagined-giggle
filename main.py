2d045ad4b037b77267ae0e264671a2743b25ded9
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:21:56 +0000

    we snow pride breathe holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
