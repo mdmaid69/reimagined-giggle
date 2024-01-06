e7cf13e49935acecec0c137c06184352138510e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:35 +0000

    sorry form behind some bang

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+def print_python_version():
+        return sys.version
