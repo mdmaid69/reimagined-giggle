cce68f447e6c8a3283d249995cb97bfae06c5e8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:35:27 +0000

    achieve entrance demonstrate turnup heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
