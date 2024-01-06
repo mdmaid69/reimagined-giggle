4b5d0191c22e85aab1800a6da74cd84264622fb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:10:50 +0000

    reasonable freeze disagree week us

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
