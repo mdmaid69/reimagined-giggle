3e3376d90e2e36574e2de22d24501deb7567ecb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:50:10 +0000

    runover steep listenfor phone shine

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def multiply_numbers(x, y):
+        return x * y
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
