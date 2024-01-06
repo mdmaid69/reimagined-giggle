225fd7eaea2277e5fe7f0cf5629b060d6dfbefff
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:22:30 +0000

    character taste politics neither excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
