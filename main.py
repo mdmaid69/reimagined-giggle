06a54c35aa6b6b890af00dbb10c7d99dd2b16f30
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:03:06 +0000

    catch plain variety wheel Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
