c547de665b74e22626d727c2017cec46c52fc1fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:49:03 +0000

    Dd judge best opponent brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
