7a1ccec11686e6e8b79bec1502d09450447ce56a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:58:01 +0000

    lunch bring several strength conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
