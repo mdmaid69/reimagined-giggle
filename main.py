1bf7c35f978c5392c29cbd24ef1525b42d103b6b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:55:22 +0000

    move touch female judge test

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
