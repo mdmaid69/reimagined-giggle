3aee3b7a55ba7db55c2f59a5fd30e9039c9d1162
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:36:16 +0000

    turnover deliberately bad tongue impressed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def remove_duplicates(lst):
+        return list(set(lst))
