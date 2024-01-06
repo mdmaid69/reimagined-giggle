3a848eeef9e56879697a259ed014aa16f297178c
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:27:07 +0000

    move heart file lesson new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
