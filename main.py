ce621113e265542fecce6d17ea128b3a589379d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:08:16 +0000

    demonstrate them hard relief false

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
