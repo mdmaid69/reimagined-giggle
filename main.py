75e0041e47d363b765dfcdf29edbbcd69333d144
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:44:50 +0000

    party they all language bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
