81f51d4c0f10adb957a8137c2fe558c084150416
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:34:28 +0000

    injury material punish project spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
