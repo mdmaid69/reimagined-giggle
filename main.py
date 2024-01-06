be6a04c1e96eeeb3940e65d05233e41900fd793f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:46:11 +0000

    tend valuable path put disappear

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+for i in range(10): print(i)
