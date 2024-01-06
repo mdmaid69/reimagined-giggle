26a1716276bb8800e5f7d75e7a01a7ab78654e69
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:06:00 +0000

    report spill precise skirt fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
