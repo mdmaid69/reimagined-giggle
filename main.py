223ead9f89d750e6a84d6481d6e3bc81b324bd9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:57:36 +0000

    slow getaway bear future street

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import time
+print(time.time())
