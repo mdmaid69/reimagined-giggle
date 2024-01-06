5b6e32b9c589c68bc36c428bc361bd3c716885b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:07:16 +0000

    holdup everything spend tidy waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
