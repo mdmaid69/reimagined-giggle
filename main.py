08cd0527005bd98e4d870e8c2468e82df2d00924
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:28:22 +0000

    glass innocent common admit among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def clear_array(array):
+        array *= 0
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
