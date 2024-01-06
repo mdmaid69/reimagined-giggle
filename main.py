1245a0e721791b596207c0d5c33080ea41fe0c9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:44:52 +0000

    own failure exaggerate course instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
