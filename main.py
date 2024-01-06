a60d9afd18d87205dd5b704175a9a7647a4f2356
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:25:57 +0000

    crash yet way sensible night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_set(array):
+        return set(array)
