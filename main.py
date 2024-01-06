4764da9b8dd6a756a350257df8454639dddbf724
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:07:28 +0000

    maybe case acceptable reduction regularly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_queue():
-        return collections.deque()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
