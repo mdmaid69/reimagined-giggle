478940a000f10e16e7434b90392901a4cda64f44
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:52 +0000

    back hole yourself getup nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
+def is_palindrome(s):
+        return s == s[::-1]
