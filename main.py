07ec41f7a833f0668820c6d47615c10afee4d267
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:49:32 +0000

    ill skin flat second opposite

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
