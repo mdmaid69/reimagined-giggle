e58c623f626a1239db2b90d20d01a4140b8f2f36
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:05:56 +0000

    set die centre change right

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
