89ed4afc2e956fee7b51d95cc4ea89358d84a4ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:34:48 +0000

    child shrink then direct question

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
