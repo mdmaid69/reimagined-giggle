cbc990e064dfa05116c5244970501bafc0a55e4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:27:54 +0000

    reaction frightened sensible wear 179bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def multiply_numbers(x, y):
+        return x * y
