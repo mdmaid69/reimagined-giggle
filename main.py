d419aaa43cc9c0e981993ac85ff597afe9c443b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:58:33 +0000

    please first dear at wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import sys
+def print_python_version():
+        return sys.version
