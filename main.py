2358e5f9a197bfba7115d6fc2a42be27787cbde2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:02:11 +0000

    mention anybody favour stand really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import getpass
+def get_username():
+        return getpass.getuser()
+import json
+print(json.dumps({"name": "John", "age": 30}))
