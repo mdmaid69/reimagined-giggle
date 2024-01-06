744dee817599c774b587223a95669f64c1aa8e4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:32:49 +0000

    complicated justice past score sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_error_function(x):
+        return math.erf(x)
