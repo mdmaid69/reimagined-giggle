99523cb00219f622231d5e112193b1cf5adbc364
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:11:01 +0000

    object soak can confidence excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_pressure(force, area):
+        return force / area
