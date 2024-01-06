70c7e6a4195934d47d077a428db6c906f48e5bff
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:05:10 +0000

    back ignore instrument determined interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area(radius):
+        return 3.14 * radius * radius
