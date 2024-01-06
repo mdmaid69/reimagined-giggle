8bd74aaa1ac7c050f42521948913138e6c36e057
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:19:37 +0000

    outside listenfor bear perform reduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_density(mass, volume):
+        return mass / volume
