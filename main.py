13a3720dfa92d043bf280c587fa8552a0c45d57c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:25:08 +0000

    never tour completely another imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def add_numbers(a, b):
-        return a + b
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_work(force, distance):
+        return force * distance
