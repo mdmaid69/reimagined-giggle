c2aaf57a5021e27cd113a41955d426052fe5500b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:52:41 +0000

    mistake every charge equal store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import logging
-def log_message(message):
-        logging.info(message)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
