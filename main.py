3da81b3b845117c75fff20830588da561b473583
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:53:32 +0000

    compare passage tap slip various

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def square_number(x):
-        return x**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
