295bd24e7b42585b2737e37e14d522e4772f2849
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:28:26 +0000

    difficulty attractive dick prove sight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
