5b0968ac8858d2265d4df95abe575440ee626c59
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:53:01 +0000

    see pencil direct house either

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def count_characters(sentence):
+        return len(sentence)
