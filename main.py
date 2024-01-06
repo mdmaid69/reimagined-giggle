0ab5d001169216d95c24a37ed2f20a445cc819f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:48:37 +0000

    scene score tough land appearance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-print(random.randint(0, 100))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
