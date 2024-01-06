708e7704a99377bc6022814898add96e69346aa0
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:55:17 +0000

    value plenty getback luck smell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
