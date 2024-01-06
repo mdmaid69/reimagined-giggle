08c49519ad7a553ff6eda95451f1f3bf4c842ebc
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:13:40 +0000

    fold factory nose runover press

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def add_numbers(x, y):
+        return x + y
