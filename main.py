3a2ecc9080d9110e7cc259240dc52ee63894248c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:10:33 +0000

    emphasis careful expect edge danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
