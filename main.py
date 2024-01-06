f64acd81f8f441eaf1c4d6e72cc9f0f0ab6d24c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:57:57 +0000

    people direct prefer dull appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
