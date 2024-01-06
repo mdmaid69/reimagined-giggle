cbe255650936e4c4167b4c9a8e997b1e8c2f588f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:28 +0000

    at destroy goup on switch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def add_numbers(x, y):
-        return x + y
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
