14d017d33c6877fe8f6aeb88f4c50e536899c20f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:22:39 +0000

    jump downstairs general famous wash

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def print_python_version():
-        return sys.version
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_int(array):
+        return int(array[0])
