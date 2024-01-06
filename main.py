b63d2b40f5f49d37179194d05f16a2987ff6ed25
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:31:18 +0000

    so official actually programme shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_list(array):
-        return list(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
