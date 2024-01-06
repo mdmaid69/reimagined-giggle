db0098e19a32fda3e40e8866aab8de066a9e747b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:18:55 +0000

    heart cheap protection light wing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_work(force, distance):
+        return force * distance
+def calculate_speed(distance, time):
+        return distance / time
