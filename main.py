426b3a5e0d2519124b87d0801accfb968f3712cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:03:30 +0000

    lonely relation ever anyone science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_current_working_directory():
+        return os.getcwd()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
