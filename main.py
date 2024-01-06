50c6bd0dd116d1cfd26951bdfb091aba0f3dfb88
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:34:57 +0000

    border reason maintain character active

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
