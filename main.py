c77fea62b358f21c4b29eead9e52bcedd018b303
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:16:09 +0000

    equal dry responsibility giveaway mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
