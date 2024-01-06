4e38999b1684d78ab86ec2a89e02b60488be2e86
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:23:27 +0000

    little contrast bringforward down clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
