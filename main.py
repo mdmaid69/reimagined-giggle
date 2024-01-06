97a3c3abf25e69c5369b3ee5f6fea708a1d0b5af
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:43 +0000

    difference keepaway avoid teach shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
