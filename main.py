d3a42dae1884ef4682503cb080c4e3efbfaa555a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:40:56 +0000

    either in seem mail large

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import sys
+def print_python_version():
+        print(sys.version)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
