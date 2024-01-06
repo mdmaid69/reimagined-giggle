6baa5a5bdea3eb549f29b1e31a1aec8eb0d46d44
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:50:30 +0000

    float pupil illness confusing light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def count_words(sentence):
-        return len(sentence.split())
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
