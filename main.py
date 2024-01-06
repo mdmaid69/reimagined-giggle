e72cbd4390d7f2b70231e3aaa1d28931e56ed075
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:07:03 +0000

    beautiful acceptable empty impressed goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
