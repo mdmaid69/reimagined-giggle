e381591b463712eace7250d0e53f7f946ac2d45d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:10:10 +0000

    concerned only surface friendly avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
