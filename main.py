593ef60df865ace313c8999e2e12f8079b2207ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:05:21 +0000

    sore sell around section flow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_density(mass, volume):
+        return mass / volume
+  def square_number(x):
+        return x**2
