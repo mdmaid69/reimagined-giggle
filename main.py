2f319535eccedbc8c20b889f27a81a941f57afcc
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:29:45 +0000

    mother appeal also away rule

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
