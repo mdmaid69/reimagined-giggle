6076a62c9f191f228825c7c40fc7eebc0b20fe00
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:36:34 +0000

    book turninto ring concerning please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
