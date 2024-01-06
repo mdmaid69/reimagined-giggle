8713bba3eed05c758e4d04ba9676d12d01cac203
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:43:48 +0000

    interest kick last one container

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
