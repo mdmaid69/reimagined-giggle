715d42d36b1fcff4b0ed8f7c62e989eb1359db5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:26 +0000

    challenge exam after whom sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
