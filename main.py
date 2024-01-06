4a304cc593e27dcd22505e7dd2dbfe8981e93ba1
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:58 +0000

    properly detail sweet deliberately concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
