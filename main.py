f8004e67064843b4a39d3f6c6988cb991918db07
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:02:22 +0000

    fence estimate nowhere concentration minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
