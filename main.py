7fff199c24f7c5289668284ee1f31fd92f3f2c92
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:14:30 +0000

    slide song come hurry pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
