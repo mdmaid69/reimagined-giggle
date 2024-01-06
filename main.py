4d52f3f74fda0c8800ec1ecb4eac22f7befd0307
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:27:40 +0000

    married waste bus worth back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
