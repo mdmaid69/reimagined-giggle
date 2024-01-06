25b1305b3aef52f6416cccbc3e6b284a7b80de36
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:22:45 +0000

    where neat disagree short display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_area_rectangle(l, w):
+        return l * w
+import collections
+def create_queue():
+        return collections.deque()
