1677eba60eab7fa597d0bfd2066313cf6f75fb6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:02:33 +0000

    agree switch knock afternoon 565end

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
