f263b21aba244ce5dac3bca936513e786c598caf
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:34:19 +0000

    deal angry that luck preparation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_user_dict():
+        return collections.UserDict()
