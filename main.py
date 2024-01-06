bd05d77710d28df7ecbcbfc4507d625361455687
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:30:20 +0000

    somehow pull keepfrom suckme recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
