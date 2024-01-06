aa0224c3dd99c276412f5455663ae8149e75c727
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:45:04 +0000

    during task many second by

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
