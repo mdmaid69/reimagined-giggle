7966115c5b8d0f0d86ab4d6f528d797871c8d6ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:44:30 +0000

    label obvious tall lonely giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
