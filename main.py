e494983a1b4df735fe9f89f08aeb0eed68210926
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:13:32 +0000

    among protect unfortunate game cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
