41cd50024aaeb5e58b3abdac0c142beb4ca3c64f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:35:53 +0000

    excited police pause note find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
+def is_odd(n):
+        return n % 2 != 0
