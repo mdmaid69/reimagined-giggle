363e1136c7a41cd760730c25c904954bd6208081
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:45:20 +0000

    name anxious trade thorough fail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
