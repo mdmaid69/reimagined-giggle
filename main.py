5829c6e58431e75cba447ba59d53cdedc0e637ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:52:55 +0000

    set help depend shoulder escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_list(array):
+        return list(array)
