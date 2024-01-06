20db15056cd90fce3816acbd9fbe36fcbddec06f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:21:13 +0000

    only run lucky exciting type

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print("Hello, world!")
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
