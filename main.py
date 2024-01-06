6c9c77dd4eba21e6524e037de95e87b3c4d628ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:39:17 +0000

    office avoid between bank sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_max(numbers):
+        return max(numbers)
