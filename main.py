d0fd3a5245361b15fc01d843aae4eca8d3fc6928
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:43:19 +0000

    with why insist silent around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
