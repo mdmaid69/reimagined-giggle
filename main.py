68b5477cdb8bf7a4b295b6b8943b193866f6bb54
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:48:41 +0000

    permission refuse open niggaacademy interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
