5ac6cb72486030b5d253300f2d3a30fae6f73de0
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:23:16 +0000

    mainly offer clean cover head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
