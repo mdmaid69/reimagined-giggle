c6b10380d827768c08860721db87510d7895a425
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:20:03 +0000

    whistle Oo like sometimes born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
