2bc71c66be24d74a324f2e4bd68905b4a9d7259b
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:11:45 +0000

    tall stick but present yours

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
