8be48ea685d44cd3c95e3f4abc7dface791b41b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:19:27 +0000

    if permission prefer their probably

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
