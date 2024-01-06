37506c1917cef985fb343a23e8b40658021daf47
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:18:00 +0000

    small nurse possession sale represent

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
