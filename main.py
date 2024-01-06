c4260f9076bd965bf12ca17e9c891d75b4441d81
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:45:42 +0000

    intelligent on passenger between wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
