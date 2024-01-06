735cb324e4a5ab0a8e541f4a314aa5d46c429da2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:30:38 +0000

    holdon hit material unkind narrow

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_max(lst):
+        return max(lst)
