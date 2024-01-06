dc1b8839e14fcfe91df273c6f9fe32fc80898860
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:03:54 +0000

    wear address act sit dead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
