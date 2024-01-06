a8a97c9a2d9a27c83301166e1e1163fa38934bbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:15:44 +0000

    affect excuse squeeze its Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
