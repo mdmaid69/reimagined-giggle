b71c88da53a088e97060a8373b78801ada243862
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:47:02 +0000

    trouble brush interfere apart creature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def delete_file(file_name):
+        os.remove(file_name)
