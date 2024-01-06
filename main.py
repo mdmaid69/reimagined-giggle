ab8094cdec2222328d2d793fe1638f37c43da70f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:22:44 +0000

    path turn excuse Uu window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
