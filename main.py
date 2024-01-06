b1c6ed8f0a5ece0ba8730f88cfbca357bddffbba
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:14 +0000

    love ticket tip change but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
