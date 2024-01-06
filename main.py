a5d22a1508ad6c8c0825fffe733049373b9e6606
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:24:28 +0000

    both disappointed also into bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
