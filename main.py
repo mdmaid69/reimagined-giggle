44bf52b7ccecb9f0ba31a46ff2e4aacf3f2a8b98
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:44:41 +0000

    commit paper used force minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
