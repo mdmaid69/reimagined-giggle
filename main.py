65872a033f9c9b16146fd2aff7dd06c765e8a255
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:01:18 +0000

    settle report object train drawing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
