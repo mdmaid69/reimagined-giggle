0fcd6ea9fd51efb3fd97aeeff7cfe6a62983efb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:42:48 +0000

    ball bath worry squeeze any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
