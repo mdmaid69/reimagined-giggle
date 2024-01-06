f2dc96f0eac27e643207f9bc23746fd34d4c9692
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:36:23 +0000

    rare head long survive come

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
