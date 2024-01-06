dee69f66bd74e4550d01dd1a77da6c54502ec9d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:09:04 +0000

    hate time appear rush forget

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
