695093e23eaff6d6d213434453ffa98abad71098
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:40 +0000

    pity innocent move Ff fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def greet(name):
+        print(f"Hello, {name}!")
