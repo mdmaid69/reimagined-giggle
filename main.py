977f5738feb402860cfe11cf9519c2d98e16738d
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:38:12 +0000

    bottom crowded tidy way hope

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def convert_to_octal(n):
-        return oct(n)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
