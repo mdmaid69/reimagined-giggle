85870b5b1ab00a87be65ba1a73b8132ec690ad12
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:13:50 +0000

    raw wire save tense nothing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
