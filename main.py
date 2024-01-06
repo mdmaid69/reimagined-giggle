1d1f440384cc8db968863b9a916d035a1e9f6472
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:42:21 +0000

    towards aim season substance protest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
