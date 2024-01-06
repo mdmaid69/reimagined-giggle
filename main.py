b87ddb67dc467056e2dabe0991641ba79a255e18
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:20:25 +0000

    gap translate race scream separate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
