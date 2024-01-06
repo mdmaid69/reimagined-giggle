dcc09efc61725e2385408784cceac14510e88ab0
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:38:28 +0000

    introduction first last more statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
