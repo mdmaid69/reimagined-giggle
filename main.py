2dc9e13bf65726bf309ed5e3552795276fad750d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:20:46 +0000

    several gate hurt fill mine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
