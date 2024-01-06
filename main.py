c61a6e03218c4d60a8bdd5040e2b329b7e34c739
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:24:02 +0000

    message interrupt opinion choose above

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
