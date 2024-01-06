122286de52c5931cac3de5bed23396a3d509f315
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:33:48 +0000

    sleep create record trust climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_directory():
+        return os.getcwd()
+import sys
+def print_python_version():
+        return sys.version
