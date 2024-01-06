fe0874170fc6bbc7c49a6d4f9b2d9e7f5762e483
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:21:03 +0000

    bridge work comedown top fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
