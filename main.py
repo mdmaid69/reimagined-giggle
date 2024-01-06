f5771da25ef9b8e6351885646e7bb0bb14847940
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:05:01 +0000

    clearly calloff wake skill steer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+print(sys.version)
