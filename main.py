728794b0f9d883ff6ea09c1b2c6bfbb0ff8884be
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:09:42 +0000

    everyone best report land backwards

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
