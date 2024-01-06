b4043e462b0669c3b603997f4efba55a0841d28f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:47:46 +0000

    example where bag spend soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
