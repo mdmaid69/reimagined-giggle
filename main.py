da7cdb8e81d23085f9c93254916172d1fe75da6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:17:10 +0000

    bored stranger mine little ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
