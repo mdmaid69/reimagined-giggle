972d198f13edbabed560105c6d117240b325f7a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:45:30 +0000

    breathe top alone wear ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
