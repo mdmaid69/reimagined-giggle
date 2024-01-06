591bec1537c4a2c049e8295c9c097f5f66ee0d35
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:34 +0000

    instruction under vanish godown pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
