adbdbf0ac000697df7376b1f2924b0a3951046db
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:21:19 +0000

    usual week exchange regret best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
