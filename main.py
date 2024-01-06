01d3bab82dc523c19765abe5f429eab6e3514293
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:33:47 +0000

    571enough Tt turnout speed temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def reverse_array(array):
+        array.reverse()
