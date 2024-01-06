e6715178974eea17bd5476a9a69037e15044525d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:04:53 +0000

    concentrate wish key takeover keepup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-print(os.getcwd())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+text = "Hello, world!"
+print("Words:", len(text.split()))
