ff7329ec9143c229f47ea5da305e60be9798f7ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:35:28 +0000

    match runinto report underneath fit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+for i in range(10): print(i)
