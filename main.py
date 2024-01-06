2fa8a11773cbc9c35d0a6b4004ca423767ae60bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:14:14 +0000

    theirs present physical again priority

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
