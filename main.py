e5b2ab91ea493e339e64b9d6b10e0b0fcb98e4fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:04:33 +0000

    also mind fierce smell also

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
