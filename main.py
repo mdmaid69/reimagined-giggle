30347229c60543c02d32c6f047998ffbfba1db8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:37:01 +0000

    war young reject keen schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
