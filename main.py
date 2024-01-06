04f6ae8e3b45be64f79c7991edd969a1d36aadec
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:54:34 +0000

    afraid active weigh Kk difficult

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
