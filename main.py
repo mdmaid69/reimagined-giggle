e046f6eaf2aa281a75bb9f40a191f9f0db406a86
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:12:53 +0000

    and talk receive fail conversation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_str(array):
-        return str(array)
+import numpy as np
+print(np.array([1, 2, 3]))
+def remove_duplicates(lst):
+        return list(set(lst))
