599c883f2cac9b017d28d8a5e5862efb72aa5608
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:08:07 +0000

    year alone suckme artificial love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_base_name(path):
+        return os.path.basename(path)
