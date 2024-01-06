d4025e50b950bb43073af88f30047b79ea522b5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:04:55 +0000

    forever person pity true neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
