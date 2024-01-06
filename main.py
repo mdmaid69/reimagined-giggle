9390c6cea8623a88f16c2edb06f6e231858f10b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:37:25 +0000

    start thought task tear recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
