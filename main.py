bb35844595f84d269ebcb30b1ee07420460b4f95
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:23:36 +0000

    foot name plan judge almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
