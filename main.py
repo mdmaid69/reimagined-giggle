7f5991bdca5a2d42d0f80356c37297beb0462468
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:18:59 +0000

    though combine become extra programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
