47569e7c149cebfc0e9bc45cec5b2671cc07a589
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:08:15 +0000

    appear separate whoever state easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import sys
-  def get_python_version():
-        return sys.version
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
