4c892bf1f8426a21d1d3959ff747bf76ceae8b97
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:14 +0000

    worse control spoil steady pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def convert_to_binary(n):
+        return bin(n)
