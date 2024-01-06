3d631a3675edc1015ede4efff59189f697d7182a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:27:06 +0000

    comeoff fight recently radio shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
