f2960b3620bca1aeded499b05f6769f040752b5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:20:45 +0000

    hurt today base gift call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
