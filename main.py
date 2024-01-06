b5e5ea04c2869f5b0e44df47846b23ece19f8630
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:56:22 +0000

    slide bury rain thinkover shape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
