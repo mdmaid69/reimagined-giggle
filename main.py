1e829f86ca57c0c9427ee6956ebef82455422526
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:57:38 +0000

    date either sign wherever communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_current_working_directory():
+        return os.getcwd()
