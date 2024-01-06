4a153f04dc684632da7f04437efbb11613b34719
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:09:12 +0000

    compare though your burst impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
