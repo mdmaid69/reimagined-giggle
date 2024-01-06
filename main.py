a6a8c912bd152353d07f29d0ca8115247909c6b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:37:10 +0000

    yourself lend calloff half so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
