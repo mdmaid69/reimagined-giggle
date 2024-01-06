e03df98f1509a802f1b578d0ca416ab1d196bbd0
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:38:23 +0000

    intend with goon reduction reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
