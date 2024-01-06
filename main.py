83fc51761cdf0be447daa3ff4108a789219e48a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:06:18 +0000

    away dress suggest another there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
