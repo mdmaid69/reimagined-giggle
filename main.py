a863b6574e6e1bd8da2204d134fa020676a6c022
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:38:23 +0000

    claim politics connect male exist

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
 def calculate_work(force, distance):
         return force * distance
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
