f0ca63da9a22c931ebf048fa7621a49b7e07de20
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:27:13 +0000

    goback(to) agreement exercise Ee represent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import numpy as np
+print(np.array([1, 2, 3]))
