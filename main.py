c3e32c4ac66b90496c25abad4f4e7f6e478abe2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:29 +0000

    know rest thinkthrough fat please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
