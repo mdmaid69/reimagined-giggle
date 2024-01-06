56ea990e5ed25a0831f89cad3e517cfeb84cc299
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:54:43 +0000

    talkinto immediate stop brief sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
