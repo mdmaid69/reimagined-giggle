5b3fe3fa444ddef2664d84b0242d0c71f4bff17c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:38:01 +0000

    heavy calloff can share emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
