233e60d5f8f412f3958682857bc4e5131f7902db
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:13:16 +0000

    usually bag uncertain film destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
