9c3fcb16777a25ae9d7b95a3fd99da1d79b0d3f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:41:12 +0000

    objection majority central offence right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def square_number(x):
+        return x**2
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
