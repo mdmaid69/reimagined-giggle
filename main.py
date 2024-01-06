b20ed1d432d91a1a404e6c3f22195ab9eb1bb0c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:56:17 +0000

    subject depend rest workupto cap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
