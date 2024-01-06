4ef8c59d0d778825186dfc230a73839f6c61a09d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:27:11 +0000

    Nn announce female bottom drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
