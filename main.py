562c58cc54486a2ae52091c2e96b069f1b9d1641
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:03 +0000

    ask wipe space cheat hide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def square_number(x):
-        return x**2
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
