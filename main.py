b61146c5d3f85a2dd1c281922136547f1c935d86
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:44:55 +0000

    provide telephone moon whether wet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import tensorflow as tf
+print(tf.__version__)
+def find_max(lst):
+        return max(lst)
