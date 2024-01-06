2f331283ccad2f256e1dc5a991b57d4b79d24a0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:39:24 +0000

    minor round different she quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
