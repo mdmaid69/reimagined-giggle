dff1225758bf15ab8eae6f6057461f55ab278931
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:26:09 +0000

    arm drink smile love feeling

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_speed(distance, time):
+        return distance / time
