a57933ac1dacdb887992f06e11b8e16b5319f5d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:36:31 +0000

    on phone stamp either tall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def cube_number(x):
-        return x**3
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def subtract_numbers(x, y):
+        return x - y
