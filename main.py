0f42b3ee84a0bef1bf184f3733863ad121809b4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:36:52 +0000

    notice raw but delight Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
