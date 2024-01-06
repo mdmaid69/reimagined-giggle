5524a663ffe7baab29da5d4e2092ad863984e74b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:18 +0000

    principle hit accident defeat regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
