cdd0eeb01a09a24eceb9086f68ccbb1c8846fb89
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:47 +0000

    blame environment prepare will someone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+print(os.getcwd())
