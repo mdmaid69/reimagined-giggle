60940ed73e07e93b5749d1601abfdb895147078a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:32:51 +0000

    fresh by explain carry sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
