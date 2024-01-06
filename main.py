e93cb155d803afded6fd8891ef4c14b89be68bb7
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:22:44 +0000

    Ee spend another chief makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
