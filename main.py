f33ae35b6cba8ea6409980e96c0862d8c9a48a0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:06:49 +0000

    also unpleasant which mark small

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-for i in range(5):
-        print(i)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
