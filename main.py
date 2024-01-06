436d843512a3f70df50de9f69509d37b413deebf
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:48:45 +0000

    sensitive right blue interfere instrument

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
