1435ae66b3a283f61e539eb63c97d6fae2944a1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:09:18 +0000

    ours store shock convinced dress

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
