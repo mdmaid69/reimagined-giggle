97a2f8bf6106f86890aee04f76fe3af625fd83d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:59:39 +0000

    normal demand ring hers boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
