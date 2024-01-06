56e2cdc3bbdedeab13027e18508d4a740c684c39
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:54:17 +0000

    along goup further something settle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def reverse_array(array):
+        array.reverse()
