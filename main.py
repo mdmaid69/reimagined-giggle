dff85eb894e261313670b8a949de96f8a38c09ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:31:06 +0000

    seriously band eager nose sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(numbers):
+        return min(numbers)
