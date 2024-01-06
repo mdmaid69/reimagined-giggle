3b57a292b43ebb834736447cb8f4e36b3da3281d
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:20:46 +0000

    ball middle mine simple foot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
