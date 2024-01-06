007c978e9af7adb9ce9489266f629e522f5f8cf9
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:44:17 +0000

    best spring fault relative record

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_characters(sentence):
+        return len(sentence)
