7ffb62c38e06c787ed76b15b11b18a6a6a51a7f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:21:54 +0000

    talk storm exchange complain shine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
+def add_numbers(x, y):
+        return x + y
