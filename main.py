8f444c0f54338d00d8f92193605b544b37a63ac2
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:33:40 +0000

    wash cheque both sorry this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
