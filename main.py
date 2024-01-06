556c32266388e02e54b0bc3fda9d886bdb28e24f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:15:24 +0000

    talkback shelter offence point thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
