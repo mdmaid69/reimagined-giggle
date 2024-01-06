78c4c29ac6c62d38f36fa85e87889f980c7edb8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:10:44 +0000

    swim clear load statement early

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
