43c95b9e06b8becf711659ab95addc485ead9d19
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:32:23 +0000

    until prize once next but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def subtract_numbers(x, y):
-        return x - y
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
