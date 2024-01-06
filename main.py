9a7af0d3f74ece7700bb67385fc14f0ad570ff12
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:11:35 +0000

    getback stay so broad file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
