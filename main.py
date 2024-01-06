fb51c6668bf3177c48c101f565fb2cc8d895cdef
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:54:45 +0000

    what region set Yy system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
