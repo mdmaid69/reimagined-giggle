d7b94f045560b10f7162ac8a5cf52e7311783688
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:22:10 +0000

    exercise accordingto release steep it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
