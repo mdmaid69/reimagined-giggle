31c933adfc692e94e3b1fa6fb8ec91331c93891e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:04:30 +0000

    party list interested mother vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
