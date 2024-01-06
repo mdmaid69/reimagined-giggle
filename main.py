c489db6b8bb1bef8b39a4a1a93e0e20b948ab5a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:09:34 +0000

    ought plenty correction slow careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import random
+print(random.randint(0, 100))
