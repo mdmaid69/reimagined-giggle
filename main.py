95b08c4761f395e9003a0edee7f3f7a0d4494a9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:24:37 +0000

    comfort unable string recognize art

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
