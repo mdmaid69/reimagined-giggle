589f460b9439c7220a089ac31b5dda2c56cd36ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:33:45 +0000

    decide lay thin side task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
