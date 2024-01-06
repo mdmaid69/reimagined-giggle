b4ebf8eb140bed108911a7ec297e430ff3843ea9
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:49:40 +0000

    quickly artificial tense any wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
