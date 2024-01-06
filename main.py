922da3e0ed3b6bd9d2440176b88cfd255c3c964c
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:11:14 +0000

    tidy generally impossible abroad but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
