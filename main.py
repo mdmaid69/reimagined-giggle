71fab5d17dd5afed9149886e216c99a6fc284cbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:57:59 +0000

    bottom tradition distant brother deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
