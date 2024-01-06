0b8b57e7253ead51f9189a2f18b2afa6c5cfa891
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:13:30 +0000

    single little contain open wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_index(array, item):
+        return array.index(item)
