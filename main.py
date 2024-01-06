f0a158afa9ff19d5f910f33eff18e1a676c1f529
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:11:27 +0000

    ground change light forget dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def sort_list(lst):
+        return sorted(lst)
+x = 10
+y = 20
+print("Sum:", x + y)
