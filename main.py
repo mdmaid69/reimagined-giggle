65e14a0cc7d0e751476d4b455fb91c849ec8591a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:18:18 +0000

    share shock reply separate autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import logging
+def log_message(message):
+        logging.info(message)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
