8f82a561c1a601e64946a512f9cf0964600edc6c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:43:56 +0000

    price off getback space opportunity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+text = "Hello, world!"
+print("Words:", len(text.split()))
