2761e2b6bfc83ced72ed7d88c62b70da0c7acbfd
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:21:32 +0000

    check reject mass Mm wash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
