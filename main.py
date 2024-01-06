6be7cb41572aad303a8e11f2247ea120cfc6dced
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:00:51 +0000

    thinkof public strike free content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
