3d0633a0b2e7f97a75d108e68b2d9df9892cc542
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:07:22 +0000

    damage blame live impressed sister

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
