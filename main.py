71cceb8ac8e02d09afbe017e0844b5f2c9a6769c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:30:54 +0000

    blind welcome odd hungry otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
