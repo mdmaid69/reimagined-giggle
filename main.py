2a64b7ff8ec9f947dd99fe223903d96a37e515c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:53:42 +0000

    achievement ring then definite try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
