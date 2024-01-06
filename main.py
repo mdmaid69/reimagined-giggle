bf76c71125221296d4d6457a84625971a9907085
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:59:01 +0000

    prepare question anyone accident sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import math
-print(math.pi)
+def calculate_acceleration(speed, time):
+        return speed / time
