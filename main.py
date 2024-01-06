071b09f5559eba975d7f2454ce995daffd23b6eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:41:28 +0000

    shut evidence university track left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def create_priority_queue():
+        return collections.deque()
