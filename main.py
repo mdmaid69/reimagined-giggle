01251a3c8357efecb3a2aabe1fc4bbcd1cc57aa4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:11:05 +0000

    television anyone extra me keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
-print(random.randint(0, 100))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
