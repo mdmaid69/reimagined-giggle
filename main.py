f559213c562df465ef9f9e2a8d1a5eb7eeffc486
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:34:26 +0000

    glad health rather truth ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
