9b99a99ee217fd8ecda0bfca194a45132d02f258
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:35:46 +0000

    welcome 179bite excuse plan treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
