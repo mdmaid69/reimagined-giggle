42b9b43463856d1f9f1bef073c7249b281151b32
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:53:36 +0000

    breakfast mixture special break backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
