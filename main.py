8a2256006bc434a73c4bbaa26170098fa514c454
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:30:15 +0000

    door dick fall scene buy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
