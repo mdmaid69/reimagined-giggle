9a3befe56b2f44ab2e2a6e114fd8362d31ee1016
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:11:47 +0000

    copy story pinch certain hospital

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
