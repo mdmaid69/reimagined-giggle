ecbb9348195a81ba854fb5cf77e9fb0545801b0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:23:37 +0000

    deep talk where view season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
