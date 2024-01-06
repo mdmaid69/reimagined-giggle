164c7e60ed8920881229eb4bde0691ea5183c82f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:21:39 +0000

    protection wash point human safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
