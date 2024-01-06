776b4de079c99547b22b96a9a92e0c1832a2af17
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:49:16 +0000

    ring load future active getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_complex(array):
+        return complex(array[0])
