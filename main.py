bb70ca6eb5e25fd18bdc3187e0fbf8821049c6d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:06:01 +0000

    survive save tool accident single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
