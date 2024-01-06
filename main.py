f78f5ab46b7c18389ea81b6b31cb89e4807e3dc3
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:45:32 +0000

    moral to whatever punish muscle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def square_number(x):
-        return x**2
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
