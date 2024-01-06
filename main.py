79817a7f63f8f25e54cf1dbb483ad59862480579
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:57:41 +0000

    manner bound might tidy scale

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 def find_max(lst):
         return max(lst)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+from collections import Counter
+print(Counter("hello world"))
