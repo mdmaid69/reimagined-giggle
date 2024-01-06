baf908220ef4abe86091ecc68ec7c5ec2b8ef471
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:12:22 +0000

    concerned fall book Ff sink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
