65f24f3590e82a673be72c168f71fd6232907709
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:12:55 +0000

    difficult cry second best afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
