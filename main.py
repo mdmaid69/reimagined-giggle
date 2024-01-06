6079e04e242e1d62ed734ede4bab29bb3f77a1b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:29:35 +0000

    entrance sometimes convinced succeed put

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
