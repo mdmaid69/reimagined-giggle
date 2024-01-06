e2960cbee57e66976e04454e4ebaa500011e4983
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:14:38 +0000

    thumb instrument reserve tip forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
