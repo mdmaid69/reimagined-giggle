019ff11bcfa4fc0134f8a14dadfc25543897f982
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:47:29 +0000

    help pocket colour wash ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def cube_number(x):
-        return x**3
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
