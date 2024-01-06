e5c26fd2990d2917cd13a91dd7714f906cde83be
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:40:08 +0000

    anxious herself repeat fix bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
