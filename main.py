e78a8052f112e3c5920d5b0da19b3122397fc973
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:37:14 +0000

    fine put yesterday everything keep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
