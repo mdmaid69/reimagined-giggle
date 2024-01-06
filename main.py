572cfcd2ebc59d210edd31f1d505a0e174f33300
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:03:10 +0000

    hand shopping wear pocket understand

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
