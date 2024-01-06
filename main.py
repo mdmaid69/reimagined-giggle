e92f7f2a8a6a3eba2386c2de9750ca16c854fa31
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:07:59 +0000

    before note alive any copy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
