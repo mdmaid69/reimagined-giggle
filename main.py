70f42374a89e1795b12de0dcd986db36ef720936
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:54:22 +0000

    conduct physical obvious point forgive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
