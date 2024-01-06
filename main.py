2be6dc61ec82cec7d7b65189e7c5bf54ab861933
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:25:38 +0000

    explain kind guilty thing object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def is_palindrome(s):
-        return s == s[::-1]
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_acceleration(speed, time):
+        return speed / time
