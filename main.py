12cf21533b7da60922359a42014d5388540da346
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:21:53 +0000

    we flow dish action bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
