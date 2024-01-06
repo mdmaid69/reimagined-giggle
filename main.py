30211d56e875f72d69f201a393e50642e78a95b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:32:06 +0000

    somewhere thanks wide pot nose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
