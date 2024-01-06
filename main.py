59d3d70b182b4e0ac4294576329ed9d4a3b804ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:43:43 +0000

    take rain point personal rob

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
