92902b4e64406210a84632a8f002fcc59de6cad0
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:52 +0000

    below fear somewhere dream destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
