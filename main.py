e572370aa76bd3c19869fee19919124978332eae
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:10:23 +0000

    crime dick mistake present round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
