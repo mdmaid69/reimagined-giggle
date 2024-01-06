0d663ce72972154e5265d80ef33e0275c3768884
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:57 +0000

    wish mind public this other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def find_max(numbers):
-        return max(numbers)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
