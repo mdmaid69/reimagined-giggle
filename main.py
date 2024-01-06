2de1941a34d0116f21afad6fb1582a4e56cfe4e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:38:43 +0000

    plenty complicated weather goup careful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
