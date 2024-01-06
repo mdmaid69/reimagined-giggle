3e0d1cdc0612ed166f8560e333e7c3c431d61dcf
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:52:06 +0000

    limit best every order Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import time
+print(time.time())
