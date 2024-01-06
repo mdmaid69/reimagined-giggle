d87300dd4c33ff367110de04c6e05b5abed24366
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:27:52 +0000

    her wide go motion workupto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Words:", len(text.split()))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_list(array):
+        return list(array)
