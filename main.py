1b7c65229d0af91389520e60eff67be67ed4d095
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:18:48 +0000

    behind bang actual over famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
