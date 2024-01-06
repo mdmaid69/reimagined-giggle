ab61d8c6133eeb9ffc2a70855e26c0fb7e53880e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:55:21 +0000

    light present check system polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
