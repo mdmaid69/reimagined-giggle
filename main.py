4cea2633061d0b00c61c9afd7c8034ffb26a5006
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:46:05 +0000

    great sweet rank speech win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
