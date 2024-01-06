99bcd9766124c1e8ae7dda0380080ae83246c44a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:09:06 +0000

    speak life attempt hope makeinto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
