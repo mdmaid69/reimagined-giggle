357277fdba27078951a4ada41f753eed89bb829a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:02:53 +0000

    knowledge plastic imagine improve listen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
