75ee46b0ec6162b79575c7717aedb1ea7c94baf2
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:11:55 +0000

    history nowhere snow cost explanation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
