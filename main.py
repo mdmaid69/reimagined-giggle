2f0879b82e07c556c232a53c747dfefb62217ca6
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:05:24 +0000

    to kick home themselves tap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
