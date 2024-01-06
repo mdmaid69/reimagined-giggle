bddb5a9ceddbb8412c04cc5bde5406b69b81b411
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:47:27 +0000

    rich bad quantity sentence brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
