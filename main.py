ca829b62725b84b56018992109b8dcb72012b8e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:28:26 +0000

    tear government phone hold bite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def get_bytes_from_array(array):
+        return array.tobytes()
