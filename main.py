6497832d4463f2457d30f8d9debd7eb15d284c11
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:43:39 +0000

    round Rr priority relation number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
