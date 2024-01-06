d4cafc069efd30968476ef103528b62fc502d8f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:35:25 +0000

    practise check opposite them punish

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+print(math.pi)
 n = 10
 print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
