0bbec60527eb62e915c392be323535a7c12f5c4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:55:18 +0000

    wealth disagree respect standard base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
