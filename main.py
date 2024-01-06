302ba9ff44b05305f29c7aaaad15645099f29545
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:48 +0000

    571enough owe dick freedom pollution

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
