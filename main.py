616cb62a87d062d2a519db38e958a6a1d2c37d3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:14 +0000

    bunch doubt holdon nose near

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
