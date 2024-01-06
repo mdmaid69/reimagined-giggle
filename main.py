6f90c79864d1265e4f5130e146f41499226f9900
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:30:46 +0000

    bottom nice both doubt talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def multiply_numbers(x, y):
+        return x * y
