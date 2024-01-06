674bef6b6dbbdbd35508f2a416981b2a6ab65829
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:56:44 +0000

    war steep Ii swimming welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
