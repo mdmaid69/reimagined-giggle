47b36127b3f85b48b0fd7342821b133ab81b1d67
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:30:18 +0000

    wonder crowd legal acceptable authority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
