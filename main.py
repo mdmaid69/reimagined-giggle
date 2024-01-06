3cb899b4e12b99267af0c38327bcdcc6291c06db
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:52:25 +0000

    window mad national height listen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def reverse_list(lst):
+        return lst[::-1]
