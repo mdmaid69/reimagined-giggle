b15c017133f3e92c9007a4a56361930137284176
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:45 +0000

    fold stir programme mistake own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
