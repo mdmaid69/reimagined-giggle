dae338f771a2196dca80ae556a39cfa1214e5ef6
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:00:45 +0000

    your Qq home worst film

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
