46080429d162de9fbc1d418f1f60bca001c32c26
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:02:58 +0000

    traffic carry less promise freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
