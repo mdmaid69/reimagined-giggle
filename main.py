53f0a154c2ce47d5336df72d1ac1193f139439ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:08:08 +0000

    disturb fresh thick emergency worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
