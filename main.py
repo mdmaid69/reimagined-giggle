2843dca217264823377b1d86eb84a9844f62149d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:18:45 +0000

    Gg foot lesson set remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
