6a38fa4b311a08fa1840cd8abdc0494d3aee78a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:10:48 +0000

    find read substance sleep usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+print("Hello, world!")
