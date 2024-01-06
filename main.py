24035638510a417076e99ece7940467b250cb13c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:27:20 +0000

    silent correct thinkthrough exit let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
