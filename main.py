5fa896196a4ac4ee5258a4e4bb2b7d4c7f2afa22
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:58:14 +0000

    only traffic ourselves now corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
