77b7387a9a478b93e54221bc3018fd1708a71281
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:53:59 +0000

    man crash whether pretty string

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+from collections import Counter
+print(Counter("hello world"))
