5844c434588712b5fa287879e8b76d9fd36354c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:48:43 +0000

    keen sting fortunately female week

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
