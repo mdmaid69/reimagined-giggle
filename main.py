afb05391fecb21ac77cd064147bd5ee5a08d028f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:44:00 +0000

    flat separate properly load giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
