a22ffef8e545c64e598b265abb858a863aac9cac
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:14:41 +0000

    comein fold nor rain study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
