1426ba233e455420f51cec63d436e3e9b30c7930
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:13:23 +0000

    Ll busy platform holdon song

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
