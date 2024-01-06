cff90ac899a298127ebfc700785ab2189676aca4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:34:37 +0000

    week represent Yy upset ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
