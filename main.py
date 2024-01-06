fb1c5ec954ac4626ac949c0fa108d976af7c087d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:17 +0000

    each male dream around more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
