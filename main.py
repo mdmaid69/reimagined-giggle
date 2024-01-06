e5b1d3b32a1527b09a9d2786e96192db531efe1d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:53:05 +0000

    fit dance speech tap steam

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
