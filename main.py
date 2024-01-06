617f664913a7bfdc95fef579f94303a8617977be
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:32:10 +0000

    promise but considering negative loud

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
