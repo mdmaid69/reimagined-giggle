bd15a7caf5df6dd19fb0b903c3bd6584c428ec65
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:18:55 +0000

    afternoon reach cry visit sweep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
