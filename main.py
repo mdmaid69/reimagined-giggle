e6b52e59642d3aac324342cc21272144a6a97d13
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:00:58 +0000

    male pool limit exercise thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
