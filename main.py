92d03133a4c0ca3d61bf5485d39ab1754a3049c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:43:43 +0000

    hang uponon before fault popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import os
+print(os.getcwd())
