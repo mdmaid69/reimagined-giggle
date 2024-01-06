595f534cc753cee5c428139a74725e364eba1121
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:48:40 +0000

    tight somewhere soak hardly translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
