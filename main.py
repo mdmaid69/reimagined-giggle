39e3b8dc573995fbf6c367f6455d97186cceb195
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:02:33 +0000

    persuade frighten low sister scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def divide_numbers(x, y):
+        return x / y
