dea9e8255e110d55ddd5dfa10c8ed456b499b6d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:12:38 +0000

    trust Vv us make chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
