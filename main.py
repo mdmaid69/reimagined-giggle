b89c4b937704a5f58171eb937a1dae2d989eae15
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:42:45 +0000

    ask exit party on kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_variance(arr):
+        return np.var(arr)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
