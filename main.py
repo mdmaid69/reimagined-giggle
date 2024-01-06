ea25c9fa309190a27495bbf11957e6ae55659299
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:11:18 +0000

    personal used cheque anywhere burst

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
