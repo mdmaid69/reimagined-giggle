23fe791409e4e37180dbc89e4ff12eed2f695773
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:36:27 +0000

    right frighten empty pretty minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def find_max(numbers):
-        return max(numbers)
+def clear_array(array):
+        array *= 0
