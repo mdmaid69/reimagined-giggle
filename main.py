7f4d0e0fa3ca9c86ed40d9a0e1518e7608fa1fa0
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:21 +0000

    through mail surprise normal getback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
