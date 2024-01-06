0f7c8f278ece65c2087973bbf08977ad69294ca3
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:29:13 +0000

    basis send voice sea season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
