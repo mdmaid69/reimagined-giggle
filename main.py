b7dc2eec861c641947bfdcf9325e4a90fc54cfe2
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:39:19 +0000

    estimate Pp shout measure upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
