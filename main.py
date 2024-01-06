740082d4bc88100ba3d6504874ac742f81cca9ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:50:24 +0000

    positive study play bottom niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
