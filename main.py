cc800a2565dce395a96fe9a35bb02d858331a86c
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:12:51 +0000

    switch cold crowd by fat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+print("Hello, world!")
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
