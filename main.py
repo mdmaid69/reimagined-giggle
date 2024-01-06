211687f64e428b5c329559ff0421e99642397a59
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:00:35 +0000

    embarrassed direction cross meanwhile little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
