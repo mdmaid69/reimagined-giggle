347179509ccb7be004eab2fb7f56098989b328d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:46:29 +0000

    tell match image sort guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
