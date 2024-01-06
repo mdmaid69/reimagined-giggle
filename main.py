a0f4c3f1cd31cce3a2b707e84cfef93ac11f586c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:17:45 +0000

    warm jump empty issue difference

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
