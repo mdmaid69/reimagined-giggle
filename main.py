3f176b2c3ba5138494773a48bd32b536da26f87d
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:46:28 +0000

    pupil partner alone afford firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
