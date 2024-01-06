a3b003bf890bff01b64e5c04405a49c314b97f33
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:36:51 +0000

    safety turnout violent underneath clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_float(array):
+        return float(array[0])
