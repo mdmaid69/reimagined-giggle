f200b8503d744b7e5c7b41e8a364fd5fc41b92ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:09:02 +0000

    Mm thumb appeal broad comeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+for i in range(5):
+        print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
