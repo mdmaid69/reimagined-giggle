42c9f1f154210bb103363cfafeaa9fa3411c2953
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:57 +0000

    Kk shape turnon view 567enemy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_user_list():
+        return collections.UserList()
