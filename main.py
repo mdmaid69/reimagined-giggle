88e81eaa7b044dd3e76c13a620e697550e2086dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:34 +0000

    fortunate type important tragedy embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
