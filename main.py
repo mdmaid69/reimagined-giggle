996eac4745708859b38550018e5a9ce57b1b5c1f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:41:33 +0000

    possible comeover worst compete like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
