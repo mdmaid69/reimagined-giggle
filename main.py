5ade90b8110cfceed32e803ba9a520fcb7d11886
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:09:46 +0000

    Yy little cheap report room

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
