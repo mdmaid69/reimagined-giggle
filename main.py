7bf735f00e69610b35d2017491599448dc88d77f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:43:43 +0000

    smart pass everywhere callout effect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
