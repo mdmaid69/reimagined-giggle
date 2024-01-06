cc30bb5a6b97f81f85d61a87256da26b6e918bf4
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:17:15 +0000

    roof no convinced platform arrange

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def add_numbers(x, y):
+        return x + y
