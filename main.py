5a2934c127ee65cc67189ec32266ee34b7ed083e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:40 +0000

    everything quick design day welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_string():
+        return collections.UserString()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
