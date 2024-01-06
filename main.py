d91914fc3563c688936e3ed1275adaeddfe07cfb
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:09:02 +0000

    here expensive fix bin peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
