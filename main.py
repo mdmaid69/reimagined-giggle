1d5c67ca11ccfb36692dc193e9c62f97d404b071
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:30:37 +0000

    expect doubt mind meet Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
