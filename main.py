aa0eaa92c036454c0c15ec309dad74a4dadfd969
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:51:02 +0000

    advise herself age left bend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def add_numbers(a, b):
+        return a + b
