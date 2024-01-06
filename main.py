55e7ce97d951500d6f8cc9af4ccfc20a14a41bb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:06 +0000

    Tt upstairs fresh criticize tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def square_number(x):
-        return x**2
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
