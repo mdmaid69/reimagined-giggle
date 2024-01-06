89a7ae5e13fa849f73158b92754a4fb5da510620
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:41:56 +0000

    hollow mystery see software flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
