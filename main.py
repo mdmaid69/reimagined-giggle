33a5e78f0336d1da2c64238c47d7544a9eacfa37
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:39:45 +0000

    collect imagination thank bush ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
