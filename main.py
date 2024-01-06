e6baf94dd3f59d010552fb6793e6cfae979dbfb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:10:19 +0000

    land mean suggestion band important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
