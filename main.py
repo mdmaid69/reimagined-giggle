c209009eb1634dd25acd5e0794938cf66985ecf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:01:32 +0000

    nerve appeal company turnover basic

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
