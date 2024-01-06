8640e73d72e526da4aef4be36de657ee41083e6c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:26:24 +0000

    centre finish hit temperature through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
