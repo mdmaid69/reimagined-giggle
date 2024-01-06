52c6ee6803f222d414d28ed6a943858c502135e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:56:31 +0000

    pretty ourselves combine hair advise

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
