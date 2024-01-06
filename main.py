63178ff1956797b7ffb70140b6b3a66dc351875c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:30:57 +0000

    fashion bag related famous charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
