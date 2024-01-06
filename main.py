e76e26a34d44a168e39156ac98ebf90c0d94c008
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:12:05 +0000

    brush squash case slowly sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
