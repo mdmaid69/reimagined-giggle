3c3c066b16dd4a60c282432b82dcfcfc83bb9c6c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:28:33 +0000

    brave coat class passenger main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_string(array):
+        return array.tostring()
