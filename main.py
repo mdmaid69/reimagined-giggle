d081c5743db3b7ae9aa68faa5aa85f539b0aca99
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:36:29 +0000

    dangerous error thank meaning final

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def square_number(x):
-        return x**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+print(random.randint(0, 100))
