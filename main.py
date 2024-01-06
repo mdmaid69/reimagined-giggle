9adc998356630d2a0942fb5002fb850ca814e11e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:20:26 +0000

    taste worry shut reply circle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
 def find_min(numbers):
         return min(numbers)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
