7e094ba4ac7f0133fecf41682451bc6eb4841912
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:06 +0000

    pay disagree loud 570enjoyable consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
