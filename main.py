d486b145a23bba544c4ac70ea34a492ba8038325
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:02:53 +0000

    uncertain fasten guard sit colour

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
