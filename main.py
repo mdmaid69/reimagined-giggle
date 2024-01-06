a9de8ade3c6261b60af789fca8792aa37fcd6e25
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:21:14 +0000

    secret dress feel our experiment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(lst):
-        return min(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
