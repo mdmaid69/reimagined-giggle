ea479e22e5ed0478743147b33611680d9e7494bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:58 +0000

    rub mood word become appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
