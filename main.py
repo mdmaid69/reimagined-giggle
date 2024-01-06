53b1c2576b11daf32db05b71d752083c4d8a7ea1
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:44:26 +0000

    attach confident evening opposite pleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
