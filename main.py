21dbcb096c557c30cfb8a2f5cade585c286fbe1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:26:48 +0000

    stir occasional shut consult left

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
