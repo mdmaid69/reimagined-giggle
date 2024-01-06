4c0783e8ed5541c572c9c8b6c8059be7bcb2c53b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:48:30 +0000

    sister beat week balance breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
