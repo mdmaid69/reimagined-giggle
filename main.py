5a8cc88430d08d1c8951287c0440b922dcf3752d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:56:02 +0000

    Zz neat spill country fond

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
