4cfc9ae57a5f3c5291b708f0802b6de237e3aab5
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:56:01 +0000

    little day decision accept search

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
