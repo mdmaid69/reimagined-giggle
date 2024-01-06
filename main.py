995d2979b667f3047ee7df4d1a9b32b46c359bb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:32:34 +0000

    film birth prevent practice phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
