45011c5d0f6ed92d1354b1153547d0c787b8b558
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:37:44 +0000

    drawing themselves seat plan unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_error_function(x):
+        return math.erf(x)
