3144caf907fa43d89d18a8ff8a316c93d98e2c6c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:52:52 +0000

    treat come release welcome fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
