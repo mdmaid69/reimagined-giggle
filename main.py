44f523aaa20a361cb9ba85bf89cbe3acd3b0a59b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:21:59 +0000

    machine recent sister past human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
