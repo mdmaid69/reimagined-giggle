bb2a3f0504cbb60ada2240863d43148f2a642d71
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:34 +0000

    like judge great question stick

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def convert_to_octal(n):
+        return oct(n)
