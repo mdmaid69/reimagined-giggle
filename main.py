174a3d3541fa6b08a6f4341ea862e26dda54b7b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:47:40 +0000

    everyone point true love proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
