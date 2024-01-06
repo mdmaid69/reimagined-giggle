b54bf2a5b0d2188ff5c3961c46ec01ee9a44b415
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:12:12 +0000

    lesson calloff cry pretty zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
