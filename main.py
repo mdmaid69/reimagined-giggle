21877ceafa111b65049d4fa1d230a28462df60ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:46:58 +0000

    exhibition line surprise exist better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
