48f9c6af7cd95a15d853b2f5500e98ea6927ead5
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:23:21 +0000

    traffic concentration breakinto knowledge power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
