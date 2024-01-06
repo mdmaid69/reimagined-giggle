cb637be7bad15ec655c03adec07c5ac906129a8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:28:52 +0000

    equal break paper preparation soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
