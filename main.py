0488b034e255f73003a719339652e16a37f7eae6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:00:08 +0000

    innocent common food unable several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
