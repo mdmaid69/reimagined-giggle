fae9ba69619eb7debda9c15503f8efb8432fb4cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:13 +0000

    measure opposite direct comment emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+print(random.randint(0, 100))
