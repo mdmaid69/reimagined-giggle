b8091ed93f4e746918700371a8cabdde34b2021c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:09:50 +0000

    round concerned getout poor standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_base_name(path):
+        return os.path.basename(path)
