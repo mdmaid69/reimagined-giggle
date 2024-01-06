cd6d428160083189d1123d07e766a51ba7fbe97e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:43:55 +0000

    fond flight mention result afraid

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-for i in range(10): print(i)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
