3cc3fef5b4e65db904a96de20ba2b0a3a21ba547
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:39:34 +0000

    past store trap legal why

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
