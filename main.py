f012237a62e53baf0792d253fe6fad57c1d1f213
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:56:30 +0000

    invitation somewhere single approve mainly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
