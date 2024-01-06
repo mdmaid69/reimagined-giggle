83bc99e4307210d2f844671e021aa5bbf88707f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:21:27 +0000

    tidy smile without meaning so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
