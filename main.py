9ce6d1ba226650fab8fe7f985140e7231f381ab0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:21:47 +0000

    fire far will risk influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
