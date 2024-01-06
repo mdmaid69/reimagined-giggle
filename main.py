105f554d78a25299abf7d4312c6c2dd98954695e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:06:17 +0000

    true passenger demonstrate full always

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_work(force, distance):
+        return force * distance
