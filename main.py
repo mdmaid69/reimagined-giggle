4bfc3b9e00ed9664a89db1923593e0d1fc5ef11a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:04:19 +0000

    twist exercise know about worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
