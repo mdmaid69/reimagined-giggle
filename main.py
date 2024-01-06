65a5eaee9fa6570ad23a62c2f806a81c8e38bb80
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:13:03 +0000

    contact completely same day press

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
