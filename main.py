5f5a1ffcfdf403cfabaa95659b623eeb81aab31b
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:22:25 +0000

    wet state doubt pretty deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
