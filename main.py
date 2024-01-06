ede79a740583f149427ab0f321056f1a2315e68e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:09:34 +0000

    really mine our false thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_work(force, distance):
+        return force * distance
