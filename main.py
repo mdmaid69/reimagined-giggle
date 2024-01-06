540c9153a1e5ec84b68766d1485da88007a5faa3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:31 +0000

    bang record dance after if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
