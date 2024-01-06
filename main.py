4a44fccbbed70ae81f8a6d8ccf43635eafde17b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:11:27 +0000

    offence party just first gradually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
