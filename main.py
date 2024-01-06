7261060252caa078edab64ab91072748f9dca3bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:56:50 +0000

    speak pressure married yet getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
