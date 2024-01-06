85748cabc6c12ebf50716bf89f3565a171cfae5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:13:34 +0000

    calloff likely needle runover interview

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_power(work, time):
+        return work / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
