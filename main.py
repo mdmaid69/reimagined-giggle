f6dc7f735df6768bb72f5c7c8414c0641068f9dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:16:23 +0000

    best difficult mouth anywayanyhow combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import time
+print(time.time())
