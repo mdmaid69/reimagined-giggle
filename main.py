043f870bb10345be53b184a53b4a23c55b0824dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:24:52 +0000

    doctor avoid surprised evening narrow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
