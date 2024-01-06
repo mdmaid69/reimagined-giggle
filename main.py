1acb0f15f9224383f739715189b3e0f01ae83387
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:18:00 +0000

    confident angry butter set advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def remove_directory(path):
+        os.rmdir(path)
