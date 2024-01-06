c84c7c263be0782087e7a29278680797a0dfbfd4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:47 +0000

    flat main owe copy move

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_volume(length, width, height):
+        return length * width * height
+def count_elements(lst):
+        return len(lst)
