c38a8ebcebf05badd52df8ec866f2e021d0e0c60
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:56:18 +0000

    movement curious contain emotion Kk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
