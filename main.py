8b6c4a9e03a45293cd4e92bd5c0d59cd6f3f98ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:54:37 +0000

    legal cinema wherever style strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
