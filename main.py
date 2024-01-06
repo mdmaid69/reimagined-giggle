676885e76fc351e11c292099ffef44a77a0af9b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:35:02 +0000

    the which their achievement fence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_acceleration(speed, time):
-        return speed / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
