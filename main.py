0232d8a2ee53d848d583a16f2f997561b0e93d03
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:35:29 +0000

    crash example wrong offence software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
