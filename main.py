e6ad735b71b519b5e0e6628eb55766c46140b89b
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:35:26 +0000

    mistake right ashamed brush wrap

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
