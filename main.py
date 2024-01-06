700ba9b3d79c6232a5530a2ada45f19320b33afe
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:40:27 +0000

    instant damage famous control complaint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
