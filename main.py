95c81e74188072a0bda673bb2fa6bb119c15b798
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:55:36 +0000

    until voice later this rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
