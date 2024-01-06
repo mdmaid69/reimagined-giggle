2c095e3915385eaaf10f6d1b24ea8270db385cdf
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:28:38 +0000

    rush deaf description comeover arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
 import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
