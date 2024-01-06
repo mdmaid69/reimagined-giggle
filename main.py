221a6c6eceb52108e6a5708fc2fd32322ae009e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:26:48 +0000

    street luck claim to sting

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
