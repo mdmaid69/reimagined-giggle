c73beb1fe7e7d8f72597787227d82415a29e5671
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:55 +0000

    aim quantity develop father wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
