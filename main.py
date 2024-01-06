6be74669829c6ccd82cec0156649b78a5f295171
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:32:20 +0000

    decision deal owe heat fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sys
+def exit_program():
+        sys.exit()
