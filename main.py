9c60e49d845310dd3919854665e2f907d8edf05a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:08:38 +0000

    takeback both reaction sharp total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Reversed:", text[::-1])
