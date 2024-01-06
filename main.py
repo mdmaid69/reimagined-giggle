1794e5b486717dd5c55d75d02c48af8176f1920f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:17 +0000

    grateful except vanish claim dig

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import json
+print(json.dumps({"name": "John", "age": 30}))
