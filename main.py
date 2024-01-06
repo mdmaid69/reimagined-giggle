ec6a08cebddfb65e6de26f8f71dc0fe08315a3f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:23:15 +0000

    spin base hour fair change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def is_odd(n):
+        return n % 2 != 0
+def add_numbers(x, y):
+        return x + y
