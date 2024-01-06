5a9b16178e5a572c03d69d83c692022f51ae160a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:11:09 +0000

    frighten Nn turn arm honest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_power(work, time):
-        return work / time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
