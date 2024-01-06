44bf74f402bde443f52016c19f7a1ec79e511494
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:12:57 +0000

    date figure body bringforward wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
