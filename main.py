bdf1e2ea2694dc704f4b4e0b095b28940cd7f266
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:20:36 +0000

    traffic pretend murder twist under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
