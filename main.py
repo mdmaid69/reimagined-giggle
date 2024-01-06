094dd6d861adfa748b4b5be3a3560c07e72f02e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:13 +0000

    someone light half today argue

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def convert_to_hex(n):
-        return hex(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
