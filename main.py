dc230185991e06a58935770e53b4afc8c728c166
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:35:16 +0000

    fence appear miss clear tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
