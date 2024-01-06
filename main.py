6ef8744b2bdaabf94c297830ce685e1b225b5548
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:16:10 +0000

    immediately buy flavour getout stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
