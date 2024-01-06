f02ece08d5f68998910e568d85f78ca249f8742b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:55:00 +0000

    light clothes the table exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
