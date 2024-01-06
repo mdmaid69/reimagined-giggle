fa726b9c7a1dd768b7a3a36cd5b26dc59f3ea7ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:26:17 +0000

    show edge deep cook claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def get_array_as_float(array):
+        return float(array[0])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
