26f3fd8a113202e5c86fa9ed207e8e57cec2d9e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:29 +0000

    juice chin to considering impressed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def divide_numbers(x, y):
-        return x / y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
