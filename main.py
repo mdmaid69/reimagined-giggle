32d48f9cab10e809500936c962fecba7054452eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:31:31 +0000

    point neck even down admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def add_numbers(a, b):
+        return a + b
