e6f99bc3df59f09a10938123d5edee56ff4ccb0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:40:51 +0000

    564encourage meaning as bill thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def get_array_item(array, i):
+        return array[i]
