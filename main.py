229594bcf61be6cc1055692b4ec961785a6d4ff5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:58:43 +0000

    underneath upset convincing peaceful board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(5):
-        print(i)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
