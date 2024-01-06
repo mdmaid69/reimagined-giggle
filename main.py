f0ba25a4b62beab3174cacfa36bac90effd2e898
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:55:06 +0000

    bringup exchange getback choose war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
