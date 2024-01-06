ecff6139e4dc5a4bd86cb3c664d89db9a65dc90a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:46:48 +0000

    scene judge fear cheap nigga

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
