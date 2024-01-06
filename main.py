51a201ca48bff1edc15d55d81c0a2dcd8e716b2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:54:33 +0000

    among stand principle alone importance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+def convert_to_json(data):
+        return json.dumps(data)
