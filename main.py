5c75e92aae4c5c26f6bece99e77ed9e76dd24f6c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:28:28 +0000

    left recently company declaration food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def reverse_array(array):
+        array.reverse()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
