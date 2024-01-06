cc790674587d93d780e857ed701f894315b871f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:33:56 +0000

    attempt emergency wear declaration family

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
