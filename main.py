ba155b82139854b6d238dcb336c8da49b8e5541f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:47:13 +0000

    consider that opposite raise responsibility

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
