b214d78eb0d1908e09523499cabd4c5250b979df
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:49:07 +0000

    dead empty performance position agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_int(array):
+        return int(array[0])
