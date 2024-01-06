559f599759cec681f7debdb44fa706833a18991c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:01:43 +0000

    rain police under underneath godown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+print(os.getcwd())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
