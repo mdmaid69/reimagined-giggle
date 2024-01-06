f2d0a4a701a1ce45803954c26078fc1bf6f2a1b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:01:30 +0000

    firm same dark realize free

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
