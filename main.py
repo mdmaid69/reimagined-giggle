54f6e064901972a58199a2360c33a62f388a4c29
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:46:52 +0000

    embarrassed tradition proud pure something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
