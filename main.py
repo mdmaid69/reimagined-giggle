e0dcc029eec4288f8df72cf40182b8318619cfb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:00:26 +0000

    I exercise rank wake above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
