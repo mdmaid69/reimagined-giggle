d0afb9f9318c29c153f8db8275a1a7d5ab8408f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:34:47 +0000

    fire complicated fortunately exist certainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
