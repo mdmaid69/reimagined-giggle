2800359aa8c511c54e90d26a8effad7db22a9541
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:09 +0000

    name medium brain clear boat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
