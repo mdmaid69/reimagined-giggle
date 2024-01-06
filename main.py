e671929fecbd1930d2cb63372ce31bb3d89e6872
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:58:59 +0000

    criticize subject day record heat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
