25195dd07ebf1d316fe835c705347247709f082a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:30 +0000

    improve extraordinary impressed runinto care

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
