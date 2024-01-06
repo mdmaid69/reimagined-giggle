aee426ebf3957379249e7461eaff57ece89855ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:03:17 +0000

    basic natural question Qq lift

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+print(sum(range(10)))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
