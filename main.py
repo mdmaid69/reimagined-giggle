9e78610ebdb3afe5843f220fccaa56ce66ad1a09
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:53:50 +0000

    pay dick legal 573entertainment boil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def sort_list(lst):
+        return sorted(lst)
