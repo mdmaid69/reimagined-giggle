f8ce24ed3ef03b5a8a6b7f6c0f389fa7caa2d02c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:25:34 +0000

    remain that conduct thin rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
