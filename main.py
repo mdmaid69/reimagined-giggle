d97a36094127f4b2fee553dfa56f43c17edc582a
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:17:26 +0000

    strict television present ability valuable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
