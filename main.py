f5fad22af30c0cbe8d172544a5327604c275b569
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:46:14 +0000

    contact snow dark national fail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_directory():
+        return os.getcwd()
+for i in range(10): print(i)
