a50fefdbb134823a9157d9bf9be1eb61455617ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:40:49 +0000

    shock time concerning machine naturally

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def reverse_list(lst):
+        return lst[::-1]
