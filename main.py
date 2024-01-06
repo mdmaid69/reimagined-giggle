fd7860f614e21afe033886872f1d04d051fc8feb
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:22:50 +0000

    side emphasis there thinkover recovery

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
