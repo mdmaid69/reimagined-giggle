22a4ed113ce78739bce59be4bb6087ea25056f9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:11:00 +0000

    you innocent draw giveback scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
