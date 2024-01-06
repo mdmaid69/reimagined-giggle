659ad1ad23782b96b5a8da27dd4dfda43eb855a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:51:17 +0000

    shoulder female wire classroom neither

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def clear_array(array):
-        array *= 0
+for i in range(5):
+        print(i)
+def count_elements(lst):
+        return len(lst)
