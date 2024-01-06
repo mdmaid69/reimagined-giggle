fc48822a6ca9a54f159479f4352cb4aabc28ec4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:18:00 +0000

    region hope important interruption try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
