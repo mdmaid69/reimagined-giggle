78e0e27b611c514e9f75943330704465e3e60bab
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:50:53 +0000

    thinkover powerful earn unite tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
