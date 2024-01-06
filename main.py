7f0f9e3043cf787f9c4aa95025007da67afb2a33
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:45:41 +0000

    lost recognize heat dress breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
