baa34ebbf1aec9b07e32fe1037f775d084cddc5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:16:08 +0000

    arrive threaten ball belong mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
