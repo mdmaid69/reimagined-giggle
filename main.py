43c8f7ea88482eebe8913bc0ef40673461e02c61
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:12 +0000

    complete woman deal so guard

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def sort_list(lst):
+        return sorted(lst)
