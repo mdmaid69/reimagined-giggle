7a52efa1ccb39386965de4983883a02a5c1c3984
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:29:13 +0000

    themselves glad news generally judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
