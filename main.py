acc99baf361bc8b30fc0c130181736d39a78ce45
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:54:30 +0000

    desperate shout alive appearance sister

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+for i in range(10): print(i)
