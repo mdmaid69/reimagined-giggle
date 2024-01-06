a8305fbbf5a1fdb20597b65211cea287810533b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:12:07 +0000

    thumb concentration ground set fold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def square_number(x):
+        return x**2
