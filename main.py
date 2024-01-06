0d31c2db80a2b1371e639e44839f44b907f1e659
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:55:27 +0000

    small and pain lucky stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
