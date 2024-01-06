85a75e931c2e7f33d5d9a07cdae4764e83a258a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:37:41 +0000

    without print habit mine tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
