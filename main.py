0e5cbfd1f7f430f57683618e1aeca683ab940cc2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:50:27 +0000

    my involve release flat contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
