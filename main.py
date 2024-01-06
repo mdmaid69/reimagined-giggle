363194e5c49f48d5dfb4fab28638ee39f313133d
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:39:55 +0000

    179bite touch total neither tomorrow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
