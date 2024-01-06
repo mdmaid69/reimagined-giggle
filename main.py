3a9d95f3c1003e25ed6f0c66ed78e08e60979505
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:01:58 +0000

    comment tradition positive bridge impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
