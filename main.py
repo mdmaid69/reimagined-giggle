e9d21bf835957598e64795088d8e71c49490e1eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:06:02 +0000

    defeat terrible sound lost name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def get_array_as_int(array):
+        return int(array[0])
