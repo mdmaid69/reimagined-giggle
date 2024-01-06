81de1b867d3a011ab39ee680b3f44462b247a421
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:02:02 +0000

    list bus workon main voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+print(sys.version)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
