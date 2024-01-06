33c19ee82707d322bdeb70e752c9fca9bb8b6aff
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:21:42 +0000

    whatever rough frame call bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+x = 10
+y = 20
+print("Sum:", x + y)
