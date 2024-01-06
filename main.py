41ec2260bd893dde960726d2bf13a6031495adc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:40:04 +0000

    breakfast collect slide mend glass

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
