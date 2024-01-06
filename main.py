96c25b1bb31b399daaeb47861517e6f6677dbe94
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:37:16 +0000

    man permission seem today steep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
