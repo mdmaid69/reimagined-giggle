c9f2264149d9184d72a9e6695dfd069a7aa0a74c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:51:50 +0000

    Rr dream street cost by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def subtract_numbers(x, y):
+        return x - y
