cc6e7c838be7822998bfd4a5859c7a7f4b99335b
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:19:11 +0000

    love bush object spread pressure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Reversed:", text[::-1])
