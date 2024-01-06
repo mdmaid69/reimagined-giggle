c185e06c126e78074da1294efb3f273f528c972b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:03:54 +0000

    behind like busy breath talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
