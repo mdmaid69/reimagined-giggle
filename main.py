270249e38eb2f6590e0bc9e6558346f62becb71c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:26:58 +0000

    Itself care style benefit ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
