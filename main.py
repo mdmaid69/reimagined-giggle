d16e86a49920e2534f58583680b0545793eac622
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:54:38 +0000

    everywhere score freeze clearly measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
