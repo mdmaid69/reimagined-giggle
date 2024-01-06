25e6332d179ef2e458419e7e1bf6d7d5e539ef1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:39:41 +0000

    nor live corner between university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_force(mass, acceleration):
+        return mass * acceleration
