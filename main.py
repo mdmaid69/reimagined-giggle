03672325267ddf340c2da31c11a90b3f67cdfd30
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:13:01 +0000

    unlucky stay regard practice worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
