6addb5b1310dee2f5470dc07cd9248a0de18d5a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:07:47 +0000

    practise capital answer brush improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
