853629cddff98946536b58e75069cabfb45de98e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:34:37 +0000

    excuse feeling tired inform even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_str(array):
+        return str(array)
