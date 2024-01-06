99e1315c0193add16e2c3ebe70f8508841905e50
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:35:48 +0000

    clothes improvement pause throw complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
