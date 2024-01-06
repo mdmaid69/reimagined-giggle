8145a11a6dac6c8c65cd8b4b2522de47d73964d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:44 +0000

    exit discussion aim goout level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
