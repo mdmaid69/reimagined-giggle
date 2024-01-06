b14a69ff6cc9e22ba9a0f5ecd0403819193cc51a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:42:51 +0000

    appearance pleased land everywhere swallow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
