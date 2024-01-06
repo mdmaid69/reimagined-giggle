4cd6588d1f5acd89a5e21b00867811c526bf1ee6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:55 +0000

    lock cheek push far meet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
+import random
+print(random.randint(0, 100))
