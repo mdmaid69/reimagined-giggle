58619e3a02a4c288fc7928c33108585a2ff044cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:25:47 +0000

    this proud appearance mend peace

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
