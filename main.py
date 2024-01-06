06a3b0b5f98a9774fb09400820c77577ea869689
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:17 +0000

    179bite trousers development plate flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
