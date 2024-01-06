2da9e8a047ae371bee36baf1f9025c9ec2f0fc85
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:40:42 +0000

    lost moment nearly tap punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
