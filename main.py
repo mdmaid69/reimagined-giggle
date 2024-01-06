b9e9b91c28ef90bb374efa7dbc92413faf13adcc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:24:14 +0000

    attach line sight immediate work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
