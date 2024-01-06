ecebaa7ed39b72eea5621f77c169992273e01e14
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:27:54 +0000

    month ilovedicks software apart commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
