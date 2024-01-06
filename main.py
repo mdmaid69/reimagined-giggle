0f2cc027e8ffb17724692b94d3d4b56868c100c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:04 +0000

    young period murder confused wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
