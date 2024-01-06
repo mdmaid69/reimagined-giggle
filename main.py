cd286b4bc2efa16b4566941253ff25f60e0a5dd9
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:43:44 +0000

    check possibly most be country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
