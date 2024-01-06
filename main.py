d7c4fb060fd3f8bddc4541df469744cf80a1e043
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:50:12 +0000

    label seriously practical table tone

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
