5cf5154084c42d43a32384991f2f96608046a105
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:35:14 +0000

    dead react dangerous cruel calm

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import logging
-def log_message(message):
-        logging.info(message)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
