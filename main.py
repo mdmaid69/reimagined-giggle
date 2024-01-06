34fcbc1134dc7f3aa8948f7005862ea5ebc0b475
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:14:01 +0000

    story long proud finger price

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def get_array_as_frozenset(array):
+        return frozenset(array)
