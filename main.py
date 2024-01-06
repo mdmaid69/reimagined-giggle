9fc59e204899c819773f4048493719103cd8822d
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:58:29 +0000

    welcome treat reach history nearly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
