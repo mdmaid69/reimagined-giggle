9e6e9b1ff88ed208c93922ab3fdd524503d37642
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:04:23 +0000

    giveout meal wall our as

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def split_path(path):
+        return os.path.split(path)
