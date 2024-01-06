44b9b19af70603b0cd7a021833b3c46e6361c88a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:09:29 +0000

    entrance guilty national target suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
