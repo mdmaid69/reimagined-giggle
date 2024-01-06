1f5a2dd4410fe2826630baf3be56fbc2bd24a254
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:48:02 +0000

    taste godown guilty Yy message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import numpy as np
+print(np.array([1, 2, 3]))
