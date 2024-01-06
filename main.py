3519a0e0226870a7b900822fbaf516ee3d7330af
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:37:42 +0000

    recover spin dare crime flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
