e1c60764d1ddd04f22ab2d059c98ad5042e3106d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:32:11 +0000

    rich contents family lookup over

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
