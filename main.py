0697cf5591a8f7591198fd0aaa1ad281d3986df9
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:56 +0000

    hardly sink workout beside fortunate

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-print(sum(range(10)))
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
