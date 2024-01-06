0b7f98e884190c4136739e22f4c89481d2a31710
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:55:02 +0000

    cut break move talk even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_speed(distance, time):
+        return distance / time
