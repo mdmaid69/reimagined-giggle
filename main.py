e775969bd3733981f9aa1b0d31db40a0612b9287
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:38:37 +0000

    history lot organization before improvement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+name = "Python"
+print("Hello,", name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
