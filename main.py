d353ea8bcd0822b77b739d79050568ad5b3c515b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:07:36 +0000

    information runinto environment lookback(on) or

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
