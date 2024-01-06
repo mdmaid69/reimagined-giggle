fa9e60f59495f2621a66e3d41cc3dfc092ff21cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:08:45 +0000

    protest proper urgent scheme thorough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
