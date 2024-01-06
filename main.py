2c12c481cf4693fdf1a5f699f3bf13afa04d0dcd
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:09:43 +0000

    pour pride please compete means

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
