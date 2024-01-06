919747510d635f357070a8ddb0c680332c3e049f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:27:32 +0000

    criticize break bus entrance value

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
