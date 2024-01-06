f820f4a0a639c46e30e2a4d300c2150cc09e6232
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:59:07 +0000

    happen pronounce after Yy loss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
