3d68b8b298196cadf6480775b2a859395c8f9a17
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:15:02 +0000

    float careless himself past punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
