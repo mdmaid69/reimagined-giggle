e691d2b6c3334c9d1398c3a47152d85fc5e39368
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:21:48 +0000

    modern day convincing question mine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
