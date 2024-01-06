6d8b264a32cf0d997a02fe06a4d85c0fc7b41050
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:58:46 +0000

    feature mysterious mind nearly system

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def convert_to_octal(n):
+        return oct(n)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
