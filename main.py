c2ec8ed2a93e5f4f6a62856b5eb40dd408c11a21
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:44:54 +0000

    advise ought expensive declaration Nn

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def sort_list(lst):
+        return sorted(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
