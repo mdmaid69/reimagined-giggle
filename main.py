e9dd33e252ef749cc527f85f2a99d79bd1c007f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:01:45 +0000

    support land remove earth after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
