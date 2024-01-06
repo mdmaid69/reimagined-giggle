42e9de2c9efafd1914d387fad300db760511b93c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:59:57 +0000

    follow inside plenty rate tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
