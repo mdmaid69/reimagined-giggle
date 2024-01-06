4c9061134699d331af33b9616e74dd36be42813c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:06:20 +0000

    unfortunate increase use over cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_stack():
-        return collections.deque()
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
