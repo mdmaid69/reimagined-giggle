440f0f6e48de59efe3eb4c70401fa6f6e45b93b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:34:19 +0000

    change protest brush spend embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_speed(distance, time):
-        return distance / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
