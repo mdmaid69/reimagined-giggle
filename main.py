21ef315768e79ae9ca1f7971fb740c27a103f7cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:11:41 +0000

    if keep spend scare nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
