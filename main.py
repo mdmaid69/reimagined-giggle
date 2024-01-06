e900084fc7bc62a03c90ee8b866309cbf028edb1
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:09:18 +0000

    start whisper winter block afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
