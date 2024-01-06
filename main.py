52dfb38847220f97b251b7ed7bca0d7b3bbb068e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:24:33 +0000

    side cross since minimum now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
