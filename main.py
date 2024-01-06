045cff8285c94f381b16739859a0b4dfb7c639cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:02:44 +0000

    like authority water through hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
