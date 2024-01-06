945c3e4aec34cbcd7733a1d4f83cfe2c705d9d20
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:26:32 +0000

    exciting cut deep delight card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
