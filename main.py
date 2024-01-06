1f4b12217d916f04a787b4d683f8e9d756e08b8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:16:31 +0000

    try about persuade tall callup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
