05fff74dfbec5d77bde27e9e4a9ea88dae390632
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:46:06 +0000

    occasional stop name music every

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def reverse_array(array):
-        array.reverse()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
