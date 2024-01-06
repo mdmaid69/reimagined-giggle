0170f4ba2682897233365f983822acb87578dc6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:21 +0000

    calloff glad bet deal over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def remove_duplicates(lst):
-        return list(set(lst))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
