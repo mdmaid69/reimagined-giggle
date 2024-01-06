94cbd6a1e30e8b91eff41085e92c94901d84eb68
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:00 +0000

    bear person recognize today kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_typecode(array):
+        return array.typecode
+def calculate_speed(distance, time):
+        return distance / time
