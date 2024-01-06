09439165f0621d33f2b521d68b6b0375af117a7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:09:20 +0000

    hair twist male charge sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
