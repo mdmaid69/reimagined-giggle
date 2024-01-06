f86a7f71d65844b6988f1d1085a33ebb24e31c25
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:50:41 +0000

    manage into fire roof admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
