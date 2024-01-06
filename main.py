07a380e8db70766942a2ba0390f565216ac7ffcb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:27:14 +0000

    factory complete dream effort wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
