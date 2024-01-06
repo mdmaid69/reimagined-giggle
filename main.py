a5eff863af9f2d77e18a8ce62df0cc818c26fb62
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:49 +0000

    aim rob exaggerate sister nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
