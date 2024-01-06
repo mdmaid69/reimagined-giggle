5ac4ec2a49c72924bac9048b70efaa3be56ea18d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:13:58 +0000

    lower scream clearly feel replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
