d2e9a8abfdea004a8059a18a75ee43ceeed1784a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:17:42 +0000

    turnup complete comeback congratulate error

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
