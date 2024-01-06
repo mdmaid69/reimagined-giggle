05442c0698c58161b90a880acb24587a09138e60
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:21:30 +0000

    suit study 565end kind broad

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
