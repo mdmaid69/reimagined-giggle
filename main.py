fef3f18ccd86f7d8dd68d7844d85ea1146700d58
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:20:27 +0000

    lookaround tell help though extremely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+print(time.time())
