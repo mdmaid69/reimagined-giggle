5fc8c3386a76bada6c61ff233a935eb092305c50
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:03:32 +0000

    breakinto invite not slightly watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
