180ec3c701fa972e72f5a4ec887b112ebf245fe5
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:49:43 +0000

    blame stir difference tight will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
