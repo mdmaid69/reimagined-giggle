05c119775d414041486f7d5ea403cc49b3247a04
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:31:39 +0000

    reserve least hard dare Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
