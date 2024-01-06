985a32ec0827a7079231cb8bd7fe58ae4d03a177
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:28 +0000

    cheat cut design remember determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
