a77b69d84e850701d67279cb18e4da8db92af7de
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:15:46 +0000

    of well measure delight various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
