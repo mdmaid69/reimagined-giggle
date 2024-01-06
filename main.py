d8070fbd686360c5ca11b6cab28e3196fe2b265b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:09:04 +0000

    step entry lead glass bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
