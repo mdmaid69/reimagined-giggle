40100fc2fcb469fb772ddd3485389294f001277e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:59:55 +0000

    unlucky defeat purpose moment angry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
