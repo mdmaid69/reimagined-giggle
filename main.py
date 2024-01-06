6027259ee9f02bd784e6ca7722f908056d67aa50
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:57:34 +0000

    cool shadow point turn thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-print(os.getcwd())
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
