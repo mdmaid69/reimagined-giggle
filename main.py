f97070a94773f92e5bb9a84f40e23ba6f7db4d7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:30:46 +0000

    however stone home 564encourage develop

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import sys
+print(sys.version)
