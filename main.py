8664486a2c425bd88e886efeb1704bc23237409f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:15:01 +0000

    give quality watch think professional

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import sys
+  def get_python_version():
+        return sys.version
