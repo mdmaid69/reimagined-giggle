3b3564b7641677f0767c50d52d5c8422a2cd1fb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:46:55 +0000

    train makeup discuss before centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
