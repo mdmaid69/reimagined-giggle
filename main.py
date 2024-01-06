77e7d18c3879129e4461966230954778497caa19
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:12:51 +0000

    instrument project dance tie cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
