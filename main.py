6623c7cdb73673d4982bb559834d1d405befb607
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:59:46 +0000

    goon phone closely squeeze information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_sine(x):
+        return math.sin(x)
