2f1114fa953cc8b9ae1e1ebe260e660fc88ba92c
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:24:05 +0000

    centre heat everybody store contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import sys
+print(sys.version)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
