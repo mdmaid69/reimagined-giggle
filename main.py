1700c9a0ee86ea3f0d036b0b6c5687a60a513d5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:41:26 +0000

    past round have bet rub

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
