10447bd9b329a0aaf08e9c90e46c4f65db6fda09
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:01:11 +0000

    worry anywayanyhow phone soft soil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
