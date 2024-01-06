fa3ed3a72825dddba089204f631486251460920c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:31:07 +0000

    quantity nerve contact choice punishment

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
