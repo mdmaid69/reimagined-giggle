5385e65ead8d8c24ceb56c193f531173a6c093fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:02:36 +0000

    godown slightly door famous month

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def count_words(sentence):
+        return len(sentence.split())
+def reverse_string(s):
+        return s[::-1]
