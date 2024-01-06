3b360e89df107914c1d76ecf7c71214e1d59a023
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:50:16 +0000

    lead mark wherever regret only

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        return sys.version
