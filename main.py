629ba57a15d7174323bc3ea61ef71351f13b61b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:11:54 +0000

    occur care manner single trip

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
