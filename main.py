04c84330ca2cbce663553347d1c36ba4efd538c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:20:13 +0000

    save idea image to centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
