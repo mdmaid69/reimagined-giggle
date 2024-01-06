1dcec9bf3b694af5d1b3e83d008469ef4b36b668
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:18:39 +0000

    goaway middle breathe later threaten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
