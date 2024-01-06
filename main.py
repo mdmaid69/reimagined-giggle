48f7423c5bb2fc90c5fd738058600ae9e8ff899e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:21:12 +0000

    improvement bunch music fault keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
