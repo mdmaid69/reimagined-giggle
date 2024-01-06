f3a53fe0e329dd1413069d299ea22270cf52a271
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:01:39 +0000

    succeed workout insist go calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
