34e1b3f04da9936b7556285a6745b851d0cee3a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:41 +0000

    heat purpose class market adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
