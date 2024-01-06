4af93668e27cf545053ff8930061f0ab824f395e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:13:47 +0000

    border hang cool Dd difficult

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
 import array
-def get_array_as_list(array):
-        return list(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
