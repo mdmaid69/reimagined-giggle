46a15fe355cf5bd9e549e309f4707cdce1064d72
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:03:57 +0000

    pull wing usual figure practise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
