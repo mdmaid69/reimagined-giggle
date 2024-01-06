34d201eb576471789bb9c4a717112c333a8f820e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:06:54 +0000

    muscle clue room authority connected

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print([x**2 for x in range(10)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
