e0827854489e4834426dd6a0374e399e17a9a18e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:00:33 +0000

    information bread tax can hole

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def cube_number(x):
+        return x**3
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
