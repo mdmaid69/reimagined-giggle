3790d52ca25b97a8085cb1cf33d4efa00c1e3486
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:32:34 +0000

    wash land sport trousers pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_even(n):
-        return n % 2 == 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
