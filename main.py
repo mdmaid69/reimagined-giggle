99841b16f44b7f539d883e054e55e38be8f4088f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:06:10 +0000

    weekend present spread remain Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
