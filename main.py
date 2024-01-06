689a9b1334ec9abc3c90a9c65e940bf7a6fb3ad6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:50:26 +0000

    seriously office geton hurt couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
