8948fc91c8a119202e31a7d8d57db517765681c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:55 +0000

    cut giveup winter happen breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
