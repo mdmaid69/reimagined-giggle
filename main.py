9e231520059719183c9ed8009ff25c59fd62d4f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:33 +0000

    centre smoke smooth especially tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
