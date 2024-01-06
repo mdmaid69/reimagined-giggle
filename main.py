eaaeec96bca531d4ce68aeed6ab66ce5fe7f7db2
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:29:04 +0000

    emergency tight curious giveaway thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
