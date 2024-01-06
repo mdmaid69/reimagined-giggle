3d1250052c4ac4ba5e638891b8dff12977006f99
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:49:33 +0000

    official exploration face problem book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
