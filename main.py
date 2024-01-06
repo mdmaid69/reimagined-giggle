dfb8da23b98001fc97f06bc5cde371ca902e107e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:25:53 +0000

    copy not safe cause original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
