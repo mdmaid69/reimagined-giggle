4ed9534888630469304691045dbe2a08bcba2d7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:07:47 +0000

    welcome start worse tap work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
