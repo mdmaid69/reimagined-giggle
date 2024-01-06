d8476f727d65925f72642e1b59242c7c7171dc9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:42 +0000

    forget environment wire married beginning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
