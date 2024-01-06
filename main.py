20d3e401871ed022ba10e47e320532e6c03ec361
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:01:59 +0000

    Oo narrow shout clue hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import os
+def change_working_directory(path):
+        os.chdir(path)
