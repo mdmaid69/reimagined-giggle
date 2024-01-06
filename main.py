ecf20b329022e0ba12f087d16522b806fe4adfe8
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:09:06 +0000

    border exam chase tidy block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
