f042272e12b5deea8497882cfdcb434cc82d1a2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:40 +0000

    equal accidental hear goout emphasize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def convert_to_hex(n):
+        return hex(n)
