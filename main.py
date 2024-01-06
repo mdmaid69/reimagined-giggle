4ebc92e0b8bdde0a62bdc89031a5a70cbc158a6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:57:19 +0000

    back conclusion sort air include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
