c03f667a756e05e3159ac0646684239bdbab13a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:32:58 +0000

    telephone upset city settle once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
