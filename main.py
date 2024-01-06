dd243c195b70d14ef01c12ec1e1342cef0626f2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:44:50 +0000

    solve negative Gg slice admit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
