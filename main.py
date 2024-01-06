a8185ef216688d5a1652e16d73cea407fdb6b903
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:08:04 +0000

    soak star soft trap measure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import datetime
-print(datetime.datetime.now())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
