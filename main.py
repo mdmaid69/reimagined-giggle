6cf460483a3f3feae2660a0e4729a0671bb82a3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:12:52 +0000

    oil guess drop Ss depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def get_array_as_int(array):
+        return int(array[0])
