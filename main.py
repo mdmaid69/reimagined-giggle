6b9988cb65f31e230741693c180766d241321444
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:11:06 +0000

    shout talk any language maximum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_set(array):
+        return set(array)
