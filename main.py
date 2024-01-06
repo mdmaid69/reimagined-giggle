6a9950516f346089c08b385fd59d1eb5c5b3a905
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:09:55 +0000

    suddenly bottom hurt lookback(on) type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
