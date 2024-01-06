4d95d4a65389140aa8f27e3ef6fee0aa0f246abe
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:53:10 +0000

    argue colour cost properly child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
