5cea8bfd9da12e425170e15f7a9b84d185814845
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:37 +0000

    breakin rubbish road point though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
