212fe65ddc98aaf8a29f1d87114702658f0dd299
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:06:16 +0000

    cry suck some occasion brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
