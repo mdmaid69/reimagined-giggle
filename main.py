026153d5f112355ae97d831ade8b03bce4e819c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:59:06 +0000

    stay ever yours capital high

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
