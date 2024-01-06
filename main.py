d4860fc87824b30a00022fb61c13a217a17a2940
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:25:19 +0000

    crash Uu deaf message extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
