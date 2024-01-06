fad65b7d3d7598693bc3f2a936c52dea5fd45dc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:19:29 +0000

    but sport on autumn not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
