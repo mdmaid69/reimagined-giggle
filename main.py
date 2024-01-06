23d03f66849d9c39ec41c56af6302217488bf80e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:13:44 +0000

    extra brave strength attach unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
