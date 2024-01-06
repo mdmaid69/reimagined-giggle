d90881204adfaaf817028e05d52f8c97243850c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:12:44 +0000

    neck accidentally relax disturbing guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
