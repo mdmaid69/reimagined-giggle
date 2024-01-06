39296d68401035140c2e918ed2470adefcc2869d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:57:26 +0000

    shut accident pay under exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
