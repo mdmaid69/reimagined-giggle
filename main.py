790c7bd37e58c6e53a40f73a63be856509780460
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:25:10 +0000

    intelligent powerful otherwise appeal interested

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import os
+print(os.getcwd())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
