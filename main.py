679c9b003962538574f65d3de5f720cc457b7b8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:15:27 +0000

    approve tight bread spring mess

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import os
+print(os.getcwd())
+def calculate_acceleration(speed, time):
+        return speed / time
