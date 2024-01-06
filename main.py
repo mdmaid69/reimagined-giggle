2f26244abfb2eb733cfefb945ad6ffa45e2845cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:52:33 +0000

    see rest lucky complicated lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
