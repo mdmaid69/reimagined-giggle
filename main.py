a88ee5a8234b0adf98495d16dcdf07903e2c4ac1
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:17:23 +0000

    summer along ought moon now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
