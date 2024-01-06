e237938e7e55de3de436c59cac26d60c37c9a46c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:28 +0000

    stamp weekend needle remark total

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_item(array, i):
+        return array[i]
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
