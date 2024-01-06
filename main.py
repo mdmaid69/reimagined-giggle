ac14f5f196e0a6874b839bddfafa0b89d24581b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:44:51 +0000

    more hard software edge mysterious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
