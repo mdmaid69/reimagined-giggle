b9785615c8cd6e9972e6fe751cefde536d58ee1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:17 +0000

    crowded each square stare breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
