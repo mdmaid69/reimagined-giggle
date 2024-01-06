18fcfcf9bce63212b0a3465eedbe32ca4997ad90
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:41:31 +0000

    platform Dd wish laugh face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_frozenset(array):
+        return frozenset(array)
