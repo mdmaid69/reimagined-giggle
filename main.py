69adcaf21702b778e619f1008e53890babc2174a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:30:06 +0000

    interesting beneath container anxious deep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def is_odd(n):
+        return n % 2 != 0
