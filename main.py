74ea35ca79c159811e6e665574ba1028772ce503
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:00:46 +0000

    father best expression chase goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
