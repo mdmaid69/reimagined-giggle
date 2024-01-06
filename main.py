360d4bbec93879a146a8309fe687065093a699e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:51:29 +0000

    dark operation always confused judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
