7243579a0a8461e0e9c241fefa56f550488e9c64
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:17:22 +0000

    sensitive miss wish quality middle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_volume(length, width, height):
-        return length * width * height
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
