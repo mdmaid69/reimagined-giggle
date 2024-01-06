a22da2f5d722fe015b063ffed0095e833381f2e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:07:51 +0000

    goaway destroy past life gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
