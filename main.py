f45241dec437496a381408f07abe5d309b9e7f0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:57 +0000

    protest price waste telephone lookafter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import random
-print(random.randint(0, 100))
+def sort_numbers(numbers):
+        return sorted(numbers)
+def count_words(sentence):
+        return len(sentence.split())
