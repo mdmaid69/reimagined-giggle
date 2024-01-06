7cbef71a9576c4b320ab6dca05a57a1764b2fa84
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:06:56 +0000

    failure our operate between scared

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
