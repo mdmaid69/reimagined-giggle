fccfec9437c626657dc4655e01f55a6c935623b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:15:41 +0000

    sail number polite walk hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
