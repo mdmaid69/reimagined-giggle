169cf3b358fcfec0bd6b6fb14c32d6b357fe2a01
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:35 +0000

    train disapprove clearly intelligent regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def square_number(x):
-        return x**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
