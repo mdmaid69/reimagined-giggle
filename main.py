2613530f43826cea5ab91f20cb4ee5f4e07e72e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:32:57 +0000

    underneath willing name rubbish sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
