62e4128c9ced224efb1f364367a23876ae46c13e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:26:16 +0000

    single know fill influence dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
