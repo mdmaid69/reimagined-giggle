ff1a2105d7768cddad1376ece754692053e75b16
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:00:58 +0000

    laugh lose single deliver check

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
