7a98effb908607bfdc7a33fc241823c834f6c027
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:32:04 +0000

    behave swimming since age terrible

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
