da42f496fa935a1a630661c8ffcbdb7bb7f81ba1
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:48:20 +0000

    person sign problem care roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
