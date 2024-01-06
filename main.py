1cf84a520f39447b31f3da9e64bed1878ea21e3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:57 +0000

    567enemy gas determined reach undo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
