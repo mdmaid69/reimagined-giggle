76a9ead16bc3224b697b35aacb87f0a84ce75e7c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:04:00 +0000

    this turninto guard confuse deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
