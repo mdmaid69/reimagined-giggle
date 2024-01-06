87b3b926bd7ccab8f27a589a6c83cab2ff657111
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:34:44 +0000

    mixture plain system turn popular

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-x = 10
-y = 20
-print("Sum:", x + y)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def print_python_version():
+        print(sys.version)
