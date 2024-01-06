36249a648854f6832300b5626bccdee3ec4ee492
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:41:21 +0000

    argument willing male occasional badly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
