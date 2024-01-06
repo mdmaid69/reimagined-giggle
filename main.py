fcb0b7f3d417928145d8264ceb1102f1912cdd83
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:20:13 +0000

    law safe slight without lead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
