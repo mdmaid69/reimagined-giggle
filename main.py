36aea4b158c269fd8f339282173d51c2943b0dea
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:32:43 +0000

    complete pretty equal standard spot

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
