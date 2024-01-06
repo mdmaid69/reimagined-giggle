d69d684548b0664ad9ef49235a5d01d83aa35800
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:15:15 +0000

    occasion next care fix deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
