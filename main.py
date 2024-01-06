0c7e3724dd99f086dd8c5e3367577311c419497d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:59:26 +0000

    advantage shock spin cheek shut

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
