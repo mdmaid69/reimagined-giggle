f3bbcf3b71852ca37258e6056a526f10a1bc916b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:35:13 +0000

    healthy fix flight special takeout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
