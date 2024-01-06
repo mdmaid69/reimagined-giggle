95674b79efdb66cc74c3a55f6099d770e2cfc8e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:00 +0000

    Bb bright treatment bottom difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
