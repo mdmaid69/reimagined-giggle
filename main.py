42f38955765c5d8e9e13a60dc76f8a4f3928b870
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:42:01 +0000

    adjective fire them beat strength

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
