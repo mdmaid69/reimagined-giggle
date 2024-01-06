e5132f06533470555f48493bb5ebe72e8544894f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:14:18 +0000

    raw character cook slide border

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
