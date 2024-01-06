fc5cc8a25ed3b6169cb7c2d1da194c6c7f97bb5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:30:47 +0000

    human building accidental floor respect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
