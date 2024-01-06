9e03f86ad182df1a3fdb059a5d34cfe47b5dd0ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:38:55 +0000

    holdup eye turnoff joke estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
