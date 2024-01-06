92efd769711c3908a69082e1461c873481e6d479
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:02:12 +0000

    relationship discussion stupid alive fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
