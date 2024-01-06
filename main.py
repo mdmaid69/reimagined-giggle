09ce164631ee1459b8500001c33e379e2f781cee
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:40:53 +0000

    object floor pattern space push

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
