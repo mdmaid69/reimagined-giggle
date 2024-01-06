b57250b04776f80f4aa451fffc9e8051a5bb14ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:34:24 +0000

    569enjoy town runaway listenfor rich

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
