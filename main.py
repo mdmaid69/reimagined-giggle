1870fc7a5d2b5a5e637e9100dc55edb65cbce6b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:16 +0000

    character undo dress apply passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
