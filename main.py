19bc57fa508cca8f5d28fa3fda3f93de4c3bd779
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:55:16 +0000

    wet bin enthusiastic direction success

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
