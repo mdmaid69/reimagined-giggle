dcd558389ac5aa8eb16a7a6e5e58f2680a580f6c
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:41:09 +0000

    bound off suggest exercise murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
