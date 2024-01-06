a389e35382ff3d4d057c3be8136c2c9b9e8b71f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:33:54 +0000

    stir miss copy spare negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
