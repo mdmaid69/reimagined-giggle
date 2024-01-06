e3f917bf0681987361ea1838f02dfac6b85dbe9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:18:18 +0000

    interest cheek sentence science head

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
