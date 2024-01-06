9e3512597f93c101fab0aa87d03b824323125334
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:17:55 +0000

    surface zero worse second round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
