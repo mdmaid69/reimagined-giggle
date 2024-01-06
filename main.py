9999710afe57b053722dae8e0b90c80c10c83ec9
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:00:04 +0000

    face about wear name sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_typecode(array):
+        return array.typecode
