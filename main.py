ff2fe65c7e5ce261ba31de644d66463f04b79d70
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:42:12 +0000

    godown bite female run price

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def convert_to_hex(n):
+        return hex(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
