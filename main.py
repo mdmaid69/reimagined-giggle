53535f123d49d896655a6a54ad473314f101ebcb
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:37:34 +0000

    scene kick shoot even left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
