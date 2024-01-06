fd70df7b124eb0dd31620b1ebda0cdc562c8763c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:19:45 +0000

    present runinto decision moment hardly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
