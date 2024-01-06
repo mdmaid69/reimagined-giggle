2378dd9c502081727827dcca3d81237c4d111aa4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:29 +0000

    raw passage giveup behind beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
