32f8beca87357f2f6f1ccaea311cd3f17386dfcc
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:21:28 +0000

    loan convenient natural system yours

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
