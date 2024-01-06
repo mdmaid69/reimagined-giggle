f26d479cade18ccc86cd5e3e3e9a82764c68b44e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:53:11 +0000

    empty fun raw spoil match

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def square_number(x):
-        return x**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
