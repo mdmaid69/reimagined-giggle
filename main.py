0c202ff9b9e6fd64577c54165ac3d8bd28614a9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:56:15 +0000

    male aim Bb still group

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def find_min(lst):
+        return min(lst)
