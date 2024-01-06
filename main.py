cb8b30cd118e442db33b81259ce28fd4e31cfe40
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:21:33 +0000

    fetch chase show before taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+x = 10
+y = 20
+print("Sum:", x + y)
