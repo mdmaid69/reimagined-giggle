64417074dedeb3636c862881ab0c800d300f50bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:03:23 +0000

    Tt face buy him kind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
