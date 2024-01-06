9912fd4a4115972ae512db374914d6200cb82044
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:07:24 +0000

    uniform behind maintain machine disgusting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
