c20ec3039524e8b3b3706937f40a143456d71ebb
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:23:44 +0000

    just range we battery bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
