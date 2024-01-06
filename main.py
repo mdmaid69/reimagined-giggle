e014ea00fa9ac8841030d748d5075f186f9f8240
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:52:01 +0000

    occasion collect whoever move discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
