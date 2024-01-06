4190ae7bc693411df66227b0cebeca79d2e09619
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:22:10 +0000

    grand live always destroy sorry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-print(os.getcwd())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_sine(x):
+        return math.sin(x)
