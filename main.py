ce7972efff02b341408d981febce1a9114ad517b
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:43:12 +0000

    strength trouble surprising search hello

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
