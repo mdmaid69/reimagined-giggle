32d81ad045c2ad3a2c46a78342ca84c4e6345cd9
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:35:05 +0000

    smooth develop tip disturb treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
 import math
 def calculate_circle_area(radius):
         return math.pi * radius**2
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
