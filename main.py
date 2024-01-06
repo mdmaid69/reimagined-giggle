12c4e16514dba1a225ca8a4525d2d7cde5565ff6
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:01:11 +0000

    tradition contribute belief present bang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
