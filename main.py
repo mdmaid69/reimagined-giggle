13c4cb64422d245ee97d3d78f7389622107a6a9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:38:11 +0000

    with brain finally remove objection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
