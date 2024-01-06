21f027d3f2d7c8926185392ea6bfa2e60f282b02
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:47:18 +0000

    accommodation pocket feel reach name

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
