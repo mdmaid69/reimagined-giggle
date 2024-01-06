ba994aeb5a89fb01b3a55181eeaf256052af0cc4
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:47:31 +0000

    science naturally violent refuse central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def convert_to_hex(n):
+        return hex(n)
