3f9363fa74899998c52ee356fe8fdace8849773c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:11:04 +0000

    arrival central university steep instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
