c38225a12e2253d9f2c1d2b235078110d4a97a9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:02:45 +0000

    chat act couple content address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
