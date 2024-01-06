7b96d369a60bc92c6d0a15bc6148ef7a8639d795
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:13 +0000

    force identify know tend interest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
