358357d7c89d4ef680e98e55b1a33f87b3fd4530
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:06:42 +0000

    lay determined large Ee prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+def get_today_date():
+        return datetime.date.today()
