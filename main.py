33e0c66f4a48848c32c54dec8363cc62b13d28ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:06:04 +0000

    present Rr stupid something circle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_speed(distance, time):
-        return distance / time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def square_number(x):
+        return x**2
