324f1b082b4c42674f9d4aa77cd83d04da4b5a54
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:44:55 +0000

    react weather shout suck like

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def is_palindrome(s):
+        return s == s[::-1]
