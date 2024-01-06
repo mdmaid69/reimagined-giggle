8883b760df35610973a118e43b3d6bf2ec37fbc1
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:47:16 +0000

    plant stop afternoon shape bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
