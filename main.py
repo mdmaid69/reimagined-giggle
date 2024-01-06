b606da527e370b44bd6284a9786787b6d0557545
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:37 +0000

    do quantity smooth style against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
