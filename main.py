4132fb4b9e8ee0eab98256434e11d95228213b22
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:57:59 +0000

    worried matter frightened emotion share

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
