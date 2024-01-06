4c57eee48fb6b4d4b52f3df9f4df970e677f9674
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:43:02 +0000

    spare Ss colour temperature bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
