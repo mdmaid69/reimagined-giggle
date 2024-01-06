757f5290f66e8d830781727402d7b0e8a3dfdbd4
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:25:57 +0000

    right if path pinch pipe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-print([x**2 for x in range(10)])
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
