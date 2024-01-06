c93ac29bc6e6a0219a52e544d202dfedb7ea3f98
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:03:30 +0000

    cry building unfair sweep getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
