2fc8f6731923dac10db69ad6736da82e0b4f4c48
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:07:59 +0000

    unite union region open replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
