8c12a2d736bb05f2a40be7f564b22b312fc7741d
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:46:50 +0000

    plant cross accidentally aware sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
