c5fce54ac78bbfe95f8374062e86c6e316ba62d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:16:46 +0000

    second comfort future combine characteristic

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
