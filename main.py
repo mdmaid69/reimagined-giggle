11fedbc667d758686cf2f31649592ce27e434041
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:16:56 +0000

    prefer way wrong declaration several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
