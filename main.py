88217ef9df1afdd65ae2866444683229c8590778
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:29:01 +0000

    left Ll passenger disapprove foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
