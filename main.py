300b5d0e7484fc83f444fc023275ca30ff7cc7f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:34:14 +0000

    arrive loose chief expression generous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_work(force, distance):
+        return force * distance
