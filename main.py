3f68851b5b0154db160833ba9b156bcf7a4cb3c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:03:31 +0000

    shame right Qq key settle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Characters:", len(text))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
