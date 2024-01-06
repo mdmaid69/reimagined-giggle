ec8772d6c54634894c8086557e89e22f01731628
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:12:43 +0000

    well maximum difficulty keepout keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_sine(x):
+        return math.sin(x)
