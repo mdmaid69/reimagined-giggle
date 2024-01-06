ee14f2551eab86d7955a50d58275abbaaee3331f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:00:34 +0000

    hope false upset platform makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
