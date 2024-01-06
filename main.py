d1bcb6701d3de0e710b62238b27db50c36abd76d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:52:09 +0000

    train suppose rich effect activity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
