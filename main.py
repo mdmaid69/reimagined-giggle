3c3ae8690c3ee315f4604d3814333f5b31524a9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:30:37 +0000

    comeoff march previous about mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
