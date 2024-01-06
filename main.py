81eed71c920a98bfa59b806d2e352fc8b1161ef6
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:10:14 +0000

    Ww butter late Vv crime

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
