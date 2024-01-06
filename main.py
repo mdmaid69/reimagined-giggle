9c5b521e1e5ad70252a7403d4841c65173fc1bd4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:44 +0000

    similar try effect appeal port

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
