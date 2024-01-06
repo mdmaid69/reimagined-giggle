cbc36769114e375feedb0fe6fb8334e11169d663
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:23:41 +0000

    believe form mean fun warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
