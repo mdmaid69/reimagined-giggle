46556ae3e971163e35eba9a6b8c40aba63f59d9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:55:29 +0000

    Ss matter never next shall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
