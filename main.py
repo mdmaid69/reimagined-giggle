2d2227e4625b5fe8737b7bced2b7ddfac6561d5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:57:38 +0000

    building building she effort connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
