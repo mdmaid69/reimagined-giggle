3c24a235e7c8fa4177c084e6a049c083e3242b08
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:21:00 +0000

    wrong glad sign anyone lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
