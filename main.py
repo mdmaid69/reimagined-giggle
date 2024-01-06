276a444cacefa702aa1fa9ddec937d5d41ae8ab2
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:30:41 +0000

    foot read limit sentence nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
