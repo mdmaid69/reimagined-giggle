9b75a9eeb475b363f97518bb8d5a2b13b2435319
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:16:17 +0000

    dry maybe the stupid height

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
