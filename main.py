718bd60666ef68fa05017a1b2cae759dd9bdc904
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:46:30 +0000

    welcome material introduction meeting delicate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
