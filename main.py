9bd365e6c26c004a7ec7b747efb4b6042062f228
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:06 +0000

    street young shadow anywayanyhow rain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_speed(distance, time):
+        return distance / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
