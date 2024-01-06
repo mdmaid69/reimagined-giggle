73803deece77de13b778916c4e3004c80f31b32e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:20:50 +0000

    note dig lonely after previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
