343f41cb28d68a9dd79477a03230c13d6c3887e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:35:54 +0000

    patience favourite conclusion strict correct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_hex(n):
+        return hex(n)
+def convert_to_binary(n):
+        return bin(n)
