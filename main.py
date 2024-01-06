5593b2f964d8217e88b154992a8dac10368ece58
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:46:32 +0000

    approval flat field condition fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
