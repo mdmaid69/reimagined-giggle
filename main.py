1745f49abc5af2f9f22b9ee8410e0f404bf1fd9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:12:25 +0000

    best abroad worry display mainly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def clear_array(array):
-        array *= 0
+def append_to_array(array, item):
+        array.append(item)
