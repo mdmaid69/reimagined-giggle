0fb2ccc7785fcc430aa576634faba84fad008533
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:50:36 +0000

    mail trade difficulty sorry pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def is_palindrome(s):
+        return s == s[::-1]
