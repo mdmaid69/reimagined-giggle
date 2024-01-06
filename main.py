adac963ebf47fc50ddf28b18cd9aeb08c5a17a35
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:14:02 +0000

    mend press pretty fence hospital

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
