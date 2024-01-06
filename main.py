14a7ffbc26e1901c786d55496206cad4d72094e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:26:29 +0000

    club turninto learn slowly onto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import time
+print(time.time())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
