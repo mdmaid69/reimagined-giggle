b32e1242443574aefd0c9d6a53f30f8996969a0c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:58:14 +0000

    choice previous take silent lookaround

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
