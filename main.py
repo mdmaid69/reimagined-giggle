8ec446a16e646a40c9b51bfcd56983a659496431
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:30:35 +0000

    trouble black react coat he

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
