49c785f449c084a531c87bb33b03d8521ad547e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:06:15 +0000

    plant think form original nowhere

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_float(array):
+        return float(array[0])
