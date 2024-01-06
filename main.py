6ef82ca1200530135ce1367deb98c2d4031ebcc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:32:28 +0000

    discovery substance custom breakfast immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
