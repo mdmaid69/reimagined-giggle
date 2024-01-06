bef6b0afdab0ea26761cfbd2021da9ade70e70a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:31:07 +0000

    unfair while Pp border thanks

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_counter():
+        return collections.Counter()
