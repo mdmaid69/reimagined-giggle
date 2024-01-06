d516e8498689c082e02feccb62dfedf98ca458f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:46:13 +0000

    country rid label club early

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
