3de22a05bc8814d83f9a7f6af1468adf797a00f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:46:03 +0000

    usually furniture OKorokay angry clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
