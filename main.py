a1c72fcd06a98905360e3f5552638740d0aa6d4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:12:25 +0000

    style unlikely bus snow opinion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_power(work, time):
-        return work / time
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
