af999f4f89841e966796313cf9511da018a700e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:37:40 +0000

    statement ball everyone thinkover save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
