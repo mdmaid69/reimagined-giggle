501e54761e5b6a1e61214407070bda8902db7719
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:26:48 +0000

    survive direct line frighten area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
