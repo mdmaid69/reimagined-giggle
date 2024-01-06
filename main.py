3c3f2c9c8d94cc3b677f2e83f72ea10a35f89784
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:50:56 +0000

    many love callup law tip

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
