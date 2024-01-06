ed64e300c34e86fb2dab7bf5d17f9b6ac83837a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:01:32 +0000

    lock slightly approve matter protest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
