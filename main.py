bb13b95f6a4ed9472cdbd9d7aefcd090c9001867
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:43:32 +0000

    indeed behind near danger smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
