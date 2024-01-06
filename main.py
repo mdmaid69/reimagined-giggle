6e81a71ffa80476615b1b6438b1eaf708179a0fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:08:48 +0000

    progress geton then key but

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+print(sum(range(10)))
