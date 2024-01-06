3bba4549f239e2ed69d822a6a724711729d23783
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:27:04 +0000

    young instant hot collection sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
