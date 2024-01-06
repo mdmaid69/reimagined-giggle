9072535869923d66e0dbc6fcb18e0d868d566a63
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:34:54 +0000

    path pleasure uncertain mouth value

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
