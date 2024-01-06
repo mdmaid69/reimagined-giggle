8eb426932289cc08688a88434b72af9668492f9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:14:50 +0000

    stop advantage satisfactory 571enough human

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
