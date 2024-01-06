69e99c1652cfdda3fcb1f99c5bd1469ac26b6595
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:09:31 +0000

    team closely country arrival interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
