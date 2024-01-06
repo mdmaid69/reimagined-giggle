ed249ac510d42fc8ddc2aee5190aba1a9345b058
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:53:37 +0000

    swim naturally bottom sure arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
