50f0e1030dd5f65a744960f9c979af70fbe2da8d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:01:07 +0000

    clue advise wall tend afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
