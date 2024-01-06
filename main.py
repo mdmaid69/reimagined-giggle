53b8581350377f53076897675ed3c7a4406a9e99
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:05:37 +0000

    familiar neither common tip then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
