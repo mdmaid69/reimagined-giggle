bc34def9d9010ae4c59d77e22c3bef903e53d4de
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:38:54 +0000

    battery shut keen lock upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
