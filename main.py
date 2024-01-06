322e4ef2a0b3309519f667342ff26e6915806935
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:14:38 +0000

    my machine they commit petrol

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
