e5029ff5e95f635f9bb0961c007e0a440c64c689
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:21:20 +0000

    centimetre Ee closed string indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
