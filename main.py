8328242de2b16c25e397bd7b7f930ae669bedb99
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:35:32 +0000

    sing use operation equipment content

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
