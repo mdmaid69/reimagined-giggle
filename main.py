c6b24590a98a41578413bf3b16335d3519fee583
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:14:04 +0000

    hope even fire bang ground

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_as_list(array):
+        return list(array)
