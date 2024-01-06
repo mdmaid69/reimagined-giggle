9e490892a6be93af133d3a8c3971dc2b237b3098
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:41:00 +0000

    birthday clue identify store know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
