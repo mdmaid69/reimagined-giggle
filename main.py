b994e42f5ba3aed3e6931f3310ea561f02d4e6cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:10 +0000

    successful record eager fall wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
