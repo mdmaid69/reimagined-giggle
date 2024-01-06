25c3b3ed898037802b507c8ed075b83c350e2cf6
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:14:09 +0000

    above reason thing office dream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
