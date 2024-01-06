58240b893273b4d736fc92a589ea918b230fb96f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:48:13 +0000

    his stick worse simple complaint

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
