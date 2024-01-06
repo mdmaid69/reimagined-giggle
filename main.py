e3ba872218e2a2577034ff21bc2430afeb151c33
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:06:48 +0000

    knowledge disappear wash pull manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_set(array):
+        return set(array)
