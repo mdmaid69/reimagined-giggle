ae4ec6bef8235711271cd6268ec8836dc92ca798
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:58:41 +0000

    pride one brother protest serve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def is_odd(n):
+        return n % 2 != 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
