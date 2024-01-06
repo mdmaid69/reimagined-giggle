e0ce356d9abbdbeaf804a44106a83acc3483dda0
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:00:40 +0000

    own sound tomorrow go once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
