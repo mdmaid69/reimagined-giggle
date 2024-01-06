0d77bc574afd3c0f04fa3b4a140598e142466688
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:55:54 +0000

    city Yy load cut genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
