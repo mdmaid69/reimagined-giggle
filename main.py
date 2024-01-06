429d553513850c7f44ec25fe11b6f2ed013c34dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:44:26 +0000

    influence lucky demand obvious appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
