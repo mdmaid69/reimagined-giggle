5383a2ff49f818423a042ebbc6f8e714c0ab56d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:58 +0000

    stupid service transport not without

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
