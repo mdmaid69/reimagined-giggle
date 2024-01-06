5ca279069b76dad035556f4b62e0176bd87db3a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:10:21 +0000

    pipe move regret relax corner

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
