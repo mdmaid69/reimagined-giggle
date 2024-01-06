fae0db0f74c52c67e06c62b04e6b82c1f141e31a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:24:17 +0000

    taste talkoutof as support would

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
