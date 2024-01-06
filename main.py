fab670e4a417659700b663a45fc8d2d4a350ac4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:00:38 +0000

    reject crash achieve solve somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
