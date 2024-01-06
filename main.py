7f7f93ce8d29d7b554a9a919dbd02e8397702b7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:59:13 +0000

    start achieve address quiet Rr

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def add_numbers(x, y):
+        return x + y
