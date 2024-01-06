f298e2149257eaba0b6d11abcc6b369b1fb2cf44
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:48:16 +0000

    commit wash view plain moment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
