ad9c0b64198650465a6cf46c70c827f3e54dbd01
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:10:37 +0000

    chase public 573entertainment road well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
