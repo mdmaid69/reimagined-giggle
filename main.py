7079daa7cad03d8acd84699e4aed2a035d3c4929
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:02:28 +0000

    act since surprising getback 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_current_time():
-        return time.time()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
