c6e88ebf728935dc3cca40c6f1fc90e440cf6bbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:59:28 +0000

    brown mend female everywhere from

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
