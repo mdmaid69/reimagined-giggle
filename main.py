5f000be89efc1ca7a91bc5ea2a6f1a059f0c2c45
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:39:52 +0000

    Gg close nerve unlucky seem

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
