e8c173ca4223d07ade32867bcc7d76f48936db50
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:21:53 +0000

    money church practice cinema well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
