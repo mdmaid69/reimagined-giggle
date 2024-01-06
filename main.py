ac787d77f5a2702106a702d553a2303c5bc5b56a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:19:54 +0000

    still among consider left stop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def reverse_list(lst):
+        return lst[::-1]
