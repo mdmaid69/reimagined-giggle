1482ad7d42d1ba485aa88627fd5de171e1e976b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:17:19 +0000

    little nature intelligent play divide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
