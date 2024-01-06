2ef3cc2b1804e59b0feb18d09f7d417485a6ff0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:21:40 +0000

    cause cause combination indeed giveback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
