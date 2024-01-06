e382d3ebdbf767a1dcd88ce4cce8d46c754ab070
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:09:21 +0000

    recently disgusting team impressive to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
