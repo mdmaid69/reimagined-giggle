1523f717c94d948ef6a45825023def53ac7ce331
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:18:21 +0000

    absolutely punish clever even gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
