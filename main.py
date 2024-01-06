e66e148d8e8ccece142aa5ef6fd6bf14a2fe51bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:56:50 +0000

    risk exchange callback classroom frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
