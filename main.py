f451c6045f5af43d13e11af81b66a02d153c3640
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:41:29 +0000

    move see powerful left average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
