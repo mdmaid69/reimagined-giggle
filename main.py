5341c3b5f50991577c326c49859f119b9aaf4ba4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:51:48 +0000

    lead compete by contents march

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
