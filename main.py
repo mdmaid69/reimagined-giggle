0b1118be1ac877dac680058203420d12abeefe69
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:33:12 +0000

    long smell geton there above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
