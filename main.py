2b33679fdd524af3aa29f4118d85013f3afe57c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:05:22 +0000

    mind under uncertain work confidence

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
