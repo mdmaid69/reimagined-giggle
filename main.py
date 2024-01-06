8774de1b001b28e36ee5ba9dbcbc262b2628b68c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:37:01 +0000

    set suggestion callup welcome flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
