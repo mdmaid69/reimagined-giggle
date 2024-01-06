90340be132a1f8bf665fdae9ca01880fa3b36040
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:22:28 +0000

    giveup wealth 568energy wake behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
