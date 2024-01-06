0d3a039a5ab6b03149f3ca9c1ac4d4bc4824d611
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:42 +0000

    back land herself reason best

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
