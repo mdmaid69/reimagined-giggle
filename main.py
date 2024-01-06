d926c899c04b3695ec12444115c1b3eafdb86908
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:50:55 +0000

    position accident star awake perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_array_buffer_info(array):
+        return array.buffer_info()
