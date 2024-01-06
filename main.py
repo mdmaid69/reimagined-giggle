86378d99c922d030155a23fce4e68f17102562f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:35:44 +0000

    relationship like shelter this shade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def multiply_numbers(x, y):
+        return x * y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
