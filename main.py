a086a6f58853bf0654d0a569e4ec5fd03313dcb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:16:05 +0000

    name table foreign production play

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
