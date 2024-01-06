a649005389227011051d648abaa07ef99586c1f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:06 +0000

    wipe intend far odd aware

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_power(work, time):
-        return work / time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def remove_duplicates(lst):
+        return list(set(lst))
