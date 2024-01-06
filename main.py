bf99e6ae2103ed83c12eb4517a0456d547b11fe8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:15:06 +0000

    touch switch dick university cross

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+for i in range(10): print(i)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
