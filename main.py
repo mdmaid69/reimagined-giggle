727a6f66e833c0175443956768694ab763de941c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:33:05 +0000

    trick neat experiment eager whole

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def square_number(x):
-        return x**2
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
