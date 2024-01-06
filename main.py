e3fe1db7e85dc2f7e5bb0c48ef6f6c5420a644ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:09:10 +0000

    along everything once spill plan

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_set(array):
+        return set(array)
