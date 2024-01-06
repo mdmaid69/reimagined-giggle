e8573a8fa3aea30ef9d1b18e82dfecf49f6bc42c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:37:49 +0000

    yesterday pin order know breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
