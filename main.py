fe29140f52184fcacbc597dae63ef591c8786446
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:02:31 +0000

    treat vote minority butter straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
