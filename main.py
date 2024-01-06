d28169cef5ad800b44dc8ceeb4be30b8b420658a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:08:24 +0000

    fortunately direction fashion pain snow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
