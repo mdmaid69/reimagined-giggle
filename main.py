642c57e8ba4f1ccf2864cc3fdd900b75372ce362
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:13:14 +0000

    borrow busy someone ourselves freedom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
 def get_array_buffer_info(array):
         return array.buffer_info()
