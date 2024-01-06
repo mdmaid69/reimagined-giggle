35734abd91edfba56d582bf082616a1c8e52c806
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:27:22 +0000

    further contribute entry contain petrol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def find_max(lst):
-        return max(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
