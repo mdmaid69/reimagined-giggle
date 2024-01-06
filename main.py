836389bf96898a5cac547e8606c0bf64e2290032
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:49:05 +0000

    quickly complete depend carry world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
