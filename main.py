5fdfdfe6f0376d18837c6e5f3ecc784e8e71fca9
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:04:21 +0000

    whichever idea shall fresh strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
