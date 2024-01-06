8f9b202a14d3e56e64c37cd986a48a9d004bda85
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:07:45 +0000

    below wipe usual flat last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def is_even(n):
+        return n % 2 == 0
