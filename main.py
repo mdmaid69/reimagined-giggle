2d094c03cfb7d2564899a00e818066c5818e403b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:24:34 +0000

    odd medium decision commit might

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_length(array):
-        return len(array)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
