9512b9b2f162c81d9f963826c4d08376612b0bdf
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:12:45 +0000

    charge neither firm pure dry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
