b7bb95beb91201e100d39111fb328fb50ed44eba
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:17:30 +0000

    different convincing basis approval angry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
