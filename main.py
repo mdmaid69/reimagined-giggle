dd9bc0b904cae119ad0cb4c05f540b50ca1c6867
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:59:10 +0000

    so maybe especially this either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
