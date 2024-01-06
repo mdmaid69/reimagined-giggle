223c995c68f6a3c902589590190362fee79b5db2
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:03:30 +0000

    physical corner pollution star section

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
