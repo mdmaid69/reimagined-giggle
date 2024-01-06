9f00df76eda4ef28f6c29c9b19aec71e5fd59fbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:36:51 +0000

    whether hot sort understand progress

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import datetime
-print(datetime.datetime.now())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
