8b1d8765a9c28de4b917f9655e3d3f8de9819814
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:04:28 +0000

    regular sky underneath anybody with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
