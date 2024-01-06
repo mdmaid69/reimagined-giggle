c9c7988c8876c3b2a41847f121397d09e9c43efb
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:54:12 +0000

    attract fear fence hot punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
