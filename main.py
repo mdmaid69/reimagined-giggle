a4083d1b4e92a0bb5e276edb0d5f325d9e5f2032
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:20:56 +0000

    whoever suggestion mother business difficulty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
