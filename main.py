1fe2210296930bde0722452f22e737fa50cf36c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:56 +0000

    serve comeup bring holdup better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
