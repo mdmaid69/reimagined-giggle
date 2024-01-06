497d670dd86bfdcb1e02b7faa3c0e903a419f161
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:50 +0000

    wrap skirt mood downstairs pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
