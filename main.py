606f9c9b757cfd7b8ebe273f038b26360438bf5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:27:05 +0000

    equal light shop order reply

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
