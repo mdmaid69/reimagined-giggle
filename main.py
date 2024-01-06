c4ac854e4b6926ea82471d8edb03cc09138df429
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:53:52 +0000

    cover feel stick through win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
