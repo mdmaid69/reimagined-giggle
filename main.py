c441e158364cc7106994950cd35d3e6e3bea2535
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:45:03 +0000

    related ours calloff forget together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
