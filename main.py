ba09edd26b67914c47b79cff7eeccc70b3b61e76
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:35:13 +0000

    study sound exit spare fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def reverse_array(array):
+        array.reverse()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
