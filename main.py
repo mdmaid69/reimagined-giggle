03b83314b72c2aa07d934a1d6dad2b07fe2ff99b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:14:17 +0000

    fair pleasant follow scream deep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
