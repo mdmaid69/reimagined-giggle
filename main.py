3330b84103c0cc6753c03450899439684963c3e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:59:08 +0000

    responsible slide moon evening dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_palindrome(s):
-        return s == s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
