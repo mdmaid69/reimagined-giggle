0ff1f315e5174dc811b4e362a4fff28324ad4884
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:11:53 +0000

    problem Itself equal pause race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_today_date():
+        return datetime.date.today()
