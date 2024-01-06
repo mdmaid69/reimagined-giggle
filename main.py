2c6c01aab990a189482db74218fcfb25262cb70e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:46:45 +0000

    petrol tidy cry frightened issue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
