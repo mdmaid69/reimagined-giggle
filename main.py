5264ec93e11c38550c80c3ab1abe0b1c7d661fba
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:22 +0000

    along arm unfair hole result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_base_name(path):
+        return os.path.basename(path)
