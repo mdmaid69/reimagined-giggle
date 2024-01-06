a279771d45db6da15e40fa24e1aab8378ec44b39
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:46:35 +0000

    fairly flavour war peaceful radio

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
