87cbb06f48b0184bb3b4c27493b45871b00602ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:51:32 +0000

    interest ought stranger successful switch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
