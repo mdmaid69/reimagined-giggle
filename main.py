a0428ec683769f7d07ea7dd1088646c049d5146b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:17:01 +0000

    clear myself no begin fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
