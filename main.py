eec1159a566fc88f0159f55933144c27fd529485
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:23:19 +0000

    anything trust win protest deal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
