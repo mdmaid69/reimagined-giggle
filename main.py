53de04c20a1b5055c9d001afbb9d4a773c369a99
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:53:42 +0000

    feature and getout swing shallow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
