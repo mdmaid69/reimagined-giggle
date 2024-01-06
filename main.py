b758e1708bb7919939ad699919f94ca03c400b18
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:02:58 +0000

    gather meeting soil spot remain

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
