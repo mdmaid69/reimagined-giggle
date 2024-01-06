9fe991ffaed43a6e071f5a636c9da6691cb2943f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:48:38 +0000

    refer workon loud introduction before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
