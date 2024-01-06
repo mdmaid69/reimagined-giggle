ab273a6068a2c1b908a111ccaf33d2ab6674b835
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:52 +0000

    tour exploration please recommend official

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+def greet(name):
+        print(f"Hello, {name}!")
