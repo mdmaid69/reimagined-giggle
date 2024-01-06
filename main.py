8eb2a3349c50189d4f9f546c9a11b00556459e81
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:10:42 +0000

    otherwise giveout tool pipe boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def clear_array(array):
+        array *= 0
