5cf143a48b9621b35deaff1916a5244fa739c9e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:35:32 +0000

    comedown impressive regret run petrol

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def divide_numbers(x, y):
+        return x / y
+  def subtract_numbers(x, y):
+        return x - y
