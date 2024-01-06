5c63b729a3ab651772f04afa42813a8676a8726f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:19:52 +0000

    ashamed wander issue attention miss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
