6238daa4256946d26a72943d9548b7d7de5d749e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:23:53 +0000

    holdon ahead many meanwhile beside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
