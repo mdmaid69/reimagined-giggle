54a53802ad2c0e55f2a88038791aa16b7fbfe14b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:26:29 +0000

    drink wonder about past mainly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
