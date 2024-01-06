d2500c34018fc970bd418940c71633ea08875c38
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:41:30 +0000

    careful blue swim clue moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
