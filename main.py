e6df0ce3792fd393501e95a1d1ebd25d5b690fe9
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:39:45 +0000

    cold explain copy circumstance book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
