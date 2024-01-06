3bb78aa023d607865f2abee3cb72d9550244a78a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:14:46 +0000

    seem still shout past breakinto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_user_string():
+        return collections.UserString()
