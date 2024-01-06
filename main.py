96d90e8e50896ad83ff3f95ee1870885ee0e693d
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:11:15 +0000

    escape satisfactory later mouth comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
