def7270afdf30afdb83920b35c46c6987bcf2f35
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:14:17 +0000

    scrape outside national grade winter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
