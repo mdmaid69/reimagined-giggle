c72f2b34aa0fba89808374bc518c13bd822f1b74
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:04:08 +0000

    telephone Yy switch instant traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_length(array):
+        return len(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
