54ed6cd2d9e5a7b6241df5aded67b3471132a35f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:20:16 +0000

    dig nervous history present fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_max(lst):
-        return max(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
