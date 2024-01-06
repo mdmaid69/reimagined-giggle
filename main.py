fa7812144f4cfdd4fa5aa342a0f0013fb952bb5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:25:22 +0000

    because shout breathe future earn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
