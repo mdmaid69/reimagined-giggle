8b70587231ce74f7c46d4d38bcd4e1f840845d03
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:08:48 +0000

    score desperate file breakdown design

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def add_numbers(x, y):
+        return x + y
