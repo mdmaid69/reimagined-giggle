d36ed649d2450d62fc4593e1760e309fca23cb2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:06:43 +0000

    bring fight stand seem risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def log_message(message):
+        logging.info(message)
