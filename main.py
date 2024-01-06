6758e40fc90ca42b8ffb1aa23cb54bf5ec358b50
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:23:15 +0000

    fine follow small her their

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
