fb810240e8d7caf5a46fa528ce99b664e63183f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:53:35 +0000

    fold glad thick spray suck

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
 import math
 def calculate_greatest_common_divisor(a, b):
         return math.gcd(a, b)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
