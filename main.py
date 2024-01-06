ddd50c920e8056a4d1d3756c1b98ca0ae3f31b0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:26:01 +0000

    mad street in sense man

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
