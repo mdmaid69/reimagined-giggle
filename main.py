470a1df6f10e5c634b928dd94c5384347d9b41f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:31:39 +0000

    light forgive pour sad quality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
