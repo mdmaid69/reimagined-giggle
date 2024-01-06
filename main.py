ea7badc302518eab3f6f8e34dadd995120601b55
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:17:36 +0000

    into knowledge introduce obvious actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
