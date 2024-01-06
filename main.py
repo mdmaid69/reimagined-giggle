0dbda7d119543163027a404efd32b213de1054eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:00:02 +0000

    couple runaway hold official rare

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sys
-def exit_program():
-        sys.exit()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
