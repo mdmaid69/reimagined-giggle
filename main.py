30e169209b0d302cff33a785bc28a86c6c133278
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:59:35 +0000

    not frightened but throw doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
