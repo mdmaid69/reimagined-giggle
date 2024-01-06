d95811e1b379bb11532b84a1fd2f704e45e3d224
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:38:10 +0000

    not traffic disagree laugh last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
