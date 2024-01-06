233959330d9292f06fd976483ffcb9af52635a70
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:13:02 +0000

    combination angry operation through whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
