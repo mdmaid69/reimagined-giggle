1c113794cb0b7df49cc25bc10f2d4e4c57dcade7
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:38:54 +0000

    impression bringback image switch patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
