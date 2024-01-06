682a139e2bdc40368731971d072dbe152dcd2438
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:01:20 +0000

    decide order Ss blow whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
