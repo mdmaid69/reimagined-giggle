78147477dff5d5d04bb2afe11ded9684ebf15918
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:01:35 +0000

    round relief less pinch today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
