82e5e4920d7706575bdc6027e08625967dd7511b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:56:54 +0000

    568energy laugh happy accordingto dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
