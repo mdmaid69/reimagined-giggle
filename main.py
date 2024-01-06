b9cd6743756d19e219a04ec237039e5f31cdc167
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:43:36 +0000

    normal unfair getback past to

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
