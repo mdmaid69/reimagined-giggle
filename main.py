af6868ea389eccd9c5d46bc8ec60b8363e8ce146
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:49:14 +0000

    reply government declare determined wheel

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
