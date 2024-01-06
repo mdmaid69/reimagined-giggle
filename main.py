e7a463fff20f7dbc32719d04fdf24c39b2d1aae6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:14:54 +0000

    government cause traffic wide person

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
