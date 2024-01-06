881467a16df78a5cc2de75a88ef7cae639a02f9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:18:58 +0000

    change seat bin reply relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def reverse_array(array):
+        array.reverse()
