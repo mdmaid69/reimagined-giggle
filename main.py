7cb0aa47c22556af12490fa838b7854088ee7f5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:23:11 +0000

    small plastic ball separate doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
