39bdc46a33f85d021cfb5202630f2ba2980b6a8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:18:31 +0000

    cry cough share body silent

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
