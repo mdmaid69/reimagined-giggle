e71715df5b09e3288a5f35f146e7dfe517b2ca59
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:21:05 +0000

    cut that carry comefrom land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
