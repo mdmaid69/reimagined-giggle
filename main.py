83af9528a1e38809354c92bd371ab344ecdb90d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:28:26 +0000

    undo bin health curious culture

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
