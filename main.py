04133e3d28935990f2b9a43e56428889d3d3e6f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:17:21 +0000

    improve manner move plant fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
