cf939f24e4a355e25304b12dc4cbfd85f5328352
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:47:27 +0000

    seem square birth finish test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_today_date():
+        return datetime.date.today()
