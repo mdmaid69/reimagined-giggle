28d175e11307d40594812a01631d7106f8d0ffa4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:56:12 +0000

    whistle left repeat there her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
