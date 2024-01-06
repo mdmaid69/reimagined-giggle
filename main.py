071f5f558e5dc6a93e96dac242e51443594f2705
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:17 +0000

    disturb target interview scrape favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import sys
+def print_python_version():
+        return sys.version
