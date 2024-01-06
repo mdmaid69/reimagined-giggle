01bf2f6ff5ad6beca0eb290afe32d374cc839088
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:45:02 +0000

    lookaround yesterday past film Kk

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
