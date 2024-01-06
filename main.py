871ada67337e2ae81e4b1bebd96da8bb93bec91c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:52:26 +0000

    small sky control imagine strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
