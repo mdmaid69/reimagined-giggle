8ceac0d3d6fce5a423ccbafd48e82f10e8ecfedc
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:40:52 +0000

    sort basic join money peace

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_ceiling(x):
+        return math.ceil(x)
