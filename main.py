4e8a29067f91648243eb94550db46beb48c3db75
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:13:01 +0000

    mind duty occasional pleased comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_min(lst):
+        return min(lst)
