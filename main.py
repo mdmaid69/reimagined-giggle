15d25c6ea884062ca7692f501f023825650603c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:42:17 +0000

    confusing rob send top accidentally

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_int(array):
+        return int(array[0])
