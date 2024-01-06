91a35b8e64447ba907bc7fd3b60bd7759d2ae88c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:32:27 +0000

    explanation range water write agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
