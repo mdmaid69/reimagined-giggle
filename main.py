2d73638db5e81b72744d2c9a7b721e048de03858
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:00 +0000

    blame everywhere square push steep

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
