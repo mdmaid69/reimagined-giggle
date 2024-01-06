1389ed450060cf974548ee1fe38d03646b873a86
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:40:32 +0000

    Pp my habit apologizeapologise hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
