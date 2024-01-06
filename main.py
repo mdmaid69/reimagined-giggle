e469af1e3b806908af3a9e8a9978c61b6189909b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:15 +0000

    please spend getback unlucky sail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
