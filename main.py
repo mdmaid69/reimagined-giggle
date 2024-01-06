b502ef9f4d54f99380664bb2bec0889c3881e77a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:22:03 +0000

    there past kind affect hungry

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+text = "Hello, world!"
+print("Characters:", len(text))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
