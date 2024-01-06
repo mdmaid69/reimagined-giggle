4b11e080b6a37d043d791560d70031de812ab5d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:32:41 +0000

    stand morning chance nonsense reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_typecode(array):
+        return array.typecode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
