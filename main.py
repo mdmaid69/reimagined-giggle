09ebd7b18a1294e82ec06672abf20f35de00528d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:36:36 +0000

    imagine exact fond their cheat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import sys
+print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
