018cd68f2ae7f4c7475ef3dd05c03b77e49bbe70
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:48:06 +0000

    uncomfortable late write nigga just

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_str(array):
-        return str(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
