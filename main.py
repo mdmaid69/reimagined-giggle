063840bb263bc3a5546b8611955c0879de1ced17
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:35:38 +0000

    instrument face illness godown better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
