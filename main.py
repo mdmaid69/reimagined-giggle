065fbf79298d61c4c843a8ffac6208a29b958ec0
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:45:15 +0000

    tongue future give minimum keen

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
