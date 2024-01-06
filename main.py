e0fe28b3b74461744dee8c6f9e77423393e421e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:11:24 +0000

    expect remark true dry pretty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_work(force, distance):
+        return force * distance
