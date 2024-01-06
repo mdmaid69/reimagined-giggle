d006176b3f2a54e8f36701e291f7236af135c108
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:26 +0000

    brain unfortunate pain false string

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
