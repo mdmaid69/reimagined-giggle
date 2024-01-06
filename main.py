8ae954665a5f0d3b03b0df2661c7e920f2b1617c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:26 +0000

    keepup collection bank wing common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
