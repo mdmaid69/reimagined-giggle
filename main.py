63e2e5a89c6ca5327e834d1ce030251ba95ac7a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:27 +0000

    cheque sting activity house before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
