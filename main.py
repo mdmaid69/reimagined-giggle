0163b6ca203f2447d066555a99a41383a579da55
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:05:25 +0000

    decision agree popular awake advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
