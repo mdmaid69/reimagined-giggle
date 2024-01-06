0c0fc9a0bd84e35882af66cbf8929cfef0069962
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:57:34 +0000

    beautiful burn trip pin exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
