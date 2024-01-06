84a15bef6f2a3f490960791a317ddde66a308973
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:11:13 +0000

    wheel tip broad murder blame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
