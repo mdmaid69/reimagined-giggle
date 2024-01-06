0d9e85432502412160b39250e3e2bd6ca6fc969e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:19:29 +0000

    tragedy turnup sorry keen address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
