e9a67dcfa74d92835bdbf11539a4a099f97e397b
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:46:59 +0000

    progress worry record entry obtain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_density(mass, volume):
-        return mass / volume
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
