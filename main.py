528a0616e9cc102817215e7c5adf1809209aaf95
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:29:36 +0000

    flood reply vote break interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
