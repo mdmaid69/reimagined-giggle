f80d9e39e4beec709cc1f708d7d8ef0e1124d54b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:44:29 +0000

    pinch sell such wheel comfort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
