448d57fa88433fcee48b022eaeda1d8ab9f16bfe
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:27:52 +0000

    seat his sleep soak promise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_repr(array):
+        return repr(array)
