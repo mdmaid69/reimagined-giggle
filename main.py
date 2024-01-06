48ca8ce72278005e035779027817fb751ae3d6f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:32:54 +0000

    whisper plain direct rush actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
