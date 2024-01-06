b153a0ec7ff82ee7bea4d8a237e268d9b57dd71f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:38:22 +0000

    twist Mm your relief disagree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
