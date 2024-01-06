0b795da7a12518f2a3a7638da956773433e2a48f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:42:49 +0000

    building goaway thanks poor breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def add_numbers(a, b):
+        return a + b
