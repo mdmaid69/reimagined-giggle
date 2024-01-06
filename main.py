d54de52536480dee86f83687f9c2e61dfb13aae0
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:25:03 +0000

    sad no unless point comein

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
