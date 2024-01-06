5d5f0bc2f32212591cacd72bbce48bf06bfd65f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:55:35 +0000

    mother listen slight second building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
