c670acbc9ad6aba6ceac91a906ef66e652e3d27e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:16:05 +0000

    complaint ability belong public hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+def find_max(numbers):
+        return max(numbers)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
