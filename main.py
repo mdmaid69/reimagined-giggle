898584cccb21cf0c002599fea03b038d902a099e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:00:23 +0000

    judge test avoid blame so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
