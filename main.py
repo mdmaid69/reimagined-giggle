bd833c5274fb21a1032d3a4674e26df22adfc2cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:27:31 +0000

    interfere hungry turndown surprise introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+print(random.randint(0, 100))
