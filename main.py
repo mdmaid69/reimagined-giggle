13c88054c421d74fa4a5baa5d90fa56592c36281
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:23:03 +0000

    danger lean welcome wall dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
