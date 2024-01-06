cfc9ba52a468ed378f01461f9ffae5bf2ff1d3b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:57:13 +0000

    relief rule boat turnaway wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
