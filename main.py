28961d8d396f8cdb6515942847649ea0f1b187ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:05:31 +0000

    free pay climb turn free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
