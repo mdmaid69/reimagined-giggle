b745da8019c9e3f4a7ca8c5e96b5325bbfb56940
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:27:13 +0000

    motion explain charge authority afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import time
+def get_time_since_epoch():
+        return time.time()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
