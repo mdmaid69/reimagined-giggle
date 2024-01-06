423442f4a9653326c663c0617f1715d654fa262d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:44:24 +0000

    recognize seem advice uncertain grade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
