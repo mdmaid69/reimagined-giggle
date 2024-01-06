373c0c38e97b915745180733448f78652487147a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:00:45 +0000

    view plant for mistake life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def get_array_length(array):
+        return len(array)
