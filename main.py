5c26fd555bd0fa5f5feb3613316151cf0e00fda6
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:39:47 +0000

    mistake notice passage apart suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
