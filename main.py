b29e9c8487a92a89889f751427ee375a209dc9dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:47:05 +0000

    turn smell match combine although

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def reverse_array(array):
-        array.reverse()
+def sort_list(lst):
+        return sorted(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
