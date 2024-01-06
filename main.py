51c09c44ef4a018797c7b24fcabb28402212561f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:18:00 +0000

    before software quality after side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
