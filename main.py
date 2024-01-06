61ebc2edf91d5e77bc47ea36fd9bace3137e9e52
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:26:01 +0000

    disgusting tape pencil or physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def clear_array(array):
-        array *= 0
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
