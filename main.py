4945286aafc9ea640b546a71c7f1313c45c811b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:09:19 +0000

    eager cook let confuse art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def convert_to_hex(n):
+        return hex(n)
