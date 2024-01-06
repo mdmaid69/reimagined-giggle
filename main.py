5c80abea12be4da8e4ecabc93399f49829fe878f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:47:36 +0000

    last organization violent size platform

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
