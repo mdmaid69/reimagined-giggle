b40d5ae1459b231e6cb67a07f3fab6a5f0c1de35
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:51:25 +0000

    stop near conclusion mail mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def add_numbers(x, y):
-        return x + y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
