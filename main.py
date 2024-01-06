688f5413dbf21f344d3fdcee6331642fc37ff78d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:06:29 +0000

    woman offence understand vision opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
