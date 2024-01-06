77e3fb68085788fe7c7d4fb5c32e01cbe8dd3568
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:30:00 +0000

    worried no occur mend length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def get_array_as_str(array):
+        return str(array)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(math.pi)
