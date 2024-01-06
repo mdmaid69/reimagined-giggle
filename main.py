a7f644dd84a09dc8c90bf1de563b701e13ca1dcf
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:54:47 +0000

    treat give discussion out official

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
