e76bc74f98c1711724050251c248e5fd8a7c8878
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:03:09 +0000

    paper emergency wire loud window

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print(sum(range(10)))
