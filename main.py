9c0d1d6f6bef4e71c81b9087c83f785414a06cb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:08:35 +0000

    picture worth sink break fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
