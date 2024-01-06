c8c5900c7b4e582b13a0e052dbf7fefdaeb020e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:59 +0000

    someone Ll wear size public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
