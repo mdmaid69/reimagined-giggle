13859f566429ed4c3b8dae60882f8a3e97e0f715
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:32:31 +0000

    once imagine defend accommodation well

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import time
-print(time.time())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def remove_duplicates(lst):
+        return list(set(lst))
