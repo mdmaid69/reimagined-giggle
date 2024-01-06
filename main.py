41cf0c52f8d4104c08855d727bfcc79bf287bb3f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:51:29 +0000

    hungry favourite walk season breakup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_power(work, time):
-        return work / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
