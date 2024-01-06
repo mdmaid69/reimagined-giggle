836265850698b73ad24a3342a776ea6e9100c345
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:30:56 +0000

    rare getaway divide common disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
