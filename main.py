4ad6c4d7d30215e05f1cfdc67c4fe836db9fafeb
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:21:41 +0000

    feeling tell swim refuse spend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
