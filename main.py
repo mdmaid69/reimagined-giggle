58c48b090579f72daadb463a739ef9d45109fe70
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:45:43 +0000

    extra may become treat major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
