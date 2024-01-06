25f8f65ea7c2ef26a068afdc1c00878aa4f15f60
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:29:49 +0000

    past dig sorry abroad nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_typecode(array):
+        return array.typecode
