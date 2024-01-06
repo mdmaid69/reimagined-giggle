9174b77efdc6169bf184d6d1b1f8e92e70eb1f65
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:40:26 +0000

    defend wrong but reject knowledge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
