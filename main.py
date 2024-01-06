c6d28fe3979d4b858e4b416ba7d0a7eefd012334
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:09:57 +0000

    near experiment final whatever burn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_min(lst):
+        return min(lst)
