da2fcb8ac6a5c70a4f85be2050ee455c99054e44
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:38:51 +0000

    distant aim ring loan identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
