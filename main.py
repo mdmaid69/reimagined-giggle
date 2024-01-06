0f9c51e2a8b0e932984a9cf98de14e2134f3ad85
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:12:04 +0000

    number comfortable father test combine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+print(sys.version)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
