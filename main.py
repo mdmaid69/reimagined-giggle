60d4ef9aa120b115eab21dc3699990c0c95f5c8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:41 +0000

    evening beneath and wing music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
