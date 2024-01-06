008fcd7ef4c0c431fee236322f1b0bf3f01e1997
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:19 +0000

    consider before information married fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
