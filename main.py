86b5bd2c648b7e2a7e1e4ea59c99dbf4683af7e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:07:11 +0000

    front finish sticky concentration concerning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
