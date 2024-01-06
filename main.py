8763d7ae325e69d82217cc85439730082e2a48da
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:11:38 +0000

    right coat morning crazy nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
