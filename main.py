687cdd746b6d519331a7229b2dbe433134c5c1c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:32:48 +0000

    create thankyou partner imagination Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
