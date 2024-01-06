c5dec60884f82244532573c7c3eb98c8ccec5745
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:48 +0000

    possession guard getback release fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
