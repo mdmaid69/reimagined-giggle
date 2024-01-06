4fd4487240b83b4da6d4f89634e5c16c681112a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:11:07 +0000

    bunch train slowly aim road

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
