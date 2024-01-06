2c13ed744ca88cce9b6bc01d5fca9659b287d501
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:09:27 +0000

    whether never control completely couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
