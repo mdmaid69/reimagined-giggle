eb56532b5432cf50adabbfdaa53319414b67100c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:01:43 +0000

    block content community perform breathe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
