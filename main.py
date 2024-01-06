d908fd6c0377daaf483825fefe7c432513f60a58
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:27:44 +0000

    surprised away fight well cruel

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
