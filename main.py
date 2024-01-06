16b6837fafa189f83c15d4403e19a49818083353
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:28:16 +0000

    temperature exercise talkinto nerve everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
