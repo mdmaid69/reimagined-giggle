3452a4ea30b8918720a9cb1bd83919b8ae73430b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:24:03 +0000

    exaggerate glad fortunate slightly Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
