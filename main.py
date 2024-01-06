56881685c061e12cfced72e00fa5e7568af17ad5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:27:34 +0000

    by jump male active short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
