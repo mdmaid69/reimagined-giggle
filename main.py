6617baa10f6385318dcfd174d853b98941a66271
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:20:29 +0000

    keepfrom lead lookafter Ee match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
