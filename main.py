d0e22208c86bf07f50c0517d8bc4f6ce3a55c028
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:19:27 +0000

    sweet might convinced kill stay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
