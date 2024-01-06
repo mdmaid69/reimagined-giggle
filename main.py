a8d899b46bdaed3e026fe2ae4e8be7e8ff6e2c2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:10:18 +0000

    advise fear polite naturally cause

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
