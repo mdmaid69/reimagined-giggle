2559709fd2cfbbe1b117742a6994bfb4ac06e2e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:22:51 +0000

    pity represent organize interfere power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
