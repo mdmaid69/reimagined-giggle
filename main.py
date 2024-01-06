5f845e77b407df36402e336dcc0faf4f1b5c28a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:04 +0000

    manner fence outside charge meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
