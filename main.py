ff879ac6613e9a5fdc6ab25b2c6b025564edfde3
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:05:36 +0000

    unite shallow trade condition style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
