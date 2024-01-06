6c3ed99cda916d837249b091dc64c894964a6a63
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:01:19 +0000

    date break interested quiet too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
