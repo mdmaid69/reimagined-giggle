e3aff1a3f5996f128bfcbbe0129ee5e090be0604
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:19:26 +0000

    after argue handle mistake problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
