6c55555ae7f3a424e3117453039a720e783f4eb9
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:06 +0000

    cinema whenever fasten yet ignore

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-print(sum(range(10)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
