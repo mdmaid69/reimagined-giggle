519b012613bf6ac7c18c97c17785f0ba2b03b383
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:53:35 +0000

    cause place wrong Oo arrangement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+print(json.dumps({"name": "John", "age": 30}))
