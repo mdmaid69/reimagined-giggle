e61f5a67a42f0855584d0f22cb808681ac4b5929
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:51:41 +0000

    after public on shade clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
