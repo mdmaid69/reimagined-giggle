a884f024d8b643c1a867b2752761cf83084c027b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:23:17 +0000

    affect fix niggaacademy up butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
