806e53678400fa260d8b39e1b96f0052665c8f22
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:30:34 +0000

    round answer early check bottom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
