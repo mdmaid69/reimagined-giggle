1797c18a5108ce87f863ac1f48fce61f6c772200
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:56:31 +0000

    course criticism hospital dull fast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
