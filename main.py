e066f55c2d24b07731c4650d569520c68d7bcdd6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:56:42 +0000

    stand cap young contrast band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
