1c552001535ae809da9866733a8035c241b357d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:44:12 +0000

    storm gas food unkind direct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
