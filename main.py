0a51acb98c73abc10bb8087a13929d313d811634
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:32:37 +0000

    destroy punishment hollow combination drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
