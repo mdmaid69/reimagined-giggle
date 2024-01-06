bac55d97c26b6753204ab1038b173f7a104e7a37
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:52:59 +0000

    car mention usually software ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import os
 def get_current_working_directory():
         return os.getcwd()
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
