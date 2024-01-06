e3992fbf56b850786f11133bc04f7c06987dffb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:55:54 +0000

    journey round similar rubbish common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
