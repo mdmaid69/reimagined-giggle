e696ebe76d44fea2af36c50ff4a584659511472f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:59 +0000

    rest may pipe praise from

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
