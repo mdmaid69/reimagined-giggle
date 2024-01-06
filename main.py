d6fb5d42797ecb4681a4b1045d6342b217f8939a
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:02:21 +0000

    bridge fashion phone this wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
