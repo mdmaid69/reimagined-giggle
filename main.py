4ced729ba99e8315bfd53b8a9800268aef01c882
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:24:14 +0000

    recent skill lot act capital

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
