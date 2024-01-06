2e87909d985ee41cb838b794d1f8e914c9afc048
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:37:14 +0000

    admit jump soft please neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
