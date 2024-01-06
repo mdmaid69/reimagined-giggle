82879152cc3b67f73ac9add584148a26e838c5d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:35:11 +0000

    afterwards on peace turnback clean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-print(datetime.datetime.now())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def sort_list(lst):
+        return sorted(lst)
