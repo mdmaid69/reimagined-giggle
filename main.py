5c09944056c8025d40332c693c9f127cf756e27c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:30:39 +0000

    fresh shop sort callout without

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
