6600dbfe6781f0e2d8feba05bc4203391d4c3410
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:46:41 +0000

    muscle Gg today because turnoff

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
