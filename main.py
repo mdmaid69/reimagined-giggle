612aea6ea7f3d6b7fbcc92909a46eaa2293dbb0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:20:50 +0000

    concentration owe save fortunately taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
