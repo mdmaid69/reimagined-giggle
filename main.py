9565189ea3c1139460e1369ccd53f31337999f89
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:09:15 +0000

    backwards rough to force war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def check_if_array_contains_item(array, item):
+        return item in array
