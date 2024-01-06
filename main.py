8fd15feca7c39a383dbdacd9d476d1be1cf7f80c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:28:33 +0000

    trouble until cost relax room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
