f700d0f9daf1407905e1916e5989693f030814ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:18:48 +0000

    ticket breakfast comeup warm complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
