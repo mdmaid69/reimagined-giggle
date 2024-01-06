94a165b1cde6c37b6e106712cb214da29b572125
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:11 +0000

    blame instant mine fix giveup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
