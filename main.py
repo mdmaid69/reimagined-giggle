ea861bee21e67a8643956102237f9600ccf37d3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:26:24 +0000

    Vv translate holiday below value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
