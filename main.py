8f135c4a69e5a6e71495d9988e69bb543b1ade0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:16:20 +0000

    bill fortunately actually certainly geton

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import sys
+def exit_program():
+        sys.exit()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
