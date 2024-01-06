ad95d70b1052b28f2b285b908a8b6cb2cef55138
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:17:05 +0000

    carry niggaacademy clear official either

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
