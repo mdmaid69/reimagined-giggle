75d6e86d2870cecc05b629b69468bcc72e096f55
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:20:28 +0000

    state attract runinto satisfied also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
