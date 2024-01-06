ced2d2e5bcc143a4895cfa3ec9487c546dcbf66e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:57:42 +0000

    rubbish maybe can wrong more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+import sys
+def print_python_version():
+        return sys.version
