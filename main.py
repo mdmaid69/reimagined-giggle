6f269d46d5233049d87855d2c30236831d14dfde
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:47:36 +0000

    stay artificial needle shape round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
