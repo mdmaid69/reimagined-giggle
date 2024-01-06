2548a28a547ebdfc8de11b6be04256bd3d55627e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:22:06 +0000

    night pool noise comeround exact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
