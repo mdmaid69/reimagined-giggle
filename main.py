905c7c9414486240078a6d0bca319bb322be1114
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:04:00 +0000

    tell Vv from tread sell

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def sort_list(lst):
+        return sorted(lst)
