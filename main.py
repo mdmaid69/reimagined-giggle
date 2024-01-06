5e5b01cd4c8df428990626234253e18acb629baf
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:54:07 +0000

    surprise kind shape splash letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
