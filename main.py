e2521858f34b8c6681458e85404d14d75628d9a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:26:55 +0000

    public giveup fault face goon

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
