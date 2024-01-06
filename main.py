266c47ce08d4422d775ede6cdd3bc2b073373391
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:04:35 +0000

    base goback(to) comeback operation thorough

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def square_number(x):
-        return x**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import tensorflow as tf
+print(tf.__version__)
