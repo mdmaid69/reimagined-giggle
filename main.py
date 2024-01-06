ac22c767cc4720d8cc9f80f603198b694e34808b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:35:17 +0000

    must appeal chin that telephone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-print(math.pi)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+from collections import Counter
+print(Counter("hello world"))
