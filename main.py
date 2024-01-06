6a7716bcdbeec2392fbdf4fc64b5cf8dabbcf4e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:33:19 +0000

    confusing in along lookup soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
