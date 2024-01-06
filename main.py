716a837363f1379f5ac8c96623820b7347a6500c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:06 +0000

    idea talk somebody angry plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area(radius):
+        return 3.14 * radius * radius
