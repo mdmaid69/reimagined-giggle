01787701012db1e685da6527efaf30e59e0c711a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:07:50 +0000

    ready list pass trade actual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
