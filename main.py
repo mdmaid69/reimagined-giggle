8a1d8d5e00210b06f95caa39a4839f7a0e90cd7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:07:19 +0000

    might mend majority after offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
