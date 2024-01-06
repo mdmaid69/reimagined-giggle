89d86f88a4bd2804364f4ba2a4c4362deddc1cc3
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:33:37 +0000

    upstairs world could address lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def subtract_numbers(x, y):
+        return x - y
