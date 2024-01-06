cb0cd431c60aaf9384003ec0d785d4c70b05ae8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:26:23 +0000

    backwards train behind measure lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_work(force, distance):
+        return force * distance
+import getpass
+def get_username():
+        return getpass.getuser()
