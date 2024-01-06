aa3935e408b9d5b90b4dd65c410f991c5ca6a5d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:41:39 +0000

    trust remark stage object float

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
