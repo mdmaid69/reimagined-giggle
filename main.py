027c7b4cd2509da7f7eacec6001c35fe632d4365
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:52:48 +0000

    unit many swallow fix injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
