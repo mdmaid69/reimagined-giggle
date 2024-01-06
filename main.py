cdb3bd88f626525431cf6478ce86f306cc423500
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:11:40 +0000

    immediately pattern swimming flight share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
