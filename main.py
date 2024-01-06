a48705258324a3143741d48cf812fdb48dde244c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:18:27 +0000

    cut couple week workupto turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import time
-def get_time_since_epoch():
-        return time.time()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
