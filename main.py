49aa261bc18a5f09a7aad4cade9335b83ea194db
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:20:23 +0000

    actual obtain basic injury similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def find_min(numbers):
-        return min(numbers)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
