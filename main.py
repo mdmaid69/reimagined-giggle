44e3061eb3fd85a81fa6ab58836eb95745e69b97
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:25:25 +0000

    bet disturb vote fire figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import sys
-def print_python_version():
-        print(sys.version)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
