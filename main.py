af3ad6f1832048309263810b1426c5a5fc6355dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:06:43 +0000

    second example union form kind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
