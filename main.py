90ad0da7470e6d1c10519886bff245a7b196a222
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:31:11 +0000

    prize keepaway rich proof Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def cube_number(x):
-        return x**3
+for i in range(5):
+        print(i)
+import os
+def get_current_working_directory():
+        return os.getcwd()
