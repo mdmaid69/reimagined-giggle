3892bf9c944fb584c8144583238c09139a54a810
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:16 +0000

    as door disturb beyond satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
