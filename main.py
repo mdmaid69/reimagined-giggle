7aaad78be9c25f820bcc66b3e914ad8c691acbac
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:05:18 +0000

    wander mind tape life glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
