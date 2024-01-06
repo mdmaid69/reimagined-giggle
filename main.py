06b4e85f9305e3c8b91bb5cb14ae342a723b53c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:08:02 +0000

    neck pressure suckme apologizeapologise supply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def convert_to_binary(n):
-        return bin(n)
+def convert_to_hex(n):
+        return hex(n)
+import random
+print(random.randint(0, 100))
