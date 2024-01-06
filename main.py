b9d239a41629e5486adbd5c643e9c46834787c54
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:31:03 +0000

    excuse class passage last if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
