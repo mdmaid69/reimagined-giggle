6668579bd2532410ab1b7c3a0a4753a442625d40
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:45:37 +0000

    final possibility instruction concentrate job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
