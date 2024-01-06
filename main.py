b2367fbe122cda2173a1ad8347aeadf4a8c6ac84
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:18:35 +0000

    open later left star appeal

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from collections import Counter
-print(Counter("hello world"))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
