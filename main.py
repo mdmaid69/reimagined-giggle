a055d1ac5921841bdd3aabc5c8cd0b05783c84c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:27:34 +0000

    land afternoon alone deliberate track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_exponential(x):
+        return math.exp(x)
