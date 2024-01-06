14bbd17d1cf4daf3fc05cc941f8f6ef936a095e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:30:16 +0000

    disapprove insist sensible odd answer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
