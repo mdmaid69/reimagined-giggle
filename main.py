ba179016ad50ccbf7719083ee40f0e35082c4e11
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:05:53 +0000

    proper even understand unfair keepup

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_counter():
+        return collections.Counter()
