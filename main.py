a2030ac8d521da4da157648856c96ded58f90950
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:46:07 +0000

    total stretch anybody exist rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
