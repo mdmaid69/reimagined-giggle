8e6c436fd86829eeed14297f9e181c549cf264e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:00:31 +0000

    moral actually high sad broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
