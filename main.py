8aee8f8961d598bde334b7d1a6d6fb7fd7eca022
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:38:32 +0000

    into what break lend save

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
