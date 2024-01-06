2bb001a6edc3f6be3b52fde46573608d0a3a3eba
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:29:30 +0000

    rest straight sink usually quantity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
