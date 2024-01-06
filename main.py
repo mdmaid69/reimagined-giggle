4642451c5196a2bea8ce49805b6ad9bb91fca4f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:44:11 +0000

    achieve ourselves Pp stare roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
