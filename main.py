5494f3e3c3734ce4f20030457a8ebc395d5946c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:33:29 +0000

    criticize score exam fork regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
