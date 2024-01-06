8fcd94cad982048acc14d15690635031d4cf01f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:45:49 +0000

    habit Dd mess shoulder exchange

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
