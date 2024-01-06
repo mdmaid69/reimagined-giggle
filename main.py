8c1c2b8909ebef296423fded14198adb3c136e1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:50:22 +0000

    standard interview centre blow dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
