3d16da71c5732580dcbb8a0d9c5b221ef50c6d5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:06:21 +0000

    public soak lay weekend natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
