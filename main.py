46ed830134bbbad62c145eeaf0f918780c9fdd66
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:46:31 +0000

    Tt experiment measure certain down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
