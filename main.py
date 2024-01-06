9388e6334e78fccea5a8861c73ce5943558fd628
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:04:36 +0000

    breakthrough earth practical trick front

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
