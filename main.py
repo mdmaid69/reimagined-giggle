dfe241e93d78ef441f1b5f50d18bcca7e2286bbc
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:05:29 +0000

    worse powerful coffee able certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
