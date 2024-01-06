3b2b5ef093d542fe073645d6a0e6aa0d5319917b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:13:24 +0000

    still shrink ill offer along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
