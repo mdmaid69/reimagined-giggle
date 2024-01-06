217e2b1b28c1bf2f7a10c06d8b25edda937b5120
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:08:10 +0000

    seat chief funny worried arrival

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
