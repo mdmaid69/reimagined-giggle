2bdbd9df3693b0384bae9200862e8e25912dcdef
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:56:42 +0000

    suggest success wind soil most

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def convert_to_binary(n):
+        return bin(n)
