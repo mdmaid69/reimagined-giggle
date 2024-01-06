57157c40ed896955ee4330777f5400d929469726
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:55:41 +0000

    callback square impatient window shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
