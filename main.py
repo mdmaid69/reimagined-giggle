1a1c83cc9f555f02ebf4a818c93c60f05e4d7078
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:12:56 +0000

    reliable exercise old stop comment

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
