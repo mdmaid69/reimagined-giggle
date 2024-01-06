a754feb2188763eef456cbca003e20094f1f47d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:05:05 +0000

    sentence preparation object wait behind

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+name = "Python"
+print("Hello,", name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
