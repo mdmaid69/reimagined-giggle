b1db93b914cd2f941a9ad23943d676d5931bade8
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:00:28 +0000

    cut hit cut reserve wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_file_size(filename):
+        return os.path.getsize(filename)
