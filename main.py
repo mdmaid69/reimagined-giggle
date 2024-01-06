0aebe81b1ca0eb701273b0ed91461413199b18f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:05:11 +0000

    strong everywhere damp emergency event

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import time
-print(time.time())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
