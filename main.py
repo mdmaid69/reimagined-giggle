eb319772a4f05b4de5590798fc4af7d3e2e5f1e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:46:20 +0000

    importance earn like nothing occasional

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
