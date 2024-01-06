7c96405104e24dfe1405c7cd24fe655806f2c45d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:00:49 +0000

    considering poor be takeover main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
