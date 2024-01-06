5aa964119684b9cbe79d49c0b5e5ab3d4e7f9f7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:50:24 +0000

    worse fork scream swallow spin

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
