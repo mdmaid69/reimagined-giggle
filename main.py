7eaed561336ac31de9a5232dcb60179e46e00bdb
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:12:38 +0000

    play post across real admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import re
+print(re.match("h.*o", "hello world"))
