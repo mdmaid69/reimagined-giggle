b28c472f56608128eeed110774d86890783b1660
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:41:41 +0000

    like may beginning sorry journey

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
