f44b78512b888e3291097f48b6a57f8b8cca5417
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:12:58 +0000

    label room night waste whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
