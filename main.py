1433b668ec0c699875f44177d94cd08778640f7c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:08 +0000

    ready bury maintain opportunity figure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
