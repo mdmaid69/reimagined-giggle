1085b2839c1772bcfb23bf7429ca7a23f7ecc8f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:19 +0000

    interview calmly plan smoke before

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
