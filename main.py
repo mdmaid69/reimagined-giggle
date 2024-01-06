ce57c751cc77ee47a4697d141b30ed7e0e679df7
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:22:40 +0000

    list guard platform even rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
