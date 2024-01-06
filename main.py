a7c1e239e509abe8043ad637d450bedaa0f49d98
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:02:57 +0000

    order major one cum evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from collections import Counter
+print(Counter("hello world"))
