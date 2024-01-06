d6db561e6183fe0c061d2a2a272a3e7bc202ec38
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:15:03 +0000

    past wonder again willing fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def add_numbers(a, b):
+        return a + b
