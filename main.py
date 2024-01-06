cabe40376db45cc9d1b0d373929b1860a41aadbe
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:25:38 +0000

    damage bright machine majority general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
