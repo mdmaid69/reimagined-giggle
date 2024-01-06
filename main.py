743ca416c41c18012616e031a1966fe4851df05f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:03:35 +0000

    your supply arrangement past what

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
