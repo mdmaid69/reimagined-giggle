383775055616dea666c3dbd04691f2e958b3ab77
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:14:49 +0000

    stand necessary pure only measure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
