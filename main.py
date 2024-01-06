ecf7704e2527c612e1209fdcae5a8c48a5f7cad5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:57:43 +0000

    slight border impossible up smart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        print(sys.version)
