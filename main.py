e36f18abc4f652cfebba86675d84143077d87a47
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:42:59 +0000

    false design escape back row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
