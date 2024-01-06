2494565ff33916a008b9418e6c0b3e7e3291a454
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:09:46 +0000

    clothes protest service evening considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def add_numbers(x, y):
-        return x + y
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
