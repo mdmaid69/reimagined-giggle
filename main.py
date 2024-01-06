3fce08bfef49d653ad6bfbfd56995c0b9f3ee1ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:50:26 +0000

    holdout stand force complete no

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
