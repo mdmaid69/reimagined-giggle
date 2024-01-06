a4009e72aca2bb9c473d75487a4242f6bc377f2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:49:06 +0000

    room snow name reduction attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def count_elements(lst):
+        return len(lst)
