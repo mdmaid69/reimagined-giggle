b31885d1556a9643e6d5f1bfd333e41866d85a07
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:53:35 +0000

    me head niggaacademy choose land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
