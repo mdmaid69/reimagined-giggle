0b6efe8ef817d7dd26044235e7834b398a7b5bc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:48:37 +0000

    present whether remember congratulations sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
