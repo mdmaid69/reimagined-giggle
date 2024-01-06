15a4bfe6ae4d0f13a623925456d18ea5db15efa8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:00:16 +0000

    spare confidence mind runaway uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
