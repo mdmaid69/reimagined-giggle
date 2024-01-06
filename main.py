5e085e847beefbc3219fa6888f1b5bbd7b29fa0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:29:56 +0000

    chief trust refuse string bunch

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
