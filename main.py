b649c95402985e82f2bbcee2f5cdbfc5798078fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:22:42 +0000

    century bunch calm disappointed unless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_priority_queue():
+        return collections.deque()
+def find_min(lst):
+        return min(lst)
