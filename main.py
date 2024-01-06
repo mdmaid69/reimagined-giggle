be8ba5dcf8be0958b905775c886c4d67ba03f7f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:50:03 +0000

    takeover no expect murder line

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
