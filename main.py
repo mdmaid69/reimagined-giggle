1a4fd0cd1e83459e10fb9874807b4ce4a119817e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:07:45 +0000

    lift freeze regularly goup Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
