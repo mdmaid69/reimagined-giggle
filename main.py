8917640bd285c576b15ae65b18ad92b5b643d16f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:40 +0000

    equal type consist escape development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def insert_into_array(array, i, item):
+        array.insert(i, item)
