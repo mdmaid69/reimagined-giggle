48dc3706c86907e26c38940ae0f78eb6e5bc53b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:37:37 +0000

    no geton replace cause sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_power(work, time):
+        return work / time
+import collections
+def create_stack():
+        return collections.deque()
