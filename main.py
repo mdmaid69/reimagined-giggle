9334ca752ceb25fab741f323ca1cfc0315bb9bb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:12:19 +0000

    near well see building check

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+print([x**2 for x in range(10)])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
