29f1b830e54e73137b106af19689521c39b13d39
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:25:23 +0000

    week Yy crime clothes age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
