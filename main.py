9196fb0eba009bff9339af8423aa5f8e7370f2de
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:34:01 +0000

    table win play blind snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sys
+def exit_program():
+        sys.exit()
