4b3b9484c045d1f590163cd0ee989ad251ec2611
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:36:42 +0000

    Qq arrangement range past work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
