327a2d128434f0aeffdf4daa37e689375ab00888
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:56:02 +0000

    happen false turnout contrast then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
