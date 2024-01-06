bed141771cffaaf091448199c24e9a56c4e44f9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:40:39 +0000

    rough runinto can furniture command

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
