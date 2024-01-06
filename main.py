6429ef05d9348dcaed11de9437bf348e9ecb5256
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:41:15 +0000

    one violent plain walk colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
