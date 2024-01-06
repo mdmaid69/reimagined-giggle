8b1722bfc1bc3b42aab757a241cf2be42c14ee7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:44:23 +0000

    extra ask hope balance listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
