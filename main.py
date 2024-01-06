b16fada27e4644a2c84dd4f0dc4cbbee34ebd42d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:50:05 +0000

    exercise moon mend tragedy middle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+def get_time_since_epoch():
+        return time.time()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
