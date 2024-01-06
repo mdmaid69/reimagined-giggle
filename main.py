6bd8949ec9bfcd880a0f77cb6781302b7f026958
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:36:14 +0000

    secret unfair bite swallow possibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
