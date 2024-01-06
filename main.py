97e75fdec71f304baa0f4e3fbc5db98e3f24729e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:08 +0000

    factory refuse person comedown common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
