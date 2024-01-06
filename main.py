c70555efb8ec34ad540cfc13971c744c00158a00
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:10:01 +0000

    else hurt schedule realize plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
