99e0a761b327a520697715532130b8a055a94aaf
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:26:11 +0000

    letter belief together offer shout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
