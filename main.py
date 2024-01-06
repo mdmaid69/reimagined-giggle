b2cea3871418f1103e8e79a19e224deab673def9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:13:40 +0000

    petrol poison side fasten mark

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def reverse_list(lst):
-        return lst[::-1]
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
