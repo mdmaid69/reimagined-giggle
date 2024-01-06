c7de996c10724dc75ebc667c699b883345db8c1a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:42:21 +0000

    actually accordingto used direct sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
