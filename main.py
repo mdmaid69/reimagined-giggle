0e9388720808f295af1da0affb38a46e54fd0a1d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:28:33 +0000

    clean near frighten straight why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
