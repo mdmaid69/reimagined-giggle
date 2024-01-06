c97f58bf6c746446d880a5eb918c18dd57e94857
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:58:02 +0000

    disapprove position pinch face towards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
