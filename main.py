ca2889e39b91d4455f4e1ef5f62bce68edacd9c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:01:17 +0000

    centimetre any advantage makeinto flight

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def square_number(x):
-        return x**2
+def count_characters(sentence):
+        return len(sentence)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
