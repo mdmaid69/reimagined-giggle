f6af7690f625a38b8c3b23f0fc04bbbe19a56806
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:01:28 +0000

    description in spring somewhere comeup

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
