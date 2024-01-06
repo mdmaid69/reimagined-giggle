b7b19dd30322a9500d242fe28a87aa21b2d441ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:59 +0000

    turn plain wish inform strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
