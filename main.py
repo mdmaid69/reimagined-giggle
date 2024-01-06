30bbd734ae39d8238b849a31b0fcf2f5f114f487
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:41:56 +0000

    common confident nigga nature trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
