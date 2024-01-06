90db06c57623732a173b781aa52420338fb7d246
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:12:15 +0000

    accordingto pencil develop behind anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
