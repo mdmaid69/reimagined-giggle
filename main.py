b00ff2330b23402391c3b2c4131afef1e507b5d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:47:14 +0000

    meaning whoever tradition thing trick

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def cube_number(x):
-        return x**3
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
