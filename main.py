04acf6b77ec11bd420303192781993873c4e1423
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:42:21 +0000

    goback(to) dream fill exercise equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
