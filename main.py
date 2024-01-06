1bbdffbef42b245f375a2848e46a2160be076b17
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:09:48 +0000

    truth group plant precise pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import os
-print(os.getcwd())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
