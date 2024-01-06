1ace6ab87fb312f48c13a771d4b372026a6ca558
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:26:53 +0000

    convenient amount clothes cross pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
