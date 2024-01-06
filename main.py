afeafae2c070d9dcc906f1368c031efcde74750f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:15 +0000

    capital blame start straight spread

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
