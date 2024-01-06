f1bb21b1e675afcd188a5e284840778954f1171e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:57:02 +0000

    but pocket keepfrom waste already

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
