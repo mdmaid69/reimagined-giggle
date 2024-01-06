3247f5f8ecc017fdbea483c49e0c672db93ad28b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:12 +0000

    piece pen whistle subject who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_str(array):
+        return str(array)
