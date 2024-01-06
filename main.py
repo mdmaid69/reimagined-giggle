1402ada0f723204337178b458a8b86d7b4a11778
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:21:10 +0000

    idea afraid old hospital criticize

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_acceleration(speed, time):
+        return speed / time
