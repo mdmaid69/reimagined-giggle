7ee84a7fc34cbc591aeb9258514a4ffee39f7f4b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:38:33 +0000

    far mistake part against tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
