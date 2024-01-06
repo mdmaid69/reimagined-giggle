e72db5f4383f880c3b1caa5d086cbde4893d1f4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:25:59 +0000

    press bush because machine cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
