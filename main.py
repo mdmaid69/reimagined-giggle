082ecb5a91e04cecdebcfe37fd21a3b44edc949b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:18:27 +0000

    satisfactory freeze value Tt failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
