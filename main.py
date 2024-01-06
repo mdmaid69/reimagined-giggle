5eb94e975ed13af685f4c1fe0f7bd132939b16dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:19:30 +0000

    appearance reject slightly test wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def square_number(x):
+        return x**2
