b38cf86be9ec3b267d5432dfe3ea5e54789495f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:56:52 +0000

    live hear claim do pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_string():
+        return collections.UserString()
