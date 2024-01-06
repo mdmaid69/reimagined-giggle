19f2a5e8d498f743d65bbf34233f57a43445fbc4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:10:47 +0000

    dress split factory absolutely post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
