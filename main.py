e3d106c7205075336e88199923b212b8c9c6bb4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:16:21 +0000

    more sharp split shame pin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def divide_numbers(x, y):
+        return x / y
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
