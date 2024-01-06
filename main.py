33e1b0238e431550a7b911b059bef3c39d02f36b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:43:43 +0000

    stage defend favourite young quality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
