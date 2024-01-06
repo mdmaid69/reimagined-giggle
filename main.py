d9a16012e19293b4588f13c902ee3db58744bf53
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:51:11 +0000

    meaning soft geton follow tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_list(array):
-        return list(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
