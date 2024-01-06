33838b538f62450d9afb9529c8ca83876c0458aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:44:28 +0000

    minute majority come dinner course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
