8639a4f19cc102d004743c1e262116e3eab12781
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:18:33 +0000

    behind stop lift level burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
