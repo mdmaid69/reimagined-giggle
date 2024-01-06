8341ef731a2fc16eec6464ac798c54b5def7c0ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:53:25 +0000

    thank snow as block operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
