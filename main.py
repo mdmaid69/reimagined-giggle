264c516556157f6ea778f0e0678bd77d0e96a284
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:41 +0000

    common excuse use valuable approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
