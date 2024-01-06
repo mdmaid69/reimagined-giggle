1104c404364e0f5439125710163181065e9c08f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:31:08 +0000

    shall so club under time

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
