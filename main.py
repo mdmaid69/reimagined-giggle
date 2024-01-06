67f6c47f0dfb1cf784979e41d83d9963cab373aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:11:59 +0000

    any run true public case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
