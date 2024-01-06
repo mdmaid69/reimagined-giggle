7af8ee7e17181cde6423e429bb6b8223a5b1cc66
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:56:10 +0000

    mad shame thinkof neither big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
