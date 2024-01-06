b32a599b4063d3dbefd4fb398ff03dd2c716c51a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:45:28 +0000

    normal reaction surprising characteristic into

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
