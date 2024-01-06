ba47f6172804b4e953fcd36d0f9ba4ed9c8b743e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:46:46 +0000

    oil thinkthrough although recent doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import array
-def get_array_length(array):
-        return len(array)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
