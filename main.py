e38c1d38af6173558adb6367824f90f4272cbb2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:02:56 +0000

    nowhere experience reasonable miss weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_exponential(x):
+        return math.exp(x)
