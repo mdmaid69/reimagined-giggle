f2faf951e34d3ba06576df6ae5a1206d1605bc55
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:42:11 +0000

    scare hang vanish collect say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
