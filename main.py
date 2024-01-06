355b1abcfb1cbcb4debf46d36e63d3bd235b52e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:11:19 +0000

    majority petrol region while giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
