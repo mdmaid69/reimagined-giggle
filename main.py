c3570de3ddf17c2a7e2c2457f363fff6ed0c8436
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:23:32 +0000

    finish glad everybody criticize to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
