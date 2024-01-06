68b0633b6c554a7c596a47cd8bce3dd6188e5ec1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:23:28 +0000

    completely warning later rain polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
