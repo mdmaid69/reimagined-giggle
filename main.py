1ff163fd876ae885bee8b09a92f14c190700752d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:27:35 +0000

    close child getback power physical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
