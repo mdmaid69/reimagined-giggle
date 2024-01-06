9aaf4220fe4405c78c80ee49dc2768c7d19e96c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:51:02 +0000

    prepare cut apologizeapologise OKorokay shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
