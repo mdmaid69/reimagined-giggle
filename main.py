9820a6bd09055464ec4bd5db97c1c9c6212fd276
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:36:38 +0000

    deliver somebody live education future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def get_array_as_int(array):
+        return int(array[0])
