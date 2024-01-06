e5470625378df19e64c5e624963a0b92d27181f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:33:30 +0000

    scheme Yy finish stop getout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
