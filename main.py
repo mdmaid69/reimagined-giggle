ab9ca97ae64efc907bea9957e1295e1ccec61fa3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:50:59 +0000

    see attempt Vv gift adjective

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def multiply_numbers(x, y):
+        return x * y
