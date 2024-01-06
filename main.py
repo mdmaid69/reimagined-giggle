38ec870f02fee76b0543459266efb358d6ad72f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:31:41 +0000

    difficulty command feature forgive prevent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
