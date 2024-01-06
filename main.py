d8cd1ca43ce6351b040bd28c911ab3e4c2baa0df
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:59:01 +0000

    passenger left phone wave permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
