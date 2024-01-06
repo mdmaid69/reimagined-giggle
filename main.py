2e5532810c94a81905b899929fe728c8e041574b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:17:51 +0000

    however complete yesterday treat burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
