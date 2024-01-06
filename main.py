3799313e82b383a6787e03db803aa87a1c364fa5
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:00:38 +0000

    silent plan 571enough wake disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
