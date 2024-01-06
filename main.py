41089bfb94ba0efdbf34f0c1bb2ca845710e34e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:10:23 +0000

    do none please weak nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
