132d512b01d3734a7e3598d3e709523c63b66451
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:51:47 +0000

    hollow press contrast television report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
