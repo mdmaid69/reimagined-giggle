3c4a7169d9aa830c95d90795e37ec6115379156c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:06:48 +0000

    cause hollow level tape weigh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
