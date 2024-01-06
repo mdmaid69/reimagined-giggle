3dbdc7d823927e24f1f02be72a0710d3f2315f60
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:32:20 +0000

    kind own interruption bring brush

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
