b2c27238f514fbfaf8b353319649c3f50023d7c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:46 +0000

    charge various classroom high action

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+for i in range(10): print(i)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
