485f890f74b2f6b390dd27c11e567a83514eaede
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:36:57 +0000

    her different bear secret off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
