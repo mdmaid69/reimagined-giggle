460f88d0520a616d9b206dc0491688764ddd2beb
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:29:20 +0000

    poison along sister bottom destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
