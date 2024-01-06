f144b70ed21edf6175662e1e8af423456b7a0039
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:24:10 +0000

    watch flat train beyond reliable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
