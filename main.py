cb73d7923a28adccea01fe6086faeac194f107d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:35:56 +0000

    terrible boil breakdown I match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
