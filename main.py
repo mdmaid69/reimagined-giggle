e6253d656a3b4ad0e162b35a6b7c8869c0f61e72
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:01:36 +0000

    quarter any about interruption together

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_speed(distance, time):
+        return distance / time
