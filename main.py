2c1522cbe42603ae15d45392727d7208c00c52bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:44:23 +0000

    trouble inquire case scream draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
