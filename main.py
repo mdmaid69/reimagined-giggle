05508746fe5b0f912651617cfc89f9ea6ed8ed89
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:44:24 +0000

    invitation off sleep best talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
