a386e8afc573a5f6acd0e22d063b2505b9d9f0e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:42:07 +0000

    makeinto project expensive excellent space

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
