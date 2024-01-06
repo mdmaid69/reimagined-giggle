e9194be14cef4cb824d076a76739f60c49485959
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:41:09 +0000

    bunch naturally file joke long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
