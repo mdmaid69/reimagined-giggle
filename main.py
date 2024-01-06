9b95c78f944849a1868018ab08a5db02113847be
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:02:14 +0000

    thing reasonable slice take thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
