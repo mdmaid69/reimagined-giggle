0958e0324bfce87ca16c9037d25827488c8862f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:53:24 +0000

    pronunciation bother shout factory she

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
