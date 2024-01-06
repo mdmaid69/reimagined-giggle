fd0f0cd50ae0ec8f0c442666221b95787c7f3fa9
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:48 +0000

    suffer game short lead way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
