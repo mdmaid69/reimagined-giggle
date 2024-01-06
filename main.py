abe4f8b0b7cb3decbebbd7d1f0cb27c8e28fa54d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:49:41 +0000

    date Rr perfect surprised bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
