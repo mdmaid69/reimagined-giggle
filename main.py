e9371a6c568e84f06f291c53eda60333aff72624
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:22:14 +0000

    doubt create name giveback 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
