0118c3f408f50763b6402e3c253772f593a5f519
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:41:06 +0000

    appeal birthday accidentally building common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
