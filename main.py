82b4c60451378ef3bb767084ec42a4cf676388dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:22:49 +0000

    bored collection disagree instant ticket

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
