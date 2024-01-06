60934182714e66e13e4446e045901b10f41a2112
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:00:46 +0000

    up wear kind usual compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
