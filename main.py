3f1e3d18aa9916bf60834b2780bccce457304720
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:41:49 +0000

    over spray book later famous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
