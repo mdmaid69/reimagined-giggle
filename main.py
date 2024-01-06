a8cff4c385c83b29c56509b4451e03998dda574c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:30:19 +0000

    real likely visit invitation passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
