a61f4b738fa0c30b1f356b1b54ffbd1ba76ebd6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:15:38 +0000

    either Itself 565end delicate subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
