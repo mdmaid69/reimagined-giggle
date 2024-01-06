4974905f3a4fe019b37b6e7e3d9a545acca75450
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:54:31 +0000

    experience willing declare aware equal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def append_to_array(array, item):
+        array.append(item)
