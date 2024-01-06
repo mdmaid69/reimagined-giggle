96159ad981687f2bb885bd91315b5f13b1801308
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:21:14 +0000

    she goback(to) stretch touch fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
