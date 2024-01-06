5da545dec3394f364e48469eaa1a1737e9884ac1
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:32:47 +0000

    average steal welcome fly embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-for i in range(5):
-        print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
