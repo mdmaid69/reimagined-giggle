c5ad7ba799038651b57182bb6b0eb19fc08b0777
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:56:19 +0000

    strange maximum appear taste comfort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
