29ffb006d08564b6e7dcd9ca928993ab1cfac3d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:49:35 +0000

    backwards low unlikely himself cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
