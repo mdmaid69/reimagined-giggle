488e3cdcfe8d8fa3ae228e2e76f70661c40fe5f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:58:45 +0000

    flavour regularly pupil cover good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
