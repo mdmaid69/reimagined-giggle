5a1530782fa00db236838676e388407d11c0932f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:38:36 +0000

    prize extremely drive similar exactly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
