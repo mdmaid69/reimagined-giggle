bdf011bad320c11fd3442e3d8601f8db52e875bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:45 +0000

    exit relative neat anxious trade

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_density(mass, volume):
+        return mass / volume
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
