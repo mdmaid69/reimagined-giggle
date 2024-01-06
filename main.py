cf46829055b04f73119621dba1045ec46cca2d7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:16:17 +0000

    no custom gift future apparently

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
