539f3f9ff14c55ea70008b5839826eff4dd86141
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:50:33 +0000

    pull influence give visit protect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def reverse_string(s):
+        return s[::-1]
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
