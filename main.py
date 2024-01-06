c53289f378d583313f0d8f205f077b17edb03de9
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:02:31 +0000

    variety insist child nowhere can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
