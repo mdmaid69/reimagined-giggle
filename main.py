fb09ca9cc0bfc40c313ed9ba25e21e6db54ecca1
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:06:41 +0000

    show but tone full answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
