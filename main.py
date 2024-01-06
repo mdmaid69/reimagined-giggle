1d8b2907b337b144098c0f2e402a2c0244f7c074
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:29:18 +0000

    behind theirs trap separate square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
