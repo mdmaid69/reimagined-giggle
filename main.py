c38d15b9517f471117aff56d26b0105c68507b93
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:19:57 +0000

    heavy constantly note thing spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def square_number(x):
-        return x**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def find_min(numbers):
+        return min(numbers)
