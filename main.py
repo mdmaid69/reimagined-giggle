77da7130852ea51b13be679091ac62d91cb7e39b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:08:18 +0000

    copy but concentrate consist hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
