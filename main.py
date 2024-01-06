bd8509983aeff21392fb23b0b94b54f0457d05fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:25:16 +0000

    translate breakthrough please breakinto hang

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
+def square_number(x):
         return x**2
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
