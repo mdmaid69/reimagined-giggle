8e336711ef6e84871556e57ed446f8acd3587c48
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:59:21 +0000

    breakup history odd frighten ticket

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-print([x**2 for x in range(10)])
+text = "Hello, world!"
+print("Characters:", len(text))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
