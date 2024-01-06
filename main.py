85de8304a9e9219e143eddcec4a2156472819f54
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:35:35 +0000

    long tour brain pollution really

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_bytes(array):
+        return bytes(array)
