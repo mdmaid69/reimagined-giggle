1c820e5a7f9992cb323c4c32868fc5af95c51586
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:00:54 +0000

    speech block food fire surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_error_function(x):
+        return math.erf(x)
