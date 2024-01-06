9220ed29aa14fc1ddd88bb4ffea58b60f7326f85
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:10:26 +0000

    cry close wire sit talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
