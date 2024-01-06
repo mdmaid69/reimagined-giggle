ae4fc992bbc9c3a0f2ebd77be7424f5d3d938dec
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:02:47 +0000

    goaway we turnaway bunch solve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(lst):
-        return max(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
