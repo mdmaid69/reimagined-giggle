dbcceaf043582527c41f83780ed7674c6fd5f9ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:21 +0000

    tip gift mouth gap giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(5):
-        print(i)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
