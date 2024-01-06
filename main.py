944bea98799b4b67c6e27ab16b405e04f7abbe08
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:39:13 +0000

    acceptable sad translate safety mean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
