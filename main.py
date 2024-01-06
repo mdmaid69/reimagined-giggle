05c07ade418b3a74f9b5ae8b63b6b520d5fa2185
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:29:47 +0000

    whom object pity space physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def reverse_array(array):
+        array.reverse()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
