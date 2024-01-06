68d7b6095a0b28f8d43443d73d16832841699d26
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:23:13 +0000

    exit Tt ordinary goodbye sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
