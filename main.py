86805277f0c08c6b667ea3c6c26696054eb39026
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:03:48 +0000

    pretty 571enough which pronunciation stamp

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
