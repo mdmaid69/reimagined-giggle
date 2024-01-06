0c214862189dca0ed709484c537deecb9af2305a
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:26:05 +0000

    later trip cope sound friend

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
