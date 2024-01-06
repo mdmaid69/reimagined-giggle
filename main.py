2f3841c2da40f72d388885d50dde54a65074b873
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:26:07 +0000

    lunch division afraid base if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
