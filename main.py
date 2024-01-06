32ec8cafa0d71d416bd1861807ecaaa331ebb761
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:17:30 +0000

    rent difficult turndown team strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
