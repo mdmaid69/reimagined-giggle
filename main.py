ede01baf5eb3ba619aa26f50e317b2f708f006ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:58 +0000

    part birth rely fork children

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
