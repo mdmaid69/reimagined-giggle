d10297487d432f8b4b9e188f31b1d37f628b45c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:52:42 +0000

    vision shout bright callup delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_dict():
+        return collections.UserDict()
