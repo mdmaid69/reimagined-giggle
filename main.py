93bfb92ff4f6909eccc6080dcf8af6377be3b4e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:05 +0000

    instead negative object turninto class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
