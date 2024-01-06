a5e992939716105d254247536283786cf0c99e3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:30:16 +0000

    pay score problem machine naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
