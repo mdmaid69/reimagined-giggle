447e8716f5bb3f6c279fa6e93d251ef3bb75d94a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:05:03 +0000

    shelter success beautiful murder pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_string_from_array(array):
+        return array.tobytes()
