1e0ddce97eaa75198083f6dbb6630f5963358ef2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:52:30 +0000

    dry affect 179bite too bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
+import sys
+def print_python_version():
+        print(sys.version)
