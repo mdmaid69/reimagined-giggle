82308c80cfc665ed84a6a6384880811b071a65ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:29:33 +0000

    hard bank crowd cruel live

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
