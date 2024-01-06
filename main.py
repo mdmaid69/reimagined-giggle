0a50f20b10a9594fb838bb302cc458bd991cd8a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:34:35 +0000

    lock related mouth buy plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
