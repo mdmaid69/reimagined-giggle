e17da0d38488938a0cc61b40d22002e290d6faba
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:45:01 +0000

    one tall moment lead hungry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
