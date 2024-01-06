cdafb0355f839b59198d9ed0c01d7f2dcef26175
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:46:46 +0000

    measure worry example cost cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
