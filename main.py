5bb6600bfe5b8d47d9635d3feb53ad92b18b946d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:27:14 +0000

    face hurry to cook poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-print(math.pi)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
