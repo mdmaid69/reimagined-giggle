9aab7998b2351b5a49d8335491801a6f4b419f37
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:59 +0000

    pay furniture use read average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
