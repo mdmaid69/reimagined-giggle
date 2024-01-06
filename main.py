e7136a8354b0239b84944f2d8964308dd79fede8
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:49:20 +0000

    hope escape no notice free

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
