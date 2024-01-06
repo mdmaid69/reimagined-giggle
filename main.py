c214fb0b1e10504e5c5c5f4d9a19bdfd520994a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:58:36 +0000

    country or stupid suggest sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
