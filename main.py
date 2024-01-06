333d7cd0a62d36a2472bbb40b1500e62564bbd54
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:50:45 +0000

    love guilty represent trip rest

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  def add_numbers(x, y):
-        return x + y
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
