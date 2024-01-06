17a6983520d8098af6bd9d21c6410a6b52f3c7eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:27:29 +0000

    maybe goodbye needle centre could

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import sys
+def print_python_version():
+        return sys.version
