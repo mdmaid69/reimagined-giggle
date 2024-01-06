8379425ad93a7636dd60cfc07edc4b6ece325efe
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:29:01 +0000

    contrast correct crash scared breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
