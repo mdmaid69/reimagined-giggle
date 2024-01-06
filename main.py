cfd928763b17d8e83470f1a78d800405459c73e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:23:06 +0000

    stir afternoon water Cc recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import sys
+def exit_program():
+        sys.exit()
