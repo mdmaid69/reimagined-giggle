30b3c4ba75e176b39f01ef66a4bd677b1210f9d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:50:28 +0000

    company treat sport surprising through

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def count_elements(lst):
-        return len(lst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
