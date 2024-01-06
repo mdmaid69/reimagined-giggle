13ba75fb6c87402bdc37063543941fe823a8ef58
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:05:23 +0000

    performance squeeze whistle row underneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
