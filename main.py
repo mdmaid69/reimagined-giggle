355828b95cfb3be9781f63f88a0f5022a73a42fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:07:21 +0000

    soak stare damp mouth mind

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_hex(n):
-        return hex(n)
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
