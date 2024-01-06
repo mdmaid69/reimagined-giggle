145837df853d0c809e1418e7c14e4673d21cd47d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:57:19 +0000

    keep split tap stamp expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
