1c4801846ad75bb8aa6c41bf3fe549fd24b1d558
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:58:32 +0000

    tap break yes Qq here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import sys
+print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
