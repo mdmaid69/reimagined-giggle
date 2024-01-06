0135db5fb9f20f08d77f267c9c2fbff67d30f463
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:49:36 +0000

    most platform reaction bite even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
