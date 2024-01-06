d2fb73381d632a87534f97a9aaa69fca80b60dbc
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:08:56 +0000

    call tape any rich unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
