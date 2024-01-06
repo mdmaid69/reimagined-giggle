3cd18ddfb149f4606b0722c1354783191bc9e9f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:56:50 +0000

    borrow upwards lock this mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
