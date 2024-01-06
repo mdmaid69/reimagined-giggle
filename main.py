1db5633a34b4aaabf618cd03451372e594da524b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:52:06 +0000

    boring wear among beat cover

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
