18fb62575455f7a98f812279cf17e9e2832e079d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:59:51 +0000

    print Ww account today passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
