3c5d6c9a1c9d62359c2e795ed13ba5d546b0c02e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:57:02 +0000

    hers middle turnout thinkof row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
