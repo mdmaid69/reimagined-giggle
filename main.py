f4e47d5f27ea1e4172173f88073566159d97de05
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:03:21 +0000

    suggest hard holdout furniture ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
