f921d24360f53e926cc8b346f7ad2a8ef3e18fea
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:34:01 +0000

    write certain emphasis around cum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
