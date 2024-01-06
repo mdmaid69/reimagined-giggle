6b4ebcadd265b4733ba99169fd3dac7f4a4f1ddf
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:34:17 +0000

    Ii while description splash please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
