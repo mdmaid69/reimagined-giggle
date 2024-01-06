3fddf62974c848734824a7b4cd7b8585d1fe6f5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:14:07 +0000

    Mm affect possession normal yet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
