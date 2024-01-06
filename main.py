b7763bfeb798da4048c7a0a7ddf733914e2261f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:30:22 +0000

    also lead comment backwards show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
