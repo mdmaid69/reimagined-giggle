442a01521796dda4d301bce17e601a82116e7e69
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:07:11 +0000

    doubt whisper continue contents week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
