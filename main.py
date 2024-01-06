f716c5e31026ccf0323ecd8c990a2e043d4f2e2b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:28:02 +0000

    phone production forget stone unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import sys
-def exit_program():
-        sys.exit()
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
