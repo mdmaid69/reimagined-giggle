c4377d0ecca5f22d88a30af5aaa76102b7fcd98b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:01:04 +0000

    last home towards off even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
