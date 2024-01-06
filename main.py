0d0fe37d63cd58f3b103b26fa087c968579bb8fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:54:13 +0000

    before scrap left comment increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
