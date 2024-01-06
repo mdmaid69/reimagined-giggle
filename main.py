b8e6a2257e32fbbf89180c616a9dbb5d5cb04a27
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:31:51 +0000

    suit noone naturally closely present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+print([x**2 for x in range(10)])
