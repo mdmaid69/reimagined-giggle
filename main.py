ceebfc86a1f5203538b8cf13805bdd177f206781
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:47:48 +0000

    excuse supply damage bad our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def get_array_as_str(array):
+        return str(array)
