22be37ea90ecf21552d767851a86e3f55048152f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:58:41 +0000

    mine goon out lend Itself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
