6440fd6c92a77cd8db4e6fbee378022e1ab93c15
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:31:55 +0000

    polite gooff combination practice hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
