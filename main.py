811c705cd94d84bc9c38fb4dad5c397dc2590a60
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:23:31 +0000

    bus judge listen tune follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def convert_to_binary(n):
+        return bin(n)
