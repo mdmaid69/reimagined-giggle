b9171e54b02d93b73b8c88673c1d5c1e3dc89d5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:23:15 +0000

    scrape urgent runout see evening

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
