b9c954ebb3c9ce7298ca5b39286303be09974991
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:47:15 +0000

    match coffee cause Ii hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
