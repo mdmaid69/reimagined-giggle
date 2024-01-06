8f6cca37b1d0dc72ca78b8716a900c74584d1931
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:54:32 +0000

    fall tell breakthrough group whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
