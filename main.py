f979e002f9bcb509983404ccc6dc26d4ff242497
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:41:27 +0000

    strike market away lead getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
