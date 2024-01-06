d3b6ac0a709da648ba2a40a4622a0643b31211c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:18:25 +0000

    calmly switch similar preparation worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
