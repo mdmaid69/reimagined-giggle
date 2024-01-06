bf8768358994449641589478620a2480d16f1914
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:38:34 +0000

    storm pretend affect rarely repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
