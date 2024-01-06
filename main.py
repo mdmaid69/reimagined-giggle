6891910e4730866f232c332d8c2e1137a2f46ceb
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:09:34 +0000

    window listen contrast grand war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
