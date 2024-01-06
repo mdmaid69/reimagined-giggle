791954c8803a5b61284644ad8f8cd697740af928
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:16:03 +0000

    tidy swing little apart guilty

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print([x**2 for x in range(10)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
