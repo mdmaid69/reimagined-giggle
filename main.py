35d61f6cf60908f40c58042c2d8195f3ca9dff30
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:45 +0000

    modern funny shock child relief

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def reverse_string(s):
+        return s[::-1]
+for i in range(10): print(i)
