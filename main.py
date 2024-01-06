2d134b115819e2d47d56aeabc10d6573e58c8df6
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:22:02 +0000

    cheat zero among luck brain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
