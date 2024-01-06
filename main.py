40d5e51d3740c83dfce75ff7719b846084e51d2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:30:36 +0000

    dance decision steady produce entry

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
