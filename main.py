bec5d25a74630fb08edfae0d1e99845599c4f605
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:07:06 +0000

    takeout because notice protection left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
