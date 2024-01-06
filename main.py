8a49b95dfcd3dfb914a7559adb2485b12fc6d9a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:21:51 +0000

    reply after petrol roll band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
