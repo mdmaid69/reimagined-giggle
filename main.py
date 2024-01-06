0b884cf00d457daac38d1b961bb09bf8aa89afb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:16:20 +0000

    hold side fire human sky

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
