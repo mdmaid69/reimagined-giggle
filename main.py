9c91409dd3f327ea472b569ed3baf045614ae7b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:29:21 +0000

    cause Zz bye present nor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
