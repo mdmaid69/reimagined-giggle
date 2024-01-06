702ac6cd327a225e542e212f66975dd171882b2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:13:21 +0000

    normal place figure everybody great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
