471a4eb4853ce9335a6017ba81a0f8d6bc8f80e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:46 +0000

    afternoon backwards keep talk towards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
