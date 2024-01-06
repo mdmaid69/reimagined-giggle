844f1b0d965ae61dd137659a49d1b895d03b99af
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:02:12 +0000

    complicated shine or dinner move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
