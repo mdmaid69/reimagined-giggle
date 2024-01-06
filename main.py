c557ed078c135ecce28c7ce7a27b67cf2906eec0
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:06:24 +0000

    mistake let home birthday detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
