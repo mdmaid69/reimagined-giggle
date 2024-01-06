85121a2c6d42e418a89ca56ace301449af7630a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:45:32 +0000

    opportunity act late surface prevent

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def append_to_array(array, item):
-        array.append(item)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
