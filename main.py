3afedd3cc17fe15ba59e80ab6ab53ea98ac56db6
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:56:07 +0000

    contribute muscle sing shut supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
