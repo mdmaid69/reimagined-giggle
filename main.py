bfc15c9b725530d2b3bfe5e877de37ed5d7d68df
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:54:33 +0000

    wait hollow other thirsty beside

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
