2011acc2e2316eac9c416be50af8e0a4226f97a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:03:01 +0000

    close type care refuse near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
