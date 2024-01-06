17803ab8a538889d35ad1d124cb1c8bcfea74fc7
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:28:27 +0000

    impression tip bath breathe age

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def reverse_list(lst):
+        return lst[::-1]
