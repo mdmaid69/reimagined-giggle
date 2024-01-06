42d548563235bc61e2335bf97d57a5dcd574760a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:35:05 +0000

    uponon comefrom bury worth blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_itemsize(array):
+        return array.itemsize
