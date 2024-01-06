0c319c1e9987343f492a1ca9d0fb8c3a914eb480
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:25:17 +0000

    succeed plate suit success awake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_area(radius):
+        return 3.14 * radius * radius
