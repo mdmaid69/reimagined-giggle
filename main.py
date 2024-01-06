0c462e27e91254a713625395b3a4d640fd2079df
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:56:52 +0000

    border Tt film patient relation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import sys
-print(sys.version)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
