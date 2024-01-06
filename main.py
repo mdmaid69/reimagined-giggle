241feabbfa570ef9e4b264ea212fd64f53f66b7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:43:41 +0000

    important except task anybody maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
