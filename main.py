822ec6473e571039a471fe8e964295bf18468934
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:28:18 +0000

    shallow address sudden refuse face

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sys
-def exit_program():
-        sys.exit()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def reverse_array(array):
+        array.reverse()
