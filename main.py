299cefd50f2c786c5e90d186ce8383d4b7f71083
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:06 +0000

    Cc without fun blue getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
