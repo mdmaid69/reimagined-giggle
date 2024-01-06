fd097253bbe0a2505c43985ca805255b00184cbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:25:07 +0000

    discuss criticize share life score

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
