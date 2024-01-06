51c9ee8ccdd6a8bb0ded3b90a30ecbeadc2d233d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:50:35 +0000

    length slightly plastic sharp otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_queue():
+        return collections.deque()
