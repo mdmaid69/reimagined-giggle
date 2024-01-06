3b3914e1a8a14c00500ccb988a9f046b7b103f60
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:35:57 +0000

    view alone goup design stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
