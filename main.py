8dfaade1570436c766ae39f4cb44f30836ee08c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:10:18 +0000

    lay Uu team still suppose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_max(numbers):
-        return max(numbers)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
