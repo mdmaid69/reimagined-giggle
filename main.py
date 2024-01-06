18f87695af7a5bf80a954f61c21fa800eadb4c7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:32:18 +0000

    string several spring tour runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
