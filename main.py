ffb0d28ea2160f56c938bd0e85674262179d1763
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:32:39 +0000

    glass convinced tip last year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_index(array, item):
+        return array.index(item)
