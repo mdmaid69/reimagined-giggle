1bea77b149c0c4cfec20a8494198fb0e566a998a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:51:45 +0000

    jump list can accidental police

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
