d6db8eee971a401c6b947281e05b7a99c931be78
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:20:07 +0000

    kick date cover thumb feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
