aeac2a26f1c3da01be90629edb7cd1e3228aa1c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:48:39 +0000

    spoil hurt pour big tight

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import datetime
-print(datetime.datetime.now())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
