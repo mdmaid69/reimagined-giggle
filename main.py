10b8c70a6753a737a3d8d2556bda3b4c91f2dc37
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:42:36 +0000

    offend raise apart gate comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
