6c4b5295c91863257cab0998a0f52e8aa68f9ff1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:12:21 +0000

    callat noise will substance light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def divide_numbers(x, y):
+        return x / y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
