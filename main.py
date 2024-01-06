f6de881fcc2e6da8f120e09bad0a53180030512d
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:00:19 +0000

    clue peace Zz mark change

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
