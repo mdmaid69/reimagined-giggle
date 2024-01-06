e43cb9dca4d09587b828ff23bd2448ef76d02531
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:35:20 +0000

    injury slice collect place cheek

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
