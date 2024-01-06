837b31efeb61e5d5da8425b81999fef36ac136df
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:10:27 +0000

    handle respect against control feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
