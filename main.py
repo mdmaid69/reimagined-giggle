91713b58d343f7e2715d6784fc5a2dd5d472a3cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:27:18 +0000

    else perfect stage little gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
