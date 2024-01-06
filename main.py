397c6a91cc4c614a55e7162418f1cbc8e4a2d5d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:00:05 +0000

    workon phone first allow complete

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
