bc4c669326b039ad9aa1f413367ccc6720ed44b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:50:59 +0000

    reject negative image admire light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
