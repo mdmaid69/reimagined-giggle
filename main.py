e968d676a74622a846c3c3f51976c82aaf409a95
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:40:01 +0000

    maximum smile statement worst late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
