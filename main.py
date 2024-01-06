96eb9b17e9abcbda09dd1f7bf9c3dcd61e4f385c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:50:26 +0000

    many side scrap appeal serve

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
