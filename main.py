e1fb9c9a664b1636e75769543cc78a09aab00359
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:47:27 +0000

    hear decision lookup add together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
