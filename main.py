b7af8efcebe7083768767e6c4829df63543bbee0
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:17:36 +0000

    pupil mainly like habit deliberately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_as_list(array):
+        return list(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
