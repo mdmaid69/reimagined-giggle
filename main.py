8c5cb50d73df20b6c836ef7b38f2e91486ae8795
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:35:56 +0000

    explain guess Rr eye relationship

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def subtract_numbers(x, y):
+        return x - y
