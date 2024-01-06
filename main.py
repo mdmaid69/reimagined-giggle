1752dbb764d3a28445b768a18f4b331b256bee32
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:39:44 +0000

    passenger copy circle die perfect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
