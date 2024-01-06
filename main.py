540cbd2e4964f3610a8c298df718dd95f232c7f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:08 +0000

    difficulty except drop heat clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
