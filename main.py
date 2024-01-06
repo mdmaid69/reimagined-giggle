4d5238bc6205fa7d76e9c9da89f3abf6bbddfb8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:07:09 +0000

    certain flat sick otherwise later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-for i in range(5):
-        print(i)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
