6503497f3bfbca7d30bc20edbe6519a419790aee
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:27:11 +0000

    medium practical quality bring miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
