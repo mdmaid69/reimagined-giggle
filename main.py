d8344ed079872cd765fb6333a9804e97b14d4fcb
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:32:25 +0000

    neat pressure shake fire thinkof

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
