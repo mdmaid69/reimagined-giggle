6c21173f9e3335b37fcc18c27c4d6c9a572e52fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:53:09 +0000

    inform place exhibition connection spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
