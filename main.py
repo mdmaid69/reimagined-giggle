00b6af13fad34a260e6d43e0f43e40d0ef929838
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:43:17 +0000

    recover certainly ashamed until idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
