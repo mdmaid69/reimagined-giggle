93f6ff6a729a9ca4293aefd9ee725cc962191377
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:09:01 +0000

    protection height forgive noise rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
