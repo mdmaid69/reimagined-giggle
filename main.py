58f7df1f235372400cfe9de14ff359bc83e4028b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:45:53 +0000

    urgent gentle live company properly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_acceleration(speed, time):
-        return speed / time
+import tensorflow as tf
+print(tf.__version__)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
