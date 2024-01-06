95f1709812a9b71989f34beb04988032f6ea1b34
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:34:51 +0000

    poison decision nor suit ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
