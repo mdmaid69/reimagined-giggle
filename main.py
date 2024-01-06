dc824c7c0e67766e9476f6afb279e7295a0bf25b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:13 +0000

    greeting bet only no Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
