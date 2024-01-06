d85a2fcdd888dc36528d09d596c688c34df17276
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:09 +0000

    bet notice nearly belief powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
