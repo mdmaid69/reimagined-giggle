dec42e85c4b6568bbd989329946796667236ffaf
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:47:30 +0000

    vote legal variety everything sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-for i in range(5):
-        print(i)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
