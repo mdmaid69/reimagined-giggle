80ffc09619d354360be18258beda4fe3a50107ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:51:58 +0000

    little necessary slowly occasion remark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
