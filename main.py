79301a6ad96eefa3aa83384b895d28e251955d34
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:58:01 +0000

    shadow meal wish secret fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
