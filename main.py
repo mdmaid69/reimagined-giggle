d438346ce01d3068901ac73293640a265a2a26ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:42:22 +0000

    number must sink try return

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
