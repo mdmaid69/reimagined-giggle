b2eacf35115cc0cc958f0a703de70cf430b5916b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:44 +0000

    hang lie listenfor fortunately demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_list_from_array(array):
+        return array.tolist()
