01da7207ec58bde9f9297a76e131a71b690534f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:11:58 +0000

    reply achieve ticket late regret

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
