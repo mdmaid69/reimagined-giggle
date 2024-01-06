cd61957f8599c5b7d1a653c962c3e1e69195d29e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:37:26 +0000

    honest somebody used trade way

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
