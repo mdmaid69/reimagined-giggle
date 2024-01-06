c7a2113a448cb7703516a1de2adb9042d1493891
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:25:29 +0000

    communicate hope real measure wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
