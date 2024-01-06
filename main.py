45e31b0dff93adf5f33d4fed9b80918542f9be51
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:38:57 +0000

    happen normal meet Ff over

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def cube_number(x):
+        return x**3
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
