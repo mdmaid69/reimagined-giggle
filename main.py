883ae6372c9dedb2e64ea02ab2435a2ff0fd787d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:13:52 +0000

    unite safety last patience recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
