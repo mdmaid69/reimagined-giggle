bce7f9692206da66c1d5036d8debfca247334d6b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:46:27 +0000

    want often heavy thought copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_elements(lst):
-        return len(lst)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
