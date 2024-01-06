9cca29939670dbafb7219d54e21b59f6e025bdb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:58:00 +0000

    mention need keepoff birth yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
