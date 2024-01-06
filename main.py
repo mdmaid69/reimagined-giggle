a7e8fe145ee38b09c1a96161a5146ca5c6668332
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:13:41 +0000

    mad common trade choose false

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
