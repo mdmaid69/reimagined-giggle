8273cc22a9192c2b0b07d2721749ce4275495697
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:59:58 +0000

    waste begin card deliberately kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
