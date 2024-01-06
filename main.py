dd7c067021f56084d48edb22375e5246eabe54fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:23:41 +0000

    at train will burn confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
