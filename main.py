e18d36b7412d32d1872ed35545b80e2a783b14b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:46:57 +0000

    but Qq angry mail leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
