4c1a7bc520ad5342bee23172b9151844e36bc7a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:58:16 +0000

    slide future negative funny murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import platform
+def get_python_version():
+        return platform.python_version()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
