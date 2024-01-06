708a8e7958cc8c70317432df019fcb3b0fb8f7ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:28 +0000

    connection warn stretch bend interview

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def add_numbers(a, b):
+        return a + b
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
