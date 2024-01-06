7db60f361d4a33d246a85330e9561e49fabd8e34
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:15:44 +0000

    start part flow example nervous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
