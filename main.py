1cf5caae6bacff44f28e9632661423da32418598
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:59:58 +0000

    slow habit always freedom importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
