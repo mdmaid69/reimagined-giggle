2eba30f8f9ad5e6c34e74009d0b3af96968a8845
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:04 +0000

    both in knock dry frequent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
