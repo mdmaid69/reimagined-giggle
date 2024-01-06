be0f36536bc1a39d6ad37ef8e9006f346c096ca3
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:40:39 +0000

    university interruption scrap combination hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
