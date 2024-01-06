a79c47f60be4eaad059980372d9d71b1057eaf56
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:05:56 +0000

    bring upstairs the suffer pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
