21e7c7a674015c859d98e9429664c3a58fdfa5db
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:36 +0000

    dead professional protection keen month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
