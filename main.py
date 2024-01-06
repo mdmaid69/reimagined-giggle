877293f303bf1ebd896517039e75a247135a194a
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:19:54 +0000

    his wander concerned swing along

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def find_max(numbers):
-        return max(numbers)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
