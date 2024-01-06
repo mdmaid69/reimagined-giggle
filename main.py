4c1dda980fcf7de5a9936299ec02a275bc6f10cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:30:56 +0000

    colour constantly account clean deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
