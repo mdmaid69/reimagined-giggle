a0375471db38e81585dc1678b5484238e51fae26
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:10:50 +0000

    already exchange past picture private

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-print("Hello, world!")
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
