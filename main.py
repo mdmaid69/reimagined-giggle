5aa842fba9f7edfb9aca6796050e19a2344b7d6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:08:44 +0000

    bag believe place try work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
