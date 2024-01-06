10e521bb698676f5f8078e9a72a8a0d62e027b60
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:38:35 +0000

    satisfied embarrassing gradually 565end solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
