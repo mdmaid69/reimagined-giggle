f96a77b32a05a9caf63b9bdc09bc9bfb4baf79a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:06 +0000

    wall city wish glad pump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_string(s):
+        return s[::-1]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
