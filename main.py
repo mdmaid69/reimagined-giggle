23ffc1c358686c49616efa273f712977f916f099
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:43:14 +0000

    mouth almost work finish dark

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
