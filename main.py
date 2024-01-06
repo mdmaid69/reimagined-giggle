b12b57d67cc321bdc623cd84fe4fbeb744a74bc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:16:01 +0000

    interfere opposite content convenient autumn

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+print([x**2 for x in range(10)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
