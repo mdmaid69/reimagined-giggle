c8aa98832a0ececa74183b27238125adbf4305ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:51:51 +0000

    body sing boat confuse cheek

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
