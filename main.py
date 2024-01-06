ccadbea5f9e339d2707ca1d6e379c75b0640dcbe
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:29:24 +0000

    Vv sensible phone set base

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(numbers):
+        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
