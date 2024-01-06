5358b1733a9f3278c281ab751c776262f18b6f35
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:58:55 +0000

    finger trade fire usually Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-print(time.time())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+i = 0
+while i < 5:
+        print(i)
+        i += 1
