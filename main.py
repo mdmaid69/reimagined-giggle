d34c89a8ff671ba2539a7b79be2f5dd6a7b8f1ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:26:55 +0000

    lay 570enjoyable getback fear interest

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def count_characters(sentence):
+        return len(sentence)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
