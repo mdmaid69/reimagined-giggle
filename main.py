abedaef2e02e3197e03413a20894a073abb61f5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:00:21 +0000

    true agreement enthusiastic file curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
