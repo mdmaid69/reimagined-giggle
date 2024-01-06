e800bca84c7e1d33397c8d37302b5f93ebd8d268
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:05:43 +0000

    makeup above approve area typical

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
