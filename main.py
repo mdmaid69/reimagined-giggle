9308abf980cecbbb318569d33845d085bc5bf2eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:25:09 +0000

    doubt immediately along offer wish

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
