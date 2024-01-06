da8862db26dbe4bb46ce0972163171ad607af1c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:38 +0000

    usual rank dry clean himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
