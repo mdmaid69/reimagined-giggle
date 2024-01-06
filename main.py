6dd3be5b95687b7bfd7fa667476c6dbd8f5745e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:40:39 +0000

    sharp begin one surprise dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
