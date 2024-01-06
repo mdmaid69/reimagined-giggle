79b28ae050ece7fa086a1830b157e928f30199e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:53:44 +0000

    over Dd high pretty splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
