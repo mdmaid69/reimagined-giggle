8b5571a869ad9c99e0c26d1b7f7a20f268e3fea1
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:52:42 +0000

    have slide anyone concern safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
