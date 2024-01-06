c5b83c8898abeab7202500d3639a87c55bcb1764
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:04:22 +0000

    combine related laugh bound contrast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
