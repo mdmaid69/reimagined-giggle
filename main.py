903652b0f1ecde0af3e7deb9176a77b20585c994
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:50:00 +0000

    excited cool pump bringback alive

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
