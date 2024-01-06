6173c5ad8a4f85e10046f1e8998bbd0d6e240416
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:47:46 +0000

    giveout string harm moral swallow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def count_elements(lst):
+        return len(lst)
