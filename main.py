abc1b01655b06b59a443ebf2f7acba647f594da5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:44:08 +0000

    makeinto confident unpleasant offence different

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
