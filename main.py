94b6bc725712d70a604c23f4f941308d159e0bf7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:28 +0000

    evening smart pocket manner down

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_square_root(x):
+        return math.sqrt(x)
