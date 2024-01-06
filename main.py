c8e3821bd2ed29a8b7a852ee9542f89c05a88337
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:30:31 +0000

    detail coat rush whenever sleep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+x = 10
+y = 20
+print("Sum:", x + y)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
