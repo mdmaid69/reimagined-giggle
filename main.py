ca72bb279b9d5d5f5e049628070f342781af5cf6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:03:55 +0000

    ilovedicks breakfast angry event boat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def is_even(n):
+        return n % 2 == 0
