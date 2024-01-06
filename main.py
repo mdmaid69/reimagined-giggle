257dc014cbe8671d35397247c83d47a1df66fde2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:03:23 +0000

    aim aim rent rest leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def reverse_string(s):
+        return s[::-1]
