07165333fde681c28353678f3c463ca6c62dc868
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:30:47 +0000

    excuse your likely pause wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
