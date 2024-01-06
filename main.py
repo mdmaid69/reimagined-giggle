56e46ab753b10ecd349f00737b78406890931cd3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:34:41 +0000

    convincing have moral respect something

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_characters(sentence):
-        return len(sentence)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_acceleration(speed, time):
+        return speed / time
