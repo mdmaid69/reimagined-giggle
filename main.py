bc0083b5b089608f3787fdadc5bf531172496ddd
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:32:47 +0000

    star provide earth promise complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def find_min(numbers):
+        return min(numbers)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
