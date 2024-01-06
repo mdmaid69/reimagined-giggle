3b0c49000e2dc12cbdd02c130966a5295f7f52dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:11:22 +0000

    floor emphasis speak comment temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
