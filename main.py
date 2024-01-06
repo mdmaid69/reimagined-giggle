a0b9f513eca24c0190d1a0c8e51e9442684bb60d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:23:43 +0000

    turnon murder achieve contribute wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
