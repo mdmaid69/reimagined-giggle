4d42db13818623af24b1d10e8c7d05050cd24305
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:34:39 +0000

    bread now ball condition rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
