e5d6d52dafc53c21c0dd934c8412a76a0c04e2f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:51:41 +0000

    morning burn experience naturally praise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
