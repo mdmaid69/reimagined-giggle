b5e6d0a0e95564300efb37f5a5f6030d6953496d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:13:54 +0000

    cheque switch invitation whatever travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
