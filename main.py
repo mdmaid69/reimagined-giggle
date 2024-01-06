daf7c7da9e3bdbe50b7614ae02a49a19a81b34da
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:31:11 +0000

    afterwards opportunity matter mood form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
