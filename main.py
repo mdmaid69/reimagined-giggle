e07dd87c15614c491c946bddbedbbd6574162151
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:10:27 +0000

    laugh exchange shop Nn nervous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
