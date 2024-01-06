67d1f194c43f522123b6149bf651e568038505e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:36:36 +0000

    achievement responsible doubt near clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
