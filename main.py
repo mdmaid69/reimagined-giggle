4cd63eba0f59d44eeed5c447304378ef331b1ef9
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:16:17 +0000

    during forget classroom wander talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
