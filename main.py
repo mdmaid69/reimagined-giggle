95bbeb5dd87f59739037a0c3ff96cc591e324190
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:27:22 +0000

    recover have inside bet shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
