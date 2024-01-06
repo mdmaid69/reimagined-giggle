3b758700be2485bfd26d64f774035484293f670d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:37:58 +0000

    correction lot load relation to

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
