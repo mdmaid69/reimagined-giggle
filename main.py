115f41baebd7a8dc7458faa739070dfc88024b5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:15:14 +0000

    praise know hot holiday mark

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_length(array):
-        return len(array)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
