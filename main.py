81343f2c1e97e38d525f3ac9acd3527ceaef7c50
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:46:59 +0000

    lock argument takeout fortunately wear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
