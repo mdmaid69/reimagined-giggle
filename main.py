34475b218ec7a55c3440b4f32b2b347eeb6c01b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:34:18 +0000

    physical strike lend uniform total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
