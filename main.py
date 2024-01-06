e436fe27f7c1feee9a35fe47242a251b18cce261
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:26:25 +0000

    566end otherwise practical nor pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
