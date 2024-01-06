fec031a632a5250dce89c4722a078754a78fb93a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:51:46 +0000

    message weigh comein reply recommend

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def is_palindrome(s):
+        return s == s[::-1]
