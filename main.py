9876cf261719bf682fd1e9e18406b53de800690e
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:08:17 +0000

    project moral scared produce thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+for i in range(5):
+        print(i)
+import time
+def get_current_time():
+        return time.time()
