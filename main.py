2c12f032a91a65032a01ba457144fad90c937fd9
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:36:23 +0000

    morning bread turndown raise music

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
