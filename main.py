0f48c642271865967851a697e90fe996f4a2021e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:51 +0000

    free sport small club float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
