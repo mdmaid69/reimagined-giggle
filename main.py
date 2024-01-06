a6c2aed46f017b50cfb08c70eb77985744f6ee58
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:36:12 +0000

    free never official project traffic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
+def reverse_list(lst):
+        return lst[::-1]
