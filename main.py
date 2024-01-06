d175dc265b596dade53e44f40c5b941149005856
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:14:06 +0000

    nigga wait connected flow rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
