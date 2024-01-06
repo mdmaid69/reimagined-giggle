623f6efc3dc152a74e51f1b25421dc31ccc5c71a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:26:31 +0000

    production temperature cheek giveup welcome

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def remove_duplicates(lst):
+        return list(set(lst))
