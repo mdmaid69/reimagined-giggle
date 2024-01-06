9647af3f1be7c07cce7882ecf05dba99586a38af
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:00:15 +0000

    petrol later basically entry idea

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def count_elements(lst):
-        return len(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
