d6c129bfc5b266e96db13b42a952790548fa03b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:25:51 +0000

    strange OKorokay to contribute comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
