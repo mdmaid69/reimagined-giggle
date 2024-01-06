779d2ab25fe2cd6edb96e84cbc09f68f24da3d92
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:26:43 +0000

    key rain use pleased direct

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
