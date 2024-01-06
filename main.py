c2acd27e669b611f443550168e0e3266dfe22458
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:17:07 +0000

    criticize region surface bank instant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
