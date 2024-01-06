baf77ade059b1b87ebef1e3038662c9293516d85
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:53:48 +0000

    Ff naturally various practice various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
