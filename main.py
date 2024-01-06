4c2b043292dade2d163737ec9a502c13ff25df35
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:18:57 +0000

    sleep discussion undo quantity interview

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
