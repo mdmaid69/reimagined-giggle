f6209dd299d176a108d604533d696a2ed9c18c8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:42:11 +0000

    Cc bring free maintain display

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
