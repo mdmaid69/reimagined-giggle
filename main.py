d50b8dfbf7e4312563a5628c9a44ba3689213263
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:12:26 +0000

    government will exciting win mess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
