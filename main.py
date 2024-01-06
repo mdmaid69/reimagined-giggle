cecbb200dc59083c1004b564afd0999563f89fbe
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:13:10 +0000

    pollution beautiful thanks mouth certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import time
+def get_time_since_epoch():
+        return time.time()
