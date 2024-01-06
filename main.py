7d70cd3ce0969c103401365316f2db8473db16b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:01:14 +0000

    shut challenge finish contribute flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
