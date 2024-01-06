d927d6aa9020473e9bba2b785c76977e4db73b9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:56:36 +0000

    scream variety more there takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
