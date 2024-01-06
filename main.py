7c1f69b1e5cf032d652560cb4020d8cd2832d8be
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:54:51 +0000

    kind live glass criticize raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def sort_list(lst):
-        return sorted(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
