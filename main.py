63f6e3b3fcc0d5ca329ab578fd3705262def2413
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:39:27 +0000

    bus unit necessary book against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
