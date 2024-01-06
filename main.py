354d09e66115f5c7c2bfe5f612f5bbfe29aa1485
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:07:48 +0000

    pin public hard trouble wind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
