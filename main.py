6231e1e04aca3e6c4b741287a0622f8ab82fd4a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:27:46 +0000

    surprise myself burst work sure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
