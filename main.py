2ed91e9ab77b01987be1763f34199724de8956f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:06:36 +0000

    bit unlucky all well exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
