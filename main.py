654a56b5531757bc332fe1540699cf170569c56b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:22 +0000

    world importance above total cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
