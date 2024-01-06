582c479f3c3439e996edc547ee474439bf6bd2c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:41:58 +0000

    communicate object mess straight wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
