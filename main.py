dfe6e5062cdc215cb6989161765434c19ba9fb58
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:05:29 +0000

    curious pretend worth lead sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+x = 10
+y = 20
+print("Sum:", x + y)
