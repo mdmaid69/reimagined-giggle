98ac4300b9537031d3253e8c07ea51fea3a25665
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:29:24 +0000

    wet hurt month quit fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
