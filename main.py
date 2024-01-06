66b62e079f78ecd2d4fbb5dd48f7aab3fc6c3832
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:29:11 +0000

    clearly contrast immediate target safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
