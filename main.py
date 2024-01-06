b1310d408d3d5c37c948ff680a37c9e4025d6bca
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:53:00 +0000

    perhaps mystery will disturbing unable

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
