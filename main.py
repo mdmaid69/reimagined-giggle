077121cf0b5ed39e8f09247435a2459b7be3a610
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:38:35 +0000

    finally afternoon alive shrink air

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-print("Hello, world!")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
