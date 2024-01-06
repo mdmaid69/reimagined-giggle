bfb308784cd30ce2a3c4bf278e9d938c5143d060
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:54:56 +0000

    ourselves classroom disappointed worse decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
