64386792a5e088e97470fb38807113bc5a7d9069
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:40:23 +0000

    consist beginning know turnaway getaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_speed(distance, time):
+        return distance / time
