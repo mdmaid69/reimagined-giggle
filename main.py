85a80edab5234ff9b367ba265e6088e0fa2e22d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:04:23 +0000

    compete yet scared comefrom before

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def square_number(x):
-        return x**2
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
