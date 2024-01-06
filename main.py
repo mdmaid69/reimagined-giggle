c31cff2b8317fd9902c4659cdc13204dde61117d
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:41:41 +0000

    cheat although contact continue violence

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def is_even(n):
-        return n % 2 == 0
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
