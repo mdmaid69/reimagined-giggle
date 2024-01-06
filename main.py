3181fed909c17d68fbf3a34de4c2446e0db68362
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:45:31 +0000

    track light human welcome undo

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_speed(distance, time):
+        return distance / time
