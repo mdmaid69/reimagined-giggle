ec17aa9eb6e49d0f8abcf2dec2f5b6c5273d58bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:39:20 +0000

    mad limit deal nose twist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import sys
-def exit_program():
-        sys.exit()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
