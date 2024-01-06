ca38a8976b3c612c7f6a29cac1b87f397b80dec5
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:34:49 +0000

    suppose left ashamed goup act

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
