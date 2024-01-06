09bf510ceec7a548131f5aff516991df80683425
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:39:59 +0000

    present used pen ago themselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
