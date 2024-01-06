bc2b1dcb6312874e3e5539361909971bb0becc2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:17:48 +0000

    embarrassing bring neither rent drawing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def square_number(x):
+        return x**2
