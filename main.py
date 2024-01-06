390f54eada4c16fd7316e5f8ad6714eb05164eb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:26:52 +0000

    object sense of shake secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
