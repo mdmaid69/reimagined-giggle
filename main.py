6a430b59a543aaa5f459bc7b02974d000b14e9a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:00:26 +0000

    fierce write guess conduct well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
