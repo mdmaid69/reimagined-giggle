108313066a549a0b8dfba5e2575ec27ac5e3e1fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:55:45 +0000

    godown shock interview mark custom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
