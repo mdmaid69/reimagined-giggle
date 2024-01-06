529b83c181466c2063faaaeb65cac785cf0e8fec
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:59:04 +0000

    between ring callback strength song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
