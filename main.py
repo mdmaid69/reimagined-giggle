ff5255b7b8f879bab5485722470057af8c2b57eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:31:47 +0000

    the wind classroom breakinto smart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
