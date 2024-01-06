bf8f89dcec3272752619d872902a6f8524a41673
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:57 +0000

    disappointed goodbye her news duty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def change_working_directory(path):
+        os.chdir(path)
