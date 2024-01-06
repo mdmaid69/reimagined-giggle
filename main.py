c765b3fca337f02a3f652961969be3c079b9bc80
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:12:31 +0000

    defeat declaration school complain generally

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
