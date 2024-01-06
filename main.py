29f594997a5b6daa951b489ff3cf5cfe018a6ea0
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:21:54 +0000

    love healthy pump record tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
