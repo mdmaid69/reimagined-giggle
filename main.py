3424e9f85b0a53d08bb4da0892231643bdf69e3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:45:08 +0000

    mix or kick scheme object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
+def is_even(n):
+        return n % 2 == 0
