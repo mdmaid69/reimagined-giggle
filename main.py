fd9e2b69d847aa55449d1525e7a6a0a528f4a3ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:01 +0000

    hurt bright confuse call usual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
