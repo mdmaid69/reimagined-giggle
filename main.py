32e0a16970e422e1d2863a51984748841e46690e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:45:01 +0000

    some concentrate noisy emphasize likely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
