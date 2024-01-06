831bee32d1bec21effeb99ad70d5711669ef63cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:02:37 +0000

    plate than Nn both score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
