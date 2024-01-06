00d0acc057ff5562dd57349b43aecfa79d7cdda1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:14:14 +0000

    talkoutof fasten loss spite science

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
