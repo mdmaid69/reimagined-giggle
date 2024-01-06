6672382becaf5586335cfde67a82e994804817fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:02:50 +0000

    both money authority present us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import time
+def get_current_time():
+        return time.time()
