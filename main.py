c04ae283822712aa8f7a4f677f86b854c7413427
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:27:10 +0000

    fear habit godown record keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
