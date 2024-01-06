8fc55caf1336971ad530d50b0d51830bf720ec55
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:47:51 +0000

    split comein suit influence cover

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
