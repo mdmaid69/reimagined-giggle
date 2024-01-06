9713d2352d14c0bdb28303ae0efc1e412b73bb59
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:45:55 +0000

    drawing secret apologizeapologise past throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import sys
+print(sys.version)
