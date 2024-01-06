97bde754c181ba259207bc1abc88756ff7ef2119
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:06:35 +0000

    reject taste game rather where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
