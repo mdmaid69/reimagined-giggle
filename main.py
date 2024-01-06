5d1fce95f9ba05cfd639dbad938ace174e46e45a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:16:04 +0000

    kill confident comein disappointed whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
