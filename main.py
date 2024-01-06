8ebccedda9884dac52ccba013630896a27c40e84
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:38:02 +0000

    rest compete effort crash comment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def find_union(list1, list2):
+        return set(list1) | set(list2)
