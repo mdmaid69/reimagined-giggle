59f7c58f63e7bf36d9e4b518f49119f0d6b9abf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:52 +0000

    winter collect year bottom silent

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import os
+def remove_directory(path):
+        os.rmdir(path)
