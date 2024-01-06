a824046f079390b30bbfed1dbe61c24387fe1417
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:40:55 +0000

    exciting radio blind substance whom

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
