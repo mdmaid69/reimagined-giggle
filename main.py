294670c08c6b2c4fd576bf3021f15335413e9861
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:36:02 +0000

    previous frighten brother exchange maintain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Characters:", len(text))
