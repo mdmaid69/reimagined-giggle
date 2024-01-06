64a5391a3b30351c814c7c5b85d9b98c95a1bdda
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:37:59 +0000

    compare home force achieve communicate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
 def calculate_speed(distance, time):
         return distance / time
-  def sort_list(lst):
-        return sorted(lst)
