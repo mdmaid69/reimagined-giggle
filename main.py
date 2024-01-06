4fa18ba13dcfc1c0203278664ef02e0408f149f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:54:10 +0000

    round threaten forever vision influence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
