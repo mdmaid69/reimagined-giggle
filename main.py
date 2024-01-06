b7a9683ac03a797a7378a3d09355deaec701d021
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:23:38 +0000

    slow thorough design it clue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
