f3980c8a2646bfe2869806a01b5c2d63eaa53ae4
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:31 +0000

    wire begin sense congratulations mistake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
