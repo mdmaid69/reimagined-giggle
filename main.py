f4165d73cc2c5c1110717cd3eff9329cdf2424c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:54:02 +0000

    runinto likely like open national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
