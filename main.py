2c3b0e501effa6f08ae3957144cae0c642213747
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:17:39 +0000

    light safety confident shopping begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
