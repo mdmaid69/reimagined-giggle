47e080e2b7fa529444dd1844f369a860e9b2753d
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:55 +0000

    cap attract ilovedicks national important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
