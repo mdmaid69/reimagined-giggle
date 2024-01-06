df2452d2ee1789eed4c11177411ad60c96274cb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:51 +0000

    gather contents move reduction refer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
