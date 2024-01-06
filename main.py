abcbae9df4cd5fbfc022ae79116901155fdbc23f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:20:50 +0000

    giveback comfortable where not takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
