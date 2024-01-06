7405f2d03d26298246ffe4ddf0d2635e025f4142
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:36 +0000

    real group soil pull would

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
