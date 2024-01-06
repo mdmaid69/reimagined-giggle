22b9bfcbac74e136b809726e0e95de2c553c242f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:53:48 +0000

    say wave let behind lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
