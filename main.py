8ffc03892e3bcfbcf5f1f80df4a51cebc6ae597f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:18:06 +0000

    way fasten stop Kk weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
