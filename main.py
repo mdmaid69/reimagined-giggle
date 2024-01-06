7a6612ee76d5b8ad5aac7cf6eba8d85b9577f6be
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:31:15 +0000

    neither shake OKorokay run suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+print(os.getcwd())
