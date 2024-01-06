18c8fb8aaa4c350401f991c90fbf8e2d3a0af9ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:51:38 +0000

    trust glad communication activity pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
