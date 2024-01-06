822dd7dc5eb1031db525da9f933246c32cf2613a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:27:16 +0000

    distant forever need instead serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
