f77dc2cb30f4368f354c8ac95c796a2e3549a00f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:38:19 +0000

    wave tradition murder meeting maybe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
