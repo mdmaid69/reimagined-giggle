b2beb4a44317984873214582caeb075f62481c0c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:02:14 +0000

    port then speech song fierce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def cube_number(x):
-        return x**3
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_itemsize(array):
+        return array.itemsize
