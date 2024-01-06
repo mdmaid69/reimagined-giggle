90f88679dc98a829cc1d4d4aac0630a0ed912db2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:16:12 +0000

    grow glass free computer away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
