9317d78aed686616a78bbe3580156d32e12318f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:58:30 +0000

    badly level see mine getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
