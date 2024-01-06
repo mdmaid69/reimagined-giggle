8e2e7da34c987f29b34f7d091742fbc0f61d539f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:22:23 +0000

    whether actually instead anywhere list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def convert_to_octal(n):
-        return oct(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
