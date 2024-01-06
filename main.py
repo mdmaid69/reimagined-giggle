8d06d1d8e994f6c2bb5d21f57c0dfd685e7a8e90
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:01:58 +0000

    plenty mouth sing Kk side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
