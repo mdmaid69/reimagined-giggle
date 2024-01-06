3eb7e49cf159a86ac9e35dbea4aa66a8b93f51e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:08:25 +0000

    spray decision keep how good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import re
-print(re.match("h.*o", "hello world"))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
