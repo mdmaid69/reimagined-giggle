8b3f53074a99671520796f411b00bf082c32cb25
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:35:25 +0000

    convenient measure charge regret show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+name = "Python"
+print("Hello,", name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
