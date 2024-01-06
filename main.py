74f0ef0669c051ef824ed0b280ca907a2fd9cc70
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:26:45 +0000

    succeed steer slowly convince stretch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def find_min(lst):
-        return min(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
