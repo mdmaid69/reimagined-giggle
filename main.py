bdea7305fcda840fba10a25a207080582a8a62be
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:59:51 +0000

    shout notice sudden rid pile

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Words:", len(text.split()))
