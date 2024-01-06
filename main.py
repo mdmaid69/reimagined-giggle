638e5500248a5b43d71146faefa1fd22deef48c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:18:55 +0000

    yesterday passage brush pair note

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def cube_number(x):
-        return x**3
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
