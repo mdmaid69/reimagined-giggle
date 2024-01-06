56830f2a6050dc857a72bc78e33c5c3f9fb244fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:25:01 +0000

    opposite improvement little shrink shade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_max(numbers):
-        return max(numbers)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
