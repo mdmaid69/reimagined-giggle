cc74a6854ee4b994bf8df208c11939d9cff7bf5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:14:24 +0000

    better fear design peaceful holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
