74b40189648f181384b8ee0c600220adac3770bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:03:08 +0000

    will deep clothes attempt power

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
