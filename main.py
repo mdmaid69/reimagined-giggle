59b63be346c4c02be80d5dc231e9b6d5e73bf677
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:59:49 +0000

    condition fond adjective unfortunate gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
