26cc58e836c8fdc9d943d56cbe5d31c276ec5e77
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:55:59 +0000

    often release male circumstance recognize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
