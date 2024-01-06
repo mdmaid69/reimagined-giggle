960bae25a7355887f5c5c88da7ffaf5ce7f23fb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:09:08 +0000

    sure goup anything central still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
