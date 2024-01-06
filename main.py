72854f9ea8aefdecf1989b15b5b5da51701de432
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:18:47 +0000

    over gentle prevent bad run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
