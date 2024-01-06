fcc08eb5df104d2888856a47aacc2f594cb618ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:27:39 +0000

    exact shopping phone date wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
