d6287d5dfb401b2d4fcd4e74dfce27ca2db4dbfb
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:43:29 +0000

    fine shape but tongue attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_current_directory():
+        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
