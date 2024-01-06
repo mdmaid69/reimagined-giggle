8d152e35adc8c85d12af122173e7866ac21cb6b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:48:09 +0000

    share now either part rush

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
