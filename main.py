eadf344ca0797ce3a69cd47ee2f51627ba51e972
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:49:23 +0000

    mark become unlikely declare production

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
