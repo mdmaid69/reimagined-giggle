84df1ae728091113e669a44f4e5125a0c56bc58c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:51:56 +0000

    ilovedicks bit little perform low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
