16bce18eb60734d3135db50f45c7e2d7f5a2fb89
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:29:16 +0000

    breakout head ours refer Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
