c66a570c1c6de6db52488ecdaa0d3379b7cc2aca
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:17:41 +0000

    fierce account race split flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
