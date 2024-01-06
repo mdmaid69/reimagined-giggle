d59c34fcc94c88fc1af1947829b6deb8ac86fde7
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:51:46 +0000

    taste ago cross perfect vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
