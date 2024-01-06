43cf194df295d3b4b19bcad08076d9488232a4aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:45:30 +0000

    full noise add share information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
