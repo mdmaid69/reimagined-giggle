68cf348ff810109bae62856bac713c0eae0b5535
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:41:07 +0000

    mess callup natural exact begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
