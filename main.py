21a6065f7032354a1681b6e44c75441000260ea3
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:27:33 +0000

    but special passage natural lead

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
