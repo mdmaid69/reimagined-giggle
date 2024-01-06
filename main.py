b414eea0169026b13ecaaad62f926d760e4dd9db
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:44:42 +0000

    question plan man remark weekend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
