6aa05a056a8d0e5b8a0b9a34a073c6c044e82aa8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:44 +0000

    find direction uncertain high Yy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_directory_name(path):
+        return os.path.dirname(path)
