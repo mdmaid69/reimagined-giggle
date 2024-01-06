d360d53077c70ca8699fa738960da741f9156b3a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:09 +0000

    closed worth war petrol thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
