836e92aa50d20d75c8db0e6e36add965225546c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:09:58 +0000

    book refer sleep offence shallow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
