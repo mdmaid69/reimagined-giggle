5e07c1a7b74eb1aaeb7863dd6b3f2bb02b637a73
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:48:28 +0000

    bridge afternoon success scrape course

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def sort_numbers(numbers):
-        return sorted(numbers)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
