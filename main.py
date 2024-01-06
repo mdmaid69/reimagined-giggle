5915030b339ab96a1023da1aa61d4cdd148d4380
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:42 +0000

    dance fun report cruelty mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
