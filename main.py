201d4733745a9118c2d06eeb7d693a136679cee5
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:46:54 +0000

    apparently fall drop seem our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_item_count(array, item):
+        return array.count(item)
