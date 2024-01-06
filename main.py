f980e16a20fd5654d7a8bad0d7351bb59e6206cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:46:34 +0000

    tune boil cause hurt trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
