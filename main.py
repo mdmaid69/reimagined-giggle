4059cbad55dbe611c25a70d03944e4d574751f98
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:17:51 +0000

    solve importance short knock will

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
