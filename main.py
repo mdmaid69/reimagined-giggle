9274d5b03d7f635b59bdfa5691c1cd41cdb3dcd6
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:00:44 +0000

    soak through until smell precise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def is_palindrome(s):
+        return s == s[::-1]
