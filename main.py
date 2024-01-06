b16c82e4c2015cc3f0795b297273fdeb6b3764d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:07:27 +0000

    grateful air finish earn aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
