1d00d9d80a4cab21eef1812c8cd4335bce8012fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:46:10 +0000

    seat dance woman attractive least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
