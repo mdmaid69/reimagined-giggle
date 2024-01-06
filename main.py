56aad9e7994bd10527b05ff57c20b87ffbac46d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:19:02 +0000

    let artificial experiment left murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
