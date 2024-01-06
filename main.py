3e43e13ce2ea60182e1aa62dde5a2eb1de3ead23
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:47:21 +0000

    interview release right wake worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
