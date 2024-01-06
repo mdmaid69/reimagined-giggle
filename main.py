79f863d582afa3a8738fc510a5812b0f0b938125
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:06:19 +0000

    correction feeling fork degree chance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def square_number(x):
+        return x**2
