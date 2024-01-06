aab84e3f6df900f4c3691ee45f0d28428007373c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:00:16 +0000

    foreign fold butter goodbye discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
