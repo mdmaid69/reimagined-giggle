e810e1ce2a603683a66ed48b94133a5c4a7f1c40
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:04 +0000

    spot swimming question very approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
