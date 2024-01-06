1e32ce2e7c037c4e88e257f4095444862a06e89f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:46 +0000

    comeround say mixture suggestion cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
