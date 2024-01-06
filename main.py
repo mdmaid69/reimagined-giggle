a2a88a8746a0aef87a2599b6c4bc07c5118a2e6b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:36 +0000

    physical tap content start cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def convert_to_hex(n):
+        return hex(n)
