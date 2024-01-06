b07dd591ab14205ab3fbaae991ae98c1ca1ef6d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:01:04 +0000

    scare convincing cause train underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
