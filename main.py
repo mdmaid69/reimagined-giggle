3b46f893cc2e9914b4354f180a54445ad1294c7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:19:10 +0000

    ashamed evening fat unfortunate skin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
