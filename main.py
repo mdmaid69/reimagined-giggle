e3d5a39fd09e707e2e25af431ac137de7b001e01
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:43:52 +0000

    sign left connected fence no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
