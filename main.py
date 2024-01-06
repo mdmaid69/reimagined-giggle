27d492198de720cbcd7678dd881af3034f9d7543
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:07:30 +0000

    whole unhappy consult final passenger

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
