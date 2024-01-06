7598201556df680139aa8b0663f4040db1ce8f9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:20:42 +0000

    field artificial operation strike then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_stack():
+        return collections.deque()
