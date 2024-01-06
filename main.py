49afac89f44f32daddcb360e081a687fdb257b16
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:22 +0000

    really fix level connected also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
