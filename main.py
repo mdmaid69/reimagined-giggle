995457ed85ced3357434deda85743c47ac215e92
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:49:00 +0000

    comefrom pool spread communicate remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
