e9aef83bb4a53b5f1e384d8a6df958026af2f3a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:33:50 +0000

    share tip closely interview environment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
