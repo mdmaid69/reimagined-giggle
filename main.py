09f95c14ebff13ae9b15599b4e5caf4cb721f137
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:40:47 +0000

    soon light confusing right they

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_min(numbers):
-        return min(numbers)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_pressure(force, area):
+        return force / area
