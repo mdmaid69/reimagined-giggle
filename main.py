99308f39bd5193309b2e462dc02032c2586b2c15
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:20:54 +0000

    each public perform tour average

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
