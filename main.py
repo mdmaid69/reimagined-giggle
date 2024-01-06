f4a2559f4d9009bdba185a05dbb6f7bb103d51ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:24:06 +0000

    unhappy basic satisfied stand dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import os
+print(os.getcwd())
