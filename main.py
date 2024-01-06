03ca0b9e471235f33e2404183220512daa201f89
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:13:37 +0000

    very transparent own cross involve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
