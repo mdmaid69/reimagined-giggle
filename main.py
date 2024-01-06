5843e971e81a9423d835c40e87b03391d03fe77c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:08:14 +0000

    no class sound mouth suit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
