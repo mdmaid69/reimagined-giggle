aa43d64c7baa0f94e375b0c677445f5e208ab726
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:50:21 +0000

    lend door disgusting soak design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
