805cdd6364780ee0871b38be74af0471e7794c80
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:27:48 +0000

    check mistake second crime whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
