abad0f8625dccfdd1629330bb94692e4a463f5c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:47 +0000

    formal steer change case passenger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import os
-print(os.getcwd())
+def calculate_speed(distance, time):
+        return distance / time
+def find_max(lst):
+        return max(lst)
