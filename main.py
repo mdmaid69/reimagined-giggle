39c6b986dc4d8ed4f6795d79157f3e797042ab2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:47:42 +0000

    about apply wait small wire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
