abf962c2f6987f6a4191f207e03ac484b61beab6
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:41 +0000

    anywayanyhow fasten until blue freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def is_even(n):
+        return n % 2 == 0
