984b86767271ac4ae43476e878a1087f4cf27893
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:37:43 +0000

    hold mood check great want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
