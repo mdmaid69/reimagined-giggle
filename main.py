ce6b27b63f16187dfaca2e4c9923468cb002902a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:09:03 +0000

    scare give total claim check

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
