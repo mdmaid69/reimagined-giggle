84d8d35265ba1cba1a2920c1410ea1d0d57fc358
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:38:46 +0000

    appearance sing badly to turnaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
