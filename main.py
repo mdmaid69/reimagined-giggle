6e221a94c47c328bdfa362a898759c5d2f722b73
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:28:29 +0000

    week twist crash plan promise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
