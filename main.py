e442c405bdd93529ce9886803c14586efb55e3ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:15:33 +0000

    everybody Ii badly large steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
