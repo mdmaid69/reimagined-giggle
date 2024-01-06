15bf6f65a3324bed9929dfe865555879860e55b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:22:36 +0000

    fix common last appear push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
