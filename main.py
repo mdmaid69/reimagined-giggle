fa85ee97e31975404b0f75c9b1b270e7ab41ce05
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:10:59 +0000

    problem flood listen organization married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
