f618a23aca5934b40d72112ee8f879197d780c68
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:45:36 +0000

    pollution waste can taste team

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
