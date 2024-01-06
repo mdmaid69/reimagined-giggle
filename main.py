28c968d7e5007aee731f966df587b7b740ca5977
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:11:25 +0000

    man prize answer unlucky control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
