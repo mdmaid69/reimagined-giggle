13aa6e5321a64c8069778718f90fa1a566e49bd7
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:29:50 +0000

    education purpose on medium worst

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_power(work, time):
+        return work / time
+def calculate_pressure(force, area):
+        return force / area
