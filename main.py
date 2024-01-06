6f31a5e4a4d995152391e4d5b84144ea5db56439
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:02:39 +0000

    immediate fold stamp off entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
