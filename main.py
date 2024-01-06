331fc0cc03f4248cff70153079eaba541e000711
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:53:09 +0000

    vote discover wear person trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+        return os.path.getsize(file_name)
