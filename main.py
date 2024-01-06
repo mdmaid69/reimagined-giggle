090361b014ed3d8155b10160c6e1070662a80190
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:19:09 +0000

    wide far round 567enemy label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_to_binary(n):
+        return bin(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
