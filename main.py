b8f77855d56cf7c030d14c3dbe2da069120c9b04
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:16:00 +0000

    aim either no production break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
