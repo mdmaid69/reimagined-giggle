57a9700f047b8d9ff1a106e89d3ef4412471a2ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:20:29 +0000

    suffer familiar determined plate turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def reverse_string(s):
+        return s[::-1]
