95ed2f2ba69b0afe298988fb8d6db4ed3018a7ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:20:57 +0000

    look fond future obtain occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
