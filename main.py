a1d6de22465fecd2386e119d847186d92b9f9f14
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:41:17 +0000

    watch modern friendly after cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
