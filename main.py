2665f87d62cd9257329d5e3e1f456804fc5a9482
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:26:22 +0000

    feed choose than during unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def set_array_item(array, i, item):
+        array[i] = item
