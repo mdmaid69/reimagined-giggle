8365558c39fe56882806a836374beeaf2159321e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:44:27 +0000

    bite tool slightly drink whether

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def divide_numbers(x, y):
-        return x / y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
