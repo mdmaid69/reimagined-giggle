475a615d7be0c982f8e645e9d11f07ee117fcb0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:47:44 +0000

    address guess increase bank breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
