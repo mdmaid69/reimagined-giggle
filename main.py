1507a030ac9aaeb7c0573184dd8c55f82fbf7695
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:22:54 +0000

    across though scream although talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+name = "Python"
+print("Hello,", name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
