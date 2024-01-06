ab2f44adba398ddca1f998033bc2ea4fe0a63d9e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:24:51 +0000

    full work swallow motion flavour

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
