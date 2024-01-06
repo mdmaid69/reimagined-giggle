351e76912498964ffe9e1af3d71721dad72cc0e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:37 +0000

    minor Jj opposite number fun

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
