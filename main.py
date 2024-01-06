6dc32ca8c441c36a265807957a028d24b797a5fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:17:39 +0000

    than scream not bin start

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+print([x**2 for x in range(10)])
