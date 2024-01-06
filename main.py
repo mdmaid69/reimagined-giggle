a791b1a4fc6b8482802fdb4548a1ac0ecff89d8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:28 +0000

    tread justice conclusion doubt rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_typecode(array):
+        return array.typecode
