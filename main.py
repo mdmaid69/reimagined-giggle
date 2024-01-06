02c48a51726b2b6dbd8a11f7929425e3cf29edf5
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:38:29 +0000

    train head boil surprise content

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
