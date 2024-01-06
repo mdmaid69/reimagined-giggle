8ce6443a85519656b1abb9b4a79def02c974d1c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:07:28 +0000

    breathe ignore think exhibition glass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_pe_ratio(price_per_share, eps):
         return price_per_share / eps
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_int(array):
+        return int(array[0])
