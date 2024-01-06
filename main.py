614412146fe5a17e11d8f13c0a6a4c7d7944df0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:59:41 +0000

    lunch centre clean knock content

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def sort_list(lst):
+        return sorted(lst)
