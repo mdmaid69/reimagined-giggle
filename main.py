4da39c74053ed2329bf72c8652a55e58eb4ac99a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:03:40 +0000

    later downstairs paper giveout 570enjoyable

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def reverse_string(s):
+        return s[::-1]
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
