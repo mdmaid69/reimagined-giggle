e11d678e75705d8d98cfa03e9fd7c722376f9f9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:58:47 +0000

    bound case read other mess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_list():
+        return collections.UserList()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
