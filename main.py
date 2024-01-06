f46b8027ad95b1de536f17cdb5a203980a816eaf
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:42:51 +0000

    plenty rub learn notice control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
+  import os
+  def get_current_directory():
         return os.getcwd()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
