20b119b359551373de284c5b5f585d0c8e0c4326
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:03:30 +0000

    cold nervous talkback pupil add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
