b0ab3c8520f4e841c51a738350b57c55a280de44
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:13:27 +0000

    block address size sweep tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
