fb7de16a5bdfb13418b792d8a3f0a343b4a6cd7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:14:23 +0000

    total swallow his out getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
