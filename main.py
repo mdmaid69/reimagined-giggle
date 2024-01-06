98df17bf9575be8257f177a9c407a6c472be2deb
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:54:06 +0000

    large base offend even bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
