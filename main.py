9d2bc9cf23f6f1520bee7305f4eef416a208a95f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:32:33 +0000

    raw already slight shallow see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
