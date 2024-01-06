bc6f88774e3e8469082b45bf529a6140569b3a6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:41:03 +0000

    to ability world life curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
