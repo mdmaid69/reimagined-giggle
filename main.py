c22dcad47f363235fd5a29d45238fc0aaa09e051
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:30:02 +0000

    kill extra involve shelter bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_volume(length, width, height):
+        return length * width * height
