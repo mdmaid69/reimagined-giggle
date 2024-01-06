cd359de80cc0850af37ba63c9bf17d3636dab224
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:40:16 +0000

    would wipe train whistle autumn

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def factorial(n):
-        if n == 0:
-        return 1
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
         else:
-        return n * factorial(n-1)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+        return -1
