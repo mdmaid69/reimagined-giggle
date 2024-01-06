5317d0d291808504d10b38efdb34c0dea35f9f24
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:00:00 +0000

    result Dd blue own fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
