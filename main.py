90677d3ce1318d63687742d2166b713f7ff54423
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:15:25 +0000

    capital tight determined bush personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
