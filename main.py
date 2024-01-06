037ccf2b3cea03399cdb78e16fa7756c17a79c12
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:47:10 +0000

    condition correct will somewhere worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
