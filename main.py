4a4096dc7565635374c8aabb59d4691558ffc762
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:53:20 +0000

    love noone completely give aim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
