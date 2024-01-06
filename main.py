0cc153658b4b083f92f2e4017d7c009be3ab3b22
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:00:53 +0000

    success regret fierce convincing original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
