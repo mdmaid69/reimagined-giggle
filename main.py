36a16803b9af556fd79b5f49d756b3ba16b97316
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:17:40 +0000

    practical freedom majority peace gap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_binary(n):
+        return bin(n)
