3e0db9bb2fbae86ffc8af042eed17d83fed0a275
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:38:29 +0000

    lead birth dish anything squeeze

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
