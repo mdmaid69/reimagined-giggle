fc4120b5a7fe37b6b66cdbfc0369262b69055a70
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:48:14 +0000

    during trial develop woman warn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_area_rectangle(l, w):
+        return l * w
