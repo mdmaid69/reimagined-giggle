390c381205e8bb9feb859f8df70614130e1e1415
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:09:32 +0000

    fit top fresh turnup doctor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
