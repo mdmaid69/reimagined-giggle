bfe7c2b3b40a165285e2976102172870adfbaf3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:21:55 +0000

    along variety thirsty physical better

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
