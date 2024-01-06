151d27f2f7b8aa12c01a939f009a24d584295706
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:34:32 +0000

    listenfor 564encourage mean ocean gradually

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
