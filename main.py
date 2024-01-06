d7ec23c8783616442d4ece24f8e26842f5e8b740
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:43:15 +0000

    by production thank scream original

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
