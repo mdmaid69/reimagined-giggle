9d88e2a47cc425b804af4593c5ee4d065aa146e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:45:43 +0000

    Ee opinion separate acceptable lay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
