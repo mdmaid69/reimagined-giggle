b0acd67ad5a3fcd9041e9a9f1223c8013772c560
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:39 +0000

    minor rely Ff hope shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
