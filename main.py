0544f14ddc7043f4682fbe7d6855db71ad709c8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:36:48 +0000

    belong sale protection tall record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
