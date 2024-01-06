a5ab4372e11c145a1f7ae827e9fb3f60ec064f16
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:11:54 +0000

    greeting window bottom base relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
