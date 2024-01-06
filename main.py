1e7638215130fac1461b63760f0967cde4ec1202
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:40:13 +0000

    otherwise chat smell bite failure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-print(time.time())
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
