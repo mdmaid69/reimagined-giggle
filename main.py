f90a3b1154d70846b68f3851d1e0778c77dd8efc
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:26:31 +0000

    bringback help please wheel circle

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
