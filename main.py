6e4566cbddb2203dedc429f8268a8e4635979827
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:32:19 +0000

    repeat male shut him nigga

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def reverse_list(lst):
+        return lst[::-1]
