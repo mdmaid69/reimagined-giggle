ec694223a7e7b5ea2d93fa8c1df4bca88049fba0
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:41:46 +0000

    side considering regional furniture exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
