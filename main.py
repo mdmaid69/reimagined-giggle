3c34de1394798bc38f216e488158c377cac95858
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:32:52 +0000

    much offend instrument behaviour return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
