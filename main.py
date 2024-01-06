f00777276c04313d1d275e60570004d1bd909638
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:39:07 +0000

    again nor means apparently sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
