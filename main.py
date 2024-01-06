cc01d0c500e79a9792a158b58742e36142030f95
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:09:35 +0000

    last equipment grade mother thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-print(datetime.datetime.now())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
