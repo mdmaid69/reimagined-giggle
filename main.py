4fa6d9af95e120dbeaea15e06a6b884c82202f2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:59:09 +0000

    holdout giveout us common maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
