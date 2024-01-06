5b85603cb6aabc353190e0b8f79d50c3b18fccc8
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:28:08 +0000

    organization praise dark release rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
