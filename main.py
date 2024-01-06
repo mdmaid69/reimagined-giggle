3fd700225b600b89419ad1a6ffbc629cd793a670
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:36:45 +0000

    afternoon positive tie suck chance

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
