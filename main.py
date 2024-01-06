3a442785f0016bf6e45c4d0d72d7112540daa706
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:08:56 +0000

    born beginning put ignore trip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import math
+print(math.pi)
