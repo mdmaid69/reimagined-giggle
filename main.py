71a8c32c1755c317488651afb2afb8c5d382e485
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:12:06 +0000

    needle aim smell flow positive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def add_numbers(x, y):
-        return x + y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
