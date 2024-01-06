5ea460ce491fb9c88385320979470ba82b848948
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:41:03 +0000

    trick where some except violence

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
