1505d3451164768c7409c64867a11aef99fb41c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:35:22 +0000

    spend laugh talk steal arm

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
