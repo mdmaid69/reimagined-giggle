bf9d946dfacad847ec1418c8b9bc843d3aa2f1a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:10:15 +0000

    raise by communication experiment lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
