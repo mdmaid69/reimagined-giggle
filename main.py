0a5ea36bd3cbc2f420a32ea57e5712236111991d
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:35:26 +0000

    settle contain 565end mind consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
