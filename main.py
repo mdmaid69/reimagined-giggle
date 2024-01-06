b9900719f342fff3fbda591b4a7a1c5760169dba
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:26:18 +0000

    lonely wire used book rule

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
