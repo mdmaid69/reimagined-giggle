e2982b6a14ee7520730b0e759afa5f7943df6a58
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:50 +0000

    form standard cry since achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def add_numbers(a, b):
+        return a + b
