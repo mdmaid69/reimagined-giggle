484fb149379a1c4e213773f2ab062def9989caea
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:17 +0000

    emergency day partner crowd common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
