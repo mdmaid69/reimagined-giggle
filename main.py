0eeb7e2051d1271b2c92efe34809e3ffdac2dee0
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:24:21 +0000

    pen check thorough figure basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
