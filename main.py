737d1d3c688dfc2cb83e70868ecb237e52a9c072
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:25:09 +0000

    class style takeoff play his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
