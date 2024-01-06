1301f1de63886395c9604e4911663728039d4934
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:34:57 +0000

    lower play defend contrast freedom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
