7781ec0a61b5cd842d440fc77d1f931c0264c32e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:12:41 +0000

    afternoon offer impressive hard plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def convert_to_octal(n):
+        return oct(n)
