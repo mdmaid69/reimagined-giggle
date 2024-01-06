810b11c61c6b35989b4ae3fba142812ac97f0ebd
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:47:14 +0000

    second eye blame low rate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
