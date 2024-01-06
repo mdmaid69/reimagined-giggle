3f62ecc0a4456aef56935815008f1621a8b75ea5
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:28:20 +0000

    hit exercise recovery outside name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
