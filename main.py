c39aacc5a48ba56063c54a6517871627bfb3d2cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:54:59 +0000

    nice rent there react smooth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
