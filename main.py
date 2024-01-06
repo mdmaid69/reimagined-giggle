08d8839c0f7dd2030381a10ecfa2c0804dc943b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:31:00 +0000

    mean store board now throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
