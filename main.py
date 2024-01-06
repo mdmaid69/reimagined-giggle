9c4edbdca7dbaabbfd388a70ef0ce6109d6e085a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:55:35 +0000

    into admire present follow funny

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import platform
-def get_os_info():
-        return platform.uname()
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
